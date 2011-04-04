23ea7c92098007c091055514c73a5231714cb873
melvinzhang@gmail.com <unknown>
2011-04-04 03:03:13 +0000

    AIs can be selected from the command line deck strength calculator, currently available AIs are MMAB (Minimax with alpha beta pruning) and RND (make random moves), default AI is MMAB

--- src/magic/DeckStrCal.java
+++ src/magic/DeckStrCal.java
@@ -16,39 +16,49 @@ import magic.model.MagicGame;
 import magic.model.MagicTournament;
 import magic.model.variable.MagicStaticLocalVariable;
 import magic.ui.GameController;
+import magic.ai.MagicAIImpl;
 
 public class DeckStrCal {
-	
-	public static void main(final String[] args) {
+
+    
+    public static void main(final String[] args) {
         int games = 10;
         int strength = 6;
         String deck1 = "";
         String deck2 = "";
+        MagicAIImpl ai1 = MagicAIImpl.DEFAULT;
+        MagicAIImpl ai2 = MagicAIImpl.DEFAULT;
 
         // Command line parsing.
         for (int i = 0; i < args.length; i += 2) {
-        	
-            if (args[i].equals("--games")) {
+            String curr = args[i];
+            String next = args[i+1];
+            if (curr.equals("--games")) {
                 try {
-                    games = Integer.parseInt(args[i+1]);
+                    games = Integer.parseInt(next);
                 } catch (final Exception ex) {
                     System.err.println("Error: number of games not an integer");
                     ex.printStackTrace();
                     System.exit(1);
                 }
-            } else if (args[i].equals("--strength")) {
+            } else if (curr.equals("--strength")) {
                 try {
-                    strength = Integer.parseInt(args[i+1]);
+                    strength = Integer.parseInt(next);
                 } catch (final Exception ex) {
                     System.err.println("Error: AI strength not an integer");
                     ex.printStackTrace();
                     System.exit(1);
                 }
-            } else if (args[i].equals("--deck1")) {
-                deck1 = args[i+1];
-            } else if (args[i].equals("--deck2")) {
-                deck2 = args[i+1];
-            } else {
+            } else if (curr.equals("--deck1")) {
+                deck1 = next;
+            } else if (curr.equals("--deck2")) {
+                deck2 = next;
+            } else if (curr.equals("--ai1")) {
+                ai1 = MagicAIImpl.valueOf(next);
+            } else if (curr.equals("--ai2")) {
+                ai2 = MagicAIImpl.valueOf(next);
+            }
+            else {
                 System.err.println("Error: unknown option");
                 System.exit(1);
             }
@@ -64,22 +74,22 @@ public class DeckStrCal {
             System.exit(1);
         }
 
-		// Load cards and cubes.
+        // Load cards and cubes.
         try {
-			CardDefinitions.getInstance().loadCardDefinitions();
-			CubeDefinitions.getInstance().loadCubeDefinitions();
-			KeywordDefinitions.getInstance().loadKeywordDefinitions();
-			TriggerDefinitions.addTriggers();
-			LocalVariableDefinitions.addLocalVariables();
-			ManaActivationDefinitions.addManaActivations();
-			PermanentActivationDefinitions.addPermanentActivations();
-			CardEventDefinitions.setCardEvents();
-			MagicStaticLocalVariable.initializeCardDefinitions();
-		} catch (final Exception ex) {
+            CardDefinitions.getInstance().loadCardDefinitions();
+            CubeDefinitions.getInstance().loadCubeDefinitions();
+            KeywordDefinitions.getInstance().loadKeywordDefinitions();
+            TriggerDefinitions.addTriggers();
+            LocalVariableDefinitions.addLocalVariables();
+            ManaActivationDefinitions.addManaActivations();
+            PermanentActivationDefinitions.addPermanentActivations();
+            CardEventDefinitions.setCardEvents();
+            MagicStaticLocalVariable.initializeCardDefinitions();
+        } catch (final Exception ex) {
             System.err.println("Error: unable to initialize game engine");
-			ex.printStackTrace();
+            ex.printStackTrace();
             System.exit(1);
-		}
+        }
 
         // Set number of games.
         final TournamentConfig config=new TournamentConfig();
@@ -89,10 +99,14 @@ public class DeckStrCal {
         final MagicTournament testTournament=new MagicTournament(config);
         testTournament.initialize();
         testTournament.setDifficulty(strength);
-      
+        
+        // Set the AI
+        testTournament.setAIs(ai1.getAI(), ai2.getAI());
+
         // Set the deck.
         DeckUtils.loadDeck(deck1, testTournament.getPlayer(0)); 
         DeckUtils.loadDeck(deck2, testTournament.getPlayer(1));
+
         
         System.out.println(
                  "#deck1" +
