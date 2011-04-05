8c8ce0f4f13ae075935eec87a499d9f5fec13e68
ubeefx <unknown>
2011-04-05 21:12:29 +0000

    Added card Barbed Battlegear.

--- src/magic/test/TestGameBuilder.java
+++ src/magic/test/TestGameBuilder.java
@@ -118,6 +118,7 @@ public class TestGameBuilder {
 		addToGraveyard(player,"Mogg Fanatic",2);
 		addToGraveyard(opponent,"Island",2);
 		addToHand(opponent,"Zephyr Sprite",3);
+		addToHand(player,"Barbed Battlegear",1);
 		addToHand(player,"Go for the Throat",1);
 		addToHand(player,"Cumber Stone",1);
 		addToHand(player,"Phyrexian Digester",1);
