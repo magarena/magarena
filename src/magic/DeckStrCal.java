26091b7405afe8506490663983eec39c9de1552b
melvinzhang@gmail.com <unknown>
2011-04-07 06:53:40 +0000

    improve error handling and provide some simple instructions on the arguments supported by DeckStrCal

--- src/magic/DeckStrCal.java
+++ src/magic/DeckStrCal.java
@@ -19,16 +19,17 @@ import magic.ui.GameController;
 import magic.ai.MagicAIImpl;
 
 public class DeckStrCal {
-   
-    public static void main(final String[] args) {
-        int games = 10;
-        int strength = 6;
-        String deck1 = "";
-        String deck2 = "";
-        MagicAIImpl ai1 = MagicAIImpl.DEFAULT;
-        MagicAIImpl ai2 = MagicAIImpl.DEFAULT;
+        
+    private static int games = 10;
+    private static int strength = 6;
+    private static String deck1 = "";
+    private static String deck2 = "";
+    private static MagicAIImpl ai1 = MagicAIImpl.DEFAULT;
+    private static MagicAIImpl ai2 = MagicAIImpl.DEFAULT;
 
-        // Command line parsing.
+    // Command line parsing.
+    private static boolean parseArguments(final String[] args) {
+        boolean validArgs = true;
         for (int i = 0; i < args.length; i += 2) {
             String curr = args[i];
             String next = args[i+1];
@@ -53,26 +54,46 @@ public class DeckStrCal {
             } else if (curr.equals("--deck2")) {
                 deck2 = next;
             } else if (curr.equals("--ai1")) {
-                ai1 = MagicAIImpl.valueOf(next);
+                try {
+                    ai1 = MagicAIImpl.valueOf(next);
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Error: " + next + " is not valid AI");
+                    validArgs = false;
+                }
             } else if (curr.equals("--ai2")) {
-                ai2 = MagicAIImpl.valueOf(next);
+                try {
+                    ai2 = MagicAIImpl.valueOf(next);
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Error: " + next + " is not valid AI");
+                    validArgs = false;
+                }
             }
             else {
-                System.err.println("Error: unknown option");
-                System.exit(1);
+                System.err.println("Error: unknown option " + curr);
+                validArgs = false;
             }
         }
-       
-        if (!(new File(deck1)).exists()) {
+      
+        if (deck1.length() == 0) {
+            System.err.println("Error: no file specified for deck 1");
+            validArgs = false;
+        } else if (!(new File(deck1)).exists()) {
             System.err.println("Error: file " + deck1 + " does not exist");
-            System.exit(1);
+            validArgs = false;
         }
         
-        if (!(new File(deck2)).exists()) {
+        if (deck2.length() == 0) {
+            System.err.println("Error: no file specified for deck 2");
+            validArgs = false;
+        } else if (!(new File(deck2)).exists()) {
             System.err.println("Error: file " + deck2 + " does not exist");
-            System.exit(1);
+            validArgs = false;
         }
 
+        return validArgs;
+    }
+
+    private static MagicTournament setupTournament() {
         // Load cards and cubes.
         try {
             CardDefinitions.getInstance().loadCardDefinitions();
@@ -106,6 +127,19 @@ public class DeckStrCal {
         DeckUtils.loadDeck(deck1, testTournament.getPlayer(0)); 
         DeckUtils.loadDeck(deck2, testTournament.getPlayer(1));
 
+        return testTournament;
+    }
+   
+    public static void main(final String[] args) {
+        if (!parseArguments(args)) {
+            System.err.println("Usage: java -jar <path to Magarena.jar/exe> magic.DeckStrCal --deck1 <.dec file> --deck2 <.dec file> [options]");
+            System.err.println("Options:");
+            System.err.println("--games <1-*> [number of games to play, default 10]");
+            System.err.println("--strength <1-8> [strength of default AI, default 6]");
+            System.exit(1);
+        }
+
+        MagicTournament testTournament = setupTournament();
         
         System.out.println(
                  "#deck1" +
