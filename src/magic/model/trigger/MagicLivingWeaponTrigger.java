3bfa412301760a9b9f311eaba3db2da8b406d7ad
melvin <unknown>
2011-05-21 17:31:09 +0800

    improved the readability and added comments

--- src/magic/model/trigger/MagicLivingWeaponTrigger.java
+++ src/magic/model/trigger/MagicLivingWeaponTrigger.java
@@ -8,26 +8,47 @@ import magic.model.action.MagicAttachEquipmentAction;
 import magic.model.action.MagicPlayTokenAction;
 import magic.model.event.MagicEvent;
 
+/**
+ * Trigger that occurs when a card with the living weapon mechanic comes into play
+ */
 public class MagicLivingWeaponTrigger extends MagicTrigger {
 
 	public MagicLivingWeaponTrigger(final String name) {
-		
 		super(MagicTriggerType.WhenComesIntoPlay,name);
 	}
 	
 	@Override
-	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+	public MagicEvent executeTrigger(
+            final MagicGame game,
+            final MagicPermanent permanent,
+            final Object data) {
 
 		final MagicPlayer player=permanent.getController();
-		return new MagicEvent(permanent,player,new Object[]{permanent,player},this,
+		return new MagicEvent(
+            permanent,
+            player,
+            new Object[]{permanent,player},
+            this,
 			"You put a 0/0 black Germ creature token onto the battlefield, then attach this to it.");
 	}
 	
 	@Override
-	public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+	public void executeEvent(
+            final MagicGame game,
+            final MagicEvent event,
+            final Object data[],
+            final Object[] choiceResults) {
 
-		final MagicPlayTokenAction action=new MagicPlayTokenAction((MagicPlayer)data[1],TokenCardDefinitions.GERM_TOKEN_CARD);
-		game.doAction(action);
-		game.doAction(new MagicAttachEquipmentAction((MagicPermanent)data[0],action.getPermanent()));
+        //create the token
+		final MagicPlayTokenAction play_token=new MagicPlayTokenAction(
+                (MagicPlayer)data[1],
+                TokenCardDefinitions.GERM_TOKEN_CARD);
+		game.doAction(play_token);
+
+        //attach the equipment to the token
+        final MagicAttachEquipmentAction attach_equip = new MagicAttachEquipmentAction(
+                (MagicPermanent)data[0],
+                action.getPermanent());
+        game.doAction(attach_equip)
 	}
 }
