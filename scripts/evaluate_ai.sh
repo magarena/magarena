e337b535f84c359b00f32be80547bf238dd921cd
melvin <unknown>
2011-05-24 09:50:25 +0800

    updated location of decks in AI evaluation script

--- scripts/evaluate_ai.sh
+++ scripts/evaluate_ai.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hg id 
-for d1 in decks/LSK_RW.dec decks/LSK_LifeGain.dec decks/LSK_UW.dec decks/LSK_Jund.dec decks/LSK_Skullclamp_Swords.dec; do
-for d2 in decks/LSK_RW.dec decks/LSK_LifeGain.dec decks/LSK_UW.dec decks/LSK_Jund.dec decks/LSK_Skullclamp_Swords.dec; do
+for d1 in release/decks/LSK_RW.dec release/decks/LSK_LifeGain.dec release/decks/LSK_UW.dec release/decks/LSK_Jund.dec release/decks/LSK_Skullclamp_Swords.dec; do
+for d2 in release/decks/LSK_RW.dec release/decks/LSK_LifeGain.dec release/decks/LSK_UW.dec release/decks/LSK_Jund.dec release/decks/LSK_Skullclamp_Swords.dec; do
 	java -ea -cp release/Magarena.jar magic.DeckStrCal --games 100 --strength 6 --deck1 $d1 --deck2 $d2 --ai1 $1 --ai2 VEGAS; 
 done
 done
