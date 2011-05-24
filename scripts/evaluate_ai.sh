c2139cac06a1b699912016d7314174b84bb8222c
melvin <unknown>
2011-05-24 17:09:40 +0800

    removed -ea and added -Xmx1G

--- scripts/evaluate_ai.sh
+++ scripts/evaluate_ai.sh
@@ -2,6 +2,6 @@
 hg id 
 for d1 in release/decks/LSK_RW.dec release/decks/LSK_LifeGain.dec release/decks/LSK_UW.dec release/decks/LSK_Jund.dec release/decks/LSK_Skullclamp_Swords.dec; do
 for d2 in release/decks/LSK_RW.dec release/decks/LSK_LifeGain.dec release/decks/LSK_UW.dec release/decks/LSK_Jund.dec release/decks/LSK_Skullclamp_Swords.dec; do
-	java -ea -cp release/Magarena.jar magic.DeckStrCal --games 100 --strength 6 --deck1 $d1 --deck2 $d2 --ai1 $1 --ai2 VEGAS; 
+	java -Xmx1G -cp release/Magarena.jar magic.DeckStrCal --games 100 --strength 6 --deck1 $d1 --deck2 $d2 --ai1 $1 --ai2 VEGAS; 
 done
 done
