f9fdc300391f577820ae7cb6f807d11db299dc3c
melvin <unknown>
2011-06-03 14:28:09 +0800

    added dummy implementation of getTypeFlags

--- src/magic/model/variable/MagicDummyLocalVariable.java
+++ src/magic/model/variable/MagicDummyLocalVariable.java
@@ -8,24 +8,26 @@ public class MagicDummyLocalVariable implements MagicLocalVariable {
 	
 	@Override
 	public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
-
+        //leave power and toughness unchanged
 	}
 
 	@Override
 	public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
-
 		return flags;
 	}
 
 	@Override
 	public int getSubTypeFlags(final MagicPermanent permanent,final int flags) {
-
+		return flags;
+	}
+	
+    @Override
+	public int getTypeFlags(final MagicPermanent permanent,final int flags) {
 		return flags;
 	}
 
 	@Override
 	public int getColorFlags(final MagicPermanent permanent,final int flags) {
-
 		return flags;
 	}	
 }
