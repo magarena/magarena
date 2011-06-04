addbd56de35de936398ce73dd4a09807df24cfde
melvin <unknown>
2011-06-04 16:58:53 +0800

    commented out SGE_TASK_ID and removed echo

--- exp/array_mag.sh
+++ exp/array_mag.sh
@@ -4,7 +4,6 @@
 #$ -cwd
 #$ -e /dev/null
 #$ -o /dev/null
-SGE_TASK_ID=100
+#SGE_TASK_ID=100
 args=`head -$SGE_TASK_ID ~/magarena/exp/args | tail -1`
-echo $args
 java -Xmx256M -DrndSeed=$SGE_TASK_ID -cp ~/magarena/build magic.DeckStrCal $args > $SGE_TASK_ID.out 2> $SGE_TASK_ID.err
