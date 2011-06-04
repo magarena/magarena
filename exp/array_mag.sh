ef775061400b94bad33d8805d9f97fe25941a905
melvin <unknown>
2011-06-04 16:58:06 +0800

    added seed to AI evaluation to improve repeatability and allow us to find bugs

--- exp/array_mag.sh
+++ exp/array_mag.sh
@@ -5,5 +5,6 @@
 #$ -e /dev/null
 #$ -o /dev/null
 SGE_TASK_ID=100
-pep=`head -$SGE_TASK_ID ~/magarena/exp/args | tail -1`
-java -Xmx1G -cp ~/magarena/build magic.DeckStrCal $pep > $SGE_TASK_ID.out 2> $SGE_TASK_ID.err
+args=`head -$SGE_TASK_ID ~/magarena/exp/args | tail -1`
+echo $args
+java -Xmx256M -DrndSeed=$SGE_TASK_ID -cp ~/magarena/build magic.DeckStrCal $args > $SGE_TASK_ID.out 2> $SGE_TASK_ID.err
