620e9f7a13fb2e51da1d5966cff85ea38b16ea90
melvin <unknown>
2011-06-07 16:19:54 +0800

    added setCardDefinition

--- src/magic/model/event/MagicSpellCardEvent.java
+++ src/magic/model/event/MagicSpellCardEvent.java
@@ -5,12 +5,15 @@ import magic.model.MagicCardDefinition;
 
 public abstract class MagicSpellCardEvent implements MagicCardEvent,MagicEventAction {
 
-	private final MagicCardDefinition card;
-	
-	public MagicSpellCardEvent(final String name) {
-		
-		card=CardDefinitions.getInstance().getCard(name);
+	private MagicCardDefinition card;
+
+    public MagicSpellCardEvent(final String name) {
+	    card=CardDefinitions.getInstance().getCard(name);
 	}
+
+    public void setCardDefinition(final MagicCardDefinition card) {
+        this.card = card;
+    }
 	
 	public final MagicCardDefinition getCardDefinition() {
 		
