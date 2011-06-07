3384c652634babb3c383467108f03690579fe72a
melvin <unknown>
2011-06-07 16:21:43 +0800

    moved Celestial Purge and Sphere of the Suns code to a separate class

---
+++ src/magic/data/Celestial_Purge.java
@@ -0,0 +1,39 @@
+package magic.data;
+
+import magic.model.event.*;
+import magic.model.stack.*;
+import magic.model.choice.*;
+import magic.model.target.*;
+import magic.model.action.*;
+import magic.model.*;
+
+class Celestial_Purge {
+    public static final MagicSpellCardEvent EXILE=new MagicSpellCardEvent(null) {
+
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player=cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_BLACK_RED_PERMANENT,
+                    MagicExileTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,				
+                    "Exile target black or red permanent$.");
+		}
+
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPermanent perm=event.getTarget(game,choiceResults,0);
+			if (perm!=null) {
+				game.doAction(new MagicRemoveFromPlayAction(perm,MagicLocationType.Exile));
+			}
+		}
+	};
+}
---
+++ src/magic/data/Sphere_of_the_Suns.java
@@ -0,0 +1,38 @@
+package magic.data;
+
+import magic.model.event.*;
+import magic.model.stack.*;
+import magic.model.choice.*;
+import magic.model.target.*;
+import magic.model.action.*;
+import magic.model.trigger.*;
+import magic.model.*;
+
+class Sphere_of_the_Suns {
+    public static final MagicTrigger SPHERE_SUN=new MagicTrigger(MagicTriggerType.WhenComesIntoPlay,"") {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+		    return new MagicEvent(permanent,permanent.getController(),new Object[]{permanent},this,
+			    permanent.getName()+" enters the battlefield tapped with three charge counters on it.");
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+
+            final MagicPermanent permanent=(MagicPermanent)data[0];
+            game.doAction(new MagicTapAction(permanent,false));
+            game.doAction(new MagicChangeCountersAction(permanent,MagicCounterType.Charge,3,false));
+		}
+	
+        @Override
+    	public boolean usesStack() {
+    		return false;
+	    }
+    };
+    
+    public static final MagicManaActivation MANA=new MagicVividManaActivation(MagicManaType.ALL_TYPES);
+}
