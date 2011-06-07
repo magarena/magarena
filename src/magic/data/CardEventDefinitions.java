8e413ede6753484ac0badc40e795265981ef1500
melvin <unknown>
2011-06-07 16:39:30 +0800

    moved Spell Pierce to separate class

--- src/magic/data/CardEventDefinitions.java
+++ src/magic/data/CardEventDefinitions.java
@@ -915,31 +915,6 @@ public class CardEventDefinitions {
 		}
 	};
 	
-    private static final MagicSpellCardEvent SPELL_PIERCE=new MagicSpellCardEvent("Spell Pierce") {
-
-		@Override
-		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			
-			return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
-                    MagicTargetChoice.NEG_TARGET_NONCREATURE_SPELL,
-                    new Object[]{cardOnStack},this,"Counter target noncreature spell$ unless its controller pays {2}.");
-		}
-
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-
-			final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-			game.doAction(new MagicMoveCardAction(cardOnStack));
-			final MagicCardOnStack targetSpell=event.getTarget(game,choiceResults,0);
-			if (targetSpell!=null) {
-				game.addEvent(new MagicCounterUnlessEvent(cardOnStack.getCard(),targetSpell,MagicManaCost.TWO));
-			}
-		}
-	};
-	
-	
 	private static final MagicSpellCardEvent MANA_LEAK=new MagicSpellCardEvent("Mana Leak") {
 
 		@Override
