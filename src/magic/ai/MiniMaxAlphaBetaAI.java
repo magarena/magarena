95e7412c32699de64d4995d639817994f209a654
melvin <unknown>
2011-06-08 10:53:24 +0800

    removed some empty lines

--- src/magic/ai/MiniMaxAlphaBetaAI.java
+++ src/magic/ai/MiniMaxAlphaBetaAI.java
@@ -4,9 +4,7 @@ import magic.model.MagicGame;
 import magic.model.MagicPlayer;
 
 public class MiniMaxAlphaBetaAI implements MagicAI {
-	
     public Object[] findNextEventChoiceResults(final MagicGame game,final MagicPlayer player) {
-
     	final ArtificialWorkerPool aiWorkerPool = new ArtificialWorkerPool(game, player);
         return aiWorkerPool.findNextEventChoiceResults();
     }
