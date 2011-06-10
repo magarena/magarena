050c98d85c72382d78ff7b3ccef28aae2b2e41c6
melvin <unknown>
2011-06-10 18:08:16 +0800

    removed some empty lines

--- src/magic/model/event/MagicDiscardEvent.java
+++ src/magic/model/event/MagicDiscardEvent.java
@@ -12,27 +12,27 @@ import magic.model.choice.MagicRandomCardChoice;
 public class MagicDiscardEvent extends MagicEvent {
 
 	private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
-		
 		@Override
 		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-
 			final MagicPlayer player=(MagicPlayer)data[0];
 			final MagicCardChoiceResult cards=(MagicCardChoiceResult)choices[0];
 			for (final MagicCard card : cards) {
-				
 				game.doAction(new MagicDiscardCardAction(player,card));
 			}
 		}
 	};
 	
 	public MagicDiscardEvent(final MagicSource source,final MagicPlayer player,final int amount,final boolean random) {
-
-		super(source,player,random?new MagicRandomCardChoice(amount):new MagicCardChoice(amount),new Object[]{player},EVENT_ACTION,
-				getDescription(amount,random));
+        super(
+            source,
+            player,
+            random ? new MagicRandomCardChoice(amount):new MagicCardChoice(amount),
+            new Object[]{player},EVENT_ACTION,
+            getDescription(amount,random)
+        );
 	}
 	
 	private static final String getDescription(final int amount,final boolean random) {
-		
 		if (amount!=1) {
 			return "You discard "+amount+" cards$.";
 		} else {
