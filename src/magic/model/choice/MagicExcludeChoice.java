14cd8927dd70d1fae51f3aa1cacab46bed924ac3
melvin <unknown>
2011-06-11 14:47:20 +0800

    clean up code, put comment to indicate that the number of options generated for this choice may be very large

--- src/magic/model/choice/MagicExcludeChoice.java
+++ src/magic/model/choice/MagicExcludeChoice.java
@@ -15,20 +15,25 @@ import magic.ui.GameController;
 public class MagicExcludeChoice extends MagicChoice {
 
 	public static final MagicChoice INSTANCE=new MagicExcludeChoice();
-	
 	private static final Collection<Object> EMPTY_EXCLUDE_RESULT=Collections.<Object>singleton(new MagicExcludeResult());
 	
 	private MagicExcludeChoice() {
-		
-		super("");
+		super("AI Exclude Mana or Combat Choice");
+	}
+	
+    public static MagicChoice getInstance() {
+		return INSTANCE;
 	}
 	
 	@Override
-	public Collection<Object> getArtificialOptions(final MagicGame game,final MagicEvent event,final MagicPlayer player,final MagicSource source) {
-
-		final List<MagicPermanent> excludePermanents=new ArrayList<MagicPermanent>();
+	public Collection<Object> getArtificialOptions(
+            final MagicGame game,
+            final MagicEvent event,
+            final MagicPlayer player,
+            final MagicSource source) {
+		
+        final List<MagicPermanent> excludePermanents=new ArrayList<MagicPermanent>();
 		for (final MagicPermanent permanent : player.getPermanents()) {
-			
 			if (permanent.getCardDefinition().hasExcludeManaOrCombat()) {
 				excludePermanents.add(permanent);
 			}
@@ -39,27 +44,26 @@ public class MagicExcludeChoice extends MagicChoice {
 		}
 		
 		// In later turns, favour mana over combat when there are more than one exclude permanents.
-		if (game.getRelativeTurn()>1&&excludePermanents.size()>1) {
+		if (game.getRelativeTurn() > 1 && excludePermanents.size() > 1) {
 			return Collections.<Object>singleton(new MagicExcludeResult(excludePermanents,0));
 		}
 
-		final List<Object> excludeOptions=new ArrayList<Object>();
-		for (int flags=(1<<excludePermanents.size())-1;flags>=0;flags--) {
-
+        //MEM possible combinatorial explosion that could lead to out of memory 
+        final int numOptions = (1<<excludePermanents.size())-1;
+		final List<Object> excludeOptions = new ArrayList<Object>();
+		for (int flags = numOptions; flags >= 0; flags--) {
 			excludeOptions.add(new MagicExcludeResult(excludePermanents,flags));
 		}
 		return excludeOptions;
 	}
 
 	@Override
-	public Object[] getPlayerChoiceResults(final GameController controller,final MagicGame game,final MagicPlayer player,final MagicSource source) {
-
-		// Should be done only by artificial player.
+	public Object[] getPlayerChoiceResults(
+            final GameController controller,
+            final MagicGame game,
+            final MagicPlayer player,
+            final MagicSource source) {
+		// Should be done only by AI player.
 		throw new UnsupportedOperationException();
-	}
-	
-	public static MagicChoice getInstance() {
-		
-		return INSTANCE;
 	}
 }
