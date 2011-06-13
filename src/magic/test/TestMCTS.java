373cac73fd76528868dc2a86694cf8120086809c
melvin <unknown>
2011-06-13 16:58:44 +0800

    Added game to test MCTS

---
+++ src/magic/test/TestMCTS.java
@@ -0,0 +1,43 @@
+package magic.test;
+
+import magic.model.*;
+import magic.model.phase.*;
+import magic.ai.*;
+
+class TestMCTS extends TestGameBuilder {    
+    public MagicGame getGame() {
+		final MagicTournament tournament=new MagicTournament();
+		tournament.setDifficulty(6);
+		
+		final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+		final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+		final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+		tournament.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+		tournament.setStartPlayer(0);
+        tournament.setAIs(new MagicAI[]{null, new MCTSAI(true, true)});
+		
+		final MagicGame game=tournament.nextGame(true);
+		game.setPhase(MagicMainPhase.getFirstInstance());
+		final MagicPlayer player=game.getPlayer(0);
+		final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(4);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+		createPermanent(game,P,"Hearthfire Hobgoblin", false, 3);
+		createPermanent(game,P,"Akrasan Squire", false, 3);
+       
+
+        P = opponent;
+		
+        P.setLife(4);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+		createPermanent(game,P,"Hearthfire Hobgoblin", false, 3);
+		createPermanent(game,P,"Akrasan Squire", false, 3);
+		
+		return game;
+    }
+}
