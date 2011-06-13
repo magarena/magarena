002b6e7e90cd448af8348d54ff0bc0e46a2587ac
melvin <unknown>
2011-06-13 12:53:01 +0800

    if phase is CombatDamage and stack is empty, go to begin else resolve what is on the stack

--- src/magic/model/event/MagicPriorityEvent.java
+++ src/magic/model/event/MagicPriorityEvent.java
@@ -6,16 +6,29 @@ import magic.model.MagicSource;
 import magic.model.choice.MagicPlayChoice;
 import magic.model.choice.MagicPlayChoiceResult;
 import magic.model.phase.MagicStep;
+import magic.model.phase.MagicPhaseType;
 
 public class MagicPriorityEvent extends MagicEvent {
 	
 	private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
 		
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
 			
 			final MagicPlayChoiceResult playChoiceResult=(MagicPlayChoiceResult)choiceResults[0];
-			if (playChoiceResult==MagicPlayChoiceResult.PASS) {
+            if (game.isPhase(MagicPhaseType.CombatDamage)) {
+		        if (game.getStack().isEmpty()) {
+                    //go back to begin and deal regular damage
+                    game.setStep(MagicStep.Begin);
+                } else {
+                    //resolve triggers due to first strike
+                    game.setStep(MagicStep.Resolve);
+                }
+            } else if (playChoiceResult==MagicPlayChoiceResult.PASS) {
 				game.incrementPriorityPassedCount();
 				// When passing, the last played activation can no longer be played.
 				game.getPriorityPlayer().getActivationPriority().activationId++;
@@ -44,7 +57,6 @@ public class MagicPriorityEvent extends MagicEvent {
 				final MagicSource source=playChoiceResult.source;
 				activation.changeActivationPriority(game,source);
 				for (final MagicEvent costEvent : activation.getCostEvent(source)) {
-					
 					game.addEvent(costEvent);
 				}
 				final MagicEvent activationEvent=activation.getEvent(source);
