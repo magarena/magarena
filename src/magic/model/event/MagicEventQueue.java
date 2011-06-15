4a85152b071769d44aea82d96b277bf097982573
melvin <unknown>
2011-06-15 15:58:11 +0800

    removed some empty lines

--- src/magic/model/event/MagicEventQueue.java
+++ src/magic/model/event/MagicEventQueue.java
@@ -8,14 +8,10 @@ public class MagicEventQueue extends LinkedList<MagicEvent> {
 	
 	private static final long serialVersionUID = 1L;
 
-	public MagicEventQueue() {
-		
-	}
+	public MagicEventQueue() {}
 	
 	public MagicEventQueue(final MagicCopyMap copyMap,final MagicEventQueue source) {
-		
 		for (final MagicEvent event : source) {
-			
 			add(copyMap.copy(event));
 		}
 	}	
