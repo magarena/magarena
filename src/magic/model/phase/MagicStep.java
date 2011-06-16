976e66abb95507602e49487fc08e2ac9d2be51d6
melvin <unknown>
2011-06-16 20:48:53 +0800

    moved comments to before the enum

--- src/magic/model/phase/MagicStep.java
+++ src/magic/model/phase/MagicStep.java
@@ -1,10 +1,20 @@
 package magic.model.phase;
 
 public enum MagicStep {
-
-	Begin, /** Begin of each phase */
-	ActivePlayer, /** Let active player play spells until pass. */
-	OtherPlayer, /** Let other player play spells until pass. If spells were played, go back to ActivePlayer. */
-	Resolve, /** If stack is empty, go to StackResolved. Otherwise resolve top spell of stack and go back to ActivePlayer. */
-	NextPhase, /** Go to the next phase. */
+    /** Begin of each phase */
+	Begin, 
+    
+    /** Let active player play spells until pass. */
+    ActivePlayer, 	
+    
+    /** Let other player play spells until pass. If spells were played, go back
+     * to ActivePlayer. */
+    OtherPlayer,	
+    
+    /** If stack is empty, go to StackResolved. Otherwise resolve top spell of
+     * stack and go back to ActivePlayer. */
+    Resolve, 	
+    
+    /** Go to the next phase. */
+    NextPhase, 
 }
