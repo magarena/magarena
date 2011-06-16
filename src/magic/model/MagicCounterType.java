89a8698438dc51ebb0194201f4ee39af843d70b3
melvin <unknown>
2011-06-16 20:50:14 +0800

    simplified getIndex

--- src/magic/model/MagicCounterType.java
+++ src/magic/model/MagicCounterType.java
@@ -2,37 +2,31 @@ package magic.model;
 
 public enum MagicCounterType {
 	
-	PlusOne("+1/+1","{+}",0),
-	MinusOne("-1/-1","{-}",1),
-	Charge("charge","{C}",2),
-	Feather("feather","{F}",3),
+	PlusOne("+1/+1","{+}"),
+	MinusOne("-1/-1","{-}"),
+	Charge("charge","{C}"),
+	Feather("feather","{F}"),
 	;
 
 	public static final int NR_COUNTERS=MagicCounterType.values().length;
 	
 	private final String name;
 	private final String text;
-	private final int index;
 	
-	private MagicCounterType(final String name,final String text,final int index) {
-		
+	private MagicCounterType(final String name,final String text) {
 		this.name=name;
 		this.text=text;
-		this.index=index;
 	}
 	
 	public String getName() {
-		
 		return name;
 	}
 	
 	public String getText() {
-		
 		return text;
 	}
 		
 	public int getIndex() {
-		
-		return index;
+		return ordinal();
 	}
 }
