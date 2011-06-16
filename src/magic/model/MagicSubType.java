7716b4d558a704dca76ae0ac8d97cbf081be1a5b
melvin <unknown>
2011-06-16 20:58:28 +0800

    simplfied enum

--- src/magic/model/MagicSubType.java
+++ src/magic/model/MagicSubType.java
@@ -1,27 +1,30 @@
 package magic.model;
 
 public enum MagicSubType {
+    //non creatures
+	Forest("Forest"),
+	Island("Island"),
+	Mountain("Mountain"),
+	Plains("Plains"),
+	Swamp("Swamp"),
+	Equipment("Equipment"),
+	Aura("Aura"),
 
-	Forest("Forest",false,0),
-	Island("Island",false,1),
-	Mountain("Mountain",false,2),
-	Plains("Plains",false,3),
-	Swamp("Swamp",false,4),
-	Equipment("Equipment",false,5),
-	Aura("Aura",false,6),
-	Bat("Bat",true,7),
-	Beast("Beast",true,8),
-	Demon("Demon",true,9),
-	Dragon("Dragon",true,10),
-	Goblin("Goblin",true,11),
-	Knight("Knight",true,12),
-	Soldier("Soldier",true,13),
-	Illusion("Illusion",true,14),
-	Elemental("Elemental",true,15),
-	Construct("Construct",true,16),
+    //creatures
+	Bat("Bat"),
+	Beast("Beast"),
+	Demon("Demon"),
+	Dragon("Dragon"),
+	Goblin("Goblin"),
+	Knight("Knight"),
+	Soldier("Soldier"),
+	Illusion("Illusion"),
+	Elemental("Elemental"),
+	Construct("Construct"),
 	;
 
-	public static final int ALL_BASIC_LANDS=Forest.getMask()|Island.getMask()|Mountain.getMask()|Plains.getMask()|Swamp.getMask();
+	public static final int ALL_BASIC_LANDS = 
+        Forest.getMask()|Island.getMask()|Mountain.getMask()|Plains.getMask()|Swamp.getMask();
 	
 	public static final int ALL_CREATURES=
         Bat.getMask()|
@@ -36,47 +39,36 @@ public enum MagicSubType {
         Construct.getMask();
 	
 	private final String name;
-	private final boolean creature;
-	private final int index;
 	private final int mask;
 	
-	private MagicSubType(final String name,final boolean creature,final int index) {
-		
+	private MagicSubType(final String name) {
 		this.name=name;
-		this.creature=creature;
-		this.index=index;
-		this.mask=1<<index;
+		this.mask=1<<ordinal();
+        System.err.println(name + " " + mask);
 	}
 	
 	public String getName() {
-		
 		return name;
 	}
 	
 	public boolean isCreatureType() {
-		
-		return creature;
+	    return (ALL_CREATURES & mask) != 0;
 	}
 	
 	public int getIndex() {
-		
-		return index;
+	    return ordinal();	
 	}
 	
 	public int getMask() {
-		
 		return mask;
 	}
 	
 	public boolean hasSubType(final int flags) {
-		
-		return (flags&mask)!=0;
+		return (flags & mask) != 0;
 	}
 	
 	public static MagicSubType getSubType(final String name) {
-		
 		for (final MagicSubType type : values()) {
-			
 			if (type.getName().equalsIgnoreCase(name)) {
 				return type;
 			}
