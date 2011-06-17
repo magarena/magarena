554e3ed51da62fd0b44fa620a4b17d6bbfc19aa9
melvin <unknown>
2011-06-17 18:48:51 +0800

    changed output from mcts6 to mctsd (debug)

--- exp/eval_mcts.sh
+++ exp/eval_mcts.sh
@@ -6,4 +6,4 @@
 #$ -o /dev/null
 #SGE_TASK_ID=100
 args=`grep MCTS exp/args | head -$SGE_TASK_ID | tail -1`
-java -ea -Xms256M -Xmx256M -Ddebug=true -DrndSeed=$SGE_TASK_ID -cp build magic.DeckStrCal $args > $SGE_TASK_ID.out.mcts6 2> $SGE_TASK_ID.err.mcts6
+java -ea -Xms256M -Xmx256M -Ddebug=true -DrndSeed=$SGE_TASK_ID -cp build magic.DeckStrCal $args > $SGE_TASK_ID.out.mctsd 2> $SGE_TASK_ID.err.mctsd
