3acf3bbcb3628447fb8127ecef3d76656dca8dc9
melvin <unknown>
2011-06-17 13:53:36 +0800

    cleaned up logic

--- src/magic/model/phase/MagicDrawPhase.java
+++ src/magic/model/phase/MagicDrawPhase.java
@@ -10,30 +10,29 @@ public class MagicDrawPhase extends MagicPhase {
 	private static final MagicPhase INSTANCE=new MagicDrawPhase();
 
 	public MagicDrawPhase() {
-		
 		super(MagicPhaseType.Draw);
 	}
 	
 	public static MagicPhase getInstance() {
-		
 		return INSTANCE;
 	}
 
 	@Override
 	public void executeBeginStep(final MagicGame game) {
-		
-		if (game.getTurn()>1) {
-			final MagicPlayer player=game.getTurnPlayer();
-			game.doAction(new MagicDrawAction(player,1));
-			game.setStep(game.canSkip()?MagicStep.NextPhase:MagicStep.ActivePlayer);
-
-            // Determines what the purpose is for permanents that can attack,
-            // block or produce mana. Do this after draw, could be a land card.
-			if (player.getPlayerDefinition().isArtificial()) {
-				game.addEvent(new MagicExcludeEvent(player));
-			}
-		} else {
+        //skip draw phase for first turn
+        if (game.getTurn() == 1) {
 			game.setStep(MagicStep.NextPhase);
-		}
+            return;
+        }
+
+        final MagicPlayer player=game.getTurnPlayer();
+        game.doAction(new MagicDrawAction(player,1));
+        game.setStep(game.canSkip() ? MagicStep.NextPhase : MagicStep.ActivePlayer);
+
+        // Determines what the purpose is for permanents that can attack,
+        // block or produce mana. Do this after draw, could be a land card.
+        if (player.getPlayerDefinition().isArtificial()) {
+            game.addEvent(new MagicExcludeEvent(player));
+        }
 	}
 }
