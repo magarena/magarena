9c995c9af08fda8dc60457dfeec6ad462d991967
melvin <unknown>
2011-06-18 17:04:25 +0800

    wrapped long lines

--- src/magic/model/event/MagicGainActivation.java
+++ src/magic/model/event/MagicGainActivation.java
@@ -15,7 +15,6 @@ public class MagicGainActivation extends MagicPermanentActivation {
 	private final MagicAbility ability;
 	
 	public MagicGainActivation(final String name,final MagicManaCost cost,final MagicAbility ability,final MagicActivationHints hints) {
-		
 		super(name,new MagicCondition[]{cost.getCondition()},hints);
 		this.cost=cost;
 		this.ability=ability;
@@ -23,19 +22,21 @@ public class MagicGainActivation extends MagicPermanentActivation {
 	
 	@Override
 	public MagicEvent[] getCostEvent(final MagicSource source) {
-
 		return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),cost)};
 	}
 
 	@Override
 	public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-
-		return new MagicEvent(source,source.getController(),new Object[]{source},this,source.getName()+" gains "+ability+" until end of turn.");
+		return new MagicEvent(
+                source,
+                source.getController(),
+                new Object[]{source},
+                this,
+                source.getName()+" gains "+ability+" until end of turn.");
 	}
 
 	@Override
 	public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-		
 		game.doAction(new MagicSetAbilityAction((MagicPermanent)data[0],ability));
 	}
 }
