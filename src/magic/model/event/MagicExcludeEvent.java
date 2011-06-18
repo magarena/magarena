bebad326e4893dfd29752b83c6f77abbf99e923b
melvin <unknown>
2011-06-18 23:41:38 +0800

    wrapped long lines

--- src/magic/model/event/MagicExcludeEvent.java
+++ src/magic/model/event/MagicExcludeEvent.java
@@ -8,17 +8,18 @@ import magic.model.choice.MagicExcludeResult;
 public class MagicExcludeEvent extends MagicEvent {
 
 	private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
-		
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-			
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
 			final MagicExcludeResult result=(MagicExcludeResult)choiceResults[0];
 			result.exclude(game);			
 		}
 	};
 	
 	public MagicExcludeEvent(final MagicPlayer player) {
-		
 		super(null,player,MagicExcludeChoice.getInstance(),MagicEvent.NO_DATA,EVENT_ACTION,null);
 	}
 }
