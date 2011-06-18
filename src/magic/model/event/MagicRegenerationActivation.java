8dcc4dc2f4ce100f8194e80f7f66cacdb55d51c5
melvin <unknown>
2011-06-18 12:05:56 +0800

    added text for regen

--- src/magic/model/event/MagicRegenerationActivation.java
+++ src/magic/model/event/MagicRegenerationActivation.java
@@ -16,26 +16,29 @@ public class MagicRegenerationActivation extends MagicPermanentActivation {
 	private final MagicManaCost cost;
 	
 	public MagicRegenerationActivation(final String name,final MagicManaCost cost) {
-		
-		super(name,new MagicCondition[]{MagicCondition.CAN_REGENERATE_CONDITION,cost.getCondition(),new MagicSingleActivationCondition()},ACTIVATION_HINTS);
+		super(
+            name,
+            new MagicCondition[]{
+                MagicCondition.CAN_REGENERATE_CONDITION,cost.getCondition(),
+                new MagicSingleActivationCondition()
+            },
+            ACTIVATION_HINTS, 
+            "Regen");
 		this.cost=cost;
 	}
 	
 	@Override
 	public MagicEvent[] getCostEvent(final MagicSource source) {
-
 		return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),cost)};
 	}
 
 	@Override
 	public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-
 		return new MagicEvent(source,source.getController(),new Object[]{source},this,"Regenerate "+source.getName()+".");
 	}
 
 	@Override
 	public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-		
 		game.doAction(new MagicRegenerateAction((MagicPermanent)data[0]));
 	}
 }
