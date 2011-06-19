97cbecc76bc577212da7deb31be243f53057d7d4
melvin <unknown>
2011-06-19 15:27:32 +0800

    removed empty lines

--- src/magic/model/stack/MagicStack.java
+++ src/magic/model/stack/MagicStack.java
@@ -16,24 +16,19 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	private final int counts[];
 	
 	public MagicStack() {
-		
 		spells=new int[2];
 		counts=new int[2];
 	}
 	
 	public MagicStack(final MagicCopyMap copyMap,final MagicStack source) {
-
 		spells=Arrays.copyOf(source.spells,2);
 		counts=Arrays.copyOf(source.counts,2);
-		
 		for (final MagicItemOnStack itemOnStack : source) {
-			
 			add(copyMap.copy(itemOnStack));
 		}
 	}
 
 	private void addCount(final MagicItemOnStack itemOnStack) {
-
 		final int index=itemOnStack.getController().getIndex();
 		counts[index]++;
 		if (itemOnStack.isSpell()) {
@@ -42,7 +37,6 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	}
 	
 	private void removeCount(final MagicItemOnStack itemOnStack) {
-
 		final int index=itemOnStack.getController().getIndex();
 		counts[index]--;
 		if (itemOnStack.isSpell()) {
@@ -51,41 +45,34 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	}
 	
 	public void addToTop(final MagicItemOnStack itemOnStack) {
-
 		addFirst(itemOnStack);
 		addCount(itemOnStack);
 	}
 	
 	public MagicItemOnStack removeFromTop() {
-		
 		final MagicItemOnStack itemOnStack=removeFirst();
 		removeCount(itemOnStack);
 		return itemOnStack;
 	}
 	
 	public void addTo(final int index,final MagicItemOnStack itemOnStack) {
-
 		add(index,itemOnStack);
 		addCount(itemOnStack);
 	}
 	
 	public void removeFrom(final int index) {
-
 		final MagicItemOnStack itemOnStack=remove(index);
 		removeCount(itemOnStack);
 	}
 	
 	public void removeAllItems() {
-		
 		clear();
 		Arrays.fill(counts,0);
 		Arrays.fill(spells,0);
 	}
 		
 	public MagicItemOnStack getItemOnStack(final long id) {
-		
 		for (final MagicItemOnStack itemOnStack : this) {
-			
 			if (itemOnStack.getId()==id) {
 				return itemOnStack;
 			}
@@ -94,7 +81,6 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	}	
 	
 	public boolean hasActivationOnTop(final MagicSource source,final MagicActivation activation) {
-
 		if (isEmpty()) {
 			return false;
 		}		
@@ -103,7 +89,6 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	}
 	
 	public boolean hasItemOnTopOfPlayer(final MagicPlayer player) {
-		
 		if (isEmpty()) {
 			return false;
 		}
@@ -111,17 +96,14 @@ public class MagicStack extends LinkedList<MagicItemOnStack> {
 	}
 	
 	public boolean isResponse(final MagicPlayer player) {
-
 		return counts[1-player.getIndex()]>0;
 	}
 	
 	public boolean containsSpells() {
-
 		return spells[0]>0||spells[1]>0;
 	}
 	
 	public boolean containsOpponentSpells(final MagicPlayer player) {
-
 		return spells[1-player.getIndex()]>0;
 	}
 
