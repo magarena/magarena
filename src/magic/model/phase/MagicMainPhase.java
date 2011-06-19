3f7a696e231b5bab826be7f3190d32fbfc0a210b
melvin <unknown>
2011-06-19 22:19:01 +0800

    changed long constants to represent booleans in gameId

--- src/magic/model/phase/MagicMainPhase.java
+++ src/magic/model/phase/MagicMainPhase.java
@@ -8,29 +8,24 @@ public class MagicMainPhase extends MagicPhase {
 	private static final MagicPhase SECOND_INSTANCE=new MagicMainPhase(MagicPhaseType.SecondMain);
 
 	public MagicMainPhase(final MagicPhaseType phaseType) {
-		
 		super(phaseType);
 	}
 	
 	public static MagicPhase getFirstInstance() {
-		
 		return FIRST_INSTANCE;
 	}
 	
 	public static MagicPhase getSecondInstance() {
-		
 		return SECOND_INSTANCE;
 	}
 
 	@Override
 	public void executeBeginStep(final MagicGame game) {
-		
 		game.setStep(MagicStep.ActivePlayer);
 	}
 
 	@Override
 	protected void executeEndOfPhase(final MagicGame game) {
-
 		game.decreaseMainPhaseCount();
 	}
 }
