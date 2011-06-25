cbe5da78dada0256074c220a85b47b395338125c
melvin <unknown>
2011-06-25 10:49:16 +0800

    removed empty lines

--- src/magic/ui/widget/PanelButton.java
+++ src/magic/ui/widget/PanelButton.java
@@ -25,13 +25,11 @@ public abstract class PanelButton extends TexturedPanel {
 			
 			@Override
 			public void mousePressed(final MouseEvent event) {
-				
 				setBorder(FontsAndBorders.DOWN_BORDER);
 			}
 
 			@Override
 			public void mouseReleased(final MouseEvent event) {
-
 				setBorder(FontsAndBorders.UP_BORDER);
 				if (PanelButton.this.contains(event.getX(),event.getY())) {
 					PanelButton.this.mouseClicked();
@@ -40,46 +38,35 @@ public abstract class PanelButton extends TexturedPanel {
 
 			@Override
 			public void mouseEntered(final MouseEvent event) {
-				
 				PanelButton.this.mouseEntered();
 			}
 
 			@Override
 			public void mouseExited(final MouseEvent event) {
-
 				PanelButton.this.mouseExited();
 			}
 		});
 	}
 	
 	public void setValid(final boolean valid) {
-		
 		this.valid=valid;
 		repaint();
 	}
 	
 	public void setComponent(final JComponent component) {
-		
 		add(component,BorderLayout.CENTER);
 	}
 	
-	public void mouseClicked() {
-		
-	}
+	public void mouseClicked() {}
 	
-	public void mouseEntered() {
-		
-	}
+	public void mouseEntered() {}
 	
-	public void mouseExited() {
-		
-	}
+	public void mouseExited() {}
 	
 	public abstract Color getValidColor();
 
 	@Override
 	public void paint(final Graphics g) {
-
 		super.paint(g);
 		final Dimension size=getSize();
 		if (valid) {
