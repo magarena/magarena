887e8d511ed258c278dbbe1deb48714447c7084c
melvin <unknown>
2011-07-01 11:33:00 +0800

    added getId

--- src/magic/model/event/MagicSourceManaActivationResult.java
+++ src/magic/model/event/MagicSourceManaActivationResult.java
@@ -10,23 +10,24 @@ public class MagicSourceManaActivationResult implements MagicMappable {
 	public final MagicManaActivation activation;
 	
 	public MagicSourceManaActivationResult(final MagicPermanent permanent,final MagicManaActivation activation) {
-		
 		this.permanent=permanent;
 		this.activation=activation;
 	}
 
 	@Override
 	public Object map(final MagicGame game) {
-
 		return new MagicSourceManaActivationResult((MagicPermanent)permanent.map(game),activation);
 	}
 
 	public void doActivation(final MagicGame game) {
-		
 		for (final MagicEvent costEvent : activation.getCostEvent(permanent)) {
-			
 			// Mana activation cost events do not have choices.
 			game.executeEvent(costEvent,MagicEvent.NO_CHOICE_RESULTS); 
 		}
 	}
+
+    @Override
+    public long getId() {
+        return permanent.getId() * 31 + activation.hashCode();
+    }
 }
