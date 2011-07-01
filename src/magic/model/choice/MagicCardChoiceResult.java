04ad856e890f3b2d1fea95a176e176d3a8093ffb
melvin <unknown>
2011-07-01 11:33:23 +0800

    added getId

--- src/magic/model/choice/MagicCardChoiceResult.java
+++ src/magic/model/choice/MagicCardChoiceResult.java
@@ -12,20 +12,15 @@ public class MagicCardChoiceResult extends ArrayList<MagicCard> implements Magic
 	private static final long serialVersionUID = 1L;
 
 	public MagicCardChoiceResult(final MagicCard cards[]) {
-
 		addAll(Arrays.asList(cards));
 	}
 	
-	public MagicCardChoiceResult() {
-		
-	}
+	public MagicCardChoiceResult() {}
 	
 	@Override
 	public Object map(final MagicGame game) {
-
 		final MagicCardChoiceResult result=new MagicCardChoiceResult();
 		for (final MagicCard card : this) {
-			
 			result.add((MagicCard)card.map(game));
 		}
 		return result;
@@ -33,10 +28,8 @@ public class MagicCardChoiceResult extends ArrayList<MagicCard> implements Magic
 
 	@Override
 	public String toString() {
-		
 		final StringBuffer buffer=new StringBuffer();
 		for (final MagicCard card : this)  {
-			
 			if (buffer.length()>0) {
 				buffer.append(", ");
 			}
@@ -44,4 +37,15 @@ public class MagicCardChoiceResult extends ArrayList<MagicCard> implements Magic
 		}
 		return buffer.toString();
 	}
+
+    @Override
+    public long getId() {
+        int idx = 0;
+		long[] keys = new long[size() + 1];
+		for (final MagicCard card : this) {
+			keys[idx] = card.getCardDefinition().getIndex();
+            idx++;
+		}
+		return magic.MurmurHash3.hash(keys);
+    }
 }
