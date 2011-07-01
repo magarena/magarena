1bfacb4b4d851721bde4a95c47872be85f588c79
melvin <unknown>
2011-07-01 11:30:16 +0800

    removed empty lines

--- src/magic/model/MagicPermanentList.java
+++ src/magic/model/MagicPermanentList.java
@@ -6,19 +6,14 @@ public class MagicPermanentList extends ArrayList<MagicPermanent> {
 
 	private static final long serialVersionUID = 1L;
 
-	public MagicPermanentList() {
-		
-	}
+	public MagicPermanentList() {}
 	
 	public MagicPermanentList(final MagicPermanentList list) {
-		
 		addAll(list);
 	}
 	
 	public MagicPermanentList(final MagicCopyMap copyMap,final MagicPermanentList list) {
-
 		for (final MagicPermanent permanent : list) {
-			
 			add(copyMap.copy(permanent));
 		}
 	}
