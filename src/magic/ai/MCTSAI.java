590cbf5db5548ed2615ebdbdf4d3f450de67806a
melvin <unknown>
2011-07-04 15:25:46 +0800

    added notes from consisteny modifications paper

--- src/magic/ai/MCTSAI.java
+++ src/magic/ai/MCTSAI.java
@@ -29,6 +29,16 @@ Enchancements to basic UCT
 
 References:
 UCT algorithm from Kocsis and Sezepesvari 2006
+Consistency Modifications for Automatically Tuned Monte-Carlo Tree Search
+  consistent -> child of root with greatest number of simulations is optimal
+  frugal -> do not need to visit the whole tree
+  eps-greedy is not consisteny for fixed eps (with prob eps select randomly, else use score)
+  eps-greedy is consistent but not frugal if eps dynamically decreases to 0
+  UCB1 is consistent but not frugal
+  score = average is not consistent
+  score = (total reward + K)/(total simulation + 2K) is consistent and frugal!
+  using v_t threshold ensures consistency for case of reward in {0,1} using any score function
+    v(s) < v_t (0.3), randomy pick a child, else pick child that maximize score
 */
 public class MCTSAI implements MagicAI {
     
