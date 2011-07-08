52ce5503d98e4d57a9a018a6c12c376a6a5552dd
melvin <unknown>
2011-07-08 13:05:13 +0800

    removed empty lines

--- src/magic/model/event/MagicPumpActivation.java
+++ src/magic/model/event/MagicPumpActivation.java
@@ -10,8 +10,7 @@ import magic.model.condition.MagicCondition;
 
 public class MagicPumpActivation extends MagicPermanentActivation {
 
-	private static final MagicActivationHints ACTIVATION_HINTS=new MagicActivationHints(MagicTiming.Pump);
-	
+	private static final MagicActivationHints ACTIVATION_HINTS = new MagicActivationHints(MagicTiming.Pump);
 	private final MagicManaCost cost;
 	private final int power;
 	private final int toughness;
@@ -25,20 +24,17 @@ public class MagicPumpActivation extends MagicPermanentActivation {
 
 	@Override
 	public MagicEvent[] getCostEvent(final MagicSource source) {
-
 		return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),cost)};
 	}
 	
 	@Override
 	public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-
 		return new MagicEvent(source,source.getController(),new Object[]{source},this,
 			source.getName()+" gets +"+power+"/+"+toughness+" until end of turn.");
 	}
 
 	@Override
 	public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-		
 		game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],power,toughness));
 	}
 }
