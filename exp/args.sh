c294dde9260f9ccef0d1cd0e36a2989dfea69309
melvin <unknown>
2011-07-13 16:36:59 +0800

    removed old exp framework, added args2

--- exp/args.sh
+++
@@ -1,9 +0,0 @@
-for i in `cat decks`; do 
-for j in `cat AIs`; do 
-for k in `cat decks`; do 
-for l in `cat games`; do 
-echo --deck1 ~/magarena/$i --ai1 $j --deck2 ~/magarena/$k --ai2 VEGAS --games $l; 
-done 
-done 
-done
-done
--- exp/array_mag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -t 1-1000
-#$ -V
-#$ -cwd
-#$ -e /dev/null
-#$ -o /dev/null
-#SGE_TASK_ID=100
-args=`head -$SGE_TASK_ID exp/args | tail -1`
-java -Xmx256M -DrndSeed=$SGE_TASK_ID -cp build magic.DeckStrCal $args > $SGE_TASK_ID.out 2> $SGE_TASK_ID.err
