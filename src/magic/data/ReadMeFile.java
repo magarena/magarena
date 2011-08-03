62fec22d21c0271c034e52c77162b0282ac3fdae
melvin <unknown>
2011-08-03 15:27:54 +0800

    simplified logic

--- src/magic/data/ReadMeFile.java
+++ src/magic/data/ReadMeFile.java
@@ -1,26 +1,25 @@
 package magic.data;
+
 import java.io.*;
 import java.util.Scanner;
 
 import magic.MagicMain;
 
 public class ReadMeFile {
-	private final String fileName;
-	public ReadMeFile(final String fileName){
-			this.fileName=fileName;
-	}
-	public String getDataFromFile() throws IOException{
-		 StringBuilder text = new StringBuilder();
-		 String NL = System.getProperty("line.separator");
-		 Scanner scanner = new Scanner(new FileInputStream(fileName));
-		 try{
-		      while (scanner.hasNextLine()){
-		        text.append(scanner.nextLine() + NL);
-		      }
-		 }
-		 finally{
-		      scanner.close();
-		 } 
-		return text.toString();
-	}
+    private final String fileName;
+    
+    public ReadMeFile(final String fileName){
+        this.fileName=fileName;
+    }
+
+    public String getDataFromFile() throws IOException{
+        StringBuilder text = new StringBuilder();
+        String NL = System.getProperty("line.separator");
+        Scanner scanner = new Scanner(new FileInputStream(fileName));
+        while (scanner.hasNextLine()){
+            text.append(scanner.nextLine() + NL);
+        }
+        scanner.close();
+        return text.toString();
+    }
 }
