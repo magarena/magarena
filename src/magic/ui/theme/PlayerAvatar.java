34ca39e2b1f9d8ede72e8e82c6a23a7eff6f0dbd
melvin <unknown>
2011-08-03 13:27:59 +0800

    simplified the constructor, added exception to getIcon

--- src/magic/ui/theme/PlayerAvatar.java
+++ src/magic/ui/theme/PlayerAvatar.java
@@ -6,32 +6,29 @@ import javax.swing.ImageIcon;
 
 public class PlayerAvatar {
 
-	private static final int LARGE_SIZE=120;
-	private static final int MEDIUM_SIZE=LARGE_SIZE/2;
-	private static final int SMALL_SIZE=LARGE_SIZE/4;
+	private static final int LARGE_SIZE  = 120;
+	private static final int MEDIUM_SIZE = LARGE_SIZE/2;
+	private static final int SMALL_SIZE  = LARGE_SIZE/4;
 
-	private ImageIcon largeIcon = null;
-	private ImageIcon mediumIcon = null;
-	private ImageIcon smallIcon = null;
+	private final ImageIcon largeIcon;
+	private final ImageIcon mediumIcon;
+	private final ImageIcon smallIcon;
 	
 	public PlayerAvatar(final BufferedImage image) {
-        try {
-            final BufferedImage largeImage=magic.GraphicsUtilities.scale(image,LARGE_SIZE,LARGE_SIZE);
-            largeIcon=new ImageIcon(largeImage);
-            final BufferedImage mediumImage=magic.GraphicsUtilities.scale(image,MEDIUM_SIZE,MEDIUM_SIZE);
-            mediumIcon=new ImageIcon(mediumImage);
-            final BufferedImage smallImage=magic.GraphicsUtilities.scale(image,SMALL_SIZE,SMALL_SIZE);
-            smallIcon=new ImageIcon(smallImage);
-        } catch (final Throwable th) {
-            System.err.println("WARNING. Unable to load player avatar");
-        }
+        largeIcon  = new ImageIcon(magic.GraphicsUtilities.scale(
+                     image,LARGE_SIZE,LARGE_SIZE));
+        mediumIcon = new ImageIcon(magic.GraphicsUtilities.scale(
+                     image,MEDIUM_SIZE,MEDIUM_SIZE));
+        smallIcon  = new ImageIcon(magic.GraphicsUtilities.scale(
+                     image,SMALL_SIZE,SMALL_SIZE));
 	}
 	
 	public ImageIcon getIcon(final int size) {
 		switch (size) {
+			case 1: return smallIcon;
 			case 2: return mediumIcon;
 			case 3: return largeIcon;
-			default: return smallIcon;
+            default: throw new RuntimeException("PlayerAvatar.getIcon: invalid size " + size);
 		}
 	}
 }
