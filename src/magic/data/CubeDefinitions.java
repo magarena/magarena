03390b6860f087fe307dfb5210f668402c47972f
melvin <unknown>
2011-08-04 15:25:37 +0800

    use TextFile and Scanner

--- src/magic/data/CubeDefinitions.java
+++ src/magic/data/CubeDefinitions.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import magic.MagicMain;
 import magic.model.MagicCubeDefinition;
@@ -17,14 +18,12 @@ public class CubeDefinitions {
 	
 	private static final String[] INCLUDED_CUBES={"all"};
 	private static final String CUBE_FILE_EXTENSION="_cube.txt";
-
-	public static final String DEFAULT_NAME=INCLUDED_CUBES[0];
+    
+    public static final String DEFAULT_NAME=INCLUDED_CUBES[0];
 	
 	private static final FileFilter CUBE_FILE_FILTER=new FileFilter() {
-
 		@Override
 		public boolean accept(final File file) {
-
 			return file.isFile()&&file.getName().endsWith(CUBE_FILE_EXTENSION);
 		}
 	};
@@ -34,28 +33,22 @@ public class CubeDefinitions {
 	private final List<MagicCubeDefinition> cubeDefinitions;
 	
 	private CubeDefinitions() {
-		
 		cubeDefinitions=new ArrayList<MagicCubeDefinition>();
 		for (final String cubeName : INCLUDED_CUBES) {
-
 			cubeDefinitions.add(new MagicCubeDefinition(cubeName));
 		}
 	}
 			
 	public String[] getCubeNames() {
-		
 		final String names[]=new String[cubeDefinitions.size()];
 		for (int index=0;index<names.length;index++) {
-			
 			names[index]=cubeDefinitions.get(index).getName();
 		}
 		return names;
 	}
 	
 	public MagicCubeDefinition getCubeDefinition(final String name) {
-		
 		for (final MagicCubeDefinition cubeDefinition : cubeDefinitions) {
-			
 			if (cubeDefinition.getName().equals(name)) {
 				return cubeDefinition;
 			}
@@ -63,52 +56,35 @@ public class CubeDefinitions {
 		return cubeDefinitions.get(0);
 	}
 	
-	private void loadCubeDefinition(final String name,final InputStream inputStream) throws IOException {
-		
-		final MagicCubeDefinition cubeDefinition=new MagicCubeDefinition(name);
-		BufferedReader reader=null;
-		try {
-			reader=new BufferedReader(new InputStreamReader(inputStream));
-			while (true) {
-				final String line=reader.readLine();
-				if (line==null) {
-					break;
-				}
-				final String cardName=line.trim();
-				if (!cardName.isEmpty()) {
-					cubeDefinition.add(cardName);
-				}
-			}
-		} finally {
-			if (reader!=null) {
-				try {
-					reader.close();
-				} catch (final IOException ex) {}
-			}
-		}
+	private void loadCubeDefinition(final String name,final File file) {
+        final String content = TextFile.getContents(file);
+        if (content == null) {
+            System.err.println("ERROR! Unable to load " + name);
+            return;
+        }
+        final Scanner sc = new Scanner(content);
+		final MagicCubeDefinition cubeDefinition = new MagicCubeDefinition(name);
+        while (sc.hasNextLine()) {
+            final String cardName = sc.nextLine().trim();
+            if (!cardName.isEmpty()) {
+                cubeDefinition.add(cardName);
+            }
+        }
 		cubeDefinitions.add(cubeDefinition);
 	}
 	
-	public void loadCubeDefinitions() throws IOException {
-				
+	public void loadCubeDefinitions() {
 		final File cubeFiles[]=new File(MagicMain.getModsPath()).listFiles(CUBE_FILE_FILTER);
 		if (cubeFiles!=null) {
 			for (final File file : cubeFiles) {
-				
-				final String name=file.getName();
-				final int index=name.indexOf(CUBE_FILE_EXTENSION);
-                    final InputStream fileInputStream=new FileInputStream(file);
-                try {
-                    loadCubeDefinition(name.substring(0,index),fileInputStream);
-                } finally {
-    				fileInputStream.close();
-                }
+				final String name = file.getName();
+				final int index = name.indexOf(CUBE_FILE_EXTENSION);
+                loadCubeDefinition(name.substring(0,index),file);
 			}
 		}
 		
 		System.err.println(cubeDefinitions.size()+" cube definitions");
 		for (final MagicCubeDefinition cubeDefinition : cubeDefinitions) {
-			
 			System.err.println("Cube "+cubeDefinition.getName()+" : "+cubeDefinition.size()+" cards");
 		}
 	}
