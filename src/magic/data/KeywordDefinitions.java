69554037ebe53041842b58011631ba85e6ba8edf
melvin <unknown>
2011-08-05 12:25:23 +0800

    read the whole file as a string first and then process it

--- src/magic/data/KeywordDefinitions.java
+++ src/magic/data/KeywordDefinitions.java
@@ -1,11 +1,11 @@
 package magic.data;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class KeywordDefinitions {
 
@@ -16,49 +16,46 @@ public class KeywordDefinitions {
 	private final List<KeywordDefinition> keywordDefinitions;
 	
 	private KeywordDefinitions() {
-
 		keywordDefinitions=new ArrayList<KeywordDefinition>();
 	}
 	
-	public void loadKeywordDefinitions() throws IOException {
-		
+	public void loadKeywordDefinitions() {
 		keywordDefinitions.clear();
-		final InputStream stream=this.getClass().getResourceAsStream(KEYWORDS_FILENAME);
-		final BufferedReader reader=new BufferedReader(new InputStreamReader(stream));
-		KeywordDefinition current=null;
-		while (true) {
-			
-			final String line=reader.readLine();
-			if (line==null) {
-				break;
-			}
+        String content = null;
+        try {
+            content = FileIO.toStr(this.getClass().getResourceAsStream(KEYWORDS_FILENAME));
+        } catch (final IOException ex) {
+            System.err.println("ERROR! Unable to load " + KEYWORDS_FILENAME);
+            return;
+        }
+
+        final Scanner sc = new Scanner(content);
+        KeywordDefinition current = null;
+		while (sc.hasNextLine()) {
+			final String line=sc.nextLine();
 			if (line.startsWith("*")) {
-				current=new KeywordDefinition();
+			    current = new KeywordDefinition();
 				current.name=line.substring(1).trim();
 				keywordDefinitions.add(current);
 			} else {
-				if (current.description.length()>0) {
+				if (current.description.length() > 0) {
 					current.description=current.description+"<br>"+line.trim();
 				} else {
 					current.description=line.trim();
 				}
 			}
 		}
- 		reader.close();
 	}
 	
 	public List<KeywordDefinition> getKeywordDefinitions() {
-		
 		return keywordDefinitions;
 	}
 	
 	public static KeywordDefinitions getInstance() {
-		
 		return INSTANCE;
 	}
 	
 	public static class KeywordDefinition {
-
 		public String name;
 		public String description="";
 	}
