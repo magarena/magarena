47d7ad98e3a7c173fad2ad52567b13a8aa01dffc
beholder <unknown>
2011-08-08 14:00:22 +0200

    added Gideon's Lawkeeper

--- src/magic/data/PermanentActivationDefinitions.java
+++ src/magic/data/PermanentActivationDefinitions.java
@@ -1008,6 +1008,42 @@ public class PermanentActivationDefinitions {
 		}
 	};
 	
+	private static final MagicPermanentActivation GIDEONS_LAWKEEPER=new MagicPermanentActivation(
+			"Gideon's Lawkeeper",
+			new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION,MagicManaCost.WHITE.getCondition()},
+			new MagicActivationHints(MagicTiming.Tapping),
+			"Tap") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostTapEvent(source,source.getController(),MagicManaCost.WHITE)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+					source,
+					source.getController(),
+					MagicTargetChoice.NEG_TARGET_CREATURE,
+					new MagicTapTargetPicker(true,false),
+					MagicEvent.NO_DATA,
+					this,
+					"Tap target creature$.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			final MagicPermanent creature=event.getTarget(game,choiceResults,0);
+			if (creature!=null&&!creature.isTapped()) {
+				game.doAction(new MagicTapAction(creature,true));
+			}
+		}
+	};
+				
 	private static final MagicPermanentActivation GLEN_ELENDRA_ARCHMAGE=new MagicPermanentActivation(
 			"Glen Elendra Archmage",
             new MagicCondition[]{MagicManaCost.BLUE.getCondition()},
