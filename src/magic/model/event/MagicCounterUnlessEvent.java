deefcefa25b1db0b31701f934c55e35c6975c5ad
melvin <unknown>
2011-08-10 11:18:36 +0800

    replaced MagicCardOnStack with MagicItemOnStack

--- src/magic/model/event/MagicCounterUnlessEvent.java
+++ src/magic/model/event/MagicCounterUnlessEvent.java
@@ -6,27 +6,33 @@ import magic.model.MagicSource;
 import magic.model.action.MagicCounterItemOnStackAction;
 import magic.model.choice.MagicMayChoice;
 import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.stack.MagicCardOnStack;
+import magic.model.stack.MagicItemOnStack;
 
 public class MagicCounterUnlessEvent extends MagicEvent {
 
 	private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
-	
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object choiceResults[]) {
-			
-			final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object choiceResults[]) {
+			final MagicItemOnStack itemOnStack=(MagicItemOnStack)data[0];
 			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-				event.payManaCost(game,cardOnStack.getController(),choiceResults,1);
+				event.payManaCost(game,itemOnStack.getController(),choiceResults,1);
 			} else {
-				game.doAction(new MagicCounterItemOnStackAction(cardOnStack));
+				game.doAction(new MagicCounterItemOnStackAction(itemOnStack));
 			}
 		}
 	};
 
-	public MagicCounterUnlessEvent(final MagicSource source,final MagicCardOnStack cardOnStack,final MagicManaCost cost) {
-	
-		super(source,cardOnStack.getController(),new MagicMayChoice("You may pay "+cost.getText()+'.',new MagicPayManaCostChoice(cost)),
-			new Object[]{cardOnStack},EVENT_ACTION,"You may$ pay "+cost.getText()+"$. If you don't, counter "+cardOnStack.getName()+".");
+	public MagicCounterUnlessEvent(final MagicSource source,final MagicItemOnStack itemOnStack,final MagicManaCost cost) {
+		super(
+            source,
+            itemOnStack.getController(),
+            new MagicMayChoice("You may pay "+cost.getText()+'.',new MagicPayManaCostChoice(cost)),
+			new Object[]{itemOnStack},
+            EVENT_ACTION,"You may$ pay "+cost.getText()+"$. If you don't, counter "+itemOnStack.getName()+"."
+        );
 	}
 }
