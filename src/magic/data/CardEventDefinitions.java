32af5ce30f4aa65574ae026b90b89163c8aadd9c
melvin <unknown>
2011-08-10 12:35:24 +0800

    wrapped long lines

--- src/magic/data/CardEventDefinitions.java
+++ src/magic/data/CardEventDefinitions.java
@@ -846,14 +846,22 @@ public class CardEventDefinitions {
 
 		@Override
 		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			
-			return new MagicEvent(cardOnStack.getCard(),cardOnStack.getController(),MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-				new MagicDamageTargetPicker(3),new Object[]{cardOnStack},this,"Lightning Bolt deals 3 damage to target creature or player$.");
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(3),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Lightning Bolt deals 3 damage to target creature or player$.");
 		}
 
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
 			final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
 			game.doAction(new MagicMoveCardAction(cardOnStack));
 			final MagicTarget target=event.getTarget(game,choiceResults,0);
