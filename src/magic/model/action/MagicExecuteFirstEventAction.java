9345841aa233065982598433bf5bd171e66723fb
melvin <unknown>
2011-08-10 12:36:22 +0800

    removed trigger hook

--- src/magic/model/action/MagicExecuteFirstEventAction.java
+++ src/magic/model/action/MagicExecuteFirstEventAction.java
@@ -2,9 +2,6 @@ package magic.model.action;
 
 import magic.model.MagicGame;
 import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicTriggerType;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.stack.MagicItemOnStack;
 
 public class MagicExecuteFirstEventAction extends MagicAction {
 
@@ -19,18 +16,10 @@ public class MagicExecuteFirstEventAction extends MagicAction {
 	public void doAction(final MagicGame game) {
 		firstEvent=game.getEvents().removeFirst();
 		game.executeEvent(firstEvent,choiceResults);
-        if (firstEvent.getChoice() instanceof MagicTargetChoice) {
-            final MagicTargetChoice tchoice = (MagicTargetChoice)firstEvent.getChoice();
-            final MagicItemOnStack item = game.getStack().get(firstEvent);
-            if (tchoice.isTargeted() && item != null) {
-                game.executeTrigger(MagicTriggerType.WhenTargeted, item);
-            }
-        }
 	}
 
 	@Override
 	public void undoAction(final MagicGame game) {
-
 		game.getEvents().addFirst(firstEvent);
 	}
 }
