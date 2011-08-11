70e1aba66802fe545fc56ae120061ba86fba55c7
melvin <unknown>
2011-08-11 14:42:31 +0800

    removed card name from constructors, wrapped long lines

--- src/magic/card/Skeletal_Vampire.java
+++ src/magic/card/Skeletal_Vampire.java
@@ -13,8 +13,10 @@ import magic.model.trigger.MagicTriggerType;
 
 public class Skeletal_Vampire {
 
-	public static final MagicPermanentActivation V1738 =new MagicPermanentActivation(            "Skeletal Vampire",
-			new MagicCondition[]{MagicManaCost.THREE_BLACK_BLACK.getCondition(),MagicCondition.CONTROL_BAT_CONDITION},
+	public static final MagicPermanentActivation A1 = new MagicPermanentActivation(
+			new MagicCondition[]{
+                MagicManaCost.THREE_BLACK_BLACK.getCondition(),
+                MagicCondition.CONTROL_BAT_CONDITION},
 			new MagicActivationHints(MagicTiming.Token,true),
             "Token") {
 
@@ -27,20 +29,30 @@ public class Skeletal_Vampire {
 		}
 
 		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+		public MagicEvent getPermanentEvent(
+                final MagicPermanent source,
+                final MagicPayedCost payedCost) {
 			final MagicPlayer player=source.getController();
-			return new MagicEvent(source,player,new Object[]{player},this,"Put two 1/1 black Bat creature tokens with flying onto the battlefield.");
+			return new MagicEvent(
+                    source,
+                    player,
+                    new Object[]{player},
+                    this,
+                    "Put two 1/1 black Bat creature tokens with flying onto the battlefield.");
 		}
 
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
 			final MagicPlayer player=(MagicPlayer)data[0];
 			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));
-			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));			
-		}
+			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));			}
 	};
 
-	public static final MagicPermanentActivation V1767 =new MagicPermanentActivation(            "Skeletal Vampire",
+	public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
 			new MagicCondition[]{
                 MagicCondition.CAN_REGENERATE_CONDITION,
                 MagicCondition.CONTROL_BAT_CONDITION,
@@ -50,36 +62,59 @@ public class Skeletal_Vampire {
 
 		@Override
 		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicSacrificePermanentEvent(source,source.getController(),MagicTargetChoice.SACRIFICE_BAT)};
+			return new MagicEvent[]{
+                new MagicSacrificePermanentEvent(
+                        source,
+                        source.getController(),
+                        MagicTargetChoice.SACRIFICE_BAT)};
 		}
 
 		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			return new MagicEvent(source,source.getController(),new Object[]{source},this,"Regenerate Skeletal Vampire.");
+		public MagicEvent getPermanentEvent(
+                final MagicPermanent source,
+                final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Regenerate Skeletal Vampire.");
 		}
 
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
 			game.doAction(new MagicRegenerateAction((MagicPermanent)data[0]));
 		}
 	};
 	
-    public static final MagicTrigger V8739 =new MagicTrigger(MagicTriggerType.WhenComesIntoPlay,"Skeletal Vampire") {
-
+    public static final MagicTrigger T1 = new MagicTrigger(MagicTriggerType.WhenComesIntoPlay) {
 		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
-
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final Object data) {
 			final MagicPlayer player=permanent.getController();
-			return new MagicEvent(permanent,player,new Object[]{player},this,"You put two 1/1 black Bat creature tokens with flying onto the battlefield.");
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{player},
+                    this,
+                    "You put two 1/1 black Bat creature tokens with flying onto the battlefield.");
 		}
 		
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
 			final MagicPlayer player=(MagicPlayer)data[0];
-			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));			
+			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));
 			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.BAT_TOKEN_CARD));
 		}		
     };
-        
 }
