b3331fc50e011eef3daaca71fd711242ddeabcb1
melvin <unknown>
2011-08-11 22:50:15 +0800

    removed card name from constructor

--- src/magic/card/Bitterblossom.java
+++ src/magic/card/Bitterblossom.java
@@ -11,27 +11,29 @@ import magic.model.trigger.MagicTrigger;
 import magic.model.trigger.MagicTriggerType;
 
 public class Bitterblossom {
-
-    public static final MagicTrigger V10003 =new MagicTrigger(MagicTriggerType.AtUpkeep,"Bitterblossom") {
-
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.AtUpkeep) {
 		@Override
 		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
-						
 			final MagicPlayer player=permanent.getController();
 			if (player==data) {
-				return new MagicEvent(permanent,player,new Object[]{player},this,
-					"You lose 1 life and put a 1/1 black Faerie Rogue creature token with flying onto the battlefield.");
+				return new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        "You lose 1 life and put a 1/1 black Faerie Rogue creature token with flying onto the battlefield.");
 			}
 			return null;
 		}
-		
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
 			final MagicPlayer player=(MagicPlayer)data[0];
 			game.doAction(new MagicChangeLifeAction(player,-1));
 			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.FAERIE_ROGUE_TOKEN_CARD));
 		}		
     };
-    
 }
