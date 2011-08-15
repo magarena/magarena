7e8e01a3092dc284e2e88ab9700c43691ed23eda
beholder <unknown>
2011-08-15 10:41:53 +0200

    added test class for Nantuko Shade. demonstrates issue 43

---
+++ src/magic/test/TestNantukoShade.java
@@ -0,0 +1,43 @@
+package magic.test;
+
+import magic.ai.MCTSAI;
+import magic.ai.MagicAI;
+import magic.model.*;
+import magic.model.phase.MagicMainPhase;
+
+class TestNantukoShade extends TestGameBuilder {    
+   
+    public MagicGame getGame() {
+        final MagicTournament tournament=new MagicTournament();
+        tournament.setDifficulty(6);
+        
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        tournament.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        tournament.setStartPlayer(0);
+        //tournament.setAIs(new MagicAI[]{null, new MCTSAI(true, true)});
+        
+        final MagicGame game=tournament.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = opponent;
+
+        P.setLife(2);
+        addToLibrary(P,"Swamp",10);
+        createPermanent(game,P,"Swamp",false,4);
+        createPermanent(game,P,"Nantuko Shade",false,1);
+
+        P = player;
+        
+        P.setLife(5);
+        addToLibrary(P,"Plains",10);
+        createPermanent(game,P,"Plains",true,4);
+        //createPermanent(game,P,"Siege Mastodon",false,1);
+        addToHand(P,"Wall of Reverence",1);
+        
+        return game;
+    }
+}
