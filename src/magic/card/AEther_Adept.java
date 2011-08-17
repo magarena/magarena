30a0b9b46729e74fadee5e84645ba16250fef87f
beholder <unknown>
2011-08-17 20:11:39 +0200

    added AEther Adept (Æther Adept)

---
+++ src/magic/card/AEther_Adept.java
@@ -0,0 +1,35 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicBounceTargetPicker;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class AEther_Adept {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenComesIntoPlay) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			return new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    MagicTargetChoice.TARGET_CREATURE,
+                    MagicBounceTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Return target creature$ to its owner's hand.");
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			final MagicPermanent creature = event.getTarget(game,choiceResults,0);
+			if (creature != null) {
+				game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+			}
+		}
+    };
+}
