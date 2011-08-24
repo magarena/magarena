962ebe31790a0cacb3f732bd19ef94a2022b1bff
beholder <unknown>
2011-08-24 14:26:04 +0200

    added Blood Seeker

---
+++ src/magic/card/Blood_Seeker.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Blood_Seeker {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenOtherComesIntoPlay) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			final MagicPermanent otherPermanent=(MagicPermanent)data;
+            final MagicPlayer player=permanent.getController();
+            final MagicPlayer controller=otherPermanent.getController();
+            //final boolean same=controller==player;
+			return (otherPermanent!=permanent&&otherPermanent.isCreature()&&controller!=player) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{controller},
+                    this,
+                    controller + " loses 1 life."):
+                null;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],-1));
+		}		
+    };
+}
