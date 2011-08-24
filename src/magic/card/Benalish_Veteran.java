a128fa60f49f76f5aec70c9d036cadc4ece09001
beholder <unknown>
2011-08-24 10:21:21 +0200

    added Benalish Veteran

---
+++ src/magic/card/Benalish_Veteran.java
@@ -0,0 +1,32 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Benalish_Veteran {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenAttacks) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			return (permanent == data) ?
+                new MagicEvent(
+                        permanent,
+                        permanent.getController(),
+                        new Object[]{permanent},
+                        this,
+                        permanent + " gets +1/+1 until end of turn."):
+                null;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],1,1));
+		}
+    };
+}
