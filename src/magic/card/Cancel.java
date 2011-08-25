0f59074ce70ff29f7fcdc3b5da8008a859824fcb
beholder <unknown>
2011-08-25 10:28:25 +0200

    added Cancel

---
+++ src/magic/card/Cancel.java
@@ -0,0 +1,39 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicCounterItemOnStackAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Cancel {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_SPELL,
+                    new Object[]{cardOnStack},
+                    this,
+                    "Counter target spell$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicCardOnStack targetSpell = event.getTarget(game,choiceResults,0);
+			if (targetSpell != null) {
+				game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+			}
+		}
+	};
+}
