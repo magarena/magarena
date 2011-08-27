003e723d0c675574339c9eeefab64fc0f0a55b50
beholder <unknown>
2011-08-27 17:21:29 +0200

    added Goblin Fireslinger

---
+++ src/magic/card/Goblin_Fireslinger.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.*;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.*;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+
+public class Goblin_Fireslinger {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Damage") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(1),
+                    new Object[]{source},
+                    this,
+                    source + " deals 1 damage to target player$.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+			final MagicTarget target=event.getTarget(game,choiceResults,0);
+			if (target!=null) {
+				final MagicDamage damage=new MagicDamage((MagicSource)data[0],target,1,false);
+				game.doAction(new MagicDealDamageAction(damage));
+			}
+		}
+	};
+}
