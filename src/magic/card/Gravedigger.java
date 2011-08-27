c20536996e6f8713ac7abe61114727fc664e1f35
beholder <unknown>
2011-08-27 20:57:36 +0200

    added Gravedigger

---
+++ src/magic/card/Gravedigger.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicGraveyardTargetPicker;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Gravedigger {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenComesIntoPlay) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			return new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+	                new MagicMayChoice(
+                        "You may return target creature card from your graveyard to your hand.",
+                        MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD),
+                    MagicGraveyardTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "You may$ return target creature card$ from your graveyard to your hand.");
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				final MagicCard card=event.getTarget(game,choiceResults,1);		
+				if (card!=null) {
+					game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+					game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+				}
+			}
+		}
+    };
+}
