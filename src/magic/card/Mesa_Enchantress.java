eb2d2eae9e4c6e444795d5deb4d1633606f35f81
beholder <unknown>
2011-08-28 13:32:06 +0200

    added Mesa Enchantress and Mind Rot

---
+++ src/magic/card/Mesa_Enchantress.java
@@ -0,0 +1,35 @@
+package magic.card;
+
+import magic.model.*;
+import magic.model.action.MagicDrawAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Mesa_Enchantress {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenSpellIsPlayed) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			final MagicPlayer player=permanent.getController();
+			final MagicCard card=((MagicCardOnStack)data).getCard();
+			return (card.getOwner() == player && card.getCardDefinition().isEnchantment()) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new MagicMayChoice("You may draw a card."),
+                        new Object[]{player},
+                        this,
+                        "You may$ draw a card."):
+                null;
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+			}
+		}
+    };
+}
---
+++ src/magic/card/Mind_Rot.java
@@ -0,0 +1,39 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Mind_Rot {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target player$ discards two cards");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			final MagicPlayer player = event.getTarget(game,choiceResults,0);
+			if (player != null) {
+				game.addEvent(new MagicDiscardEvent(cardOnStack.getCard(),player,2,false));
+			}
+		}
+	};
+}
