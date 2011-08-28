dc08c81039648305ebe8f551efecf35e69b3c3b5
beholder <unknown>
2011-08-28 15:11:54 +0200

    added Pride Guardian

---
+++ src/magic/card/Pride_Guardian.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Pride_Guardian {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenBlocks) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			if (permanent == data) {
+				if (permanent.getBlockedCreature() != null) {
+					final MagicPlayer player = permanent.getController();
+					return new MagicEvent(
+                            permanent,
+                            player,
+                            new Object[]{player},
+                            this,
+                            player + " gains 3 life.");
+				}
+			}
+			return null;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],3));
+		}
+    };
+}
