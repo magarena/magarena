d8577f0d301f084a0b909ba77b5f61046f72b235
melvin <unknown>
2011-08-29 15:13:18 +0800

    wrapped long lines

--- src/magic/model/event/MagicPayManaCostEvent.java
+++ src/magic/model/event/MagicPayManaCostEvent.java
@@ -8,14 +8,23 @@ import magic.model.choice.MagicPayManaCostChoice;
 
 public class MagicPayManaCostEvent extends MagicEvent {
 	private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
-		
 		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
 			event.payManaCost(game,(MagicPlayer)data[0],choiceResults,0);
 		}		
 	};
 	
 	public MagicPayManaCostEvent(final MagicSource source,final MagicPlayer player,final MagicManaCost cost) {
-		super(source,player,new MagicPayManaCostChoice(cost),new Object[]{player},EVENT_ACTION,"Pay "+cost.getText()+"$.");
-	}			
+		super(
+            source,
+            player,
+            new MagicPayManaCostChoice(cost),
+            new Object[]{player},
+            EVENT_ACTION,
+            "Pay "+cost.getText()+"$.");
+	}
 }
