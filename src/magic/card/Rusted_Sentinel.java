370e82ef0a714c289bbf9b5d3077fe97663180aa
beholder <unknown>
2011-08-29 15:13:21 +0200

    added Rusted Sentinel

---
+++ src/magic/card/Rusted_Sentinel.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicTappedIntoPlayTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Rusted_Sentinel {
+    public static final MagicTrigger T = new MagicTappedIntoPlayTrigger();
+}
