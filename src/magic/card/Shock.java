fc977e69f281e2a5b473b8c54c03e5738f61a912
beholder <unknown>
2011-08-29 15:20:47 +0200

    added Shock

---
+++ src/magic/card/Shock.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+
+public class Shock {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(2),
+                    new Object[]{cardOnStack},
+                    this,
+                    cardOnStack.getCard() + " deals 2 damage to target creature or player$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			final MagicTarget target = event.getTarget(game,choiceResults,0);
+			if (target != null) {
+				final MagicDamage damage = new MagicDamage(cardOnStack.getCard(),target,2,false);
+				game.doAction(new MagicDealDamageAction(damage));
+			}
+		}
+	};
+}
