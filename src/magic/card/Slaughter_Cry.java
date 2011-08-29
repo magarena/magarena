b7eff44586b0d5560f9a2f7cb715c4205b3b9e9c
beholder <unknown>
2011-08-29 21:08:47 +0200

    added Skywinder Drake and Slaughter Cry

---
+++ src/magic/card/Slaughter_Cry.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicFirstStrikeTargetPicker;
+
+public class Slaughter_Cry {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicFirstStrikeTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets +3/+0 and gains first strike until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPermanent creature=event.getTarget(game,choiceResults,0);
+			if (creature!=null) {
+				game.doAction(new MagicChangeTurnPTAction(creature,3,0));
+				game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike));
+			}
+		}
+	};
+}
