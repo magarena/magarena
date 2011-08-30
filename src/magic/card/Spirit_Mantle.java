f77c585811f2b431f45f52f94da6ca11c8eac737
beholder <unknown>
2011-08-30 08:17:57 +0200

    added Spirit Mantle

---
+++ src/magic/card/Spirit_Mantle.java
@@ -0,0 +1,12 @@
+package magic.card;
+
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicPlayAuraEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.target.MagicPumpTargetPicker;
+
+public class Spirit_Mantle {
+	public static final MagicSpellCardEvent S = new MagicPlayAuraEvent(
+			MagicTargetChoice.POS_TARGET_CREATURE,
+            MagicPumpTargetPicker.getInstance());
+}
