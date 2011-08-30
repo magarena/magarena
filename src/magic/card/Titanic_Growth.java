80c21037e8a225e8e16c08f9d3d173a4534e5310
beholder <unknown>
2011-08-30 11:18:03 +0200

    added Titanic Growth

---
+++ src/magic/card/Titanic_Growth.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicPumpTargetPicker;
+
+public class Titanic_Growth {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets +4/+4 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			final MagicPermanent creature = event.getTarget(game,choiceResults,0);
+			if (creature != null) {
+				game.doAction(new MagicChangeTurnPTAction(creature,4,4));
+			}
+		}
+	};
+}
