d94b995db6c25fba2aa6c1179c582801097c02f5
beholder <unknown>
2011-08-30 14:02:01 +0200

    added Wall of Torches and Warstorm Surge

---
+++ src/magic/card/Warstorm_Surge.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.*;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Warstorm_Surge {
+	public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenOtherComesIntoPlay) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			final MagicPermanent otherPermanent = (MagicPermanent)data;
+			final MagicPlayer player = permanent.getController();
+            return (otherPermanent.isCreature() && otherPermanent.getController() == player) ?
+				new MagicEvent(
+                        permanent,
+                        player,
+                        MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                        new MagicDamageTargetPicker(otherPermanent.getPower(game)),
+                        new Object[]{otherPermanent},
+                        this,
+                        "Whenever a creature enters the battlefield under your control, " +
+                        "it deals damage equal to its power to target creature or player$.") :
+                null;
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			final MagicTarget target = event.getTarget(game,choiceResults,0);
+			if (target != null) {
+				final MagicDamage damage = new MagicDamage(permanent,target,permanent.getPower(game),false);
+				game.doAction(new MagicDealDamageAction(damage));
+			}
+		}	
+    };
+}
