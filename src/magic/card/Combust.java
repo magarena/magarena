f4f9ee6aca24c04725b6322bdd743a676cb9df13
beholder <unknown>
2011-08-30 17:10:43 +0200

    added Combust

---
+++ src/magic/card/Combust.java
@@ -0,0 +1,51 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicCard;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDamageTargetPicker;
+
+public class Combust {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+            final MagicCard card = cardOnStack.getCard();
+			return new MagicEvent(
+                    card,
+                    player,
+                    MagicTargetChoice.NEG_WHITE_OR_BLUE_CREATURE,
+                    new MagicDamageTargetPicker(5,true),
+				    new Object[]{cardOnStack},
+                    this,
+                    card + " deals 5 damage to target white or blue creature$. " +
+                    		"The damage can't be prevented.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+				public void doAction(final MagicPermanent target) {
+					final MagicDamage damage = new MagicDamage(cardOnStack.getCard(),target,5,false);
+					damage.setUnpreventable();
+					game.doAction(new MagicDealDamageAction(damage));
+				}
+			});
+		}
+	};
+}
