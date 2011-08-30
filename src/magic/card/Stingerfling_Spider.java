0301e63e49e48387ec3b754c794f4bac635cd808
beholder <unknown>
2011-08-30 08:41:43 +0200

    added Stingerfling Spider

---
+++ src/magic/card/Stingerfling_Spider.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDestroyAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDestroyTargetPicker;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Stingerfling_Spider {
+	public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenComesIntoPlay) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			return new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new MagicMayChoice(
+                            "You may destroy target creature with flying.",
+                            MagicTargetChoice.NEG_TARGET_CREATURE_WITH_FLYING),
+                    new MagicDestroyTargetPicker(false),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "You may$ destroy target creature$ with flying.");
+		}
+                	
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				final MagicPermanent permanent = event.getTarget(game,choiceResults,1);
+				if (permanent != null) {
+					game.doAction(new MagicDestroyAction(permanent));
+				}
+			}
+		}
+    };
+}
