a24c2c6145cef850602d1394463bc774b3849659
beholder <unknown>
2011-08-30 14:43:04 +0200

    added Zombie Infestation

--- src/magic/model/condition/MagicCondition.java
+++ src/magic/model/condition/MagicCondition.java
@@ -44,6 +44,12 @@ public interface MagicCondition {
 		}
 	};
 	
+	public static final MagicCondition HAS_TWO_CARDS_CONDITION = new MagicCondition() {
+		public boolean accept(final MagicGame game,final MagicSource source) {
+			return source.getController().getHandSize() > 1;
+		}
+	};
+	
 	public static final MagicCondition CAN_TAP_CONDITION=new MagicCondition() {
 		public boolean accept(final MagicGame game,final MagicSource source) {
 			final MagicPermanent permanent=(MagicPermanent)source;
