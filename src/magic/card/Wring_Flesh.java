f4b26dad9398fae0913925641e2294aec8f22a44
beholder <unknown>
2011-08-30 14:19:11 +0200

    added Wring Flesh

---
+++ src/magic/card/Wring_Flesh.java
@@ -0,0 +1,40 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicWeakenTargetPicker;
+
+public class Wring_Flesh {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    new MagicWeakenTargetPicker(3,1),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets -3/-1 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPermanent creature = event.getTarget(game,choiceResults,0);
+			if (creature != null) {
+				game.doAction(new MagicChangeTurnPTAction(creature,-3,-1));
+			}
+		}
+	};
+}
