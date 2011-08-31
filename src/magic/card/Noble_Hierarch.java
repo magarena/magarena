a0e76825631e0cae173d6ecc7e6c4a831c058c7c
melvin <unknown>
2011-08-31 22:22:40 +0800

    typo in companion class for Noble Hierarch, was misspelled as Nobel_Hierarch
    fixes issue 46.

--- src/magic/card/Noble_Hierarch.java
+++ src/magic/card/Noble_Hierarch.java
@@ -8,7 +8,7 @@ import magic.model.event.MagicTapManaActivation;
 
 import java.util.Arrays;
 
-public class Nobel_Hierarch {
+public class Noble_Hierarch {
 
     public static final MagicManaActivation V1 = new MagicTapManaActivation(
             Arrays.asList(MagicManaType.Colorless, MagicManaType.Blue, MagicManaType.Green, MagicManaType.White), 1);
