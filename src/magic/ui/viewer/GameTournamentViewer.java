167ab74fd5e99e72d0bdefed0886d1e106a41963
melvin <unknown>
2011-08-31 12:57:57 +0800

    replaced empty string + int with Integer.toString

--- src/magic/ui/viewer/GameTournamentViewer.java
+++ src/magic/ui/viewer/GameTournamentViewer.java
@@ -61,7 +61,7 @@ public class GameTournamentViewer extends JPanel implements ChangeListener {
 	@Override
 	public void stateChanged(ChangeEvent e) {
 		final int selectedTab=tabSelector.getSelectedTab();
-		cardLayout.show(cardPanel,""+selectedTab);
+		cardLayout.show(cardPanel,Integer.toString(selectedTab));
 		update();
 	}
 }
