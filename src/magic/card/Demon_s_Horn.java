1f742644bf306387ef79a79f1349a6fd64ada461
beholder <unknown>
2011-09-01 09:41:46 +0200

    added 5 "lucky charms", Angel's Feather, Demon's Horn, Dragon's Claw, Kraken's Eye, Wurm's Tooth.

---
+++ src/magic/card/Demon_s_Horn.java
@@ -0,0 +1,39 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicColor;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Demon_s_Horn {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenSpellIsPlayed) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final Object data) {
+			final MagicPlayer player = permanent.getController();
+			final MagicCard card = ((MagicCardOnStack)data).getCard();
+			return (MagicColor.Black.hasColor(card.getColorFlags())) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new MagicMayChoice("You may gain 1 life."),
+                        new Object[]{player},
+                        this,
+                        "You may$ gain 1 life.") :
+                null;
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],1));
+			}
+		}
+    };
+}
