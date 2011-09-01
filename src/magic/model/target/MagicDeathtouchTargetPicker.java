0f48ebf30a73fae556f71e9db378986eb18b7fce
beholder <unknown>
2011-09-01 08:01:37 +0200

    changed logic of lifeLinkTargetPicker. added comments

--- src/magic/model/target/MagicDeathtouchTargetPicker.java
+++ src/magic/model/target/MagicDeathtouchTargetPicker.java
@@ -9,7 +9,7 @@ public class MagicDeathtouchTargetPicker extends MagicTargetPicker {
 
 	private static final MagicTargetPicker INSTANCE = new MagicDeathtouchTargetPicker();
 	
-	private static final int ATTACKING = 4<<8;
+	private static final int BLOCKED = 4<<8;
 	private static final int BLOCKING = 3<<8;
 	private static final int CAN_TAP = 2<<8;
 	private static final int FIRST_STRIKE = 1<<8;
@@ -24,22 +24,26 @@ public class MagicDeathtouchTargetPicker extends MagicTargetPicker {
 		final long flags = permanent.getAllAbilityFlags(game);
 		int score = 0;
 
+		// no score for ability overlap or not being able to deal combat damage
 		if (MagicAbility.Deathtouch.hasAbility(flags) ||
 			MagicAbility.CannotAttackOrBlock.hasAbility(flags)) {
 			return 0;
 		}
-		
-		
+			
 		if (permanent.isBlocked()) {
-			score = ATTACKING;
+			score = BLOCKED;
+			// possibility to destroy more than one blocker
 		} else if (permanent.isBlocking()) {
+			// good chance to destroy attacker
 			score = BLOCKING;
 		} else if (permanent.canTap(game)) {
+			// can be in combat later or possibly use a damage ability
 			score = CAN_TAP;
 		} 
 
 		if (MagicAbility.FirstStrike.hasAbility(flags) ||
 			MagicAbility.DoubleStrike.hasAbility(flags)) {
+			// higher chance to deal combat damage
 			score += FIRST_STRIKE;
 		}
 		
--- src/magic/model/target/MagicLifelinkTargetPicker.java
+++ src/magic/model/target/MagicLifelinkTargetPicker.java
@@ -10,10 +10,10 @@ public class MagicLifelinkTargetPicker extends MagicTargetPicker {
 	private static final MagicTargetPicker INSTANCE = new MagicLifelinkTargetPicker();
 	
 	private static final int ATTACKING_UNBLOCKED = 5<<8;
-	private static final int ATTACKING = 4<<8;
-	private static final int BLOCKING = 3<<8;
-	private static final int CAN_TAP = 2<<8;
-	private static final int DOUBLE_STRIKE = 1<<8;
+	private static final int BLOCKED_OR_BLOCKING = 4<<8;
+	private static final int CAN_TAP = 3<<8;
+	private static final int DOUBLE_STRIKE = 2<<8;
+	private static final int FIRST_STRIKE = 1<<8;
 	
 	private MagicLifelinkTargetPicker() {
 	}
@@ -21,29 +21,39 @@ public class MagicLifelinkTargetPicker extends MagicTargetPicker {
 	@Override
 	protected int getTargetScore(final MagicGame game,final MagicPlayer player,final Object target) {
 
-		final MagicPermanent permanent=(MagicPermanent)target;
+		final MagicPermanent permanent = (MagicPermanent)target;
 		final long flags = permanent.getAllAbilityFlags(game);
 		int score = 0;
 
+		// no score for ability overlap or not being able to deal combat damage
 		if (MagicAbility.LifeLink.hasAbility(flags) ||
 			MagicAbility.CannotAttackOrBlock.hasAbility(flags)) {
 			return 0;
 		}
 		
 		if (permanent.isAttacking()) {
-			if (permanent.isBlocked()) {
-				score = ATTACKING;
-			} else {
+			if (!permanent.isBlocked()) {
+				// unblocked attacker has the highest chance of gaining life
 				score = ATTACKING_UNBLOCKED;
+			} else {
+				// possible to not gain life when blocker has first strike
+				score = BLOCKED_OR_BLOCKING;
 			}
 		} else if (permanent.isBlocking()) {
-			score = BLOCKING;
+			// possible to not gain life when attacker has first strike
+			score = BLOCKED_OR_BLOCKING;
 		} else if (permanent.canTap(game)) {
+			// can be in combat later or possibly use a damage ability
 			score = CAN_TAP;
 		} 
 
 		if (MagicAbility.DoubleStrike.hasAbility(flags)) {
+			// chance to deal combat damage twice
 			score += DOUBLE_STRIKE;
+		}
+		if (MagicAbility.FirstStrike.hasAbility(flags)) {
+			// higher chance to deal combat damage
+			score += FIRST_STRIKE;
 		}
 		
 		return permanent.getPower(game) + score;
