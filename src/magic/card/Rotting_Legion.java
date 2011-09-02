98c01829cbdbae65c9f35028f0e5b68e5374716c
beholder <unknown>
2011-09-02 11:18:33 +0200

    added Prodigal Pyromancer and Rotting Legion

---
+++ src/magic/card/Rotting_Legion.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicTappedIntoPlayTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Rotting_Legion {
+    public static final MagicTrigger T = new MagicTappedIntoPlayTrigger();
+}
