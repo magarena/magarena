5b03db020780388ed3bbd753c1502c2d5254f1c0
beholder <unknown>
2011-09-02 11:35:30 +0200

    added Scroll Thief

---
+++ src/magic/card/Scroll_Thief.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class Scroll_Thief {
+    public static final MagicTrigger T = new MagicTrigger(MagicTriggerType.WhenDamageIsDealt) {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+			final MagicPlayer player = permanent.getController();
+			return (damage.getSource() == permanent &&
+					damage.getTarget().isPlayer() &&
+					damage.isCombat()) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        player + " draws a card."):
+                null;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+		}
+    };
+}
