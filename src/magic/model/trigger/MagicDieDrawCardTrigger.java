cd54e525715b1e31420f0624d4f56d1d01f79c14
melvin <unknown>
2011-09-02 14:46:21 +0800

    extend more specific subclass of MagicTrigger

--- src/magic/model/trigger/MagicDieDrawCardTrigger.java
+++ src/magic/model/trigger/MagicDieDrawCardTrigger.java
@@ -7,22 +7,20 @@ import magic.model.event.MagicDrawEvent;
 import magic.model.event.MagicEvent;
 
 //When C is put into a graveyard from the battlefield, you draw a card.
-public class MagicDieDrawCardTrigger {
-	public static final MagicTrigger INSTANCE = new MagicTrigger(MagicTriggerType.WhenPutIntoGraveyard) {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
-            if (MagicLocationType.Play==triggerData.fromLocation) {
-                return new MagicDrawEvent(permanent,permanent.getController(),1);
-            }
-            return null;
+public class MagicDieDrawCardTrigger extends MagicWhenPutIntoGraveyardTrigger {
+    @Override
+    public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+        if (MagicLocationType.Play==triggerData.fromLocation) {
+            return new MagicDrawEvent(permanent,permanent.getController(),1);
         }
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-            //do nothing
-        }
-    };
+        return null;
+    }
+    @Override
+    public void executeEvent(
+            final MagicGame game,
+            final MagicEvent event,
+            final Object data[],
+            final Object[] choiceResults) {
+        //do nothing
+    }
 }
