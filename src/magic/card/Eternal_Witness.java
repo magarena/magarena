92719d727c08a33e5c619cafe027960c6151eed6
beholder <unknown>
2011-09-03 17:27:27 +0200

    added Eternal Witness

---
+++ src/magic/card/Eternal_Witness.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicCardAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicGraveyardTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+
+public class Eternal_Witness {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+	                new MagicMayChoice(
+                        "You may return target card from your graveyard to your hand.",
+                        MagicTargetChoice.TARGET_CARD_FROM_GRAVEYARD),
+                    MagicGraveyardTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "You may$ return target card$ from your graveyard to your hand.");
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+                event.processTargetCard(game,choiceResults,1,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+					game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+					game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+				}
+                });
+			}
+		}
+    };
+}
