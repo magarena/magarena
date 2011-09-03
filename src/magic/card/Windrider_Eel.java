361ddde1aab685a1df9ed1a2f777b3f44a7170bb
beholder <unknown>
2011-09-03 12:02:46 +0200

    added Windrider Eel

---
+++ src/magic/card/Windrider_Eel.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
+
+public class Windrider_Eel {
+    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent played) {
+			final MagicPlayer player = permanent.getController();
+			if (player == played.getController() && 
+                played.isLand()) {
+				return new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{permanent},
+                        this,
+                        permanent + " gets +2/+2 until end of turn.");
+			} else {
+    			return null;
+            }
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],2,2));
+		}		
+    };
+}
