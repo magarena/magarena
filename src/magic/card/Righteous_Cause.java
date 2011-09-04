fd93ea25d14c17b210abcd2b9beef35d4bd9c483
beholder <unknown>
2011-09-04 19:56:10 +0200

    added Righteous Cause

---
+++ src/magic/card/Righteous_Cause.java
@@ -0,0 +1,33 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+
+public class Righteous_Cause {
+    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+			final MagicPlayer player = permanent.getController();
+			return new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        player + " gains 1 life.");
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],1));
+		}
+    };
+}
