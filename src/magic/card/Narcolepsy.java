162b6eaf644a7cee1aaaa0dc107bc3c551a8b9bf
melvin <unknown>
2011-09-04 15:38:11 +0800

    replaced null with MagicPermanent.NONE

--- src/magic/card/Narcolepsy.java
+++ src/magic/card/Narcolepsy.java
@@ -21,7 +21,7 @@ public class Narcolepsy {
 		@Override
 		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
 			final MagicPermanent enchantedCreature=permanent.getEnchantedCreature();
-            return (enchantedCreature!=null&&!enchantedCreature.isTapped()) ?
+            return (enchantedCreature!=MagicPermanent.NONE&&!enchantedCreature.isTapped()) ?
                 new MagicEvent(
                         permanent,
                         permanent.getController(),
@@ -38,7 +38,7 @@ public class Narcolepsy {
                 final Object[] choiceResults) {
 			final MagicPermanent permanent=(MagicPermanent)data[0];
 			final MagicPermanent enchantedCreature=permanent.getEnchantedCreature();
-			if (enchantedCreature!=null&&!enchantedCreature.isTapped()) {
+			if (enchantedCreature!=MagicPermanent.NONE&&!enchantedCreature.isTapped()) {
 				game.doAction(new MagicTapAction(enchantedCreature,true));
 			}
 		}		
