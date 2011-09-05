720dbf2d99b90813a50dde555163fc3af8865353
beholder <unknown>
2011-09-05 17:37:40 +0200

    added Vision Skeins

---
+++ src/magic/card/Vision_Skeins.java
@@ -0,0 +1,35 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Vision_Skeins {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    new Object[]{cardOnStack,},
+                    this,
+                    "Each player draws two cards.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			for (final MagicPlayer player : game.getPlayers()) {
+				game.doAction(new MagicDrawAction(player,2));
+			}
+		}
+	};
+}
