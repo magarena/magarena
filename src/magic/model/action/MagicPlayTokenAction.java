24a07b5ca5f326af28d222ea7610761e64f3d599
melvin <unknown>
2011-09-06 17:57:09 +0800

    removed empty lines

--- src/magic/model/action/MagicPlayTokenAction.java
+++ src/magic/model/action/MagicPlayTokenAction.java
@@ -11,13 +11,11 @@ public class MagicPlayTokenAction extends MagicPutIntoPlayAction {
 	private final MagicCard card;
 
 	public MagicPlayTokenAction(final MagicPlayer player,final MagicCardDefinition cardDefinition) {
-
 		card=MagicCard.createTokenCard(cardDefinition,player);
 	}
 
 	@Override
 	protected MagicPermanent createPermanent(final MagicGame game) {
-
 		return game.createPermanent(card,card.getController());
 	}
 }
