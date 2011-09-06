41aadcc2d59de4c37607b397b860b5a6196882aa
melvin <unknown>
2011-09-06 15:48:17 +0800

    create MagicEventAction in new MagicEvent

--- src/magic/model/trigger/MagicTappedIntoPlayTrigger.java
+++ src/magic/model/trigger/MagicTappedIntoPlayTrigger.java
@@ -5,23 +5,22 @@ import magic.model.MagicPlayer;
 import magic.model.MagicPermanent;
 import magic.model.action.MagicTapAction;
 import magic.model.event.MagicEvent;
+import magic.model.event.MagicEventAction;
 
 public class MagicTappedIntoPlayTrigger extends MagicWhenComesIntoPlayTrigger {
 	@Override
 	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
 		return new MagicEvent(
-                permanent,
-                permanent.getController(),
-                new Object[]{permanent},
-                this,
-                permanent + " enters the battlefield tapped.");
+            permanent,
+            permanent.getController(),
+            MagicEvent.NO_DATA,
+            new MagicEventAction() {
+            @Override
+            public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
+                game.doAction(new MagicTapAction(permanent.map(game),false));
+            }},
+            permanent + " enters the battlefield tapped.");
 	}
-	
-	@Override
-	public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-		game.doAction(new MagicTapAction((MagicPermanent)data[0],false));
-	}
-	
 	@Override
 	public boolean usesStack() {
 		return false;
