a02da355f26f1191d32c061b9d0dd32919a594a4
melvin <unknown>
2011-09-07 12:45:49 +0800

    create MagicEventAction in new MagicEvent

--- src/magic/model/trigger/MagicTappedIntoPlayUnlessTrigger.java
+++ src/magic/model/trigger/MagicTappedIntoPlayUnlessTrigger.java
@@ -6,6 +6,7 @@ import magic.model.MagicPlayer;
 import magic.model.MagicSubType;
 import magic.model.action.MagicTapAction;
 import magic.model.event.MagicEvent;
+import magic.model.event.MagicEventAction;
 
 public class MagicTappedIntoPlayUnlessTrigger extends MagicWhenComesIntoPlayTrigger {
 
@@ -21,17 +22,20 @@ public class MagicTappedIntoPlayUnlessTrigger extends MagicWhenComesIntoPlayTrig
 	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
 		return (!player.controlsPermanentWithSubType(subType1)&&!player.controlsPermanentWithSubType(subType2)) ?
             new MagicEvent(
-                    permanent,
-                    player,
-                    new Object[]{permanent},
-                    this,
-                    permanent+" enters the battlefield tapped."):
+                permanent, 
+                player, 
+                MagicEvent.NO_DATA, 
+                permanent+" enters the battlefield tapped.",
+                new MagicEventAction() {
+                @Override
+                public void executeEvent(
+                    final MagicGame game,
+                    final MagicEvent event,
+                    final Object[] data,
+                    final Object[] choices) {
+                    game.doAction(new MagicTapAction(permanent.map(game),false));
+                }}):
             MagicEvent.NONE;
-	}
-	
-	@Override
-	public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choices) {
-		game.doAction(new MagicTapAction((MagicPermanent)data[0],false));
 	}
 	
 	@Override
