277baaacbefc6af2c28c7e8f44ba794861ecc6ea
melvin <unknown>
2011-09-08 11:21:02 +0800

    added comment for rules deviation

--- src/magic/card/Hypnotic_Specter.java
+++ src/magic/card/Hypnotic_Specter.java
@@ -4,5 +4,6 @@ import magic.model.trigger.MagicSpecterTrigger;
 import magic.model.trigger.MagicTrigger;
 
 public class Hypnotic_Specter {
+    //should not cause discard if player is not opponent
     public static final MagicTrigger T = new MagicSpecterTrigger(false,true);
 }
