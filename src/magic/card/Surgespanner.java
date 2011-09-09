76a8f76f28e6d1eed5e737a3507e9ce68f453969
beholder <unknown>
2011-09-09 12:08:49 +0200

    added Surgespanner

---
+++ src/magic/card/Surgespanner.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicManaCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicPayManaCostChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicBounceTargetPicker;
+import magic.model.trigger.MagicWhenBecomesTappedTrigger;
+
+public class Surgespanner {
+    public static final MagicWhenBecomesTappedTrigger T = new MagicWhenBecomesTappedTrigger() {
+    	@Override
+    	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+    		final MagicPlayer player = permanent.getController();
+    		return (permanent == data) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new MagicMayChoice(
+                                "You may pay {1}{U}.",
+                                new MagicPayManaCostChoice(MagicManaCost.ONE_BLUE),
+                                MagicTargetChoice.TARGET_PERMANENT),
+                        MagicBounceTargetPicker.getInstance(),
+                        MagicEvent.NO_DATA,
+                        this,
+                        "You may$ pay {1}{U}$. If you do, " +
+                        "Return target permanent$ to its owner's hand.") :
+                MagicEvent.NONE;
+    	}
+    	@Override
+    	public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+    		if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+    			event.processTargetPermanent(game,choiceResults,2,new MagicPermanentAction() {
+                    public void doAction(final MagicPermanent permanent) {
+                        game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
+                    }
+    			});
+            }
+    	}
+    };
+}
