92664892e08546b39d5445d4679594533f5dd6a4
melvin <unknown>
2011-09-10 17:54:47 +0800

    removed empty lines

--- src/magic/model/phase/MagicUpkeepPhase.java
+++ src/magic/model/phase/MagicUpkeepPhase.java
@@ -8,18 +8,15 @@ public class MagicUpkeepPhase extends MagicPhase {
 	private static final MagicPhase INSTANCE=new MagicUpkeepPhase();
 	
 	private MagicUpkeepPhase() {
-	
 		super(MagicPhaseType.Upkeep);
 	}
 	
 	public static MagicPhase getInstance() {
-		
 		return INSTANCE;
 	}
 
 	@Override
 	public void executeBeginStep(final MagicGame game) {
-
 		game.executeTrigger(MagicTriggerType.AtUpkeep,game.getTurnPlayer());
 		game.setStep(game.canSkip()?MagicStep.NextPhase:MagicStep.ActivePlayer);
 	}
