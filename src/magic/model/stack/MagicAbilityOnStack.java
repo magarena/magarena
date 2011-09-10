d89dd2d998c785a24b7a01d6a1122c7c02774a89
melvin <unknown>
2011-09-10 19:49:41 +0800

    added copy constructor, copy method

--- src/magic/model/stack/MagicAbilityOnStack.java
+++ src/magic/model/stack/MagicAbilityOnStack.java
@@ -4,45 +4,45 @@ import magic.data.IconImages;
 import magic.model.MagicCopyable;
 import magic.model.MagicPayedCost;
 import magic.model.MagicPermanent;
+import magic.model.MagicCopyMap;
 import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicEvent;
 
 import javax.swing.ImageIcon;
 
 public class MagicAbilityOnStack extends MagicItemOnStack {
 		
-	public MagicAbilityOnStack(final MagicPermanentActivation activation,final MagicPermanent permanent,final MagicPayedCost payedCost) {
-
+	public MagicAbilityOnStack(
+            final MagicPermanentActivation activation,
+            final MagicPermanent permanent,
+            final MagicPayedCost payedCost) {
 		setSource(permanent);
 		setController(permanent.getController());
 		setActivation(activation);
 		setEvent(activation.getPermanentEvent(permanent,payedCost));
 	}
-
-	private MagicAbilityOnStack() {
-		
-	}
 	
-	@Override
-	public MagicCopyable create() {
-		
-		return new MagicAbilityOnStack();
+    private MagicAbilityOnStack(final MagicCopyMap copyMap, final MagicAbilityOnStack source) {
+        super(copyMap, source);
 	}
+
+	@Override
+	public MagicAbilityOnStack copy(final MagicCopyMap copyMap) {
+        return new MagicAbilityOnStack(copyMap, this);
+    }
 
 	@Override
 	public boolean isSpell() {
-
 		return false;
 	}
 
 	@Override
 	public boolean canBeCountered() {
-
 		return true;
 	}
 		
 	@Override
 	public ImageIcon getIcon() {
-		
 		return IconImages.ABILITY;
 	}	
 }
