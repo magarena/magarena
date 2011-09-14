5bdd07d803e30bae5c51c314655370138201f2ea
melvin <unknown>
2011-09-14 21:45:32 +0800

    give layers symbolic names

--- src/magic/model/MagicLayer.java
+++ src/magic/model/MagicLayer.java
@@ -1,20 +1,20 @@
 package magic.model; 
 
 public enum MagicLayer {
-    Layer1,  //copy 
-    Layer2,  //control changing
-    Layer3,  //text changing
-    Layer4a, //CDA: subtype
-    Layer4,  //type-changing (include sub and super types)
-    Layer5a, //CDA: color
-    Layer5,  //color changing
-    Layer6,  //ability adding/removing
-    Layer7a, //CDA: p/t
-    Layer7b, //set p/t to specific value
-    Layer7c, //modify p/t
-    Layer7d, //p/t changes due to counters
-    Layer7e, //switch p/t 
-    Layer8,  //affect player, not objects
-    Layer9,  //affect game rules, not objects
+    Copy,        //1.  copy 
+    Control,     //2.  control changing
+    Text,        //3.  text changing
+    CDASubtype,  //4a. CDA subtype
+    Type,        //4b. type-changing (include sub and super types)
+    CDAColor,    //5a. CDA color
+    Color,       //5b. color changing
+    Ability,     //6.  ability adding/removing
+    CDAPT,       //7a. CDA p/t
+    SetPT,       //7b. set p/t to specific value
+    ModPT,       //7c. modify p/t
+    CountersPT,  //7d. p/t changes due to counters
+    SwitchPT,    //7e. switch p/t 
+    Player,      //8.  affect player, not objects
+    Game,        //9.  affect game rules, not objects
     ;
 }
