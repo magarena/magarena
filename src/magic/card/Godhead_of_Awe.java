9719b12bbdca7e4dffb21db783b2eab4edf28178
melvin <unknown>
2011-09-14 21:49:34 +0800

    moved four static abilities to card class

---
+++ src/magic/card/Godhead_of_Awe.java
@@ -0,0 +1,25 @@
+
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Godhead_of_Awe {
+    public static final MagicStatic S = new MagicStatic(
+        MagicLayer.SetPT, 
+        MagicTargetFilter.TARGET_CREATURE) {
+        @Override
+        public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.power = 1;
+            pt.toughness = 1;
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target && filter.accept(game, source.getController(), target);
+        }
+    };
+}
