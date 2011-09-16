27b76ab0d5f5078a41796566e4530883d5404bc3
beholder <unknown>
2011-09-16 13:56:05 +0200

    added Kor Spiritdancer

---
+++ src/magic/card/Kor_Spiritdancer.java
@@ -0,0 +1,60 @@
+
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLayer;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.action.MagicDrawAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicSimpleMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.mstatic.MagicStatic;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.trigger.MagicWhenSpellIsPlayedTrigger;
+
+
+public class Kor_Spiritdancer {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
+		@Override
+		public void getPowerToughness(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+			if (permanent.isEnchanted()) {
+				pt.add(
+					2 * permanent.getAuraPermanents().size(),
+					2 * permanent.getAuraPermanents().size());
+			}
+		}
+    };
+    
+    public static final MagicWhenSpellIsPlayedTrigger T = new MagicWhenSpellIsPlayedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack data) {
+			final MagicPlayer player = permanent.getController();
+			return (data.getController() == player &&
+					data.getCardDefinition().isAura()) ?
+					new MagicEvent(
+	                        permanent,
+	                        player,
+	                        new MagicSimpleMayChoice(
+	                                player + " may draw a card.",
+	                                MagicSimpleMayChoice.DRAW_CARDS,
+	                                1,
+	                                MagicSimpleMayChoice.DEFAULT_NONE),
+	                        new Object[]{player},
+	                        this,
+	                        player + " may$ draw a card.") :
+	                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+			}
+		}		
+    };
+}
