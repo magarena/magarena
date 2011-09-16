7ba55dfe07b737a94301b5caaf16d1eaa4d0998c
beholder <unknown>
2011-09-16 12:28:33 +0200

    added Kor Duelist

---
+++ src/magic/card/Kor_Duelist.java
@@ -0,0 +1,22 @@
+
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicLayer;
+import magic.model.MagicPermanent;
+import magic.model.mstatic.MagicStatic;
+
+public class Kor_Duelist {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.Ability) {
+		@Override
+		public long getAbilityFlags(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final long flags) {
+			return permanent.isEquipped() ? 
+				flags|MagicAbility.DoubleStrike.getMask():
+	            flags;
+		}
+    };
+}
