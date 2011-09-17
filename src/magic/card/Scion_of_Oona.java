3520dcdf94a3a5cb0bc0fb0504152eb392fd6326
beholder <unknown>
2011-09-17 07:28:50 +0200

    added Scion of Oona

---
+++ src/magic/card/Scion_of_Oona.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicLayer;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Scion_of_Oona {
+	public static final MagicStatic S1 = new MagicStatic(
+			MagicLayer.ModPT, 
+			MagicTargetFilter.TARGET_FAERIE_YOU_CONTROL) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			pt.add(1,1);
+		}
+		@Override
+		public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+			return source != target;
+		}
+	};
+	
+	public static final MagicStatic S2 = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_FAERIE_YOU_CONTROL) {
+		@Override
+        public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+            return flags | MagicAbility.Shroud.getMask();
+        }
+		@Override
+		public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+			return source != target;
+		}
+	};
+}
