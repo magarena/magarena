41725ec9c5e0fb06df8e8b60fa90ae22071c03f6
beholder <unknown>
2011-09-17 11:25:06 +0200

    added Gluttonous Slime and Gorger Wurm

---
+++ src/magic/card/Gluttonous_Slime.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicDevourTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Gluttonous_Slime {
+    public static final MagicTrigger T = new MagicDevourTrigger(1);
+}
---
+++ src/magic/card/Gorger_Wurm.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicDevourTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Gorger_Wurm {
+    public static final MagicTrigger T = new MagicDevourTrigger(1);
+}
