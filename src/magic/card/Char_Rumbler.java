c36530c3e6eb949a7c8ea0544f04b4c20422aeba
beholder <unknown>
2011-09-17 13:37:52 +0200

    added Cavern Thoctar and Char-Rumbler

---
+++ src/magic/card/Char_Rumbler.java
@@ -0,0 +1,9 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPumpActivation;
+
+public class Char_Rumbler {
+	public static final MagicPermanentActivation A = new MagicPumpActivation(MagicManaCost.RED,1,0);
+}
