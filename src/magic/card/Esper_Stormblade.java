828e0e1dc67910e01d3fe7bf51d472b86586f7cf
beholder <unknown>
2011-09-18 18:47:20 +0200

    replaced MagicBladeLocalVariable with MagicStatic

--- src/magic/card/Esper_Stormblade.java
+++ src/magic/card/Esper_Stormblade.java
@@ -1,11 +1,38 @@
 package magic.card;
 
 import magic.model.MagicAbility;
-import magic.model.MagicCardDefinition;
-import magic.model.MagicChangeCardDefinition;
-import magic.model.variable.MagicBladeLocalVariable;
-import magic.model.variable.MagicLocalVariable;
+import magic.model.MagicColor;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
 
 public class Esper_Stormblade {
-	public static final MagicLocalVariable ESPER_STORMBLADE=new MagicBladeLocalVariable(MagicAbility.Flying.getMask());
+	private static boolean isValid(final MagicPermanent owner, final MagicGame game) {
+		for (final MagicPermanent permanent : owner.getController().getPermanents()) {
+			if (permanent != owner && MagicColor.isMulti(permanent.getColorFlags(game))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			if (isValid(permanent,game)) {
+				pt.add(1,1);
+			}
+		}		
+	};
+	
+	public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return (isValid(permanent,game)) ?
+                flags | MagicAbility.Flying.getMask() :
+                flags;
+        }
+	};
 }
