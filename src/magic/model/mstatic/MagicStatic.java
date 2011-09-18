33ac4bf2cd44d8516cedef1c0f46c45f538d2226
melvin <unknown>
2011-09-18 20:06:29 +0800

    added argument to indicate this this effects lasts until end of turn

--- src/magic/model/mstatic/MagicStatic.java
+++ src/magic/model/mstatic/MagicStatic.java
@@ -21,14 +21,24 @@ public abstract class MagicStatic extends MagicDummyLocalVariable {
     //card definition providing the effect
 	private int cardIndex;
 
-    protected MagicStatic(final MagicLayer aLayer, final MagicTargetFilter aFilter) {
+    private boolean isUntilEOT;
+
+    protected MagicStatic(final MagicLayer aLayer, final MagicTargetFilter aFilter, final boolean aIsUntilEOT) {
         filter = aFilter;
         layer = aLayer;
+        isUntilEOT = aIsUntilEOT;
+	}
+    
+    protected MagicStatic(final MagicLayer aLayer, final MagicTargetFilter aFilter) {
+        this(aLayer, aFilter, false);
+	}
+    
+    protected MagicStatic(final MagicLayer aLayer, final boolean aIsUntilEOT) {
+        this(aLayer, MagicTargetFilter.SELF, aIsUntilEOT);
 	}
     
     protected MagicStatic(final MagicLayer aLayer) {
-        filter = MagicTargetFilter.SELF;
-        layer = aLayer;
+        this(aLayer, MagicTargetFilter.SELF, false);
 	}
 
     public void setCardIndex(final int cardIndex) {
@@ -42,6 +52,10 @@ public abstract class MagicStatic extends MagicDummyLocalVariable {
 	public final MagicLayer getLayer() {
 		return layer;
 	}
+
+    public final boolean isUntilEOT() {
+        return isUntilEOT;
+    }
 
     public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
         if (filter == MagicTargetFilter.SELF) {
