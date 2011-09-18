fc9a21cf65249042150f558f69d300d86f8ce0cf
melvin <unknown>
2011-09-18 22:43:27 +0800

    added game as argument

--- src/magic/model/phase/MagicUntapPhase.java
+++ src/magic/model/phase/MagicUntapPhase.java
@@ -14,17 +14,14 @@ public class MagicUntapPhase extends MagicPhase {
 	private static final MagicPhase INSTANCE=new MagicUntapPhase();
 	
 	private MagicUntapPhase() {
-	
 		super(MagicPhaseType.Untap);
 	}
 	
 	public static MagicPhase getInstance() {
-		
 		return INSTANCE;
 	}
 	
 	private static void untap(final MagicGame game) {
-
 		final MagicPlayer player=game.getTurnPlayer();
 		final boolean exhausted=player.hasState(MagicPlayerState.Exhausted);
 		game.doAction(new MagicChangePlayerStateAction(player,MagicPlayerState.Exhausted,false));
@@ -36,7 +33,7 @@ public class MagicUntapPhase extends MagicPhase {
 			}
 			if (permanent.hasState(MagicPermanentState.DoesNotUntap)) {
 				game.doAction(new MagicChangeStateAction(permanent,MagicPermanentState.DoesNotUntap,false));
-			} else if (permanent.isTapped()&&(!exhausted||!(permanent.isLand()||permanent.isCreature()))) {
+			} else if (permanent.isTapped()&&(!exhausted||!(permanent.isLand()||permanent.isCreature(game)))) {
 				game.doAction(new MagicUntapAction(permanent));
 			}
 		}		
@@ -44,7 +41,6 @@ public class MagicUntapPhase extends MagicPhase {
 
 	@Override
 	public void executeBeginStep(final MagicGame game) {
-
 		untap(game);
 		game.setStep(MagicStep.NextPhase);
 	}
