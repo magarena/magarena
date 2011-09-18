710ff13cfff8e24aa00a40ba23edc7ebf62f186b
melvin <unknown>
2011-09-18 11:02:58 +0800

    added copy constructor and setContent method

--- src/magic/model/MagicDeck.java
+++ src/magic/model/MagicDeck.java
@@ -7,6 +7,19 @@ public class MagicDeck extends ArrayList<MagicCardDefinition> {
 	private static final long serialVersionUID = 1L;
 	
 	private String name="Unsaved Deck";
+
+    public MagicDeck() {}
+
+    public MagicDeck(final MagicDeck deck) {
+        super(deck);
+        name = deck.name;
+    }
+
+    public void setContent(final MagicDeck deck) {
+        clear();
+        addAll(deck);
+        name = deck.name;
+    }
 	
 	public void setName(final String name) {
 		this.name=name;
