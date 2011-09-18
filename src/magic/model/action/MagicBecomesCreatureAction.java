d71eeea394aa9d3c2e0d4d35303621877b1c1ec3
melvin <unknown>
2011-09-18 22:46:44 +0800

    replaced localVariable with MagicStatic

--- src/magic/model/action/MagicBecomesCreatureAction.java
+++ src/magic/model/action/MagicBecomesCreatureAction.java
@@ -4,25 +4,28 @@ import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicPermanentState;
 import magic.model.variable.MagicLocalVariable;
+import magic.model.mstatic.MagicStatic;
 
 public class MagicBecomesCreatureAction extends MagicAction {
 
 	private final MagicPermanent permanent;
-	private final MagicLocalVariable localVariable;
+	private final MagicStatic mstatics[];
 	private boolean oldState;
 	
-	public MagicBecomesCreatureAction(final MagicPermanent permanent,final MagicLocalVariable localVariable) {
-		this.permanent=permanent;
-		this.localVariable=localVariable;
+	public MagicBecomesCreatureAction(final MagicPermanent aPermanent,final MagicStatic... aMstatics) {
+        permanent = aPermanent;
+        mstatics = aMstatics;
 	}
 	
 	@Override
 	public void doAction(final MagicGame game) {
-		oldState=permanent.hasState(MagicPermanentState.Animated);
+		oldState = permanent.hasState(MagicPermanentState.Animated);
 		if (!oldState) {
 			permanent.setState(MagicPermanentState.Animated);
 		}
-		permanent.addTurnLocalVariable(localVariable);
+        for (final MagicStatic mstatic : mstatics) {
+            game.doAction(new MagicAddStaticAction(permanent, mstatic));
+        }
 		game.setStateCheckRequired();
 	}
 
@@ -31,6 +34,5 @@ public class MagicBecomesCreatureAction extends MagicAction {
 		if (!oldState) {
 			permanent.clearState(MagicPermanentState.Animated);
 		}
-		permanent.removeTurnLocalVariable();
 	}
 }
