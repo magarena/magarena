ddcb0ff393450d6a98c71df7e8e501a42bc7cc49
melvin <unknown>
2011-09-19 14:35:07 +0800

    reduce nestedness of logic

--- src/magic/model/action/MagicDestroyAction.java
+++ src/magic/model/action/MagicDestroyAction.java
@@ -16,30 +16,36 @@ public class MagicDestroyAction extends MagicAction {
 	
 	@Override
 	public void doAction(final MagicGame game) {
-		if (!permanent.hasAbility(game,MagicAbility.Indestructible)) {		
-			if (permanent.isRegenerated()) {
-				game.logAppendMessage(permanent.getController(),permanent.getName()+" is regenerated.");
-				game.doAction(new MagicTapAction(permanent,false));
-				game.doAction(new MagicRemoveAllDamageAction(permanent));
-				game.doAction(new MagicRemoveFromCombatAction(permanent));
-				game.doAction(new MagicChangeStateAction(permanent,MagicPermanentState.Regenerated,false));
-			} else {
-				// Totem armor.
-				if (permanent.isEnchanted()) {
-					for (final MagicPermanent aura : permanent.getAuraPermanents()) {
-						
-						if (aura.getCardDefinition().hasAbility(MagicAbility.TotemArmor)) {
-							game.logAppendMessage(permanent.getController(),"Remove all damage from "+permanent.getName()+'.');
-							game.doAction(new MagicRemoveAllDamageAction(permanent));
-							game.doAction(new MagicDestroyAction(aura));
-							return;
-						}
-					}
-				}							
-				game.logAppendMessage(permanent.getController(),permanent.getName()+" is destroyed.");
-				game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.Graveyard));
-			}
-		}
+        // Indestructible
+		if (permanent.hasAbility(game,MagicAbility.Indestructible)) {	
+            return;
+        }
+
+        // Regeneration
+        if (permanent.isRegenerated()) {
+            game.logAppendMessage(permanent.getController(),permanent.getName()+" is regenerated.");
+            game.doAction(new MagicTapAction(permanent,false));
+            game.doAction(new MagicRemoveAllDamageAction(permanent));
+            game.doAction(new MagicRemoveFromCombatAction(permanent));
+            game.doAction(new MagicChangeStateAction(permanent,MagicPermanentState.Regenerated,false));
+            return;
+        } 
+        
+        // Totem armor.
+        if (permanent.isEnchanted()) {
+            for (final MagicPermanent aura : permanent.getAuraPermanents()) {
+                if (aura.getCardDefinition().hasAbility(MagicAbility.TotemArmor)) {
+                    game.logAppendMessage(permanent.getController(),"Remove all damage from "+permanent.getName()+'.');
+                    game.doAction(new MagicRemoveAllDamageAction(permanent));
+                    game.doAction(new MagicDestroyAction(aura));
+                    return;
+                }
+            }
+        }				
+
+        // Destroyed
+        game.logAppendMessage(permanent.getController(),permanent.getName()+" is destroyed.");
+        game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.Graveyard));
 	}
 
 	@Override
