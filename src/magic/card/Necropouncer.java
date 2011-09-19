4325a99cef5e3ba0f16e0e9b27ab7d27e4410348
beholder <unknown>
2011-09-19 13:25:42 +0200

    added Blight Sickle, Bone Saw, Necropouncer, Neurok Hoversail, No-Dachi

---
+++ src/magic/card/Necropouncer.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicLivingWeaponTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Necropouncer {
+    public static final MagicTrigger T = new MagicLivingWeaponTrigger();
+}
