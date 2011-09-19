9780f44373922d013f415aaadcc0c9b1aaa9ee42
wait321 <unknown>
2011-09-18 19:46:40 -0700

    added error message for unsupported cards, fixed a bug where old deck wasn't cleared before loading new deck

--- src/magic/test/TestAbilities.java
+++ src/magic/test/TestAbilities.java
@@ -33,7 +33,8 @@ class TestAbilities extends TestGameBuilder {
 		createPermanent(game,P,"Lightning Greaves",false,1);
 		createPermanent(game,P,"Sword of Body and Mind",false,1);	
 		createPermanent(game,P,"Darksteel Axe",false,1);
-		createPermanent(game,P,"Akroma's Memorial",false,1);						
+		createPermanent(game,P,"Akroma's Memorial",false,1);	
+		createPermanent(game,P,"Creeping Tar Pit",false,1);					
 	    addToHand(P,"Naturalize",5); 
 	    addToHand(P,"Demystify",3); 
 	    addToHand(P,"Doom Blade",5); 
