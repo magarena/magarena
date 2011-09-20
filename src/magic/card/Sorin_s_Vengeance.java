cf1530c610c49749c0931c045f61386811e71fd4
beholder <unknown>
2011-09-20 10:46:37 +0200

    added Primordial Hydra and Sorin's Vengeance

---
+++ src/magic/card/Sorin_s_Vengeance.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+
+public class Sorin_s_Vengeance {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+            final MagicCard card = cardOnStack.getCard();
+			return new MagicEvent(
+                    card,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(10),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    card + " deals 10 damage to target player$ and " +
+                    player + " gains 10 life.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));			
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(cardOnStack.getCard(),target,10,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[1],10));
+		}
+	};
+}
