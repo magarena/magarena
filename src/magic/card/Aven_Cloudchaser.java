aae8c6cba6595cda514fbd44d383aede18d88418
beholder <unknown>
2011-09-20 12:24:36 +0200

    added Aven Cloudchaser

---
+++ src/magic/card/Aven_Cloudchaser.java
@@ -0,0 +1,36 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDestroyTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Aven_Cloudchaser {
+	public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_ENCHANTMENT,
+                    new MagicDestroyTargetPicker(false),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Destroy target enchantment$.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent perm) {
+                    game.doAction(new MagicDestroyAction(perm));
+                }
+			});
+		}
+    };
+}
