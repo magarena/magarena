13d39c22b690ae2548f566e34c1af68418fda8b3
beholder <unknown>
2011-09-21 19:49:50 +0200

    added Loxodon Mystic and Loyal Sentry

---
+++ src/magic/card/Loxodon_Mystic.java
@@ -0,0 +1,25 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.MagicSource;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostTapEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapCreatureActivation;
+import magic.model.event.MagicTiming;
+
+public class Loxodon_Mystic {
+	public static final MagicPermanentActivation A1 = new MagicTapCreatureActivation(
+			new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION,MagicManaCost.WHITE.getCondition()},
+			new MagicActivationHints(MagicTiming.Tapping),
+			"Tap") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[]{
+                new MagicPayManaCostTapEvent(source,source.getController(),
+                MagicManaCost.WHITE)};
+        }
+    };
+}
---
+++ src/magic/card/Loyal_Sentry.java
@@ -0,0 +1,33 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDestroyAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBlocksTrigger;
+
+public class Loyal_Sentry {
+    public static final MagicWhenBlocksTrigger T = new MagicWhenBlocksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+            final MagicPermanent blocked = permanent.getBlockedCreature();
+			return (permanent == data && blocked.isValid()) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{blocked,permanent},
+                    this,
+                    "Destroy " + blocked + ". Destroy " + permanent + "."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicDestroyAction((MagicPermanent)data[0]));
+			game.doAction(new MagicDestroyAction((MagicPermanent)data[1]));
+		}
+    };
+}
