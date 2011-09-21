1cbb0d6d5f93c9e895ae63d95c5932e8b4a694b6
beholder <unknown>
2011-09-21 07:52:44 +0200

    added Condemn and Counsel of the Soratami

---
+++ src/magic/card/Condemn.java
@@ -0,0 +1,48 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicExileTargetPicker;
+
+public class Condemn {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
+                    MagicExileTargetPicker.getInstance(), // could use a better TargetPicker
+                    new Object[]{cardOnStack},
+                    this,
+                    "Put target creature$ on the bottom of its owner's library. " +
+                    "Its controller gains life equal to its toughness.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeLifeAction(
+                                creature.getController(),
+                                creature.getToughness(game)));
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.BottomOfOwnersLibrary));
+                }
+			});
+		}
+	};
+}
