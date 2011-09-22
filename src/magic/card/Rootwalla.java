cb770d2c63f5e65f41487465e953722d5e784d14
beholder <unknown>
2011-09-22 11:33:30 +0200

    added Rock Badger and Rootwalla

---
+++ src/magic/card/Rootwalla.java
@@ -0,0 +1,49 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPlayAbilityEvent;
+import magic.model.event.MagicTiming;
+
+public class Rootwalla {
+	public static final MagicPermanentActivation T = new MagicPermanentActivation(
+			new MagicCondition[]{
+					MagicCondition.ABILITY_ONCE_CONDITION,
+					MagicManaCost.ONE_GREEN.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+                new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.ONE_GREEN),
+                new MagicPlayAbilityEvent((MagicPermanent)source)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    source + " gets +2/+2 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPermanent permanent=(MagicPermanent)data[0];
+			game.doAction(new MagicChangeTurnPTAction(permanent,2,2));
+		}
+	};
+}
