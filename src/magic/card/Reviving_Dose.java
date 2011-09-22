12f3ded9acdd0511e5263aca39905e5a1bfd074a
beholder <unknown>
2011-09-22 11:06:20 +0200

    added Remove Soul, Reviving Dose and Reya Dawnbringer

---
+++ src/magic/card/Reviving_Dose.java
@@ -0,0 +1,36 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Reviving_Dose {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    player + " gains 3 life and draws a card.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[1],3));
+			game.doAction(new MagicDrawAction((MagicPlayer)data[1],1));
+		}
+	};
+}
---
+++ src/magic/card/Reya_Dawnbringer.java
@@ -0,0 +1,51 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicCardAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicReanimateAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicGraveyardTargetPicker;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+
+
+public class Reya_Dawnbringer {
+    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
+			final MagicPlayer player = permanent.getController();
+			return (player != data) ?
+                MagicEvent.NONE :
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new MagicMayChoice(
+                            "You may return target creature card from your graveyard to the battlefield.",
+                            MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD),
+                    new MagicGraveyardTargetPicker(true),
+                    new Object[]{player},
+                    this,
+                    player + " may$ return target creature card$ from " +
+                    		"his or her graveyard to the battlefield.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				event.processTargetCard(game,choiceResults,1,new MagicCardAction() {
+					public void doAction(final MagicCard card) {
+						game.doAction(new MagicReanimateAction((MagicPlayer)data[0],card,MagicPlayCardAction.NONE));
+					}
+				});
+			}
+		}
+    };
+}
