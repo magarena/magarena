12ad6232ab158dec8741e16262d1fa3e5c352572
beholder <unknown>
2011-09-23 08:39:50 +0200

    added Tempest of Light and Threaten

---
+++ src/magic/card/Tempest_of_Light.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Tempest_of_Light {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Destroy all enchantments.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			final Collection<MagicTarget> targets =
+                game.filterTargets(cardOnStack.getController(),MagicTargetFilter.TARGET_ENCHANTMENT);
+			for (final MagicTarget target : targets) {
+				game.doAction(new MagicDestroyAction((MagicPermanent)target));
+			}
+		}
+	};
+}
