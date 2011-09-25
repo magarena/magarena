79ebbd22d777800468e3a106a28e752d81a365b2
beholder <unknown>
2011-09-25 15:00:53 +0200

    added Doomed Traveler and Elder Cathar

---
+++ src/magic/card/Elder_Cathar.java
@@ -0,0 +1,53 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicPumpTargetPicker;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Elder_Cathar {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+			final MagicPlayer player = permanent.getController();
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    player + " puts a +1/+1 counter on target creature he or she controls. " +
+                    "If that creature is a Human, put two +1/+1 counters on it instead.") :
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                	final int amount = creature.hasSubType(MagicSubType.Human,game) ? 2 : 1;
+                	game.doAction(new MagicChangeCountersAction(
+        					creature,
+        					MagicCounterType.PlusOne,
+        					amount,
+        					true));
+                }
+            });
+		}
+    };
+}
