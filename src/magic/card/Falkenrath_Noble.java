0782931b7c7714bb0dffe6a856c78f6e7e5533b1
beholder <unknown>
2011-09-26 14:52:36 +0200

    added Falkenrath Noble and Feral Ridgewolf

---
+++ src/magic/card/Falkenrath_Noble.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Falkenrath_Noble {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			final MagicPlayer player = permanent.getController();
+			return (otherPermanent.isCreature(game)) ?
+                    		new MagicEvent(
+        		                    permanent,
+        		                    player,
+        		                    MagicTargetChoice.NEG_TARGET_PLAYER,
+        		                    new Object[]{player},
+        		                    this,
+        		                    "Target player$ loses 1 life and you gain 1 life.") :
+        		                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-1));
+                    game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],1));
+                }
+			});
+		}
+    };
+}
---
+++ src/magic/card/Feral_Ridgewolf.java
@@ -0,0 +1,9 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPumpActivation;
+
+public class Feral_Ridgewolf {
+	public static final MagicPermanentActivation A = new MagicPumpActivation(MagicManaCost.ONE_RED,2,0);
+}
