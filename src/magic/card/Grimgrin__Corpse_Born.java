07d8212e59852a99860189847c7881359dfe100d
beholder <unknown>
2011-09-28 11:12:14 +0200

    added Grimgrin, Corpse-Born and Grimoire of the Dead

---
+++ src/magic/card/Grimgrin__Corpse_Born.java
@@ -0,0 +1,105 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicUntapAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicSacrificePermanentEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicDestroyTargetPicker;
+import magic.model.target.MagicTargetFilter;
+import magic.model.target.MagicTargetHint;
+import magic.model.trigger.MagicTappedIntoPlayTrigger;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+public class Grimgrin__Corpse_Born {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{
+                MagicCondition.TWO_CREATURES_CONDITION
+            },
+			new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			final MagicTargetFilter targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+					MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL,(MagicPermanent)source);
+			final MagicTargetChoice targetChoice = new MagicTargetChoice(
+					targetFilter,false,MagicTargetHint.None,"a creature other than " + source + " to sacrifice");
+			return new MagicEvent[]{new MagicSacrificePermanentEvent(
+					source,
+					source.getController(),
+					targetChoice)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+					source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Untap " + source + " and put a +1/+1 counter on it.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+			game.doAction(new MagicUntapAction((MagicPermanent)data[0]));
+			game.doAction(new MagicChangeCountersAction(
+					(MagicPermanent)data[0],
+					MagicCounterType.PlusOne,
+					1,
+					true));
+		}
+	};
+	
+	public static final MagicWhenAttacksTrigger T1 = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPlayer player = permanent.getController();
+			return (permanent == creature) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                        new MagicDestroyTargetPicker(false),
+                        new Object[]{permanent},
+                        this,
+                        "Destroy target creature$ your opponent controls, " +
+                        "then put a +1/+1 counter on " + permanent + "."):
+                MagicEvent.NONE;           
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDestroyAction(creature));
+                }
+			});
+			game.doAction(new MagicChangeCountersAction(
+					(MagicPermanent)data[0],
+					MagicCounterType.PlusOne,
+					1,
+					true));
+		}
+    };
+	
+    public static final MagicTrigger T2 = new MagicTappedIntoPlayTrigger();
+}
---
+++ src/magic/card/Grimoire_of_the_Dead.java
@@ -0,0 +1,131 @@
+package magic.card;
+
+import java.util.Collection;
+import java.util.EnumSet;
+
+import magic.model.MagicCard;
+import magic.model.MagicColor;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.MagicSubType;
+import magic.model.action.MagicAddStaticAction;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicRemoveCounterEvent;
+import magic.model.event.MagicSacrificeEvent;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Grimoire_of_the_Dead {
+	private static final MagicStatic Zombie = new MagicStatic(MagicLayer.Type) {
+		@Override
+		public EnumSet<MagicSubType> getSubTypeFlags(final MagicPermanent permanent,final EnumSet<MagicSubType> flags) {
+			final EnumSet<MagicSubType> mod = flags.clone();
+			mod.add(MagicSubType.Zombie);
+			return mod;
+		}
+	};
+
+	private static final MagicStatic Black = new MagicStatic(MagicLayer.Color) {
+		@Override
+		public int getColorFlags(final MagicPermanent permanent,final int flags) {
+			return flags|MagicColor.Black.getMask();
+		}
+	};
+	public static final MagicPermanentActivation A1 = new MagicPermanentActivation(
+			new MagicCondition[]{
+				MagicCondition.CAN_TAP_CONDITION,
+                MagicCondition.HAS_CARD_CONDITION,
+                MagicManaCost.ONE.getCondition()},
+            new MagicActivationHints(MagicTiming.Main,true),
+            "Add counter") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+				new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.ONE),
+				new MagicDiscardEvent(source,source.getController(),1,false)
+			};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Put a study counter on " + source + ".");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction((MagicPermanent)data[0],MagicCounterType.Charge,1,true));
+		}
+	};
+	
+	public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
+			new MagicCondition[]{
+					MagicCondition.CAN_TAP_CONDITION,
+					MagicCondition.THREE_CHARGE_COUNTERS_CONDITION},
+            new MagicActivationHints(MagicTiming.Token),
+            "Reanimate") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+				new MagicRemoveCounterEvent((MagicPermanent)source,MagicCounterType.Charge,3),
+				new MagicSacrificeEvent((MagicPermanent)source)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			return new MagicEvent(
+                    source,
+                    player,
+                    new Object[]{player},
+                    this,
+                    "Put all creature cards from all graveyards onto the " +
+                    "battlefield under your control. They're black Zombies " +
+                    "in addition to their other colors and types.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPlayer player = (MagicPlayer)data[0];
+			final Collection<MagicTarget> targets =
+					game.filterTargets(player,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_ALL_GRAVEYARDS);
+			for (final MagicTarget target : targets) {
+				final MagicCard card = (MagicCard) target;
+				if (card.getOwner().getGraveyard().contains(card)) {
+					final MagicPlayCardAction action = new MagicPlayCardAction(card,player,MagicPlayCardAction.NONE);
+					game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+					game.doAction(action);
+
+					final MagicPermanent permanent = action.getPermanent();
+					game.doAction(new MagicAddStaticAction(permanent, Zombie));
+					game.doAction(new MagicAddStaticAction(permanent, Black));
+				}
+			}
+		}
+	};
+}
