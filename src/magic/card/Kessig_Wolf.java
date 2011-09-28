6128dee112c4684e19400fe8b2d36fde4be5f8e2
beholder <unknown>
2011-09-28 19:16:43 +0200

    added Kessig Wolf and Kindercatch

---
+++ src/magic/card/Kessig_Wolf.java
@@ -0,0 +1,16 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicManaCost;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicGainActivation;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+
+public class Kessig_Wolf {
+	public static final MagicPermanentActivation A = new MagicGainActivation(
+            MagicManaCost.ONE_RED,
+            MagicAbility.FirstStrike,
+            new MagicActivationHints(MagicTiming.Pump,true,1),
+            "First strike");
+}
