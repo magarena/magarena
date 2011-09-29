3c18f3211699761f4d2fdc92c6dc25931eb9b14d
beholder <unknown>
2011-09-29 16:25:22 +0200

    added Pitchburn Devils

---
+++ src/magic/card/Pitchburn_Devils.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Pitchburn_Devils {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+                new MagicEvent(
+                        permanent,
+                        permanent.getController(),
+                        MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                        new MagicDamageTargetPicker(3),
+                        new Object[]{permanent},
+                        this,
+                        permanent + " deals 3 damage to target creature or player$."):
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],target,3,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+    };
+}
