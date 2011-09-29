33293cdc492eca4e877ff7df7ccb5860ec4a773c
beholder <unknown>
2011-09-29 11:14:38 +0200

    added Mentor of the Meek and Midnight Haunting

---
+++ src/magic/card/Mentor_of_the_Meek.java
@@ -0,0 +1,45 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicPayManaCostChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
+
+public class Mentor_of_the_Meek {
+    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			final MagicPlayer player = permanent.getController();
+			return (otherPermanent != permanent &&
+					otherPermanent.isCreature(game) && 
+                    otherPermanent.getController() == player &&
+                    otherPermanent.getPower(game) <= 2) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new MagicMayChoice(
+                                "You may pay {1}.",
+                                new MagicPayManaCostChoice(MagicManaCost.ONE)),
+                        new Object[]{player},
+                        this,
+                        "You may$ pay {1}$. If you do, draw a card."):
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+            }
+		}		
+    };
+}
---
+++ src/magic/card/Midnight_Haunting.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Midnight_Haunting {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    player + " puts two 1/1 white Spirit creature " +
+                    "tokens with flying onto the battlefield.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPlayer player = (MagicPlayer)data[1];
+			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.SPIRIT2_TOKEN_CARD));
+			game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.SPIRIT2_TOKEN_CARD));
+		}
+	};
+}
