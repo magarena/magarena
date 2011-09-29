016293fdbe067c8458767f5e57cdf886d3e13aca
beholder <unknown>
2011-09-29 18:56:42 +0200

    added Rebuke and Riot Devils

---
+++ src/magic/card/Rebuke.java
@@ -0,0 +1,45 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDestroyTargetPicker;
+
+public class Rebuke {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
+                    new MagicDestroyTargetPicker(false),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Destroy target attacking creature$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDestroyAction(creature));
+                }
+			});
+		}
+	};
+}
