1c79a6dcce174726789f3becdcc6a184986e2d79
beholder <unknown>
2011-09-29 12:40:07 +0200

    added Mikaeus, the Lunarch and Mindshrieker

---
+++ src/magic/card/Mikaeus__the_Lunarch.java
@@ -0,0 +1,129 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicCard;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPlayCardFromStackAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicRemoveCounterEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Mikaeus__the_Lunarch {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+			final int amount = payedCost.getX();
+            final MagicCard card = cardOnStack.getCard();
+			return new MagicEvent(
+                    card,
+                    cardOnStack.getController(),
+                    new Object[]{cardOnStack,amount},
+                    this,
+                    card + " enters the battlefield with " + amount + " +1/+1 counters on it.");
+		}
+
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			final MagicPlayCardFromStackAction action = new MagicPlayCardFromStackAction(cardOnStack);
+			game.doAction(action);
+			final MagicPermanent permanent = action.getPermanent();
+			game.doAction(new MagicChangeCountersAction(
+                        permanent,
+                        MagicCounterType.PlusOne,
+                        (Integer)data[1],
+                        true));
+		}
+	};
+	
+	public static final MagicPermanentActivation A1 = new MagicPermanentActivation(
+			new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Add") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			return new MagicEvent(
+                    source,
+                    player,
+                    new Object[]{player,source},
+                    this,
+                    "Put a +1/+1 counter on " + source + ".");
+        }
+		
+        @Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            game.doAction(new MagicChangeCountersAction((MagicPermanent)data[1],MagicCounterType.PlusOne,1,true));
+        }
+	};
+	
+	public static final MagicPermanentActivation A2 = new MagicPermanentActivation( 
+			new MagicCondition[]{
+				MagicCondition.CAN_TAP_CONDITION,
+                MagicCondition.PLUS_COUNTER_CONDITION,
+            },
+            new MagicActivationHints(MagicTiming.Pump),
+            "Remove") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+				new MagicTapEvent((MagicPermanent)source),
+				new MagicRemoveCounterEvent((MagicPermanent)source,MagicCounterType.PlusOne,1)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Put a +1/+1 counter on each other creature you control.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPermanent creature = (MagicPermanent)data[0];
+			final Collection<MagicTarget> targets =
+                game.filterTargets(creature.getController(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				if (target != creature) {
+					game.doAction(new MagicChangeCountersAction((MagicPermanent)target,MagicCounterType.PlusOne,1,true));
+				}
+			}
+		}
+	};
+}
---
+++ src/magic/card/Mindshrieker.java
@@ -0,0 +1,62 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+
+public class Mindshrieker {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicManaCost.TWO.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump,true),
+            "Mill"
+            ) {
+		
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.TWO)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+					source,
+					source.getController(),
+					MagicTargetChoice.TARGET_PLAYER,
+					new Object[]{source},
+					this,
+                    "Target player$ puts the top card of his or her library " +
+                    "into his or her graveyard. " + source + " gets +X/+X " +
+                    "until end of turn, where X is that card's converted mana cost");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                	game.doAction(new MagicMillLibraryAction(player,1));
+                	final MagicCard card = player.getGraveyard().get(player.getGraveyard().size()-1);
+                	final int amount = card.getCardDefinition().getConvertedCost();
+                	game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],amount,amount));
+                }
+			});
+		}
+	};
+}
