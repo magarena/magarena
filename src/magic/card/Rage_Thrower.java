b80590ec59045255798a3cdc63f23eef233e05b7
beholder <unknown>
2011-09-29 18:28:59 +0200

    added Rage Thrower and Rakish Heir

---
+++ src/magic/card/Rage_Thrower.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Rage_Thrower {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			return (permanent != otherPermanent &&
+					otherPermanent.isCreature(game)) ?
+				new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new Object[]{permanent},
+                    this,
+                    permanent + " deals 2 damage to target player."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],player,2,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+    };
+}
---
+++ src/magic/card/Rakish_Heir.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+
+public class Rakish_Heir {
+    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+			final MagicPlayer player = permanent.getController();
+			final MagicSource source = damage.getSource();
+			return (damage.isCombat() && 
+					damage.getTarget().isPlayer() &&
+					source.getController() == player &&
+					((MagicPermanent)source).hasSubType(MagicSubType.Vampire,game)) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{source},
+                        this,
+                        "Put a +1/+1 counter on " + source + "."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction((MagicPermanent)data[0],MagicCounterType.PlusOne,1,true));
+		}
+    };
+}
