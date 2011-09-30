6c0445fe2a738c5954740ed012cb5c92f28f62c3
beholder <unknown>
2011-09-30 11:56:12 +0200

    added Stromkirk Patrol and Sturmgeist

---
+++ src/magic/card/Sturmgeist.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.action.MagicDrawAction;
+import magic.model.event.MagicEvent;
+import magic.model.mstatic.MagicCDA;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class Sturmgeist {
+	public static final MagicCDA CDA = new MagicCDA() {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+			final int amount = player.getHandSize();
+			pt.set(amount,amount);
+		}
+	};
+	
+	public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+			final MagicPlayer player = permanent.getController();
+			return (damage.getSource() == permanent &&
+					damage.getTarget().isPlayer() &&
+					damage.isCombat()) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        player + " draws a card."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+		}
+    };
+}
