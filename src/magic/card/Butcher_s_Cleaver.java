dda4dfcb27bf19eefa8cd919859b86aa10e027d9
beholder <unknown>
2011-09-30 09:12:04 +0200

    added Butcher's Cleaver and Sharpened Pitchfork

---
+++ src/magic/card/Butcher_s_Cleaver.java
@@ -0,0 +1,25 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicSubType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Butcher_s_Cleaver {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_CREATURE) {
+		@Override
+		public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return flags | MagicAbility.LifeLink.getMask();
+		}
+		@Override
+		public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+			return source.getEquippedCreature().hasSubType(MagicSubType.Human,game) &&
+					target == source.getEquippedCreature();
+		}
+	};
+}
---
+++ src/magic/card/Sharpened_Pitchfork.java
@@ -0,0 +1,25 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSubType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Sharpened_Pitchfork {
+    public static final MagicStatic S = new MagicStatic(
+        MagicLayer.ModPT, 
+	    MagicTargetFilter.TARGET_CREATURE) {
+        @Override
+        public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {	
+        	return source.getEquippedCreature().hasSubType(MagicSubType.Human,game) &&
+        			target == source.getEquippedCreature();
+        }
+    };
+}
