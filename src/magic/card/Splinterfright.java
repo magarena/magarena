1ed03d7149434de1e0972304d67dcbb59b59a799
beholder <unknown>
2011-09-30 11:27:53 +0200

    added Splinterfright

---
+++ src/magic/card/Splinterfright.java
@@ -0,0 +1,45 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.event.MagicEvent;
+import magic.model.mstatic.MagicCDA;
+
+public class Splinterfright {
+	public static final MagicCDA CDA = new MagicCDA() {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+			final int size = game.filterTargets(player,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD).size();
+			pt.set(size,size);
+		}
+	};
+	
+	public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
+			final MagicPlayer player = permanent.getController();
+			return (player == data) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        player + " puts the top two cards of " +
+                        "his or her library into his or her graveyard."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicMillLibraryAction((MagicPlayer)data[0],2));
+		}
+    };
+}
