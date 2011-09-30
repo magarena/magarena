5c633e782a76f73d8db31cac18e536f92fbdc279
beholder <unknown>
2011-09-30 12:05:55 +0200

    added Thraben Purebloods and Traitorous Blood

---
+++ src/magic/card/Traitorous_Blood.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentState;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeStateAction;
+import magic.model.action.MagicGainControlAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.action.MagicUntapAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicExileTargetPicker;
+
+public class Traitorous_Blood {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    MagicExileTargetPicker.getInstance(),
+				    new Object[]{cardOnStack,player},
+				    this,
+				    "Gain control of target creature$ until end of turn. Untap it. " +
+                    "It gains trample and haste until end of turn.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainControlAction((MagicPlayer)data[1],creature));
+                    game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.ReturnToOwnerAtEndOfTurn,true));
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
+                }
+			});
+		}
+	};
+
+}
