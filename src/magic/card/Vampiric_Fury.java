7726e6635a6d37e091485419ee59f2293f4c2468
beholder <unknown>
2011-10-01 11:46:46 +0200

    added Vampire Interloper and Vampiric Fury

---
+++ src/magic/card/Vampiric_Fury.java
@@ -0,0 +1,51 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Vampiric_Fury {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Vampire creatures you control get +2/+0 " +
+                    "and gain first strike until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final Collection<MagicTarget> targets = game.filterTargets(
+                    (MagicPlayer)data[1],
+                    MagicTargetFilter.TARGET_VAMPIRE_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				final MagicPermanent creature = (MagicPermanent)target;
+				game.doAction(new MagicChangeTurnPTAction(creature,2,0));
+				game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike.getMask()));
+			}
+		}
+	};
+}
--- src/magic/model/target/MagicTargetFilter.java
+++ src/magic/model/target/MagicTargetFilter.java
@@ -729,6 +729,17 @@ public interface MagicTargetFilter {
 		}		
 	};
 	
+	MagicTargetFilter TARGET_VAMPIRE_YOU_CONTROL = new MagicTargetFilter() {
+		public boolean accept(final MagicGame game,final MagicPlayer player,final MagicTarget target) {
+			final MagicPermanent permanent = (MagicPermanent)target;
+			return target.getController() == player &&
+					permanent.hasSubType(MagicSubType.Vampire,game);
+		}
+		public boolean acceptType(final MagicTargetType targetType) {	
+			return targetType == MagicTargetType.Permanent;
+		}		
+	};
+	
 	MagicTargetFilter TARGET_VAMPIRE_WEREWOLF_OR_ZOMBIE = new MagicTargetFilter() {
 		public boolean accept(final MagicGame game,final MagicPlayer player,final MagicTarget target) {
 			final MagicPermanent permanent = (MagicPermanent)target;
