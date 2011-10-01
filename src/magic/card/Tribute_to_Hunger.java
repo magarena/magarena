96698ba6747d3e0caa235128e83cfc35aa995199
beholder <unknown>
2011-10-01 09:25:46 +0200

    added Tribute to Hunger, Typhoid Rats, Unbreathing Horde

---
+++ src/magic/card/Tribute_to_Hunger.java
@@ -0,0 +1,75 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicType;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.action.MagicSacrificeAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicEventAction;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicSacrificeTargetPicker;
+
+public class Tribute_to_Hunger {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player=cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.TARGET_OPPONENT,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Target opponent$ sacrifices a creature. " + player + 
+                    " gains life equal to that creature's toughness.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer opponent) {
+        			if (opponent.controlsPermanentWithType(MagicType.Creature,game)) {
+        				game.addEvent(new MagicEvent(
+                            cardOnStack.getCard(),
+                            opponent,
+                            MagicTargetChoice.SACRIFICE_CREATURE,
+                            MagicSacrificeTargetPicker.getInstance(),
+                            new Object[]{(MagicPlayer)data[1]},
+                            EVENT_ACTION,
+                            "Choose a creature to sacrifice$."));
+                    }
+                }
+			});
+		}
+	};
+	
+	private static final MagicEventAction EVENT_ACTION = new MagicEventAction() {
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicSacrificeAction(permanent));
+                    final int toughness = permanent.getToughness(game);
+                    game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],toughness));
+                }
+            });
+        }
+    };
+}
---
+++ src/magic/card/Unbreathing_Horde.java
@@ -0,0 +1,78 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Unbreathing_Horde {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			final MagicTargetFilter targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+					MagicTargetFilter.TARGET_ZOMBIE_YOU_CONTROL,permanent);
+			int amount = game.filterTargets(player,targetFilter).size();
+			amount += game.filterTargets(player,MagicTargetFilter.TARGET_ZOMBIE_CARD_FROM_GRAVEYARD).size();
+			game.doAction(new MagicChangeCountersAction(
+					permanent,
+					MagicCounterType.PlusOne,
+					amount,
+					true));
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent},
+                    this,
+					permanent + " enters the battlefield with " + 
+                    amount + " +1/+1 counters on it.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+		}
+    };
+    
+    public static final MagicIfDamageWouldBeDealtTrigger T1 = new MagicIfDamageWouldBeDealtTrigger(4) {
+		@Override
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicDamage damage) {
+			if (!damage.isUnpreventable() && 
+				damage.getAmount() > 0 && 
+                damage.getTarget() == permanent &&
+                permanent.getCounters(MagicCounterType.PlusOne) > 0) {
+					// Prevention effect.
+					damage.setAmount(0);
+					return new MagicEvent(
+                            permanent,
+                            permanent.getController(),
+                            new Object[]{permanent},
+                            this,
+                            "Remove a +1/+1 counter from " + permanent + ".");
+			}
+			return MagicEvent.NONE;
+		}	
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction(
+                        (MagicPermanent)data[0],
+                        MagicCounterType.PlusOne,
+                        -1,
+                        true));
+		}
+    };
+}
