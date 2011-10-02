683dee7fff3a066e0fc8a56c540752d464a6ea55
melvin <unknown>
2011-10-02 16:16:42 +0800

    removed empty lines

--- src/magic/model/action/MagicAddEventAction.java
+++ src/magic/model/action/MagicAddEventAction.java
@@ -8,19 +8,16 @@ public class MagicAddEventAction extends MagicAction {
 	private final MagicEvent event;
 
 	public MagicAddEventAction(final MagicEvent event) {
-		
 		this.event=event;
 	}
 	
 	@Override
 	public void doAction(final MagicGame game) {
-
 		game.getEvents().addLast(event);
 	}
 
 	@Override
 	public void undoAction(final MagicGame game) {
-		
 		game.getEvents().removeLast();
 	}
 }
