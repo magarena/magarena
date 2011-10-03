dbafe1ab6c29715706396a6c80c2f2f053221592
beholder <unknown>
2011-10-03 09:57:30 +0200

    added Indomitable Archangel and Lumengrid Drake

---
+++ src/magic/card/Indomitable_Archangel.java
@@ -0,0 +1,24 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Indomitable_Archangel {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_ARTIFACT_YOU_CONTROL) {
+		@Override
+		public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return flags | MagicAbility.Shroud.getMask();
+		}
+		@Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+			return source.getController().getNrOfPermanentsWithType(MagicType.Artifact,game) >= 3;
+        }
+	};
+}
---
+++ src/magic/card/Lumengrid_Drake.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicType;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicBounceTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Lumengrid_Drake {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return player.getNrOfPermanentsWithType(MagicType.Artifact,game) >= 3 ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    MagicBounceTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Return target creature$ to its owner's hand.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+                }
+			});
+		}
+    };
+}
