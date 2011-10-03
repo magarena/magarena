315810818789993c0bf24a931bfdeab43c81bdbe
beholder <unknown>
2011-10-03 08:51:01 +0200

    added Chrome Steed and Etched Champion

---
+++ src/magic/card/Etched_Champion.java
@@ -0,0 +1,19 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicAbility;
+import magic.model.MagicType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+
+public class Etched_Champion {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return permanent.getController().getNrOfPermanentsWithType(MagicType.Artifact,game) >= 3 ?
+                flags | MagicAbility.ProtectionFromAllColors.getMask() :
+                flags;
+        }
+	};
+}
