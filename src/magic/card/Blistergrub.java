043e83cbdc8910ed297e8f50869658757970871f
beholder <unknown>
2011-10-03 11:35:01 +0200

    added Blight Mamba and Blistergrub

---
+++ src/magic/card/Blistergrub.java
@@ -0,0 +1,36 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Blistergrub {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+			final MagicPlayer player = permanent.getController();
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player},
+                        this,
+                        game.getOpponent(player)+ " loses 2 life."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPlayer player=(MagicPlayer)data[0];
+			game.doAction(new MagicChangeLifeAction(game.getOpponent(player),-2));
+		}
+    };
+}
