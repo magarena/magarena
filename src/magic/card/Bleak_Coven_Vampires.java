2e73859e40956daa7af39b9864d1bbb038ec7db1
beholder <unknown>
2011-10-03 08:44:12 +0200

    added Bleak Coven Vampires and Carapace Forger

---
+++ src/magic/card/Bleak_Coven_Vampires.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.MagicType;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Bleak_Coven_Vampires {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return player.getNrOfPermanentsWithType(MagicType.Artifact,game) >= 3 ?
+				new MagicEvent(
+					permanent,
+					player,
+					MagicTargetChoice.NEG_TARGET_PLAYER,
+					new Object[]{player},
+					this,
+					"Target player$ loses 4 life and " +
+					player + " gains 4 life.") :
+				MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-4));
+                }
+			});
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],4));
+		}
+    };
+}
---
+++ src/magic/card/Carapace_Forger.java
@@ -0,0 +1,19 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+
+public class Carapace_Forger {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			if (permanent.getController().getNrOfPermanentsWithType(MagicType.Artifact,game) >= 3) {
+				pt.add(2,2);
+			}
+		}
+	};
+}
