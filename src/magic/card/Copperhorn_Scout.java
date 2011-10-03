9b2268d79acf80046ae6ae5e18dbed15dfb400a4
beholder <unknown>
2011-10-03 12:58:45 +0200

    added Copper Myr and Copperhorn Scout

---
+++ src/magic/card/Copperhorn_Scout.java
@@ -0,0 +1,48 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicUntapAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+import java.util.Collection;
+
+public class Copperhorn_Scout {
+    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent data) {
+            final MagicPlayer player = permanent.getController();
+			return (permanent == data) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent,player},
+                    this,
+                    "Untap each other creature you control."):
+                MagicEvent.NONE;
+		}	
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			final Collection<MagicTarget> targets = game.filterTargets(
+                    (MagicPlayer)data[1],
+                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				if (target != permanent) {
+					game.doAction(new MagicUntapAction((MagicPermanent)target));
+				}
+			}
+		}		
+    };
+}
