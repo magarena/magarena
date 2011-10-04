f3521a1ba128ad151610960608940c00cbdfd617
beholder <unknown>
2011-10-04 07:13:57 +0200

    added Ferrovore and Recurring Nightmare

---
+++ src/magic/card/Ferrovore.java
@@ -0,0 +1,56 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicSacrificePermanentEvent;
+import magic.model.event.MagicTiming;
+
+public class Ferrovore {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{
+				MagicManaCost.RED.getCondition(),
+                MagicCondition.ONE_CREATURE_CONDITION,
+                MagicCondition.CONTROL_ARTIFACT_CONDITION
+            },
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			final MagicPlayer player = source.getController();
+			return new MagicEvent[]{
+					new MagicPayManaCostEvent(source,player,MagicManaCost.RED),
+					new MagicSacrificePermanentEvent(
+                    source,
+                    player,
+                    MagicTargetChoice.SACRIFICE_ARTIFACT)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    source + " gets +3/+0 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],3,0));
+		}
+	};
+}
---
+++ src/magic/card/Recurring_Nightmare.java
@@ -0,0 +1,64 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicCardAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicReanimateAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicSacrificePermanentEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicGraveyardTargetPicker;
+
+public class Recurring_Nightmare {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{
+	                MagicCondition.ONE_CREATURE_CONDITION,
+	                MagicCondition.SORCERY_CONDITION
+	            },
+            new MagicActivationHints(MagicTiming.Pump),
+            "Return") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_CREATURE)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(true),
+                    new Object[]{source,source.getController()},
+                    this,
+                    "Return " + source + " to its owner's hand. Return " +
+                    "target creature card$ from your graveyard to the battlefield.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicRemoveFromPlayAction((MagicPermanent)data[0],MagicLocationType.OwnersHand));
+			event.processTargetCard(game,choiceResults,0,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    game.doAction(new MagicReanimateAction((MagicPlayer)data[1],targetCard,MagicPlayCardAction.NONE));
+                }
+			});
+		}
+	};
+}
