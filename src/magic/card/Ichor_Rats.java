af844f6ad4e5fbddbf61246f300e127b558c7c2b
beholder <unknown>
2011-10-04 14:18:15 +0200

    added Ichor Rats and Instill Infection

---
+++ src/magic/card/Ichor_Rats.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangePoisonAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Ichor_Rats {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Each player gets a poison counter.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangePoisonAction(game.getPlayer(0),1));
+			game.doAction(new MagicChangePoisonAction(game.getPlayer(1),1));
+		}
+    };
+}
---
+++ src/magic/card/Instill_Infection.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicWeakenTargetPicker;
+
+public class Instill_Infection {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    new MagicWeakenTargetPicker(-1,-1),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Put a -1/-1 counter on target creature$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.MinusOne,1,true));
+                }
+			});
+            game.doAction(new MagicDrawAction((MagicPlayer)data[1],1));
+		}
+	};
+}
