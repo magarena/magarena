a454a0fc71ad0236f8f75d503463da57c82cd0a5
beholder <unknown>
2011-10-04 07:26:45 +0200

    added Regrowth and Blinking Spirit

---
+++ src/magic/card/Blinking_Spirit.java
@@ -0,0 +1,44 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+
+public class Blinking_Spirit {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{MagicManaCost.ZERO.getCondition()},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Return") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.ZERO)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Return " + source + " to its owner's hand.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicRemoveFromPlayAction((MagicPermanent)data[0],MagicLocationType.OwnersHand));
+		}
+	};
+}
