783b1545b7a05df4636adefe62cfc54e5efb7ff4
beholder <unknown>
2011-10-04 18:21:38 +0200

    added Molder Beast

---
+++ src/magic/card/Molder_Beast.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Molder_Beast {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			return (otherPermanent.isArtifact()) ?
+				new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{permanent},
+                    this,
+                    permanent + " gets +2/+0 until end of turn.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],2,0));
+		}
+    };
+}
