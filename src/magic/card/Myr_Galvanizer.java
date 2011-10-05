5a00e673275c6526c1f891f1a564272a9940a55b
beholder <unknown>
2011-10-05 10:53:48 +0200

    added Myr Galvanizer and Myr Propagator

---
+++ src/magic/card/Myr_Galvanizer.java
@@ -0,0 +1,74 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSource;
+import magic.model.action.MagicUntapAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostTapEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Myr_Galvanizer {
+    public static final MagicStatic S = new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_MYR_YOU_CONTROL) {
+        @Override
+        public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+    
+    public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{
+					MagicCondition.CAN_TAP_CONDITION,
+					MagicManaCost.ONE.getCondition()},
+            new MagicActivationHints(MagicTiming.Tapping),
+            "Untap") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostTapEvent(source,source.getController(),MagicManaCost.ONE)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source,source.getController()},
+                    this,
+                    "Untap each other Myr you control.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			final Collection<MagicTarget> targets = game.filterTargets(
+                    (MagicPlayer)data[1],
+                    MagicTargetFilter.TARGET_MYR_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				if (target != permanent) {
+					game.doAction(new MagicUntapAction((MagicPermanent)target));
+				}
+			}
+		}
+	};
+}
--- src/magic/model/target/MagicTargetFilter.java
+++ src/magic/model/target/MagicTargetFilter.java
@@ -763,6 +763,17 @@ public interface MagicTargetFilter {
 		}		
 	};
 	
+	MagicTargetFilter TARGET_MYR_YOU_CONTROL = new MagicTargetFilter() {
+		public boolean accept(final MagicGame game,final MagicPlayer player,final MagicTarget target) {
+			return target.getController() == player &&
+                   ((MagicPermanent)target).isCreature(game) && 
+                   ((MagicPermanent)target).hasSubType(MagicSubType.Myr,game);
+		}
+		public boolean acceptType(final MagicTargetType targetType) {
+			return targetType == MagicTargetType.Permanent;
+		}		
+	};
+	
 	MagicTargetFilter TARGET_VAMPIRE_YOU_CONTROL = new MagicTargetFilter() {
 		public boolean accept(final MagicGame game,final MagicPlayer player,final MagicTarget target) {
 			final MagicPermanent permanent = (MagicPermanent)target;
