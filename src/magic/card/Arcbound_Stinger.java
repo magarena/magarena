fec4b341aaaf7b7f3381c7092c6a5a9f694b4500
beholder <unknown>
2011-10-06 08:42:33 +0200

    added Arcbound Stinger and Arcbound Worker

---
+++ src/magic/card/Arcbound_Stinger.java
@@ -0,0 +1,12 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.trigger.MagicComesIntoPlayWithCounterTrigger;
+import magic.model.trigger.MagicModularTrigger;
+
+public class Arcbound_Stinger {
+	public static final MagicComesIntoPlayWithCounterTrigger T1 = 
+			new MagicComesIntoPlayWithCounterTrigger(MagicCounterType.PlusOne,"+1/+1",1);
+    
+    public static final MagicModularTrigger T2 = new MagicModularTrigger();
+}
