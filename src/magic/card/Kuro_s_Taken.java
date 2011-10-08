58f0e4ca0c6fb8cc0aeb8168d624458f669fdbe1
beholder <unknown>
2011-10-08 14:48:53 +0200

    added Kuro's Taken and Mothrider Samurai

---
+++ src/magic/card/Kuro_s_Taken.java
@@ -0,0 +1,17 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicRegenerationActivation;
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+import magic.model.trigger.MagicWhenBlocksPumpTrigger;
+
+public class Kuro_s_Taken {
+	private static final int amount = 1;
+	
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(amount,amount);
+	
+	public static final MagicWhenBlocksPumpTrigger T2 = new MagicWhenBlocksPumpTrigger(amount,amount);
+	
+	public static final MagicPermanentActivation A = new MagicRegenerationActivation(MagicManaCost.ONE_BLACK);
+}
