ea6ac20ca46cdbeb42eb41cc7b0667c8eb68b755
beholder <unknown>
2011-10-08 11:21:39 +0200

    added MagicBecomesBlockedPumpTrigger. changed Ichorclaw Myr to use MagicBecomesBlockedPumpTrigger

--- src/magic/card/Ichorclaw_Myr.java
+++ src/magic/card/Ichorclaw_Myr.java
@@ -1,31 +1,7 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
 
 public class Ichorclaw_Myr {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
-			return (permanent == data ) ?
-                new MagicEvent(
-                        permanent,
-                        permanent.getController(),
-                        new Object[]{permanent},
-                        this,
-                        permanent + " gets +2/+2 until end of turn."):
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeTurnPTAction((MagicPermanent)data[0],2,2));
-		}
-    };
+	public static final MagicBecomesBlockedPumpTrigger T = new MagicBecomesBlockedPumpTrigger(2,2);
 }
