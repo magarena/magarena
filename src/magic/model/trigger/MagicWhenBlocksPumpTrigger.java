da391c72b84906ecc79e33675aa2d060d73d23c1
beholder <unknown>
2011-10-08 11:30:12 +0200

    added MagicWhenBlocksPumpTrigger. added Araba Mothrider

---
+++ src/magic/model/trigger/MagicWhenBlocksPumpTrigger.java
@@ -0,0 +1,41 @@
+package magic.model.trigger;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+
+public class MagicWhenBlocksPumpTrigger extends MagicWhenBlocksTrigger {
+
+	private final int amountPower;
+	private final int amountToughness;
+	
+    public MagicWhenBlocksPumpTrigger(final int amountPower,final int amountToughness) {
+    	this.amountPower = amountPower;
+    	this.amountToughness = amountToughness;
+	}
+
+    @Override
+	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+		return (permanent == data ) ?
+            new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{permanent},
+                    this,
+                    permanent + " gets +" + amountPower + "/+" +  amountToughness + " until end of turn."):
+            MagicEvent.NONE;
+	}
+	@Override
+	public void executeEvent(
+            final MagicGame game,
+            final MagicEvent event,
+            final Object data[],
+            final Object[] choiceResults) {
+		game.doAction(new MagicChangeTurnPTAction(
+				(MagicPermanent)data[0],
+				amountPower,
+				amountToughness));
+	}
+}
+
