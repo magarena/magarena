62b0e9d6b128e6da92e01748f80c7da003406f06
beholder <unknown>
2011-10-08 14:13:18 +0200

    added Inner-Chamber Guard and Isao, Enlightened Bushi

---
+++ src/magic/card/Isao__Enlightened_Bushi.java
@@ -0,0 +1,17 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicRegenerationActivation;
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+import magic.model.trigger.MagicWhenBlocksPumpTrigger;
+
+public class Isao__Enlightened_Bushi {
+	private static final int amount = 2;
+	
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(amount,amount);
+	
+	public static final MagicWhenBlocksPumpTrigger T2 = new MagicWhenBlocksPumpTrigger(amount,amount);
+	
+	public static final MagicPermanentActivation A = new MagicRegenerationActivation(MagicManaCost.TWO);
+}
