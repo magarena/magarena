6c59a6ee45b8bc27f7bd7599a0f571a99afbf936
beholder <unknown>
2011-10-08 17:25:01 +0200

    added Silverstorm Samurai and Sokenzan Spellblade

---
+++ src/magic/card/Sokenzan_Spellblade.java
@@ -0,0 +1,58 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+import magic.model.trigger.MagicWhenBlocksPumpTrigger;
+
+public class Sokenzan_Spellblade {
+	private static final int amount = 1;
+	
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(amount,amount);
+	
+	public static final MagicWhenBlocksPumpTrigger T2 = new MagicWhenBlocksPumpTrigger(amount,amount);
+	
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicManaCost.ONE_RED.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.ONE_RED)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source,source.getController()},
+                    this,
+                    source + " gets +X/+0 until end of turn, where " +
+                    "X is the number of cards in your hand.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPlayer player = (MagicPlayer)data[1];
+			game.doAction(new MagicChangeTurnPTAction(
+					(MagicPermanent)data[0],
+					player.getHandSize(),
+					0));
+			
+		}
+	};
+}
