fe6b9d590e313821165ac0225352d81e06bd51c1
beholder <unknown>
2011-10-09 09:08:02 +0200

    added Cave Tiger and Chambered Nautilus

---
+++ src/magic/card/Cave_Tiger.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+
+public class Cave_Tiger {
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(1,1);
+}
