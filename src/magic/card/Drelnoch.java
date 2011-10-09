229484f1bc155918759a8d6b74f08ec11bd415c6
beholder <unknown>
2011-10-09 15:41:05 +0200

    added Drelnoch and Dromosaur

---
+++ src/magic/card/Drelnoch.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicSimpleMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Drelnoch {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			final MagicPlayer player = permanent.getController();
+			return (permanent == data ) ?
+		            new MagicEvent(
+		                    permanent,
+		                    player,
+		                    new MagicSimpleMayChoice(
+	                                player + " may draw two cards.",
+	                                MagicSimpleMayChoice.DRAW_CARDS,
+	                                2,
+	                                MagicSimpleMayChoice.DEFAULT_NONE),
+		                    new Object[]{player},
+		                    this,
+		                    player + " may$ draw two cards."):
+		            MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicDrawAction((MagicPlayer)data[0],2));
+			}
+		}
+    };
+}
