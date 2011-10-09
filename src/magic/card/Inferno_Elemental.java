71833401e28abb20d8c9b34114da7d204e7a6d26
beholder <unknown>
2011-10-09 12:12:15 +0200

    added Inferno Elemental and Wooden Stake

---
+++ src/magic/card/Inferno_Elemental.java
@@ -0,0 +1,70 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentList;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+import magic.model.trigger.MagicWhenBlocksTrigger;
+
+public class Inferno_Elemental {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            if (creature == permanent) {
+            	final MagicPermanentList plist = permanent.getBlockingCreatures();
+            	return new MagicEvent(
+            			permanent,
+            			permanent.getController(),
+            			new Object[]{permanent,plist},
+            			this,
+            			permanent + " deals 3 damage to blocking creature.");
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			final MagicPermanentList plist = (MagicPermanentList)data[1];
+			for (final MagicPermanent blocker : plist) {
+				final MagicDamage damage = new MagicDamage(permanent,blocker,3,false);
+                game.doAction(new MagicDealDamageAction(damage));
+        	}
+		}
+    };
+    
+    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+            final MagicPermanent blocked = permanent.getBlockedCreature();
+			return (permanent == data && blocked.isValid()) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{permanent,blocked},
+                    this,
+                    permanent + " deals 3 damage to " + blocked + "."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicDamage damage = new MagicDamage(
+					(MagicPermanent)data[0],
+					(MagicPermanent)data[1],
+					3,
+					false);
+            game.doAction(new MagicDealDamageAction(damage));
+		}
+    };
+}
---
+++ src/magic/card/Wooden_Stake.java
@@ -0,0 +1,81 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentList;
+import magic.model.MagicPermanentState;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeStateAction;
+import magic.model.action.MagicDestroyAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+import magic.model.trigger.MagicWhenBlocksTrigger;
+
+public class Wooden_Stake {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+			final MagicPermanent equippedCreature = permanent.getEquippedCreature();
+            if (creature == equippedCreature) {
+            	final MagicPermanentList plist = new MagicPermanentList();
+            	for (final MagicPermanent blocker : equippedCreature.getBlockingCreatures()) {
+            		if (blocker.hasSubType(MagicSubType.Vampire,game)) {
+            			plist.add(blocker);
+            		}
+            	}
+            	if (!plist.isEmpty()) {
+            		return new MagicEvent(
+            				permanent,
+            				permanent.getController(),
+            				new Object[]{plist},
+            				this,
+            				plist.size() > 1 ?
+            					"Destroy blocking Vampires. They can't be regenerated." :
+            					"Destroy blocking Vampire. It can't be regenerated.");
+            	}
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanentList plist = (MagicPermanentList)data[0];
+			for (final MagicPermanent blocker : plist) {
+				game.doAction(new MagicChangeStateAction(blocker,MagicPermanentState.CannotBeRegenerated,true));
+				game.doAction(new MagicDestroyAction(blocker));
+        	}
+		}
+    };
+    
+    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			final MagicPermanent equippedCreature = permanent.getEquippedCreature();
+            final MagicPermanent blocked = equippedCreature.getBlockedCreature();
+			return (equippedCreature == data &&
+					blocked.isValid() &&
+					blocked.hasSubType(MagicSubType.Vampire,game)) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{blocked},
+                    this,
+                    "Destroy " + blocked + ". It can't be regenerated."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent creature = (MagicPermanent)data[0];
+			game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.CannotBeRegenerated,true));
+			game.doAction(new MagicDestroyAction(creature));
+		}
+    };
+}
