a9c7b81e38b9245c925ef2b3a08ba33ba7c714ae
beholder <unknown>
2011-10-09 06:58:55 +0200

    added Alley Grifters. added static field to Imperious Perfect

---
+++ src/magic/card/Alley_Grifters.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Alley_Grifters {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			final MagicPlayer defendingPlayer = game.getOpponent(permanent.getController());
+			return (permanent == data ) ?
+		            new MagicEvent(
+		                    permanent,
+		                    permanent.getController(),
+		                    new Object[]{permanent,defendingPlayer},
+		                    this,
+		                    defendingPlayer + " discards a card."):
+		            MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.addEvent(new MagicDiscardEvent(
+					(MagicPermanent)data[0],
+					(MagicPlayer)data[1],
+					1,
+					false));
+		}
+    };
+}
