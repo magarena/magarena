1d184a3fa9f5646d9ce3bc1b54384f491d7c83bd
beholder <unknown>
2011-10-09 14:14:44 +0200

    added Beastmaster's Magemark and Berserk Murlodont

---
+++ src/magic/card/Beastmaster_s_Magemark.java
@@ -0,0 +1,65 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPlayAuraEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.mstatic.MagicLayer;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicPumpTargetPicker;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Beastmaster_s_Magemark {
+    public static final MagicStatic S = new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+    	@Override
+        public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+    	@Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+    		return target.isEnchanted();
+        }
+    };
+    
+    public static final MagicSpellCardEvent E = new MagicPlayAuraEvent(
+			MagicTargetChoice.POS_TARGET_CREATURE,
+			MagicPumpTargetPicker.getInstance());
+    
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			final MagicPlayer player = permanent.getController();
+            if (player == data.getController() &&
+            	data.isEnchanted()) {
+            	final int amount = data.getBlockingCreatures().size();
+            	return new MagicEvent(
+            			permanent,
+            			player,
+            			new Object[]{data,amount},
+            			this,
+            			data + " gets +" + amount + "/+" +  amount + " until end of turn.");
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction(
+					(MagicPermanent)data[0],
+					(Integer)data[1],
+					(Integer)data[1]));
+		}
+    };
+}
---
+++ src/magic/card/Berserk_Murlodont.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Berserk_Murlodont {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			final MagicPlayer player = permanent.getController();
+            if (player == data.getController() &&
+            	data.hasSubType(MagicSubType.Beast,game)) {
+            	final int amount = data.getBlockingCreatures().size();
+            	return new MagicEvent(
+            			permanent,
+            			player,
+            			new Object[]{data,amount},
+            			this,
+            			data + " gets +" + amount + "/+" +  amount + " until end of turn.");
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction(
+					(MagicPermanent)data[0],
+					(Integer)data[1],
+					(Integer)data[1]));
+		}
+    };
+}
