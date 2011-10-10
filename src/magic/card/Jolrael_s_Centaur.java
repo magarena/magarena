5b3a8a1fb8b496668242ad7e1934b47454496bee
beholder <unknown>
2011-10-10 12:56:50 +0200

    added Jolrael's Centaur and Mtenda Herder

---
+++ src/magic/card/Jolrael_s_Centaur.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicFlankingTrigger;
+
+public class Jolrael_s_Centaur {
+    public static final MagicFlankingTrigger T = new MagicFlankingTrigger();
+}
---
+++ src/magic/card/Mtenda_Herder.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicFlankingTrigger;
+
+public class Mtenda_Herder {
+    public static final MagicFlankingTrigger T = new MagicFlankingTrigger();
+}
