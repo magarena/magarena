e2b1e7e27b7de38b7653a4d026b055f783adf02c
beholder <unknown>
2011-10-10 13:14:16 +0200

    added Shadow Rider and Sidar Jabari

---
+++ src/magic/card/Shadow_Rider.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicFlankingTrigger;
+
+public class Shadow_Rider {
+    public static final MagicFlankingTrigger T = new MagicFlankingTrigger();
+}
---
+++ src/magic/card/Sidar_Jabari.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicTapAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTapTargetPicker;
+import magic.model.trigger.MagicFlankingTrigger;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+public class Sidar_Jabari {
+    public static final MagicFlankingTrigger T1 = new MagicFlankingTrigger();
+    
+    public static final MagicWhenAttacksTrigger T2 = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPlayer player = permanent.getController();
+			return (permanent == creature) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                        new MagicTapTargetPicker(true,false),
+                        MagicEvent.NO_DATA,
+                        this,
+                        "Tap target creature defending player controls."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    if (!creature.isTapped()) {
+                        game.doAction(new MagicTapAction(creature,true));
+                    }
+                }
+            });
+		}		
+    };
+}
