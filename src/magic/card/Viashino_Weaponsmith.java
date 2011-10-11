5630b5b085b9ef3b22495ddb90fcbafc127ba944
beholder <unknown>
2011-10-11 14:00:35 +0200

    added Venomous Dragonfly and Viashino Weaponsmith

---
+++ src/magic/card/Viashino_Weaponsmith.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+
+public class Viashino_Weaponsmith {
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(2,2,true);
+}
