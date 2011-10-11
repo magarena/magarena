43635e9c924479ca64a12a3b6c4529701e80d02b
beholder <unknown>
2011-10-11 11:19:46 +0200

    added Sparring Golem and Sylvan Basilisk

---
+++ src/magic/card/Sparring_Golem.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+
+public class Sparring_Golem {
+	public static final MagicBecomesBlockedPumpTrigger T = new MagicBecomesBlockedPumpTrigger(1,1,true);
+
+}
