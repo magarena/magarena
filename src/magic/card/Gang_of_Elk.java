8acaf26d0ff67c97f895b1a1bff5f271791ebe5a
beholder <unknown>
2011-10-11 10:24:31 +0200

    changed cards to make use of forEachBlocker parameter

--- src/magic/card/Gang_of_Elk.java
+++ src/magic/card/Gang_of_Elk.java
@@ -1,37 +1,7 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
 
 public class Gang_of_Elk {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
-            if (data == permanent) {
-            	final int amount = permanent.getBlockingCreatures().size() * 2;
-            	return new MagicEvent(
-            			permanent,
-            			permanent.getController(),
-            			new Object[]{permanent,amount},
-            			this,
-            			permanent + " gets +" + amount + "/+" +  amount + " until end of turn.");
-            }
-            return MagicEvent.NONE;
-		}
-		
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeTurnPTAction(
-					(MagicPermanent)data[0],
-					(Integer)data[1],
-					(Integer)data[1]));
-		}
-    };
+	public static final MagicBecomesBlockedPumpTrigger T = new MagicBecomesBlockedPumpTrigger(2,2,true);
 }
