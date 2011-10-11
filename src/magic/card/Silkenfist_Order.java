cb321e3f8ff8716a17a5f527ec1521bcf390e8a3
beholder <unknown>
2011-10-11 10:52:10 +0200

    added Silkenfist Fighter and Silkenfist Order

---
+++ src/magic/card/Silkenfist_Order.java
@@ -0,0 +1,34 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicUntapAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Silkenfist_Order {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+			final MagicPlayer player = permanent.getController();
+            return (creature == permanent) ?
+            		new MagicEvent(
+                            permanent,
+                            player,
+                            new Object[]{permanent},
+                            this, 
+                            "Untap " + permanent + ".") :
+            MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+				game.doAction(new MagicUntapAction((MagicPermanent)data[0]));
+		}
+    };
+}
