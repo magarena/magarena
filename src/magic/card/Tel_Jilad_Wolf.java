c1b72b93e099db7ffeb6a1b111c8980f89ad4515
beholder <unknown>
2011-10-11 13:02:50 +0200

    added Tel-Jilad Wolf and Thicket Basilisk

---
+++ src/magic/card/Tel_Jilad_Wolf.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentList;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+
+public class Tel_Jilad_Wolf {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            if (creature == permanent) {
+            	final MagicPermanentList plist = new MagicPermanentList();
+            	for (final MagicPermanent blocker : permanent.getBlockingCreatures()) {
+            		if (blocker.isArtifact(game) && blocker.isCreature(game)) {
+            			plist.add(blocker);
+            		}
+            	}
+            	if (!plist.isEmpty()) {
+            		final int amount = permanent.getBlockingCreatures().size() * 3;
+            		return new MagicEvent(
+                            permanent,
+                            permanent.getController(),
+                            new Object[]{permanent,amount},
+                            this,
+                            permanent + " gets +" + amount + "/+" +  amount + " until end of turn.");
+            	}
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeTurnPTAction(
+					(MagicPermanent)data[0],
+					(Integer)data[1],
+					(Integer)data[1]));
+		}
+    };
+}
---
+++ src/magic/card/Thicket_Basilisk.java
@@ -0,0 +1,76 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentList;
+import magic.model.MagicPermanentState;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeStateAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
+import magic.model.trigger.MagicWhenBlocksTrigger;
+
+public class Thicket_Basilisk {
+    public static final MagicWhenBecomesBlockedTrigger T1 = new MagicWhenBecomesBlockedTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            if (creature == permanent) {
+            	final MagicPermanentList plist = new MagicPermanentList();
+            	for (final MagicPermanent blocker : permanent.getBlockingCreatures()) {
+            		if (!blocker.hasSubType(MagicSubType.Wall,game)) {
+            			plist.add(blocker);
+            		}
+            	}
+            	if (!plist.isEmpty()) {
+            		return new MagicEvent(
+                            permanent,
+                            permanent.getController(),
+                            new Object[]{plist},
+                            this,
+                            plist.size() > 1 ?
+                					"Destroy blocking non-Wall creatures at end of combat." :
+                					"Destroy " + plist.get(0) + " at end of combat.");
+            	}
+            }
+            return MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanentList plist = (MagicPermanentList)data[0];
+			for (final MagicPermanent blocker : plist) {
+				game.doAction(new MagicChangeStateAction(blocker,MagicPermanentState.DestroyAtEndOfCombat,true));
+        	}
+		}
+    };
+    
+    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+            final MagicPermanent blocked = permanent.getBlockedCreature();
+			return (permanent == data &&
+					blocked.isValid() &&
+					!blocked.hasSubType(MagicSubType.Wall,game)) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{blocked},
+                    this,
+                    "Destroy " + blocked + " at end of combat."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent creature = (MagicPermanent)data[0];
+			game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.DestroyAtEndOfCombat,true));
+		}
+    };
+}
