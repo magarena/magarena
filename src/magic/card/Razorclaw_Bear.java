a704091f39eb9ea5bdbfd81ce36e445b800f5a99
beholder <unknown>
2011-10-11 09:06:23 +0200

    added Razorclaw Bear and Saprazzan Heir

---
+++ src/magic/card/Razorclaw_Bear.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicBecomesBlockedPumpTrigger;
+
+public class Razorclaw_Bear {
+	public static final MagicBecomesBlockedPumpTrigger T1 = new MagicBecomesBlockedPumpTrigger(2,2);
+}
