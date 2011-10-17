35c06bc62d9bc4a881fa69d6c99df58af7cea2ff
beholder <unknown>
2011-10-17 15:54:50 +0200

    changed Mutavault to get all creature types instead of setting the Changeling ability

--- src/magic/card/Mutavault.java
+++ src/magic/card/Mutavault.java
@@ -1,8 +1,5 @@
 package magic.card;
 
-import magic.model.MagicAbility;
-import magic.model.MagicCardDefinition;
-import magic.model.MagicChangeCardDefinition;
 import magic.model.MagicGame;
 import magic.model.MagicManaCost;
 import magic.model.MagicManaType;
@@ -10,6 +7,7 @@ import magic.model.MagicPayedCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicPowerToughness;
 import magic.model.MagicSource;
+import magic.model.MagicSubType;
 import magic.model.MagicType;
 import magic.model.action.MagicBecomesCreatureAction;
 import magic.model.condition.MagicArtificialCondition;
@@ -25,6 +23,7 @@ import magic.model.mstatic.MagicStatic;
 import magic.model.mstatic.MagicLayer;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 
 public class Mutavault {
     private static final MagicStatic PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
@@ -33,18 +32,22 @@ public class Mutavault {
 			pt.set(2,2);
 		}
     };
-    private static final MagicStatic AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
-        @Override
-        public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
-            return flags|MagicAbility.Changeling.getMask();
-        }
-    };
-    private static final MagicStatic ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    private static final MagicStatic TYPE = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
         @Override
 		public int getTypeFlags(final MagicPermanent permanent,final int flags) {
 			return flags|MagicType.Creature.getMask();
 		}
 	};
+    private static final MagicStatic STYPE = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+        @Override
+		public EnumSet<MagicSubType> getSubTypeFlags(
+                final MagicPermanent permanent,
+                final EnumSet<MagicSubType> flags) {
+            final EnumSet<MagicSubType> mod = flags.clone();
+            mod.addAll(MagicSubType.ALL_CREATURES);
+			return mod;
+        }
+    };
 
     public static final MagicManaActivation M = new MagicTapManaActivation(Arrays.asList(MagicManaType.Colorless),0);
 
@@ -76,7 +79,7 @@ public class Mutavault {
                 final Object[] data,
                 final Object[] choiceResults) {
 			final MagicPermanent permanent=(MagicPermanent)data[0];
-			game.doAction(new MagicBecomesCreatureAction(permanent,PT,AB,ST));
+			game.doAction(new MagicBecomesCreatureAction(permanent,PT,TYPE,STYPE));
 		}
 	};
 }
