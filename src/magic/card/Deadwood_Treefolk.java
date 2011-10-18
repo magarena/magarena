5e153abdcecbae3d32c46467b1f89546d83bda3a
beholder <unknown>
2011-10-18 17:27:50 +0200

    added Deadwood Treefolk

---
+++ src/magic/card/Deadwood_Treefolk.java
@@ -0,0 +1,79 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicCardAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicGraveyardTargetPicker;
+import magic.model.trigger.MagicComesIntoPlayWithCounterTrigger;
+import magic.model.trigger.MagicFadeVanishCounterTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+import magic.model.trigger.MagicWhenLeavesPlayTrigger;
+
+public class Deadwood_Treefolk {
+	public static final MagicComesIntoPlayWithCounterTrigger T1 = 
+			new MagicComesIntoPlayWithCounterTrigger(MagicCounterType.Charge,"time",3);
+	
+    public static final MagicFadeVanishCounterTrigger T2 = new MagicFadeVanishCounterTrigger("time");
+    
+    public static final MagicWhenComesIntoPlayTrigger T3 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(false),
+                    new Object[]{player},
+                    this,
+                    "Return another target creature card$ from your graveyard to your hand.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetCard(game,choiceResults,0,new MagicCardAction() {
+				public void doAction(final MagicCard targetCard) {
+					game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
+					game.doAction(new MagicMoveCardAction(targetCard,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+				}
+			});
+		}
+    };
+    
+    public static final MagicWhenLeavesPlayTrigger T4 = new MagicWhenLeavesPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			return new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(false),
+                    new Object[]{permanent.getController()},
+                    this,
+                    "Return another target creature card$ from your graveyard to your hand.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetCard(game,choiceResults,0,new MagicCardAction() {
+				public void doAction(final MagicCard targetCard) {
+					game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
+					game.doAction(new MagicMoveCardAction(targetCard,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+				}
+			});
+		}
+    };
+}
