0403ff6560be8646014443770e27051f31ae2573
beholder <unknown>
2011-10-19 09:12:34 +0200

    added Lavacore Elemental and Ravaging Riftwurm

---
+++ src/magic/card/Lavacore_Elemental.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicComesIntoPlayWithCounterTrigger;
+import magic.model.trigger.MagicFadeVanishCounterTrigger;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class Lavacore_Elemental {
+	public static final MagicComesIntoPlayWithCounterTrigger T1 = 
+			new MagicComesIntoPlayWithCounterTrigger(MagicCounterType.Charge,"time",1);
+	
+    public static final MagicFadeVanishCounterTrigger T2 = new MagicFadeVanishCounterTrigger("time");
+    
+    public static final MagicWhenDamageIsDealtTrigger T3 = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+			final MagicPlayer player = permanent.getController();
+			final MagicSource source = damage.getSource();
+			return (damage.isCombat() && 
+					damage.getTarget().isPlayer() &&
+					source.getController() == player) ?
+							new MagicEvent(
+								permanent,
+								player,
+								new Object[]{permanent},
+								this,
+								player + " puts a time counter on " + permanent + ".") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction((MagicPermanent)data[0],MagicCounterType.Charge,1,true));
+		}
+    };
+}
