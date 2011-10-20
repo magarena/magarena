51f36b621aecf0c9540b23cd438cc3f2f9173f37
beholder <unknown>
2011-10-20 16:50:27 +0200

    added Dauthi Ghoul and Dauthi Mercenary

---
+++ src/magic/card/Dauthi_Ghoul.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Dauthi_Ghoul {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			final MagicPlayer player = permanent.getController();
+			return (otherPermanent != permanent &&
+                    otherPermanent.isCreature(game) && 
+                    otherPermanent.hasAbility(game,MagicAbility.Shadow)) ?
+                    		new MagicEvent(
+        		                    permanent,
+        		                    player,
+        		                    new Object[]{permanent},
+        		                    this,
+        		                    player + " puts a +1/+1 counter on " + permanent + ".") :
+        		            MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction((MagicPermanent)data[0],MagicCounterType.PlusOne,1,true));
+		}
+    };
+}
---
+++ src/magic/card/Dauthi_Mercenary.java
@@ -0,0 +1,9 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPumpActivation;
+
+public class Dauthi_Mercenary {
+	public static final MagicPermanentActivation A = new MagicPumpActivation(MagicManaCost.ONE_BLACK,1,0);
+}
