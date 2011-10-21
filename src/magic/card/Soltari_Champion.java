028f22533fa2c43a910764666aea554a2ea2b41b
beholder <unknown>
2011-10-21 08:01:56 +0200

    added Soltari Champion and Soltari Crusader

---
+++ src/magic/card/Soltari_Champion.java
@@ -0,0 +1,48 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+import java.util.Collection;
+
+public class Soltari_Champion {
+    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent data) {
+            final MagicPlayer player = permanent.getController();
+			return (permanent == data) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent,player},
+                    this,
+                    "Other creatures you control get +1/+1 until end of turn."):
+                MagicEvent.NONE;
+		}	
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			final Collection<MagicTarget> targets = game.filterTargets(
+                    (MagicPlayer)data[1],
+                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				if (target != permanent) {
+					game.doAction(new MagicChangeTurnPTAction((MagicPermanent)target,1,1));
+				}
+			}
+		}		
+    };
+}
