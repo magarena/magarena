9030cee52786245f689dd5151ced619e7caafe00
melvin <unknown>
2011-10-21 16:34:22 +0800

    added test case for interaction between Hero of Bladehold and Circle of Flame

---
+++ src/magic/test/TestHoBBCoF.java
@@ -0,0 +1,43 @@
+package magic.test;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.MagicDuel;
+import magic.model.phase.MagicMainPhase;
+
+class TestHoBBCoF extends TestGameBuilder {    
+    public MagicGame getGame() {
+		final MagicDuel duel=new MagicDuel();
+		duel.setDifficulty(6);
+		
+		final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+		final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+		final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+		duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+		duel.setStartPlayer(0);
+		
+		final MagicGame game=duel.nextGame(true);
+		game.setPhase(MagicMainPhase.getFirstInstance());
+		final MagicPlayer player=game.getPlayer(0);
+		final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+	    addToHand(P,"Hero of Bladehold",1); 
+       
+
+        P = opponent;
+		
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+        createPermanent(game,P,"Circle of Flame",false,1);
+		
+		return game;
+    }
+}
