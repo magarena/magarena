733fbe7ac85fd8f0ed6a9a67b9b48a376eaf5493
beholder <unknown>
2011-10-21 16:46:29 +0200

    added MagicShadowTargetPicker. added Dauthi Embrace

---
+++ src/magic/model/target/MagicShadowTargetPicker.java
@@ -0,0 +1,52 @@
+package magic.model.target;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+
+public class MagicShadowTargetPicker extends MagicTargetPicker<MagicPermanent> {
+
+	private static final MagicTargetPicker INSTANCE = new MagicShadowTargetPicker();
+	
+	private MagicShadowTargetPicker() {
+	}
+
+	@Override
+	protected int getTargetScore(final MagicGame game,final MagicPlayer player,final MagicPermanent permanent) {
+		final long flags = permanent.getAllAbilityFlags(game);
+
+		// no score for ability overlap, not being able to deal combat damage,
+		// being blocked or blocking
+		if (MagicAbility.Shadow.hasAbility(flags) ||
+			MagicAbility.CannotAttackOrBlock.hasAbility(flags) ||
+			permanent.isBlocked() ||
+			permanent.isBlocking()) {
+			return 0;
+		}
+		
+		// attacking
+		if (game.getTurnPlayer() == player) {
+			// no score for not being able to attack or being unblockable
+			if (!permanent.canAttack(game) ||
+				!permanent.canBeBlocked(game,game.getOpponent(player))) {
+				return 0;
+			}
+		}
+		// blocking
+		else {
+			// no score for not being able to block
+			// or opponent has no shadow creatures
+			if (!permanent.canBlock(game) ||
+				!game.getOpponent(player).controlsPermanentWithAbility(MagicAbility.Shadow,game)) {
+				return 0;
+			}
+		}
+
+		return 10 + permanent.getPower(game);
+	}
+	
+	public static MagicTargetPicker getInstance() {
+		return INSTANCE;
+	}
+}
