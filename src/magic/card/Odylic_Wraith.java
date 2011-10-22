b40c720f30895b878da6a955dcac48ffd7d70829
beholder <unknown>
2011-10-22 08:36:37 +0200

    added Odylic Wraith and Order of Yawgmoth

---
+++ src/magic/card/Odylic_Wraith.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicSpecterTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Odylic_Wraith {
+    public static final MagicTrigger T = new MagicSpecterTrigger(false,false,false);
+}
---
+++ src/magic/card/Order_of_Yawgmoth.java
@@ -0,0 +1,8 @@
+package magic.card;
+
+import magic.model.trigger.MagicSpecterTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class Order_of_Yawgmoth {
+    public static final MagicTrigger T = new MagicSpecterTrigger(false,false,false);
+}
