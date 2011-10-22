3ccdf14cb1a0b2480c4ef5f79cd809e4dfbf699a
beholder <unknown>
2011-10-22 17:15:14 +0200

    added Jotun Owl Keeper and Kjeldoran Javelineer

---
+++ src/magic/card/Jotun_Owl_Keeper.java
@@ -0,0 +1,49 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+import magic.model.trigger.MagicWhenLeavesPlayTrigger;
+
+public class Jotun_Owl_Keeper {
+	public static final MagicCumulativeUpkeepTrigger T1 = new MagicCumulativeUpkeepTrigger("{W/U}");
+    
+    public static final MagicWhenLeavesPlayTrigger T2 = new MagicWhenLeavesPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+			if (permanent == data) {
+				final MagicPlayer player = permanent.getController();
+				final int amount = permanent.getCounters(MagicCounterType.Charge);
+				if (amount > 0) {
+				return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{player,amount},
+                    this,
+                    amount > 1 ?
+                    	player + " puts " + amount + " 1/1 white Bird creature tokens with flying onto the battlefield." :
+                    	player + " puts a 1/1 white Bird creature token with flying onto the battlefield.");
+				}
+			}
+			return MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			int amount = (Integer)data[1];
+			for (;amount>0;amount--) {
+				game.doAction(new MagicPlayTokenAction(
+						(MagicPlayer)data[0],
+						TokenCardDefinitions.getInstance().getTokenDefinition("Bird1")));
+			}
+		}
+    };
+}
---
+++ src/magic/card/Kjeldoran_Javelineer.java
@@ -0,0 +1,62 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Kjeldoran_Javelineer {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{1}");
+	
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION,},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Damage") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			final int amount = source.getCounters(MagicCounterType.Charge);
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.NEG_TARGET_ATTACKING_OR_BLOCKING_CREATURE,
+                    new MagicDamageTargetPicker(amount),
+                    new Object[]{source,amount},
+                    this,
+                    source + " deals " + amount + " damage to target creature$.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(
+                    		(MagicSource)data[0],
+                    		target,
+                    		(Integer)data[1],
+                    		false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+	};
+}
