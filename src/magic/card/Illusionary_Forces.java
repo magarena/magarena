75164f3157cd3056e73ccee19985330c1948f126
beholder <unknown>
2011-10-22 16:35:33 +0200

    added Illusionary Forces and Illusionary Wall

---
+++ src/magic/card/Illusionary_Forces.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Illusionary_Forces {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{U}");
+}
---
+++ src/magic/card/Illusionary_Wall.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Illusionary_Wall {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{U}");
+}
