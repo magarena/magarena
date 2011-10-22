2a301ab2cc2faf3772c83414b8976b6ce5167497
beholder <unknown>
2011-10-22 17:42:57 +0200

    added Phobian Phantasm and Uktabi Efreet

---
+++ src/magic/card/Phobian_Phantasm.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Phobian_Phantasm {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{B}");
+}
---
+++ src/magic/card/Uktabi_Efreet.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Uktabi_Efreet {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{G}");
+}
