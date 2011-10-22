f5170751df2e07fb5f28da26b7eb34c25f9ae9ec
beholder <unknown>
2011-10-22 19:37:00 +0200

    added Yavimaya Ants

---
+++ src/magic/card/Yavimaya_Ants.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicCumulativeUpkeepTrigger;
+
+public class Yavimaya_Ants {
+	public static final MagicCumulativeUpkeepTrigger T = new MagicCumulativeUpkeepTrigger("{G}{G}");
+}
