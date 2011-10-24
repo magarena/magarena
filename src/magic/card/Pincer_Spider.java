cb884d6a6a458fa0affb58d0644282bfe2991e2d
beholder <unknown>
2011-10-24 11:39:37 +0200

    added Llanowar Elite and Pincer Spider

---
+++ src/magic/card/Pincer_Spider.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPlayCardFromStackAction;
+import magic.model.choice.MagicKickerChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Pincer_Spider {
+    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+            final MagicCard card = cardOnStack.getCard();
+			return new MagicEvent(
+                    card,
+                    player,
+                    new MagicKickerChoice(MagicManaCost.THREE,false),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "$Play " + card + ". If " + card + " was kicked$, " +
+                    "it enters the battlefield with a +1/+1 counter on it.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final int kickerCount = (Integer)choiceResults[1];
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			final MagicPlayCardFromStackAction action = new MagicPlayCardFromStackAction(cardOnStack);
+			game.doAction(action);
+			if (kickerCount > 0) {
+				final MagicPermanent permanent = action.getPermanent();
+				game.doAction(new MagicChangeCountersAction(
+						permanent,
+	            		MagicCounterType.PlusOne,
+	            		1,
+	            		true));
+			}
+		}
+	};
+}
