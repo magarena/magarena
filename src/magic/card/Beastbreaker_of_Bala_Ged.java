a5acae88cbaf6956d22986321d95caa80e1de083
beholder <unknown>
2011-10-25 12:45:30 +0200

    added Beastbreaker of Bala Ged and Brimstone Mage

---
+++ src/magic/card/Beastbreaker_of_Bala_Ged.java
@@ -0,0 +1,44 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.mstatic.MagicLayer;
+import magic.model.MagicManaCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.event.MagicLevelUpActivation;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.mstatic.MagicStatic;
+
+public class Beastbreaker_of_Bala_Ged {
+    public static final MagicStatic S1 = new MagicStatic(MagicLayer.SetPT) {
+		@Override
+		public void getPowerToughness(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+			final int charges = permanent.getCounters(MagicCounterType.Charge);
+			if (charges >= 4) {
+				pt.set(6,6);
+			} else if (charges >= 1) {
+				pt.set(4,4);
+			}
+		}
+    };
+
+    public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
+		@Override
+		public long getAbilityFlags(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final long flags) {
+			if (permanent.getCounters(MagicCounterType.Charge) >= 4) {
+				return flags|MagicAbility.Trample.getMask();
+			}
+			return flags;
+		}
+    };
+
+	public static final MagicPermanentActivation A = new MagicLevelUpActivation(MagicManaCost.TWO_GREEN,4);
+}
---
+++ src/magic/card/Brimstone_Mage.java
@@ -0,0 +1,87 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicLevelUpActivation;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+
+public class Brimstone_Mage {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.SetPT) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			final int charges = permanent.getCounters(MagicCounterType.Charge);
+			if (charges >= 3) {
+				pt.set(2,4);
+			} else if (charges >= 1) {
+				pt.set(2,3);
+			}
+		}		
+	};
+
+	public static final MagicPermanentActivation A1 = new MagicLevelUpActivation(MagicManaCost.THREE_RED,3);
+	
+	public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
+			new MagicCondition[]{
+                MagicCondition.CHARGE_COUNTER_CONDITION,
+                MagicCondition.CAN_TAP_CONDITION
+			},
+			new MagicActivationHints(MagicTiming.Removal),
+            "Damage") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			final int amount = source.getCounters(MagicCounterType.Charge) >= 3 ? 3:1;
+			return new MagicEvent(
+                    source,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(amount),
+                    new Object[]{source,amount},
+                    this,
+                    source + " deals " + amount + " damage to target creature or player$.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(
+                    		(MagicSource)data[0],
+                    		target,
+                    		(Integer)data[1],
+                    		false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+	};
+}
