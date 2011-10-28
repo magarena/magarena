a1b0b39cb22500a3f3f39a232332b417d31e4d1b
beholder <unknown>
2011-10-28 18:56:45 +0200

    added Lighthouse Chronologist and Null Champion

---
+++ src/magic/card/Null_Champion.java
@@ -0,0 +1,70 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSource;
+import magic.model.action.MagicRegenerateAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicLevelUpActivation;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+
+public class Null_Champion {
+	public static final MagicStatic S1 = new MagicStatic(MagicLayer.SetPT) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			final int charges = permanent.getCounters(MagicCounterType.Charge);
+			if (charges >= 4) {
+				pt.set(7,3);
+			} else if (charges >= 1) {
+				pt.set(4,2);
+			}
+		}		
+	};
+
+	public static final MagicPermanentActivation A1 = new MagicLevelUpActivation(MagicManaCost.THREE,4);
+	
+	public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
+			new MagicCondition[]{
+                MagicCondition.FOUR_CHARGE_COUNTERS_CONDITION,
+                MagicCondition.CAN_REGENERATE_CONDITION,
+                MagicManaCost.BLACK.getCondition()
+			},
+			new MagicActivationHints(MagicTiming.Pump),
+            "Regen") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+					new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.BLACK)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    "Regenerate " + source + ".");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			game.doAction(new MagicRegenerateAction((MagicPermanent)data[0]));
+		}
+	};
+}
