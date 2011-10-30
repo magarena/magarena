8f0781923f49dd7b99202183458bd275a26b797f
beholder <unknown>
2011-10-30 11:32:39 +0100

    added Deathknell Kami and Gibbering Kami

---
+++ src/magic/card/Deathknell_Kami.java
@@ -0,0 +1,56 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentState;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeStateAction;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Deathknell_Kami {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(1);
+	
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{MagicManaCost.TWO.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicPayManaCostEvent(
+					source,
+					source.getController(),
+					MagicManaCost.TWO)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    source + " gets +1/+1 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			game.doAction(new MagicChangeTurnPTAction(permanent,1,1));
+			game.doAction(new MagicChangeStateAction(
+					permanent,
+					MagicPermanentState.SacrificeAtEndOfTurn,
+					true));
+		}
+	};
+}
