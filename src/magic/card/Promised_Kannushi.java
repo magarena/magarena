07b1ba8045938302134efc17a6551b814bde8162
beholder <unknown>
2011-10-30 15:18:23 +0100

    added Nightsoil Kami and Promised Kannushi

---
+++ src/magic/card/Promised_Kannushi.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Promised_Kannushi {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(7);
+}
