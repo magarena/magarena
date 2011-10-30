7e3a8318d49349c21e9aa098ed004b4c0c3e3e5d
beholder <unknown>
2011-10-30 11:16:05 +0100

    added Burr Grafter and Crawling Filth

---
+++ src/magic/card/Crawling_Filth.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Crawling_Filth {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(5);
+}
