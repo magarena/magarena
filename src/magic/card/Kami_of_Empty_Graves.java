36377f6743337bf1251e985cf9bba41f9f72994b
beholder <unknown>
2011-10-30 13:49:46 +0100

    added Kami of Empty Graves and Kami of Lunacy

---
+++ src/magic/card/Kami_of_Empty_Graves.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Kami_of_Empty_Graves {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(3);
+}
