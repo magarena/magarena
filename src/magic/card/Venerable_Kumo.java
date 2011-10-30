905c0a37b63341a39b6abf36eb6dec77734c997d
beholder <unknown>
2011-10-30 16:44:33 +0100

    added Venerable Kumo

---
+++ src/magic/card/Venerable_Kumo.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Venerable_Kumo {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(4);
+}
