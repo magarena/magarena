97049a1a17bbafc856bed4aed8f120687e87bb1b
beholder <unknown>
2011-10-30 11:41:11 +0100

    added Forked-Branch Garami and Hundred-Talon Kami

---
+++ src/magic/card/Forked_Branch_Garami.java
@@ -0,0 +1,9 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Forked_Branch_Garami {
+	public static final MagicSoulshiftTrigger T1 = new MagicSoulshiftTrigger(4);
+	
+	public static final MagicSoulshiftTrigger T2 = new MagicSoulshiftTrigger(4);
+}
---
+++ src/magic/card/Hundred_Talon_Kami.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Hundred_Talon_Kami {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(4);
+}
