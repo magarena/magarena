ce12a88cbf013f37ec18543adccb7031990e7afe
beholder <unknown>
2011-10-30 16:41:25 +0100

    added Thousand-legged Kami and Torii Watchward

---
+++ src/magic/card/Thousand_legged_Kami.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicSoulshiftTrigger;
+
+public class Thousand_legged_Kami {
+	public static final MagicSoulshiftTrigger T = new MagicSoulshiftTrigger(7);
+}
