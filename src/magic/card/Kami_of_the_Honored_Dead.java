418ce44fb1557f69e24a9ed6f205ac2de887bbf0
beholder <unknown>
2011-10-30 13:59:37 +0100

    added Kami of the Honored Dead and Kami of the Palace Fields

---
+++ src/magic/card/Kami_of_the_Honored_Dead.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicSoulshiftTrigger;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class Kami_of_the_Honored_Dead {
+	public static final MagicSoulshiftTrigger T1 = new MagicSoulshiftTrigger(6);
+	
+	public static final MagicWhenDamageIsDealtTrigger T2 = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicPlayer player = permanent.getController();
+            final int amount = damage.getDealtAmount();
+			return (damage.getTarget() == permanent) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{player,amount},
+                        this,
+                        player + " gains " + amount + " life.") :
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeLifeAction(
+					(MagicPlayer)data[0],
+					(Integer)data[1]));
+		}
+    };
+}
