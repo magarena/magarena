3ae34f65763667238099c5c7f8e8704576ff2624
beholder <unknown>
2011-10-31 10:33:57 +0100

    corrected Abyssal Nightstalker and Bone Dancer

--- src/magic/card/Abyssal_Nightstalker.java
+++ src/magic/card/Abyssal_Nightstalker.java
@@ -1,29 +1,27 @@
 package magic.card;
 
-import magic.model.MagicDamage;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicPlayer;
 import magic.model.event.MagicDiscardEvent;
 import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
+import magic.model.trigger.MagicWhenAttacksUnblockedTrigger;
 
 public class Abyssal_Nightstalker {
-	public static final MagicIfDamageWouldBeDealtTrigger T = new MagicIfDamageWouldBeDealtTrigger(1) {
+	public static final MagicWhenAttacksUnblockedTrigger T = new MagicWhenAttacksUnblockedTrigger() {
 		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-			return (damage.getSource() == permanent &&
-				damage.isCombat() &&
-				damage.getTarget().isPlayer() &&
-				((MagicPlayer)damage.getTarget()).getHandSize() > 0 &&
-				!permanent.isBlocked()) ?
-					new MagicEvent(
-						permanent,
-						permanent.getController(),
-						new Object[]{permanent,damage.getTarget()},
-						this,
-						damage.getTarget() + " discards a card.") :
-			MagicEvent.NONE;
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            if (creature == permanent) {
+            	final MagicPlayer player = permanent.getController();
+    			final MagicPlayer opponent = game.getOpponent(player);
+    			return new MagicEvent(
+    					permanent,
+    					player,
+    					new Object[]{permanent,opponent},
+    					this,
+    					opponent + " discards a card.");
+            }
+            return MagicEvent.NONE;
 		}
 		
 		@Override
--- src/magic/card/Bone_Dancer.java
+++ src/magic/card/Bone_Dancer.java
@@ -3,41 +3,37 @@ package magic.card;
 import java.util.List;
 
 import magic.model.MagicCard;
-import magic.model.MagicDamage;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.MagicPermanentState;
 import magic.model.MagicPlayer;
-import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicChangeStateAction;
 import magic.model.action.MagicPlayCardAction;
 import magic.model.action.MagicReanimateAction;
 import magic.model.choice.MagicMayChoice;
 import magic.model.event.MagicEvent;
 import magic.model.target.MagicTarget;
 import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
+import magic.model.trigger.MagicWhenAttacksUnblockedTrigger;
 
 public class Bone_Dancer {
-	public static final MagicIfDamageWouldBeDealtTrigger T = new MagicIfDamageWouldBeDealtTrigger(1) {
+	public static final MagicWhenAttacksUnblockedTrigger T = new MagicWhenAttacksUnblockedTrigger() {
 		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-			if (damage.getSource() == permanent &&
-				damage.isCombat() &&
-				damage.getTarget().isPlayer() &&
-				!permanent.isBlocked()) {
-				final MagicPlayer player = permanent.getController();
-				final int amount = damage.getAmount();
-				damage.setAmount(0);
-				return new MagicEvent(
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            if (creature == permanent) {
+            	final MagicPlayer player = permanent.getController();
+            	final MagicPlayer opponent = game.getOpponent(player);
+            	return new MagicEvent(
 						permanent,
 						player,
 						new MagicMayChoice(
 	                            player + " may put the top creature card of " +
-	                            damage.getTarget() + "'s graveyard onto the " +
+	                            opponent + "'s graveyard onto the " +
 	                            "battlefield under his or her control."),
-						new Object[]{player,permanent,damage.getTarget(),amount},
+						new Object[]{player,opponent,permanent},
 						this,
 						player + " may put the top creature card of " +
-	                    damage.getTarget() + "'s graveyard onto the " +
+						opponent + "'s graveyard onto the " +
 	                    "battlefield under his or her control.");
             }
             return MagicEvent.NONE;
@@ -50,7 +46,7 @@ public class Bone_Dancer {
                 final Object data[],
                 final Object[] choiceResults) {
 			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-				final MagicPlayer opponent = (MagicPlayer)data[2];
+				final MagicPlayer opponent = (MagicPlayer)data[1];
 				final List<MagicTarget> targets =
 		                game.filterTargets(opponent,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD);
 				if (targets.size() > 0) {
@@ -60,14 +56,10 @@ public class Bone_Dancer {
 							card,
 							MagicPlayCardAction.NONE));
 				}
-			} else {
-				final MagicDamage damage = new MagicDamage(
-						(MagicPermanent)data[1],
-						(MagicTarget)data[2],
-						(Integer)data[3],
-						false);
-                game.doAction(new MagicDealDamageAction(damage));
-			}	
+				game.doAction(new MagicChangeStateAction(
+						(MagicPermanent)data[2],
+						MagicPermanentState.NoCombatDamage,true));
+			}
 		}
     };
 }
