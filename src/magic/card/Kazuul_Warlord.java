e50154ac1b6cde3efe891e00674373174c9a18c0
beholder <unknown>
2011-11-01 09:04:48 +0100

    added Kazandu Blademaster and Kazuul Warlord

---
+++ src/magic/card/Kazuul_Warlord.java
@@ -0,0 +1,60 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSubType;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicSimpleMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
+
+public class Kazuul_Warlord {
+    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+			final MagicPlayer player = permanent.getController();
+			return (otherPermanent.getController() == player &&
+                    otherPermanent.hasSubType(MagicSubType.Ally,game)) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new MagicSimpleMayChoice(
+                        		player + " may put a +1/+1 counter on " +
+                        		"each Ally creature he or she controls.",
+                                MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
+                                1,
+                                MagicSimpleMayChoice.DEFAULT_YES),
+                        new Object[]{player},
+                        this,
+                        player + " may$ put a +1/+1 counter on " +
+                        "each Ally creature he or she controls.") :
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				final Collection<MagicTarget> targets =
+						game.filterTargets((MagicPlayer)data[0],MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
+				for (final MagicTarget target : targets) {
+					game.doAction(new MagicChangeCountersAction(
+							(MagicPermanent)target,
+							MagicCounterType.PlusOne,
+							1,
+							true));
+				}
+			}			
+		}		
+    };
+}
