8de1e9048cbebadc37d41efbcd21450d43f3a6ef
beholder <unknown>
2011-11-02 16:37:22 +0100

    added Skyhunter Cub and Sunspear Shikari

---
+++ src/magic/card/Sunspear_Shikari.java
@@ -0,0 +1,22 @@
+
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+
+public class Sunspear_Shikari {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.Ability) {
+		@Override
+		public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return permanent.isEquipped() ? 
+                flags | 
+                MagicAbility.FirstStrike.getMask() |
+                MagicAbility.LifeLink.getMask()
+                :
+                flags;
+		}
+	};
+}
