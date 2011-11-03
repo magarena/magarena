b0284f66ed7f027b451ad5e6e118f93c6c41cca8
beholder <unknown>
2011-11-03 10:25:56 +0100

    added Spiraling Duelist and Concussive Bolt

---
+++ src/magic/card/Concussive_Bolt.java
@@ -0,0 +1,62 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicAbility;
+import magic.model.MagicCard;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Concussive_Bolt {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+            final MagicCard card = cardOnStack.getCard();
+			return new MagicEvent(
+                    card,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(4),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    card + " deals 4 damage to target player$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));			
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage = new MagicDamage(cardOnStack.getCard(),player,4,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    final Collection<MagicTarget> targets =
+                            game.filterTargets(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                        for (final MagicTarget target : targets) {
+                        	game.doAction(new MagicSetAbilityAction(
+                        			(MagicPermanent)target,
+                        			MagicAbility.CannotBlock));
+                        }
+                }
+			});
+		}
+	};
+}
