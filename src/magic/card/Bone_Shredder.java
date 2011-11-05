01f635753dc24a4e5afe6418592fe6f380eb004d
beholder <unknown>
2011-11-05 08:09:46 +0100

    added Basalt Gargoyle and Bone Shredder

---
+++ src/magic/card/Bone_Shredder.java
@@ -0,0 +1,39 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDestroyTargetPicker;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Bone_Shredder {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_NONARTIFACT_NONBLACK_CREATURE,
+                    new MagicDestroyTargetPicker(false),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Destroy target nonartifact, nonblack creature$.");
+		}
+
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDestroyAction(creature));
+                }
+			});
+		}
+    };
+}
