c58d78d1f354699a928623a34fb27164faa408e1
beholder <unknown>
2011-11-05 12:55:13 +0100

    Death Baron should give others deathtouch

--- src/magic/card/Death_Baron.java
+++ src/magic/card/Death_Baron.java
@@ -1,5 +1,6 @@
 package magic.card;
 
+import magic.model.MagicAbility;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicPowerToughness;
@@ -9,18 +10,31 @@ import magic.model.mstatic.MagicStatic;
 import magic.model.target.MagicTargetFilter;
 
 public class Death_Baron {
-    public static final MagicStatic S = new MagicStatic(
-        MagicLayer.ModPT, 
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
-        @Override
-        public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return filter.accept(game, source.getController(), target) &&
-                   (target.hasSubType(MagicSubType.Skeleton,game) || 
-                    (source != target && target.hasSubType(MagicSubType.Zombie,game)));
-        }
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.ModPT, 
+			MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+		@Override
+		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+			pt.add(1,1);
+		}
+		@Override
+		public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+			return target.hasSubType(MagicSubType.Skeleton,game) || 
+					(source != target && target.hasSubType(MagicSubType.Zombie,game));
+		}
+	};
+    
+    public static final MagicStatic S2 = new MagicStatic(
+    		MagicLayer.Ability, 
+    		MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+    	@Override
+    	public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+    		return flags | MagicAbility.Deathtouch.getMask();
+    	}
+    	@Override
+    	public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+    		return target.hasSubType(MagicSubType.Skeleton,game) || 
+    				(source != target && target.hasSubType(MagicSubType.Zombie,game));
+    	}
     };
 }
