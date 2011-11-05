a52b838f77775acd10d91ecdd84930c1c3b8c933
beholder <unknown>
2011-11-05 08:15:28 +0100

    added Citanul Centaurs and Cradle Guard

---
+++ src/magic/card/Citanul_Centaurs.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Citanul_Centaurs {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
---
+++ src/magic/card/Cradle_Guard.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Cradle_Guard {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
