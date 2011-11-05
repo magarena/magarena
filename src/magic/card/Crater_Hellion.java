9b0ced63c96461f23cf7324522ac7fe04dfec261
beholder <unknown>
2011-11-05 14:56:15 +0100

    added Crater Hellion and Deepcavern Imp

---
+++ src/magic/card/Crater_Hellion.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Crater_Hellion {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent,player},
+                    this,
+                    permanent + " deals 4 damage to each other creature.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicSource permanent = (MagicPermanent)data[0];
+			final Collection<MagicTarget> creatures =
+                game.filterTargets((MagicPlayer)data[1],MagicTargetFilter.TARGET_CREATURE);
+			for (final MagicTarget creature : creatures) {
+				if (permanent != creature) {
+					final MagicDamage damage=new MagicDamage(permanent,creature,4,false);
+					game.doAction(new MagicDealDamageAction(damage));
+				}
+			}
+		}
+    };
+}
---
+++ src/magic/card/Deepcavern_Imp.java
@@ -0,0 +1,58 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPermanentState;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeStateAction;
+import magic.model.action.MagicSacrificeAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+
+public class Deepcavern_Imp {
+    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
+			final MagicPlayer player = permanent.getController();
+			if (player == data &&
+				permanent.hasState(MagicPermanentState.MustPayEchoCost)) {
+					if (player.getHandSize() == 0) {
+						game.doAction(new MagicSacrificeAction(permanent));
+					} else {
+						return new MagicEvent(
+								permanent,
+								player,
+								new MagicMayChoice(
+										player + " may discard a card."),
+								new Object[]{permanent,player},
+								this,
+								player + " may$ discard a card. " +
+								"If he or she doesn't, sacrifice " + permanent + ".");
+					}
+				}
+				return MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+				game.addEvent(new MagicDiscardEvent(
+						permanent,
+						(MagicPlayer)data[1],
+						1,
+						false));
+				game.doAction(new MagicChangeStateAction(
+						permanent,
+						MagicPermanentState.MustPayEchoCost,false));
+			} else {
+				game.doAction(new MagicSacrificeAction(permanent));
+			}
+		}		
+    };
+}
