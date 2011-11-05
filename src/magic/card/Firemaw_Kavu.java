5013805c738d5eb70db5243616b869f5d60b761d
beholder <unknown>
2011-11-05 19:25:33 +0100

    added Firemaw Kavu and Flamecore Elemental

---
+++ src/magic/card/Firemaw_Kavu.java
@@ -0,0 +1,71 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+import magic.model.trigger.MagicWhenLeavesPlayTrigger;
+
+public class Firemaw_Kavu {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    new MagicDamageTargetPicker(2),
+                    new Object[]{permanent},
+                    this,
+                    permanent + " deals 2 damage to target creature$.");
+		}	
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+				public void doAction(final MagicPermanent creature) {
+                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],creature,2,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+    };
+    
+    public static final MagicWhenLeavesPlayTrigger T3 = new MagicWhenLeavesPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent data) {
+			final MagicPlayer player = permanent.getController();
+			return (permanent == data) ?
+					new MagicEvent(
+							permanent,
+		                    player,
+		                    MagicTargetChoice.TARGET_CREATURE,
+		                    new MagicDamageTargetPicker(4),
+		                    new Object[]{permanent},
+		                    this,
+		                    permanent + " deals 4 damage to target creature$.") :
+					MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+				public void doAction(final MagicPermanent creature) {
+                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],creature,4,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+    };
+}
---
+++ src/magic/card/Flamecore_Elemental.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Flamecore_Elemental {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
