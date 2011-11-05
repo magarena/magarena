f4ad99de3202ceec933a6e160b93d1bac736dca3
melvin <unknown>
2011-11-05 21:01:15 +0800

    test exile of tokens, should not be able to return

---
+++ src/magic/test/TestExileToken.java
@@ -0,0 +1,44 @@
+package magic.test;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.MagicDuel;
+import magic.model.phase.MagicMainPhase;
+
+class TestExileToken extends TestGameBuilder {    
+    public MagicGame getGame() {
+		final MagicDuel tournament=new MagicDuel();
+		tournament.setDifficulty(6);
+		
+		final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+		final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+		final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+		tournament.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+		tournament.setStartPlayer(0);
+		
+		final MagicGame game=tournament.nextGame(true);
+		game.setPhase(MagicMainPhase.getFirstInstance());
+		final MagicPlayer player=game.getPlayer(0);
+		final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+	    addToHand(P,"Journey to Nowhere",1); 
+	    addToHand(P,"Naturalize",1); 
+       
+
+        P = opponent;
+		
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+		createPermanent(game,P,"Rupture Spire",false,8);
+	    addToHand(P,"Rise of the Hobgoblins",1); 
+		
+		return game;
+    }
+}
