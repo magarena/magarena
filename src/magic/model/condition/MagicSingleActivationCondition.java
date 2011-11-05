153ea2703ae115861989675dc418a6b59fd3c97e
melvin <unknown>
2011-11-05 20:58:33 +0800

    store a reference to the MagicActivation object instead of just the id

--- src/magic/model/condition/MagicSingleActivationCondition.java
+++ src/magic/model/condition/MagicSingleActivationCondition.java
@@ -2,18 +2,19 @@ package magic.model.condition;
 
 import magic.model.MagicGame;
 import magic.model.MagicSource;
+import magic.model.event.MagicActivation;
 
 public class MagicSingleActivationCondition implements MagicCondition {
 
-	private long actId;
+	private MagicActivation act;
 
 	/** This must be set by the activation. */
-	public void setActivation(final long aActId) {
-		this.actId = aActId;
+	public void setActivation(final MagicActivation aAct) {
+        act = aAct;
 	}
 
 	@Override
 	public boolean accept(final MagicGame game,final MagicSource source) {
-		return !game.getStack().hasActivationOnTop(source,actId);
+		return !game.getStack().hasActivationOnTop(source,act);
 	}
 }
