82d47001827e82afefeccc7258a1df688c69cdea
beholder <unknown>
2011-11-06 07:16:20 +0100

    added Goblin Patrol and Goblin War Buggy

---
+++ src/magic/card/Goblin_Patrol.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Goblin_Patrol {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
---
+++ src/magic/card/Goblin_War_Buggy.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Goblin_War_Buggy {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
