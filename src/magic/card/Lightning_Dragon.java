b9dc2d7447d636ce7da0929f9b693ceae2de8fcf
beholder <unknown>
2011-11-06 16:50:58 +0100

    added Keldon Vandals and Lightning Dragon

---
+++ src/magic/card/Lightning_Dragon.java
@@ -0,0 +1,12 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPumpActivation;
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Lightning_Dragon {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+	
+	public static final MagicPermanentActivation A = new MagicPumpActivation(MagicManaCost.RED,1,0);
+}
