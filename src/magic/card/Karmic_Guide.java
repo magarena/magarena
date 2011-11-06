3d185dfe896be2d12c741a01ca428a397fa19d4c
beholder <unknown>
2011-11-06 16:41:35 +0100

    added Karmic Guide and Keldon Champion

---
+++ src/magic/card/Karmic_Guide.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicCardAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicReanimateAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicGraveyardTargetPicker;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Karmic_Guide {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+	                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(true),
+                    new Object[]{player},
+                    this,
+                    "Return target creature card$ from " +
+                    "your graveyard to the battlefield.");
+		}
+		
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object data[],
+				final Object[] choiceResults) {
+			event.processTargetCard(game,choiceResults,0,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+					game.doAction(new MagicReanimateAction(
+							(MagicPlayer)data[0],
+							targetCard,
+							MagicPlayCardAction.NONE));
+                }
+			});
+		}
+    };
+}
---
+++ src/magic/card/Keldon_Champion.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Keldon_Champion {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(3),
+                    MagicEvent.NO_DATA,
+                    this,
+                    permanent + " deals 3 damage to target player$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+					final MagicDamage damage = new MagicDamage(
+							event.getSource(),
+							target,
+							3,
+							false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+    };
+}
