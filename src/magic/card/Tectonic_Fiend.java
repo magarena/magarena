42bc6341c5291e763c5fbea497bc61cb8d41fd54
beholder <unknown>
2011-11-07 09:39:20 +0100

    added Tectonic Fiend and Thran War Machine

---
+++ src/magic/card/Tectonic_Fiend.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Tectonic_Fiend {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
---
+++ src/magic/card/Thran_War_Machine.java
@@ -0,0 +1,7 @@
+package magic.card;
+
+import magic.model.trigger.MagicEchoTrigger;
+
+public class Thran_War_Machine {
+	public static final MagicEchoTrigger T = new MagicEchoTrigger();
+}
