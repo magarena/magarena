320084404b0a2038b2f22b30790b087ac10d03eb
beholder <unknown>
2011-11-07 08:50:22 +0100

    added Stingscourger and Subterranean Shambler

---
+++ src/magic/card/Stingscourger.java
@@ -0,0 +1,49 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicBounceTargetPicker;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Stingscourger {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger("{3}{R}");
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                    MagicBounceTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Return target creature$ your opponent " +
+                    "controls to its owner's hand.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(
+                    		creature,
+                    		MagicLocationType.OwnersHand));
+                }
+			});
+		}
+    };
+}
---
+++ src/magic/card/Subterranean_Shambler.java
@@ -0,0 +1,80 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicEchoTrigger;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+import magic.model.trigger.MagicWhenLeavesPlayTrigger;
+
+public class Subterranean_Shambler {
+	public static final MagicEchoTrigger T1 = new MagicEchoTrigger();
+	
+	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent,player},
+                    this,
+                    permanent + " deals 1 damage to each creature without flying.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicSource source = (MagicSource)data[0];
+			final Collection<MagicTarget> creatures =
+                game.filterTargets((MagicPlayer)data[1],MagicTargetFilter.TARGET_CREATURE_WITHOUT_FLYING);
+			for (final MagicTarget creature : creatures) {
+				final MagicDamage damage = new MagicDamage(source,creature,1,false);
+				game.doAction(new MagicDealDamageAction(damage));
+			}
+		}
+    };
+    
+    public static final MagicWhenLeavesPlayTrigger T3 = new MagicWhenLeavesPlayTrigger() {
+    	@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent data) {
+			return (permanent == data) ?
+				new MagicEvent(
+						permanent,
+						permanent.getController(),
+	                    new Object[]{permanent,permanent.getController()},
+	                    this,
+	                    permanent + " deals 1 damage to each creature without flying.") :
+			MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicSource source = (MagicSource)data[0];
+			final Collection<MagicTarget> creatures =
+                game.filterTargets((MagicPlayer)data[1],MagicTargetFilter.TARGET_CREATURE_WITHOUT_FLYING);
+			for (final MagicTarget creature : creatures) {
+				final MagicDamage damage = new MagicDamage(source,creature,1,false);
+				game.doAction(new MagicDealDamageAction(damage));
+			}
+		}
+    };
+}
