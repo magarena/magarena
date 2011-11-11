a9d70d70862d000651fc8f8cd7c8bae7646f4133
beholder <unknown>
2011-11-11 11:30:17 +0100

    added Echoing Calm and Echoing Courage

---
+++ src/magic/card/Echoing_Calm.java
@@ -0,0 +1,57 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDestroyTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Echoing_Calm {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_ENCHANTMENT,
+                    new MagicDestroyTargetPicker(false),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Destroy target enchantment$ and all other enchantments " +
+                    "with the same name as that enchantment.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent targetPermanent) {
+                    final MagicTargetFilter targetFilter = 
+                        new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
+                    final Collection<MagicTarget> targets = 
+                        game.filterTargets(cardOnStack.getController(),targetFilter);
+                    for (final MagicTarget target : targets) {
+                    	final MagicPermanent permanent = (MagicPermanent)target;
+                    	if (permanent.isEnchantment()) {
+                    		game.doAction(new MagicDestroyAction(permanent));
+                    	}
+                    }
+                }
+			});
+		}
+	};
+}
---
+++ src/magic/card/Echoing_Courage.java
@@ -0,0 +1,57 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicPumpTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Echoing_Courage {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ and all other creatures with the same " +
+                    "name as that creature get +2/+2 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent targetPermanent) {
+                    final MagicTargetFilter targetFilter = 
+                        new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
+                    final Collection<MagicTarget> targets = 
+                        game.filterTargets(cardOnStack.getController(),targetFilter);
+                    for (final MagicTarget target : targets) {
+                    	final MagicPermanent permanent = (MagicPermanent)target;
+                    	if (permanent.isCreature(game)) {
+                    		game.doAction(new MagicChangeTurnPTAction(permanent,2,2));
+                    	}
+                    }
+                }
+			});
+		}
+	};
+}
