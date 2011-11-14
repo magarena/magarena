0a12f7b556115b0bf6aae12b06796dcf01d4f006
chens1992@gmail.com <unknown>
2011-11-14 22:34:08 +0800

    added Carrion Ants

---
+++ src/magic/card/Carrion_Ants.java
@@ -0,0 +1,9 @@
+package magic.card;
+
+import magic.model.MagicManaCost;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPumpActivation;
+
+public class Carrion_Ants {
+	public static final MagicPermanentActivation A = new MagicPumpActivation(MagicManaCost.ONE,1,1);
+}
