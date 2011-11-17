13ee08efd90d3878fb71a72592608f16adf61499
melvin <unknown>
2011-11-17 12:57:48 +0800

    moved fading to card script, corrected implementation of Rusting Golem to use MagicCDA instead of MagicStatic

--- src/magic/card/Rusting_Golem.java
+++ src/magic/card/Rusting_Golem.java
@@ -2,24 +2,21 @@ package magic.card;
 
 import magic.model.MagicCounterType;
 import magic.model.MagicGame;
+import magic.model.MagicPlayer;
 import magic.model.MagicPermanent;
 import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.trigger.MagicComesIntoPlayWithCounterTrigger;
-import magic.model.trigger.MagicFadeVanishCounterTrigger;
+import magic.model.mstatic.MagicCDA;
 
 public class Rusting_Golem {
-	public static final MagicStatic S = new MagicStatic(MagicLayer.SetPT) {
+	public static final MagicCDA cda = new MagicCDA() {
 		@Override
-		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+		public void getPowerToughness(
+                final MagicGame game,
+                final MagicPlayer player,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
 			final int amount = permanent.getCounters(MagicCounterType.Charge);
 			pt.set(amount,amount);
 		}
     };
-	
-	public static final MagicComesIntoPlayWithCounterTrigger T1 = 
-			new MagicComesIntoPlayWithCounterTrigger(MagicCounterType.Charge,"fade",5);
-	
-    public static final MagicFadeVanishCounterTrigger T2 = new MagicFadeVanishCounterTrigger("fade");
 }
