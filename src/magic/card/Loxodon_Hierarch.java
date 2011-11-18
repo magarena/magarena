d911b06afbe307e2ac30b17071faf44706bd291b
melvin <unknown>
2011-11-18 11:16:59 +0800

    simplified more cards to use the 'enters gain life n' keyword ability

--- src/magic/card/Loxodon_Hierarch.java
+++ src/magic/card/Loxodon_Hierarch.java
@@ -59,25 +59,4 @@ public class Loxodon_Hierarch {
 			}
 		}
 	};
-	
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-			
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    new Object[]{player},
-                    this,
-                    player + " gains 4 life.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],4));
-		}
-    };
 }
--- src/magic/card/Pelakka_Wurm.java
+++ src/magic/card/Pelakka_Wurm.java
@@ -9,25 +9,5 @@ import magic.model.trigger.MagicDieDrawCardTrigger;
 import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
 
 public class Pelakka_Wurm {
-    public static final MagicWhenComesIntoPlayTrigger T1 = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-			
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    new Object[]{player},
-                    this,
-                    player + " gains 7 life.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],7));
-		}
-    };
     public static final MagicDieDrawCardTrigger T2 = new MagicDieDrawCardTrigger(true);
 }
