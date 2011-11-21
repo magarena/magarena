dccdd52cf19240dfe64048317bd8daf36b0f4e4d
melvin <unknown>
2011-11-21 09:50:58 +0800

    replaced card code with ping keyword

--- src/magic/card/Gelectrode.java
+++ src/magic/card/Gelectrode.java
@@ -1,65 +1,20 @@
 package magic.card;
 
-import magic.model.MagicDamage;
 import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicPlayer;
-import magic.model.MagicSource;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
 import magic.model.action.MagicUntapAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.event.MagicActivationHints;
 import magic.model.event.MagicEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTapEvent;
-import magic.model.event.MagicTiming;
 import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
 import magic.model.trigger.MagicWhenSpellIsPlayedTrigger;
 
-
 public class Gelectrode {
-	public static final MagicPermanentActivation A = new MagicPermanentActivation(
-            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
-            new MagicActivationHints(MagicTiming.Removal),
-            "Damage") {
-		@Override
-		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
-		}
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			return new MagicEvent(
-                    source,
-                    source.getController(),
-                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                    new MagicDamageTargetPicker(1),
-                    new Object[]{source},
-                    this,
-                    source + " deals 1 damage to target creature or player$.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage((MagicSource)data[0],target,1,false);
-                    game.doAction(new MagicDealDamageAction(damage));
-                }
-			});
-		}
-	};
-	
     public static final MagicWhenSpellIsPlayedTrigger T = new MagicWhenSpellIsPlayedTrigger() {
 		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicCardOnStack cardOnStack) {
 			final MagicPlayer player=permanent.getController();
 			return (cardOnStack.getController()==player&&cardOnStack.getCardDefinition().isSpell()) ?
                 new MagicEvent(
--- src/magic/card/Goblin_Sharpshooter.java
+++ src/magic/card/Goblin_Sharpshooter.java
@@ -42,37 +42,4 @@ public class Goblin_Sharpshooter {
 			game.doAction(new MagicUntapAction((MagicPermanent)data[0]));
 		}
     };
-    
-    public static final MagicPermanentActivation A = new MagicPermanentActivation(
-            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
-            new MagicActivationHints(MagicTiming.Removal),
-            "Damage") {
-
-		@Override
-		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
-		}
-
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			return new MagicEvent(
-                    source,
-                    source.getController(),
-                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                    new MagicDamageTargetPicker(1),
-                    new Object[]{source},
-                    this,
-                    source + " deals 1 damage to target creature or player$.");
-		}
-
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage((MagicSource)data[0],target,1,false);
-                    game.doAction(new MagicDealDamageAction(damage));
-                }
-			});
-		}
-	};
 }
