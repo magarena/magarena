d46cb68e58af8c5f1aba47f012be07f59da2e062
melvin <unknown>
2011-11-22 10:40:14 +0800

    fixed bug in Sparkmage Apprentice, previously was only able to deal damage to creaetures where it should be able to deal damage to creature or player.
    fixes issue 95.
    fixes issue 96.

--- src/magic/card/Sparkmage_Apprentice.java
+++ src/magic/card/Sparkmage_Apprentice.java
@@ -3,9 +3,10 @@ package magic.card;
 import magic.model.MagicDamage;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.target.MagicTarget;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicTargetAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.event.MagicEvent;
 import magic.model.target.MagicDamageTargetPicker;
@@ -28,9 +29,9 @@ public class Sparkmage_Apprentice {
 		
 		@Override
 		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-				public void doAction(final MagicPermanent creature) {
-                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],creature,1,false);
+			event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+				public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],target,1,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
 			});
