de3f46e62bfde020130b892ab6fc10b152fe7e16
melvin <unknown>
2011-11-25 20:24:45 +0800

    added copyFile

--- src/magic/data/FileIO.java
+++ src/magic/data/FileIO.java
@@ -168,4 +168,37 @@ public class FileIO {
             }
         }
     }
+
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+        FileInputStream fIn = null;
+        FileOutputStream fOut = null;
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            fIn = new FileInputStream(sourceFile);
+            source = fIn.getChannel();
+            fOut = new FileOutputStream(destFile);
+            destination = fOut.getChannel();
+            long transfered = 0;
+            long bytes = source.size();
+            while (transfered < bytes) {
+                transfered += destination.transferFrom(source, 0, source.size());
+                destination.position(transfered);
+            }
+        } finally {
+            if (source != null) {
+                close(source);
+            } else if (fIn != null) {
+                close(fIn);
+            }
+            if (destination != null) {
+                close(destination);
+            } else if (fOut != null) {
+                close(fOut);
+            }
+        }
+    }
 } 
