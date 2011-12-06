5cc220f4ad0bc760cce6873de4a1d11c3b03d517
beholder <unknown>
2011-12-06 12:51:37 +0100

    added insect token. added Deadly Grub

---
+++ src/magic/card/Deadly_Grub.java
@@ -0,0 +1,44 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Deadly_Grub {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicGraveyardTriggerData triggerData) {
+			final MagicPlayer player = permanent.getController();
+			return (MagicLocationType.Play == triggerData.fromLocation &&
+					permanent.getCounters(MagicCounterType.Charge) == 0) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{player},
+                    this,
+                    player + " puts a 6/1 green Insect creature token " +
+                    "with shroud onto the battlefield.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicPlayTokenAction(
+					(MagicPlayer)data[0],
+					TokenCardDefinitions.get("Insect2")));
+		}
+    };
+}
