1dca7c23595620517ef1fd88761c6f6b757d9459
beholder <unknown>
2012-02-16 15:49:45 +0100

    changed MagicMillLibraryAction to make use of relevant actions. fixes issue 152

--- src/magic/model/action/MagicMillLibraryAction.java
+++ src/magic/model/action/MagicMillLibraryAction.java
@@ -4,16 +4,13 @@ import magic.ai.ArtificialScoringSystem;
 import magic.model.MagicCard;
 import magic.model.MagicCardList;
 import magic.model.MagicGame;
+import magic.model.MagicLocationType;
 import magic.model.MagicPlayer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MagicMillLibraryAction extends MagicAction {
 
 	private final MagicPlayer player;
 	private final int amount;
-	private List<MagicCard> milledCards;
 	
 	public MagicMillLibraryAction(final MagicPlayer player,final int amount) {
 		
@@ -23,34 +20,20 @@ public class MagicMillLibraryAction extends MagicAction {
 
 	@Override
 	public void doAction(final MagicGame game) {
-
-		milledCards=new ArrayList<MagicCard>();
 		final MagicCardList library=player.getLibrary();
 		final int size=library.size();
 		final int count=size>=amount?amount:size;		
 		if (count>0) {
-			final MagicCardList graveyard=player.getGraveyard();
 			setScore(player,ArtificialScoringSystem.getMillScore(count));
 			for (int c=count;c>0;c--) {
-
-				final MagicCard milledCard=library.removeCardAtTop();
-				graveyard.addToTop(milledCard);
-				milledCards.add(milledCard);
+				final MagicCard milledCard = library.getCardAtTop();
+				game.doAction(new MagicRemoveCardAction(milledCard,MagicLocationType.OwnersLibrary));
+				game.doAction(new MagicMoveCardAction(milledCard,MagicLocationType.OwnersLibrary,MagicLocationType.Graveyard));
 			}
 			game.logMessage(player,"You put the top "+count+" cards of your library into your graveyard.");
 		}
 	}
 
 	@Override
-	public void undoAction(final MagicGame game) {
-
-		final MagicCardList library=player.getLibrary();
-		final MagicCardList graveyard=player.getGraveyard();
-		for (int index=milledCards.size()-1;index>=0;index--) {
-			
-			final MagicCard milledCard=milledCards.get(index);
-			graveyard.remove(milledCard);
-			library.addToTop(milledCard);
-		}
-	}
+	public void undoAction(final MagicGame game) {}
 }
