4d150b7751061fa4f5b9bcdbf61670118eab562e
beholder <unknown>
2012-02-19 17:34:03 +0100

    added missing MagicUndyingTrigger. missing from revision 380e7f068f7f

---
+++ src/magic/model/trigger/MagicUndyingTrigger.java
@@ -0,0 +1,54 @@
+package magic.model.trigger;
+
+import magic.model.MagicCard;
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicReanimateAction;
+import magic.model.event.MagicEvent;
+
+public class MagicUndyingTrigger extends MagicWhenPutIntoGraveyardTrigger {
+
+	private static final MagicTrigger INSTANCE = new MagicUndyingTrigger();
+
+    private MagicUndyingTrigger() {}
+	
+	@Override
+	public MagicEvent executeTrigger(
+			final MagicGame game,
+			final MagicPermanent permanent,
+			final MagicGraveyardTriggerData triggerData) {
+		System.err.println("undying trigger");
+		if (triggerData.fromLocation == MagicLocationType.Play &&
+			permanent.getCounters(MagicCounterType.PlusOne) == 0) {
+			final MagicCard card = triggerData.card;
+			return new MagicEvent(
+					permanent,
+					permanent.getController(),
+					new Object[] {card},
+					this,
+					"Return " + card.getName() + " to play under its " +
+					"owner's control with a +1/+1 counter on it.");
+		}
+		return MagicEvent.NONE;
+	}
+
+	@Override
+	public void executeEvent(
+			final MagicGame game,
+			final MagicEvent event,
+			final Object[] data,
+			final Object[] choiceResults) {
+		final MagicCard card = (MagicCard)data[0];
+		game.doAction(new MagicReanimateAction(
+				card.getOwner(),
+				card,
+				MagicPlayCardAction.UNDYING));
+	}
+	
+	public static final MagicTrigger getInstance() {	
+		return INSTANCE;
+	}
+}
