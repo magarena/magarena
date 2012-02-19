398dc33c8374fbdedcb603f959e1b0fd2f85a775
beholder <unknown>
2012-02-19 14:45:31 +0100

    added Subversion and Weatherseed Treefolk

---
+++ src/magic/card/Weatherseed_Treefolk.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Weatherseed_Treefolk {
+	public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicGraveyardTriggerData triggerData) {
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+                new MagicEvent(
+                        permanent,
+                        permanent.getController(),
+                        new Object[]{permanent.getCard()},
+                        this,
+                        "Return " + permanent + " to its owner's hand."):
+                MagicEvent.NONE;
+		}
+
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicCard card = (MagicCard)data[0];
+			game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+            game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+		}
+    };
+}
