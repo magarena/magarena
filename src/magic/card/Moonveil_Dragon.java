41a322fbe1c7cc675722611da5a8be8f4f3f3f9c
beholder <unknown>
2012-02-22 14:57:49 +0100

    added Moonveil Dragon and Nearheath Stalker

---
+++ src/magic/card/Moonveil_Dragon.java
@@ -0,0 +1,63 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Moonveil_Dragon {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{
+            		MagicManaCost.RED.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[]{
+					new MagicPayManaCostEvent(
+							source,
+							source.getController(),
+							MagicManaCost.RED)};
+        }
+
+        @Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source.getController()},
+                    this,
+                    "Each creature you control gets +1/+0 until end of turn.");
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final Collection<MagicTarget> targets = game.filterTargets(
+					(MagicPlayer)data[0],
+					MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+    			for (final MagicTarget target : targets) {
+    				game.doAction(new MagicChangeTurnPTAction((MagicPermanent)target,1,0));
+    			}
+        }
+    };
+}
