f11e9cdc617254ba66e6f039d20a49b170528cee
beholder <unknown>
2012-02-22 11:27:57 +0100

    revert changes made to Laccolith Grunt in revision 4d239ea8691e

--- src/magic/card/Laccolith_Grunt.java
+++ src/magic/card/Laccolith_Grunt.java
@@ -7,17 +7,15 @@ import magic.model.MagicPermanentState;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeStateAction;
 import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicDrawAction;
 import magic.model.action.MagicPermanentAction;
 import magic.model.choice.MagicMayChoice;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.event.MagicEvent;
 import magic.model.target.MagicDamageTargetPicker;
 import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-import magic.model.trigger.MagicWhenLeavesPlayTrigger;
 
 public class Laccolith_Grunt {
-    public static final MagicWhenBecomesBlockedTrigger T1 = new MagicWhenBecomesBlockedTrigger() {
+    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
 		@Override
 		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             if (creature == permanent) {
@@ -48,14 +46,10 @@ public class Laccolith_Grunt {
 				event.processTargetPermanent(game,choiceResults,1,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent creature) {
                     	final MagicPermanent permanent = (MagicPermanent)data[0];
-                    	final int amount = (permanent.isOnBattlefield(game)) ?
-                    			permanent.getPower(game)
-                    			:
-                    			permanent.getLastKnownPowerToughness().power();
                     	final MagicDamage damage = new MagicDamage(
                     			permanent,
                     			creature,
-                    			amount,
+                    			permanent.getPower(game),
                     			false);
 	                    game.doAction(new MagicDealDamageAction(damage));
 	                    game.doAction(new MagicChangeStateAction(
@@ -64,26 +58,6 @@ public class Laccolith_Grunt {
 				    }
                 });
 			};
-		}
-    };
-    
-    public static final MagicWhenLeavesPlayTrigger T2 = new MagicWhenLeavesPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(
-				final MagicGame game,
-				final MagicPermanent permanent,
-				final MagicPermanent data) {
-			if (permanent == data) {
-				permanent.setLastKnownPowerToughness(game);
-			}
-			return MagicEvent.NONE; 
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
 		}
     };
 }
