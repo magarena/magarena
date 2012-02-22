e3ab0c1ec208d68c3ddc01f66af4841c65b9ae51
beholder <unknown>
2012-02-22 17:04:48 +0100

    added Stromkirk Captain and Wrack with Madness

---
+++ src/magic/card/Wrack_with_Madness.java
@@ -0,0 +1,53 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+
+public class Wrack_with_Madness {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Target creature$ deals damage to itself equal to its power.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                	MagicPermanent creature = (MagicPermanent)target;
+                    final MagicDamage damage = new MagicDamage(
+                    		(MagicSource)target,
+                    		target,
+                    		creature.getPower(game),
+                    		false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+	};
+}
