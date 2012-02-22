2f3476d95b400cafc29e4c6185248ce52041705f
beholder <unknown>
2012-02-22 13:49:03 +0100

    added Forge Devil and Heckling Fiends

---
+++ src/magic/card/Forge_Devil.java
@@ -0,0 +1,49 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicTargetAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Forge_Devil {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    new MagicDamageTargetPicker(1),
+                    new Object[]{permanent,player},
+                    this,
+                    permanent + " deals 1 damage to target " +
+                    "creature$ and 1 damage to " + player + ".");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPermanent permanent = (MagicPermanent)data[0];
+			event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(permanent,target,1,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+			final MagicDamage damage = new MagicDamage(permanent,(MagicPlayer)data[1],1,false);
+			game.doAction(new MagicDealDamageAction(damage));
+		}
+    };
+}
---
+++ src/magic/card/Heckling_Fiends.java
@@ -0,0 +1,62 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicMustAttackTargetPicker;
+
+public class Heckling_Fiends {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation( 
+			new MagicCondition[]{MagicManaCost.TWO_RED.getCondition()},
+            new MagicActivationHints(MagicTiming.MustAttack),
+            "Attacks") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[] {
+					new MagicPayManaCostEvent(
+					source,
+					source.getController(),
+					MagicManaCost.TWO_RED)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    MagicMustAttackTargetPicker.getInstance(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Target creature$ attacks this turn if able.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(
+                    		creature,
+                    		MagicAbility.AttacksEachTurnIfAble));
+                }
+			});
+		}
+	};
+}
