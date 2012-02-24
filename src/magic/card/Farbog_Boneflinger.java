cf7fcb248efd526b2269e6442ebb24487a85043d
beholder <unknown>
2012-02-24 11:08:48 +0100

    added Falkenrath Torturer and Farbog Boneflinger

---
+++ src/magic/card/Farbog_Boneflinger.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicWeakenTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Farbog_Boneflinger {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    new MagicWeakenTargetPicker(2,2),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Target creature$ gets -2/-2 until end of turn.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object data[],
+				final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-2,-2));
+                }
+			});
+		}
+    };
+}
