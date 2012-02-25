db4a9eed1ba227fe3b1cf3d5f50d48ed12d9802b
beholder <unknown>
2012-02-25 16:16:52 +0100

    added Tragic Slip and Vengeful Vampire

---
+++ src/magic/card/Tragic_Slip.java
@@ -0,0 +1,48 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicWeakenTargetPicker;
+
+public class Tragic_Slip {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    new MagicWeakenTargetPicker(1,1),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets -1/-1 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));		
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {	
+                public void doAction(final MagicPermanent creature) {
+                	int amount = -1;
+        			if (game.getCreatureDiedThisTurn()) {
+        				amount = -13;
+        			}
+                    game.doAction(new MagicChangeTurnPTAction(creature,amount,amount));
+                }
+			});
+		}
+	};
+}
