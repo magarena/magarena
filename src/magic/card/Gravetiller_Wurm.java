7eed5a9aad2e8ccd38fa11bc9c972728bc6be685
beholder <unknown>
2012-02-26 18:33:54 +0100

    added Gravetiller Wurm and Grim Flowering

---
+++ src/magic/card/Gravetiller_Wurm.java
@@ -0,0 +1,45 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Gravetiller_Wurm {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			if (game.getCreatureDiedThisTurn()) {
+				return new MagicEvent(
+						permanent,
+						player,
+						new Object[]{permanent},
+						this,
+						permanent + " enters the battlefield with four +1/+1 counters on it ");
+			}
+			return MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction(
+					(MagicPermanent)data[0],
+					MagicCounterType.PlusOne,
+					4,
+					true));
+		}
+		@Override
+		public boolean usesStack() {
+			return false;
+		}
+    };
+}
---
+++ src/magic/card/Grim_Flowering.java
@@ -0,0 +1,40 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTargetFilter;
+
+public class Grim_Flowering {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    player + " draws a card for each creature " +
+                    	"card in his or her graveyard.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPlayer player = (MagicPlayer)data[1];
+			final int amount = game.filterTargets(
+					player,
+					MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD).size();
+			game.doAction(new MagicDrawAction(player,amount));
+		}
+	};
+}
