fda45a147d2ef1ac44201699e68dc70845efbc42
beholder <unknown>
2012-02-26 18:43:22 +0100

    added Hollowhenge Beast and Hunger of the Howlpack

---
+++ src/magic/card/Hunger_of_the_Howlpack.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicPumpTargetPicker;
+
+public class Hunger_of_the_Howlpack {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    player + " puts a +1/+1 counter on target creature$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                	final int amount = game.getCreatureDiedThisTurn() ? 3 : 1;
+                    game.doAction(new MagicChangeCountersAction(
+                    		creature,
+                    		MagicCounterType.PlusOne,
+                    		amount,
+                    		true));
+                }
+			});
+		}
+	};
+}
