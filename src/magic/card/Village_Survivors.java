177b1add38bf02c9bf03598d393138cfa35e0002
beholder <unknown>
2012-02-27 12:25:47 +0100

    added Ulvenwald Bear and Village Survivors

---
+++ src/magic/card/Village_Survivors.java
@@ -0,0 +1,20 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Village_Survivors {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+		@Override
+		public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+			return permanent.getController().getLife() <= 5 ?
+					flags|MagicAbility.Vigilance.getMask() : flags;
+		}
+	};
+}
