7d1d601e4516fd1ac71e1547e1d77135054d39c4
beholder <unknown>
2012-03-02 14:53:03 +0100

    removed unused import

--- src/magic/card/Gavony_Township.java
+++ src/magic/card/Gavony_Township.java
@@ -12,7 +12,6 @@ import magic.model.condition.MagicCondition;
 import magic.model.event.MagicActivationHints;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicPayManaCostEvent;
-import magic.model.event.MagicPayManaCostTapEvent;
 import magic.model.event.MagicPermanentActivation;
 import magic.model.event.MagicTapEvent;
 import magic.model.event.MagicTiming;
