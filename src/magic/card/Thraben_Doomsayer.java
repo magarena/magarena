78900e5273612947c0317ae3f3991da3adbd66a7
beholder <unknown>
2012-03-03 13:55:50 +0100

    added Thraben Doomsayer and Bone to Ash

---
+++ src/magic/card/Thraben_Doomsayer.java
@@ -0,0 +1,76 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSource;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Thraben_Doomsayer {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.ModPT, 
+			MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+		@Override
+		public void getPowerToughness(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPowerToughness pt) {
+			pt.add(2,2);
+		}
+		@Override
+		public boolean condition(
+				final MagicGame game,
+				final MagicPermanent source,
+				final MagicPermanent target) {
+			return source != target && source.getController().getLife() <= 5;
+		}
+	};
+		    
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+            new MagicActivationHints(MagicTiming.Token),
+            "Token") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			return new MagicEvent(
+                    source,
+                    player,
+                    new Object[]{player},
+                    this,
+                    player + " puts a 1/1 white Human " +
+                    "creature token onto the battlefield.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			game.doAction(new MagicPlayTokenAction(
+					(MagicPlayer)data[0],
+					TokenCardDefinitions.get("Human1")));
+		}
+	};
+}
