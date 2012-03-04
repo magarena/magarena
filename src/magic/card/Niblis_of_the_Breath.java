ba36131c4149492e4f8b7a3611058fae8fc06c81
beholder <unknown>
2012-03-04 11:25:38 +0100

    added Nephalia Seakite and Niblis of the Breath

---
+++ src/magic/card/Niblis_of_the_Breath.java
@@ -0,0 +1,83 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicUntapAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostTapEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapCreatureActivation;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicTapTargetPicker;
+
+public class Niblis_of_the_Breath {
+	public static final MagicPermanentActivation A1 = new MagicTapCreatureActivation(
+			new MagicCondition[]{
+					MagicCondition.CAN_TAP_CONDITION,
+					MagicManaCost.WHITE.getCondition()
+			},
+			new MagicActivationHints(MagicTiming.Tapping),
+			"Tap") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[]{
+                new MagicPayManaCostTapEvent(
+                		source,
+                		source.getController(),
+                		MagicManaCost.WHITE)
+            };
+        }
+    };
+    
+    public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
+    		new MagicCondition[]{
+					MagicCondition.CAN_TAP_CONDITION,
+					MagicManaCost.WHITE.getCondition()
+			},
+			new MagicActivationHints(MagicTiming.Tapping),
+			"Untap") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[]{
+                new MagicPayManaCostTapEvent(
+                		source,
+                		source.getController(),
+                		MagicManaCost.WHITE)
+            };
+        }
+
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+					source,
+					source.getController(),
+					MagicTargetChoice.POS_TARGET_CREATURE,
+					new MagicTapTargetPicker(false,true),
+					MagicEvent.NO_DATA,
+					this,
+					"Untap target creature$.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+			    public void doAction(final MagicPermanent creature) {
+			    	game.doAction(new MagicUntapAction(creature));
+			    }
+			});
+		}
+	};
+}
