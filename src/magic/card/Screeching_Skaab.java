1b37cd578a622c3fae76ccb9821ffb9914296a4a
beholder <unknown>
2012-03-04 14:05:59 +0100

    added Screeching Skaab and Shriekgeist

---
+++ src/magic/card/Screeching_Skaab.java
@@ -0,0 +1,34 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Screeching_Skaab {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{player},
+                    this,
+                    player + " puts the top two cards of his " +
+                    "or her library into his or her graveyard.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicMillLibraryAction((MagicPlayer)data[0],2));
+		}
+    };
+}
---
+++ src/magic/card/Shriekgeist.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class Shriekgeist {
+    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicDamage damage) {
+			final MagicTarget target = damage.getTarget();
+			final MagicPlayer player = permanent.getController();
+			return (permanent == damage.getSource() && 
+                    target.isPlayer() && 
+                    damage.isCombat()) ?
+                new MagicEvent(
+                        permanent,
+                        player,
+                        new Object[]{target},
+                        this,
+                        target + " puts the top two cards of " +
+                        "his or her library into his or her graveyard."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicMillLibraryAction((MagicPlayer)data[0],2));
+		}
+    };
+}
