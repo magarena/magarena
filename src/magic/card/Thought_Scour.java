c5211fc75a67befc7be4b3bb3d7dec8d4a3ede3d
beholder <unknown>
2012-03-04 14:19:59 +0100

    added Stormbound Geist and Thought Scour

---
+++ src/magic/card/Thought_Scour.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Thought_Scour {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.TARGET_PLAYER,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Target player$ puts the top two cards of his or her " +
+                    "library into his or her graveyard. Draw a card.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                	game.doAction(new MagicMillLibraryAction(player,2));
+                    game.doAction(new MagicDrawAction((MagicPlayer)data[1],1));
+                }
+			});
+		}
+	};
+}
