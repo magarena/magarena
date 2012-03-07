d209b92137ac562b75facc78532b6d5ed355e609
beholder <unknown>
2012-03-07 11:59:46 +0100

    added Druid's Call and Krosan Beast

---
+++ src/magic/card/Druid_s_Call.java
@@ -0,0 +1,53 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPlayAuraEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.target.MagicNoCombatTargetPicker;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class Druid_s_Call {
+	public static final MagicSpellCardEvent E = new MagicPlayAuraEvent(
+			MagicTargetChoice.POS_TARGET_CREATURE,
+            new MagicNoCombatTargetPicker(true,true,true));	
+
+    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicDamage damage) {
+            final MagicPlayer player = permanent.getController();
+            final int amount = damage.getDealtAmount();
+            final MagicPermanent enchantedCreature = permanent.getEnchantedCreature();
+			return (damage.getTarget() == enchantedCreature) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{enchantedCreature.getController(),amount},
+                    this,
+                    enchantedCreature.getController() + " puts " + amount +
+                    " 1/1 green Squirrel creature tokens onto the battlefield.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			for (int i=(Integer)data[1];i>0;i--) {
+				game.doAction(new MagicPlayTokenAction(
+						(MagicPlayer)data[0],
+						TokenCardDefinitions.get("Squirrel1")));
+			}
+		}
+    };
+}
---
+++ src/magic/card/Krosan_Beast.java
@@ -0,0 +1,21 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.mstatic.MagicLayer;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicStatic;
+
+public class Krosan_Beast {
+	public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
+		@Override
+		public void getPowerToughness(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+			if (permanent.getController().getGraveyard().size() >= 7) {
+				pt.add(7,7);
+			}
+		}
+    };
+}
