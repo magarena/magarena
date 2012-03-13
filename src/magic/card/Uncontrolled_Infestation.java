e71cd273316b3e994b47b6727dd5c7d15895c679
melvin <unknown>
2012-03-13 23:35:57 +0800

    refer to Brink of Disaster

--- src/magic/card/Uncontrolled_Infestation.java
+++ src/magic/card/Uncontrolled_Infestation.java
@@ -1,33 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesTappedTrigger;
-
 public class Uncontrolled_Infestation {
-	public static final MagicWhenBecomesTappedTrigger T = new MagicWhenBecomesTappedTrigger() {
-    	@Override
-    	public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
-    		final MagicPermanent enchantedPermanent = permanent.getEnchantedCreature();
-    		return (enchantedPermanent == data) ?
-                new MagicEvent(
-                        permanent,
-                        permanent.getController(),
-                        new Object[]{enchantedPermanent},
-                        this,
-                        "Destroy " + enchantedPermanent + ".") :
-                MagicEvent.NONE;
-    	}
-    	@Override
-    	public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-    		game.doAction(new MagicDestroyAction((MagicPermanent)data[0]));
-    	}
-    };
+	public static final Object T = Brink_of_Disaster.T;
 }
