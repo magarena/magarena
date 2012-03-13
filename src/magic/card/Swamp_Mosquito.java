2b5936c5e536040f5d894729b7fb531b4e393c9d
melvin <unknown>
2012-03-13 23:38:11 +0800

    refer to Crypt Cobra's ability

--- src/magic/card/Swamp_Mosquito.java
+++ src/magic/card/Swamp_Mosquito.java
@@ -1,36 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangePoisonAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenAttacksUnblockedTrigger;
-
 public class Swamp_Mosquito {
-	public static final MagicWhenAttacksUnblockedTrigger T = new MagicWhenAttacksUnblockedTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            if (creature == permanent) {
-            	final MagicPlayer player = permanent.getController();
-    			final MagicPlayer opponent = game.getOpponent(player);
-    			return new MagicEvent(
-    					permanent,
-    					player,
-    					new Object[]{opponent},
-    					this,
-    					opponent + " gets a poison counter.");
-            }
-            return MagicEvent.NONE;
-		}
-		
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangePoisonAction((MagicPlayer)data[0],1));
-		}
-    };
+	public static final Object T = Crypt_Cobra.T;
 }
