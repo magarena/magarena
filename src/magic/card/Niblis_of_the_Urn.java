47869f9e39d7c522389db8188e207cc445f991a8
melvin <unknown>
2012-03-13 22:41:08 +0800

    refer to Gustcloak Cavalier's ability

--- src/magic/card/Niblis_of_the_Urn.java
+++ src/magic/card/Niblis_of_the_Urn.java
@@ -1,50 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicTapAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTapTargetPicker;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
 public class Niblis_of_the_Urn {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
-		@Override
-		public MagicEvent executeTrigger(
-				final MagicGame game,
-				final MagicPermanent permanent,
-				final MagicPermanent creature) {
-            final MagicPlayer player = permanent.getController();
-			return (permanent == creature) ?
-                new MagicEvent(
-                        permanent,
-                        player,
-                        new MagicMayChoice(
-                        		player + " may tap target creature.",
-                        		MagicTargetChoice.NEG_TARGET_CREATURE),
-                        new MagicTapTargetPicker(true,false),
-                        MagicEvent.NO_DATA,
-                        this,
-                        player + " may$ tap target creature$."):
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                event.processTargetPermanent(game,choiceResults,1,new MagicPermanentAction() {
-                    public void doAction(final MagicPermanent creature) {
-                        game.doAction(new MagicTapAction(creature,true));
-                    }
-                });
-			}
-		}		
-    };
+    public static final Object T = Gustcloak_Cavalier.T2;
 }
