45a3d470f8f8355ea54007932f52646011c302ce
melvin <unknown>
2012-03-13 21:39:28 +0800

    simplified to use effect from Moonglove Extract

--- src/magic/card/Seal_of_Fire.java
+++ src/magic/card/Seal_of_Fire.java
@@ -1,57 +1,5 @@
 package magic.card;
 
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicSource;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicActivation;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicSacrificeEvent;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-
 public class Seal_of_Fire {
-	public static final MagicPermanentActivation A = new MagicPermanentActivation(
-            MagicActivation.NO_COND,
-            new MagicActivationHints(MagicTiming.Removal),
-            "Damage") {
-
-		@Override
-		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicSacrificeEvent((MagicPermanent)source)};
-		}
-
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			return new MagicEvent(
-                    source,
-                    source.getController(),
-                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                    new MagicDamageTargetPicker(2),
-                    new Object[]{source},
-                    this,
-                    source + " deals 2 damage to target creature or player$.");
-		}
-
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage((MagicSource)data[0],target,2,false);
-                    game.doAction(new MagicDealDamageAction(damage));
-                }
-			});
-		}
-	};
+	public static final Object A = Moonglove_Extract.A;
 }
