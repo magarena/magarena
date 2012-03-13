ef57554e169e78487324bb8b415441e6518d438b
melvin <unknown>
2012-03-13 13:35:06 +0800

    removed duplicte code, refers to Tangle_Asp's trigger

--- src/magic/card/Venomous_Dragonfly.java
+++ src/magic/card/Venomous_Dragonfly.java
@@ -1,66 +1,9 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentList;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.event.MagicEvent;
 import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
 import magic.model.trigger.MagicWhenBlocksTrigger;
 
 public class Venomous_Dragonfly {
-    public static final MagicWhenBecomesBlockedTrigger T1 = new MagicWhenBecomesBlockedTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            if (creature == permanent) {
-            	final MagicPermanentList plist = new MagicPermanentList(permanent.getBlockingCreatures());
-            	return new MagicEvent(
-            			permanent,
-            			permanent.getController(),
-            			new Object[]{plist},
-            			this,
-            			plist.size() > 1 ?
-            				"Destroy blocking creatures at end of combat." :
-            				"Destroy " + plist.get(0) + " at end of combat.");
-            }
-            return MagicEvent.NONE;
-		}
-		
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			final MagicPermanentList plist = (MagicPermanentList)data[0];
-			for (final MagicPermanent blocker : plist) {
-				game.doAction(new MagicChangeStateAction(blocker,MagicPermanentState.DestroyAtEndOfCombat,true));
-        	}
-		}
-    };
-    
-    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
-            final MagicPermanent blocked = permanent.getBlockedCreature();
-			return (permanent == data && blocked.isValid()) ?
-                new MagicEvent(
-                    permanent,
-                    permanent.getController(),
-                    new Object[]{blocked},
-                    this,
-                    "Destroy " + blocked + " at end of combat."):
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			final MagicPermanent blocker = (MagicPermanent)data[0];
-			game.doAction(new MagicChangeStateAction(blocker,MagicPermanentState.DestroyAtEndOfCombat,true));
-		}
-    };
+    public static final MagicWhenBecomesBlockedTrigger T1 = Tangle_Asp.T1;
+    public static final MagicWhenBlocksTrigger T2 = Tangle_Asp.T2;
 }
