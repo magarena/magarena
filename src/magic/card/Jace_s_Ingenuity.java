f4ab71b45b6c620d07550b32e5f6a338340ee3b9
melvin <unknown>
2012-03-13 23:36:24 +0800

    refer to Harmonize

--- src/magic/card/Jace_s_Ingenuity.java
+++ src/magic/card/Jace_s_Ingenuity.java
@@ -1,34 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
 public class Jace_s_Ingenuity {
-	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
-		@Override
-		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			final MagicPlayer player=cardOnStack.getController();
-			return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    new Object[]{cardOnStack,player},
-                    this,
-                    "Draw three cards.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
-			game.doAction(new MagicDrawAction((MagicPlayer)data[1],3));
-		}
-	};
+	public static final Object S = Harmonize.S;
 }
