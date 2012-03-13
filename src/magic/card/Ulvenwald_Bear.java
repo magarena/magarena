958c2aee668aae694bad2ff0def80316dc2b4a4b
melvin <unknown>
2012-03-13 23:37:40 +0800

    refer to Festerhide Boar's ability

--- src/magic/card/Ulvenwald_Bear.java
+++ src/magic/card/Ulvenwald_Bear.java
@@ -1,42 +1,5 @@
 package magic.card;
 
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPlayer;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
 public class Ulvenwald_Bear {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-			if (game.getCreatureDiedThisTurn()) {
-				return new MagicEvent(
-						permanent,
-						player,
-						new Object[]{permanent},
-						this,
-						permanent + " enters the battlefield with two +1/+1 counters on it ");
-			}
-			return MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeCountersAction(
-					(MagicPermanent)data[0],
-					MagicCounterType.PlusOne,
-					2,
-					true));
-		}
-		@Override
-		public boolean usesStack() {
-			return false;
-		}
-    };
+    public static final Object T = Festerhide_Boar.T;
 }
