07894b41407a1c8d752cbe64212c058eb75623d5
melvin <unknown>
2012-03-13 23:36:11 +0800

    refer to Kitesail Apprentice's ability

--- src/magic/card/Skyhunter_Cub.java
+++ src/magic/card/Skyhunter_Cub.java
@@ -1,29 +1,6 @@
-
 package magic.card;
 
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
 public class Skyhunter_Cub {
-	public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
-		@Override
-		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
-			if (permanent.isEquipped()) {
-				pt.add(1,1);
-			}
-		}		
-	};
-	
-	public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
-		@Override
-		public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
-			return permanent.isEquipped() ? 
-                flags|MagicAbility.Flying.getMask():
-                flags;
-		}
-	};
+	public static final Object S1 = Kitesail_Apprentice.S1;
+	public static final Object S2 = Kitesail_Apprentice.S2;
 }
