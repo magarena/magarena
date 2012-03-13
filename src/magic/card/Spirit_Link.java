e2fe03cea95d2f019e28d76627993610070dc781
melvin <unknown>
2012-03-13 23:39:09 +0800

    refer to Soul Link's ability

--- src/magic/card/Spirit_Link.java
+++ src/magic/card/Spirit_Link.java
@@ -1,35 +1,5 @@
 package magic.card;
 
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
 public class Spirit_Link {
-	public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            final MagicPlayer player=permanent.getController();
-            final int amount = damage.getDealtAmount();
-			return (damage.getSource() == permanent.getEnchantedCreature()) ?
-                new MagicEvent(
-                        permanent,
-                        player,
-                        new Object[]{player,amount},
-                        this,
-                        player + " gains " + amount + " life.") :
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],(Integer)data[1]));
-		}
-    };
+	public static final Object T = Soul_Link.T1;
 }
