f62bd023d2a187ec60ca27b984f69ff84024d655
melvin <unknown>
2012-03-13 21:39:50 +0800

    simplified to use effect from Joraga Warcaller

--- src/magic/card/Gnarlid_Pack.java
+++ src/magic/card/Gnarlid_Pack.java
@@ -1,52 +1,5 @@
 package magic.card;
 
-import magic.model.MagicCard;
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicPlayCardFromStackAction;
-import magic.model.choice.MagicKickerChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
 public class Gnarlid_Pack {  		
-	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
-		@Override
-		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			final MagicPlayer player = cardOnStack.getController();
-            final MagicCard card = cardOnStack.getCard();
-			return new MagicEvent(
-                    card,
-                    player,
-                    new MagicKickerChoice(MagicManaCost.ONE_GREEN,true),
-                    new Object[]{cardOnStack},
-                    this,
-                    "$Play " + card + ". " + card + " enters the battlefield " + 
-                    "with a +1/+1 counter on it for each time it was kicked$");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-			final int kickerCount = (Integer)choiceResults[1];
-			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-			final MagicPlayCardFromStackAction action = new MagicPlayCardFromStackAction(cardOnStack);
-			game.doAction(action);
-			if (kickerCount > 0) {
-				final MagicPermanent permanent = action.getPermanent();
-				game.doAction(new MagicChangeCountersAction(
-						permanent,
-	            		MagicCounterType.PlusOne,
-	            		kickerCount,
-	            		true));
-			}
-		}
-	};
+	public static final Object S = Joraga_Warcaller.S;
 }
