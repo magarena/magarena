1e2b8ae50f4acc23d02bc57d98d9ed0905967c4f
melvin <unknown>
2012-03-13 22:41:26 +0800

    refer to Kitsune Dawnblade's ability

--- src/magic/card/Niblis_of_the_Mist.java
+++ src/magic/card/Niblis_of_the_Mist.java
@@ -1,47 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicTapAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTapTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
 public class Niblis_of_the_Mist {
-	public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(
-				final MagicGame game,
-				final MagicPermanent permanent,
-				final MagicPlayer player) {
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    new MagicMayChoice(
-                    		player + " may tap target creature.",
-                    		MagicTargetChoice.NEG_TARGET_CREATURE),
-                    new MagicTapTargetPicker(true,false),
-                    MagicEvent.NO_DATA,
-                    this,
-                    player + " may$ tap target creature$.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                event.processTargetPermanent(game,choiceResults,1,new MagicPermanentAction() {
-                    public void doAction(final MagicPermanent creature) {
-                        game.doAction(new MagicTapAction(creature,true));
-                    }
-                });
-			}
-		}
-    };
+	public static final Object T = Kitsune_Dawnblade.T;
 }
