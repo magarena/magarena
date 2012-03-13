86dabe5d916476c16903e911d03b67bb35faff90
melvin <unknown>
2012-03-13 13:35:31 +0800

    remove duplicate code, refer to Goblin Artillery

--- src/magic/card/Orcish_Artillery.java
+++ src/magic/card/Orcish_Artillery.java
@@ -1,59 +1,7 @@
 package magic.card;
 
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicSource;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicEvent;
 import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTapEvent;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
 
 public class Orcish_Artillery {
-	public static final MagicPermanentActivation A = new MagicPermanentActivation(
-            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
-            new MagicActivationHints(MagicTiming.Removal),
-            "Damage") {
-
-		@Override
-		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
-		}
-
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			final MagicPlayer player=source.getController();
-			return new MagicEvent(
-                    source,
-                    player,
-                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                    new MagicDamageTargetPicker(2),
-                    new Object[]{source,player},
-                    this,
-                    source + " deals 2 damage to target creature or player$ and 3 damage to you.");
-		}
-
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-
-			final MagicSource source=(MagicSource)data[0];
-            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    final MagicDamage damage1=new MagicDamage(source,target,2,false);
-                    game.doAction(new MagicDealDamageAction(damage1));
-                    final MagicDamage damage2=new MagicDamage(source,(MagicTarget)data[1],3,false);
-                    game.doAction(new MagicDealDamageAction(damage2));
-                }
-			});
-		}
-	};
+	public static final MagicPermanentActivation A = Goblin_Artillery.A;
 }
