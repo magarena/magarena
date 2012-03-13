93241f3b1039e0a29f2d1185ef9a9b68247c507d
melvin <unknown>
2012-03-13 23:39:42 +0800

    refer to Crossway Vampire's ability

--- src/magic/card/Goblin_Shortcutter.java
+++ src/magic/card/Goblin_Shortcutter.java
@@ -1,38 +1,5 @@
 package magic.card;
 
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicNoCombatTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
 public class Goblin_Shortcutter {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    MagicTargetChoice.TARGET_CREATURE,
-                    new MagicNoCombatTargetPicker(false,true,false),
-                    MagicEvent.NO_DATA,
-                    this,
-                    "Target creature$ can't block this turn.");
-		}
-
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.CannotBlock));
-                }
-			});
-		}
-    };
+    public static final Object T = Crossway_Vampire.T;
 }
