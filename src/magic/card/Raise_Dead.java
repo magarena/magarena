56b64d32139998f872ed4ff6a00d4c5ad811790a
melvin <unknown>
2012-03-13 21:37:56 +0800

    simplified to use effect from Disentomb

--- src/magic/card/Raise_Dead.java
+++ src/magic/card/Raise_Dead.java
@@ -1,46 +1,5 @@
 package magic.card;
 
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicGraveyardTargetPicker;
-
 public class Raise_Dead {
-	public static final MagicSpellCardEvent S =new MagicSpellCardEvent() {
-		@Override
-		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			final MagicPlayer player=cardOnStack.getController();
-			return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
-                    new MagicGraveyardTargetPicker(false),
-                    new Object[]{cardOnStack,player},
-                    this,
-                    "Return target creature card$ from your graveyard to your hand.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
-            event.processTargetCard(game,choiceResults,0,new MagicCardAction() {
-            public void doAction(final MagicCard targetCard) {
-				game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
-				game.doAction(new MagicMoveCardAction(targetCard,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
-			}
-            });
-		}
-	};
+	public static final Object S = Disentomb.S;
 }
