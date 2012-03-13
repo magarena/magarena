73328484ec48a96470ce9ad7dbd84274127d8e91
melvin <unknown>
2012-03-13 23:38:39 +0800

    refer to Arc Runner's ability

--- src/magic/card/Spark_Elemental.java
+++ src/magic/card/Spark_Elemental.java
@@ -1,33 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicSacrificeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtEndOfTurnTrigger;
-
 public class Spark_Elemental {
-    public static final MagicAtEndOfTurnTrigger T = new MagicAtEndOfTurnTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
-			final MagicPlayer player = permanent.getController();
-			return (player == data) ?
-				new MagicEvent(
-                    permanent,
-                    player,
-                    new Object[]{permanent},
-                    this,
-                    "Sacrifice " + permanent + "."):
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicSacrificeAction((MagicPermanent)data[0]));
-		}
-    };
+    public static final Object T = Arc_Runner.T;
 }
