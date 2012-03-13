b78342100525a934d068ddf67ba0d227c9856311
melvin <unknown>
2012-03-13 13:22:46 +0800

    simplified Thicket Basilisk to refer to Rock Basilisk's triggers

--- src/magic/card/Dread_Specter.java
+++ src/magic/card/Dread_Specter.java
@@ -1,12 +1,5 @@
 package magic.card;
 
-import magic.model.MagicColor;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentList;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.event.MagicEvent;
 import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
 import magic.model.trigger.MagicWhenBlocksTrigger;
 
