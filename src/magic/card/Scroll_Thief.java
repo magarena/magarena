175a1874e1c473f0cf1d5c29d18b083394cab2f6
melvin <unknown>
2012-03-13 23:39:24 +0800

    refer to Sturmgeist's ability

--- src/magic/card/Scroll_Thief.java
+++ src/magic/card/Scroll_Thief.java
@@ -1,36 +1,5 @@
 package magic.card;
 
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
 public class Scroll_Thief {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-			final MagicPlayer player = permanent.getController();
-			return (damage.getSource() == permanent &&
-					damage.getTarget().isPlayer() &&
-					damage.isCombat()) ?
-                new MagicEvent(
-                        permanent,
-                        player,
-                        new Object[]{player},
-                        this,
-                        player + " draws a card."):
-                MagicEvent.NONE;
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object data[],
-                final Object[] choiceResults) {
-			game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
-		}
-    };
+    public static final Object T = Sturmgeist.T;
 }
