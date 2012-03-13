bf146423ecd4ec6e9b19ea3cb65a0d519577eff0
melvin <unknown>
2012-03-13 22:36:00 +0800

    refer to Bone to Ash

--- src/magic/card/Exclude.java
+++ src/magic/card/Exclude.java
@@ -1,43 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardOnStackAction;
-import magic.model.action.MagicCounterItemOnStackAction;
-import magic.model.action.MagicDrawAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
 public class Exclude {
-	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
-		@Override
-		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-			final MagicPlayer player = cardOnStack.getController();
-			return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    MagicTargetChoice.NEG_TARGET_CREATURE_SPELL,
-                    new Object[]{cardOnStack,player},
-                    this,
-                    "Counter target creature spell$. Draw a card.");
-		}
-		@Override
-		public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
-            event.processTargetCardOnStack(game,choiceResults,0,new MagicCardOnStackAction() {
-                public void doAction(final MagicCardOnStack targetSpell) {
-                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
-                    game.doAction(new MagicDrawAction((MagicPlayer)data[1],1));
-                }
-			});
-		}
-	};
+	public static final Object S = Bone_to_Ash.S;
 }
