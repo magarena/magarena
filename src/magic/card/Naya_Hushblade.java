2c27171b5f5baea43e4549ed063d895953cae283
melvin <unknown>
2012-03-13 22:32:01 +0800

    refer to Band Sureblade's first ability

--- src/magic/card/Naya_Hushblade.java
+++ src/magic/card/Naya_Hushblade.java
@@ -1,4 +1,3 @@
-
 package magic.card;
 
 import magic.model.MagicAbility;
@@ -10,28 +9,12 @@ import magic.model.mstatic.MagicLayer;
 import magic.model.mstatic.MagicStatic;
 
 public class Naya_Hushblade {
-	private static boolean isValid(final MagicPermanent owner, final MagicGame game) {
-		for (final MagicPermanent permanent : owner.getController().getPermanents()) {
-			if (permanent != owner && MagicColor.isMulti(permanent.getColorFlags(game))) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
-		@Override
-		public void getPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
-			if (isValid(permanent,game)) {
-				pt.add(1,1);
-			}
-		}		
-	};
+	public static final MagicStatic S1 = Bant_Sureblade.S1;
 	
 	public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
         @Override
         public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
-			return (isValid(permanent,game)) ?
+			return (Bant_Sureblade.isValid(permanent,game)) ?
                 flags | MagicAbility.Shroud.getMask() :
                 flags;
         }
