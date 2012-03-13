f2a25597c47a65a9e744a3440d27e401f1b1fdd1
melvin <unknown>
2012-03-13 23:37:24 +0800

    refer to Manic Vandal's ability

--- src/magic/card/Keldon_Vandals.java
+++ src/magic/card/Keldon_Vandals.java
@@ -1,43 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
 public class Keldon_Vandals {
-	public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(
-				final MagicGame game,
-				final MagicPermanent permanent,
-				final MagicPlayer player) {
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    MagicTargetChoice.TARGET_ARTIFACT,
-                    new MagicDestroyTargetPicker(false),
-                    MagicEvent.NO_DATA,
-                    this,
-                    "Destroy target artifact$.");
-		}
-
-		@Override
-		public void executeEvent(
-				final MagicGame game,
-				final MagicEvent event,
-				final Object data[],
-				final Object[] choiceResults) {
-            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent permanent) {
-                    game.doAction(new MagicDestroyAction(permanent));
-                }
-			});
-		}
-    };
+	public static final Object T = Manic_Vandal.T;
 }
