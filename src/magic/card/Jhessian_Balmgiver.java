c575b1aa98a5bf5f937fb6b2d26f03187ebefeb2
melvin <unknown>
2012-03-13 22:33:44 +0800

    refer to Abuna_Acolyte's ability

--- src/magic/card/Jhessian_Balmgiver.java
+++ src/magic/card/Jhessian_Balmgiver.java
@@ -21,38 +21,7 @@ import magic.model.target.MagicTarget;
 import magic.model.target.MagicUnblockableTargetPicker;
 
 public class Jhessian_Balmgiver {
-
-	public static final MagicPermanentActivation A = new MagicPermanentActivation(
-            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
-            new MagicActivationHints(MagicTiming.Pump),
-            "Prevent") {
-
-		@Override
-		public MagicEvent[] getCostEvent(final MagicSource source) {
-			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
-		}
-
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-			return new MagicEvent(
-                    source,
-                    source.getController(),
-                    MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER,
-                    MagicPreventTargetPicker.getInstance(),
-                    MagicEvent.NO_DATA,
-                    this,
-                    "Prevent the next 1 damage that would be dealt to target creature or player$ this turn.");
-		}
-
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-			event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    game.doAction(new MagicPreventDamageAction(target,1));
-                }
-			});
-		}
-	};
+	public static final MagicPermanentActivation A1 = Abuna_Acolyte.A1;
 
 	public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
             new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
--- src/magic/card/Samite_Healer.java
+++ src/magic/card/Samite_Healer.java
@@ -1,5 +1,5 @@
 package magic.card;
 
 public class Samite_Healer {
-	public static final Object A = Jhessian_Balmgiver.A1;
+	public static final Object A = Abuna_Acolyte.A1;
 }
