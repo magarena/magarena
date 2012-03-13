2a57a8595a4ac683840cbf6ee8b01c32c08315aa
melvin <unknown>
2012-03-13 23:33:55 +0800

    refer to AEther Adept's ability

--- src/magic/card/Man_o__War.java
+++ src/magic/card/Man_o__War.java
@@ -1,38 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicRemoveFromPlayAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicBounceTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
 public class Man_o__War {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-			return new MagicEvent(
-                    permanent,
-                    player,
-                    MagicTargetChoice.TARGET_CREATURE,
-                    MagicBounceTargetPicker.getInstance(),
-                    MagicEvent.NO_DATA,
-                    this,
-                    "Return target creature$ to its owner's hand.");
-		}
-		
-		@Override
-		public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
-                }
-			});
-		}
-    };
+    public static final Object T = AEther_Adept.T;
 }
