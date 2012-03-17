e8de2b5f77972e936dce19b2a8775db94c3dcadf
melvin <unknown>
2012-03-17 21:32:14 +0800

    simplified by removing encoding of name by using built-in toString method

--- src/magic/model/MagicType.java
+++ src/magic/model/MagicType.java
@@ -3,52 +3,46 @@ package magic.model;
 public enum MagicType {
 
     // these are supertypes
-	Basic("Basic"),
-	Legendary("Legendary"),
-    Onging("Onging"),
-    Snow("Snow"),
-    World("World"),
-	
+    Basic,
+    Legendary,
+    Onging,
+    Snow,
+    World,
+    
     // these are card types
-	Artifact("Artifact"),
-	Creature("Creature"),
-	Enchantment("Enchantment"),
-	Instant("Instant"),
-	Land("Land"),
-	Plane("Plane"),
-	Planeswalker("Planeswalker"),
-	Scheme("Scheme"),
-	Sorcery("Sorcery"),
-    Tribal("Tribal"),
-    Vanguard("Vanguard"),
-	;
-	
-	private final String name;
+    Artifact,
+    Creature,
+    Enchantment,
+    Instant,
+    Land,
+    Plane,
+    Planeswalker,
+    Scheme,
+    Sorcery,
+    Tribal,
+    Vanguard,
+    ;
+    
     private final int mask;
-	
-	private MagicType(final String name) {
-		this.name=name;
-        this.mask=1<<ordinal();
-	}
-	
-	private String getName() {
-		return name;
-	}
-	
-	public int getMask() {
-		return mask;
-	}
-	
-	public static MagicType getType(final String name) {
-		for (final MagicType type : values()) {
-			if (type.getName().equalsIgnoreCase(name)) {
-				return type;
-			}
-		}
+    
+    private MagicType() {
+        mask=1<<ordinal();
+    }
+    
+    public int getMask() {
+        return mask;
+    }
+    
+    public static MagicType getType(final String name) {
+        for (final MagicType type : values()) {
+            if (type.toString().equalsIgnoreCase(name)) {
+                return type;
+            }
+        }
         throw new RuntimeException("No corresponding MagicType for " + name);
-	}
-	
+    }
+    
     public boolean hasType(final int flags) {
-		return (flags & getMask()) != 0;
-	}
+        return (flags & getMask()) != 0;
+    }
 }
