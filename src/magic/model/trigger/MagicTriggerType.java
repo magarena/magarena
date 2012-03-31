e0ae88a299b0de292a22938334e88b8e03feae91
melvin <unknown>
2012-03-31 19:00:33 +0800

    convert tab to spaces

--- src/magic/model/trigger/MagicTriggerType.java
+++ src/magic/model/trigger/MagicTriggerType.java
@@ -1,31 +1,31 @@
 package magic.model.trigger;
 
 public enum MagicTriggerType {
-	AtUpkeep,               // player
-	AtEndOfTurn,            // player
-	WhenDamageIsDealt,      // damage
-	WhenOtherSpellIsCast,	// card on stack
-	WhenSpellIsCast,		// card on stack
-	WhenComesIntoPlay,      // controller
-	WhenLeavesPlay,      	// permanent
-	WhenBecomesTapped,      // permanent
-	WhenDiscarded,			// card
-	WhenDrawn,				// card
-	WhenLifeIsGained,		// player, life gained
-	WhenOtherComesIntoPlay, // permanent
-	WhenPutIntoGraveyard,   // graveyard trigger data
-	WhenOtherPutIntoGraveyardFromPlay, // permanent
-	WhenAttacks,            // permanent
-	WhenBlocks,             // permanent
-	WhenBecomesBlocked,		// permanent
-	WhenAttacksUnblocked,	// permanent
+    AtUpkeep,               // player
+    AtEndOfTurn,            // player
+    WhenDamageIsDealt,      // damage
+    WhenOtherSpellIsCast,   // card on stack
+    WhenSpellIsCast,        // card on stack
+    WhenComesIntoPlay,      // controller
+    WhenLeavesPlay,         // permanent
+    WhenBecomesTapped,      // permanent
+    WhenDiscarded,          // card
+    WhenDrawn,              // card
+    WhenLifeIsGained,       // player, life gained
+    WhenOtherComesIntoPlay, // permanent
+    WhenPutIntoGraveyard,   // graveyard trigger data
+    WhenOtherPutIntoGraveyardFromPlay, // permanent
+    WhenAttacks,            // permanent
+    WhenBlocks,             // permanent
+    WhenBecomesBlocked,     // permanent
+    WhenAttacksUnblocked,   // permanent
     WhenTargeted,           // permanent
-    	WhenLoseControl,	// permanent
-	IfDamageWouldBeDealt,   // item on stack
+    WhenLoseControl,        // permanent
+    IfDamageWouldBeDealt,   // item on stack
     IfPlayerWouldLose,      // player[]
-	;
-	
-	public boolean usesStack() {
+    ;
+    
+    public boolean usesStack() {
         return this != IfDamageWouldBeDealt;
-	}
+    }
 }
