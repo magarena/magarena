46f668f4d4ef1b3ce7e4467ae4ea50b7e653b39c
beholder <unknown>
2012-05-05 13:30:01 +0200

    added Dingus Egg and Stone Rain. forum request

---
+++ src/magic/card/Dingus_Egg.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Dingus_Egg {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent otherPermanent) {
+			return (otherPermanent.isLand()) ?
+				new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new Object[]{otherPermanent.getController()},
+                    this,
+                    permanent + " deals 2 damage to " +
+                    		otherPermanent.getController() + ".") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicDamage damage = new MagicDamage(
+					event.getSource(),
+					(MagicTarget)data[0],
+					2,
+					false);
+            game.doAction(new MagicDealDamageAction(damage));
+		}
+    };
+}
---
+++ src/magic/card/Stone_Rain.java
@@ -0,0 +1,5 @@
+package magic.card;
+
+public class Stone_Rain {
+	public static final Object S = Rain_of_Tears.S;
+}
