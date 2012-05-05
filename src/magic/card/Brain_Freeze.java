dab5bb9e1b125957ac4b5d1b0a4c142ec2ec4395
beholder <unknown>
2012-05-05 15:47:38 +0200

    added Brain Freeze and Grapeshot

---
+++ src/magic/card/Brain_Freeze.java
@@ -0,0 +1,43 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicMillLibraryAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Brain_Freeze {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.TARGET_PLAYER,
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target player$ puts the top three cards of his or her " +
+                    "library into his or her graveyard.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                	game.doAction(new MagicMillLibraryAction(player,3));
+                }
+			});
+		}
+	};
+}
