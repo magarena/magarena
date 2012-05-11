796911274b2fcb3892cd9e783343d77d577352f2
beholder <unknown>
2012-05-11 09:10:43 +0200

    added Bladed Bracers and Blood Artist

---
+++ src/magic/card/Bladed_Bracers.java
@@ -0,0 +1,32 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicSubType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Bladed_Bracers {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_CREATURE) {
+		@Override
+		public long getAbilityFlags(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final long flags) {
+			return flags | MagicAbility.Vigilance.getMask();
+		}
+		@Override
+		public boolean condition(
+				final MagicGame game,
+				final MagicPermanent source,
+				final MagicPermanent target) {
+			return target == source.getEquippedCreature() &&
+					(source.getEquippedCreature().hasSubType(MagicSubType.Human,game) ||
+					source.getEquippedCreature().hasSubType(MagicSubType.Angel,game));
+		}
+	};
+}
---
+++ src/magic/card/Blood_Artist.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Blood_Artist {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent otherPermanent) {
+			final MagicPlayer controller = permanent.getController();
+			return (otherPermanent.isCreature(game)) ?
+				new MagicEvent(
+                    permanent,
+                    controller,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new Object[]{controller},
+                    this,
+                    "Target player$ loses 1 life and " +
+                    controller + " gains 1 life."):
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-1));
+                }
+			});
+			game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],1));
+		}
+    };
+}
