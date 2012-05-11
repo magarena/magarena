ba52a6183eb98e29d7d90d7cde8cf1b6615f2b90
beholder <unknown>
2012-05-11 08:37:22 +0200

    added Banners Raised and Barter in Blood

---
+++ src/magic/card/Banners_Raised.java
@@ -0,0 +1,46 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Banners_Raised {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Creatures you control get +1/+0 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));			
+			final Collection<MagicTarget> targets = game.filterTargets(
+					(MagicPlayer)data[1],
+					MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				game.doAction(new MagicChangeTurnPTAction((MagicPermanent)target,1,0));
+			}
+		}
+	};
+}
---
+++ src/magic/card/Barter_in_Blood.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.MagicType;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSacrificePermanentEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Barter_in_Blood {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+				    new Object[]{cardOnStack},
+				    this,
+				    "Each player sacrifices two creatures.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			for (final MagicPlayer player : game.getPlayers()) {
+				for (int i=2;i>0;i--) {
+					if (player.controlsPermanentWithType(MagicType.Creature,game)) {
+	    				game.addEvent(new MagicSacrificePermanentEvent(
+	                        cardOnStack.getCard(),
+	                        player,
+	                        MagicTargetChoice.SACRIFICE_CREATURE));
+	                }
+				}
+			}
+		}
+	};
+
+}
