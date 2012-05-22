cfdaf506dc2c139daa78c5b210ccb4bda01d856b
melvin <melvinzhang@gmail.com>
2012-05-22 12:55:24 +0800

    added Wasteland

---
+++ src/magic/test/TestWasteland.java
@@ -0,0 +1,50 @@
+package magic.test;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.MagicDuel;
+import magic.model.phase.MagicMainPhase;
+
+class TestWasteland extends TestGameBuilder {    
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+        
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+        
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(4);
+        P.setPoison(6);
+        addToLibrary(P,"Plains",10);
+        createPermanent(game,P,"Rupture Spire",false,1);
+        addToHand(P,"Wasteland",1);
+        addToHand(P,"Vivid Crag",1);
+        addToHand(P,"Stonework Puma",1);
+        addToHand(P,"Llanowar Elves",1);
+
+
+        P = opponent;
+        
+        P.setLife(1);
+        P.setPoison(8);
+        addToLibrary(P,"Island",10);
+        createPermanent(game,P,"Rupture Spire",false,1);
+        addToHand(P,"Wasteland",1);
+        addToHand(P,"Vines of Vastwood",1);
+        addToHand(P,"Inkwell Leviathan",1);
+        
+        return game;
+    }
+}
