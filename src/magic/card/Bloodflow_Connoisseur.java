fbe6ec0e96711f4bcc86be8d447fae71e0169c6e
beholder <unknown>
2012-06-09 13:04:13 +0200

    added Bloodflow Connoisseur

---
+++ src/magic/card/Bloodflow_Connoisseur.java
@@ -0,0 +1,54 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicSacrificePermanentEvent;
+import magic.model.event.MagicTiming;
+
+public class Bloodflow_Connoisseur {
+    public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{MagicCondition.ONE_CREATURE_CONDITION},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_CREATURE)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    source.getController() + " puts a +1/+1 counter on "
+                    + source + ".");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction(
+					(MagicPermanent)data[0],
+					MagicCounterType.PlusOne,
+					1,
+					true));
+		}
+	};
+}
