7b32f19c764ff2a5b78626d30e7564a23e7796d6
melvin <melvinzhang@gmail.com>
2012-06-09 18:15:41 +0800

    convert tab to four spaces

--- src/magic/model/action/MagicGainControlAction.java
+++ src/magic/model/action/MagicGainControlAction.java
@@ -8,47 +8,44 @@ import magic.model.trigger.MagicTriggerType;
 
 public class MagicGainControlAction extends MagicAction {
 
-	private final MagicPlayer player;
-	private final MagicPermanent permanent;
-	private MagicPlayer oldController;
-	
-	public MagicGainControlAction(final MagicPlayer player,final MagicPermanent permanent) {
-		
-		this.player = player;
-		this.permanent = permanent;
-	}
-	
-	@Override
-	public void doAction(final MagicGame game) {
+    private final MagicPlayer player;
+    private final MagicPermanent permanent;
+    private MagicPlayer oldController;
+    
+    public MagicGainControlAction(final MagicPlayer player,final MagicPermanent permanent) {
+        this.player = player;
+        this.permanent = permanent;
+    }
+    
+    @Override
+    public void doAction(final MagicGame game) {
+        oldController=permanent.getController();
+        if (player!=oldController) {
+            int score = permanent.getScore();
+            
+            // Execute trigger here so that full permanent state is preserved.
+            game.executeTrigger(MagicTriggerType.WhenLoseControl, permanent);
+            
+            oldController.removePermanent(permanent);
+            permanent.setController(player);
+            player.addPermanent(permanent);
+            permanent.setState(MagicPermanentState.Summoned);
+            if (permanent.getPairedCreature().isValid()) {;
+                game.doAction(new MagicSoulbondAction(permanent,permanent.getPairedCreature(),false));
+            }
+            score += permanent.getScore();
+            setScore(player, score);
+            game.setStateCheckRequired();
+        }
+    }
 
-		oldController=permanent.getController();
-		if (player!=oldController) {
-			int score = permanent.getScore();
-			
-			// Execute trigger here so that full permanent state is preserved.
-			game.executeTrigger(MagicTriggerType.WhenLoseControl, permanent);
-			
-			oldController.removePermanent(permanent);
-			permanent.setController(player);
-			player.addPermanent(permanent);
-			permanent.setState(MagicPermanentState.Summoned);
-			if (permanent.getPairedCreature().isValid()) {;
-				game.doAction(new MagicSoulbondAction(permanent,permanent.getPairedCreature(),false));
-			}
-			score += permanent.getScore();
-			setScore(player, score);
-			game.setStateCheckRequired();
-		}
-	}
-
-	@Override
-	public void undoAction(final MagicGame game) {
-
-		if (player!=oldController) {
-			player.removePermanent(permanent);
-			permanent.setController(oldController);
-			oldController.addPermanent(permanent);
-			permanent.clearState(MagicPermanentState.Summoned);
-		}
-	}
+    @Override
+    public void undoAction(final MagicGame game) {
+        if (player!=oldController) {
+            player.removePermanent(permanent);
+            permanent.setController(oldController);
+            oldController.addPermanent(permanent);
+            permanent.clearState(MagicPermanentState.Summoned);
+        }
+    }
 }
