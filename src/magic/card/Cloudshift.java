f5bb7247ba390ac4d44f75b69b74e64afbb876f3
beholder <unknown>
2012-06-10 11:22:05 +0200

    added Cloudshift and Commander's Authority

---
+++ src/magic/card/Cloudshift.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicBounceTargetPicker;
+
+public class Cloudshift {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                    MagicBounceTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Exile target creature$ you control, then return " +
+                    "that card to the battlefield under your control.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                	game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                	game.doAction(new MagicRemoveCardAction(creature.getCard(),MagicLocationType.Exile));
+                	game.doAction(new MagicPlayCardAction(creature.getCard(),event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+			});
+		}
+	};
+}
---
+++ src/magic/card/Commander_s_Authority.java
@@ -0,0 +1,41 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+
+public class Commander_s_Authority {
+    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer data) {
+			final MagicPermanent enchanted = permanent.getEnchantedCreature();
+		    final MagicPlayer controller = enchanted.getController();
+			return (controller == data) ?
+				new MagicEvent(
+                        enchanted,
+                        controller,
+                        new Object[]{controller},
+                        this,
+                        controller + " puts a 1/1 white Human " +
+                        "creature token onto the battlefield.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicPlayTokenAction(
+					(MagicPlayer)data[0],
+					TokenCardDefinitions.get("Human1")));
+		}
+    };
+}
