43cb91d02e1667e84cfa10457079cea95c09deff
melvin <melvinzhang@gmail.com>
2012-06-10 18:28:11 +0800

    removed empty lines

--- src/magic/model/phase/MagicDeclareAttackersPhase.java
+++ src/magic/model/phase/MagicDeclareAttackersPhase.java
@@ -8,18 +8,15 @@ public class MagicDeclareAttackersPhase extends MagicPhase {
 	private static final MagicPhase INSTANCE=new MagicDeclareAttackersPhase();
 	
 	private MagicDeclareAttackersPhase() {
-		
 		super(MagicPhaseType.DeclareAttackers);	
 	}
 	
 	public static MagicPhase getInstance() {
-		
 		return INSTANCE;
 	}
 	
 	@Override
 	public void executeBeginStep(final MagicGame game) {
-
 		game.addEvent(new MagicDeclareAttackersEvent(game.getTurnPlayer()));
 		game.setStep(MagicStep.ActivePlayer);
 	}
