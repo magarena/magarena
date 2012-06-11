993019261f1ff64a8090765e00f2f2f8b4a3d231
beholder <unknown>
2012-06-11 10:34:37 +0200

    added white space, wrapped long lines

--- src/magic/model/action/MagicMillLibraryAction.java
+++ src/magic/model/action/MagicMillLibraryAction.java
@@ -12,23 +12,27 @@ public class MagicMillLibraryAction extends MagicAction {
 	private final MagicPlayer player;
 	private final int amount;
 	
-	public MagicMillLibraryAction(final MagicPlayer player,final int amount) {
-		
-		this.player=player;
-		this.amount=amount;
+	public MagicMillLibraryAction(final MagicPlayer player,final int amount) {	
+		this.player = player;
+		this.amount = amount;
 	}
 
 	@Override
 	public void doAction(final MagicGame game) {
-		final MagicCardList library=player.getLibrary();
-		final int size=library.size();
-		final int count=size>=amount?amount:size;		
-		if (count>0) {
+		final MagicCardList library = player.getLibrary();
+		final int size = library.size();
+		final int count = size >= amount ? amount : size;		
+		if (count > 0) {
 			setScore(player,ArtificialScoringSystem.getMillScore(count));
 			for (int c=count;c>0;c--) {
 				final MagicCard milledCard = library.getCardAtTop();
-				game.doAction(new MagicRemoveCardAction(milledCard,MagicLocationType.OwnersLibrary));
-				game.doAction(new MagicMoveCardAction(milledCard,MagicLocationType.OwnersLibrary,MagicLocationType.Graveyard));
+				game.doAction(new MagicRemoveCardAction(
+						milledCard,
+						MagicLocationType.OwnersLibrary));
+				game.doAction(new MagicMoveCardAction(
+						milledCard,
+						MagicLocationType.OwnersLibrary,
+						MagicLocationType.Graveyard));
 			}
 			game.logMessage(
 					player,
