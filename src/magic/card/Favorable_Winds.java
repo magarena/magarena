af81b3f003396558507367b03821ae7dbad932a6
beholder <unknown>
2012-06-11 16:33:23 +0200

    added Favorable Winds

---
+++ src/magic/card/Favorable_Winds.java
@@ -0,0 +1,22 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Favorable_Winds {
+    public static final MagicStatic S = new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_CREATURE_WITH_FLYING_YOU_CONTROL) {
+        @Override
+		public void getPowerToughness(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+    };
+}
