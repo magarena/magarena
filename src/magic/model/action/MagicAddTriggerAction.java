e572c26b045848486ba9457b6b5109c5f40e33aa
melvin <melvinzhang@gmail.com>
2012-06-12 09:39:37 +0800

    added missing MagicAddTriggerAction

---
+++ src/magic/model/action/MagicAddTriggerAction.java
@@ -0,0 +1,28 @@
+package magic.model.action;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.trigger.MagicPermanentTrigger;
+import magic.model.trigger.MagicTrigger;
+
+public class MagicAddTriggerAction extends MagicAction {
+
+	private final MagicPermanent permanent;
+	private final MagicTrigger<?> trigger;
+	private MagicPermanentTrigger permanentTrigger;
+	
+	public MagicAddTriggerAction(final MagicPermanent permanent,final MagicTrigger<?> trigger) {
+		this.permanent=permanent;
+		this.trigger=trigger;
+	}
+
+	@Override
+	public void doAction(final MagicGame game) {
+		permanentTrigger=game.addTrigger(permanent,trigger);
+	}
+
+	@Override
+	public void undoAction(final MagicGame game) {
+		game.removeTrigger(permanentTrigger);
+	}
+}
