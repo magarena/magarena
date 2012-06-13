b5c4fb2ae315064efc5f338e3bf06a9c5c229a50
beholder <unknown>
2012-06-13 18:33:37 +0200

    added Hanweir Lancer and Harvester of Souls

---
+++ src/magic/card/Harvester_of_Souls.java
@@ -0,0 +1,45 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicDrawAction;
+import magic.model.choice.MagicChoice;
+import magic.model.choice.MagicSimpleMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Harvester_of_Souls {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent otherPermanent) {
+			return (permanent != otherPermanent &&
+					otherPermanent.isCreature() &&
+					!otherPermanent.getCard().isToken()) ?
+				new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new MagicSimpleMayChoice(
+                            "You may draw a card.",
+                            MagicSimpleMayChoice.DRAW_CARDS,
+                            1,
+                            MagicSimpleMayChoice.DEFAULT_NONE),
+                        MagicEvent.NO_DATA,
+                        this,
+                        "You may$ draw a card."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			if (MagicChoice.isYesChoice(choiceResults[0])) {
+				game.doAction(new MagicDrawAction(event.getPlayer(),1));
+			}
+		}
+    };
+}
