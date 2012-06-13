b970f2afd905e8efa109c001477c9b72aeb88105
beholder <unknown>
2012-06-13 09:47:53 +0200

    added blue spirit token. added Geist Snatch

---
+++ src/magic/card/Geist_Snatch.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.action.MagicCardOnStackAction;
+import magic.model.action.MagicCounterItemOnStackAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Geist_Snatch {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE_SPELL,
+                    new Object[]{cardOnStack},
+                    this,
+                    "Counter target creature spell$. Put a 1/1 blue " +
+                    "Spirit creature token with flying onto the battlefield.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetCardOnStack(game,choiceResults,0,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicPlayTokenAction(
+                			event.getPlayer(),
+                    		TokenCardDefinitions.get("Spirit1 Blue")));
+                }
+			});
+		}
+	};
+}
