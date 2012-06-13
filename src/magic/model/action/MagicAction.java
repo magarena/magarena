7da8e72bb23ef3a3bbd2d6db96da63320305af36
melvin <melvinzhang@gmail.com>
2012-06-13 22:47:48 +0800

    toString returns class name

--- src/magic/model/action/MagicAction.java
+++ src/magic/model/action/MagicAction.java
@@ -4,25 +4,25 @@ import magic.model.MagicGame;
 import magic.model.MagicPlayer;
 
 public abstract class MagicAction {
-	
-	private MagicPlayer scorePlayer;
-	private int score=0;
-	
-	final void setScore(final MagicPlayer aScorePlayer, final int aScore) {
-		this.scorePlayer=aScorePlayer;
-		this.score=aScore;
-	}
-	
-	public final int getScore(final MagicPlayer player) {
-	    return (player==scorePlayer) ? score : -score;
-	}
-	
-	public abstract void doAction(final MagicGame game);
-	
-	public abstract void undoAction(final MagicGame game);
+    
+    private MagicPlayer scorePlayer;
+    private int score=0;
+    
+    final void setScore(final MagicPlayer aScorePlayer, final int aScore) {
+        this.scorePlayer=aScorePlayer;
+        this.score=aScore;
+    }
+    
+    public final int getScore(final MagicPlayer player) {
+        return (player==scorePlayer) ? score : -score;
+    }
+    
+    public abstract void doAction(final MagicGame game);
+    
+    public abstract void undoAction(final MagicGame game);
 
-	@Override
-	public String toString() {
-		return "";
-	}
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
 }
