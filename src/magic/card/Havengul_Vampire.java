78c774c80f331c8daed66fb435ef60bd9e79fe37
beholder <unknown>
2012-06-13 19:05:01 +0200

    added Havengul Vampire and Heirs of Stromkirk

---
+++ src/magic/card/Havengul_Vampire.java
@@ -0,0 +1,42 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
+
+public class Havengul_Vampire {
+    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent otherPermanent) {
+			final MagicPlayer player = permanent.getController();
+			return (permanent != otherPermanent &&
+					otherPermanent.isCreature()) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{permanent},
+                    this,
+                    player + " puts a +1/+1 counter on " + permanent + "."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			game.doAction(new MagicChangeCountersAction(
+					(MagicPermanent)data[0],
+					MagicCounterType.MinusOne,
+					1,
+					true));
+		}
+    };
+}
