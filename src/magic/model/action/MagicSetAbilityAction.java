587fdd428e41057197e5c9d59f79013ca4ee267f
melvin <melvinzhang@gmail.com>
2012-06-13 23:02:26 +0800

    convert tabs to four spaces

--- src/magic/model/action/MagicSetAbilityAction.java
+++ src/magic/model/action/MagicSetAbilityAction.java
@@ -9,30 +9,30 @@ import magic.model.mstatic.MagicStatic;
 // Set an ability until end of turn.
 public class MagicSetAbilityAction extends MagicAction {
 
-	private final MagicPermanent permanent;
-	private final long abilities;
+    private final MagicPermanent permanent;
+    private final long abilities;
     private final boolean duration;
-	
-	public MagicSetAbilityAction(final MagicPermanent permanent,final long abilities, final boolean duration) {
-		this.permanent=permanent;
-		this.abilities=abilities;
+    
+    public MagicSetAbilityAction(final MagicPermanent permanent,final long abilities, final boolean duration) {
+        this.permanent=permanent;
+        this.abilities=abilities;
         this.duration=duration;
-	}
-	
+    }
+    
     public MagicSetAbilityAction(final MagicPermanent permanent,final long abilities) {
         this(permanent,abilities,MagicStatic.UntilEOT);
-	}
+    }
     
     public MagicSetAbilityAction(final MagicPermanent permanent,final MagicAbility ability,final boolean duration) {
-		this(permanent,ability.getMask(),duration);
-	}
-	
-	public MagicSetAbilityAction(final MagicPermanent permanent,final MagicAbility ability) {
-		this(permanent,ability.getMask(),MagicStatic.UntilEOT);
-	}
-		
-	@Override
-	public void doAction(final MagicGame game) {
+        this(permanent,ability.getMask(),duration);
+    }
+    
+    public MagicSetAbilityAction(final MagicPermanent permanent,final MagicAbility ability) {
+        this(permanent,ability.getMask(),MagicStatic.UntilEOT);
+    }
+        
+    @Override
+    public void doAction(final MagicGame game) {
         game.doAction(new MagicAddStaticAction(permanent, new MagicStatic(
                 MagicLayer.Ability,
                 duration) {
@@ -44,14 +44,14 @@ public class MagicSetAbilityAction extends MagicAction {
                 return flags | abilities;
             }   
         }));
-	}
+    }
 
-	@Override
-	public void undoAction(final MagicGame game) {
-	}
-	
     @Override
-	public String toString() {
+    public void undoAction(final MagicGame game) {
+    }
+    
+    @Override
+    public String toString() {
         return super.toString() + " (" + permanent + "," + abilities + ')';
-	}
+    }
 }
