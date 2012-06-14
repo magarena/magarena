d54b6072368f0d799c3aab5516fcf5e7cef218d8
beholder <unknown>
2012-06-14 10:32:43 +0200

    added Joint Assault and Kessig Malcontents

---
+++ src/magic/card/Joint_Assault.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicPumpTargetPicker;
+
+public class Joint_Assault {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.create(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets +2/+2 until end of turn. " +
+                    "If it's paired with a creature, that creature " +
+                    "also gets +2/+2 until end of turn.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                    if (creature.isPaired()) {
+                    	game.doAction(new MagicChangeTurnPTAction(
+                    			creature.getPairedCreature(),
+                    			2,
+                    			2));
+                    }
+                }
+			});
+		}
+	};
+}
---
+++ src/magic/card/Kessig_Malcontents.java
@@ -0,0 +1,54 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSubType;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicDamageTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+
+public class Kessig_Malcontents {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			final int amount = player.getNrOfPermanentsWithSubType(MagicSubType.Human);
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(amount),
+                    new Object[]{permanent},
+                    this,
+                    permanent + " deals damage to target player$ " +
+                    "equal to the number of Humans " + player + " controls.");
+		}
+		
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object data[],
+				final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                	final int amount = event.getPlayer().getNrOfPermanentsWithSubType(MagicSubType.Human);
+                	final MagicDamage damage = new MagicDamage(
+                			event.getSource(),
+                			player,
+                			amount,
+                			false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+		}
+    };
+}
