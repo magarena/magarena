8d8c73577428bdf6e731f3fa30bf0dcfd6830635
beholder <unknown>
2012-06-14 14:46:09 +0200

    added Mental Agony and Midnight Duelist

---
+++ src/magic/card/Mental_Agony.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Mental_Agony {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player=cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target player$ discards two cards and loses 2 life.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(
+                    		cardOnStack.getCard(),
+                    		player,
+                    		2,
+                    		false));
+                    game.doAction(new MagicChangeLifeAction(player,-2));
+                }
+			});
+		}
+	};
+}
