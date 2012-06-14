9b8fe6ffec3520f8d9625a0cedf5fac2a61ce024
beholder <unknown>
2012-06-14 13:04:24 +0200

    added Maalfeld Twins and Mad Prophet

---
+++ src/magic/card/Maalfeld_Twins.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Maalfeld_Twins {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicGraveyardTriggerData triggerData) {
+			final MagicPlayer player = permanent.getController();
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+				new MagicEvent(
+                    permanent,
+                    player,
+                    MagicEvent.NO_DATA,
+                    this,
+                    player + " puts two 2/2 black Zombie " +
+                    "creature tokens onto the battlefield.") :
+                MagicEvent.NONE;
+		}
+		
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPlayer player = event.getPlayer();
+			game.doAction(new MagicPlayTokenAction(
+            		player,
+            		TokenCardDefinitions.get("Zombie")));
+			game.doAction(new MagicPlayTokenAction(
+            		player,
+            		TokenCardDefinitions.get("Zombie")));
+		}
+    };
+}
---
+++ src/magic/card/Mad_Prophet.java
@@ -0,0 +1,56 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicDrawAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+
+public class Mad_Prophet {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+			new MagicCondition[]{
+					MagicCondition.CAN_TAP_CONDITION,
+					MagicCondition.HAS_CARD_CONDITION
+			},
+            new MagicActivationHints(MagicTiming.Draw),
+            "Draw") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+					new MagicTapEvent((MagicPermanent)source),
+					new MagicDiscardEvent(source,source.getController(),1,false)
+			};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			return new MagicEvent(
+                    source,
+                    player,
+                    MagicEvent.NO_DATA,
+                    this,
+                    player + " draws a card.");
+		}
+
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicDrawAction(event.getPlayer(),1));
+		}
+	};
+}
