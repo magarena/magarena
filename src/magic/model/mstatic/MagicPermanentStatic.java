d1c9569de57aa2a54f14d59a415cff814b5e736f
melvin <melvinzhang@gmail.com>
2012-06-14 10:00:44 +0800

    convert tabs to four spaces

--- src/magic/model/mstatic/MagicPermanentStatic.java
+++ src/magic/model/mstatic/MagicPermanentStatic.java
@@ -23,46 +23,46 @@ public class MagicPermanentStatic implements Comparable<MagicPermanentStatic> {
             }
         });
 
-	private final long id;
-	private final MagicPermanent permanent;
-	private final MagicStatic mstatic;
-	
-	public MagicPermanentStatic(final long id,final MagicPermanent permanent,final MagicStatic mstatic) {
-		this.id=id;
-		this.permanent=permanent;
-		this.mstatic=mstatic;
-	}
-	
-	public MagicPermanentStatic(final MagicCopyMap copyMap,final MagicPermanentStatic source) {
-		this(source.id, copyMap.copy(source.permanent), source.mstatic);
-	}
-	
-	public long getId() {
-		return id;
-	}
-	
-	public MagicPermanent getPermanent() {
-		return permanent;
-	}
-	
-	public MagicStatic getStatic() {
+    private final long id;
+    private final MagicPermanent permanent;
+    private final MagicStatic mstatic;
+    
+    public MagicPermanentStatic(final long id,final MagicPermanent permanent,final MagicStatic mstatic) {
+        this.id=id;
+        this.permanent=permanent;
+        this.mstatic=mstatic;
+    }
+    
+    public MagicPermanentStatic(final MagicCopyMap copyMap,final MagicPermanentStatic source) {
+        this(source.id, copyMap.copy(source.permanent), source.mstatic);
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public MagicPermanent getPermanent() {
+        return permanent;
+    }
+    
+    public MagicStatic getStatic() {
         mstatic.setSource(permanent);
-		return mstatic;
-	}
-	
+        return mstatic;
+    }
+    
     public MagicLayer getLayer() {
-		return mstatic.getLayer();
-	}
-	
-	@Override
-	public int compareTo(final MagicPermanentStatic permanentStatic) {
+        return mstatic.getLayer();
+    }
+    
+    @Override
+    public int compareTo(final MagicPermanentStatic permanentStatic) {
         //sort by layer
-		final int layerDif = mstatic.getLayer().compareTo(permanentStatic.mstatic.getLayer());
-		if (layerDif != 0) {
-			return layerDif;
-		} 
+        final int layerDif = mstatic.getLayer().compareTo(permanentStatic.mstatic.getLayer());
+        if (layerDif != 0) {
+            return layerDif;
+        } 
         
         //break ties by id
-		return Long.signum(id-permanentStatic.id);
-	}
+        return Long.signum(id-permanentStatic.id);
+    }
 }
