05ab2de3dc6fdedaf7c4b5719edf3322775c1494
beholder <unknown>
2012-06-14 14:34:36 +0200

    added Malignus and Mass Appeal

---
+++ src/magic/card/Mass_Appeal.java
@@ -0,0 +1,40 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPlayer;
+import magic.model.MagicSubType;
+import magic.model.action.MagicDrawAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+
+public class Mass_Appeal {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    new Object[]{cardOnStack},
+                    this,
+                    player + " draws a card for each Human he or she controls.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+			final MagicPlayer player = event.getPlayer();
+			game.doAction(new MagicDrawAction(
+					player,
+					player.getNrOfPermanentsWithSubType(MagicSubType.Human)));
+		}
+	};
+}
