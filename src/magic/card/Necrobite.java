cdacdba6a15d4482300e173a3d9688bebfc38d47
beholder <unknown>
2012-06-15 10:40:44 +0200

    added Necrobite and Nephalia Smuggler

---
+++ src/magic/card/Necrobite.java
@@ -0,0 +1,51 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRegenerateAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDeathtouchTargetPicker;
+
+public class Necrobite {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicDeathtouchTargetPicker.getInstance(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gains deathtouch until end of turn. " +
+                    "Regenerate it.");
+		}
+
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(
+                    		creature,
+                    		MagicAbility.Deathtouch));
+                    game.doAction(new MagicRegenerateAction(creature));
+                }
+			});
+		}
+	};
+}
---
+++ src/magic/card/Nephalia_Smuggler.java
@@ -0,0 +1,68 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicPlayCardAction;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicBounceTargetPicker;
+import magic.model.target.MagicTargetFilter;
+import magic.model.target.MagicTargetHint;
+
+public class Nephalia_Smuggler {
+    public static final MagicPermanentActivation A = new MagicPermanentActivation( 
+			new MagicCondition[] {
+					MagicManaCost.THREE_BLUE.getCondition(),
+					MagicCondition.CAN_TAP_CONDITION,
+			},
+            new MagicActivationHints(MagicTiming.Main),
+            "Card") {
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{
+					new MagicTapEvent((MagicPermanent)source),
+					new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.THREE_BLUE)};
+		}
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			final MagicTargetFilter targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+	                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL,source);
+			final MagicTargetChoice targetChoice = new MagicTargetChoice(
+	                targetFilter,true,MagicTargetHint.None,"another creature to exile");
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    targetChoice,
+                    MagicBounceTargetPicker.getInstance(),
+				    MagicEvent.NO_DATA,
+                    this,
+                    "Exile another target creature you control$, then " +
+                    "return that card to the battlefield under your control.");
+		}
+		@Override
+		public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                	game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                	game.doAction(new MagicRemoveCardAction(creature.getCard(),MagicLocationType.Exile));
+                	game.doAction(new MagicPlayCardAction(creature.getCard(),event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+			});
+		}
+	};
+}
