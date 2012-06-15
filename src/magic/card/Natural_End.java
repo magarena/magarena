22ee43c6482aba38444ee73e687b36e21df40ea1
beholder <unknown>
2012-06-15 10:20:57 +0200

    added Natural End and Nearheath Pilgrim

---
+++ src/magic/card/Natural_End.java
@@ -0,0 +1,49 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDestroyTargetPicker;
+
+public class Natural_End {
+	public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(
+				final MagicCardOnStack cardOnStack,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.TARGET_ARTIFACT_OR_ENCHANTMENT,
+                    new MagicDestroyTargetPicker(false),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    "Destroy target artifact or enchantment$. " +
+                    		player + " gains 3 life.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    game.doAction(new MagicChangeLifeAction((MagicPlayer)data[1],3));
+                }
+			});
+		}
+	};
+}
