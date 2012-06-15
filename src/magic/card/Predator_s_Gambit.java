4ca8fe65a31cd592a474feb28642d7130a60016b
beholder <unknown>
2012-06-15 11:50:11 +0200

    added Predator's Gambit and Raging Poltergeist

---
+++ src/magic/card/Predator_s_Gambit.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicType;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Predator_s_Gambit {
+	public static final MagicStatic S = new MagicStatic(
+			MagicLayer.Ability, 
+			MagicTargetFilter.TARGET_CREATURE) {
+		@Override
+		public long getAbilityFlags(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final long flags) {
+			return flags | MagicAbility.Intimidate.getMask();
+		}
+		@Override
+		public boolean condition(
+				final MagicGame game,
+				final MagicPermanent source,
+				final MagicPermanent target) {
+			return target == source.getEnchantedCreature() &&
+					source.getController().getNrOfPermanentsWithType(MagicType.Creature) == 1;
+		}
+	};
+}
