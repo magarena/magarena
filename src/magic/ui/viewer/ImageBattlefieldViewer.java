e55264be16823d9fcc8dc3a62c30ce7c2f82faad
melvin <melvinzhang@gmail.com>
2012-06-16 11:23:14 +0800

    changed tabs to four spaces

--- src/magic/ui/viewer/ImageBattlefieldViewer.java
+++ src/magic/ui/viewer/ImageBattlefieldViewer.java
@@ -13,51 +13,48 @@ import java.util.Set;
 
 public class ImageBattlefieldViewer extends JPanel implements ChoiceViewer {
 
-	private static final long serialVersionUID = 1L;
-	
-	private final ViewerInfo viewerInfo;
-	private final boolean opponent;
-	private final ImagePermanentsViewer permanentsViewer;
-	private final PermanentFilter permanentFilter;
-	
-	public ImageBattlefieldViewer(final ViewerInfo viewerInfo,final GameController controller,final boolean opponent) {
-		
-		this.viewerInfo=viewerInfo;
-		this.opponent=opponent;
+    private static final long serialVersionUID = 1L;
+    
+    private final ViewerInfo viewerInfo;
+    private final boolean opponent;
+    private final ImagePermanentsViewer permanentsViewer;
+    private final PermanentFilter permanentFilter;
+    
+    public ImageBattlefieldViewer(final ViewerInfo viewerInfo,final GameController controller,final boolean opponent) {
+        this.viewerInfo=viewerInfo;
+        this.opponent=opponent;
 
-		final Theme theme=ThemeFactory.getInstance().getCurrentTheme();
-		
-		controller.registerChoiceViewer(this);
-		
-		setOpaque(false);
-		
-		setLayout(new BorderLayout(6,0));
-		
-		final JPanel leftPanel=new JPanel(new BorderLayout());
-		leftPanel.setOpaque(false);
-		add(leftPanel,BorderLayout.WEST);
-		
-		final JLabel iconLabel=new JLabel(theme.getIcon(Theme.ICON_SMALL_BATTLEFIELD));
-		iconLabel.setOpaque(true);
-		iconLabel.setBackground(theme.getColor(Theme.COLOR_ICON_BACKGROUND));
-		iconLabel.setPreferredSize(new Dimension(24,24));
-		iconLabel.setBorder(FontsAndBorders.BLACK_BORDER);
-		leftPanel.add(iconLabel,BorderLayout.NORTH);
-		
-		permanentsViewer=new ImagePermanentsViewer(controller, opponent);
-		add(permanentsViewer,BorderLayout.CENTER);
-		
-		permanentFilter=new PermanentFilter(this,controller);
-	}
-	
-	public void update() {
-
-		permanentsViewer.viewPermanents(permanentFilter.getPermanents(viewerInfo,opponent));
-	}
-	
-	@Override
-	public void showValidChoices(final Set<Object> validChoices) {
-
-		permanentsViewer.showValidChoices(validChoices);
-	}
+        final Theme theme=ThemeFactory.getInstance().getCurrentTheme();
+        
+        controller.registerChoiceViewer(this);
+        
+        setOpaque(false);
+        
+        setLayout(new BorderLayout(6,0));
+        
+        final JPanel leftPanel=new JPanel(new BorderLayout());
+        leftPanel.setOpaque(false);
+        add(leftPanel,BorderLayout.WEST);
+        
+        final JLabel iconLabel=new JLabel(theme.getIcon(Theme.ICON_SMALL_BATTLEFIELD));
+        iconLabel.setOpaque(true);
+        iconLabel.setBackground(theme.getColor(Theme.COLOR_ICON_BACKGROUND));
+        iconLabel.setPreferredSize(new Dimension(24,24));
+        iconLabel.setBorder(FontsAndBorders.BLACK_BORDER);
+        leftPanel.add(iconLabel,BorderLayout.NORTH);
+        
+        permanentsViewer=new ImagePermanentsViewer(controller, opponent);
+        add(permanentsViewer,BorderLayout.CENTER);
+        
+        permanentFilter=new PermanentFilter(this,controller);
+    }
+    
+    public void update() {
+        permanentsViewer.viewPermanents(permanentFilter.getPermanents(viewerInfo,opponent));
+    }
+    
+    @Override
+    public void showValidChoices(final Set<Object> validChoices) {
+        permanentsViewer.showValidChoices(validChoices);
+    }
 }
