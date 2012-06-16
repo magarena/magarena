557217d7b77c623ed1cd0667f6ced18151d4b777
beholder <unknown>
2012-06-16 10:12:41 +0200

    added Scalding Devil and Scrapskin Drake

---
+++ src/magic/card/Scalding_Devil.java
@@ -0,0 +1,63 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicPlayerAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicDamageTargetPicker;
+
+public class Scalding_Devil {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicManaCost.TWO_RED.getCondition()},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Damage") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[] {new MagicPayManaCostEvent(
+					source,
+					source.getController(),
+					MagicManaCost.TWO_RED)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    new MagicDamageTargetPicker(1),
+                    new Object[]{source},
+                    this,
+                    source + " deals 1 damage to target player$.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+			event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                	final MagicDamage damage=new MagicDamage((MagicSource)data[0],player,1,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+	};
+}
