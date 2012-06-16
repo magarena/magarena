b64fef0ede8f5c5300ac8a58d0b0a662083e3756
melvin <melvinzhang@gmail.com>
2012-06-16 23:26:32 +0800

    added Terminus

---
+++ src/magic/card/Terminus.java
@@ -0,0 +1,44 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicLocationType;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicRemoveFromPlayAction;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+import java.util.Collection;
+
+public class Terminus {
+    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Put all creatures on the bottom of their owners' libraries.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+            game.doAction(new MagicMoveCardAction(cardOnStack));
+            final Collection<MagicTarget> targets = 
+                game.filterTargets(cardOnStack.getController(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicTarget target : targets) {
+                final MagicPermanent permanent = (MagicPermanent)target;
+                game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.BottomOfOwnersLibrary));
+            }
+        }
+    };
+}
