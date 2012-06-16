55c7212fa57114212ea596743b91f0b898ae6444
beholder <unknown>
2012-06-16 09:42:25 +0200

    added Righteous Blow and Riot Ringleader

---
+++ src/magic/card/Righteous_Blow.java
@@ -0,0 +1,52 @@
+package magic.card;
+
+import magic.model.MagicDamage;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicDealDamageAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicDamageTargetPicker;
+
+public class Righteous_Blow {
+	public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+		@Override
+		public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+			final MagicPlayer player = cardOnStack.getController();
+			return new MagicEvent(
+                    cardOnStack.getCard(),
+                    player,
+                    MagicTargetChoice.NEG_TARGET_ATTACKING_OR_BLOCKING_CREATURE,
+                    new MagicDamageTargetPicker(2),
+                    new Object[]{cardOnStack,player},
+                    this,
+                    cardOnStack.getCard() + " deals 2 damage to " +
+                    "target attacking or blocking creature$.");
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+			final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
+			game.doAction(new MagicMoveCardAction(cardOnStack));
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final MagicDamage damage = new MagicDamage(
+                    		cardOnStack.getCard(),
+                    		creature,
+                    		2,
+                    		false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+			});
+		}
+	};
+}
---
+++ src/magic/card/Riot_Ringleader.java
@@ -0,0 +1,48 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenAttacksTrigger;
+
+
+public class Riot_Ringleader {
+    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPermanent creature) {
+			return (permanent == creature) ?
+                new MagicEvent(
+                        permanent,
+                        permanent.getController(),
+                        MagicEvent.NO_DATA,
+                        this,
+                        "Human creatures " + permanent.getController() +
+                        " controls get +1/+0 until end of turn."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final Collection<MagicTarget> targets = game.filterTargets(
+					event.getPlayer(),
+					MagicTargetFilter.TARGET_HUMAN_YOU_CONTROL);
+			for (final MagicTarget target : targets) {
+				game.doAction(new MagicChangeTurnPTAction(
+						(MagicPermanent)target,
+						1,
+						0));
+			}
+		}
+    };
+}
