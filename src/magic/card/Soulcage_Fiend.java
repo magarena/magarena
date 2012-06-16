f4630aef1b173bc80c4483d300df43474c536d43
beholder <unknown>
2012-06-16 13:43:47 +0200

    added Soulcage Fiend and Spectral Gateguards

---
+++ src/magic/card/Soulcage_Fiend.java
@@ -0,0 +1,39 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeLifeAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicGraveyardTriggerData;
+import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+
+public class Soulcage_Fiend {
+    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicGraveyardTriggerData triggerData) {
+			return (MagicLocationType.Play == triggerData.fromLocation) ?
+                new MagicEvent(
+                        permanent,
+                        permanent.getController(),
+                        MagicEvent.NO_DATA,
+                        this,
+                        "Each player loses 3 life."):
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			for (final MagicPlayer player : game.getPlayers()) {
+				game.doAction(new MagicChangeLifeAction(player,-3));
+			}
+		}
+    };
+}
