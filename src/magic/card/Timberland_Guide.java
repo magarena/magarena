34eceaff8f79e32fc1cd0e0448507c2096a36be3
beholder <unknown>
2012-06-17 08:59:41 +0200

    added Timberland Guide and Triumph of Cruelty

---
+++ src/magic/card/Timberland_Guide.java
@@ -0,0 +1,47 @@
+package magic.card;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicPumpTargetPicker;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Timberland_Guide {
+	public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer player) {
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    MagicPumpTargetPicker.create(),
+                    MagicEvent.NO_DATA,
+                    this,
+                    player + " puts a +1/+1 counter on target creature$.");
+		}	
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object data[],
+				final Object[] choiceResults) {
+			event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                	game.doAction(new MagicChangeCountersAction(
+        					creature,
+        					MagicCounterType.PlusOne,
+        					1,
+        					true));
+                }
+            });
+		}
+    };
+}
---
+++ src/magic/card/Triumph_of_Cruelty.java
@@ -0,0 +1,59 @@
+package magic.card;
+
+import java.util.Collection;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.event.MagicDiscardEvent;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+
+public class Triumph_of_Cruelty {
+    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+		@Override
+		public MagicEvent executeTrigger(
+				final MagicGame game,
+				final MagicPermanent permanent,
+				final MagicPlayer data) {
+			final MagicPlayer player = permanent.getController();
+			return (player == data) ?
+					new MagicEvent(
+	                        permanent,
+	                        player,
+	                        new Object[]{player},
+	                        this,
+	                        game.getOpponent(player) + " discards a card " +
+	                        "if you control the creature with the greatest " +
+	                        "power or tied for the greatest power.") :
+                MagicEvent.NONE;
+		}
+		@Override
+		public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+			final MagicPlayer player = event.getPlayer();
+			final Collection<MagicTarget> targets = game.filterTargets(
+					player,
+        			MagicTargetFilter.TARGET_CREATURE);
+        	MagicPermanent highest = MagicPermanent.NONE; 	
+        	for (final MagicTarget target : targets) {
+        		final MagicPermanent creature = (MagicPermanent)target;
+        		if (creature.getPower() > highest.getPower()) {
+        			highest = creature;
+        		}
+        	}
+        	if (highest.getController() == player) {
+        		game.addEvent(new MagicDiscardEvent(
+        				event.getSource(),
+        				game.getOpponent(player),
+        				1,
+        				false));
+        	}
+		}		
+    };
+}
