6ac0d40e016cccb6f8445fa5f6050983cf4148b8
beholder <unknown>
2012-06-18 09:48:55 +0200

    added Yew Spirit and Zealous Strike

---
+++ src/magic/card/Yew_Spirit.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTiming;
+
+public class Yew_Spirit {
+    public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicManaCost.TWO_GREEN_GREEN.getCondition()},
+            new MagicActivationHints(MagicTiming.Pump),
+            "Pump") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[] {new MagicPayManaCostEvent(
+                    source,
+                    source.getController(),
+                    MagicManaCost.TWO_GREEN_GREEN)};
+        }
+        @Override
+        public MagicEvent getPermanentEvent(
+                final MagicPermanent source,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    source,
+                    source.getController(),
+                    new Object[]{source},
+                    this,
+                    source + " gets +X/+X until end of turn, where X is its power.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            final MagicPermanent permanent = (MagicPermanent)data[0];
+            final int power = permanent.getPower();
+            game.doAction(new MagicChangeTurnPTAction(permanent,power,power));
+        }
+    };
+}
---
+++ src/magic/card/Zealous_Strike.java
@@ -0,0 +1,50 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeTurnPTAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSpellCardEvent;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicFirstStrikeTargetPicker;
+
+public class Zealous_Strike {
+    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    cardOnStack.getCard(),
+                    cardOnStack.getController(),
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicFirstStrikeTargetPicker.create(),
+                    new Object[]{cardOnStack},
+                    this,
+                    "Target creature$ gets +2/+2 and gains " +
+                    "first strike until end of turn.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                    game.doAction(new MagicSetAbilityAction(
+                            creature,
+                            MagicAbility.FirstStrike));
+                }
+            });
+        }
+    };
+}
