9e6704a6b3bc6b77bc99d4808067fa6b8f9a2b86
beholder <unknown>
2012-06-18 08:41:31 +0200

    added Voice of the Provinces and Vorstclaw

---
+++ src/magic/card/Voice_of_the_Provinces.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Voice_of_the_Provinces {  
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                    permanent,
+                    player,
+                    new Object[]{player},
+                    this,
+                    player + " puts a 1/1 white Human " +
+                    "creature token onto the battlefield.");
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(
+                    (MagicPlayer)data[0],
+                    TokenCardDefinitions.get("Human1")));
+        }        
+    };
+}
