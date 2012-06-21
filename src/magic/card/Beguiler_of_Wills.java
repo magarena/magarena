b3c6273a484e416fa9d7abcf1ee6a447944aabbf
beholder <unknown>
2012-06-21 07:29:54 +0200

    added Beguiler of Wills and Master Thief

---
+++ src/magic/card/Beguiler_of_Wills.java
@@ -0,0 +1,71 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.MagicType;
+import magic.model.action.MagicGainControlAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicExileTargetPicker;
+import magic.model.target.MagicTargetFilter;
+import magic.model.target.MagicTargetHint;
+
+public class Beguiler_of_Wills {
+	public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Control") {
+
+		@Override
+		public MagicEvent[] getCostEvent(final MagicSource source) {
+			return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+		}
+
+		@Override
+		public MagicEvent getPermanentEvent(
+				final MagicPermanent source,
+				final MagicPayedCost payedCost) {
+			final MagicPlayer player = source.getController();
+			final MagicTargetFilter targetFilter =
+                    new MagicTargetFilter.MagicPowerTargetFilter(
+                    MagicTargetFilter.TARGET_CREATURE,
+                    player.getNrOfPermanentsWithType(MagicType.Creature));
+            final MagicTargetChoice targetChoice = 
+                    new MagicTargetChoice(
+                    targetFilter,true,MagicTargetHint.Negative,"target creature to gain control of");
+			return new MagicEvent(
+                    source,
+                    player,
+                    targetChoice,
+                    MagicExileTargetPicker.create(),
+                    new Object[]{player},
+                    this,
+                    "Gain control of target creature$ with power less " +
+                    "than or equal to the number of creatures you control.");
+		}
+
+		@Override
+		public void executeEvent(
+				final MagicGame game,
+				final MagicEvent event,
+				final Object[] data,
+				final Object[] choiceResults) {
+		    event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    if (creature.getPower() <= event.getPlayer().getNrOfPermanentsWithType(MagicType.Creature)) {
+                        game.doAction(new MagicGainControlAction(event.getPlayer(),creature));
+                    }  
+                }
+            });
+		}
+	};
+}
---
+++ src/magic/card/Master_Thief.java
@@ -0,0 +1,74 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicAddStaticAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicRemoveStaticAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicExileTargetPicker;
+import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+
+public class Master_Thief {   
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+		@Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {			
+			return new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.TARGET_ARTIFACT,
+                    MagicExileTargetPicker.create(),
+                    new Object[]{player,permanent},
+                    this,
+                    "Gain control of target artifact$ " +
+                    "for as long as you control " + permanent + ".");
+		}
+		
+		@Override
+        public void executeEvent(
+                final MagicGame game, 
+                final MagicEvent event, 
+                final Object data[], 
+                final Object[] choiceResults) {
+		    event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+		        public void doAction(final MagicPermanent perm) {
+		            final MagicPermanent source = (MagicPermanent)event.getSource();
+		            final MagicTargetFilter filter = new MagicTargetFilter.MagicPermanentTargetFilter(perm);
+		            final MagicStatic S = new MagicStatic(MagicLayer.Control,filter) {
+		                final int you = source.getController().getIndex();
+		                @Override
+		                public MagicPlayer getController(
+		                        final MagicPermanent source, 
+		                        final MagicPermanent permanent, 
+		                        final MagicPlayer player) {
+		                    return source.getController();
+		                }
+		                @Override
+		                public boolean condition(
+		                        final MagicGame game,
+		                        final MagicPermanent source,
+		                        final MagicPermanent target) {
+		                    if (you != source.getController().getIndex()) {
+		                        //remove this static after the update
+		                        game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+		                        return false;
+		                    } else {
+		                        return true;
+		                    }
+		                }
+		            };
+		            game.doAction(new MagicAddStaticAction(source, S));
+		        };
+		    });
+        }
+    };
+}
