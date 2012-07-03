7174ebdba346a507e77f60d2d02bd8a289e25d14
melvin <melvinzhang@gmail.com>
2012-07-03 15:38:56 +0800

    added Goblin King

---
+++ src/magic/card/Goblin_King.java
@@ -0,0 +1,37 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.MagicAbility;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Goblin_King {
+    public static final MagicStatic S2 = new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_GOBLIN_YOU_CONTROL) {
+        @Override
+        public long getAbilityFlags(final MagicGame game,final MagicPermanent permanent,final long flags) {
+            return flags | MagicAbility.Mountainwalk.getMask();
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+    
+    public static final MagicStatic S1 = new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_GOBLIN_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicGame game,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+}
