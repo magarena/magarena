1c54e685faf5583d11a74eae154e3a74e2653330
melvin <melvinzhang@gmail.com>
2012-08-12 10:32:26 +0800

    added Intrepid Hero's ability

---
+++ src/magic/card/Intrepid_Hero.java
@@ -0,0 +1,54 @@
+package magic.card;
+
+import magic.model.MagicGame;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicDestroyAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicTapEvent;
+import magic.model.event.MagicTiming;
+import magic.model.target.MagicDestroyTargetPicker;
+
+public class Intrepid_Hero {
+
+    public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+            new MagicActivationHints(MagicTiming.Removal),
+            "Destroy") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicSource source) {
+            return new MagicEvent[]{new MagicTapEvent((MagicPermanent)source)};
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.NEG_TARGET_CREATURE_POWER_4_OR_MORE,
+                    new MagicDestroyTargetPicker(true),
+                    MagicEvent.NO_DATA,
+                    this,
+                    "Destroy target creature$ with power 4 or greater.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    if (creature.getPower() >= 4) {
+                        game.doAction(new MagicDestroyAction(creature));
+                    }
+                }
+            });
+        }
+    };
+}
