0669fb9e5b8a2b0c409022192af2b18a42f39de1
melvin <melvinzhang@gmail.com>
2012-08-12 10:25:52 +0800

    Smite the Monstrous refers to Reprisal's effect

--- src/magic/card/Smite_the_Monstrous.java
+++ src/magic/card/Smite_the_Monstrous.java
@@ -1,44 +1,5 @@
 package magic.card;
 
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDestroyTargetPicker;
-
 public class Smite_the_Monstrous {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
-            return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    MagicTargetChoice.NEG_TARGET_CREATURE_POWER_4_OR_MORE,
-                    new MagicDestroyTargetPicker(false),
-                    new Object[]{cardOnStack},
-                    this,
-                    "Destroy target creature$ with power 4 or greater. It can't be regenerated.");
-        }
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
-            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent permanent) {
-                    game.doAction(new MagicDestroyAction(permanent));
-                }
-            });
-        }
-    };
+    public static final Object S = Reprisal.S;
 }
