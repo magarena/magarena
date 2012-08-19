cc592b61369b23f9ae60112d73204e76d52d85ff
melvin <melvinzhang@gmail.com>
2012-08-19 16:30:14 +0800

    fixed code for Vacuumelt

--- src/magic/card/Vacuumelt.java
+++ src/magic/card/Vacuumelt.java
@@ -3,12 +3,14 @@ package magic.card;
 import magic.model.MagicGame;
 import magic.model.MagicLocationType;
 import magic.model.MagicPayedCost;
+import magic.model.MagicManaCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicMoveCardAction;
 import magic.model.action.MagicPermanentAction;
 import magic.model.action.MagicRemoveFromPlayAction;
 import magic.model.choice.MagicTargetChoice;
+import magic.model.choice.MagicKickerChoice;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicSpellCardEvent;
 import magic.model.stack.MagicCardOnStack;
@@ -24,7 +26,7 @@ public class Vacuumelt {
             return new MagicEvent(
                     cardOnStack.getCard(),
                     player,
-                    MagicTargetChoice.TARGET_CREATURE,
+                    new MagicKickerChoice(MagicTargetChoice.TARGET_CREATURE, MagicManaCost.TWO_BLUE, true, true),
                     MagicBounceTargetPicker.getInstance(),
                     new Object[]{cardOnStack},
                     this,
