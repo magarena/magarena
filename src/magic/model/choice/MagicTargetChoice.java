ade672847e4250ee067a8d60450d876aa21c7e6e
melvin <melvinzhang@gmail.com>
2012-08-26 21:37:18 +0800

    replace card code for Smite with effect property in card script

--- src/magic/model/choice/MagicTargetChoice.java
+++ src/magic/model/choice/MagicTargetChoice.java
@@ -493,6 +493,7 @@ public class MagicTargetChoice extends MagicChoice {
         factory.put("neg target creature or enchantment", NEG_TARGET_CREATURE_OR_ENCHANTMENT); 
         factory.put("neg target artifact or enchantment", NEG_TARGET_ARTIFACT_OR_ENCHANTMENT); 
         factory.put("neg target attacking creature", NEG_TARGET_ATTACKING_CREATURE); 
+        factory.put("neg target blocked creature", NEG_TARGET_BLOCKED_CREATURE); 
         factory.put("neg target Human creature", NEG_TARGET_HUMAN_CREATURE); 
         factory.put("neg target artifact or land", NEG_TARGET_ARTIFACT_OR_LAND);
     }
