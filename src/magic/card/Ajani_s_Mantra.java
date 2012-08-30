8df804fc3bef7dbeb4b3494255aaccb49cc61b7e
melvin <melvinzhang@gmail.com>
2012-08-30 20:56:23 +0800

    removed Object[] data

--- src/magic/card/Ajani_s_Mantra.java
+++ src/magic/card/Ajani_s_Mantra.java
@@ -23,7 +23,6 @@ public class Ajani_s_Mantra {
                                     MagicSimpleMayChoice.GAIN_LIFE,
                                     1,
                                     MagicSimpleMayChoice.DEFAULT_YES),
-                            new Object[]{player},
                             this,
                             player + " may$ gain 1 life.") :
                 MagicEvent.NONE;
@@ -35,7 +34,7 @@ public class Ajani_s_Mantra {
                 final Object data[],
                 final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],1));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
             }
         }        
     };
