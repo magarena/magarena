fd27058d11ed3a827adf5ac205da7480aee7fcb3
melvin <melvinzhang@gmail.com>
2012-08-30 20:18:12 +0800

    simplified using MagicLandfallTrigger

--- src/magic/card/Grazing_Gladehart.java
+++ src/magic/card/Grazing_Gladehart.java
@@ -7,15 +7,14 @@ import magic.model.action.MagicChangeLifeAction;
 import magic.model.choice.MagicMayChoice;
 import magic.model.choice.MagicSimpleMayChoice;
 import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
+import magic.model.trigger.MagicLandfallTrigger;
 
 public class Grazing_Gladehart {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+    public static final MagicLandfallTrigger T = new MagicLandfallTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent played) {
+        public MagicEvent getEvent(final MagicPermanent permanent) {
             final MagicPlayer player = permanent.getController();
-            return (player == played.getController() && played.isLand()) ?
-                new MagicEvent(
+            return new MagicEvent(
                     permanent,
                     player,
                     new MagicSimpleMayChoice(
@@ -23,10 +22,9 @@ public class Grazing_Gladehart {
                             MagicSimpleMayChoice.GAIN_LIFE,
                             2,
                             MagicSimpleMayChoice.DEFAULT_YES),
-                    new Object[]{player},
+                    MagicEvent.NO_DATA,
                     this,
-                    player + " may gain 2 life."):
-                MagicEvent.NONE;
+                    player + " may gain 2 life.");
         }
         
         @Override
@@ -36,7 +34,7 @@ public class Grazing_Gladehart {
                 final Object data[],
                 final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],2));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
             }
         }        
     };
