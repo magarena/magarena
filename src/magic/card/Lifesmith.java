a2820646e1c15b4d23cc15e6805c7eac65e13d18
melvin <melvinzhang@gmail.com>
2012-08-30 22:00:35 +0800

    removed Object[] data

--- src/magic/card/Lifesmith.java
+++ src/magic/card/Lifesmith.java
@@ -26,7 +26,6 @@ public class Lifesmith {
                         new MagicMayChoice(
                                 "You may pay {1}.",
                                 new MagicPayManaCostChoice(MagicManaCost.ONE)),
-                        new Object[]{player},
                         this,
                         player + " may$ pay {1}$. If you do, you gain 3 life."):
                 MagicEvent.NONE;
@@ -35,7 +34,7 @@ public class Lifesmith {
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],3));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
             }
         }
     };
