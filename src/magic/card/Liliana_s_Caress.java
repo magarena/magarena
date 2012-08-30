bd4e92bf941a627916d0ea51b99ce768158d3a13
melvin <melvinzhang@gmail.com>
2012-08-30 22:34:53 +0800

    removed Object[] data

--- src/magic/card/Liliana_s_Caress.java
+++ src/magic/card/Liliana_s_Caress.java
@@ -11,14 +11,13 @@ import magic.model.trigger.MagicWhenDiscardedTrigger;
 public class Liliana_s_Caress {
     public static final MagicWhenDiscardedTrigger T = new MagicWhenDiscardedTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCard data) {
-            final MagicPlayer otherController = data.getOwner();
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCard card) {
+            final MagicPlayer otherController = card.getOwner();
             final MagicPlayer player = permanent.getController();
             return (otherController != player) ?
                 new MagicEvent(
                         permanent,
-                        player,
-                        new Object[]{otherController},
+                        otherController,
                         this,
                         otherController + " loses 2 life."):
                 MagicEvent.NONE;
@@ -29,7 +28,7 @@ public class Liliana_s_Caress {
                 final MagicEvent event,
                 final Object data[],
                 final Object[] choiceResults) {
-            game.doAction(new MagicChangeLifeAction((MagicPlayer)data[0],-2));
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),-2));
         }
     };
 }
