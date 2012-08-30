752498a3848452e6a30953c0ec0bb3b1df30ef9f
melvin <melvinzhang@gmail.com>
2012-08-30 21:01:54 +0800

    removed Object[] data

--- src/magic/card/Chambered_Nautilus.java
+++ src/magic/card/Chambered_Nautilus.java
@@ -23,7 +23,6 @@ public class Chambered_Nautilus {
                                     MagicSimpleMayChoice.DRAW_CARDS,
                                     1,
                                     MagicSimpleMayChoice.DEFAULT_NONE),
-                            new Object[]{player},
                             this,
                             player + " may$ draw a card."):
                     MagicEvent.NONE;
@@ -36,7 +35,7 @@ public class Chambered_Nautilus {
                 final Object data[],
                 final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                game.doAction(new MagicDrawAction((MagicPlayer)data[0],1));
+                game.doAction(new MagicDrawAction(event.getPlayer(),1));
             }
         }
     };
