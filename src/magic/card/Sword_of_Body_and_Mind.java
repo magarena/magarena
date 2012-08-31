a7fdac83ff2db4146fda86701596643f0059e42e
melvin <melvinzhang@gmail.com>
2012-08-31 17:24:26 +0800

    removed player from Object[] data

--- src/magic/card/Sword_of_Body_and_Mind.java
+++ src/magic/card/Sword_of_Body_and_Mind.java
@@ -22,7 +22,7 @@ public class Sword_of_Body_and_Mind {
                 new MagicEvent(
                     permanent,
                     player,
-                    new Object[]{player,targetPlayer},
+                    new Object[]{targetPlayer},
                     this,
                     player + " puts a 2/2 green Wolf creature token onto the battlefield and " + targetPlayer +
                         " puts the top ten cards of his or her library into his or her graveyard."):
@@ -34,8 +34,8 @@ public class Sword_of_Body_and_Mind {
                 final MagicEvent event,
                 final Object data[],
                 final Object[] choiceResults) {
-            game.doAction(new MagicPlayTokenAction((MagicPlayer)data[0],TokenCardDefinitions.get("Wolf")));
-            game.doAction(new MagicMillLibraryAction((MagicPlayer)data[1],10));
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Wolf")));
+            game.doAction(new MagicMillLibraryAction((MagicPlayer)data[0],10));
         }
     };
 }
