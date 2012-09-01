9cb9b37ec4ab3f81b41475e5a2177775a3c02be5
melvin <melvinzhang@gmail.com>
2012-09-01 22:20:27 +0800

    removed player from Object[] data

--- src/magic/card/Harmonize.java
+++ src/magic/card/Harmonize.java
@@ -17,7 +17,7 @@ public class Harmonize {
             return new MagicEvent(
                     cardOnStack.getCard(),
                     player,
-                    new Object[]{cardOnStack,player},
+                    new Object[]{cardOnStack},
                     this,
                     "Draw three cards.");
         }
@@ -28,7 +28,7 @@ public class Harmonize {
                 final Object[] data,
                 final Object[] choiceResults) {
             game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
-            game.doAction(new MagicDrawAction((MagicPlayer)data[1],3));
+            game.doAction(new MagicDrawAction(event.getPlayer(),3));
         }
     };
 }
