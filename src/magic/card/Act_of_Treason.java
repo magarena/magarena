a3321648b07221046e6200113d2820f053bebf3c
melvin <melvinzhang@gmail.com>
2012-09-01 20:35:28 +0800

    removed player from Object[] data

--- src/magic/card/Act_of_Treason.java
+++ src/magic/card/Act_of_Treason.java
@@ -27,7 +27,8 @@ public class Act_of_Treason {
                     player,
                     MagicTargetChoice.NEG_TARGET_CREATURE,
                     MagicExileTargetPicker.create(),
-                    new Object[]{cardOnStack,player},this,
+                    new Object[]{cardOnStack},
+                    this,
                     "Gain control of target creature$ until end of turn. Untap that creature. " +
                     "It gains haste until end of turn.");
         }
@@ -37,7 +38,7 @@ public class Act_of_Treason {
             game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicGainControlAction((MagicPlayer)data[1],creature,MagicStatic.UntilEOT));
+                    game.doAction(new MagicGainControlAction(event.getPlayer(),creature,MagicStatic.UntilEOT));
                     game.doAction(new MagicUntapAction(creature));
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
                 }
