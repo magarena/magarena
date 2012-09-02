74c07d016b6fc3b36465f28512138d0206963e0b
melvin <melvinzhang@gmail.com>
2012-09-02 15:48:41 +0800

    removed Object[] data

--- src/magic/card/Murderous_Redcap.java
+++ src/magic/card/Murderous_Redcap.java
@@ -19,18 +19,17 @@ public class Murderous_Redcap {
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             final int power=permanent.getPower();
             return new MagicEvent(
-                    permanent,
-                    player,
-                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                    new MagicDamageTargetPicker(power),
-                    new Object[]{permanent},
-                    this,
-                    permanent + " deals damage equal to its power to target creature or player$.");
+                permanent,
+                player,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(power),
+                this,
+                permanent + " deals damage equal to its power to target creature or player$.");
         }
         
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
-            final MagicPermanent permanent=(MagicPermanent)data[0];
+            final MagicPermanent permanent=event.getPermanent();
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
                     final MagicDamage damage=new MagicDamage(permanent,target,permanent.getPower(),false);
