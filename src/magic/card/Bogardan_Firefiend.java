7a469a822302c4b61c42ec7489fa6ae893715d36
melvin <melvinzhang@gmail.com>
2012-09-02 14:57:45 +0800

    removed Object[] data

--- src/magic/card/Bogardan_Firefiend.java
+++ src/magic/card/Bogardan_Firefiend.java
@@ -18,13 +18,12 @@ public class Bogardan_Firefiend {
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
             return (MagicLocationType.Play == triggerData.fromLocation) ?
                 new MagicEvent(
-                        permanent,
-                        permanent.getController(),
-                        MagicTargetChoice.TARGET_CREATURE,
-                        new MagicDamageTargetPicker(2),
-                        new Object[]{permanent},
-                        this,
-                        permanent + " deals 2 damage to target creature$."):
+                    permanent,
+                    permanent.getController(),
+                    MagicTargetChoice.TARGET_CREATURE,
+                    new MagicDamageTargetPicker(2),
+                    this,
+                    permanent + " deals 2 damage to target creature$."):
                 MagicEvent.NONE;
         }
         
@@ -32,7 +31,7 @@ public class Bogardan_Firefiend {
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
-                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],creature,2,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),creature,2,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
