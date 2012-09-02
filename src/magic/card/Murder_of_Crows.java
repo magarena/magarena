4222225974d7d5efec9aafc0ed3c124507f761a3
melvin <melvinzhang@gmail.com>
2012-09-02 10:46:28 +0800

    removed Object[] data

--- src/magic/card/Murder_of_Crows.java
+++ src/magic/card/Murder_of_Crows.java
@@ -14,19 +14,18 @@ public class Murder_of_Crows {
     public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (permanent != otherPermanent &&
-                    otherPermanent.isCreature()) ?
+            return (permanent != otherPermanent && otherPermanent.isCreature()) ?
                 new MagicEvent(
                     permanent,
                     permanent.getController(),
                     new MagicSimpleMayChoice(
-                            "You may draw a card. If you do, discard a card.",
-                            MagicSimpleMayChoice.DRAW_CARDS,
-                            1,
-                            MagicSimpleMayChoice.DEFAULT_NONE),
-                        new Object[]{permanent,permanent.getController()},
-                        this,
-                        "You may$ draw a card. If you do, discard a card."):
+                        "You may draw a card. If you do, discard a card.",
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                    this,
+                    "You may$ draw a card. If you do, discard a card."):
                 MagicEvent.NONE;
         }
         @Override
@@ -36,9 +35,9 @@ public class Murder_of_Crows {
                 final Object data[],
                 final Object[] choiceResults) {
             if (MagicChoice.isYesChoice(choiceResults[0])) {
-                final MagicPlayer player=(MagicPlayer)data[1];
+                final MagicPlayer player=event.getPlayer();
                 game.doAction(new MagicDrawAction(player,1));
-                game.addEvent(new MagicDiscardEvent((MagicPermanent)data[0],player,1,false));
+                game.addEvent(new MagicDiscardEvent(event.getSource(),player,1,false));
             }
         }
     };
