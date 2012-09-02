6d3009c753a95e451ae749f179b54a3e2bb61b6e
melvin <melvinzhang@gmail.com>
2012-09-02 15:41:08 +0800

    removed Object[] data

--- src/magic/card/Close_Quarters.java
+++ src/magic/card/Close_Quarters.java
@@ -15,18 +15,17 @@ import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
 public class Close_Quarters {
     public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             final MagicPlayer player = permanent.getController();
-            return (player == data.getController() ) ?
-                    new MagicEvent(
-                            permanent,
-                            player,
-                            MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                            new MagicDamageTargetPicker(1),
-                            new Object[]{permanent},
-                            this,
-                            permanent + " deals 1 damage to target creature or player$."):
-                    MagicEvent.NONE;
+            return (player == attacker.getController()) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(1),
+                    this,
+                    permanent + " deals 1 damage to target creature or player$."):
+                MagicEvent.NONE;
         }
         
         @Override
@@ -37,7 +36,7 @@ public class Close_Quarters {
                 final Object[] choiceResults) {
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage = new MagicDamage((MagicPermanent)data[0],target,1,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,1,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
