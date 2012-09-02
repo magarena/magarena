6ddef00bdebb576fe82efee0c723956aa4b40df6
melvin <melvinzhang@gmail.com>
2012-09-02 10:08:35 +0800

    simplify using MagicWhenDiesTrigger

--- src/magic/card/Perilous_Myr.java
+++ src/magic/card/Perilous_Myr.java
@@ -11,30 +11,28 @@ import magic.model.event.MagicEvent;
 import magic.model.target.MagicDamageTargetPicker;
 import magic.model.target.MagicTarget;
 import magic.model.trigger.MagicGraveyardTriggerData;
-import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
+import magic.model.trigger.MagicWhenDiesTrigger;
 
 public class Perilous_Myr {
-    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+    public static final MagicWhenDiesTrigger T = new MagicWhenDiesTrigger() {
 
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
-            return (MagicLocationType.Play==triggerData.fromLocation) ?
-                new MagicEvent(
-                        permanent,
-                        permanent.getController(),
-                        MagicTargetChoice.TARGET_CREATURE_OR_PLAYER,
-                        new MagicDamageTargetPicker(2),
-                        new Object[]{permanent},
-                        this,
-                        permanent + " deals 2 damage to target creature or player$."):
-                MagicEvent.NONE;
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                permanent.getController(),
+                MagicTargetChoice.TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(2),
+                this,
+                permanent + " deals 2 damage to target creature or player$."
+            );
         }
         
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object data[],final Object[] choiceResults) {
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage((MagicPermanent)data[0],target,2,false);
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,2,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
