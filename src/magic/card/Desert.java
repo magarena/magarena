07584c67638a0496bbfa22da43034d8df10671e4
melvin <melvinzhang@gmail.com>
2012-09-03 21:33:54 +0800

    removed Object[] data

--- src/magic/card/Desert.java
+++ src/magic/card/Desert.java
@@ -37,7 +37,6 @@ public class Desert {
                     source.getController(),
                     MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
                     new MagicDamageTargetPicker(1),
-                    new Object[]{source},
                     this,
                     source + " deals 1 damage to target attacking creature.");
         }
@@ -46,7 +45,7 @@ public class Desert {
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage = new MagicDamage((MagicSource)data[0],target,1,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,1,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
