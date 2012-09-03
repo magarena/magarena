ccc5ad7dfee4c4b63f1b2723b6399ba92e2bc2be
melvin <melvinzhang@gmail.com>
2012-09-03 20:51:09 +0800

    removed Object[] data

--- src/magic/card/Barrage_Ogre.java
+++ src/magic/card/Barrage_Ogre.java
@@ -42,7 +42,6 @@ public class Barrage_Ogre {
                     source.getController(),
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(2),
-                    new Object[]{source},
                     this,
                     source + " deals 2 damage to target creature or player$.");
         }
@@ -54,7 +53,7 @@ public class Barrage_Ogre {
                 final Object[] choiceResults) {
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage = new MagicDamage((MagicSource)data[0],target,2,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,2,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
