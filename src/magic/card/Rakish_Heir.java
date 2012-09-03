e5fe5f92fb077c02f43f620d973bb907ec53b4c1
melvin <melvinzhang@gmail.com>
2012-09-03 21:55:23 +0800

    renamed source to dmgSource

--- src/magic/card/Rakish_Heir.java
+++ src/magic/card/Rakish_Heir.java
@@ -17,17 +17,17 @@ public class Rakish_Heir {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicPlayer player = permanent.getController();
-            final MagicSource source = damage.getSource();
+            final MagicSource dmgSource = damage.getSource();
             return (damage.isCombat() && 
                     damage.getTarget().isPlayer() &&
-                    source.getController() == player &&
-                    ((MagicPermanent)source).hasSubType(MagicSubType.Vampire)) ?
+                    dmgSource.getController() == player &&
+                    ((MagicPermanent)dmgSource).hasSubType(MagicSubType.Vampire)) ?
                 new MagicEvent(
                         permanent,
                         player,
-                        new Object[]{source},
+                        new Object[]{dmgSource},
                         this,
-                        "Put a +1/+1 counter on " + source + "."):
+                        "Put a +1/+1 counter on " + dmgSource + "."):
                 MagicEvent.NONE;
         }
         @Override
