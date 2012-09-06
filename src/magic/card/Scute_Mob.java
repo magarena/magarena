0c384602c8341db57d95907392883b23a76b657a
melvin <melvinzhang@gmail.com>
2012-09-06 22:14:54 +0800

    removed Object[] data

--- src/magic/card/Scute_Mob.java
+++ src/magic/card/Scute_Mob.java
@@ -12,14 +12,13 @@ import magic.model.trigger.MagicAtUpkeepTrigger;
 public class Scute_Mob {
     public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             final MagicPlayer player = permanent.getController();
-            return (player == data &&
+            return (player == upkeepPlayer &&
                     player.getNrOfPermanentsWithType(MagicType.Land) >= 5) ?
                 new MagicEvent(
                         permanent,
                         player,
-                        new Object[]{permanent},
                         this,
                         "Put four +1/+1 counters on " + permanent + ".") :
                 MagicEvent.NONE;
@@ -31,7 +30,7 @@ public class Scute_Mob {
                 final Object data[],
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeCountersAction(
-                    (MagicPermanent)data[0],
+                    event.getPermanent(),
                     MagicCounterType.PlusOne,
                     4,
                     true));
