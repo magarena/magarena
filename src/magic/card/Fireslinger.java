627e3057ffa7a3a2e3d8c2c860bb2bfcd5525f84
melvin <melvinzhang@gmail.com>
2012-09-06 22:31:06 +0800

    removed Object[] data

--- src/magic/card/Fireslinger.java
+++ src/magic/card/Fireslinger.java
@@ -37,21 +37,20 @@ public class Fireslinger {
                     player,
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(1),
-                    new Object[]{source,player},
                     this,
                     source + " deals 1 damage to target creature or player$ and 1 damage to you.");
         }
 
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] data,final Object[] choiceResults) {
-            final MagicSource source=(MagicSource)data[0];
+            final MagicSource source=event.getSource();
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
                     final MagicDamage damage1=new MagicDamage(source,target,1,false);
                     game.doAction(new MagicDealDamageAction(damage1));
                 }
             });
-            final MagicDamage damage2=new MagicDamage(source,(MagicTarget)data[1],1,false);
+            final MagicDamage damage2=new MagicDamage(source,event.getPlayer(),1,false);
             game.doAction(new MagicDealDamageAction(damage2));
         }
     };
