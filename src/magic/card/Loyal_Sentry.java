4ef1780ef2458113af0c51b9a87eb0f66d06160a
melvin <melvinzhang@gmail.com>
2012-09-08 11:32:09 +0800

    removed permanent from Object[] data

--- src/magic/card/Loyal_Sentry.java
+++ src/magic/card/Loyal_Sentry.java
@@ -9,13 +9,13 @@ import magic.model.trigger.MagicWhenBlocksTrigger;
 public class Loyal_Sentry {
     public static final MagicWhenBlocksTrigger T = new MagicWhenBlocksTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent defender) {
             final MagicPermanent blocked = permanent.getBlockedCreature();
-            return (permanent == data && blocked.isValid()) ?
+            return (permanent == defender && blocked.isValid()) ?
                 new MagicEvent(
                     permanent,
                     permanent.getController(),
-                    new Object[]{blocked,permanent},
+                    new Object[]{blocked},
                     this,
                     "Destroy " + blocked + ". Destroy " + permanent + "."):
                 MagicEvent.NONE;
@@ -27,7 +27,7 @@ public class Loyal_Sentry {
                 final Object data[],
                 final Object[] choiceResults) {
             game.doAction(new MagicDestroyAction((MagicPermanent)data[0]));
-            game.doAction(new MagicDestroyAction((MagicPermanent)data[1]));
+            game.doAction(new MagicDestroyAction(event.getPermanent()));
         }
     };
 }
