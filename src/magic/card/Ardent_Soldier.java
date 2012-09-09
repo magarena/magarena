5ff12154ea133d5c42336c2dee794a3ca83b8c92
melvin <melvinzhang@gmail.com>
2012-09-09 14:47:48 +0800

    simplify Ardent Soldier using kicker in card script

--- src/magic/card/Ardent_Soldier.java
+++ src/magic/card/Ardent_Soldier.java
@@ -1,5 +1,6 @@
 package magic.card;
 
+import magic.model.MagicAbility;
 import magic.model.MagicCard;
 import magic.model.MagicCounterType;
 import magic.model.MagicGame;
@@ -8,45 +9,26 @@ import magic.model.MagicPayedCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicPlayCardFromStackAction;
-import magic.model.choice.MagicKickerChoice;
+import magic.model.action.MagicSetAbilityAction;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
 import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
+import magic.model.mstatic.MagicStatic;
 
-public class Ardent_Soldier {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+public class Ardent_Soldier {                 
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
         @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
-            final MagicCard card = cardOnStack.getCard();
-            return new MagicEvent(
-                    card,
-                    player,
-                    new MagicKickerChoice(MagicManaCost.TWO,false),
-                    new Object[]{cardOnStack,player},
-                    this,
-                    "$Play " + card + ". If " + card + " was kicked$, " +
-                    "it enters the battlefield with a +1/+1 counter on it.");
-        }
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-            final int kickerCount = (Integer)choiceResults[1];
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-            final MagicPlayCardFromStackAction action = new MagicPlayCardFromStackAction(cardOnStack);
-            game.doAction(action);
-            if (kickerCount > 0) {
-                final MagicPermanent permanent = action.getPermanent();
+        public MagicEvent executeTrigger(
+            final MagicGame game,
+            final MagicPermanent permanent,
+            final MagicPlayer player) {   
+            if (permanent.isKicked()) {
                 game.doAction(new MagicChangeCountersAction(
                         permanent,
                         MagicCounterType.PlusOne,
                         1,
                         true));
             }
+            return MagicEvent.NONE;
         }
     };
 }
