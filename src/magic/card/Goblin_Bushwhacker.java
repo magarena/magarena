bf017e3c15c204010a47b46e6a202abe914462ad
melvin <melvinzhang@gmail.com>
2012-09-09 15:07:35 +0800

    simplify Goblin Bushwhacker using kicker in card script

--- src/magic/card/Goblin_Bushwhacker.java
+++ src/magic/card/Goblin_Bushwhacker.java
@@ -8,23 +8,31 @@ import magic.model.MagicPayedCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicPlayCardFromStackAction;
-import magic.model.action.MagicPutItemOnStackAction;
 import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicKickerChoice;
 import magic.model.event.MagicEvent;
-import magic.model.event.MagicEventAction;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.stack.MagicTriggerOnStack;
 import magic.model.target.MagicTarget;
 import magic.model.target.MagicTargetFilter;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
 
 import java.util.Collection;
 
 public class Goblin_Bushwhacker {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+            final MagicGame game,
+            final MagicPermanent permanent,
+            final MagicPlayer player) {   
+            return permanent.isKicked() ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    this,
+                    "Creatures " + player +
+                    " controls get +1/+0 and gain haste until end of turn."):
+                MagicEvent.NONE;
+        }
                         
-    private static final MagicEventAction KICKED = new MagicEventAction() {
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -38,44 +46,6 @@ public class Goblin_Bushwhacker {
                 game.doAction(new MagicChangeTurnPTAction(creature,1,0));
                 game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
             }            
-        }
-    };
-
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicCard card = cardOnStack.getCard();
-            return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
-                    new MagicKickerChoice(MagicManaCost.RED,false),
-                    new Object[]{cardOnStack},
-                    this,
-                    "$Play " + card + ". If " + card + " was kicked$, " + 
-                    "Creatures " + cardOnStack.getController() +
-                    " controls get +1/+0 and gain haste until end of turn.");
-        }
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event,
-                final Object[] data,
-                final Object[] choiceResults) {
-            final int kickerCount=(Integer)choiceResults[1];
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-            final MagicPlayCardFromStackAction action=new MagicPlayCardFromStackAction(cardOnStack);
-            game.doAction(action);
-            if (kickerCount>0) {
-                final MagicPermanent permanent=action.getPermanent();
-                final MagicPlayer player=permanent.getController();
-                final MagicEvent triggerEvent=new MagicEvent(
-                        permanent,
-                        player,
-                        KICKED,
-                        "Creatures " + player +
-                        " controls get +1/+0 and gain haste until end of turn.");
-                game.doAction(new MagicPutItemOnStackAction(new MagicTriggerOnStack(triggerEvent)));
-            }
         }
     };
 }
