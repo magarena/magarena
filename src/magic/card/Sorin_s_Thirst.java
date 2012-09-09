eac2b63a8bd66416b6389a0dbbcfc8481b83a4c9
melvin <melvinzhang@gmail.com>
2012-09-09 10:59:41 +0800

    simplifiy code

--- src/magic/card/Sorin_s_Thirst.java
+++ src/magic/card/Sorin_s_Thirst.java
@@ -1,7 +1,5 @@
 package magic.card;
 
-import magic.model.event.MagicSpellCardEvent;
-
 public class Sorin_s_Thirst {
-    public static final MagicSpellCardEvent S = Douse_in_Gloom.S;
+    public static final Object S = Douse_in_Gloom.S;
 }
