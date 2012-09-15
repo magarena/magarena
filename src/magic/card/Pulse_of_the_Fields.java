f59ae86640a1ed20425b92a5355536df68e1fe13
melvin <melvinzhang@gmail.com>
2012-09-15 20:48:22 +0800

    remove Object[] data

--- src/magic/card/Pulse_of_the_Fields.java
+++ src/magic/card/Pulse_of_the_Fields.java
@@ -6,7 +6,7 @@ import magic.model.MagicLocationType;
 import magic.model.MagicPayedCost;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicChangeCardDestinationAction;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicSpellCardEvent;
 import magic.model.stack.MagicCardOnStack;
@@ -15,16 +15,11 @@ public class Pulse_of_the_Fields {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
-            final MagicCard card = cardOnStack.getCard();
             return new MagicEvent(
-                    card,
-                    player,
-                    new Object[]{cardOnStack},
+                    cardOnStack,
                     this,
-                    player + " gains 4 life. Then if your opponent " +
-                    "has more life than you, return " + 
-                    card + " to its owner's hand.");
+                    cardOnStack.getController() + " gains 4 life. Then if your opponent " +
+                    "has more life than you, return " + cardOnStack + " to its owner's hand.");
         }
         @Override
         public void executeEvent(
@@ -32,12 +27,12 @@ public class Pulse_of_the_Fields {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
             final MagicPlayer player = event.getPlayer();
             game.doAction(new MagicChangeLifeAction(player,4));
             final boolean more = player.getOpponent().getLife() > player.getLife();
-            final MagicLocationType location = more ? MagicLocationType.OwnersHand : MagicLocationType.Graveyard;
-            game.doAction(new MagicMoveCardAction(cardOnStack.getCard(),MagicLocationType.Stack,location));
+            if (more) {
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersHand));
+            }
         }
     };
 }
