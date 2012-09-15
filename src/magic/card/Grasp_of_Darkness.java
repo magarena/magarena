c03832efbe490509bb892337b020b914ee92a3d7
melvin <melvinzhang@gmail.com>
2012-09-15 22:26:07 +0800

    remove Object[] data

--- src/magic/card/Grasp_of_Darkness.java
+++ src/magic/card/Grasp_of_Darkness.java
@@ -17,11 +17,9 @@ public class Grasp_of_Darkness {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_CREATURE,
                     new MagicWeakenTargetPicker(4,4),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ gets -4/-4 until end of turn.");
         }
@@ -31,7 +29,6 @@ public class Grasp_of_Darkness {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeTurnPTAction(creature,-4,-4));
