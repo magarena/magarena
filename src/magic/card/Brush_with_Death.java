55f2641c790742d17e22242ed5cb1f2e41d8d9cb
melvin <melvinzhang@gmail.com>
2012-09-15 13:47:38 +0800

    removed Object[]

--- src/magic/card/Brush_with_Death.java
+++ src/magic/card/Brush_with_Death.java
@@ -8,6 +8,7 @@ import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeLifeAction;
 import magic.model.action.MagicMoveCardAction;
 import magic.model.action.MagicPlayerAction;
+import magic.model.action.MagicChangeCardDestinationAction;
 import magic.model.choice.MagicBuybackChoice;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.event.MagicEvent;
@@ -18,16 +19,13 @@ public class Brush_with_Death {
     public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
+                    cardOnStack,
                     new MagicBuybackChoice(
                             MagicTargetChoice.TARGET_OPPONENT,
                             MagicManaCost.TWO_BLACK_BLACK),
-                    new Object[]{cardOnStack},
                     this,
-                    "Target opponent$ loses 2 life and " + player + " gains " +
+                    "Target opponent$ loses 2 life and " + cardOnStack.getController() + " gains " +
                     "2 life. If the buyback cost was payed$, return " +
                     cardOnStack + " to its owner's hand as it resolves.");
         }
@@ -38,28 +36,15 @@ public class Brush_with_Death {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-            final boolean hasTarget = event.processTargetPlayer(
-                    game,
-                    choiceResults,
-                    0,
-                    new MagicPlayerAction() {
+            final boolean hasTarget = event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
                     game.doAction(new MagicChangeLifeAction(player,-2));
                     game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
-                    if (MagicBuybackChoice.isYesChoice(choiceResults[1])) {
-                        game.doAction(new MagicMoveCardAction(
-                                cardOnStack.getCard(),
-                                MagicLocationType.Stack,
-                                MagicLocationType.OwnersHand));
-                    } else {
-                        game.doAction(new MagicMoveCardAction(cardOnStack));
-                    }
                 }
             });
-            if (!hasTarget) {
-                game.doAction(new MagicMoveCardAction(cardOnStack));
-            }
+            if (MagicBuybackChoice.isYesChoice(choiceResults[1])) {
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+            } 
         }
     };
 }
