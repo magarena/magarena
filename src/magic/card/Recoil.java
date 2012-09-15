ff2734b4116014bf5329e90764de60ed745c8658
melvin <melvinzhang@gmail.com>
2012-09-15 18:29:54 +0800

    remove Object[] data

--- src/magic/card/Recoil.java
+++ src/magic/card/Recoil.java
@@ -20,11 +20,9 @@ public class Recoil {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.TARGET_PERMANENT,
                     MagicBounceTargetPicker.getInstance(),
-                    new Object[]{cardOnStack},
                     this,
                     "Return target permanent$ to its owner's hand. Then that player discards a card.");
         }
@@ -35,13 +33,10 @@ public class Recoil {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent permanent) {
-                    final MagicPlayer owner=permanent.getCard().getOwner();
                     game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
-                    game.addEvent(new MagicDiscardEvent(cardOnStack.getCard(),owner,1,false));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),permanent.getOwner(),1,false));
                 }
             });
         }
