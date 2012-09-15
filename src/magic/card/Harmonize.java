fefdb513481763011ad6cfdea412ca3f7b8761b0
melvin <melvinzhang@gmail.com>
2012-09-15 14:01:39 +0800

    removed Object[] data

--- src/magic/card/Harmonize.java
+++ src/magic/card/Harmonize.java
@@ -13,11 +13,8 @@ public class Harmonize {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player=cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    new Object[]{cardOnStack},
+                    cardOnStack,
                     this,
                     "Draw three cards.");
         }
@@ -27,7 +24,6 @@ public class Harmonize {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             game.doAction(new MagicDrawAction(event.getPlayer(),3));
         }
     };
