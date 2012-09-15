325fcb0a659addc347f276de69264ddd41f1b0c1
melvin <melvinzhang@gmail.com>
2012-09-15 21:51:23 +0800

    remove Object[] data

--- src/magic/card/Evincar_s_Justice.java
+++ src/magic/card/Evincar_s_Justice.java
@@ -10,7 +10,7 @@ import magic.model.MagicPayedCost;
 import magic.model.MagicPlayer;
 import magic.model.MagicSource;
 import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicChangeCardDestinationAction;
 import magic.model.choice.MagicBuybackChoice;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicSpellCardEvent;
@@ -23,12 +23,10 @@ public class Evincar_s_Justice {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     new MagicBuybackChoice(MagicManaCost.THREE),
-                    new Object[]{cardOnStack},
                     this,
-                    cardOnStack.getCard() + " deals 2 damage to each creature " +
+                    cardOnStack + " deals 2 damage to each creature " +
                     "and each player. If the buyback cost was payed$, " +
                     "return " + cardOnStack + " to its owner's hand as it resolves.");
         }
@@ -39,26 +37,19 @@ public class Evincar_s_Justice {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-            final MagicSource source = cardOnStack.getCard();
             final Collection<MagicTarget> targets = 
-                game.filterTargets(cardOnStack.getController(),MagicTargetFilter.TARGET_CREATURE);
+                game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
             for (final MagicTarget target : targets) {
-                final MagicDamage damage = new MagicDamage(source,target,2,false);
+                final MagicDamage damage = new MagicDamage(event.getSource(),target,2,false);
                 game.doAction(new MagicDealDamageAction(damage));
             }
             for (final MagicPlayer player : game.getPlayers()) {
-                final MagicDamage damage = new MagicDamage(source,player,2,false);
+                final MagicDamage damage = new MagicDamage(event.getSource(),player,2,false);
                 game.doAction(new MagicDealDamageAction(damage));
             }
             if (MagicBuybackChoice.isYesChoice(choiceResults[1])) {
-                game.doAction(new MagicMoveCardAction(
-                        cardOnStack.getCard(),
-                        MagicLocationType.Stack,
-                        MagicLocationType.OwnersHand));
-            } else {
-                game.doAction(new MagicMoveCardAction(cardOnStack));
-            }
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+            } 
         }
     };
 }
