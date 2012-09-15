c1df8eca93f229499cbeca8739ca1ace2a385f2d
melvin <melvinzhang@gmail.com>
2012-09-15 18:47:45 +0800

    remove Object[] data

--- src/magic/card/Rally_the_Forces.java
+++ src/magic/card/Rally_the_Forces.java
@@ -20,11 +20,8 @@ public class Rally_the_Forces {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player=cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
-                    new Object[]{cardOnStack},
+                    cardOnStack,
                     this,
                     "Attacking creatures get +1/+0 and gain first strike until end of turn.");
         }
@@ -34,10 +31,8 @@ public class Rally_the_Forces {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];            
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             final Collection<MagicTarget> targets=
-                game.filterTargets(cardOnStack.getController(),MagicTargetFilter.TARGET_ATTACKING_CREATURE);
+                game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_ATTACKING_CREATURE);
             for (final MagicTarget target : targets) {
                 final MagicPermanent creature=(MagicPermanent)target;
                 game.doAction(new MagicChangeTurnPTAction(creature,1,0));
