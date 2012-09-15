bd3bdd8f9acc5fcd9ebc4d70ed51b543e2c3f793
melvin <melvinzhang@gmail.com>
2012-09-15 21:20:00 +0800

    remove Object[] data

--- src/magic/card/Withstand_Death.java
+++ src/magic/card/Withstand_Death.java
@@ -18,13 +18,10 @@ public class Withstand_Death {
     public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicIndestructibleTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ is indestructible turn.");
         }
@@ -35,7 +32,6 @@ public class Withstand_Death {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Indestructible));
