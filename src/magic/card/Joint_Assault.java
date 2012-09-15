f4adcbf83a3626fdd633ec978bb170aa867cfdbe
melvin <melvinzhang@gmail.com>
2012-09-15 21:23:34 +0800

    remove Object[] data

--- src/magic/card/Joint_Assault.java
+++ src/magic/card/Joint_Assault.java
@@ -19,11 +19,9 @@ public class Joint_Assault {
                 final MagicCardOnStack cardOnStack,
                 final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicPumpTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ gets +2/+2 until end of turn. " +
                     "If it's paired with a creature, that creature " +
@@ -35,7 +33,6 @@ public class Joint_Assault {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeTurnPTAction(creature,2,2));
