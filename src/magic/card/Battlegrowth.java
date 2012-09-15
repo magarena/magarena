1e90459a39ceb20b7b6f58c732d4d7908273dbd9
melvin <melvinzhang@gmail.com>
2012-09-15 13:25:44 +0800

    removed Object[]

--- src/magic/card/Battlegrowth.java
+++ src/magic/card/Battlegrowth.java
@@ -18,13 +18,10 @@ public class Battlegrowth {
     public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicPumpTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Put a +1/+1 counter on target creature$.");
         }
@@ -34,7 +31,6 @@ public class Battlegrowth {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeCountersAction(
