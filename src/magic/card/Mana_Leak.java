16e2d3e2ab3d97fdf0a52f5825cc1bf688e3ea09
melvin <melvinzhang@gmail.com>
2012-09-15 22:24:49 +0800

    remove Object[] data

--- src/magic/card/Mana_Leak.java
+++ src/magic/card/Mana_Leak.java
@@ -16,10 +16,8 @@ public class Mana_Leak {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_SPELL,
-                    new Object[]{cardOnStack},
                     this,
                     "Counter target spell$ unless its controller pays {3}.");
         }
@@ -29,11 +27,9 @@ public class Mana_Leak {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             event.processTargetCardOnStack(game,choiceResults,0,new MagicCardOnStackAction() {
                 public void doAction(final MagicCardOnStack targetSpell) {
-                    game.addEvent(new MagicCounterUnlessEvent(cardOnStack.getCard(),targetSpell,MagicManaCost.THREE));
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.THREE));
                 }
             });
         }
