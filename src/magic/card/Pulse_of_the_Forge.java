7baacb5dfb393291cd187db8ddd86748d7be96b9
melvin <melvinzhang@gmail.com>
2012-09-15 22:09:09 +0800

    remove Object[] data

--- src/magic/card/Pulse_of_the_Forge.java
+++ src/magic/card/Pulse_of_the_Forge.java
@@ -7,7 +7,7 @@ import magic.model.MagicLocationType;
 import magic.model.MagicPayedCost;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicChangeCardDestinationAction;
 import magic.model.action.MagicPlayerAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.event.MagicEvent;
@@ -19,17 +19,14 @@ public class Pulse_of_the_Forge {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicCard card = cardOnStack.getCard();
             return new MagicEvent(
-                    card,
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_PLAYER,
                     new MagicDamageTargetPicker(4),
-                    new Object[]{cardOnStack},
                     this,
-                    card + " deals 4 damage to target player$." +
+                    cardOnStack + " deals 4 damage to target player$." +
                     "Then if that player has more life than you, " +
-                    "return " + card + " to its owner's hand.");
+                    "return " + cardOnStack + " to its owner's hand.");
         }
         @Override
         public void executeEvent(
@@ -37,14 +34,14 @@ public class Pulse_of_the_Forge {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];        
             event.processTargetPlayer(game,choiceResults,0,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer targetPlayer) {
-                    final MagicDamage damage = new MagicDamage(cardOnStack.getCard(),targetPlayer,4,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),targetPlayer,4,false);
                     game.doAction(new MagicDealDamageAction(damage));
-                    final boolean more = targetPlayer.getLife() > cardOnStack.getController().getLife();
-                    final MagicLocationType location = more ? MagicLocationType.OwnersHand : MagicLocationType.Graveyard;
-                    game.doAction(new MagicMoveCardAction(cardOnStack.getCard(),MagicLocationType.Stack,location));
+                    final boolean more = targetPlayer.getLife() > event.getPlayer().getLife();
+                    if (more) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersHand));
+                    }
                 }
             });
             
