83da38badbad00013721c6412e25a511e8fb325b
melvin <melvinzhang@gmail.com>
2012-09-15 20:58:59 +0800

    removed Object[] data

--- src/magic/card/Wretched_Banquet.java
+++ src/magic/card/Wretched_Banquet.java
@@ -23,11 +23,9 @@ public class Wretched_Banquet {
                 final MagicCardOnStack cardOnStack,
                 final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_CREATURE,
                     new MagicDestroyTargetPicker(false),
-                    new Object[]{cardOnStack},
                     this,
                     "Destroy target creature$ if it has the least power or is " +
                     "tied for least power among creatures on the battlefield.");
@@ -38,11 +36,10 @@ public class Wretched_Banquet {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     final Collection<MagicTarget> targets = game.filterTargets(
-                            game.getPlayer(0),
+                            event.getPlayer(),
                             MagicTargetFilter.TARGET_CREATURE);
                     final int power = creature.getPower();
                     boolean least = true;
