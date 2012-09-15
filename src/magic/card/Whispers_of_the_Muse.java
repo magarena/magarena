29c59701d43d3007de80d2f9af62a8f4d445bc8b
melvin <melvinzhang@gmail.com>
2012-09-15 19:56:15 +0800

    remove Object[] data

--- src/magic/card/Whispers_of_the_Muse.java
+++ src/magic/card/Whispers_of_the_Muse.java
@@ -7,7 +7,7 @@ import magic.model.MagicManaCost;
 import magic.model.MagicPayedCost;
 import magic.model.MagicPlayer;
 import magic.model.action.MagicDrawAction;
-import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicChangeCardDestinationAction;
 import magic.model.choice.MagicBuybackChoice;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicSpellCardEvent;
@@ -17,16 +17,12 @@ public class Whispers_of_the_Muse {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
-            final MagicCard card = cardOnStack.getCard();
-            return new MagicEvent(
-                    card,
-                    player,
+           return new MagicEvent(
+                    cardOnStack,
                     new MagicBuybackChoice(MagicManaCost.FIVE),
-                    new Object[]{cardOnStack},
                     this,
-                    player + " $draws a card. If the buyback cost was payed$, " +
-                    "return " + card + " to its owner's hand as it resolves.");
+                    cardOnStack.getController() + " $draws a card. If the buyback cost was payed$, " +
+                    "return " + cardOnStack + " to its owner's hand as it resolves.");
         }
         @Override
         public void executeEvent(
@@ -34,16 +30,10 @@ public class Whispers_of_the_Muse {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
             game.doAction(new MagicDrawAction(event.getPlayer(),1));
             if (MagicBuybackChoice.isYesChoice(choiceResults[1])) {
-                game.doAction(new MagicMoveCardAction(
-                        cardOnStack.getCard(),
-                        MagicLocationType.Stack,
-                        MagicLocationType.OwnersHand));
-            } else {
-                game.doAction(new MagicMoveCardAction(cardOnStack));
-            }
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+            } 
         }
     };
 }
