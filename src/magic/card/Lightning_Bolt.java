cd33a0a415c84aed20baa998cee8f96665225d6e
melvin <melvinzhang@gmail.com>
2012-09-15 22:14:42 +0800

    remove Object[] data

--- src/magic/card/Lightning_Bolt.java
+++ src/magic/card/Lightning_Bolt.java
@@ -18,13 +18,11 @@ public class Lightning_Bolt {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(3),
-                    new Object[]{cardOnStack},
                     this,
-                    cardOnStack.getCard() + " deals 3 damage to target creature or player$.");
+                    cardOnStack + " deals 3 damage to target creature or player$.");
         }
         @Override
         public void executeEvent(
@@ -32,11 +30,9 @@ public class Lightning_Bolt {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage(cardOnStack.getCard(),target,3,false);
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,3,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
