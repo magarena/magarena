f8a800bdff8cbfbc8828994472f14c8d94183e5b
melvin <melvinzhang@gmail.com>
2012-09-15 21:20:53 +0800

    remove Object[] data

--- src/magic/card/Incinerate.java
+++ src/magic/card/Incinerate.java
@@ -19,16 +19,12 @@ public class Incinerate {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player=cardOnStack.getController();
-            final MagicCard card=cardOnStack.getCard();
             return new MagicEvent(
-                    card,
-                    player,
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(3,true),
-                    new Object[]{cardOnStack},
                     this,
-                    card + " deals 3 damage to target creature or player$. " + 
+                    cardOnStack + " deals 3 damage to target creature or player$. " + 
                     "A creature dealt damage this way can't be regenerated this turn.");
         }
         @Override
@@ -37,11 +33,9 @@ public class Incinerate {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack=(MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage(cardOnStack.getCard(),target,3,false);
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,3,false);
                     damage.setNoRegeneration();
                     game.doAction(new MagicDealDamageAction(damage));
                 }
