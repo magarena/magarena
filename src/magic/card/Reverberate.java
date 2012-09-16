d00c0e8ecad67f76bf3a2bfd29ee8fe36b649232
melvin <melvinzhang@gmail.com>
2012-09-16 09:39:02 +0800

    remove Object[] data

--- src/magic/card/Reverberate.java
+++ src/magic/card/Reverberate.java
@@ -15,12 +15,9 @@ public class Reverberate {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player = cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
+                    cardOnStack,
                     MagicTargetChoice.TARGET_INSTANT_OR_SORCERY_SPELL,
-                    new Object[]{cardOnStack},
                     this,
                     "Copy target instant or sorcery spell$. You may choose new targets for the copy.");
         }
@@ -30,7 +27,6 @@ public class Reverberate {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetCardOnStack(game,choiceResults,0,new MagicCardOnStackAction() {
                 public void doAction(final MagicCardOnStack targetSpell) {
                     game.doAction(new MagicCopyCardOnStackAction(event.getPlayer(),targetSpell));
