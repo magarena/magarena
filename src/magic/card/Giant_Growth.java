27fff719929c2e1b9cd100b3194c50230a994097
melvin <melvinzhang@gmail.com>
2012-09-16 10:07:26 +0800

    remove Object[] data

--- src/magic/card/Giant_Growth.java
+++ src/magic/card/Giant_Growth.java
@@ -17,11 +17,9 @@ public class Giant_Growth {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicPumpTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ gets +3/+3 until end of turn.");
         }
@@ -31,7 +29,6 @@ public class Giant_Growth {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeTurnPTAction(creature,3,3));
