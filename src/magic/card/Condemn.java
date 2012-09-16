447dca12d055525f9ba96708d9883d75c33fbfbe
melvin <melvinzhang@gmail.com>
2012-09-16 09:17:40 +0800

    remove Object[] data

--- src/magic/card/Condemn.java
+++ src/magic/card/Condemn.java
@@ -19,11 +19,9 @@ public class Condemn {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
                     MagicExileTargetPicker.create(), // could use a better TargetPicker
-                    new Object[]{cardOnStack},
                     this,
                     "Put target creature$ on the bottom of its owner's library. " +
                     "Its controller gains life equal to its toughness.");
@@ -34,7 +32,6 @@ public class Condemn {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeLifeAction(
