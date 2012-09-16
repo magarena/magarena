f64d2de37b92a837bbe284121673c8eaab4b6ab6
melvin <melvinzhang@gmail.com>
2012-09-16 09:33:56 +0800

    remove Object[] data

--- src/magic/card/Wrath_of_God.java
+++ src/magic/card/Wrath_of_God.java
@@ -20,9 +20,7 @@ public class Wrath_of_God {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
-                    new Object[]{cardOnStack},
+                    cardOnStack,
                     this,
                     "Destroy all creatures. They can't be regenerated.");
         }
@@ -32,10 +30,8 @@ public class Wrath_of_God {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             final Collection<MagicTarget> targets =
-                game.filterTargets(cardOnStack.getController(),MagicTargetFilter.TARGET_CREATURE);            
+                game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);            
             for (final MagicTarget target : targets) {
                 game.doAction(new MagicChangeStateAction((MagicPermanent)target,MagicPermanentState.CannotBeRegenerated,true));
             }
