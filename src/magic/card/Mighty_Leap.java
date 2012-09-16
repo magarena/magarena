cf62330c99ff160e81a08f80b67d1068768552fd
melvin <melvinzhang@gmail.com>
2012-09-16 11:27:13 +0800

    remove Object[] data

--- src/magic/card/Mighty_Leap.java
+++ src/magic/card/Mighty_Leap.java
@@ -19,11 +19,9 @@ public class Mighty_Leap {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicFlyingTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ gets +2/+2 and gains flying until end of turn.");
         }
@@ -33,7 +31,6 @@ public class Mighty_Leap {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeTurnPTAction(creature,2,2));
