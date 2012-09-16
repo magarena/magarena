89d69671a3221828112166b5f0be8990e39f5870
melvin <melvinzhang@gmail.com>
2012-09-16 10:57:30 +0800

    remove Object[] data

--- src/magic/card/Fists_of_the_Anvil.java
+++ src/magic/card/Fists_of_the_Anvil.java
@@ -17,13 +17,10 @@ public class Fists_of_the_Anvil {
     public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final MagicPlayer player=cardOnStack.getController();
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    player,
+                    cardOnStack,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     MagicPumpTargetPicker.create(),
-                    new Object[]{cardOnStack},
                     this,
                     "Target creature$ gets +4/+0 until end of turn.");
         }
@@ -33,7 +30,6 @@ public class Fists_of_the_Anvil {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            game.doAction(new MagicMoveCardAction((MagicCardOnStack)data[0]));
             event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeTurnPTAction(creature,4,0));
