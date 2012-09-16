0a91e51f130c9312b46083d20f911a990224ef0e
melvin <melvinzhang@gmail.com>
2012-09-16 20:40:14 +0800

    replace source with SN

--- src/magic/card/Chameleon_Colossus.java
+++ src/magic/card/Chameleon_Colossus.java
@@ -26,9 +26,8 @@ public class Chameleon_Colossus {
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
             return new MagicEvent(
                     source,
-                    source.getController(),
                     this,
-                    source + " gets +X/+X until end of turn, where X is its power.");
+                    "SN gets +X/+X until end of turn, where X is its power.");
         }
         @Override
         public void executeEvent(
