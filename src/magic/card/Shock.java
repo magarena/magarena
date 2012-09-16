5b2b963bbf2db2061e4a6c7aeecf98a725e5a18c
melvin <melvinzhang@gmail.com>
2012-09-16 09:48:26 +0800

    remove Object[] data

--- src/magic/card/Shock.java
+++ src/magic/card/Shock.java
@@ -18,13 +18,11 @@ public class Shock {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack.getCard(),
-                    cardOnStack.getController(),
+                    cardOnStack,
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(2),
-                    new Object[]{cardOnStack},
                     this,
-                    cardOnStack.getCard() + " deals 2 damage to target creature or player$.");
+                    cardOnStack + " deals 2 damage to target creature or player$.");
         }
         @Override
         public void executeEvent(
@@ -32,11 +30,9 @@ public class Shock {
                 final MagicEvent event,
                 final Object[] data,
                 final Object[] choiceResults) {
-            final MagicCardOnStack cardOnStack = (MagicCardOnStack)data[0];
-            game.doAction(new MagicMoveCardAction(cardOnStack));
             event.processTarget(game,choiceResults,0,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
-                    final MagicDamage damage = new MagicDamage(cardOnStack.getCard(),target,2,false);
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,2,false);
                     game.doAction(new MagicDealDamageAction(damage));
                 }
             });
