0358cde92f8e737a068ad06342c7c90c3d7c50cb
melvin <melvinzhang@gmail.com>
2012-09-28 16:16:18 +0800

    compare enum with ==

--- src/magic/data/CardDefinitions.java
+++ src/magic/data/CardDefinitions.java
@@ -172,15 +172,15 @@ public class CardDefinitions {
     }
         
     public static MagicCardDefinition getBasicLand(final MagicColor color) {
-        if (MagicColor.Black.equals(color)) {
+        if (MagicColor.Black == color) {
             return getCard("Swamp");
-        } else if (MagicColor.Blue.equals(color)) {
+        } else if (MagicColor.Blue == color) {
             return getCard("Island");
-        } else if (MagicColor.Green.equals(color)) {
+        } else if (MagicColor.Green == color) {
             return getCard("Forest");
-        } else if (MagicColor.Red.equals(color)) {
+        } else if (MagicColor.Red == color) {
             return getCard("Mountain");
-        } else if (MagicColor.White.equals(color)) {
+        } else if (MagicColor.White == color) {
             return getCard("Plains");
         }
         throw new RuntimeException("No matching basic land for MagicColor " + color);
