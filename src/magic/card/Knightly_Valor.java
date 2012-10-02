8ffd75a160b254925e9889e5531f635a4337a3b4
a. benedict balbue na <abcbalbuena@gmail.com>
2012-10-02 21:34:17 +0800

    added Knightly Valor

---
+++ src/magic/card/Knightly_Valor.java
@@ -0,0 +1,30 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Knightly_Valor {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a 2/2 white Knight creature token with vigilance onto the battlefield.");
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Knight2")));
+        }
+    };
+}
