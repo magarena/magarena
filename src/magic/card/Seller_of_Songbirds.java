536c16b2291ca1a00b03f8f23acb18875d05472e
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-06 12:27:22 +0800

    added Seller of Songbirds

---
+++ src/magic/card/Seller_of_Songbirds.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Seller_of_Songbirds {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 white Bird creature token with flying onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Bird1")));
+        }
+    };
+}
