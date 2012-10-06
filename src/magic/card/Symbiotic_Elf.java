4f1f5321ac62bc5b393dd61f0bf94a5a510758c5
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-06 12:56:26 +0800

    added Symbiotic Elf

---
+++ src/magic/card/Symbiotic_Elf.java
@@ -0,0 +1,32 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenDiesTrigger;
+
+public class Symbiotic_Elf {
+    public static final MagicWhenDiesTrigger T = new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts two 1/1 green Insect creature tokens onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Insect4")));
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Insect4")));
+        }
+    };
+}
