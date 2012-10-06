ef112bbee43a6305642e441731b3d81210512c33
melvin <melvinzhang@gmail.com>
2012-10-06 22:32:47 +0800

    added Thragtusk

---
+++ src/magic/card/Thragtusk.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenDiesTrigger;
+
+public class Thragtusk {
+    public static final Object T = new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 3/3 green Beast creature token onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Beast3")));
+        }
+    };
+}
