98a2a7d4cb9aae8a26d0ba145de182ca20074a1a
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-06 11:48:33 +0800

    added Hornet Queen and Insect token

---
+++ src/magic/card/Hornet_Queen.java
@@ -0,0 +1,32 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
+
+public class Hornet_Queen {
+    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts four 1/1 green Insect creature tokens with flying and deathtouch onto the battlefield.");
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            for (int i = 0 ; i < 4; i++) {
+                game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Insect3")));
+            }
+        }
+    };
+}
