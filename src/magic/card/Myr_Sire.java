d8997c77d5135074add30293bf3ec961c64a030f
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-06 11:52:57 +0800

    added Myr Sire

---
+++ src/magic/card/Myr_Sire.java
@@ -0,0 +1,31 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicWhenDiesTrigger;
+
+public class Myr_Sire {
+    public static final Object T = new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 colorless Myr artifact creature token onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object data[],
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Myr1")));
+        }
+    };
+}
