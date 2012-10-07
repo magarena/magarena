925f4fde1b6db0ff59cf0a040f968b0385bfbc82
melvin <melvinzhang@gmail.com>
2012-10-07 17:07:38 +0800

    replace getController().getOpponent() with getOpponent()

--- src/magic/card/Scourge_of_Geier_Reach.java
+++ src/magic/card/Scourge_of_Geier_Reach.java
@@ -10,7 +10,7 @@ public class Scourge_of_Geier_Reach {
     public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final int amount = permanent.getController().getOpponent().getNrOfPermanentsWithType(MagicType.Creature);
+            final int amount = permanent.getOpponent().getNrOfPermanentsWithType(MagicType.Creature);
             pt.add(amount,amount);
         }
     };
