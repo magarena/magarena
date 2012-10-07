1c73131383234f08c2dc588eb57f285bf7e66bd4
melvin <melvinzhang@gmail.com>
2012-10-07 22:32:32 +0800

    replace filterTargets with filterPermanents

--- src/magic/card/To_Arms_.java
+++ src/magic/card/To_Arms_.java
@@ -29,10 +29,10 @@ public class To_Arms_ {
                 final MagicEvent event,
                 final Object[] choiceResults) {
             final MagicPlayer player=event.getPlayer();
-            final Collection<MagicTarget> targets=
-                game.filterTargets(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicTarget target : targets) {
-                game.doAction(new MagicUntapAction((MagicPermanent)target));
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicUntapAction(target));
             }
             game.doAction(new MagicDrawAction(player,1));
         }
