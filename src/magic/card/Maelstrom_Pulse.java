71e4dbecc8b06c9b475011e454162419d667bc85
melvin <melvinzhang@gmail.com>
2012-10-07 22:37:03 +0800

    replace filterTargets with filterPermanents

--- src/magic/card/Maelstrom_Pulse.java
+++ src/magic/card/Maelstrom_Pulse.java
@@ -35,10 +35,10 @@ public class Maelstrom_Pulse {
                 public void doAction(final MagicPermanent targetPermanent) {
                     final MagicTargetFilter targetFilter = 
                         new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
-                    final Collection<MagicTarget> targets = 
-                        game.filterTargets(event.getPlayer(),targetFilter);
-                    for (final MagicTarget target : targets) {
-                        game.doAction(new MagicDestroyAction((MagicPermanent)target));
+                    final Collection<MagicPermanent> targets = 
+                        game.filterPermanents(event.getPlayer(),targetFilter);
+                    for (final MagicPermanent target : targets) {
+                        game.doAction(new MagicDestroyAction(target));
                     }
                 }
             });
