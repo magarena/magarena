f87d4ab5c39342f26f342b5532fd852ceb4287ae
melvin <melvinzhang@gmail.com>
2012-10-07 22:30:35 +0800

    replace filterTargets with filterPermanents

--- src/magic/card/Wrap_in_Vigor.java
+++ src/magic/card/Wrap_in_Vigor.java
@@ -27,10 +27,9 @@ public class Wrap_in_Vigor {
                 final MagicGame game,
                 final MagicEvent event,
                 final Object[] choiceResults) {
-            final Collection<MagicTarget> targets = 
-                game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicTarget target : targets) {
-                final MagicPermanent creature=(MagicPermanent)target;
+            final Collection<MagicPermanent> targets = 
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent creature : targets) {
                 game.doAction(new MagicRegenerateAction(creature));
             }
         }
