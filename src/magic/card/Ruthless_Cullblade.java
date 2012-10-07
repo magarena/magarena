e3e2da5b209620564a2de030c6d8115670c34b4d
melvin <melvinzhang@gmail.com>
2012-10-07 17:08:06 +0800

    replace getController().getOpponent() with getOpponent()

--- src/magic/card/Ruthless_Cullblade.java
+++ src/magic/card/Ruthless_Cullblade.java
@@ -10,7 +10,7 @@ public class Ruthless_Cullblade {
     public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (permanent.getController().getOpponent().getLife()<=10) {
+            if (permanent.getOpponent().getLife()<=10) {
                 pt.add(2,1);
             }
         }        
