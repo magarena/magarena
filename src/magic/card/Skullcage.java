e0cdbb15b0ad9acd875acc915bc2a72c1b8fe1d8
melvin <melvinzhang@gmail.com>
2012-10-07 17:05:37 +0800

    rename data to upkeepPlayer

--- src/magic/card/Skullcage.java
+++ src/magic/card/Skullcage.java
@@ -12,15 +12,14 @@ import magic.model.trigger.MagicAtUpkeepTrigger;
 public class Skullcage {
     public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer data) {
-            final MagicPlayer player=permanent.getController();
-            return (player!=data) ?
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isOpponent(upkeepPlayer) ?
                 new MagicEvent(
                     permanent,
-                    player,
+                    upkeepPlayer,
                     this,
-                    "SN deals 2 damage to your opponent " + 
-                    "unless your opponent has exactly three or exactly four cards in hand."):
+                    "SN deals 2 damage to PN unless PN has exactly three or exactly four cards in hand."
+                ):
                 MagicEvent.NONE;
         }
         @Override
@@ -28,9 +27,9 @@ public class Skullcage {
                 final MagicGame game,
                 final MagicEvent event,
                 final Object[] choiceResults) {
-            final MagicPlayer opponent=event.getPlayer().getOpponent();
+            final MagicPlayer opponent=event.getPlayer();
             final int amount=opponent.getHandSize();
-            if (amount<3||amount>4) {
+            if (amount < 3 || amount > 4) {
                 final MagicDamage damage=new MagicDamage(event.getSource(),opponent,2,false);
                 game.doAction(new MagicDealDamageAction(damage));
             }
