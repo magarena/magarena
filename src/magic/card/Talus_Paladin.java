9ae18614b29a863bdf195581232c324fb0b5fb66
melvin <melvinzhang@gmail.com>
2012-10-07 22:33:09 +0800

    replace filterTargets with filterPermanents

--- src/magic/card/Talus_Paladin.java
+++ src/magic/card/Talus_Paladin.java
@@ -40,10 +40,9 @@ public class Talus_Paladin {
                 final MagicEvent event,
                 final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                final Collection<MagicTarget> targets =
-                        game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
-                for (final MagicTarget target : targets) {
-                    final MagicPermanent creature = (MagicPermanent)target;
+                final Collection<MagicPermanent> targets =
+                        game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
+                for (final MagicPermanent creature : targets) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.LifeLink));
                 }
             }            
