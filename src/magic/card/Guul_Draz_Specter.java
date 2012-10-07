eaef52b75302bd1a0fe29ab8457633e48626511a
melvin <melvinzhang@gmail.com>
2012-10-07 17:18:33 +0800

    replace getController().getOpponent() with getOpponent()

--- src/magic/card/Guul_Draz_Specter.java
+++ src/magic/card/Guul_Draz_Specter.java
@@ -9,7 +9,7 @@ public class Guul_Draz_Specter {
     public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (permanent.getController().getOpponent().getHand().isEmpty()) {
+            if (permanent.getOpponent().getHand().isEmpty()) {
                 pt.add(3,3);
             }
         }        
