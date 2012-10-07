81f330131c6e599c5b40da7b0bbf4a19a2c4d715
melvin <melvinzhang@gmail.com>
2012-10-07 22:37:36 +0800

    replace filterTargets with filterPermanents

--- src/magic/card/Inspired_Charge.java
+++ src/magic/card/Inspired_Charge.java
@@ -26,10 +26,10 @@ public class Inspired_Charge {
                 final MagicGame game,
                 final MagicEvent event,
                 final Object[] choiceResults) {
-            final Collection<MagicTarget> targets=
-                game.filterTargets(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicTarget target : targets) {
-                game.doAction(new MagicChangeTurnPTAction((MagicPermanent)target,2,1));
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,2,1));
             }
         }
     };
