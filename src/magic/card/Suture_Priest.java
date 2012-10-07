7cc3ef4772e079cc9ec16af4568b27088d431dc1
melvin <melvinzhang@gmail.com>
2012-10-07 09:38:25 +0800

    create separate event for may gain life and may lose lofe

--- src/magic/card/Suture_Priest.java
+++ src/magic/card/Suture_Priest.java
@@ -7,37 +7,43 @@ import magic.model.action.MagicChangeLifeAction;
 import magic.model.choice.MagicMayChoice;
 import magic.model.choice.MagicSimpleMayChoice;
 import magic.model.event.MagicEvent;
+import magic.model.event.MagicEventAction;
 import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
 
 public class Suture_Priest {
     public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            final MagicPlayer controller = otherPermanent.getController();
-            final boolean same = permanent.isFriend(otherPermanent);
-            final int lifeChange = same ? 1 : -1;
-            return (otherPermanent != permanent &&
-                    otherPermanent.isCreature()) ?
+        public boolean accept(final MagicPermanent permanent, final MagicPermanent other) {
+            return other != permanent && other.isCreature();
+        }
+        
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent other) {
+            return permanent.isFriend(other) ?
                 new MagicEvent(
                     permanent,
-                    same ?
-                        new MagicSimpleMayChoice(
-                            MagicSimpleMayChoice.GAIN_LIFE,
-                            1,
-                            MagicSimpleMayChoice.DEFAULT_YES
-                        ) :
-                        new MagicSimpleMayChoice(
-                            MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
-                            1,
-                            MagicSimpleMayChoice.DEFAULT_YES
-                        ),
-                    new Object[]{controller,lifeChange},
-                    this,
-                    same ?
-                        "PN may$ gain 1 life." :
-                        "PN may$ have " + controller + " lose 1 life.") :
-                MagicEvent.NONE;
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.GAIN_LIFE,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ), 
+                    MAY_GAIN_ONE_LIFE,
+                    "PN may$ gain 1 life."
+                ):
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    MAY_LOSE_ONE_LIFE,
+                    "PN may$ have " + other.getController() + " lose 1 life."
+                );
         }
+    };
+        
+    private static final MagicEventAction MAY_GAIN_ONE_LIFE = new MagicEventAction() {
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -45,9 +51,20 @@ public class Suture_Priest {
                 final Object[] data,
                 final Object[] choiceResults) {
             if (MagicMayChoice.isYesChoice(choiceResults[0])) {
-                game.doAction(new MagicChangeLifeAction(
-                        (MagicPlayer)data[0],
-                        (Integer)data[1]));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(), 1));
+            }
+        }        
+    };
+    
+    private static final MagicEventAction MAY_LOSE_ONE_LIFE = new MagicEventAction() {
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] data,
+                final Object[] choiceResults) {
+            if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer().getOpponent(), -1));
             }
         }        
     };
