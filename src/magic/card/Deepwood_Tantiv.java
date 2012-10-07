fd717e2c5a28270a2bf12fe04ceb45e4c4e8e069
melvin <melvinzhang@gmail.com>
2012-10-07 17:21:51 +0800

    rename data to blocked

--- src/magic/card/Deepwood_Tantiv.java
+++ src/magic/card/Deepwood_Tantiv.java
@@ -9,8 +9,8 @@ import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
 public class Deepwood_Tantiv {
     public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent data) {
-            return (permanent == data ) ?
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocked) {
+            return (permanent == blocked) ?
                 new MagicEvent(
                     permanent,
                     this,
