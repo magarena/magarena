9b8fc4672dd1cc21734a4d8c23400ccdb9aa1bbe
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-30 22:31:01 +0800

    added Muscle Sliver

---
+++ src/magic/card/Muscle_Sliver.java
@@ -0,0 +1,21 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+
+public class Muscle_Sliver {
+    public static final MagicStatic S2 = new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+    };
+}
