12333cc406c6d55948242473e2a320dbfed5a21d
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-30 21:45:35 +0800

    added Lord of Atlantis

---
+++ src/magic/card/Lord_of_Atlantis.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+
+public class Lord_of_Atlantis {
+    public static final MagicStatic S1 = new MagicStatic(
+            MagicLayer.Ability,
+            MagicTargetFilter.TARGET_MERFOLK_CREATURE) {
+        @Override
+        public long getAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final long flags) {
+            return flags | MagicAbility.Islandwalk.getMask();
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+
+    public static final MagicStatic S2 = new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_MERFOLK_CREATURE) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+}
