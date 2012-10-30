39b6b880a87ff8a4b3e03e9a4320192763d9c817
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-30 22:39:59 +0800

    added Synchronous Sliver

---
+++ src/magic/card/Synchronous_Sliver.java
@@ -0,0 +1,20 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+public class Synchronous_Sliver {
+    public static final MagicStatic S = new MagicStatic(
+            MagicLayer.Ability,
+            MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public long getAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final long flags) {
+            return flags | MagicAbility.Vigilance.getMask();
+        }
+    };
+}
