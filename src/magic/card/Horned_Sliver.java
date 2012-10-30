ed608a13980f0978f81cf00bbaa41af5ad58d823
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-30 22:27:15 +0800

    added Horned Sliver

---
+++ src/magic/card/Horned_Sliver.java
@@ -0,0 +1,21 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+
+public class Horned_Sliver {
+    public static final MagicStatic S1 = new MagicStatic(
+            MagicLayer.Ability,
+            MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public long getAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final long flags) {
+            return flags | MagicAbility.Trample.getMask();
+        }
+    };
+}
