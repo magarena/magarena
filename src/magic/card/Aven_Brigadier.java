d13b2c3ab7c898ff2f360b37c1939c3e9364824c
a. benedict balbuena <abcbalbuena@gmail.com>
2012-10-30 22:22:35 +0800

    added Aven Brigadier

---
+++ src/magic/card/Aven_Brigadier.java
@@ -0,0 +1,38 @@
+package magic.card;
+
+import magic.model.MagicAbility;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.target.MagicTargetFilter;
+
+
+public class Aven_Brigadier {
+    public static final MagicStatic S1 = new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_BIRD) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+
+    public static final MagicStatic S2 = new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_SOLDIER) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    };
+}
