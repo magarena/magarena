632d36f403b342e6f39373dead62e7c321198f81
PhazedOut <unknown>
2012-12-27 14:32:25 -0600

    add Myr Matrix

---
+++ src/magic/card/Myr_Matrix.java
@@ -0,0 +1,65 @@
+package magic.card;
+
+import magic.data.TokenCardDefinitions;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicPowerToughness;
+import magic.model.MagicSource;
+import magic.model.MagicPlayer;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicSimpleMayChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.event.MagicActivationHints;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicPermanentActivation;
+import magic.model.event.MagicPayManaCostEvent;
+import magic.model.event.MagicTiming;
+import magic.model.mstatic.MagicLayer;
+import magic.model.mstatic.MagicStatic;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.target.MagicTarget;
+import magic.model.target.MagicTargetFilter;
+
+public class Myr_Matrix {
+	public static final MagicStatic S = new MagicStatic(
+	        MagicLayer.ModPT, 
+	        MagicTargetFilter.TARGET_MYR_YOU_CONTROL) {
+	        @Override
+	        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+	            pt.add(1,1);
+	        }
+	        @Override
+	        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+	            return source != target;
+	        }
+	    };
+    
+    public static final MagicPermanentActivation A = new MagicPermanentActivation(
+            new MagicCondition[]{MagicManaCost.FIVE.getCondition()},
+            new MagicActivationHints(MagicTiming.Token),
+            "Token") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return new MagicEvent[]{
+            		new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.FIVE)};
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                    source,
+                    this,
+                    "PN puts a 1/1 colorless Myr artifact creature token onto the battlefield.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Myr1")));
+        }
+    };    
+}
