b096d6f7b1205fd8d652b2096b63dcef5ddff5a6
swanninger <unknown>
2012-12-27 01:34:22 -0600

    BlazeTest

--- src/magic/card/Blaze.java
+++ src/magic/card/Blaze.java
@@ -13,7 +13,31 @@ import magic.model.target.MagicDamageTargetPicker;
 import magic.model.target.MagicTarget;
 
 public class Blaze {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+    public static final MagicSpellCardEvent S1 = new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+            return new MagicEvent(
+                    cardOnStack,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(amount),
+                    this,
+                    "SN deals "+amount+" damage to target creature or player$.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getCardOnStack().getX(),false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    };
+    public static final MagicSpellCardEvent S2 = new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             final int amount=payedCost.getX();
