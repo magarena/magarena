8432da875a3ba0a7c94a94ac608cb8e2908b57f3
melvin <melvinzhang@gmail.com>
2013-01-06 10:34:38 +0800

    added show method to print the tree to stdout

--- src/magic/grammar/Node.java
+++ src/magic/grammar/Node.java
@@ -20,6 +20,24 @@ public class Node {
         sb.append(')');
         return sb.toString();
     }
+    
+    public void show() {
+        show(0);
+    }
+
+    public void show(int gap) {
+        for (int i = 0; i < gap; i++) {
+            System.out.print(" ");
+        }
+        System.out.print(rule);
+        if (!text.isEmpty()) {
+            System.out.print(" - " + text);
+        }
+        System.out.println();
+        for (final Node n : children) {
+            n.show(gap + 4);
+        }
+    }
 
     public void add(final Node n) {
         children.add(n);
