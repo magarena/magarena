067ccde1df81c7f7d1df68853266f4075cbb5061
melvin <melvinzhang@gmail.com>
2013-01-12 10:47:44 +0800

    move prevent 1 activation from Abuna Acolyte to Samite Healer

--- src/magic/card/Abuna_Acolyte.java
+++ src/magic/card/Abuna_Acolyte.java
@@ -17,36 +17,7 @@ import magic.model.target.MagicPreventTargetPicker;
 import magic.model.target.MagicTarget;
 
 public class Abuna_Acolyte {
-    public static final MagicPermanentActivation A1 = new MagicPermanentActivation(
-            new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
-            new MagicActivationHints(MagicTiming.Pump),
-            "Prevent 1") {
-
-        @Override
-        public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{new MagicTapEvent(source)};
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER,
-                MagicPreventTargetPicker.getInstance(),
-                this,
-                "Prevent the next 1 damage that would be dealt to target creature or player$ this turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
-            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    game.doAction(new MagicPreventDamageAction(target,1));
-                }
-            });
-        }
-    };
+    public static final Object A1 = Samite_Healer.A1;
 
     public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
             new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
