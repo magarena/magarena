1e83239e119934ad5ea87a7500911c3366e0a382
melvin <melvinzhang@gmail.com>
2013-01-13 21:54:28 +0800

    move Scythe Tiger incomplete to scripts by adding groovy script

---
+++ release/Magarena/scripts/Scythe_Tiger.groovy
@@ -0,0 +1,29 @@
+[
+new MagicWhenComesIntoPlayTrigger() {
+    @Override
+    public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+        return new MagicEvent(
+            permanent,
+            new MagicMayChoice(MagicTargetChoice.TARGET_LAND_YOU_CONTROL),
+            MagicSacrificeTargetPicker.create(),
+            this,
+            "You may sacrifice a land. If you don't, sacrifice SN."
+        );
+    }
+    @Override
+    public void executeEvent(
+            final MagicGame game,
+            final MagicEvent event,
+            final Object[] choiceResults) {
+        if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+            event.processTargetPermanent(game,choiceResults,1,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent land) {
+                    game.doAction(new MagicSacrificeAction(land));
+                }
+            });
+        } else {
+            game.doAction(new MagicSacrificeAction(event.getPermanent()));
+        }
+    }
+}
+]
