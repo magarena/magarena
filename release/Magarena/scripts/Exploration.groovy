f6242ba76b9a01feb5fc00931bae156275cdd029
melvin <melvinzhang@gmail.com>
2013-02-10 12:37:05 +0800

    added Exploration

---
+++ release/Magarena/scripts/Exploration.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.Game) {
+        @Override
+        public void modGame(final MagicPermanent source, final MagicGame game) {
+            game.incMaxLand();
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return game.getTurnPlayer() == source.getController();
+        }
+    }
+]
