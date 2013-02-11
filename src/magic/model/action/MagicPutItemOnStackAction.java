bb76facbc2198f047fec549e433f2a4900998a5e
melvin <melvinzhang@gmail.com>
2013-02-11 13:10:21 +0800

    move actions when spell is cast into a separate event so that it occurs after getting choices, fixes replicate mechanic

--- src/magic/model/action/MagicPutItemOnStackAction.java
+++ src/magic/model/action/MagicPutItemOnStackAction.java
@@ -4,6 +4,7 @@ import magic.ai.ArtificialScoringSystem;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.event.MagicStackGetChoicesEvent;
+import magic.model.event.MagicSpellCastEvent;
 import magic.model.stack.MagicCardOnStack;
 import magic.model.stack.MagicItemOnStack;
 import magic.model.trigger.MagicTrigger;
@@ -25,19 +26,7 @@ public class MagicPutItemOnStackAction extends MagicAction {
             game.addEvent(new MagicStackGetChoicesEvent(itemOnStack));
         }
         if (itemOnStack.isSpell()) {
-            // execute spell is cast triggers
-            for (final MagicWhenSpellIsCastTrigger trigger : itemOnStack.getSource().getCardDefinition().getSpellIsCastTriggers()) {
-                game.executeTrigger(
-                    trigger,
-                    MagicPermanent.NONE,
-                    itemOnStack.getSource(),
-                    (MagicCardOnStack)itemOnStack
-                );
-            }
-            
-            // execute other spell is cast triggers
-            game.executeTrigger(MagicTriggerType.WhenOtherSpellIsCast,itemOnStack);
-            game.incSpellsPlayed();
+            game.addEvent(new MagicSpellCastEvent(itemOnStack));
         }
         // Avoid unnecessary actions on stack by reducing score.
         setScore(itemOnStack.getController(),ArtificialScoringSystem.ITEM_ON_STACK_SCORE);
---
+++ src/magic/model/event/MagicSpellCastEvent.java
@@ -0,0 +1,45 @@
+package magic.model.event;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.stack.MagicItemOnStack;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicWhenSpellIsCastTrigger;
+import magic.model.trigger.MagicTriggerType;
+
+public class MagicSpellCastEvent extends MagicEvent {
+    public MagicSpellCastEvent(final MagicItemOnStack itemOnStack) {
+        super(
+            itemOnStack.getEvent().getSource(),
+            itemOnStack,
+            EVENT_ACTION,
+            ""
+        );
+    }
+    
+    private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final MagicItemOnStack itemOnStack = event.getRefItemOnStack();
+            
+            // execute spell is cast triggers
+            for (final MagicWhenSpellIsCastTrigger trigger : itemOnStack.getSource().getCardDefinition().getSpellIsCastTriggers()) {
+                game.executeTrigger(
+                    trigger,
+                    MagicPermanent.NONE,
+                    itemOnStack.getSource(),
+                    (MagicCardOnStack)itemOnStack
+                );
+            }
+            
+            // execute other spell is cast triggers
+            game.executeTrigger(MagicTriggerType.WhenOtherSpellIsCast,itemOnStack);
+            game.incSpellsPlayed();
+        }
+    };
+}
