f531fc6a0b5bdd73038745e6a02f3899e902c13b
melvin <melvinzhang@gmail.com>
2013-02-12 11:25:31 +0800

    adjust spacing for clarity

--- src/magic/model/choice/MagicSimpleMayChoice.java
+++ src/magic/model/choice/MagicSimpleMayChoice.java
@@ -18,17 +18,17 @@ public class MagicSimpleMayChoice extends MagicChoice {
     public static final int DRAW_CARDS = 1;
     public static final int GAIN_LIFE = 2;             // always returns YES_CHOICE_LIST
     public static final int LOSE_LIFE = 3;
-    public static final int OPPONENT_LOSE_LIFE = 4; // always returns YES_CHOICE_LIST
+    public static final int OPPONENT_LOSE_LIFE = 4;    // always returns YES_CHOICE_LIST
     public static final int UNTAP = 5;                 // always returns YES_CHOICE_LIST
-    public static final int BECOME_CREATURE = 6;     // always returns YES_CHOICE_LIST
+    public static final int BECOME_CREATURE = 6;       // always returns YES_CHOICE_LIST
     public static final int ADD_CHARGE_COUNTER = 7;    // always returns YES_CHOICE_LIST
-    public static final int ADD_PLUSONE_COUNTER = 8;// always returns YES_CHOICE_LIST
-    public static final int PLAY_TOKEN = 9;         // always returns YES_CHOICE_LIST
+    public static final int ADD_PLUSONE_COUNTER = 8;   // always returns YES_CHOICE_LIST
+    public static final int PLAY_TOKEN = 9;            // always returns YES_CHOICE_LIST
     public static final int PUMP = 10;                 // always returns YES_CHOICE_LIST
     
     public static final int DEFAULT_NONE = 0;
-    public static final int DEFAULT_NO = 1;
-    public static final int DEFAULT_YES = 2;
+    public static final int DEFAULT_NO   = 1;
+    public static final int DEFAULT_YES  = 2;
 
     private static final List<Object[]> YES_CHOICE_LIST = Collections.singletonList(new Object[]{YES_CHOICE});
     private static final List<Object[]> NO_CHOICE_LIST = Collections.singletonList(new Object[]{NO_CHOICE});
