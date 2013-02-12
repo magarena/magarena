b1803eea13f1e3435e00db3774029ddfc119743b
melvin <melvinzhang@gmail.com>
2013-02-12 09:21:10 +0800

    move Fleshbag Marauder from incomplete to scripts by adding groovy code

---
+++ release/Magarena/scripts/Fleshbag_Marauder.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Each player sacrifices a creature."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            for (final MagicPlayer player : game.getPlayers()) {
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    player,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                ));
+            }
+        }
+    }
+]
