2925a8501fe328777a37fa10d928d564db694a71
melvin <melvinzhang@gmail.com>
2013-02-13 12:54:38 +0800

    added Mitotic Slime

---
+++ release/Magarena/scripts/Mitotic_Slime.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts two 2/2 green Ooze creature tokens onto the battlefield. " + 
+                "They have \"When this creature dies, put two 1/1 green Ooze creature tokens onto the battlefield.\""
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokensAction(event.getPlayer(), TokenCardDefinitions.get("Ooze2"), 2));
+        }
+    }
+]
