2a9aab654b562d8985d5121a15e687f99903bb7b
melvin <melvinzhang@gmail.com>
2013-02-14 14:26:30 +0800

    added Kher Keep and Kobolds of Kher Keep token

---
+++ release/Magarena/scripts/Kher_Keep.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicManaCost.ONE_RED.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.ONE_RED)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(
+                final MagicPermanent source,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a 0/1 red Kobold creature token named Kobolds of Kher Keep onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Kobolds of Kher Keep")
+            ));
+        }
+    }
+]
