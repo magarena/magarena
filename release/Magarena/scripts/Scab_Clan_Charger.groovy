2a2f879a4756c98c51b7f3b70eca47f29f978512
melvin <melvinzhang@gmail.com>
2013-02-14 22:34:53 +0800

    added Scab-Clan Charger

---
+++ release/Magarena/scripts/Scab_Clan_Charger.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicBloodrushActivation(
+        MagicManaCost.ONE_GREEN,
+        "Target attacking creature\$ gets +2/+4 until end of turn.") {
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,4));
+                }
+            });
+        }
+    }
+]
