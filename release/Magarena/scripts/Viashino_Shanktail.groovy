81949595ea1a9691c492d7f9126e210a1d8ba692
melvin <melvinzhang@gmail.com>
2013-02-16 13:08:07 +0800

    added Viashino Shanktail

---
+++ release/Magarena/scripts/Viashino_Shanktail.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicBloodrushActivation(
+        MagicManaCost.TWO_RED,
+        "Target attacking creature\$ gets +3/+1 and gains first strike until end of turn.") {
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,3,1));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike));
+                }
+            });
+        }
+    }
+]
