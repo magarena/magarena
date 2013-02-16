8e92daaa1ab7a18e9091e7a032938c53cbcfcdd8
melvin <melvinzhang@gmail.com>
2013-02-16 13:08:35 +0800

    added Wasteland Viper

---
+++ release/Magarena/scripts/Wasteland_Viper.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicBloodrushActivation(
+        MagicManaCost.GREEN,
+        "Target attacking creature\$ gets +1/+2 and gains deathtouch until end of turn.") {
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,2));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Deathtouch));
+                }
+            });
+        }
+    }
+]
