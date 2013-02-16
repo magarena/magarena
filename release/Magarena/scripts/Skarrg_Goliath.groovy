1e2aac832f13a0a658dbf24af1a5594baeb78d4a
melvin <melvinzhang@gmail.com>
2013-02-16 13:05:03 +0800

    added Skarrg Goliath

---
+++ release/Magarena/scripts/Skarrg_Goliath.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicBloodrushActivation(
+        MagicManaCost.FIVE_GREEN_GREEN,
+        "Target attacking creature\$ gets +9/+9 and gains trample until end of turn.") {
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,9,9));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                }
+            });
+        }
+    }
+]
