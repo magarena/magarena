3fdcb06e553e2ed1c328d69b7268a5406fdff201
melvin <melvinzhang@gmail.com>
2013-02-17 20:23:53 +0800

    added Inaction Injunction

---
+++ release/Magarena/scripts/Inaction_Injunction.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                new MagicNoCombatTargetPicker(true,true,false),
+                this,
+                "Detain target creature\$ an opponent controls. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDetainAction(creature));
+                    game.doAction(new MagicDrawAction(event.getPlayer(), 1));
+                }
+            });
+        }
+    }
+]
