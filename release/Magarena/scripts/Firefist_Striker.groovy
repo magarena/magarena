3de5d7e8091510df461e8f4a64319d204170947b
melvin <melvinzhang@gmail.com>
2013-02-17 13:58:21 +0800

    added Firefist Striker

---
+++ release/Magarena/scripts/Firefist_Striker.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicBattalionTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent attacker) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicNoCombatTargetPicker(false,true,false),
+                this,
+                "Target creature\$ can't block this turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.CannotBlock));
+                }
+            });
+        }
+    }
+]
