8f2c79e683405fd8b88a05eac3adfc5086e4f0d8
melvin <melvinzhang@gmail.com>
2013-02-17 20:24:16 +0800

    added Martial Law

---
+++ release/Magarena/scripts/Martial_Law.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                    new MagicNoCombatTargetPicker(true,true,false),
+                    this,
+                    "Detain target creature\$ an opponent controls."
+                ):
+                MagicEvent.NONE
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDetainAction(creature));
+                }
+            });
+        }
+    }
+]
