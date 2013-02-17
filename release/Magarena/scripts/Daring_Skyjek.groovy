21873ed1b760ae77e0cf13b4a165f6e4ad04a349
melvin <melvinzhang@gmail.com>
2013-02-17 13:58:05 +0800

    added Daring Skyjek

---
+++ release/Magarena/scripts/Daring_Skyjek.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicBattalionTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent attacker) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "SN gains flying until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicSetAbilityAction(event.getPermanent(),MagicAbility.Flying));
+        }
+    }
+]
