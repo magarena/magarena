8a1d324bed1c8029d3baed2781e95b0c179ff9f6
melvin <melvinzhang@gmail.com>
2013-02-17 10:06:05 +0800

    added Bomber Corps

---
+++ release/Magarena/scripts/Bomber_Corps.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicBattalionTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent attacker) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target creature or player\$."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,1,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
