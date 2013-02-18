0b30b341040ff90a8d2b6972fbc846a6e620e260
melvin <melvinzhang@gmail.com>
2013-02-18 21:33:12 +0800

    added Coursers' Accord

---
+++ release/Magarena/scripts/Coursers__Accord.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Put a 3/3 green Centaur creature token onto the battlefield, then populate.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Centaur3")));
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+        }
+    }
+]
