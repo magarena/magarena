a4fdac344860cec6d0464e962bfdabf2131097b7
melvin <melvinzhang@gmail.com>
2013-02-18 21:49:34 +0800

    added Horncaller's Chant

---
+++ release/Magarena/scripts/Horncaller_s_Chant.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Put a 4/4 green Rhino creature token with trample onto the battlefield, then populate.");
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Rhino4")));
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+        }
+    }
+]
