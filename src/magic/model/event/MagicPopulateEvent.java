f7e4e65cee155ca8b1988ed2ed9ef4fcc3abff5a
melvin <melvinzhang@gmail.com>
2013-02-18 21:32:51 +0800

    add support for populate mechanic

---
+++ src/magic/model/event/MagicPopulateEvent.java
@@ -0,0 +1,36 @@
+package magic.model.event;
+
+import magic.model.MagicGame;
+import magic.model.MagicSource;
+import magic.model.MagicPayedCost;
+import magic.model.MagicPermanent;
+import magic.model.MagicCounterType;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicPlayTokenAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.event.MagicEvent;
+import magic.model.target.MagicCopyTargetPicker;
+
+public class MagicPopulateEvent extends MagicEvent {
+
+    public MagicPopulateEvent(final MagicSource source) {
+        super(
+            source,
+            MagicTargetChoice.CREATURE_TOKEN_YOU_CONTROL,
+            MagicCopyTargetPicker.create(),
+            EA,
+            "Put a token onto the battlefield that's a copy of a creature token you control."
+        );
+    }
+    
+    private static final MagicEventAction EA = new MagicEventAction() {
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicPlayTokenAction(event.getPlayer(), creature.getCardDefinition()));
+                }
+            });
+        }
+    };
+}
