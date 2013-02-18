c2809baba62fdab3a7b78c19038d7e8bd03e6a65
melvin <melvinzhang@gmail.com>
2013-02-18 18:34:27 +0800

    added Hellhole Flailer

---
+++ release/Magarena/scripts/Hellhole_Flailer.groovy
@@ -0,0 +1,45 @@
+[
+    new MagicPermanentActivation(
+        [MagicManaCost.TWO_BLACK_RED.getCondition()],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            final MagicPermanent permanent=source;
+            return [
+                new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.TWO_BLACK_RED),
+                new MagicSacrificeEvent(permanent)
+            ];
+        }
+        
+        @Override
+        public MagicEvent getPermanentEvent(
+                final MagicPermanent source,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                new MagicDamageTargetPicker(source.getPower()),
+                this,
+                "SN deals damage equal to its power to target player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicPermanent source=event.getPermanent();
+                    final int amount=source.getPower();
+                    if (amount > 0) {
+                        final MagicDamage damage=new MagicDamage(source,target,amount,false);
+                        game.doAction(new MagicDealDamageAction(damage));
+                    }
+                }
+            });
+        }
+    }
+]
