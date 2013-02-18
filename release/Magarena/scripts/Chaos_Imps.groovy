7c16e179ad0858b749fff2cde1070d47084d84b7
melvin <melvinzhang@gmail.com>
2013-02-18 18:33:54 +0800

    added Chaos Imps

---
+++ release/Magarena/scripts/Chaos_Imps.groovy
@@ -0,0 +1,15 @@
+[ 
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(
+            final MagicPermanent source,
+            final MagicPermanent permanent,
+            final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Trample);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getCounters(MagicCounterType.PlusOne) > 0;
+        }
+    }
+]
