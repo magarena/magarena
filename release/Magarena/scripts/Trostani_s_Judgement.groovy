fc9d73c7b913a726fc22080735fe21005f48703c
melvin <melvinzhang@gmail.com>
2013-02-19 21:42:50 +0800

    added Trostani's Judgement

---
+++ release/Magarena/scripts/Trostani_s_Judgement.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Exile target creature\$, then populate."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                    game.addEvent(new MagicPopulateEvent(event.getSource()));
+                }
+            });
+        }
+    }
+]
