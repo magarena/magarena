2a2f3f57ac08516d33fd0bcd778dd8f9272dc634
melvin <melvinzhang@gmail.com>
2013-02-19 21:43:04 +0800

    added Trostani, Selesnya's Voice

---
+++ release/Magarena/scripts/Trostani__Selesnya_s_Voice.groovy
@@ -0,0 +1,54 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (permanent != otherPermanent &&
+                    otherPermanent.isCreature() && 
+                    otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    otherPermanent,
+                    this,
+                    "PN gains life equal to " + otherPermanent + "'s toughness."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            // get toughness here so counters on the creature are considered
+            final int toughness = (event.getRefPermanent()).getToughness();
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),toughness));
+        }
+    },
+    new MagicPermanentActivation(
+        [
+            MagicManaCost.ONE_GREEN_WHITE.getCondition(),
+            MagicCondition.CAN_TAP_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Flash),
+        "Populate") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source, source.getController(), MagicManaCost.ONE_GREEN_WHITE)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Populate."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+        }
+    }
+]
