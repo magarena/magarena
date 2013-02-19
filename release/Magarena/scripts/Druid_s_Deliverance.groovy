2d040969b346d1c0f712e01177752dcf4c12d13f
melvin <melvinzhang@gmail.com>
2013-02-19 21:41:31 +0800

    added Druid's Deliverance

---
+++ release/Magarena/scripts/Druid_s_Deliverance.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Prevent all combat damage that would be dealt to you this turn. Populate."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicChangePlayerStateAction(event.getPlayer(),MagicPlayerState.PreventAllCombatDamage));
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+        }
+    }
+]
