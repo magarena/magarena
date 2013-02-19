96af2283ebc0759bc0934433b13a105524e9f393
melvin <melvinzhang@gmail.com>
2013-02-19 21:41:57 +0800

    added Rootborn Defenses

---
+++ release/Magarena/scripts/Rootborn_Defenses.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Populate. Creatures you control are indestructible this turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+            game.doAction(new MagicAddStaticAction(new MagicStatic(
+                MagicLayer.Ability, 
+                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL, 
+                MagicStatic.UntilEOT) {
+                @Override
+                public void modAbilityFlags(
+                        final MagicPermanent source,
+                        final MagicPermanent permanent,
+                        final Set<MagicAbility> flags) {
+                    flags.add(MagicAbility.Indestructible);
+                }   
+            }));
+        }
+    }
+]
