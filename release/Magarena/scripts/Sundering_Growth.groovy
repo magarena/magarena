a9d851d31427c47232994d424c176c453860a786
melvin <melvinzhang@gmail.com>
2013-02-19 21:42:12 +0800

    added Sundering Growth

---
+++ release/Magarena/scripts/Sundering_Growth.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact or enchantment, then populate."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDestroyAction(creature));
+                    game.addEvent(new MagicPopulateEvent(event.getSource()));
+                }
+            });
+        }
+    }
+]
