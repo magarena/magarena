06d2b43c030f8d466778c8943d3bbbd5e6945d4b
melvin <melvinzhang@gmail.com>
2013-02-19 21:41:44 +0800

    added Growing Ranks

---
+++ release/Magarena/scripts/Growing_Ranks.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Populate."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.addEvent(new MagicPopulateEvent(event.getSource()));
+        }        
+    }
+]
