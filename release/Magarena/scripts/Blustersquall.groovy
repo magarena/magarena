d8beaf12c7d16669a6be5a1cf989f007686e1b74
melvin <melvinzhang@gmail.com>
2013-02-20 22:03:02 +0800

    simplify groovy script for Blustersquall

--- release/Magarena/scripts/Blustersquall.groovy
+++ release/Magarena/scripts/Blustersquall.groovy
@@ -1,52 +1,27 @@
-def standard = new MagicSpellCardEvent() {
-    @Override
-    public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            cardOnStack,
-            MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
-            new MagicTapTargetPicker(true,false),
-            this,
-            "Tap target creature\$ you don't control."
-        );
-    }
-    @Override
-    public void executeEvent(
-            final MagicGame game,
-            final MagicEvent event,
-            final Object[] choiceResults) {
-        event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
-            public void doAction(final MagicPermanent creature) {
-                game.doAction(new MagicTapAction(creature,true));
-            }
-        });
-    }
-}
-
-def overload = new MagicSpellCardEvent() {
-    @Override
-    public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            cardOnStack,
-            this,
-            "Tap each creature\$ you don't control."
-        );
-    }
-    @Override
-    public void executeEvent(
-            final MagicGame game,
-            final MagicEvent event,
-            final Object[] choiceResults) {
-        final Collection<MagicPermanent> targets=
-            game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS);
-        for (final MagicPermanent target : targets) {
-            game.doAction(new MagicTapAction(target,true));
-        }
-    }
-}
-
-
 [
-    standard,
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target creature\$ you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                }
+            });
+        }
+    },
     new MagicCardActivation(
         [
             MagicManaCost.THREE_BLUE.getCondition()
@@ -59,8 +34,24 @@ def overload = new MagicSpellCardEvent() {
                 new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.THREE_BLUE)
             ];
         }
-        public MagicCardEvent getCardEvent(final MagicCard source,final MagicPayedCost payedCost) {
-            return overload;
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Tap each creature\$ you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicTapAction(target,true));
+            }
         }
     }
 ]
