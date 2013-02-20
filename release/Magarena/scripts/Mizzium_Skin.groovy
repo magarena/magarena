11f5e7ee9c5e2dedd8b6f99f43059616c8382e4f
melvin <melvinzhang@gmail.com>
2013-02-20 22:15:40 +0800

    added Mizzium Skin

---
+++ release/Magarena/scripts/Mizzium_Skin.groovy
@@ -0,0 +1,59 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Target creature\$ you control gets +0/+1 and gains hexproof until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature, 0, 1));
+                    game.doAction(new MagicSetAbilityAction(creature, MagicAbility.Hexproof));
+                }
+            });
+        }
+    },
+    new MagicCardActivation(
+        [
+            MagicManaCost.ONE_BLUE.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Tapping,true),
+        "Overload"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.ONE_BLUE)
+            ];
+        }
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each creature you control gets +0/+1 and gains hexproof until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature, 0, 1));
+                game.doAction(new MagicSetAbilityAction(creature, MagicAbility.Hexproof));
+            }
+        }
+    }
+]
