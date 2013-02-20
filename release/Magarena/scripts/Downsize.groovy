2d9a1ec12e5809fff22e4f762ca40979ce5641f4
melvin <melvinzhang@gmail.com>
2013-02-20 22:15:00 +0800

    added Downsize

---
+++ release/Magarena/scripts/Downsize.groovy
@@ -0,0 +1,57 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Target creature\$ you don't control gets -4/-0 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature, -4, 0));
+                }
+            });
+        }
+    },
+    new MagicCardActivation(
+        [
+            MagicManaCost.TWO_BLUE.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Removal,true),
+        "Overload"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.TWO_BLUE)
+            ];
+        }
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each creature you don't control gets -4/-0 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS);
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature, -4, 0));
+            }
+        }
+    }
+]
