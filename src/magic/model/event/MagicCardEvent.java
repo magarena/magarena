d43f28b229e8080b978c0140ab6c7a03f19fce2b
melvin <melvinzhang@gmail.com>
2013-02-20 22:00:40 +0800

    changed to UNIX line endings

--- src/magic/model/event/MagicCardEvent.java
+++ src/magic/model/event/MagicCardEvent.java
@@ -4,6 +4,5 @@ import magic.model.MagicPayedCost;
 import magic.model.stack.MagicCardOnStack;
 
 public interface MagicCardEvent {
-
     MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost);
 }
