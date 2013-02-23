af68a753560bf07fbb46892749f239a00c110673
melvin <melvinzhang@gmail.com>
2013-02-23 11:20:05 +0800

    added Vandalblast

---
+++ release/Magarena/scripts/Vandalblast.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicCardActivation(
+        [
+            MagicManaCost.FOUR_RED.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Tapping,true),
+        "Overload"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.FOUR_RED)
+            ];
+        }
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy each artifact you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets= 
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ARTIFACT_YOUR_OPPONENT_CONTROLS);
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
