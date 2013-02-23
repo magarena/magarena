2dbb06b7be94eb60597d1f22a223c868650f73f6
melvin <melvinzhang@gmail.com>
2013-02-23 11:19:57 +0800

    added Street Spasm

---
+++ release/Magarena/scripts/Street_Spasm.groovy
@@ -0,0 +1,63 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int x = payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_WITHOUT_FLYING_YOU_DONT_CONTROL,
+                new MagicDamageTargetPicker(x),
+                x,
+                this,
+                "SN deals " + x + " damage to target creature\$ without flying you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getRefInt(),false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    },
+    new MagicCardActivation(
+        [
+            MagicManaCost.X_X_RED_RED.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Tapping,true),
+        "Overload"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.X_X_RED_RED)
+            ];
+        }
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int x = payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                x,
+                this,
+                "SN deals " + x + " damage to each creature\$ without flying you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_WITHOUT_FLYING_YOUR_OPPONENT_CONTROLS);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getRefInt(),false);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
