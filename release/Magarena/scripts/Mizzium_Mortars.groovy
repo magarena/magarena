8aa2231c37cf74f8ffe6b4b7273397df066c4328
melvin <melvinzhang@gmail.com>
2013-02-23 11:19:48 +0800

    added Mizzium Mortars

---
+++ release/Magarena/scripts/Mizzium_Mortars.groovy
@@ -0,0 +1,59 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 4 damage to target creature\$ you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTarget(game,choiceResults,0,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,4,false);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    },
+    new MagicCardActivation(
+        [
+            MagicManaCost.THREE_RED_RED_RED.getCondition()
+        ],
+        new MagicActivationHints(MagicTiming.Tapping,true),
+        "Overload"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, source.getController(), MagicManaCost.THREE_RED_RED_RED)
+            ];
+        }
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "SN deals 4 damage to each creature\$ you don't control."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,4,false);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
