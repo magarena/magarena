6c782c082a4ca461c14cb7ac302f0ae8c3bf4ff1
melvin <melvinzhang@gmail.com>
2013-02-25 13:36:44 +0800

    added Sharuum the Hegemon

---
+++ release/Magarena/scripts/Sharuum_the_Hegemon.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(
+                    MagicTargetChoice.TARGET_ARTIFACT_CARD_FROM_GRAVEYARD,
+                ),
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "PN may\$ return target artifact card\$ from your graveyard to the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            if (MagicMayChoice.isYesChoice(choiceResults[0])) {
+                event.processTargetCard(game,choiceResults,1,new MagicCardAction() {
+                    public void doAction(final MagicCard card) {
+                        game.doAction(new MagicReanimateAction(event.getPlayer(),card,MagicPlayCardAction.NONE));
+                    }
+                });
+            }
+        }
+    }
+]
