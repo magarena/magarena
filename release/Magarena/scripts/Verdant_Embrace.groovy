f994c097830730112888b372ba420d0134fb488a
melvin <melvinzhang@gmail.com>
2013-03-06 22:20:36 +0800

    added Verdant Embrace

---
+++ release/Magarena/scripts/Verdant_Embrace.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.getEnchantedCreature().isValid() ?
+                new MagicEvent(
+                    permanent.getEnchantedCreature(),
+                    this,
+                    "PN puts a 1/1 green Saproling creature token onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Saproling"),
+            ));
+        }
+    }
+]
