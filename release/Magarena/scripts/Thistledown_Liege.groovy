4667939bf22ee1023dbbe5a1d11051bf59511020
melvin <melvinzhang@gmail.com>
2013-03-08 14:58:42 +0800

    added missing groovy script for Thistledown Liege

---
+++ release/Magarena/scripts/Thistledown_Liege.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_WHITE_CREATURE_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_BLUE_CREATURE_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    }
+]
