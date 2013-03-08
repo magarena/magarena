973b44c4eac4bf532f44efd49d829a91829176a2
melvin <melvinzhang@gmail.com>
2013-03-08 11:02:14 +0800

    added stub for MTD(f) AI

---
+++ src/magic/ai/MTDF.java
@@ -0,0 +1,16 @@
+package magic.ai;
+
+import magic.model.MagicGame;
+import magic.model.MagicGameLog;
+import magic.model.MagicPlayer;
+import magic.model.event.MagicEvent;
+
+/*
+ * MTD(f) algorithm by Aske Plaat
+ * https://askeplaat.wordpress.com/publications/mtdf-algorithm/
+ */
+public class MTDF implements MagicAI {
+    public Object[] findNextEventChoiceResults(final MagicGame sourceGame, final MagicPlayer scorePlayer) {
+        return new Object[0];
+    }
+}
