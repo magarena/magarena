d8074b61ccbcca7d8d2cf6cbf4bc36f0a2048f21
melvin <melvinzhang@gmail.com>
2013-03-08 17:01:44 +0800

    added Thallid

---
+++ release/Magarena/scripts/Thallid.groovy
@@ -0,0 +1,47 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a spore counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,1,true));
+        }
+    },
+    new MagicPermanentActivation(
+            [MagicConditionFactory.ChargeCountersAtLeast(3)],
+            new MagicActivationHints(MagicTiming.Token),
+            "Token") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,3)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a a 1/1 green Saproling creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Saproling")));
+        }
+    }
+]
