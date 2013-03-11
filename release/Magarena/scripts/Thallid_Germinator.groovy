44ab978a639d3a8a3e5421f01182f686b32b191d
melvin <melvinzhang@gmail.com>
2013-03-11 13:27:54 +0800

    added Thallid Shell-Dweller and Thallid Germinator

---
+++ release/Magarena/scripts/Thallid_Germinator.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.ONE_SAPROLING_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_SAPROLING
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1+1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,1));
+                }
+            });
+        }
+    }
+]
