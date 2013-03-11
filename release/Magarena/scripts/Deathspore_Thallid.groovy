ad69c56ad47e5b3f6b1645f8fe75979632623425
melvin <melvinzhang@gmail.com>
2013-03-11 15:33:19 +0800

    added missing groovy code for Deathspore Thallid

---
+++ release/Magarena/scripts/Deathspore_Thallid.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.ONE_SAPROLING_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "-1/-1") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_SAPROLING
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(1,1),
+                this,
+                "Target creature\$ gets -1/-1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            event.processTargetPermanent(game,choiceResults,0,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-1,-1));
+                }
+            });
+        }
+    }
+]
