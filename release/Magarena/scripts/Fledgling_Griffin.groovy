f83e224c550096d22cf3bbcd2f63aa037eeba181
melvin <melvinzhang@gmail.com>
2013-03-13 21:27:02 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Fledgling_Griffin.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "SN gains flying until end of turn."
+            );
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicSetAbilityAction(event.getPermanent(),MagicAbility.Flying));
+        }        
+    }
+]
