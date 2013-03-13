e9d3e5c060173c61e20505f65f83a7af2028061f
melvin <melvinzhang@gmail.com>
2013-03-13 21:25:40 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Flight_of_Fancy.groovy
+++ release/Magarena/scripts/Flight_of_Fancy.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Flight_of_Fancy {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -24,5 +15,5 @@ public class Flight_of_Fancy {
                 final Object[] choiceResults) {
             game.doAction(new MagicDrawAction(event.getPlayer(),2));
         }        
-    };
-}
+    }
+]
