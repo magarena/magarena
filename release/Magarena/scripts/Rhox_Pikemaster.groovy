27139fa6f4e777e9b6c3d785238e693bafd90080
melvin <melvinzhang@gmail.com>
2013-03-13 20:59:53 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Rhox_Pikemaster.groovy
+++ release/Magarena/scripts/Rhox_Pikemaster.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Set;
-
-public class Rhox_Pikemaster {
-    public static final MagicStatic S = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.Ability, 
         MagicTargetFilter.TARGET_SOLDIER_YOU_CONTROL) {
         @Override
@@ -21,6 +10,5 @@ public class Rhox_Pikemaster {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    
-}
+    }
+]
