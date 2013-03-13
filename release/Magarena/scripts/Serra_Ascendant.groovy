71ae39a5a92d0455db6e7efdbffccf9b439917a5
melvin <melvinzhang@gmail.com>
2013-03-13 22:19:32 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Serra_Ascendant.groovy
+++ release/Magarena/scripts/Serra_Ascendant.groovy
@@ -1,29 +1,18 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
-import java.util.Set;
-
-public class Serra_Ascendant {
-    public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
+[
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             if (permanent.getController().getLife() >= 30) {
                 pt.add(5,5);
             }
         }
-    };
-    
-    public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
+    },
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             if (permanent.getController().getLife() >= 30) {
                 flags.add(MagicAbility.Flying);
             }
         }
-    };
-}
+    }
+]
