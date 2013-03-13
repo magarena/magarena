7742af46593ecb53aa85d89d4443d2efa0f84226
melvin <melvinzhang@gmail.com>
2013-03-13 20:58:34 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Battleground_Geist.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,0);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    }
+]
