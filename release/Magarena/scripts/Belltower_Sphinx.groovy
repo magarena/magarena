1c6483d82a8f4760646855254d371b2b08b80fe5
melvin <melvinzhang@gmail.com>
2013-03-13 22:22:20 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Belltower_Sphinx.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final int amount = damage.getDealtAmount();
+            if (damage.getTarget() == permanent) {
+                game.doAction(new MagicMillLibraryAction(damage.getSource().getController(),amount));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
