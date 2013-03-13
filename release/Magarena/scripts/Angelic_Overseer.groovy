7a0b76ea1d8a035bcf78e9ac4b1e19f74d190df4
melvin <melvinzhang@gmail.com>
2013-03-13 20:59:08 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Angelic_Overseer.groovy
+++ release/Magarena/scripts/Angelic_Overseer.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
-import java.util.Set;
-
-public class Angelic_Overseer {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.Ability) {
+[
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             for (final MagicPermanent target : permanent.getController().getPermanents()) {
@@ -20,5 +10,5 @@ public class Angelic_Overseer {
                 }
             }
         }
-    };
-}
+    }
+]
