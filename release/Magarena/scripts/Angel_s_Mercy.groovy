eaf001e3fcce63b15c9440731b87dca5c65630cb
melvin <melvinzhang@gmail.com>
2013-03-13 21:24:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Angel_s_Mercy.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN gains 7 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),7));
+        }
+    }
+]
