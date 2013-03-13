7ca328119a716e7ecdd736e55daf712ae5475f86
melvin <melvinzhang@gmail.com>
2013-03-13 20:57:51 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Wizened_Cenn.groovy
+++ release/Magarena/scripts/Wizened_Cenn.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.MagicAbility;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-public class Wizened_Cenn {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT,
         MagicTargetFilter.TARGET_KITHKIN_YOU_CONTROL) {
         @Override
@@ -20,5 +10,5 @@ public class Wizened_Cenn {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-}
+    }
+]
