cd75692d42b0f3f8a6c532969c63d3c511949061
melvin <melvinzhang@gmail.com>
2013-03-13 22:15:49 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Lord_of_the_Unreal.groovy
+++ release/Magarena/scripts/Lord_of_the_Unreal.groovy
@@ -1,29 +1,18 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Set;
-
-public class Lord_of_the_Unreal {
-    public static final MagicStatic S = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_ILLUSION_YOU_CONTROL) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.Ability, 
         MagicTargetFilter.TARGET_ILLUSION_YOU_CONTROL) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Hexproof);
         }
-    };
-}
+    }
+]
