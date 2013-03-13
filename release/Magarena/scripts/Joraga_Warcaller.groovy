78bff5e2014d0ce87ce5f349e00f0b52865948c0
melvin <melvinzhang@gmail.com>
2013-03-13 21:25:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Joraga_Warcaller.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_ELF_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final int amount = source.getCounters(MagicCounterType.PlusOne);
+            pt.add(amount, amount);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    }
+]
