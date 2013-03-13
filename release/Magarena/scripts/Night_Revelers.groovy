d194d24766e96bf3a77a39d7aeb36254ff02425e
melvin <melvinzhang@gmail.com>
2013-03-13 20:49:03 +0800

    convert Night Revelers to groovy

--- release/Magarena/scripts/Night_Revelers.groovy
+++ release/Magarena/scripts/Night_Revelers.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
-import java.util.Set;
-
-public class Night_Revelers {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.Ability) {
+[
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             for (final MagicPermanent target : permanent.getOpponent().getPermanents()) {
@@ -19,5 +9,5 @@ public class Night_Revelers {
                 }
             }
         }
-    };
-}
+    }
+]
