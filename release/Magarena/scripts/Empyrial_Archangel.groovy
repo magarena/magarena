e8d20c6133e373a339675376b09f44a70465a9c5
melvin <melvinzhang@gmail.com>
2013-03-13 21:21:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Empyrial_Archangel.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(2) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (permanent.isController(damage.getTarget())) {
+                // Replacement effect. Generates no event or action.
+                damage.setTarget(permanent);                
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
