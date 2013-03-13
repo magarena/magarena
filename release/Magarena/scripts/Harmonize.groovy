3fc4237e9d0b48b03f26f4bc1127247275cf4f4a
melvin <melvinzhang@gmail.com>
2013-03-13 21:20:49 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Harmonize.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Draw three cards."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),3));
+        }
+    }
+]
