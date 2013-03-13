91cc489876bbd83ffd72994e3ba32d8ab984c908
melvin <melvinzhang@gmail.com>
2013-03-13 22:19:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Defang.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(1) {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicDamage damage) {
+            if (damage.getSource() == permanent.getEnchantedCreature()) {
+                // Replacement effect. Generates no event or action.
+                damage.setAmount(0);
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
