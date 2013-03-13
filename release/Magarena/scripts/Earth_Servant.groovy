c34fa80b1c47753137448dca385196220a67b7f2
melvin <melvinzhang@gmail.com>
2013-03-13 21:00:21 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Earth_Servant.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final MagicGame game = source.getGame();
+            final Collection<MagicPermanent> targets =
+                    game.filterPermanents(permanent.getController(),MagicTargetFilter.TARGET_MOUNTAIN_YOU_CONTROL);
+            pt.add(0,targets.size());
+        }
+    }
+]
