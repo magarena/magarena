a4b309bdfada070f843aa41600cfb8830832cf61
melvin <melvinzhang@gmail.com>
2013-03-13 21:22:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Time_Walk.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Take an extra turn after this one."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicChangeExtraTurnsAction(event.getPlayer(),1));
+        }
+    }
+]
