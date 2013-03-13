30e513c2066e8f3a76a0c3a3e4a06f6378a9e801
melvin <melvinzhang@gmail.com>
2013-03-13 22:23:26 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Duskworker.groovy
+++ release/Magarena/scripts/Duskworker.groovy
@@ -1,13 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicRegenerateAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-
-public class Duskworker {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocked) {
             return (permanent == blocked) ?
@@ -18,7 +10,6 @@ public class Duskworker {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -26,5 +17,5 @@ public class Duskworker {
                 final Object[] choiceResults) {
             game.doAction(new MagicRegenerateAction(event.getPermanent()));
         }
-    };
-}
+    }
+]
