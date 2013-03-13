85b7c535a2b039e0ece26a62285866d49535db50
melvin <melvinzhang@gmail.com>
2013-03-13 20:59:41 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kulrath_Knight.groovy
+++ release/Magarena/scripts/Kulrath_Knight.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Set;
-
-public class Kulrath_Knight {
-    public static final MagicStatic S2 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.Ability, 
         MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS) {
         @Override
@@ -21,5 +10,5 @@ public class Kulrath_Knight {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return target.hasCounters();
         }
-    };
-}
+    }
+]
