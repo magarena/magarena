b0100adc658887a2364e58964d08d4503a008a66
melvin <melvinzhang@gmail.com>
2013-03-13 21:00:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Platinum_Angel.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicIfPlayerWouldLoseTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer[] playerRef) {
+            final MagicPlayer controller = permanent.getController();
+            if (controller == playerRef[0]) {
+                playerRef[0] = MagicPlayer.NONE;
+            }            
+            return MagicEvent.NONE;
+        }
+    }
+]
