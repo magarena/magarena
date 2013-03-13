263ff931e3a81088f23a682cd67f29cef076be01
melvin <melvinzhang@gmail.com>
2013-03-13 22:21:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Trip_Noose.groovy
@@ -0,0 +1,17 @@
+[
+    new MagicTapCreatureActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{2}")
+        ],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,source.getController(),
+                MagicManaCost.create("{2}"))
+            ];
+        }
+    }
+]
