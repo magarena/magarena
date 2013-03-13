cf1c5d22bc3d2f49b74129ea7e20104d2c02c953
melvin <melvinzhang@gmail.com>
2013-03-13 22:12:50 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/AEther_Figment.groovy
+++ release/Magarena/scripts/AEther_Figment.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class AEther_Figment {                 
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -24,5 +14,5 @@ public class AEther_Figment {
             }
             return MagicEvent.NONE;
         }
-    };
-}
+    }
+]
