79042752d73b8a4a3b4ea9dea2bbc77776c9ab0a
melvin <melvinzhang@gmail.com>
2013-03-13 22:15:19 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Butcher_s_Cleaver.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_CREATURE) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.LifeLink);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getEquippedCreature().hasSubType(MagicSubType.Human) &&
+                   target == source.getEquippedCreature();
+        }
+    }
+]
