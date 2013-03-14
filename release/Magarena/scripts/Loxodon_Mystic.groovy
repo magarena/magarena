3b5fe61d72ee6acfa81f4443ead18f5753c11190
melvin <melvinzhang@gmail.com>
2013-03-14 13:25:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Loxodon_Mystic.groovy
@@ -0,0 +1,17 @@
+[
+    new MagicTapCreatureActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{W}")
+        ],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,source.getController(),
+                MagicManaCost.create("{W}"))
+            ];
+        }
+    };
+]
