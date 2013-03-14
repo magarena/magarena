b4f42cfe6474405be9d78cb7610fd0251a5a240d
melvin <melvinzhang@gmail.com>
2013-03-14 21:34:01 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Favor_of_the_Woods.groovy
+++ release/Magarena/scripts/Favor_of_the_Woods.groovy
@@ -1,13 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBlocksTrigger;
-
-public class Favor_of_the_Woods {
-    public static final MagicWhenBlocksTrigger T = new MagicWhenBlocksTrigger() {
+[
+    new MagicWhenBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -21,7 +13,6 @@ public class Favor_of_the_Woods {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -29,5 +20,5 @@ public class Favor_of_the_Woods {
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
         }
-    };
-}
+    }
+]
