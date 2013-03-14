989d0b7933019def97472f92adc88b58269dd313
melvin <melvinzhang@gmail.com>
2013-03-14 13:22:51 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Skullclamp.groovy
+++ release/Magarena/scripts/Skullclamp.groovy
@@ -1,13 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Skullclamp {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
             return (permanent.getEquippedCreature()==died)?
@@ -25,5 +17,5 @@ public class Skullclamp {
                 final Object[] choiceResults) {
             game.doAction(new MagicDrawAction(event.getPlayer(),2));
         }
-    };
-}
+    }
+]
