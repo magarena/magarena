3e534217d0ba90c02d34879a001ee915948efd46
melvin <melvinzhang@gmail.com>
2013-03-14 13:27:36 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Seller_of_Songbirds.groovy
+++ release/Magarena/scripts/Seller_of_Songbirds.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Seller_of_Songbirds {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -18,7 +8,6 @@ public class Seller_of_Songbirds {
                 "PN puts a 1/1 white Bird creature token with flying onto the battlefield."
             );
         }
-
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -26,5 +15,5 @@ public class Seller_of_Songbirds {
                 final Object[] choiceResults) {
             game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Bird1")));
         }
-    };
-}
+    }
+]
