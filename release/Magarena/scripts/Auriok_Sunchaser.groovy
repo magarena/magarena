2285e2a37436201690893942f50a6b5d7a06dff7
melvin <melvinzhang@gmail.com>
2013-03-14 13:28:21 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Auriok_Sunchaser.groovy
+++ release/Magarena/scripts/Auriok_Sunchaser.groovy
@@ -1,30 +1,18 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.condition.MagicCondition;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
-import java.util.Set;
-
-public class Auriok_Sunchaser {
-    public static final MagicStatic S1 = new MagicStatic(MagicLayer.Ability) {
+[
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
                 flags.add(MagicAbility.Flying);
             }
         }
-    };
-    
-    public static final MagicStatic S2 = new MagicStatic(MagicLayer.ModPT) {
+    },
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
                 pt.add(2,2);
             }
         }
-    };
-}
+    }
+]
