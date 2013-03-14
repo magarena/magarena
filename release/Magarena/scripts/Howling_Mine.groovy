68b5b2a1b203de911eb1be387bd74035ddb31ce9
melvin <melvinzhang@gmail.com>
2013-03-14 13:25:52 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Howling_Mine.groovy
+++ release/Magarena/scripts/Howling_Mine.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Howling_Mine {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isUntapped() ?
@@ -20,11 +11,10 @@ public class Howling_Mine {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
             final MagicPlayer player = event.getPlayer();
             game.doAction(new MagicDrawAction(player,1));
         }
-    };
-}
+    }
+]
