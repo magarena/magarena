4c10efdf1a2e26352122b61b20c70f2e62ffffde
melvin <melvinzhang@gmail.com>
2013-03-14 13:26:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Attended_Knight.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 white Soldier creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Soldier")));
+        }
+    }
+]
