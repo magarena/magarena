d81570a501c6ceaa45d76b3adf9b4bb0df678a72
melvin <melvinzhang@gmail.com>
2013-03-14 13:29:29 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Ichor_Rats.groovy
+++ release/Magarena/scripts/Ichor_Rats.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangePoisonAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Ichor_Rats {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -25,5 +16,5 @@ public class Ichor_Rats {
             game.doAction(new MagicChangePoisonAction(game.getPlayer(0),1));
             game.doAction(new MagicChangePoisonAction(game.getPlayer(1),1));
         }
-    };
-}
+    }
+]
