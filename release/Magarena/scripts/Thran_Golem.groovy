57f83f8a3b8e5e9d4f24994b679c65536b1efc6e
melvin <melvinzhang@gmail.com>
2013-03-14 13:27:12 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Thran_Golem.groovy
+++ release/Magarena/scripts/Thran_Golem.groovy
@@ -1,24 +1,13 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-
-import java.util.Set;
-
-public class Thran_Golem {
-    public static final MagicStatic S1 = new MagicStatic(MagicLayer.ModPT) {
+[
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             if (permanent.isEnchanted()) {
                 pt.add(2,2);
             }
         }    
-    };
-    
-    public static final MagicStatic S2 = new MagicStatic(MagicLayer.Ability) {
+    },
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             if (permanent.isEnchanted()) {
@@ -27,5 +16,5 @@ public class Thran_Golem {
                 flags.add(MagicAbility.Trample);
             }
         }
-    };
-}
+    }
+]
