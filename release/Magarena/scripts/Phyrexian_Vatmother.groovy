24e306ff59c3aa9c493aff5393266652f1afda16
melvin <melvinzhang@gmail.com>
2013-03-14 13:21:54 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Phyrexian_Vatmother.groovy
+++ release/Magarena/scripts/Phyrexian_Vatmother.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangePoisonAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Phyrexian_Vatmother {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -27,5 +17,5 @@ public class Phyrexian_Vatmother {
                 final Object[] choiceResults) {
             game.doAction(new MagicChangePoisonAction(event.getPlayer(),1));
         }
-    };
-}
+    }
+]
