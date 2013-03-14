e34847c6ca824ec4f7d322971a4e45fac6440e78
melvin <melvinzhang@gmail.com>
2013-03-14 21:35:27 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Black_Cat.groovy
+++ release/Magarena/scripts/Black_Cat.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.event.MagicDiscardEvent;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDiesTrigger;
-
-public class Black_Cat {
-    public static final MagicWhenDiesTrigger T = new MagicWhenDiesTrigger() {
+[
+    new MagicWhenDiesTrigger() {
         @Override
         public MagicEvent getEvent(final MagicPermanent permanent) {
             final MagicPlayer opponent = permanent.getOpponent();
@@ -18,17 +9,17 @@ public class Black_Cat {
                     this,
                     opponent + " discards a card at random.");
         }
-
         @Override
         public void executeEvent(
                 final MagicGame game,
                 final MagicEvent event,
                 final Object[] choiceResults) {
             game.addEvent(new MagicDiscardEvent(
-                    event.getSource(),
-                    event.getPlayer(),
-                    1,
-                    true));
+                event.getSource(),
+                event.getPlayer(),
+                1,
+                true
+            ));
         }
-    };
-}
+    }
+]
