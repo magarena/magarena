3b73c14a5da25b9ee230e0b8d02fa6bce44afd78
melvin <melvinzhang@gmail.com>
2013-03-14 21:35:09 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Knightly_Valor.groovy
+++ release/Magarena/scripts/Knightly_Valor.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Knightly_Valor {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -25,5 +15,5 @@ public class Knightly_Valor {
                 final Object[] choiceResults) {
             game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Knight2")));
         }
-    };
-}
+    }
+]
