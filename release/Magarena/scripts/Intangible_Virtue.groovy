bd0db3b91fcb73c43dc5d8db8ad7bcf1290a02a8
melvin <melvinzhang@gmail.com>
2013-03-14 13:23:29 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Intangible_Virtue.groovy
+++ release/Magarena/scripts/Intangible_Virtue.groovy
@@ -1,31 +1,18 @@
-
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Set;
-
-public class Intangible_Virtue {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_CREATURE_TOKEN_YOU_CONTROL) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
         }
-    };
-    
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
             MagicLayer.Ability, 
             MagicTargetFilter.TARGET_CREATURE_TOKEN_YOU_CONTROL) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Vigilance);
         }
-    };
-}
+    }
+]
