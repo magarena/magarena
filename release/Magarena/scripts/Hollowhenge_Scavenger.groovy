123475906c00c4507e15778f4e44302f2102e81c
melvin <melvinzhang@gmail.com>
2013-03-14 13:28:33 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Hollowhenge_Scavenger.groovy
+++ release/Magarena/scripts/Hollowhenge_Scavenger.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Hollowhenge_Scavenger {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return game.getCreatureDiedThisTurn() ?
@@ -26,5 +17,5 @@ public class Hollowhenge_Scavenger {
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),5));
         }
-    };
-}
+    }
+]
