3cdf8f038e5d55c30ea266ad986b1e11df51832a
melvin <melvinzhang@gmail.com>
2013-03-14 21:36:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Predator_s_Gambit.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_CREATURE) {
+        @Override
+        public void modAbilityFlags(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Intimidate);
+        }
+        @Override
+        public boolean condition(
+                final MagicGame game,
+                final MagicPermanent source,
+                final MagicPermanent target) {
+            return target == source.getEnchantedCreature() &&
+                   source.getController().getNrOfPermanentsWithType(MagicType.Creature) == 1;
+        }
+    }
+]
