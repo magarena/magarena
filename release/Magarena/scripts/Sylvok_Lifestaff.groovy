03d7ccd212b93623b794bda075c18566c6e967d2
melvin <melvinzhang@gmail.com>
2013-03-14 13:26:21 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Sylvok_Lifestaff.groovy
+++ release/Magarena/scripts/Sylvok_Lifestaff.groovy
@@ -1,13 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Sylvok_Lifestaff {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
             return (permanent.getEquippedCreature()==died) ?
@@ -25,5 +17,5 @@ public class Sylvok_Lifestaff {
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
         }
-    };
-}
+    }
+]
