d08edf496594bcf113688fd11d8c41e5769a1814
melvin <melvinzhang@gmail.com>
2013-03-14 21:34:55 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Phyrexian_Rager.groovy
+++ release/Magarena/scripts/Phyrexian_Rager.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
-public class Phyrexian_Rager {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -27,5 +16,5 @@ public class Phyrexian_Rager {
             game.doAction(new MagicDrawAction(player,1));
             game.doAction(new MagicChangeLifeAction(player,-1));
         }
-    };
-}
+    }
+]
