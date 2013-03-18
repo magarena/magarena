f856f7390db13e6041385ff405064bb129c75515
melvin <melvinzhang@gmail.com>
2013-03-18 14:04:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Night_s_Whisper.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN draws two cards and loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicDrawAction(player,2));
+            game.doAction(new MagicChangeLifeAction(player,-2));
+        }
+    }
+]
