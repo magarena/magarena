509e1a42d07e43daa6f645a4ef4cb185ebcc3e75
melvin <melvinzhang@gmail.com>
2013-03-18 14:04:18 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Battered_Golem.groovy
+++ release/Magarena/scripts/Battered_Golem.groovy
@@ -1,13 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicUntapAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Battered_Golem {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (permanent != otherPermanent &&
@@ -26,5 +18,5 @@ public class Battered_Golem {
                 final Object[] choiceResults) {
             game.doAction(new MagicUntapAction(event.getPermanent()));
         }
-    };
-}
+    }
+]
