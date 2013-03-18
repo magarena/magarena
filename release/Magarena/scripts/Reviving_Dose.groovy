4965fdc33fa3c96a6e06cbf99e01c54ebc8f2fd2
melvin <melvinzhang@gmail.com>
2013-03-18 14:04:29 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Reviving_Dose.groovy
+++ release/Magarena/scripts/Reviving_Dose.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
-public class Reviving_Dose {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -27,5 +16,5 @@ public class Reviving_Dose {
             game.doAction(new MagicChangeLifeAction(player,3));
             game.doAction(new MagicDrawAction(player,1));
         }
-    };
-}
+    }
+]
