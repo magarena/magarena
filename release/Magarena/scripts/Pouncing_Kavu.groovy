f51917a4312060b28a463f41d65645144850f90c
melvin <melvinzhang@gmail.com>
2013-03-18 17:52:57 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Pouncing_Kavu.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+            final MagicGame game,
+            final MagicPermanent permanent,
+            final MagicPlayer player) {   
+            if (permanent.isKicked()) {
+                game.doAction(new MagicChangeCountersAction(
+                    permanent,
+                    MagicCounterType.PlusOne,
+                    2,
+                    true
+                ));
+                game.doAction(new MagicSetAbilityAction(permanent,MagicAbility.Haste,MagicStatic.Forever));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
