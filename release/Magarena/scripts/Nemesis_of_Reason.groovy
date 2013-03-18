a816c5b84e820ad5d19d49b27458f69e77d7f83f
melvin <melvinzhang@gmail.com>
2013-03-18 21:18:20 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Nemesis_of_Reason.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPlayer player = permanent.getController();
+            return (permanent == creature) ?
+                new MagicEvent(
+                    permanent,
+                    player.getOpponent(),
+                    this,
+                    "PN puts the top ten cards of his or her library into his or her graveyard."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicMillLibraryAction(event.getPlayer(),10));
+        }
+    }
+]
