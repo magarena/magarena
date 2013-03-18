c32268a63e21d6bc5a471fe5f995936180de0adc
melvin <melvinzhang@gmail.com>
2013-03-18 17:53:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Midnight_Haunting.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts two 1/1 white Spirit creature " +
+                "tokens with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Spirit2"),
+                2
+            ));
+        }
+    }
+]
