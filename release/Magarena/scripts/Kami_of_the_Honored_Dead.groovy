12042e6aad99db722ac6315f4a3e69b3e4f61112
melvin <melvinzhang@gmail.com>
2013-03-18 21:19:29 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kami_of_the_Honored_Dead.groovy
+++ release/Magarena/scripts/Kami_of_the_Honored_Dead.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-public class Kami_of_the_Honored_Dead {
-    public static final MagicWhenDamageIsDealtTrigger T2 = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final int amount = damage.getDealtAmount();
@@ -21,15 +12,15 @@ public class Kami_of_the_Honored_Dead {
                 ) :
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
                 final MagicEvent event,
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeLifeAction(
-                    event.getPlayer(),
-                    event.getRefInt()));
+                event.getPlayer(),
+                event.getRefInt()
+            ));
         }
-    };
-}
+    }
+]
