2ebd655814b0fb3e8babac804c4afee9b26bbfb6
melvin <melvinzhang@gmail.com>
2013-03-18 14:07:19 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Ascendant_Evincar.groovy
+++ release/Magarena/scripts/Ascendant_Evincar.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-
-public class Ascendant_Evincar {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_BLACK_CREATURE) {
         @Override
@@ -20,13 +10,13 @@ public class Ascendant_Evincar {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_NONBLACK_CREATURE) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(-1,-1);
         }
-    };
-}
+    }
+]
