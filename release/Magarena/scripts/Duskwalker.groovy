74e6db2856941dbb7a1651936f281c103788dc7d
melvin <melvinzhang@gmail.com>
2013-03-18 17:51:48 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Duskwalker.groovy
+++ release/Magarena/scripts/Duskwalker.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicStatic;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Duskwalker {                 
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -28,5 +15,5 @@ public class Duskwalker {
             }
             return MagicEvent.NONE;
         }
-    };
-}
+    }
+]
