13e415980b987ae58acc1d037fffa20c1e86d804
melvin <melvinzhang@gmail.com>
2013-03-18 17:49:58 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Gideon_s_Avenger.groovy
+++ release/Magarena/scripts/Gideon_s_Avenger.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesTappedTrigger;
-
-public class Gideon_s_Avenger {
-    public static final MagicWhenBecomesTappedTrigger T = new MagicWhenBecomesTappedTrigger() {
+[
+    new MagicWhenBecomesTappedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
             return (permanent.isEnemy(tapped) && tapped.isCreature()) ?
@@ -27,5 +17,5 @@ public class Gideon_s_Avenger {
                 final Object[] choiceResults) {
             game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
         }
-    };
-}
+    }
+]
