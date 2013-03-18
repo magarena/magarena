0589a9544175c91c9c86e604facfe65398ca73c1
melvin <melvinzhang@gmail.com>
2013-03-18 14:06:30 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Elesh_Norn__Grand_Cenobite.groovy
+++ release/Magarena/scripts/Elesh_Norn__Grand_Cenobite.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-public class Elesh_Norn__Grand_Cenobite {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
         @Override
@@ -19,13 +10,13 @@ public class Elesh_Norn__Grand_Cenobite {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(-2,-2);
         }
-    };
-}
+    }
+]
