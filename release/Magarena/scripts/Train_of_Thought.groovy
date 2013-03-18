76ae7a727881919a77bdd5d85a470eeea48f9ddc
melvin <melvinzhang@gmail.com>
2013-03-18 14:03:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Train_of_Thought.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicKickerChoice(MagicManaCost.create("{1}{U}"), true, true),
+                this,
+                "Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event,
+                final Object[] choiceResults) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+        }
+    }
+]
