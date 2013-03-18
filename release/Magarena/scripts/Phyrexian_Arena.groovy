88d383b46fc3618240f76215880c31e9735b22a3
melvin <melvinzhang@gmail.com>
2013-03-18 14:09:21 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Phyrexian_Arena.groovy
+++ release/Magarena/scripts/Phyrexian_Arena.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Phyrexian_Arena {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -29,5 +18,5 @@ public class Phyrexian_Arena {
             game.doAction(new MagicDrawAction(player,1));
             game.doAction(new MagicChangeLifeAction(player,-1));
         }
-    };
-}
+    }
+]
