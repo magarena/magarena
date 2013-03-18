524ed7b9bf071022d06daeb2918c8e1b1c675c48
melvin <melvinzhang@gmail.com>
2013-03-18 14:07:33 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Fire_Servant.groovy
+++ release/Magarena/scripts/Fire_Servant.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicColor;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicSource;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
-
-public class Fire_Servant {
-    public static final MagicIfDamageWouldBeDealtTrigger T = new MagicIfDamageWouldBeDealtTrigger(3) {
+[
+    new MagicIfDamageWouldBeDealtTrigger(3) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicSource source=damage.getSource();
@@ -22,9 +12,5 @@ public class Fire_Servant {
             }            
             return MagicEvent.NONE;
         }
-        @Override
-        public void executeEvent(final MagicGame game,final MagicEvent event,final Object[] choiceResults) {
-        
-        }
-    };
-}
+    }
+]
