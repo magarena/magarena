7c3c7b02d9079908ad9a3bd3a75746e90365e16f
melvin <melvinzhang@gmail.com>
2013-03-18 14:05:39 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Fists_of_Ironwood.groovy
+++ release/Magarena/scripts/Fists_of_Ironwood.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokensAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
-public class Fists_of_Ironwood {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -30,5 +19,5 @@ public class Fists_of_Ironwood {
                 2
             ));
         }
-    };
-}
+    }
+]
