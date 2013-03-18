04225d39c79eac5dfbc5991270603e5503f99193
melvin <melvinzhang@gmail.com>
2013-03-18 17:51:59 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Lavaborn_Muse.groovy
+++ release/Magarena/scripts/Lavaborn_Muse.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Lavaborn_Muse {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
             return (permanent.getController()!=player&&player.getHandSize()<3) ?
@@ -21,7 +10,6 @@ public class Lavaborn_Muse {
                         "SN deals 3 damage to PN."):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -30,5 +18,5 @@ public class Lavaborn_Muse {
             final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),3);
             game.doAction(new MagicDealDamageAction(damage));
         }
-    };
-}
+    }
+]
