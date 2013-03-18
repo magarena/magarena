d074bf18960e493f38698998b354b953a908bbe5
melvin <melvinzhang@gmail.com>
2013-03-18 17:50:38 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Day_of_Judgment.groovy
+++ release/Magarena/scripts/Day_of_Judgment.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicTarget;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-
-public class Day_of_Judgment {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -30,5 +16,5 @@ public class Day_of_Judgment {
                 game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
             game.doAction(new MagicDestroyAction(targets));
         }
-    };
-}
+    }
+]
