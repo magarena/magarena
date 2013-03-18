3d0d05df6365b2edcb442dec98970ad3aaf10a0b
melvin <melvinzhang@gmail.com>
2013-03-18 14:07:47 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Hornet_Queen.groovy
+++ release/Magarena/scripts/Hornet_Queen.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Hornet_Queen {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -17,7 +7,6 @@ public class Hornet_Queen {
                     this,
                     "PN puts four 1/1 green Insect creature tokens with flying and deathtouch onto the battlefield.");
         }
-
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -27,5 +16,5 @@ public class Hornet_Queen {
                 game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Insect3")));
             }
         }
-    };
-}
+    }
+]
