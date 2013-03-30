bba3b3d3ad5fcb933369236f4ef1b17a8116b5ca
melvin <melvinzhang@gmail.com>
2013-03-30 11:33:24 +0800

    completed Garruk's Packleader by adding groovy code

---
+++ release/Magarena/scripts/Garruk_s_Packleader.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent otherPermanent) {
+            return (otherPermanent != permanent &&
+                    otherPermanent.isCreature() &&
+                    otherPermanent.getPower() >= 3 &&
+                    otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                    this,
+                    'You may$ draw a card.'
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            }            
+        }        
+    }
+]
