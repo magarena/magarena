20948e5f4ca24492026a1d06b2a96034e8b7c43a
melvin <melvinzhang@gmail.com>
2013-04-01 21:22:02 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Trumpet_Blast.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Attacking creatures get +2/+0 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets=game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilter.TARGET_ATTACKING_CREATURE
+            );
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,2,0));
+            }
+        }
+    }
+]
