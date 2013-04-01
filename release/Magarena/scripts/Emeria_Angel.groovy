253cb624bf907418f4db56a0cd0c2c2b107aea6d
melvin <melvinzhang@gmail.com>
2013-04-01 21:47:52 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Emeria_Angel.groovy
+++ release/Magarena/scripts/Emeria_Angel.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicLandfallTrigger;
-
-public class Emeria_Angel {
-    public static final MagicLandfallTrigger T = new MagicLandfallTrigger() {
+[
+    new MagicLandfallTrigger() {
         @Override
         protected MagicEvent getEvent(final MagicPermanent permanent) {
             return new MagicEvent(
@@ -17,22 +7,23 @@ public class Emeria_Angel {
                 new MagicSimpleMayChoice(
                     MagicSimpleMayChoice.PLAY_TOKEN,
                     1,
-                    MagicSimpleMayChoice.DEFAULT_YES),
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
                 this,
                 "PN may$ put a 1/1 white Bird creature " +
                 "token with flying onto the battlefield."
             );
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
                 final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicPlayTokenAction(
-                        event.getPlayer(),
-                        TokenCardDefinitions.get("Bird1")));
+                    event.getPlayer(),
+                    TokenCardDefinitions.get("Bird1")
+                ));
             }
         }        
-    };
-}
+    }
+]
