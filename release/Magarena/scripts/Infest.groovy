d25d5b8e04725a2ec227bcfb68a5122142f2e297
melvin <melvinzhang@gmail.com>
2013-04-01 13:37:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Infest.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "All creatures get -2/-2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,-2,-2));
+            }
+        }
+    }
+]
