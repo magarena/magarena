9b9cff778ae19967c4104543002439aa4fdd85cb
melvin <melvinzhang@gmail.com>
2013-04-01 13:41:38 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Pierce_Strider.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Target opponent$ loses 3 life."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-3));
+                }
+            });
+        }        
+    }
+]
