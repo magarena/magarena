17683fc5e14ff9ee7b759a0173789b70f1a57c85
melvin <melvinzhang@gmail.com>
2013-04-01 21:18:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ancestral_Recall.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player$ draws three cards."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicDrawAction(player,3));
+                }
+            });
+        }
+    }
+]
