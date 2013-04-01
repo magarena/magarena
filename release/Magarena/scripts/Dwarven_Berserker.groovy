e72ac1f50317f6750a46f4dd9b52754bb1eed975
melvin <melvinzhang@gmail.com>
2013-04-01 21:22:34 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Dwarven_Berserker.groovy
+++ release/Magarena/scripts/Dwarven_Berserker.groovy
@@ -1,22 +1,13 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-
-public class Dwarven_Berserker {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             return (permanent == attacker) ?
                 new MagicEvent(
                     permanent,
                     this,
-                    "SN gets +3/+0 and gains trample until end of turn."):
+                    "SN gets +3/+0 and gains trample until end of turn."
+                ):
                 MagicEvent.NONE;
         }
         
@@ -28,5 +19,5 @@ public class Dwarven_Berserker {
             game.doAction(new MagicChangeTurnPTAction(creature,3,0));
             game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
         }
-    };
-}
+    }
+]
