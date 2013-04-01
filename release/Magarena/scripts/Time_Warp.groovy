f94c31525e2d9456c0563da7cea750b2cb93549b
melvin <melvinzhang@gmail.com>
2013-04-01 21:26:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Time_Warp.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_PLAYER,                    
+                this,
+                "Target player$ takes an extra turn after this one."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeExtraTurnsAction(player,1));
+                }
+            });
+        }
+    }
+]
