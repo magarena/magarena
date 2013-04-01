4bc613744a22263dc7d7ff1f8c35eb7ed92df4e9
melvin <melvinzhang@gmail.com>
2013-04-01 21:43:24 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Boartusk_Liege.groovy
+++ release/Magarena/scripts/Boartusk_Liege.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-public class Boartusk_Liege {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_RED_CREATURE_YOU_CONTROL) {
         @Override
@@ -19,8 +10,8 @@ public class Boartusk_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_GREEN_CREATURE_YOU_CONTROL) {
         @Override
@@ -31,5 +22,5 @@ public class Boartusk_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-}
+    }
+]
