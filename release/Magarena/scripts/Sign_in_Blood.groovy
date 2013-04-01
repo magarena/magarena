3342b243de714fa3c3df902eb723a02a34010161
melvin <melvinzhang@gmail.com>
2013-04-01 21:58:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sign_in_Blood.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player$ draws two cards and loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicDrawAction(player,2));
+                    game.doAction(new MagicChangeLifeAction(player,-2));
+                }
+            });
+        }
+    }
+]
