38427911d4a8824e65d58245015688e27636ead8
melvin <melvinzhang@gmail.com>
2013-04-01 21:52:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ghostway.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Exile each creature you control. " + 
+                "Return those cards to the battlefield under their owners' control at end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicExileUntilEndOfTurnAction(target));
+            }
+        }
+    }
+]
