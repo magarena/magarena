d8e8b8d496af160e1d352810d70dec8acd6ffe04
melvin <melvinzhang@gmail.com>
2013-04-01 21:43:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Terminus.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Put all creatures on the bottom of their owners' libraries."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets = 
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicPermanent permanent : targets) {
+                game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.BottomOfOwnersLibrary));
+            }
+        }
+    }
+]
