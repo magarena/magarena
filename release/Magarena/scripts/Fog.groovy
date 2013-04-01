a6bce3db8e428b039f1b3487b40a94ad24354dab
melvin <melvinzhang@gmail.com>
2013-04-01 13:37:59 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Fog.groovy
+++ release/Magarena/scripts/Fog.groovy
@@ -1,22 +1,12 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.MagicPlayerState;
-import magic.model.action.MagicChangePlayerStateAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
-public class Fog {
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack,
-                    this,
-                    "Prevent all combat damage that would be dealt this turn.");
+                cardOnStack,
+                this,
+                "Prevent all combat damage that would be dealt this turn."
+            );
         }
         @Override
         public void executeEvent(
@@ -26,5 +16,5 @@ public class Fog {
             game.doAction(new MagicChangePlayerStateAction(player,MagicPlayerState.PreventAllCombatDamage));
             game.doAction(new MagicChangePlayerStateAction(player.getOpponent(),MagicPlayerState.PreventAllCombatDamage));
         }
-    };
-}
+    }
+]
