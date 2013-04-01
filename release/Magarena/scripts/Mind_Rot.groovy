7bf696cf85bdaff12bec642a2019dc4111368bf1
melvin <melvinzhang@gmail.com>
2013-04-01 21:26:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mind_Rot.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player$ discards two cards."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),player,2,false));
+                }
+            });
+        }
+    }
+]
