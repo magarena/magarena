b240c45009d2470bb094f276d30ece9753f23af7
melvin <melvinzhang@gmail.com>
2013-04-01 21:51:48 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Affa_Guard_Hound.groovy
+++ release/Magarena/scripts/Affa_Guard_Hound.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicPumpTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Affa_Guard_Hound {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -23,7 +10,6 @@ public class Affa_Guard_Hound {
                 "Target creature$ gets +0/+3 until end of turn."
             );
         }
-
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
@@ -32,5 +18,5 @@ public class Affa_Guard_Hound {
                 }
             });
         }
-    };
-}
+    }
+]
