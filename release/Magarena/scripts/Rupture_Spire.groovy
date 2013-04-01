0e5d4712b3f8db96d287b368408321e987423e8d
melvin <melvinzhang@gmail.com>
2013-04-01 21:27:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Rupture_Spire.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                new MagicMayChoice(
+                    new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
+                ),
+                this,
+                "You may$ pay {1}. If you don't, sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
+        }
+    }
+]
