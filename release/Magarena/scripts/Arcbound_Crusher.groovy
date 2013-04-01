a658c561b899078c9b426d95423a55ee240b7973
melvin <melvinzhang@gmail.com>
2013-04-01 21:23:31 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Arcbound_Crusher.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent != permanent &&
+                    otherPermanent.isArtifact()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Put a +1/+1 counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                1,
+                true
+            ));
+        }        
+    }
+]
