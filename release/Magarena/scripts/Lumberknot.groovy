67916082af12684ede3badc186d4836802ed25c8
melvin <melvinzhang@gmail.com>
2013-04-01 13:40:01 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Lumberknot.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a +1/+1 counter on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                1,
+                true
+            ));
+        }
+    }
+]
