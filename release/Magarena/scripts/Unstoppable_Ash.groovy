1520099c11476ea90fee28e30bf737cd5674e194
melvin <melvinzhang@gmail.com>
2013-04-01 21:42:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Unstoppable_Ash.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenBecomesBlockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
+            final MagicPlayer player = permanent.getController();
+            return (player == attacker.getController()) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    attacker,
+                    this,
+                    attacker + " gets +0/+5 until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(
+                event.getRefPermanent(),
+                0,
+                5
+            ));
+        }
+    }
+]
