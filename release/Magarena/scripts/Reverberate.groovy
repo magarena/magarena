76c483bb66e954b2fbf8d83a468b832c3fe916d8
melvin <melvinzhang@gmail.com>
2013-04-01 21:57:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Reverberate.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_INSTANT_OR_SORCERY_SPELL,
+                this,
+                'Copy target instant or sorcery spell$. You may choose new targets for the copy.'
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCopyCardOnStackAction(event.getPlayer(),targetSpell));
+                }
+            });
+        }
+    }
+]
