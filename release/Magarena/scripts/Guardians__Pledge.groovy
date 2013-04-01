85962f430280a5ba98662efa44b9c095b0d1b5f7
melvin <melvinzhang@gmail.com>
2013-04-01 21:30:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Guardians__Pledge.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "White creatures PN controls get +2/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_WHITE_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,2,2));
+            }
+        }
+    }
+]
