be50c408e9d9df0af65a3ae0e4b858ac1c11bd7b
melvin <melvinzhang@gmail.com>
2013-04-01 21:26:45 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Burning_Inquiry.groovy
+++ release/Magarena/scripts/Burning_Inquiry.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicDiscardEvent;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
-public class Burning_Inquiry {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -19,7 +8,6 @@ public class Burning_Inquiry {
                 "Each player draw three cards then discard three cards."
             );
         }
-
         @Override
         public void executeEvent(
                 final MagicGame game,
@@ -34,5 +22,5 @@ public class Burning_Inquiry {
                 ));
             }
         }
-    };
-}
+    }
+]
