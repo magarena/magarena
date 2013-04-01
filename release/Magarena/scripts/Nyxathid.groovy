d808c86c4ce3e6c8485455cba60876f9b9df9363
melvin <melvinzhang@gmail.com>
2013-04-01 13:37:16 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Nyxathid.groovy
+++ release/Magarena/scripts/Nyxathid.groovy
@@ -1,25 +1,13 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Nyxathid {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
+[
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             final MagicPlayer opponent = permanent.getChosenPlayer();
             final int amount = opponent.getHandSize();
             pt.add(-amount,-amount);
         }
-    };
-    
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+    },
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -28,5 +16,5 @@ public class Nyxathid {
             permanent.setChosenTarget(player.getOpponent());
             return MagicEvent.NONE;
         }
-    };
-}
+    }
+]
