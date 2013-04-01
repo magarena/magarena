c66a78046c943df7ab4218a7f00e73d3fd710ce1
melvin <melvinzhang@gmail.com>
2013-04-01 13:40:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Battlegrace_Angel.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPlayer player=permanent.getController();
+            return (creature.getController()==player&&player.getNrOfAttackers()==1)?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    creature,
+                    this,
+                    creature+" gains lifelink until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicSetAbilityAction(event.getRefPermanent(),MagicAbility.LifeLink));
+        }
+    }
+]
