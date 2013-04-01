0e77007005997d2dcf9cd5910430fecce53b3c8d
melvin <melvinzhang@gmail.com>
2013-04-01 21:39:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Falkenrath_Marauders.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return (damage.getSource() == permanent &&
+                    damage.getTarget().isPlayer() &&
+                    damage.isCombat()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Put two +1/+1 counters on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                2,
+                true
+            ));
+        }
+    }
+]
