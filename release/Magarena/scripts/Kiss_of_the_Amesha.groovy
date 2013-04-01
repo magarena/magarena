d6d9d3ed9c65f210a2c66d4ce70220be88b247b7
melvin <melvinzhang@gmail.com>
2013-04-01 22:01:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Kiss_of_the_Amesha.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_PLAYER,
+                this,
+                'Target player$ gains 7 life and draws two cards.'
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,7));
+                    game.doAction(new MagicDrawAction(player,2));
+                }
+            });
+        }
+    }
+]
