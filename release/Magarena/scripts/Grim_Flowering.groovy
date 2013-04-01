99e43c4524216050fadc884d365618e5d6b05dd9
melvin <melvinzhang@gmail.com>
2013-04-01 21:21:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Grim_Flowering.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN draws a card for each creature " +
+                "card in his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = game.filterCards(player, MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD).size();
+            game.doAction(new MagicDrawAction(player,amount));
+        }
+    }
+]
