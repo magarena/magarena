e072d0eb812ec1285036d61febf9f8c335c64a84
melvin <melvinzhang@gmail.com>
2013-04-01 21:50:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hedron_Crab.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player$ puts the top three cards " +
+                "of his or her library into his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicMillLibraryAction(player,3));
+                }
+            });
+        }        
+    }
+]
