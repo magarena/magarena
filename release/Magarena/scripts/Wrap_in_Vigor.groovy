5f6b410cc012ff3a1ac1dada1b02d2823ce07de4
melvin <melvinzhang@gmail.com>
2013-04-01 13:38:46 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Wrap_in_Vigor.groovy
+++ release/Magarena/scripts/Wrap_in_Vigor.groovy
@@ -1,24 +1,12 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicRegenerateAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-
-public class Wrap_in_Vigor {
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack,
-                    this,
-                    "Regenerate each creature you control.");
+                cardOnStack,
+                this,
+                "Regenerate each creature you control."
+            );
         }
 
         @Override
@@ -31,5 +19,5 @@ public class Wrap_in_Vigor {
                 game.doAction(new MagicRegenerateAction(creature));
             }
         }
-    };
-}
+    }
+]
