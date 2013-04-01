d1a2ba2356464197459820d2ad6e29d9ee9bfc30
melvin <melvinzhang@gmail.com>
2013-04-01 21:59:14 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Arcanis_the_Omnipotent.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Token),
+        "Draw") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws three cards."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),3));
+        }
+    }
+]
