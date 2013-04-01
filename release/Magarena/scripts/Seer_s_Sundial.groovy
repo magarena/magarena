17f89dec9a9b08693232d2a6714fb999e9916df0
melvin <melvinzhang@gmail.com>
2013-04-01 13:39:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Seer_s_Sundial.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                permanent.getController(),
+                new MagicMayChoice(
+                    new MagicPayManaCostChoice(MagicManaCost.create("{2}"))
+                ),
+                this,
+                "You may$ pay {2}$. If you do, draw a card."
+            );
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            }
+        }        
+    }
+]
