e9b094fcb42d400746c1f581092a33fd70178321
melvin <melvinzhang@gmail.com>
2013-04-01 21:20:12 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Lowland_Basilisk.groovy
+++ release/Magarena/scripts/Lowland_Basilisk.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-public class Lowland_Basilisk {
-    public static final MagicWhenDamageIsDealtTrigger T3 = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return (damage.getSource() == permanent &&
@@ -22,12 +12,11 @@ public class Lowland_Basilisk {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
                 final MagicEvent event) {
             game.doAction(new MagicChangeStateAction(event.getRefPermanent(),MagicPermanentState.DestroyAtEndOfCombat,true));
         }
-    };
-}
+    }
+]
