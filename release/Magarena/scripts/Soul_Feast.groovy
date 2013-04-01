e56f3634ea49e197cea4d8359e2d34b9b222dd22
melvin <melvinzhang@gmail.com>
2013-04-01 21:56:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Soul_Feast.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                'Target player$ loses 4 life and PN gains 4 life.'
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-4));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+                }
+            });
+        }
+    }
+]
