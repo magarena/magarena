40841c8929514bf575c5395383dafa0e19191efa
melvin <melvinzhang@gmail.com>
2013-04-01 21:21:38 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Natural_Spring.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_PLAYER,
+                this,
+                "Target player$ gains 8 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,8));
+                }
+            });
+        }
+    }
+]
