88d6194dba1692b07f654eea07bc7c08ef43f598
melvin <melvinzhang@gmail.com>
2013-04-01 21:28:03 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Master_of_Etherium.groovy
+++ release/Magarena/scripts/Master_of_Etherium.groovy
@@ -1,26 +1,14 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicCDA;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-public class Master_of_Etherium {
-    public static final MagicCDA CDA = new MagicCDA() {
+[
+    new MagicCDA() {
         @Override
         public void modPowerToughness(final MagicGame game, final MagicPlayer player, final MagicPowerToughness pt) {
             final int size = game.filterPermanents(player, MagicTargetFilter.TARGET_ARTIFACT_YOU_CONTROL).size();
             pt.set(size, size);
         }
-    };
-    
-    public static final MagicStatic S = new MagicStatic(
-            MagicLayer.ModPT, 
-            MagicTargetFilter.TARGET_ARTIFACT_CREATURE_YOU_CONTROL) {
+    },
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_ARTIFACT_CREATURE_YOU_CONTROL) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
@@ -29,5 +17,5 @@ public class Master_of_Etherium {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-}
+    }
+]
