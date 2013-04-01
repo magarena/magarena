06bc243789d3091a8db880ae3811b17de8cd1172
melvin <melvinzhang@gmail.com>
2013-04-01 21:52:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hideous_Visage.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Creatures PN controls gain intimidate until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicSetAbilityAction(target,MagicAbility.Intimidate));
+            }
+        }
+    }
+]
