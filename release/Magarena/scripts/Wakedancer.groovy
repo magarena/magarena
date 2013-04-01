a8bcb65cbc02f2dc742a45a680f58cd792ec11ff
melvin <melvinzhang@gmail.com>
2013-04-01 13:40:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Wakedancer.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return (game.getCreatureDiedThisTurn()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a 2/2 black Zombie creature token onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Zombie")
+            ));
+        }        
+    }
+]
