40c20693cce403fba0889ae76f705eff38f3ac85
melvin <melvinzhang@gmail.com>
2013-04-01 22:00:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Stupor.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Target opponent$ discards a card at random, then discards a card."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),player,1,true));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),player,1,false));
+                }
+            });
+        }
+    }
+]
