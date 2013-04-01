30896a7f3940e6c7edc1ebbc6808b0468718ce0b
melvin <melvinzhang@gmail.com>
2013-04-01 21:41:49 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Aven_Brigadier.groovy
+++ release/Magarena/scripts/Aven_Brigadier.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-
-public class Aven_Brigadier {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT,
         MagicTargetFilter.TARGET_BIRD) {
         @Override
@@ -21,9 +10,8 @@ public class Aven_Brigadier {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT,
         MagicTargetFilter.TARGET_SOLDIER) {
         @Override
@@ -34,5 +22,5 @@ public class Aven_Brigadier {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-}
+    }
+]
