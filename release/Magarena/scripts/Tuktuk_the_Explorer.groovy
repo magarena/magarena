25b42d5058bb13bc0d5045b32567f1b5f6491301
melvin <melvinzhang@gmail.com>
2013-04-01 21:55:12 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Tuktuk_the_Explorer.groovy
+++ release/Magarena/scripts/Tuktuk_the_Explorer.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicGraveyardTriggerData;
-import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
-
-public class Tuktuk_the_Explorer {
-    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+[
+    new MagicWhenPutIntoGraveyardTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
             return (triggerData.fromLocation == MagicLocationType.Play) ?
@@ -22,12 +11,11 @@ public class Tuktuk_the_Explorer {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(
                 final MagicGame game,
                 final MagicEvent event) {
             game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Tuktuk the Returned")));
         }
-    };
-}
+    }
+]
