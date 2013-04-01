199e02fd1a22885a7952541fdc18015079c765b5
melvin <melvinzhang@gmail.com>
2013-04-01 21:44:00 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Bitterblossom.groovy
+++ release/Magarena/scripts/Bitterblossom.groovy
@@ -1,16 +1,5 @@
-        package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Bitterblossom {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -18,7 +7,8 @@ public class Bitterblossom {
                     permanent,
                     this,
                     "PN loses 1 life and puts a 1/1 black Faerie Rogue creature token with flying " +
-                    "onto the battlefield."):
+                    "onto the battlefield."
+                ):
                 MagicEvent.NONE;
         }
         @Override
@@ -29,5 +19,5 @@ public class Bitterblossom {
             game.doAction(new MagicChangeLifeAction(player,-1));
             game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.get("Faerie Rogue")));
         }
-    };
-}
+    }
+]
