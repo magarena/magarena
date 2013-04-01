16a15497eb80da9764d55d10fd1905323ac4a73d
melvin <melvinzhang@gmail.com>
2013-04-01 21:51:33 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bottle_Gnomes.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life+3") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains 3 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
+        }
+    }
+]
