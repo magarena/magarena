0c5ed7b0aca79fbfd68e130343ad701893fa47bc
melvin <melvinzhang@gmail.com>
2013-04-01 21:54:59 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Bar_the_Door.groovy
+++ release/Magarena/scripts/Bar_the_Door.groovy
@@ -1,26 +1,14 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-
-public class Bar_the_Door {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(
                 final MagicCardOnStack cardOnStack,
                 final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack,
-                    this,
-                    "Creatures PN controls get +0/+4 until end of turn.");
+                cardOnStack,
+                this,
+                "Creatures PN controls get +0/+4 until end of turn."
+            );
         }
         @Override
         public void executeEvent(
@@ -33,5 +21,5 @@ public class Bar_the_Door {
                 game.doAction(new MagicChangeTurnPTAction(target,0,4));
             }
         }
-    };
-}
+    }
+]
