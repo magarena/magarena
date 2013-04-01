5a44219d8781e462ed03fd43bff7cb773f73fb7d
melvin <melvinzhang@gmail.com>
2013-04-01 21:39:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mystic_Snake.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                MagicTargetChoice.TARGET_SPELL,
+                this,
+                'Counter target spell$.'
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                }
+            });
+        }
+    }
+]
