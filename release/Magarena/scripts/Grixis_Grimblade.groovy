cd6692d7191d3d94ff6bc8134cab92a68b6d87bc
melvin <melvinzhang@gmail.com>
2013-04-01 22:38:08 +0800

    remove reference to Bant Sureblade

--- release/Magarena/scripts/Grixis_Grimblade.groovy
+++ release/Magarena/scripts/Grixis_Grimblade.groovy
@@ -1,9 +1,16 @@
 [
-    Bant_Sureblade.S1,
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (MagicCondition.CONTROL_ANOTHER_MULTICOLORED_PERMANENT.accept(permanent)) {
+                pt.add(1,1);
+            }
+        }        
+    },
     new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            if (Bant_Sureblade.isValid(permanent)) {
+            if (MagicCondition.CONTROL_ANOTHER_MULTICOLORED_PERMANENT.accept(permanent)) {
                 flags.add(MagicAbility.Deathtouch);
             }
         }
