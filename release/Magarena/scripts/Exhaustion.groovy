db2a2bc5f9ebdb4bc1bed03cf0a8cd131c0673db
melvin <melvinzhang@gmail.com>
2013-04-03 16:03:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Exhaustion.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Creatures and lands target opponent$ controls don't untap during that opponent's next untap step."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangePlayerStateAction(player,MagicPlayerState.Exhausted));
+                }
+            });
+        }
+    }
+]
