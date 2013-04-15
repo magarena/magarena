b94a2e551423f41b908ec6397139ed8482bf7a39
melvin <melvinzhang@gmail.com>
2013-04-15 17:17:12 +0800

    add script to compute WHR Gamma scores

---
+++ exp/whr.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'whole_history_rating'
+
+# w = 0 for static ratings aka bayeselo
+@whr = WholeHistoryRating::Base.new(:w2 => 0)
+
+# WholeHistoryRating::Base#create_game arguments: black player name, white player name, winner, day number, handicap
+# Handicap should generally be less than 500 elo
+ARGF.each do |line|
+    tokens = line.split(' ')
+    @whr.create_game(tokens[0], tokens[1], tokens[2], 0, 0)
+end
+
+# Iterate the WHR algorithm towards convergence with more players/games, more iterations are needed.
+@whr.iterate(1000)
+
+players = @whr.players.values.select {|p| p.days.count > 0}
+players.sort_by { |p| p.days.last.gamma }.each_with_index do |p,idx|
+    if p.days.count > 0
+      puts "#{p.name}\t#{p.days[0].gamma}"
+    end
+end
