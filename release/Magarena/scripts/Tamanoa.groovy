6f4d9b17f1dda2e841464a84646e66b9042f137a
melvin <melvinzhang@gmail.com>
2013-05-12 18:52:02 +0800

    completed Tamanoa

---
+++ release/Magarena/scripts/Tamanoa.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final int amount = damage.getDealtAmount();
+            final MagicSource source = damage.getSource();
+            return (!source.isCreature() && permanent.isFriend(source)) ?
+                new MagicEvent(
+                    permanent,
+                    amount,
+                    this,
+                    "PN gains " + amount + " life.") 
+                :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),event.getRefInt()));
+        }
+    }
+]
