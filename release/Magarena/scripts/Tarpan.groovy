91e95b5ce67b8a9dd911a9c5163320d9555af9d0
melvin <melvinzhang@gmail.com>
2013-05-12 18:36:35 +0800

    completed Tarpan

---
+++ release/Magarena/scripts/Tarpan.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN gains 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+        }
+    }
+]
