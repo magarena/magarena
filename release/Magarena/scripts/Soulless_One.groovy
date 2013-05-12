5fd579114c2dd368e2410c500f3b3b3ef5a4b11b
melvin <melvinzhang@gmail.com>
2013-05-12 21:58:32 +0800

    completed Soulless One

---
+++ release/Magarena/scripts/Soulless_One.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicCDA() {
+        @Override
+        public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+            final int battlefield = 
+                player.getNrOfPermanentsWithSubType(MagicSubType.Zombie) +
+                player.getOpponent().getNrOfPermanentsWithSubType(MagicSubType.Zombie); 
+            final int graveyard = 
+                game.filterCards(player,MagicTargetFilter.TARGET_ZOMBIE_CARD_FROM_GRAVEYARD).size() +
+                game.filterCards(player.getOpponent(),MagicTargetFilter.TARGET_ZOMBIE_CARD_FROM_GRAVEYARD).size();
+            final int amount = battlefield + graveyard;
+            pt.set(amount,amount);
+        }
+    }
+]
