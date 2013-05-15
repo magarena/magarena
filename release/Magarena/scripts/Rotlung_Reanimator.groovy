cb12f39e77915bb4ebf4f4a9b466db310eadd8e3
melvin <melvinzhang@gmail.com>
2013-05-15 22:07:44 +0800

    completed Rotlung Reanimator

---
+++ release/Magarena/scripts/Rotlung_Reanimator.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent diedPermanent) {
+            return (permanent == diedPermanent || diedPermanent.hasSubType(MagicSubType.Cleric)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a 2/2 black Zombie creature token onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Zombie")
+            ));
+        }
+    }
+]
