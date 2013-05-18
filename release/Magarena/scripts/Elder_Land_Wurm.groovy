3240093af7754ee719f118687f5222bbf51fdc55
melvin <melvinzhang@gmail.com>
2013-05-18 19:34:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Elder_Land_Wurm.groovy
@@ -0,0 +1,31 @@
+def AB = new MagicStatic(MagicLayer.Ability) {
+    @Override
+    public void modAbilityFlags(
+            final MagicPermanent source,
+            final MagicPermanent permanent,
+            final Set<MagicAbility> flags) {
+        flags.remove(MagicAbility.Defender);
+    }
+}
+
+[
+    new MagicWhenBlocksTrigger() {
+       @Override
+       public MagicEvent executeTrigger(
+               final MagicGame game,
+               final MagicPermanent permanent,
+               final MagicPermanent blocker) {
+           return (permanent == blocker) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN loses defender."
+                ):
+                MagicEvent.NONE;
+       }
+       @Override
+       public void executeEvent(final MagicGame game, final MagicEvent event) {
+           game.doAction(new MagicAddStaticAction(event.getPermanent(), AB));
+       }
+   }
+]
