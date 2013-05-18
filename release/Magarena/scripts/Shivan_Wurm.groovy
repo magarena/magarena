8b2be891516f9bebef2a136a712138a5e0d7c3b8
melvin <melvinzhang@gmail.com>
2013-05-18 15:36:19 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Shivan_Wurm.groovy
+++ release/Magarena/scripts/Shivan_Wurm.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicRemoveFromPlayAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicBounceTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
-public class Shivan_Wurm {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
-
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -22,10 +7,9 @@ public class Shivan_Wurm {
                 MagicTargetChoice.RED_OR_GREEN_CREATURE_YOU_CONTROL,
                 MagicBounceTargetPicker.getInstance(),
                 this,
-                "Return a red or green creature you control$ to its owner's hand."
+                "Return a red or green creature you control\$ to its owner's hand."
             );
         }
-
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
@@ -34,5 +18,5 @@ public class Shivan_Wurm {
                 }
             });
         }
-    };
-}
+    }
+]
