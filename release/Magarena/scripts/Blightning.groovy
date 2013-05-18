4133de160028e0860a3901658b93e8f0771ded63
melvin <melvinzhang@gmail.com>
2013-05-18 20:53:31 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Blightning.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "SN deals 3 damage to target player\$. That player discards two cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),player,3);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),player,2,false));
+                }
+            });
+        }
+    }
+]
