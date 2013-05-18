9ac2960e1da1c2bf2b982ead1875621a9a6f2cff
melvin <melvinzhang@gmail.com>
2013-05-18 11:51:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Drifter_il_Dal.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        new MagicPayManaCostChoice(MagicManaCost.create("{U}"))
+                    ),
+                    this,
+                    "You may\$ pay {U}\$. If you don't, sacrifice SN.") :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }            
+        }
+    }
+]
