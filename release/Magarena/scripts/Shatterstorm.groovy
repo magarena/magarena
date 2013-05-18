036b8fd0714e7f40d4de62bd8914baf5525e5d5c
melvin <melvinzhang@gmail.com>
2013-05-18 15:16:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Shatterstorm.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all artifacts. They can't be regenerated."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ARTIFACT);            
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeStateAction(target,MagicPermanentState.CannotBeRegenerated,true));
+            }
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
