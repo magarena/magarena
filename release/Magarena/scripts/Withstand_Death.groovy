b050ff99ddbbba984dfb3d4122caf7caa467957d
melvin <melvinzhang@gmail.com>
2013-05-18 15:36:01 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Withstand_Death.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicIndestructibleTargetPicker.create(),
+                this,
+                "Target creature\$ is indestructible turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Indestructible));
+                }
+            });
+        }
+    }
+]
