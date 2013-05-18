3e851faf713385f6924a36bc8fa7b2e0a8a2e89a
melvin <melvinzhang@gmail.com>
2013-05-18 15:06:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Geralf_s_Mindcrusher.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ puts the top five cards of " +
+                "his or her library into his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer targetPlayer) {
+                    game.doAction(new MagicMillLibraryAction(targetPlayer,5));
+                }
+            });    
+        }
+    }
+]
