8bf6e212edb7b3be84415c46465da99bf85b3761
melvin <melvinzhang@gmail.com>
2013-05-18 15:05:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Briarpack_Alpha.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                }
+            });
+        }
+    }
+]
