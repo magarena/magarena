baab817c8c70ce2fc63e2cad8acaf71a853681d6
melvin <melvinzhang@gmail.com>
2013-05-18 11:52:51 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Dragon_s_Claw.groovy
+++ release/Magarena/scripts/Dragon_s_Claw.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicColor;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-public class Dragon_s_Claw {
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return cardOnStack.hasColor(MagicColor.Red) ?
@@ -19,18 +8,18 @@ public class Dragon_s_Claw {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.GAIN_LIFE,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
                     "PN may$ gain 1 life."
-                ) :
+                ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
             }
         }
-    };
-}
+    }
+]
