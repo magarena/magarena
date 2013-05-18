540e9205ec737f757fc04c1132514d7aba4735cf
melvin <melvinzhang@gmail.com>
2013-05-18 19:41:00 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Zombify.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Return target creature card\$ from your graveyard to the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    game.doAction(new MagicReanimateAction(event.getPlayer(),targetCard,MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
