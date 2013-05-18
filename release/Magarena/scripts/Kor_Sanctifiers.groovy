f1985412ca9f1dda14dd76700fc2d86b910ee42e
melvin <melvinzhang@gmail.com>
2013-05-18 21:03:07 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kor_Sanctifiers.groovy
+++ release/Magarena/scripts/Kor_Sanctifiers.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Kor_Sanctifiers {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -23,20 +11,18 @@ public class Kor_Sanctifiers {
                     MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "Destroy target artifact or enchantment$."
+                    "Destroy target artifact or enchantment\$."
                 ):
                 MagicEvent.NONE;
         }
                     
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent target) {
                     game.doAction(new MagicDestroyAction(target));
                 }
             });
         }
-    };
-}
+    }
+]
