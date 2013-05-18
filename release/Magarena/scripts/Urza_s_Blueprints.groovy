650d1d389ffd4fac2a2e5471b3d89cc60b424d15
melvin <melvinzhang@gmail.com>
2013-05-18 13:15:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Urza_s_Blueprints.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+        }
+    }
+]
