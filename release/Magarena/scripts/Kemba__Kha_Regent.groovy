898aa8d550cfbbcd4486806d1cf1ee96356d1b80
melvin <melvinzhang@gmail.com>
2013-05-18 12:02:18 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kemba__Kha_Regent.groovy
+++ release/Magarena/scripts/Kemba__Kha_Regent.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Kemba__Kha_Regent {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -21,14 +11,12 @@ public class Kemba__Kha_Regent {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent = event.getPermanent();
             int amount = permanent.getEquipmentPermanents().size();
             for (;amount>0;amount--) {
                 game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Cat2")));
             }
         }        
-    };
-}
+    }
+]
