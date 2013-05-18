6e196e4b64b3b5bc68314452002ad8ca46505817
melvin <melvinzhang@gmail.com>
2013-05-18 14:02:36 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Mycoloth.groovy
+++ release/Magarena/scripts/Mycoloth.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokensAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Mycoloth {
-    public static final Object T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return (permanent.isController(upkeepPlayer) && 
@@ -24,9 +13,7 @@ public class Mycoloth {
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final int amt = event.getPermanent().getCounters(MagicCounterType.PlusOne); 
             game.doAction(new MagicPlayTokensAction(
                 event.getPlayer(),
@@ -34,5 +21,5 @@ public class Mycoloth {
                 amt
             ));
         }
-    };
-}
+    }
+]
