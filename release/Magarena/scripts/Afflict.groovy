7d98cfcaa9cb67671adb6da8023b751da6351ec5
melvin <melvinzhang@gmail.com>
2013-05-18 19:57:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Afflict.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(1,1),
+                this,
+                "Target creature\$ gets -1/-1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-1,-1));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
