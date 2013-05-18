dc5d7899a6dffaed75bdeec24a1ec707b625a72a
melvin <melvinzhang@gmail.com>
2013-05-18 15:33:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Taurean_Mauler.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return cardOnStack.getController() != permanent.getController() ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ put a +1/+1 counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
+            }
+        }
+    }
+]
