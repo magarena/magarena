efcfad6e7422fffeec9ea978dae7f626d7f36532
melvin <melvinzhang@gmail.com>
2013-05-18 18:38:40 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Flame_Slash.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(4),
+                this,
+                "SN deals 4 damage to target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,4);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
