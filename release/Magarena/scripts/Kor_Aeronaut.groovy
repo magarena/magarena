28304fda0d7f9f58dabbaa375aa6b5817a5d6e2c
melvin <melvinzhang@gmail.com>
2013-05-18 19:50:46 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kor_Aeronaut.groovy
+++ release/Magarena/scripts/Kor_Aeronaut.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Kor_Aeronaut {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -23,20 +11,18 @@ public class Kor_Aeronaut {
                     player,
                     MagicTargetChoice.POS_TARGET_CREATURE,
                     this,
-                    "Target creature$ gains flying until end of turn."
+                    "Target creature\$ gains flying until end of turn."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Flying));
                 }
             });
         }
-    };
-}
+    }
+]
