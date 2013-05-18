b7441c878ae24da6a02963e697ecd23af90c0af7
melvin <melvinzhang@gmail.com>
2013-05-18 20:00:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bleak_Coven_Vampires.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return MagicCondition.METALCRAFT_CONDITION.accept(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    this,
+                    "Target player\$ loses 4 life and PN gains 4 life."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-4));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+                }
+            });
+        }
+    }
+]
