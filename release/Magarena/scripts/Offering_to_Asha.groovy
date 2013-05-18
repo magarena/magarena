e41dbe807f39925061ad8bda41dbffb17ab22874
melvin <melvinzhang@gmail.com>
2013-05-18 19:59:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Offering_to_Asha.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {4}. " +
+                "PN gains 4 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.create("{4}")));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+                }
+            });
+        }
+    }
+]
