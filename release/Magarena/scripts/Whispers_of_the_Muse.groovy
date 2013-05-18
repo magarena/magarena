e22f257e38e95f1374b351440df9f1067ba7e7d7
melvin <melvinzhang@gmail.com>
2013-05-18 13:50:37 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Whispers_of_the_Muse.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(MagicManaCost.create("{5}")),
+                this,
+                "PN draws a card. If the buyback cost was payed\$, " +
+                "return SN to its owner's hand as it resolves."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            if (event.isBuyback()) {
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+            } 
+        }
+    }
+]
