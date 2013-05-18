e47a9d61f246e9860ae7b150dc8feaeed8b33664
melvin <melvinzhang@gmail.com>
2013-05-18 20:50:23 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Phyrexian_Prowler.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ChargeCountersAtLeast(1)],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump") {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+1 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,1));
+        }
+    }
+]
