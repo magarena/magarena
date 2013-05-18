001c5b3d6a4e1c5e454c726bf27af8bbcba54d68
melvin <melvinzhang@gmail.com>
2013-05-18 14:13:46 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Soul_s_Majesty.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicPowerTargetPicker.getInstance(),
+                this,
+                "Draw cards equal to the power of target creature\$ you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDrawAction(event.getPlayer(),creature.getPower()));
+                }
+            });
+        }
+    }
+]
