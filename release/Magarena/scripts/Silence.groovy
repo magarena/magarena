3acd335fce021611814a48af6e61d83d32c0555a
melvin <melvinzhang@gmail.com>
2013-05-18 15:09:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Silence.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Your opponents can't cast spells this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangePlayerStateAction(
+                event.getPlayer().getOpponent(),
+                MagicPlayerState.CantCastSpells
+            ));
+        }
+    }
+]
