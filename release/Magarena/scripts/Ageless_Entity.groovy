b4bbc44d23ac7d19ac30015511364920cdb6e4cf
melvin <melvinzhang@gmail.com>
2013-05-18 15:21:55 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ageless_Entity.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenLifeIsGainedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicLifeChangeTriggerData lifeChange) {
+            final int amount = lifeChange.amount;
+            return permanent.isController(lifeChange.player) ?
+                new MagicEvent(
+                    permanent,
+                    amount,
+                    this,
+                    amount > 1 ?
+                        "Put " + amount + " +1/+1 counters on SN." :
+                        "Put a +1/+1 counter on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                event.getRefInt(),
+                true
+            ));
+        }
+    }
+]
