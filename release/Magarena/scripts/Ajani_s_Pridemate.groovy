0f0c3ea3a3c1fac4eb7b2d7c6ae4dd142eeb5b0a
melvin <melvinzhang@gmail.com>
2013-05-18 20:53:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ajani_s_Pridemate.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenLifeIsGainedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicLifeChangeTriggerData lifeChange) {
+            return permanent.isController(lifeChange.player) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ put a +1/+1 counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }
+        }
+    }
+]
