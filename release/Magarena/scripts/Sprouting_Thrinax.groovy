e3785a5c277435b78b4d7717cf0e902f40b434d0
melvin <melvinzhang@gmail.com>
2013-05-18 13:53:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sprouting_Thrinax.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenPutIntoGraveyardTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+            return (triggerData.fromLocation == MagicLocationType.Play) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts three 1/1 green Saproling creature tokens onto the battlefield."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            game.doAction(new MagicPlayTokensAction(player,TokenCardDefinitions.get("Saproling"), 3));
+        }
+    }
+]
