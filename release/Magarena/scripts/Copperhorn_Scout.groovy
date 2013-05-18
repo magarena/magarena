ec7ca56da9c91f0ec706c0fca4f3d4634bb6126c
melvin <melvinzhang@gmail.com>
2013-05-18 19:49:32 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Copperhorn_Scout.groovy
+++ release/Magarena/scripts/Copperhorn_Scout.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicUntapAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-import java.util.Collection;
-
-public class Copperhorn_Scout {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+[
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -25,9 +14,7 @@ public class Copperhorn_Scout {
                 MagicEvent.NONE;
         }    
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent = event.getPermanent();
             final Collection<MagicPermanent> targets = game.filterPermanents(
                     event.getPlayer(),
@@ -38,5 +25,5 @@ public class Copperhorn_Scout {
                 }
             }
         }        
-    };
-}
+    }
+]
