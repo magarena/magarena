7db3a8ad913fcbf7ebfa57ea468ad6d3727b0fcb
melvin <melvinzhang@gmail.com>
2013-05-18 14:03:26 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Drogskol_Captain.groovy
+++ release/Magarena/scripts/Drogskol_Captain.groovy
@@ -1,19 +1,7 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Set;
-
-public class Drogskol_Captain {
-    public static final MagicStatic S1 = new MagicStatic(
-            MagicLayer.ModPT, 
-            MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL) {
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
@@ -22,11 +10,10 @@ public class Drogskol_Captain {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-
-    public static final MagicStatic S2 = new MagicStatic(
-            MagicLayer.Ability, 
-            MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL) {
+    },
+    new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Hexproof);
@@ -35,5 +22,5 @@ public class Drogskol_Captain {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-}
+    }
+]
