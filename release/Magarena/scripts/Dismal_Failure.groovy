8a29eb579da35b5aa856f503e4402475ece93b65
melvin <melvinzhang@gmail.com>
2013-05-18 15:17:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dismal_Failure.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. Its controller discards a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack counteredCard) {
+                    game.doAction(new MagicCounterItemOnStackAction(counteredCard));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),counteredCard.getController(),1,false));
+                }
+            });
+        }
+    }
+]
