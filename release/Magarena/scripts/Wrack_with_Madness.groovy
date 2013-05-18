ead85cd9b83b7423897f245139f284e0cb9fadc3
melvin <melvinzhang@gmail.com>
2013-05-18 21:25:21 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Wrack_with_Madness.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                this,
+                "Target creature\$ deals damage to itself equal to its power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final MagicDamage damage = new MagicDamage(
+                        creature,
+                        creature,
+                        creature.getPower()
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
