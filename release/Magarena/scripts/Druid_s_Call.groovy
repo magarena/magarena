df4cf63f5d26bac07cf207c4cbcfb72e9d680161
melvin <melvinzhang@gmail.com>
2013-05-18 20:47:49 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Druid_s_Call.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicDamage damage) {
+            final int amount = damage.getDealtAmount();
+            final MagicPermanent enchanted = permanent.getEnchantedCreature();
+            return damage.getTarget() == enchanted ?
+                new MagicEvent(
+                    permanent,
+                    enchanted.getController(),
+                    amount,
+                    this,
+                    "PN puts RN 1/1 green Squirrel creature tokens onto the battlefield."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Squirrel1"),
+                event.getRefInt()
+            ));
+        }
+    }
+]
