0325283aa9d786b418da91a443d7fac8c19ee2c1
melvin <melvinzhang@gmail.com>
2013-05-18 21:02:17 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Wall_of_Reverence.groovy
+++ release/Magarena/scripts/Wall_of_Reverence.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicPowerTargetPicker;
-import magic.model.trigger.MagicAtEndOfTurnTrigger;
-
-public class Wall_of_Reverence {
-    public static final MagicAtEndOfTurnTrigger T = new MagicAtEndOfTurnTrigger() {
+[
+    new MagicAtEndOfTurnTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
             return permanent.isController(eotPlayer) ?
@@ -20,19 +8,17 @@ public class Wall_of_Reverence {
                     MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
                     MagicPowerTargetPicker.getInstance(),
                     this,
-                    "PN gains life equal to the power of target creature$ he or she controls."
+                    "PN gains life equal to the power of target creature\$ he or she controls."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeLifeAction(event.getPlayer(),creature.getPower()));
                 }
             });
         }
-    };
-}
+    }
+]
