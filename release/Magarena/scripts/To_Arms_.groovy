5a5b01f0d8741e4262c27f4a570a2585bd17323b
melvin <melvinzhang@gmail.com>
2013-05-18 13:53:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/To_Arms_.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Untap all creatures you control. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicUntapAction(target));
+            }
+            game.doAction(new MagicDrawAction(player,1));
+        }
+    }
+]
