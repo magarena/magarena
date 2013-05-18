fcda4395498265eb0931705f7017fc5d7af4e2b7
melvin <melvinzhang@gmail.com>
2013-05-18 21:29:07 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sudden_Impact.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "SN deals damage equal to the " +
+                "number of cards in target player's hand to that player$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage = new MagicDamage(
+                        event.getSource(),
+                        player,
+                        player.getHandSize()
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
