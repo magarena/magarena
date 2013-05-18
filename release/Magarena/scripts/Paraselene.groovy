b634eccf5b4e842eac7723ecd1e8e78df8f3b731
melvin <melvinzhang@gmail.com>
2013-05-18 19:43:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Paraselene.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all enchantments. " +
+                "PN gains 1 life for each enchantment destroyed this way."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(player,MagicTargetFilter.TARGET_ENCHANTMENT);
+            game.doAction(new MagicDestroyAction(targets));
+            if (targets.size() > 0) {
+                game.doAction(new MagicChangeLifeAction(player,targets.size()));                
+            }
+        }
+    }
+]
