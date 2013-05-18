9bd92d2ad202d8270a2f950d39ea4a47220bdd0a
melvin <melvinzhang@gmail.com>
2013-05-18 15:17:45 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Countersquall.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_NONCREATURE_SPELL,
+                this,
+                "Counter target noncreature spell\$. Its controller loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicChangeLifeAction(targetSpell.getController(),-2));
+                }
+            });
+        }
+    }
+]
