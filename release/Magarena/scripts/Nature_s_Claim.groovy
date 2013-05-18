c20bf2de1e10a570027d5610a3d5d8ecf65b7f65
melvin <melvinzhang@gmail.com>
2013-05-18 21:31:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Nature_s_Claim.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact or enchantment\$. Its controller gains 4 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    game.doAction(new MagicChangeLifeAction(permanent.getController(),4));
+                }
+            });
+        }
+    }
+]
