41d96681882096e7e94307d22e45f187bbf9fd3b
melvin <melvinzhang@gmail.com>
2013-05-18 12:35:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Augur_il_Vec.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.YOUR_UPKEEP_CONDITION],
+        new MagicActivationHints(MagicTiming.None),
+        "Life+4") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains 4 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+        }
+    }
+]
