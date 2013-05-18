a4fe5a3966038cba81fe2184403bec6908a84375
melvin <melvinzhang@gmail.com>
2013-05-18 18:43:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Plague_Wind.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all creatures you don't control. They can't be regenerated."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS);            
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeStateAction(target,MagicPermanentState.CannotBeRegenerated,true));
+            }
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
