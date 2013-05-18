95c4f196de9564d9b260c08737e4b880a170630c
melvin <melvinzhang@gmail.com>
2013-05-18 18:36:33 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Weatherseed_Treefolk.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenPutIntoGraveyardTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicGraveyardTriggerData triggerData) {
+            return (triggerData.fromLocation == MagicLocationType.Play) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Return SN to its owner's hand."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard card = event.getPermanent().getCard();
+            game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+            game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+        }
+    }
+]
