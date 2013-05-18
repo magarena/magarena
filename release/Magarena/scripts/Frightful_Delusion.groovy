04d6f3c4c8d0f65bb22f9770ab0476309caf1975
melvin <melvinzhang@gmail.com>
2013-05-18 21:29:51 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Frightful_Delusion.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {1}. " +
+                "That player discards a card."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.create("{1}")));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),targetSpell.getController(),1,false));
+                }
+            });
+        }
+    }
+]
