54ed06ece3946b961e830f0b14978d729913ae20
melvin <melvinzhang@gmail.com>
2013-05-18 12:00:48 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Wurm_s_Tooth.groovy
+++ release/Magarena/scripts/Wurm_s_Tooth.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicColor;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-
-public class Wurm_s_Tooth {
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return cardOnStack.hasColor(MagicColor.Green) ?
@@ -23,15 +11,14 @@ public class Wurm_s_Tooth {
                         MagicSimpleMayChoice.DEFAULT_YES
                     ),
                     this,
-                    "PN may$ gain 1 life.") :
+                    "PN may\$ gain 1 life.") :
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
             }
         }
-    };
-}
+    }
+]
