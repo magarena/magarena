7efb23b2382476999fa01cd97f63cc2c1b37ff57
melvin <melvinzhang@gmail.com>
2013-05-18 13:16:04 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Claustrophobia.groovy
+++ release/Magarena/scripts/Claustrophobia.groovy
@@ -1,14 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicTapAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Claustrophobia {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
             final MagicPermanent enchantedCreature = permanent.getEnchantedCreature();
@@ -17,18 +8,17 @@ public class Claustrophobia {
                     permanent,
                     this,
                     "Tap " + enchantedCreature + ". It doesn't " +
-                    "untap during its controller's untap step") :
+                    "untap during its controller's untap step"
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent= event.getPermanent();
             final MagicPermanent enchantedCreature = permanent.getEnchantedCreature();
             if (enchantedCreature.isValid()) {
                 game.doAction(new MagicTapAction(enchantedCreature,true));
             }
         }        
-    };
-}
+    }
+]
