005c448a5623f3fe67fdfb5c9006f30999f05f08
melvin <melvinzhang@gmail.com>
2013-05-18 13:14:15 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Alchemist_s_Apprentice.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+        ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+        }
+    }
+]
