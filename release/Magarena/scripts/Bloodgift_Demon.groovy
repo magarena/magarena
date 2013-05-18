50f05dffe2f5e51f4b02fc0f4ee54226288d352a
melvin <melvinzhang@gmail.com>
2013-05-18 15:34:25 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Bloodgift_Demon.groovy
+++ release/Magarena/scripts/Bloodgift_Demon.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicDrawAction;
-import magic.model.action.MagicPlayerAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Bloodgift_Demon {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -24,9 +12,7 @@ public class Bloodgift_Demon {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
                     game.doAction(new MagicDrawAction(player,1));
@@ -34,5 +20,5 @@ public class Bloodgift_Demon {
                 }
             });
         }
-    };
-}
+    }
+]
