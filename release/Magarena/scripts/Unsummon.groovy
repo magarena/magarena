cfbc6eeceab3c2968a9d5255c1b5efdc6b869814
melvin <melvinzhang@gmail.com>
2013-05-18 15:33:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Unsummon.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target creature$ to its owner's hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+                }
+            });
+        }
+    }
+]
