dbfec10dfabc7d652593bd5539928f2e92cee3a2
melvin <melvinzhang@gmail.com>
2013-05-18 18:46:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Kitsune_Dawnblade.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(MagicTargetChoice.NEG_TARGET_CREATURE),
+                new MagicTapTargetPicker(true,false),
+                this,
+                "PN may\$ tap target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetPermanent(game,new MagicPermanentAction() {
+                    public void doAction(final MagicPermanent creature) {
+                        game.doAction(new MagicTapAction(creature,true));
+                    }
+                });
+            }
+        }
+    }
+]
