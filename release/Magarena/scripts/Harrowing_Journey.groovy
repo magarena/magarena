019fe4a65e3f8337a920eccb8cad6003e08305d5
melvin <melvinzhang@gmail.com>
2013-05-18 12:10:05 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Harrowing_Journey.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ draws three cards and loses 3 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicDrawAction(player,3));
+                    game.doAction(new MagicChangeLifeAction(player,-3));
+                }
+            });
+        }
+    }
+]
