c0b8322cc91318c6010a744681d1d0219f178d5c
melvin <melvinzhang@gmail.com>
2013-05-18 15:34:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Raksha_Golden_Cub.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_CAT_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(2,2);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.isEquipped();
+        }
+    },
+    new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_CAT_YOU_CONTROL) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.DoubleStrike);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.isEquipped();
+        }
+    }
+]
