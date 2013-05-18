f9094cb3d675803bd32b7fa0e74243f1799a1a0d
melvin <melvinzhang@gmail.com>
2013-05-18 14:15:03 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Argentum_Armor.groovy
+++ release/Magarena/scripts/Argentum_Armor.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-
-public class Argentum_Armor {
-    public static final MagicWhenAttacksTrigger T =new MagicWhenAttacksTrigger(1) {
+[
+    new MagicWhenAttacksTrigger(1) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             return permanent.getEquippedCreature() == attacker ?
@@ -20,19 +8,17 @@ public class Argentum_Armor {
                     MagicTargetChoice.TARGET_PERMANENT,
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "Destroy target permanent$."
+                    "Destroy target permanent\$."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent permanent) {
                     game.doAction(new MagicDestroyAction(permanent));
                 }
             });
         }
-    };
-}
+    }
+]
