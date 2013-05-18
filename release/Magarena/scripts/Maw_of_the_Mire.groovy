378c93d5fa16b0488ba8ac08c40d092f1be0ad24
melvin <melvinzhang@gmail.com>
2013-05-18 19:57:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Maw_of_the_Mire.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target land\$. PN gains 4 life."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                     game.doAction(new MagicDestroyAction(permanent));
+                     game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+                }
+            });
+        }
+    }
+]
