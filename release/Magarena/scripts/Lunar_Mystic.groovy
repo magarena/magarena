c1eaccb3d493aeab655d1a3f59219f82b62be6ea
melvin <melvinzhang@gmail.com>
2013-05-18 18:42:51 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Lunar_Mystic.groovy
+++ release/Magarena/scripts/Lunar_Mystic.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-public class Lunar_Mystic {
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -25,17 +13,16 @@ public class Lunar_Mystic {
                         new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
                     ),
                     this,
-                    "PN may$ pay {1}$. If you do, draw a card."):
+                    "PN may\$ pay {1}\$. If you do, draw a card."
+                ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicDrawAction(event.getPlayer(),1));
             }
         }
-    };
-}
+    }
+]
