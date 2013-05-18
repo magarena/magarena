851887941a46ad4f343de307181317d36e1e01d0
melvin <melvinzhang@gmail.com>
2013-05-18 13:10:50 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Absorb.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. PN gains 3 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
+                }
+            });
+        }
+    }
+]
