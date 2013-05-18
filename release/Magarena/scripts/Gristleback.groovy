846d3c37339dc4d22896868775b1fc674691019d
melvin <melvinzhang@gmail.com>
2013-05-18 18:35:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Gristleback.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains life equal to SN's power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(
+                event.getPlayer(),
+                event.getPermanent().getPower()
+            ));
+        }
+    }
+]
