2c2f0b54cb74fb6303784125a96caea53c9aebff
melvin <melvinzhang@gmail.com>
2013-05-18 20:49:02 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Chasm_Drake.groovy
+++ release/Magarena/scripts/Chasm_Drake.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicFlyingTargetPicker;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-
-public class Chasm_Drake {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+[
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent == creature) ?
@@ -21,20 +8,18 @@ public class Chasm_Drake {
                     MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
                     MagicFlyingTargetPicker.create(),
                     this,
-                    "Target creature$ you control gains flying until end of turn."
+                    "Target creature\$ you control gains flying until end of turn."
                 ):
                 MagicEvent.NONE;           
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Flying));
                 }
             });
         }
-    };
-}
+    }
+]
