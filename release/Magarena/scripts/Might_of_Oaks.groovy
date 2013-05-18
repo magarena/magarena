93004fbe4f3ab3b1238e1a1f2b56e1f0845ece10
melvin <melvinzhang@gmail.com>
2013-05-18 12:36:14 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Might_of_Oaks.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +7/+7 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,7,7));
+                }
+            });
+        }
+    }
+]
