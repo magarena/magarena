541bfb5f1c5b29dd54ac202ea32d435f020f9042
melvin <melvinzhang@gmail.com>
2013-05-18 12:09:45 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Deathgreeter.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (permanent != otherPermanent &&
+                    otherPermanent.isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.GAIN_LIFE,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ gain 1 life.") :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+            }
+        }
+    }
+]
