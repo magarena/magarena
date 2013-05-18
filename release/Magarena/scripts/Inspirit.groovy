b5d6d463571274735fbf432844a52b3823832998
melvin <melvinzhang@gmail.com>
2013-05-18 19:55:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Inspirit.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Untap target creature\$. It gets +2/+4 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,4));
+                }
+            });
+        }
+    }
+]
