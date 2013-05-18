8e7d104fe7a81baede7f122c5b5c2de31984348a
melvin <melvinzhang@gmail.com>
2013-05-18 18:42:21 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Mask_of_Riddles.groovy
+++ release/Magarena/scripts/Mask_of_Riddles.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-public class Mask_of_Riddles {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -23,20 +13,18 @@ public class Mask_of_Riddles {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.DRAW_CARDS,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_NONE),
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
                     this,
                     "PN may$ draw a card."
                 ) :
                 MagicEvent.NONE;
         }
-
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicDrawAction(event.getPlayer(), 1));
             }
         }
-    };
-}
+    }
+]
