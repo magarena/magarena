60c0ef81bf6330cbd5bc92214585c77323918813
melvin <melvinzhang@gmail.com>
2013-05-18 20:49:50 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Tidewalker.groovy
+++ release/Magarena/scripts/Tidewalker.groovy
@@ -1,28 +1,12 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicFadeVanishCounterTrigger;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Tidewalker {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.SetPT) {
+[
+    new MagicStatic(MagicLayer.SetPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             final int amount = permanent.getCounters(MagicCounterType.Charge);
             pt.set(amount,amount);
         }
-    };
-    
-    public static final MagicWhenComesIntoPlayTrigger T2 = new MagicWhenComesIntoPlayTrigger() {
+    },
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             final int amount = game.filterPermanents(player,MagicTargetFilter.TARGET_ISLAND_YOU_CONTROL).size();
@@ -34,5 +18,5 @@ public class Tidewalker {
             ));
             return MagicEvent.NONE;
         }
-    };
-}
+    }
+]
