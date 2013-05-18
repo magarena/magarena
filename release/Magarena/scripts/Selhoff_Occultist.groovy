ba6a2e8561b1102120d2b3dfcaae42ea075bd7dd
melvin <melvinzhang@gmail.com>
2013-05-18 19:42:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Selhoff_Occultist.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return otherPermanent.isCreature() ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    this,
+                    "Target player\$ puts the top card of his or her library into his or her graveyard."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicMillLibraryAction(player,1));
+                }
+            });
+        }
+    }
+]
