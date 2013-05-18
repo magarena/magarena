068cbc49cda22f438d45df3667ee9d064b0ded27
melvin <melvinzhang@gmail.com>
2013-05-18 13:54:13 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Midnight_Banshee.groovy
+++ release/Magarena/scripts/Midnight_Banshee.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-import java.util.Collection;
-
-public class Midnight_Banshee {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -24,14 +11,12 @@ public class Midnight_Banshee {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets=
                 game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_NONBLACK_CREATURE);
             for (final MagicPermanent target : targets) {
                 game.doAction(new MagicChangeCountersAction(target,MagicCounterType.MinusOne,1,true));
             }
         }
-    };
-}
+    }
+]
