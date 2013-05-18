639fa4aa69825175923fae830cbc82e33cdb66dc
melvin <melvinzhang@gmail.com>
2013-05-18 18:41:15 +0800

    fix scripts

--- release/Magarena/scripts/Caustic_Crawler.groovy
+++ release/Magarena/scripts/Caustic_Crawler.groovy
@@ -9,7 +9,7 @@
                 ),
                 new MagicWeakenTargetPicker(1,1),
                 this,
-                "PN may$ have target creature$ get -1/-1 until end of turn."
+                "PN may\$ have target creature\$ get -1/-1 until end of turn."
             );
         }
         @Override
