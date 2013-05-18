d18ef5c7a4920ccba1aaf92f588d4c598fa9f5a4
melvin <melvinzhang@gmail.com>
2013-05-18 21:31:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Starlight_Invoker.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{7}{W}")],
+        new MagicActivationHints(MagicTiming.Main,true),
+        "Life"
+        ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{7}{W}"))];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains 5 life."
+            );
+        }
+
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),5));
+        }
+    }
+]
