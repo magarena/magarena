2b29b7a84eb7f899a4cd3f7a24837f8df6e6e2d1
melvin <melvinzhang@gmail.com>
2013-05-18 15:14:18 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Farbog_Boneflinger.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_CREATURE,
+                new MagicWeakenTargetPicker(2,2),
+                this,
+                "Target creature\$ gets -2/-2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-2,-2));
+                }
+            });
+        }
+    }
+]
