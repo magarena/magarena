3832afc441b206332b3ad915c1e463fc11698993
melvin <melvinzhang@gmail.com>
2013-05-18 12:03:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Scute_Mob.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return (permanent.isController(upkeepPlayer) &&
+                    upkeepPlayer.getNrOfPermanentsWithType(MagicType.Land) >= 5) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Put four +1/+1 counters on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                4,
+                true
+            ));
+        }
+    }
+]
