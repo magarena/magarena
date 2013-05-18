3beddf5829c5d3f440eb02b3a4633a60b9d3827f
melvin <melvinzhang@gmail.com>
2013-05-18 19:58:17 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Somberwald_Vigilante.groovy
+++ release/Magarena/scripts/Somberwald_Vigilante.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentList;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-
-public class Somberwald_Vigilante {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -27,16 +17,13 @@ public class Somberwald_Vigilante {
                 "SN deals 1 damage to each blocking creature."
             );
         }
-        
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanentList plist = event.getRefPermanentList();
             for (final MagicPermanent blocker : plist) {
                 final MagicDamage damage = new MagicDamage(event.getSource(),blocker,1);
                 game.doAction(new MagicDealDamageAction(damage));
             }
         }
-    };
-}
+    }
+]
