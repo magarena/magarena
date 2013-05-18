e9bc779b3a601dc730b689aae4c08fb7442e0a1d
melvin <melvinzhang@gmail.com>
2013-05-18 13:12:23 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Dimir_Cutpurse.groovy
+++ release/Magarena/scripts/Dimir_Cutpurse.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicDiscardEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-public class Dimir_Cutpurse {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return (damage.getSource() == permanent &&
@@ -20,17 +10,14 @@ public class Dimir_Cutpurse {
                     permanent.getController(),
                     damage.getTarget(),
                     this,
-                    damage.getTarget() + " discards a card and PN draws a card"
+                    "RN discards a card and PN draws a card"
                 ) :
                 MagicEvent.NONE;
         }
-        
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.addEvent(new MagicDiscardEvent(event.getSource(), event.getRefPlayer(), 1, false));
             game.doAction(new MagicDrawAction(event.getPlayer(),1));
         }
-    };
-}
+    }
+]
