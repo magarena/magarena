5dd3b255607f2fa7e3869ca16caafd7356930c0a
melvin <melvinzhang@gmail.com>
2013-05-18 15:07:01 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Griselbrand.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicPermanentActivation( 
+        [MagicCondition.SEVEN_LIFE_CONDITION],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayLifeEvent(source,source.getController(),7),
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Draw seven cards"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),7));
+        }
+    }
+]
