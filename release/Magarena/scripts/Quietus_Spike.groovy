b00460660173dd8865db5eeb852faf05b3289e83
melvin <melvinzhang@gmail.com>
2013-05-18 13:13:41 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Quietus_Spike.groovy
+++ release/Magarena/scripts/Quietus_Spike.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-
-public class Quietus_Spike {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicTarget target=damage.getTarget();
@@ -27,11 +15,9 @@ public class Quietus_Spike {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             game.doAction(new MagicChangeLifeAction(player,-(player.getLife()+1)/2));
         }
-    };
-}
+    }
+]
