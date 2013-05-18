d27656d91dd289b6cd7b035f0f52ea76cdbb0d50
melvin <melvinzhang@gmail.com>
2013-05-18 13:51:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hollowhenge_Spirit.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                MagicTargetChoice.TARGET_ATTACKING_OR_BLOCKING_CREATURE,
+                this,
+                "Remove target attacking or blocking creature\$ from combat."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromCombatAction(creature));
+                }
+            });
+        }
+    }
+]
