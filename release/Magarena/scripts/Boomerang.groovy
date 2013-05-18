b18046e85a73b57e9af0b6936034a96e08824fd8
melvin <melvinzhang@gmail.com>
2013-05-18 15:37:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Boomerang.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PERMANENT,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target permanent\$ to its owner's hand."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
+                }
+            });
+        }
+    }
+]
