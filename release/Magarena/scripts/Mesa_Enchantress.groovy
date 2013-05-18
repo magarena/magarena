a4ea8daecce14b2940ab30a7bd0869302312af0a
melvin <melvinzhang@gmail.com>
2013-05-18 12:08:39 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Mesa_Enchantress.groovy
+++ release/Magarena/scripts/Mesa_Enchantress.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-
-public class Mesa_Enchantress {
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
             return (permanent.isFriend(spell) && 
@@ -20,18 +9,18 @@ public class Mesa_Enchantress {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.DRAW_CARDS,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_NONE),
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
                     this,
-                    "PN may$ draw a card."
+                    "PN may\$ draw a card."
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicDrawAction(event.getPlayer(),1));
             }
         }
-    };
-}
+    }
+]
