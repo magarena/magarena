8cecea504c75ae96f2701a6ca7d0b4ed135e309b
melvin <melvinzhang@gmail.com>
2013-05-18 21:23:38 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Awakening_Zone.groovy
+++ release/Magarena/scripts/Awakening_Zone.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Awakening_Zone {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -19,21 +8,20 @@ public class Awakening_Zone {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.PLAY_TOKEN,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a 0/1 colorless Eldrazi Spawn " +
+                    "PN may\$ put a 0/1 colorless Eldrazi Spawn " +
                     "creature token onto the battlefield. It has " +
                     "\"Sacrifice this creature: Add {1} to your mana pool.\""
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Eldrazi Spawn")));
             }
         }        
-    };
-}
+    }
+]
