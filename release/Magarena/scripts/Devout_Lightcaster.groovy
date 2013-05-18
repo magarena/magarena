8ff2e4b3ca6799d906b5516270508dd6b102e7aa
melvin <melvinzhang@gmail.com>
2013-05-18 15:14:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Devout_Lightcaster.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_BLACK_PERMANENT,
+                MagicExileTargetPicker.create(),
+                this,
+                "Exile target black permanent\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.Exile));
+                }
+            });
+        }
+    }
+]
