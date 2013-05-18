eb06f01f48939dd31226cbe0a33bda0eccb07382
melvin <melvinzhang@gmail.com>
2013-05-18 21:06:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vampiric_Fury.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Vampire creatures PN controls get +2/+0 " +
+                "and gain first strike until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                    event.getPlayer(),
+                    MagicTargetFilter.TARGET_VAMPIRE_YOU_CONTROL);
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature,2,0));
+                game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike));
+            }
+        }
+    }
+]
