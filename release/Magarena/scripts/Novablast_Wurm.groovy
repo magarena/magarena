b514255eac978dfe47fd07ba63085e57a7e3f9ce
melvin <melvinzhang@gmail.com>
2013-05-18 15:35:37 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Novablast_Wurm.groovy
+++ release/Magarena/scripts/Novablast_Wurm.groovy
@@ -1,30 +1,17 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-import java.util.Collection;
-
-public class Novablast_Wurm {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+[
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent==creature) ?
                 new MagicEvent(
-                        permanent,
-                        this,
-                        "Destroy all creatures other than SN.") :
+                    permanent,
+                    this,
+                    "Destroy all creatures other than SN."
+                ) :
                 MagicEvent.NONE;
         }
-        
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent=event.getPermanent();
             final MagicTargetFilter<MagicPermanent> targetFilter = 
                     new MagicTargetFilter.MagicOtherPermanentTargetFilter(
@@ -33,5 +20,5 @@ public class Novablast_Wurm {
                 game.filterPermanents(permanent.getController(),targetFilter);
             game.doAction(new MagicDestroyAction(targets));
         }
-    };
-}
+    }
+]
