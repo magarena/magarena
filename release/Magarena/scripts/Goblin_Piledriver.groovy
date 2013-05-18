91d6faffd7edc47be9ecc2a81c7f3273cea9d495
melvin <melvinzhang@gmail.com>
2013-05-18 14:14:07 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Goblin_Piledriver.groovy
+++ release/Magarena/scripts/Goblin_Piledriver.groovy
@@ -1,29 +1,17 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-import java.util.Collection;
-
-public class Goblin_Piledriver {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+[
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent==creature) ?
                 new MagicEvent(
                     permanent,
                     this,
-                    "SN gets +2/+0 until end of turn for each other attacking Goblin."):
+                    "SN gets +2/+0 until end of turn for each other attacking Goblin."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent creature=event.getPermanent();
             final Collection<MagicPermanent> targets=
                 game.filterPermanents(creature.getController(),MagicTargetFilter.TARGET_ATTACKING_GOBLIN);
@@ -33,5 +21,5 @@ public class Goblin_Piledriver {
                 game.doAction(new MagicChangeTurnPTAction(creature,power,0));
             }
         }
-    };
-}
+    }
+]
