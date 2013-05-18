90fe822019cf9706f4e1d04b0b067eb02496ef9f
melvin <melvinzhang@gmail.com>
2013-05-18 15:08:32 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Thresher_Beast.groovy
+++ release/Magarena/scripts/Thresher_Beast.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicType;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSacrificePermanentEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-
-public class Thresher_Beast {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocked) {
             return (permanent == blocked) ?
@@ -22,11 +11,8 @@ public class Thresher_Beast {
                 ):
                 MagicEvent.NONE;
         }
-        
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer opponent = event.getPlayer();
             if (opponent.controlsPermanentWithType(MagicType.Land)) {
                 game.addEvent(new MagicSacrificePermanentEvent(
@@ -36,5 +22,5 @@ public class Thresher_Beast {
                 ));
             }
         }
-    };
-}
+    }
+]
