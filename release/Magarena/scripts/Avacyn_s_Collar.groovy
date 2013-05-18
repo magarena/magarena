a70200e8cd7e29a249c490e8e3e22f007c497c2f
melvin <melvinzhang@gmail.com>
2013-05-18 19:59:10 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Avacyn_s_Collar.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent died) {
+            final MagicPermanent equipped = permanent.getEquippedCreature();
+            return (equipped == died &&
+                    equipped.hasSubType(MagicSubType.Human)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a 1/1 white Spirit creature " +
+                    "token with flying onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicPlayTokenAction(
+                player,
+                TokenCardDefinitions.get("Spirit2")
+            ));
+        }
+    }
+]
