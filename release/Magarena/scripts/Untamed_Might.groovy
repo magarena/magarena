2b40275bc3b53eaf91fd3e0106f263cf0e4ab5c9
melvin <melvinzhang@gmail.com>
2013-05-18 20:49:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Untamed_Might.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature$ gets +" + amount + "/+" + amount + " until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final int X = event.getCardOnStack().getX();
+                    game.doAction(new MagicChangeTurnPTAction(creature,X,X));
+                }
+            });
+        }
+    }
+]
