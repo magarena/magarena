306f5121d4d336771038311327a438b5bd80fe62
melvin <melvinzhang@gmail.com>
2013-05-18 15:10:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Psychic_Barrier.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_SPELL,
+                this,
+                "Counter target creature spell\$. Its controller loses 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicChangeLifeAction(targetSpell.getController(),-1));
+                }
+            });
+        }
+    }
+]
