222e372295d5d518b047a9a212d88ba140149351
melvin <melvinzhang@gmail.com>
2013-05-18 11:54:07 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Seedborn_Muse.groovy
+++ release/Magarena/scripts/Seedborn_Muse.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicUntapAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-import java.util.Collection;
-
-public class Seedborn_Muse {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isOpponent(upkeepPlayer) ?
@@ -23,9 +11,7 @@ public class Seedborn_Muse {
                 MagicEvent.NONE;
         }    
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final Collection<MagicPermanent> targets =
                 game.filterPermanents(player,MagicTargetFilter.TARGET_PERMANENT_YOU_CONTROL);
@@ -33,5 +19,5 @@ public class Seedborn_Muse {
                 game.doAction(new MagicUntapAction(target));
             }
         }
-    };
-}
+    }
+]
