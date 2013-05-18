662150e2d312d916d5217598536158cfb3913142
melvin <melvinzhang@gmail.com>
2013-05-18 20:50:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Thalakos_Scout.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.HAS_CARD_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Return") {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicDiscardEvent(source,source.getController(),1,false)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Return SN to its owner's hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicRemoveFromPlayAction(event.getPermanent(),MagicLocationType.OwnersHand));
+        }
+    }
+]
