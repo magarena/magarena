081f17e680fcda5450fa7d0ec59db846566d59a9
melvin <melvinzhang@gmail.com>
2013-05-18 20:52:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Glissa_s_Scorn.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact\$. Its controller loses 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    game.doAction(new MagicChangeLifeAction(permanent.getController(),-1));
+                }
+            });
+        }
+    }
+]
