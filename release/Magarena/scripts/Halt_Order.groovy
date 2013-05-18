7905fc9f5419cced57d3316dc0c68f1421915250
melvin <melvinzhang@gmail.com>
2013-05-18 13:14:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Halt_Order.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_SPELL,
+                this,
+                "Counter target artifact spell\$. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
