cd62033cb19162369c16e3c91447714485f47575
melvin <melvinzhang@gmail.com>
2013-05-18 14:14:23 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Szadek__Lord_of_Secrets.groovy
+++ release/Magarena/scripts/Szadek__Lord_of_Secrets.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicMillLibraryAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
-
-public class Szadek__Lord_of_Secrets {
-    public static final MagicIfDamageWouldBeDealtTrigger T = new MagicIfDamageWouldBeDealtTrigger(6) {
+[
+    new MagicIfDamageWouldBeDealtTrigger(6) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final int amount=damage.getAmount();
@@ -26,12 +14,5 @@ public class Szadek__Lord_of_Secrets {
             }            
             return MagicEvent.NONE;
         }
-        
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
-        
-        }
-    };
-}
+    }
+]
