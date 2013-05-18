2c6fdadc16cc0b30ef1e6a395295ad128431677f
melvin <melvinzhang@gmail.com>
2013-05-18 13:45:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Guiltfeeder.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getOpponent(),
+                    this,
+                    "PN loses 1 life for each card in his or her graveyard."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicChangeLifeAction(
+                player,
+                -player.getGraveyard().size()
+            ));
+        }
+    }
+]
