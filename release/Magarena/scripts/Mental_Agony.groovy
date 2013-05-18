4aa332dfa72ec5215df5c1884c4aed2fc01ffb09
melvin <melvinzhang@gmail.com>
2013-05-18 21:06:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mental_Agony.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ discards two cards and loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(
+                        event.getSource(),
+                        player,
+                        2,
+                        false
+                    ));
+                    game.doAction(new MagicChangeLifeAction(player,-2));
+                }
+            });
+        }
+    }
+]
