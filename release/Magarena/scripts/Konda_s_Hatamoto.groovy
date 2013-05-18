d70b5d4e9e3cb87fd9e3ffe978b194d1426b5e48
melvin <melvinzhang@gmail.com>
2013-05-18 19:50:22 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Konda_s_Hatamoto.groovy
+++ release/Magarena/scripts/Konda_s_Hatamoto.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTarget;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-import java.util.Set;
-
-public class Konda_s_Hatamoto {
-    
-    public static final MagicStatic S1 = new MagicStatic(MagicLayer.Ability) {
+[
+    new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             final MagicGame game = source.getGame();
@@ -22,9 +7,8 @@ public class Konda_s_Hatamoto {
                 flags.add(MagicAbility.Vigilance);
             }
         }
-    };
-    
-    public static final MagicStatic S2 = new MagicStatic(MagicLayer.ModPT) {
+    },
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             final MagicGame game = source.getGame();
@@ -34,5 +18,5 @@ public class Konda_s_Hatamoto {
                 pt.add(1,2);
             }        
         }
-    };
-}
+    }
+]
