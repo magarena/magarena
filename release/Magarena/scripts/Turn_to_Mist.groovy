db23b35ab3ea1813731350a8a6f1a92b30825e6f
melvin <melvinzhang@gmail.com>
2013-05-18 15:23:38 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Turn_to_Mist.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "Exile target creature\$. Return that card to the battlefield under its owner's control at end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicExileUntilEndOfTurnAction(creature));
+                }
+            });
+        }
+    }
+]
