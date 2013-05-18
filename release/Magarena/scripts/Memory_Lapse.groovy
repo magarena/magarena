f13ce9879c938855f066d6d321d78d32344b7372
melvin <melvinzhang@gmail.com>
2013-05-18 12:09:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Memory_Lapse.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. If you do, put it on top of its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell,MagicLocationType.TopOfOwnersLibrary));
+                }
+            });
+        }
+    }
+]
