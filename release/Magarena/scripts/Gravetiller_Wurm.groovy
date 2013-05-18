993debb9f61fb35a0c4a9db341f2791cdb94e4ca
melvin <melvinzhang@gmail.com>
2013-05-18 14:01:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Gravetiller_Wurm.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            if (game.getCreatureDiedThisTurn()) {
+                game.doAction(new MagicChangeCountersAction(
+                    permanent,
+                    MagicCounterType.PlusOne,
+                    4,
+                    true
+                ));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
