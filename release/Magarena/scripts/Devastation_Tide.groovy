d5c23a83105414779fe30212b7843737c1c2b9f4
melvin <melvinzhang@gmail.com>
2013-05-18 15:23:16 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Devastation_Tide.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Return all nonland permanents to their owners' hands."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                    event.getPlayer(),
+                    MagicTargetFilter.TARGET_NONLAND_PERMANENT);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicRemoveFromPlayAction(
+                    target,
+                    MagicLocationType.OwnersHand
+                ));
+            }
+        }
+    }
+]
