3c6e808c3a056208920e9765ed754e7c1d1a92ad
melvin <melvinzhang@gmail.com>
2013-05-18 21:26:10 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vault_Skyward.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicFlyingTargetPicker.create(),
+                this,
+                "Target creature\$ gains flying until end of turn. Untap it."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Flying));
+                    game.doAction(new MagicUntapAction(creature));
+                }
+            });
+        }
+    }
+]
