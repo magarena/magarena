ce94449971d7d0f0ab8d527c7527df492bacf443
melvin <melvinzhang@gmail.com>
2013-05-18 14:00:49 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Wheel_of_Fortune.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player discards his or her hand and draws seven cards."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getPlayers()) {
+                final MagicCardList hand = new MagicCardList(player.getHand());
+                for (final MagicCard card : hand) {
+                    game.doAction(new MagicDiscardCardAction(player,card));
+                }            
+                game.doAction(new MagicDrawAction(player, 7));
+            }
+        }
+    }
+]
