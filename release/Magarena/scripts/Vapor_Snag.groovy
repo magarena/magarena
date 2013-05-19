c6704f32fff5b52cb6f73d8f1518a9772f8c4545
melvin <melvinzhang@gmail.com>
2013-05-19 13:30:34 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vapor_Snag.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target creature\$ to its owner's hand. " +
+                "Its controller loses 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicChangeLifeAction(creature.getController(),-1));
+                }
+            });
+        }
+    }
+]
