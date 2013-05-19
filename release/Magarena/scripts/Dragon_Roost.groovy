f26ab4f3280d38cbca3a2666456559b80de0edb9
melvin <melvinzhang@gmail.com>
2013-05-19 16:19:31 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dragon_Roost.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{5}{R}{R}")],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{5}{R}{R}"))];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a 5/5 red Dragon creature token with flying onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Dragon5")));
+        }
+    }
+]
