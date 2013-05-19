12545c21b1100c89df4a4c19a75dacb15ba39ce4
melvin <melvinzhang@gmail.com>
2013-05-19 10:24:32 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Close_Quarters.groovy
+++ release/Magarena/scripts/Close_Quarters.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-
-public class Close_Quarters {
-    public static final MagicWhenBecomesBlockedTrigger T = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             return permanent.isFriend(attacker) ?
@@ -21,15 +8,13 @@ public class Close_Quarters {
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(1),
                     this,
-                    "SN deals 1 damage to target creature or player$."
+                    "SN deals 1 damage to target creature or player\$."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
                     final MagicDamage damage = new MagicDamage(event.getSource(),target,1);
@@ -37,5 +22,5 @@ public class Close_Quarters {
                 }
             });
         }
-    };
-}
+    }
+]
