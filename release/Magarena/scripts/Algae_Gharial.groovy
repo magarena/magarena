9ef2f0963b4caa70dbca31dc3c80a8619947876f
melvin <melvinzhang@gmail.com>
2013-05-19 09:22:01 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Algae_Gharial.groovy
+++ release/Magarena/scripts/Algae_Gharial.groovy
@@ -1,15 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Algae_Gharial {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (permanent != otherPermanent &&
@@ -19,15 +9,15 @@ public class Algae_Gharial {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a +1/+1 counter on SN.") :
+                    "PN may\$ put a +1/+1 counter on SN."
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeCountersAction(
                     event.getPermanent(),
@@ -37,5 +27,5 @@ public class Algae_Gharial {
                 ));
             }
         }
-    };
-}
+    }
+]
