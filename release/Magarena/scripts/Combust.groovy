a39d0a964753acec1e170fa9334e49542fae8f2c
melvin <melvinzhang@gmail.com>
2013-05-19 10:21:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Combust.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_WHITE_OR_BLUE_CREATURE,
+                new MagicDamageTargetPicker(5,true),
+                this,
+                "SN deals 5 damage to target white or blue creature\$. " +
+                "The damage can't be prevented.");
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent target) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,5);
+                    damage.setUnpreventable();
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
