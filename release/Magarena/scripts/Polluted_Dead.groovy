925893ce9e8d4ce228ae337aa64d54b778be7fad
melvin <melvinzhang@gmail.com>
2013-05-19 10:26:18 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Polluted_Dead.groovy
+++ release/Magarena/scripts/Polluted_Dead.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicGraveyardTriggerData;
-import magic.model.trigger.MagicWhenPutIntoGraveyardTrigger;
-
-public class Polluted_Dead {
-    public static final MagicWhenPutIntoGraveyardTrigger T = new MagicWhenPutIntoGraveyardTrigger() {
+[
+    new MagicWhenPutIntoGraveyardTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -24,20 +11,18 @@ public class Polluted_Dead {
                     MagicTargetChoice.TARGET_LAND,
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "Destroy target land$."
+                    "Destroy target land\$."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent permanent) {
                     game.doAction(new MagicDestroyAction(permanent));
                 }
             });
         }
-    };
-}
+    }
+]
