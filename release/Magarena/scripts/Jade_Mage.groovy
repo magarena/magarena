03685ed04cabad4a5c2393935e61087bf52397cd
melvin <melvinzhang@gmail.com>
2013-05-19 13:34:16 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Jade_Mage.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{2}{G}")],
+        new MagicActivationHints(MagicTiming.Token,true),
+        "Token"
+    ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{2}{G}"))];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 1/1 green Saproling creature token onto the battlefield."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Saproling")));
+        }
+    }
+]
