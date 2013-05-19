99b9ebc418e3b04908900cc28ed974c6708009ee
melvin <melvinzhang@gmail.com>
2013-05-19 16:08:55 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Darkthicket_Wolf.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ABILITY_ONCE_CONDITION,
+            MagicConditionFactory.ManaCost("{2}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{2}{G}")),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +2/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent=event.getPermanent();
+            game.doAction(new MagicChangeTurnPTAction(permanent,2,2));
+        }
+    }
+]
