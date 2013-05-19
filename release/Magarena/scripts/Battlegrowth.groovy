da9d35470b10194fc73e4d0f6634c92e97476eb1
melvin <melvinzhang@gmail.com>
2013-05-19 09:17:50 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Battlegrowth.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Put a +1/+1 counter on target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        1,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
