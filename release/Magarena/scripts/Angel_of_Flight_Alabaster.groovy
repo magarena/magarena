bb6366da465db118dde5d02081c17009d9a2658f
melvin <melvinzhang@gmail.com>
2013-05-19 16:05:11 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Angel_of_Flight_Alabaster.groovy
+++ release/Magarena/scripts/Angel_of_Flight_Alabaster.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicGraveyardTargetPicker;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Angel_of_Flight_Alabaster {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -24,14 +8,12 @@ public class Angel_of_Flight_Alabaster {
                     MagicTargetChoice.TARGET_SPIRIT_CARD_FROM_GRAVEYARD,
                     new MagicGraveyardTargetPicker(false),
                     this,
-                    "Return target Spirit card$ from your graveyard to your hand."
+                    "Return target Spirit card\$ from your graveyard to your hand."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetCard(game,new MagicCardAction() {
                 public void doAction(final MagicCard card) {
                     game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
@@ -39,5 +21,5 @@ public class Angel_of_Flight_Alabaster {
                 }
             });
         }
-    };
-}
+    }
+]
