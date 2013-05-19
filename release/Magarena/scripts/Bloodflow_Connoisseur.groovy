73d01a0aa64cce6929aa55a1703f82d2560e73e2
melvin <melvinzhang@gmail.com>
2013-05-19 16:20:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bloodflow_Connoisseur.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.ONE_CREATURE_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a +1/+1 counter on SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                1,
+                true
+            ));
+        }
+    }
+]
