3456a35caf89265ff3baa8ef848eb14f614d3e87
melvin <melvinzhang@gmail.com>
2013-05-19 09:30:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Instill_Infection.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(1,1),
+                this,
+                "Put a -1/-1 counter on target creature\$. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.MinusOne,1,true));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
