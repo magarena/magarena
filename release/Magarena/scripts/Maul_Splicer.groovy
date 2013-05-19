495e9e1146031b8913daf00a815759913791373f
melvin <melvinzhang@gmail.com>
2013-05-19 13:58:13 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Maul_Splicer.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicStatic(
+            MagicLayer.Ability, 
+            MagicTargetFilter.TARGET_GOLEM_YOU_CONTROL) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Trample);
+        }
+    },
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                this,
+                "PN puts two 3/3 colorless Golem artifact creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Golem3"),
+                2
+            ));
+        }        
+    }
+]
