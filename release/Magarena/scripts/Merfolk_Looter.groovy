e2bf4e89becae3b1effd45dee7eb157cc03b6511
melvin <melvinzhang@gmail.com>
2013-05-19 09:41:02 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Merfolk_Looter.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card, then discards a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicDrawAction(player,1));
+            game.addEvent(new MagicDiscardEvent(event.getPermanent(),player,1,false));
+        }
+    }
+]
