be3578bd389a746ffc371d8449bb651459dbc27b
melvin <melvinzhang@gmail.com>
2013-05-19 13:29:15 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Blood_Artist.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    this,
+                    "Target player\$ loses 1 life and PN gains 1 life."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-1));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
