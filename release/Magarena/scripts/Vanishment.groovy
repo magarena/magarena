59653efbf3fc4239c4dd166ebb86031e148f895a
melvin <melvinzhang@gmail.com>
2013-05-19 09:39:51 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vanishment.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_NONLAND_PERMANENT,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Put target nonland permanent\$ on top of its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(
+                        creature,
+                        MagicLocationType.TopOfOwnersLibrary
+                    ));
+                }
+            });
+        }
+    }
+]
