32c8b4e5daab7bba883fc158f5654197beaa5065
melvin <melvinzhang@gmail.com>
2013-05-19 10:56:54 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Blowfly_Infestation.groovy
+++ release/Magarena/scripts/Blowfly_Infestation.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicWeakenTargetPicker;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Blowfly_Infestation {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isCreature() &&
@@ -21,19 +9,17 @@ public class Blowfly_Infestation {
                     MagicTargetChoice.TARGET_CREATURE,
                     new MagicWeakenTargetPicker(1,1),
                     this,
-                    "Put a -1/-1 counter on target creature$."
+                    "Put a -1/-1 counter on target creature\$."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.MinusOne,1,true));
                 }
             });
         }
-    };
-}
+    }
+]
