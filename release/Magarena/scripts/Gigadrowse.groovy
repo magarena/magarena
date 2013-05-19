7883acc4b4c30e8223fc3dffd30c9f32a7de93f2
melvin <melvinzhang@gmail.com>
2013-05-19 10:25:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Gigadrowse.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicKickerChoice(
+                    MagicTargetChoice.NEG_TARGET_PERMANENT, 
+                    MagicManaCost.create("{U}"), 
+                    true, 
+                    true
+                ),
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target permanent\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicTapAction(permanent,true));
+                }
+            });
+        }
+    }
+]
