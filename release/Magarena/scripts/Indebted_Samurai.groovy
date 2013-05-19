e85759fa6195d81841cba19bde9922b849b0b1a0
melvin <melvinzhang@gmail.com>
2013-05-19 09:42:28 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Indebted_Samurai.groovy
+++ release/Magarena/scripts/Indebted_Samurai.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Indebted_Samurai {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isCreature() &&
@@ -21,19 +10,19 @@ public class Indebted_Samurai {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may put a +1/+1 counter on SN."):
+                    "PN may\$ put a +1/+1 counter on SN."
+                ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
             }
         }
-    };
-}
+    }
+]
