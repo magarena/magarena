f4152ae7a746d99ff8d7ffb07e81869135825593
melvin <melvinzhang@gmail.com>
2013-05-19 15:49:04 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Reclaim.groovy
+++ release/Magarena/scripts/Reclaim.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPayedCost;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicGraveyardTargetPicker;
-
-public class Reclaim {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -22,13 +7,11 @@ public class Reclaim {
                 MagicTargetChoice.TARGET_CARD_FROM_GRAVEYARD,
                 new MagicGraveyardTargetPicker(false),
                 this,
-                "Put target card$ from your graveyard on top of your library."
+                "Put target card\$ from your graveyard on top of your library."
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetCard(game,new MagicCardAction() {
                 public void doAction(final MagicCard targetCard) {
                     game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
@@ -40,5 +23,5 @@ public class Reclaim {
                 }
             });
         }
-    };
-}
+    }
+]
