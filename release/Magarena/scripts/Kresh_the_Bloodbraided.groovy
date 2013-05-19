6978498fff3e93ee4cd79274b9978361ed263dd1
melvin <melvinzhang@gmail.com>
2013-05-19 13:40:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Kresh_the_Bloodbraided.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (permanent != otherPermanent &&
+                    otherPermanent.isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    otherPermanent.getPower(),
+                    this,
+                    "PN may$ put RN +1/+1 counters on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    event.getRefInt(),
+                    true
+                ));
+            }
+        }
+    }
+]
