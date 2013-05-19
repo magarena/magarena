ef170426e541e3f6618411a3cb6008c176c14fd5
melvin <melvinzhang@gmail.com>
2013-05-19 10:28:11 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Shattering_Spree.groovy
+++ release/Magarena/scripts/Shattering_Spree.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicKickerChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDestroyTargetPicker;
-
-public class Shattering_Spree {
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(
                 final MagicCardOnStack cardOnStack,
@@ -29,17 +14,16 @@ public class Shattering_Spree {
                 ),
                 new MagicDestroyTargetPicker(false),
                 this,
-                "Destroy target artifact$.");
+                "Destroy target artifact\$."
+            );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent permanent) {
                     game.doAction(new MagicDestroyAction(permanent));
                 }
             });
         }
-    };
-}
+    }
+]
