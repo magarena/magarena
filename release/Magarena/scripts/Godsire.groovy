4461b63cc3688b2bfd6d75ab5ac329eb84edb94f
melvin <melvinzhang@gmail.com>
2013-05-19 09:39:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Godsire.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put an 8/8 Beast creature token that's red, green and white onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Beast8")));
+        }
+    }
+]
