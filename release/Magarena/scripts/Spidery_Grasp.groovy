e5eff0c758bd13dc4ecb937ece1fcfbe05f0e9ae
melvin <melvinzhang@gmail.com>
2013-05-19 15:48:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Spidery_Grasp.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Untap target creature\$. It gets +2/+4 " +
+                "and gains reach until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,4));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Reach));
+                }
+            });
+        }
+    }
+]
