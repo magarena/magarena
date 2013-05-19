d78f696a5b368c7849ee0e0e78dec5c1fb89aedc
melvin <melvinzhang@gmail.com>
2013-05-19 13:54:13 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Wildsize.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+2 and gains trample until end of turn. Draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
