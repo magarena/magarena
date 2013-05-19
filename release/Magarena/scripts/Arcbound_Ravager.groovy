818cf2375083920918fb44353da508826d615253
melvin <melvinzhang@gmail.com>
2013-05-19 11:09:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Arcbound_Ravager.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CONTROL_ARTIFACT_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_ARTIFACT
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a +1/+1 counter on SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
+        }
+    }
+]
