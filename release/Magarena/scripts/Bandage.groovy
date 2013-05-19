e245701c196df3769d6bb241d468853e22f4aca4
melvin <melvinzhang@gmail.com>
2013-05-19 09:29:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bandage.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER,
+                MagicPreventTargetPicker.getInstance(),
+                this,
+                "Prevent the next 1 damage that would be dealt to target " +
+                "creature or player\$ this turn. Draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    game.doAction(new MagicPreventDamageAction(target,1));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
