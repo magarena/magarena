2b7cdd990fe0a7bdb8b416e2fbaed18dd4838274
melvin <melvinzhang@gmail.com>
2013-05-19 09:30:25 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Repulse.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target creature\$ to its owner's hand. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
