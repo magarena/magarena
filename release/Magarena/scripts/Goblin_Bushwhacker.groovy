3a5d384d82465f582276255fa08e17f8d5ceecf8
melvin <melvinzhang@gmail.com>
2013-05-19 10:27:52 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Goblin_Bushwhacker.groovy
+++ release/Magarena/scripts/Goblin_Bushwhacker.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-import java.util.Collection;
-
-public class Goblin_Bushwhacker {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -29,9 +15,7 @@ public class Goblin_Bushwhacker {
         }
                         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets = 
                 game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
             for (final MagicPermanent creature : targets) {
@@ -39,5 +23,5 @@ public class Goblin_Bushwhacker {
                 game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
             }            
         }
-    };
-}
+    }
+]
