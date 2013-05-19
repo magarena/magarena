f669c22c55af027ba4514625c26e72051208d842
melvin <melvinzhang@gmail.com>
2013-05-19 09:18:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Shadow_Rift.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicShadowTargetPicker.getInstance(),
+                this,
+                "Target creature\$ gains shadow until end of turn. Draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Shadow));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
