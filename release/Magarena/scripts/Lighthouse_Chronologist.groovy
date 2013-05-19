7606455abb6bcea16bda75793e835eabd6390f18
melvin <melvinzhang@gmail.com>
2013-05-19 16:09:21 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Lighthouse_Chronologist.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicStatic(MagicLayer.SetPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final int charges = permanent.getCounters(MagicCounterType.Charge);
+            if (charges >= 7) {
+                pt.set(3,5);
+            } else if (charges >= 4) {
+                pt.set(2,4);
+            }
+        }        
+    },
+    new MagicAtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
+            return (permanent.getCounters(MagicCounterType.Charge) >= 7 &&
+                    permanent.isOpponent(eotPlayer)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN takes an extra turn after this one."
+                ) :
+                MagicEvent.NONE;
+        }    
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeExtraTurnsAction(event.getPlayer(),1));
+        }
+    }
+]
