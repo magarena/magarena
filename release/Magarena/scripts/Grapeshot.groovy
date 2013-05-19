0bdc347483423c08d6a99b45a01df22dbf1c8a43
melvin <melvinzhang@gmail.com>
2013-05-19 10:37:20 +0800

    fix requires and script

--- release/Magarena/scripts/Grapeshot.groovy
+++ release/Magarena/scripts/Grapeshot.groovy
@@ -7,7 +7,7 @@
                 MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                 new MagicDamageTargetPicker(1),
                 this,
-                "SN deals 1 damage to target creature or player$."
+                "SN deals 1 damage to target creature or player\$."
             );
         }
         @Override
