7ab0e0ae1fe7e6f1c2e9cb0bbb73950c3115556d
melvin <melvinzhang@gmail.com>
2013-05-19 16:17:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sudden_Disappearance.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Exile all nonland permanents target player\$ controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final Collection<MagicPermanent> targets =
+                            game.filterPermanents(player,MagicTargetFilter.TARGET_PERMANENT_YOU_CONTROL);
+                    for (final MagicPermanent permanent : targets) {
+                        if (!permanent.isLand()) {
+                            game.doAction(new MagicExileUntilEndOfTurnAction(permanent));    
+                        }
+                    }
+                }
+            });
+        }
+    }
+]
