a82f435dfd8eb87b8be88a42fa65fd392d2c3c58
melvin <melvinzhang@gmail.com>
2013-05-19 10:16:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Kindled_Fury.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicFirstStrikeTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1/+0 and gains first strike until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,0));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike));
+                }
+            });
+        }
+    }
+]
