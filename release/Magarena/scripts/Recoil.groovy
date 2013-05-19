8da974b626c253aca3792f26dfd2359aea831779
melvin <melvinzhang@gmail.com>
2013-05-19 10:48:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Recoil.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PERMANENT,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target permanent\$ to its owner's hand. Then that player discards a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
+                    game.addEvent(new MagicDiscardEvent(event.getSource(),permanent.getOwner(),1,false));
+                }
+            });
+        }
+    }
+]
