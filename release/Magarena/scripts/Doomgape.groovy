b4c4001735e17f457fcc80ebd0c8b8a071fbeaf4
melvin <melvinzhang@gmail.com>
2013-05-19 15:49:58 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Doomgape.groovy
+++ release/Magarena/scripts/Doomgape.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSacrificeAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicSacrificeTargetPicker;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Doomgape {
-
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -30,9 +15,7 @@ public class Doomgape {
         }
 
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     final int toughness=creature.getToughness();
@@ -41,5 +24,5 @@ public class Doomgape {
                 }
             });
         }
-    };
-}
+    }
+]
