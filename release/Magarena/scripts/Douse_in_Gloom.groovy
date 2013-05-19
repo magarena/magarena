102f6f07cfa18488f914c3b0303b1d704f7497f2
melvin <melvinzhang@gmail.com>
2013-05-19 10:53:32 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Douse_in_Gloom.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(2),
+                this,
+                "SN deals 2 damage to target creature\$ and " +
+                "PN gains 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,2);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
+                }
+            });
+        }
+    }
+]
