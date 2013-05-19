ed3f756f12abc15d62486681d51b678ed2d9ed1b
melvin <melvinzhang@gmail.com>
2013-05-19 16:10:18 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Draining_Whelk.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                player,
+                MagicTargetChoice.TARGET_SPELL,
+                this,
+                "Counter target spell\$. Put X +1/+1 counters on SN, " + 
+                "where X is that spell's converted mana cost.");
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack card) {
+                    game.doAction(new MagicCounterItemOnStackAction(card));
+                    game.doAction(new MagicChangeCountersAction(
+                        event.getPermanent(),
+                        MagicCounterType.PlusOne,
+                        card.getConvertedCost(),
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
