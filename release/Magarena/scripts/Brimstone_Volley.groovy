8f937807701c487e0ef04c9fb905ca5c131cc504
melvin <melvinzhang@gmail.com>
2013-05-19 09:31:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Brimstone_Volley.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(3),
+                this,
+                "SN deals 3 damage to target creature or player\$. " + 
+                "SN deals 5 damage to that creature or player instead if a creature died this turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final int amount = game.getCreatureDiedThisTurn() ? 5 : 3;
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,amount);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
