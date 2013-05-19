8a0b724008c5318e864a990bf0ecc653c2c3b1c1
melvin <melvinzhang@gmail.com>
2013-05-19 10:18:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Rush_of_Blood.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature$ gets +X/+0 until " +
+                "end of turn, where X is its power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(
+                        creature,
+                        creature.getPower(),
+                        0
+                    ));
+                }
+            });
+        }
+    }
+]
