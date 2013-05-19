00aa9bb8676b098cdc5fe36b6bd7b7a67074b7af
melvin <melvinzhang@gmail.com>
2013-05-19 09:17:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Cosi_s_Ravager.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(MagicTargetChoice.NEG_TARGET_PLAYER),
+                new MagicDamageTargetPicker(1),
+                this,
+                "PN may\$ have SN deal 1 damage to target player\$"
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTarget(game,new MagicTargetAction() {
+                    public void doAction(final MagicTarget target) {
+                        final MagicDamage damage = new MagicDamage(event.getPermanent(),target,1);
+                        game.doAction(new MagicDealDamageAction(damage));
+                    }
+                });
+            }
+        }        
+    }
+]
