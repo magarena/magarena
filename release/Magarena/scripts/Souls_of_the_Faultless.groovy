2acebcbd5b586368893620528ee150e21e036ce0
melvin <melvinzhang@gmail.com>
2013-05-19 11:07:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Souls_of_the_Faultless.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicPlayer attackingPlayer = damage.getSource().getController();
+            return (damage.getTarget() == permanent && 
+                    damage.isCombat()) ?
+                new MagicEvent(
+                    permanent,
+                    damage.getDealtAmount(),
+                    this,
+                    "PN gains RN life and " +  
+                    attackingPlayer + " loses RN life."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer attackingPlayer = game.getTurnPlayer();
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(), event.getRefInt()));
+            game.doAction(new MagicChangeLifeAction(attackingPlayer, -event.getRefInt()));
+        }
+    }
+]
