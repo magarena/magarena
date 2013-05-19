2e848315781b2f061ba6f018e3b84fdcee06f9f6
melvin <melvinzhang@gmail.com>
2013-05-19 13:40:32 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Backlash.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_UNTAPPED_CREATURE,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target untapped creature\$. That creature deals damage equal to its power to its controller."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                    final MagicDamage damage=new MagicDamage(creature,creature.getController(),creature.getPower());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
