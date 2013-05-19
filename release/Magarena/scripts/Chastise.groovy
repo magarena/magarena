a7f7b76c996ccdd19809c48ad3b040cbaffa386e
melvin <melvinzhang@gmail.com>
2013-05-19 10:33:38 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Chastise.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target attacking creature\$. " + 
+                "PN gains life equal to its power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final int power=creature.getPower();
+                    game.doAction(new MagicDestroyAction(creature));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),power));
+                }
+            });
+        }
+    }
+]
