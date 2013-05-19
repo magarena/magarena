1559535727058123f20df419ade8c2e176c21c9d
melvin <melvinzhang@gmail.com>
2013-05-19 10:29:18 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Volcanic_Fallout.groovy
+++ release/Magarena/scripts/Volcanic_Fallout.groovy
@@ -1,26 +1,12 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.MagicPermanent;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-
-public class Volcanic_Fallout {
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack,
-                    this,
-                    "SN deals 2 damage to each creature and each player.");
+                cardOnStack,
+                this,
+                "SN deals 2 damage to each creature and each player."
+            );
         }
         @Override
         public void executeEvent(
@@ -37,5 +23,5 @@ public class Volcanic_Fallout {
                 game.doAction(new MagicDealDamageAction(damage));
             }
         }
-    };
-}
+    }
+]
