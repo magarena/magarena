d6b7cad6d11b24d427f5d9f9206625217978ea3c
melvin <melvinzhang@gmail.com>
2013-05-19 13:30:53 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Craterhoof_Behemoth.groovy
+++ release/Magarena/scripts/Craterhoof_Behemoth.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-import java.util.Collection;
-
-public class Craterhoof_Behemoth {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -28,9 +14,7 @@ public class Craterhoof_Behemoth {
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets = game.filterPermanents(
                     event.getPlayer(),
                     MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
@@ -40,5 +24,5 @@ public class Craterhoof_Behemoth {
                 game.doAction(new MagicChangeTurnPTAction(creature,amount,amount));
             }
         }
-    };
-}
+    }
+]
