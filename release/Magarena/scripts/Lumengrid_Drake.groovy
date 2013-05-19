06e1de1c327c5c74411c94d9d409a568bcede88d
melvin <melvinzhang@gmail.com>
2013-05-19 10:33:57 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Lumengrid_Drake.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return MagicCondition.METALCRAFT_CONDITION.accept(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_CREATURE,
+                    MagicBounceTargetPicker.getInstance(),
+                    this,
+                    "Return target creature\$ to its owner's hand."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
+                }
+            });
+        }
+    }
+]
