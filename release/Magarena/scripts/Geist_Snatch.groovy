9711975105f1964c7a37ce5fcc8ea900bbd6a4f7
melvin <melvinzhang@gmail.com>
2013-05-19 10:54:40 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Geist_Snatch.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_SPELL,
+                this,
+                "Counter target creature spell\$. Put a 1/1 blue " +
+                "Spirit creature token with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicPlayTokenAction(
+                        event.getPlayer(),
+                        TokenCardDefinitions.get("Spirit1 Blue")
+                    ));
+                }
+            });
+        }
+    }
+]
