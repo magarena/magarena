422599cb389ea7139aabcda93e1a4fede2ea3f0b
melvin <melvinzhang@gmail.com>
2013-05-19 13:39:30 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Sword_of_Body_and_Mind.groovy
+++ release/Magarena/scripts/Sword_of_Body_and_Mind.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicMillLibraryAction;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-
-public class Sword_of_Body_and_Mind {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicTarget targetPlayer=damage.getTarget();
@@ -25,16 +12,14 @@ public class Sword_of_Body_and_Mind {
                     targetPlayer,
                     this,
                     "PN puts a 2/2 green Wolf creature token onto the battlefield and " + 
-                    targetPlayer + " puts the top ten cards of his or her library into his or her graveyard."
+                    "RN puts the top ten cards of his or her library into his or her graveyard."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Wolf")));
             game.doAction(new MagicMillLibraryAction(event.getRefPlayer(),10));
         }
-    };
-}
+    }
+]
