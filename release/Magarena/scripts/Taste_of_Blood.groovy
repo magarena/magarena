4a91fd5b275e1c635e56d2d04c3b9d1684ea6328
melvin <melvinzhang@gmail.com>
2013-05-19 10:51:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Taste_of_Blood.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target player\$ and " +
+                "PN gains 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(),player,1);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
