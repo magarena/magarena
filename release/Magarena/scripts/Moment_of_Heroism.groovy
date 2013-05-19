e49315797d73e65599c5aa7e09332f179c711c5e
melvin <melvinzhang@gmail.com>
2013-05-19 09:41:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Moment_of_Heroism.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicLifelinkTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+2 and gains lifelink until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.LifeLink));
+                }
+            });
+        }
+    }
+]
