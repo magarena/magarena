aef262d196d7a544f008e1c848faba8ad673e16f
melvin <melvinzhang@gmail.com>
2013-05-19 15:49:39 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Phyrexian_Obliterator.groovy
+++ release/Magarena/scripts/Phyrexian_Obliterator.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSacrificePermanentEvent;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-public class Phyrexian_Obliterator {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -23,14 +12,13 @@ public class Phyrexian_Obliterator {
                     damage.getSource().getController(),
                     amount,
                     this,
-                    "PN sacrifices " + amount + " permanents."):
+                    "PN sacrifices RN permanents."
+                ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent = event.getPermanent();
             final MagicPlayer player = event.getPlayer();
             int amount = event.getRefInt();
@@ -43,5 +31,5 @@ public class Phyrexian_Obliterator {
                 amount--;
             }
         }
-    };
-}
+    }
+]
