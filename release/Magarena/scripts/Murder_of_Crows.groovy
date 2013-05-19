fd5d3f065f7fa7552b1531a0834a24d4eb77c5ca
melvin <melvinzhang@gmail.com>
2013-05-19 10:51:58 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Murder_of_Crows.groovy
+++ release/Magarena/scripts/Murder_of_Crows.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicDiscardEvent;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Murder_of_Crows {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (permanent != otherPermanent && otherPermanent.isCreature()) ?
@@ -23,18 +12,17 @@ public class Murder_of_Crows {
                         MagicSimpleMayChoice.DEFAULT_NONE
                     ),
                     this,
-                    "You may$ draw a card. If you do, discard a card."):
+                    "You may\$ draw a card. If you do, discard a card."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 final MagicPlayer player=event.getPlayer();
                 game.doAction(new MagicDrawAction(player,1));
                 game.addEvent(new MagicDiscardEvent(event.getSource(),player,1,false));
             }
         }
-    };
-}
+    }
+]
