e133e0e04db10fe1fd7349f76e5be13a73733490
melvin <melvinzhang@gmail.com>
2013-05-19 09:20:08 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Splinterfright.groovy
+++ release/Magarena/scripts/Splinterfright.groovy
@@ -1,25 +1,12 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicMillLibraryAction;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicCDA;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Splinterfright {
-    public static final MagicCDA CDA = new MagicCDA() {
+[
+    new MagicCDA() {
         @Override
         public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
             final int size = game.filterCards(player,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD).size();
             pt.set(size,size);
         }
-    };
-    
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+    },
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -32,10 +19,8 @@ public class Splinterfright {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicMillLibraryAction(event.getPlayer(),2));
         }
-    };
-}
+    }
+]
