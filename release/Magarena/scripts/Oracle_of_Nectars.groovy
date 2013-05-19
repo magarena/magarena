9bfadae74c2713b553af8fb244818bd3cc347b85
melvin <melvinzhang@gmail.com>
2013-05-19 11:10:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Oracle_of_Nectars.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{X}")
+        ],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Life+X"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source,source.getController(),MagicManaCost.create("{X}"))];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+            return new MagicEvent(
+                source,
+                amount,
+                this,
+                "PN gains RN life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),event.getRefInt()));
+        }
+    }
+]
