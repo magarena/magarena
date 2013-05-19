e19c3bf96e07cc966cf87af2506821155f7eb3eb
melvin <melvinzhang@gmail.com>
2013-05-19 09:28:07 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Colossal_Might.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +4/+2 and gains trample until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,4,2));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                }
+            });
+        }
+    }
+]
