bf9555dc1e9db5b090f31a5d5a9ef4d65182ea4d
melvin <melvinzhang@gmail.com>
2013-05-19 16:03:15 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Timely_Reinforcements.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "If you have less life than an opponent, you gain 6 life. " +
+                "If you control fewer creatures than an opponent, " +
+                "put three 1/1 white Soldier creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            if (player.getLife() < player.getOpponent().getLife()) {
+                game.doAction(new MagicChangeLifeAction(player,6));
+            }    
+            if (player.getNrOfPermanentsWithType(MagicType.Creature) < 
+                player.getOpponent().getNrOfPermanentsWithType(MagicType.Creature)) {
+                game.doAction(new MagicPlayTokensAction(player,TokenCardDefinitions.get("Soldier"),3));
+            }
+        }
+    }
+]
