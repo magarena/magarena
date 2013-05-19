0f51bf86e6f1e5b2c6e2f96f8ad0a59d67f2ec92
melvin <melvinzhang@gmail.com>
2013-05-19 15:46:14 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Chandra_s_Outrage.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(4),
+                this,
+                "SN deals 4 damage to target creature\$ and " + 
+                         "2 damage to that creature's controller."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    MagicDamage damage = new MagicDamage(event.getSource(),target,4);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    damage = new MagicDamage(event.getSource(),target.getController(),2);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
