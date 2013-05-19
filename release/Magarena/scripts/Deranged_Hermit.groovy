8f739c9b7cf18f75e98633cd5ce44113e4e0cf16
melvin <melvinzhang@gmail.com>
2013-05-19 10:51:13 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Deranged_Hermit.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_SQUIRREL_CREATURE
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+    },
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts four 1/1 green Squirrel creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Squirrel1"),
+                4
+            ));
+        }        
+    }
+]
