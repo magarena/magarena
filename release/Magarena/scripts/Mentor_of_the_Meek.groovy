59e2f7b57002bcc046469950cecb1b28797394fd
melvin <melvinzhang@gmail.com>
2013-05-19 10:57:17 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Mentor_of_the_Meek.groovy
+++ release/Magarena/scripts/Mentor_of_the_Meek.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Mentor_of_the_Meek {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             final MagicPlayer player = permanent.getController();
@@ -26,18 +14,16 @@ public class Mentor_of_the_Meek {
                         new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
                     ),
                     this,
-                    "You may$ pay {1}$. If you do, draw a card."
+                    "You may\$ pay {1}\$. If you do, draw a card."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicDrawAction(event.getPlayer(),1));
             }
         }        
-    };
-}
+    }
+]
