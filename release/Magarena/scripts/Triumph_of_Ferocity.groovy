2f8a1e9d6efd9f10e22b425cba957bf95704a4ff
melvin <melvinzhang@gmail.com>
2013-05-19 16:23:58 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Triumph_of_Ferocity.groovy
+++ release/Magarena/scripts/Triumph_of_Ferocity.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDrawAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-import java.util.Collection;
-
-public class Triumph_of_Ferocity {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -23,26 +11,26 @@ public class Triumph_of_Ferocity {
                     this,
                     "PN draws a card if he or she " +
                     "controls the creature with the greatest " +
-                    "power or tied for the greatest power.") :
+                    "power or tied for the greatest power."
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final Collection<MagicPermanent> targets = game.filterPermanents(
-                    player,
-                    MagicTargetFilter.TARGET_CREATURE);
+                player,
+                MagicTargetFilter.TARGET_CREATURE
+            );
             MagicPermanent highest = MagicPermanent.NONE;   
             for (final MagicPermanent creature : targets) {
                 if (creature.getPower() > highest.getPower()) {
                     highest = creature;
                 }
             }
-            if (highest.getController() == player) {
+            if (highest.isController(player)) {
                 game.doAction(new MagicDrawAction(player,1));
             }
         }       
-    };
-}
+    }
+]
