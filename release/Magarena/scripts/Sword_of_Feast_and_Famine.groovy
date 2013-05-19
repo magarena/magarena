e48c9888121c3803eb5ba9dc5d501d82796e9384
melvin <melvinzhang@gmail.com>
2013-05-19 16:26:09 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Sword_of_Feast_and_Famine.groovy
+++ release/Magarena/scripts/Sword_of_Feast_and_Famine.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicUntapAction;
-import magic.model.event.MagicDiscardEvent;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-import java.util.Collection;
-
-public class Sword_of_Feast_and_Famine {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicPlayer player=permanent.getController();
@@ -25,14 +11,13 @@ public class Sword_of_Feast_and_Famine {
                     player,
                     damage.getTarget(),
                     this,
-                    damage.getTarget() + " discards a card and you untap all lands you control."):
+                    "RN discards a card and you untap all lands you control."
+                ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.addEvent(new MagicDiscardEvent(event.getPermanent(),event.getRefPlayer(),1,false));
             final Collection<MagicPermanent> targets = 
                 game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_LAND_YOU_CONTROL);
@@ -40,5 +25,5 @@ public class Sword_of_Feast_and_Famine {
                 game.doAction(new MagicUntapAction(target));
             }
         }
-    };
-}
+    }
+]
