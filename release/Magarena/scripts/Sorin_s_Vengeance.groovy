939972428945b9a849b66f3b269415fef0e8820b
melvin <melvinzhang@gmail.com>
2013-05-19 10:38:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sorin_s_Vengeance.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                new MagicDamageTargetPicker(10),
+                this,
+                "SN deals 10 damage to target player\$ and PN gains 10 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,10);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),10));
+                }
+            });
+        }
+    }
+]
