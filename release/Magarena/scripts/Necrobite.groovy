2e7dadd1950adb0dd853c0bba3ce9fb61e028617
melvin <melvinzhang@gmail.com>
2013-05-19 13:57:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Necrobite.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicDeathtouchTargetPicker.getInstance(),
+                this,
+                "Target creature\$ gains deathtouch until end of turn. " +
+                "Regenerate it."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(
+                        creature,
+                        MagicAbility.Deathtouch
+                    ));
+                    game.doAction(new MagicRegenerateAction(creature));
+                }
+            });
+        }
+    }
+]
