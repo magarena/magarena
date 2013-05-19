c4b166861c69957ec1d31cb2f45b290752706c02
melvin <melvinzhang@gmail.com>
2013-05-19 16:15:21 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Thatcher_Revolt.groovy
+++ release/Magarena/scripts/Thatcher_Revolt.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicCardDefinition;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanentState;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.action.MagicPutIntoPlayAction;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-
-public class Thatcher_Revolt {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(
                 final MagicCardOnStack cardOnStack,
@@ -28,9 +13,7 @@ public class Thatcher_Revolt {
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final MagicCardDefinition token = TokenCardDefinitions.get("Human2");
             for (int x=3;x>0;x--) {
@@ -42,5 +25,5 @@ public class Thatcher_Revolt {
                 ));
             }
         }
-    };
-}
+    }
+]
