3d5adec162bb90f94a14d1b7922dc55a94cd00d0
melvin <melvinzhang@gmail.com>
2013-05-19 10:26:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Calcite_Snapper.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(),
+                this,
+                "PN may\$ switch SN's power and toughness until end of turn."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicAddStaticAction(event.getPermanent(), new MagicStatic(
+                    MagicLayer.SwitchPT,
+                    MagicStatic.UntilEOT) {
+                    @Override
+                    public void modPowerToughness(
+                            final MagicPermanent source,
+                            final MagicPermanent permanent,
+                            final MagicPowerToughness pt) {
+                        pt.set(pt.toughness(),pt.power());
+                    }   
+                }));
+            }
+        }        
+    }
+]
