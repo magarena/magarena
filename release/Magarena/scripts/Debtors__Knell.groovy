afd9dfe46c2ecd12d5ae1a4ff45fa5b0fefb3312
melvin <melvinzhang@gmail.com>
2013-05-19 09:43:06 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Debtors__Knell.groovy
+++ release/Magarena/scripts/Debtors__Knell.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicPlayCardAction;
-import magic.model.action.MagicReanimateAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicGraveyardTargetPicker;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Debtors__Knell {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -23,7 +8,7 @@ public class Debtors__Knell {
                     MagicTargetChoice.TARGET_CREATURE_CARD_FROM_ALL_GRAVEYARDS,
                     new MagicGraveyardTargetPicker(true),
                     this,
-                    "Put target creature card$ in a graveyard onto the battlefield under your control."
+                    "Put target creature card\$ in a graveyard onto the battlefield under your control."
                 ):
                 MagicEvent.NONE;
         }
@@ -36,5 +21,5 @@ public class Debtors__Knell {
                 }
             });
         }
-    };
-}
+    }
+]
