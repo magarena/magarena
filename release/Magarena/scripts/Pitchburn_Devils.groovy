8fe2886b5a0f8a171db9d09582b92d680c2c6877
melvin <melvinzhang@gmail.com>
2013-05-19 16:02:02 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Pitchburn_Devils.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenPutIntoGraveyardTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+            return (triggerData.fromLocation == MagicLocationType.Play) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(3),
+                    this,
+                    "SN deals 3 damage to target creature or player\$."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(event.getPermanent(),target,3);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
