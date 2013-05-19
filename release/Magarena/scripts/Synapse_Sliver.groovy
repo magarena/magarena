f5ee7fb3bf6117d8e8458a126004e997dc2587f0
melvin <melvinzhang@gmail.com>
2013-05-19 15:46:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Synapse_Sliver.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicSource source=damage.getSource();
+            final MagicPlayer player=source.getController();
+            return (source.hasSubType(MagicSubType.Sliver)  &&
+                    damage.isCombat() &&
+                    damage.getTarget().isPlayer()) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                    this,
+                    "PN may$ draw a card."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            }
+        }
+    }
+]
