8ddc32664425cae4e31d17b5245ec4850653a3ef
melvin <melvinzhang@gmail.com>
2013-05-19 13:32:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Titanic_Ultimatum.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(
+                final MagicCardOnStack cardOnStack,
+                final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Until end of turn, creatures you control get +5/+5 and gain first strike, lifelink and trample."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=game.filterPermanents(
+                    event.getPlayer(),
+                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature,5,5));
+                game.doAction(new MagicSetAbilityAction(
+                    creature,
+                    [
+                        MagicAbility.FirstStrike,
+                        MagicAbility.LifeLink,
+                        MagicAbility.Trample
+                    ]
+                ));
+            }
+        }
+    }
+]
