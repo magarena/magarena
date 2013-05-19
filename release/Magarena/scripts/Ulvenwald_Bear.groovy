3a09c5131f22210ae7dedb881269185848bec625
melvin <melvinzhang@gmail.com>
2013-05-19 11:13:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ulvenwald_Bear.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return (game.getCreatureDiedThisTurn()) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicPumpTargetPicker.create(),
+                    this,
+                    "PN puts two +1/+1 counters on target creature\$."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        2,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
