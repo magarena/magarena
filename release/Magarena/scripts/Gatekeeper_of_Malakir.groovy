dc92f523fcd6ca07dab14bf62bded3efb26e5783
melvin <melvinzhang@gmail.com>
2013-05-19 09:21:07 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Gatekeeper_of_Malakir.groovy
+++ release/Magarena/scripts/Gatekeeper_of_Malakir.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayerAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSacrificePermanentEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Gatekeeper_of_Malakir {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
             final MagicGame game,
@@ -22,23 +11,22 @@ public class Gatekeeper_of_Malakir {
                     player,
                     MagicTargetChoice.NEG_TARGET_PLAYER,
                     this,
-                    "Target player$ sacrifices a creature."
+                    "Target player\$ sacrifices a creature."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
                     game.addEvent(new MagicSacrificePermanentEvent(
                         event.getSource(),
                         player,
-                        MagicTargetChoice.SACRIFICE_CREATURE));
+                        MagicTargetChoice.SACRIFICE_CREATURE
+                    ));
                 }
             });
         }
-    };
-}
+    }
+]
