61b4858761d3fc3f8f34562c15da610535f14147
melvin <melvinzhang@gmail.com>
2013-05-19 09:34:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Emancipation_Angel.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_PERMANENT_YOU_CONTROL,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return a permanent\$ you control to its owner's hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(
+                        permanent,
+                        MagicLocationType.OwnersHand
+                    ));
+                }
+            });
+        }
+    }
+]
