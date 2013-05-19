b523a78d8e27e39f7b46c075a432bbbe38f6f6b6
melvin <melvinzhang@gmail.com>
2013-05-19 10:24:10 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dramatic_Entrance.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_GREEN_CREATURE_CARD_FROM_HAND,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Put a green creature card\$ from your hand onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicPlayCardAction(card,event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
