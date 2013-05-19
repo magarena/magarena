6e13d984bf20543de7ee9dc26efe81759dc25951
melvin <melvinzhang@gmail.com>
2013-05-19 10:35:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Zombie_Infestation.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.HAS_TWO_CARDS_CONDITION],
+        new MagicActivationHints(MagicTiming.Token,true),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicDiscardEvent(source,source.getController(),2,false)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 2/2 black Zombie creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Zombie")
+            ));
+        }
+    }
+]
