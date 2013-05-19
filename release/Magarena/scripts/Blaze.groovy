f36ac541f68a403f65b4bb5e4fbe0cb55a88cae3
melvin <melvinzhang@gmail.com>
2013-05-19 09:26:09 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Blaze.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(amount),
+                this,
+                "SN deals "+amount+" damage to target creature or player\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getCardOnStack().getX());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
