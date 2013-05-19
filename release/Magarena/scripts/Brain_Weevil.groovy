d8d2fd4bacbdee233a43498780d10cbd1044e2fa
melvin <melvinzhang@gmail.com>
2013-05-19 16:18:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Brain_Weevil.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.SORCERY_CONDITION],
+        new MagicActivationHints(MagicTiming.Main),
+        "Discard"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ discards two cards."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(event.getPermanent(),player,2,false));
+                }
+            });
+        }
+    }
+]
