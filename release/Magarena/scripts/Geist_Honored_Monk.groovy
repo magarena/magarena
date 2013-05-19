388b031a7eb776b78342e3dd1e0c59f28e36cf18
melvin <melvinzhang@gmail.com>
2013-05-19 09:34:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Geist_Honored_Monk.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicCDA() {
+        @Override
+        public void modPowerToughness(final MagicGame game, final MagicPlayer player, final MagicPowerToughness pt) {
+            final int amount = player.getNrOfPermanentsWithType(MagicType.Creature);
+            pt.set(amount, amount);
+        }
+    },
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts two 1/1 white Spirit creature tokens with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Spirit2"),
+                2
+            ));
+        }        
+    }
+]
