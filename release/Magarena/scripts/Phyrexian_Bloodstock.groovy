85e558a01eb22354c05d9457fcb9fa7057e68588
melvin <melvinzhang@gmail.com>
2013-05-19 10:54:13 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Phyrexian_Bloodstock.groovy
+++ release/Magarena/scripts/Phyrexian_Bloodstock.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenLeavesPlayTrigger;
-
-public class Phyrexian_Bloodstock {
-    public static final MagicWhenLeavesPlayTrigger T = new MagicWhenLeavesPlayTrigger() {
+[
+    new MagicWhenLeavesPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent left) {
             return (permanent == left) ?
@@ -21,14 +8,12 @@ public class Phyrexian_Bloodstock {
                     MagicTargetChoice.TARGET_WHITE_CREATURE,
                     new MagicDestroyTargetPicker(true),
                     this,
-                    "Destroy target white creature$. It can't be regenerated."
+                    "Destroy target white creature\$. It can't be regenerated."
                 ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.CannotBeRegenerated,true));
@@ -36,5 +21,5 @@ public class Phyrexian_Bloodstock {
                 }
             });
         }
-    };
-}
+    }
+]
