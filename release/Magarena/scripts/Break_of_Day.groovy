0cca4880b2d6cfb59c640e544f4a4fdba1e8ab71
melvin <melvinzhang@gmail.com>
2013-05-19 13:33:40 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Break_of_Day.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Creatures PN controls get +1/+1 until end of turn. " +
+                "If PN has 5 or less life, those creatures also are indestructible this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                player,
+                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+            );
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature,1,1));
+                if (MagicCondition.FATEFUL_HOUR.accept(event.getSource())) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Indestructible));
+                }
+            }
+        }
+    }
+]
