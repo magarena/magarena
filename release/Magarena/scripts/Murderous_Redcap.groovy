59801cbf4f84d1296ecc4fa6bc736d320f72c67a
melvin <melvinzhang@gmail.com>
2013-05-19 10:50:43 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Murderous_Redcap.groovy
+++ release/Magarena/scripts/Murderous_Redcap.groovy
@@ -1,29 +1,14 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-
-public class Murderous_Redcap {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
-            final int power=permanent.getPower();
             return new MagicEvent(
                 permanent,
                 MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                new MagicDamageTargetPicker(power),
+                new MagicDamageTargetPicker(permanent.getPower()),
                 this,
-                "SN deals damage equal to its power to target creature or player$.");
+                "SN deals damage equal to its power to target creature or player\$."
+            );
         }
         
         @Override
@@ -36,5 +21,5 @@ public class Murderous_Redcap {
                 }
             });
         }
-    };
-}
+    }
+]
