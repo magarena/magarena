ede0bb35633a1a3fc6a1cef03abc901dd2bcb014
melvin <melvinzhang@gmail.com>
2013-05-20 13:29:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Rootwalla.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ABILITY_ONCE_CONDITION,
+            MagicConditionFactory.ManaCost("{1}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{1}{G}")),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +2/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),2,2));
+        }
+    }
+]
