72f6eaaa693917dab8bfc08ef31b11f2d8c15319
melvin <melvinzhang@gmail.com>
2013-05-20 13:30:21 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Act_of_Treason.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "Gain control of target creature\$ until end of turn. Untap that creature. " +
+                "It gains haste until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainControlAction(event.getPlayer(),creature,MagicStatic.UntilEOT));
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
+                }
+            });
+        }
+    }
+]
