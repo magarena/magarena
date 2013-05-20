ef716436ae0dadb504e9031dca5e65339c7f151e
melvin <melvinzhang@gmail.com>
2013-05-20 13:32:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sokenzan_Spellblade.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}{R}")],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,source.getController(),MagicManaCost.create("{1}{R}"))];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +X/+0 until end of turn, " + 
+                "where X is the number of cards in your hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicChangeTurnPTAction(
+                event.getPermanent(),
+                player.getHandSize(),
+                0
+            ));
+        }
+    }
+]
