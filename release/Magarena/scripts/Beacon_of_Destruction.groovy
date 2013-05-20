a3272f0e45c1bbf5e29d5e397bcd47cd727b2c5c
melvin <melvinzhang@gmail.com>
2013-05-20 13:23:44 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Beacon_of_Destruction.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(5),
+                this,
+                "SN deals 5 damage to target creature or player\$. " + 
+                "Shuffle SN into its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,5);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersLibrary));
+                }
+            });
+        }
+    }
+]
