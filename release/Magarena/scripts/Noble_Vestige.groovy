5cbb3bcdc2fb56059c3f8fd61b613f12e303c241
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-22 17:24:34 +0800

    added Noble Vestige

---
+++ release/Magarena/scripts/Noble_Vestige.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Prevent 1"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_PLAYER,
+                MagicPreventTargetPicker.getInstance(),
+                this,
+                "Prevent the next 1 damage that would be dealt to target player\$ this turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    game.doAction(new MagicPreventDamageAction(target,1));
+                }
+            });
+        }
+    }
+]
