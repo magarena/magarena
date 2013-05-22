8df3c8fee4387dec64ac67c08bb28b98562f28e2
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-22 17:25:07 +0800

    added Reinforced Bulwark

---
+++ release/Magarena/scripts/Reinforced_Bulwark.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Prevent 1"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Prevent the next 1 damage that would be dealt to PN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPreventDamageAction(event.getPlayer(),1));
+        }
+    }
+]
