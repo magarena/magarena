89c78b907f8386bcff99fc5ea4e60f4aaea643e1
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:27:42 +0800

    added Arctic Aven

---
+++ release/Magarena/scripts/Arctic_Aven.groovy
@@ -0,0 +1,10 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().getNrOfPermanentsWithSubType(MagicSubType.Plains) > 0) {
+                pt.add(1,1);
+            }
+        }
+    }
+]
