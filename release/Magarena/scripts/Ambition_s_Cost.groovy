11ea4999494f17a1e1b05a501b16305c507e27fa
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 22:23:01 +0800

    added Ambition's Cost

---
+++ release/Magarena/scripts/Ambition_s_Cost.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN draws three cards and loses 3 life."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicDrawAction(player,3));
+            game.doAction(new MagicChangeLifeAction(player,-3));
+        }
+    }
+]
