75a8e0ec671cf1b0154b76a59aa391eb1f054894
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:59:11 +0800

    added Advent of the Wurm

---
+++ release/Magarena/scripts/Advent_of_the_Wurm.groovy
@@ -0,0 +1,17 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts a 5/5 green Wurm creature token with trample onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Wurm5")));
+        }
+    }
+]
