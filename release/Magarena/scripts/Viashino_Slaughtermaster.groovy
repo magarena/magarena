9212b1736546756122d9728dc0cb9a3c35811b78
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:17:57 +0800

    added Viashino Slaughtermaster

---
+++ release/Magarena/scripts/Viashino_Slaughtermaster.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ABILITY_ONCE_CONDITION,
+            MagicConditionFactory.ManaCost("{B}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}{G}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,1));
+        }
+    }
+]
