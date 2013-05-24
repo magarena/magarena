c326b2fcb60b2d2804945444384d7bf90719060c
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 22:32:41 +0800

    added Haunter of Nightveil

---
+++ release/Magarena/scripts/Haunter_of_Nightveil.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(-1,0);
+        }
+    }
+]
