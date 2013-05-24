c5bf06b7cf40575cae48f10a1138ba7e6d3259bd
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 17:38:01 +0800

    added Heedless One

---
+++ release/Magarena/scripts/Heedless_One.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicCDA() {
+        @Override
+        public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+            final int amount = 
+                player.getNrOfPermanentsWithSubType(MagicSubType.Elf) +
+                player.getOpponent().getNrOfPermanentsWithSubType(MagicSubType.Elf); 
+            pt.set(amount,amount);
+        }
+    }
+]
