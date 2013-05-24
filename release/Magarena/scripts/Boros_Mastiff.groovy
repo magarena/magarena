2f9ad89bc3fa3bbd85a76336c07e47784cac1531
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 17:50:01 +0800

    added Boros Mastiff

---
+++ release/Magarena/scripts/Boros_Mastiff.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicBattalionTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent attacker) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "SN gains lifelink until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicSetAbilityAction(event.getPermanent(),MagicAbility.Lifelink));
+        }
+    }
+]
