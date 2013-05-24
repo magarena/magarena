521218980e013c40bfa0f61452d969f71c93b590
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:25:51 +0800

    added Knight Watch

---
+++ release/Magarena/scripts/Knight_Watch.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts two 2/2 white Knight creature tokens with vigilance onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Knight2"),
+                2
+            ));
+        }
+    }
+]
