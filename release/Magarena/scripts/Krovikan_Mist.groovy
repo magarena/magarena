169e189b6a4c9a8ce63a73600638881dc7a9f412
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:34:07 +0800

    added Krovikan Mist

---
+++ release/Magarena/scripts/Krovikan_Mist.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicCDA() {
+        @Override
+        public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+            final int amount = 
+                player.getNrOfPermanentsWithSubType(MagicSubType.Illusion) +
+                player.getOpponent().getNrOfPermanentsWithSubType(MagicSubType.Illusion); 
+            pt.set(amount,amount);
+        }
+    }
+]
