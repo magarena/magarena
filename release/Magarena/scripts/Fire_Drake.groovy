8ce699773a3d5c304a5227e89e44867407662572
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:42:45 +0800

    added Fire Drake

---
+++ release/Magarena/scripts/Fire_Drake.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ABILITY_ONCE_CONDITION,
+            MagicConditionFactory.ManaCost("{R}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{R}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+0 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,0));
+        }
+    }
+]
