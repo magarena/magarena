0ea86c2387575cc69cbf36c1bdf6eb63c245f565
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:38:59 +0800

    added Phyrexian Gargantua

---
+++ release/Magarena/scripts/Phyrexian_Gargantua.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN draws two cards and loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            game.doAction(new MagicDrawAction(player,2));
+            game.doAction(new MagicChangeLifeAction(player,-2));
+        }
+    }
+]
