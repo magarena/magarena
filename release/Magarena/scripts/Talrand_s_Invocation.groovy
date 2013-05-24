dc977fec1c47d1f382c3d1cf116ec4c899601aa1
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 17:47:54 +0800

    added Talrand's Invocation

---
+++ release/Magarena/scripts/Talrand_s_Invocation.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts two 2/2 blue Drake creature tokens with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Drake"),
+                2
+            ));
+        }
+    }
+]
