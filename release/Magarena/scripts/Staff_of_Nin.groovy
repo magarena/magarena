9c62589ee53e16e8f2f7172ffc87eff870b287c3
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 11:54:01 +0800

    added Staff of Nin

---
+++ release/Magarena/scripts/Staff_of_Nin.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN draws a card."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+        }
+    }
+]
