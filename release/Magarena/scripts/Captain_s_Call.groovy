a464b6fb897b42dde33f25e07da0f4e268c22db0
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 22:27:10 +0800

    added Captain's Call

---
+++ release/Magarena/scripts/Captain_s_Call.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts three 1/1 white Soldier creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Soldier"),
+                3
+            ));
+        }
+    }
+]
