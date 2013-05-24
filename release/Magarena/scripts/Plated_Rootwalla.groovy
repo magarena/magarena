21526fd380ceff57bc3aea55232a5fd2365fb397
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:21:04 +0800

    added Plated Rootwalla

---
+++ release/Magarena/scripts/Plated_Rootwalla.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ABILITY_ONCE_CONDITION,
+            MagicConditionFactory.ManaCost("{2}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{G}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +3/+3 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),3,3));
+        }
+    }
+]
