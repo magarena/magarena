7ddf5b705423a33b978640067868a3113fe1f3f9
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 17:52:20 +0800

    added Armageddon

---
+++ release/Magarena/scripts/Armageddon.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all lands."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_LAND);
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
