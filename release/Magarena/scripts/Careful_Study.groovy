46d8c31eb72cd5023fd10da024d5133b6a3cede3
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 22:27:59 +0800

    added Careful Study

---
+++ release/Magarena/scripts/Careful_Study.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Draw two cards, then discard two cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            game.doAction(new MagicDrawAction(player,2));
+            game.addEvent(new MagicDiscardEvent(event.getSource(),player,2,false));
+        }
+    }
+]
