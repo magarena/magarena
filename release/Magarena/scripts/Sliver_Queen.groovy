b463431c73267c258d9731d1a04e5cfdbc908e5a
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 11:40:15 +0800

    added Sliver Queen

---
+++ release/Magarena/scripts/Sliver_Queen.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{2}"),
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{2}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 1/1 colorless Sliver creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Sliver")));
+        }
+    }
+]
