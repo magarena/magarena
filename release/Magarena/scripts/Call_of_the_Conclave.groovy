4048a84f58b3e11738f330bf9aec299884e7e4fe
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:46:09 +0800

    added Call of the Conclave

---
+++ release/Magarena/scripts/Call_of_the_Conclave.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts a 3/3 green Centaur creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Centaur3")));
+        }
+    }
+]
