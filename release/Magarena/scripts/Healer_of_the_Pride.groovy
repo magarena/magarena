878d920cf6154064c04fb516152504bf4119195a
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:41:00 +0800

    added Healer of the Pride

---
+++ release/Magarena/scripts/Healer_of_the_Pride.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent != permanent && 
+                    otherPermanent.isCreature() &&
+                    otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN gains 2 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));            
+        }        
+    }
+]
