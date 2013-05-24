3ef88b9cc1d708ea9c775535f6b291e10899e6bd
a. benedict balbuena <abcbalbuena@gmail.com>
2013-05-24 18:48:19 +0800

    added Brilliant Plan

---
+++ release/Magarena/scripts/Brilliant_Plan.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN draws three cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),3));
+        }
+    }
+]
