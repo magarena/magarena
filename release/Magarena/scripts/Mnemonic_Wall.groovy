6fa2623ac179ca58f65ceb84a2f9444a7ef4a895
melvin <melvinzhang@gmail.com>
2013-05-26 11:20:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mnemonic_Wall.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(
+                    MagicTargetChoice.TARGET_INSTANT_OR_SORCERY_CARD_FROM_GRAVEYARD
+                ),
+                new MagicGraveyardTargetPicker(false),
+                this,
+                "PN may\$ return target instant or sorcery card\$ " +
+                "from his or her graveyard to his or her hand."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetCard(game,new MagicCardAction() {
+                    public void doAction(final MagicCard card) {
+                        game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                        game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+                    }
+                });
+            }
+        }
+    }
+]
