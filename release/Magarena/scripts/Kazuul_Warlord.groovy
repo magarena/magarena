26edd016b2e0572108f6d5f5bba25507ffa65e09
melvin <melvinzhang@gmail.com>
2013-05-26 21:29:57 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kazuul_Warlord.groovy
+++ release/Magarena/scripts/Kazuul_Warlord.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-import java.util.Collection;
-
-public class Kazuul_Warlord {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isFriend(permanent) &&
@@ -23,18 +9,17 @@ public class Kazuul_Warlord {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a +1/+1 counter on " +
+                    "PN may\$ put a +1/+1 counter on " +
                     "each Ally creature he or she controls."
                 ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 final Collection<MagicPermanent> targets =
                         game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
@@ -48,5 +33,5 @@ public class Kazuul_Warlord {
                 }
             }            
         }        
-    };
-}
+    }
+]
