a0b5d0c40fb85ec7bee25b72070e6799b198a9fe
melvin <melvinzhang@gmail.com>
2013-05-26 21:20:51 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Anoint.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicManaCost.create("{3}")
+                ),
+                MagicPreventTargetPicker.getInstance(),
+                this,
+                "Prevent the next 3 damage that would be dealt to target creature\$ this turn. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicPreventDamageAction(permanent,3));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
