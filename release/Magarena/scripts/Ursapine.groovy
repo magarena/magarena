dc0cf85e4193de5c27e28ad149192a6ebf7ed0c2
melvin <melvinzhang@gmail.com>
2013-05-26 19:01:26 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ursapine.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{G}")],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{G}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1/+1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,1));
+                }
+            });
+        }
+    }
+]
