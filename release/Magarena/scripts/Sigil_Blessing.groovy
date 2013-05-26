e27c0417202eaa3076ae2808b390612181dd78d2
melvin <melvinzhang@gmail.com>
2013-05-26 21:27:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sigil_Blessing.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Until end of turn, target creature\$ PN controls gets +3/+3 and other creatures PN controls get +1/+1."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final Collection<MagicPermanent> targets=game.filterPermanents(
+                            event.getPlayer(),
+                            MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                    for (final MagicPermanent permanent : targets) {
+                        if (permanent==creature) {
+                            game.doAction(new MagicChangeTurnPTAction(permanent,3,3));
+                        } else {
+                            game.doAction(new MagicChangeTurnPTAction(permanent,1,1));
+                        }
+                    }
+                }
+            });
+        }
+    }
+]
