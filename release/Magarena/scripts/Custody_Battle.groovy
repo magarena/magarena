bc894232367e2a98b72e56c90655ab379f982f68
melvin <melvinzhang@gmail.com>
2013-05-26 18:20:56 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Custody_Battle.groovy
+++ release/Magarena/scripts/Custody_Battle.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicGainControlAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSacrificeAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-public class Custody_Battle {
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+[
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             final MagicPermanent enchanted = permanent.getEnchantedCreature();
@@ -23,14 +10,13 @@ public class Custody_Battle {
                         MagicTargetChoice.SACRIFICE_LAND
                     ),
                     this,
-                    "PN may$ sacrifice a land$. If you don't, " +
-                    upkeepPlayer.getOpponent() + " gains control of SN.") :
+                    "PN may\$ sacrifice a land$. If you don't, " +
+                    upkeepPlayer.getOpponent() + " gains control of SN."
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent perm) {
@@ -44,5 +30,5 @@ public class Custody_Battle {
                 ));
             }
         }
-    };
-}
+    }
+]
