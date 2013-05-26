f220068712739c5f60d334b6f72cc84e4bb48d54
melvin <melvinzhang@gmail.com>
2013-05-26 18:36:45 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Surgespanner.groovy
+++ release/Magarena/scripts/Surgespanner.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicRemoveFromPlayAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicBounceTargetPicker;
-import magic.model.trigger.MagicWhenBecomesTappedTrigger;
-
-public class Surgespanner {
-    public static final MagicWhenBecomesTappedTrigger T = new MagicWhenBecomesTappedTrigger() {
+[
+    new MagicWhenBecomesTappedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
             return (permanent == tapped) ?
@@ -26,15 +11,13 @@ public class Surgespanner {
                     ),
                     MagicBounceTargetPicker.getInstance(),
                     this,
-                    "PN may$ pay {1}{U}$. If you do, " +
-                    "return target permanent$ to its owner's hand."
+                    "PN may\$ pay {1}{U}\$. If you do, " +
+                    "return target permanent\$ to its owner's hand."
                 ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent permanent) {
@@ -43,5 +26,5 @@ public class Surgespanner {
                 });
             }
         }
-    };
-}
+    }
+]
