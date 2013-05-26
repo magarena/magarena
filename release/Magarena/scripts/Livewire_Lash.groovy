3c518b77b01f51271e2f1bbfb0d0bb9b61ad0f8f
melvin <melvinzhang@gmail.com>
2013-05-26 18:54:12 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Livewire_Lash.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenTargetedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicItemOnStack target) {
+            final MagicPermanent equippedCreature = permanent.getEquippedCreature();
+            return (target.containsInChoiceResults(equippedCreature) &&
+                    target.isSpell()) ?
+                new MagicEvent(
+                    equippedCreature,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(2),
+                    this,
+                    "SN deals 2 damage to target creature or player\$."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,2);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
