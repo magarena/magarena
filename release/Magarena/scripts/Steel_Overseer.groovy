2f603a3b9b39f021eebddcaca5d592b4d1b679d8
melvin <melvinzhang@gmail.com>
2013-05-26 21:29:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Steel_Overseer.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicCondition.CAN_TAP_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a +1/+1 counter on each artifact creature you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(player,MagicTargetFilter.TARGET_ARTIFACT_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeCountersAction(
+                    target,
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }
+        }
+    }
+]
