5149e87a303d75d0b3fb882eed444c96e046913a
melvin <melvinzhang@gmail.com>
2013-05-26 17:45:15 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Into_the_Roil.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicKickerChoice(
+                    MagicTargetChoice.TARGET_NONLAND_PERMANENT,
+                    MagicManaCost.create("{1}{U}"),false
+                ),
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target nonland permanent\$ to its owner's hand. If SN was kicked\$, draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
+                    if (event.isKicked()) {
+                        game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                    }
+                }
+            });
+        }
+    }
+]
