aaf58d50f810ab10eb2405c02c959d09de699936
melvin <melvinzhang@gmail.com>
2013-05-26 19:55:45 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mind_Games.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_CREATURE_OR_LAND,
+                    MagicManaCost.create("{2}{U}")),
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target artifact, creature, or land\$. " +
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicTapAction(permanent,true));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
