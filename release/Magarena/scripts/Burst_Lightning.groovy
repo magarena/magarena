c577b60a830b5e3bdf9a86a802113d00762bb7fa
melvin <melvinzhang@gmail.com>
2013-05-26 13:57:49 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Burst_Lightning.groovy
+++ release/Magarena/scripts/Burst_Lightning.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicKickerChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-
-public class Burst_Lightning {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -32,9 +16,7 @@ public class Burst_Lightning {
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
                     final int amount = event.isKicked() ? 4 : 2;
@@ -43,5 +25,5 @@ public class Burst_Lightning {
                 }
             });
         }
-    };
-}
+    }
+]
