5786cc112ac0cfae4dc8aa345090e23c1730da59
melvin <melvinzhang@gmail.com>
2013-05-26 19:58:34 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Kor_Spiritdancer.groovy
+++ release/Magarena/scripts/Kor_Spiritdancer.groovy
@@ -1,20 +1,5 @@
-
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicDrawAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-
-public class Kor_Spiritdancer {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.ModPT) {
+[
+    new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(
                 final MagicPermanent source,
@@ -23,12 +8,12 @@ public class Kor_Spiritdancer {
             if (permanent.isEnchanted()) {
                 pt.add(
                     2 * permanent.getAuraPermanents().size(),
-                    2 * permanent.getAuraPermanents().size());
+                    2 * permanent.getAuraPermanents().size()
+                );
             }
         }
-    };
-    
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+    },
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
             return (permanent.isFriend(spell) &&
@@ -38,9 +23,10 @@ public class Kor_Spiritdancer {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.DRAW_CARDS,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_NONE),
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
                     this,
-                    "PN may$ draw a card."
+                    "PN may\$ draw a card."
                 ) :
                 MagicEvent.NONE;
         }
@@ -51,5 +37,5 @@ public class Kor_Spiritdancer {
                 game.doAction(new MagicDrawAction(event.getPlayer(),1));
             }
         }        
-    };
-}
+    }
+]
