aa9c6e0a34c16c79256e9c221f9b6a254a03d276
melvin <melvinzhang@gmail.com>
2013-05-26 21:28:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Echoing_Calm.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ENCHANTMENT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target enchantment\$ and all other enchantments " +
+                "with the same name as that enchantment."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent targetPermanent) {
+                    final MagicTargetFilter<MagicPermanent> targetFilter = 
+                        new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
+                    final Collection<MagicPermanent> targets = 
+                        game.filterPermanents(event.getPlayer(),targetFilter);
+                    for (final MagicPermanent permanent : targets) {
+                        if (permanent.isEnchantment()) {
+                            game.doAction(new MagicDestroyAction(permanent));
+                        }
+                    }
+                }
+            });
+        }
+    }
+]
