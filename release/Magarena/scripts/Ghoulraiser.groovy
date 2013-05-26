6f0a18a00409db8559514d131db20d2a85a5fa59
melvin <melvinzhang@gmail.com>
2013-05-26 13:44:00 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Ghoulraiser.groovy
+++ release/Magarena/scripts/Ghoulraiser.groovy
@@ -1,32 +1,16 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-import java.util.List;
-
-public class Ghoulraiser {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
-                    permanent,
-                    player,
-                    this,
-                    "Return a Zombie card at random from your graveyard to your hand.");
+                permanent,
+                player,
+                this,
+                "Return a Zombie card at random from your graveyard to your hand."
+            );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final List<MagicCard> targets = game.filterCards(player,MagicTargetFilter.TARGET_ZOMBIE_CARD_FROM_GRAVEYARD);
             if (targets.size() > 0) {
@@ -38,5 +22,5 @@ public class Ghoulraiser {
                 game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
             }
         }
-    };
-}
+    }
+]
