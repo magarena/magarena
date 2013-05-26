2b6f664ca58dfa598bb6ea2c68286d74d30781a2
melvin <melvinzhang@gmail.com>
2013-05-26 19:07:50 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Glen_Elendra_Archmage.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{U}")],
+        new MagicActivationHints(MagicTiming.Counter),
+        "Counter"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostSacrificeEvent(source,"{U}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_NONCREATURE_SPELL,
+                this,
+                "Counter target noncreature spell\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                }
+            });
+        }
+    }
+]
