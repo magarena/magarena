6c0e989e0c925572016a83d9ed37f9dec03ab01b
melvin <melvinzhang@gmail.com>
2013-05-26 19:52:48 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vedalken_Certarch.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.METALCRAFT_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_CREATURE_OR_LAND,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target artifact, creature, or land\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                }
+            });
+        }
+    }
+]
