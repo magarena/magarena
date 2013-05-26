a5c86771acaa722ff9b70dcf2e5760b4bb56612c
melvin <melvinzhang@gmail.com>
2013-05-26 14:00:01 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Faceless_Butcher.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            final MagicTargetFilter<MagicPermanent> targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+                    MagicTargetFilter.TARGET_CREATURE,permanent);
+            final MagicTargetChoice targetChoice = new MagicTargetChoice(
+                    targetFilter,true,MagicTargetHint.None,"another creature to exile");
+            return new MagicEvent(
+                permanent,
+                targetChoice,
+                MagicExileTargetPicker.create(),
+                this,
+                "Exile another creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicExileUntilThisLeavesPlayAction(event.getPermanent(),creature));
+                }
+            });
+        }
+    }
+]
