e73b082663d30c55d68e0832062b48fa51254f02
melvin <melvinzhang@gmail.com>
2013-05-26 13:44:34 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Smallpox.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player loses 1 life, discards a card, " +
+                "sacrifices a creature, then sacrifices a land."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getPlayers()) {
+                game.doAction(new MagicChangeLifeAction(player,-1));
+                game.addEvent(new MagicDiscardEvent(
+                    event.getSource(),
+                    player,
+                    1,
+                    false
+                ));
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    player,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                ));
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    player,
+                    MagicTargetChoice.SACRIFICE_LAND
+                ));
+            }
+        }
+    }
+]
