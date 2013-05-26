a186a8d3353d27a487aee47365c61b347db39069
melvin <melvinzhang@gmail.com>
2013-05-26 21:26:31 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Desert.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.END_OF_COMBAT_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ATTACKING_CREATURE,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target attacking creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,1);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
