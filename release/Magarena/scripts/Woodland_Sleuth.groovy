1a5f138fafed56434a76f02616bb2c8ed1fe7741
melvin <melvinzhang@gmail.com>
2013-05-26 19:08:15 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Woodland_Sleuth.groovy
+++ release/Magarena/scripts/Woodland_Sleuth.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-import java.util.List;
-
-public class Woodland_Sleuth {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return (game.getCreatureDiedThisTurn()) ?
@@ -22,13 +7,12 @@ public class Woodland_Sleuth {
                     permanent,
                     player,
                     this,
-                    "Return a creature card at random from your graveyard to your hand.") :
+                    "Return a creature card at random from your graveyard to your hand."
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final List<MagicCard> targets = game.filterCards(player,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD);
             if (targets.size() > 0) {
@@ -40,5 +24,5 @@ public class Woodland_Sleuth {
                 game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
             }
         }
-    };
-}
+    }
+]
