e45ce9ef38801fc98556d45fe00b4d41610a45fe
melvin <melvinzhang@gmail.com>
2013-05-26 13:59:23 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Reanimate.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_ALL_GRAVEYARDS,
+                new MagicGraveyardTargetPicker(false), // no mana cost but payed with life
+                this,
+                "Put target creature card$ from a graveyard onto the battlefield under your control. " +
+                "PN loses life equal to its converted mana cost."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    final MagicPlayer player=event.getPlayer();
+                    game.doAction(new MagicReanimateAction(player,targetCard,MagicPlayCardAction.NONE));
+                    game.doAction(new MagicChangeLifeAction(player,-targetCard.getCardDefinition().getConvertedCost()));
+                }
+            });
+        }
+    }
+]
