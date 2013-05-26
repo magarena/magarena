5b66fac27cf1c8f7f479f92ac2587913446dc56d
melvin <melvinzhang@gmail.com>
2013-05-26 17:42:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Royal_Assassin.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_TAPPED_CREATURE,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target tapped creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    if (creature.isTapped()) {                    
+                        game.doAction(new MagicDestroyAction(creature));
+                    }
+                }
+            });
+        }
+    }
+]
