5087c37a82756660c1012e02aa6209d0a52971ca
melvin <melvinzhang@gmail.com>
2013-05-26 19:53:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Elvish_Fury.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.POS_TARGET_CREATURE,
+                    MagicManaCost.create("{4}")
+                ),
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+2 until end of turn. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
