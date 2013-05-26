aa6983665fd38411436644762ec2fca1d99e6648
melvin <melvinzhang@gmail.com>
2013-05-26 10:35:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Intrepid_Hero.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_POWER_4_OR_MORE,
+                new MagicDestroyTargetPicker(true),
+                this,
+                "Destroy target creature$ with power 4 or greater."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicDestroyAction(creature));
+                }
+            });
+        }
+    }
+]
