f2efe2f6f6cae827baefe7a186c526732f99f0fa
melvin <melvinzhang@gmail.com>
2013-05-26 20:05:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sleep.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Tap all creatures target player\$ controls. Those creatures don't untap during their controller's next untap step."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final Collection<MagicPermanent> targets=
+                        game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                    for (final MagicPermanent creature : targets) {
+                        game.doAction(new MagicTapAction(creature,true));
+                        game.doAction(new MagicChangeStateAction(
+                            creature,
+                            MagicPermanentState.DoesNotUntapDuringNext,
+                            true
+                        ));
+                    }
+                }
+            });
+        }
+    }
+]
