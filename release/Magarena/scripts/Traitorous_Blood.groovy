547eb523b9dfa32bac08ce7e5aa953b769de1081
melvin <melvinzhang@gmail.com>
2013-05-26 17:47:38 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Traitorous_Blood.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "Gain control of target creature\$ until end of turn. Untap it. " +
+                "It gains trample and haste until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainControlAction(event.getPlayer(),creature,MagicStatic.UntilEOT));
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
+                }
+            });
+        }
+    }
+]
