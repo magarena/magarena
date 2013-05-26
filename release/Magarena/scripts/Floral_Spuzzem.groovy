561c4a211dd124f49052d7c3a6e837813bb3c131
melvin <melvinzhang@gmail.com>
2013-05-26 18:29:29 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Floral_Spuzzem.groovy
+++ release/Magarena/scripts/Floral_Spuzzem.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenAttacksUnblockedTrigger;
-
-public class Floral_Spuzzem {
-    public static final MagicWhenAttacksUnblockedTrigger T = new MagicWhenAttacksUnblockedTrigger() {
+[
+    new MagicWhenAttacksUnblockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (creature == permanent) ?
@@ -24,15 +10,13 @@ public class Floral_Spuzzem {
                     ),
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "PN may$ destroy target artifact$."
+                    "PN may\$ destroy target artifact\$."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent target) {
@@ -40,9 +24,11 @@ public class Floral_Spuzzem {
                     }
                 });
                 game.doAction(new MagicChangeStateAction(
-                        event.getPermanent(),
-                        MagicPermanentState.NoCombatDamage,true));
+                    event.getPermanent(),
+                    MagicPermanentState.NoCombatDamage,
+                    true
+                ));
             }
         }
-    };
-}
+    }
+]
