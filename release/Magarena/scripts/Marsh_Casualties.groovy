e17166c8d99c3412e77291c919efd1f3e4ba74b7
melvin <melvinzhang@gmail.com>
2013-05-26 19:16:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Marsh_Casualties.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicKickerChoice(
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    MagicManaCost.create("{3}"),
+                    false
+                ),
+                this,
+                "Creatures target player\$ controls get -1/-1 until end of turn. " +
+                "If SN was kicked\$, those creatures get -2/-2 until end of turn instead."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final int amount = event.isKicked() ? -2 : -1;
+                    final Collection<MagicPermanent> targets=
+                        game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                    for (final MagicPermanent target : targets) {
+                        game.doAction(new MagicChangeTurnPTAction(target,amount,amount));
+                    }
+                }
+            });
+        }
+    }
+]
