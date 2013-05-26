a776519c77168569f7fe1dc5eaee5d9aaf949ab2
melvin <melvinzhang@gmail.com>
2013-05-26 11:22:57 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Leap_of_Faith.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicFlyingTargetPicker.create(),
+                this,
+                "Target creature\$ gains flying until end of turn. "
+                "Prevent all damage that would be dealt to that creature this turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Flying));
+                    game.doAction(new MagicChangeStateAction(
+                        creature,
+                        MagicPermanentState.PreventAllDamage,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
