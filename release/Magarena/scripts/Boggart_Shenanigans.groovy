51a61e0543eb5b5e9803e834001109fb0190a8f5
melvin <melvinzhang@gmail.com>
2013-05-26 10:39:18 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Boggart_Shenanigans.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+            final MagicGame game,
+            final MagicPermanent permanent, 
+            final MagicPermanent died) {
+            return (died != permanent &&
+                    died.hasSubType(MagicSubType.Goblin) &&
+                    died.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(MagicTargetChoice.NEG_TARGET_PLAYER),
+                    new MagicDamageTargetPicker(1),
+                    this,
+                    "PN may$ have SN deal 1 damage to target player$."
+                ) : 
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTarget(game, new MagicTargetAction() {
+                    public void doAction(final MagicTarget target) {
+                        final MagicDamage damage = new MagicDamage(event.getSource(), target, 1);
+                        game.doAction( new MagicDealDamageAction(damage) );
+                    }
+                });
+            }
+        }
+    }
+]
