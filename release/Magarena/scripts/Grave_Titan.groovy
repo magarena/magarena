2b5f519d25e63e1cd93a87dac14b079c7c5b01fb
melvin <melvinzhang@gmail.com>
2013-05-26 20:08:46 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Grave_Titan.groovy
+++ release/Magarena/scripts/Grave_Titan.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPlayTokensAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Grave_Titan {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
             return new MagicEvent(
@@ -20,18 +9,15 @@ public class Grave_Titan {
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicPlayTokensAction(
                 event.getPlayer(),
                 TokenCardDefinitions.get("Zombie"),
                 2
             ));
         }        
-    };
-
-    public static final MagicWhenAttacksTrigger T2 = new MagicWhenAttacksTrigger() {
+    },
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent==creature) ?
@@ -43,14 +29,12 @@ public class Grave_Titan {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicPlayTokensAction(
                 event.getPlayer(),
                 TokenCardDefinitions.get("Zombie"),
                 2
             ));
         }        
-    };
-}
+    }
+]
