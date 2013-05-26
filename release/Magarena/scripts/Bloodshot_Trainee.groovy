e5adbf7ee64c7e7674c7db7db82b733befcba98b
melvin <melvinzhang@gmail.com>
2013-05-26 21:24:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bloodshot_Trainee.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.POWER_4_OR_GREATER_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(4),
+                this,
+                "SN deals 4 damage to target creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,4);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
