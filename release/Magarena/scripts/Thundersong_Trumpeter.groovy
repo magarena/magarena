ed6d2589e0805a594d8be5dbe303fb8f0f8daa78
melvin <melvinzhang@gmail.com>
2013-05-26 18:35:58 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Thundersong_Trumpeter.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Disable"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicNoCombatTargetPicker(true,true,false),
+                this,
+                "Target creature\$ can't attack or block this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.CannotAttackOrBlock));
+                }
+            });
+        }
+    }
+]
