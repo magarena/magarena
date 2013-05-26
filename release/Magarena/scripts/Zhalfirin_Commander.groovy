c6bdb61580fc27a62b15526d5b971605f01a728f
melvin <melvinzhang@gmail.com>
2013-05-26 19:58:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Zhalfirin_Commander.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{1}{W}{W}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}{W}{W}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_KNIGHT_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target Knight creature\$ gets +1/+1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,1));
+                }
+            });
+        }
+    }
+]
