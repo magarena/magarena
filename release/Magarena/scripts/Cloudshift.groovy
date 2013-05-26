2c822bd3ea30fea1c9dd13027cbe5e6822c4a6f6
melvin <melvinzhang@gmail.com>
2013-05-26 18:20:22 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Cloudshift.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Exile target creature$ you control, then return " +
+                "that card to the battlefield under your control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                    game.doAction(new MagicRemoveCardAction(creature.getCard(),MagicLocationType.Exile));
+                    game.doAction(new MagicPlayCardAction(creature.getCard(),event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
