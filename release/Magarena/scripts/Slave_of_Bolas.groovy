648b0ecb7de1980c250c0d60c4ae918b2d6cfdd9
melvin <melvinzhang@gmail.com>
2013-05-26 20:02:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Slave_of_Bolas.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "Gain control of target creature$. Untap that creature. " + 
+                "It gains haste until end of turn. Sacrifice it at end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainControlAction(event.getPlayer(),creature));
+                    game.doAction(new MagicUntapAction(creature));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
+                    game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.SacrificeAtEndOfTurn,true));
+                }
+            });
+        }
+    }
+]
