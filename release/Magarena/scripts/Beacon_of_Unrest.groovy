47b8dddc75e33e89ae104ba43bc84b3d1c702c4b
melvin <melvinzhang@gmail.com>
2013-05-26 10:26:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Beacon_of_Unrest.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_ARTIFACT_OR_CREATURE_CARD_FROM_ALL_GRAVEYARDS,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Return target artifact or creature card$ from a graveyard onto the battlefield under your control. "+
+                "Shuffle SN into its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    game.doAction(new MagicReanimateAction(event.getPlayer(),targetCard,MagicPlayCardAction.NONE));
+                    game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersLibrary));
+                }
+            });
+        }
+    }
+]
