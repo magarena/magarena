03c438bb2db6b93fb5f4f8d5d2631dd73cc872a8
melvin <melvinzhang@gmail.com>
2013-05-26 11:21:30 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Corpse_Cur.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(
+                    MagicTargetChoice.TARGET_CREATURE_CARD_WITH_INFECT_FROM_GRAVEYARD
+                ),
+                new MagicGraveyardTargetPicker(false),
+                this,
+                "PN may\$ return target creature card\$ with " +
+                "infect from his or her graveyard to his or her hand."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetCard(game,new MagicCardAction() {
+                    public void doAction(final MagicCard card) {
+                        game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                        game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+                    }
+                });
+            }
+        }
+    }
+]
