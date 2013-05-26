f7a167bee6043de7e6f3b5c230368b653195c85e
melvin <melvinzhang@gmail.com>
2013-05-26 18:49:59 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dauthi_Cutthroat.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation( 
+        [MagicConditionFactory.ManaCost("{1}{B}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}{B}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_WITH_SHADOW,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target creature with shadow\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
