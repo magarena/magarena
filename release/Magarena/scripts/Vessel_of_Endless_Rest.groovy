a3dbac47fe5fa712ef9df334cefb543cf9b26e6d
melvin <melvinzhang@gmail.com>
2013-05-26 13:42:42 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Vessel_of_Endless_Rest.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_CARD_FROM_ALL_GRAVEYARDS,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Put target card\$ from a graveyard " +
+                "on the bottom of its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
+                    game.doAction(new MagicMoveCardAction(
+                        targetCard,
+                        MagicLocationType.Graveyard,
+                        MagicLocationType.BottomOfOwnersLibrary
+                    ));
+                }
+            });
+        }
+    }
+]
