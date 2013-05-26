9977c7105542521c55717c5d1e687ce64e7ce056
melvin <melvinzhang@gmail.com>
2013-05-26 10:35:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Pendelhaven.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_1_1_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target 1/1 creature$ gets +1/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,2));
+                }
+            });
+        }
+    }
+]
