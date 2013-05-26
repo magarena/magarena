1276b387e8fe90d36d772486dd1a0af7673fe1a1
melvin <melvinzhang@gmail.com>
2013-05-26 21:20:24 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Crimson_Mage.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{R}")],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Haste"
+    ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{R}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicHasteTargetPicker.create(),
+                this,
+                "Target creature\$ you control gains haste until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
+                }
+            });
+        }
+    }
+]
