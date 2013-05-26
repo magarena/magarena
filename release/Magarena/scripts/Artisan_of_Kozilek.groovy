1533545b98ed2f2c6120472d5b4a8b5c72ffab1a
melvin <melvinzhang@gmail.com>
2013-05-26 17:47:13 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Artisan_of_Kozilek.groovy
+++ release/Magarena/scripts/Artisan_of_Kozilek.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicPlayCardAction;
-import magic.model.action.MagicReanimateAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicGraveyardTargetPicker;
-import magic.model.trigger.MagicWhenSpellIsCastTrigger;
-
-public class Artisan_of_Kozilek {
-    public static final MagicWhenSpellIsCastTrigger T = new MagicWhenSpellIsCastTrigger() {
+[
+    new MagicWhenSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -27,15 +12,13 @@ public class Artisan_of_Kozilek {
                 ),
                 new MagicGraveyardTargetPicker(true),
                 this,
-                "PN may$ return target creature card$ from " +
+                "PN may\$ return target creature card\$ from " +
                 "his or her graveyard to the battlefield."
             );
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetCard(game,new MagicCardAction() {
                     public void doAction(final MagicCard card) {
@@ -48,5 +31,5 @@ public class Artisan_of_Kozilek {
                 });
             }
         }
-    };
-}
+    }
+]
