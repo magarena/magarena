cd0e1997c430d12731bd7a7e0e7176ed2cb7a439
melvin <melvinzhang@gmail.com>
2013-05-26 20:06:40 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dauthi_Embrace.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{B}{B}")],
+        new MagicActivationHints(MagicTiming.Animate,true),
+        "Shadow"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{B}{B}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicShadowTargetPicker.getInstance(),
+                this,
+                "Target creature\$ gains shadow until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Shadow));
+                }
+            });
+        }
+    }
+]
