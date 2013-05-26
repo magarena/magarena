ef5e762dd7950303d16ace5919827cfebeabe222
melvin <melvinzhang@gmail.com>
2013-05-26 19:03:16 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Captive_Flame.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{R}")],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{R}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1/+0 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,0));
+                }
+            });
+        }
+    }
+]
