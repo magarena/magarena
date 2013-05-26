cfb006137cfd0d7bd34aea36329f2f82eb80dbcc
melvin <melvinzhang@gmail.com>
2013-05-26 18:36:24 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Alluring_Siren.groovy
+++ release/Magarena/scripts/Alluring_Siren.groovy
@@ -1,30 +1,13 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTapEvent;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicMustAttackTargetPicker;
-
-public class Alluring_Siren {
-    public static final MagicPermanentActivation A = new MagicPermanentActivation( 
-        new MagicCondition[]{MagicCondition.CAN_TAP_CONDITION},
+[
+    new MagicPermanentActivation( 
+        [MagicCondition.CAN_TAP_CONDITION],
         new MagicActivationHints(MagicTiming.MustAttack),
         "Attacks"
     ) {
         
         @Override
         public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{new MagicTapEvent(source)};
+            return [new MagicTapEvent(source)];
         }
 
         @Override
@@ -34,7 +17,7 @@ public class Alluring_Siren {
                 MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
                 MagicMustAttackTargetPicker.create(),
                 this,
-                "Target creature$ an opponent controls attacks you this turn if able."
+                "Target creature\$ an opponent controls attacks you this turn if able."
             );
         }
 
@@ -46,5 +29,5 @@ public class Alluring_Siren {
                 }
             });
         }
-    };
-}
+    }
+]
