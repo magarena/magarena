881f22d944abd7e80be94ea114a2ddf94bd664b8
melvin <melvinzhang@gmail.com>
2013-05-26 13:58:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Spiketail_Hatchling.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Counter),
+        "Counter"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell$ unless its controller pays {1}."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.create("{1}")));
+                }
+            });
+        }
+    }
+]
