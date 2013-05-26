83ca8090d7780c86b5376f35c6b40f3abc990932
melvin <melvinzhang@gmail.com>
2013-05-26 17:43:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Crippling_Chill.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target creature\$. " +
+                "It doesn't untap during its controller's next untap step. " +
+                "Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(
+                final MagicGame game,
+                final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                    game.doAction(new MagicChangeStateAction(
+                        creature,
+                        MagicPermanentState.DoesNotUntapDuringNext,
+                        true
+                    ));
+                    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+                }
+            });
+        }
+    }
+]
