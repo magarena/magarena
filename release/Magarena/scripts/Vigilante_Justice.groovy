e73699d01c593774d5a2b990a773129a088d4913
melvin <melvinzhang@gmail.com>
2013-05-26 19:54:03 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Vigilante_Justice.groovy
+++ release/Magarena/scripts/Vigilante_Justice.groovy
@@ -1,19 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Vigilante_Justice {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(
                 final MagicGame game,
@@ -27,15 +13,13 @@ public class Vigilante_Justice {
                     MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                     new MagicDamageTargetPicker(1),
                     this,
-                    "SN deals 1 damage to target creature or player$."
+                    "SN deals 1 damage to target creature or player\$."
                 ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game,new MagicTargetAction() {
                 public void doAction(final MagicTarget target) {
                     final MagicDamage damage = new MagicDamage(
@@ -47,5 +31,5 @@ public class Vigilante_Justice {
                 }
             });            
         }        
-    };
-}
+    }
+]
