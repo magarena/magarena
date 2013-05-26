a76ea9e184515939b6f3d0c5f21d350d55dd5261
melvin <melvinzhang@gmail.com>
2013-05-26 18:39:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Air_Servant.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{2}{U}")],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{2}{U}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_WITH_FLYING,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target creature\$ with flying."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                }
+            });
+        }
+    }
+]
