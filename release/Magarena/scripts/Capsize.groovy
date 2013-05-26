6fb8933a109483d9b09abcc7c5324677f92e3d3f
melvin <melvinzhang@gmail.com>
2013-05-26 19:15:55 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Capsize.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(MagicTargetChoice.TARGET_PERMANENT,MagicManaCost.create("{3}")),     
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target permanent\$ to its owner's hand. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves.");
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
