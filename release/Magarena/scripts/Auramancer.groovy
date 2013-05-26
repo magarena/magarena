e8369ca622038d279d67889c5a50b3494e29b1d6
melvin <melvinzhang@gmail.com>
2013-05-26 11:19:42 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Auramancer.groovy
+++ release/Magarena/scripts/Auramancer.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicGraveyardTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Auramancer {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -25,7 +9,7 @@ public class Auramancer {
                 ),
                 new MagicGraveyardTargetPicker(false),
                 this,
-                "PN may$ return target enchantment card$ from " +
+                "PN may\$ return target enchantment card\$ from " +
                 "his or her graveyard to his or her hand."
             );
         }
@@ -41,5 +25,5 @@ public class Auramancer {
                 });
             }
         }
-    };
-}
+    }
+]
