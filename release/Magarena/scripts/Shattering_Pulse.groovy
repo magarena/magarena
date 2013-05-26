e9afebea45ca9e8ed2cb7e3da40a58187b68d81e
melvin <melvinzhang@gmail.com>
2013-05-26 19:06:33 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Shattering_Pulse.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.TARGET_ARTIFACT,
+                    MagicManaCost.create("{3}")
+                ),
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact\$. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
