03112a201f5d27dfe731d59ad23193db733ef4f3
melvin <melvinzhang@gmail.com>
2013-05-26 11:19:25 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Frenzied_Goblin.groovy
+++ release/Magarena/scripts/Frenzied_Goblin.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicNoCombatTargetPicker;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-
-public class Frenzied_Goblin {
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+[
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent==creature) ?
@@ -27,7 +11,7 @@ public class Frenzied_Goblin {
                     ),
                     new MagicNoCombatTargetPicker(false,true,false),
                     this,
-                    "You may$ pay {R}$. If you do, target creature$ can't block this turn."
+                    "You may\$ pay {R}\$. If you do, target creature\$ can't block this turn."
                 ):
                 MagicEvent.NONE;
         }
@@ -42,5 +26,5 @@ public class Frenzied_Goblin {
                 });
             }
         }
-    };
-}
+    }
+]
