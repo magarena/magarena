38f6dda49c708e963ed5f98ae8ee7755a8910f51
melvin <melvinzhang@gmail.com>
2013-05-26 17:44:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Dauthi_Trapper.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Animate),
+        "Shadow"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicShadowTargetPicker.getInstance(),
+                this,
+                "Target creature\$ gains shadow until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Shadow));
+                }
+            });
+        }
+    }
+]
