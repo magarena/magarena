e402ed3a7142fc2eaa84270e45e3e2ac264250b9
melvin <melvinzhang@gmail.com>
2013-05-26 10:37:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Grave_Pact.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            final MagicPlayer player = permanent.getController();
+            return (otherPermanent.isCreature() &&
+                    otherPermanent.getController() == player) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Each other player sacrifices a creature."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer opponent = event.getPlayer().getOpponent();
+            if (opponent.controlsPermanentWithType(MagicType.Creature)) {
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    opponent,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                ));
+            }
+        }
+    }
+]
