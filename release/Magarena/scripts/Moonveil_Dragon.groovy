642ea9e050ee292684d742f3eae3b17482049454
melvin <melvinzhang@gmail.com>
2013-05-26 20:04:30 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Moonveil_Dragon.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{R}")],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Each creature you control gets +1/+0 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+            );
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,1,0));
+            }
+        }
+    }
+]
