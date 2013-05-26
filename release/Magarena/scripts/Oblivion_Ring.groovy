7e81c4325ecf2c157d9b79f64823c71b394676a1
melvin <melvinzhang@gmail.com>
2013-05-26 18:31:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Oblivion_Ring.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            final MagicTargetFilter<MagicPermanent> targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+                    MagicTargetFilter.TARGET_NONLAND_PERMANENT,permanent);
+            final MagicTargetChoice targetChoice = new MagicTargetChoice(
+                    targetFilter,true,MagicTargetHint.None,"another target nonland permanent to exile");
+            return new MagicEvent(
+                permanent,
+                targetChoice,
+                MagicExileTargetPicker.create(),
+                this,
+                "Exile another target nonland permanent\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicExileUntilThisLeavesPlayAction(event.getPermanent(),creature));
+                }
+            });
+        }
+    }
+]
