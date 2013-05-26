7136d6b79ad7a39ff882e01597d076c24fb6f37b
melvin <melvinzhang@gmail.com>
2013-05-26 10:32:14 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Glint_Hawk.groovy
+++ release/Magarena/scripts/Glint_Hawk.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicRemoveFromPlayAction;
-import magic.model.action.MagicSacrificeAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicBounceTargetPicker;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-
-public class Glint_Hawk {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
@@ -29,9 +14,7 @@ public class Glint_Hawk {
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isNo()) {
                 game.doAction(new MagicSacrificeAction(event.getPermanent()));
             } else {
@@ -42,5 +25,5 @@ public class Glint_Hawk {
                 });
             }
         }
-    };
-}
+    }
+]
