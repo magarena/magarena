dc97bc385b1ece3c4a219e258fa7016a5cc5029a
melvin <melvinzhang@gmail.com>
2013-05-27 15:09:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Brittle_Effigy.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{4}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Exile"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{4}"),
+                new MagicExileEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "Exile target creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                }
+            });
+        }
+    }
+]
