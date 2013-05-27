ea43e1721c17575b421473a49547c40dce6f566d
melvin <melvinzhang@gmail.com>
2013-05-27 17:16:20 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Diregraf_Captain.groovy
@@ -0,0 +1,42 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_ZOMBIE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent otherPermanent) {
+            return (permanent != otherPermanent &&
+                    otherPermanent.getController() == permanent.getController() &&
+                    otherPermanent.isCreature() &&
+                    otherPermanent.hasSubType(MagicSubType.Zombie)) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_OPPONENT,
+                    this,
+                    "Target opponent\$ loses 1 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-1));
+                }
+            });
+        }
+    }
+]
