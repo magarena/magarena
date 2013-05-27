7d2572c1ef1445222a122ebda8489d73fd2ff4e9
melvin <melvinzhang@gmail.com>
2013-05-27 17:17:37 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Goblin_Marshal.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts two 1/1 red Goblin creature tokens onto the battlefield."
+            );
+        }    
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Goblin1"),
+                2
+            ));
+        }        
+    },
+    new MagicWhenPutIntoGraveyardTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicGraveyardTriggerData triggerData) {
+            return (triggerData.fromLocation == MagicLocationType.Play) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts two 1/1 red Goblin creature tokens onto the battlefield."
+                ) :
+                MagicEvent.NONE;
+        }    
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Goblin1"),
+                2
+            ));
+        }
+    }
+]
