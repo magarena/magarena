954bbf49a5e021844cf0418105a457bfdd9db9e3
melvin <melvinzhang@gmail.com>
2013-05-27 17:46:49 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bone_Dancer.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    this,
+                    "PN may\$ put the top creature card of " +
+                    "opponent's graveyard onto the " +
+                    "battlefield under his or her control. " +
+                    "If you do, SN assigns no combat damage this turn."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                final MagicPlayer opponent = event.getPlayer().getOpponent();
+                final List<MagicCard> targets =
+                        game.filterCards(opponent,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD);
+                if (targets.size() > 0) {
+                    final MagicCard card = targets.get(targets.size()-1);
+                    game.doAction(new MagicReanimateAction(
+                        event.getPlayer(),
+                        card,
+                        MagicPlayCardAction.NONE
+                     ));
+                }
+                game.doAction(new MagicChangeStateAction(
+                    event.getPermanent(),
+                    MagicPermanentState.NoCombatDamage,
+                    true
+                ));
+            }
+        }
+    }
+]
