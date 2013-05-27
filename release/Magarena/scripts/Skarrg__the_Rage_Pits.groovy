086bc5efcefcaba3184a8f92767a88abe98ad558
melvin <melvinzhang@gmail.com>
2013-05-27 22:49:48 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Skarrg__the_Rage_Pits.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{R}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{R}{G}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1/+1 and gains trample until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,1,1));
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+                }
+            });
+        }
+    }
+]
