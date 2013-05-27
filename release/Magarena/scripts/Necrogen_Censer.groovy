0d5b13bfe36d7088199e6fb91e1e9262324bbace
melvin <melvinzhang@gmail.com>
2013-05-27 16:36:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Necrogen_Censer.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ChargeCountersAtLeast(1)
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-2));
+                }
+            });
+        }
+    }
+]
