63a5765316a22e4656f6478a16c7dc3e9c1bc170
melvin <melvinzhang@gmail.com>
2013-05-27 17:47:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Exhume.groovy
@@ -0,0 +1,38 @@
+def EVENT_ACTION = new MagicEventAction() {
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        event.processTargetCard(game,new MagicCardAction() {
+            public void doAction(final MagicCard card) {
+                game.doAction(new MagicReanimateAction(event.getPlayer(),card,MagicPlayCardAction.NONE));
+            }
+        });
+    }
+};
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player puts a creature card from " +
+                "his or her graveyard onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getPlayers()) {
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    player,
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(true),
+                    EVENT_ACTION,
+                    "PN puts a creature card from his or her graveyard onto the battlefield."
+                ));
+            }
+        }
+    }
+]
