4f3f3d27f551c67efaa6b51a3f37731aa4075b55
melvin <melvinzhang@gmail.com>
2013-05-27 17:43:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Kazandu_Tuskcaller.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ChargeCountersAtLeast(2),
+            MagicCondition.CAN_TAP_CONDITION,
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final int amount = source.getCounters(MagicCounterType.Charge);
+            return new MagicEvent(
+                source,
+                amount >= 6 ? 2 : 1,
+                this,
+                "PN puts RN 3/3 green Elephant creature tokens onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Elephant"),
+                event.getRefInt()
+            ));
+        }
+    }
+]
