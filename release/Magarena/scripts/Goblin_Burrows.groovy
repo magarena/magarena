40034f36c381339a701868295dd24950b6204abf
melvin <melvinzhang@gmail.com>
2013-05-27 15:25:32 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Goblin_Burrows.groovy
+++ release/Magarena/scripts/Goblin_Burrows.groovy
@@ -1,38 +1,19 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeTurnPTAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.condition.MagicConditionFactory;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicPayManaCostEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTapEvent;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicPumpTargetPicker;
-
-public class Goblin_Burrows {
-    public static final MagicPermanentActivation A = new MagicPermanentActivation(
-        new MagicCondition[]{
+[
+    new MagicPermanentActivation(
+        [
             MagicConditionFactory.ManaCost("{2}{R}"), //add ONE for the card itself
             MagicCondition.CAN_TAP_CONDITION    
-        },
+        ],
         new MagicActivationHints(MagicTiming.Pump),
         "Pump"
     ) {
 
         @Override
         public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{
+            return [
                 new MagicTapEvent(source),
                 new MagicPayManaCostEvent(source,"{1}{R}")
-            };
+            ];
         }
 
         @Override
@@ -42,7 +23,7 @@ public class Goblin_Burrows {
                 MagicTargetChoice.POS_TARGET_GOBLIN_CREATURE,
                 MagicPumpTargetPicker.create(),
                 this,
-                "Target Goblin creature$ gets +2/+0 until end of turn."
+                "Target Goblin creature\$ gets +2/+0 until end of turn."
             );
         }
 
@@ -54,5 +35,5 @@ public class Goblin_Burrows {
                 }
             });
         }
-    };
-}
+    }
+]
