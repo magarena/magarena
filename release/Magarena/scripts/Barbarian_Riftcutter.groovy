ba5001514914cf29cd7287410340161822554bd5
melvin <melvinzhang@gmail.com>
2013-05-27 15:03:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Barbarian_Riftcutter.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{R}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostSacrificeEvent(
+                    source,
+                    source.getController(),
+                    MagicManaCost.create("{R}")
+                )
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target land\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+               }
+            });
+        }
+    }
+]
