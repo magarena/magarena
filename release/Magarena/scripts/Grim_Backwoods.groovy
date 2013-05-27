7c3736f948d82d8c30fe109e424aa722a4c974d3
melvin <melvinzhang@gmail.com>
2013-05-27 15:00:33 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Grim_Backwoods.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicConditionFactory.ManaCost("{3}{B}{G}"), //add ONE for the card itself
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.ONE_CREATURE_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{2}{B}{G}"),
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+        }
+    }
+]
