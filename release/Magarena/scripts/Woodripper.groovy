92c0194a33c8793b923b03a5453e7ebc8914396b
melvin <melvinzhang@gmail.com>
2013-05-27 17:18:04 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Woodripper.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicConditionFactory.ChargeCountersAtLeast(1),
+            MagicConditionFactory.ManaCost("{1}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}"),
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
