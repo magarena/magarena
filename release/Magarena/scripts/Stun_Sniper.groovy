964373b50d23b7c41db58fe07f64479a3fafcf2b
melvin <melvinzhang@gmail.com>
2013-05-27 22:50:28 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Stun_Sniper.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{1}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source,"{1}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "SN deals 1 damage to target creature\$. Tap that creature."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final MagicDamage damage=new MagicDamage(event.getPermanent(),creature,1);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicTapAction(creature,true));
+                }
+            });
+        }
+    }
+]
