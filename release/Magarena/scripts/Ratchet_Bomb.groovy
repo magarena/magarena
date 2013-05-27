113b35d781cb3fb3b990883190bebe39481946c3
melvin <melvinzhang@gmail.com>
2013-05-27 22:57:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ratchet_Bomb.groovy
@@ -0,0 +1,41 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Destroy each nonland permanent with converted mana cost equal to the number of charge counters on SN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent source=event.getPermanent();
+            final int amount=source.getCounters(MagicCounterType.Charge);
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(
+                    source.getController(), 
+                    new MagicTargetFilter.MagicCMCPermanentFilter(
+                        MagicTargetFilter.TARGET_NONLAND_PERMANENT, 
+                        MagicTargetFilter.Operator.EQUAL, 
+                        amount
+                    )
+                );
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
