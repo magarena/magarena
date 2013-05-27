7453468382f5096eb71a90f3e6639002c2c6f1af
melvin <melvinzhang@gmail.com>
2013-05-27 22:30:23 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Lord_of_Shatterskull_Pass.groovy
+++ release/Magarena/scripts/Lord_of_Shatterskull_Pass.groovy
@@ -1,45 +1,25 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.MagicSource;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenAttacksTrigger;
-
-import java.util.Collection;
-
-public class Lord_of_Shatterskull_Pass {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.SetPT) {
+[
+    new MagicStatic(MagicLayer.SetPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             if (permanent.getCounters(MagicCounterType.Charge)>0) {
                 pt.set(6,6);
             } 
         }        
-    };
-        
-    public static final MagicWhenAttacksTrigger T = new MagicWhenAttacksTrigger() {
+    },
+    new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (permanent==creature&&permanent.getCounters(MagicCounterType.Charge)>=6) ?
                 new MagicEvent(
-                        permanent,
-                        this,
-                        "SN deals 6 damage to each creature defending player controls."):
+                    permanent,
+                    this,
+                    "SN deals 6 damage to each creature defending player controls."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicSource source=event.getPermanent();
             final MagicPlayer defendingPlayer=event.getPlayer().getOpponent();
             final Collection<MagicPermanent> creatures=
@@ -49,5 +29,5 @@ public class Lord_of_Shatterskull_Pass {
                 game.doAction(new MagicDealDamageAction(damage));
             }
         }
-    };
-}
+    }
+]
