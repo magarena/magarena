aa205cd4437de73318cea3a5e54e61493bc14b76
melvin <melvinzhang@gmail.com>
2013-05-27 16:32:50 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Turntimber_Ranger.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.getController() == permanent.getController() &&
+                    otherPermanent.hasSubType(MagicSubType.Ally)) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.PLAY_TOKEN,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    new MagicDestroyTargetPicker(false),
+                    this,
+                    "PN may\$ put a 2/2 green Wolf creature " +
+                    "token onto the battlefield. If you do, put a " +
+                    "+1/+1 counter on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicPlayTokenAction(
+                    event.getPlayer(),
+                    TokenCardDefinitions.get("Wolf")
+                ));
+                game.doAction(new MagicChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }        
+        }        
+    }
+]
