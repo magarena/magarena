f89bdb6a4e71a219e9cdf1cf24fff76180722842
melvin <melvinzhang@gmail.com>
2013-05-27 22:32:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Fireslinger.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target creature or player\$ and 1 damage to PN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicSource source=event.getSource();
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage1=new MagicDamage(source,target,1);
+                    game.doAction(new MagicDealDamageAction(damage1));
+                    final MagicDamage damage2=new MagicDamage(source,event.getPlayer(),1);
+                    game.doAction(new MagicDealDamageAction(damage2));
+                }
+            });
+        }
+    }
+]
