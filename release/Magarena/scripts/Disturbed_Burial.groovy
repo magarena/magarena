5a7709958187176a7308a878d9ea9bf462fdc8c1
melvin <melvinzhang@gmail.com>
2013-05-27 17:41:13 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Disturbed_Burial.groovy
+++ release/Magarena/scripts/Disturbed_Burial.groovy
@@ -1,23 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicChangeCardDestinationAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicBuybackChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicGraveyardTargetPicker;
-
-public class Disturbed_Burial {
-    public static final MagicSpellCardEvent E = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
@@ -28,15 +10,13 @@ public class Disturbed_Burial {
                 ),
                 new MagicGraveyardTargetPicker(false),
                 this,
-                "Return target creature card$ from your graveyard to your hand. " +
-                "If the buyback cost was payed$, return SN to its owner's hand as it resolves."
+                "Return target creature card\$ from your graveyard to your hand. " +
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
             );
         }
 
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetCard(game,new MagicCardAction() {
                 public void doAction(final MagicCard targetCard) {
                     game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
@@ -47,5 +27,5 @@ public class Disturbed_Burial {
                 }
             });
         }
-    };
-}
+    }
+]
