614eae351478904b28db4a6bf2e9d88d2a46c8f8
melvin <melvinzhang@gmail.com>
2013-05-27 13:28:25 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Shivan_Hellkite.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}{R}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}{R}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,1);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
