bfc18c1cee05bfdc1a7fdc43b76c5de2ca1795bd
melvin <melvinzhang@gmail.com>
2013-05-27 13:08:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Alabaster_Mage.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}{W}")],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Lifelink"
+    ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}{W}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicLifelinkTargetPicker.create(),
+                this,
+                "Target creature\$ you control gains lifelink until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Lifelink));
+                }
+            });
+        }
+    }
+]
