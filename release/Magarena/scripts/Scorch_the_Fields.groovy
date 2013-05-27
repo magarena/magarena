ece686de2ec6471f83424e3b9b90fa48986152f2
melvin <melvinzhang@gmail.com>
2013-05-27 17:38:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Scorch_the_Fields.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target land\$. " + 
+                "SN deals 1 damage to each Human creature."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    final Collection<MagicPermanent> targets = game.filterPermanents(
+                            event.getPlayer(),
+                            MagicTargetFilter.TARGET_HUMAN);
+                    for (final MagicPermanent target : targets) {
+                        final MagicDamage damage = new MagicDamage(
+                            event.getSource(),
+                            target,
+                            1
+                        );
+                        game.doAction(new MagicDealDamageAction(damage));
+                    }
+                }
+            });
+        }
+    }
+]
