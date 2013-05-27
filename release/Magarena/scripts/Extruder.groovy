2b6e84f968293668acdde4f9fb5ddb0ff7d74305
melvin <melvinzhang@gmail.com>
2013-05-27 22:29:57 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Extruder.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CONTROL_ARTIFACT_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    source.getController(),
+                    MagicTargetChoice.SACRIFICE_ARTIFACT
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN puts a +1/+1 counter on target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        1,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
