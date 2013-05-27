201d2614914a76fb081de1a8000f864c6139292d
melvin <melvinzhang@gmail.com>
2013-05-27 15:07:49 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Fledgling_Dragon.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+            if (MagicCondition.THRESHOLD_CONDITION.accept(permanent)) {
+                pt.add(3,3);
+            }
+        }
+    },
+    new MagicPermanentActivation(
+        [
+            MagicCondition.THRESHOLD_CONDITION,
+            MagicConditionFactory.ManaCost("{R}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+0 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,0));
+        }
+    }
+]
