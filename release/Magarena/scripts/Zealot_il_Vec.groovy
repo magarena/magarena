e56bf29094288bd6a43d2162e4b341bf6ebb8057
melvin <melvinzhang@gmail.com>
2013-05-27 17:39:52 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Zealot_il_Vec.groovy
+++ release/Magarena/scripts/Zealot_il_Vec.groovy
@@ -1,20 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentState;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.trigger.MagicWhenAttacksUnblockedTrigger;
-
-public class Zealot_il_Vec {
-    public static final MagicWhenAttacksUnblockedTrigger T = new MagicWhenAttacksUnblockedTrigger() {
+[
+    new MagicWhenAttacksUnblockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
             return (creature == permanent) ?
@@ -23,15 +8,14 @@ public class Zealot_il_Vec {
                     new MagicMayChoice(MagicTargetChoice.NEG_TARGET_CREATURE),
                     new MagicDamageTargetPicker(1),
                     this,
-                    "PN may$ have SN deal 1 damage to target creature$."
+                    "PN may\$ have SN deal 1 damage to target creature\$. " +
+                    "If you do, prevent all combat damage SN would deal this turn."
                 ):
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent creature) {
@@ -43,11 +27,13 @@ public class Zealot_il_Vec {
                         );
                         game.doAction(new MagicDealDamageAction(damage));
                         game.doAction(new MagicChangeStateAction(
-                                permanent,
-                                MagicPermanentState.NoCombatDamage,true));
+                            permanent,
+                            MagicPermanentState.NoCombatDamage,
+                            true
+                        ));
                     }
                 });
             }
         }
-    };
-}
+    }
+]
