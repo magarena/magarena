910f37b99e477b69d8f54bd9e62c19c202d886cf
melvin <melvinzhang@gmail.com>
2013-05-27 16:30:23 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Scalding_Devil.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{2}{R}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),player,1);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
