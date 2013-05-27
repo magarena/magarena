5e6cc9f3cb819cb29a47df1b6a9d3a242e750461
melvin <melvinzhang@gmail.com>
2013-05-27 13:28:57 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Rootrunner.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{G}{G}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Bounce"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostSacrificeEvent(source,"{G}{G}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Put target land\$ on top of its owner's library."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(
+                        creature,
+                        MagicLocationType.TopOfOwnersLibrary
+                    ));
+                }
+            });
+        }
+    }
+]
