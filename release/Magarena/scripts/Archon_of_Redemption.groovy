cb750506dfd92f3245319d2bddc55a8fdd6cb2ec
melvin <melvinzhang@gmail.com>
2013-05-27 15:05:23 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Archon_of_Redemption.groovy
+++ release/Magarena/scripts/Archon_of_Redemption.groovy
@@ -1,32 +1,19 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Archon_of_Redemption {
-    public static final MagicWhenComesIntoPlayTrigger T = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
                 permanent,
                 this,
-                "PN gains life equal to SN's power.");
+                "PN gains life equal to SN's power."
+            );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),event.getPermanent().getPower()));
         }        
-    };
-
-    public static final MagicWhenOtherComesIntoPlayTrigger T2 = new MagicWhenOtherComesIntoPlayTrigger() {
+    },
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent!=permanent && 
@@ -37,15 +24,14 @@ public class Archon_of_Redemption {
                     permanent,
                     otherPermanent,
                     this,
-                    "PN gains life equal to the power of "+otherPermanent+'.'):
+                    "PN gains life equal to the power of "+otherPermanent+'.'
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent=event.getRefPermanent();
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),permanent.getPower()));
         }        
-    };
-}
+    }
+]
