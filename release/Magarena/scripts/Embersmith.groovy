b9470c9bc65f254cefcfd7990df27ce7dc9f03c4
melvin <melvinzhang@gmail.com>
2013-05-27 15:05:44 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Embersmith.groovy
+++ release/Magarena/scripts/Embersmith.groovy
@@ -1,22 +1,5 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicTargetAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDamageTargetPicker;
-import magic.model.target.MagicTarget;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-public class Embersmith {
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
             return (permanent.isFriend(spell) && 
@@ -29,15 +12,13 @@ public class Embersmith {
                     ),
                     new MagicDamageTargetPicker(2),
                     this,
-                    "PN may$ pay {1}$. If you do, SN deals 1 " +
-                    "damage to target creature or player$."
+                    "PN may\$ pay {1}\$. If you do, SN deals 1 " +
+                    "damage to target creature or player\$."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTarget(game,new MagicTargetAction() {
                     public void doAction(final MagicTarget target) {
@@ -47,5 +28,5 @@ public class Embersmith {
                 });
             }
         }        
-    };
-}
+    }
+]
