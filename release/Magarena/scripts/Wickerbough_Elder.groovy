b4d1cd54a304ac99f2045644e8179e251d15148a
melvin <melvinzhang@gmail.com>
2013-05-27 22:31:33 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Wickerbough_Elder.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicCondition.MINUS_COUNTER_CONDITION,
+            MagicConditionFactory.ManaCost("{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{G}"),
+                new MagicRemoveCounterEvent(source,MagicCounterType.MinusOne,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact or enchantment\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
