3adafc08feb67d195d490f6cf8390e1548bab4c6
melvin <melvinzhang@gmail.com>
2013-05-27 15:08:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Myr_Matrix.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_MYR_CREATURE) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+    },
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{5}")],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{5}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 1/1 colorless Myr artifact creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Myr1")));
+        }
+    }
+]
