6247c56b8e76d1015322f1082f15c9c9f9aa4911
melvin <melvinzhang@gmail.com>
2013-05-27 13:23:36 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Loxodon_Hierarch.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{G}{W}")],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Regen"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostSacrificeEvent(source, "{G}{W}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Regenerate each creature you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent permanent : targets) {
+                game.doAction(new MagicRegenerateAction(permanent));
+            }
+        }
+    }
+]
