e51361f4dfb3c64718aff2d16331ce43fea20a3e
melvin <melvinzhang@gmail.com>
2013-05-27 17:43:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Pongify.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                new MagicDestroyTargetPicker(true),
+                this,
+                "Destroy target creature\$. It can't be regenerated. " +
+                "That creature's controller puts a 3/3 green Ape " +
+                "creature onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final MagicPlayer controller = creature.getController();
+                    game.doAction(new MagicChangeStateAction(
+                        creature,
+                        MagicPermanentState.CannotBeRegenerated,
+                        true
+                    ));
+                    game.doAction(new MagicDestroyAction(creature));
+                    game.doAction(new MagicPlayTokenAction(
+                        controller,
+                        TokenCardDefinitions.get("Ape")
+                    ));
+                }
+            });
+        }
+    }
+]
