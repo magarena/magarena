d9986fd76c38cda35aa951e57adb55cd1c7e4a19
melvin <melvinzhang@gmail.com>
2013-05-27 15:03:15 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Rite_of_Replication.groovy
+++ release/Magarena/scripts/Rite_of_Replication.groovy
@@ -1,22 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCardDefinition;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.choice.MagicKickerChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicCopyTargetPicker;
-
-public class Rite_of_Replication {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(
                 final MagicCardOnStack cardOnStack,
@@ -30,14 +13,12 @@ public class Rite_of_Replication {
                 ),
                 MagicCopyTargetPicker.create(),
                 this,
-                "Put a token onto the battlefield that's a copy of target creature$. " + 
-                "If SN was kicked$, put five of those tokens onto the battlefield instead."
+                "Put a token onto the battlefield that's a copy of target creature\$. " + 
+                "If SN was kicked\$, put five of those tokens onto the battlefield instead."
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     final MagicPlayer player = event.getPlayer();
@@ -49,5 +30,5 @@ public class Rite_of_Replication {
                 }
             });
         }
-    };
-}
+    }
+]
