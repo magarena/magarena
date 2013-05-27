33ac9981463a55aeec21aa40f885a560e71f7448
melvin <melvinzhang@gmail.com>
2013-05-27 13:08:21 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Imps__Taunt.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.NEG_TARGET_CREATURE,
+                    MagicManaCost.create("{3}")
+                ),
+                MagicMustAttackTargetPicker.create(),
+                this,
+                "Target creature\$ attacks this turn if able. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicSetAbilityAction(creature,MagicAbility.AttacksEachTurnIfAble));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
