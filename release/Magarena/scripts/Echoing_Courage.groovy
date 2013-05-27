9a159dba946e4136fe6842676347183685ae7dfc
melvin <melvinzhang@gmail.com>
2013-05-27 13:13:29 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Echoing_Courage.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ and all other creatures with the same " +
+                "name as that creature get +2/+2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent targetPermanent) {
+                    final MagicTargetFilter<MagicPermanent> targetFilter = 
+                        new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
+                    final Collection<MagicPermanent> targets = 
+                        game.filterPermanents(event.getPlayer(),targetFilter);
+                    for (final MagicPermanent permanent : targets) {
+                        if (permanent.isCreature()) {
+                            game.doAction(new MagicChangeTurnPTAction(permanent,2,2));
+                        }
+                    }
+                }
+            });
+        }
+    }
+]
