d6c86f2be466dcfa9247a94e285909b96337f518
melvin <melvinzhang@gmail.com>
2013-05-27 16:33:15 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Echoing_Truth.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_NONLAND_PERMANENT,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Return target nonland permanent\$ and all other " +
+                "permanents with the same name as that permanent " +
+                "to their owners' hands."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent targetPermanent) {
+                    final MagicTargetFilter<MagicPermanent> targetFilter = 
+                        new MagicTargetFilter.NameTargetFilter(targetPermanent.getName());
+                    final Collection<MagicPermanent> targets = 
+                        game.filterPermanents(event.getPlayer(),targetFilter);
+                    for (final MagicPermanent target : targets) {
+                        game.doAction(new MagicRemoveFromPlayAction(
+                            target,
+                            MagicLocationType.OwnersHand
+                        ));
+                    }
+                }
+            });
+        }
+    }
+]
