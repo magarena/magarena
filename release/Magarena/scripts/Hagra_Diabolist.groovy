5ca4fa817ef9d7fb0bbe60f61df5467bdf9ab3d4
melvin <melvinzhang@gmail.com>
2013-05-27 13:23:54 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Hagra_Diabolist.groovy
+++ release/Magarena/scripts/Hagra_Diabolist.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicSubType;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicPlayerAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Hagra_Diabolist {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isFriend(permanent) &&
@@ -23,16 +10,14 @@ public class Hagra_Diabolist {
                         MagicTargetChoice.NEG_TARGET_PLAYER
                     ),
                     this,
-                    "PN may$ have target player$ lose life " +
+                    "PN may\$ have target player\$ lose life " +
                     "equal to the number of Allies he or she controls."
                 ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPlayer(game,new MagicPlayerAction() {
                     public void doAction(final MagicPlayer targetPlayer) {
@@ -46,5 +31,5 @@ public class Hagra_Diabolist {
                 });
             }            
         }        
-    };
-}
+    }
+]
