2fec8ba07896922ff5722604f03ab760af700771
melvin <melvinzhang@gmail.com>
2013-05-27 17:48:03 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Searing_Touch.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicBuybackChoice(
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    MagicManaCost.create("{4}")
+                ),
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target creature or player\$. " + 
+                "If the buyback cost was payed\$, return SN to its owner's hand as it resolves."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(
+                        event.getSource(),
+                        target,
+                        1
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
+                    if (event.isBuyback()) {
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                    } 
+                }
+            });
+        }
+    }
+]
