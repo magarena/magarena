be5fceedf1bc978db6f53671c8cc93aa8f51d56f
melvin <melvinzhang@gmail.com>
2013-05-27 13:29:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ember_Hauler.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostSacrificeEvent(source,"{1}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(2),
+                this,
+                "SN deals 2 damage to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,2);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
