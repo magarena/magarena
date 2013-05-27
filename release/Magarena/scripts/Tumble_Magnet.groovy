d8b476d889b2333c324a6b449fc8fa97d9a14bf7
melvin <melvinzhang@gmail.com>
2013-05-27 22:33:37 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Tumble_Magnet.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ChargeCountersAtLeast(1)
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Tap"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_CREATURE,
+                new MagicTapTargetPicker(true,false),
+                this,
+                "Tap target artifact or creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature,true));
+                }
+            });
+        }
+    }
+]
