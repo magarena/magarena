09f40eb85a3609fc9562a16035c694ddf979a2ea
melvin <melvinzhang@gmail.com>
2013-05-27 22:56:04 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Sword_of_Light_and_Shadow.groovy
+++ release/Magarena/scripts/Sword_of_Light_and_Shadow.groovy
@@ -1,23 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicGraveyardTargetPicker;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-
-public class Sword_of_Light_and_Shadow {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return (damage.getSource() == permanent.getEquippedCreature() && 
@@ -30,14 +12,13 @@ public class Sword_of_Light_and_Shadow {
                     ),
                     new MagicGraveyardTargetPicker(false),
                     this,
-                    "PN gains 3 life and may$ return target creature card$ " +
-                    "from his or her graveyard to his or her hand."):
+                    "PN gains 3 life and may\$ return target creature card\$ " +
+                    "from his or her graveyard to his or her hand."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
             if (event.isYes()) {
                 event.processTargetCard(game,new MagicCardAction() {
@@ -48,5 +29,5 @@ public class Sword_of_Light_and_Shadow {
                 });
             }
         }
-    };
-}
+    }
+]
