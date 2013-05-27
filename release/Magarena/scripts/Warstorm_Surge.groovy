7bfded01be9a38e25eeb4b0057dddf33a0835ff1
melvin <melvinzhang@gmail.com>
2013-05-27 13:10:20 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Warstorm_Surge.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature() && otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(otherPermanent.getPower()),
+                    otherPermanent,
+                    this,
+                    "RN deals damage equal to its power to target creature or player$."
+                ) :
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getRefPermanent();
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(permanent,target,permanent.getPower());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }    
+    }
+]
