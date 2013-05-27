fbf1143a0912ba27b5672666e40b0dbb912b2f00
melvin <melvinzhang@gmail.com>
2013-05-27 22:34:30 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Hedron_Field_Purists.groovy
+++ release/Magarena/scripts/Hedron_Field_Purists.groovy
@@ -1,18 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.trigger.MagicIfDamageWouldBeDealtTrigger;
-
-public class Hedron_Field_Purists {
-    public static final MagicStatic S = new MagicStatic(MagicLayer.SetPT) {
+[
+    new MagicStatic(MagicLayer.SetPT) {
         @Override
         public void modPowerToughness(
                 final MagicPermanent source,
@@ -25,9 +12,8 @@ public class Hedron_Field_Purists {
                 pt.set(1,4);
             }
         }
-    };
-    
-    public static final MagicIfDamageWouldBeDealtTrigger T = new MagicIfDamageWouldBeDealtTrigger(5) {
+    },
+    new MagicIfDamageWouldBeDealtTrigger(5) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicPlayer player = permanent.getController();
@@ -47,10 +33,5 @@ public class Hedron_Field_Purists {
             }            
             return MagicEvent.NONE;
         }
-        @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
-        }
-    };
-}
+    }
+]
