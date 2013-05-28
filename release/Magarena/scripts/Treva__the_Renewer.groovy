3aec54ac3018efff7a7aa48ada1874b48a4feb34
melvin <melvinzhang@gmail.com>
2013-05-28 16:01:52 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Treva__the_Renewer.groovy
+++ release/Magarena/scripts/Treva__the_Renewer.groovy
@@ -1,23 +1,5 @@
-package magic.card;
-
-import magic.model.MagicColor;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.choice.MagicColorChoice;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
-
-import java.util.Collection;
-
-public class Treva__the_Renewer {
-    public static final MagicWhenDamageIsDealtTrigger T = new MagicWhenDamageIsDealtTrigger() {
+[
+    new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return (damage.getSource() == permanent && 
@@ -30,15 +12,13 @@ public class Treva__the_Renewer {
                         MagicColorChoice.MOST_INSTANCE
                     ),
                     this,
-                    "PN may$ pay {2}{W}$. If you do, choose a color$. " + 
+                    "PN may\$ pay {2}{W}\$. If you do, choose a color\$. " + 
                     "PN gains 1 life for each permanent of that color."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 int life=0;
                 final MagicPlayer player=event.getPlayer();
@@ -54,5 +34,5 @@ public class Treva__the_Renewer {
                 }
             }
         }        
-    };
-}
+    }
+]
