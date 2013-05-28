fa0b986207ceb0124d05e75ab90a444649f05282
melvin <melvinzhang@gmail.com>
2013-05-28 15:57:37 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sosuke__Son_of_Seshiro.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_SNAKE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,0);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return (damage.getSource().getController() == permanent.getController() &&
+                    damage.getSource().isPermanent() &&
+                    damage.getSource().hasSubType(MagicSubType.Warrior) &&
+                    damage.isCombat() &&
+                    damage.getTarget().isPermanent() &&
+                    damage.getTarget().isCreature()) ?
+                new MagicEvent(
+                    damage.getSource(),
+                    permanent.getController(),
+                    damage.getTarget(),
+                    this,
+                    "Destroy RN at end of combat."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeStateAction(event.getRefPermanent(),MagicPermanentState.DestroyAtEndOfCombat,true));
+        }
+    }
+]
