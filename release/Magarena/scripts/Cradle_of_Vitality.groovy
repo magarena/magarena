7ff721d1d2945af90d275da77ea9e8573387737a
melvin <melvinzhang@gmail.com>
2013-05-28 16:19:10 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Cradle_of_Vitality.groovy
+++ release/Magarena/scripts/Cradle_of_Vitality.groovy
@@ -1,22 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicPayManaCostChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicPumpTargetPicker;
-import magic.model.trigger.MagicLifeChangeTriggerData;
-import magic.model.trigger.MagicWhenLifeIsGainedTrigger;
-
-public class Cradle_of_Vitality {
-    public static final MagicWhenLifeIsGainedTrigger T = new MagicWhenLifeIsGainedTrigger() {
+[
+    new MagicWhenLifeIsGainedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicLifeChangeTriggerData lifeChange) {
             final MagicPlayer player = permanent.getController();
@@ -32,14 +15,13 @@ public class Cradle_of_Vitality {
                     MagicPumpTargetPicker.create(),
                     amount,
                     this,
-                    "You may$ pay {1}{W}$. If you do, put a +1/+1 counter " +
-                    "on target creature$ for each 1 life you gained."):
+                    "You may\$ pay {1}{W}\$. If you do, put a +1/+1 counter " +
+                    "on target creature\$ for each 1 life you gained."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent creature) {
@@ -53,5 +35,5 @@ public class Cradle_of_Vitality {
                 });
             }
         }
-    };
-}
+    }
+]
