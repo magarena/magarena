764c162057b32733a613fb174231c6722dde8f03
melvin <melvinzhang@gmail.com>
2013-05-28 15:08:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Bonfire_of_the_Damned.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                new MagicDamageTargetPicker(amount),
+                this,
+                "SN deals " + amount +
+                " damage to target player$ and each creature he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final int amount = event.getCardOnStack().getX();
+                    MagicDamage damage = new MagicDamage(event.getSource(), player, amount);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    final Collection<MagicPermanent> targets = game.filterPermanents(
+                            player,
+                            MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                    for (final MagicPermanent target : targets) {
+                        damage = new MagicDamage(event.getSource(), target, amount);
+                        game.doAction(new MagicDealDamageAction(damage));
+                    }
+                }
+            });
+        }
+    }
+]
