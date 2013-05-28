d917a917ade2b173e7c8b3115ba93a9dc966431b
melvin <melvinzhang@gmail.com>
2013-05-28 15:13:31 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hoard_Smelter_Dragon.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{3}{R}")],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{3}{R}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_ARTIFACT,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target artifact\$. SN gets +X/+0 " +
+                "until end of turn, where X is that artifact's converted mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    game.doAction(new MagicChangeTurnPTAction(
+                        event.getPermanent(),
+                        permanent.getConvertedCost(),
+                        0
+                    ));
+                }
+            });
+        }
+    }
+]
