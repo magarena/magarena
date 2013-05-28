b0dc3c5a066490a9ea6ce18ea9e7dc89a9c08f03
melvin <melvinzhang@gmail.com>
2013-05-28 20:14:58 +0800

    fix requires, scripts

--- release/Magarena/scripts/Burr_Grafter.groovy
+++ release/Magarena/scripts/Burr_Grafter.groovy
@@ -1,3 +1,33 @@
 [
-    Fires_of_Yavimaya.A
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificeEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+2 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
+                }
+            });
+        }
+    }
 ]
--- release/Magarena/scripts/Fires_of_Yavimaya.groovy
+++ release/Magarena/scripts/Fires_of_Yavimaya.groovy
@@ -7,36 +7,5 @@
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Haste);
         }
-    },
-    new MagicPermanentActivation(
-        MagicActivation.NO_COND,
-        new MagicActivationHints(MagicTiming.Pump),
-        "Pump"
-    ) {
-
-        @Override
-        public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return [new MagicSacrificeEvent(source)];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.POS_TARGET_CREATURE,
-                MagicPumpTargetPicker.create(),
-                this,
-                "Target creature\$ gets +2/+2 until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature,2,2));
-                }
-            });
-        }
     }
 ]
