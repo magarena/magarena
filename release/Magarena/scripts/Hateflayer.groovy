f5f1e8ad2b3c0fb82aa2d05c62a52985f5a6dc6d
melvin <melvinzhang@gmail.com>
2013-05-28 15:58:14 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hateflayer.groovy
@@ -0,0 +1,41 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_UNTAP_CONDITION,
+            MagicConditionFactory.ManaCost("{2}{R}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{R}"),
+                new MagicUntapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(5),
+                this,
+                "SN deals damage equal to its power to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicPermanent permanent=event.getPermanent();
+                    final MagicDamage damage=new MagicDamage(permanent,target,permanent.getPower());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
