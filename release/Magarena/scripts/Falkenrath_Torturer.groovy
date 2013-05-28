4362d4cbf6d840b077fadc9e5fe9edde31e15d0e
melvin <melvinzhang@gmail.com>
2013-05-28 16:17:39 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Falkenrath_Torturer.groovy
@@ -0,0 +1,46 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ONE_CREATURE_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final boolean isHuman = payedCost.getTarget().hasSubType(MagicSubType.Human);
+            final String message = 
+                "SN gains flying until end of turn." +
+                (isHuman ? " Put a +1/+1 counter on SN." : "");
+            return new MagicEvent(
+                source,
+                isHuman ? 1 : 0,
+                this,
+                message
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicSetAbilityAction(
+                event.getPermanent(),
+                MagicAbility.Flying
+            ));
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                event.getRefInt(),
+                true
+            ));
+        }
+    }
+]
