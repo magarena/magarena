aea36ed3b600ee1320defb6c86e981f8cd780767
melvin <melvinzhang@gmail.com>
2013-05-28 20:17:26 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Stitch_Together.groovy
+++ release/Magarena/scripts/Stitch_Together.groovy
@@ -1,24 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicPlayCardAction;
-import magic.model.action.MagicReanimateAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicGraveyardTargetPicker;
-
-public class Stitch_Together {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             // estimated number of cards in the graveyard. this may change
@@ -28,15 +9,13 @@ public class Stitch_Together {
                 MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
                 new MagicGraveyardTargetPicker(MagicCondition.THRESHOLD_CONDITION.accept(cardOnStack)),
                 this,
-                "Return target creature card$ from your graveyard to your hand. " +
+                "Return target creature card\$ from your graveyard to your hand. " +
                 "Return that card from your graveyard to the battlefield instead " +
                 "if seven or more cards are in your graveyard."
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             event.processTargetCard(game,new MagicCardAction() {
                 public void doAction(final MagicCard targetCard) {              
@@ -49,5 +28,5 @@ public class Stitch_Together {
                 }
             });
         }
-    };
-}
+    }
+]
