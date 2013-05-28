5a23158431bec74ea25fd2567040dd30461917cf
melvin <melvinzhang@gmail.com>
2013-05-28 16:36:32 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Cinder_Elemental.groovy
@@ -0,0 +1,42 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{X}{R}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{X}{R}"),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(amount),
+                amount,
+                this,
+                "SN deals RN damage to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getRefInt());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
