cd41d33fc017747c05e71a7d7bfb5f5e5167e14b
melvin <melvinzhang@gmail.com>
2013-05-28 16:07:37 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Hand_of_the_Praetors.groovy
+++ release/Magarena/scripts/Hand_of_the_Praetors.groovy
@@ -1,24 +1,8 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicChangePoisonAction;
-import magic.model.action.MagicPlayerAction;
-import magic.model.event.MagicEvent;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-public class Hand_of_the_Praetors {
-    public static final MagicStatic S = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+    ) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
@@ -27,9 +11,8 @@ public class Hand_of_the_Praetors {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target && target.hasAbility(MagicAbility.Infect);
         }
-    };
-    
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+    },
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return (permanent.isFriend(cardOnStack) &&
@@ -39,19 +22,17 @@ public class Hand_of_the_Praetors {
                     permanent,
                     MagicTargetChoice.NEG_TARGET_PLAYER,
                     this,
-                    "Target player$ gets a poison counter."
+                    "Target player\$ gets a poison counter."
                 ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
                     game.doAction(new MagicChangePoisonAction(player,1));
                 }
             });
         }        
-    };
-}
+    }
+]
