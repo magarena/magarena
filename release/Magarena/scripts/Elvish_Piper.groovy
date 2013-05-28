3cdded8a82ed1b927058e5df4aaa5baede682078
melvin <melvinzhang@gmail.com>
2013-05-28 17:35:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Elvish_Piper.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{G}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_HAND,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Put a creature card\$ from your hand onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicPlayCardAction(card,event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
