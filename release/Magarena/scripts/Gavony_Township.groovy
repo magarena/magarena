4c3cd522ef9813cef7a7e97b1d347d1acb38add3
melvin <melvinzhang@gmail.com>
2013-05-28 16:16:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Gavony_Township.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicConditionFactory.ManaCost("{3}{G}{W}"), //add ONE for the card itself
+            MagicCondition.CAN_TAP_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{2}{G}{W}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a +1/+1 counter on each creature you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeCountersAction(
+                    target,
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }
+        }
+    }
+]
