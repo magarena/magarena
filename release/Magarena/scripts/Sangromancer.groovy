cf102953f4e23fa378b6af386fa40a9ddcdee967
melvin <melvinzhang@gmail.com>
2013-05-28 16:20:13 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Sangromancer.groovy
+++ release/Magarena/scripts/Sangromancer.groovy
@@ -1,16 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicChangeLifeAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenDiscardedTrigger;
-import magic.model.trigger.MagicWhenOtherPutIntoGraveyardFromPlayTrigger;
-
-public class Sangromancer {
-    public static final MagicWhenOtherPutIntoGraveyardFromPlayTrigger T1 = new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+[
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isEnemy(permanent) && 
@@ -23,20 +12,18 @@ public class Sangromancer {
                         MagicSimpleMayChoice.DEFAULT_YES
                     ),
                     this,
-                    "PN may$ gain 3 life.") :
+                    "PN may\$ gain 3 life."
+                ) :
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
             }
         }
-    };
-    
-    public static final MagicWhenDiscardedTrigger T2 =new MagicWhenDiscardedTrigger() {
+    },
+    new MagicWhenDiscardedTrigger() {
         @Override
         public MagicEvent getEvent(final MagicPermanent permanent,final MagicCard card) {
             return permanent.isEnemy(card) ?
@@ -45,19 +32,19 @@ public class Sangromancer {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.GAIN_LIFE,
                         3,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ gain 3 life.") :
+                    "PN may\$ gain 3 life."
+                ) :
                 MagicEvent.NONE;
         }
 
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
             }
         }
-    };
-}
+    }
+]
