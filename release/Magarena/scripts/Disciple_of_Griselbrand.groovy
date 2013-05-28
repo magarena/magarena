5f68aeb1b3e039de4bdd87c6bdfce14d3cf14a85
melvin <melvinzhang@gmail.com>
2013-05-28 16:02:23 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Disciple_of_Griselbrand.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{1}"),
+            MagicCondition.ONE_CREATURE_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.SACRIFICE_CREATURE,
+                MagicSacrificeTargetPicker.create(),
+                this,
+                "Sacrifice a creature. PN gains life " +
+                "equal to sacrificed creature's toughness."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final int toughness=creature.getToughness();
+                    game.doAction(new MagicSacrificeAction(creature));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),toughness));
+                }
+            });
+        }
+    }
+]
