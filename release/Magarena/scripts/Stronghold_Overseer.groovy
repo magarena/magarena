8879f3d45fa9e7f7bb5d87aab48758c30cc53b21
melvin <melvinzhang@gmail.com>
2013-05-28 15:58:48 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Stronghold_Overseer.groovy
@@ -0,0 +1,39 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{B}{B}")],
+        new MagicActivationHints(MagicTiming.Pump,false,1),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}{B}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Creatures with shadow get +1/+0 until end of turn and " +
+                "creatures without shadow get -1/-0 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            Collection<MagicPermanent> targets =
+                    game.filterPermanents(game.getPlayer(0),MagicTargetFilter.TARGET_CREATURE_WITH_SHADOW);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,1,0));
+            }
+            targets = game.filterPermanents(game.getPlayer(0),MagicTargetFilter.TARGET_CREATURE_WITHOUT_SHADOW);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeTurnPTAction(target,-1,0));
+            }
+        }
+    }
+]
