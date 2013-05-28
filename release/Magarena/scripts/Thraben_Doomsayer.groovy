377158b3ffc79a82dccd35284df3df8211ff2b7b
melvin <melvinzhang@gmail.com>
2013-05-28 16:35:56 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Thraben_Doomsayer.groovy
@@ -0,0 +1,49 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+            pt.add(2,2);
+        }
+        @Override
+        public boolean condition(
+                final MagicGame game,
+                final MagicPermanent source,
+                final MagicPermanent target) {
+            return source != target && source.getController().getLife() <= 5;
+        }
+    },
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 1/1 white Human " +
+                "creature token onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Human1")
+            ));
+        }
+    }
+]
