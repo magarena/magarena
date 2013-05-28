fffbf9fa1d320fdf94e00417314d77a7f9181308
melvin <melvinzhang@gmail.com>
2013-05-28 16:02:54 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Crypt_Creeper.groovy
@@ -0,0 +1,41 @@
+[
+    new MagicPermanentActivation(
+        MagicActivation.NO_COND,
+        new MagicActivationHints(MagicTiming.Removal),
+        "Exile"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CARD_FROM_ALL_GRAVEYARDS,
+                // exiling a high cost card is good here
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Exile target card\$ from a graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicRemoveCardAction(
+                        card,
+                        MagicLocationType.Graveyard
+                    ));
+                    game.doAction(new MagicMoveCardAction(
+                        card,
+                        MagicLocationType.Graveyard,
+                        MagicLocationType.Exile
+                    ));
+                }
+            });
+        }
+    }
+]
