c555813758a725085d35c1a90066a9b1cf98212e
melvin <melvinzhang@gmail.com>
2013-05-28 15:10:37 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Carnifex_Demon.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicCondition.MINUS_COUNTER_CONDITION,
+            MagicConditionFactory.ManaCost("{B}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "-1/-1"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}"),
+                new MagicRemoveCounterEvent(source,MagicCounterType.MinusOne,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a -1/-1 counter on each other creature."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent creature=event.getPermanent();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(creature.getController(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicPermanent target : targets) {
+                if (target!=creature) {
+                    game.doAction(new MagicChangeCountersAction(target,MagicCounterType.MinusOne,1,true));
+                }
+            }
+        }
+    }
+]
