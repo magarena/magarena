49ef5acfd265ac05350f0b02da010b2adc0f572a
melvin <melvinzhang@gmail.com>
2013-05-28 16:37:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Captain_of_the_Watch.groovy
@@ -0,0 +1,46 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT, 
+        MagicTargetFilter.TARGET_SOLDIER_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicStatic(
+        MagicLayer.Ability, 
+        MagicTargetFilter.TARGET_SOLDIER_YOU_CONTROL
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Vigilance);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts three 1/1 white Soldier creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Soldier"),
+                3
+            ));
+        }        
+    }
+]
