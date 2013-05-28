157b9ee2f21b34da4e86e49ba0975c7268fe952b
melvin <melvinzhang@gmail.com>
2013-05-28 20:50:26 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Academy_Ruins.groovy
+++ release/Magarena/scripts/Academy_Ruins.groovy
@@ -1,40 +1,18 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.condition.MagicConditionFactory;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicPayManaCostEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTapEvent;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicGraveyardTargetPicker;
-
-public class Academy_Ruins {
-    public static final MagicPermanentActivation A = new MagicPermanentActivation( 
-        new MagicCondition[] {
+[
+    new MagicPermanentActivation( 
+        [
             MagicConditionFactory.ManaCost("{2}{U}"),
             MagicCondition.CAN_TAP_CONDITION,
-        },
+        ],
         new MagicActivationHints(MagicTiming.Main),
         "Card"
     ) {
         @Override
         public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{
+            return [
                 new MagicTapEvent(source),
                 new MagicPayManaCostEvent(source,"{1}{U}")
-            };
+            ];
         }
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
@@ -43,7 +21,7 @@ public class Academy_Ruins {
                 MagicTargetChoice.TARGET_ARTIFACT_CARD_FROM_GRAVEYARD,
                 new MagicGraveyardTargetPicker(false),
                 this,
-                "Put target artifact card$ from your graveyard on top of your library"
+                "Put target artifact card\$ from your graveyard on top of your library"
             );
         }
         @Override
@@ -59,5 +37,5 @@ public class Academy_Ruins {
                 }
             });
         }
-    };
-}
+    }
+]
