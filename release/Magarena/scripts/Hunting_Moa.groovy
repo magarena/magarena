1caeab41fcf65eafbc4de51886e44899ab77c429
melvin <melvinzhang@gmail.com>
2013-05-30 14:28:35 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Hunting_Moa.groovy
@@ -0,0 +1,56 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN puts a +1/+1 counter on target creature\$."
+            );
+        }    
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        1,
+                        true
+                    ));
+                }
+            });
+        }
+    },
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN puts a +1/+1 counter on target creature\$."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        1,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
