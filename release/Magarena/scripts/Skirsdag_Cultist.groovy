fb04642a526c5b0d854812a750ccee62f29a66f6
melvin <melvinzhang@gmail.com>
2013-05-30 13:30:59 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Skirsdag_Cultist.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.ONE_CREATURE_CONDITION,
+            MagicConditionFactory.ManaCost("{R}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Exile"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{R}"),
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(2),
+                this,
+                "SN deals 2 damage to target creature or player\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(),target,2);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
