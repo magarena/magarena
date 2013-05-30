e7eb43f7e00a45e34a65ad2ebeae0d9fe02d1ec9
melvin <melvinzhang@gmail.com>
2013-05-30 20:30:08 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sun_Titan.groovy
@@ -0,0 +1,35 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetCard(game,new MagicCardAction() {
+            public void doAction(final MagicCard card) {
+            game.doAction(new MagicReanimateAction(event.getPlayer(),card,MagicPlayCardAction.NONE));
+        }
+    });
+} as MagicEventAction
+
+def event = {
+    final MagicPermanent permanent ->
+    return new MagicEvent(
+        permanent,
+        MagicTargetChoice.TARGET_PERMANENT_CARD_CMC_LEQ_3_FROM_GRAVEYARD,
+        new MagicGraveyardTargetPicker(true),
+        this,
+        "Return target permanent card$ with converted mana cost 3 or less " + 
+        "from your graveyard to the battlefield."
+    );
+}
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return event(permanent);
+        }
+    },
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
+            return (permanent==attacker) ? event(permanent) : MagicEvent.NONE;
+        }
+    }
+]
