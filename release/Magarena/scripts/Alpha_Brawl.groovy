e0a61d5ae02597dbd49fecd23cf0f57762f3be65
melvin <melvinzhang@gmail.com>
2013-05-30 16:04:36 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Alpha_Brawl.groovy
+++ release/Magarena/scripts/Alpha_Brawl.groovy
@@ -1,39 +1,20 @@
-package magic.card;
-
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicSpellCardEvent;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicPowerTargetPicker;
-import magic.model.target.MagicTargetFilter;
-
-import java.util.Collection;
-
-public class Alpha_Brawl {
-    public static final MagicSpellCardEvent S = new MagicSpellCardEvent() {
+[
+    new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
-                    cardOnStack,
-                    MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
-                    MagicPowerTargetPicker.getInstance(),
-                    this,
-                    "Target creature$ an opponent controls deals damage equal to " +
-                    "its power to each other creature that player controls, then " +
-                    "each of those creatures deals damage equal to its power to that creature.");
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                MagicPowerTargetPicker.getInstance(),
+                this,
+                "Target creature\$ an opponent controls deals damage equal to " +
+                "its power to each other creature that player controls, then " +
+                "each of those creatures deals damage equal to its power to that creature."
+            );
         }
 
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 final MagicPlayer player = event.getPlayer();
                 public void doAction(final MagicPermanent permanent) {
@@ -58,5 +39,5 @@ public class Alpha_Brawl {
                 }
             });
         }
-    };
-}
+    }
+]
