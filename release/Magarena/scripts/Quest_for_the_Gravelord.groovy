89e2957d890d91bb1411f1a8c9a59b31920a12b5
melvin <melvinzhang@gmail.com>
2013-05-30 21:41:47 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Quest_for_the_Gravelord.groovy
@@ -0,0 +1,55 @@
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ChargeCountersAtLeast(3)],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,3),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a 5/5 black Zombie Giant creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Zombie Giant")));
+        }
+    },
+    new MagicWhenOtherPutIntoGraveyardFromPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_CHARGE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ put a quest counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.Charge,
+                    1,
+                    true
+                ));
+            }
+        }
+    }
+]
