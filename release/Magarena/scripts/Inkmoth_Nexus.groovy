3d7a38e13c44c2ccc68df3c6b3f851c3dd4bd60c
melvin <melvinzhang@gmail.com>
2013-05-30 21:28:46 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Inkmoth_Nexus.groovy
@@ -0,0 +1,53 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(1,1);
+    }
+};
+def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        flags.add(MagicAbility.Flying);
+        flags.add(MagicAbility.Infect);
+    }
+};
+def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags|MagicType.Artifact.getMask()|MagicType.Creature.getMask();
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        [
+            new MagicArtificialCondition(
+                MagicConditionFactory.ManaCost("{1}"),
+                MagicConditionFactory.ManaCost("{2}")
+            )
+        ],
+        new MagicActivationHints(MagicTiming.Animate),
+        "Animate"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this, 
+                "SN becomes a 1/1 Blinkmoth artifact creature with flying and infect until end of turn. " + 
+                "It's still a land."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,AB,ST));
+        }
+    }
+]
