4f8b56b1d5a6061caa03fb8fb34c11e1556eba47
melvin <melvinzhang@gmail.com>
2013-05-30 13:28:45 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Deranged_Outcast.groovy
@@ -0,0 +1,44 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{1}{G}"),
+            MagicCondition.ONE_HUMAN_CONDITION,
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Pump"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}{G}"),
+                new MagicSacrificePermanentEvent(
+                    source,
+                    MagicTargetChoice.SACRIFICE_HUMAN
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN puts two +1/+1 counters on target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        2,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
