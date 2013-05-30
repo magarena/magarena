5abae982aa35cff7a2731b009ae9656452be00cf
melvin <melvinzhang@gmail.com>
2013-05-30 13:32:16 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ballista_Squad.groovy
@@ -0,0 +1,45 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{X}{W}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{X}{W}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ATTACKING_OR_BLOCKING_CREATURE,
+                new MagicDamageTargetPicker(amount),
+                amount,
+                this,
+                "SN deals RN damage to target creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(
+                        event.getSource(),
+                        target,
+                        event.getRefInt()
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
