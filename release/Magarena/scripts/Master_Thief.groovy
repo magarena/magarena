8f56e201b4a822c7ee94198dea10f9c2b086c18e
melvin <melvinzhang@gmail.com>
2013-05-30 21:44:48 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Master_Thief.groovy
@@ -0,0 +1,55 @@
+def control = {
+    final int you ->
+    return new MagicStatic(MagicLayer.Control,filter) {
+        @Override
+        public MagicPlayer getController(
+                final MagicPermanent source, 
+                final MagicPermanent permanent, 
+                final MagicPlayer player) {
+            return source.getController();
+        }
+        @Override
+        public boolean condition(
+                final MagicGame game,
+                final MagicPermanent source,
+                final MagicPermanent target) {
+            if (you != source.getController().getIndex()) {
+                //remove this static after the update
+                game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+}
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_ARTIFACT,
+                MagicExileTargetPicker.create(),
+                this,
+                "Gain control of target artifact$ for as long as you control SN."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent perm) {
+                    final MagicPermanent source = event.getPermanent();
+                    final MagicTargetFilter<MagicPermanent> filter = new MagicTargetFilter.MagicPermanentTargetFilter(perm);
+                    final int you = source.getController().getIndex();
+                    game.doAction(new MagicAddStaticAction(source, control(you));
+                };
+            });
+        }
+    }
+]
