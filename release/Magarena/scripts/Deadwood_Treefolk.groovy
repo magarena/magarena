9d2867398c8ee6bef2b7e50693a678ecab6e1c0f
melvin <melvinzhang@gmail.com>
2013-05-30 20:46:59 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Deadwood_Treefolk.groovy
@@ -0,0 +1,35 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetCard(game,new MagicCardAction() {
+        public void doAction(final MagicCard targetCard) {
+            game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
+            game.doAction(new MagicMoveCardAction(targetCard,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+        }
+    });
+}
+
+def event = {
+    final MagicPermanent permanent ->
+    return new MagicEvent(
+        permanent,
+        MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+        new MagicGraveyardTargetPicker(false),
+        this,
+        "Return another target creature card\$ from your graveyard to your hand."
+    );
+}
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return event(permanent);
+        }
+    },
+    new MagicWhenLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent left) {
+            return (permanent == left) ? event(permanent) : MagicEvent.NONE; 
+        }
+    }
+]
