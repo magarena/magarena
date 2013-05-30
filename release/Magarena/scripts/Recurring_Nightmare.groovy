93f31ae1d42f3f691959bd2ea0eda1cc4d032b8b
melvin <melvinzhang@gmail.com>
2013-05-30 13:56:10 +0800

    converted java code to groovy script

---
+++ release/Magarena/scripts/Recurring_Nightmare.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.ONE_CREATURE_CONDITION,
+            MagicCondition.SORCERY_CONDITION,
+            MagicCondition.GRAVEYARD_CONTAINS_CREATURE
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Return"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    SACRIFICE_CREATURE
+                )
+                new MagicBouncePermanentEvent(
+                    source,
+                    source
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "Return target creature card\$ from your graveyard to the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard targetCard) {
+                    game.doAction(new MagicReanimateAction(event.getPlayer(),targetCard,MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
