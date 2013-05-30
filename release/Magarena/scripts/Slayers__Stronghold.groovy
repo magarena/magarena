ec78cdce7f23c94acecbbdacb032e770b6983d8a
melvin <melvinzhang@gmail.com>
2013-05-30 13:35:52 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Slayers__Stronghold.groovy
@@ -0,0 +1,47 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{R}{W}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{R}{W}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +2/+0 and gains " +
+                "vigilance and haste until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,2,0));
+                    game.doAction(new MagicSetAbilityAction(
+                        creature,
+                        MagicAbility.Vigilance
+                    ));
+                    game.doAction(new MagicSetAbilityAction(
+                        creature,
+                        MagicAbility.Haste
+                    ));
+                }
+            });
+        }
+    }
+]
