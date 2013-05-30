49ed3b5b656173ec0f5aca3b985161ff64991caf
melvin <melvinzhang@gmail.com>
2013-05-30 20:18:35 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Quest_for_Renewal.groovy
+++ release/Magarena/scripts/Quest_for_Renewal.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicUntapAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-import magic.model.trigger.MagicWhenBecomesTappedTrigger;
-
-import java.util.Collection;
-
-public class Quest_for_Renewal {
-    public static final MagicWhenBecomesTappedTrigger T1 = new MagicWhenBecomesTappedTrigger() {
+[
+    new MagicWhenBecomesTappedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
             return (tapped.isFriend(permanent) && 
@@ -25,15 +9,15 @@ public class Quest_for_Renewal {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.ADD_CHARGE_COUNTER,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a quest counter on SN."):
+                    "PN may\$ put a quest counter on SN."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeCountersAction(
                     event.getPermanent(),
@@ -43,9 +27,8 @@ public class Quest_for_Renewal {
                 ));
             }
         }
-    };
-    
-    public static final MagicAtUpkeepTrigger T2 = new MagicAtUpkeepTrigger() {
+    },
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return (permanent.isOpponent(upkeepPlayer) &&
@@ -53,13 +36,12 @@ public class Quest_for_Renewal {
                 new MagicEvent(
                     permanent,
                     this,
-                    "Untap all creatures you control."):
+                    "Untap all creatures you control."
+                ):
                 MagicEvent.NONE;
         }    
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             final Collection<MagicPermanent> targets =
                 game.filterPermanents(player,MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
@@ -67,5 +49,5 @@ public class Quest_for_Renewal {
                 game.doAction(new MagicUntapAction(target));
             }
         }
-    };
-}
+    }
+]
