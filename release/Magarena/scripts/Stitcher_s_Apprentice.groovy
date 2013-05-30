ba5668e1ab4bacd55f84d86115cad6165a2deed1
melvin <melvinzhang@gmail.com>
2013-05-30 20:34:17 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Stitcher_s_Apprentice.groovy
@@ -0,0 +1,46 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetPermanent(game,new MagicPermanentAction() {
+        public void doAction(final MagicPermanent permanent) {
+            game.doAction(new MagicSacrificeAction(permanent));
+        }
+    });
+} as MagicEventAction;
+
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{1}{U}")
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source,"{1}{U}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 2/2 blue Homunculus creature token " +
+                "onto the battlefield, then sacrifices a creature."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicPlayTokenAction(player,TokenCardDefinitions.get("Homunculus2")));
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                player,
+                MagicTargetChoice.SACRIFICE_CREATURE,
+                MagicSacrificeTargetPicker.create(),
+                action,
+                "Choose a creature to sacrifice\$."
+            ));
+        }
+    }
+]
