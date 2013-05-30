fa599468632e77fc4edb5ea25fee0ae56ece943c
melvin <melvinzhang@gmail.com>
2013-05-30 20:57:27 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Golem_Foundry.groovy
@@ -0,0 +1,49 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (permanent.isFriend(cardOnStack) && 
+                    cardOnStack.getCardDefinition().isArtifact()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_CHARGE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ put a charge counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,1,true));
+            }    
+        }       
+    },
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ChargeCountersAtLeast(3)],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token") {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,3)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 3/3 colorless Golem artifact creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Golem3")));
+        }
+    }
+]
