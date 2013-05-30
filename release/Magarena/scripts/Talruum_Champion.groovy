32aa36e6da796ea2cc39b3b82578b42aeb626256
melvin <melvinzhang@gmail.com>
2013-05-30 20:43:40 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Talruum_Champion.groovy
+++ release/Magarena/scripts/Talruum_Champion.groovy
@@ -1,27 +1,12 @@
-package magic.card;
+def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        flags.remove(MagicAbility.FirstStrike);
+    }
+};
 
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentList;
-import magic.model.action.MagicAddStaticAction;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-import magic.model.trigger.MagicWhenBlocksTrigger;
-
-import java.util.Set;
-
-public class Talruum_Champion {
-    private static final MagicStatic AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.remove(MagicAbility.FirstStrike);
-        }
-    };
-    
-    public static final MagicWhenBecomesBlockedTrigger T1 = new MagicWhenBecomesBlockedTrigger() {
+[    
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             if (permanent != attacker) {
@@ -31,7 +16,6 @@ public class Talruum_Champion {
             final MagicPermanentList plist = new MagicPermanentList(permanent.getBlockingCreatures());
             return new MagicEvent(
                 permanent,
-                permanent.getController(),
                 plist,
                 this,
                 plist.size() == 1 ?
@@ -41,34 +25,29 @@ public class Talruum_Champion {
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanentList plist = event.getRefPermanentList();
             for (final MagicPermanent blocker : plist) {
                 game.doAction(new MagicAddStaticAction(blocker,AB));
             }
         }
-    };
-    
-    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
+    },
+    new MagicWhenBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
             final MagicPermanent blocked = permanent.getBlockedCreature();
             return (permanent == blocker && blocked.isValid()) ?
                 new MagicEvent(
                     permanent,
-                    permanent.getController(),
                     blocked,
                     this,
-                    blocked + " loses first strike until end of turn."):
+                    "RN loses first strike until end of turn."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicAddStaticAction(event.getRefPermanent(),AB));
         }
-    };
-}
+    }
+]
