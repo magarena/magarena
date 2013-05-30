326fc69fe5ca513b5838565e7b00a25d9f4cf146
melvin <melvinzhang@gmail.com>
2013-05-30 21:38:41 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Nephalia_Smuggler.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicPermanentActivation( 
+        [
+            MagicConditionFactory.ManaCost("{3}{U}"),
+            MagicCondition.CAN_TAP_CONDITION,
+        ],
+        new MagicActivationHints(MagicTiming.Main),
+        "Card"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{3}{U}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final MagicTargetFilter<MagicPermanent> targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL,source);
+            final MagicTargetChoice targetChoice = new MagicTargetChoice(
+                    targetFilter,true,MagicTargetHint.None,"another creature to exile");
+            return new MagicEvent(
+                source,
+                targetChoice,
+                MagicBounceTargetPicker.getInstance(),
+                this,
+                "Exile another target creature you control\$, then " +
+                "return that card to the battlefield under your control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
+                    game.doAction(new MagicRemoveCardAction(creature.getCard(),MagicLocationType.Exile));
+                    game.doAction(new MagicPlayCardAction(creature.getCard(),event.getPlayer(),MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    }
+]
