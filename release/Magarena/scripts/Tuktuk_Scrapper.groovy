656e2982db54f6f282ac9e8b033712e230424c80
melvin <melvinzhang@gmail.com>
2013-05-30 20:45:08 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Tuktuk_Scrapper.groovy
+++ release/Magarena/scripts/Tuktuk_Scrapper.groovy
@@ -1,22 +1,5 @@
-package magic.card;
-
-import magic.model.MagicCard;
-import magic.model.MagicDamage;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicSubType;
-import magic.model.action.MagicDealDamageAction;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-public class Tuktuk_Scrapper {
-    public static final MagicWhenOtherComesIntoPlayTrigger T = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isFriend(permanent) &&
@@ -28,15 +11,14 @@ public class Tuktuk_Scrapper {
                     ),
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "PN may$ destroy target artifact$."
+                    "PN may\$ destroy target artifact\$. "
+                    "If that artifact is put into a graveyard this way, SN deals damage to that artifact's controller equal to the number of Allies you control."
                 ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 event.processTargetPermanent(game,new MagicPermanentAction() {
                     public void doAction(final MagicPermanent target) {
@@ -63,5 +45,5 @@ public class Tuktuk_Scrapper {
                 });
             }            
         }        
-    };
-}
+    }
+}]
