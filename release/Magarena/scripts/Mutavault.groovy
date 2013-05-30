bbf3cbec8d19930cba2ab1e639630c328a0f2836
melvin <melvinzhang@gmail.com>
2013-05-30 20:26:06 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mutavault.groovy
@@ -0,0 +1,51 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(2,2);
+    }
+};
+
+def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public void modSubTypeFlags(
+            final MagicPermanent permanent,
+            final Set<MagicSubType> flags) {
+        flags.addAll(MagicSubType.ALL_CREATURES);
+    }
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags|MagicType.Creature.getMask();
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        [
+            new MagicArtificialCondition(
+                MagicConditionFactory.ManaCost("{1}"),
+                MagicConditionFactory.ManaCost("{2}")
+            )
+        ],
+        new MagicActivationHints(MagicTiming.Animate),
+        "Animate"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN becomes a 2/2 creature with all creature types until end of turn. " + 
+                "It's still a land."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,ST));
+        }
+    }
+]
