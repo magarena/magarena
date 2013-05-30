1b5b2ce47480ae18995d6da619c163c39cc553b0
melvin <melvinzhang@gmail.com>
2013-05-30 20:31:53 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Sheoldred__Whispering_One.groovy
@@ -0,0 +1,50 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                    new MagicGraveyardTargetPicker(true),
+                    this,
+                    "Return target creature card\$ from your graveyard to the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicReanimateAction(event.getPlayer(),card,MagicPlayCardAction.NONE));
+                }
+            });
+        }
+    },
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            final MagicPlayer player=permanent.getController();
+            return (player != upkeepPlayer) ? 
+                new MagicEvent(
+                    permanent,
+                    upkeepPlayer,
+                    this,
+                    "PN sacrifices a creature."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer opponent=event.getPlayer();
+            if (opponent.controlsPermanentWithType(MagicType.Creature)) {
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    event.getSource(),
+                    opponent,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                ));
+            }
+        }
+    }
+]
