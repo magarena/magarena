947b11f098143d11f407c0cf800e57d9b5295822
melvin <melvinzhang@gmail.com>
2013-05-30 20:41:50 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Brimstone_Mage.groovy
@@ -0,0 +1,50 @@
+[
+    new MagicStatic(MagicLayer.SetPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final int charges = permanent.getCounters(MagicCounterType.Charge);
+            if (charges >= 3) {
+                pt.set(2,4);
+            } else if (charges >= 1) {
+                pt.set(2,3);
+            }
+        }        
+    },
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ChargeCountersAtLeast(1),
+            MagicCondition.CAN_TAP_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final int amount = source.getCounters(MagicCounterType.Charge) >= 3 ? 3:1;
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(amount),
+                amount,
+                this,
+                "SN deals RN damage to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage = new MagicDamage(event.getSource(), target, event.getRefInt());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
