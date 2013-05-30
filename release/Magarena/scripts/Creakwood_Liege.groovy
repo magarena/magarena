c28d7680f64cbb9d26fc1738a5832a48b950f1d9
melvin <melvinzhang@gmail.com>
2013-05-30 14:01:17 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Creakwood_Liege.groovy
+++ release/Magarena/scripts/Creakwood_Liege.groovy
@@ -1,23 +1,8 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicAtUpkeepTrigger;
-
-
-public class Creakwood_Liege {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
-        MagicTargetFilter.TARGET_BLACK_CREATURE_YOU_CONTROL) {
+        MagicTargetFilter.TARGET_BLACK_CREATURE_YOU_CONTROL
+    ) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
@@ -26,10 +11,11 @@ public class Creakwood_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT, 
-        MagicTargetFilter.TARGET_GREEN_CREATURE_YOU_CONTROL) {
+        MagicTargetFilter.TARGET_GREEN_CREATURE_YOU_CONTROL
+    ) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
@@ -38,8 +24,8 @@ public class Creakwood_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicAtUpkeepTrigger T = new MagicAtUpkeepTrigger() {
+    },
+    new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
@@ -48,19 +34,19 @@ public class Creakwood_Liege {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.PLAY_TOKEN,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a 1/1 black and green " +
-                    "Worm creature token onto the battlefield."):
+                    "PN may\$ put a 1/1 black and green " +
+                    "Worm creature token onto the battlefield."
+                ):
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Worm")));
             }
         }        
-    };
-}
+    }
+]
