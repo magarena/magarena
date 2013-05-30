0675c38a162ca02a1c76844124f062720314a8d8
melvin <melvinzhang@gmail.com>
2013-05-30 21:47:02 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Mikaeus__the_Lunarch.groovy
@@ -0,0 +1,61 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.CAN_TAP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Add") {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a +1/+1 counter on SN."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
+        }
+    },
+    new MagicPermanentActivation( 
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicCondition.PLUS_COUNTER_CONDITION,
+        ],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Remove"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicRemoveCounterEvent(source,MagicCounterType.PlusOne,1)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put a +1/+1 counter on each other creature you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent creature = event.getPermanent();
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(creature.getController(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                if (target != creature) {
+                    game.doAction(new MagicChangeCountersAction(target,MagicCounterType.PlusOne,1,true));
+                }
+            }
+        }
+    }
+]
