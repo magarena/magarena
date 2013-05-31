1f218900e705c0f6d790383aa6297a3da23dd2a2
melvin <melvinzhang@gmail.com>
2013-05-31 13:00:55 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Graveyard_Shovel.groovy
@@ -0,0 +1,57 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetCard(game,new MagicCardAction() {
+        public void doAction(final MagicCard card) {
+            game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+            game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.Exile));
+            if (card.getCardDefinition().isCreature()) {
+                game.doAction(new MagicChangeLifeAction(event.getRefPlayer(),2));
+            }
+        }
+    });
+} as MagicEventAction
+
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{2}")
+        ],
+        new MagicActivationHints(MagicTiming.Main),
+        "Exile"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source,"{2}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ exiles a card from his or her graveyard. " +
+                "If it's a creature card, PN gains 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer targetPlayer) {
+                    if (targetPlayer.getGraveyard().size() > 0) {
+                        final MagicPlayer player = event.getPlayer();
+                        game.addEvent(new MagicEvent(
+                            event.getSource(),
+                            targetPlayer,
+                            MagicTargetChoice.TARGET_CARD_FROM_GRAVEYARD,
+                            new MagicGraveyardTargetPicker(true),
+                            player,
+                            action,
+                            "PN exiles a card\$ from his or her graveyard."
+                        ));
+                    }
+                }
+            });
+        } 
+    }
+]
