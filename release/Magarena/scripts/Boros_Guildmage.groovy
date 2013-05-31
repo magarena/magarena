d70e8d69b98b3f542b3c6d95c34775527975034b
melvin <melvinzhang@gmail.com>
2013-05-31 10:54:01 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Boros_Guildmage.groovy
+++ release/Magarena/scripts/Boros_Guildmage.groovy
@@ -1,33 +1,13 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicGame;
-import magic.model.MagicManaCost;
-import magic.model.MagicPayedCost;
-import magic.model.MagicPermanent;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.condition.MagicCondition;
-import magic.model.condition.MagicConditionFactory;
-import magic.model.event.MagicActivationHints;
-import magic.model.event.MagicEvent;
-import magic.model.event.MagicPayManaCostEvent;
-import magic.model.event.MagicPermanentActivation;
-import magic.model.event.MagicTiming;
-import magic.model.target.MagicFirstStrikeTargetPicker;
-import magic.model.target.MagicHasteTargetPicker;
-
-public class Boros_Guildmage {
-    public static final MagicPermanentActivation A1 = new MagicPermanentActivation(
-        new MagicCondition[]{MagicConditionFactory.ManaCost("{1}{R}")},
+[
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}{R}")],
         new MagicActivationHints(MagicTiming.Pump,true),
         "Haste"
     ) {
 
         @Override
         public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{new MagicPayManaCostEvent(source,"{1}{R}")};
+            return [new MagicPayManaCostEvent(source,"{1}{R}")];
         }
 
         @Override
@@ -37,31 +17,28 @@ public class Boros_Guildmage {
                 MagicTargetChoice.POS_TARGET_CREATURE,
                 MagicHasteTargetPicker.create(),
                 this,
-                "Target creature$ gains haste until end of turn."
+                "Target creature\$ gains haste until end of turn."
             );
         }
 
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Haste));
                 }
             });
         }
-    };
-
-    public static final MagicPermanentActivation A2 = new MagicPermanentActivation(
-        new MagicCondition[]{MagicConditionFactory.ManaCost("{1}{W}")},
+    },
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{1}{W}")],
         new MagicActivationHints(MagicTiming.Block,true),
         "First strike"
     ) {
 
         @Override
         public MagicEvent[] getCostEvent(final MagicPermanent source) {
-            return new MagicEvent[]{new MagicPayManaCostEvent(source,"{1}{W}")};
+            return [new MagicPayManaCostEvent(source,"{1}{W}")];
         }
 
         @Override
@@ -71,18 +48,16 @@ public class Boros_Guildmage {
                 MagicTargetChoice.POS_TARGET_CREATURE,
                 MagicFirstStrikeTargetPicker.create(),
                 this,
-                "Target creature$ gains first strike until end of turn."
+                "Target creature\$ gains first strike until end of turn."
             );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,new MagicPermanentAction() {
                 public void doAction(final MagicPermanent creature) {
                     game.doAction(new MagicSetAbilityAction(creature,MagicAbility.FirstStrike));
                 }
             });
         }    
-    };
-}
+    }
+]
