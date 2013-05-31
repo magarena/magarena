03d9b2f64677ad21b81b9d3ae49be693854a21c3
melvin <melvinzhang@gmail.com>
2013-05-31 12:03:43 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Rakdos_Guildmage.groovy
@@ -0,0 +1,60 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicCondition.HAS_CARD_CONDITION,
+            MagicConditionFactory.ManaCost("{3}{B}")
+        ],
+        new MagicActivationHints(MagicTiming.Removal,true),
+        "-2/-2"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{3}{B}"),
+                new MagicDiscardEvent(source,source.getController(),1,false)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(2,2),
+                this,
+                "Target creature\$ gets -2/-2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-2,-2));
+                }
+            });
+        }
+    },
+    new MagicPermanentActivation(
+        [MagicConditionFactory.ManaCost("{3}{R}")],
+        new MagicActivationHints(MagicTiming.Token,true),
+        "Token"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{3}{R}")];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 2/1 red Goblin creature token with haste onto the battlefield. Exile it at end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            final MagicCard card=MagicCard.createTokenCard(TokenCardDefinitions.get("Goblin2"),player);
+            game.doAction(new MagicPlayCardAction(card,player,MagicPlayCardAction.REMOVE_AT_END_OF_TURN));
+        }
+    }
+]
