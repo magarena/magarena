471e286f32d40a669238f319c135aae4e822078b
melvin <melvinzhang@gmail.com>
2013-05-31 13:52:12 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Deathbringer_Liege.groovy
+++ release/Magarena/scripts/Deathbringer_Liege.groovy
@@ -1,25 +1,5 @@
-package magic.card;
-
-import magic.model.MagicColor;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPowerToughness;
-import magic.model.action.MagicDestroyAction;
-import magic.model.action.MagicPermanentAction;
-import magic.model.action.MagicTapAction;
-import magic.model.choice.MagicMayChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.event.MagicEvent;
-import magic.model.mstatic.MagicLayer;
-import magic.model.mstatic.MagicStatic;
-import magic.model.stack.MagicCardOnStack;
-import magic.model.target.MagicDestroyTargetPicker;
-import magic.model.target.MagicTapTargetPicker;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenOtherSpellIsCastTrigger;
-
-public class Deathbringer_Liege {
-    public static final MagicStatic S1 = new MagicStatic(
+[
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_BLACK_CREATURE_YOU_CONTROL) {
         @Override
@@ -30,8 +10,8 @@ public class Deathbringer_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicStatic S2 = new MagicStatic(
+    },
+    new MagicStatic(
         MagicLayer.ModPT, 
         MagicTargetFilter.TARGET_WHITE_CREATURE_YOU_CONTROL) {
         @Override
@@ -42,8 +22,8 @@ public class Deathbringer_Liege {
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
-    };
-    public static final MagicWhenOtherSpellIsCastTrigger T = new MagicWhenOtherSpellIsCastTrigger() {
+    },
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return (permanent.isFriend(cardOnStack) && 
@@ -55,7 +35,7 @@ public class Deathbringer_Liege {
                     ),
                     new MagicDestroyTargetPicker(false),
                     this,
-                    "PN may$ destroy target creature$ if it's tapped."
+                    "PN may\$ destroy target creature\$ if it's tapped."
                 ):
                 MagicEvent.NONE;
         }
@@ -72,9 +52,8 @@ public class Deathbringer_Liege {
                 });
             }
         }
-    };
-    
-    public static final MagicWhenOtherSpellIsCastTrigger T2 = new MagicWhenOtherSpellIsCastTrigger() {
+    },
+    new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return (permanent.isFriend(cardOnStack) && 
@@ -86,7 +65,7 @@ public class Deathbringer_Liege {
                     ),
                     new MagicTapTargetPicker(true,false),
                     this,
-                    "PN may$ tap target creature$."
+                    "PN may\$ tap target creature\$."
                 ):
                 MagicEvent.NONE;
         }
@@ -100,5 +79,5 @@ public class Deathbringer_Liege {
                 });
             }
         }
-    };
-}
+    }
+]
