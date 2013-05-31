40600b8f90f627922b69518d52245dfe9651395e
melvin <melvinzhang@gmail.com>
2013-05-31 13:13:46 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ezuri__Renegade_Leader.groovy
@@ -0,0 +1,71 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump,false),
+        "Regen"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{G}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final MagicTargetFilter<MagicPermanent> targetFilter = new MagicTargetFilter.MagicOtherPermanentTargetFilter(
+                    MagicTargetFilter.TARGET_ELF,source);
+            final MagicTargetChoice targetChoice = new MagicTargetChoice(
+                    targetFilter,true,MagicTargetHint.Positive,"another target Elf");
+            return new MagicEvent(
+                source,
+                targetChoice,
+                MagicRegenerateTargetPicker.getInstance(),
+                this,
+                "Regenerate another target Elf\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRegenerateAction(creature));
+                }
+            });
+        }
+    },
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{2}{G}{G}{G}")
+        ],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Pump"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{2}{G}{G}{G}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Elf creatures PN controls get +3/+3 and gain trample until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                    game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ELF_YOU_CONTROL);
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature,3,3));
+                game.doAction(new MagicSetAbilityAction(creature,MagicAbility.Trample));
+            }
+        }
+    }
+]
