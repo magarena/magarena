8cbb86f11f65a9e77e5b5b9f73d587ccc302df6d
melvin <melvinzhang@gmail.com>
2013-05-31 10:35:20 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Ghost_Council_of_Orzhova.groovy
@@ -0,0 +1,51 @@
+[
+    new MagicPermanentActivation(
+        [
+            MagicConditionFactory.ManaCost("{1}"),
+            MagicCondition.ONE_CREATURE_CONDITION
+        ],
+        new MagicActivationHints(MagicTiming.Removal,false,1),
+        "Exile"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}"),
+                new MagicPlayAbilityEvent(source),
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Exile SN. Return it to the battlefield under its owner's control at end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicExileUntilEndOfTurnAction(event.getPermanent()));
+        }
+    },
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Target opponent\$ loses 1 life and PN gains 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicChangeLifeAction(player,-1));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+                }
+            });
+        }        
+    }
+]
