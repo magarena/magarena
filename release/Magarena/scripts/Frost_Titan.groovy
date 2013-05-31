442325520549233021512772442b123d7a733296
melvin <melvinzhang@gmail.com>
2013-05-31 13:21:25 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Frost_Titan.groovy
@@ -0,0 +1,58 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetPermanent(game,new MagicPermanentAction() {
+        public void doAction(final MagicPermanent perm) {
+            game.doAction(new MagicTapAction(perm,true));
+            game.doAction(new MagicChangeStateAction(perm,MagicPermanentState.DoesNotUntapDuringNext,true));
+        }
+    });
+}
+
+def event = {
+    final MagicPermanent permanent ->
+    return new MagicEvent(
+        permanent,
+        MagicTargetChoice.TARGET_PERMANENT,
+        this,
+        "Tap target permanent\$. It doesn't untap during its controller's next untap step."
+    );
+}
+
+[
+    //counter opponent spell or ability unless its controller pay {2}
+    new MagicWhenTargetedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicItemOnStack target) {
+            return (target.containsInChoiceResults(permanent) &&
+                    target.getController() != permanent.getController()) ?
+                new MagicEvent(
+                    permanent,
+                    target,
+                    this,
+                    "Counter spell or ability\$ unless its controller pays {2}."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicSource source = event.getSource();
+            final MagicItemOnStack target = event.getRefItemOnStack();
+            game.addEvent(new MagicCounterUnlessEvent(source,target,MagicManaCost.create("{2}")));
+        }
+    },
+    //tap target permanent. It doesn't untap during its controller's next untap step.
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return event(permanent);
+        }
+    },
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (permanent == creature) ?
+                event(permanent) : MagicEvent.NONE;
+        }
+    }
+]
