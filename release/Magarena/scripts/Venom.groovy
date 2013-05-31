5c2448e344f490796a9ab029a0961b77f96b9734
melvin <melvinzhang@gmail.com>
2013-05-31 10:32:16 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Venom.groovy
+++ release/Magarena/scripts/Venom.groovy
@@ -1,17 +1,5 @@
-package magic.card;
-
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicPermanentList;
-import magic.model.MagicPermanentState;
-import magic.model.MagicSubType;
-import magic.model.action.MagicChangeStateAction;
-import magic.model.event.MagicEvent;
-import magic.model.trigger.MagicWhenBecomesBlockedTrigger;
-import magic.model.trigger.MagicWhenBlocksTrigger;
-
-public class Venom {
-    public static final MagicWhenBecomesBlockedTrigger T1 = new MagicWhenBecomesBlockedTrigger() {
+[
+    new MagicWhenBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             final MagicPermanent enchantedCreature = permanent.getEnchantedCreature();
@@ -27,7 +15,6 @@ public class Venom {
             return !plist.isEmpty() ?
                 new MagicEvent(
                     permanent,
-                    permanent.getController(),
                     plist,
                     this,
                     plist.size() == 1 ?
@@ -38,17 +25,14 @@ public class Venom {
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanentList plist = event.getRefPermanentList();
             for (final MagicPermanent blocker : plist) {
                 game.doAction(new MagicChangeStateAction(blocker,MagicPermanentState.DestroyAtEndOfCombat,true));
             }
         }
-    };
-    
-    public static final MagicWhenBlocksTrigger T2 = new MagicWhenBlocksTrigger() {
+    },
+    new MagicWhenBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
             final MagicPermanent enchantedCreature = permanent.getEnchantedCreature();
@@ -65,11 +49,9 @@ public class Venom {
                 MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent creature = event.getRefPermanent();
             game.doAction(new MagicChangeStateAction(creature,MagicPermanentState.DestroyAtEndOfCombat,true));
         }
-    };
-}
+    }
+]
