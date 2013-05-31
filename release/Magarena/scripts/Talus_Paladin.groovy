8d5427b3866fbe43d6a86e2622f2fa94f497becb
melvin <melvinzhang@gmail.com>
2013-05-31 10:33:02 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Talus_Paladin.groovy
+++ release/Magarena/scripts/Talus_Paladin.groovy
@@ -1,21 +1,5 @@
-package magic.card;
-
-import magic.model.MagicAbility;
-import magic.model.MagicCounterType;
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
-import magic.model.MagicSubType;
-import magic.model.action.MagicChangeCountersAction;
-import magic.model.action.MagicSetAbilityAction;
-import magic.model.choice.MagicSimpleMayChoice;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenOtherComesIntoPlayTrigger;
-
-import java.util.Collection;
-
-public class Talus_Paladin {
-    public static final MagicWhenOtherComesIntoPlayTrigger T1 = new MagicWhenOtherComesIntoPlayTrigger() {
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isFriend(permanent) &&
@@ -25,17 +9,17 @@ public class Talus_Paladin {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.PUMP,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ have Ally creatures he or " +
-                    "she controls gain lifelink until end of turn.") :
+                    "PN may\$ have Ally creatures he or " +
+                    "she controls gain lifelink until end of turn."
+                ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 final Collection<MagicPermanent> targets =
                         game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
@@ -44,9 +28,8 @@ public class Talus_Paladin {
                 }
             }            
         }        
-    };
-    
-    public static final MagicWhenOtherComesIntoPlayTrigger T2 = new MagicWhenOtherComesIntoPlayTrigger() {
+    },
+    new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isFriend(permanent) &&
@@ -56,23 +39,24 @@ public class Talus_Paladin {
                     new MagicSimpleMayChoice(
                         MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
                         1,
-                        MagicSimpleMayChoice.DEFAULT_YES),
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "PN may$ put a +1/+1 counter on SN.") :
+                    "PN may\$ put a +1/+1 counter on SN."
+                ) :
                 MagicEvent.NONE;
         }
         
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 game.doAction(new MagicChangeCountersAction(
-                        event.getPermanent(),
-                        MagicCounterType.PlusOne,
-                        1,
-                        true));
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
             }            
         }        
-    };
-}
+    }
+]
