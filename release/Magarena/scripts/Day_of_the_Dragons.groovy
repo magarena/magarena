580c67db3f6285d0bbd50d0cfb270290749abaf2
melvin <melvinzhang@gmail.com>
2013-05-31 12:59:33 +0800

    convert from java code to groovy code

--- release/Magarena/scripts/Day_of_the_Dragons.groovy
+++ release/Magarena/scripts/Day_of_the_Dragons.groovy
@@ -1,37 +1,16 @@
-package magic.card;
-
-import magic.data.TokenCardDefinitions;
-import magic.model.MagicCardList;
-import magic.model.MagicGame;
-import magic.model.MagicLocationType;
-import magic.model.MagicPermanent;
-import magic.model.MagicPlayer;
-import magic.model.action.MagicExileUntilThisLeavesPlayAction;
-import magic.model.action.MagicPlayTokenAction;
-import magic.model.action.MagicReturnExiledUntilThisLeavesPlayAction;
-import magic.model.action.MagicSacrificeAction;
-import magic.model.event.MagicEvent;
-import magic.model.target.MagicTargetFilter;
-import magic.model.trigger.MagicWhenComesIntoPlayTrigger;
-import magic.model.trigger.MagicWhenLeavesPlayTrigger;
-
-import java.util.Collection;
-
-public class Day_of_the_Dragons {
-    public static final MagicWhenComesIntoPlayTrigger T1 = new MagicWhenComesIntoPlayTrigger() {
+[
+    new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
             return new MagicEvent(
-                    permanent,
-                    player,
-                    this,
-                    "Exile all creatures you control. Then put that many 5/5 " +
-                    "red Dragon creature tokens with flying onto the battlefield.");
+                permanent,
+                this,
+                "Exile all creatures you control. Then put that many 5/5 " +
+                "red Dragon creature tokens with flying onto the battlefield."
+            );
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets =
                     game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
             for (final MagicPermanent target : targets) {
@@ -40,16 +19,14 @@ public class Day_of_the_Dragons {
                     target
                 ));
             }
-            int amount = targets.size();
-            for (;amount>0;amount--) {
-                game.doAction(new MagicPlayTokenAction(
-                        event.getPlayer(),
-                        TokenCardDefinitions.get("Dragon5")));
-            }
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Dragon5"),
+                targets.size()
+            ));
         }
-    };
-    
-    public static final MagicWhenLeavesPlayTrigger T2 = new MagicWhenLeavesPlayTrigger() {
+    },
+    new MagicWhenLeavesPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent left) {
             if (permanent == left &&
@@ -67,9 +44,7 @@ public class Day_of_the_Dragons {
             return MagicEvent.NONE;
         }
         @Override
-        public void executeEvent(
-                final MagicGame game,
-                final MagicEvent event) {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets =
                     game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_DRAGON_YOU_CONTROL);
             for (final MagicPermanent target : targets) {
@@ -77,9 +52,10 @@ public class Day_of_the_Dragons {
             }
             final MagicPermanent permanent = event.getPermanent();
             game.doAction(new MagicReturnExiledUntilThisLeavesPlayAction(
-                    permanent,
-                    MagicLocationType.Play,
-                    event.getPlayer()));
+                permanent,
+                MagicLocationType.Play,
+                event.getPlayer()
+            ));
         }
-    };
-}
+    }
+]
