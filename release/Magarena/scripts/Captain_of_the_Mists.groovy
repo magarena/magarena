d0029d595fe2334dbee01100749674d3b1317306
melvin <melvinzhang@gmail.com>
2013-05-31 13:05:11 +0800

    convert from java code to groovy code

---
+++ release/Magarena/scripts/Captain_of_the_Mists.groovy
@@ -0,0 +1,74 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent otherPermanent) {
+            return (otherPermanent != permanent &&
+                    otherPermanent.isCreature() && 
+                    otherPermanent.isFriend(permanent) &&
+                    otherPermanent.hasSubType(MagicSubType.Human)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Untap SN.") :
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicUntapAction(event.getPermanent()));            
+        }        
+    },
+    new MagicTapCreatureActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{1}{U}")
+        ],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{1}{U}")
+            ];
+        }
+    },
+    new MagicPermanentActivation(
+        [
+            MagicCondition.CAN_TAP_CONDITION,
+            MagicConditionFactory.ManaCost("{1}{U}")
+        ],
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Untap"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{1}{U}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                new MagicTapTargetPicker(false,true),
+                this,
+                "Untap target creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicUntapAction(creature));
+                }
+            });
+        }
+    }
+]
