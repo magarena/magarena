9def61553c13ccb5526e0748162c115c281514ea
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-03 13:31:00 +0800

    added Crimson Muckwader

---
+++ release/Magarena/scripts/Crimson_Muckwader.groovy
@@ -0,0 +1,10 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().controlsPermanent(MagicSubType.Swamp)) {
+                pt.add(1,1);
+            }
+        }
+    }
+]
