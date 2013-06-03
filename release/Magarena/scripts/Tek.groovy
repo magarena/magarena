7f26c3398675c7b5f90fd38aa4ae45054bb7f48f
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-03 17:00:26 +0800

    added Tek

---
+++ release/Magarena/scripts/Tek.groovy
@@ -0,0 +1,46 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().controlsPermanent(MagicSubType.Plains)) {
+                pt.add(0,2);
+            }
+        }
+    },
+
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().controlsPermanent(MagicSubType.Swamp)) {
+                pt.add(2,0);
+            }
+        }
+    },
+
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            if (permanent.getController().controlsPermanent(MagicSubType.Island)) {
+        		flags.add(MagicAbility.Flying);
+            }
+        }
+    },
+
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            if (permanent.getController().controlsPermanent(MagicSubType.Mountain)) {
+        		flags.add(MagicAbility.FirstStrike);
+            }
+        }
+    },
+
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            if (permanent.getController().controlsPermanent(MagicSubType.Forest)) {
+	        	flags.add(MagicAbility.Trample);
+            }
+        }
+    }
+]
