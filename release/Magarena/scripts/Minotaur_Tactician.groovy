d3ff3fcf48e757bae228b3ec433e5f4222ff17f4
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-03 13:32:35 +0800

    added Minotaur Tactician

---
+++ release/Magarena/scripts/Minotaur_Tactician.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().controlsPermanent(MagicColor.White)) {
+                pt.add(1,1);
+            }        
+        }
+    },
+
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (source.getController().controlsPermanent(MagicColor.Blue)) {
+                pt.add(1,1);
+            }        
+        }
+    }
+]
