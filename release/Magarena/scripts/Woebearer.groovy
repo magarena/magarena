282d722f18b979ae71669ef6c3415c296bf108e9
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-03 13:35:52 +0800

    added Woebearer

---
+++ release/Magarena/scripts/Woebearer.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return (damage.getSource() == permanent &&
+                    damage.isCombat() &&
+                    damage.getTarget().isPlayer()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        MagicTargetChoice.TARGET_ARTIFACT_CARD_FROM_GRAVEYARD
+                    ),
+                    new MagicGraveyardTargetPicker(false),
+                    this,
+                    "PN may\$ return target artifact card\$ from " +
+                    "his or her graveyard to his or her hand."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetCard(game,new MagicCardAction() {
+                    public void doAction(final MagicCard card) {
+                        game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                        game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+                    }
+                });
+            }
+        }
+    }
+]
