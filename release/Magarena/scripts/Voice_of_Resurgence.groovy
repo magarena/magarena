44edb5343c98405659425954b20614aed049e0a3
melvin <melvinzhang@gmail.com>
2013-06-08 16:41:58 +0800

    improved script

--- release/Magarena/scripts/Voice_of_Resurgence.groovy
+++ release/Magarena/scripts/Voice_of_Resurgence.groovy
@@ -1,30 +1,26 @@
-def action = {
-    final MagicGame game, final MagicEvent event ->
-    game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("GW Elemental")));
-} as MagicEventAction
-
-def getEvent = {
-    final MagicPermanent permanent ->
-    return new MagicEvent(
-        permanent,
-        action,
-        "PN puts a green and white Elemental creature token onto the battlefield with " +
-        "\"This creature's power and toughness are each equal to the number of creatures you control.\""
-    );
+def WhenDies = new MagicWhenDiesTrigger() {
+    @Override
+    public MagicEvent getEvent(final MagicPermanent permanent) {
+        return new MagicEvent(
+            permanent,
+            this,
+            "PN puts a green and white Elemental creature token onto the battlefield with " +
+            "\"This creature's power and toughness are each equal to the number of creatures you control.\""
+        );
+    }
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("GW Elemental")));
+    }
 }
 
 [
-    new MagicWhenDiesTrigger() {
-        @Override
-        public MagicEvent getEvent(final MagicPermanent permanent) {
-            return getEvent(permanent);
-        }
-    },
+    WhenDies,
     new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
             return permanent.isEnemy(cardOnStack) && permanent.isController(game.getTurnPlayer()) ?
-                getEvent(permanent) : MagicEvent.NONE;
+                WhenDies.getEvent(permanent) : MagicEvent.NONE;
         }
     }
 ]
