73e99e27c2b6b9da88835d3aea5c300ed8cdc691
melvin <melvinzhang@gmail.com>
2013-06-08 18:18:38 +0800

    added Eyes of the Wisent

---
+++ release/Magarena/scripts/Eyes_of_the_Wisent.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return permanent.isEnemy(cardOnStack) && permanent.isController(game.getTurnPlayer()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    this,
+                    "PN may\$ put a 4/4 green Elemental creature token onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("G Elemental")));
+            }
+        }
+    }
+]
