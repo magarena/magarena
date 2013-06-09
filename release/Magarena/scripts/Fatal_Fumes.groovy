dd6a1736929a6b9ca3d1628161c86a9b09bd4ad9
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 11:52:05 +0800

    added Fatal Fumes

---
+++ release/Magarena/scripts/Fatal_Fumes.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(4,2),
+                this,
+                "Target creature\$ gets -4/-2 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeTurnPTAction(creature,-4,-2));
+                }
+            });
+        }
+    }
+]
