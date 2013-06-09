8e9b5585c0cfe87607aad9fd95bf539fc12c31a0
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 12:08:11 +0800

    added Punish Ignorance

---
+++ release/Magarena/scripts/Punish_Ignorance.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. Its controller loses 3 life and PN gains 3 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCounterItemOnStackAction(targetSpell));
+                    game.doAction(new MagicChangeLifeAction(targetSpell.getController(),-3));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),3));
+                }
+            });
+        }
+    }
+]
