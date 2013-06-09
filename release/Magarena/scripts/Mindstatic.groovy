6fa911b1a73abc8c6842ce38b3518d94bbc3726d
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 12:01:50 +0800

    added Mindstatic

---
+++ release/Magarena/scripts/Mindstatic.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {6}."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.create("{6}")));
+                }
+            });
+        }
+    }
+]
