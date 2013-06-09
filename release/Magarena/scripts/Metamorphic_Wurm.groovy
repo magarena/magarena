44a15e386b94ae2d7049b0833a4d035501eb16ad
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 12:08:58 +0800

    added Metamorphic Wurm

---
+++ release/Magarena/scripts/Metamorphic_Wurm.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+            if (MagicCondition.THRESHOLD_CONDITION.accept(permanent)) {
+                pt.add(4,4);
+            }
+        }
+    }
+]
