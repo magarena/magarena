6ce3c8ed87818cd592f7b9ec380c7c866d110779
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 12:04:49 +0800

    added Thoughtflare

---
+++ release/Magarena/scripts/Thoughtflare.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Draw four cards, then discard two cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player=event.getPlayer();
+            game.doAction(new MagicDrawAction(player,4));
+            game.addEvent(new MagicDiscardEvent(event.getSource(),player,2,false));
+        }
+    }
+]
