512189eb5b41d8101bd19305c486e5224f3c21a3
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 11:51:02 +0800

    added Celestial Ancient

---
+++ release/Magarena/scripts/Celestial_Ancient.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return (permanent.isFriend(spell) && 
+                    spell.getCardDefinition().isEnchantment()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a +1/+1 counter on each creature he or she controls."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+            );
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicChangeCountersAction(
+                    target,
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }
+        }
+    }
+]
