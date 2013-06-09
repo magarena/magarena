e226095d15635e82111eed8a7b7fcbb0e946141e
a. benedict balbuena <abcbalbuena@gmail.com>
2013-06-09 11:35:28 +0800

    added Worldspine Wurm

---
+++ release/Magarena/scripts/Worldspine_Wurm.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts three 5/5 green Wurm creature tokens with trample onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(), 
+                TokenCardDefinitions.get("Wurm5"),
+                3
+            ));
+        }
+    }
+]
