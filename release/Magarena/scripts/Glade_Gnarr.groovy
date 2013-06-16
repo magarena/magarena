0ac085b2d03cee7dc8e2f379a1929f882e582712
melvin <melvinzhang@gmail.com>
2013-06-16 14:52:59 +0800

    completed Glade Gnarr

---
+++ release/Magarena/scripts/Glade_Gnarr.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return cardOnStack.hasColor(MagicColor.Blue) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN gets +2/+2 until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),2,2));
+        }
+    }
+]
