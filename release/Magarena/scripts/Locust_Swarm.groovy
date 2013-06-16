4d35c6e204142d9c82577d3888619bd01277061a
melvin <melvinzhang@gmail.com>
2013-06-16 10:34:45 +0800

    completed Locust Swarm by adding groovy script

---
+++ release/Magarena/scripts/Locust_Swarm.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Untap"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{G}"),
+                new MagicPlayAbilityEvent(source),
+                new MagicUntapConditionsEvent(source,this)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Untap SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicUntapAction(event.getPermanent()));
+        }
+    }
+]
