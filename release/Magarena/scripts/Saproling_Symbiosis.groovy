73e36a59a0e94f16b4a4cb74bfb0a641fc096886
melvin <melvinzhang@gmail.com>
2013-06-16 09:42:02 +0800

    added Saproling Symbiosis

---
+++ release/Magarena/scripts/Saproling_Symbiosis.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Put a 1/1 green Saproling creature token onto the battlefield for each creature you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Saproling"),
+                event.getPlayer().getNrOfPermanentsWithType(MagicType.Creature)
+            ));
+        }
+    },
+    new MagicCardActivation(
+        new MagicActivationHints(MagicTiming.Token,true),
+        "Instant"
+    ) {
+        public MagicEvent[] getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, "{5}{G}")
+            ];
+        }
+    }
+]
