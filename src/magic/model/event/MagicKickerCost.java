16b1c2925e5afaadb600f11181af2214939e05a7
melvin <melvinzhang@gmail.com>
2013-06-18 20:43:52 +0800

    replace MagicKickerChoice with MagicMayChoice and MagicPayManaCostChoice

--- src/magic/model/event/MagicKickerCost.java
+++ src/magic/model/event/MagicKickerCost.java
@@ -3,7 +3,8 @@ package magic.model.event;
 import magic.model.MagicManaCost;
 import magic.model.MagicSource;
 import magic.model.MagicGame;
-import magic.model.choice.MagicKickerChoice;
+import magic.model.choice.MagicPayManaCostChoice;
+import magic.model.choice.MagicMayChoice;
 import magic.model.action.MagicSetKickerAction;
 
 public class MagicKickerCost extends MagicAdditionalCost implements MagicEventAction {
@@ -21,16 +22,19 @@ public class MagicKickerCost extends MagicAdditionalCost implements MagicEventAc
     public MagicEvent getEvent(final MagicSource source) {
         return new MagicEvent(
             source,
-            new MagicKickerChoice(manaCost),
+            new MagicMayChoice(
+                "Pay the kicker " + manaCost.getText() + '?',
+                new MagicPayManaCostChoice(manaCost)
+            ),
             this,
-            ""
+            "PN may$$ pay " + manaCost.getText() + "$."
         );
     }
         
     @Override
     public void executeEvent(final MagicGame game, final MagicEvent event) {
-        event.payManaCost(game,event.getPlayer());
-        // TODO: use action for reversibility
-        game.doAction(new MagicSetKickerAction(event.getCard(), event.getKickerCount()));
+        if (event.isYes()) {
+            game.doAction(new MagicSetKickerAction(event.getCard(), 1));
+        }
     }
 }
