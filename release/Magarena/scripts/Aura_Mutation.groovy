d7a3f045f5a0cbd0194a65378b73b4fbfb38aaf1
melvin <melvinzhang@gmail.com>
2013-06-22 22:01:09 +0800

    completed Aura Mutation

---
+++ release/Magarena/scripts/Aura_Mutation.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ENCHANTMENT,
+                new MagicDestroyTargetPicker(true),
+                this,
+                "Destroy target enchantment\$. " + 
+                "Put X 1/1 green Saproling creature tokens onto the battlefield, where X is that enchantment's converted mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                    game.doAction(new MagicPlayTokensAction(
+                        event.getPlayer(),
+                        TokenCardDefinitions.get("Saproling"),
+                        permanent.getConvertedCost()
+                    ));
+                }
+            });
+        }
+    }
+]
