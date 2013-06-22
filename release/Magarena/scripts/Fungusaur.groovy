955af45c994ebb5b6395736bc6dc8f8256546427
melvin <melvinzhang@gmail.com>
2013-06-22 17:25:53 +0800

    completed Fungusaur

---
+++ release/Magarena/scripts/Fungusaur.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return damage.getTarget() == permanent ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Put a +1/+1 counter on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                1,
+                true
+            ));
+        }
+    }
+]
