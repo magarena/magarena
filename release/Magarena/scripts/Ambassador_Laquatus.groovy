6a3f5b3758bf43ddfe5aa25a202c33d63f44b9a0
melvin <melvinzhang@gmail.com>
2013-06-22 17:35:46 +0800

    completed Ambassador Laquatus

---
+++ release/Magarena/scripts/Ambassador_Laquatus.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump,false),
+        "Mill"
+    ) {
+        
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{3}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ puts the top three cards of his or her library " +
+                "into his or her graveyard."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicMillLibraryAction(player,3));
+                }
+            });
+        }
+    }
+]
