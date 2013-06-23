2a548bd446862340b348b08f64077223bd58b6c4
melvin <melvinzhang@gmail.com>
2013-06-23 17:49:11 +0800

    simplify code

--- release/Magarena/scripts/Bounteous_Kirin.groovy
+++ release/Magarena/scripts/Bounteous_Kirin.groovy
@@ -1,31 +1,24 @@
 [
-    new MagicWhenOtherSpellIsCastTrigger() {
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
         @Override
-        public MagicEvent executeTrigger(
-                final MagicGame game,
-                final MagicPermanent permanent,
-                final MagicCardOnStack cardOnStack) {
-            return permanent.isFriend(cardOnStack) && 
-                   (cardOnStack.hasSubType(MagicSubType.Spirit) || cardOnStack.hasSubType(MagicSubType.Arcane)) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.GAIN_LIFE,
-                        cardOnStack.getConvertedCost(),
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    cardOnStack,
-                    this,
-                    "You may gain " + cardOnStack.getConvertedCost() + " life."
-                ):
-                MagicEvent.NONE;
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicCardOnStack cardOnStack) {
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.GAIN_LIFE,
+                    cardOnStack.getConvertedCost(),
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
+                cardOnStack.getConvertedCost(),
+                this,
+                "You may gain RN life."
+            );
         }
         
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                final MagicCardOnStack spell = event.getRefCardOnStack();
-                game.doAction(new MagicChangeLifeAction(event.getPlayer(), spell.getConvertedCost()));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(), event.getRefInt()));
             }
         }
     }
