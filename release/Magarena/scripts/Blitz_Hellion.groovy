792adaaf32a56f21571604fb21ff615878bd97b4
melvin <melvinzhang@gmail.com>
2013-06-23 20:37:28 +0800

    completed Blitz Hellion

---
+++ release/Magarena/scripts/Blitz_Hellion.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicAtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer eotPlayer) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Shuffle SN into its owner's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicRemoveFromPlayAction(
+                event.getPermanent(),
+                MagicLocationType.OwnersLibrary
+            ));
+        }
+    }
+]
