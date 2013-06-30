0a5b0e741301b7920ae0047af6d5f38b3e637300
melvin <melvinzhang@gmail.com>
2013-06-30 18:45:32 +0800

    simplify script for Kodama of the Center Tree

--- release/Magarena/scripts/Kodama_of_the_Center_Tree.groovy
+++ release/Magarena/scripts/Kodama_of_the_Center_Tree.groovy
@@ -13,44 +13,7 @@
                 permanent.getController(),
                 MagicTargetFilter.TARGET_SPIRIT_YOU_CONTROL
             ).size()+1;
-            final MagicTargetFilter<MagicCard> targetFilter =
-                new MagicTargetFilter.MagicCMCCardFilter(
-                    MagicTargetFilter.TARGET_SPIRIT_CARD_FROM_GRAVEYARD,
-                    MagicTargetFilter.Operator.LESS_THAN_OR_EQUAL,
-                    cmc
-                );
-            final MagicTargetChoice targetChoice =
-                new MagicTargetChoice(
-                    targetFilter,false,MagicTargetHint.None,
-                    "a Spirit card from your graveyard"
-                );
-            return new MagicEvent(
-                permanent,
-                new MagicMayChoice(targetChoice),
-                new MagicGraveyardTargetPicker(false),
-                this,
-                "PN may\$ return target Spirit card\$ with " +
-                "converted mana cost " + cmc + " or less " +
-                "from his or her graveyard to his or her hand."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                event.processTargetCard(game,new MagicCardAction() {
-                    public void doAction(final MagicCard card) {
-                        game.doAction(new MagicRemoveCardAction(
-                            card,
-                            MagicLocationType.Graveyard
-                        ));
-                        game.doAction(new MagicMoveCardAction(
-                            card,
-                            MagicLocationType.Graveyard,
-                            MagicLocationType.OwnersHand
-                        ));
-                    }
-                });
-            }
+            return new MagicSoulshiftTrigger(cmc).getEvent(permanent);
         }
     }
 ]
