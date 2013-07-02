28f6288970c0d9862e921b4f2986367d6a9c8450
melvin <melvinzhang@gmail.com>
2013-07-02 17:58:42 +0800

    tighten card implementation

--- release/Magarena/scripts/Entreat_the_Angels.groovy
+++ release/Magarena/scripts/Entreat_the_Angels.groovy
@@ -2,23 +2,20 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
-            final int x = payedCost.getX();
             return new MagicEvent(
                 cardOnStack,
+                payedCost.getX(),
                 this,
-                "PN puts " + x + " 4/4 white Angel " +
-                "creature tokens with flying onto the battlefield."
+                "PN puts RN 4/4 white Angel creature tokens with flying onto the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            int x = event.getCardOnStack().getX();
-            for (;x>0;x--) {
-                game.doAction(new MagicPlayTokenAction(
-                    event.getPlayer(),
-                    TokenCardDefinitions.get("Angel4")
-                ));
-            }
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Angel4"),
+                event.getRefInt()
+            ));
         }
     }
 ]
