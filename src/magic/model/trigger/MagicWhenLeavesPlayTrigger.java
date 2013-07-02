68b76f6d056a34da21268bb02282d96a03b4e153
melvin <melvinzhang@gmail.com>
2013-07-02 20:43:28 +0800

    added Pillar of Flame

--- src/magic/model/trigger/MagicWhenLeavesPlayTrigger.java
+++ src/magic/model/trigger/MagicWhenLeavesPlayTrigger.java
@@ -1,5 +1,9 @@
 package magic.model.trigger;
 
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.event.MagicEvent;
+import magic.model.MagicLocationType;
 import magic.model.action.MagicRemoveFromPlayAction;
 
 public abstract class MagicWhenLeavesPlayTrigger extends MagicTrigger<MagicRemoveFromPlayAction> {
@@ -12,4 +16,15 @@ public abstract class MagicWhenLeavesPlayTrigger extends MagicTrigger<MagicRemov
     public MagicTriggerType getType() {
         return MagicTriggerType.WhenLeavesPlay;
     }
+
+    // replacement effect has priority 1
+    public static final MagicWhenLeavesPlayTrigger IfDieExileInstead = new MagicWhenLeavesPlayTrigger(1) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicRemoveFromPlayAction act) {
+            if (act.isPermanent(permanent) && act.getToLocation() == MagicLocationType.Graveyard) {
+                act.setToLocation(MagicLocationType.Exile);
+            }
+            return MagicEvent.NONE;
+        }
+    };
 }
