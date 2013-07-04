bb8fb7b3e9e7da3e122bb2c9a2554052a42b018e
melvin <melvinzhang@gmail.com>
2013-07-04 15:06:32 +0800

    added Warleader's Helix

---
+++ release/Magarena/scripts/Warleader_s_Helix.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(4),
+                this,
+                "SN deals 4 damage to target creature or player\$ and " +
+                "PN gains 4 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,4);
+                    game.doAction(new MagicDealDamageAction(damage));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+                }
+            });
+        }
+    }
+]
