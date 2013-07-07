0f6247c6c5d341b1556dcea445f11ecb4c5d3f6c
melvin <melvinzhang@gmail.com>
2013-07-07 09:42:51 +0800

    added Armored Ascension

---
+++ release/Magarena/scripts/Armored_Ascension.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final int amount = source.getController().getNrOfPermanentsWithSubType(MagicSubType.Plains);
+            pt.add(amount,amount);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
