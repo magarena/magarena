b3f348879c4bd53d3753acf607bb1eb7911e97f0
melvin <melvinzhang@gmail.com>
2013-07-08 13:37:55 +0800

    fixed Vigor script as return on a single line is treated as return false
    fixes issue 363

--- release/Magarena/scripts/Vigor.groovy
+++ release/Magarena/scripts/Vigor.groovy
@@ -3,12 +3,11 @@
         @Override
         public boolean accept(final MagicPermanent permanent, final MagicDamage damage) {
             final MagicTarget target = damage.getTarget();
-            return
-                damage.isPreventable() &&
-                damage.getAmount() > 0 &&
-                target != permanent &&
-                target.isCreature() &&
-                permanent.isFriend(target);
+            return damage.isPreventable() &&
+                   damage.getAmount() > 0 &&
+                   target != permanent &&
+                   target.isCreature() &&
+                   permanent.isFriend(target);
         }
 
         @Override
@@ -17,21 +16,20 @@
 
             // Prevention effect.
             damage.setAmount(0);
+                    
+            game.doAction(new MagicChangeCountersAction(
+                damage.getTargetPermanent(),
+                MagicCounterType.PlusOne,
+                amount,
+                true
+            ));
 
-            return new MagicEvent(
-                permanent,
-                damage.getTarget(),
-                {
-                    final MagicGame G, final MagicEvent E ->
-                    G.doAction(new MagicChangeCountersAction(
-                        E.getRefPermanent(),
-                        MagicCounterType.PlusOne,
-                        amount,
-                        true
-                    ));
-                } as MagicEventAction ,
-                "Put "+amount+" +1/+1 counters on RN."
+            game.logMessage(
+                permanent.getController(),
+                "Prevent ${amount} damage and put ${amount} +1/+1 counters on ${damage.getTarget()}"
             );
+
+            return MagicEvent.NONE;
         }
     }
 ]
