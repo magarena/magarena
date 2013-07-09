1f86f543a8605cff678104e404d1fa268c20f33f
melvin <melvinzhang@gmail.com>
2013-07-09 14:58:56 +0800

    completed Wilt-Leaf Liege

---
+++ release/Magarena/scripts/Wilt_Leaf_Liege.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_GREEN_CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_WHITE_CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    }
+]
