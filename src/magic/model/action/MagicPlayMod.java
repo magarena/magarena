442a270208a4eb9ecb2be6790695655fd84f0552
melvin <melvinzhang@gmail.com>
2013-07-10 14:26:56 +0800

    added enum for come into play modifications

---
+++ src/magic/model/action/MagicPlayMod.java
@@ -0,0 +1,16 @@
+package magic.model.action;
+
+public enum MagicPlayMod {
+    REMOVE_AT_END_OF_TURN,
+    REMOVE_AT_END_OF_YOUR_TURN,
+    SACRIFICE_AT_END_OF_TURN,
+    EXILE_AT_END_OF_COMBAT,
+    ATTACKING,
+    TAPPED,
+    HASTE_UEOT,
+    HASTE,
+    PERSIST,
+    UNDYING,
+    BLACK,
+    ZOMBIE
+}
