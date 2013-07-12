bb832f8ce3018039faa47a372e841e2cae68fcc8
melvin <melvinzhang@gmail.com>
2013-07-12 21:23:52 +0800

    added Splinter Twin

---
+++ release/Magarena/scripts/Splinter_Twin.groovy
@@ -0,0 +1,47 @@
+def Copy = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Copy"
+    ) {
+
+    @Override
+    public MagicEvent[] getCostEvent(final MagicPermanent source) {
+        return [new MagicTapEvent(source)];
+    }
+
+    @Override
+    public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+        return new MagicEvent(
+            source,
+            this,
+            "Put a token that's a copy of SN onto the battlefield. "+
+            "That token has haste. Sacrifice it at the beginning of the next end step."
+        );
+    }
+
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        final MagicPlayer player=event.getPlayer();
+        final MagicCard card=MagicCard.createTokenCard(
+            event.getPermanent().getCardDefinition(),
+            player
+        );
+        game.doAction(new MagicPlayCardAction(
+            card,
+            player,
+            [MagicPlayMod.HASTE, MagicPlayMod.EXILE_AT_END_OF_TURN]
+        ));
+    }
+}
+
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addActivation(Copy);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
