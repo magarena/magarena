880f99e3007daa09d88638631cb71288e1a03c37
melvin <melvinzhang@gmail.com>
2013-07-12 15:55:58 +0800

    added Fulminator Mage

---
+++ release/Magarena/scripts/Fulminator_Mage.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source),
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_NONBASIC_LAND,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target nonbasic land\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
