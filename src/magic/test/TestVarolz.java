c3c25a876e507ffde4e1bc3d241460294b63d15d
melvin <melvinzhang@gmail.com>
2013-07-12 16:01:41 +0800

    added test scenario for Varolz

---
+++ src/magic/test/TestVarolz.java
@@ -0,0 +1,47 @@
+
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.phase.MagicMainPhase;
+
+class TestVarolz extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(10);
+        addToLibrary(P, "Mountain", 20);
+        addToGraveyard(P, "Vexing Devil", 1);
+        createPermanent(game,P,"Rupture Spire",false,9);
+        createPermanent(game,P,"Vigor",false,1);
+        createPermanent(game,P,"Goblin King",false,1);
+        addToHand(P, "Varolz, the Scar-Striped", 1);
+
+        P = opponent;
+
+        P.setLife(10);
+        addToLibrary(P, "Mountain", 20);
+        createPermanent(game,P,"Rupture Spire",false,9);
+        createPermanent(game,P,"Goblin King",false,2);
+        addToHand(P, "Grizzly Bears", 2);
+
+        return game;
+    }
+}
