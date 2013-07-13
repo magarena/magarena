11c6ed39cae5a88e920a9da28789e3838df389d5
melvin <melvinzhang@gmail.com>
2013-07-13 22:46:55 +0800

    completed Agility

---
+++ release/Magarena/scripts/Agility.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Flanking);
+            permanent.addTrigger(MagicFlankingTrigger.create());
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
