20552e554ab85a4c0f33840b9b383a4afb03317c
melvin <melvinzhang@gmail.com>
2013-07-14 18:15:28 +0800

    completed Tahngarth's Rage

---
+++ release/Magarena/scripts/Tahngarth_s_Rage.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            if (permanent.isAttacking()) {
+                pt.add(3, 0);
+            } else {
+                pt.add(-2, -1);
+            }
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
