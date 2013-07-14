8d037cfe2b8906b9a3b27217be09309642f861d4
melvin <melvinzhang@gmail.com>
2013-07-14 11:24:04 +0800

    able to specify player to gain life

--- src/magic/model/event/MagicGainLifeEvent.java
+++ src/magic/model/event/MagicGainLifeEvent.java
@@ -2,17 +2,24 @@ package magic.model.event;
 
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.MagicPlayer;
 import magic.model.action.MagicChangeLifeAction;
 
 public class MagicGainLifeEvent extends MagicEvent {
-    public MagicGainLifeEvent(final MagicPermanent permanent, final int amt) {
+    public MagicGainLifeEvent(final MagicPermanent permanent, final MagicPlayer player, final int amt) {
         super(
             permanent,
+            player,
             amt,
             EVENT_ACTION,
             "PN gains RN life."
         );
     }
+    
+    public MagicGainLifeEvent(final MagicPermanent permanent, final int amt) {
+        this(permanent, permanent.getController(), amt);
+    }
+
     private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
