b3bf067c410c36ba817bbf9d250d57ab5a2aaf29
melvin <melvinzhang@gmail.com>
2013-07-14 19:50:30 +0800

    added Mutilate

---
+++ release/Magarena/scripts/Mutilate.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "All creatures get -1/-1 until end of turn for each Swamp you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer you = event.getPlayer();
+            Collection<MagicPermanent> creatures = game.filterPermanents(
+                you,
+                MagicTargetFilter.TARGET_CREATURE
+            )
+            final int amt = you.getNrOfPermanents(MagicSubType.Swamp);
+            for (final MagicPermanent creature : creatures) {
+                game.doAction(new MagicChangeTurnPTAction(
+                    creature,
+                    -amt,
+                    -amt
+                ));
+            }
+        }
+    }
+]
