2485be57e0c65b0d4509a886b72580d02f2d76aa
melvin <melvinzhang@gmail.com>
2013-07-15 11:54:53 +0800

    avoid constructing new activation/trigger each update, seems to cause inconsistent node issue with MCTS

--- release/Magarena/scripts/Eldrazi_Conscription.groovy
+++ release/Magarena/scripts/Eldrazi_Conscription.groovy
@@ -1,8 +1,10 @@
+def Annihilator2 = new MagicAnnihilatorTrigger(2);
+
 [
     new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
-            permanent.addTrigger(new MagicAnnihilatorTrigger(2));
+            permanent.addTrigger(Annihilator2);
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
--- release/Magarena/scripts/Psionic_Gift.groovy
+++ release/Magarena/scripts/Psionic_Gift.groovy
@@ -1,8 +1,10 @@
+def Ping1 = new MagicPingActivation(1);
+
 [
     new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
-            permanent.addActivation(new MagicPingActivation(1));
+            permanent.addActivation(Ping1);
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
