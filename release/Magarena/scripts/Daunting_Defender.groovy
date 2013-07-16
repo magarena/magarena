d31627fe5bc3c1435f26523bb3c54d25205ec4e9
hong yie <unknown>
2013-07-16 10:51:43 +0800

    added Daunting Defender

---
+++ release/Magarena/scripts/Daunting_Defender.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(5) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (damage.getTarget().isCreature() && damage.getTarget().hasSubType(MagicSubType.Cleric) && permanent.isFriend(damage.getTarget())) {
+                // Prevention effect.
+                damage.prevent(1);
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
