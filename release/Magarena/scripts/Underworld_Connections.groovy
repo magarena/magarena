6edea67f47d2c314cf931adbc8720684d69e4394
melvin <melvinzhang@gmail.com>
2013-07-16 13:28:22 +0800

    added Underworld Connections

---
+++ release/Magarena/scripts/Underworld_Connections.groovy
@@ -0,0 +1,40 @@
+def Draw = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayLifeEvent(source,1),
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer()));
+        }
+    }
+
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addActivation(Draw);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
