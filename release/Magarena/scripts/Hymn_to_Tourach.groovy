e70573178072f29d13e71329f7dd5674f4436cba
hmarinmacias <unknown>
2013-07-18 18:11:31 +0800

    added Hymn to Tourach

---
+++ release/Magarena/scripts/Hymn_to_Tourach.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ discards two cards at random."
+            );
+        }
+      
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(MagicDiscardEvent.Random(event.getSource(),player, 2));
+                }
+            });
+        }
+    }
+]
