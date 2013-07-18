b36baa252a7046eb7cbeddd1f93d261229289e27
hong yie <unknown>
2013-07-18 18:20:28 +0800

    added Blessed Orator

---
+++ release/Magarena/scripts/Blessed_Orator.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(0,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    }
+]
