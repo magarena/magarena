d651c56b058b3eef8e3709cb413cb84adb4e603e
Hector Marin <hmarinmacias@gmail.com>
2013-07-19 09:44:04 +0800

    added Ankh of Mishra

---
+++ release/Magarena/scripts/Ankh_of_Mishra.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent otherPermanent) {
+            return (otherPermanent.isLand()) ?
+                new MagicEvent(
+                    permanent,
+                    otherPermanent.getController(),
+                    this,
+                    "SN deals 2 damage to PN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicDamage damage = new MagicDamage(
+                event.getSource(),
+                event.getPlayer(),
+                2
+            );
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
