c9179856904941009acbfd93cb3c7dac1f9164c6
a. benedict balbuena <abcbalbuena@gmail.com>
2013-07-19 20:41:20 +0800

    added Tolarian Emissary

---
+++ release/Magarena/scripts/Tolarian_Emissary.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return payedCost.isKicked() ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.NEG_TARGET_ENCHANTMENT,
+                    new MagicDestroyTargetPicker(false),
+                    this,
+                    "Destroy target enchantment\$."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent target) {
+                    game.doAction(new MagicDestroyAction(target));
+                }
+            });
+        }
+    }
+]
