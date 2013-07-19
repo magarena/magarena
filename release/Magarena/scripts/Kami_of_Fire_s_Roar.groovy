0deec7f7472699a73bd67eb26910e79a2327f4fc
a. benedict balbuena <abcbalbuena@gmail.com>
2013-07-19 20:44:58 +0800

    added Kami of Fire's Roar

---
+++ release/Magarena/scripts/Kami_of_Fire_s_Roar.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicNoCombatTargetPicker(false,true,false),
+                this,
+                "Target creature\$ can't block this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainAbilityAction(creature,MagicAbility.CannotBlock));
+                }
+            });
+        }
+    }
+]
