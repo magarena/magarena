5fcc98f099bc87e22b7e4eb80b04e9e67b77f5d5
a. benedict balbuena <abcbalbuena@gmail.com>
2013-07-19 20:39:57 +0800

    added Sire of the Storm

---
+++ release/Magarena/scripts/Sire_of_the_Storm.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.DRAW_CARDS,
+                    1,
+                    MagicSimpleMayChoice.DEFAULT_NONE
+                ),
+                this,
+                "PN may\$ draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    }
+]
