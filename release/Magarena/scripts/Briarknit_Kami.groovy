5f6b922bfa5473bc4178a7d567770b142291f7ca
a. benedict balbuena <abcbalbuena@gmail.com>
2013-07-19 21:19:49 +0800

    added Briarknit Kami

---
+++ release/Magarena/scripts/Briarknit_Kami.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent source,final MagicCardOnStack spell) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN puts a +1/+1 counter on target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicChangeCountersAction(
+                        creature,
+                        MagicCounterType.PlusOne,
+                        1,
+                        true
+                    ));
+                }
+            });
+        }
+    }
+]
