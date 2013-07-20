e844121245104b1ffe1c77b66b19f6b7acaf80b3
Hector Marin <hmarinmacias@gmail.com>
2013-07-20 09:34:10 +0800

    added Strip Mine

---
+++ release/Magarena/scripts/Strip_Mine.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                new MagicDestroyTargetPicker(false),
+                this,
+                "Destroy target land\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
