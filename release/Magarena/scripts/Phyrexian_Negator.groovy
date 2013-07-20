02451b57fad12f1219804626188d12f745755932
Hector Marin <hmarinmacias@gmail.com>
2013-07-20 09:36:37 +0800

    added Phyrexian Negator

---
+++ release/Magarena/scripts/Phyrexian_Negator.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return damage.getTarget() == permanent ?
+                new MagicEvent(
+                    permanent,
+                    damage.getDealtAmount(),
+                    this,
+                    "PN sacrifices RN permanents."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            final MagicPlayer player = event.getPlayer();
+            int amount = event.getRefInt();
+            while (amount > 0 && player.getPermanents().size() > 0) {
+                game.addEvent(new MagicSacrificePermanentEvent(
+                    permanent,
+                    player,
+                    MagicTargetChoice.SACRIFICE_PERMANENT
+                ));
+                amount--;
+            }
+        }
+    }
+]
