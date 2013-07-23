326cad6802c8a4d5acca6cbbed1a4a20dfd0cfd8
Hector Marin <hmarinmacias@gmail.com>
2013-07-23 16:02:40 +0800

    added Death Pits of Rath

---
+++ release/Magarena/scripts/Death_Pits_of_Rath.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicTarget target = damage.getTarget();
+            return target.isCreature() ?
+                new MagicEvent(
+                    permanent,
+                    target,
+                    this,
+                    "Destroy RN. It can't be regenerated."
+                ): 
+                MagicEvent.NONE;
+        }
+        @Override       
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent creature = event.getRefPermanent();
+            game.doAction(MagicChangeStateAction.Set(creature,MagicPermanentState.CannotBeRegenerated));
+            game.doAction(new MagicDestroyAction(creature));
+        }
+    }
+]
