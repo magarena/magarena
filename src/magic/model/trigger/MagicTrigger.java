8cf3f065f8c1f0b28eda9ca17195c7f34099f42a
melvin <melvinzhang@gmail.com>
2013-07-24 14:38:00 +0800

    added CANT_BE_PREVENTED constant

--- src/magic/model/trigger/MagicTrigger.java
+++ src/magic/model/trigger/MagicTrigger.java
@@ -11,9 +11,10 @@ import magic.model.event.MagicEventAction;
 public abstract class MagicTrigger<T> implements MagicEventAction,MagicChangeCardDefinition {
 
     public  static final int REPLACEMENT = 1;
-    public  static final int PREVENT_DAMAGE = 4;
-    public  static final int REPLACE_DAMAGE = 6;
-    private static final int DEFAULT_PRIORITY = 10;
+    public  static final int CANT_BE_PREVENTED = 2;
+    public  static final int PREVENT_DAMAGE    = 4;
+    public  static final int REPLACE_DAMAGE    = 6;
+    private static final int DEFAULT_PRIORITY  = 10;
 
     private final int priority;
 
