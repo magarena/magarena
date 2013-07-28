1ebc4fb046cec048730a6389c22e6f5f026ce0ad
hong yie <unknown>
2013-07-28 09:20:49 +0800

    added Enchantress's Presence

---
+++ release/Magarena/scripts/Enchantress_s_Presence.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return (permanent.isFriend(spell) &&
+                    spell.getCardDefinition().isEnchantment()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN draw a card."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer()));
+        }
+    }
+]
