e0d9bfa728441e209f889256a8c77b11df8b0e11
Hector Marin <hmarinmacias@gmail.com>
2013-07-29 14:18:44 +0800

    added Battlegate Mimic

---
+++ release/Magarena/scripts/Battlegate_Mimic.groovy
@@ -0,0 +1,33 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(4,2);
+    }
+};
+
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {  
+            return (permanent.isFriend(cardOnStack) &&
+                    cardOnStack.hasColor(MagicColor.Red) && 
+                    cardOnStack.hasColor(MagicColor.White)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN becomes 4/2 and gains first strike until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicAddStaticAction(
+                event.getPermanant(), PT
+            ));
+            game.doAction(new MagicGainAbilityAction(
+                event.getPermanant(), 
+                MagicAbility.FirstStrike
+            ));
+        }
+    }
+]
