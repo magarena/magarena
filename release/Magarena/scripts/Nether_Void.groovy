5533e5a8af3d2eeea51d54eff7996fd20fedfa38
Hector Marin <hmarinmacias@gmail.com>
2013-07-29 14:13:44 +0800

    added Nether Void

---
+++ release/Magarena/scripts/Nether_Void.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return new MagicEvent(
+                permanent, 
+                cardOnStack.getController(),
+                cardOnStack,
+                this,
+                "Counter RN unless PN pays {3}"
+            ); 
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) { 
+            game.addEvent(new MagicCounterUnlessEvent(
+                event.getSource(),
+                event.getRefCardOnStack(),
+                MagicManaCost.create("{3}")
+            ));
+        }
+    }
+]
