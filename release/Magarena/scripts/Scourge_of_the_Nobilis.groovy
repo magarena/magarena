0d7bcac9e556f84c0550584db0e97d71ceb57397
Hector Marin <hmarinmacias@gmail.com>
2013-07-29 14:08:44 +0800

    added Scourge of the Nobilis

---
+++ release/Magarena/scripts/Scourge_of_the_Nobilis.groovy
@@ -0,0 +1,45 @@
+def Pump = new MagicPumpActivation("{R/W}",1,0);
+
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1, 1);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Red);
+        }
+    },
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addActivation(Pump);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Red);
+        }
+    },
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1, 1);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+        }
+    },
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Lifelink);  
+        }
+
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+        }
+    }
+]
