13925e2652c5be124597b19a28d1606ef70b2299
Hector Marin <hmarinmacias@gmail.com>
2013-07-29 14:20:33 +0800

    added Nightsky Mimic

---
+++ release/Magarena/scripts/Nightsky_Mimic.groovy
@@ -0,0 +1,33 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(4,4);
+    }
+};
+
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {  
+            return (permanent.isFriend(cardOnStack) &&
+                    cardOnStack.hasColor(MagicColor.Black) && 
+                    cardOnStack.hasColor(MagicColor.White)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN becomes 4/4 and gains flying until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicAddStaticAction(
+                event.getPermanant(), PT
+            ));
+            game.doAction(new MagicGainAbilityAction(
+                event.getPermanant(), 
+                MagicAbility.Flying
+            ));
+        }
+    }
+]
