376666d0ebf8d4b7b6e9336ab59a7472a8151062
jericho.pumpkin <jericho.pumpkin@gmail.com>
2013-08-06 20:57:48 +0800

    added Captured Sunlight

---
+++ release/Magarena/scripts/Captured_Sunlight.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN gains 4 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+        }
+    }
+]
