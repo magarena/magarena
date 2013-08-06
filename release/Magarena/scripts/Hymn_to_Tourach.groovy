89a43ca44eca1c3d8ab05d3d7bc2e410ac36ad21
jericho.pumpkin <jericho.pumpkin@gmail.com>
2013-08-06 20:41:44 +0800

    remove targetting hint

--- release/Magarena/scripts/Hymn_to_Tourach.groovy
+++ release/Magarena/scripts/Hymn_to_Tourach.groovy
@@ -4,12 +4,12 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                MagicTargetChoice.NEG_TARGET_PLAYER,
+                MagicTargetChoice.TARGET_PLAYER,
                 this,
                 "Target player\$ discards two cards at random."
             );
         }
-      
+
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
