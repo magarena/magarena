021a5b80d658d33c9a89a8f8804f204859b5820b
melvin <melvinzhang@gmail.com>
2013-08-09 17:04:16 +0800

    added test scenario for clone cards

---
+++ src/magic/test/TestClone.java
@@ -0,0 +1,48 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.phase.MagicMainPhase;
+
+class TestClone extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(6);
+        addToLibrary(P, "Swamp", 20);
+        createPermanent(game,P,"Rupture Spire",false,9);
+        createPermanent(game,P,"Glorious Anthem",false,1);
+        createPermanent(game,P,"Asceticism",false,1);
+        addToHand(P, "Phantasmal Image", 1);
+        addToHand(P, "Clone", 1);
+        addToHand(P, "Evil Twin", 1);
+        addToHand(P, "Progenitor Mimic", 1);
+
+        P = opponent;
+
+        P.setLife(6);
+        addToLibrary(P, "Mountain", 20);
+        createPermanent(game,P,"Rupture Spire",false,9);
+        createPermanent(game,P, "Grizzly Bears", false, 1);
+        addToGraveyard(P, "Ink-Eyes, Servant of Oni", 1);
+
+        return game;
+    }
+}
