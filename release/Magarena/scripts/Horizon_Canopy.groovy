33bdede2c585af1292b9f2d4c1257d3b34733dd6
Project Firemind <projectfiremind@gmail.com>
2013-08-09 08:21:25 +0000

    Add script for Horizon Canopy

---
+++ release/Magarena/scripts/Horizon_Canopy.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [
+          MagicConditionFactory.ManaCost("{2}")
+        ],
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificeEvent(source),
+                new MagicPayManaCostEvent(source,"{1}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer()));
+        }
+    }
+]
