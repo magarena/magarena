c598786d53b4306905f1bf74782277c3f29c3a11
melvin <melvinzhang@gmail.com>
2013-08-10 18:46:25 +0800

    added event and trigger for cipher mechanic

---
+++ src/magic/model/event/MagicCipherEvent.java
@@ -0,0 +1,50 @@
+package magic.model.event;
+
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.choice.MagicMayChoice;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.action.MagicChangeCardDestinationAction;
+import magic.model.action.MagicMoveCardAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.action.MagicAddTriggerAction;
+import magic.model.trigger.MagicWhenDamageIsDealtTrigger;
+
+public class MagicCipherEvent extends MagicEvent {
+
+    public MagicCipherEvent(final MagicSource source,final MagicPlayer player) {
+        super(
+            source,
+            player,
+            new MagicMayChoice(
+                "Exile " + source + " encoded on a creature you control?",
+                MagicTargetChoice.CREATURE_YOU_CONTROL
+            ),
+            EVENT_ACTION,
+            "PN may$ exile SN encoded on a creature$ you control."
+        );
+    }
+
+    private static final MagicEventAction EVENT_ACTION=new MagicEventAction() {
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.Exile));
+                event.processTargetPermanent(game, new MagicPermanentAction() {
+                    public void doAction(final MagicPermanent creatureToEncode) {
+                        game.doAction(new MagicAddTriggerAction(
+                            creatureToEncode,
+                            MagicWhenDamageIsDealtTrigger.Cipher(event.getCardOnStack().getCardDefinition())
+                        ));
+                    }
+                });
+            } else {
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.Graveyard));
+            }
+            game.doAction(new MagicMoveCardAction(event.getCardOnStack()));
+        }
+    };
+}
--- src/magic/model/trigger/MagicWhenDamageIsDealtTrigger.java
+++ src/magic/model/trigger/MagicWhenDamageIsDealtTrigger.java
@@ -1,7 +1,15 @@
 package magic.model.trigger;
 
+import magic.model.MagicGame;
+import magic.model.MagicCard;
 import magic.model.MagicDamage;
 import magic.model.MagicPermanent;
+import magic.model.MagicPayedCost;
+import magic.model.MagicCardDefinition;
+import magic.model.stack.MagicCardOnStack;
+import magic.model.choice.MagicMayChoice;
+import magic.model.event.MagicEvent;
+import magic.model.action.MagicPutItemOnStackAction;
 
 public abstract class MagicWhenDamageIsDealtTrigger extends MagicTrigger<MagicDamage> {
     public MagicWhenDamageIsDealtTrigger(final int priority) {
@@ -16,5 +24,33 @@ public abstract class MagicWhenDamageIsDealtTrigger extends MagicTrigger<MagicDa
 
     public MagicTriggerType getType() {
         return MagicTriggerType.WhenDamageIsDealt;
+    }
+
+    public static MagicWhenDamageIsDealtTrigger Cipher(final MagicCardDefinition cardDef) {
+        return new MagicWhenDamageIsDealtTrigger() {
+            @Override
+            public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+                return (damage.isCombat() && damage.getTarget().isPlayer()) ?
+                    new MagicEvent(
+                        permanent,
+                        new MagicMayChoice(),
+                        this,
+                        "PN may$ cast " + cardDef + " without paying its mana cost"
+                    ):
+                    MagicEvent.NONE;
+            }
+            @Override
+            public void executeEvent(final MagicGame game, final MagicEvent event) {
+                if (event.isYes()) {
+                    game.doAction(new MagicPutItemOnStackAction(
+                        new MagicCardOnStack(
+                            MagicCard.createTokenCard(cardDef,event.getPlayer()),
+                            event.getPlayer(),
+                            MagicPayedCost.NO_COST
+                        )
+                    ));
+                }
+            }
+        };
     }
 }
