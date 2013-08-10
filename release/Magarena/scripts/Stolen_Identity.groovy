be343937797d40515e19372de4ba7b66cb124db1
melvin <melvinzhang@gmail.com>
2013-08-10 19:34:30 +0800

    added Stolen Identity

---
+++ release/Magarena/scripts/Stolen_Identity.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_ARTIFACT_OR_CREATURE,
+                MagicCopyTargetPicker.create(),
+                this,
+                "Put a token onto the battlefield that's a copy of target artifact or creature\$. " +
+                "Cipher."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    final MagicPlayer player = event.getPlayer();
+                    game.doAction(new MagicPlayCardAction(
+                        MagicCard.createTokenCard(creature.getCardDefinition(), player),
+                        player
+                    ));
+                    if (event.getCardOnStack().getCard().isToken() == false) {
+                        // prevent auto move to graveyard
+                        game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.Play));
+                        game.addEvent(new MagicCipherEvent(event.getSource(), event.getPlayer()));
+                    }
+                }
+            });
+        }
+    }
+]
