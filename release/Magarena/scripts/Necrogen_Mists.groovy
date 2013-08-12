1d770bdc72c3844468bfad6b6af442a4e964bb7d
hong yie <unknown>
2013-08-12 15:34:30 +0800

    added Necrogen Mists

---
+++ release/Magarena/scripts/Necrogen_Mists.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                upkeepPlayer,
+                this,
+                "PN discards a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            game.addEvent(new MagicDiscardEvent(
+                event.getSource(),
+                event.getPlayer()
+            ));
+        }
+    }
+]
