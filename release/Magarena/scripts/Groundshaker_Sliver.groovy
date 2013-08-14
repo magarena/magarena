ab5784d27454e7c55f25d7ffa9edf09dedc22fbc
ember\ hauler <unknown>
2013-08-14 16:34:16 +0000

    Add script for Groundshaker Sliver

---
+++ release/Magarena/scripts/Groundshaker_Sliver.groovy
@@ -0,0 +1,10 @@
+[
+    new MagicStatic(
+            MagicLayer.Ability,
+            MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Trample);
+        }
+    }
+]
