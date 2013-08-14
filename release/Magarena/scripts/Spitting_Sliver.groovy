adf6abe180f7609e602a9dd2a78757bb3927e7b3
ember\ hauler <unknown>
2013-08-14 16:38:19 +0000

    Add script for Spitting Sliver

---
+++ release/Magarena/scripts/Spitting_Sliver.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicStatic(
+            MagicLayer.Ability,
+            MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.FirstStrike);
+        }
+    }
+]
+
