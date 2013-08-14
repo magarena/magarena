d3f51f599536e0274d35ee7dd6c274b999cd415c
ember\ hauler <unknown>
2013-08-14 16:33:29 +0000

    Add script for Galerider Sliver

--- release/Magarena/scripts/Galerider_Sliver.groovy
+++ release/Magarena/scripts/Galerider_Sliver.groovy
@@ -1,7 +1,7 @@
 [
     new MagicStatic(
             MagicLayer.Ability,
-            MagicTargetFilter.TARGET_SLIVER_YOU_CONTROL) {
+            MagicTargetFilter.TARGET_SLIVER) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Flying);
