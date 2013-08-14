94eb121673f552ec656b8db6e5f0dd6c5c27608a
ember\ hauler <unknown>
2013-08-14 16:31:00 +0000

    Add script for Battle Sliver

---
+++ release/Magarena/scripts/Battle_Sliver.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_SLIVER) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            pt.add(2,0);
+        }
+    }
+]
+
