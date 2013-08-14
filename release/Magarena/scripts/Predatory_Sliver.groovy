fa462a685182156c9688d770770601e992dd4188
ember\ hauler <unknown>
2013-08-14 16:35:20 +0000

    Add script for Predatory Sliver

--- release/Magarena/scripts/Predatory_Sliver.groovy
+++ release/Magarena/scripts/Predatory_Sliver.groovy
@@ -1,8 +1,7 @@
 [
     new MagicStatic(
         MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_SLIVER_YOU_CONTROL
-    ) {
+        MagicTargetFilter.TARGET_SLIVER) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(1,1);
