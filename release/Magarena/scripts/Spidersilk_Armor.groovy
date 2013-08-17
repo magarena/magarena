3316bfc42ee3a50c0fa01f33ee54e8bf0170d180
hong\ yie <unknown>
2013-08-17 10:13:15 +0000

    Add script for Spidersilk Armor

--- release/Magarena/scripts/Spidersilk_Armor.groovy
+++ release/Magarena/scripts/Spidersilk_Armor.groovy
@@ -6,8 +6,12 @@
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
             flags.add(MagicAbility.Reach);
         }
-		@Override
-		public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+	},
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
             pt.add(0,1);
         }
     }
