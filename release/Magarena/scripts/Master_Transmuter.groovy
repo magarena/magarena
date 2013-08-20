86e6e207f0e10cdd9bd783fb0f1f7e384b637eac
melvin <melvinzhang@gmail.com>
2013-08-20 14:52:27 +0800

    added Master Transmuter

---
+++ release/Magarena/scripts/Master_Transmuter.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Put"
+    ) {
+
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source, "{U}"),
+                new MagicBounceChosenPermanentEvent(source, MagicTargetChoice.ARTIFACT_YOU_CONTROL)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.ARTIFACT_CARD_FROM_HAND,
+                new MagicGraveyardTargetPicker(true),
+                this,
+                "PN puts an artifact card\$ from his or her hand onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
+                    game.doAction(new MagicPlayCardAction(card,event.getPlayer()));
+                }
+            });
+        }
+    }
+]
