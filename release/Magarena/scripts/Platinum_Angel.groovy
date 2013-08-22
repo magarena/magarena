68f7bb00a248c1100a23fd91ddc6f6a7877d9587
melvin <melvinzhang@gmail.com>
2013-08-22 09:32:56 +0800

    fix Platinum Angel groovy script

--- release/Magarena/scripts/Platinum_Angel.groovy
+++ release/Magarena/scripts/Platinum_Angel.groovy
@@ -3,7 +3,7 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicLoseGameAction loseAct) {
             if (permanent.isController(loseAct.getPlayer())) {
-                loseAct.setPlayers(MagicPlayer.NONE);
+                loseAct.setPlayer(MagicPlayer.NONE);
             }
             return MagicEvent.NONE;
         }
