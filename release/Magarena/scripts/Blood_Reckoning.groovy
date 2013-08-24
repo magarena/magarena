57ec84b60bfc35297cde11888b0c4b3cd1a2a9cc
ember hauler <unknown>
2013-08-24 19:30:16 +0800

    added Blood Reckoning

---
+++ release/Magarena/scripts/Blood_Reckoning.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return permanent.isEnemy(creature) ?
+                new MagicEvent(
+                    permanent,
+                    creature.getController(),
+                    this,
+                    "PN loses 1 life."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),-1));
+        }
+    }
+]
