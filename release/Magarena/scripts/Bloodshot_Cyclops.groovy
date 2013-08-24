f1b2db854c8dd561a27b5460433b163fd84d9687
hong yie <unknown>
2013-08-24 19:29:02 +0800

    added Bloodshot Cyclops

---
+++ release/Magarena/scripts/Bloodshot_Cyclops.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+				new MagicTapEvent(source),
+				new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE)				
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                payedCost.getTarget(),
+                this,
+                "SN deals damage equal to the power of RN to target creature or player\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game,new MagicTargetAction() {
+                public void doAction(final MagicTarget target) {
+                    final MagicPermanent sacrificed=event.getRefPermanent();
+                    final MagicDamage damage=new MagicDamage(event.getSource(),target,sacrificed.getPower());
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            });
+        }
+    }
+]
