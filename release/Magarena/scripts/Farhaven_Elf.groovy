efa1e4e6c5fd43842602bbd307894c288faf2432
hong yie <unknown>
2013-08-25 17:28:22 +0800

    added Farhaven Elf

---
+++ release/Magarena/scripts/Farhaven_Elf.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {      
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN may search his or her library for a basic land card and put that card onto the battlefield tapped, then shuffle his or her library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicSearchOntoBattlefieldEvent(
+                event,
+                new MagicMayChoice(
+                    "Search for a basic land card?",
+                    MagicTargetChoice.BASIC_LAND_CARD_FROM_LIBRARY
+                ),
+                MagicPlayMod.TAPPED
+            ));
+        }
+    }
+]
