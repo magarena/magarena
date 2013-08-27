910fd7abac8d558d0cfb5d7a949d987b18c98e37
melvin <melvinzhang@gmail.com>
2013-08-27 11:09:42 +0800

    improve code formatting

--- release/Magarena/scripts/Armament_Master.groovy
+++ release/Magarena/scripts/Armament_Master.groovy
@@ -1,12 +1,12 @@
 [
     new MagicStatic(
         MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_KOR_YOU_CONTROL) {
-
+        MagicTargetFilter.TARGET_KOR_YOU_CONTROL
+    ) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-                final int amount = 2 * source.getEquipmentPermanents().size();
-                pt.add(amount, amount);
+            final int amount = 2 * source.getEquipmentPermanents().size();
+            pt.add(amount, amount);
         }
         @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
