f0fa97d087cf18b42324bc6cdd131c6f9c6b246c
melvin <melvinzhang@gmail.com>
2013-08-27 10:48:48 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Murkfiend_Liege.groovy
+++ release/Magarena/scripts/Murkfiend_Liege.groovy
@@ -1,30 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_GREEN_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_BLUE_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
