4df34e70ee0155d8a968f15fe5b15cce593d575f
melvin <melvinzhang@gmail.com>
2013-08-27 10:58:54 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Eldrazi_Monument.groovy
+++ release/Magarena/scripts/Eldrazi_Monument.groovy
@@ -1,23 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Flying);
-            flags.add(MagicAbility.Indestructible);
-        }
-    },
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-    },
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
