53b540469aa5aad43ab3748efb421bd52cdc0f08
melvin <melvinzhang@gmail.com>
2013-08-27 10:59:28 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Diregraf_Captain.groovy
+++ release/Magarena/scripts/Diregraf_Captain.groovy
@@ -1,17 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_ZOMBIE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicWhenOtherDiesTrigger() {
         @Override
         public MagicEvent executeTrigger(
