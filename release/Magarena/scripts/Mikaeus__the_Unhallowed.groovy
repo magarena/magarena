0aa7982b13a1ec78345436f7d326b279813b4ef7
melvin <melvinzhang@gmail.com>
2013-08-27 10:51:28 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Mikaeus__the_Unhallowed.groovy
+++ release/Magarena/scripts/Mikaeus__the_Unhallowed.groovy
@@ -1,30 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_NONHUMAN_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_NONHUMAN_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Undying);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(
