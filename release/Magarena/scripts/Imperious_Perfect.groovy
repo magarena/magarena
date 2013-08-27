d24acaabc40e014ee8ef5c47e785e54ceac48f68
melvin <melvinzhang@gmail.com>
2013-08-27 10:59:55 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Imperious_Perfect.groovy
+++ release/Magarena/scripts/Imperious_Perfect.groovy
@@ -1,17 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_ELF_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Token),
         "Token"
