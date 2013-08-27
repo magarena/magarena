61fae4a86ceb72be8d81f3a644faa8c5bbe8d8aa
melvin <melvinzhang@gmail.com>
2013-08-27 09:41:30 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Wing_Splicer.groovy
+++ release/Magarena/scripts/Wing_Splicer.groovy
@@ -1,12 +1,3 @@
 [
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_GOLEM_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Flying);
-        }
-    },
     MagicWhenComesIntoPlayTrigger.PutGolemOntoTheBattlefield
 ]
