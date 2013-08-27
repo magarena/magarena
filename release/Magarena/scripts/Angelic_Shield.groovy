3e92985377803383d1b64d55572fbb5d37b96632
melvin <melvinzhang@gmail.com>
2013-08-27 11:09:57 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Angelic_Shield.groovy
+++ release/Magarena/scripts/Angelic_Shield.groovy
@@ -1,13 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(0,1);
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
         "Return"
