23804fcb44b09a562f26d2cf65067f23c4ddf270
melvin <melvinzhang@gmail.com>
2013-08-27 10:48:21 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Mindwrack_Liege.groovy
+++ release/Magarena/scripts/Mindwrack_Liege.groovy
@@ -1,28 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_BLUE_CREATURE_YOU_CONTROL) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_RED_CREATURE_YOU_CONTROL) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Token),
         "Put"
