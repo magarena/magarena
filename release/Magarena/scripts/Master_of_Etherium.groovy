99627e59709ffe824977fd7575e33e76e04add61
melvin <melvinzhang@gmail.com>
2013-08-27 10:58:02 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Master_of_Etherium.groovy
+++ release/Magarena/scripts/Master_of_Etherium.groovy
@@ -5,17 +5,5 @@
             final int size = game.filterPermanents(player, MagicTargetFilter.TARGET_ARTIFACT_YOU_CONTROL).size();
             pt.set(size, size);
         }
-    },
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_ARTIFACT_CREATURE_YOU_CONTROL) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
     }
 ]
