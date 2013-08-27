5e9927e4ae8a0f161cc966a4a3c114f61c99e3ac
melvin <melvinzhang@gmail.com>
2013-08-27 11:03:50 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Captain_of_the_Watch.groovy
+++ release/Magarena/scripts/Captain_of_the_Watch.groovy
@@ -1,30 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.ModPT,
-        MagicTargetFilter.TARGET_SOLDIER_YOU_CONTROL
-    ) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(1,1);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_SOLDIER_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Vigilance);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
