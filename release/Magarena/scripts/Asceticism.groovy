3202fe6c629c23c885d4e5e5026ed9f6a25f1f48
melvin <melvinzhang@gmail.com>
2013-08-27 11:09:20 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Asceticism.groovy
+++ release/Magarena/scripts/Asceticism.groovy
@@ -1,12 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Hexproof);
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump,true),
         "Regen"
