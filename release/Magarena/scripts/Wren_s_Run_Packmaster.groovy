11199c7e82e625f9bf900ee266a7c5ba2ada67fc
melvin <melvinzhang@gmail.com>
2013-08-27 09:40:47 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Wren_s_Run_Packmaster.groovy
+++ release/Magarena/scripts/Wren_s_Run_Packmaster.groovy
@@ -1,13 +1,4 @@
 [
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_WOLF_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            flags.add(MagicAbility.Deathtouch);
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Token,true),
         "Token"
