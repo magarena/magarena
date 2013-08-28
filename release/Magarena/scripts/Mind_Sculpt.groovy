69b12d104040951d6c21876380cb5b74bd8e229f
ember hauler <unknown>
2013-08-28 13:36:41 +0800

    added Mind Sculpt

---
+++ release/Magarena/scripts/Mind_Sculpt.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Target opponent\$ puts the top seven cards of his or her " +
+                "library into his or her graveyard. Draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.doAction(new MagicMillLibraryAction(player,7));
+                }
+            });
+        }
+    }
+]
