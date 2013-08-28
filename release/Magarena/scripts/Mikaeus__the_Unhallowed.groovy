f76b31b190dad21780fb954ee4937ad8a211147f
melvin <melvinzhang@gmail.com>
2013-08-28 10:07:38 +0800

    undying is a trigger that has to be added in groovy script

--- release/Magarena/scripts/Mikaeus__the_Unhallowed.groovy
+++ release/Magarena/scripts/Mikaeus__the_Unhallowed.groovy
@@ -1,4 +1,18 @@
 [
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_NONHUMAN_CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Undying);
+            permanent.addAbility(MagicUndyingTrigger.create());
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source != target;
+        }
+    },
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(
