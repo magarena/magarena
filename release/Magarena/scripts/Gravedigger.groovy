454c8e2c362b9e9f911af80666347c5cbb177274
melvin <melvinzhang@gmail.com>
2013-08-28 15:43:55 +0800

    use groovy anon class syntax

--- release/Magarena/scripts/Gravedigger.groovy
+++ release/Magarena/scripts/Gravedigger.groovy
@@ -17,12 +17,11 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                event.processTargetCard(game,new MagicCardAction() {
-                public void doAction(final MagicCard card) {
+                event.processTargetCard(game, {
+                    final MagicCard card ->
                     game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
                     game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
-                }
-                });
+                } as MagicCardAction);
             }
         }
     }
