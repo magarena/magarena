959c6e3b8a27b37574f9b51455b8deea3aa6296c
hong\ yie <unknown>
2013-08-29 15:59:39 +0000

    Add script for Vinelasher Kudzu

---
+++ release/Magarena/scripts/Vinelasher_Kudzu.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicLandfallTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN put a +1/+1 counter on SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+			game.doAction(new MagicChangeCountersAction(
+				event.getPermanent(),
+				MagicCounterType.PlusOne,
+				1,
+				true
+			));
+        }
+    }
+]
