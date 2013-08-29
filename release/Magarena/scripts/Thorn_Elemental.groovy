1afd65686f234324413136fac4c8228ad1fcd906
hong\ yie <unknown>
2013-08-29 15:06:34 +0000

    Add script for Thorn Elemental

---
+++ release/Magarena/scripts/Thorn_Elemental.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicWhenBecomesBlockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent attacker) {
+            return (permanent == attacker) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    this,
+                    "PN may\$ have SN deal its combat damage " +
+                    "to defending player as though it weren't blocked."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                final MagicPermanent permanent = event.getPermanent();
+                final MagicDamage damage = MagicDamage.Combat(
+                    permanent,
+                    event.getPlayer().getOpponent(),
+                    permanent.getPower()
+                );
+                game.doAction(new MagicDealDamageAction(damage));
+                game.doAction(MagicChangeStateAction.Set(
+                    permanent,
+                    MagicPermanentState.NoCombatDamage
+                ));
+            }
+        }
+    }
+]
