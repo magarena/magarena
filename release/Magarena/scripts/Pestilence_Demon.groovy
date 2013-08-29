13063722099e0e407a95926222fb0f9312b59ed3
hong\ yie <unknown>
2013-08-29 15:05:48 +0000

    Add script for Pestilence Demon

---
+++ release/Magarena/scripts/Pestilence_Demon.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.None),"Damage"
+    ) {
+        @Override
+        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN deals 1 damage to all creatures and players."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,1);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+            for (final MagicPlayer player : game.getPlayers()) {
+                final MagicDamage damage=new MagicDamage(event.getSource(),player,1);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
