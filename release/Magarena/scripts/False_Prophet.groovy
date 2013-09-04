30ebc403d34cb969805107bfc5d74dd2ee0eefa3
hong\ yie <unknown>
2013-09-04 15:05:53 +0000

    Add script for False Prophet

---
+++ release/Magarena/scripts/False_Prophet.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Exile all creatures."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicRemoveFromPlayAction(
+                    target,
+                    MagicLocationType.Exile
+                ));
+            }			
+        }
+    }
+]
