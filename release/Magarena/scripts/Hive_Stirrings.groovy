c425c27da4a2bcc813adbe26a29dc3d61cc17592
ember hauler <unknown>
2013-09-09 16:19:23 +0000

    Add script for Hive Stirrings

---
+++ release/Magarena/scripts/Hive_Stirrings.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts two 1/1 colorless Sliver creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Sliver"),
+                2
+            ));
+        }
+    }
+]
