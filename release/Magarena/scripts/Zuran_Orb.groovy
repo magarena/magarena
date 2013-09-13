b7fb380c5183c95524b1761d1a0df7ab1fed59bb
melvin <melvinzhang@gmail.com>
2013-09-13 10:19:36 +0800

    fix return type of getCostEvent

--- release/Magarena/scripts/Zuran_Orb.groovy
+++ release/Magarena/scripts/Zuran_Orb.groovy
@@ -4,7 +4,7 @@
         "Life+2"
     ) {
         @Override
-        public MagicEvent[] getCostEvent(final MagicPermanent source) {
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
             return [
                 new MagicSacrificePermanentEvent(
                     source,
