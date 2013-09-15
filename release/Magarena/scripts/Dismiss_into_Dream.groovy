c3016d3f2b61dffd13c883e119895568cc1568d9
melvin <melvinzhang@gmail.com>
2013-09-15 17:16:25 +0800

    added Dismiss into Dream

---
+++ release/Magarena/scripts/Dismiss_into_Dream.groovy
@@ -0,0 +1,11 @@
+[
+    new MagicStatic(
+        MagicLayer.Type,
+        MagicTargetFilter.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS
+    ) {
+         @Override
+         public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
+             flags.add(MagicSubType.Illusion);
+         }
+    }
+]
