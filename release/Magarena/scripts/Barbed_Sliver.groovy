0ab9cc44aa07fab020f616bab309a4ad37c3e796
Jericho Pumpkin <unknown>
2013-09-20 16:33:21 +0000

    Add script for Barbed Sliver

---
+++ release/Magarena/scripts/Barbed_Sliver.groovy
@@ -0,0 +1,37 @@
+
+def BarbedPump = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Barbed"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+0 until end of turn. "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new MagicChangeTurnPTAction(permanent,1,0));
+        }
+    };
+
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_SLIVER
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(BarbedPump);
+        }
+    }
+]
