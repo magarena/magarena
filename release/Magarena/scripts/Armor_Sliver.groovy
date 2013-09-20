83e927ec7d9aff8dab463108fc05c9153af97800
Jericho Pumpkin <unknown>
2013-09-20 16:32:03 +0000

    Add script for Armor Sliver

---
+++ release/Magarena/scripts/Armor_Sliver.groovy
@@ -0,0 +1,37 @@
+
+def ArmorPump = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Armor"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +0/+1 until end of turn. "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new MagicChangeTurnPTAction(permanent,0,1));
+        }
+    };
+
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_SLIVER
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(ArmorPump);
+        }
+    }
+]
