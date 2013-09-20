85ae9d324c3b05b34cf14c71d9a1cdadf91160f9
melvin <melvinzhang@gmail.com>
2013-09-20 22:18:25 +0800

    added Dragon Broodmother

---
+++ release/Magarena/scripts/Dragon_Broodmother.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 red and green Dragon creature token with flying and devour 2 onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("Dragon1")
+            ));
+        }
+    }
+]
