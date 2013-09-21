09ff8171a579ffdc08de87faaa4208f2e4553484
Jericho Pumpkin <unknown>
2013-09-21 20:10:34 +0000

    Add script for Opaline Sliver

---
+++ release/Magarena/scripts/Opaline_Sliver.groovy
@@ -0,0 +1,40 @@
+
+def OpalineDraw = new MagicWhenTargetedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicItemOnStack target) {
+            final MagicPlayer targetPlayer = target.getController();
+            return (target.containsInChoiceResults(permanent) &&
+                    targetPlayer != permanent.getController()) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+					new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                    this,
+                    "PN may draw a card."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            }
+        }
+    };
+
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_SLIVER
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {    
+            permanent.addAbility(OpalineDraw);
+        }
+    }
+]
