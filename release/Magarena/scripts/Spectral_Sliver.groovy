9db293c8cc15eab27015f84684553686dcccd15b
Jericho Pumpkin <unknown>
2013-09-24 11:40:10 +0000

    Add script for Spectral Sliver

---
+++ release/Magarena/scripts/Spectral_Sliver.groovy
@@ -0,0 +1,37 @@
+
+def SpectralPump = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Spectral"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +1/+1 until end of turn. "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new MagicChangeTurnPTAction(permanent,1,1));
+        }
+    };
+
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_SLIVER
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(SpectralPump);
+        }
+    }
+]
