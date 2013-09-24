3777cdc981b1434ee104be68122dea96c0454ce3
Jericho Pumpkin <unknown>
2013-09-24 11:38:36 +0000

    Add script for Frenzy Sliver

---
+++ release/Magarena/scripts/Frenzy_Sliver.groovy
@@ -0,0 +1,31 @@
+
+def FrenzyPump = new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    creature,
+                    this,
+                    "RN gets +1/0 until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent creature = event.getRefPermanent();
+            game.doAction(new MagicChangeTurnPTAction(creature, 1, 0));
+        }
+    };
+[    
+	new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_SLIVER
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(FrenzyPump);
+        }
+    }
+]
