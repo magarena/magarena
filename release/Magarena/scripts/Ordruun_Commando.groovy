34ae62c248deadc2da78891c80b732a333a9b6bb
Jericho Pumpkin <unknown>
2013-10-03 16:25:55 +0000

    Add script for Ordruun Commando

---
+++ release/Magarena/scripts/Ordruun_Commando.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Prevent 1"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{W}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Prevent the next damage that would be dealt to SN this turn"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {    
+            game.doAction(new MagicPreventDamageAction(event.getPermanent(),1));    
+        }
+    }
+]
