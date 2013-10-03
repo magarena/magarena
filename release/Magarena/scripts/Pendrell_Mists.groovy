fb4728dd31982c38160fa1e2a1cd2dd22f8ed24b
Jericho Pumpkin <unknown>
2013-10-03 18:53:52 +0000

    Add script for Pendrell Mists

---
+++ release/Magarena/scripts/Pendrell_Mists.groovy
@@ -0,0 +1,34 @@
+
+def MistsUpkeep = new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
+                    ),
+                    this,
+                    "You may\$ pay {1}\$. If you don't, destroy SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
+        }
+    };
+
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilter.TARGET_CREATURE
+    ) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(MistsUpkeep);
+        }
+    }
+]
