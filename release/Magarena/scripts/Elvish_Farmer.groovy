5481e64c680d65d713775f6990a3b5aadd37f48c
Jericho Pumpkin <unknown>
2013-10-07 19:23:42 +0000

    Add script for Elvish Farmer

---
+++ release/Magarena/scripts/Elvish_Farmer.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(
+                    source,
+                    MagicTargetChoice.SACRIFICE_SAPROLING
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains 2 life."
+            );
+        }
+        @Override
+		public void executeEvent(final MagicGame game, final MagicEvent event) {
+			game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
+		}
+    }
+]
