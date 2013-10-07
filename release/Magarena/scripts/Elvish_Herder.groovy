01920e3a2b59a5ac705b0e0e97900a4e16116dab
Jericho Pumpkin <unknown>
2013-10-07 19:26:29 +0000

    Add script for Elvish Herder

---
+++ release/Magarena/scripts/Elvish_Herder.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Trample"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{G}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicTrampleTargetPicker.create(),
+                this,
+                "Target creature\$ gains trample until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Trample));
+                }
+            });
+        }
+    }
+]
