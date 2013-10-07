f820f54b0f5dff641af1ffb0eeeea5d5a0d41f46
Jericho Pumpkin <unknown>
2013-10-07 19:24:58 +0000

    Add script for Elder Druid

---
+++ release/Magarena/scripts/Elder_Druid.groovy
@@ -0,0 +1,64 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{3}{G}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_CREATURE_OR_LAND,
+                MagicTapTargetPicker.Tap,
+                this,
+                "Tap target artifact, creature, or land\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicTapAction(creature, true));
+                }
+            });
+        }
+    },
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Untap"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{3}{G}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.Positive("target artifact, creature, or land"),
+                MagicTapTargetPicker.Untap,
+                this,
+                "Untap target artifact, creature, or land\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicUntapAction(creature));
+                }
+            });
+        }
+    }
+]
