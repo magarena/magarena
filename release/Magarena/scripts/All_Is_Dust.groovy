9df564ae4c30e6b95f30cf2f2feaaa4c627b1f31
Jericho Pumpkin <unknown>
2013-10-10 15:40:07 +0000

    Add script for All Is Dust

---
+++ release/Magarena/scripts/All_Is_Dust.groovy
@@ -0,0 +1,32 @@
+def COLORED_PERMANENT = new MagicPermanentFilterImpl() {
+        public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+            return (target.hasColor(MagicColor.Black) 
+				|| target.hasColor(MagicColor.Red) 
+				|| target.hasColor(MagicColor.White) 
+				|| target.hasColor(MagicColor.Green) 
+				|| target.hasColor(MagicColor.Blue));
+        }
+	};
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player sacrifices all colored permanents."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getPlayers()) {               
+				final Collection<MagicPermanent> targets =
+                    game.filterPermanents(player, COLORED_PERMANENT);
+				for (final MagicPermanent target : targets) {
+					game.doAction(new MagicSacrificeAction(target));
+				}    
+            }
+        }
+    }
+]
