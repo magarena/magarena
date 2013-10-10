4fa54bc7ac2b91cfaf19cbe05ad6193c0e2bf6a9
hong yie <unknown>
2013-10-10 11:15:53 +0000

    Add script for Ashen Rider

---
+++ release/Magarena/scripts/Ashen_Rider.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_PERMANENT,
+                MagicExileTargetPicker.create(),
+                this,
+                "When Ashen Rider enter battlefield, exile target permanent\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.Exile));
+                }
+            });
+        }
+    },
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_PERMANENT,
+                MagicExileTargetPicker.create(),
+                this,
+                "When Ashen Rider dies, exile target permanent\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.Exile));
+                }
+            });
+        }
+    }
+]
