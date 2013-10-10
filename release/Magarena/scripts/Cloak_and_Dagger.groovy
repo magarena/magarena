4d4a084e94591dce89b50d63d829dd6bbc26dede
Jericho Pumpkin <unknown>
2013-10-10 17:46:01 +0000

    Add script for Cloak and Dagger

---
+++ release/Magarena/scripts/Cloak_and_Dagger.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature() &&
+					otherPermanent.hasSubType(MagicSubType.Rogue)) ?
+                new MagicEvent(
+                    permanent,
+					new MagicMayChoice(),
+					otherPermanent,
+                    this,
+                    "You may attach SN to RN."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+				game.doAction(new MagicAttachAction(
+					event.getPermanent(),
+					event.getRefPermanent()
+				));
+			}
+        }
+    }
+]
