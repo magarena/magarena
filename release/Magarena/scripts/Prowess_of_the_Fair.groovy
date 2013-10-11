84f95a8b8d1475dca1973c88c539288f81d7aef4
Jericho Pumpkin <unknown>
2013-10-11 18:25:40 +0000

    Add script for Prowess of the Fair

---
+++ release/Magarena/scripts/Prowess_of_the_Fair.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenOtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (permanent != otherPermanent && otherPermanent.isCreature() 
+					&& otherPermanent.hasSubType(MagicSubType.Elf) && !otherPermanent.isToken()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    this,
+                    "PN may\$ put a 1/1 green Elf Warrior creature token onto the battlefield."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Elf1")));
+            }
+        }
+    }
+]
