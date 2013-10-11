c9f6e92d1c230ad3d5ef3f4362ec05e1b22136c7
Jericho Pumpkin <unknown>
2013-10-11 11:28:47 +0000

    Add script for Wings of Velis Vel

---
+++ release/Magarena/scripts/Wings_of_Velis_Vel.groovy
@@ -0,0 +1,39 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(4,4);
+    }
+};
+def AB1 = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        flags.add(MagicAbility.Flying);
+    }
+};
+def AB2 = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        flags.add(MagicAbility.Changeling);
+    }
+};
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                new MagicDestroyTargetPicker(true),
+                this,
+                "Target creature\$ becomes 4/4, gains all creature types and flying until the end of the turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                MagicPermanent creature ->
+                    game.doAction(new MagicBecomesCreatureAction(creature,PT,AB1,AB2));
+            } as MagicPermanentAction);
+        }
+    }
+]
