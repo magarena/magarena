5bdfb62d5fe2e0b9a83afdec9e56f4ed77307a8f
Jericho Pumpkin <unknown>
2013-10-11 18:26:13 +0000

    Add script for Faerie Tauntings

---
+++ release/Magarena/scripts/Faerie_Tauntings.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (permanent.isFriend(cardOnStack) && !permanent.isController(game.getTurnPlayer())) ?
+                new MagicEvent(
+					permanent,
+					new MagicMayChoice(),
+					this,
+					"You may have each opponent loses 1 life."
+				): MagicEvent.NONE;
+        }
+		@Override
+		public void executeEvent(final MagicGame game, final MagicEvent event) {
+			if (event.isYes()) {
+				game.doAction(new MagicChangeLifeAction(event.getPlayer().getOpponent(),-1));
+			}
+		}
+    }
+]
