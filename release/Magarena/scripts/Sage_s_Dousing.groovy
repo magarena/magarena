b67ab57cfa249a5d777dea47564052a5d083ead9
Jericho Pumpkin <unknown>
2013-10-11 21:03:36 +0000

    Add script for Sage's Dousing

---
+++ release/Magarena/scripts/Sage_s_Dousing.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {3}. " +
+				"If you control a Wizard, draw a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.addEvent(new MagicCounterUnlessEvent(event.getSource(),targetSpell,MagicManaCost.create("{3}")));
+					if(event.getPlayer().getNrOfPermanents(MagicSubType.Wizard) > 0){
+						game.doAction(new MagicDrawAction(event.getPlayer()));
+					}
+                }
+            });
+        }
+    }
+]
