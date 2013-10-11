9b6b99e68070d36807600c9564537b9ee30e013b
Jericho Pumpkin <unknown>
2013-10-11 22:07:53 +0000

    Add script for Diviner's Wand

---
+++ release/Magarena/scripts/Diviner_s_Wand.groovy
@@ -0,0 +1,80 @@
+def Draw = new MagicPermanentActivation(
+    new MagicActivationHints(MagicTiming.Token),
+    "Draw"
+) {
+
+     @Override
+    public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+        return [
+			new MagicPayManaCostEvent(source,"{4}")
+        ];
+    }
+
+    @Override
+	public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+		return new MagicEvent(
+			source,
+			this,
+			"PN draws a card."
+		);
+	}
+	@Override
+	public void executeEvent(final MagicGame game, final MagicEvent event) {
+		game.doAction(new MagicDrawAction(event.getPlayer(),1));
+	}
+};
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(Draw);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    },
+	new MagicWhenOtherDrawnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCard card) {
+            final MagicPermanent equippedCreature = permanent.getEquippedCreature();
+			return equippedCreature.isFriend(card) ?
+                new MagicEvent(
+					equippedCreature,
+					this,
+					"SN gets +1/+1 and gains flying until end of turn."
+				):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicGainAbilityAction(event.getPermanent(),MagicAbility.Flying));
+			game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,1));
+        }
+    },
+	new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isCreature() &&
+					otherPermanent.hasSubType(MagicSubType.Wizard)) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+					otherPermanent,
+                    this,
+                    "You may\$ attach SN to RN."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+				game.doAction(new MagicAttachAction(
+					event.getPermanent(),
+					event.getRefPermanent()
+				));
+			}
+        }
+    }
+]
