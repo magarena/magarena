3f8dce77fc5eef42e30497214acf910c1750f007
Jericho Pumpkin <unknown>
2013-10-11 11:31:17 +0000

    Add script for Hoofprints of the Stag

---
+++ release/Magarena/scripts/Hoofprints_of_the_Stag.groovy
@@ -0,0 +1,58 @@
+def YOUR_TURN_CONDITION = new MagicCondition() {
+        public boolean accept(final MagicSource source) {
+            final MagicGame game = source.getGame();
+            return (game.getTurnPlayer() == source.getController());
+        }
+    };
+[
+    new MagicWhenOtherDrawnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCard card) {
+            return permanent.isFriend(card) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_CHARGE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    this,
+                    "PN may\$ put a hoofprint counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,1,true));
+            }
+        }
+    },
+    new MagicPermanentActivation(
+		[
+            YOUR_TURN_CONDITION,
+        ],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{W}"),
+				new MagicRemoveCounterEvent(source,MagicCounterType.Charge,4)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 4/4 white Elemental creature token with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Elemental2")));
+        }
+    }
+]
