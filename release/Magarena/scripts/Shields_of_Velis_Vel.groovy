9b0b77c9f03438910a8ac28f2dae69e4f92f11c2
melvin <melvinzhang@gmail.com>
2013-10-14 10:23:08 +0800

    simplify groovy code

--- release/Magarena/scripts/Shields_of_Velis_Vel.groovy
+++ release/Magarena/scripts/Shields_of_Velis_Vel.groovy
@@ -6,14 +6,14 @@
                 cardOnStack,
                 MagicTargetChoice.POS_TARGET_PLAYER,
                 this,
-                "Creature PN controls get +0/+1 and gains all creature types until the end of the turn."
+                "Creatures PN controls get +0/+1 and gains all creature types until the end of the turn."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
-                    final Collection<MagicPermanent> targets = game.filterPermanents(player, MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                    final Collection<MagicPermanent> targets = player.filterPermanents(MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
                     for (final MagicPermanent creature : targets) {
                         game.doAction(new MagicChangeTurnPTAction(creature,0,1));
                         game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Changeling));
