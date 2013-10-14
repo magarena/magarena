006ed566b170a685473fcdc2b7f6580986942dd0
melvin <melvinzhang@gmail.com>
2013-10-14 10:26:32 +0800

    simplify groovy code

--- release/Magarena/scripts/Surge_of_Thoughtweft.groovy
+++ release/Magarena/scripts/Surge_of_Thoughtweft.groovy
@@ -11,14 +11,13 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                    event.getPlayer(),
-                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicPermanent target : targets) {
-                game.doAction(new MagicChangeTurnPTAction(target,1,1));
+            final MagicPlayer you = event.getPlayer();
+            final Collection<MagicPermanent> creatures = you.filterPermanents(MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent creature : creatures) {
+                game.doAction(new MagicChangeTurnPTAction(creature,1,1));
             }
-            if(event.getPlayer().getNrOfPermanents(MagicSubType.Kithkin) > 0){
-                game.doAction(new MagicDrawAction(event.getPlayer()));
+            if (you.controlsPermanent(MagicSubType.Kithkin)) {
+                game.doAction(new MagicDrawAction(you));
             }
         }
     }
