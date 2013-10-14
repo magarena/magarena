c229c81d7cf52c5d4d0dc8660ca213d1919fa09b
melvin <melvinzhang@gmail.com>
2013-10-14 11:00:56 +0800

    simplify groovy script

--- release/Magarena/scripts/Feudkiller_s_Verdict.groovy
+++ release/Magarena/scripts/Feudkiller_s_Verdict.groovy
@@ -5,21 +5,19 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "PN gains 10 life. Then if you have more life" +
-                " than an opponent, put a 5/5 white Giant Warrior creature token onto the battlefield."
+                "PN gains 10 life. Then if you have more life than an opponent, " +
+                "put a 5/5 white Giant Warrior creature token onto the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
             game.doAction(new MagicChangeLifeAction(player,10));
-            final boolean more = player.getLife() > player.getOpponent().getLife();
-            if (more) {
-                game.doAction(new MagicPlayTokensAction(
-                        player,
-                        TokenCardDefinitions.get("Giant5"),
-                        1
-                    ));
+            if (player.getLife() > player.getOpponent().getLife()) {
+                game.doAction(new MagicPlayTokenAction(
+                    player,
+                    TokenCardDefinitions.get("Giant5"),
+                ));
             }
         }
     }
