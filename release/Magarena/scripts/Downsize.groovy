8afbe131b380cbed3906ee9940efaa67edddb000
melvin <melvinzhang@gmail.com>
2013-10-15 17:49:08 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Downsize.groovy
+++ release/Magarena/scripts/Downsize.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
-                new MagicWeakenTargetPicker(4, 0),
-                this,
-                "Target creature\$ you don't control gets -4/-0 until end of turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature, -4, 0));
-                }
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Removal) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
