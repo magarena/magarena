dac4ea073c4cc21182b209f69a332da93476f20d
melvin <melvinzhang@gmail.com>
2013-10-15 11:15:12 +0800

    fix condition

--- release/Magarena/scripts/Godtracker_of_Jund.groovy
+++ release/Magarena/scripts/Godtracker_of_Jund.groovy
@@ -2,19 +2,31 @@
     new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (otherPermanent != permanent &&
-                    otherPermanent.isCreature() &&
-                    otherPermanent.getPower() >= 5) ?
+            return (otherPermanent.isCreature() &&
+                    otherPermanent.getPower() >= 5
+                    otherPermanent.isFriend(permanent)) ?
                 new MagicEvent(
                     permanent,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.ADD_PLUSONE_COUNTER,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
                     this,
-                    "Put a +1/+1 counter on SN."
+                    "PN may\$ put a +1/+1 counter on SN."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1,true));
+            if (event.isYes()) {
+                game.doAction(new MagicChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    1,
+                    true
+                ));
+            }
         }
     }
 ]
