390d9409b7d2b8ef4c27ca0e9eadf19f0d3ed3ce
melvin <melvinzhang@gmail.com>
2013-10-15 13:53:50 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Serum_Tank.groovy
+++ release/Magarena/scripts/Serum_Tank.groovy
@@ -18,33 +18,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,1,true));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Draw),
-        "Draw"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostTapEvent(source,"{3}"),
-                new MagicRemoveCounterEvent(
-                    source,
-                    MagicCounterType.Charge,
-                    1
-                )
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN draws a card"
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicDrawAction(event.getPlayer()));
-        }
     }
 ]
