49552eb70c2f4368b42ccdded6b104ce16bb5d07
melvin <melvinzhang@gmail.com>
2013-10-15 13:42:45 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Cautery_Sliver.groovy
+++ release/Magarena/scripts/Cautery_Sliver.groovy
@@ -14,38 +14,6 @@ def POS_TARGET_SLIVER_CREATURE_OR_PLAYER = new MagicTargetChoice(
     "target Sliver creature or player"
 )
 
-def CauteryDamage = new MagicPermanentActivation(
-    new MagicActivationHints(MagicTiming.Removal),
-    "Damage"
-) {
-    @Override
-    public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-        return [
-            new MagicPayManaCostEvent(source,"{1}"),
-            new MagicSacrificeEvent(source)
-        ];
-    }
-    @Override
-    public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            source,
-            MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-            new MagicDamageTargetPicker(1),
-            this,
-            "SN deals 1 damage to target creature or player\$."
-        );
-    }
-    @Override
-     public void executeEvent(final MagicGame game, final MagicEvent event) {
-        event.processTarget(game,new MagicTargetAction() {
-            public void doAction(final MagicTarget target) {
-                final MagicDamage damage=new MagicDamage(event.getSource(),target,1);
-                game.doAction(new MagicDealDamageAction(damage));
-            }
-        });
-    }
-};
-
 def CauteryPrevent = new MagicPermanentActivation(
     new MagicActivationHints(MagicTiming.Pump),
     "Prevent"
@@ -84,7 +52,6 @@ def CauteryPrevent = new MagicPermanentActivation(
     ) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            permanent.addAbility(CauteryDamage);
             permanent.addAbility(CauteryPrevent);
         }
     }
