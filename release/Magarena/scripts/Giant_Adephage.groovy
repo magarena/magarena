43fdf7fa051e42092dc40cf5c7316d50ad79053d
ember hauler <unknown>
2013-10-15 08:46:31 +0000

    Add script for Giant Adephage

---
+++ release/Magarena/scripts/Giant_Adephage.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return (damage.getSource() == permanent &&
+                    damage.isCombat() &&
+                    damage.getTarget().isPlayer()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+					"PN puts a token that's a copy of SN onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final MagicPermanent permanent = event.getPermanent();
+            final MagicCard card = MagicCard.createTokenCard(permanent.getCardDefinition(),player);
+            game.doAction(new MagicPlayCardAction(card,player));
+        }
+    }
+]
