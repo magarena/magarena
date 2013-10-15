ad2e73a01305e082a47bfcb9b9abe107744f6a7c
melvin <melvinzhang@gmail.com>
2013-10-15 17:45:25 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Esper_Battlemage.groovy
+++ release/Magarena/scripts/Esper_Battlemage.groovy
@@ -22,35 +22,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicPreventDamageAction(event.getPlayer(),2));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
-        "-1/-1"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostTapEvent(source,"{B}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.NEG_TARGET_CREATURE,
-                new MagicWeakenTargetPicker(1,1),
-                this,
-                "Target creature\$ gets -1/-1 until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature,-1,-1));
-                }
-            });
-        }
     }
 ]
