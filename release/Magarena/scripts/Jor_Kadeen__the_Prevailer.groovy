0da3883333424e98c85fa354431962811039acf2
Jericho Pumpkin <unknown>
2013-10-15 18:08:27 +0000

    Add script for Jor Kadeen, the Prevailer

---
+++ release/Magarena/scripts/Jor_Kadeen__the_Prevailer.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(
+		MagicLayer.ModPT,
+		MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+	) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
+                pt.add(3,0);
+            }
+        }
+    }
+]
