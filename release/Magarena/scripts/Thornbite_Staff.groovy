76c597fab2de8458f14ff4ea57d73a35a766f297
melvin <melvinzhang@gmail.com>
2013-10-15 13:46:52 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Thornbite_Staff.groovy
+++ release/Magarena/scripts/Thornbite_Staff.groovy
@@ -1,38 +1,3 @@
-def Ping = new MagicPermanentActivation(
-    new MagicActivationHints(MagicTiming.Removal),
-    "Damage"
-) {
-
-     @Override
-    public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-        return [
-            new MagicPayManaCostEvent(source,"{2}"),
-            new MagicTapEvent(source)
-        ];
-    }
-
-    @Override
-    public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            source,
-            MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-            new MagicDamageTargetPicker(1),
-            this,
-            "SN deals 1 damage to target creature or player\$."
-        );
-    }
-
-   @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        event.processTarget(game,new MagicTargetAction() {
-            public void doAction(final MagicTarget target) {
-                final MagicDamage damage=new MagicDamage(event.getSource(),target,1);
-                game.doAction(new MagicDealDamageAction(damage));
-            }
-        });
-    }
-};
-    
 def DiesUntap = new MagicWhenOtherDiesTrigger() {
     @Override
     public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
@@ -54,7 +19,6 @@ def DiesUntap = new MagicWhenOtherDiesTrigger() {
     new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
-            permanent.addAbility(Ping);
             permanent.addAbility(DiesUntap);
         }
         @Override
