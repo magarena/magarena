32a58e877e0865b8b4acacf4b4b1c19610701aca
ember hauler <unknown>
2013-10-16 06:35:18 +0000

    Add script for Guard Gomazoa

--- release/Magarena/scripts/Guard_Gomazoa.groovy
+++ release/Magarena/scripts/Guard_Gomazoa.groovy
@@ -2,7 +2,8 @@
     new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            if (damage.getTarget()==permanent&&damage.isCombat()) {
+            if (damage.isCombat() && 
+                (damage.getTarget() == permanent)) {
                 // Replacement effect. Generates no event or action.
                 damage.prevent();
             }
