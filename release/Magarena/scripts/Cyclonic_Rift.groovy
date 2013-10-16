582c7d4fbfaa74e2ad657623f95f09aa15a57ca5
melvin <melvinzhang@gmail.com>
2013-10-16 16:11:59 +0800

    replace groovy script with ability property

--- release/Magarena/scripts/Cyclonic_Rift.groovy
+++ release/Magarena/scripts/Cyclonic_Rift.groovy
@@ -1,25 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.TARGET_NONLAND_PERMANENT_YOU_DONT_CONTROL,
-                MagicBounceTargetPicker.create(),
-                this,
-                "Return target nonland permanent\$ you don't control to its owner's hand."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent permanent) {
-                    game.doAction(new MagicRemoveFromPlayAction(permanent,MagicLocationType.OwnersHand));
-                }
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Removal) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
