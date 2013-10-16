8b7b43f7571723b4f4f3f020d957eb469f3eb916
melvin <melvinzhang@gmail.com>
2013-10-16 21:02:12 +0800

    replace groovy code with ability in card script

--- src/magic/model/event/MagicPermanentActivation.java
+++ src/magic/model/event/MagicPermanentActivation.java
@@ -137,6 +137,8 @@ public abstract class MagicPermanentActivation extends MagicActivation<MagicPerm
                         events.add(new MagicUntapEvent(source));
                     } else if (cost.equals("{L1}")) {
                         events.add(new MagicPayLifeEvent(source, 1));
+                    } else if (cost.equals("{L7}")) {
+                        events.add(new MagicPayLifeEvent(source, 7));
                     } else if (cost.equals("{+1/+1}")) {
                         events.add(new MagicRemoveCounterEvent(source,MagicCounterType.PlusOne,1));
                     } else if (cost.equals("{C}")) {
