7d66484af06e730ea9f2b6282226356c1bd14a0d
melvin <melvinzhang@gmail.com>
2013-10-16 21:38:26 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Culling_Dais.groovy
+++ release/Magarena/scripts/Culling_Dais.groovy
@@ -1,38 +1,6 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump),
-        "Charge"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicTapEvent(source),
-                new MagicSacrificePermanentEvent(
-                    source,
-                    MagicTargetChoice.SACRIFICE_CREATURE
-                )
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Put a charge counter on SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeCountersAction(
-                event.getPermanent(),
-                MagicCounterType.Charge,
-                1,
-                true
-            ));
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
         "Draw"
     ) {
         @Override
