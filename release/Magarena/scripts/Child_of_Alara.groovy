f930b4f7c46add253776c5aeb09356b0d00509fd
ember hauler <unknown>
2013-10-16 08:31:22 +0000

    Add script for Child of Alara

---
+++ release/Magarena/scripts/Child_of_Alara.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+		public MagicEvent getEvent(final MagicPermanent permanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Destroy all nonland permanents. They can't be regenerated."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_NONLAND_PERMANENT);
+            for (final MagicPermanent target : targets) {
+                game.doAction(MagicChangeStateAction.Set(target,MagicPermanentState.CannotBeRegenerated));
+            }
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
