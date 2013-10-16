eb3560d55e09cecfd70c61ac3ec1f86367a074ea
melvin <melvinzhang@gmail.com>
2013-10-16 13:20:25 +0800

    replace groovy script with ability property

--- release/Magarena/scripts/Umezawa_s_Jitte.groovy
+++ release/Magarena/scripts/Umezawa_s_Jitte.groovy
@@ -24,63 +24,6 @@
             game.doAction(new MagicChangeTurnPTAction(event.getRefPermanent(),2,2));
         }
     },
-    // target creature gets -1/-1 until end of turn
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
-        "-1/-1"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.NEG_TARGET_CREATURE,
-                new MagicWeakenTargetPicker(1,1),
-                this,
-                "Target creature\$ gets -1/-1 until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature,-1,-1));
-                }
-            });
-        }
-    },
-
-    // you gain 2 life
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Life+2"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,1)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN gains 2 life."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
-        }
-    },
-
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
