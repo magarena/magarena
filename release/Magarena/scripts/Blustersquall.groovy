14f939e943f2f6ca94074fd2ac5e84740adfc530
melvin <melvinzhang@gmail.com>
2013-10-16 20:25:21 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Blustersquall.groovy
+++ release/Magarena/scripts/Blustersquall.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.TARGET_CREATURE_YOU_DONT_CONTROL,
-                MagicTapTargetPicker.Tap,
-                this,
-                "Tap target creature\$ you don't control."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicTapAction(creature,true));
-                }
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Tapping) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
