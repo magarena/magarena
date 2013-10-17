0d2cb1d28953a72561cefd3766d180bfed5605a7
melvin <melvinzhang@gmail.com>
2013-10-17 15:32:39 +0800

    replace groovy script with ability property

--- release/Magarena/scripts/Rakdos_Guildmage.groovy
+++ release/Magarena/scripts/Rakdos_Guildmage.groovy
@@ -1,35 +1,5 @@
 [
     new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal,true),
-        "-2/-2"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{3}{B}"),
-                new MagicDiscardEvent(source)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.NEG_TARGET_CREATURE,
-                new MagicWeakenTargetPicker(2,2),
-                this,
-                "Target creature\$ gets -2/-2 until end of turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature,-2,-2));
-                }
-            });
-        }
-    },
-    new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Token,true),
         "Token"
     ) {
