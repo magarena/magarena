f6ce1312038ddb3ad35f3eedb6a6f85e940fb10b
ember hauler <unknown>
2013-10-17 08:21:51 +0000

    Add script for Traumatize

---
+++ release/Magarena/scripts/Traumatize.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ puts the top half of his or her library, rounded down, into " +
+                "his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer target) {
+                    def amount = target.getLibrary().size().intdiv(2);
+                    game.doAction(new MagicMillLibraryAction(target,amount));
+                }
+            });
+        }
+    }
+]
