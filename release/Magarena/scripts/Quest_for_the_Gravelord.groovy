84f5f34c3b68a066f50de4fd827d59359be0d06e
melvin <melvinzhang@gmail.com>
2013-10-18 15:34:55 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Quest_for_the_Gravelord.groovy
+++ release/Magarena/scripts/Quest_for_the_Gravelord.groovy
@@ -1,28 +1,4 @@
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Token),
-        "Token"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,3),
-                new MagicSacrificeEvent(source)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Put a 5/5 black Zombie Giant creature token onto the battlefield."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("Zombie Giant")));
-        }
-    },
     new MagicWhenOtherDiesTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
