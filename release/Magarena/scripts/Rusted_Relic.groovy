9dbbb64a417e88dcd3292eaa5ecf0bb2759437f4
melvin <melvinzhang@gmail.com>
2013-10-19 21:09:24 +0800

    simplify groovy code

--- release/Magarena/scripts/Rusted_Relic.groovy
+++ release/Magarena/scripts/Rusted_Relic.groovy
@@ -2,27 +2,27 @@
     new MagicStatic(MagicLayer.SetPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
-                pt.set(5,5);
-            }
+            pt.set(5,5);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicCondition.METALCRAFT_CONDITION.accept(source);
         }
     }, 
     new MagicStatic(MagicLayer.Type) {
         @Override
-        public int getTypeFlags(final MagicPermanent permanent,final int flags) {
-            if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
-                return flags|MagicType.Creature.getMask();
-            }else{
-                return flags;
-            }
-        }
-    },
-    new MagicStatic(MagicLayer.Type) {
-        @Override
         public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
-            if (MagicCondition.METALCRAFT_CONDITION.accept(permanent)) {
-                flags.add(MagicSubType.Golem);
-            }
+            flags.add(MagicSubType.Golem);
         }
-    },
+        @Override
+        public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+            return flags |
+                   MagicType.Artifact.getMask() |
+                   MagicType.Creature.getMask();
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicCondition.METALCRAFT_CONDITION.accept(source);
+        }
+    }
 ]
