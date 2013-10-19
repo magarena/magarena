71a8d6b8c0b17d6b4869f256b86370a16bb3a8f6
melvin <melvinzhang@gmail.com>
2013-10-19 20:57:58 +0800

    remove unnecessary check

--- release/Magarena/scripts/Sands_of_Delirium.groovy
+++ release/Magarena/scripts/Sands_of_Delirium.groovy
@@ -17,7 +17,7 @@
                 MagicTargetChoice.NEG_TARGET_PLAYER,
                 amount,
                 this,
-                "Target player\$ puts the top "+amount+" cards of his or her library " +
+                "Target player\$ puts the top RN cards of his or her library " +
                 "into his or her graveyard."
             );
         }
@@ -26,10 +26,10 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game,new MagicPlayerAction() {
                 public void doAction(final MagicPlayer player) {
-                    final int amount = event.getRefInt();
-                    if (amount > 0) {
-                        game.doAction(new MagicMillLibraryAction(player,amount));
-                    }
+                    game.doAction(new MagicMillLibraryAction(
+                        player,
+                        event.getRefInt()
+                    ));
                 }
             });
         }
