0c8bec2fb10b2352df2faab4825401e9a0e30583
melvin <melvinzhang@gmail.com>
2013-10-19 20:59:28 +0800

    fix url link

--- release/Magarena/scripts/Child_of_Alara.groovy
+++ release/Magarena/scripts/Child_of_Alara.groovy
@@ -11,7 +11,7 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets =
-                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_NONLAND_PERMANENT);
+                game.filterPermanents(MagicTargetFilter.TARGET_NONLAND_PERMANENT);
             for (final MagicPermanent target : targets) {
                 game.doAction(MagicChangeStateAction.Set(target,MagicPermanentState.CannotBeRegenerated));
             }
