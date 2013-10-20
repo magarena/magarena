9ae5206d5bd3e02380e0766bd82c753e259947f7
melvin <melvinzhang@gmail.com>
2013-10-20 13:42:19 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Mishra_s_Factory.groovy
+++ release/Magarena/scripts/Mishra_s_Factory.groovy
@@ -46,35 +46,4 @@ def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
             game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,ST));
         }
     }
-    ,  
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Pump"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicTapEvent(source)];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.POS_TARGET_ASSEMBLY_WORKER_CREATURE,
-                MagicPumpTargetPicker.create(),
-                this,
-                "Target Assembly-Worker creature gets +1/+1 until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature,1,1));
-                }
-            });
-        }
-    }
 ]
