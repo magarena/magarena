21e1ef8df5c835ed69c5fad97449191cfa3f9efb
melvin <melvinzhang@gmail.com>
2013-10-20 20:48:05 +0800

    fix script bugs

--- release/Magarena/scripts/Liliana_s_Reaver.groovy
+++ release/Magarena/scripts/Liliana_s_Reaver.groovy
@@ -7,15 +7,15 @@
                     damage.getTarget().isPlayer()) ?
                 new MagicEvent(
                     permanent,
-                    this,
                     damage.getTarget(),
+                    this,
                     "RN discards a card and PN puts a 2/2 black Zombie creature token onto the battlefield."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new DiscardEvent(
+            game.addEvent(new MagicDiscardEvent(
                 event.getSource(),
                 event.getRefPlayer()
             ))
