8a63e8fb0685023730a48cb8b0af5d21598b92fa
melvin <melvinzhang@gmail.com>
2013-10-20 14:02:17 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Massacre_Wurm.groovy
+++ release/Magarena/scripts/Massacre_Wurm.groovy
@@ -1,22 +1,4 @@
 [
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                this,
-                "Creatures your opponent controls get -2/-2 until end of turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(event.getPlayer().getOpponent(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicPermanent target : targets) {
-                game.doAction(new MagicChangeTurnPTAction(target,-2,-2));
-            }
-        }
-    },
     new MagicWhenOtherDiesTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
