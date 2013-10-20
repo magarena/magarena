44145fe3f5404f89d8ac7f3849e4bf2a708518b1
melvin <melvinzhang@gmail.com>
2013-10-20 11:03:13 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Raka_Disciple.groovy
+++ release/Magarena/scripts/Raka_Disciple.groovy
@@ -1,35 +1,6 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump),
-        "Prevent"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostTapEvent(source, "{W}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER,
-                MagicPreventTargetPicker.getInstance(),
-                this,
-                "Prevent the next 1 damage that would be dealt to target creature or player\$ this turn"
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTarget(game,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    game.doAction(new MagicPreventDamageAction(target,1));
-                }
-            });
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
         "Flying"
     ) {
         @Override
