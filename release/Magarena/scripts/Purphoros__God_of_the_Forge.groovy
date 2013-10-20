43f28eadf51069eabde2d9c28f4998ed70a38555
melvin <melvinzhang@gmail.com>
2013-10-20 13:50:59 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Purphoros__God_of_the_Forge.groovy
+++ release/Magarena/scripts/Purphoros__God_of_the_Forge.groovy
@@ -36,35 +36,5 @@
                 )
             ));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Pump"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{2}{R}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Creatures PN controls get +1/+0 until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                event.getPlayer(),
-                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
-            );
-            for (final MagicPermanent creature : targets) {
-                game.doAction(new MagicChangeTurnPTAction(creature,1,0));
-            }
-        }
     }
 ]
