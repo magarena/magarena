d741303330342b6b592503cef23d489b9b701683
melvin <melvinzhang@gmail.com>
2013-10-20 11:18:23 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Dynacharge.groovy
+++ release/Magarena/scripts/Dynacharge.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
-                MagicPumpTargetPicker.create(),
-                this,
-                "Target creature\$ you control gets +2/+0 until end of turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicChangeTurnPTAction(creature, 2, 0));
-                }
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Pump) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
