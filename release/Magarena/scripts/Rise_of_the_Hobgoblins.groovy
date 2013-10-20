f52eb349044ee1c43a96460d06194a44502c934e
melvin <melvinzhang@gmail.com>
2013-10-20 16:52:26 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Rise_of_the_Hobgoblins.groovy
+++ release/Magarena/scripts/Rise_of_the_Hobgoblins.groovy
@@ -1,40 +1,4 @@
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Block,true,1),
-        "First strike"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{R/W}"),
-                new MagicPlayAbilityEvent(source)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Red creatures and white creatures you control gain first strike until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer player=event.getPlayer();
-            final Collection<MagicPermanent> targets=game.filterPermanents(
-                    player,
-                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-            for (final MagicPermanent creature : targets) {
-                if (creature.hasColor(MagicColor.Red) ||
-                    creature.hasColor(MagicColor.White)) {
-                    game.doAction(new MagicGainAbilityAction(creature,MagicAbility.FirstStrike));
-                }
-            }
-        }
-    },
     new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
