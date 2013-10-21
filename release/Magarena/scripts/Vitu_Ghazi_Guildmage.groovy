a4555367b8c2ac534fd0994ccf753adc61fbbe96
melvin <melvinzhang@gmail.com>
2013-10-21 20:50:47 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Vitu_Ghazi_Guildmage.groovy
+++ release/Magarena/scripts/Vitu_Ghazi_Guildmage.groovy
@@ -1,27 +1,6 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Flash),
-        "Token"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{4}{G}{W}")];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Put a 3/3 green Centaur creature token onto the battlefield."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Centaur3")));
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Flash),
         "Populate"
     ) {
         @Override
