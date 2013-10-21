5ad1bd3cbab8d8f59c50d1d136b7a44ba9e58b8d
melvin <melvinzhang@gmail.com>
2013-10-21 20:50:09 +0800

    correct token fullname

--- release/Magarena/scripts/Coursers__Accord.groovy
+++ release/Magarena/scripts/Coursers__Accord.groovy
@@ -5,12 +5,15 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "Put a 3/3 green Centaur creature token onto the battlefield, then populate."
+                "PN puts a 3/3 green Centaur creature token onto the battlefield, then populate."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Centaur3")));
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(), 
+                TokenCardDefinitions.get("3/3 green Centaur creature token")
+            ));
             game.addEvent(new MagicPopulateEvent(event.getSource()));
         }
     }
