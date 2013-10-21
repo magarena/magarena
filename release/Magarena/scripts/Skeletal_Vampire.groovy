6e934a2750917cdd0a59bc6bfa4afa362d615e88
melvin <melvinzhang@gmail.com>
2013-10-21 13:28:21 +0800

    replace groovy script with ability in card script

--- release/Magarena/scripts/Skeletal_Vampire.groovy
+++ release/Magarena/scripts/Skeletal_Vampire.groovy
@@ -1,46 +1,4 @@
-def action = {
-    final MagicGame game, final MagicEvent event ->
-    game.doAction(new MagicPlayTokensAction(
-        event.getPlayer(),
-        TokenCardDefinitions.get("Bat"),
-        2
-    ));
-} as MagicEventAction
-
-def getEvent = {
-    final MagicPermanent permanent ->
-    return new MagicEvent(
-        permanent,
-        action,
-        "PN puts two 1/1 black Bat creature tokens with flying onto the battlefield."
-    );
-}
-
 [
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
-            return getEvent(permanent);
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Token,true),
-        "Token"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{3}{B}{B}"),
-                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_BAT)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return getEvent(source);
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump),
         "Regen"
