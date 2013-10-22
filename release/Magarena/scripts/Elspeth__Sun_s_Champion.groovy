220bd105f92073ed272eb6140a98c148402d07dd
melvin <melvinzhang@gmail.com>
2013-10-22 13:35:40 +0800

    added Elspeth, Sun's Champion

---
+++ release/Magarena/scripts/Elspeth__Sun_s_Champion.groovy
@@ -0,0 +1,79 @@
+[
+    new MagicPlaneswalkerActivation(1) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put three 1/1 white Soldier creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("1/1 white Soldier creature token"),
+                3
+            ));
+        }
+    },
+    new MagicPlaneswalkerActivation(-3) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Destroy all creatures with power 4 or greater."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDestroyAction(
+                game.filterPermanents(MagicTargetFilter.TARGET_CREATURE_POWER_4_OR_MORE)
+            ));
+        }
+    },
+    new MagicPlaneswalkerActivation(-7) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gets an emblem with \"Creatures you control get +2/+2 and have flying.\""
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame outerGame, final MagicEvent event) {
+            outerGame.doAction(new MagicAddStaticAction(
+                new MagicStatic(
+                    MagicLayer.ModPT,
+                    MagicTargetFilter.ANY
+                ) {
+                    @Override
+                    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+                        pt.add(2, 2);
+                    }
+                    @Override
+                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+                        return target.getController().getId() == event.getPlayer().getId() && target.isCreature();
+                    }
+                }
+            ));
+            outerGame.doAction(new MagicAddStaticAction(
+                new MagicStatic(
+                    MagicLayer.Ability,
+                    MagicTargetFilter.ANY
+                ) {
+                    @Override
+                    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+                        flags.add(MagicAbility.Flying);
+                    }
+                    @Override
+                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+                        return target.getController().getId() == event.getPlayer().getId() && target.isCreature();
+                    }
+                }
+            ));
+        }
+    }
+]
