b353c1449a776742d2519afac15cb17eb5055ac3
melvin <melvinzhang@gmail.com>
2013-10-23 21:12:34 +0800

    remove duplication

--- release/Magarena/scripts/Noxious_Hatchling.groovy
+++ release/Magarena/scripts/Noxious_Hatchling.groovy
@@ -1,38 +1,26 @@
+def Trigger = {
+    final MagicColor color ->
+    return new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (permanent.isFriend(cardOnStack) &&
+                    cardOnStack.hasColor(color) &&
+                    permanent.getCounters(MagicCounterType.MinusOne) > 0) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Remove a -1/-1 counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.MinusOne,-1,true));
+        }
+    };
+}
+
 [
-    new MagicWhenOtherSpellIsCastTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
-            return (permanent.isFriend(cardOnStack) &&
-                    cardOnStack.hasColor(MagicColor.Black) &&
-                    permanent.getCounters(MagicCounterType.MinusOne) > 0) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "Remove a -1/-1 counter on SN."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.MinusOne,-1,true));
-        }
-    },
-    new MagicWhenOtherSpellIsCastTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
-            return (permanent.isFriend(cardOnStack) &&
-                    cardOnStack.hasColor(MagicColor.Green) &&
-                    permanent.getCounters(MagicCounterType.MinusOne) > 0) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "Remove a -1/-1 counter on SN."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.MinusOne,-1,true));
-        }
-    }
+    Trigger(MagicColor.Black),
+    Trigger(MagicColor.Green)
 ]
