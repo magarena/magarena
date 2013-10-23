6d8f7fd523160fcc56eadddcd0b3b4895751397a
melvin <melvinzhang@gmail.com>
2013-10-23 11:34:36 +0800

    replace groovy code with ability

--- release/Magarena/scripts/Geist_Honored_Monk.groovy
+++ release/Magarena/scripts/Geist_Honored_Monk.groovy
@@ -5,23 +5,5 @@
             final int amount = player.getNrOfPermanents(MagicType.Creature);
             pt.set(amount, amount);
         }
-    },
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                this,
-                "PN puts two 1/1 white Spirit creature tokens with flying onto the battlefield."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokensAction(
-                event.getPlayer(),
-                TokenCardDefinitions.get("Spirit2"),
-                2
-            ));
-        }
     }
 ]
