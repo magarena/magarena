0a3f4ee6999274fb01bb70b6924be9a4a10a0ad1
melvin <melvinzhang@gmail.com>
2013-10-23 16:03:07 +0800

    replace groovy code with ability

--- release/Magarena/scripts/Thallid.groovy
+++ release/Magarena/scripts/Thallid.groovy
@@ -14,28 +14,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,1,true));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Token),
-        "Token"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicRemoveCounterEvent(source,MagicCounterType.Charge,3)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN puts a a 1/1 green Saproling creature token onto the battlefield."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("1/1 green Saproling creature token")));
-        }
     }
 ]
