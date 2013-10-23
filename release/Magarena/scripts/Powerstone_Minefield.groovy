382c042c26374e3887070c8e85ec6adb45067e76
melvin <melvinzhang@gmail.com>
2013-10-23 21:19:33 +0800

    reduce duplication

--- release/Magarena/scripts/Powerstone_Minefield.groovy
+++ release/Magarena/scripts/Powerstone_Minefield.groovy
@@ -1,35 +1,29 @@
+def Action = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicDamage damage = new MagicDamage(event.getSource(),event.getRefPermanent(),2);
+    game.doAction(new MagicDealDamageAction(damage));
+} as MagicEventAction
+
+def Event = {
+    final MagicPermanent permanent, final MagicPermanent creature ->
+    return new MagicEvent(
+        permanent,
+        creature,
+        this,
+        "SN deals 2 damage to RN."
+    );
+}
+
 [
     new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return new MagicEvent(
-                permanent,
-                creature,
-                this,
-                "SN deals 2 damage to attacking creature."
-            );
+            return Event(permanent, creature);
         }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicDamage damage = new MagicDamage(event.getSource(),event.getRefPermanent(),2);
-            game.doAction(new MagicDealDamageAction(damage));
-        }
-        
     },
     new MagicWhenBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return new MagicEvent(
-                permanent,
-                creature,
-                this,
-                "SN deals 2 damage to blocking creature."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicDamage damage = new MagicDamage(event.getSource(),event.getRefPermanent(),2);
-            game.doAction(new MagicDealDamageAction(damage));
-        }
+            return Event(permanent, creature);
     }
 ]
