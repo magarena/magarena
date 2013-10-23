a4e1d25a46d33c8f7ca4770cb14895687b008317
melvin <melvinzhang@gmail.com>
2013-10-23 11:39:03 +0800

    replace groovy code with ability

--- release/Magarena/scripts/Twilight_Drover.groovy
+++ release/Magarena/scripts/Twilight_Drover.groovy
@@ -20,34 +20,5 @@
                 true
             ));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Token),
-        "Token"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{2}{W}"),
-                new MagicRemoveCounterEvent(source,MagicCounterType.PlusOne,1)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN puts two 1/1 white Spirit creature " +
-                "tokens with flying onto the battlefield."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokensAction(
-                event.getPlayer(),
-                TokenCardDefinitions.get("Spirit2"),
-                2
-            ));
-        }
     }
 ]
