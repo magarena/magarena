30a54957c4a019d96d0871f86253e507fc589b17
melvin <melvinzhang@gmail.com>
2013-10-25 15:21:21 +0800

    correct token name and filename

--- release/Magarena/scripts/Ajani_Goldmane.groovy
+++ release/Magarena/scripts/Ajani_Goldmane.groovy
@@ -52,7 +52,7 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("Avatar")));
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), TokenCardDefinitions.get("white Avatar creature token")));
         }
     }
 ]
