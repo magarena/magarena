4a265f98fc5d838a90721dbe683c800774db7ec1
melvin <melvinzhang@gmail.com>
2013-10-25 13:47:56 +0800

    fix syntax errors

--- release/Magarena/scripts/Phyrexian_Ingester.groovy
+++ release/Magarena/scripts/Phyrexian_Ingester.groovy
@@ -41,7 +41,7 @@ def NONTOKEN_CREATURE = new MagicPermanentFilterImpl() {
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final MagicCard card = source.getExiledCard();.
+            final MagicCard card = source.getExiledCard();
             pt.add(card.getPower(),card.getToughness());
         }
         @Override
