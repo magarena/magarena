846de544274ee0d13699a9bfba26d43d4997df4e
melvin <melvinzhang@gmail.com>
2013-10-28 17:25:38 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Ghost_Council_of_Orzhova.groovy
+++ release/Magarena/scripts/Ghost_Council_of_Orzhova.groovy
@@ -23,25 +23,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicExileUntilEndOfTurnAction(event.getPermanent()));
         }
-    },
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                MagicTargetChoice.TARGET_OPPONENT,
-                this,
-                "Target opponent\$ loses 1 life and PN gains 1 life."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPlayer(game,new MagicPlayerAction() {
-                public void doAction(final MagicPlayer player) {
-                    game.doAction(new MagicChangeLifeAction(player,-1));
-                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
-                }
-            });
-        }
     }
 ]
--- release/Magarena/scripts/Obzedat__Ghost_Council.groovy
+++ release/Magarena/scripts/Obzedat__Ghost_Council.groovy
@@ -43,26 +43,6 @@ def DelayedTrigger = {
     };
 }
 [
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                MagicTargetChoice.TARGET_OPPONENT,
-                this,
-                "Target opponent\$ loses 2 life and PN gains 2 life."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPlayer(game,new MagicPlayerAction() {
-                public void doAction(final MagicPlayer player) {
-                    game.doAction(new MagicChangeLifeAction(player,-2));
-                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
-                }
-            });
-        }
-    },
     new MagicAtEndOfTurnTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer eotPlayer) {
