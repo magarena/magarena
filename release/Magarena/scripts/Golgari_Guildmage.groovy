c9db797000fcb0d288357069a0fad7621a0e815b
melvin <melvinzhang@gmail.com>
2013-10-29 21:31:07 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Golgari_Guildmage.groovy
+++ release/Magarena/scripts/Golgari_Guildmage.groovy
@@ -1,39 +1,5 @@
 [
     new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Draw),
-        "Return"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{4}{B}"),
-                new MagicSacrificePermanentEvent(source, MagicTargetChoice.SACRIFICE_CREATURE)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
-                MagicGraveyardTargetPicker.ReturnToHand,
-                this,
-                "Return target creature card\$ from your graveyard to your hand."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {           
-            event.processTargetCard(game,new MagicCardAction() {
-                public void doAction(final MagicCard card) {
-                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
-                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
-                }
-            });
-        }
-    },
-    new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump),
         "Pump"
     ) {
