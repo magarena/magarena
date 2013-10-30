eb48589f4613d0275700f17775efde1451bb12ae
Jericho Pumpkin <unknown>
2013-10-30 10:45:14 +0000

    Add script for Grim Tutor

---
+++ release/Magarena/scripts/Grim_Tutor.groovy
@@ -0,0 +1,34 @@
+def CARD_FROM_LIBRARY = new MagicCardFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicCard target) {
+        return true;
+    }
+    public boolean acceptType(final MagicTargetType targetType) {
+        return targetType == MagicTargetType.Library;
+    }
+};
+
+def A_CARD_FROM_LIBRARY = new MagicTargetChoice(
+    CARD_FROM_LIBRARY,
+    "a card"
+);
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN searches his or her library for a card and put it into his or her hand. Then shuffle PN's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicSearchIntoHandEvent(
+                event,
+                A_CARD_FROM_LIBRARY
+            ));
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),-3));
+        }
+    }
+]
