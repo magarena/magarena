7aa33a5b96bc57b5bbdbc3bd5fcec64feec07930
Jericho Pumpkin <unknown>
2013-11-08 18:23:57 +0000

    Add script for Civic Guildmage

---
+++ release/Magarena/scripts/Civic_Guildmage.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Bounce"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostTapEvent(source, "{U}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                MagicBounceTargetPicker.create(),
+                this,
+                "Put target creature\$ you control on top of its owner's library."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent creature) {
+                    game.doAction(new MagicRemoveFromPlayAction(
+                        creature,
+                        MagicLocationType.TopOfOwnersLibrary
+                    ));
+                }
+            });
+        }
+    }
+]
