389dd77e76869b44448b017e6304b34b84f53445
Jericho Pumpkin <unknown>
2013-11-09 19:13:59 +0000

    Add script for Nivix Guildmage

---
+++ release/Magarena/scripts/Nivix_Guildmage.groovy
@@ -0,0 +1,57 @@
+[
+     new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}{U}{R}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN draws a card, then discards a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicDrawAction(player));
+            game.addEvent(new MagicDiscardEvent(event.getPermanent(),player));
+        }
+    },
+	new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Spell),
+        "Copy"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{2}{U}{R}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_INSTANT_OR_SORCERY_SPELL,
+                this,
+                "Copy target instant or sorcery spell\$. You may choose new targets for the copy."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game,new MagicCardOnStackAction() {
+                public void doAction(final MagicCardOnStack targetSpell) {
+                    game.doAction(new MagicCopyCardOnStackAction(event.getPlayer(),targetSpell));  
+                }
+            });
+        }
+    }	
+]
