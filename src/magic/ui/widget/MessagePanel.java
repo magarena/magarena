46a0a78cfbb189e9be75c2425c8117745c40adce
Lodici <Lodici>
2013-11-10 11:44:47 +0000

    - Refactoring MessagePanel; Log message displays health value below icon.

--- src/magic/ui/widget/MessagePanel.java
+++ src/magic/ui/widget/MessagePanel.java
@@ -4,12 +4,14 @@ import magic.data.IconImages;
 import magic.model.MagicMessage;
 import magic.ui.theme.Theme;
 import magic.ui.theme.ThemeFactory;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 
 public class MessagePanel extends JPanel {
 
@@ -20,28 +22,8 @@ public class MessagePanel extends JPanel {
         setBorder(FontsAndBorders.EMPTY_BORDER);
         setLayout(new BorderLayout(4,0));
         setOpaque(true);
-        if (message.getPlayer().getIndex()==1) {
-            setBackground(FontsAndBorders.GRAY2);
-        }
 
-        final JPanel leftPanel=new JPanel(new BorderLayout(2,0));
-        leftPanel.setOpaque(false);
-        add(leftPanel,BorderLayout.WEST);
-
-        final Theme theme=ThemeFactory.getInstance().getCurrentTheme();
-        final int face=message.getPlayer().getPlayerDefinition().getFace();
-        final JLabel playerLabel=new JLabel(theme.getAvatarIcon(face,1));
-        leftPanel.add(playerLabel,BorderLayout.WEST);
-
-        final int life=message.getLife();
-        final JLabel lifeLabel=new JLabel(String.valueOf(Math.abs(life)));
-        if (life<=0) {
-            lifeLabel.setForeground(Color.RED);
-        }
-        lifeLabel.setPreferredSize(new Dimension(35,0));
-        lifeLabel.setIcon(IconImages.REGENERATED);
-        lifeLabel.setIconTextGap(2);
-        leftPanel.add(lifeLabel,BorderLayout.EAST);
+        add(getPlayerPanel(message), BorderLayout.WEST);
 
         final TextLabel textLabel=new TextLabel(message.getText(),maxWidth,false);
         textLabel.setColors(Color.BLACK,Color.BLUE);
@@ -60,5 +42,36 @@ public class MessagePanel extends JPanel {
         phaseLabel.setFont(FontsAndBorders.FONT0);
         phaseLabel.setHorizontalAlignment(JLabel.RIGHT);
         gamePanel.add(phaseLabel,BorderLayout.SOUTH);
+    }
+
+    /**
+     *  Displays the player avatar & health.
+     */
+    private JPanel getPlayerPanel(final MagicMessage message) {
+        final JPanel playerPanel = new JPanel(new MigLayout("insets 0", "[]2[]"));
+        playerPanel.setOpaque(false);
+        playerPanel.add(getPlayerAvatar(message));
+        playerPanel.add(getPlayerLifePanel(message));
+        return playerPanel;
+    }
+
+    private JPanel getPlayerLifePanel(final MagicMessage message) {
+        final JPanel lifePanel = new JPanel(new MigLayout("insets 0, gap 0, flowy"));
+        lifePanel.setOpaque(false);
+        final int life=message.getLife();
+        final JLabel lifeLabel=new JLabel(String.valueOf(Math.abs(life)));
+        lifeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        if (life<=0) {
+            lifeLabel.setForeground(Color.RED);
+        }
+        lifePanel.add(new JLabel(IconImages.REGENERATED), "center");
+        lifePanel.add(lifeLabel);
+        return lifePanel;
+    }
+
+    private JLabel getPlayerAvatar(final MagicMessage message) {
+        final Theme theme = ThemeFactory.getInstance().getCurrentTheme();
+        final int face = message.getPlayer().getPlayerDefinition().getFace();
+        return new JLabel(theme.getAvatarIcon(face,1));
     }
 }
