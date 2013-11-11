778aa08bd77d491f4e6d744b7defdf82a723f84d
Lodici <Lodici>
2013-11-11 15:33:23 +0000

    - Removed splash from main screen; Version now displayed in titlebar.

--- src/magic/ui/VersionPanel.java
+++ src/magic/ui/VersionPanel.java
@@ -1,21 +1,14 @@
 package magic.ui;
 
 import magic.data.CardDefinitions;
-import magic.data.IconImages;
 import magic.model.MagicCardDefinition;
 import magic.model.MagicRandom;
 import magic.ui.viewer.CardViewer;
 import magic.ui.widget.FontsAndBorders;
 import magic.ui.widget.ZoneBackgroundLabel;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -30,12 +23,8 @@ public class VersionPanel extends JPanel implements ActionListener {
     private static final String VERSION = "1.44";
     private static final String VERSION_TEXT = "Magarena " + VERSION;
 
-    private static final Border LOGO_BORDER=BorderFactory.createMatteBorder(2,2,2,2,new Color(0x8C,0x78,0x53));
-
     private final MagicFrame frame;
     private final ZoneBackgroundLabel backgroundLabel;
-    private final JLabel versionLabel;
-    private final JLabel logoLabel;
     private final CardViewer cardViewer;
     private final JButton newButton;
     private final JButton loadButton;
@@ -43,23 +32,11 @@ public class VersionPanel extends JPanel implements ActionListener {
     public VersionPanel(final MagicFrame frame) {
 
         this.frame=frame;
+        frame.setTitle(VERSION_TEXT);
 
         setLayout(null);
 
-        versionLabel=new JLabel(VERSION_TEXT);
-        versionLabel.setForeground(Color.WHITE);
-        versionLabel.setIcon(IconImages.CUBE);
-        versionLabel.setFont(FontsAndBorders.FONT2);
-        versionLabel.setBorder(FontsAndBorders.EMPTY_BORDER);
-        add(versionLabel,BorderLayout.SOUTH);
-
         backgroundLabel=new ZoneBackgroundLabel();
-
-        final ImageIcon logoIcon=new ImageIcon(IconImages.LOGO);
-        logoLabel=new JLabel(logoIcon);
-        logoLabel.setBorder(LOGO_BORDER);
-        logoLabel.setSize(logoIcon.getIconWidth(),logoIcon.getIconHeight());
-        versionLabel.setSize(logoIcon.getIconWidth(),20);
 
         cardViewer=new CardViewer(true,true);
         final List<MagicCardDefinition> spellCards=CardDefinitions.getSpellCards();
@@ -79,8 +56,6 @@ public class VersionPanel extends JPanel implements ActionListener {
         add(newButton);
         add(loadButton);
         add(cardViewer);
-        add(versionLabel);
-        add(logoLabel);
         add(backgroundLabel);
 
         addComponentListener(new ComponentAdapter()  {
@@ -90,10 +65,6 @@ public class VersionPanel extends JPanel implements ActionListener {
 
                 final Dimension size=getSize();
                 backgroundLabel.setSize(size);
-                final int lx=(size.width-logoLabel.getWidth()-cardViewer.getWidth()-10)/2;
-                final int ly=(size.height-logoLabel.getHeight())/2;
-                logoLabel.setLocation(lx>0?lx:0,ly>0?ly:0);
-                versionLabel.setLocation(lx+10,ly+10);
                 cardViewer.setLocation(size.width-cardViewer.getWidth()-10,size.height-cardViewer.getHeight()-10);
                 newButton.setBounds(10,size.height-60,130,50);
                 loadButton.setBounds(150,size.height-60,130,50);
