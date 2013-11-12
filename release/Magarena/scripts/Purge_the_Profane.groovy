699ddb707a0b5bc37ef248da0677ae41e1499fa0
Jericho Pumpkin <unknown>
2013-11-12 09:46:19 +0000

    Add script for Purge the Profane

---
+++ release/Magarena/scripts/Purge_the_Profane.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ discards two cards and PN gains 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    game.addEvent(new MagicDiscardEvent(
+                        event.getSource(),
+                        player,
+                        2
+                    ));
+                    game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
+                }
+            });
+        }
+    }
+]
