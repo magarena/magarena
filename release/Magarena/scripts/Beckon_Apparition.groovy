1b80ab4c389f9f44c4b5ff13db599b8da843a387
Jericho Pumpkin <unknown>
2013-11-12 09:45:50 +0000

    Add script for Beckon Apparition

---
+++ release/Magarena/scripts/Beckon_Apparition.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CARD_FROM_ALL_GRAVEYARDS,
+                MagicGraveyardTargetPicker.ExileOpp,
+                this,
+                "Exile target card\$ from a graveyard. " +
+                "PN puts a 1/1 white and black Spirit creature token with flying onto the battlefield."
+            );
+        }
+		
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            
+			event.processTargetCard(game,new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicRemoveCardAction(
+                        card,
+                        MagicLocationType.Graveyard
+                    ));
+                    game.doAction(new MagicMoveCardAction(
+                        card,
+                        MagicLocationType.Graveyard,
+                        MagicLocationType.Exile
+                    ));
+					game.doAction(new MagicPlayTokenAction(
+                        event.getPlayer(),
+                        TokenCardDefinitions.get("1/1 white and black Spirit creature token with flying")
+                    ));
+                }
+            });
+        }
+    }
+]
