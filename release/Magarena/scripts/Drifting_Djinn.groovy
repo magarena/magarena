7520a4fbd9f7178f5f627c27080c9d1f2ae275f2
Shawn Howard <unknown>
2013-11-14 08:41:22 +0000

    Add script for Drifting Djinn

---
+++ release/Magarena/scripts/Drifting_Djinn.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        new MagicPayManaCostChoice(MagicManaCost.create("{1}{U}"))
+                    ),
+                    this,
+                    "PN may\$ pay {1}{U}\$. If you don't, sacrifice SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
+        }
+    }
+]
