43583b310d0221e6f2e2ed8bbac8a8356a9b8e0c
Jericho Pumpkin <unknown>
2013-11-15 10:40:38 +0000

    Add script for Magister Sphinx

---
+++ release/Magarena/scripts/Magister_Sphinx.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {     
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$'s life total becomes 10."
+            );
+        }
+		@Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game,new MagicPlayerAction() {
+                public void doAction(final MagicPlayer player) {
+                    final int amount = 10 - player.getLife();
+					game.doAction(new MagicChangeLifeAction(player,amount));
+                }
+            });
+        }
+    }    
+]
