0375d4dedd8a0b74f91314b00181f72f7b7cdb87
Jericho Pumpkin <unknown>
2013-11-15 10:39:04 +0000

    Add script for High Priest of Penance

---
+++ release/Magarena/scripts/High_Priest_of_Penance.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final int amount = damage.getDealtAmount();
+            return (damage.getTarget() == permanent) ?
+                new MagicEvent(
+                    permanent,
+					new MagicMayChoice(
+						MagicTargetChoice.NEG_TARGET_NONLAND_PERMANENT
+					),
+					new MagicDestroyTargetPicker(false),
+					this,
+					"Destroy target nonland permanent."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+			if(event.isYes()){
+				event.processTargetPermanent(game,new MagicPermanentAction() {
+					public void doAction(final MagicPermanent creature) {
+						game.doAction(new MagicDestroyAction(creature));
+					}
+				});
+			}
+        }
+    }
+]
