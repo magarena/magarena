b125d86f4f494fdec33af787bdb5b6df70d7185b
Jericho Pumpkin <unknown>
2013-11-19 13:50:41 +0000

    Add script for Shield of the Oversoul

---
+++ release/Magarena/scripts/Shield_of_the_Oversoul.groovy
@@ -0,0 +1,43 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1, 1);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+        }
+    },
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Flying);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+        }
+    },
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1, 1);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Green);
+        }
+    },
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            flags.add(MagicAbility.Indestructible);  
+        }
+
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Green);
+        }
+    }
+]
