8873151e3b9ff56a23e3f5fffb1de6b3eb92be89
Jericho Pumpkin <unknown>
2013-11-26 11:10:31 +0000

    Add script for 1/1 white and black Cleric creature token

---
+++ release/Magarena/scripts/1_1_white_and_black_Cleric_creature_token.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Animate"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostTapEvent(source,"{3}{W}{B}{B}"),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Return a creature card named Deathpact Angel from PN's graveyard to the battlefield under his or her control."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final List<MagicCard> cards = game.filterCards(
+                    event.getPlayer(),
+                    MagicTargetFilter.TARGET_CARD_FROM_GRAVEYARD);
+            for (final MagicCard card : cards) {
+                if (card.getName().equals("Deathpact Angel")) {
+                    game.doAction(new MagicReanimateAction(
+                        card,
+                        event.getPlayer()
+                    ));
+					break;
+                }
+            }
+        }
+    }
+]
