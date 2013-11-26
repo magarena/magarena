ec06818e45d386df0173d08a85a2d016f376e02a
Jericho Pumpkin <unknown>
2013-11-26 18:53:24 +0000

    Add script for Gaze of Granite

---
+++ release/Magarena/scripts/Gaze_of_Granite.groovy
@@ -0,0 +1,30 @@
+[
+	new MagicSpellCardEvent() {
+
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+			return new MagicEvent(
+                cardOnStack,
+				amount,
+                this,
+                "Destroy each nonland permanent with converted mana cost RN or less."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+			final int amount=event.getRefInt();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(
+                    event.getSource().getController(),
+                    new MagicTargetFilter.MagicCMCPermanentFilter(
+                        MagicTargetFilter.TARGET_NONLAND_PERMANENT,
+                        MagicTargetFilter.Operator.LESS_THAN_OR_EQUAL,
+                        amount
+                    )
+                );
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
