964014c7b1813af47860d333eecf3a2823f84221
melvin <melvinzhang@gmail.com>
2013-12-01 20:14:12 +0800

    replace annonymous class with closure

--- release/Magarena/scripts/Skyfire_Kirin.groovy
+++ release/Magarena/scripts/Skyfire_Kirin.groovy
@@ -24,14 +24,13 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                event.processTargetPermanent(game,new MagicPermanentAction() {
-                    public void doAction(final MagicPermanent creature) {
-                        game.doAction(new MagicGainControlAction(
-                            event.getPlayer(),
-                            creature,
-                            MagicStatic.UntilEOT
-                        ));
-                    }
+                event.processTargetPermanent(game, {
+                    final MagicPermanent creature ->
+                    game.doAction(new MagicGainControlAction(
+                        event.getPlayer(),
+                        creature,
+                        MagicStatic.UntilEOT
+                    ));
                 });
             }
         }
