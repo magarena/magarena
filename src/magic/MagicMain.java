afc59754737fd7faa96f14e2e041de15c854729a
Lodici <Lodici>
2013-12-01 18:38:31 +0000

    - Moved version info to MagicMain so it can be accessed without requiring VersionPanel to be loaded.

--- src/magic/MagicMain.java
+++ src/magic/MagicMain.java
@@ -18,7 +18,7 @@ import java.io.File;
 
 public class MagicMain {
 
-    public static final String VERSION = "1.44";
+    public static final String VERSION = "1.45";
     public static final String SOFTWARE_TITLE = "Magarena " + VERSION;
 
     public static JFrame rootFrame;
