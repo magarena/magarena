605572a35e0fb6855d90c0b60a53322e9127a7b9
melvin <melvinzhang@gmail.com>
2013-12-01 20:07:44 +0800

    remove raw content, to be integrated later

--- release/Magarena/scripts/Legacy_Weapon.groovy
+++ release/Magarena/scripts/Legacy_Weapon.groovy
@@ -24,10 +24,9 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
-                }
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.Exile));
             });
         }
     }
--- release/Magarena/scripts/Martial_Law.groovy
+++ release/Magarena/scripts/Martial_Law.groovy
@@ -14,10 +14,9 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
-                public void doAction(final MagicPermanent creature) {
-                    game.doAction(new MagicDetainAction(event.getPlayer(), creature));
-                }
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicDetainAction(event.getPlayer(), creature));
             });
         }
     }
