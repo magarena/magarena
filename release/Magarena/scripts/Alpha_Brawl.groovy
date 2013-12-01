49e263a3984543d941f6d9b163b84dc613b899f2
melvin <melvinzhang@gmail.com>
2013-12-01 20:56:44 +0800

    replace annonymous class with closure

--- release/Magarena/scripts/Alpha_Brawl.groovy
+++ release/Magarena/scripts/Alpha_Brawl.groovy
@@ -15,27 +15,26 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game,new MagicPermanentAction() {
+            event.processTargetPermanent(game, {
+                final MagicPermanent permanent ->
                 final MagicPlayer player = event.getPlayer();
-                public void doAction(final MagicPermanent permanent) {
-                    final Collection<MagicPermanent> creatures =
-                            game.filterPermanents(player.getOpponent(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-                    for (final MagicPermanent creature : creatures) {
-                        final MagicDamage damage = new MagicDamage(
-                            permanent,
-                            creature,
-                            permanent.getPower()
-                        );
-                        game.doAction(new MagicDealDamageAction(damage));
-                    }
-                    for (final MagicPermanent creature : creatures) {
-                        final MagicDamage damage = new MagicDamage(
-                            creature,
-                            permanent,
-                            creature.getPower()
-                        );
-                        game.doAction(new MagicDealDamageAction(damage));
-                    }
+                final Collection<MagicPermanent> creatures =
+                        game.filterPermanents(player.getOpponent(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                for (final MagicPermanent creature : creatures) {
+                    final MagicDamage damage = new MagicDamage(
+                        permanent,
+                        creature,
+                        permanent.getPower()
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+                for (final MagicPermanent creature : creatures) {
+                    final MagicDamage damage = new MagicDamage(
+                        creature,
+                        permanent,
+                        creature.getPower()
+                    );
+                    game.doAction(new MagicDealDamageAction(damage));
                 }
             });
         }
