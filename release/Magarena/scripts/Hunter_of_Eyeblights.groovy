12ec2905435e779854aaa47f520e52ba526bacf9
Jericho Pumpkin <unknown>
2013-12-08 20:50:47 +0000

    Add script for Hunter of Eyeblights

---
+++ release/Magarena/scripts/Hunter_of_Eyeblights.groovy
@@ -0,0 +1,39 @@
+def TARGET_CREATURE_WITH_COUNTER = new MagicPermanentFilterImpl() {
+	public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+		return target.isCreature() && target.hasCounters();
+	}
+};
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source), new MagicPayManaCostEvent(source,"{2}{B}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                new MagicTargetChoice(TARGET_CREATURE_WITH_COUNTER, MagicTargetHint.Negative, "a creature with a counter on it"),
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target creature\$ with a counter on it."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game,new MagicPermanentAction() {
+                public void doAction(final MagicPermanent permanent) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
