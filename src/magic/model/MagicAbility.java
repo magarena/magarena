2432fbb194f93c030245975b5db5a6d860e5025b
melvin <melvinzhang@gmail.com>
2013-12-15 09:30:28 +0800

    complete implementation of MagicAbility.ProtectionFromLegendaryCreatures

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -154,7 +154,7 @@ public enum MagicAbility {
     ProtectionFromGoblins("protection from Kavu",10),
     ProtectionFromKavu("protection from Goblins",10),
     ProtectionFromSnow("protection from snow",10),
-    ProtectionFromLegendary("protection from legendary",10),
+    ProtectionFromLegendaryCreatures("protection from legendary creatures",10),
     Reach("reach",20),
     Shadow("shadow",30),
     Shroud("shroud",60),
