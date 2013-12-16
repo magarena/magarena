5bbf7dede7b557e7ce78372e69489403a930be8a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2013-12-16 17:52:14 +0000

    New Card Scripts

---
+++ release/Magarena/scripts/Wind_Zendikon.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenOtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent died) {
+            final MagicPermanent enchanted = permanent.getEnchantedCreature();
+            return (enchanted == died) ?
+                new MagicEvent(
+                    permanent,
+                    enchanted.getCard(),
+                    this,
+                    "Return RN to its owner's hand."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard card = event.getRefCard();
+            if (card.isInGraveyard()) {
+                game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+            }
+        }
+    }
+]
