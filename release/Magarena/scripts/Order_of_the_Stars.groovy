d401ef3f4887dfa0787d75dc323bfb9cbe1de589
ShawnieBoy <ShawnieBoyUK@gmail.com>
2013-12-26 01:48:50 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Order_of_the_Stars.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                MagicColorChoice.ALL_INSTANCE,
+                this,
+                "Choose a color\$. " +
+                "SN has protection from chosen color."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicColor color = event.getChosenColor();
+            final MagicAbility protection = color.getProtectionAbility();
+            game.doAction(new MagicAddStaticAction(event.getPermanent(), new MagicStatic(
+                MagicLayer.Ability) {
+                    @Override
+                    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+                        flags.add(protection);
+                    }
+                }
+            ));
+        }
+    }
+]
