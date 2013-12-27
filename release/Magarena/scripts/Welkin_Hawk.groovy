7f4977e03485972ee4499d0a9c5d66f8229902f1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2013-12-27 00:58:52 +0000

    Add Card Welkin Hawk

---
+++ release/Magarena/scripts/Welkin_Hawk.groovy
@@ -0,0 +1,35 @@
+def CARD_NAMED_WELKIN_HAWK = new MagicCardFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicCard target) {
+        return target.getName().equals("Welkin Hawk");
+    }
+    public boolean acceptType(final MagicTargetType targetType) {
+        return targetType==MagicTargetType.Library;
+    }
+};
+
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicMoveCardAction data) {      
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(),
+                this,
+                "When SN dies, you may\$ search your library for a card named Welkin Hawk" + 
+                "and put that card into your hand. Then shuffle your library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.addEvent(new MagicSearchIntoHandEvent(
+                    event,
+                    new MagicTargetChoice(
+                        CARD_NAMED_WELKIN_HAWK, 
+                        "a card named Welkin Hawk"
+                    )
+                ));
+            }
+        }
+    }
+]
