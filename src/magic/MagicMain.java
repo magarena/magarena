7bb2be2e5c1be1c55ffcc0cb35b7e50e063364eb
melvin <melvinzhang@gmail.com>
2014-01-03 14:01:44 +0800

    bump version to 1.46

--- src/magic/MagicMain.java
+++ src/magic/MagicMain.java
@@ -22,7 +22,7 @@ import java.nio.file.Paths;
 
 public class MagicMain {
 
-    public static final String VERSION = "1.45";
+    public static final String VERSION = "1.46";
     public static final String SOFTWARE_TITLE = "Magarena " + VERSION;
 
     public static JFrame rootFrame;
