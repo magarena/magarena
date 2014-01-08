b69c8f0a2ca6fc51983e77150b0355bc66c1c7bd
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-08 18:25:33 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Haunted_Fengraf.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.None),
+        "Return"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source),
+                    new MagicPayManaCostEvent(source,"{3}"),
+                    new MagicSacrificeEvent(source)];
+        }
+        
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Return a creature card at random from your graveyard to your hand."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final List<MagicCard> targets = game.filterCards(player,MagicTargetFilter.TARGET_CREATURE_CARD_FROM_GRAVEYARD);
+            if (targets.size() > 0) {
+                final MagicPermanent permanent = event.getPermanent();
+                final MagicRandom rng = new MagicRandom(player.getGraveyard().getStateId());
+                final int index = rng.nextInt(targets.size());
+                final MagicCard card = targets.get(index);
+                game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+            }
+        }
+    }
+]
