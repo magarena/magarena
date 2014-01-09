92cf8994a51183bdf201c35a1ab3c92145cbe4ff
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-09 18:09:08 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Hunted_Horror.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "Target opponent\$ puts two 3/3 green Centaur creature tokens with protection from black onto the battlefield."
+            )
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer opponent ->
+               game.doAction(new MagicPlayTokensAction(
+                opponent,
+                TokenCardDefinitions.get("3/3 green Centaur creature token with protection from black"),
+                2
+            )); 
+            })
+        }
+    }
+]
