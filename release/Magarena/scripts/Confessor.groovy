0d2705ed432b98e0ef379be063cee7146ad7b301
melvin <melvinzhang@gmail.com>
2014-01-12 13:37:46 +0800

    fix missing return keyword

--- release/Magarena/scripts/Confessor.groovy
+++ release/Magarena/scripts/Confessor.groovy
@@ -2,17 +2,16 @@
     new MagicWhenDiscardedTrigger() {
         @Override
         public MagicEvent getEvent(final MagicPermanent permanent,final MagicCard card) {
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.GAIN_LIFE,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    this,
-                    "PN may\$ gain 1 life."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.GAIN_LIFE,
+                    1,
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
+                this,
+                "PN may\$ gain 1 life."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
