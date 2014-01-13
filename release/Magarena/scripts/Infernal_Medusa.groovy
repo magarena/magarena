75eb635f82a917d2247f8ecdd11c5e41ce063c11
melvin <melvinzhang@gmail.com>
2014-01-13 14:15:49 +0800

    simplify and correct groovy script

--- release/Magarena/scripts/Infernal_Medusa.groovy
+++ release/Magarena/scripts/Infernal_Medusa.groovy
@@ -1,9 +1,9 @@
 [
-    new MagicWhenBecomesBlockedTrigger() {
+    new MagicWhenBlocksOrBecomesBlockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
             final MagicPermanent target = permanent == blocker ? blocker.getBlockedCreature() : blocker;
-            return !target.hasSubType(MagicSubType.Wall) ?
+            return permanent == blocker || !target.hasSubType(MagicSubType.Wall) ?
                 new MagicEvent(
                     permanent,
                     target,
@@ -22,28 +22,5 @@
                 ))
             });
         }
-    },
-    new MagicWhenBlocksTrigger() {
-       @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
-            final MagicPermanent target = permanent == blocker ? blocker.getBlockedCreature() : blocker;
-            return 
-                new MagicEvent(
-                    permanent,
-                    target,
-                    this,
-                    "Destroy RN at end of combat."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processRefPermanent(game, {
-                final MagicPermanent permanent ->
-                game.doAction(new MagicAddTriggerAction(
-                    permanent,
-                    MagicAtEndOfCombatTrigger.Destroy
-                ))
-            });
-        }
+    }
 ]
