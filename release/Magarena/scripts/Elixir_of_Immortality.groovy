9b8be233c79b8c954f62cd25a87cc13c1d9fb453
melvin <melvinzhang@gmail.com>
2014-01-16 13:12:02 +0800

    add missing groovy script

---
+++ release/Magarena/scripts/Elixir_of_Immortality.groovy
@@ -0,0 +1,45 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{2}"),
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gains 5 life, then shuffle SN and his or her graveyard into their owner's library."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicChangeLifeAction(player, 5));
+
+            game.doAction(new MagicRemoveFromPlayAction(event.getPermanent(), MagicLocationType.OwnersLibrary));
+            
+            final MagicCardList graveyard = new MagicCardList(player.getGraveyard());
+            for (final MagicCard card : graveyard) {
+                game.doAction(new MagicRemoveCardAction(
+                    card,
+                    MagicLocationType.Graveyard
+                ));
+                game.doAction(new MagicMoveCardAction(
+                    card,
+                    MagicLocationType.Graveyard,
+                    MagicLocationType.OwnersLibrary
+                ));
+            }
+        }
+    }
+]
