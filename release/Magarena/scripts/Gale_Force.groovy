43184fae9035011487ebc3d739b5c9dd64a5b01d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-16 19:58:12 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Gale_Force.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "SN deals 5 damage to each creature with flying."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicSource source = event.getSource();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_WITH_FLYING);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(source,target,5);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
