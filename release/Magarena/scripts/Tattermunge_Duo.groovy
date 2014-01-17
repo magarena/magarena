41d83dc088ba1ecb43dae544a6daa3968060da1b
Jericho Pumpkin <unknown>
2014-01-17 10:38:17 +0000

    Add script for Tattermunge Duo

---
+++ release/Magarena/scripts/Tattermunge_Duo.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (permanent.isFriend(cardOnStack) &&
+                    cardOnStack.hasColor(MagicColor.Red)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN gets +1/+1 until the end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,1));
+        }
+    },
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (permanent.isFriend(cardOnStack) &&
+                    cardOnStack.hasColor(MagicColor.Green)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN gains forestwalk until the end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicGainAbilityAction(event.getPermanent(), MagicAbility.Forestwalk));
+        }
+    }
+]
