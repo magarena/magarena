db3c212a35544459167f79762be96b8fe46e4ed6
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-17 02:26:36 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Goblin_War_Wagon.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return (permanent.isController(upkeepPlayer)&&
+                    permanent.isTapped()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        new MagicPayManaCostChoice(MagicManaCost.create("{2}"))
+                    ),
+                    this,
+                    "PN may\$ pay {2}. If you do, untap SN."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicUntapAction(event.getPermanent()));
+            }
+        }
+    }
+]
