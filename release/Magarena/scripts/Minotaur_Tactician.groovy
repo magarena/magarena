bde303347ff601d543472e0bc2a5728e843c8f07
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-21 17:52:20 +0000

    Correct Minotaur_Tactician

--- release/Magarena/scripts/Minotaur_Tactician.groovy
+++ release/Magarena/scripts/Minotaur_Tactician.groovy
@@ -2,7 +2,7 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (source.getController().controlsPermanent(MagicColor.White)) {
+            if (source.getController().controlsPermanent(MagicTargetFilter.TARGET_WHITE_CREATURE_YOU_CONTROL)) {
                 pt.add(1,1);
             }
         }
@@ -11,7 +11,7 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (source.getController().controlsPermanent(MagicColor.Blue)) {
+            if (source.getController().controlsPermanent(MagicTargetFilter.TARGET_BLUE_CREATURE_YOU_CONTROL)) {
                 pt.add(1,1);
             }
         }
