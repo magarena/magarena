c54ecd1601423226f44f1cfaae4d7e59cc7b99e0
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-21 22:05:33 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Miren__the_Moaning_Well.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Gain Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{3}"),
+                new MagicSacrificePermanentEvent(
+                    source,
+                    MagicTargetChoice.SACRIFICE_CREATURE
+                )
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getTarget(),
+                this,
+                "PN gains life equal to RN's toughness."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int sacrificed = event.getRefPermanent().getToughness();
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),sacrificed));
+        }
+    }
+]
