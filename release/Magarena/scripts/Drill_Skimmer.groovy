61885564f7132b64482cd3ae00c89bb84e3a4df3
melvin <melvinzhang@gmail.com>
2014-01-22 11:55:47 +0800

    correct method signature

--- release/Magarena/scripts/Drill_Skimmer.groovy
+++ release/Magarena/scripts/Drill_Skimmer.groovy
@@ -1,10 +1,10 @@
 [
     new MagicStatic(MagicLayer.Ability) {
         @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
             final MagicPermanentFilterImpl filter = new MagicOtherPermanentTargetFilter(MagicTargetFilter.TARGET_ARTIFACT_CREATURE, source);
             if (source.getController().controlsPermanent(filter)) {
-                flags.add(MagicAbility.Flying);
+                flags.add(MagicAbility.Shroud);
             }
         }
     }
