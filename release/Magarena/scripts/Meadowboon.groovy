1bfdef2011a45a00da68634d251abeaba53aad73
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-01-23 01:55:51 +0000

    New Card Scripts + Groovy

---
+++ release/Magarena/scripts/Meadowboon.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicWhenLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicRemoveFromPlayAction act) {
+            return act.isPermanent(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_PLAYER,
+                    this,
+                    "PN puts a +1/+1 counter on each creature target player\$ controls."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer targetPlayer ->
+                final Collection<MagicPermanent> targets = game.filterPermanents(
+                    targetPlayer,
+                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+                );
+                for (final MagicPermanent target : targets) {
+                    game.doAction(new MagicChangeCountersAction(target,MagicCounterType.PlusOne,1,true));
+                }
+            });
+        }
+    }
+]
