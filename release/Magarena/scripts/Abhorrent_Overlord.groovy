f5e9b49e9f2143e5611ada630e005a7cbdcd79fe
Jericho Pumpkin <unknown>
2014-01-24 17:37:38 +0000

    Add script for Abhorrent Overlord

---
+++ release/Magarena/scripts/Abhorrent_Overlord.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN sacrifices a creature."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent=event.getPermanent();
+            final MagicPlayer player=event.getPlayer();
+            game.addEvent(new MagicSacrificePermanentEvent(permanent,player,MagicTargetChoice.SACRIFICE_CREATURE));
+        }
+    },
+	new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 black Harpy creature token with flying onto the battlefield for each black mana symbol in the mana costs of permanents he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getDevotion(MagicColor.Black);
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("1/1 black Harpy creature token with flying"),
+                amount
+            ));
+        }
+    }
+]
