a3b59374663f832f2a97a8354d736274173431ff
melvin <melvinzhang@gmail.com>
2014-02-11 20:33:42 +0800

    normalize script, correct order of subtypes

--- release/Magarena/scripts/Deus_of_Calamity.groovy
+++ release/Magarena/scripts/Deus_of_Calamity.groovy
@@ -1,7 +1,7 @@
 def LAND_YOUR_OPPONENT_CONTROLS = new MagicPermanentFilterImpl() {
     public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
-		return target.isLand() && target.isOpponent(player);
-	}
+        return target.isLand() && target.isOpponent(player);
+    }
 };
 
 def TARGET_LAND_YOUR_OPPONENT_CONTROLS = new MagicTargetChoice(
@@ -15,19 +15,19 @@ def TARGET_LAND_YOUR_OPPONENT_CONTROLS = new MagicTargetChoice(
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return (damage.getSource() == permanent &&
                     permanent.isOpponent(damage.getTarget()) &&
-					damage.getAmount() >= 6) ?
-				new MagicEvent(
-					permanent,
-					TARGET_LAND_YOUR_OPPONENT_CONTROLS,
-					MagicDestroyTargetPicker.Destroy,
-					this,
-					"Destroy target land\$."
-				):
+                    damage.getAmount() >= 6) ?
+                new MagicEvent(
+                    permanent,
+                    TARGET_LAND_YOUR_OPPONENT_CONTROLS,
+                    MagicDestroyTargetPicker.Destroy,
+                    this,
+                    "Destroy target land\$."
+                ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-			event.processTargetPermanent(game, {
+            event.processTargetPermanent(game, {
                 final MagicPermanent land ->
                 game.doAction(new MagicDestroyAction(land));
             });
