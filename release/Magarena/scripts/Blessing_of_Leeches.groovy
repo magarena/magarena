26a63428d3d3c4d3ebfbe24773224865ee39c9b1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-13 03:28:19 +0000

    Change Blessing of Leeches to regenerate enchanted, not give ability

---
+++ release/Magarena/scripts/Blessing_of_Leeches.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Regen"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{0}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                source.getEnchantedCreature(),
+                this,
+                "Regenerate RN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicRegenerateAction(event.getRefPermanent()));
+        }
+    }
+]
