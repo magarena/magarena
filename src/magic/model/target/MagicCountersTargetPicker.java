7ab2089dbe3598ba97cecbffa7ecdcc45490d26a
melvin <melvinzhang@gmail.com>
2014-02-13 10:09:21 +0800

    made value final as it is used only in the loop

--- src/magic/model/target/MagicCountersTargetPicker.java
+++ src/magic/model/target/MagicCountersTargetPicker.java
@@ -14,16 +14,11 @@ public class MagicCountersTargetPicker extends MagicTargetPicker<MagicPermanent>
     @Override
     protected int getTargetScore(final MagicGame game,final MagicPlayer player,final MagicPermanent permanent) {
         int calculation = 0;
-        int value = 0;
         for (final MagicCounterType counterType : MagicCounterType.values()) {
-        	if (counterType.getScore() == 0) {
-        		value=1;
-        	} else {
-        		value=counterType.getScore();
-        	}
-        	calculation += (permanent.getCounters(counterType)*value);
+            final int value = (counterType.getScore() == 0) ? 1 : counterType.getScore();
+        	calculation += permanent.getCounters(counterType) * value;
         }
-    	
+
     	final int score=calculation;
         return permanent.getController()==player?-score:score;
     }
