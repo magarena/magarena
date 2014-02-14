9d22c55573d4189b7fdf4b8f88bd928a7699bc74
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-14 20:09:10 +0000

    New card scripts

--- release/Magarena/scripts/Aspect_of_Wolf.groovy
+++ release/Magarena/scripts/Aspect_of_Wolf.groovy
@@ -2,8 +2,9 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amt = source.getController().getNrOfPermanents(MagicSubType.Forest);
-            pt.add(amt, amt);
+            final int p = (int)Math.floor(source.getController().getNrOfPermanents(MagicSubType.Forest)/2);
+            final int t = (int)Math.ceil(source.getController().getNrOfPermanents(MagicSubType.Forest)/2);
+            pt.add(p,t);
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
---
+++ release/Magarena/scripts/Tawnos_s_Wand.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Attack),
+        "Unblockable"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+            new MagicPayManaCostEvent(source,"{2}"),
+            new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_POWER_2_OR_LESS,
+                MagicUnblockableTargetPicker.create(),
+                this,
+                "Target creature\$ with power 2 or less is unblockable this turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Unblockable));
+            });
+        }
+    }
+]
