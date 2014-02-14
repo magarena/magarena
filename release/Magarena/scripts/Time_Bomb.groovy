33bde9e72a3c96fe44cd4880fb99b44854e3680d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-14 03:51:18 +0000

    New card scripts

---
+++ release/Magarena/scripts/Time_Bomb.groovy
@@ -0,0 +1,42 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{1}"),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN deals damage equal to the number of charge counters on it to each creature and each player."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent source=event.getPermanent();
+            final int amount=source.getCounters(MagicCounterType.Charge);
+            if (amount>0) {
+                final Collection<MagicPermanent> creatures=
+                    game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE);
+                for (final MagicPermanent creature : creatures) {
+                    final MagicDamage damage=new MagicDamage(source,creature,amount);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+                for (final MagicPlayer player : game.getPlayers()) {
+                    final MagicDamage damage=new MagicDamage(source,player,amount);
+                    game.doAction(new MagicDealDamageAction(damage));
+                }
+            }
+        }
+    }
+]
