a2bd114c97d87fe7e5c1651110890baa606310cf
melvin <melvinzhang@gmail.com>
2014-02-14 10:09:15 +0800

    add missing 'snow land you control' filter

--- release/Magarena/scripts/Drift_of_the_Dead.groovy
+++ release/Magarena/scripts/Drift_of_the_Dead.groovy
@@ -1,8 +1,14 @@
+def SNOW_LAND_YOU_CONTROL=new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.isLand() && target.hasType(MagicType.Snow) && target.isController(player);
+    }
+};
+
 [
     new MagicCDA() {
         @Override
         public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
-            final int size = game.filterPermanents(player,MagicTargetFilter.TARGET_SNOW_LAND_YOU_CONTROL).size();
+            final int size = game.filterPermanents(player,SNOW_LAND_YOU_CONTROL).size();
             pt.set(size, size);
         }
     }
