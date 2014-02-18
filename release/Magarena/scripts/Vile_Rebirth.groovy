079004ee7b319b68fb7bf2b2b778a11400e396ea
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-18 20:30:27 +0000

    New card scripts

---
+++ release/Magarena/scripts/Vile_Rebirth.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.Negative("target creature card from a graveyard"),
+                MagicGraveyardTargetPicker.ExileOpp,
+                this,
+                "Exile target creature card\$ from a graveyard. " +
+                "Put a 2/2 black Zombie creature token onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                final MagicCard card ->
+                final MagicPlayer player=event.getPlayer();
+                game.doAction(new MagicRemoveCardAction(
+                    card,
+                    MagicLocationType.Graveyard
+                ));
+                game.doAction(new MagicMoveCardAction(
+                    card,
+                    MagicLocationType.Graveyard,
+                    MagicLocationType.Exile
+                ));
+                game.doAction(new MagicPlayTokenAction(
+                    player,
+                    TokenCardDefinitions.get("2/2 black Zombie creature token")
+                ));
+            });
+        }
+    }
+]
