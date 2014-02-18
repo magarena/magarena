bbd9fa6b8cfba4f6cbdaf904bc0e4afb07270ba4
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-18 05:57:33 +0000

    New card scripts

---
+++ release/Magarena/scripts/Nevinyrral_s_Disk.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+    
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{1}"),
+                new MagicTapEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Destroy all artifacts, creatures and enchantments."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ARTIFACT);
+            targets.addAll(game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE));
+            targets.addAll(game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ENCHANTMENT));
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
