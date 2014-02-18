b79653663c35dbc97312c09ff673aab9576c2a8d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-18 07:12:14 +0000

    New card scripts

---
+++ release/Magarena/scripts/Spitting_Spider.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_LAND)];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN deals 1 damage to each creature with flying."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent=event.getPermanent();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(permanent.getController(),MagicTargetFilter.TARGET_CREATURE_WITH_FLYING);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(permanent,target,1);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
