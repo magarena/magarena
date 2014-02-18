54ac0593a4de3dbd062d04d67eebf09d164784e6
melvin <melvinzhang@gmail.com>
2014-02-18 10:23:17 +0800

    normalize and correct filename

---
+++ release/Magarena/scripts/Trolls_of_Tel_Jilad.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Regen"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{1}{G}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_GREEN_CREATURE,
+                MagicRegenerateTargetPicker.create(),
+                this,
+                "Regenerate target green creature\$."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicRegenerateAction(creature));
+            });
+        }
+    }
+]
