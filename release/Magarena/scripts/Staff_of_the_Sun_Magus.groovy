0fc2d2e6b0a873c831536c85fe7c047e78b63773
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-18 22:23:39 +0000

    New card scripts

---
+++ release/Magarena/scripts/Staff_of_the_Sun_Magus.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (cardOnStack.hasColor(MagicColor.White) && cardOnStack.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN gains 1 life."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+        }
+    },
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.hasSubType(MagicSubType.Plains) && otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                   "PN gains 1 life."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),1));
+        }
+    }
+]
