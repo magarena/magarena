7a8699e4fa2c0cafdea3a441a0d1439365e276a3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-18 04:24:24 +0000

    New Scarab Scripts

---
+++ release/Magarena/scripts/Green_Scarab.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicStatic(
+        MagicLayer.ModPT,
+        MagicTargetFilter.TARGET_CREATURE) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(2,2);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return target == source.getEnchantedCreature() &&
+                   source.getController().getOpponent().getNrOfPermanents(MagicTargetFilter.TARGET_GREEN_PERMANENT) >= 1;
+        }
+    }
+]
