f774613a99d5284ddd0670d14344fd9ec0a4d0fc
melvin <melvinzhang@gmail.com>
2014-02-20 11:55:23 +0800

    replace custom groovy code with MagicSearchIntoHandEvent

--- release/Magarena/scripts/Aurochs_Herd.groovy
+++ release/Magarena/scripts/Aurochs_Herd.groovy
@@ -1,27 +1,3 @@
-def act = {
-    final MagicGame game, final MagicEvent event ->
-    if (event.isNo() == false) {
-        event.processTargetCard(game, new MagicCardAction() {
-            public void doAction(final MagicCard card) {
-                game.logAppendMessage(event.getPlayer(), "Found " + card + ".");
-                game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
-                game.doAction(new MagicShuffleLibraryAction(event.getPlayer()));
-                game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.OwnersHand));
-            }
-        });
-    }
-} as MagicEventAction
-
-def evt = {
-    final MagicEvent event, final MagicChoice choice ->
-    return new MagicEvent(
-        event.getSource(),
-        event.getPlayer(), 
-        choice,
-        act,
-        "PN may search PN's library for an Aurochs card, reveal it, and put it into PN's hand."
-    );
-}
 [
     new MagicWhenComesIntoPlayTrigger() {
        @Override
@@ -34,7 +10,7 @@ def evt = {
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.addEvent(evt(
+            game.addEvent(new MagicSearchIntoHandEvent(
                 event,
                 new MagicMayChoice(
                     "Search for an Aurochs card?",
