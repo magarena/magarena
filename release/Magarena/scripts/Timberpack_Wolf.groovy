be5a7d94778bc94a802598942c224d990dc8eb34
melvin <melvinzhang@gmail.com>
2014-02-20 10:18:59 +0800

    fix parenthesis

--- release/Magarena/scripts/Timberpack_Wolf.groovy
+++ release/Magarena/scripts/Timberpack_Wolf.groovy
@@ -3,7 +3,7 @@
         @Override
         public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPermanent permanent,final MagicPowerToughness pt) {
             final MagicTargetFilter<MagicPermanent> targetFilter = new MagicTargetFilter.NameTargetFilter(permanent.getName());
-            final int size = game.filterPermanents(player,MagicOtherPermanentTargetFilter(targetFilter),permanent).size();
+            final int size = game.filterPermanents(player,new MagicOtherPermanentTargetFilter(targetFilter,permanent)).size();
             pt.add(size,size);
         }
     }
