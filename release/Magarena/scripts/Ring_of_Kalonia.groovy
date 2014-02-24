93bf362eb61eec42b988e8a7ea8376c4b50d0045
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-24 05:52:54 +0000

    New card scripts

---
+++ release/Magarena/scripts/Ring_of_Kalonia.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            final MagicPermanent equipped = permanent.getEquippedCreature();
+            return permanent.isController(upkeepPlayer) && equipped.isValid() && equipped.hasColor(MagicColor.Green) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a +1/+1 counter on creature equipped by SN."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent=event.getPermanent();
+            final MagicPermanent equipped=permanent.getEnchantedCreature();
+            if (equipped.isValid() && equipped.hasColor(MagicColor.Green)) {
+                game.doAction(new MagicChangeCountersAction(equipped,MagicCounterType.PlusOne,1,true));
+            }
+        }
+    }
+]
