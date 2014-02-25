56c39bb0854bb3c60cab22d9d4721ab6cb215424
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-25 01:37:05 +0000

    New card scripts

---
+++ release/Magarena/scripts/Royal_Decree.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenBecomesTappedTrigger() {
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
+            return (tapped.hasSubType(MagicSubType.Swamp) ||
+                    tapped.hasSubType(MagicSubType.Mountain) ||
+                    tapped.hasColor(MagicColor.Black) ||
+                    tapped.hasColor(MagicColor.Red)) ?
+                new MagicEvent(
+                    permanent,
+                    tapped.getController(),
+                    this,
+                    "SN deals 1 damage to "+tapped.getController().getName()+"."
+                ) :
+                MagicEvent.NONE;
+        }
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),1);
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
