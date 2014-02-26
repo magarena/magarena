7cda9ab7d1a1fa28d14a8082b649575cc1e00bb1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-02-26 02:54:40 +0000

    Rhystic Study may draw fix

--- release/Magarena/scripts/Rhystic_Study.groovy
+++ release/Magarena/scripts/Rhystic_Study.groovy
@@ -11,13 +11,29 @@
                         new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
                     ),
                     this,
-                    "PN may\$ pay {1}\$. If PN doesn't, "+permanent.getController().toString()+" draws a card."
+                    "PN may\$ pay {1}\$. If PN doesn't, "+permanent.getController().toString()+" may draw a card."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isNo()) {
+                final MagicPermanent enchantment = event.getPermanent();
+                game.addEvent(new MagicEvent(
+                    enchantment,
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_YES
+                    ),
+                    {
+                        final MagicGame G, final MagicEvent E ->
+                        if (E.isYes()) {
+                            G.doAction(new MagicDrawAction(E.getPermanent().getController()));
+                        }
+                    },
+                    enchantment.getController().toString()+" may\$ Draw a card."
+                ));
                 game.doAction(new MagicDrawAction(event.getPermanent().getController()));
             }
         }
