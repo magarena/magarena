ece4a7700f9570064cf1bde41da0ffdbf06171e5
melvin <melvinzhang@gmail.com>
2014-02-27 20:16:19 +0800

    add test scenario for phyrexian mana cards

---
+++ src/magic/test/TestPhyrexianMana.java
@@ -0,0 +1,54 @@
+
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.phase.MagicMainPhase;
+
+class TestPhyrexianMana extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(6);
+        addToLibrary(P, "Forest", 20);
+        addToLibrary(P, "Island", 20);
+        addToLibrary(P, "Entreat the Angels", 20);
+        addToLibrary(P, "Sliver Overlord", 1);
+        addToLibrary(P, "Wingsteed Rider", 1);
+        addToGraveyard(P, "Ink-Eyes, Servant of Oni", 3);
+        createPermanent(game,P, "Rupture Spire", false, 9);
+        createPermanent(game,P, "Rhystic Study", false, 1);
+        addToHand(P, "Rupture Spire", 1);
+        addToHand(P, "Feast of Blood", 1);
+        addToHand(P, "Lightning Bolt", 1);
+        addToHand(P, "Mental Misstep", 1);
+        addToHand(P, "Vault Skirge", 1);
+
+        P = opponent;
+
+        P.setLife(6);
+        addToLibrary(P, "Forest", 20);
+        //createPermanent(game,P,"Rupture Spire",false,1);
+        addToHand(P, "Trained Jackal", 2);
+        addToGraveyard(P, "Ink-Eyes, Servant of Oni", 1);
+
+        return game;
+    }
+}
