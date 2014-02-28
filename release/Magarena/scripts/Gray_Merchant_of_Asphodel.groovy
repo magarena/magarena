2bab6fa0eb3661895a37550282adfa66f7dfc139
melvin <melvinzhang@gmail.com>
2014-02-28 20:07:50 +0800

    normalize card script, fix groovy script

--- release/Magarena/scripts/Gray_Merchant_of_Asphodel.groovy
+++ release/Magarena/scripts/Gray_Merchant_of_Asphodel.groovy
@@ -1,19 +1,19 @@
-[ // enters effect Target opponent loses 2 life and PN gains 2 life.            
-    new MagicWhenComesIntoPlayTrigger() {                                       
-        @Override                                                               
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayed
-            return new MagicEvent(                                              
-                permanent,                                                      
-                this,                                                           
-                "Target opponent loses life and PN gains life equal to PN's devotion to black. ("+permanent.ge
-            );                                                                  
-        }                                                                       
-        @Override                                                               
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payed) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Each opponent loses X life and PN gains X life where X is PN's devotion to black."
+            );
+        }
+        @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getPlayer().getDevotion(MagicColor.Black); 
-            final MagicPlayer player=event.getPlayer();                         
+            final int amount = event.getPlayer().getDevotion(MagicColor.Black);
+            final MagicPlayer player=event.getPlayer();
             game.doAction(new MagicChangeLifeAction(player.getOpponent(),-1*amount));
-            game.doAction(new MagicChangeLifeAction(player, amount));           
-        }                                                                       
-    }                                                                           
-]     
+            game.doAction(new MagicChangeLifeAction(player, amount));
+        }
+    }
+]
