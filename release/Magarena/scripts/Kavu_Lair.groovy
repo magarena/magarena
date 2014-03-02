01b4df38edb138d2a1eac8a2fe4dd6a9443fc2e1
melvin <melvinzhang@gmail.com>
2014-03-02 20:36:55 +0800

    add missing requires_groovy_code, shortened event description

--- release/Magarena/scripts/Kavu_Lair.groovy
+++ release/Magarena/scripts/Kavu_Lair.groovy
@@ -4,15 +4,16 @@
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return (otherPermanent.isCreature() && otherPermanent.getPower() >= 4 ) ?
                 new MagicEvent(
-                    otherPermanent,
+                    permanent,
+                    otherPermanent.getController(),
                     this,
-                    "Whenever a creature with power 4 or greater enters the battlefield, its controller draws a card."
+                    "PN draws a card."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-			game.doAction(new MagicDrawAction(event.getPlayer()));
+            game.doAction(new MagicDrawAction(event.getPlayer()));
         }
     }
 ]
