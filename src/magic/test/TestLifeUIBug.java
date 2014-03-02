bedb6eadc9a9befdc72bb37ef612adcb0d8f4a6a
melvin <melvinzhang@gmail.com>
2014-03-02 09:39:36 +0800

    add test scenario for UI bug

---
+++ src/magic/test/TestLifeUIBug.java
@@ -0,0 +1,38 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicPlayerProfile;
+import magic.model.phase.MagicMainPhase;
+
+class TestLifeUIBug extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicPlayerProfile profile=new MagicPlayerProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(6);
+        addToLibrary(P, "Forest", 20);
+
+        P = opponent;
+
+        P.setLife(-1287867);
+        addToLibrary(P, "Forest", 20);
+
+        return game;
+    }
+}
