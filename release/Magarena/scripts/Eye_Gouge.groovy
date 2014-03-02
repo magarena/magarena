33f6f9037bfb59f65c780098fabadc023bc06e10
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-03-02 23:03:02 +0000

    New card scripts

---
+++ release/Magarena/scripts/Eye_Gouge.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(1,1),
+                this,
+                "Target creature\$ gets -1/-1 until end of turn. If it's a Cyclops, destroy it."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicChangeTurnPTAction(creature,-1,-1));
+                if (creature.hasSubType(MagicSubType.Cyclops)) {
+                    game.doAction(new MagicDestroyAction(creature));
+                }
+            });
+        }
+    }
+]
