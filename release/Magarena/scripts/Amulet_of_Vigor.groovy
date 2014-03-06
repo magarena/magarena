644dc21c0699c333855563fe4b36b92bb193f5fd
melvin <melvinzhang@gmail.com>
2014-03-06 13:32:29 +0800

    add Amulet of Vigor

---
+++ release/Magarena/scripts/Amulet_of_Vigor.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isTapped() && otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    otherPermanent,
+                    this,
+                    "Untap RN."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicUntapAction(event.getRefPermanent()));
+        }
+    }
+]
