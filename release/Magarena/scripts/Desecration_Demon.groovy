b301ffded53a05e18fa7f516e4717d0658b5f43c
melvin <melvinzhang@gmail.com>
2014-03-06 13:32:08 +0800

    change sacrifice event to sacrifice action

--- release/Magarena/scripts/Desecration_Demon.groovy
+++ release/Magarena/scripts/Desecration_Demon.groovy
@@ -4,22 +4,25 @@
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
             return permanent.isController(upkeepPlayer) ?
                 new MagicEvent(
-                        permanent,
-                        upkeepPlayer.opponent, //should be player.opponent
-                        new MagicMayChoice(MagicTargetChoice.SACRIFICE_CREATURE),
-                        MagicSacrificeTargetPicker.create(),
-                        this,
-                        "You may sacrifice a creature. If you do, tap SN and put a +1/+1 counter on it."
+                    permanent,
+                    upkeepPlayer.opponent, //should be player.opponent
+                    new MagicMayChoice(MagicTargetChoice.SACRIFICE_CREATURE),
+                    MagicSacrificeTargetPicker.create(),
+                    this,
+                    "PN may\$ sacrifice a creature\$. If you do, tap SN and put a +1/+1 counter on it."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent perm = event.getPermanent();
-            if(event.isYes()){
-                game.addEvent(new MagicSacrificePermanentEvent(perm, event.getPlayer(),MagicTargetChoice.SACRIFICE_CREATURE));
-                game.doAction(new MagicTapAction(perm, true)); //tap
-                game.doAction(new MagicChangeCountersAction(perm, MagicCounterType.PlusOne, 1, true));
+            if (event.isYes()) {
+                event.processTargetPermanent(game, {
+                    final MagicPermanent toSacrifice ->
+                    game.doAction(new MagicSacrificeAction(toSacrifice));
+                    final MagicPermanent perm = event.getPermanent();
+                    game.doAction(new MagicTapAction(perm, true)); //tap
+                    game.doAction(new MagicChangeCountersAction(perm, MagicCounterType.PlusOne, 1, true));
+                });
             }
         }
     }
