ef7667b274687028cde23f8e65dc2443884a644a
melvin <melvinzhang@gmail.com>
2014-03-09 17:54:04 +0800

    simplify groovy code

--- release/Magarena/scripts/Wild_Nacatl.groovy
+++ release/Magarena/scripts/Wild_Nacatl.groovy
@@ -2,16 +2,11 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final MagicGame game = source.getGame();
-            final Collection<MagicPermanent> targets1 =
-                    game.filterPermanents(permanent.getController(),MagicTargetFilter.TARGET_MOUNTAIN_YOU_CONTROL);
-            if (targets1.size() > 0) {
+            final MagicPlayer you = permanent.getController();
+            if (you.controlsPermanent(MagicSubType.Mountain)) {
                 pt.add(1,1);
             }
-
-            final Collection<MagicPermanent> targets2 =
-                    game.filterPermanents(permanent.getController(),MagicTargetFilter.TARGET_PLAINS_YOU_CONTROL);
-            if (targets2.size() > 0) {
+            if (you.controlsPermanent(MagicSubType.Plains)) {
                 pt.add(1,1);
             }
         }
