63cf48740099e8c2bb64b0a002863713ad06225b
melvin <melvinzhang@gmail.com>
2014-03-10 19:47:11 +0800

    add missing delayed trigger for Ray of Command
    fixes issue 503

--- release/Magarena/scripts/Ray_of_Command.groovy
+++ release/Magarena/scripts/Ray_of_Command.groovy
@@ -1,3 +1,25 @@
+def LoseControlTap = {
+    final MagicPlayer you ->
+    return new MagicWhenLoseControlTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent target) {
+            if (permanent == target && target.getController().getId() != you.getId()) {
+                game.addDelayedAction(new MagicRemoveTriggerAction(permanent, this));
+                return new MagicEvent(
+                    permanent,
+                    this,
+                    "Tap SN"
+                );
+            }
+            return MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicTapAction(event.getPermanent(), true));
+        }
+    };
+}
+
 [
     new MagicSpellCardEvent() {
         @Override
@@ -7,8 +29,8 @@
                 MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
                 MagicExileTargetPicker.create(),
                 this,
-                "Gain control of target creature\$ an opponent controls until end of turn. " +
-                "Untap that creature. It gains haste until end of turn."
+                "Untap target creature\$ an opponent controls and gain control of it until end of turn. " + 
+                "That creature gains haste until end of turn. When you lose control of the creature, tap it."
             );
         }
 
@@ -19,6 +41,7 @@
                 game.doAction(new MagicGainControlAction(event.getPlayer(),creature,MagicStatic.UntilEOT));
                 game.doAction(new MagicUntapAction(creature));
                 game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Haste));
+                game.doAction(new MagicAddTriggerAction(creature, LoseControlTap(event.getPlayer())));
             });
         }
     }
