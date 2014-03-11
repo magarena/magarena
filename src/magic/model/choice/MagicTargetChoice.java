6135c7226a0777a3072143d123c96852018d7622
ShawnieBoy <unknown>
2014-03-11 01:39:12 +0000

    Add Island you control to targetChoice

--- src/magic/model/choice/MagicTargetChoice.java
+++ src/magic/model/choice/MagicTargetChoice.java
@@ -149,6 +149,9 @@ public class MagicTargetChoice extends MagicChoice {
     
     public static final MagicTargetChoice LAND_YOU_CONTROL =
         new MagicTargetChoice("a land you control");
+    
+    public static final MagicTargetChoice ISLAND_YOU_CONTROL =
+        new MagicTargetChoice("an Island you control");
 
     public static final MagicTargetChoice TARGET_NONLAND_PERMANENT = 
         new MagicTargetChoice("target nonland permanent");
