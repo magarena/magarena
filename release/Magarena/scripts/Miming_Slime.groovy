73dca4ba65b8a5f2e8230758127a2f810af96f4b
melvin <melvinzhang@gmail.com>
2014-03-16 19:57:23 +0800

    improve formatting of groovy script

--- release/Magarena/scripts/Miming_Slime.groovy
+++ release/Magarena/scripts/Miming_Slime.groovy
@@ -10,25 +10,26 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            int x = 0;
             final Collection<MagicPermanent> targets = game.filterPermanents(
-                        event.getPlayer(),
-                        MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+                event.getPlayer(),
+                MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL
+            );
+            int x = 0;
             for (final MagicPermanent creature : targets) {
-                    x = Math.max(x,creature.getPower());
-                }
-            final MagicPlayer player=event.getPlayer();
+                x = Math.max(x,creature.getPower());
+            }
+
             final MagicCardDefinition oozeDef = TokenCardDefinitions.get("green Ooze creature token");
-            final MagicPlayTokenAction act = new MagicPlayTokenAction(player,oozeDef)
-            final MagicStatic PT = new MagicStatic(MagicLayer.SetPT){
-                    @Override
-                    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-                        pt.set(x,x);
-                    }
-                };
+            final MagicPlayTokenAction act = new MagicPlayTokenAction(event.getPlayer(),oozeDef)
             game.doAction(act);
+
+            final MagicStatic PT = new MagicStatic(MagicLayer.SetPT){
+                @Override
+                public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+                    pt.set(x,x);
+                }
+            };
             game.doAction(new MagicAddStaticAction(act.getPermanent(),PT));
-            
         }
     }
 ]
