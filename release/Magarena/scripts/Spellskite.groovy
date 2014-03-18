ba7a8c875216ed5e01a8b6376cefb1c4d81bba51
melvin <melvinzhang@gmail.com>
2014-03-18 22:08:28 +0800

    add Spellskite

---
+++ release/Magarena/scripts/Spellskite.groovy
@@ -0,0 +1,50 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetItemOnStack(game, {
+        final MagicItemOnStack item ->
+        game.doAction(new MagicChangeTargetAction(item, event.getPermanent()));
+    });
+}
+
+def event = {
+    final MagicPermanent source ->
+    return new MagicEvent(
+        source,
+        new MagicTargetChoice(MagicTargetFilter.SPELL_OR_ABILITY_WITH_TARGET, MagicTargetHint.Negative, "target spell or ability"), 
+        action,
+        "Change the target of target spell or ability\$ to SN."
+    );
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Spell),
+        "Retarget"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{U}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return event(source);
+        }
+    },
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Spell),
+        "Pay 2 life"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayLifeEvent(source, 2)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return event(source);
+        }
+    }
+]
