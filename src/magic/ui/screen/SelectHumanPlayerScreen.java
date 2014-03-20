7ee3cf7026f3e2aa54d35bcd8abdb87fefe3a1c3
Lodici <Lodici>
2014-03-20 17:26:29 +0000

    refactor: extract class.

--- src/magic/ui/screen/SelectHumanPlayerScreen.java
+++ src/magic/ui/screen/SelectHumanPlayerScreen.java
@@ -130,33 +130,10 @@ public class SelectHumanPlayerScreen
     public List<MenuButton> getMiddleActions() {
         final List<MenuButton> buttons = new ArrayList<MenuButton>();
         buttons.add(new MenuButton("New", new NewPlayerAction(), "Create a new player profile."));
-        buttons.add(new MenuButton("Edit", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                doEditPlayerProfile();
-                profilesJList.repaint();
-            }
-        }, "Update name and duel settings for selected player."));
+        buttons.add(new MenuButton("Edit", new EditPlayerAction(), "Update name and duel settings for selected player."));
         buttons.add(new MenuButton("Delete", new DeletePlayerAction(), "Delete selected player profile."));
         buttons.add(getAvatarActionButton());
         return buttons;
-    }
-
-    private void doEditPlayerProfile() {
-        final PlayerProfile profile = getSelectedPlayerProfile();
-        final String newName = (String)JOptionPane.showInputDialog(
-                getFrame(),
-                "<html><b>Player Name</b><br></html>",
-                "Update Player",
-                JOptionPane.PLAIN_MESSAGE,
-                null, null, profile.getPlayerName());
-        if (newName != null && !newName.trim().isEmpty()) {
-            profile.setPlayerName(newName.trim());
-            profile.save();
-            if (profile.equals(playerProfile)) {
-                consumer.setPlayerProfile(getSelectedPlayerProfile());
-            }
-        }
     }
 
     private HumanPlayer getSelectedPlayerProfile() {
@@ -251,6 +228,33 @@ public class SelectHumanPlayerScreen
                 newProfile.save();
                 PlayerProfiles.getPlayerProfiles().put(newProfile.getId(), newProfile);
                 refreshProfilesJList(newProfile);
+            }
+        }
+
+    }
+
+    private class EditPlayerAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            doEditPlayerProfile();
+            profilesJList.repaint();
+        }
+
+        private void doEditPlayerProfile() {
+            final PlayerProfile profile = getSelectedPlayer();
+            final String newName = (String)JOptionPane.showInputDialog(
+                    getFrame(),
+                    "<html><b>Player Name</b><br></html>",
+                    "Update Player",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null, null, profile.getPlayerName());
+            if (newName != null && !newName.trim().isEmpty()) {
+                profile.setPlayerName(newName.trim());
+                profile.save();
+                if (profile.equals(consumer.getPlayer())) {
+                    consumer.setPlayerProfile(getSelectedPlayer());
+                }
             }
         }
 
