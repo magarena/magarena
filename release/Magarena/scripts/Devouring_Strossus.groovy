3936d9296c09dd45c11298db33794b6e7d977e29
melvin <melvinzhang@gmail.com>
2014-03-22 20:25:35 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Devouring_Strossus.groovy
+++ release/Magarena/scripts/Devouring_Strossus.groovy
@@ -1,28 +1,4 @@
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Regen"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE),
-                new MagicRegenerationConditionsEvent(source,this)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Regenerate SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicRegenerateAction(event.getPermanent()));
-        }
-    },
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
