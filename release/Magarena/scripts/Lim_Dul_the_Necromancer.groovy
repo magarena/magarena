33d42f5cf292e9cf8f277cfb55af17aa1a7f4f98
melvin <melvinzhang@gmail.com>
2014-03-22 20:39:35 +0800

    replace groovy reference with ability in card script

--- release/Magarena/scripts/Lim_Dul_the_Necromancer.groovy
+++ release/Magarena/scripts/Lim_Dul_the_Necromancer.groovy
@@ -22,34 +22,5 @@
                 game.doAction(new MagicReanimateAction(event.getRefCard(), event.getPlayer(), [MagicPlayMod.ZOMBIE]));
             }
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump,true),
-        "Regen"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{1}{B}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.POS_TARGET_ZOMBIE,
-                MagicRegenerateTargetPicker.create(),
-                this,
-                "Regenerate target Zombie\$."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                final MagicPermanent creature ->
-                game.doAction(new MagicRegenerateAction(creature));
-            });
-        }
     }
 ]
