f39000279768eebb074c5878c6701bbfb5b95574
melvin <melvinzhang@gmail.com>
2014-03-22 20:08:30 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Eater_of_Hope.groovy
+++ release/Magarena/scripts/Eater_of_Hope.groovy
@@ -12,41 +12,6 @@ def TWO_OTHER_CREATURES_CONDITION = new MagicCondition() {
 
 [
     new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Regen"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            final MagicTargetChoice targetChoice = new MagicTargetChoice(
-                new MagicOtherPermanentTargetFilter(
-                    MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL,
-                    source
-                ),
-                MagicTargetHint.None,
-                "a creature other than " + source + " to sacrifice"
-            );
-            return [
-                new MagicPayManaCostEvent(source,"{B}"),
-                new MagicSacrificePermanentEvent(
-                    source,
-                    targetChoice
-                )
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Regenerate SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicRegenerateAction(event.getPermanent()));
-        }
-    },
-    new MagicPermanentActivation(
         [
             TWO_OTHER_CREATURES_CONDITION,
         ],
