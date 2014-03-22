37035c1d3361ea9360d50a2b2411c11be0e28dee
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-03-22 12:56:52 +0000

    New card scripts

---
+++ release/Magarena/scripts/Haunted_Crossroads.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Main),
+        "Card"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}")
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                MagicGraveyardTargetPicker.ReturnToHand,
+                this,
+                "Put target creature card\$ from your graveyard on top of your library"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                final MagicCard targetCard ->
+                game.doAction(new MagicRemoveCardAction(targetCard,MagicLocationType.Graveyard));
+                game.doAction(new MagicMoveCardAction(
+                    targetCard,
+                    MagicLocationType.Graveyard,
+                    MagicLocationType.TopOfOwnersLibrary
+                ));
+            });
+        }
+    }
+]
