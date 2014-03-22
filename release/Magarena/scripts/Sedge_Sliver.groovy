6e283d529cb7d08302ef8af516f50f1a7a881054
melvin <melvinzhang@gmail.com>
2014-03-22 20:11:42 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Sedge_Sliver.groovy
+++ release/Magarena/scripts/Sedge_Sliver.groovy
@@ -1,28 +1,3 @@
-
-def SedgeRegen = new MagicPermanentActivation(
-    new MagicActivationHints(MagicTiming.Pump),
-    "Regen"
-) {
-
-    @Override
-    public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-        return [new MagicPayManaCostEvent(source,"{B}")];
-    }
-
-   @Override
-    public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            source,
-            this,
-            "Regenerate SN."
-        );
-    }
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        game.doAction(new MagicRegenerateAction(event.getPermanent()));
-    }
-};
-
 [
     new MagicStatic(
         MagicLayer.ModPT,
@@ -33,15 +8,6 @@ def SedgeRegen = new MagicPermanentActivation(
             if (permanent.getController().controlsPermanent(MagicSubType.Swamp)) {
                 pt.add(1,1);
             }
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilter.TARGET_SLIVER
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            permanent.addAbility(SedgeRegen);
         }
     }
 ]
