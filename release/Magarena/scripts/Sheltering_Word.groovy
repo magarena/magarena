15ef4d4599af7db5b6efee3c95a1f5adf82627c8
ShawnieBoy <unknown>
2014-03-26 07:06:24 +0000

    Fix issue 554 and fix other errors

--- release/Magarena/scripts/Sheltering_Word.groovy
+++ release/Magarena/scripts/Sheltering_Word.groovy
@@ -4,10 +4,10 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                MagicTargetChoice.TARGET_CREATURE,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
                 MagicShroudTargetPicker.create(),
                 this,
-                "Target creature\$ gains hexproof until end of turn. PN gains life equal to RN's toughness."
+                "Target creature you control\$ gains hexproof until end of turn. PN gains life equal to that creature's toughness."
             );
         }
 
