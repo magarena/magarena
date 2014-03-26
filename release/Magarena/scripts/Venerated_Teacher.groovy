d19b08e781c1b6f4fa7d9bd0b3a7f2627153be49
melvin <melvinzhang@gmail.com>
2014-03-26 14:22:26 +0800

    normalize card script

--- release/Magarena/scripts/Venerated_Teacher.groovy
+++ release/Magarena/scripts/Venerated_Teacher.groovy
@@ -1,27 +1,27 @@
 [
-	new MagicWhenComesIntoPlayTrigger() {
-		@Override
-		public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
-			return new MagicEvent(
-				permanent,
-				this,
-				"PN places two level tokens on each creature with level up he or she controls."
-			);
-		}
-		@Override
-		public void executeEvent(final MagicGame game, final MagicEvent event) {
-			final Collection<MagicPermanent> targets=
-				game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
-			for (final MagicPermanent target : targets) {
-				if (target.hasAbility(MagicAbility.LevelUp)) {
-					game.doAction(new MagicChangeCountersAction(
-						target,
-						MagicCounterType.Level,
-						2,
-						true
-					));
-				}
-			}
-		}
-	}
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN places two level tokens on each creature with level up he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                if (target.hasAbility(MagicAbility.LevelUp)) {
+                    game.doAction(new MagicChangeCountersAction(
+                        target,
+                        MagicCounterType.Level,
+                        2,
+                        true
+                    ));
+                }
+            }
+        }
+    }
 ]
