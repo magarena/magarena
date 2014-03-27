7b29784172bfb44c209819b5a8dcf037aa099767
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-03-27 15:34:31 +0000

    Fix issue 557 - Sadistic Glee only triggered with your creatures

--- release/Magarena/scripts/Sadistic_Glee.groovy
+++ release/Magarena/scripts/Sadistic_Glee.groovy
@@ -2,8 +2,7 @@
     new MagicWhenOtherDiesTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (otherPermanent.isCreature() &&
-                    otherPermanent.isFriend(permanent)) ?
+            return otherPermanent.isCreature() ?
                 new MagicEvent(
                     permanent,
                     permanent.getEnchantedPermanent(),
