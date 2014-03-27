46f71ac854538d8626f15d65df96d5918c91b27f
a. benedict balbuena (chuckie) <unknown>
2014-03-27 12:23:45 +0000

    Add script for Urza's Rage

---
+++ release/Magarena/scripts/Urza_s_Rage.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount = payedCost.isKicked() ? 10 : 3;
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                new MagicDamageTargetPicker(amount),
+                this,
+                "SN deals 3 damage to target creature or player\$. "+
+                "If SN was kicked, instead it deals 10 damage to that creature or player and the damage can't be prevented."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                final MagicTarget target ->
+                final int amount = event.isKicked() ? 10 : 3;
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,amount);
+                if (event.isKicked()) {
+			 damage.setUnpreventable();
+                 }
+                game.doAction(new MagicDealDamageAction(damage));
+            });
+        }
+    }
+]
