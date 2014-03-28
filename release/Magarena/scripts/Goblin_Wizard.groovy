13f33254f1eff6cd39f1532894884b8cdec9709b
ShawnieBoy <unknown>
2014-03-28 03:24:33 +0000

    New card scripts

---
+++ release/Magarena/scripts/Goblin_Wizard.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Put"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                new MagicMayChoice(
+                    MagicTargetChoice.GOBLIN_CARD_FROM_HAND
+                ),
+                MagicGraveyardTargetPicker.PutOntoBattlefield,
+                this,
+                "PN may\$ put a Goblin permanent card\$ from his or her hand onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicPutOntoBattlefieldEvent(
+                event,
+                new MagicMayChoice(
+                    "Put a Goblin permanent card onto the battlefield?",
+                    MagicTargetChoice.GOBLIN_CARD_FROM_HAND
+                )
+            ));
+        }
+    }
+]
