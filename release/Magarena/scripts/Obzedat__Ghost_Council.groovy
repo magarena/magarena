d1413629b73597afe795408fae390443d814e86b
melvin <melvinzhang@gmail.com>
2014-03-28 16:20:36 +0800

    simplify delayed trigger

--- release/Magarena/scripts/Obzedat__Ghost_Council.groovy
+++ release/Magarena/scripts/Obzedat__Ghost_Council.groovy
@@ -1,41 +1,34 @@
 def DelayedTrigger = {
-    final MagicPermanent source, final MagicPlayer player, final MagicCard card ->
+    final MagicCard origCard, final MagicPlayer origPlayer ->
     return new MagicAtUpkeepTrigger() {
         @Override
         public boolean accept(final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return player.getId() == upkeepPlayer.getId();
+            return origPlayer.getId() == upkeepPlayer.getId();
         }
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            final MagicCard mappedCard = card.getOwner().map(game).getExile().getCard(card.getId());
-            final MagicCard mappedSource = new MagicCard(
-                source.getCardDefinition(), 
-                source.getOwner().map(game), 
-                source.getCard().getId()
-            );
-            
             game.addDelayedAction(new MagicRemoveTriggerAction(this));
+            
+            final MagicCard mappedCard = origCard.getOwner().map(game).getExile().getCard(origCard.getId());
             
             return mappedCard.isInExile() ?
                 new MagicEvent(
-                    mappedSource,
-                    mappedCard.getOwner(),
                     mappedCard,
                     this,
-                    "Return RN to the battlefield under PN's control. It gains haste."
+                    "Return SN to the battlefield under PN's control. It gains haste."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.getRefCard().isInExile()) {
+            final MagicCard card = event.getCard();
+            if (card.isInExile()) {
                 game.doAction(new MagicRemoveCardAction(
-                    event.getRefCard(),
+                    card,
                     MagicLocationType.Exile
                 ));
                 game.doAction(new MagicPlayCardAction(
-                    event.getRefCard(),
-                    event.getPlayer(),
+                    card,
                     [MagicPlayMod.HASTE]
                 ));
             }
@@ -51,7 +44,7 @@ def DelayedTrigger = {
                     permanent,
                     new MagicMayChoice(),
                     this,
-                    "PM may\$ exile SN. " + 
+                    "PN may\$ exile SN. " + 
                     "If you do, return it to the battlefield under its owner's control at the beginning of your next upkeep. " + 
                     "It gains haste."
                 ):
@@ -65,9 +58,8 @@ def DelayedTrigger = {
                     MagicLocationType.Exile
                 ));
                 game.doAction(new MagicAddTriggerAction(DelayedTrigger(
-                    event.getPermanent(),
-                    event.getPlayer(),
-                    event.getPermanent().getCard()
+                    event.getPermanent().getCard(),
+                    event.getPlayer()
                 )));
             }
         }
