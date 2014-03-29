2be5b334d3062636371b845a1cd99e454c0dd92c
a. benedict balbuena (chuckie) <unknown>
2014-03-29 02:56:44 +0000

    Add script for Corrupt

---
+++ release/Magarena/scripts/Corrupt.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                this,
+                "SN deals damage to target creature or player\$ equal to the number of Swamps PN controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = player.getNrOfPermanents(MagicSubType.Swamp);
+            event.processTarget(game, {
+                final MagicTarget target ->
+                final MagicDamage damage = new MagicDamage(
+                    event.getSource(),
+                    target,
+                    amount
+                );
+                game.doAction(new MagicDealDamageAction(damage));
+		game.doAction(new MagicChangeLifeAction(event.getPlayer(),amount));
+            });
+        }
+    }
+]
