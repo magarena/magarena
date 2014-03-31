945b524a88afb01b6afc03c173077d259086766c
ShawnieBoy <unknown>
2014-03-31 05:34:52 +0100

    New card scripts

---
+++ release/Magarena/scripts/Whirlwind.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all creatures with flying."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_CREATURE_WITH_FLYING);
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
