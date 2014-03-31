a55852254512504cb2bbafa8585a7aac8f1c3e0d
melvin <melvinzhang@gmail.com>
2014-03-31 10:13:39 +0800

    add comma before where clause in description

--- release/Magarena/scripts/Ivory_Tower.groovy
+++ release/Magarena/scripts/Ivory_Tower.groovy
@@ -7,7 +7,7 @@
                     permanent,
                     upkeepPlayer,
                     this,
-                    "PN gains X life where X is the number of cards in his or her hand minus 4."
+                    "PN gains X life, where X is the number of cards in his or her hand minus 4."
                 ) :
                 MagicEvent.NONE;
         }
