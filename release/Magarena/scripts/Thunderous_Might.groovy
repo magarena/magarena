e5be40082316ca4ceaf3e3c949199397255a2e58
melvin <melvinzhang@gmail.com>
2014-03-31 10:14:08 +0800

    put enchanted permanent in event, use RN in description

--- release/Magarena/scripts/Thunderous_Might.groovy
+++ release/Magarena/scripts/Thunderous_Might.groovy
@@ -5,15 +5,16 @@
             return (permanent.getEnchantedPermanent()==creature) ?
                 new MagicEvent(
                     permanent,
+                    permanent.getEnchantedPermanent(),
                     this,
-                    permanent.getEnchantedPermanent().toString()+" gets +X/+0 until end of turn where X is PN's devotion to red."
+                    "RN gets +X/+0 until end of turn, where X is PN's devotion to red."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature=event.getPermanent().getEnchantedPermanent();
-            final int power = event.getPermanent().getController().getDevotion(MagicColor.Red);
+            final MagicPermanent creature=event.getRefPermanent();
+            final int power = event.getPlayer().getDevotion(MagicColor.Red);
             game.doAction(new MagicChangeTurnPTAction(creature,power,0));
         }
     }
