47c07282551a897dabb0a17dc71b0727df193541
melvin <melvinzhang@gmail.com>
2014-03-31 10:12:50 +0800

    add comma before where clause in description, simplify executeEvent

--- release/Magarena/scripts/Akroan_Hoplite.groovy
+++ release/Magarena/scripts/Akroan_Hoplite.groovy
@@ -6,16 +6,14 @@
                 new MagicEvent(
                     permanent,
                     this,
-                    "SN gets +X/+0 until end of turn where X is the number of attacking creatures PN controls."
+                    "SN gets +X/+0 until end of turn, where X is the number of attacking creatures PN controls."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature=event.getPermanent();
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(creature.getController(),MagicTargetFilter.TARGET_ATTACKING_CREATURE);
-            final int power = targets.size();
+            final MagicPermanent creature = event.getPermanent();
+            final int power = event.getPlayer().getNrOfPermanents(MagicTargetFilter.TARGET_ATTACKING_CREATURE)
             game.doAction(new MagicChangeTurnPTAction(creature,power,0));
         }
     }
