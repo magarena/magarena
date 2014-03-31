f84a3d777df8bb6c1b5b3ff03c86b880844b59d7
melvin <melvinzhang@gmail.com>
2014-03-31 20:41:46 +0800

    remove the need to start activated ability with the word 'cost'

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -727,9 +727,9 @@ public enum MagicAbility {
             card.add(new MagicBestowActivation(manaCost));
         }
     },
-    ActivatedAbility("cost " + ARG.ANY, 10) {
+    ActivatedAbility("[^\"]*:" + ARG.ANY, 10) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            card.add(MagicPermanentActivation.create(ARG.any(arg)));
+            card.add(MagicPermanentActivation.create(arg.group()));
         }
     },
     AlternateCost("alt cost " + ARG.ANY, 10) {
