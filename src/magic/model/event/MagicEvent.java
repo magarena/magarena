f1a662dd968e8d77e80d415f16e2c1a300ef1bac
melvin <melvinzhang@gmail.com>
2014-04-02 22:00:12 +0800

    use stateId of choice instead of hashcode

--- src/magic/model/event/MagicEvent.java
+++ src/magic/model/event/MagicEvent.java
@@ -644,7 +644,7 @@ public class MagicEvent implements MagicCopyable {
             //don't call getStateId if source is MagicItemOnStack to avoid infinite loop
             (source instanceof MagicItemOnStack) ? -1L : source.getStateId(),
             player.getId(),
-            choice.hashCode(),
+            choice.getStateId(),
             targetPicker.hashCode(),
             action.hashCode(),
             description.hashCode(),
