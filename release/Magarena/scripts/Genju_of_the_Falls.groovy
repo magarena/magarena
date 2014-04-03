81d4f1525978c9d318aecda3765256387599c004
Frank Schreiber <unknown>
2014-04-03 20:12:17 +0000

    Add script for Genju of the Falls

---
+++ release/Magarena/scripts/Genju_of_the_Falls.groovy
@@ -0,0 +1,96 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(3,2);
+    }
+};
+def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        permanent.addAbility(MagicAbility.Flying, flags);
+    }
+};
+def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags | MagicType.Creature.getMask();
+    }
+    @Override
+    public void modTypeFlags(final MagicPermanent permanent, final Set<MagicType> flags) {    
+        flags.add(MagicType.Creature);
+    }
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Spirit);
+    }
+};
+def LC = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public void modColorFlags(final MagicPermanent permanent, final Set<MagicColor> flags) {    
+        flags.remove(MagicColor.White);
+        flags.remove(MagicColor.Green);
+        flags.remove(MagicColor.Black);
+        flags.remove(MagicColor.Red);
+        flags.add(MagicColor.Blue);
+    }
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Spirit);
+    }
+};
+[
+    new MagicPermanentActivation(
+        [
+            new MagicArtificialCondition(
+                MagicConditionFactory.ManaCost("{3}")
+            )
+        ],
+        new MagicActivationHints(MagicTiming.Animate),
+        "Animate"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{2}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source.getEnchantedPermanent(),
+                this,
+                "SN becomes a 3/2 blue Spirit creature with flying until end of turn." +
+                "It's still a land."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,AB,ST,LC));
+        }
+    },
+    new MagicWhenOtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(
+                final MagicGame game,
+                final MagicPermanent permanent,
+                final MagicPermanent died) {
+            return (permanent.getEnchantedPermanent() == died) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getCard(),
+                    this,
+                    "Return RN to its owner's hand."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard card = event.getRefCard();
+            if (card.isInGraveyard()) {
+                game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
+                game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
+            }
+        }
+    }
+]
