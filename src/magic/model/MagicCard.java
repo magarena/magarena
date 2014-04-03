28512108ebaaf79113a72aa1f51ed6f08599cf05
melvin <melvinzhang@gmail.com>
2014-04-03 15:45:04 +0800

    change MagicCard.NONE's name is NONE instead of Unknown to differentiate between hidden cards and NONE

--- src/magic/model/MagicCard.java
+++ src/magic/model/MagicCard.java
@@ -25,6 +25,10 @@ public class MagicCard
         public MagicCard copy(final MagicCopyMap copyMap) {
             return this;
         }
+        @Override
+        public String getName() {
+            return "NONE";
+        }
     };
 
     private static final int TOKEN_ID=-1;
