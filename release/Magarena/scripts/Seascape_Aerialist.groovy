82490e3bc7118a3f21220b29402941453181d719
ShawnieBoy <unknown>
2014-04-05 12:44:58 +0100

    Correct filter targets in groovy code

--- release/Magarena/scripts/Seascape_Aerialist.groovy
+++ release/Magarena/scripts/Seascape_Aerialist.groovy
@@ -22,7 +22,7 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 final Collection<MagicPermanent> targets =
-                        game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_YOU_CONTROL);
+                        game.filterPermanents(event.getPlayer(),MagicTargetFilter.TARGET_ALLY_CREATURE_YOU_CONTROL);
                 for (final MagicPermanent creature : targets) {
                     game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Flying));
                 }
