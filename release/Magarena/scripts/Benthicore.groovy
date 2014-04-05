31067eaf8e2807a86f7d04d119c8cb797509169f
melvin <melvinzhang@gmail.com>
2014-04-05 13:07:18 +0800

    convert groovy to script, made formatting of groovy more consistent

--- release/Magarena/scripts/Benthicore.groovy
+++ release/Magarena/scripts/Benthicore.groovy
@@ -1,59 +1,47 @@
 def UNTAPPED_MERFOLK_YOU_CONTROL=new MagicPermanentFilterImpl(){
-        public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target)
-        {
-            return target.hasSubType(MagicSubType.Merfolk) && !target.isTapped() && !target.isOpponent(player);
-        }
-    };
-def TWO_UNTAPPED_MERFOLK_CONDITION = new MagicCondition() {
-    public boolean accept(final MagicSource source) {
-        final MagicTargetFilter<MagicPermanent> filter =new MagicOtherPermanentTargetFilter(
-            UNTAPPED_MERFOLK_YOU_CONTROL, 
-            (MagicPermanent)source
-        );
-        final MagicGame game = source.getGame();
-        final MagicPlayer player = source.getController();
-        return game.filterPermanents(player, filter).size() >= 2;
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target)
+    {
+        return target.hasSubType(MagicSubType.Merfolk) && 
+               target.isUntapped() && 
+               target.isController(player);
     }
 };
+
+def TWO_UNTAPPED_MERFOLK_CONDITION = new MagicCondition() {
+    public boolean accept(final MagicSource source) {
+        return source.getController().getNrOfPermanents(UNTAPPED_MERFOLK_YOU_CONTROL) >= 2;
+    }
+};
+
 def AN_UNTAPPED_MERFOLK_YOU_CONTROL = new MagicTargetChoice(UNTAPPED_MERFOLK_YOU_CONTROL,"an untapped Merfolk you control");
 
 [
-    new MagicWhenComesIntoPlayTrigger() 
-    {
+    new MagicPermanentActivation(
+        [TWO_UNTAPPED_MERFOLK_CONDITION],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Untap"
+    ) {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) 
-        {
-            return new MagicEvent(permanent,this,"PN puts two 1/1 blue Merfolk Wizard creature token onto the battlefield.");
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) 
-        {
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("1/1 blue Merfolk Wizard creature token")));
-            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("1/1 blue Merfolk Wizard creature token")));
-        }
-    },
-    new MagicPermanentActivation([TWO_UNTAPPED_MERFOLK_CONDITION],new MagicActivationHints(MagicTiming.Removal),"Untap") 
-    {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) 
-        {
-            return [new MagicTapPermanentEvent(source, AN_UNTAPPED_MERFOLK_YOU_CONTROL),new MagicTapPermanentEvent(source, AN_UNTAPPED_MERFOLK_YOU_CONTROL)];
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapPermanentEvent(source, AN_UNTAPPED_MERFOLK_YOU_CONTROL),
+                new MagicTapPermanentEvent(source, AN_UNTAPPED_MERFOLK_YOU_CONTROL)
+            ];
         }
 
         @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost)
-        {
-            
-            return new MagicEvent(source,source,this,"Untap SN. SN gains shroud until end of turn.");
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Untap SN. SN gains shroud until end of turn."
+            );
         }
 
         @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event)
-        {
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicUntapAction(event.getPermanent()));
             game.doAction(new MagicGainAbilityAction(event.getPermanent(),MagicAbility.Shroud));
         }
-      
     }
 ]
