baca2332525770c676c6afbd218191f4264eeb64
melvin <melvinzhang@gmail.com>
2014-04-07 10:53:37 +0800

    merge similar static ability into a single one, should count number of Auras not Enchantments

--- release/Magarena/scripts/Eidolon_of_Countless_Battles.groovy
+++ release/Magarena/scripts/Eidolon_of_Countless_Battles.groovy
@@ -2,20 +2,13 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int size = source.getController().getNrOfPermanents(MagicType.Creature) + source.getController().getNrOfPermanents(MagicType.Enchantment);
-            pt.add(size,size);
-        }
-        
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int size = source.getController().getNrOfPermanents(MagicType.Creature) + source.getController().getNrOfPermanents(MagicType.Enchantment);
+            final int size = source.getController().getNrOfPermanents(MagicType.Creature) + 
+                             source.getController().getNrOfPermanents(MagicSubType.Aura);
             pt.add(size,size);
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return MagicStatic.acceptLinked(game, source, target);
+            return source == target || MagicStatic.acceptLinked(game, source, target);
         }
     }
 ]
