ce183304e54a5dacdddd68fe0374a2b92a47acd4
melvin <melvinzhang@gmail.com>
2014-04-07 16:22:14 +0800

    complete Siren of the Silent Song
    fixes issue 581

---
+++ release/Magarena/scripts/Siren_of_the_Silent_Song.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenSelfBecomesUntappedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent untapped) {
+            return new MagicEvent(
+                permanent,
+                permanent.getOpponent(),
+                MagicTargetChoice.TARGET_CARD_FROM_HAND,
+                this,
+                "PN discards a card\$, then puts the top card of his or her library into his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                final MagicCard targetCard ->
+                game.doAction(new MagicDiscardCardAction(event.getPlayer(),targetCard));
+                game.doAction(new MagicMillLibraryAction(event.getPlayer(),1));
+            });
+        }
+    }
+]
