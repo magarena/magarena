9c75763395610c8332b50b45d4b0e7126e74253e
melvin <melvinzhang@gmail.com>
2014-04-08 10:59:42 +0800

    simplified code, replace May choice with Or choice

--- release/Magarena/scripts/Blizzard_Specter.groovy
+++ release/Magarena/scripts/Blizzard_Specter.groovy
@@ -7,38 +7,29 @@
                     damage.isCombat()) ?
                 new MagicEvent(
                     permanent,
-                    permanent.getController(),
-                    new MagicMayChoice(),
+                    new MagicOrChoice(
+                        MagicChoice.NONE,
+                        MagicChoice.NONE
+                    ),
                     damage.getTarget(),
                     this,
-                    "PN may\$ have RN discard a card. If PN chooses not to, RN returns a creature he or she controls to owner's hand."
+                    "Choose one\$ - RN discards a card; or RN returns a permanent he or she controls to its owner's hand."
                 ) :
                 MagicEvent.NONE;
         }
-        public MagicEvent bounceChoice(MagicPermanent permanent, MagicPlayer player){
-            return new MagicEvent(
-                permanent,
-                player,
-                MagicTargetChoice.CREATURE_YOU_CONTROL,
-                MagicBounceTargetPicker.create(),
-                MagicPlayer.NONE,
-                this,
-                "PN has to return a creature\$ he or she controls to its owner's hand."
-            );
-        }                  
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if(event.getRefPlayer().isValid() && event.isYes()) {
-                game.addEvent(new MagicDiscardEvent(event.getSource(),event.getRefPlayer()));
-            }
-            if(event.getRefPlayer().isValid() && event.isNo()) {
-                game.addEvent(bounceChoice(event.getPermanent(),event.getRefPlayer()));
-            }
-            if(!event.getRefPlayer().isValid()) {
-                event.processTargetPermanent(game, {
-                    final MagicPermanent creature ->
-                        game.doAction(new MagicRemoveFromPlayAction(creature,MagicLocationType.OwnersHand));
-                });
+            if (event.isMode(1)) {
+                game.addEvent(new MagicDiscardEvent(
+                    event.getSource(),
+                    event.getRefPlayer()
+                ));
+            } else if (event.isMode(2)) {
+                game.addEvent(new MagicBounceChosenPermanentEvent(
+                    event.getSource(), 
+                    event.getRefPlayer(), 
+                    MagicTargetChoice.PERMANENT_YOU_CONTROL
+                ));
             }
         }
     }
