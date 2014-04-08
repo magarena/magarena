3144bfc288882efa6196afc3938575e4d8551d9f
melvin <melvinzhang@gmail.com>
2014-04-08 22:03:59 +0800

    correct description to follow other discard events

--- src/magic/model/event/MagicDiscardChosenEvent.java
+++ src/magic/model/event/MagicDiscardChosenEvent.java
@@ -2,16 +2,13 @@ package magic.model.event;
 
 import magic.model.MagicCard;
 import magic.model.MagicGame;
-import magic.model.MagicLocationType;
 import magic.model.MagicPlayer;
 import magic.model.MagicSource;
 import magic.model.action.MagicCardAction;
-import magic.model.action.MagicMoveCardAction;
-import magic.model.action.MagicRemoveCardAction;
+import magic.model.action.MagicDiscardCardAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.condition.MagicCondition;
 import magic.model.condition.MagicConditionFactory;
-import magic.model.target.MagicTargetType;
 
 public class MagicDiscardChosenEvent extends MagicEvent {
 
@@ -30,24 +27,18 @@ public class MagicDiscardChosenEvent extends MagicEvent {
             player,
             targetChoice,
             EVENT_ACTION,
-            "Choose " + targetChoice.getTargetDescription() + "$."
+            "PN discards " + targetChoice.getTargetDescription().replace(" from your hand", "") + "$."
         );
         conds = new MagicCondition[]{MagicConditionFactory.HasOptions(targetChoice)};
     }
 
     private static final MagicEventAction EVENT_ACTION = new MagicEventAction() {
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicLocationType fromLocation = MagicLocationType.OwnersHand;
             event.processTargetCard(game,new MagicCardAction() {
                 public void doAction(final MagicCard card) {
-                    game.doAction(new MagicRemoveCardAction(
-                        card,
-                        fromLocation
-                    ));
-                    game.doAction(new MagicMoveCardAction(
-                        card,
-                        fromLocation,
-                        MagicLocationType.Graveyard
+                    game.doAction(new MagicDiscardCardAction(
+                        event.getPlayer(),
+                        card
                     ));
                 }
             });
