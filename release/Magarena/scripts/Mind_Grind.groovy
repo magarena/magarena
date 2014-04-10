7555fc796e81456d8d35d19cb58b03bbd8c46362
melvin <melvinzhang@gmail.com>
2014-04-10 10:35:53 +0800

    show value of X in description, replace for loop with while loop for greater clarity

--- release/Magarena/scripts/Mind_Grind.groovy
+++ release/Magarena/scripts/Mind_Grind.groovy
@@ -5,20 +5,23 @@
             return new MagicEvent(
                 cardOnStack,
                 cardOnStack.getOpponent(),
+                payedCost.getX(),
                 this,
-                "PN reveals cards from the top of his or her library until he or she reveals X land cards, then puts all cards revealed this way into his or her graveyard. X can't be 0."
+                "PN reveals cards from the top of his or her library until he or she reveals RN land cards, " + 
+                "then puts all cards revealed this way into his or her graveyard."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getCardOnStack().getX();
+            final int amount = event.getRefInt();
             final MagicPlayer player = event.getPlayer();
-            for(int i = 0; i < amount; ) {
-                if(player.getLibrary().size() <= 0){ i=amount; }
-                else {
-                    if(player.getLibrary().getCardAtTop().hasType(MagicType.Land)){ i++; }
-                    game.doAction(new MagicMillLibraryAction(player,1));
+            final MagicCardList library = player.getLibrary();
+            int landCards = 0;
+            while (landCards < amount && library.size() > 0) {
+                if (library.getCardAtTop().hasType(MagicType.Land)) {
+                    landCards++;
                 }
+                game.doAction(new MagicMillLibraryAction(player,1));
             }
         }
     }
