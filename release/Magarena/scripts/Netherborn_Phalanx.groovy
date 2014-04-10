9974d0e768010417c64881bd64fb8dcca9ca21d1
Landhotel.Westerwald@gmail.com <unknown>
2014-04-10 23:44:32 +0200

    new script w/ Transmute

---
+++ release/Magarena/scripts/Netherborn_Phalanx.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                permanent.getController().getOpponent(),
+                this,
+                "PN loses 1 life for each creature PN controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                    event.getPlayer(),
+                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            final int amount = - targets.size();
+            game.doAction(new MagicChangeLifeAction(player,amount));
+        }
+    }
+]
