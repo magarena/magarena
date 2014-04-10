c66264c9fbf4cb44cf8b267f8b32b4d8f5cee8aa
Landhotel.Westerwald@gmail.com <unknown>
2014-04-10 21:51:14 +0200

    new script w/transmute

---
+++ release/Magarena/scripts/Perplex.groovy
@@ -0,0 +1,42 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller discards his or her hand."
+            );
+        }
+        public MagicEvent perplexChoice(final MagicSource source, final MagicCardOnStack card) {
+            return new MagicEvent(
+                source,
+                card.getController(),
+                new MagicMayChoice(),
+                card,
+                new MagicEventAction() {
+                    @Override
+                    public void executeEvent(final MagicGame game, final MagicEvent event) {
+                        if (event.isYes()) {
+                            game.addEvent(new MagicDiscardEvent(event.getSource(),event.getPlayer(),event.getPlayer().getHandSize()));
+                        } else {
+                            game.doAction(new MagicCounterItemOnStackAction(
+                                event.getRefItemOnStack(),
+                                MagicLocationType.Graveyard
+                            ));
+                        }
+                    } 
+                },
+                ""+card+" is countered, unless PN discards his or her hand.\$ "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                final MagicCardOnStack counteredCard ->
+                game.addEvent(perplexChoice(event.getSource(),counteredCard));
+            });
+        }
+    }
+]
