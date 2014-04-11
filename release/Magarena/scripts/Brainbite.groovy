b65da7afef44bfda6b5d51159e494631d7cbdd5a
melvin <melvinzhang@gmail.com>
2014-04-11 13:38:30 +0800

    Brainbite should choose target opponent as opponent may have hexproof

--- release/Magarena/scripts/Brainbite.groovy
+++ release/Magarena/scripts/Brainbite.groovy
@@ -1,23 +1,36 @@
+def DiscardDraw = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.getCardChoice().size() > 0) {
+        game.doAction(new MagicDiscardCardAction(event.getRefPlayer(),event.getCardChoice().get(0)));
+    }
+    game.doAction(new MagicDrawAction(event.getPlayer(),1));
+};
+
+
 [
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                cardOnStack.getController(),
-                new MagicFromCardListChoice(cardOnStack.getController().getOpponent().getHand(),1),
-                cardOnStack.getController().getOpponent(),
+                MagicTargetChoice.TARGET_OPPONENT,
                 this,
-                "RN reveals his or her hand. PN chooses a card\$ from it. RN discards that card."+
+                "Target opponent\$ reveals his or her hand. PN chooses a card from it. That player discards that card. "+
                 "PN draws a card."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if(event.getCardChoice().size() > 0) {
-                game.doAction(new MagicDiscardCardAction(event.getRefPlayer(),event.getCardChoice().get(0)));
-            }
-            game.doAction(new MagicDrawAction(event.getPlayer(),1));
+            event.processTargetPlayer(game, {
+                final MagicPlayer player ->
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    new MagicFromCardListChoice(player.getHand(),1),
+                    player,
+                    DiscardDraw,
+                    ""
+                ));
+            });
         }
     }
 ]
