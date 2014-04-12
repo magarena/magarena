caad9a69adf8bb8ea310bf8057a9b9bf033f12f5
melvin <melvinzhang@gmail.com>
2014-04-12 10:53:52 +0800

    add stub reveal card action

---
+++ src/magic/model/action/MagicRevealAction.java
@@ -0,0 +1,19 @@
+package magic.model.action;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+
+public class MagicRevealAction extends MagicAction {
+
+    public MagicRevealAction(final MagicCard card) {
+
+    }
+
+    public void doAction(final MagicGame game) {
+        //do nothing for now
+    }
+
+    public void undoAction(final MagicGame game) {
+        //do nothing for now
+    }
+}
