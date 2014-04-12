434223eaaa5d2a202c69f381f4879c0e138f0467
melvin <melvinzhang@gmail.com>
2014-04-12 16:19:48 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Ink_Eyes__Servant_of_Oni.groovy
+++ release/Magarena/scripts/Ink_Eyes__Servant_of_Oni.groovy
@@ -1,21 +1,17 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             final MagicTarget target = damage.getTarget();
-            return damage.getSource() == permanent &&
-                   damage.isCombat() &&
-                   target.isPlayer() ?
-                new MagicEvent(
-                    permanent,
-                    permanent.isController(target) ?
-                        MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD :
-                        MagicTargetChoice.TARGET_CREATURE_CARD_FROM_OPPONENTS_GRAVEYARD,
-                    MagicGraveyardTargetPicker.PutOntoBattlefield,
-                    this,
-                    "PN may put target creature card from ${target.toString()}'s graveyard\$ onto the battlefield under PN's control."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                permanent.isController(target) ?
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_GRAVEYARD :
+                    MagicTargetChoice.TARGET_CREATURE_CARD_FROM_OPPONENTS_GRAVEYARD,
+                MagicGraveyardTargetPicker.PutOntoBattlefield,
+                this,
+                "PN may put target creature card from ${target.toString()}'s graveyard\$ onto the battlefield under PN's control."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
