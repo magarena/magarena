0a032865a1e435c16c9bbbc58aa4bc39f3364c1d
melvin <melvinzhang@gmail.com>
2014-04-12 16:15:11 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Giant_Adephage.groovy
+++ release/Magarena/scripts/Giant_Adephage.groovy
@@ -1,16 +1,12 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            return (damage.getSource() == permanent &&
-                    damage.isCombat() &&
-                    damage.getTarget().isPlayer()) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "PN puts a token that's a copy of SN onto the battlefield."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a token that's a copy of SN onto the battlefield."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
