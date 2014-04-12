8dc24028210275cf53dcf382ff74c5f46d19390e
melvin <melvinzhang@gmail.com>
2014-04-12 18:26:11 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Nath_of_the_Gilt_Leaf.groovy
+++ release/Magarena/scripts/Nath_of_the_Gilt_Leaf.groovy
@@ -1,29 +1,4 @@
 [
-    new MagicAtUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.isController(upkeepPlayer) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicMayChoice(
-                        MagicTargetChoice.TARGET_OPPONENT
-                    ),
-                    this,
-                    "Target opponent\$ discards a card at random."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()){
-                event.processTargetPlayer(game,new MagicPlayerAction() {
-                    public void doAction(final MagicPlayer player) {
-                        game.addEvent(MagicDiscardEvent.Random(event.getSource(),player));
-                    }
-                });
-            }
-        }
-    },
     new MagicWhenDiscardedTrigger() {
         @Override
         public MagicEvent getEvent(final MagicPermanent permanent,final MagicCard card) {
