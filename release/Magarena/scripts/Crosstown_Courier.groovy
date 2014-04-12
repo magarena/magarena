7c7804c18dead160d812d582966a9e9dbeb724f6
melvin <melvinzhang@gmail.com>
2014-04-12 16:14:03 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Crosstown_Courier.groovy
+++ release/Magarena/scripts/Crosstown_Courier.groovy
@@ -1,20 +1,14 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            final int amount = damage.getDealtAmount();
-            return (amount > 0 &&
-                    damage.getSource() == permanent &&
-                    damage.getTarget().isPlayer() &&
-                    damage.isCombat()) ?
-                new MagicEvent(
-                    permanent,
-                    damage.getTargetPlayer(),
-                    amount,
-                    this,
-                    "PN puts the top RN cards into PN's graveyard."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                damage.getDealtAmount(),
+                this,
+                "PN puts the top RN cards into PN's graveyard."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
