9098ed70cf8dbe6bf0a1886981bf7adc118e583e
melvin <melvinzhang@gmail.com>
2014-04-12 16:13:19 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Balefire_Dragon.groovy
+++ release/Magarena/scripts/Balefire_Dragon.groovy
@@ -1,20 +1,14 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            final int amount = damage.getDealtAmount();
-            return (amount > 0 &&
-                    damage.getSource() == permanent &&
-                    damage.getTarget().isPlayer() &&
-                    damage.isCombat()) ?
-                new MagicEvent(
-                    permanent,
-                    damage.getTargetPlayer(),
-                    amount,
-                    this,
-                    "SN deals RN damage to each creature defending player controls."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                damage.getDealtAmount(),
+                this,
+                "SN deals RN damage to each creature PN controls."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
