e535f6bdb4c9b9381c6c3c6edd87641e860e278a
melvin <melvinzhang@gmail.com>
2014-04-12 16:10:45 +0800

    fix choice not targetting, replace condition with proper trigger

--- release/Magarena/scripts/Throat_Slitter.groovy
+++ release/Magarena/scripts/Throat_Slitter.groovy
@@ -6,24 +6,20 @@ def NONBLACK_CREATURE_YOUR_OPPONENT_CONTROLS=new MagicPermanentFilterImpl() {
 
 def TARGET_NONBLACK_CREATURE_YOUR_OPPONENT_CONTROLS = new MagicTargetChoice(
     NONBLACK_CREATURE_YOUR_OPPONENT_CONTROLS,
-    "a nonblack creature your opponent controls"
+    "target nonblack creature your opponent controls"
 );
 
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            return (damage.getSource() == permanent &&
-            damage.getTarget().isPlayer() &&
-            damage.isCombat()) ?
-        new MagicEvent(
-            permanent,
-            TARGET_NONBLACK_CREATURE_YOUR_OPPONENT_CONTROLS,
-            MagicDestroyTargetPicker.Destroy,
-            this,
-            "Destroy target nonblack creature defending player controls."
-        ):
-        MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                TARGET_NONBLACK_CREATURE_YOUR_OPPONENT_CONTROLS,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target nonblack creature defending player controls."
+            );
         }
 
         @Override
