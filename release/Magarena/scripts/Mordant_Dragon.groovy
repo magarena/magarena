b5b4950c16c52602e22f5aa7fd38426a68f6a09b
melvin <melvinzhang@gmail.com>
2014-04-12 16:21:16 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Mordant_Dragon.groovy
+++ release/Magarena/scripts/Mordant_Dragon.groovy
@@ -1,20 +1,16 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            final int amount=damage.getAmount();
-            return (damage.getSource() == permanent &&
-                    damage.getTarget().isPlayer() &&
-                    damage.isCombat()) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicMayChoice(MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS),
-                    new MagicDamageTargetPicker(amount),
-                    amount,
-                    this,
-                    "PN may\$ have SN deal RN damage to target creature\$ your opponent controls."
-                ):
-                MagicEvent.NONE;
+            final int amount = damage.getDealtAmount();
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS),
+                new MagicDamageTargetPicker(amount),
+                amount,
+                this,
+                "PN may\$ have SN deal RN damage to target creature\$ your opponent controls."
+            );
         }
 
         @Override
