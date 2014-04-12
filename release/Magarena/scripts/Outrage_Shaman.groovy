366c602edb76c65dc4f3bbc9f10d9a5a26cb70a2
melvin <melvinzhang@gmail.com>
2014-04-12 18:45:27 +0800

    replace MagicTargetAction with groovy closure

--- release/Magarena/scripts/Outrage_Shaman.groovy
+++ release/Magarena/scripts/Outrage_Shaman.groovy
@@ -7,19 +7,17 @@
                 permanent,
                 MagicTargetChoice.NEG_TARGET_CREATURE,
                 new MagicDamageTargetPicker(amount),
-                amount,
                 this,
                 "SN deals damage to target creature\$ equal the number of red mana symbols in the mana costs of permanents PN controls."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTarget(game,new MagicTargetAction() {
-                public void doAction(final MagicTarget target) {
-                    final MagicDamage damage=new MagicDamage(event.getSource(),target,event.getRefInt());
-                    game.doAction(new MagicDealDamageAction(damage));
-                }
+            event.processTarget(game, {
+                final MagicTarget target ->
+                final int amount = event.getPlayer().getDevotion(MagicColor.Red);
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,amount);
+                game.doAction(new MagicDealDamageAction(damage));
             });
         }
-    }
-]
+    }]
