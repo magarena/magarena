9298bb3887718ecbe26cbe5dbe2d945bd2910687
melvin <melvinzhang@gmail.com>
2014-04-12 10:14:45 +0800

    replace getHand with getHandSize

--- release/Magarena/scripts/Guul_Draz_Specter.groovy
+++ release/Magarena/scripts/Guul_Draz_Specter.groovy
@@ -2,7 +2,7 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (permanent.getOpponent().getHand().isEmpty()) {
+            if (permanent.getOpponent().getHandSize() == 0) {
                 pt.add(3,3);
             }
         }
