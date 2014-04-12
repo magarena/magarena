926076807faab04f57e9cec47662f21475330efb
melvin <melvinzhang@gmail.com>
2014-04-12 11:07:02 +0800

    allow multiple cards to be revealed at once

--- src/magic/model/action/MagicRevealAction.java
+++ src/magic/model/action/MagicRevealAction.java
@@ -3,10 +3,19 @@ package magic.model.action;
 import magic.model.MagicCard;
 import magic.model.MagicGame;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class MagicRevealAction extends MagicAction {
+    
+    private final Collection<MagicCard> cards = new ArrayList<MagicCard>();
 
-    public MagicRevealAction(final MagicCard card) {
+    public MagicRevealAction(final MagicCard aCard) {
+        cards.add(aCard);
+    }
 
+    public MagicRevealAction(final Collection<MagicCard> aCards) {
+        cards.addAll(aCards);
     }
 
     public void doAction(final MagicGame game) {
