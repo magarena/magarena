007c564c010f1cfcf6e01c0e0c81e24a4b46a817
melvin <melvinzhang@gmail.com>
2014-04-12 16:20:10 +0800

    replace condition with more specific trigger

--- release/Magarena/scripts/Shriekgeist.groovy
+++ release/Magarena/scripts/Shriekgeist.groovy
@@ -1,22 +1,13 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
-        public MagicEvent executeTrigger(
-                final MagicGame game,
-                final MagicPermanent permanent,
-                final MagicDamage damage) {
-            final MagicTarget target = damage.getTarget();
-            return (permanent == damage.getSource() &&
-                    target.isPlayer() &&
-                    damage.isCombat()) ?
-                new MagicEvent(
-                    permanent,
-                    damage.getTargetPlayer(),
-                    this,
-                    "PN puts the top two cards of " +
-                    "his or her library into his or her graveyard."
-                ):
-                MagicEvent.NONE;
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                this,
+                "PN puts the top two cards of his or her library into his or her graveyard."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
