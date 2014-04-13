afde64c33b7f0cc5c96b94feb88bef77a3eec8d6
melvin <melvinzhang@gmail.com>
2014-04-13 13:35:26 +0800

    alternative fix for Fling that checks payedCost.getTarget

--- release/Magarena/scripts/Fling.groovy
+++ release/Magarena/scripts/Fling.groovy
@@ -2,24 +2,24 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                payedCost.getTarget(),
-                this,
-                "SN deals damage equal to RN's power to target creature or player\$."
-            );
+            // FIXME: getTarget may be MagicTargetNone when not cast from hand
+            return payedCost.getTarget().isPermanent() ?
+                new MagicEvent(
+                    cardOnStack,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                    payedCost.getTarget(),
+                    this,
+                    "SN deals damage equal to RN's power to target creature or player\$."
+                ):
+                MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTarget(game, {
                 final MagicTarget target ->
-                // temp fix for case where additional cost is not payed when spell is not cast form hand
-                event.processRefPermanent(game, {
-                    final MagicPermanent sacrificed ->
-                    final MagicDamage damage=new MagicDamage(event.getSource(),target,sacrificed.getPower());
-                    game.doAction(new MagicDealDamageAction(damage));
-                });
+                final MagicPermanent sacrificed = event.getRefPermanent();
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,sacrificed.getPower());
+                game.doAction(new MagicDealDamageAction(damage));
             });
         }
     }
