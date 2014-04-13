92cffafb69f6f6da4e55445e11a2c78ba22fe1fd
Landhotel.Westerwald@gmail.com <unknown>
2014-04-13 11:17:51 +0200

    rewrote Diabolic Vision, working better now

--- release/Magarena/scripts/Diabolic_Vision.groovy
+++ release/Magarena/scripts/Diabolic_Vision.groovy
@@ -1,53 +1,3 @@
-def DrawOne = {
-    final MagicGame game, final MagicEvent event ->
-    event.processChosenCards(game, {
-        final MagicCard card ->
-        game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
-        game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.OwnersHand));
-    });    
-};
-
-def PutBackRest = {
-    final MagicGame prevGame, final MagicEvent prevEvent ->
-    final MagicCardList putBackList = new MagicCardList();
-    final int size = prevEvent.getPlayer().getLibrary().size();
-    if (5 > size && size >= 2) {
-        for(int i = 1; i <= size; i++) {
-            putBackList.add(prevEvent.getPlayer().getLibrary().get(size-i));
-        }
-        prevGame.addEvent(new MagicEvent(
-            prevEvent.getSource(),
-            new MagicFromCardListChoice(putBackList,size),
-            {
-                final MagicGame game, final MagicEvent event ->
-                event.processChosenCards(game, {
-                    final MagicCard card ->
-                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
-                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.TopOfOwnersLibrary));
-                });
-            },
-            ""
-        ));           
-    } else if (size > 4) {
-        for(int i = 0; i < 4; i++) {
-            putBackList.add(prevEvent.getPlayer().getLibrary().get(4-i));
-        }
-        prevGame.addEvent(new MagicEvent(
-            prevEvent.getSource(),
-            new MagicFromCardListChoice(putBackList,4),
-            {
-                final MagicGame game, final MagicEvent event ->
-                event.processChosenCards(game, {
-                    final MagicCard card ->
-                    game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
-                    game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.TopOfOwnersLibrary));
-                });
-            },
-            ""
-        ));
-    }
-};
-
 [
     new MagicSpellCardEvent() {
         @Override
@@ -63,38 +13,50 @@ def PutBackRest = {
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicCardList choiceList = new MagicCardList();
             final int size = event.getPlayer().getLibrary().size();
+            final int amount = (size > 4) ? 5 : size;
             if (size == 1) game.doAction(new MagicDrawAction(event.getPlayer(),1));
-            else if (6 > size && size >= 2) {
-                for(int i = 1; i <= size; i++) {
+            else if (size > 1) {
+                for(int i = 1; i <= amount; i++) {
                     choiceList.add(event.getPlayer().getLibrary().get(size-i));
                 }
                 game.addEvent(new MagicEvent(
                     event.getSource(),
                     new MagicFromCardListChoice(choiceList,1),
-                    DrawOne,
-                    "\$"
-                ));
-                game.addEvent(new MagicEvent(
-                    event.getSource(),
-                    PutBackRest,
+                    {
+                        final MagicGame game2, final MagicEvent event2 ->
+                        event2.processChosenCards(game2, {
+                            final MagicCard card ->
+                            game2.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
+                            game2.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.OwnersHand));
+                        }); 
+                        game.addEvent(new MagicEvent(
+                            event2.getSource(),
+                            {
+                                final MagicGame game3, final MagicEvent event3 ->
+                                final MagicCardList putBackList = new MagicCardList();
+                                for(int i = 2; i <= amount; i++) {
+                                    putBackList.add(event3.getPlayer().getLibrary().get(size-i));
+                                }
+                                game3.addEvent(new MagicEvent(
+                                    event3.getSource(),
+                                    new MagicFromCardListChoice(putBackList,putBackList.size()),
+                                    {
+                                        final MagicGame game4, final MagicEvent event4 ->
+                                        event4.processChosenCards(game4, {
+                                            final MagicCard card ->
+                                            game4.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersLibrary));
+                                            game4.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersLibrary,MagicLocationType.TopOfOwnersLibrary));
+                                        });
+                                    },
+                                    ""
+                                ));           
+                            },
+                            ""
+                        ));
+                    },
                     ""
-                ));
-            } else if (size > 5) {
-                for(int i = 0; i < 5; i++) {
-                    choiceList.add(event.getPlayer().getLibrary().get(5-i));
-                }
-                game.addEvent(new MagicEvent(
-                    event.getSource(),
-                    new MagicFromCardListChoice(choiceList,1),
-                    DrawOne,
-                    "\$"
-                ));
-                game.addEvent(new MagicEvent(
-                    event.getSource(),
-                    PutBackRest,
-                    ""
-                ));
-            } 
+                )); 
+            }
         }
     }
 ]
