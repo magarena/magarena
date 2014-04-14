eb256d42a1ad7d75bfaa74f10ec75786efc3a67c
melvin <melvinzhang@gmail.com>
2014-04-14 14:49:45 +0800

    add test for Cascade into Fling

---
+++ src/magic/test/TestCascade.java
@@ -0,0 +1,48 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.MagicDeckProfile;
+import magic.model.phase.MagicMainPhase;
+
+class TestCascade extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicDeckProfile profile=new MagicDeckProfile("bgruw");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile,15);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile,14);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame(true);
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(13);
+        addToLibrary(P, "Lightning Bolt", 1);
+        addToLibrary(P, "Fling", 1);
+        addToGraveyard(P, "Lightning Bolt", 1);
+        createPermanent(game,P, "Mountain", false, 8);
+        createPermanent(game,P, "Chromatic Lantern", false, 1);
+        addToHand(P, "Bloodbraid Elf", 1);
+
+        P = opponent;
+
+        P.setLife(13);
+        addToLibrary(P, "Forest", 20);
+        addToLibrary(P, "Grizzly Bears", 1);
+        addToHand(P, "Brainbite", 1);
+        createPermanent(game,P,"Forest",false,5);
+        createPermanent(game,P, "Glasses of Urza", false, 1);
+        addToGraveyard(P, "Grizzly Bears", 1);
+
+        return game;
+    }
+}
