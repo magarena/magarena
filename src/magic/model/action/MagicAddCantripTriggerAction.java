d9742471fee4fb701dfe5fca6722754f2ee14f19
Landhotel.Westerwald@gmail.com <unknown>
2014-04-14 13:26:34 +0200

    export: cantrip to MagicAddCantripTriggerAction, add: Mind Ravel, not yet working properly

---
+++ src/magic/model/action/MagicAddCantripTriggerAction.java
@@ -0,0 +1,56 @@
+package magic.model.action;
+
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.trigger.MagicPermanentTrigger;
+import magic.model.trigger.MagicTrigger;
+import magic.model.event.MagicEvent;
+import magic.model.trigger.MagicAtUpkeepTrigger;
+import magic.model.MagicPlayer;
+
+public class MagicAddCantripTriggerAction extends MagicAction {
+
+    private final MagicPermanent permanent;
+    private final MagicTrigger<?> trigger;
+    private MagicPermanentTrigger permanentTrigger;
+
+    public MagicAddCantripTriggerAction(final MagicEvent event) {
+        this.permanent=MagicPermanent.NONE;
+        this.trigger=cantrip(event);
+    }
+    
+    public MagicAtUpkeepTrigger cantrip(final MagicEvent event) {
+        return new MagicAtUpkeepTrigger() {
+            @Override
+            public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+                return new MagicEvent(
+                    event.getSource(),
+                    event.getPlayer(),
+                    this,         
+                    "PN draws a card"
+                );
+            }
+        
+            @Override
+            public void executeEvent(final MagicGame game, final MagicEvent event) {
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+                game.doAction(new MagicRemoveTriggerAction(this));
+            }
+        }; 
+    }
+    
+    @Override
+    public void doAction(final MagicGame game) {
+        permanentTrigger=game.addTrigger(permanent,trigger);
+    }
+
+    @Override
+    public void undoAction(final MagicGame game) {
+        game.removeTrigger(permanentTrigger);
+    }
+    
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+" ("+permanent+','+trigger+')';
+    }
+}
---
+++ src/magic/model/event/Mind_Ravel.groovy
@@ -0,0 +1,41 @@
+def MagicAtUpkeepTrigger cantrip(final MagicEvent event) {
+    return new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                event.getSource(),
+                event.getPlayer(),
+                {
+                    final MagicGame game2, final MagicEvent event2 ->
+                    game2.doAction(new MagicDrawAction(event2.getPlayer()));
+                    game2.doAction(new MagicRemoveTriggerAction(this));
+                },
+                "PN draws a card"
+            );
+        }
+    }
+}; 
+
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ discards a card."+
+                "PN draws a card at the beginning of the next turn's upkeep."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer player ->
+                game.addEvent(new MagicDiscardEvent(event.getSource(), player, 1));
+            });
+            game.doAction(new MagicAddTriggerAction(cantrip(event))); 
+        }
+    }
+]
