a371a48055517a1e9ac9c97ea63481c48bb4da1e
Lodici <Lodici>
2014-04-16 19:41:08 +0100

    issue 587: Display Player which is going first on Mulligan Screen.

--- src/magic/ui/screen/MulliganScreen.java
+++ src/magic/ui/screen/MulliganScreen.java
@@ -1,18 +1,25 @@
 package magic.ui.screen;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.AbstractAction;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
+import net.miginfocom.swing.MigLayout;
 import magic.data.GeneralConfig;
 import magic.data.IconImages;
 import magic.model.MagicCardList;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
 import magic.ui.canvas.cards.CardsCanvas;
 import magic.ui.canvas.cards.CardsCanvas.LayoutMode;
 import magic.ui.choice.MulliganChoicePanel;
@@ -33,10 +40,12 @@ public class MulliganScreen
     private CardsCanvas content;
     private MulliganChoicePanel choicePanel;
     private final AbstractAction takeMulliganAction = new TakeMulliganAction();
+    private final StatusPanel statusPanel;
 
     public MulliganScreen(final MulliganChoicePanel choicePanel, final MagicCardList hand) {
         this.choicePanel = choicePanel;
         isActive = true;
+        statusPanel = new StatusPanel(choicePanel.getGameController().getGame());
         setContent(getScreenContent(hand));
         setEscapeKeyInputMap();
     }
@@ -92,7 +101,7 @@ public class MulliganScreen
      */
     @Override
     public String getScreenCaption() {
-        return "Confirm Hand or Take a Mulligan ";
+        return "Mulligan?";
     }
 
     /* (non-Javadoc)
@@ -169,7 +178,42 @@ public class MulliganScreen
      */
     @Override
     public JPanel getStatusPanel() {
-        return null;
+        return statusPanel;
+    }
+
+    private class StatusPanel extends JPanel {
+
+        // ui
+        private final MigLayout migLayout = new MigLayout();
+        private final JLabel playingFirstLabel = new JLabel();
+
+        public StatusPanel(final MagicGame game) {
+            setLookAndFeel();
+            setContent(game);
+        }
+
+        private void setLookAndFeel() {
+            setOpaque(false);
+            setLayout(migLayout);
+            // playing first label
+            playingFirstLabel.setForeground(Color.WHITE);
+            playingFirstLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+            playingFirstLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        }
+
+        private void refreshLayout() {
+            removeAll();
+            migLayout.setLayoutConstraints("insets 0, gap 2, flowy");
+            add(playingFirstLabel, "w 100%");
+        }
+
+        private void setContent(final MagicGame game) {
+            final MagicPlayer turnPlayer = game.getTurnPlayer();
+            final MagicPlayer humanPlayer = game.getPlayer(0);
+            playingFirstLabel.setText("You play " + (turnPlayer == humanPlayer ? "first." : "second."));
+            refreshLayout();
+        }
+
     }
 
 }
