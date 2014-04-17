0969e5849336e81ba77e7da2daf0bd8ed8ebd12f
ShawnieBoy <unknown>
2014-04-17 15:37:37 +0100

    And the EnglishToInt.java would help as well...

---
+++ src/magic/data/EnglishToInt.java
@@ -0,0 +1,27 @@
+package magic.data;
+
+public class EnglishToInt {
+
+    public static int convert(String num) {
+        if (num == null) {
+            return 1;
+        }
+        switch (num) {
+            case "a": return 1;
+            case "an": return 1;
+            case "two": return 2;
+            case "three" : return 3;
+            case "four" : return 4;
+            case "five" : return 5;
+            case "six" : return 6;
+            case "seven" : return 7;
+            case "eight" : return 8;
+            case "nine" : return 9;
+            case "ten" : return 10;
+            case "eleven" : return 11;
+            case "twelve" : return 12;
+            case "thirteen" : return 13;
+            default: throw new RuntimeException("Unknown count: " + num);
+        }
+    }
+}
