9635bec14862de7f6745c65a6ad7b176479b802b
melvin <melvinzhang@gmail.com>
2014-04-18 19:57:47 +0800

    add Hydra Broodmaster, missing image for Hydra token

---
+++ release/Magarena/incomplete/Hydra_Broodmaster.groovy
@@ -0,0 +1,72 @@
+def PutHydra = new MagicTrigger<Integer>() {
+    @Override
+    public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent perm, final Integer X) {
+        return new MagicEvent(
+            perm,
+            X,
+            this,
+            "Put RN RN/RN green Hydra creature tokens onto the battlefield."
+        );
+    }
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        final int X = event.getRefInt();
+        final MagicCardDefinition tokenDef = TokenCardDefinitions.get("green Hydra creature token");
+        final MagicStatic PT = new MagicStatic(MagicLayer.SetPT) {
+            @Override
+            public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+                pt.set(X,X);
+            }
+        };
+        for (int i = 0; i < X; i++) {
+            final MagicPlayTokenAction act = new MagicPlayTokenAction(event.getPlayer(), tokenDef)
+            game.doAction(act);
+            game.doAction(new MagicAddStaticAction(act.getPermanent(),PT));
+        }
+    }
+    @Override
+    public MagicTriggerType getType() {
+        return MagicTriggerType.WhenBecomesState;
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        [MagicCondition.NOT_MONSTROUS_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Monstrosity"
+    ) {
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{X}{X}{G}")
+            ];
+        }
+        
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getX(),
+                this,
+                "If SN isn't monstrous, put RN +1/+1 counter on it and it becomes monstrous."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOne,
+                event.getRefInt(),
+                true
+            ));
+            final MagicChangeStateAction act = MagicChangeStateAction.Set(
+                event.getPermanent(),
+                MagicPermanentState.Monstrous
+            );
+            game.doAction(act);
+            game.executeTrigger(PutHydra, event.getPermanent(), event.getSource(), event.getRefInt());
+        }
+    }
+]
