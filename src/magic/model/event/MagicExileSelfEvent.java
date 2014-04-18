0b5ea7faa583cbf7b3f5994e93a2e0f2f2b8ea95
melvin <melvinzhang@gmail.com>
2014-04-18 19:53:08 +0800

    support 'scavenge <mana cost>' in ability

---
+++ src/magic/model/event/MagicExileSelfEvent.java
@@ -0,0 +1,38 @@
+package magic.model.event;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicSource;
+import magic.model.MagicLocationType;
+import magic.model.action.MagicRemoveCardAction;
+import magic.model.action.MagicMoveCardAction;
+
+public class MagicExileSelfEvent extends MagicEvent {
+
+    public MagicExileSelfEvent(final MagicCard card, final MagicLocationType fromLocation) {
+        super(
+            card,
+            fromLocation.ordinal(),
+            EVENT_ACTION,
+            "PN exiles SN."
+        );
+    }
+
+    private static final MagicEventAction EVENT_ACTION = new MagicEventAction() {
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard card = event.getCard();
+            final MagicLocationType fromLocation = MagicLocationType.values()[event.getRefInt()];
+            game.doAction(new MagicRemoveCardAction(
+                card,
+                fromLocation
+            ));
+            game.doAction(new MagicMoveCardAction(
+                card,
+                fromLocation,
+                MagicLocationType.Exile
+            ));
+        }
+    };
+}
---
+++ src/magic/model/event/MagicScavengeActivation.java
@@ -0,0 +1,95 @@
+package magic.model.event;
+
+import magic.model.MagicCard;
+import magic.model.MagicGame;
+import magic.model.MagicManaCost;
+import magic.model.MagicPayedCost;
+import magic.model.MagicSource;
+import magic.model.MagicCardDefinition;
+import magic.model.MagicLocationType;
+import magic.model.MagicCounterType;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicPutItemOnStackAction;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.action.MagicPermanentAction;
+import magic.model.choice.MagicTargetChoice;
+import magic.model.choice.MagicChoice;
+import magic.model.condition.MagicCondition;
+import magic.model.stack.MagicAbilityOnStack;
+import magic.model.target.MagicPumpTargetPicker;
+
+import java.util.Arrays;
+
+public class MagicScavengeActivation extends MagicGraveyardActivation {
+
+    private static final MagicCondition[] COND = new MagicCondition[]{ MagicCondition.SORCERY_CONDITION };
+    private static final MagicActivationHints HINT = new MagicActivationHints(MagicTiming.Pump,true);
+    final MagicManaCost cost;
+    final MagicCardDefinition cdef;
+
+    public MagicScavengeActivation(final MagicCardDefinition aCdef, final MagicManaCost aCost) {
+        super(
+            COND,
+            HINT,
+            "Scavenge"
+        );
+        cost = aCost;
+        cdef = aCdef;
+    }
+
+    public Iterable<? extends MagicEvent> getCostEvent(final MagicCard source) {
+        return Arrays.asList(
+            new MagicPayManaCostEvent(source, cost),
+            new MagicExileSelfEvent(source, MagicLocationType.Graveyard)
+        );
+    }
+
+    @Override
+    public MagicEvent getEvent(final MagicSource source) {
+        return new MagicEvent(
+            source,
+            new MagicEventAction() {
+                @Override
+                public void executeEvent(final MagicGame game, final MagicEvent event) {
+                    final MagicAbilityOnStack abilityOnStack = new MagicAbilityOnStack(
+                        MagicScavengeActivation.this,
+                        getCardEvent(event.getCard(), game.getPayedCost()),
+                        game.getPayedCost()
+                    );
+                    game.doAction(new MagicPutItemOnStackAction(abilityOnStack));
+                }
+            },
+            "Scavenge SN."
+        );
+    }
+
+    public MagicEvent getCardEvent(final MagicCard source,final MagicPayedCost payedCost) {
+        return new MagicEvent(
+            source,
+            MagicTargetChoice.POS_TARGET_CREATURE,
+            MagicPumpTargetPicker.create(),
+            cdef.getCardPower(),
+            this,
+            "Put RN +1/+1 counters on target creature$."
+        );
+    }
+    
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        event.processTargetPermanent(game, new MagicPermanentAction() {
+            public void doAction(final MagicPermanent perm) {
+                game.doAction(new MagicChangeCountersAction(
+                    perm,
+                    MagicCounterType.PlusOne,
+                    event.getRefInt(),
+                    true
+                ));
+            }
+        });
+    }
+
+    @Override
+    final MagicChoice getChoice(final MagicCard source) {
+        return getCardEvent(source,MagicPayedCost.NO_COST).getChoice();
+    }
+}
