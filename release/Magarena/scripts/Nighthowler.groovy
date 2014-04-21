d9898b086c4f61ba148c97dc905686aa04176a13
hong yie <unknown>
2014-04-21 15:43:51 +0000

    Add script for Nighthowler

---
+++ release/Magarena/scripts/Nighthowler.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            final int size =
+                game.filterCards(player,MagicTargetFilterFactory.CREATURE_CARD_FROM_ALL_GRAVEYARDS).size();							 
+            pt.add(size,size);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source == target || MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
