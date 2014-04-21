f932de19ba00d18d195f48170822e80dc1caa277
melvin <melvinzhang@gmail.com>
2014-04-21 10:49:51 +0800

    check that spell is targeted, target choice can be shared across MagicGames

--- release/Magarena/scripts/Deflection.groovy
+++ release/Magarena/scripts/Deflection.groovy
@@ -1,6 +1,8 @@
 def SINGLE_TARGET_SPELL = new MagicStackFilterImpl() {
     public boolean accept(final MagicGame game,final MagicPlayer player,final MagicItemOnStack target) {
-        return target.isSpell() && target.getEvent().getTargetChoice() != MagicTargetChoice.NONE // we need something here to establish singleTarget and multiTarget Conditions
+        // we need something here to establish singleTarget and multiTarget Conditions
+        final MagicTargetChoice tchoice = target.getEvent().getTargetChoice();
+        return target.isSpell() && tchoice.isValid() && tchoice.isTargeted();
     }
 };
 
@@ -18,7 +20,11 @@ def ChangeTargetAction = {
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                new MagicTargetChoice(SINGLE_TARGET_SPELL, MagicTargetHint.None, "target spell with a single target"),
+                new MagicTargetChoice(
+                    SINGLE_TARGET_SPELL,
+                    MagicTargetHint.Negative,
+                    "target spell with a single target"
+                ),
                 this,
                 "PN changes the target of target spell with a single target.\$"
             );
@@ -27,14 +33,9 @@ def ChangeTargetAction = {
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetCardOnStack(game, {
                 final MagicCardOnStack targetSpell ->
-                final MagicChoice targetChoice = targetSpell.getEvent().getTargetChoice();
-                final MagicTargetFilter targetFilter = targetChoice.getTargetFilter();
-                final MagicTargetHint targetHint = targetChoice.getTargetHint(true);
-                final String targetDescription = targetChoice.getTargetDescription();
-                
                 game.addFirstEvent(new MagicEvent( 
                     event.getSource(),
-                    new MagicTargetChoice(targetFilter, targetHint, targetDescription),
+                    targetSpell.getEvent().getTargetChoice(),
                     targetSpell,
                     ChangeTargetAction,
                     ""
