8bb2d0ac26c4d0b51d64b40e4b98acd0b49fc2bd
Landhotel.Westerwald@gmail.com <unknown>
2014-04-22 19:25:16 +0200

    add: Bog Hoodlums

---
+++ release/Magarena/scripts/Bog_Hoodlums.groovy
@@ -0,0 +1,13 @@
+def clashAction = {
+    final MagicGame game, final MagicEvent event ->
+    game.doAction(new MagicChangeCountersAction(event.getPermanent(), MagicCounterType.PlusOne, 1, true));
+}
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicClashEvent(permanent, permanent.getController(), clashAction);
+        }
+    }
+]
