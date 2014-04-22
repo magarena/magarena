e7a7869d35d048de3c5e96eecf1c8296804bec3b
melvin <melvinzhang@gmail.com>
2014-04-22 21:33:54 +0800

    clean up groovy code for Rebellion of the Flamekin

--- release/Magarena/scripts/Rebellion_of_the_Flamekin.groovy
+++ release/Magarena/scripts/Rebellion_of_the_Flamekin.groovy
@@ -8,16 +8,19 @@
                 new MagicMayChoice(new MagicPayManaCostChoice(MagicManaCost.create("{1}"))),
                 winInt,
                 this,
-                "PN may pay 1. If he or she does, PN puts a 3/1 red Elemental Shaman creature token onto the battlefield. If PN won the clash, that token gains haste until end of turn. "
+                "PN may\$ pay {1}. If he or she does, PN puts a 3/1 red Elemental Shaman creature token onto the battlefield. " + 
+                "If PN won the clash, that token gains haste until end of turn. "
             );
-
         }
         
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if(event.isYes()) {
-                if(event.getRefInt() == 1) game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("3/1 red Elemental Shaman creature token"), MagicPlayMod.HASTE_UEOT));
-                else game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("3/1 red Elemental Shaman creature token")));
+            if (event.isYes()) {
+                game.doAction(new MagicPlayTokenAction(
+                    event.getPlayer(),
+                    TokenCardDefinitions.get("3/1 red Elemental Shaman creature token"),
+                    (event.getRefInt() == 1) ? [MagicPlayMod.HASTE_UEOT] : [MagicPlayMod.NONE]
+                ));
             }
         }
     }
