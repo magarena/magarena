79a5cb3427e1f7f0356563d012516a1ab3049537
melvin <melvinzhang@gmail.com>
2014-04-22 13:57:39 +0800

    add missing requires_groovy_code, add check during resolution for intervening if clause

--- release/Magarena/scripts/Damia__Sage_of_Stone.groovy
+++ release/Magarena/scripts/Damia__Sage_of_Stone.groovy
@@ -2,19 +2,21 @@
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return (permanent.getController() == upkeepPlayer && permanent.getController().getHandSize()<7) ? new MagicEvent(
-                permanent,
-                permanent.getController(),
-                this,
-                "if PN have fewer than seven cards in hand, draw cards equal to the difference."
-            ):
-            MagicEvent.NONE
+            return (permanent.isController(upkeepPlayer) && upkeepPlayer.getHandSize() < 7) ? 
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "If PN has fewer than seven cards in hand, draw cards equal to the difference."
+                ):
+                MagicEvent.NONE
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPlayer player = event.getPlayer();
-            final int amount = 7 - player.getHandSize() ;            
-            game.doAction(new MagicDrawAction(event.getPlayer(),amount));
+            final int amount = 7 - player.getHandSize();
+            if (amount > 0) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),amount));
+            }
         }
     }
 ]
