95e95c7ecd20c56d59cbe995b6fe7aa5b46544c4
melvin <melvinzhang@gmail.com>
2014-04-23 10:42:50 +0800

    startScrying should use source, player from event passed in from executeEvent, not source, player from event creation

--- src/magic/model/event/MagicScryXEvent.java
+++ src/magic/model/event/MagicScryXEvent.java
@@ -13,52 +13,30 @@ import magic.model.choice.MagicFromCardListChoice;
 public class MagicScryXEvent extends MagicEvent {
 
     public MagicScryXEvent(final MagicSource source, final MagicPlayer player, final int X) {
-          super(source, player, X, startScrying(source, player, X), "");
+        super(
+            source, 
+            player, 
+            X, 
+            StartScrying, 
+            ""
+        );
     }
     
-    private static int getActualX(final MagicPlayer player, final int X) {
-        final int size = player.getLibrary().size();
-        return (size > X) ? X : size;
-    }
-    
-    private static MagicCardList getScryList(final MagicPlayer player, final int X) {
-        final MagicCardList scryList = new MagicCardList();
-        final int size = player.getLibrary().size();
-        for(int i = 1; i <= getActualX(player, X); i++) {
-                scryList.add(player.getLibrary().get(size-i));
-        }
-        return scryList;
-    }
-        
-    private static MagicEventAction startScrying(final MagicSource source, final MagicPlayer player, final int X) {
-        return new MagicEventAction() {
-            @Override
-            public void executeEvent(final MagicGame game, final MagicEvent event) {
-                game.addFirstEvent(new MagicEvent(
-                    source, 
-                    player, 
-                    new MagicFromCardListChoice(getScryList(player, X), 
-                    getActualX(player, X), true), 
-                    new MagicInteger(getActualX(player, X)), 
-                    BottomAction, 
-                    ""
-                ));
-            }
-        };
-    }
-    
-    private static final MagicEventAction TopAction = new MagicEventAction() {
+    private static final MagicEventAction StartScrying = new MagicEventAction() {
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processChosenCards(game, new MagicCardAction() {
-                public void doAction(final MagicCard card) {
-                    game.doAction(new MagicScryComplAction(event.getPlayer(), card, false));
-                    game.logAppendMessage(event.getPlayer(), event.getPlayer() + " puts back a card to the top of his or her library.");
-                }
-            });
+            final int X = event.getRefInt();
+            final MagicCardList choiceList = event.getPlayer().getLibrary().getCardsFromTop(X);
+            game.addFirstEvent(new MagicEvent(
+                event.getSource(),
+                event.getPlayer(),
+                new MagicFromCardListChoice(choiceList, choiceList.size(), true), 
+                choiceList.size(),
+                BottomAction, 
+                ""
+            ));
         }
     };
-    
     
     private static final MagicEventAction BottomAction = new MagicEventAction() {
         @Override
@@ -71,19 +49,27 @@ public class MagicScryXEvent extends MagicEvent {
                     game.logAppendMessage(event.getPlayer(), event.getPlayer() + " moves a card from top of his or her library to the bottom.");
                 }
             });
-            final int actualX =  getActualX(event.getPlayer(), event.getRefInt() - processedCards.size());
-            //escape, no sense in calling TopAction if there is no choice to make
-            //addFirstEvent, Bottom and TopAction need to be execute in immediate succession, otherwise it breaks     
-            if(actualX > 1) {                                     
-                game.addFirstEvent(new MagicEvent(                    
-                    event.getSource(),
-                    event.getPlayer(),
-                    new MagicFromCardListChoice(getScryList(event.getPlayer(), actualX), actualX),
-                    actualX,
-                    TopAction,
-                    ""
-                ));
-            }
+            final int X = event.getRefInt() - processedCards.size();
+            final MagicCardList choiceList = event.getPlayer().getLibrary().getCardsFromTop(X);
+            game.addFirstEvent(new MagicEvent(                    
+                event.getSource(),
+                event.getPlayer(),
+                new MagicFromCardListChoice(choiceList, choiceList.size()),
+                TopAction,
+                ""
+            ));
+        }
+    };
+    
+    private static final MagicEventAction TopAction = new MagicEventAction() {
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processChosenCards(game, new MagicCardAction() {
+                public void doAction(final MagicCard card) {
+                    game.doAction(new MagicScryComplAction(event.getPlayer(), card, false));
+                    game.logAppendMessage(event.getPlayer(), event.getPlayer() + " puts back a card to the top of his or her library.");
+                }
+            });
         }
     };
 }
