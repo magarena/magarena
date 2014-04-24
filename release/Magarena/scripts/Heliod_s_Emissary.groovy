a47e2f6434a45351081a2e65cc9db3b9a6a3a80f
ShawnieBoy <unknown>
2014-04-24 19:18:35 +0100

    Edit enchantment scripts for new syntax

---
+++ release/Magarena/scripts/Heliod_s_Emissary.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (permanent.getEnchantedPermanent()==creature) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
+                    MagicTapTargetPicker.Tap,
+                    this,
+                    "PN taps target creature\$ an opponent controls."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicTapAction(creature,true));
+            });
+        }
+    }
+]
