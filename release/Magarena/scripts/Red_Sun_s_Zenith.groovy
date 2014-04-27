9d6af61dd8e5d7c3a8b096e1bc07ceeabca2e9c0
melvin <melvinzhang@gmail.com>
2014-04-27 09:47:33 +0800

    replace cast with processTargetPermanent

--- release/Magarena/scripts/Red_Sun_s_Zenith.groovy
+++ release/Magarena/scripts/Red_Sun_s_Zenith.groovy
@@ -2,30 +2,38 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final int amount=payedCost.getX();
+            final int amount = payedCost.getX();
             return new MagicEvent(
                 cardOnStack,
                 MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                 new MagicDamageTargetPicker(amount),
+                amount,
                 this,
-                "SN deals X ("+amount+") damage to target creature or player.\$ " +
+                "SN deals RN damage to target creature or player.\$ " +
                 "If a creature dealt damage this way would die this turn, exile it instead. "+
                 "Shuffle SN into its owner's library."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getCardOnStack().getX();
+            final int amount = event.getRefInt();
+            int dealtDamage = 0;
             event.processTarget(game, {
                 final MagicTarget target ->
                 final MagicDamage damage=new MagicDamage(event.getSource(),target,amount);
                 game.doAction(new MagicDealDamageAction(damage));
-                if (damage.getDealtAmount() > 0 && target.isCreature()) {
-                    final MagicPermanent creature = (MagicPermanent)target;
-                    game.doAction(new MagicAddTurnTriggerAction(creature,MagicWhenSelfLeavesPlayTrigger.IfDieExileInstead));
+                dealtDamage = damage.getDealtAmount();
+                game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersLibrary));
+            });
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                if (dealtDamage > 0) {
+                    game.doAction(new MagicAddTurnTriggerAction(
+                        creature,
+                        MagicWhenSelfLeavesPlayTrigger.IfDieExileInstead
+                    ));
                 }
             });
-            game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(),MagicLocationType.OwnersLibrary));
         }
     }
 ]
