46bb697ac4ec1791d82a8f4e1fd602c11888d7c4
melvin <melvinzhang@gmail.com>
2014-04-30 12:06:52 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Gobhobbler_Rats.groovy
+++ release/Magarena/scripts/Gobhobbler_Rats.groovy
@@ -9,32 +9,5 @@
                 pt.add(1,0);
             }
         }
-    },
-    new MagicPermanentActivation(
-        [
-            MagicCondition.HELLBENT,
-        ],
-        new MagicActivationHints(MagicTiming.Pump),
-        "Regen"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{B}"),
-                new MagicRegenerationConditionsEvent(source,this)
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Regenerate SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicRegenerateAction(event.getPermanent()));
-        }
     }
 ]
