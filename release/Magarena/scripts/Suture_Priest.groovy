7e5400b75f6359fae5343ca910359edadc1404c7
melvin <melvinzhang@gmail.com>
2014-05-01 21:42:49 +0800

    simplified groovy code

--- release/Magarena/scripts/Suture_Priest.groovy
+++ release/Magarena/scripts/Suture_Priest.groovy
@@ -1,51 +1,29 @@
-def MAY_GAIN_ONE_LIFE = new MagicEventAction() {
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        if (event.isYes()) {
-            game.doAction(new MagicChangeLifeAction(event.getPlayer(), 1));
-        }
-    }
-};
-
-def MAY_LOSE_ONE_LIFE = new MagicEventAction() {
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        if (event.isYes()) {
-            game.doAction(new MagicChangeLifeAction(event.getPlayer().getOpponent(), -1));
-        }
-    }
-};
-
 [
     new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public boolean accept(final MagicPermanent permanent, final MagicPermanent other) {
-            return other != permanent && other.isCreature();
+            return other != permanent && other.isCreature() && permanent.isEnemy(other);
         }
 
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent other) {
-            return permanent.isFriend(other) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.GAIN_LIFE,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    MAY_GAIN_ONE_LIFE,
-                    "PN may\$ gain 1 life."
-                ):
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    MAY_LOSE_ONE_LIFE,
-                    "PN may\$ have " + other.getController() + " lose 1 life."
-                );
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
+                    1,
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
+                other.getController(),
+                this,
+                "PN may\$ have RN lose 1 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer().getOpponent(), -1));
+            }
         }
     }
 ]
