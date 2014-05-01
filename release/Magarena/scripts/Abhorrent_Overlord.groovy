a9cf9d5e482a883ead1f8648e78916103055ccdf
melvin <melvinzhang@gmail.com>
2014-05-01 21:54:45 +0800

    replace requires_groovy_code with ability property

--- release/Magarena/scripts/Abhorrent_Overlord.groovy
+++ release/Magarena/scripts/Abhorrent_Overlord.groovy
@@ -1,23 +1,4 @@
 [
-    new MagicAtUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.isController(upkeepPlayer) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "PN sacrifices a creature."
-                ):
-                MagicEvent.NONE;
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent permanent=event.getPermanent();
-            final MagicPlayer player=event.getPlayer();
-            game.addEvent(new MagicSacrificePermanentEvent(permanent,player,MagicTargetChoice.SACRIFICE_CREATURE));
-        }
-    },
     new MagicWhenComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
