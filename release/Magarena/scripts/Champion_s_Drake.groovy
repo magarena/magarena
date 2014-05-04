674d6f2dffd4657ea1274f020d6f8ab22e385d6c
melvin <melvinzhang@gmail.com>
2014-05-04 08:41:01 +0800

    simplify groovy code by defining filter, normalize card script

--- release/Magarena/scripts/Champion_s_Drake.groovy
+++ release/Magarena/scripts/Champion_s_Drake.groovy
@@ -1,18 +1,19 @@
+def CREATURE_AT_LEAST_3_LEVEL_COUNTERS = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.isCreature() && 
+               target.getCounters(MagicCounterType.Level) >= 3;
+    }
+};
+
 [
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final MagicGame game = source.getGame();
-        	final int boost = 0;
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                    permanent.getController(),
-                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
-            for (final MagicPermanent target : targets) {
-                if (target.getCounters(MagicCounterType.Level) >= 3) {
-                    boost = 3;
-                }
-            }
-            pt.add(boost,boost);
+            pt.add(3,3);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getController().controlsPermanent(CREATURE_AT_LEAST_3_LEVEL_COUNTERS);
         }
     }
 ]
