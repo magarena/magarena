0690f65a7cf90c4cae8970dbcea9bf680ef7a898
melvin <melvinzhang@gmail.com>
2014-05-04 16:29:49 +0800

    replace duplicate groovy code with reference in requires_groovy_code

--- release/Magarena/scripts/Blood_Seeker.groovy
+++ release/Magarena/scripts/Blood_Seeker.groovy
@@ -1,21 +1,22 @@
 [
     new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (otherPermanent.isCreature() &&
-                    otherPermanent.isEnemy(permanent)) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    otherPermanent.getController(),
-                    this,
-                    "PN may\$ have RN lose 1 life."
-                ):
-                MagicEvent.NONE;
+        public boolean accept(final MagicPermanent permanent, final MagicPermanent other) {
+            return other.isCreature() && other.isEnemy(permanent);
+        }
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent other) {
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.OPPONENT_LOSE_LIFE,
+                    1,
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
+                other.getController(),
+                this,
+                "PN may\$ have RN lose 1 life."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
