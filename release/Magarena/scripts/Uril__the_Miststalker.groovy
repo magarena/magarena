8d6b70b3eed7199f033ebed6b369cb75e99d76b5
hong yie <unknown>
2014-05-05 00:55:31 +0000

    Add script for Uril, the Miststalker

---
+++ release/Magarena/scripts/Uril__the_Miststalker.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final MagicPowerToughness pt) {
+            if (permanent.isEnchanted()) {
+                pt.add(
+                    permanent.getAuraPermanents().size()*2,
+                    permanent.getAuraPermanents().size()*2
+                );
+            }
+        }
+    }
+]
