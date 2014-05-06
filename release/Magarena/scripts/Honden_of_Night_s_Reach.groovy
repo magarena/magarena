e67edb6e8c3988bb00228aa30c7540e0eb2321ef
melvin <melvinzhang@gmail.com>
2014-05-06 11:01:12 +0800

    replace filter with getNrOfPermanents

--- release/Magarena/scripts/Honden_of_Night_s_Reach.groovy
+++ release/Magarena/scripts/Honden_of_Night_s_Reach.groovy
@@ -1,27 +1,21 @@
-def SHRINES_YOU_CONTROL = new MagicPermanentFilterImpl() {
-        public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
-            return target.isController(player) &&
-                   target.hasSubType(MagicSubType.Shrine);
-        }
-    };
-
 [
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return (permanent.getController() == upkeepPlayer) ? new MagicEvent(
-                permanent,
-                MagicTargetChoice.TARGET_OPPONENT,
-                this,
-                "Target opponent\$ discards a card for each Shrine PN controls."
-            ):
-            MagicEvent.NONE
+            return permanent.isController(upkeepPlayer) ? 
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.TARGET_OPPONENT,
+                    this,
+                    "Target opponent\$ discards a card for each Shrine PN controls."
+                ):
+                MagicEvent.NONE
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int size = (game.filterPermanents(event.getPlayer(),SHRINES_YOU_CONTROL).size());
             event.processTarget(game, {
                 final MagicPlayer player ->
+                final int size =  event.getPlayer().getNrOfPermanents(MagicSubType.Shrine);
                 game.addEvent(MagicDiscardEvent.Random(event.getSource(), player, size));
             });
         }
