7b2773149c10b2a24a25decc0f6c451e4359baeb
melvin <melvinzhang@gmail.com>
2014-05-06 11:08:55 +0800

    add override annotation, replace getController with PN

--- release/Magarena/scripts/Royal_Decree.groovy
+++ release/Magarena/scripts/Royal_Decree.groovy
@@ -1,5 +1,6 @@
 [
     new MagicWhenBecomesTappedTrigger() {
+        @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
             return (tapped.hasSubType(MagicSubType.Swamp) ||
                     tapped.hasSubType(MagicSubType.Mountain) ||
@@ -9,10 +10,11 @@
                     permanent,
                     tapped.getController(),
                     this,
-                    "SN deals 1 damage to "+tapped.getController().getName()+"."
+                    "SN deals 1 damage to PN."
                 ) :
                 MagicEvent.NONE;
         }
+        @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),1);
             game.doAction(new MagicDealDamageAction(damage));
