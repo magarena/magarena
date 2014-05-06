4dbb50dfa2240289676a6f48e4e761061e24d502
melvin <melvinzhang@gmail.com>
2014-05-06 12:56:50 +0800

    add @Override annotation

--- release/Magarena/scripts/Saproling_Symbiosis.groovy
+++ release/Magarena/scripts/Saproling_Symbiosis.groovy
@@ -21,6 +21,7 @@
         new MagicActivationHints(MagicTiming.Token,true),
         "Instant"
     ) {
+        @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
             return [
                 new MagicPayManaCostEvent(source, "{5}{G}")
