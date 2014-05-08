a93d27798081fbcdec866fbc396a2ddc68664b8e
ShawnieBoy <unknown>
2014-05-08 13:37:11 +0100

    New card scripts

---
+++ release/Magarena/scripts/Dreamcatcher.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice("Sacrifice Dreamcatcher?"),
+                this,
+                "PN may\$ sacrifice SN. If PN does, PN draws a card."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    }
+]
---
+++ release/Magarena/scripts/Harbor_Guardian.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenSelfAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent creature) {
+            return new MagicEvent(
+                permanent,
+                game.getDefendingPlayer(),
+                new MagicSimpleMayChoice(
+                    MagicSimpleMayChoice.DRAW_CARDS,
+                    1,
+                    MagicSimpleMayChoice.DEFAULT_YES
+                ),
+                this,
+                "PN may\$ draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    }
+]
