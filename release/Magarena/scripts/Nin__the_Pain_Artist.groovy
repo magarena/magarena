8aab47fb33c2f4f147e85dad4d83c404b392b1b4
melvin <melvinzhang@gmail.com>
2014-05-11 09:10:41 +0800

    normalize scripts

--- release/Magarena/scripts/Nin__the_Pain_Artist.groovy
+++ release/Magarena/scripts/Nin__the_Pain_Artist.groovy
@@ -1,39 +1,40 @@
 [
-	new MagicPermanentActivation(
-		new MagicActivationHints(MagicTiming.Removal),
-		"Damage"
-	) {
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
 
-		@Override
-		public Iterable getCostEvent(final MagicPermanent source) {
-			return [
-			new MagicPayManaCostEvent(source,"{X}{U}{R}"),
-			new MagicTapEvent(source)
-		 ];
-		}
+        @Override
+        public Iterable getCostEvent(final MagicPermanent source) {
+            return [
+            new MagicPayManaCostEvent(source,"{X}{U}{R}"),
+            new MagicTapEvent(source)
+         ];
+        }
 
-		@Override
-		public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-		 final int amount=payedCost.getX();
-			return new MagicEvent(
-				source,
-				MagicTargetChoice.NEG_TARGET_CREATURE,
-				new MagicDamageTargetPicker(amount),
-				amount,
-				this,
-				"SN deals "+amount+" damage to target creature. That creature's controller draws "+amount+" cards."
-			);
-		}
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE,
+                new MagicDamageTargetPicker(amount),
+                amount,
+                this,
+                "SN deals RN damage to target creature\$. That creature's controller draws RN cards."
+            );
+        }
 
-		@Override
-		public void executeEvent(final MagicGame game, final MagicEvent event) {
-			event.processTargetPermanent(game, {
-				final MagicPermanent creature ->
-				final int amount = event.getRefInt();
-				final MagicDamage damage=new MagicDamage(event.getSource(),creature,amount);
-				game.doAction(new MagicDealDamageAction(damage));
-				game.doAction(new MagicDrawAction(creature.getController(),amount));
-			});
-		}
-	}
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                final int amount = event.getRefInt();
+                game.doAction(new MagicDealDamageAction(
+                    new MagicDamage(event.getSource(),creature,amount)
+                ));
+                game.doAction(new MagicDrawAction(creature.getController(),amount));
+            });
+        }
+    }
 ]
