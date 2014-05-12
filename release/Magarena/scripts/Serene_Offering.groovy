a08772597559a519201de95cd8d2dbd64cd33f6c
ShawnieBoy <unknown>
2014-05-12 20:43:11 +0100

    New card scripts

---
+++ release/Magarena/scripts/Serene_Offering.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ENCHANTMENT,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target enchantment\$. PN gains life equal to its converted mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent permanent ->
+                game.doAction(new MagicDestroyAction(permanent));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),permanent.getConvertedCost()));
+            });
+        }
+    }
+]
