471e8e9aefefa172c8f61976874b98ed56d61adf
ShawnieBoyUK@gmail.com <unknown>
2014-05-15 17:00:05 +0100

    New card scripts

---
+++ release/Magarena/scripts/Acidic_Soil.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "SN deals damage to each player equal to the number of lands he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicSource source=event.getSource();
+            for (final MagicPlayer player : game.getPlayers()) {
+                final int amount = player.getNrOfPermanents(MagicType.Land);
+                final MagicDamage damage=new MagicDamage(source,player,amount);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+        }
+    }
+]
