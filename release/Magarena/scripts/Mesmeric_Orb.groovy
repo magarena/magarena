9d3ba4a28ff1096e04d5665ce786ced23109b55b
melvin <melvinzhang@gmail.com>
2014-05-16 09:50:06 +0800

    replace player.getName() with PN

--- release/Magarena/scripts/Mesmeric_Orb.groovy
+++ release/Magarena/scripts/Mesmeric_Orb.groovy
@@ -2,19 +2,17 @@
     new MagicWhenBecomesUntappedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            final MagicPlayer player = otherPermanent.getController();
             return new MagicEvent(
                 permanent,
-                otherPermanent,
+                otherPermanent.getController(),
                 this,
-                player.getName()+" puts the top card of his or her library into his or her graveyard."
+                "PN puts the top card of his or her library into his or her graveyard."
             )
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer player = event.getRefPermanent().getController();
-            game.doAction(new MagicMillLibraryAction(player,1));
+            game.doAction(new MagicMillLibraryAction(event.getPlayer(),1));
         }
     }
 ]
