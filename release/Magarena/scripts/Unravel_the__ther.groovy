8428b7e79c33037913e57e3f33ee7d334dd4198a
ShawnieBoyUK@gmail.com <unknown>
2014-05-17 18:49:50 +0100

    Edited Filenames for non-ascii characters

---
+++ release/Magarena/scripts/Unravel_the__ther.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
+                MagicBounceTargetPicker.create(),
+                this,
+                "Choose target artifact or enchantment\$. It's owner shuffles it into his or her library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent permanent ->
+                game.doAction(new MagicMoveCardAction(permanent,MagicLocationType.OwnersLibrary));
+            });
+        }
+    }
+]
