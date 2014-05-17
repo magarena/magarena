33d019e47e7039180cf968f425bfcf1e7952d0cb
a. benedict balbuena (chuckie) <unknown>
2014-05-17 05:55:13 +0000

    Add script for Engulfing Flames

---
+++ release/Magarena/scripts/Engulfing_Flames.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(1,true),
+                this,
+                "SN deals 1 damage to target creature\$. " +
+                "It can't be regenerated this turn. "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                final MagicTarget target ->
+                final MagicDamage damage=new MagicDamage(event.getSource(),target,1);
+                damage.setNoRegeneration();
+                game.doAction(new MagicDealDamageAction(damage));
+            });
+        }
+    }
+]
