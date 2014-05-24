84d366094bc25df164df44f9a10ebb1a7561151b
ShawnieBoyUK@gmail.com <unknown>
2014-05-24 01:42:07 +0100

    Minor edits for layouts/extra spaces in scripts

--- release/Magarena/scripts/Acorn_Catapult.groovy
+++ release/Magarena/scripts/Acorn_Catapult.groovy
@@ -7,7 +7,7 @@
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
             return [
-                new MagicTapEvent(source), new MagicPayManaCostEvent(source,  "{1}")
+                new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{1}")
             ];
         }
 
