2a0a8131af32107b711492c78dcc17046915772c
ShawnieBoyUK@gmail.com <unknown>
2014-05-27 13:04:15 +0100

    Give feedback to log when Worship triggers.

--- release/Magarena/scripts/Worship.groovy
+++ release/Magarena/scripts/Worship.groovy
@@ -6,6 +6,7 @@
             final MagicTarget target = damage.getTarget();
             if (player == target && player.controlsPermanent(MagicType.Creature) && player.getLife() < 1) {
                 player.setLife(1);
+                game.logAppendMessage(player,""+permanent.getName()+" prevented any further damage.");
             }
             return MagicEvent.NONE;
         }
