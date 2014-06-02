99b92a4effed658a4c7307081027ce8549b9d503
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-06-02 19:12:52 +0100

    Add missing 'can't be regenerated' to game log for Agonizing Demise

--- release/Magarena/scripts/Agonizing_Demise.groovy
+++ release/Magarena/scripts/Agonizing_Demise.groovy
@@ -7,7 +7,7 @@
                 MagicTargetChoice.NEG_TARGET_NONBLACK_CREATURE,
                 MagicDestroyTargetPicker.DestroyNoRegen,
                 this,
-                "Destroy target nonblack creature\$. "+
+                "Destroy target nonblack creature\$. It can't be regenerated."+
                 "If SN was kicked, it deals damage equal to that creature's power to the creature's controller."
             );
         }
