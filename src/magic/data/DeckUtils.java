a10f3e498813d79a3e66cb732e0243bf4ced2b60
Lodici <Lodici>
2014-06-08 09:13:13 +0100

    wrap Scanner in try-with-resources to ensure it is closed.

--- src/magic/data/DeckUtils.java
+++ src/magic/data/DeckUtils.java
@@ -127,39 +127,39 @@ public class DeckUtils {
 
     private static MagicDeck parseDeckFileContent(final String content) {
 
-        final Scanner sc = new Scanner(content);
-        final int[] colorCount = new int[MagicColor.NR_COLORS];
-        final MagicDeck deck = new MagicDeck();
         final MagicDeck unsupported = new MagicDeck();
+        final MagicDeck deck = new MagicDeck();
 
-        while (sc.hasNextLine()) {
-            final String line = sc.nextLine().trim();
-            if (!line.isEmpty()&&!line.startsWith("#")) {
-                if (line.startsWith(">")) {
-                    deck.setDescription(line.substring(1));
-                } else {
-                    final int index = line.indexOf(' ');
-                    final int amount = Integer.parseInt(line.substring(0,index));
-                    final String name=line.substring(index+1).trim();
-                    final MagicCardDefinition cardDefinition = CardDefinitions.getCard(name);
-                    for (int count=amount;count>0;count--) {
-                        final int colorFlags=cardDefinition.getColorFlags();
-                        for (final MagicColor color : MagicColor.values()) {
-                            if (color.hasColor(colorFlags)) {
-                                colorCount[color.ordinal()]++;
+        try (final Scanner sc = new Scanner(content)) {
+            final int[] colorCount = new int[MagicColor.NR_COLORS];
+            while (sc.hasNextLine()) {
+                final String line = sc.nextLine().trim();
+                if (!line.isEmpty()&&!line.startsWith("#")) {
+                    if (line.startsWith(">")) {
+                        deck.setDescription(line.substring(1));
+                    } else {
+                        final int index = line.indexOf(' ');
+                        final int amount = Integer.parseInt(line.substring(0,index));
+                        final String name=line.substring(index+1).trim();
+                        final MagicCardDefinition cardDefinition = CardDefinitions.getCard(name);
+                        for (int count=amount;count>0;count--) {
+                            final int colorFlags=cardDefinition.getColorFlags();
+                            for (final MagicColor color : MagicColor.values()) {
+                                if (color.hasColor(colorFlags)) {
+                                    colorCount[color.ordinal()]++;
+                                }
                             }
-                        }
-                        if (cardDefinition.isValid()) {
-                            deck.add(cardDefinition);
-                        } else {
-                            unsupported.add(cardDefinition);
-                            break; // multiple copies of unsupported card -> ignore other copies
+                            if (cardDefinition.isValid()) {
+                                deck.add(cardDefinition);
+                            } else {
+                                unsupported.add(cardDefinition);
+                                break; // multiple copies of unsupported card -> ignore other copies
+                            }
                         }
                     }
                 }
             }
-        }
-        sc.close();
+        };
 
         showUnsupportedCards(unsupported);
 
