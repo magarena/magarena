d9d906231fc8734946415f75d83ac8824dc787ab
ShawnieBoy <unknown>
2014-06-09 22:37:19 +0100

    Remove log comment when damage is prevented for Worship and Ali from Cairo to prevent spam

--- release/Magarena/scripts/Worship.groovy
+++ release/Magarena/scripts/Worship.groovy
@@ -6,7 +6,6 @@
             final MagicTarget target = damage.getTarget();
             if (player == target && player.controlsPermanent(MagicType.Creature) && player.getLife() < 1) {
                 player.setLife(1);
-                game.logAppendMessage(player,""+permanent.getName()+" prevented any further damage.");
             }
             return MagicEvent.NONE;
         }
