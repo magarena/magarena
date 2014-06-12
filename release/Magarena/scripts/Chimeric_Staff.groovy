4efb01e84e06f96a5edd43aba1f18ed26482f1ab
ShawnieBoy <unknown>
2014-06-12 14:51:28 +0100

    New card scripts - That's 50% folks!

---
+++ release/Magarena/scripts/Chimeric_Staff.groovy
@@ -0,0 +1,47 @@
+def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent,final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Construct);
+    }
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags|MagicType.Creature.getMask()|MagicType.Artifact.getMask();
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Animate),
+        "Animate"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{X}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getX(),
+                this,
+                "SN becomes an X/X Construct artifact creature until end of turn. (X="+payedCost.getX()+")"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getRefInt();
+            final MagicStatic PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+                @Override
+                public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+                    pt.set(amount,amount);
+                }
+            };
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,ST));
+        }
+    }
+]
