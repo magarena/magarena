0344f794e527b561fdfab4f3a587022b73e56f26
ShawnieBoy <unknown>
2014-06-12 23:55:03 +0100

    New card scripts

---
+++ release/Magarena/scripts/Marrow_Gnawer.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificePermanentEvent(source, new MagicTargetChoice("a Rat to sacrifice"))
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts X 1/1 black Rat creature tokens onto the battlefield, where X is the number of Rats PN controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getNrOfPermanents(MagicSubType.Rat);
+            game.doAction(new MagicPlayTokensAction(event.getPlayer(),TokenCardDefinitions.get("1/1 black Rat creature token"),amount));
+        }
+    }
+]
