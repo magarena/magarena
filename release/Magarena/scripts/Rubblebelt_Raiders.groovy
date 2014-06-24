8ab4a576dd78d9a4bff90d5e04c7d807eb625860
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-06-24 18:34:15 +0100

    New card scripts

---
+++ release/Magarena/scripts/Rubblebelt_Raiders.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenSelfAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent creature) {
+            final int amount = permanent.getCounters(MagicCounterType.PlusOne)
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a +1/+1 counter on SN for each attacking creature PN controls."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer()
+            final int attackers = player.getNrOfPermanents(
+                MagicTargetFilterFactory.ATTACKING_CREATURE_YOU_CONTROL
+            );
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,attackers,true));
+            game.logAppendMessage(player," ("+attackers+")");
+        }
+    }
+]
