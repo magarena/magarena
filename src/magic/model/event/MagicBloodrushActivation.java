b89907914845f19b5a52070c0e32d6e5dde23fb7
melvin <melvinzhang@gmail.com>
2014-07-03 15:38:50 +0800

    simplify MagicBloodrushActivation by extending MagicCardAbilityActivation

--- src/magic/model/event/MagicBloodrushActivation.java
+++ src/magic/model/event/MagicBloodrushActivation.java
@@ -4,16 +4,12 @@ import magic.model.MagicCard;
 import magic.model.MagicGame;
 import magic.model.MagicManaCost;
 import magic.model.MagicPayedCost;
-import magic.model.MagicSource;
-import magic.model.action.MagicPutItemOnStackAction;
-import magic.model.choice.MagicChoice;
 import magic.model.choice.MagicTargetChoice;
-import magic.model.stack.MagicAbilityOnStack;
 import magic.model.target.MagicPumpTargetPicker;
 
 import java.util.Arrays;
 
-public abstract class MagicBloodrushActivation extends MagicCardActivation {
+public abstract class MagicBloodrushActivation extends MagicCardAbilityActivation {
 
     final MagicManaCost cost;
     final String desc;
@@ -27,34 +23,15 @@ public abstract class MagicBloodrushActivation extends MagicCardActivation {
         desc = aDesc;
     }
 
+    @Override
     public Iterable<? extends MagicEvent> getCostEvent(final MagicCard source) {
         return Arrays.asList(
-            new MagicPayManaCostEvent(
-                source,
-                cost
-            ),
+            new MagicPayManaCostEvent(source, cost),
             new MagicDiscardSelfEvent(source)
         );
     }
 
     @Override
-    public MagicEvent getEvent(final MagicSource source) {
-        return new MagicEvent(
-            source,
-            new MagicEventAction() {
-                @Override
-                public void executeEvent(final MagicGame game, final MagicEvent event) {
-                    final MagicAbilityOnStack abilityOnStack = new MagicAbilityOnStack(
-                        MagicBloodrushActivation.this,
-                        getCardEvent(event.getCard(), game.getPayedCost())
-                    );
-                    game.doAction(new MagicPutItemOnStackAction(abilityOnStack));
-                }
-            },
-            "Bloodrush."
-        );
-    }
-
     public MagicEvent getCardEvent(final MagicCard source,final MagicPayedCost payedCost) {
         return new MagicEvent(
             source,
@@ -63,10 +40,5 @@ public abstract class MagicBloodrushActivation extends MagicCardActivation {
             this,
             desc
         );
-    }
-
-    @Override
-    final MagicChoice getChoice(final MagicCard source) {
-        return getCardEvent(source,MagicPayedCost.NO_COST).getChoice();
     }
 }
