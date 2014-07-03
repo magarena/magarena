703cf6d224dbc33e83e29cb58a76c2f0f70641c5
melvin <melvinzhang@gmail.com>
2014-07-03 09:39:22 +0800

    fix groovy: should be getPlayer() not getPlayer

--- release/Magarena/scripts/Cold_Snap.groovy
+++ release/Magarena/scripts/Cold_Snap.groovy
@@ -3,11 +3,11 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             return new MagicEvent(
-                    permanent,
-                    upkeepPlayer,
-                    this,
-                    "SN deals damage to PN equal to the number of snow lands he or she controls."
-                );
+                permanent,
+                upkeepPlayer,
+                this,
+                "SN deals damage to PN equal to the number of snow lands he or she controls."
+            );
         }
         
         @Override
@@ -15,7 +15,7 @@
             final MagicPlayer player = event.getPlayer();
             final int amount = player.getNrOfPermanents(MagicTargetFilterFactory.SNOW_LAND_YOU_CONTROL);
             final MagicDamage damage = new MagicDamage(event.getSource(),player,amount);
-            game.logAppendMessage(event.getPlayer,"("+amount+")");
+            game.logAppendMessage(event.getPlayer(),"("+amount+")");
             game.doAction(new MagicDealDamageAction(damage));
         }
     }
