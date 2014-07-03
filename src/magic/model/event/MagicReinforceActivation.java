a758e819f3c0ebf48c006065e5599830409870f2
melvin <melvinzhang@gmail.com>
2014-07-03 15:27:01 +0800

    simplify MagicReinforceActivation by extending MagicCardAbilityActivation

--- src/magic/model/event/MagicReinforceActivation.java
+++ src/magic/model/event/MagicReinforceActivation.java
@@ -4,14 +4,11 @@ import magic.model.MagicCard;
 import magic.model.MagicGame;
 import magic.model.MagicManaCost;
 import magic.model.MagicSource;
-import magic.model.action.MagicPutItemOnStackAction;
-import magic.model.choice.MagicChoice;
-import magic.model.choice.MagicTargetChoice;
-import magic.model.stack.MagicAbilityOnStack;
+import magic.model.MagicPayedCost;
 
 import java.util.Arrays;
 
-public class MagicReinforceActivation extends MagicCardActivation {
+public class MagicReinforceActivation extends MagicCardAbilityActivation {
 
     final MagicManaCost cost;
     final int amount;
@@ -25,36 +22,16 @@ public class MagicReinforceActivation extends MagicCardActivation {
         amount = n;
     }
 
+    @Override
     public Iterable<? extends MagicEvent> getCostEvent(final MagicCard source) {
         return Arrays.asList(
-            new MagicPayManaCostEvent(
-                source,
-                cost
-            ),
+            new MagicPayManaCostEvent(source, cost),
             new MagicDiscardSelfEvent(source)
         );
     }
-
+    
     @Override
-    public MagicEvent getEvent(final MagicSource source) {
-        return new MagicEvent(
-            source,
-            this,
-            "Reinforce."
-        );
-    }
-
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        final MagicAbilityOnStack abilityOnStack = new MagicAbilityOnStack(
-            this,
-            new MagicPutCounterEvent(event.getSource(), amount)
-        );
-        game.doAction(new MagicPutItemOnStackAction(abilityOnStack));
-    }
-
-    @Override
-    final MagicChoice getChoice(final MagicCard source) {
-        return MagicTargetChoice.POS_TARGET_CREATURE;
+    public MagicEvent getCardEvent(final MagicCard card, final MagicPayedCost payedCost) {
+        return new MagicPutCounterEvent(card, amount);
     }
 }
