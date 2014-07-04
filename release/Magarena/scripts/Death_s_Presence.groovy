deee707bbfb53366567405c35b683bc013acc0de
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-07-04 11:44:15 +0100

    New card scripts

---
+++ release/Magarena/scripts/Death_s_Presence.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenOtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
+            return died.hasType(MagicType.Creature) && died.isFriend(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    MagicTargetChoice.Positive("target creature you control"),
+                    died.getPower(),
+                    this,
+                    "PN puts X +1/+1 counters on target creature he or she controls\$, where X is the power of "+died.getName()+". (X="+died.getPower()+")"
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent creature ->
+                game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.PlusOne,event.getRefInt(),true));
+            });
+        }
+    }
+]
