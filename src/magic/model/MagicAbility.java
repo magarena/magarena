99058ee34d705194f82a96b43efbb61bd2da4ac8
melvin <melvinzhang@gmail.com>
2014-07-04 11:53:26 +0800

    move activated ability back to original position

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -849,6 +849,16 @@ public enum MagicAbility {
             card.add(new MagicBestowActivation(manaCost));
         }
     },
+    Channel("Channel — " + ARG.ANY, 10) {
+        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
+            card.add(MagicCardAbilityActivation.create(ARG.any(arg), "Channel"));
+        }
+    },
+    ActivatedAbility("[^\"]+:(?! Add)" + ARG.ANY, 10) {
+        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
+            card.add(MagicPermanentActivation.create(arg.group()));
+        }
+    },
     AlternateCost("alt cost " + ARG.ANY, 10) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher matcher) {
             final String arg = matcher.group("any");
@@ -1059,16 +1069,6 @@ public enum MagicAbility {
     Dethrone("dethrone",10) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
             card.add(MagicDethroneTrigger.create());
-        }
-    },
-    Channel("Channel — " + ARG.ANY, 10) {
-        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            card.add(MagicCardAbilityActivation.create(ARG.any(arg), "Channel"));
-        }
-    },
-    ActivatedAbility("[^\"]+:(?! Add)" + ARG.ANY, 10) {
-        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            card.add(MagicPermanentActivation.create(arg.group()));
         }
     },
     ;
