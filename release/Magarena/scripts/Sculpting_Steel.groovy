b5ba5483ceb1455a34f014ded53f208afe0b4b1e
Jericho Pumpkin <unknown>
2014-07-05 15:53:41 +0000

    Add script for Sculpting Steel

---
+++ release/Magarena/scripts/Sculpting_Steel.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(new MagicTargetChoice("an artifact")),
+                MagicCopyPermanentPicker.create(),
+                this,
+                "You may\$ have SN enter the battlefield as a copy of any creature\$ on the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetPermanent(game, {
+                    final MagicPermanent chosen ->
+                    game.doAction(new MagicEnterAsCopyAction(event.getCardOnStack(), chosen))
+                });
+            } else {
+                game.logAppendMessage(event.getPlayer(), "Put ${event.getCardOnStack()} onto the battlefield.");
+                game.doAction(new MagicPlayCardFromStackAction(
+                    event.getCardOnStack()
+                ));
+            }
+        }
+    }
+]
