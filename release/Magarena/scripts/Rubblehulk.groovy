60fdc2d62549f3bcd512e52f47254686a56737e5
melvin <melvinzhang@gmail.com>
2014-07-06 08:54:57 +0800

    remove MagicBloodrushActivation

--- release/Magarena/scripts/Rubblehulk.groovy
+++ release/Magarena/scripts/Rubblehulk.groovy
@@ -6,10 +6,29 @@
             pt.set(size, size);
         }
     },
-    new MagicBloodrushActivation(
-        MagicManaCost.create("{1}{R}{G}"),
-        "Target attacking creature\$ gets +X/+X until end of turn, where X is the number of lands you control."
+    new MagicCardAbilityActivation(
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Bloodrush"
     ) {
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, "{1}{R}{G}"),
+                new MagicDiscardSelfEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getCardEvent(final MagicCard source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_ATTACKING_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target attacking creature\$ gets +X/+X until end of turn, where X is the number of lands you control."
+            );
+        }
+
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event) {
             final int size = game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.LAND_YOU_CONTROL).size();
