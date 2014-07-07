2625d37c93e5dbf81973af6ca6fbf73e7ec82d53
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-07-07 16:33:09 +0100

    New card scripts

---
+++ release/Magarena/scripts/Drowned_Rusalka.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Draw),
+        "Draw"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{U}"),
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN discards a card, then draws a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicDiscardEvent(event.getSource(),event.getPlayer()));
+            game.addEvent(new MagicDrawEvent(event.getSource(),event.getPlayer(),1));
+        }
+    }
+]
---
+++ release/Magarena/scripts/Manaplasm.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            final int amount = spell.getConvertedCost();
+            return spell.isFriend(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    amount,
+                    this,
+                    "SN gets +X/+X until end of turn, where X is ("+spell.getName()+")'s converted mana cost. (X="+amount+")"
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getRefInt();
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),amount,amount));
+        }
+    }
+]
