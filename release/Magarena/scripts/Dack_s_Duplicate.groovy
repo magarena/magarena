a3680bdea55d18c9aeddf3e8ae5d7a4923c6afea
Jericho Pumpkin <unknown>
2014-07-08 02:55:12 +0100

    Add Dack's Duplicate

---
+++ release/Magarena/scripts/Dack_s_Duplicate.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(MagicTargetChoice.CREATURE),
+                MagicCopyPermanentPicker.create(),
+                this,
+                "Put SN onto the battlefield. You may\$ have SN enter the battlefield as a copy of any creature\$ on the battlefield, except it gains haste and dethrone."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetPermanent(game, {
+                    final MagicPermanent chosen ->
+                    final MagicPlayCardFromStackAction action = MagicPlayCardFromStackAction.EnterAsCopy(
+                        event.getCardOnStack(),
+                        chosen
+                    );
+                    game.doAction(action);
+                    final MagicPermanent permanent = action.getPermanent();
+                    game.doAction(new MagicGainAbilityAction(permanent,MagicAbility.Haste, MagicStatic.Forever));
+               game.doAction(new MagicGainAbilityAction(permanent,MagicAbility.Dethrone, MagicStatic.Forever));
+
+                });
+            } else {
+                game.doAction(new MagicPlayCardFromStackAction(
+                    event.getCardOnStack()
+                ));
+            }
+        }
+    }
+]
