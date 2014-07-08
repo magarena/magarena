fe0381d534cbe744677855dee82f773596ad96b6
melvin <melvinzhang@gmail.com>
2014-07-08 13:08:16 +0800

    make MagicDate immutable

--- src/magic/data/MagicDate.java
+++ src/magic/data/MagicDate.java
@@ -2,20 +2,26 @@ package magic.data;
 
 public class MagicDate {
     
-    private int year;
-    private int month;
-    private int day;
+    private final int year;
+    private final int month;
+    private final int day;
     
-    public MagicDate(final int year, final int month, final int day) {
-        this.setYear(year);
-        this.setMonth(month);
-        this.setDay(day);
+    public MagicDate(final int aYear, final int aMonth, final int aDay) {
+        year = aYear;
+        if (aMonth < 1 || aMonth > 12){
+            throw new IllegalArgumentException("Month must be between 1 and 12, inclusive");
+        } else {
+            month = aMonth;
+        }
+        if (aDay < 1 || aDay > 31){
+            throw new IllegalArgumentException("Day must be between 1 and 31, inclusive");
+        }else{
+            day = aDay;
+        }
     }
 
     public MagicDate(final int year, int month) {
-        this.setYear(year);
-        this.setMonth(month);
-        this.setDay(1);
+        this(year, month, 1);
     }
 
     /**
@@ -26,13 +32,6 @@ public class MagicDate {
     }
 
     /**
-     * @param year the year to set
-     */
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    /**
      * @return the month
      */
     public int getMonth() {
@@ -40,31 +39,9 @@ public class MagicDate {
     }
 
     /**
-     * @param month the month to set
-     */
-    public void setMonth(int month) {
-        if(month < 1 || month > 12){
-            throw new IllegalArgumentException("Month must be between 1 and 12, inclusive");
-        }else{
-            this.month = month;
-        }
-    }
-
-    /**
      * @return the day
      */
     public int getDay() {
         return day;
-    }
-
-    /**
-     * @param day the day to set
-     */
-    public void setDay(int day) {
-        if(day < 1 || day > 31){
-            throw new IllegalArgumentException("Day must be between 1 and 31, inclusive");
-        }else{
-            this.day = day;
-        }
     }
 }
