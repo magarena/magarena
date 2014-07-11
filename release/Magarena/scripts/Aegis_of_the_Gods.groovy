09f269707b37cd36d5b4b2fc0329822d0e19113b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-07-11 18:50:57 +0100

    Remove duplicate groovy code

--- release/Magarena/scripts/Aegis_of_the_Gods.groovy
+++
@@ -1,8 +0,0 @@
-[
-    new MagicStatic(MagicLayer.Player) {
-        @Override
-        public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            source.getController().addAbility(MagicAbility.Hexproof);
-        }
-    }
-]
