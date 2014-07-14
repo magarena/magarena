2188bfe40bae6d90eb6869be139abcb995930132
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-07-14 21:27:12 +0100

    Fix: Field of Souls trigger isn't optional

--- release/Magarena/scripts/Field_of_Souls.groovy
+++ release/Magarena/scripts/Field_of_Souls.groovy
@@ -7,20 +7,17 @@
                     otherPermanent.getCard().isFriend(permanent)) ?
                 new MagicEvent(
                     permanent,
-                    new MagicMayChoice(),
                     this,
-                    "PN may\$ put a 1/1 white Spirit creature token with flying onto the battlefield."
+                    "PN puts a 1/1 white Spirit creature token with flying onto the battlefield."
                 ) :
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                game.doAction(new MagicPlayTokenAction(
-                    event.getPlayer(), 
-                    TokenCardDefinitions.get("1/1 white Spirit creature token with flying")
-                ));
-            }
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(), 
+                TokenCardDefinitions.get("1/1 white Spirit creature token with flying")
+            ));
         }
     }
 ]
