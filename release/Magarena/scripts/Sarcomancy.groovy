c4ad28a500ce2f9d8bfc0ca64fd03d55297dcce3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-07-17 15:28:25 +0100

    Add Sarcomancy

---
+++ release/Magarena/scripts/Sarcomancy.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) && game.getNrOfPermanents(MagicSubType.Zombie) == 0 ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN deals 1 damage to PN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicDamage damage = new MagicDamage(event.getSource(),event.getPlayer(),1);
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
