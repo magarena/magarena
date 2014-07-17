c81ed8cc1602da2b48eccc5a387533d75a155a0f
melvin <melvinzhang@gmail.com>
2014-07-17 13:41:47 +0800

    ability not supported as card script, add groovy script

---
+++ release/Magarena/scripts/Carnophage.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice("Pay 1 life?"),
+                    this,
+                    "PN may\$ pay 1 life. If PN doesn't, tap SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.addEvent(new MagicPayLifeEvent(event.getSource(), event.getPlayer(), 1));
+            } else {
+                game.doAction(new MagicTapAction(event.getPermanent(), true));
+            }
+        }
+    }
+]
