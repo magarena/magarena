b66655e3f2bf9acd2e810bdc5ef392dbebb0107c
melvin <melvinzhang@gmail.com>
2014-07-28 16:10:55 +0800

    correct event description

--- release/Magarena/scripts/Sudden_Strength.groovy
+++ release/Magarena/scripts/Sudden_Strength.groovy
@@ -7,15 +7,14 @@
                 MagicTargetChoice.POS_TARGET_CREATURE,
                 MagicPumpTargetPicker.create(),
                 this,
-                "Target blocking creature\$ gets +3/+3 until end of turn." +
+                "Target creature\$ gets +3/+3 until end of turn." +
                 "PN draws a card."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final MagicPermanent creature ->
-                game.doAction(new MagicChangeTurnPTAction(creature,+3,+3));
+                game.doAction(new MagicChangeTurnPTAction(it,3,3));
                 game.doAction(new MagicDrawAction(event.getPlayer()));
             });
         }
