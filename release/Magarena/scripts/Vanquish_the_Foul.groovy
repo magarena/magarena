a52e7d9e21c93bb09d89eda350d3fc92890cfc05
melvin <melvinzhang@gmail.com>
2014-07-28 19:44:23 +0800

    replace parameter for single parameter closure with 'it'

--- release/Magarena/scripts/Vanquish_the_Foul.groovy
+++ release/Magarena/scripts/Vanquish_the_Foul.groovy
@@ -13,8 +13,7 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final MagicPermanent permanent ->
-                game.doAction(new MagicDestroyAction(permanent));
+                game.doAction(new MagicDestroyAction(it));
                 game.addEvent(new MagicScryEvent(event.getSource(),event.getPlayer()));
             });
         }
