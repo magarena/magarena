904472285142e111bc1c8fd2773caf0051c491fe
melvin <melvinzhang@gmail.com>
2014-07-28 10:33:59 +0800

    add EnterAsCopy constructor that takes in var args of MagicPermanentAction

--- src/magic/model/action/MagicPlayCardFromStackAction.java
+++ src/magic/model/action/MagicPlayCardFromStackAction.java
@@ -8,17 +8,22 @@ import magic.model.MagicPermanent;
 import magic.model.stack.MagicCardOnStack;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MagicPlayCardFromStackAction extends MagicPutIntoPlayAction {
 
     private final MagicCardOnStack cardOnStack;
     private final MagicCardDefinition cardDef;
-
-    public MagicPlayCardFromStackAction(final MagicCardOnStack aCardOnStack, final MagicCardDefinition aCardDef, final MagicPermanentAction... aModifications) {
+    
+    public MagicPlayCardFromStackAction(final MagicCardOnStack aCardOnStack, final MagicCardDefinition aCardDef, final List<? extends MagicPermanentAction> aModifications) {
         cardOnStack = aCardOnStack;
         cardDef = aCardDef; 
         setPayedCost(aCardOnStack.getPayedCost());
-        setModifications(Arrays.asList(aModifications));
+        setModifications(aModifications);
+    }
+
+    public MagicPlayCardFromStackAction(final MagicCardOnStack aCardOnStack, final MagicCardDefinition aCardDef, final MagicPermanentAction... aModifications) {
+        this(aCardOnStack, aCardDef, Arrays.asList(aModifications));
     }
     
     public MagicPlayCardFromStackAction(final MagicCardOnStack aCardOnStack, final MagicPermanentAction... aModifications) {
@@ -32,6 +37,10 @@ public class MagicPlayCardFromStackAction extends MagicPutIntoPlayAction {
 
     static final public MagicPlayCardFromStackAction EnterAsCopy(final MagicCardOnStack cardOnStack, final MagicObject obj) {
         return new MagicPlayCardFromStackAction(cardOnStack, obj.getCardDefinition());
+    }
+    
+    static final public MagicPlayCardFromStackAction EnterAsCopy(final MagicCardOnStack cardOnStack, final MagicObject obj, final MagicPermanentAction... aModifications) {
+        return new MagicPlayCardFromStackAction(cardOnStack, obj.getCardDefinition(), aModifications);
     }
 
     @Override
