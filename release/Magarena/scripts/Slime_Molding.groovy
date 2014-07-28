a515f48e5d232a7ef7d76c0a417eccb45f202a78
melvin <melvinzhang@gmail.com>
2014-07-28 13:07:37 +0800

    construct token from new card definition so that power/toughness can be copied

--- release/Magarena/scripts/Slime_Molding.groovy
+++ release/Magarena/scripts/Slime_Molding.groovy
@@ -6,26 +6,22 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "Put an "+x+"/"+x+" green Ooze creature token onto the battlefield."
+                "Put an ${x}/${x} green Ooze creature token onto the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final int x = event.getCardOnStack().getX();
-            final MagicStatic PT = new MagicStatic(MagicLayer.SetPT) {
-                @Override
-                public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-                    pt.set(x,x);
-                }
-            };
-            game.doAction(new MagicPlayTokenAction(
-                event.getPlayer(),
-                TokenCardDefinitions.get("green Ooze creature token"),
-                {
-                    final MagicPermanent perm ->
-                    game.doAction(new MagicAddStaticAction(perm,PT));
-                }
-            ));
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(), MagicCardDefinition.create({
+                it.setName("Ooze");
+                it.setFullName("green Ooze creature token");
+                it.setPowerToughness(x, x);
+                it.setColors("g");
+                it.addSubType(MagicSubType.Ooze);
+                it.addType(MagicType.Creature);
+                it.setToken();
+                it.setValue(x);
+            })));
         }
     }
 ]
