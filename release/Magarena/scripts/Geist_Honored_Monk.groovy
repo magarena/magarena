e7a214beeb09c41d2a472612912659b3dd68d8ae
melvin <melvinzhang@gmail.com>
2014-08-02 11:31:23 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Geist_Honored_Monk.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicCDA() {
-        @Override
-        public void modPowerToughness(final MagicGame game, final MagicPlayer player, final MagicPowerToughness pt) {
-            final int amount = player.getNrOfPermanents(MagicType.Creature);
-            pt.set(amount, amount);
-        }
-    }
-]
