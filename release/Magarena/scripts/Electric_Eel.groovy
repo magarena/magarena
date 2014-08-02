748883395c0b8d31b645b200141bcca206a42444
PalladiaMors <unknown>
2014-08-02 16:00:11 +0000

    Add script for Electric Eel

---
+++ release/Magarena/scripts/Electric_Eel.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,MagicManaCost.create("{R}{R}"))
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +2/+0 until end of turn and does 1 damage to PN"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),1,0));
+            final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),1);
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
