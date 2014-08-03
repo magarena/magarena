f81e488618b7728baaf19a5145cddd2bc76370a3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-03 23:01:22 +0100

    Fix MagicWhenTurnedFaceUpTrigger (Other permanents)

---
+++ release/Magarena/scripts/Aphetto_Runecaster.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenTurnedFaceUpTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return new MagicEvent(
+                permanent,
+                new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                this,
+                "PN may\$ draw a card."
+            )
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    }
+]
