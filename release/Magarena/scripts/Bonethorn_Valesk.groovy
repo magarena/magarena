b2ecb36115604aceeb7d80343cd3b52d9c69955e
melvin <melvinzhang@gmail.com>
2014-08-04 08:48:07 +0800

    missing new in expression to create damage target picker, missing processTarget in executeEvent

--- release/Magarena/scripts/Bonethorn_Valesk.groovy
+++ release/Magarena/scripts/Bonethorn_Valesk.groovy
@@ -5,7 +5,7 @@
             return new MagicEvent(
                 permanent,
                 MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
-                MagicDamageTargetPicker(1),
+                new MagicDamageTargetPicker(1),
                 this,
                 "SN deals 1 damage to target creature or player\$."
             )
@@ -13,8 +13,10 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicDamage damage = new MagicDamage(event.getSource(),it,1);
-            game.doAction(new MagicDealDamageAction(damage));
+            event.processTarget(game, {
+                final MagicDamage damage = new MagicDamage(event.getSource(),it,1);
+                game.doAction(new MagicDealDamageAction(damage));
+            });
         }
     }
 ]
