16a9051a93d0012e4253f18c554395f749f2f976
PalladiaMors <unknown>
2014-08-04 03:21:27 +0000

    Add script for Rainbow Crow

---
+++ release/Magarena/scripts/Rainbow_Crow.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump,1),
+        "Color"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}"),
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicColorChoice.RED_WHITE_BLUE_INSTANCE,
+                this,
+                "SN becomes the color\$ of PN's choice until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            final MagicColor color = event.getChosenColor();
+            game.doAction(new MagicAddStaticAction(permanent,
+                new MagicStatic(MagicLayer.Color,MagicStatic.UntilEOT) {
+                @Override
+                public int getColorFlags(final MagicPermanent perm, final int flags) {
+                    return color.getMask();
+                }
+            }));
+        }
+    }
+]
