0662f6e7d72f9ea7e2aa2b88aa8521073cca1e41
melvin <melvinzhang@gmail.com>
2014-08-06 11:16:24 +0800

    add Orochi Eggwatcher

---
+++ release/Magarena/scripts/Orochi_Eggwatcher.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{2}{G}"),
+                new MagicTapEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN puts a 1/1 green Snake creature token onto the battlefield. "+
+                "If you control ten or more creatures, flip SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("1/1 green Snake creature token")
+            ));
+            if (event.getPlayer().getNrOfPermanents(MagicType.Creature) >= 10) {
+                game.doAction(new MagicFlipAction(event.getPermanent()));
+            }
+        }
+    }
+]
