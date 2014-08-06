acb4a4d14fb50d7d98e7ed74df2b779f37a0707c
PalladiaMors <unknown>
2014-08-06 05:17:32 +0000

    Add script for Force of Nature

---
+++ release/Magarena/scripts/Force_of_Nature.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        "Pay {G}{G}{G}{G}?",
+                        new MagicPayManaCostChoice(MagicManaCost.create("{G}{G}{G}{G}"))
+                    ),
+                    this,
+                    "PN may\$ pay {G}{G}{G}{G}\$. If PN doesn't, SN deals 8 damage to PN."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+			final MagicDamage damage = new MagicDamage(event.getSource(),event.getPlayer(),8)
+                game.doAction(new MagicDealDamageAction(damage)  
+                );
+            }
+        }
+    }
+]
