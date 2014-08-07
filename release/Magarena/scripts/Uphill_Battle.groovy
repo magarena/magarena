617c42eba8599e3996056bdaff1809e3805dff27
PalladiaMors <unknown>
2014-08-07 20:53:13 +0000

    Add script for Uphill Battle

---
+++ release/Magarena/scripts/Uphill_Battle.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            if (otherPermanent.isEnemy(permanent) &&
+            otherPermanent.isCreature()) {
+                game.doAction(new MagicTapAction(otherPermanent,false));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
