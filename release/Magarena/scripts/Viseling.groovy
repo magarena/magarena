be73d9fae75502d640133c37b4ec5f4beee34b66
PalladiaMors <unknown>
2014-08-07 14:57:58 +0000

    Add script for Viseling

---
+++ release/Magarena/scripts/Viseling.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer player) {
+            return permanent.isOpponent(player) ?
+			new MagicEvent(
+                    permanent,
+                    player,
+                    this,
+                    "SN deals X damage to PN, where X is the number of cards in his or her hand minus 4."
+                ):
+			MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = player.getHandSize() - 4;
+            final MagicDamage damage = new MagicDamage(
+                event.getSource(),
+                player,
+                amount
+            );
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
