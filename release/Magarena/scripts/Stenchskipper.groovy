315e102e436d5dd9589b7086fa98fbb97cc690a7
melvin <melvinzhang@gmail.com>
2014-08-08 22:25:18 +0800

    normalize and fix card script

--- release/Magarena/scripts/Stenchskipper.groovy
+++ release/Magarena/scripts/Stenchskipper.groovy
@@ -2,17 +2,19 @@
     new MagicAtEndOfTurnTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.isController(upkeepPlayer) && game.getNrOfPermanents(MagicSubType.Goblin) == 0 ?
+            return permanent.getController().controlsPermanent(MagicSubType.Goblin) == false ?
                 new MagicEvent(
                     permanent,
                     this,
-                    "Sacrifice SN."
-                ) :
+                    "If PN controls no Goblins, sacrifice SN."
+                ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            if (event.getPlayer().controlsPermanent(MagicSubType.Goblin) == false) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
         }
     }
 ]
