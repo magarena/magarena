04c463f80cb7628a7201d2bf0c26485134377895
PalladiaMors <unknown>
2014-08-09 00:23:03 +0000

    Add script for Longhorn Firebeast

---
+++ release/Magarena/scripts/Longhorn_Firebeast.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                permanent.getOpponent(),
+                new MagicMayChoice(),
+                this,
+                "PN may\$ have SN deal 5 damage to him or her. If a he or she does, sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),5);
+                game.doAction(new MagicDealDamageAction(damage));
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            } 
+        }
+    }
+]
