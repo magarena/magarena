43d2aa6f23f4e42544080021d86368c018571132
PalladiaMors <unknown>
2014-08-10 21:43:49 +0000

    Add script for Rout

---
+++ release/Magarena/scripts/Rout.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicCardActivation(
+        new MagicActivationHints(MagicTiming.Removal,true),
+        "Instant"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, "{5}{W}{W}")
+            ];
+        }
+    }
+]
