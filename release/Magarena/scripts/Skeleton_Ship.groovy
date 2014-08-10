37c47054d2b59ea160cbe418e5fada4d72eeca70
PalladiaMors <unknown>
2014-08-10 15:13:13 +0000

    Add script for Skeleton Ship

---
+++ release/Magarena/scripts/Skeleton_Ship.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicStatic(MagicLayer.Game) {
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getController().controlsPermanent(MagicSubType.Island) == false;
+        }
+        @Override
+        public void modGame(final MagicPermanent source, final MagicGame game) {
+            game.doAction(new MagicPutStateTriggerOnStackAction(new MagicEvent(
+                source,
+                {
+                    final MagicGame G, final MagicEvent E ->
+                    G.doAction(new MagicSacrificeAction(E.getPermanent()));
+                },
+                "Sacrifice SN."
+            )));
+        }
+    }
+]
