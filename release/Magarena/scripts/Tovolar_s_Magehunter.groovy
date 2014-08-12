1b0d7052cac8c351e610ea755e1c62276a142626
melvin <melvinzhang@gmail.com>
2014-08-12 09:44:44 +0800

    add Mondronen Shaman

---
+++ release/Magarena/scripts/Tovolar_s_Magehunter.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return permanent.isEnemy(cardOnStack) ?
+                new MagicEvent(
+                    permanent,
+                    cardOnStack.getController(),
+                    this,
+                    "SN deals 2 damage to PN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDealDamageAction(new MagicDamage(
+                event.getPermanent(),
+                event.getPlayer(),
+                2
+            )));
+        }
+    }
+]
