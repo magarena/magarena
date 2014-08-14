d1d27eee319609a84277a85038d0a08de0082820
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-14 22:07:13 +0100

    Add MagicAddCounterEvent

---
+++ src/magic/model/event/MagicAddCounterEvent.java
@@ -0,0 +1,39 @@
+package magic.model.event;
+
+import magic.model.MagicCounterType;
+import magic.model.MagicGame;
+import magic.model.MagicPermanent;
+import magic.model.action.MagicChangeCountersAction;
+import magic.model.condition.MagicConditionFactory;
+
+public class MagicAddCounterEvent extends MagicEvent {
+
+    public MagicAddCounterEvent(final MagicPermanent permanent,final MagicCounterType counterType,final int amount) {
+        super(
+            permanent,
+            new MagicEventAction() {
+                @Override
+                public void executeEvent(final MagicGame game, final MagicEvent event) {
+                    game.doAction(new MagicChangeCountersAction(
+                        event.getPermanent(),
+                        counterType,
+                        amount,
+                        true
+                    ));
+                }
+            },
+            genDescription(permanent,counterType,amount)
+        );
+    }
+
+    private static String genDescription(final MagicPermanent permanent,final MagicCounterType counterType,final int amount) {
+        final StringBuilder description=new StringBuilder("Put ");
+        if (amount==1) {
+            description.append("a ").append(counterType.getName()).append(" counter");
+        } else {
+            description.append(amount).append(' ').append(counterType.getName()).append(" counters");
+        }
+        description.append(" on ").append(permanent.getName()).append('.');
+        return description.toString();
+    }
+}
