9c7125774d2a0597c5aa6fb88e4d4bc97b54fb5f
melvin <melvinzhang@gmail.com>
2014-08-16 11:29:30 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Vodalian_Serpent.groovy
+++ release/Magarena/scripts/Vodalian_Serpent.groovy
@@ -1,22 +1,9 @@
 [
-
-    new MagicStatic(MagicLayer.Ability) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            permanent.addAbility(MagicAbility.CannotAttack, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return !source.getController().getOpponent().controlsPermanent(MagicSubType.Island);
-        }
-    },
-    
     new MagicComesIntoPlayWithCounterTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
             if (payedCost.isKicked()) {
                 game.doAction(new MagicChangeCountersAction(permanent,MagicCounterType.PlusOne,4));
-                game.doAction(new MagicGainAbilityAction(permanent,MagicAbility.FirstStrike,MagicStatic.Forever));
             }
             return MagicEvent.NONE;
         }
