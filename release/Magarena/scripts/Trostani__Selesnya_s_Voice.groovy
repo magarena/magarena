c3763dd374cdddcf66008917aa9765e9239bd181
melvin <melvinzhang@gmail.com>
2014-08-16 09:08:59 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Trostani__Selesnya_s_Voice.groovy
+++ release/Magarena/scripts/Trostani__Selesnya_s_Voice.groovy
@@ -20,26 +20,5 @@
             final int toughness = (event.getRefPermanent()).getToughness();
             game.doAction(new MagicChangeLifeAction(event.getPlayer(),toughness));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Flash),
-        "Populate"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{1}{G}{W}")];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Populate."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.addEvent(new MagicPopulateEvent(event.getSource()));
-        }
     }
 ]
