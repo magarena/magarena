df3e6109d0dbcf3ae2f599681adb8a1223b4c210
melvin <melvinzhang@gmail.com>
2014-08-17 10:02:01 +0800

    normalize card script, use Other factory method

--- release/Magarena/scripts/Lord_of_the_Pit.groovy
+++ release/Magarena/scripts/Lord_of_the_Pit.groovy
@@ -6,7 +6,7 @@
                 new MagicEvent(
                     permanent,
                     this,
-                    "sacrifice a creature other than SN. If you can't, SN deals 7 damage to you."
+                    "Sacrifice a creature other than SN. If you can't, SN deals 7 damage to you."
                 ) :
                 MagicEvent.NONE;
         }
@@ -15,20 +15,14 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent permanent = event.getPermanent();
             final MagicPlayer player = event.getPlayer();
-            final MagicTargetChoice targetChoice = new MagicTargetChoice(
-                new MagicOtherPermanentTargetFilter(
-                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
-                    permanent
-                ),
-                MagicTargetHint.None,
-                "a creature other than " + permanent + " to sacrifice"
-            );
-		if (player.getNrOfPermanents(MagicType.Creature) >=2) {
-            game.addEvent(new MagicSacrificePermanentEvent(permanent,player,targetChoice));
-		} else {
-		final MagicDamage damage = new MagicDamage(event.getSource(),event.getPlayer(),7)
-                game.doAction(new MagicDealDamageAction(damage));
-		}
+            final MagicTargetChoice targetChoice = MagicTargetChoice.Other("a creature to sacrifice", permanent);
+            if (targetChoice.hasOptions(game, player, permanent, false)) {
+                game.addEvent(new MagicSacrificePermanentEvent(permanent,player,targetChoice));
+            } else {
+                game.doAction(new MagicDealDamageAction(
+                    new MagicDamage(permanent,player,7)
+                ));
+            }
         }
     }
 ]
