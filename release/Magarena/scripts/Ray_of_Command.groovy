bf78dffef42564f91e926f5e2c3f2d85c0785927
melvin <melvinzhang@gmail.com>
2014-08-17 17:05:58 +0800

    remove default value from constructor

--- release/Magarena/scripts/Ray_of_Command.groovy
+++ release/Magarena/scripts/Ray_of_Command.groovy
@@ -15,7 +15,7 @@ def LoseControlTap = {
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicTapAction(event.getPermanent(), true));
+            game.doAction(new MagicTapAction(event.getPermanent()));
         }
     };
 }
