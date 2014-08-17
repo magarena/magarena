79746963ce51ce0089660887e2d79f8f7bc58497
melvin <melvinzhang@gmail.com>
2014-08-17 11:03:44 +0800

    wrap long expression

--- release/Magarena/scripts/Cinder_Giant.groovy
+++ release/Magarena/scripts/Cinder_Giant.groovy
@@ -14,8 +14,13 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent creature = event.getPermanent();
-            final Collection<MagicPermanent> targets =
-                game.filterPermanents(creature.getController(),new MagicOtherPermanentTargetFilter(MagicTargetFilterFactory.CREATURE_YOU_CONTROL,creature));
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                creature.getController(),
+                new MagicOtherPermanentTargetFilter(
+                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
+                    creature
+                )
+            );
             for (final MagicPermanent target : targets) {
                 final MagicDamage damage = new MagicDamage(creature,target,2);
                 game.doAction(new MagicDealDamageAction(damage));
