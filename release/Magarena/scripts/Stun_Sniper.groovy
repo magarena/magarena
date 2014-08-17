f9fe538b1b13aa9dffc91522de3b976a6dc0741b
melvin <melvinzhang@gmail.com>
2014-08-17 17:02:11 +0800

    remove default value from constructor

--- release/Magarena/scripts/Stun_Sniper.groovy
+++ release/Magarena/scripts/Stun_Sniper.groovy
@@ -5,7 +5,10 @@
     ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{1}")];
+            return [
+                new MagicTapEvent(source), 
+                new MagicPayManaCostEvent(source, "{1}")
+            ];
         }
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
@@ -22,7 +25,7 @@
             event.processTargetPermanent(game, {
                 final MagicDamage damage=new MagicDamage(event.getPermanent(),it,1);
                 game.doAction(new MagicDealDamageAction(damage));
-                game.doAction(new MagicTapAction(it,true));
+                game.doAction(new MagicTapAction(it));
             });
         }
     }
