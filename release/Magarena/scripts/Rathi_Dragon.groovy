fcfed17c5fa0ebf805bba269bdde5f7e1eb2ebb3
PalladiaMors <unknown>
2014-08-17 18:20:30 +0000

    Add script for Rathi Dragon

---
+++ release/Magarena/scripts/Rathi_Dragon.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice("Sacrifice two Mountains?"),
+                this,
+                "PN may\$ sacrifice two Mountains. If you don't, sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent perm = event.getPermanent();
+            if (event.isYes() && event.getPlayer().getNrOfPermanents(MagicSubType.Mountain) >=2) {
+                game.addEvent(new MagicSacrificePermanentEvent(perm,event.getPlayer(),MagicTargetChoice.SACRIFICE_MOUNTAIN));
+		game.addEvent(new MagicSacrificePermanentEvent(perm,event.getPlayer(),MagicTargetChoice.SACRIFICE_MOUNTAIN));
+            } else {
+                game.doAction(new MagicSacrificeAction(perm));
+            }
+        }
+    }
+]
