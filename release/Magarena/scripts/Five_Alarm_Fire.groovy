fa529617d914b8b7b0f0606c99833d422eda407b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-17 15:19:32 +0100

    Add Five-Alarm Fire

---
+++ release/Magarena/scripts/Five_Alarm_Fire.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicSource source=damage.getSource();
+            final MagicTarget target=damage.getTarget();
+            return (damage.isCombat() &&
+                    permanent.isFriend(source) &&
+                    source.isPermanent() &&
+                    (source).isCreature()) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN puts a blaze counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Blaze,1));
+        }
+    }
+]
