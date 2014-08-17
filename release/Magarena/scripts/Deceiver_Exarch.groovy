9e5b986bce8e5917783a2bef9c1f97eb8bb4518e
melvin <melvinzhang@gmail.com>
2014-08-17 17:09:55 +0800

    replace MagicEvent constructor with MagicRuleEventAction.create

--- release/Magarena/scripts/Deceiver_Exarch.groovy
+++ release/Magarena/scripts/Deceiver_Exarch.groovy
@@ -1,38 +1,18 @@
+def tapEvent = MagicRuleEventAction.create("Untap target permanent you control.");
+
 def TapTrigger = new MagicWhenComesIntoPlayTrigger() {
     @Override
     public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            permanent,
-            MagicTargetChoice.TARGET_PERMANENT_YOU_CONTROL,
-            MagicTapTargetPicker.Untap,
-            this,
-            "PN untaps target permanent\$ PN controls."
-        );
-    }
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        event.processTargetPermanent(game, {
-            game.doAction(new MagicUntapAction(it));
-        });
+        return tapEvent.getEvent(permanent);
     }
 }
+
+def untapEvent = MagicRuleEventAction.create("Tap target permanent an opponent controls.");
 
 def UntapTrigger = new MagicWhenComesIntoPlayTrigger() {
     @Override
     public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
-        return new MagicEvent(
-            permanent,
-            MagicTargetChoice.TARGET_PERMANENT_AN_OPPONENT_CONTROLS,
-            MagicTapTargetPicker.Tap,
-            this,
-            "PN taps target permanent\$ an opponent controls."
-        );
-    }
-    @Override
-    public void executeEvent(final MagicGame game, final MagicEvent event) {
-        event.processTargetPermanent(game, {
-            game.doAction(new MagicTapAction(it, true));
-        });
+        return untapEvent.getEvent(permanent);
     }
 }
 
