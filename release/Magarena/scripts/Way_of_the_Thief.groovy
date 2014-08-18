53257bc437f5b377bde45e837f0bd525c40924da
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-18 21:51:07 +0100

    Add Way of the Thief

---
+++ release/Magarena/scripts/Way_of_the_Thief.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            if (permanent.getController().controlsPermanent(MagicSubType.Gate)) {
+                permanent.addAbility(MagicAbility.Unblockable, flags);
+            };
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
