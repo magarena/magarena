992e89f702a3d4b307e05e57a3e0de446129d267
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-18 17:58:05 +0100

    Add Spirit of Resistance

---
+++ release/Magarena/scripts/Spirit_of_Resistance.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            final MagicPlayer player = permanent.getController();
+            if (permanent.isController(damage.getTarget()) &&
+                player.controlsPermanent(MagicColor.White) &&
+                player.controlsPermanent(MagicColor.Blue) &&
+                player.controlsPermanent(MagicColor.Black) &&
+                player.controlsPermanent(MagicColor.Red) &&
+                player.controlsPermanent(MagicColor.Green)
+            ) {
+                // Prevention effect, All damage.
+                damage.prevent();
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
