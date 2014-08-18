47dd6dbc2b873ed52c8cf3d0fc2bacb4d43f8d86
PalladiaMors <unknown>
2014-08-18 01:46:04 +0000

    Add script for Gutwrencher Oni

---
+++ release/Magarena/scripts/Gutwrencher_Oni.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) && 
+permanent.getController().controlsPermanent(MagicSubType.Ogre) == false ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "If PN controls no Ogres, discard a card."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.getPlayer().controlsPermanent(MagicSubType.Ogre) == false) {
+                game.addEvent(new MagicDiscardEvent(event.getSource(),event.getPlayer(),1));
+            }
+        }
+    }
+]
