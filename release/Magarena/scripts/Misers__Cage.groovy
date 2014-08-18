b88a6253f3e85a064619d9c02a9a8c4b0d3baab6
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-18 18:13:10 +0100

    Add Misers' Cage

---
+++ release/Magarena/scripts/Misers__Cage.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return (permanent.isOpponent(player) &&
+                    player.getHandSize() >= 5) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    this,
+                    "SN deals 2 damage to PN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),2);
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
