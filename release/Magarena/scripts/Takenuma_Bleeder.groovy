6d7be1f5c03163b7a10f37d39e750010b580a6ba
melvin <melvinzhang@gmail.com>
2014-08-18 09:41:58 +0800

    normalize card script

--- release/Magarena/scripts/Takenuma_Bleeder.groovy
+++ release/Magarena/scripts/Takenuma_Bleeder.groovy
@@ -1,39 +1,34 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.getPlayer().controlsPermanent(MagicSubType.Demon) == false) {
+        game.doAction(new MagicChangeLifeAction(event.getPlayer(),-1));
+    }
+}
+
+def event = {
+    final MagicPermanent permanent ->
+    return new MagicEvent(
+        permanent,
+        action,
+        "If PN controls no Demons, lose 1 life."
+    );
+}
+
 [
-    new MagicWhenAttacksTrigger() {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
-            return permanent == attacker && permanent.getController().controlsPermanent(MagicSubType.Demon) == false ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "If PN controls no Demons, lose 1 life."
-                ):
+            return permanent.getController().controlsPermanent(MagicSubType.Demon) == false ?
+                event(permanent):
                 MagicEvent.NONE;
         }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.getPlayer().controlsPermanent(MagicSubType.Demon) == false) {
-                game.doAction(new MagicChangeLifeAction(event.getPlayer(),-1));
-            }
-        }
-    },	
-	new MagicWhenBlocksTrigger() {
+    },    
+    new MagicWhenSelfBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
-		final MagicPermanent blocked = permanent.getBlockedCreature();
-            return (permanent == blocker && blocked.isValid()) && permanent.getController().controlsPermanent(MagicSubType.Demon) == false ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "If PN controls no Demons, lose 1 life."
-                ):
+            return permanent.getController().controlsPermanent(MagicSubType.Demon) == false ?
+                event(permanent):
                 MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.getPlayer().controlsPermanent(MagicSubType.Demon) == false) {
-                game.doAction(new MagicChangeLifeAction(event.getPlayer(),-1));
-            }
         }
     }
 ]
