22d89c50ad13d33719e790014ef2b0a77397fc5b
PalladiaMors <unknown>
2014-08-18 01:28:15 +0000

    Add script for Painwracker Oni

---
+++ release/Magarena/scripts/Painwracker_Oni.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return permanent.isController(upkeepPlayer) && 
+permanent.getController().controlsPermanent(MagicSubType.Ogre) == false ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "If PN controls no Ogres, sacrifice a creature."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.getPlayer().controlsPermanent(MagicSubType.Ogre) == false) {
+                game.addEvent(new MagicSacrificePermanentEvent(event.getPermanent(),event.getPlayer(),MagicTargetChoice.SACRIFICE_CREATURE));
+            }
+        }
+    }
+]
