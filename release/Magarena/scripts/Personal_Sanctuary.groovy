82593b537279640f1dc9f4fa8eac0acaae6bb4a9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-18 22:10:39 +0100

    Add Personal Sanctuary

---
+++ release/Magarena/scripts/Personal_Sanctuary.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (permanent.isController(damage.getTarget()) &&
+                permanent.isFriend(game.getTurnPlayer())
+            ) {
+                // Prevention effect, All damage.
+                damage.prevent();
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
