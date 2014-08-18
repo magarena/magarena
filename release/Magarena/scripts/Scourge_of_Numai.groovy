115440025c1b706f8ba2dca357c8f9c2eceb12e5
melvin <melvinzhang@gmail.com>
2014-08-18 09:46:49 +0800

    normalize card script

--- release/Magarena/scripts/Scourge_of_Numai.groovy
+++ release/Magarena/scripts/Scourge_of_Numai.groovy
@@ -2,8 +2,7 @@
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.isController(upkeepPlayer) && 
-permanent.getController().controlsPermanent(MagicSubType.Ogre) == false ?
+            return permanent.isController(upkeepPlayer) && permanent.getController().controlsPermanent(MagicSubType.Ogre) == false ?
                 new MagicEvent(
                     permanent,
                     this,
