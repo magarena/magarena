738f834106b3457a17f02ae1c1177cc31a15ec2b
PalladiaMors <unknown>
2014-08-20 21:29:34 +0000

    Add script for Dread Slag

---
+++ release/Magarena/scripts/Dread_Slag.groovy
@@ -0,0 +1,9 @@
+[
+    new MagicCDA() {
+        @Override
+        public void modPowerToughness(final MagicGame game,final MagicPlayer player,final MagicPowerToughness pt) {
+            final int amount = player.getHandSize() * 4;
+            pt.add(-amount,-amount);
+        }
+    }
+]
