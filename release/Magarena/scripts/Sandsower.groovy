729e3ce6db0c3c71f2daa4012be74f4a2abea71c
melvin <melvinzhang@gmail.com>
2014-08-21 09:01:00 +0800

    normalize card script

--- release/Magarena/scripts/Sandsower.groovy
+++ release/Magarena/scripts/Sandsower.groovy
@@ -14,25 +14,27 @@ def THREE_UNTAPPED_CREATURE_CONDITION = new MagicCondition() {
 };
 
 def AN_UNTAPPED_CREATURE_YOU_CONTROL = new MagicTargetChoice(UNTAPPED_CREATURE_YOU_CONTROL,"an untapped creature you control");
+            
+def sourceEvent = MagicRuleEventAction.create("Tap target creature.");
 
 [
     new MagicPermanentActivation(
         [THREE_UNTAPPED_CREATURE_CONDITION],
         new MagicActivationHints(MagicTiming.Removal),
-        "Untap"
+        "Tap"
     ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
             return [
                 new MagicTapPermanentEvent(source, AN_UNTAPPED_CREATURE_YOU_CONTROL),
-			new MagicTapPermanentEvent(source, AN_UNTAPPED_CREATURE_YOU_CONTROL),
+                new MagicTapPermanentEvent(source, AN_UNTAPPED_CREATURE_YOU_CONTROL),
                 new MagicTapPermanentEvent(source, AN_UNTAPPED_CREATURE_YOU_CONTROL)
             ];
         }
 
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return MagicRuleEventAction.create(source, "Tap target creature.");
+            return sourceEvent.getEvent(source);
         }
     }
 ]
