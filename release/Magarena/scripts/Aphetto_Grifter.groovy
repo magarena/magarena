329816e6996547566a9c1bbc95598bb524356bb3
melvin <melvinzhang@gmail.com>
2014-08-21 09:09:11 +0800

    normalize card script

--- release/Magarena/scripts/Aphetto_Grifter.groovy
+++ release/Magarena/scripts/Aphetto_Grifter.groovy
@@ -1,6 +1,5 @@
 def UNTAPPED_WIZARD_YOU_CONTROL=new MagicPermanentFilterImpl(){
-    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target)
-    {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
         return target.hasSubType(MagicSubType.Wizard) &&  
                target.isUntapped() && 
                target.isController(player);
@@ -14,12 +13,14 @@ def TWO_UNTAPPED_WIZARD_CONDITION = new MagicCondition() {
 };
 
 def AN_UNTAPPED_WIZARD_YOU_CONTROL = new MagicTargetChoice(UNTAPPED_WIZARD_YOU_CONTROL,"an untapped Wizard you control");
+            
+def sourceEvent = MagicRuleEventAction.create("Tap target permanent.");
 
 [
     new MagicPermanentActivation(
         [TWO_UNTAPPED_WIZARD_CONDITION],
         new MagicActivationHints(MagicTiming.Removal),
-        "Untap"
+        "Tap"
     ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
@@ -31,7 +32,7 @@ def AN_UNTAPPED_WIZARD_YOU_CONTROL = new MagicTargetChoice(UNTAPPED_WIZARD_YOU_C
 
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return MagicRuleEventAction.create(source, "Tap target permanent.");
+            return sourceEvent.getEvent(source);
         }
     }
 ]
