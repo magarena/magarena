a462d9282a00001731f5ab3dc2f25581d9bd6e0e
melvin <melvinzhang@gmail.com>
2014-08-23 09:19:23 +0800

    generalize SacrificeChosen to SacrificeMultiple

--- src/magic/model/event/MagicCostEvent.java
+++ src/magic/model/event/MagicCostEvent.java
@@ -38,10 +38,12 @@ public enum MagicCostEvent {
             );
         }
     },
-    SacrificeChosen("Sacrifice " + ARG.ANY) {
+    SacrificeMultiple("Sacrifice " + ARG.AMOUNT + " " + ARG.ANY) {
         public MagicEvent toEvent(final Matcher arg, final MagicSource source) {
-            final String chosen = ARG.any(arg) + " to sacrifice";
-            return new MagicSacrificePermanentEvent(source, new MagicTargetChoice(chosen));
+            final int amt = ARG.amount(arg);
+            final String spec = ARG.any(arg);
+            final String chosen = ("aeiou".indexOf(spec.charAt(0)) >= 0 ? "an " : "a ") + spec + " to sacrifice";
+            return new MagicSacrificePermanentsEvent(source, new MagicTargetChoice(chosen), amt);
         }
     },
     DiscardSelf("Discard SN") {
