cad8ac5281351bbaa8032af5e24acd1431e8e385
melvin <melvinzhang@gmail.com>
2014-08-23 09:41:41 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Ib_Halfheart__Goblin_Tactician.groovy
+++ release/Magarena/scripts/Ib_Halfheart__Goblin_Tactician.groovy
@@ -28,37 +28,5 @@
                 }
             }
         }
-    },
-    new MagicPermanentActivation(
-        [MagicCondition.TWO_MOUNTAINS_CONDITION],
-        new MagicActivationHints(MagicTiming.Token),
-        "Token"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicSacrificePermanentEvent(source, MagicTargetChoice.SACRIFICE_MOUNTAIN),
-                new MagicSacrificePermanentEvent(source, MagicTargetChoice.SACRIFICE_MOUNTAIN)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN puts two 1/1 red Goblin creature tokens into play."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicPlayTokensAction(
-                event.getPlayer(),
-                TokenCardDefinitions.get("1/1 red Goblin creature token"),
-                2
-            ));
-        }
     }
 ]
