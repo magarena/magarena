a61666e00245e543066fdd43cea95dd8b7aee3cf
melvin <melvinzhang@gmail.com>
2014-08-23 16:41:21 +0800

    normalize card script

--- src/magic/model/target/MagicTargetFilterFactory.java
+++ src/magic/model/target/MagicTargetFilterFactory.java
@@ -2427,6 +2427,7 @@ public class MagicTargetFilterFactory {
             .replaceAll("\\bvampires\\b", "vampire")
             .replaceAll("\\bmercenaries\\b", "mercenary")
             .replaceAll("\\bbirds\\b", "bird")
+            .replaceAll("\\bbeasts\\b", "beast")
             .replaceAll("\\band\\b", "or")
             .replaceAll("\\bthem\\b", "it")
             .replaceAll("\\bin your hand\\b", "from your hand")
