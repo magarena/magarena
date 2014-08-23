7a22a9ff4d1dc9d1a88b08b3aeec1f0a7cff832d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-23 20:32:10 +0100

    Edit groovy code

--- release/Magarena/scripts/Nivix_Guildmage.groovy
+++ release/Magarena/scripts/Nivix_Guildmage.groovy
@@ -24,32 +24,5 @@
             game.doAction(new MagicDrawAction(player));
             game.addEvent(new MagicDiscardEvent(event.getPermanent(),player));
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Spell),
-        "Copy"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{2}{U}{R}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                MagicTargetChoice.TARGET_INSTANT_OR_SORCERY_SPELL,
-                this,
-                "Copy target instant or sorcery spell\$. You may choose new targets for the copy."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetCardOnStack(game, {
-                game.doAction(new MagicCopyCardOnStackAction(event.getPlayer(),it));  
-            });
-        }
-    }    
+    }
 ]
