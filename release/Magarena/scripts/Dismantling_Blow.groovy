3b8fe4de16a432a743a68a9ca3c45cefa44e317b
PalladiaMors <unknown>
2014-08-24 22:13:05 +0000

    Add script for Dismantling Blow

---
+++ release/Magarena/scripts/Dismantling_Blow.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_ARTIFACT_OR_ENCHANTMENT,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target artifact or enchantment\$." +
+                "If SN was kicked, draw two cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicDestroyAction(it));
+                if (!event.isKicked()) {
+                    return;
+                }
+                game.doAction(new MagicDrawAction(event.getPlayer(),2));
+            });
+        }
+    }
+]
