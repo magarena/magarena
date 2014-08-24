fdcfe5878f5fdcd7ed439d1e0ac2e4d3fde100d2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-24 20:00:48 +0100

    Add Soul Barrier

---
+++ release/Magarena/scripts/Soul_Barrier.groovy
@@ -0,0 +1,38 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.isYes()) {
+        event.payManaCost(game);
+    } else {
+        final MagicDamage damage = new MagicDamage(event.getSource(),event.getPlayer(),2);
+        game.doAction(new MagicDealDamageAction(damage));
+    }
+}
+
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+       public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return permanent.isEnemy(cardOnStack) && cardOnStack.hasType(MagicType.Creature) ?
+                new MagicEvent(
+                    permanent,
+                    cardOnStack.getController(),
+                    this,
+                    "SN deals 2 damage to PN unless PN pays {2}."
+                ):
+            MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                event.getPlayer(),
+                new MagicMayChoice(
+                    new MagicPayManaCostChoice(MagicManaCost.create("{2}"))
+                ),
+                action,
+                "PN may\$ pay {2}."
+            ));
+        }
+    }
+]
