561e09e68b5612b86c0f61062b72a0b86cd2a25c
PalladiaMors <unknown>
2014-08-24 14:57:44 +0000

    Add script for Nyx Infusion

---
+++ release/Magarena/scripts/Nyx_Infusion.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            if (permanent.hasType(MagicType.Enchantment)) {
+                pt.add(2, 2);
+            } else {
+                pt.add(-2, -2);
+            }
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
