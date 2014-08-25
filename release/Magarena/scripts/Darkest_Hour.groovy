0e55552ccd1a008521b9d533589f8d40ce78920c
melvin <melvinzhang@gmail.com>
2014-08-25 09:03:15 +0800

    normalize and fix card script

--- release/Magarena/scripts/Darkest_Hour.groovy
+++ release/Magarena/scripts/Darkest_Hour.groovy
@@ -2,7 +2,7 @@
     new MagicStatic(MagicLayer.Color,MagicTargetFilterFactory.CREATURE) {
         @Override
         public int getColorFlags(final MagicPermanent permanent, final int flags) {
-            return flags | MagicColor.Black.getMask();
+            return MagicColor.Black.getMask();
         }
     }
 ]
