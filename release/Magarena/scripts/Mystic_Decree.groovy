fc84d3866fce8a170be394906dadd81d1fcadf3a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-28 18:46:02 +0100

    add Mystic Decree

---
+++ release/Magarena/scripts/Mystic_Decree.groovy
@@ -0,0 +1,9 @@
+[
+    new MagicStatic(MagicLayer.Ability,MagicTargetFilterFactory.CREATURE) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            flags.remove(MagicAbility.Flying);
+            flags.remove(MagicAbility.Islandwalk);
+        }
+    }
+]
