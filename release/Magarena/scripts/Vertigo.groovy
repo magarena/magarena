85f6b00e2b51f0a3ce1f3a25f1c9cbdf6be69688
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-29 00:08:57 +0100

    add Vertigo

---
+++ release/Magarena/scripts/Vertigo.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_WITH_FLYING,
+                MagicLoseAbilityTargetPicker.create(MagicAbility.Flying),
+                this,
+                "SN deals 2 to target creature with flying. That creature loses flying until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicDamage damage = new MagicDamage(event.getSource(),it,2);
+                game.doAction(new MagicDealDamageAction(damage));
+                game.doAction(new MagicLoseAbilityAction(it,MagicAbility.Flying));
+            });
+        }
+    }
+]
