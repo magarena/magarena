d2e33ade250f21c4039a96165493f9058a6e757c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-28 23:34:54 +0100

    add Hammerheim

---
+++ release/Magarena/scripts/Hammerheim.groovy
@@ -0,0 +1,39 @@
+def List<MagicAbility> landwalkAbilities = [
+    MagicAbility.Plainswalk,
+    MagicAbility.Islandwalk,
+    MagicAbility.Swampwalk,
+    MagicAbility.Mountainwalk,
+    MagicAbility.Forestwalk,
+    MagicAbility.NonbasicLandwalk,
+    MagicAbility.LegendaryLandwalk
+];
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.LoseEvasion),
+        "Lose landwalk"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                this,
+                "Target creature\$ loses all landwalk abilities until end of turn"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicLoseAbilityAction(it,landwalkAbilities));
+            });
+        }
+    }
+]
