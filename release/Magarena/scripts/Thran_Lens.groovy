6b558bb90fc87f4f5f220d35ea78c921d1981e8d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-28 18:45:51 +0100

    add Thran Lens

---
+++ release/Magarena/scripts/Thran_Lens.groovy
@@ -0,0 +1,8 @@
+[
+    new MagicStatic(MagicLayer.Color,MagicTargetFilterFactory.PERMANENT) {
+        @Override
+        public int getColorFlags(final MagicPermanent permanent, final int flags) {
+            return 0;
+        }
+    }
+]
