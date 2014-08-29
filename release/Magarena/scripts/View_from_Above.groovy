9c615acf276ebd5b0271878f77990247a0960ca2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-29 23:47:23 +0100

    add View from Above

---
+++ release/Magarena/scripts/View_from_Above.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicFlyingTargetPicker.create(),
+                this,
+                "Target creature\$ gains flying until end of turn. " +
+                "If PN controls a white permanent, return SN to its owner's hand."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicGainAbilityAction(it,MagicAbility.Flying));
+                if (event.getPlayer().controlsPermanent(MagicColor.White)) {
+                    game.doAction(new MagicChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+                }
+            });
+        }
+    }
+]
