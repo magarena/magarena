9f253bb5a6c08cedfba630284c6c949e21cc5431
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-29 23:46:22 +0100

    add Mirror-Sigil Sergeant

---
+++ release/Magarena/scripts/Mirror_Sigil_Sergeant.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return (upkeepPlayer.controlsPermanent(MagicColor.Blue)) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    this,
+                    "If PN controls a blue permanent, PN may\$ put a token that's a copy of SN onto the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes() && event.getPlayer().controlsPermanent(MagicColor.Blue)) {
+                game.doAction(new MagicPlayTokenAction(
+                    event.getPlayer(),
+                    event.getPermanent()
+                ));
+            }
+        }
+    }
+]
