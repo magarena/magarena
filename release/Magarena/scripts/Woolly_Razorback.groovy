5e59e18388addfd7419c14392e632d8697bc6a38
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-29 01:48:38 +0100

    add Woolly Razorback

---
+++ release/Magarena/scripts/Woolly_Razorback.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (damage.isCombat() && damage.isSource(permanent) && permanent.getCounters(MagicCounterType.Ice) >= 1) {
+                // Replacement effect. Generates no event or action.
+                damage.prevent();
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
