5169e1882b0e4088799813deba60fb2e11f25691
melvin <melvinzhang@gmail.com>
2014-08-30 09:32:27 +0800

    normalize card script

--- release/Magarena/scripts/Kagemaro__First_to_Suffer.groovy
+++ release/Magarena/scripts/Kagemaro__First_to_Suffer.groovy
@@ -3,33 +3,28 @@
         new MagicActivationHints(MagicTiming.Removal),
         "Weaken"
     ) {
-		@Override
+        @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{B}"), new MagicSacrificeEvent(source)];
+            return [
+                new MagicPayManaCostEvent(source,"{B}"), 
+                new MagicSacrificeEvent(source)
+            ];
         }
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 source,
                 this,
-                "All creatures get -X/-X until end of turn, where X is the number of cards in your hand."
+                "All creatures get -X/-X until end of turn, where X is the number of cards in PN's hand."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer player = event.getPlayer();
-            Collection<MagicPermanent> creatures = game.filterPermanents(
-                player,
-                MagicTargetFilterFactory.CREATURE
-            )
-            final int amt = player.getHandSize();
-            for (final MagicPermanent creature : creatures) {
-                game.doAction(new MagicChangeTurnPTAction(
-                    creature,
-                    -amt,
-                    -amt
-                ));
-		}
+            final int X = event.getPlayer().getHandSize();
+            final Collection<MagicPermanent> creatures = game.filterPermanents(MagicTargetFilterFactory.CREATURE)
+            for (final MagicPermanent it : creatures) {
+                game.doAction(new MagicChangeTurnPTAction(it, -X, -X));
+            }
         }
     }
 ]
