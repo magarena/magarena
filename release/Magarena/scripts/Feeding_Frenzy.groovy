06d6f6e4b6c20499079e2e6f47481d4b6baedd03
melvin <melvinzhang@gmail.com>
2014-08-30 09:28:26 +0800

    normalize card script and correct indentation

--- release/Magarena/scripts/Feeding_Frenzy.groovy
+++ release/Magarena/scripts/Feeding_Frenzy.groovy
@@ -2,19 +2,21 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int X = game.getNrOfPermanents(MagicSubType.Zombie);
             return new MagicEvent(
                 cardOnStack,
-			MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(-X, -X)
                 this,
                 "Target creature\$ gets -X/-X until end of turn, where X is the number of Zombies on the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-		event.processTargetPermanent(game, {
-		final int amount =game.getNrOfPermanents(MagicSubType.Zombie);
-            game.doAction(new MagicChangeTurnPTAction(it,-amount,-amount));
-	});
+            event.processTargetPermanent(game, {
+                final int X = game.getNrOfPermanents(MagicSubType.Zombie);
+                game.doAction(new MagicChangeTurnPTAction(it, -X, -X));
+            });
         }
     }
 ]
