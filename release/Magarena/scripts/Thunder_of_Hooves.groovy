a8a11330583a714e369299516ec84376379aee54
PalladiaMors <unknown>
2014-08-30 17:51:08 +0000

    Add script for Thunder of Hooves

---
+++ release/Magarena/scripts/Thunder_of_Hooves.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int X = cardOnStack.getGame().getNrOfPermanents(MagicSubType.Beast);
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "SN deals X damage to each creature without flying and each player, where X is the number of Beasts on the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+		final MagicSource source=event.getSource();
+                final int X = game.getNrOfPermanents(MagicSubType.Beast);
+                final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE_WITHOUT_FLYING);
+            for (final MagicPermanent target : targets) {
+                final MagicDamage damage=new MagicDamage(source,target,X);
+                game.doAction(new MagicDealDamageAction(damage));
+            }
+            for (final MagicPlayer player : game.getAPNAP()) {
+                final MagicDamage damage=new MagicDamage(source,player,X);
+                game.doAction(new MagicDealDamageAction(damage)); 
+	    }
+        }
+    }
+]
