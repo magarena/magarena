97761464cbd20c4f2f2238ae76ef258cb7ed0109
melvin <melvinzhang@gmail.com>
2014-08-30 11:29:47 +0800

    normalize card script

--- release/Magarena/scripts/Wirewood_Pride.groovy
+++ release/Magarena/scripts/Wirewood_Pride.groovy
@@ -4,17 +4,18 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-			MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
                 this,
                 "Target creature\$ gets +X/+X until end of turn, where X is the number of Elves on the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-		event.processTargetPermanent(game, {
-		final int amount =game.getNrOfPermanents(MagicSubType.Elf);
-            game.doAction(new MagicChangeTurnPTAction(it,+amount,+amount));
-	});
+            event.processTargetPermanent(game, {
+                final int X = game.getNrOfPermanents(MagicSubType.Elf);
+                game.doAction(new MagicChangeTurnPTAction(it, +X, +X));
+            });
         }
     }
 ]
