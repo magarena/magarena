9fc9dfbe81a10a5234d288cfd6d6bb4a57859c5a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-31 00:54:14 +0100

    add Second Chance

---
+++ release/Magarena/scripts/Second_Chance.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return upkeepPlayer.getLife() <= 5 ? 
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN sacrifices SN. PN takes an extra turn after this one."
+            ):
+            MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            if (player.getLife() <= 5) {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+                game.doAction(new MagicChangeExtraTurnsAction(player,1));
+            }
+        }
+    }
+]
