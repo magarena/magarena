fb8b77f2930430f7a8db484a3edc8f13b5a3bf03
PalladiaMors <unknown>
2014-08-30 01:44:38 +0000

    Add script for Wirewood Pride

---
+++ release/Magarena/scripts/Wirewood_Pride.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+			MagicTargetChoice.POS_TARGET_CREATURE,
+                this,
+                "Target creature\$ gets +X/+X until end of turn, where X is the number of Elves on the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+		event.processTargetPermanent(game, {
+		final int amount =game.getNrOfPermanents(MagicSubType.Elf);
+            game.doAction(new MagicChangeTurnPTAction(it,+amount,+amount));
+	});
+        }
+    }
+]
