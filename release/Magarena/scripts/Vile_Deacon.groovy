df2e2b12f8fd2c2e0f027500ce6047c556c4c48a
melvin <melvinzhang@gmail.com>
2014-08-30 11:32:13 +0800

    normalize card script

--- release/Magarena/scripts/Vile_Deacon.groovy
+++ release/Magarena/scripts/Vile_Deacon.groovy
@@ -5,13 +5,13 @@
             return new MagicEvent(
                 permanent,
                 this,
-                "SN gets +X/+X until end of turn, where X is the number of Clerics in play."
+                "SN gets +X/+X until end of turn, where X is the number of Clerics on the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount =game.getNrOfPermanents(MagicSubType.Cleric);
-            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),amount,amount));
+            final int X = game.getNrOfPermanents(MagicSubType.Cleric);
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(), +X, +X));
         }
     }
 ]
