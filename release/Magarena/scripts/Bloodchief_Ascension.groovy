d15e0e1c66f414bb189fa57d57e3cbaad22eb14c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-08-31 00:53:22 +0100

    add Bloodchief Ascension

--- release/Magarena/scripts/Bloodchief_Ascension.groovy
+++ release/Magarena/scripts/Bloodchief_Ascension.groovy
@@ -1,35 +1,4 @@
 [
-    new MagicAtEndOfTurnTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
-            return (eotPlayer == permanent.getController().getOpponent() &&
-                    eotPlayer.lifeGained() <= -2) ? //Is this possible?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.ADD_POS_COUNTER,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    this,
-                    "PN may\$ put a quest counter on SN."
-                ):
-                MagicEvent.NONE;
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                game.doAction(new MagicChangeCountersAction(
-                    event.getPermanent(),
-                    MagicCounterType.Quest,
-                    1,
-                    true
-                ));
-            }
-        }
-    },
-    
     new MagicWhenOtherPutIntoGraveyardTrigger() {
         @Override
         public boolean accept(final MagicPermanent permanent, final MagicMoveCardAction act) {
@@ -56,9 +25,9 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes){
-                game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
+            if (event.isYes()){
                 game.doAction(new MagicChangeLifeAction(event.getPlayer().getOpponent(),-2));
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),2));
             }
         }
     }
