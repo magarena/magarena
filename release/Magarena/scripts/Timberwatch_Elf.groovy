c7be8d57bffe4dde71898b52b792e11c29987157
melvin <melvinzhang@gmail.com>
2014-08-30 11:31:13 +0800

    normalize card script

--- release/Magarena/scripts/Timberwatch_Elf.groovy
+++ release/Magarena/scripts/Timberwatch_Elf.groovy
@@ -3,7 +3,7 @@
         new MagicActivationHints(MagicTiming.Pump),
         "Pump"
     ) {
-		@Override
+        @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
             return [
                 new MagicTapEvent(source)
@@ -13,17 +13,18 @@
         public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 source,
-			MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
                 this,
                 "Target creature\$ gets +X/+X until end of turn, where X is the number of Elves on the battlefield."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-		event.processTargetPermanent(game, {
-		final int amount =game.getNrOfPermanents(MagicSubType.Elf);
-            game.doAction(new MagicChangeTurnPTAction(it,+amount,+amount));
-	});
+            event.processTargetPermanent(game, {
+                final int X = game.getNrOfPermanents(MagicSubType.Elf);
+                game.doAction(new MagicChangeTurnPTAction(it, +X, +X));
+            });
         }
     }
 ]
