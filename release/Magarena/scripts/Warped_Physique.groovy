03b0267ae3ca1fac9768c5d5211451e30c23ad8b
melvin <melvinzhang@gmail.com>
2014-08-30 09:10:33 +0800

    normalize and correct indentation

--- release/Magarena/scripts/Warped_Physique.groovy
+++ release/Magarena/scripts/Warped_Physique.groovy
@@ -4,18 +4,18 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-			MagicTargetChoice.TARGET_CREATURE,
+                MagicTargetChoice.TARGET_CREATURE,
                 this,
-                "Target creature\$ gets +X/-X until end of turn, where X is the number of cards in your hand."
+                "Target creature\$ gets +X/-X until end of turn, where X is the number of cards in PN's hand."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-		event.processTargetPermanent(game, {
-		final MagicPlayer player = event.getPlayer();
-		final int amount = player.getHandSize();
-            game.doAction(new MagicChangeTurnPTAction(it,+amount,-amount));
-	});
+            event.processTargetPermanent(game, {
+                final MagicPlayer player = event.getPlayer();
+                final int amount = player.getHandSize();
+                game.doAction(new MagicChangeTurnPTAction(it,+amount,-amount));
+            });
         }
     }
 ]
