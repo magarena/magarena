1299c3bf351b05e14e3f4ab46e21aab18dd2cf8c
melvin <melvinzhang@gmail.com>
2014-08-31 09:46:39 +0800

    normalize card script

--- release/Magarena/scripts/Sparksmith.groovy
+++ release/Magarena/scripts/Sparksmith.groovy
@@ -13,23 +13,24 @@
 
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final int X = game.getNrOfPermanents(MagicSubType.Goblin);
             return new MagicEvent(
                 source,
                 MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(X),
                 this,
                 "SN deals X damage to target creature\$ and X damage to PN, "+
-                "where X is the number of Goblins in play."
+                "where X is the number of Goblins on the battlefield."
             );
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game,{
-                final MagicPlayer player = event.getPlayer()
                 final int X = game.getNrOfPermanents(MagicSubType.Goblin);
                 final MagicDamage damage=new MagicDamage(event.getSource(),it,X);
                 game.doAction(new MagicDealDamageAction(damage));
-		final MagicDamage damage2=new MagicDamage(event.getSource(),player,X);
+                final MagicDamage damage2=new MagicDamage(event.getSource(),event.getPlayer(),X);
                 game.doAction(new MagicDealDamageAction(damage2));
             });
         }
