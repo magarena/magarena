2cc12f766f51d04f1fe0c3f3f3c8ec6b67298ba8
Lodici <Lodici>
2014-09-01 10:32:46 +0100

    consolidate image scaling code into single class.

--- src/magic/ui/canvas/cards/ImageHandler.java
+++ src/magic/ui/canvas/cards/ImageHandler.java
@@ -5,21 +5,17 @@ import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.RenderingHints;
-import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.IOException;
+import magic.ui.utility.GraphicsUtilities;
 
 public class ImageHandler {
 
     private final ImageObserver observer;
     private final ImageCache imageCache;
-    private boolean useScaledInstanceMethod = false;
 
     public ImageHandler(final ImageObserver observer0) {
         this.observer = observer0;
@@ -41,14 +37,14 @@ public class ImageHandler {
     }
 
     public BufferedImage getAnnotatedImage(final String id, final Image sourceImage) {
-        final BufferedImage pic =
-                getCompatibleBufferedImage(
+        final BufferedImage pic
+                = GraphicsUtilities.getCompatibleBufferedImage(
                         sourceImage.getWidth(observer),
                         sourceImage.getHeight(observer));
-        Graphics2D g2d = (Graphics2D)pic.getGraphics();
+        Graphics2D g2d = (Graphics2D) pic.getGraphics();
         g2d.drawImage(sourceImage, 0, 0, null);
         g2d.setColor(Color.WHITE);
-        g2d.setFont(new Font("dialog",Font.BOLD,32));
+        g2d.setFont(new Font("dialog", Font.BOLD, 32));
         g2d.drawString(id, 40, 110);
         g2d.dispose();
         return pic;
@@ -68,20 +64,14 @@ public class ImageHandler {
             BufferedImage scaledImage = imageCache.get(cacheKey);
             if (scaledImage == null) {
                 // not in cache so will need to scale.
-                if (useScaledInstanceMethod) {
-                    final Image image = ((Image)sourceImage).getScaledInstance(newWidth, -1, Image.SCALE_SMOOTH);
-                    scaledImage = getCompatibleBufferedImage(image.getWidth(null), image.getHeight(null));
-                    scaledImage.getGraphics().drawImage(image, 0, 0 , null);
-                } else {
-                    final double aspectRatio = (double)sourceImage.getWidth() / sourceImage.getHeight();
-                    final int newHeight = (int)(newWidth / aspectRatio);
-                    final boolean useQualityScale = newWidth < sourceImage.getWidth();
-                    scaledImage = getScaledInstance(
-                            sourceImage,
-                            newWidth, newHeight,
-                            RenderingHints.VALUE_INTERPOLATION_BILINEAR,
-                            useQualityScale);
-                }
+                final double aspectRatio = (double) sourceImage.getWidth() / sourceImage.getHeight();
+                final int newHeight = (int) (newWidth / aspectRatio);
+                final boolean useQualityScale = newWidth < sourceImage.getWidth();
+                scaledImage = GraphicsUtilities.scale(
+                        sourceImage,
+                        newWidth, newHeight,
+                        RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+                        useQualityScale);
                 // add scaled image to cache for faster access next time.
                 imageCache.put(cacheKey, scaledImage);
             }
@@ -94,15 +84,12 @@ public class ImageHandler {
     /**
      * Loads an image from file and ensures it is in optimum format.
      * <p>
-     * The images returned by ImageIO are often in custom formats which can draw really,
-     * really slowly. For best performance, it's often best to draw any image returned by
-     * ImageIO into a new image of the appropriate pixel format for your system.
-     * (http://www.jhlabs.com/ip/managed_images.html)
+     * The images returned by ImageIO are often in custom formats which can draw really, really slowly. For best performance, it's often best to draw any image returned by ImageIO into a new image of the appropriate pixel format for your system. (http://www.jhlabs.com/ip/managed_images.html)
      */
     private BufferedImage loadImageResourceFromFile(final String filename) {
         final BufferedImage source = loadImage(filename);
-        final BufferedImage buffImage = getCompatibleBufferedImage(source.getWidth(), source.getHeight());
-        buffImage.getGraphics().drawImage(source, 0, 0 , observer);
+        final BufferedImage buffImage = GraphicsUtilities.getCompatibleBufferedImage(source.getWidth(), source.getHeight());
+        buffImage.getGraphics().drawImage(source, 0, 0, observer);
         return buffImage;
     }
 
@@ -117,85 +104,6 @@ public class ImageHandler {
             throw new NullPointerException("Failed to load image file: " + filename);
         }
         return img;
-    }
-
-    private BufferedImage getCompatibleBufferedImage(final int width, final int height) {
-        final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        final GraphicsDevice gs = ge.getDefaultScreenDevice();
-        final GraphicsConfiguration gc = gs.getDefaultConfiguration();
-        return gc.createCompatibleImage(width, height, Transparency.OPAQUE);
-    }
-
-    /**
-     * Convenience method that returns a scaled instance of the
-     * provided {@code BufferedImage}.
-     *
-     * @see https://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
-     *
-     * @param img the original image to be scaled
-     * @param targetWidth the desired width of the scaled instance,
-     *    in pixels
-     * @param targetHeight the desired height of the scaled instance,
-     *    in pixels
-     * @param hint one of the rendering hints that corresponds to
-     *    {@code RenderingHints.KEY_INTERPOLATION} (e.g.
-     *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
-     *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
-     *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
-     * @param higherQuality if true, this method will use a multi-step
-     *    scaling technique that provides higher quality than the usual
-     *    one-step technique (only useful in downscaling cases, where
-     *    {@code targetWidth} or {@code targetHeight} is
-     *    smaller than the original dimensions, and generally only when
-     *    the {@code BILINEAR} hint is specified)
-     * @return a scaled version of the original {@code BufferedImage}
-     */
-    public BufferedImage getScaledInstance(BufferedImage img,
-                                           int targetWidth,
-                                           int targetHeight,
-                                           Object hint,
-                                           boolean higherQuality)
-    {
-        BufferedImage ret = img;
-        int w, h;
-        if (higherQuality) {
-            // Use multi-step technique: start with original size, then
-            // scale down in multiple passes with drawImage()
-            // until the target size is reached
-            w = img.getWidth();
-            h = img.getHeight();
-        } else {
-            // Use one-step technique: scale directly from original
-            // size to target size with a single drawImage() call
-            w = targetWidth;
-            h = targetHeight;
-        }
-
-        do {
-            if (higherQuality && w > targetWidth) {
-                w /= 2;
-                if (w < targetWidth) {
-                    w = targetWidth;
-                }
-            }
-
-            if (higherQuality && h > targetHeight) {
-                h /= 2;
-                if (h < targetHeight) {
-                    h = targetHeight;
-                }
-            }
-
-            BufferedImage tmp = getCompatibleBufferedImage(w, h);
-            Graphics2D g2 = tmp.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
-            g2.drawImage(ret, 0, 0, w, h, null);
-            g2.dispose();
-
-            ret = tmp;
-        } while (w != targetWidth || h != targetHeight);
-
-        return ret;
     }
 
 }
