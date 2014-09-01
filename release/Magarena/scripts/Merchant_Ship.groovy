ef8d0ed725926046a49243282cf8a58630a93ad2
PalladiaMors <unknown>
2014-09-01 16:13:06 +0000

    Add script for Merchant Ship

---
+++ release/Magarena/scripts/Merchant_Ship.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN gains 2 life."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(), 2));
+        }
+    }
+]
