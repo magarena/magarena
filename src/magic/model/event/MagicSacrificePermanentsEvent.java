69c383c51e5ecd203d4b49fe45643e98c865f76b
melvin <melvinzhang@gmail.com>
2014-09-01 08:46:45 +0800

    change MagicSacrificePermanentsEvent to sacrifice one permanent, then add first n - 1 other events

--- src/magic/model/event/MagicSacrificePermanentsEvent.java
+++ src/magic/model/event/MagicSacrificePermanentsEvent.java
@@ -13,20 +13,24 @@ public class MagicSacrificePermanentsEvent extends MagicEvent {
     public MagicSacrificePermanentsEvent(final MagicSource source, final MagicTargetChoice targetChoice, final int amt) {
         super(
             source,
-            amt,
-            new MagicEventAction() {
-                @Override
-                public void executeEvent(final MagicGame game, final MagicEvent event) {
-                    final MagicEvent sac = new MagicSacrificePermanentEvent(event.getSource(), targetChoice);
-                    for (int i = 0; i < amt; i++) {
-                        game.addEvent(sac);
-                    }
-                }
-            },
+            targetChoice,
+            amt - 1,
+            EVENT_ACTION,
             ""
         );
         conds = new MagicCondition[]{MagicConditionFactory.YouControlAtLeast(targetChoice.getPermanentFilter(), amt)};
     }
+            
+    public static MagicEventAction EVENT_ACTION = new MagicEventAction() {
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent sac = new MagicSacrificePermanentEvent(event.getSource(), event.getTargetChoice());
+            sac.executeEvent(game, event.getChosen());
+            for (int i = 0; i < event.getRefInt(); i++) {
+                game.addFirstEvent(sac);
+            }
+        }
+    };
 
     @Override
     public MagicCondition[] getConditions() {
