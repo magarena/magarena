0163e11d90f201bf9ae14a00ced48ad18c91411e
melvin <melvinzhang@gmail.com>
2014-09-02 12:04:15 +0800

    move Gelatinous Genesis to scripts

---
+++ release/Magarena/scripts/Gelatinous_Genesis.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.getX(),
+                this,
+                "Put RN RN/RN green Ooze creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int x = event.getRefInt();
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(), 
+                MagicCardDefinition.create({
+                    it.setName("Ooze");
+                    it.setFullName("green Ooze creature token");
+                    it.setPowerToughness(x, x);
+                    it.setColors("g");
+                    it.addSubType(MagicSubType.Ooze);
+                    it.addType(MagicType.Creature);
+                    it.setToken();
+                    it.setValue(x);
+                }),
+                x
+            ));
+        }
+    }
+]
