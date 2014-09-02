d3bd5b47be1067f9093d2fe81008bff9a13b0604
melvin <melvinzhang@gmail.com>
2014-09-02 10:48:57 +0800

    normalize card script

---
+++ release/Magarena/scripts/Lim_D_l_s_Paladin.groovy
@@ -0,0 +1,45 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice("Discard a card?"),
+                this,
+                "PN may\$ discard a card. If PN doesn't, sacrifice SN and draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent discard = new MagicDiscardEvent(event.getSource(), event.getPlayer());
+            if (event.isYes() && discard.isSatisfied(game)) {
+                game.addEvent(discard);
+            } else {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    },
+    new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN assigns no combat damage this turn and defending player loses 4 life."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(MagicChangeStateAction.Set(
+                event.getPermanent(),
+                MagicPermanentState.NoCombatDamage
+            ));
+            game.doAction(new MagicChangeLifeAction(game.getDefendingPlayer(), -4));
+        }
+    }
+]
