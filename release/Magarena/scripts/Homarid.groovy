5a415951db8e29317e81334135d1bd15a64c23ac
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-04 00:42:36 +0100

    add Homarid

---
+++ release/Magarena/scripts/Homarid.groovy
@@ -0,0 +1,25 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    game.doAction(new MagicChangeCountersAction(
+        event.getPermanent(), 
+        MagicCounterType.Tide,
+        -event.getPermanent().getCounters(MagicCounterType.Tide)
+    ));
+}
+
+[
+    new MagicStatic(MagicLayer.Game) {
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getCounters(MagicCounterType.Tide) == 4;
+        }
+        @Override
+        public void modGame(final MagicPermanent source, final MagicGame game) {
+            game.doAction(new MagicPutStateTriggerOnStackAction(new MagicEvent(
+                source,
+                action,
+                "PN removes all tide counters from SN."
+            )));
+        }
+    }
+]
