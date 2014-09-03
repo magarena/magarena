21e5a9ba6c166fabd9941eecbd48449b823f2ecb
PalladiaMors <unknown>
2014-09-03 03:07:01 +0200

    Add script for Lust for War

---
+++ release/Magarena/scripts/Lust_for_War.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenBecomesTappedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent tapped) {
+            final MagicPermanent enchantedCreature = permanent.getEnchantedPermanent();
+            return (enchantedCreature.isCreature() && enchantedCreature==tapped) ?
+                new MagicEvent(
+                    permanent,
+                    tapped.getController(),
+                    this,
+                    "SN deals 3 damage to PN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            final MagicDamage damage=new MagicDamage(event.getSource(),event.getPlayer(),3);
+            game.doAction(new MagicDealDamageAction(damage));
+        }
+    }
+]
