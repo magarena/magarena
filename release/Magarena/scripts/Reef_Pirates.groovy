9b817efa030ea2deb523064ddd3fd583da09faa6
melvin <melvinzhang@gmail.com>
2014-09-03 14:33:54 +0800

    normalize card script

--- release/Magarena/scripts/Reef_Pirates.groovy
+++ release/Magarena/scripts/Reef_Pirates.groovy
@@ -2,18 +2,19 @@
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            return (permanent == damage.getSource() &&
+            return (permanent == damage.getSource() && 
                     permanent.isOpponent(damage.getTarget())) ?
                 new MagicEvent(
                     permanent,
+                    damage.getTargetPlayer(),
                     this,
-                    "Opponent puts the top card of his or her library into his or her graveyard."
+                    "PN puts the top card of his or her library into his or her graveyard."
                 ) :
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicMillLibraryAction(event.getPlayer().getOpponent(),1));
+            game.doAction(new MagicMillLibraryAction(event.getPlayer(),1));
         }
     }
 ]
