006211ac0dc6814c7c2de67498493207263f4dfc
melvin <melvinzhang@gmail.com>
2014-09-03 14:47:17 +0800

    normalize card script

--- release/Magarena/scripts/Spirit_Shackle.groovy
+++ release/Magarena/scripts/Spirit_Shackle.groovy
@@ -3,22 +3,20 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent tapped) {
             final MagicPermanent enchantedCreature = permanent.getEnchantedPermanent();
-            return (enchantedCreature.isCreature() && enchantedCreature==tapped) ?
+            return enchantedCreature == tapped ?
                 new MagicEvent(
                     permanent,
                     enchantedCreature,
                     this,
-                    "PN puts a -0/-2 counter on creature enchanted by SN."
+                    "PN puts a -0/-2 counter on RN."
                 ) :
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event) {
-            final MagicPermanent permanent=event.getPermanent();
-            final MagicPermanent enchanted=permanent.getEnchantedPermanent();
-            if (enchanted.isValid()) {
-                game.doAction(new MagicChangeCountersAction(enchanted,MagicCounterType.MinusZeroMinusTwo,1));
-		 }
+            event.processRefPermanent(game, {
+                game.doAction(new MagicChangeCountersAction(it,MagicCounterType.MinusZeroMinusTwo,1));
+            });
         }
     }
 ]
