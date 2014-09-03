2670ceec21d1f2d24d73cb267f74c3ad576c75e3
melvin <melvinzhang@gmail.com>
2014-09-03 14:24:35 +0800

    normalize card script

--- release/Magarena/scripts/Psionic_Entity.groovy
+++ release/Magarena/scripts/Psionic_Entity.groovy
@@ -25,10 +25,8 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final MagicDamage damage=new MagicDamage(event.getSource(),it,2);
-                game.doAction(new MagicDealDamageAction(damage));
-                final MagicDamage damage2=new MagicDamage(event.getSource(),event.getPermanent(),3);
-                game.doAction(new MagicDealDamageAction(damage2));
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,2);
+                game.doAction(new MagicDealDamageAction(event.getSource(),event.getPermanent(),3);
             });
         }
     }
