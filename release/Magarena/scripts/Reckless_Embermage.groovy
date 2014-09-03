d378b9fa09d263db0c8529af83b320aaf085e6ea
melvin <melvinzhang@gmail.com>
2014-09-03 14:27:36 +0800

    normalize card script

--- release/Magarena/scripts/Reckless_Embermage.groovy
+++ release/Magarena/scripts/Reckless_Embermage.groovy
@@ -18,17 +18,15 @@
                 MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
                 new MagicDamageTargetPicker(1),
                 this,
-                "SN deals 1 damage to target creature\$ or player\$ and 1 damage to itself."
+                "SN deals 1 damage to target creature or player\$ and 1 damage to itself."
             );
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final MagicDamage damage=new MagicDamage(event.getSource(),it,1);
-                game.doAction(new MagicDealDamageAction(damage));
-                final MagicDamage damage2=new MagicDamage(event.getSource(),event.getPermanent(),1);
-                game.doAction(new MagicDealDamageAction(damage2));
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,1));
+                game.doAction(new MagicDealDamageAction(event.getSource(),event.getPermanent(),1));
             });
         }
     }
