17856ba525ae8d7d19395cbddb9f1e16c46684bc
melvin <melvinzhang@gmail.com>
2014-09-04 11:10:57 +0800

    use more specific trigger

--- release/Magarena/scripts/Soltari_Visionary.groovy
+++ release/Magarena/scripts/Soltari_Visionary.groovy
@@ -1,18 +1,16 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            return (damage.isSource(permanent) && damage.isTargetPlayer()) ?
-                new MagicEvent(
-                    permanent,
-                    permanent.isController(damage.getTarget()) ?
-                        MagicTargetChoice.TARGET_ENCHANTMENT_YOU_CONTROL :
-                        MagicTargetChoice.TARGET_ENCHANTMENT_YOUR_OPPONENT_CONTROLS,
-                    MagicDestroyTargetPicker.Destroy,
-                    this,
-                    "Destroy target enchantment\$."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                permanent.isController(damage.getTarget()) ?
+                    MagicTargetChoice.TARGET_ENCHANTMENT_YOU_CONTROL :
+                    MagicTargetChoice.TARGET_ENCHANTMENT_YOUR_OPPONENT_CONTROLS,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target enchantment\$."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
