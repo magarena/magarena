b553e74d028f5eef93301d7d092fada0401b1557
melvin <melvinzhang@gmail.com>
2014-09-04 10:46:24 +0800

    normalize and replace groovy code with card script

--- release/Magarena/scripts/Deep_Spawn.groovy
+++ release/Magarena/scripts/Deep_Spawn.groovy
@@ -12,40 +12,11 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes() && event.getPlayer().getLibrary().size() >=2) {
+            if (event.isYes() && event.getPlayer().getLibrary().size() >= 2) {
                 game.doAction(new MagicMillLibraryAction(event.getPlayer(),2));
             } else {
                 game.doAction(new MagicSacrificeAction(event.getPermanent()));
             }
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.PumpFlash),
-        "+Abilities"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{U}")
-            ];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "SN gains shroud until end of turn and doesn't untap during your next untap step. Tap SN."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicGainAbilityAction(event.getPermanent(),MagicAbility.Shroud));
-            game.doAction(new MagicTapAction(event.getPermanent()));
-		game.doAction(MagicChangeStateAction.Set(
-                event.getPermanent(),
-                MagicPermanentState.DoesNotUntapDuringNext
-                ));
         }
     }
 ]
