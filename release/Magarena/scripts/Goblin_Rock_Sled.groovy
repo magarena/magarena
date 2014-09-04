551fe2da53af719d20f472ca1091499b3050a500
melvin <melvinzhang@gmail.com>
2014-09-04 10:50:35 +0800

    normalize card script

--- release/Magarena/scripts/Goblin_Rock_Sled.groovy
+++ release/Magarena/scripts/Goblin_Rock_Sled.groovy
@@ -2,19 +2,11 @@
     new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent creature) {
-            return new MagicEvent(
-                permanent,
-                this,
-                "SN doesn't untap during your next untap step."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(MagicChangeStateAction.Set(
                 event.getPermanent(),
                 MagicPermanentState.DoesNotUntapDuringNext
-                ));
+            ));
+            return MagicEvent.NONE;
         }
     }
 ]
