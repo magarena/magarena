0e20074dbe26b5ff9cc8bdd5027752189b4c6997
melvin <melvinzhang@gmail.com>
2014-09-04 10:58:49 +0800

    add MagicWhenSelfDamagePlayerTrigger

--- src/magic/model/trigger/MagicWhenSelfDamagePlayerTrigger.java
+++ src/magic/model/trigger/MagicWhenSelfDamagePlayerTrigger.java
@@ -3,13 +3,12 @@ package magic.model.trigger;
 import magic.model.MagicDamage;
 import magic.model.MagicPermanent;
 
-public abstract class MagicWhenSelfCombatDamagePlayerTrigger extends MagicWhenDamageIsDealtTrigger {
-    public MagicWhenSelfCombatDamagePlayerTrigger() {}
+public abstract class MagicWhenSelfDamagePlayerTrigger extends MagicWhenDamageIsDealtTrigger {
+    public MagicWhenSelfDamagePlayerTrigger() {}
     
     public boolean accept(final MagicPermanent permanent, final MagicDamage damage) {
         return super.accept(permanent, damage) && 
                damage.isSource(permanent) && 
-               damage.isCombat() && 
                damage.isTargetPlayer();
     }
 }
