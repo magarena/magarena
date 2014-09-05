e50d7f212feed41a057e0413b59fe2643992aba4
PalladiaMors <unknown>
2014-09-05 02:04:30 +0200

    Add script for Rakdos Ickspitter

---
+++ release/Magarena/scripts/Rakdos_Ickspitter.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                new MagicDamageTargetPicker(1),
+                this,
+                "SN deals 1 damage to target creature\$. Its controller loses 1 life."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,1));
+                game.doAction(new MagicChangeLifeAction(it.getController(),-1));
+            });
+        }
+    }
+]
