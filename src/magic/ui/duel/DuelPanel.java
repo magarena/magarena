e8d282be2b1946392bb96fa2a01b063faac36ec7
Lodici <Lodici>
2014-09-05 11:59:24 +0100

    simplify DuelPanel by picking apart the code for Image and Text mode into separate BattlefieldPanel classes. Also moved out the sidebar code into separate DuelSideBarPanel class.

--- src/magic/ui/duel/DuelPanel.java
+++ src/magic/ui/duel/DuelPanel.java
@@ -1,45 +1,28 @@
 package magic.ui.duel;
 
+import magic.ui.duel.animation.PlayCardAnimation;
 import java.awt.Dimension;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.AbstractAction;
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import magic.data.GeneralConfig;
-import magic.model.MagicCardDefinition;
 import magic.model.MagicCardList;
 import magic.model.MagicGame;
-import magic.model.MagicPlayer;
 import magic.model.event.MagicEvent;
 import magic.ui.GameController;
 import magic.ui.MagicFrame;
 import magic.ui.duel.animation.GamePlayAnimator;
-import magic.ui.duel.animation.PlayCardAnimation;
 import magic.ui.duel.resolution.DefaultResolutionProfile;
 import magic.ui.duel.resolution.ResolutionProfileResult;
-import magic.ui.duel.resolution.ResolutionProfileType;
 import magic.ui.duel.resolution.ResolutionProfiles;
-import magic.ui.duel.viewer.BattlefieldViewer;
 import magic.ui.duel.viewer.CardViewer;
-import magic.ui.duel.viewer.GameDuelViewer;
-import magic.ui.duel.viewer.HandGraveyardExileViewer;
-import magic.ui.duel.viewer.ImageBattlefieldViewer;
-import magic.ui.duel.viewer.ImageCardListViewer;
-import magic.ui.duel.viewer.ImageCombatViewer;
-import magic.ui.duel.viewer.ImageHandGraveyardExileViewer;
 import magic.ui.duel.viewer.LogBookViewer;
-import magic.ui.duel.viewer.LogStackViewer;
-import magic.ui.duel.viewer.PlayerViewer;
-import magic.ui.duel.viewer.StackCombatViewer;
-import magic.ui.duel.viewer.StackViewer;
-import magic.ui.widget.FontsAndBorders;
 import magic.ui.widget.ZoneBackgroundLabel;
 import net.miginfocom.swing.MigLayout;
 
@@ -53,87 +36,56 @@ public final class DuelPanel extends JPanel {
     private static final String PASS_KEY="pass";
 
     private final MagicFrame frame;
-    private final MagicGame game;
     private final ZoneBackgroundLabel backgroundLabel;
     private final GameController controller;
-    private final PlayerViewer playerViewer;
-    private final PlayerViewer opponentViewer;
-    private final GameDuelViewer gameDuelViewer;
-    private final LogBookViewer logBookViewer;
-    private final StackCombatViewer stackCombatViewer;
-    private final HandGraveyardExileViewer handGraveyardViewer;
-    private final BattlefieldViewer playerPermanentViewer;
-    private final BattlefieldViewer opponentPermanentViewer;
+  
     private final CardViewer imageCardViewer;
-    private final StackViewer imageStackViewer;
-    private final ImageHandGraveyardExileViewer imageHandGraveyardViewer;
-    private final ImageBattlefieldViewer imagePlayerPermanentViewer;
-    private final ImageBattlefieldViewer imageOpponentPermanentViewer;
-    private final ImageCombatViewer imageCombatViewer;
-    private final JPanel lhsPanel, rhsPanel;
+
+    private final DuelSideBarPanel sidebarPanel;
+    private BattlefieldPanel battlefieldPanel;
+    private final BattlefieldPanel textView;
+    private final BattlefieldPanel imageView;
 
     private ResolutionProfileResult result;
-    private final LogStackViewer logStackViewer;
 
     private final GamePlayAnimator animator;
-    private PlayCardAnimation animationEvent = null;
 
     public DuelPanel(
             final MagicFrame frame,
             final MagicGame game,
             final ZoneBackgroundLabel backgroundLabel) {
 
-        this.frame=frame;
-        this.game=game;
-        this.backgroundLabel=backgroundLabel;
+        this.frame = frame;
+        this.backgroundLabel = backgroundLabel;
         
         controller = new GameController(this, game);
-
         animator = new GamePlayAnimator(frame, this);
 
         setOpaque(false);
         setFocusable(true);
 
-        // One-time creation of UI panels.
-        lhsPanel = new JPanel();
-        lhsPanel.setOpaque(false);
-        rhsPanel = new JPanel(null);
-        rhsPanel.setOpaque(false);
-
-        logBookViewer=new LogBookViewer(game.getLogBook());
-        logBookViewer.setVisible(!CONFIG.isLogViewerDisabled());
-
-        imageCardViewer=new CardViewer(true);
+        imageCardViewer = new CardViewer(true);
         imageCardViewer.setSize(CONFIG.getMaxCardImageSize());
         imageCardViewer.setVisible(false);
         controller.setImageCardViewer(imageCardViewer);
 
-        playerViewer=new PlayerViewer(controller,false);
-        opponentViewer=new PlayerViewer(controller,true);
+        textView = new TextModeBattlefieldPanel(controller);
+        imageView = new ImageModeBattlefieldPanel(controller);
+        battlefieldPanel = isTextView() ? textView : imageView;
 
-        gameDuelViewer=new GameDuelViewer(controller);
-        gameDuelViewer.setBackground(FontsAndBorders.TRANSLUCENT_WHITE_STRONG);
+        sidebarPanel = new DuelSideBarPanel(controller, battlefieldPanel.getStackViewer());
 
-        controller.setGameViewer(gameDuelViewer.getGameViewer());
-
-        createActionMaps();
-        createShortcutKeys();
-
-        stackCombatViewer=new StackCombatViewer(controller);
-        handGraveyardViewer=new HandGraveyardExileViewer(controller);
-        playerPermanentViewer=new BattlefieldViewer(controller,false);
-        opponentPermanentViewer=new BattlefieldViewer(controller,true);
-        imageStackViewer=new StackViewer(controller,true);
-        logStackViewer = new LogStackViewer(logBookViewer, imageStackViewer);
-        logStackViewer.setBackground(FontsAndBorders.TRANSLUCENT_WHITE_STRONG);
-
-        imageHandGraveyardViewer=new ImageHandGraveyardExileViewer(controller);
-        imagePlayerPermanentViewer=new ImageBattlefieldViewer(controller,false);
-        imageOpponentPermanentViewer=new ImageBattlefieldViewer(controller,true);
-        imageCombatViewer=new ImageCombatViewer(controller);
+        controller.setGameViewer(sidebarPanel.getGameDuelViewer().getGameViewer());
 
         updateView();
+        
+        createActionMaps();
+        createShortcutKeys();
+        createMouseListener();
 
+    }
+
+    private void createMouseListener() {
         //hide info when mouse moves onto background
         backgroundLabel.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
@@ -141,7 +93,6 @@ public final class DuelPanel extends JPanel {
                 controller.hideInfo();
             }
         });
-
     }
 
     public void startGameThread() {
@@ -177,59 +128,42 @@ public final class DuelPanel extends JPanel {
     }
 
     private void createActionMaps() {
-
         getActionMap().put(ACTION_KEY, new AbstractAction() {
-            private static final long serialVersionUID = 1L;
             @Override
             public void actionPerformed(final ActionEvent event) {
                 controller.actionKeyPressed();
             }
         });
-
         getActionMap().put(UNDO_KEY, new AbstractAction() {
-            private static final long serialVersionUID = 1L;
             @Override
             public void actionPerformed(final ActionEvent event) {
                 controller.undoKeyPressed();
             }
         });
-
         getActionMap().put(SWITCH_KEY, new AbstractAction() {
-            private static final long serialVersionUID = 1L;
             @Override
             public void actionPerformed(final ActionEvent e) {
-                switchKeyPressed();
+                controller.switchKeyPressed();
             }
         });
-
         getActionMap().put(PASS_KEY, new AbstractAction() {
-            private static final long serialVersionUID = 1L;
             @Override
             public void actionPerformed(final ActionEvent e) {
                 controller.passKeyPressed();
             }
         });
-
     }
 
     public boolean canClickAction() {
-        return gameDuelViewer.getGameViewer().isActionEnabled();
+        return sidebarPanel.getGameDuelViewer().getGameViewer().isActionEnabled();
     }
 
     public boolean canClickUndo() {
-        return gameDuelViewer.getGameViewer().isUndoEnabled();
-    }
-
-    private void switchKeyPressed() {
-        controller.switchKeyPressed();
+        return sidebarPanel.getGameDuelViewer().getGameViewer().isUndoEnabled();
     }
 
     private static boolean isTextView() {
         return CONFIG.getTextView();
-    }
-
-    public MagicGame getGame() {
-        return game;
     }
 
     public GameController getController() {
@@ -241,72 +175,26 @@ public final class DuelPanel extends JPanel {
     }
 
     public LogBookViewer getLogBookViewer() {
-        return logBookViewer;
+        return sidebarPanel.getLogBookViewer();
     }
 
     public void focusViewers(final int handGraveyard) {
-        if (isTextView()) {
-            handGraveyardViewer.setSelectedTab(handGraveyard);
-        } else {
-            imageHandGraveyardViewer.setSelectedTab(handGraveyard);
-        }
+        battlefieldPanel.focusViewers(handGraveyard);
     }
 
     public void showCards(final MagicCardList cards) {
-        if (isTextView()) {
-            handGraveyardViewer.showCards(cards);
-            handGraveyardViewer.setSelectedTab(5);
-        } else {
-            imageHandGraveyardViewer.showCards(cards);
-            imageHandGraveyardViewer.setSelectedTab(5);
-        }
+        battlefieldPanel.showCards(cards);
     }
 
     public void update() {
         assert SwingUtilities.isEventDispatchThread();
-
-        playerViewer.update();
-        opponentViewer.update();
-        gameDuelViewer.update();
-        imageStackViewer.update();
-
-        if (isTextView()) {
-            handGraveyardViewer.update();
-            stackCombatViewer.update();
-            playerPermanentViewer.update();
-            opponentPermanentViewer.update();
-        } else {
-            imageHandGraveyardViewer.update();
-            imagePlayerPermanentViewer.update();
-            imageOpponentPermanentViewer.update();
-            imageCombatViewer.update();
-        }
+        sidebarPanel.doUpdate();
+        battlefieldPanel.doUpdate();
     }
 
     public void updateView() {
-        if (isTextView()) {
-            backgroundLabel.setImage(false);
-            rhsPanel.remove(imageHandGraveyardViewer);
-            rhsPanel.remove(imagePlayerPermanentViewer);
-            rhsPanel.remove(imageOpponentPermanentViewer);
-            rhsPanel.remove(imageCombatViewer);
-            rhsPanel.add(handGraveyardViewer);
-            rhsPanel.add(stackCombatViewer);
-            rhsPanel.add(playerPermanentViewer);
-            rhsPanel.add(opponentPermanentViewer);
-            imageCardViewer.setVisible(false);
-        } else if (imageHandGraveyardViewer!=null) {
-            backgroundLabel.setImage(true);
-            rhsPanel.remove(handGraveyardViewer);
-            rhsPanel.remove(stackCombatViewer);
-            rhsPanel.remove(playerPermanentViewer);
-            rhsPanel.remove(opponentPermanentViewer);
-            rhsPanel.add(imageStackViewer);
-            rhsPanel.add(imageHandGraveyardViewer);
-            rhsPanel.add(imagePlayerPermanentViewer);
-            rhsPanel.add(imageOpponentPermanentViewer);
-            rhsPanel.add(imageCombatViewer);
-        }
+        backgroundLabel.setImage(!isTextView());
+        battlefieldPanel = isTextView() ? textView : imageView;
         resizeComponents();
         revalidate();
         repaint();
@@ -317,31 +205,12 @@ public final class DuelPanel extends JPanel {
     }
 
     public void resizeComponents() {
-
-        final Dimension size=getSize();
+        final Dimension size = getSize();
         result = ResolutionProfiles.calculate(size);
-
-        backgroundLabel.setZones(result);
-
-        playerViewer.setBounds(result.getBoundary(ResolutionProfileType.GamePlayerViewer));
-        playerViewer.setSmall(result.getFlag(ResolutionProfileType.GamePlayerViewerSmall));
-        opponentViewer.setSmall(result.getFlag(ResolutionProfileType.GamePlayerViewerSmall));
-        gameDuelViewer.setBounds(result.getBoundary(ResolutionProfileType.GameDuelViewer));
-
-        if (isTextView()) {
-            stackCombatViewer.setBounds(result.getBoundary(ResolutionProfileType.GameStackCombatViewer));
-            handGraveyardViewer.setBounds(result.getBoundary(ResolutionProfileType.GameHandGraveyardViewer));
-            playerPermanentViewer.setBounds(result.getBoundary(ResolutionProfileType.GamePlayerPermanentViewer));
-            opponentPermanentViewer.setBounds(result.getBoundary(ResolutionProfileType.GameOpponentPermanentViewer));
-        } else {
-            imageHandGraveyardViewer.setBounds(result.getBoundary(ResolutionProfileType.GameImageHandGraveyardViewer));
-            imagePlayerPermanentViewer.setBounds(result.getBoundary(ResolutionProfileType.GameImagePlayerPermanentViewer));
-            imageOpponentPermanentViewer.setBounds(result.getBoundary(ResolutionProfileType.GameImageOpponentPermanentViewer));
-            imageCombatViewer.setBounds(result.getBoundary(ResolutionProfileType.GameImageCombatViewer));
-        }
-
+        backgroundLabel.setZones(result);                
+        sidebarPanel.resizeComponents(result);
+        battlefieldPanel.resizeComponents(result);
         setGamePanelLayout();
-
         // defer until all pending events on the EDT have been processed.
         // this ensures that cards layout is adjusted correctly if the screen is resized.
         SwingUtilities.invokeLater(new Runnable() {
@@ -350,7 +219,6 @@ public final class DuelPanel extends JPanel {
                 update();
             }
         });
-
     }
 
     /**
@@ -359,37 +227,11 @@ public final class DuelPanel extends JPanel {
      * The RHS column represents the battlefield and uses absolute positioning.
      */
     private void setGamePanelLayout() {
-
         removeAll();
         setLayout(new MigLayout("insets 0, gap 0, flowx"));
-
-        add(lhsPanel, "w " + DefaultResolutionProfile.getPanelWidthLHS() +"!, h 100%");
-        add(rhsPanel, "w 100%, h 100%");
-
-        setLeftSideLayout();
-
-    }
-
-    /**
-     *  The LHS of the game screen contains the two player info views, the Log/Stack
-     *  viewer and the game console. All are at a fixed height except the Log/Stack view
-     *  which should expand and contract to fill any remaining space depending on screen height.
-     */
-    private void setLeftSideLayout() {
-
-        final int insets = 6;
-        final int maxWidth = DefaultResolutionProfile.getPanelWidthLHS() - (insets * 2);
-
-        lhsPanel.removeAll();
-        lhsPanel.setLayout(new MigLayout("insets " + insets + ", gap 0 10, flowy"));
-
-        lhsPanel.add(opponentViewer, "w " + maxWidth + "!, h " + DefaultResolutionProfile.PLAYER_VIEWER_HEIGHT_SMALL + "!");
-        lhsPanel.add(logStackViewer, "w " + maxWidth + "!, h 100%");
-        lhsPanel.add(gameDuelViewer, "w " + maxWidth + "!, h " + DefaultResolutionProfile.GAME_VIEWER_HEIGHT + "!");
-        lhsPanel.add(playerViewer,   "w " + maxWidth + "!, h " + DefaultResolutionProfile.PLAYER_VIEWER_HEIGHT_SMALL + "!");
-
-        logStackViewer.setLogStackLayout();
-
+        add(sidebarPanel, "w " + DefaultResolutionProfile.getPanelWidthLHS() +"!, h 100%");
+        add(battlefieldPanel, "w 100%, h 100%");
+        sidebarPanel.doSetLayout();
     }
 
     /**
@@ -399,11 +241,11 @@ public final class DuelPanel extends JPanel {
      */
     public void runAnimation() {
         assert !SwingUtilities.isEventDispatchThread();
-
+        final PlayCardAnimation animationEvent = battlefieldPanel.getPlayCardFromHandAnimation();
         if (animationEvent != null && CONFIG.isAnimateGameplay()) {
             animator.runAnimation(animationEvent);
         }
-        animationEvent = null;
+        battlefieldPanel.setPlayCardFromHandAnimation(null);
     }
 
     /**
@@ -411,46 +253,7 @@ public final class DuelPanel extends JPanel {
      * to the battlefield or stack the next time GamePanel is refreshed.
      */
     public void setAnimationEvent(final MagicEvent event) {
-        final MagicCardDefinition card = event.getSource().getCardDefinition();
-        final MagicPlayer player = event.getPlayer();
-        animationEvent = new PlayCardAnimation(player, card, this);
-        setAnimationStartPoint(player, card);
-        if (card.usesStack()) {
-            animationEvent.setEndPoint(new Point(150, imageStackViewer.getLocation().y));
-        } else {
-            if (player.getIndex() == 0) {
-                animationEvent.setEndPoint(getLocationOnGamePanel(imagePlayerPermanentViewer));
-            } else {
-                animationEvent.setEndPoint(getLocationOnGamePanel(imageOpponentPermanentViewer));
-            }
-        }
-    }
-
-    /**
-     * If player Hand is visible then start animation from position of the card
-     * in the Hand otherwise start animation from the Hand icon next to player portrait.
-     */
-    private void setAnimationStartPoint(final MagicPlayer player, final MagicCardDefinition card) {
-        if (isPlayerHandVisible(player)) {
-            final ImageCardListViewer handViewer = imageHandGraveyardViewer.getCardListViewer();
-            final Point startPoint = handViewer.getCardPosition(card);
-            animationEvent.setStartSize(handViewer.getCardSize());
-            animationEvent.setStartPoint(startPoint);
-        }
-    }
-
-    /**
-     * TODO: checking the player index to determine if the Hand is visible is
-     * not really desirable but it works. The index is a bad code smell. It is
-     * used to position the player on screen and determine whether a player
-     * is human or AI (except for an AI v AI game). It seems very arbitrary.
-     */
-    private boolean isPlayerHandVisible(final MagicPlayer player) {
-        return player.getIndex() == 0;
-    }
-
-    private Point getLocationOnGamePanel(final JComponent component) {
-        return SwingUtilities.convertPoint(component.getParent(), component.getLocation(), this);
+        battlefieldPanel.setAnimationEvent(event, this);
     }
 
     private void doThreadSleep(final long msecs) {
@@ -468,7 +271,7 @@ public final class DuelPanel extends JPanel {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                gameDuelViewer.showNewTurnNotification(game);
+                sidebarPanel.getGameDuelViewer().showNewTurnNotification(game);
             }
         });
 
@@ -478,9 +281,10 @@ public final class DuelPanel extends JPanel {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                gameDuelViewer.hideNewTurnNotification();
+                sidebarPanel.getGameDuelViewer().hideNewTurnNotification();
             }
         });
 
     }
+
 }
