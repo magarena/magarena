69bffeabd5c0b00111e575ac8e34ec0b25337379
PalladiaMors <unknown>
2014-09-06 15:24:17 +0200

    Add script for Gigantomancer

---
+++ release/Magarena/scripts/Gigantomancer.groovy
@@ -0,0 +1,37 @@
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(7,7);
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source,"{1}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                new MagicBecomeTargetPicker(7,7,false),
+                this,
+                "Target creature\$ you control becomes 7/7 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicBecomesCreatureAction(it,PT));
+            });
+        }
+    }
+]
