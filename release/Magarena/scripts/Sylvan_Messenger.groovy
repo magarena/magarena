ee5c0d7e1318c2025b8cf8df2cb95b85b69cebd8
PalladiaMors <unknown>
2014-09-07 23:28:21 +0200

    Add script for Sylvan Messenger

---
+++ release/Magarena/scripts/Sylvan_Messenger.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Reveal the top four cards of your library. Put all Elf cards revealed this way into your hand and the rest on the bottom of your library in any order."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCardList library = event.getPlayer().getLibrary();
+            for (int i = 0; i < 4 && library.isEmpty() == false; i++) {
+                final MagicCard top = library.getCardAtTop();
+                game.doAction(new MagicRemoveCardAction(
+                    top,
+                    MagicLocationType.OwnersLibrary
+                ));
+                game.doAction(new MagicMoveCardAction(
+                    top,
+                    MagicLocationType.OwnersLibrary,
+                    top.hasSubType(MagicSubType.Elf) ?
+                      MagicLocationType.OwnersHand :
+                      MagicLocationType.BottomOfOwnersLibrary
+                ));
+            }
+        }
+    }
+]
