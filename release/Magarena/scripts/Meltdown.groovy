d1c114174f77e4b9faadfcafc52650136ecf91a9
PalladiaMors <unknown>
2014-09-08 18:54:31 +0200

    Add script for Meltdown

---
+++ release/Magarena/scripts/Meltdown.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            final int amount=payedCost.getX();
+            return new MagicEvent(
+                cardOnStack,
+                amount,
+                this,
+                "Destroy all artifacts with converted mana cost RN or less."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount=event.getRefInt();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(
+                    event.getSource().getController(),
+                    new MagicCMCPermanentFilter(
+                        MagicTargetFilterFactory.ARTIFACT,
+                        Operator.LESS_THAN_OR_EQUAL,
+                        amount
+                    )
+                );
+            game.doAction(new MagicDestroyAction(targets));
+        }
+    }
+]
