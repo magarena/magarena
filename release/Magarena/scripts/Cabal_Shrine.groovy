163b565be138797876d057bc7e46f2821b5d8e89
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-09 21:41:07 +0100

    add Cabal Shrine

---
+++ release/Magarena/scripts/Cabal_Shrine.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return new MagicEvent(
+                permanent,
+				cardOnStack,
+                this,
+                ""+cardOnStack.getController().toString()+" discards X cards, where X is the number of cards in all graveyards with the same name as RN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+			final MagicCardOnStack card = event.getRefCardOnStack();
+			final MagicPlayer player = card.getController();
+			final String name = card.getCard().getName();
+			final int graveyard = game.filterCards(player,MagicTargetFilterFactory.cardName(name).from(MagicTargetType.Graveyard)).size();
+			final int oppGraveyard = game.filterCards(player,MagicTargetFilterFactory.cardName(name).from(MagicTargetType.OpponentsGraveyard)).size();
+			final int amount = graveyard + oppGraveyard;
+			game.logAppendMessage(event.getPermanent().getController(),"(X="+amount+")")
+			if (amount > 0) {
+				game.addEvent(new MagicDiscardEvent(event.getSource(),player,amount));
+			}
+        }
+    }
+]
