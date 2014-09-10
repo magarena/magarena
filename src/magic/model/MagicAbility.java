1c29bb2ca69de8fd26f0b31f1443ba794900761a
melvin <melvinzhang@gmail.com>
2014-09-10 09:59:55 +0800

    move static factory method into MagicWhenAttacksTrigger

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -213,7 +213,7 @@ public enum MagicAbility {
     },
     AttacksAnyEffect("Whenever a creature attacks, " + ARG.EFFECT, 10) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            card.add(MagicWhenAnyAttacksTrigger.create(
+            card.add(MagicWhenAttacksTrigger.create(
                 MagicRuleEventAction.create(ARG.effect(arg))
             ));
         }
--- src/magic/model/trigger/MagicWhenAttacksTrigger.java
+++ src/magic/model/trigger/MagicWhenAttacksTrigger.java
@@ -1,6 +1,9 @@
 package magic.model.trigger;
 
+import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.event.MagicEvent;
+import magic.model.event.MagicSourceEvent;
 
 public abstract class MagicWhenAttacksTrigger extends MagicTrigger<MagicPermanent> {
     public MagicWhenAttacksTrigger(final int priority) {
@@ -13,4 +16,12 @@ public abstract class MagicWhenAttacksTrigger extends MagicTrigger<MagicPermanen
         return MagicTriggerType.WhenAttacks;
     }
     
+    public static final MagicWhenAttacksTrigger create(final MagicSourceEvent sourceEvent) {
+        return new MagicWhenAttacksTrigger() {
+            @Override
+            public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent attacker) {
+                return sourceEvent.getEvent(permanent);
+            }
+        };
+    }
 }
