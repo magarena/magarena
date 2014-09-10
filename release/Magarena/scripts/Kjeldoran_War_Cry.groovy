b049258de95912a9f5ca2948bf902f1cf0988549
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-10 06:05:36 +0100

    add Kjeldoran War Cry

---
+++ release/Magarena/scripts/Kjeldoran_War_Cry.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Creatures PN controls get +X/+X until end of turn, " +
+                "where X is 1 plus the number of cards named Kjeldoran War Cry in all graveyards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = game.filterCards(
+                MagicTargetFilterFactory.cardName("Kjeldoran War Cry")
+                .from(MagicTargetType.Graveyard)
+                .from(MagicTargetType.OpponentsGraveyard)
+            ).size()+1;
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+            );
+            for (final MagicPermanent creature : targets) {
+                game.doAction(new MagicChangeTurnPTAction(creature,amount,amount));
+            }
+        }
+    }
+]
