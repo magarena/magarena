5e5d51c2d2c24b5e2f70b7240ba18462acfda9aa
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-10 04:38:47 +0100

    add Wake of Destruction

---
+++ release/Magarena/scripts/Wake_of_Destruction.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_LAND,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target land\$ and all other lands " +
+                "with the same name as that land."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicTargetFilter<MagicPermanent> targetFilter = new MagicNameTargetFilter(
+                    MagicTargetFilterFactory.LAND,
+                    it.getName()
+                );
+                final Collection<MagicPermanent> targets = game.filterPermanents(event.getPlayer(),targetFilter);
+                for (final MagicPermanent permanent : targets) {
+                    game.doAction(new MagicDestroyAction(permanent));
+                }
+            });
+        }
+    }
+]
