adcb125f480d060a5f4dead809def2d2187e10ea
PalladiaMors <unknown>
2014-09-10 02:20:19 +0200

    Add script for Hunger of the Nim

---
+++ release/Magarena/scripts/Hunger_of_the_Nim.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),         
+	        this,
+                "Target creature\$ gets +1/+0 until end of turn for each artifact you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int amount = event.getPlayer().getNrOfPermanents(MagicType.Artifact);
+                game.doAction(new MagicChangeTurnPTAction(it, amount, 0));
+            });
+        }
+    }
+]
