2bb65e1648ae40bb472b5329faf8c993ae48b9e5
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-10 06:06:39 +0100

    add Rune Snag

---
+++ release/Magarena/scripts/Rune_Snag.groovy
@@ -0,0 +1,30 @@
+[
+   new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {2} "+
+                "plus an additional {2} for each card named Rune Snag in each graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = (game.filterCards(
+                MagicTargetFilterFactory.cardName("Rune Snag")
+                .from(MagicTargetType.Graveyard)
+                .from(MagicTargetType.OpponentsGraveyard)
+            ).size()*2)+2;
+            event.processTargetCardOnStack(game, {
+                game.logAppendMessage(event.getPlayer()," (X="+amount+")");
+                game.addEvent(new MagicCounterUnlessEvent(
+                    event.getSource(),
+                    it,
+                    MagicManaCost.create("{"+amount+"}")
+                ));
+            });
+        }
+    }
+]
