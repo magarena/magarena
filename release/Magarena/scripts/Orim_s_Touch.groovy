567e559159c5284664b0a34aa2b267996e3c391d
PalladiaMors <unknown>
2014-09-11 15:58:48 +0200

    Add script for Orim's Touch

---
+++ release/Magarena/scripts/Orim_s_Touch.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER,
+                MagicPreventTargetPicker.create(),
+                this,
+                "Prevent the next 2 damage that would be dealt to target creature or player\$ this turn. "+
+                "If SN was kicked, prevent the next 4 damage instead."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                final int amount = event.isKicked() ? 4 : 2;
+                game.doAction(new MagicPreventDamageAction(it,amount));
+            });
+        }
+    }
+]
