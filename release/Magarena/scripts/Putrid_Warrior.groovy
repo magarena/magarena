4d6f4b95dc67774ffb39ce10101f9699b46ec9b6
PalladiaMors <unknown>
2014-09-11 05:39:40 +0200

    Add script for Putrid Warrior

---
+++ release/Magarena/scripts/Putrid_Warrior.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+	    return permanent == damage.getSource() ?
+            new MagicEvent(
+                permanent,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicChoice.NONE
+                ),
+                damage.getTarget(),
+                this,
+                "Choose one\$ - Each player loses 1 life; or each player gains 1 life."
+            ):
+		MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                for (final MagicPlayer player : game.getAPNAP()) {
+                game.doAction(new MagicChangeLifeAction(player,-1));
+		}
+            } else if (event.isMode(2)) {
+                for (final MagicPlayer player : game.getAPNAP()) {
+                game.doAction(new MagicChangeLifeAction(player,1));
+		}
+            }
+        }
+    }
+]
