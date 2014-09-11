39c24b2e48e927cb8e6783a7e4f3b4201d85d33d
PalladiaMors <unknown>
2014-09-11 15:43:35 +0200

    Add script for Hypnotic Cloud

---
+++ release/Magarena/scripts/Hypnotic_Cloud.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ discards a card. " +
+                "If SN was kicked, that player discards 3 cards instead."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                int amount = event.isKicked() ? 3 : 1;
+                game.addEvent(new MagicDiscardEvent(event.getSource(), it, amount));
+            });
+        }
+    }
+]
