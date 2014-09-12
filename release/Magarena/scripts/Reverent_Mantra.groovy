584b3af8e80c91eb5d1cba102fb104d3a451bf84
PalladiaMors <unknown>
2014-09-13 01:21:46 +0200

    Add script for Reverent Mantra

---
+++ release/Magarena/scripts/Reverent_Mantra.groovy
@@ -0,0 +1,42 @@
+[
+    new MagicCardActivation(
+        [MagicCondition.CARD_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump,true),
+        "Alt"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+            final MagicTargetChoice targetChoice = new MagicTargetChoice(
+                new MagicOtherCardTargetFilter(
+                    MagicTargetFilterFactory.WHITE_CARD_FROM_HAND, 
+                    source
+                ),
+                MagicTargetHint.None,
+                "a white card from your hand"
+            );
+            return [new MagicExileCardEvent(source, targetChoice)];
+        }
+    },
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicColorChoice.ALL_INSTANCE,
+                this,
+                "Choose a color\$. " +
+                "All creatures gain protection from the chosen color until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicColor color = event.getChosenColor();
+            final MagicAbility protection = color.getProtectionAbility();
+            final Collection<MagicPermanent> targets =
+                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicGainAbilityAction(target, protection));
+            }
+        }
+    }
+]
