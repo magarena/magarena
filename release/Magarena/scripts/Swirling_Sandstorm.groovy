89d53b70bda217b454cdd930162bde09c2dfa185
PalladiaMors <unknown>
2014-09-14 00:59:53 +0200

    Add script for Swirling Sandstorm

---
+++ release/Magarena/scripts/Swirling_Sandstorm.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "SN deals 5 damage to each creature without flying if you have 7 or more cards in your graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+                if (MagicCondition.THRESHOLD_CONDITION.accept(event.getSource())) {
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE_WITHOUT_FLYING);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicDealDamageAction(event.getSource(),target,5));
+        }
+            }
+        }
+    }
+]
