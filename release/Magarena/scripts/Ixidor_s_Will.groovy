5b3bc47f4b2a88cca249e8005ee8050bf7b92fae
PalladiaMors <unknown>
2014-09-14 03:23:58 +0200

    Add script for Ixidor's Will

---
+++ release/Magarena/scripts/Ixidor_s_Will.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {2} for each Wizard on the battlefield. "
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                final int amount = game.getNrOfPermanents(MagicSubType.Wizard) * 2;
+                game.addEvent(new MagicCounterUnlessEvent(event.getSource(),it,MagicManaCost.create("{"+amount+"}")));
+            });
+        }
+    }
+]
