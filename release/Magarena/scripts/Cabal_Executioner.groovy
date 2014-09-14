4a1afb444b7dd139f7ca2eb551429e9c040ac0de
PalladiaMors <unknown>
2014-09-14 02:35:39 +0200

    Add script for Cabal Executioner

---
+++ release/Magarena/scripts/Cabal_Executioner.groovy
@@ -0,0 +1,17 @@
+[
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                this,
+                "PN sacrifices a creature."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicSacrificePermanentEvent(event.getSource(),event.getPlayer(),MagicTargetChoice.CREATURE_YOU_CONTROL));
+        }
+    }
+]
