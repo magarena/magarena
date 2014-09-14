ad3c70dc29f7503f6050a3cd43af6051974deae2
PalladiaMors <unknown>
2014-09-14 14:48:50 +0200

    Add script for In the Eye of Chaos

---
+++ release/Magarena/scripts/In_the_Eye_of_Chaos.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
+            return (cardOnStack.hasType(MagicType.Instant)) ?
+                new MagicEvent(
+                    permanent,
+	            cardOnStack,
+                    this,
+                    "Whenever a player casts an instant spell, counter it unless that player pays {X}, where X is its converted mana cost."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+                final int amount = event.getRefCardOnStack().getConvertedCost();
+                game.addEvent(new MagicCounterUnlessEvent(event.getSource(),event.getRefCardOnStack(),MagicManaCost.create("{"+amount+"}")));
+        }
+    }
+]
