d184062635e4fd64ee12b5531b693e415c2d5468
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-17 00:40:29 +0100

    fix Unwinding Clock

--- release/Magarena/scripts/Unwinding_Clock.groovy
+++ release/Magarena/scripts/Unwinding_Clock.groovy
@@ -4,8 +4,8 @@
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
             if (permanent.isOpponent(upkeepPlayer)) {
                 final Collection<MagicPermanent> targets = game.filterPermanents(permanent.getController(),MagicTargetFilterFactory.ARTIFACT_YOU_CONTROL);
-                for (final MagicPermanent permanent : targets) {
-                    game.doAction(new MagicUntapAction(permanent));
+                for (final MagicPermanent artifact : targets) {
+                    game.doAction(new MagicUntapAction(artifact));
                 }
             }
             return MagicEvent.NONE;
