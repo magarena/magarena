e5095d86554970bcd728176984e2640ccaf55bcb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-17 12:53:04 +0100

    add Carnage Gladiator

---
+++ release/Magarena/scripts/Carnage_Gladiator.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenBlocksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
+            return new MagicEvent(
+                permanent,
+                blocker.getController(),
+                this,
+                "PN loses 1 life."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),-1));
+        }
+    }
+]
