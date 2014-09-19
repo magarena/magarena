c46fe7d419ba0a682631f54b89408ce57ba65388
PalladiaMors <unknown>
2014-09-19 22:01:08 +0200

    Add script for Imaginary Pet

---
+++ release/Magarena/scripts/Imaginary_Pet.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return upkeepPlayer.getHandSize() >= 1 ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "If PN has a card in hand, return SN to its owner's hand."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.getPlayer().getHandSize() >= 1) {
+                game.doAction(new MagicRemoveFromPlayAction(event.getPermanent(),MagicLocationType.OwnersHand));
+            }
+        }
+    }
+]
