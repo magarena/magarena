83f4f996e3c1d26eef2310168e454d2c46f53a0b
PalladiaMors <unknown>
2014-09-20 01:52:47 +0200

    Add script for Lurking Skirge

---
+++ release/Magarena/scripts/Lurking_Skirge.groovy
@@ -0,0 +1,42 @@
+def PT = new MagicStatic(MagicLayer.SetPT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(3,2);
+    }
+};
+def ST = new MagicStatic(MagicLayer.Type) {
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent,final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Imp);
+    }
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return MagicType.Creature.getMask();
+    }
+};
+def AB = new MagicStatic(MagicLayer.Ability) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        permanent.addAbility(MagicAbility.Flying, flags);
+    }
+};
+[
+    new MagicWhenOtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return (otherPermanent.isNonToken() &&
+                    otherPermanent.isCreature() && 
+                    otherPermanent.isEnemy(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "SN becomes a 3/2 Imp creature with flying."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,AB,ST));
+        }
+    }
+]
