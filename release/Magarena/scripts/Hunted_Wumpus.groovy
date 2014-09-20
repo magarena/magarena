c5ec6ca04cd44d996b9ceded156d7b17df93bec3
PalladiaMors <unknown>
2014-09-20 03:26:38 +0200

    Add script for Hunted Wumpus

---
+++ release/Magarena/scripts/Hunted_Wumpus.groovy
@@ -0,0 +1,24 @@
+def choice = new MagicTargetChoice("a creature card from your hand");
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                permanent.getOpponent(),
+                this,
+                "PN may put a creature card from his or her hand onto the battlefield."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+                game.addEvent(new MagicPutOntoBattlefieldEvent(
+                    event,
+                    new MagicMayChoice(choice)
+                ));
+
+        }
+    }
+]
