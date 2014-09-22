27b3cbc63d05af4afbfa62be2a94edc51910d81b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-22 22:26:03 +0100

    add End Hostilities

---
+++ release/Magarena/scripts/End_Hostilities.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Destroy all creatures and all permanents attached to creatures."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanentList perms = new MagicPermanentList();
+            final Collection<MagicPermanent> targets = game.filterPermanents(MagicTargetFilterFactory.CREATURE);
+            for (final MagicPermanent creature : targets) {
+                perms.add(creature);
+                perms.addAll(creature.getEquipmentPermanents());
+                perms.addAll(creature.getAuraPermanents());
+            }
+            game.doAction(new MagicDestroyAction(perms));
+        }
+    }
+]
