86a4917704f91c6da0873d9e58c9b1300deeae39
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-22 22:26:55 +0100

    add Hornet Nest

---
+++ release/Magarena/scripts/Hornet_Nest.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return (damage.getTarget() == permanent) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    damage.getDealtAmount(),
+                    this,
+                    "PN puts RN 1/1 green Insect creature tokens with flying and deathtouch onto the battlefield."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("1/1 green Insect creature token with flying and deathtouch"),
+                event.getRefInt()
+            ));
+        }
+    }
+]
