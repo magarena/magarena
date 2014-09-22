71c9d34b08953667004a2960963adc28f3b8094e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-22 22:23:19 +0100

    add Countermand

---
+++ release/Magarena/scripts/Countermand.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. Its controller puts the top four cards of his or her library "+
+                "into his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                game.doAction(new MagicCounterItemOnStackAction(it));
+                game.doAction(new MagicMillLibraryAction(it.getCard().getController(),4));
+            });
+        }
+    }
+]
