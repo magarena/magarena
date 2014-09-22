369ce07ee1e20b4146794265067b3a19eaff05b3
melvin <melvinzhang@gmail.com>
2014-09-22 11:31:26 +0800

    normalize card scripts

--- release/Magarena/scripts/Flame_Kin_War_Scout.groovy
+++ release/Magarena/scripts/Flame_Kin_War_Scout.groovy
@@ -2,13 +2,13 @@
     new MagicWhenOtherComesIntoPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (otherPermanent.hasColor(MagicColor.Black) == false &&
+            return (otherPermanent != permanent &&
                     otherPermanent.isCreature()) ?
                 new MagicEvent(
                     permanent,
                     otherPermanent,
                     this,
-                    "When a nonblack creature enters the battlefield, sacrifice SN. If you do, destroy that creature."
+                    "When another creature enters the battlefield, sacrifice Flame-Kin War Scout. If you do, Flame-Kin War Scout deals 4 damage to that creature."
                 ):
                 MagicEvent.NONE;
         }
@@ -18,7 +18,7 @@
             final MagicSacrificeAction sac = new MagicSacrificeAction(event.getPermanent());
             game.doAction(sac);
             if (sac.isValid()) {
-                game.doAction(new MagicDestroyAction(event.getRefPermanent()));
+                game.doAction(new MagicDealDamageAction(event.getPermanent(),event.getRefPermanent(),4));
             }
         }
     }
