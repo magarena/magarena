ae918187e9f9eb8f351fbc8e3894759a479c31bd
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-24 00:13:08 +0100

    add Undergrowth Scavenger

---
+++ release/Magarena/scripts/Undergrowth_Scavenger.groovy
@@ -0,0 +1,10 @@
+[
+    new MagicWhenComesIntoPlayTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            final int amount = game.filterCards(permanent.getController(), MagicTargetFilterFactory.CREATURE_CARD_FROM_ALL_GRAVEYARDS).size();
+            game.doAction(MagicChangeCountersAction.Enters(permanent,MagicCounterType.PlusOne,amount));
+            return MagicEvent.NONE;
+        }
+    }
+]
