5e8796d756b306c9af116ee5c65de6a7c14729d0
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-23 23:03:30 +0100

    add Balthor the Defiled

---
+++ release/Magarena/scripts/Balthor_the_Defiled.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Reanimate"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicExileEvent(source),
+                new MagicPayManaCostEvent(source,"{B}{B}{B}")
+
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Each player returns all black and all red creature cards from his or her graveyard to the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getAPNAP()) {
+                final Collection<MagicCard> targets = game.filterCards(
+                    player,
+                    MagicTargetFilterFactory.BLACK_OR_RED_CREATURE_CARD_FROM_GRAVEYARD
+                );
+                for (final MagicCard card : targets) {
+                    game.doAction(new MagicReanimateAction(card, player));
+                }
+            }
+        }
+    }
+]
