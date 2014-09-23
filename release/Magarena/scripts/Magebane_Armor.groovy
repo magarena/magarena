7866f933c11e4cf1b7f4f13a34b94d7c6ba6bb53
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-23 21:43:32 +0100

    add Magebane Armor

---
+++ release/Magarena/scripts/Magebane_Armor.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            flags.remove(MagicAbility.Flying);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    },
+    
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (!damage.isCombat() && damage.isTargetCreature()) {
+                if (damage.getTargetPermanent() == permanent.getEquippedCreature()) {
+                    // Prevention effect.
+                    damage.prevent();
+                }
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
