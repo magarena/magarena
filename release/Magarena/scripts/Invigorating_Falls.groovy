b0d0ed8eb7960f33377806f512cb07c02381c959
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-24 00:11:46 +0100

    add Invigorating Falls

---
+++ release/Magarena/scripts/Invigorating_Falls.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN gains life equal to the number of creature cards in all graveyards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = game.filterCards(player,MagicTargetFilterFactory.CREATURE_CARD_FROM_ALL_GRAVEYARDS).size();
+            game.doAction(new MagicChangeLifeAction(player,amount));
+            game.logAppendMessage(player," ("+amount+")");
+        }
+    }
+]
