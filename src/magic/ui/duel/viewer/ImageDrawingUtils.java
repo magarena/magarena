1f0f0fffaec4d2440d673632242075cd8f761dae
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-24 22:27:14 +0100

    add Credit counter image (duplicate bribe)

--- src/magic/ui/duel/viewer/ImageDrawingUtils.java
+++ src/magic/ui/duel/viewer/ImageDrawingUtils.java
@@ -305,6 +305,10 @@ public class ImageDrawingUtils {
                     g.drawImage(IconImages.GEMCOUNTER.getImage(),ax,ay,observer);
                     if (amount > 1){drawStringWithOutline(g, str, ax+6, ay+14, observer);}
                     ax+=inc;
+                } else if (counterType == MagicCounterType.Credit) {
+                    g.drawImage(IconImages.BRIBECOUNTER.getImage(),ax,ay,observer);
+                    if (amount > 1){drawStringWithOutline(g, str, ax+6, ay+14, observer);}
+                    ax+=inc;
                 }
             }
         }
