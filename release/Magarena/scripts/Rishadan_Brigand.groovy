86bb67311ca378e52594032c8b885e2410c9d946
PalladiaMors <unknown>
2014-09-24 23:06:32 +0200

    Add script for Rishadan Brigand

---
+++ release/Magarena/scripts/Rishadan_Brigand.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+                new MagicEvent(
+                    permanent,
+                    permanent.getOpponent(),
+                    new MagicMayChoice(
+                        "Pay {3}?",
+                        new MagicPayManaCostChoice(MagicManaCost.create("{3}"))
+                    ),
+                    this,
+                    "PN may\$ pay {3}\$. If PN doesn't, PN sacrifices a permanent."
+                );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.addEvent(new MagicSacrificePermanentEvent(
+                event.getSource(), 
+                event.getPermanent().getOpponent(),
+                MagicTargetChoice.SACRIFICE_PERMANENT
+            ));
+            }
+        }
+    }
+]
