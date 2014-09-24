70083292da7e7a7ed99047874f485cd1812c325f
PalladiaMors <unknown>
2014-09-24 18:59:10 +0200

    Add script for Weed-Pruner Poplar

---
+++ release/Magarena/scripts/Weed_Pruner_Poplar.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NegOther("target creature", permanent),
+                new MagicWeakenTargetPicker(-1,-1),
+                this,
+                "Target creature\$ other than SN gets -1/-1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicChangeTurnPTAction(it, -1, -1));
+            });
+        }
+    }
+]
