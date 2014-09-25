0301314929abe52076e0250aa64d253810634c07
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-25 02:23:23 +0100

    add Surge of Strength

---
+++ release/Magarena/scripts/Surge_of_Strength.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gains trample and gets +X/+0 until end of turn, where X is its power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int amount = it.getPower();
+                game.doAction(new MagicGainAbilityAction(it, MagicAbility.Trample));
+                game.doAction(new MagicChangeTurnPTAction(
+                    it,
+                    amount,
+                    0
+                ));
+                game.logAppendMessage(event.getPlayer()," (X="+amount+")");
+            });
+        }
+    }
+]
