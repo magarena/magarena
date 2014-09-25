69474ff9aae3b5836219396045638d76242e105a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-25 18:26:23 +0100

    fix Surge of Strength to pump based on CMC not power

--- release/Magarena/scripts/Surge_of_Strength.groovy
+++ release/Magarena/scripts/Surge_of_Strength.groovy
@@ -7,13 +7,13 @@
                 MagicTargetChoice.POS_TARGET_CREATURE,
                 MagicPumpTargetPicker.create(),
                 this,
-                "Target creature\$ gains trample and gets +X/+0 until end of turn, where X is its power."
+                "Target creature\$ gains trample and gets +X/+0 until end of turn, where X is that creature's converted mana cost."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final int amount = it.getPower();
+                final int amount = it.getConvertedCost();
                 game.doAction(new MagicGainAbilityAction(it, MagicAbility.Trample));
                 game.doAction(new MagicChangeTurnPTAction(
                     it,
