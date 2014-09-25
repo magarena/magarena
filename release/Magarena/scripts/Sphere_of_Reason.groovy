a9a35439b9bf05e277280d8db84bd56d160ff9ee
PalladiaMors <unknown>
2014-09-26 01:28:43 +0200

    Add script for Sphere of Reason

---
+++ release/Magarena/scripts/Sphere_of_Reason.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (permanent.isController(damage.getTarget()) && damage.getSource().hasColor(MagicColor.Blue)) {
+                // Prevention effect.
+                damage.prevent(2);
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
