2f75450eb408e067e19812f2fa4f5ff51642a82d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-25 02:22:57 +0100

    add Ovinomancer

---
+++ release/Magarena/scripts/Ovinomancer.groovy
@@ -0,0 +1,61 @@
+def choice = new MagicTargetChoice("a basic land you control");
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice("Return three basic lands you control to their owner's hand?"),
+                this,
+                "PN may\$ return three basic lands he or she controls to their owner's hand. If PN doesn't, sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent costEvent = new MagicRepeatedPermanentsEvent(event.getSource(), choice, 3, MagicChainEventFactory.Bounce);
+            if (event.isYes() && costEvent.isSatisfied()) {
+                game.addEvent(costEvent);
+            } else {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
+        }
+    },
+    
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source), new MagicBouncePermanentEvent(source, source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                MagicDestroyTargetPicker.DestroyNoRegen,
+                this,
+                "Destroy target creature\$. It can't be regenerated. "+
+                "That creature's controller puts a 0/1 green Sheep creature token onto the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(MagicChangeStateAction.Set(it,MagicPermanentState.CannotBeRegenerated));
+                game.doAction(new MagicDestroyAction(it));
+                game.doAction(new MagicPlayTokenAction(
+                    it.getController(),
+                    TokenCardDefinitions.get("0/1 green Sheep creature token")
+                ));
+            });
+        }
+    }
+]
