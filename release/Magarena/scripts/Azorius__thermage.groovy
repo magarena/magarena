d21a9d4a34823f21894762cd57a43725ad509565
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-26 00:53:47 +0100

    add Azorius Æthermage

---
+++ release/Magarena/scripts/Azorius__thermage.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicRemoveFromPlayAction act) {
+            if (act.getPermanent().getOwner() == permanent.getController() &&
+                act.getToLocation() == MagicLocationType.OwnersHand) {
+                return new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(
+                        new MagicPayManaCostChoice(MagicManaCost.create("{1}"))
+                    ),
+                    this,
+                    "PN may pay {1}\$. If PN does, PN draws a card."
+                );
+            }
+            return MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicDrawAction(event.getPlayer()));
+            }
+        }
+    }
+]
