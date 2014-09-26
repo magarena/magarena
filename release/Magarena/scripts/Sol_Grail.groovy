a03988f31ffb9d34bd67b49dcc59b85dbf51e176
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-26 11:27:48 +0100

    add Sol Grail

---
+++ release/Magarena/scripts/Sol_Grail.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicWhenComesIntoPlayTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                MagicColorChoice.ALL_INSTANCE,
+                this,
+                "Choose a color\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicColor color = event.getChosenColor();
+            final MagicAbilityList ab = new MagicAbilityList();
+            ab.add(new MagicTapManaActivation(Collections.singletonList(color.getManaType())));
+            game.doAction(new MagicGainAbilityAction(event.getPermanent(), ab, MagicStatic.Forever));
+        }
+    }
+]
