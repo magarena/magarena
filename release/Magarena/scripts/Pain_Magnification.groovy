4bd674503a8ec36eaacc4f701f745ade01e3fd3c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-09-26 01:30:09 +0100

    add Pain Magnification

---
+++ release/Magarena/scripts/Pain_Magnification.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return (permanent.isOpponent(damage.getTarget()) && damage.getDealtAmount() >= 3) ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getOpponent(),
+                    this,
+                    "PN discards a card."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicDiscardEvent(event.getPermanent(),event.getPlayer(),1));
+        }
+    }
+]
