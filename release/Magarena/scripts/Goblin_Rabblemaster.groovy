ba3f5c72bfac8522d4d790569abc71730c44ec63
melvin <melvinzhang@gmail.com>
2014-09-28 18:02:42 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Goblin_Rabblemaster.groovy
+++ release/Magarena/scripts/Goblin_Rabblemaster.groovy
@@ -1,55 +1,29 @@
-def GOBLIN_CREATURE_YOU_CONTROL=new MagicPermanentFilterImpl() {
-    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
-        return target.hasSubType(MagicSubType.Goblin) && target.isCreature() && target.isController(player);
-    } 
-};
+def EFFECT = MagicRuleEventAction.create("Put a 1/1 red Goblin creature token with haste onto the battlefield.");
+
 [
-    new MagicStatic(
-        MagicLayer.Ability,
-        GOBLIN_CREATURE_YOU_CONTROL
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            permanent.addAbility(MagicAbility.AttacksEachTurnIfAble, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source != target;
-        }
-    },
     new MagicAtBeginOfCombatTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer attackingPlayer) {
-            return permanent.getController() == attackingPlayer ? 
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "Put a 1/1 red Goblin creature token with haste onto the battlefield."
-                ):
+            return permanent.isController(attackingPlayer) ? 
+                EFFECT.getEvent(permanent) :
                 MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-                game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("1/1 red Goblin creature token with haste")));
         }
     },
     new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
             return new MagicEvent(
-                    permanent,
-                    this,
-                    "SN gets +1/+0 until end of turn for each other attacking Goblin."
-                );
+                permanent,
+                this,
+                "SN gets +1/+0 until end of turn for each other attacking Goblin."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature=event.getPermanent();
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(creature.getController(),MagicTargetFilterFactory.ATTACKING_GOBLIN);
-            //excluding itself
-            final int power = (targets.size() - 1);
-            game.doAction(new MagicChangeTurnPTAction(creature,power,0));
+            final MagicPermanent it = event.getPermanent();
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(MagicTargetFilterFactory.ATTACKING_GOBLIN, it);
+            final int amt = game.filterPermanents(filter).size();
+            game.doAction(new MagicChangeTurnPTAction(it,amt,0));
         }
     }
 ]
