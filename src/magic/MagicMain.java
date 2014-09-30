d2af9c52f48b45d6b01565edf71fcbfc9f685fd7
melvin <melvinzhang@gmail.com>
2014-09-30 09:47:55 +0800

    bump version to 1.55

--- src/magic/MagicMain.java
+++ src/magic/MagicMain.java
@@ -30,7 +30,7 @@ import magic.utility.MagicFileSystem.DataPath;
 
 public class MagicMain {
 
-    public static final String VERSION = "1.54";
+    public static final String VERSION = "1.55";
     public static final String SOFTWARE_TITLE =
             "Magarena " + VERSION +
             (MagicUtility.isDevMode() ? " [DEV MODE]" : "");
