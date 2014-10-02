3703fac48e40cf7d050f5b735626981c804ccd7e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-10-02 15:45:01 +0100

    add Frightshroud Courier

---
+++ release/Magarena/scripts/Frightshroud_Courier.groovy
@@ -0,0 +1,76 @@
+def tappedPower = {
+    final MagicTargetFilter<MagicPermanent> filter ->
+    new MagicStatic(MagicLayer.ModPT, filter) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(2,2);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            if (source.isUntapped()) {
+                //remove this static after the update
+                game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+}
+
+def tappedAbility = {
+    final MagicTargetFilter<MagicPermanent> filter ->
+    new MagicStatic(MagicLayer.Ability, filter) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.Fear, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            if (source.isUntapped()) {
+                //remove this static after the update
+                game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+}
+
+def choice = MagicTargetChoice.Positive("target zombie creature");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{2}{B}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target Zombie creature\$ gets +2/+2 and has fear for as long as SN remains tapped."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent source = event.getPermanent();
+                final MagicTargetFilter<MagicPermanent> filter = new MagicPermanentTargetFilter(it);
+                game.doAction(new MagicAddStaticAction(source, tappedPower(filter)));
+                game.doAction(new MagicAddStaticAction(source, tappedAbility(filter)));
+            });
+        }
+    }
+]
