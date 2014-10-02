f09afce63a54d7246eb4895357c67f58fee74a34
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-10-02 15:10:17 +0100

    Incorporate check for Deathtouch in MagicTrampleTargetPicker to allow detection of lethal damage instead of just blockers toughness

--- src/magic/model/target/MagicTrampleTargetPicker.java
+++ src/magic/model/target/MagicTrampleTargetPicker.java
@@ -3,6 +3,7 @@ package magic.model.target;
 import magic.model.MagicAbility;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.MagicPermanentList;
 import magic.model.MagicPlayer;
 
 public class MagicTrampleTargetPicker extends MagicTargetPicker<MagicPermanent> {
@@ -17,19 +18,22 @@ public class MagicTrampleTargetPicker extends MagicTargetPicker<MagicPermanent> 
 
     @Override
     protected int getTargetScore(final MagicGame game,final MagicPlayer player,final MagicPermanent permanent) {
+        int power = permanent.getPower();
         if (!permanent.hasAbility(MagicAbility.Trample)&&
             !permanent.hasAbility(MagicAbility.Defender)&&
             !permanent.hasAbility(MagicAbility.CannotAttackOrBlock)) {
             if (permanent.isBlocked()) {
-                int toughness = 0; 
-                for (final MagicPermanent blocker : permanent.getBlockingCreatures()) {
-                    toughness += blocker.getToughness();
+                int blockersToughness = 0;
+                MagicPermanentList blockers = permanent.getBlockingCreatures();
+                for (final MagicPermanent blocker : blockers) {
+                    blockersToughness += blocker.getToughness();
                 }
-                if (toughness < permanent.getPower()) {
-                    return 10+permanent.getPower();
+                if (blockersToughness < power || 
+                   (permanent.hasAbility(MagicAbility.Deathtouch) && power > blockers.size())) {
+                    return 10+power;
                 }
             }
-            return 1+permanent.getPower();
+            return 1+power;
         }
         return 0;
     }
