666ba657519b95e6de907784698118c1afb01b70
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-10-02 14:27:06 +0100

    add Tawnos's Weaponry

---
+++ release/Magarena/scripts/Tawnos_s_Weaponry.groovy
@@ -0,0 +1,53 @@
+def tapped = {
+    final MagicTargetFilter<MagicPermanent> filter ->
+    new MagicStatic(MagicLayer.ModPT, filter) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            if (source.isUntapped()) {
+                //remove this static after the update
+                game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{2}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +1/+1 for as long as SN remains tapped."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent source = event.getPermanent();
+                final MagicTargetFilter<MagicPermanent> filter = new MagicPermanentTargetFilter(it);
+                game.doAction(new MagicAddStaticAction(source, tapped(filter)));
+            });
+        }
+    }
+]
