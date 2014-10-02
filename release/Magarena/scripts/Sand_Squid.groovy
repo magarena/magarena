0a5892218870f876b560d9f7bececcf4a582ea11
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-10-02 17:58:27 +0100

    add Sand Squid

---
+++ release/Magarena/scripts/Sand_Squid.groovy
@@ -0,0 +1,54 @@
+def tappedAbility = {
+    final MagicTargetFilter<MagicPermanent> filter ->
+    new MagicStatic(MagicLayer.Ability, filter) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.DoesNotUntap, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            if (source.isUntapped()) {
+                //remove this static after the update
+                game.addDelayedAction(new MagicRemoveStaticAction(source, this));
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+}
+
+def choice = MagicTargetChoice.Negative("target creature");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                MagicTapTargetPicker.Tap,
+                this,
+                "Target creature\$ doesn't untap during its controller's untap step for as long as SN remains tapped."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent source = event.getPermanent();
+                final MagicTargetFilter<MagicPermanent> filter = new MagicPermanentTargetFilter(it);
+                game.doAction(new MagicTapAction(it));
+                game.doAction(new MagicAddStaticAction(source, tappedAbility(filter)));
+            });
+        }
+    }
+]
