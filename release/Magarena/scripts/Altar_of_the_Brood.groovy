5911b24e6be37f90c792dbc3639bf2875d902b63
ShawnieBoy <ShawnieBoyUK@gmail.com>
2014-10-11 06:12:05 +0100

    Add Altar of the Brood

---
+++ release/Magarena/scripts/Altar_of_the_Brood.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return otherPermanent.isFriend(permanent) && otherPermanent != permanent?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Each opponent puts the top card of his or her library into his or her graveyard." 
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new MagicMillLibraryAction(player.getOpponent(),1));
+        }
+    }
+]
