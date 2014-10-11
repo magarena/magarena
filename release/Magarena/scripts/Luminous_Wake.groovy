3f26d808ed48c4af28908b729faf96c7643416e8
PalladiaMors <unknown>
2014-10-11 17:07:40 +0200

    Add script for Luminous Wake

---
+++ release/Magarena/scripts/Luminous_Wake.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPermanent enchantedPermanent = permanent.getEnchantedPermanent();
+            return creature == enchantedPermanent ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    this,
+                    "PN gains 4 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+        }
+    },
+    new MagicWhenBlocksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            final MagicPermanent enchantedPermanent = permanent.getEnchantedPermanent();
+            return creature == enchantedPermanent ?
+                new MagicEvent(
+                    permanent,
+                    permanent.getController(),
+                    this,
+                    "PN gains 4 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeLifeAction(event.getPlayer(),4));
+        }
+    }
+]
