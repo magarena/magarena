0629b279aec428373f013600c4651d6be49e5f99
PalladiaMors <unknown>
2014-10-12 20:48:47 +0200

    Add script for Halam Djinn

---
+++ release/Magarena/scripts/Halam_Djinn.groovy
@@ -0,0 +1,49 @@
+def BLACK_PERMANENT=new MagicPermanentFilterImpl(){
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Black) && 
+               target.isPermanent();
+    }
+};
+
+def BLUE_PERMANENT=new MagicPermanentFilterImpl(){
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Blue) && 
+               target.isPermanent();
+    }
+};
+
+def GREEN_PERMANENT=new MagicPermanentFilterImpl(){
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Green) && 
+               target.isPermanent();
+    }
+};
+
+def RED_PERMANENT=new MagicPermanentFilterImpl(){
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.Red) && 
+               target.isPermanent();
+    }
+};
+
+def WHITE_PERMANENT=new MagicPermanentFilterImpl(){
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasColor(MagicColor.White) && 
+               target.isPermanent();
+    }
+};
+
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            if (source.getGame().getNrOfPermanents(RED_PERMANENT) >=
+                source.getGame().getNrOfPermanents(BLUE_PERMANENT) +
+                source.getGame().getNrOfPermanents(GREEN_PERMANENT) +
+                source.getGame().getNrOfPermanents(BLACK_PERMANENT) +
+                source.getGame().getNrOfPermanents(WHITE_PERMANENT)) {
+            pt.add(-2,-2);
+            }
+        }
+    }
+]
