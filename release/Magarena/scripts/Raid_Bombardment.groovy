2a81fbc03efe267405ee1a4713b2589e5043cd10
melvin <melvinzhang@gmail.com>
2014-10-12 09:13:53 +0800

    replace definding player with PN

--- release/Magarena/scripts/Raid_Bombardment.groovy
+++ release/Magarena/scripts/Raid_Bombardment.groovy
@@ -2,19 +2,22 @@
     new MagicWhenAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return (permanent.isFriend(creature) &&
-                    creature.getPower() <= 2) ?
+            return (permanent.isFriend(creature) && creature.getPower() <= 2) ?
                 new MagicEvent(
                     permanent,
                     game.getDefendingPlayer(),
                     this,
-                    "SN deals 1 damage to defending player."
+                    "SN deals 1 damage to PN."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicDealDamageAction(event.getSource(),game.getDefendingPlayer(),1));
+            game.doAction(new MagicDealDamageAction(
+                event.getSource(),
+                event.getPlayer(),
+                1
+            ));
         }
     }
 ]
