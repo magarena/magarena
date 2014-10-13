774c696a676b15807aaac6ed85ff6e8af9065ed3
melvin <melvinzhang@gmail.com>
2014-10-13 13:17:46 +0800

    X is determine upon resolution

--- release/Magarena/scripts/Inner_Calm__Outer_Strength.groovy
+++ release/Magarena/scripts/Inner_Calm__Outer_Strength.groovy
@@ -7,13 +7,14 @@
                 MagicTargetChoice.POS_TARGET_CREATURE,
                 MagicPumpTargetPicker.create(),
                 this,
-                "Target creature\$ gets +X/+X until end of turn, where X is the number of cards in PN's hand. ("+cardOnStack.getController().getHandSize()+")"
+                "Target creature\$ gets +X/+X until end of turn, where X is the number of cards in PN's hand."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final int X = event.getCardOnStack().getController().getHandSize();
+                final int X = event.getPlayer().getHandSize();
+                game.logAppendMessage(event.getPlayer()," (X="+X+")");
                 game.doAction(new MagicChangeTurnPTAction(it,X,X));
             });
         }
