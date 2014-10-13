88653c4bc63e6a31a2460f635d2e31a5c062c39d
melvin <melvinzhang@gmail.com>
2014-10-13 13:16:19 +0800

    correct event description, fix indentation

--- release/Magarena/scripts/Bulwark.groovy
+++ release/Magarena/scripts/Bulwark.groovy
@@ -2,18 +2,18 @@
     new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-                new MagicEvent(
-                    permanent,
-                    MagicTargetChoice.TARGET_OPPONENT,
-                    this,
-                    "SN deals X damage to RN, where X is the number of cards in PN's hand minus the number of cards in RN's hand."
-                );
+            new MagicEvent(
+                permanent,
+                MagicTargetChoice.TARGET_OPPONENT,
+                this,
+                "SN deals X damage to target opponent\$, where X is the number of cards in PN's hand minus the number of cards in RN's hand."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
-            final int amount = event.getPlayer().getHandSize() - it.getHandSize();
-            game.doAction(new MagicDealDamageAction(event.getPermanent(),it,amount));
+                final int amount = event.getPlayer().getHandSize() - it.getHandSize();
+                game.doAction(new MagicDealDamageAction(event.getPermanent(),it,amount));
             });
         }
     }
