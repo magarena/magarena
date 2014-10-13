0cbc88ef1753fb29dbc0cdb90827c133f033ccee
melvin <melvinzhang@gmail.com>
2014-10-13 11:25:52 +0800

    wrap long line

--- release/Magarena/scripts/Weapon_Surge.groovy
+++ release/Magarena/scripts/Weapon_Surge.groovy
@@ -35,8 +35,10 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+            );
             for (final MagicPermanent creature : targets) {
                 game.doAction(new MagicChangeTurnPTAction(creature, 1, 0));
                 game.doAction(new MagicGainAbilityAction(creature, MagicAbility.FirstStrike));
