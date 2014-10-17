1c878ed3acadd09572ecfafc97b2ba025a1484f8
PalladiaMors <unknown>
2014-10-17 15:53:16 +0200

    Add script for Soulshriek

---
+++ release/Magarena/scripts/Anaba_Ancestor.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.PosOther("target Minotaur creature", source),
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target Minotaur creature\$ other than SN gets +1/+1 until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicChangeTurnPTAction(it,1,1));
+            });
+        }
+    }
+]
