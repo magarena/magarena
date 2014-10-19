f0c5320f0e05f8eded8d839bd71d5dc47c215710
melvin <melvinzhang@gmail.com>
2014-10-19 21:31:31 +0800

    normalize card scripts

--- release/Magarena/scripts/Opalescence.groovy
+++ release/Magarena/scripts/Opalescence.groovy
@@ -1,11 +1,11 @@
 [
     new MagicStatic(MagicLayer.SetPT, MagicTargetFilterFactory.NON_AURA_ENCHANTMENT) {
-		@Override
-		public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-			final int cmc = permanent.getConvertedCost();
-			pt.set(cmc,cmc);
-		}
-		@Override
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            final int cmc = permanent.getConvertedCost();
+            pt.set(cmc,cmc);
+        }
+        @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
@@ -15,7 +15,7 @@
         public int getTypeFlags(final MagicPermanent permanent, final int flags) {
             return flags | MagicType.Creature.getMask();
         }
-		@Override
+        @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return source != target;
         }
