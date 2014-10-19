251373afc3487564de80578a00be31f23e975e34
melvin <melvinzhang@gmail.com>
2014-10-19 16:59:48 +0800

    X is determined on resolution

--- release/Magarena/scripts/Kitsune_Loreweaver.groovy
+++ release/Magarena/scripts/Kitsune_Loreweaver.groovy
@@ -12,19 +12,17 @@
 
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            final int amount = source.getController().getHandSize();
             return new MagicEvent(
                 source,
-                amount,
                 this,
-                "SN gets +0/+X until end of turn. (X="+amount+")"
+                "SN gets +0/+X until end of turn, where X is the number of cards in PN's hand."
             );
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount=event.getRefInt();
-                game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),0,amount));
+            final int X = event.getPlayer().getHandSize();
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),0,X));
         }
     }
 ]
