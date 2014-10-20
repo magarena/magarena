2265eee514eb72151169742e82a0945911540071
PalladiaMors <unknown>
2014-10-20 20:21:45 +0200

    Add script for Mtenda Lion

---
+++ release/Magarena/scripts/Mtenda_Lion.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenSelfAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent creature) {
+            return new MagicEvent(
+                    permanent,
+                    game.getDefendingPlayer(),
+                    new MagicMayChoice(
+                        "Pay {U}?",
+                        new MagicPayManaCostChoice(MagicManaCost.create("{U}"))
+                    ),
+                    this,
+                    "PN may\$ pay {U}\$. If PN does, prevent all combat damage that would be dealt by SN this turn."
+                );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(MagicChangeStateAction.Set(
+                        event.getPermanent(),
+                        MagicPermanentState.NoCombatDamage
+                    ));
+            }
+        }
+    }
+]
