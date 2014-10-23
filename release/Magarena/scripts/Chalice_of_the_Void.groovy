dd2d85dfabe9ce4a5477826efd01b3bfec75c5a6
melvin <melvinzhang@gmail.com>
2014-10-23 10:02:39 +0800

    replace groovy code with ability in card script

--- release/Magarena/scripts/Chalice_of_the_Void.groovy
+++ release/Magarena/scripts/Chalice_of_the_Void.groovy
@@ -1,26 +1,19 @@
 [
-    new MagicWhenComesIntoPlayTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
-            game.doAction(new MagicChangeCountersAction(permanent,MagicCounterType.Charge,payedCost.getX()));
-            return MagicEvent.NONE;
-        }
-    },
     new MagicWhenOtherSpellIsCastTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
-            return new MagicEvent(
-                permanent,
-                cardOnStack,
-                this,
-                "Counter RN if its converted mana cost is equal to the number of charge counters on SN."
-            );
+            return cardOnStack.getConvertedCost() == permanent.getCounters(MagicCounterType.Charge) ?
+                new MagicEvent(
+                    permanent,
+                    cardOnStack,
+                    this,
+                    "Counter RN."
+                ):
+                MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.getRefCardOnStack().getConvertedCost() == event.getPermanent().getCounters(MagicCounterType.Charge)) {
-                game.doAction(new MagicCounterItemOnStackAction(event.getRefCardOnStack()));
-            }
+            game.doAction(new MagicCounterItemOnStackAction(event.getRefCardOnStack()));
         }
     }
 ]
