06bf602bbc1ac991f9e6f5daa76b4edeefd31109
PalladiaMors <unknown>
2014-10-27 13:59:34 +0100

    Add script for Nantuko Mentor

---
+++ release/Magarena/scripts/Nantuko_Mentor.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{G}"),
+                new MagicTapEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +X/+X until end of turn, where X is that creature's power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int X = it.getPower();
+                game.doAction(new MagicChangeTurnPTAction(it, +X, +X));
+            });
+        }
+    }
+]
