fbcc40f7675326bdf2e5f90122e0eba5271d6b27
PalladiaMors <unknown>
2014-10-27 03:22:20 +0100

    Add script for Laquatus's Creativity

---
+++ release/Magarena/scripts/Laquatus_s_Creativity.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.TARGET_PLAYER,
+                this,
+                "Target player\$ draws cards equal to the number of cards in his or her hand, then discards that many cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final int amount = it.getHandSize();
+                game.doAction(new MagicDrawAction(it,amount));
+                game.addEvent(new MagicDiscardEvent(event.getSource(), it, amount));
+            });
+        }
+    }
+]
