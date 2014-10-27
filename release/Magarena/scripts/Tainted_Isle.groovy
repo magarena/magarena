ac2388b9b80d937352d4d935e023c078fe83ae89
PalladiaMors <unknown>
2014-10-27 19:12:55 +0100

    Add script for Tainted Isle

---
+++ release/Magarena/scripts/Tainted_Isle.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(new MagicTapManaActivation(MagicManaType.getList("{U}")));
+            permanent.addAbility(new MagicTapManaActivation(MagicManaType.getList("{B}")));
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicCondition.SWAMP_CONDITION.accept(source);
+        }
+    }
+]
