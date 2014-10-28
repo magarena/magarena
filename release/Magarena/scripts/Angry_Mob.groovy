87ec6c63efba359c621221e5927ce2d21ce38a8d
melvinzhang <melvinzhang@gmail.com>
2014-10-28 22:14:12 +0800

    replace two MagicStatic with a single one

--- release/Magarena/scripts/Angry_Mob.groovy
+++ release/Magarena/scripts/Angry_Mob.groovy
@@ -2,22 +2,12 @@
     new MagicStatic(MagicLayer.SetPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.set(2,2);
+            if (MagicCondition.YOUR_TURN_CONDITION.accept(source)) {
+                final int amount = source.getOpponent().getNrOfPermanents(MagicSubType.Swamp) + 2;
+                pt.set(amount,amount);
+            } else {
+                pt.set(2,2);
+            }
         }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return MagicCondition.NOT_YOUR_TURN_CONDITION.accept(source);
-        }
-    }, 
-    new MagicStatic(MagicLayer.SetPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final int amount = source.getOpponent().getNrOfPermanents(MagicSubType.Swamp) + 2;
-            pt.set(amount,amount);
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return MagicCondition.YOUR_TURN_CONDITION.accept(source);
-        }
-    } 
+    }
 ]
