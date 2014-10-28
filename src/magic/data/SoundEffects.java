d16aceb413ca9475d35ba84c1a08753cd0f1f2d7
lodici <lod@ici>
2014-10-28 18:59:18 +0000

    fix #8 : 1.55 Doesn't work on Linux, same for the previous version.

--- src/magic/data/SoundEffects.java
+++ src/magic/data/SoundEffects.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import magic.model.MagicGame;
 import magic.utility.MagicFileSystem;
 import magic.utility.MagicFileSystem.DataPath;
 
-public class SoundEffects implements LineListener {
+public class SoundEffects {
 
     public static final String WIN_SOUND="win.au";
     public static final String LOSE_SOUND="lose.au";
@@ -22,24 +20,27 @@ public class SoundEffects implements LineListener {
     public static final String COMBAT_SOUND="combat.au";
 
     private static final File SOUNDS_PATH = MagicFileSystem.getDataPath(DataPath.SOUNDS).toFile();
-    private static final SoundEffects INSTANCE=new SoundEffects();
 
     private SoundEffects() {}
 
     public static void playClip(final String name) {
         if (GeneralConfig.getInstance().isSound()) {
-            AudioInputStream ins = null;
-            try { //load sound clip
-                final Clip clip = AudioSystem.getClip();
-                ins = AudioSystem.getAudioInputStream(new File(SOUNDS_PATH,name));
-                clip.open(ins);
-                clip.addLineListener(INSTANCE);
-                clip.start();
-            } catch (final LineUnavailableException | UnsupportedAudioFileException | IOException ex) {
-                System.err.println("WARNING. Unable to load clip " + name);
-            } finally {
-                magic.data.FileIO.close(ins);
-            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try (final AudioInputStream ins = AudioSystem.getAudioInputStream(new File(SOUNDS_PATH, name))) {
+                        try {
+                            final Clip clip = AudioSystem.getClip();
+                            clip.open(ins);
+                            clip.start();
+                        } catch (LineUnavailableException | IOException ex) {
+                            System.err.println("WARNING. Unable to play clip " + name + ", " + ex.getMessage());
+                        }
+                    } catch (UnsupportedAudioFileException | IOException ex) {
+                        System.err.println("WARNING. Unable to load clip " + name + ", " + ex.getMessage());
+                    }
+                }
+            }).start();
         }
     }
 
@@ -49,10 +50,4 @@ public class SoundEffects implements LineListener {
         }
     }
 
-    @Override
-    public void update(final LineEvent event) {
-        if (LineEvent.Type.STOP.equals(event.getType())) {
-            event.getLine().close();
-        }
-    }
 }
