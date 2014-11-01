001b38a5ccc6fa03a890dcdb710a9bc3fe0fca0f
PalladiaMors <unknown>
2014-11-01 14:37:36 +0100

    Add script for Jeweled Spirit

---
+++ release/Magarena/scripts/Jeweled_Spirit.groovy
@@ -0,0 +1,57 @@
+def ProtectionFromArtifacts = MagicAbility.getAbilityList("protection from artifacts");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Color"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_LAND),
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_LAND)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicColorChoice.ALL_INSTANCE,
+                this,
+                "SN gains protection from the color\$ of your choice until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicGainAbilityAction(
+                event.getPermanent(),
+                event.getChosenColor().getProtectionAbility()
+            ));
+        }
+    },
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Artifacts"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_LAND),
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_LAND)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gains protection from artifacts until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicGainAbilityAction(event.getPermanent(),ProtectionFromArtifacts))
+
+        }
+    }
+]
