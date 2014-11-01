84a8d688aa754f82cebe4f4825c72e4b95e8d2b2
PalladiaMors <unknown>
2014-11-01 18:19:41 +0100

    Add script for Greel, Mind Raker

---
+++ release/Magarena/scripts/Greel__Mind_Raker.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Draw),
+        "Discard"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                    new MagicPayManaCostEvent(source,"{X}{B}"),
+                    new MagicTapEvent(source),
+                    new MagicDiscardEvent(source),
+                    new MagicDiscardEvent(source)
+                ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                payedCost.getX(),          
+                this,
+                "Target player\$ discards RN cards at random."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.addEvent(MagicDiscardEvent.Random(event.getSource(), it, event.getRefInt()));
+            });
+        }
+    }
+]
