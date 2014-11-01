4962055830d35307882ccf29d9afb587f800f5ef
PalladiaMors <unknown>
2014-11-01 19:05:26 +0100

    Add script for Latulla, Keldon Overseer

---
+++ release/Magarena/scripts/Latulla__Keldon_Overseer.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                    new MagicPayManaCostEvent(source,"{X}{R}"),
+                    new MagicTapEvent(source),
+                    new MagicDiscardEvent(source),
+                    new MagicDiscardEvent(source)
+                ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                payedCost.getX(), 
+                this,
+                "SN deals RN damage to target creature or player\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,event.getRefInt()));
+            });
+        }
+    }
+]
