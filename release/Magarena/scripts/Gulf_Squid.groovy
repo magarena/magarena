ab3ebc75184d2ca47f97f76d8c59ad255e2b0137
PalladiaMors <unknown>
2014-11-01 18:34:05 +0100

    Add script for Gulf Squid

---
+++ release/Magarena/scripts/Gulf_Squid.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Tap all lands target player\$ controls."
+            );
+        }
+
+         @Override
+         public void executeEvent(final MagicGame game, final MagicEvent event) {
+             event.processTargetPlayer(game, {
+                final Collection<MagicPermanent> targets = it.filterPermanents(MagicTargetFilterFactory.LAND_YOU_CONTROL);
+                for (final MagicPermanent land : targets) {
+                    game.doAction(new MagicTapAction(land));
+                }
+             });
+        }
+    }
+]
