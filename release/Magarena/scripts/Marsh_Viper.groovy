705d0502848bf9ba0be60e1bc54feefe570301f1
melvinzhang <melvinzhang@gmail.com>
2014-11-02 10:33:39 +0800

    should trigger for any damage not just combat damage

--- release/Magarena/scripts/Marsh_Viper.groovy
+++ release/Magarena/scripts/Marsh_Viper.groovy
@@ -1,13 +1,13 @@
 [
-    new MagicWhenSelfCombatDamagePlayerTrigger() {
+    new MagicWhenSelfDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return new MagicEvent(
                 permanent,
                 damage.getTargetPlayer(),
-                    this,
-                    "PN gets two poison counters."
-                );
+                this,
+                "PN gets two poison counters."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
--- release/Magarena/scripts/Pit_Scorpion.groovy
+++ release/Magarena/scripts/Pit_Scorpion.groovy
@@ -1,13 +1,13 @@
 [
-    new MagicWhenSelfCombatDamagePlayerTrigger() {
+    new MagicWhenSelfDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             return new MagicEvent(
                 permanent,
                 damage.getTargetPlayer(),
-                    this,
-                    "PN gets a poison counter."
-                );
+                this,
+                "PN gets a poison counter."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
