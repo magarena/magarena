c13fb160a8538a25358404a48c83bc5261d9ee77
PalladiaMors <unknown>
2014-11-02 20:15:01 +0100

    Add script for Lead-Belly Chimera

---
+++ release/Magarena/scripts/Lead_Belly_Chimera.groovy
@@ -0,0 +1,40 @@
+def AB = new MagicStatic(MagicLayer.Ability) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+        permanent.addAbility(MagicAbility.Trample, flags);
+    }
+};
+
+def choice = MagicTargetChoice.Positive("target Chimera creature");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Sacrifice"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                this,
+                "Put a +2/+2 counter on target Chimera creature\$. It gains trample."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicChangeCountersAction(it,MagicCounterType.PlusTwo,1));
+                game.doAction(new MagicAddStaticAction(it, AB));        
+            });
+        }
+    }
+]
