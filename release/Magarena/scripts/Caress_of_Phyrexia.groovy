d5f245aecd9731dff0aa00d77e7fdf5880d8e56f
melvinzhang <melvinzhang@gmail.com>
2014-11-02 10:37:03 +0800

    change description to follow oracle text, fix effect to lose life instead of gain life

--- release/Magarena/scripts/Caress_of_Phyrexia.groovy
+++ release/Magarena/scripts/Caress_of_Phyrexia.groovy
@@ -6,15 +6,15 @@
                 cardOnStack,
                 MagicTargetChoice.TARGET_PLAYER,
                 this,
-                "Target player\$ draws 3 cards, loses 3 life and gets 3 poison counters."
+                "Target player\$ draws three cards, loses 3 life, and gets three poison counters."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
-                game.doAction(new MagicDrawAction(it,3));
-                game.doAction(new MagicChangeLifeAction(it, 3));
-                game.doAction(new MagicChangePoisonAction(it,3));
+                game.doAction(new MagicDrawAction(it, 3));
+                game.doAction(new MagicChangeLifeAction(it, -3));
+                game.doAction(new MagicChangePoisonAction(it, 3));
             });
         }
     }
