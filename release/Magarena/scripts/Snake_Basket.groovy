787f0b157ae309b379b5a20ffd41405fcc0a5b76
PalladiaMors <unknown>
2014-11-02 20:39:32 +0100

    Add script for Snake Basket

---
+++ release/Magarena/scripts/Snake_Basket.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.SORCERY_CONDITION],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{X}"),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getX(),
+                this,
+                "Put RN 1/1 green Snake creature tokens onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokensAction(
+                event.getPlayer(),
+                TokenCardDefinitions.get("1/1 green Snake creature token"),
+                event.getRefInt()
+            ));
+        }
+    }
+]
