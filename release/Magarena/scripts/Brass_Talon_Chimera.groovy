73ad1bab464a306ec5ef59a655669b1858b06aeb
PalladiaMors <unknown>
2014-11-02 20:08:46 +0100

    Add script for Brass-Talon Chimera

---
+++ release/Magarena/scripts/Brass_Talon_Chimera.groovy
@@ -0,0 +1,39 @@
+def AB = new MagicStatic(MagicLayer.Ability) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+        permanent.addAbility(MagicAbility.FirstStrike, flags);
+    }
+};
+
+def choice = MagicTargetChoice.Positive("target Chimera creature");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Sacrifice"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                this,
+                "Put a +2/+2 counter on target Chimera creature\$. It gains first strike."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicChangeCountersAction(it,MagicCounterType.PlusTwo,1));
+                game.doAction(new MagicAddStaticAction(it, AB));            });
+        }
+    }
+]
