bff9ffca33b2907d279baa40eca9cf1d6befe1aa
PalladiaMors <unknown>
2014-11-03 02:24:05 +0100

    Add script for Ebon Praetor

---
+++ release/Magarena/scripts/Ebon_Praetor.groovy
@@ -0,0 +1,44 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.YOUR_UPKEEP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Sacrifice"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificePermanentEvent(source,MagicTargetChoice.SACRIFICE_CREATURE),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getTarget(),
+                this,
+                "Remove a -2/-2 counter from SN. " +
+                "If the sacrificed creature was a Thrull, put a +1/+0 counter on SN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent sacrificed = event.getRefPermanent();
+            new MagicRemoveCounterEvent(
+            event.getPermanent(),
+            MagicCounterType.MinusTwo,
+            1
+            );
+            if (sacrificed.hasSubType(MagicSubType.Thrull)) { 
+                game.doAction(new MagicChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.PlusOnePlusZero,
+                1
+                ));
+            }
+        }
+    }
+]
+
