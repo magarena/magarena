2f5e00ce7b41148e3e3448363551a77db06546ef
PalladiaMors <unknown>
2014-11-08 15:52:04 +0100

    Add script for Nakaya Shade

---
+++ release/Magarena/scripts/Nakaya_Shade.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{B}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                source.getOpponent(),
+                new MagicMayChoice(
+                    "Pay {2}?",
+                    new MagicPayManaCostChoice(MagicManaCost.create("{2}"))
+                ),
+                this,
+                "PN may\$ pay {2}\$. If PN doesn't, SN gets +1/+1 until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicChangeTurnPTAction(event.getPermanent(), 1, 1));
+            }
+        }
+    }
+]
