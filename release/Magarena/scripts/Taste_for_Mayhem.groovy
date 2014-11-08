573ab7a09b8c5d75e5986bae5566ca77cd25b2cb
PalladiaMors <unknown>
2014-11-08 15:48:13 +0100

    Add script for Taste for Mayhem

---
+++ release/Magarena/scripts/Taste_for_Mayhem.groovy
@@ -0,0 +1,16 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            if (MagicCondition.HELLBENT.accept(source)) {
+                pt.add(4, 0);
+            } else {
+                pt.add(2, 0);
+            }
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
