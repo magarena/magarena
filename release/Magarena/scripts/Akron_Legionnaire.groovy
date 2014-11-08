20481c910426bd889fe0023598d8cde970577237
PalladiaMors <unknown>
2014-11-08 15:44:33 +0100

    Add script for Akron Legionnaire

---
+++ release/Magarena/scripts/Akron_Legionnaire.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+    ) {
+        @Override
+        public void modAbilityFlags(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.CannotAttack, flags);
+        }
+        @Override
+        public boolean condition(
+                final MagicGame game,
+                final MagicPermanent source,
+                final MagicPermanent target) {
+            return !target.getName().equals("Akron Legionnaire") || !target.hasType(MagicType.Artifact);
+        }
+    }
+]
