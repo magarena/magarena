46ac5bf8d4d836bd543fd46d06989b524d1c3fb9
PalladiaMors <unknown>
2014-11-08 15:44:28 +0100

    Add script for Ensnaring Bridge

---
+++ release/Magarena/scripts/Ensnaring_Bridge.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicStatic(
+        MagicLayer.Ability,
+        MagicTargetFilterFactory.CREATURE
+    ) {
+        @Override
+        public void modAbilityFlags(
+                final MagicPermanent source,
+                final MagicPermanent permanent,
+                final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.CannotAttack, flags);
+        }
+        @Override
+        public boolean condition(
+                final MagicGame game,
+                final MagicPermanent source,
+                final MagicPermanent target) {
+            return target.getPower() > permanent.getController().getHandSize();
+        }
+    }
+]
