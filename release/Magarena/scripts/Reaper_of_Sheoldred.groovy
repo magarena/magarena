f673ccde04c754aa1ef53cfe1c452e0498dc8a1c
melvinzhang <melvinzhang@gmail.com>
2014-11-09 20:08:32 +0800

    ability should trigger and be put on the stack

--- release/Magarena/scripts/Reaper_of_Sheoldred.groovy
+++ release/Magarena/scripts/Reaper_of_Sheoldred.groovy
@@ -2,10 +2,18 @@
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            if (damage.getTarget() == permanent) {
-                game.doAction(new MagicChangePoisonAction(damage.getSource().getController(),1));
-            }
-            return MagicEvent.NONE;
+            return damage.isTarget(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    damage.getSource().getController(),
+                    this,
+                    "PN gets a poison counter."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangePoisonAction(event.getPlayer(), 1));
         }
     }
 ]
