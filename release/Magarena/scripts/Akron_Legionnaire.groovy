ce3a3df09ee45a64b836beb0b6431203c4951e90
melvinzhang <melvinzhang@gmail.com>
2014-11-09 09:30:09 +0800

    correct condition for Akron Legionnaire

--- release/Magarena/scripts/Akron_Legionnaire.groovy
+++ release/Magarena/scripts/Akron_Legionnaire.groovy
@@ -4,18 +4,12 @@
         MagicTargetFilterFactory.CREATURE_YOU_CONTROL
     ) {
         @Override
-        public void modAbilityFlags(
-                final MagicPermanent source,
-                final MagicPermanent permanent,
-                final Set<MagicAbility> flags) {
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
             permanent.addAbility(MagicAbility.CannotAttack, flags);
         }
         @Override
-        public boolean condition(
-                final MagicGame game,
-                final MagicPermanent source,
-                final MagicPermanent target) {
-            return !target.getName().equals("Akron Legionnaire") || !target.hasType(MagicType.Artifact);
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return !target.getName().equals("Akron Legionnaire") && !target.hasType(MagicType.Artifact);
         }
     }
 ]
