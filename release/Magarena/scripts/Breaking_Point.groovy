f8bd3cf5b5585dc4f3a4640d6d194cb461d4946a
melvinzhang <melvinzhang@gmail.com>
2014-11-09 20:10:30 +0800

    replace SN in MagicMayChoice text with inline String interpolation, fix indentation

--- release/Magarena/scripts/Breaking_Point.groovy
+++ release/Magarena/scripts/Breaking_Point.groovy
@@ -1,27 +1,26 @@
 [
     new MagicSpellCardEvent() {
         @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {   
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
                 cardOnStack.getOpponent(),
-                new MagicMayChoice("Have SN deal 6 damage to you?"),
+                new MagicMayChoice("Have ${cardOnStack} deal 6 damage to you?"),
                 this,
-                "PN may\$ have SN deal 6 damage to him or her. If PN doesn't, destroy all creatures."
-                + " Creatures destroyed this way can't be regenerated."
+                "PN may\$ have SN deal 6 damage to him or her. If PN doesn't, destroy all creatures. " +
+                "Creatures destroyed this way can't be regenerated."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                game.doAction(new MagicDealDamageAction(event.getCardOnStack(),event.getCardOnStack().getOpponent(),6));
+                game.doAction(new MagicDealDamageAction(event.getSource(), event.getPlayer(), 6));
             } else {
-                 final Collection<MagicPermanent> targets = game.filterPermanents(MagicTargetFilterFactory.CREATURE);
-            for (final MagicPermanent target : targets) {
-                game.doAction(MagicChangeStateAction.Set(target,MagicPermanentState.CannotBeRegenerated));
-            }
-            final MagicDestroyAction destroy = new MagicDestroyAction(targets);         
-                game.doAction(destroy); 
+                final Collection<MagicPermanent> targets = game.filterPermanents(MagicTargetFilterFactory.CREATURE);
+                for (final MagicPermanent target : targets) {
+                    game.doAction(MagicChangeStateAction.Set(target,MagicPermanentState.CannotBeRegenerated));
+                }
+                game.doAction(new MagicDestroyAction(targets));
             }
         }
     }
