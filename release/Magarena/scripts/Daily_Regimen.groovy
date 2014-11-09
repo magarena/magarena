8f560b4c788c747c6402fa4c6f2a543316e3e2c6
melvinzhang <melvinzhang@gmail.com>
2014-11-09 20:09:41 +0800

    improve activated ability short text

--- release/Magarena/scripts/Daily_Regimen.groovy
+++ release/Magarena/scripts/Daily_Regimen.groovy
@@ -1,7 +1,7 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Pump),
-        "Counter"
+        "+Counter"
     ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
