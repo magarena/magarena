f1d795d382ea0f0c0ae90f6c6c99b3e83ec58f70
PalladiaMors <unknown>
2014-11-10 14:33:35 +0100

    Add script for Seizures

---
+++ release/Magarena/scripts/Seizures.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicWhenBecomesTappedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent tapped) {
+            final MagicPermanent enchanted = permanent.getEnchantedPermanent();
+            return enchanted==tapped ?
+                new MagicEvent(
+                    permanent,
+                    tapped.getController(),
+                    new MagicMayChoice(
+                        "Pay {3}?",
+                        new MagicPayManaCostChoice(MagicManaCost.create("{3}"))
+                    ),
+                    this,
+                    "PN may\$ pay {3}\$. If PN doesn't, SN deals 3 damage to him or her."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicDealDamageAction(event.getSource(),event.getPlayer(),3));
+            }
+        }
+    }
+]
