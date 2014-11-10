5c762dd70ca58249ef6b16f0aaee38f119509c26
PalladiaMors <unknown>
2014-11-10 22:22:54 +0100

    Add script for Phyrexian Etchings

---
+++ release/Magarena/scripts/Phyrexian_Etchings.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicAtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer endOfTurnPlayer) {
+            return permanent.isController(endOfTurnPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN draws a card for each age counter on SN."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPermanent().getCounters(MagicCounterType.Age);
+            game.doAction(new MagicDrawAction(event.getPlayer(),amount));
+        }
+    },
+    new MagicWhenDiesTrigger() {     
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent died) {    
+            new MagicEvent(
+                permanent,
+                this,
+                "PN loses 2 life for each age counter on SN."
+            );
+    }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = -2 * event.getPermanent().getCounters(MagicCounterType.Age);
+            game.doAction(new MagicChangeLifeAction(event.getPermanent().getController(),amount));
+        }
+    }
+]
