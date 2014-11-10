c56c88369fd974f7472548c41fb558dff45aee44
PalladiaMors <unknown>
2014-11-10 18:34:16 +0100

    Add script for Jokulmorder

---
+++ release/Magarena/scripts/Jokulmorder.groovy
@@ -0,0 +1,45 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice("Sacrifice five lands?"),
+                this,
+                "PN may\$ sacrifice five lands. If PN doesn't, sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent costEvent = new MagicRepeatedPermanentsEvent(
+                event.getSource(),
+                MagicTargetChoice.SACRIFICE_LAND,
+                5,
+                MagicChainEventFactory.Sac
+                );  
+            if (event.isYes() && costEvent.isSatisfied()) {
+                game.addEvent(costEvent);
+            } else {
+                game.doAction(new MagicSacrificeAction(event.getPermanent()));
+            }
+        }
+    },
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent other) {
+            return (other.hasSubType(MagicSubType.Island) &&
+                    other.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Untap SN."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicUntapAction(event.getPermanent()));
+        }
+    }
+]
