2df74e24cf00b85add7da90be703be2ab5ec8907
melvinzhang <melvinzhang@gmail.com>
2014-11-10 09:18:45 +0800

    revert incorrect fix for Desecration Demon, bug is in HasOptions

--- release/Magarena/scripts/Desecration_Demon.groovy
+++ release/Magarena/scripts/Desecration_Demon.groovy
@@ -2,24 +2,22 @@
     new MagicAtBeginOfCombatTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            final MagicPlayer opponent = permanent.getOpponent();
-            return opponent.controlsPermanent(MagicType.Creature) ? 
-                new MagicEvent(
+            return new MagicEvent(
                 permanent,
-                opponent,
+                permanent.getOpponent(),
                 new MagicMayChoice("Sacrifice a creature?"),
                 this,
-                "PN may\$ sacrifice a creature. If PN does, tap SN and put a +1/+1 counter on it."
-                ):
-            MagicEvent.NONE
+                "PN may\$ sacrifice a creature. If you do, tap SN and put a +1/+1 counter on it."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature = event.getPermanent();
-            if (event.isYes()) {
-                game.addEvent(new MagicSacrificePermanentEvent(creature,event.getPlayer(),MagicTargetChoice.SACRIFICE_CREATURE));
-                game.doAction(new MagicTapAction(creature));
-                game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.PlusOne,1));
+            final MagicPermanent perm = event.getPermanent();
+            final MagicEvent sac = new MagicSacrificePermanentEvent(perm,event.getPlayer(),MagicTargetChoice.SACRIFICE_CREATURE)
+            if (event.isYes() && sac.isSatisfied()) {
+                game.addEvent(sac);
+                game.doAction(new MagicTapAction(perm));
+                game.doAction(new MagicChangeCountersAction(perm,MagicCounterType.PlusOne,1));
             }
         }
     }
