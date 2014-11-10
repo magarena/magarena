6f18540c48db8862de29ba78ec31dd4eb7fc09ff
lodici <spr11766@fastmail.net>
2014-11-10 09:25:57 +0000

    refactor; document.

--- src/magic/ui/theme/AbilityIcon.java
+++ src/magic/ui/theme/AbilityIcon.java
@@ -3,22 +3,17 @@ package magic.ui.theme;
 import javax.swing.ImageIcon;
 import magic.data.IconImages;
 
+/**
+ * Used to assign an ability with an icon which is displayed
+ * on the annotated card image.
+ */
 public enum AbilityIcon {
-
-    FLYING("Flying", IconImages.FLYING, "A creature with flying cannot be blocked except by other creatures with either flying or reach. Creatures with flying can block other creatures with or without flying."),
+    
+    // Please try to keep in order so it is easier to maintain.
     FIRST_STRIKE("First Strike", IconImages.STRIKE, "First strike is a static ability that creates an additional combat damage step. A creature with first strike will deal its combat damage before a creature that doesn't. This often leads to the other creature dying before it gets a chance to strike."),
+    FLYING("Flying", IconImages.FLYING, "A creature with flying cannot be blocked except by other creatures with either flying or reach. Creatures with flying can block other creatures with or without flying."),
     TRAMPLE("Trample", IconImages.TRAMPLE, "If a creature with trample would deal enough combat damage to its blockers to destroy them, it deals the rest of its damage to the defending player."),
     ;
-//    INFECT(IconImages.INFECT),
-//    REACH(IconImages.REACH),
-//    DEATHTOUCH(IconImages.DEATHTOUCH),
-//    TOTEM_ARMOR(),
-
-//    SHROUD(IconImages.SHROUD),
-//    VIGILANCE(IconImages.VIGILANCE),
-//    DOUBLE_STRIKE(IconImages.DOUBLESTRIKE),
-//    BATTLECRY(),
-//    HASTE();
 
     private static final Theme THEME = ThemeFactory.getInstance().getCurrentTheme();
     
