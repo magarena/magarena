06fb44dfbe04b5e03284ac5ca5a354ea7d70b4a6
PalladiaMors <unknown>
2014-11-10 14:43:14 +0100

    Add script for Slow Motion

---
+++ release/Magarena/scripts/Slow_Motion.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            final MagicPermanent enchanted = permanent.getEnchantedPermanent();
+            return enchanted.isController(upkeepPlayer) ?
+                new MagicEvent(
+                    permanent,
+                    upkeepPlayer,
+                    new MagicMayChoice(
+                        "Pay {2}?",
+                        new MagicPayManaCostChoice(MagicManaCost.create("{2}"))
+                    ),
+                    enchanted,
+                    this,
+                    "PN may\$ pay {2}\$. If PN doesn't, sacrifice RN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new MagicSacrificeAction(event.getRefPermanent()));
+            }
+        }
+    }
+]
