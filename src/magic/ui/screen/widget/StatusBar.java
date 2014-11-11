10d60e66c92c2845334a0ca1fb053b9f380856a3
lodici <spr11766@fastmail.net>
2014-11-11 16:07:29 +0000

    use MagicStyle theme in StatusBar.

--- src/magic/ui/screen/widget/StatusBar.java
+++ src/magic/ui/screen/widget/StatusBar.java
@@ -15,8 +15,8 @@ import magic.ui.screen.interfaces.IOptionsMenu;
 import magic.ui.screen.interfaces.IStatusBar;
 import magic.ui.screen.interfaces.IThemeStyle;
 import magic.ui.theme.Theme;
-import magic.ui.theme.ThemeFactory;
 import magic.ui.widget.TexturedPanel;
+import magic.utility.MagicStyle;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
@@ -47,8 +47,7 @@ public class StatusBar extends TexturedPanel implements IThemeStyle {
 
     @Override
     public void refreshStyle() {
-        final Theme THEME = ThemeFactory.getInstance().getCurrentTheme();
-        final Color refBG = THEME.getColor(Theme.COLOR_TITLE_BACKGROUND);
+        final Color refBG = MagicStyle.getTheme().getColor(Theme.COLOR_TITLE_BACKGROUND);
         final Color thisBG = new Color(refBG.getRed(), refBG.getGreen(), refBG.getBlue(), 220);
         setBackground(thisBG);
     }
