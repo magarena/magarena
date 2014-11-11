fc38924a7b1db19981fb1056644fd1749bc14523
PalladiaMors <unknown>
2014-11-11 14:27:42 +0100

    Add script for Orchard Warden

---
+++ release/Magarena/scripts/Orchard_Warden.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return otherPermanent != permanent &&
+                   otherPermanent.isCreature() &&
+                   otherPermanent.hasSubType(MagicSubType.Treefolk) &&
+                   otherPermanent.isFriend(permanent) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice("gain life?"),
+                    otherPermanent,
+                    this,
+                    "PN may gain life equal to RN's toughness." 
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),event.getRefPermanent().getToughness()));
+            }
+        }
+    }
+]
