a34ed75aea845f0e68e3f0688746d0c4b9d87769
PalladiaMors <unknown>
2014-11-11 14:03:11 +0100

    Add script for Slithermuse

---
+++ release/Magarena/scripts/Slithermuse.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicWhenLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicRemoveFromPlayAction act) {
+            return permanent.getOpponent().getHandSize() >
+                   permanent.getController().getHandSize() ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "If opponent has more cards in hand than you, draw cards equal to the difference."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPermanent().getOpponent().getHandSize() - 
+                               event.getPermanent().getController().getHandSize();
+            game.doAction(new MagicDrawAction(event.getPlayer(),amount));
+        }
+    }
+]
