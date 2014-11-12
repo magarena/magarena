e035df649693a69ac619f23259f583380da31f87
lodici <spr11766@fastmail.net>
2014-11-12 09:12:47 +0000

    add PROTECTION_BLACK ability icon.

--- src/magic/ui/theme/AbilityIcon.java
+++ src/magic/ui/theme/AbilityIcon.java
@@ -32,6 +32,7 @@ public enum AbilityIcon {
     INDESTRUCTIBLE(MagicAbility.Indestructible, "Indestructible", IconImages.INDESTRUCTIBLE, "Any permanent that has indestructible cannot be destroyed, and cannot die as a result of lethal damage."),
     INFECT(MagicAbility.Infect, "Infect", IconImages.INFECT, "A creature with infect doesn't deal damage as normal. If an infect creature deals damage to another creature, it will put that many -1/-1 counters on that creature instead. If an infect creature tries to deal damage to a player, that player gains that many poison counters instead. A player with 10 or more poison counters loses the game."),
     LIFELINK(MagicAbility.Lifelink, "Lifelink", IconImages.LIFELINK, "When a creature with lifelink deals combat damage, the controller of that creature also gains an amount of life equal to the amount of damage dealt damage, in addition to dealing the damage as normal."),
+    PROTECTION_BLACK(MagicAbility.ProtectionFromBlack, "Protection from Black", IconImages.PROTBLACK, AbilityIcon.PROTECTION_TOOLTIP),
     REACH(MagicAbility.Reach, "Reach", IconImages.REACH, "Creatures with reach can block creatures with flying."),
     SHROUD(MagicAbility.Shroud, "Shroud", IconImages.SHROUD, "A permanent or player with shroud cannot be the target of any spells or abilities. Shroud can still be affected by effects that do not target. Such as deathtouch; spells that affect all creatures, such as Planar Cleansing; or effects that don't have the word target in them, such as Clone."),
     TOTEM_ARMOR(MagicAbility.TotemArmor, "Totem Armor", null, "If a creature enchanted by an Aura with totem armor would be destroyed, all damage is removed from the creature and the Aura with totem armor is destroyed instead. If a creature is enchanted by multiple Auras with totem armor, only one Aura with totem armor (of the creature’s controller’s choice) is destroyed."),
@@ -39,6 +40,8 @@ public enum AbilityIcon {
     VIGILANCE(MagicAbility.Vigilance, "Vigilance", IconImages.VIGILANCE, "A creature with vigilance does not need to tap when attacking, this allows that creature to be able to block on the next player's turn."),
     WITHER(MagicAbility.Wither, "Wither", IconImages.WITHER, "Whenever a creature with wither deals damage to another creature, a -1/-1 counter is put on that creature instead for each point of damage dealt. A creature with more than one instance of wither does not deal twice the amount of -1/-1 counters in damage."),
     ;
+
+    private final static String PROTECTION_TOOLTIP = "Protection is normally written \"Protection from {quality}\". For example, something with protection from black can’t be the target of black spells or abilities of black cards. Black spells and abilities that don’t use the word “target” still affect a creature with protection. Anything with protection from black can’t be damaged by black cards. Black creatures can’t block an attacking creature with protection from black.";
 
     private final ImageIcon defaultIcon;
     private final String iconName;
