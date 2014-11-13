9e568f13419102a1b70b4f399a6beb121e3a5540
PalladiaMors <unknown>
2014-11-13 23:48:57 +0100

    Add script for Soldevi Digger

---
+++ release/Magarena/scripts/Soldevi_Digger.groovy
@@ -0,0 +1,37 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Draw),
+        "Card"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Put the top card of PN's graveyard on the bottom of PN's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCardList top1 = event.getPlayer().getGraveyard().getCardsFromTop(1) ;
+            for (final MagicCard top : top1) {
+                game.doAction(new MagicRemoveCardAction(
+                    top,
+                    MagicLocationType.Graveyard
+                ));
+                game.doAction(new MagicMoveCardAction(
+                    top,
+                    MagicLocationType.Graveyard,
+                    MagicLocationType.BottomOfOwnersLibrary
+                ));
+            }
+        }
+    }
+]
