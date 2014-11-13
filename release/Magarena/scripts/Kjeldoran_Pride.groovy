05ed416bafdaa0631ac70ab375ae37c41f6786f2
PalladiaMors <unknown>
2014-11-13 22:10:12 +0100

    Add script for Kjeldoran Pride

---
+++ release/Magarena/scripts/Kjeldoran_Pride.groovy
@@ -0,0 +1,33 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Attach"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}{U}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.PosOther("target creature", source.getEnchantedPermanent()),
+                this,
+                "Attach SN to target creature\$."
+                );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicAttachAction(
+                    event.getPermanent(),
+                    it
+                ));
+            });
+        }
+    }
+]
