6bd98b44ee3d21e1438a19ef836389b72c2bada9
PalladiaMors <unknown>
2014-11-13 18:46:48 +0100

    Add script for Taste of Paradise

---
+++ release/Magarena/scripts/Taste_of_Paradise.groovy
@@ -0,0 +1,17 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.getKicker(),
+                this,
+                "PN gains 3 life plus 3 life for each additional {1}{G} spent to cast SN. (RN)"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(),3 + 3 * event.getRefInt()));
+        }
+    }
+]
