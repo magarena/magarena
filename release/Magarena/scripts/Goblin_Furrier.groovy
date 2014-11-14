5a7009893cdd05190fffac17579a11189401f70a
PalladiaMors <elvanin_1@hotmail.com>
2014-11-14 00:50:56 -0200

    Update Goblin_Furrier.groovy

--- release/Magarena/scripts/Goblin_Furrier.groovy
+++ release/Magarena/scripts/Goblin_Furrier.groovy
@@ -3,8 +3,8 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             if (damage.getSource() == permanent &&
-                damage.getTarget().hasType(MagicType.Creature) &&
-                damage.getTarget().hasType(MagicType.Snow)) {
+                damage.getTarget().hasType(MagicType.Snow) &&
+                damage.getTarget().hasType(MagicType.Creature)) {
                 // Replacement effect. Generates no event or action.
                 damage.prevent();
             }
