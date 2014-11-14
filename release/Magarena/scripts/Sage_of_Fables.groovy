53683b9f10819fd5b124d05190b6e3e0de9d476b
PalladiaMors <elvanin_1@hotmail.com>
2014-11-14 14:17:56 -0200

    Update Sage_of_Fables.groovy

--- release/Magarena/scripts/Sage_of_Fables.groovy
+++ release/Magarena/scripts/Sage_of_Fables.groovy
@@ -1,12 +1,12 @@
 [
-    new MagicWhenOtherComesIntoPlayTrigger() {
+    new MagicWhenOtherComesIntoPlayTrigger(MagicTrigger.REPLACEMENT) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent otherPermanent) {
             if (otherPermanent != permanent &&
                 otherPermanent.isCreature() &&
                 otherPermanent.isFriend(permanent) &&
                 otherPermanent.hasSubType(MagicSubType.Wizard)) {
-                game.doAction(new MagicChangeCountersAction(otherPermanent,MagicCounterType.PlusOne,1));
+                game.doAction(MagicChangeCountersAction.Enters(otherPermanent,MagicCounterType.PlusOne,1));
             }
             return MagicEvent.NONE;
         }
