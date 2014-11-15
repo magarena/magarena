cf5416c9b261bfa689651a5a2d8f81df556241b2
PalladiaMors <unknown>
2014-11-15 20:02:33 +0100

    Add script for Cackling Flames

---
+++ release/Magarena/scripts/Cackling_Flames.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER,
+                this,
+                "SN deals 3 damage to target creature or player\$. " +
+                "SN deals 5 damage to that creature or player instead if you have no cards in hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                game.doAction(new MagicDealDamageAction(
+                    event.getSource(),
+                    it,
+                    MagicCondition.HELLBENT.accept(event.getSource()) ? 5 : 3
+                ))
+            });
+        }
+    }
+]
