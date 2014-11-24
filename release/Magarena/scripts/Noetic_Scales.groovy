272756a75bd32a43cea359698a45372678bd8421
Guest <unknown>
2014-11-24 20:29:51 +0100

    Add script for Noetic Scales

---
+++ release/Magarena/scripts/Noetic_Scales.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+           return new MagicEvent(
+                    permanent,
+                    upkeepPlayer,
+                    this,
+                    "Return to PN's hand all the creatures PN controls " +
+                    "with power greater than the number of cards in PN's hand."
+                );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getHandSize();
+            final Collection<MagicPermanent> targets=
+                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            for (final MagicPermanent target : targets) {
+                if(target.getPower() > amount){
+                    game.doAction(new MagicRemoveFromPlayAction(target,MagicLocationType.OwnersHand));
+                }
+            }
+        }
+    }
+]
