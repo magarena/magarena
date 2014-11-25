f43008d5acdabdb905c641611fd6d06aaf6fe30f
melvinzhang <melvinzhang@gmail.com>
2014-11-25 17:30:17 +0800

    fix indentation and improve description

--- release/Magarena/scripts/Noetic_Scales.groovy
+++ release/Magarena/scripts/Noetic_Scales.groovy
@@ -3,20 +3,19 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
            return new MagicEvent(
-                    permanent,
-                    upkeepPlayer,
-                    this,
-                    "Return to PN's hand all the creatures PN controls " +
-                    "with power greater than the number of cards in PN's hand."
-                );
+                permanent,
+                upkeepPlayer,
+                this,
+                "Return to its owner's hand each creature PN controls " +
+                "with power greater than the number of cards in PN's hand."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final int amount = event.getPlayer().getHandSize();
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(event.getPlayer(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            final Collection<MagicPermanent> targets = event.getPlayer().filterPermanents(MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
             for (final MagicPermanent target : targets) {
-                if(target.getPower() > amount){
+                if (target.getPower() > amount){
                     game.doAction(new MagicRemoveFromPlayAction(target,MagicLocationType.OwnersHand));
                 }
             }
