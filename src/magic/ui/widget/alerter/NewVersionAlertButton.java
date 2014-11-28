01ef48186eaf7a19f70378c498b14f8195e72bf1
lodici <spr11766@fastmail.net>
2014-11-28 09:59:45 +0000

    Make NewVersionAlertButton a subclass of AlertButton;

--- src/magic/ui/widget/alerter/NewVersionAlertButton.java
+++ src/magic/ui/widget/alerter/NewVersionAlertButton.java
@@ -1,72 +1,54 @@
 package magic.ui.widget.alerter;
 
-import java.awt.Cursor;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.concurrent.ExecutionException;
 import javax.swing.AbstractAction;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import magic.MagicMain;
 import magic.data.GeneralConfig;
-import magic.data.SoundEffects;
 import magic.data.URLUtils;
 import magic.data.json.NewVersionJsonParser;
 
 @SuppressWarnings("serial")
-public class NewVersionAlertButton extends JButton {
+public class NewVersionAlertButton extends AlertButton {
 
     private String newVersion = "";
     private static boolean hasChecked = false;
+    private AbstractAction alertAction;
 
-    public NewVersionAlertButton() {
-
-        setAction(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-                final String caption = "Version " + newVersion + " has been released.";
-                String[] buttons = {"Open download page", "Don't remind me again", "Cancel"};
-                int rc = JOptionPane.showOptionDialog(
-                        MagicMain.rootFrame,
-                        caption,
-                        "New version alert",
-                        0,
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null,
-                        buttons, buttons[0]);
-                if (rc == 0) {
-                    URLUtils.openURL(URLUtils.URL_HOMEPAGE);
-                } else if (rc == 1) {
-                    // suppress alert for this release.
-                    final GeneralConfig config = GeneralConfig.getInstance();
-                    config.setIgnoredVersionAlert(newVersion);
-                    config.save();
+    @Override
+    protected AbstractAction getAlertAction() {
+        if (alertAction == null) {
+            alertAction = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
                     setVisible(false);
-                } else {
-                    setVisible(true);
+                    final String caption = "Version " + newVersion + " has been released.";
+                    String[] buttons = {"Open download page", "Don't remind me again", "Cancel"};
+                    int rc = JOptionPane.showOptionDialog(
+                            MagicMain.rootFrame,
+                            caption,
+                            "New version alert",
+                            0,
+                            JOptionPane.INFORMATION_MESSAGE,
+                            null,
+                            buttons, buttons[0]);
+                    if (rc == 0) {
+                        URLUtils.openURL(URLUtils.URL_HOMEPAGE);
+                    } else if (rc == 1) {
+                        // suppress alert for this release.
+                        final GeneralConfig config = GeneralConfig.getInstance();
+                        config.setIgnoredVersionAlert(newVersion);
+                        config.save();
+                        setVisible(false);
+                    } else {
+                        setVisible(true);
+                    }
                 }
-            }
-        });
-        setFont(getFont().deriveFont(Font.BOLD));
-        setVisible(false);
-
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-            }
-            @Override
-            public void mouseExited(MouseEvent e) {
-                super.mouseExited(e);
-                setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-            }
-        });
-        
+            };
+        }
+        return alertAction;
     }
 
     private boolean isNewVersionAvailable() {
@@ -99,10 +81,6 @@ public class NewVersionAlertButton extends JButton {
                 }
             }.execute();
         }
-    }
-
-    private void playNewAlertSoundEffect() {
-        SoundEffects.playClip(SoundEffects.RESOLVE_SOUND);
     }
 
 }
