c8f2b9372a15f476e5002d7c2ffad5dca01066e9
lodici <spr11766@fastmail.net>
2015-01-08 14:54:40 +0000

    consolidate duplicate code in new method.

--- src/magic/utility/MagicResources.java
+++ src/magic/utility/MagicResources.java
@@ -10,21 +10,18 @@ public final class MagicResources {
     private MagicResources() { }
 
     public static String getSetFileContent(final MagicSets magicSet) {
-        final String filename = "/magic/data/sets/" + magicSet.toString().replace("_", "") + ".txt";
+        return getResourceFileContent("/magic/data/sets/" + magicSet.toString().replace("_", "") + ".txt");
+    }
+
+    public static String getFormatFileContent(final MagicFormats magicFormat) {
+        return getResourceFileContent("/magic/data/formats/" + magicFormat.getFilename() + ".fmt");
+    }
+
+    private static String getResourceFileContent(final String filename) {
         try (final InputStream inputStream = MagicFileSystem.getJarResourceStream(filename)) {
             return inputStream != null ? FileIO.toStr(inputStream) : "";
         } catch (final IOException ex) {
             System.err.println(filename + " : " + ex.getMessage());
-            return "";
-        }
-    }
-
-    public static String getFormatFileContent(final MagicFormats magicFormat) {
-        final String filename = "/magic/data/formats/" + magicFormat.getFilename() + ".fmt";
-        try (final InputStream inputStream = MagicFileSystem.getJarResourceStream(filename)) {
-            return inputStream != null ? FileIO.toStr(inputStream) : "";
-        } catch (final IOException ex) {
-            System.err.println(magicFormat.getFilename() + " : " + ex.getMessage());
             return "";
         }
     }
