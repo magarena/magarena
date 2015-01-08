7c90fa8f8c8f74b2142654c429bef71807d44e98
lodici <spr11766@fastmail.net>
2015-01-08 14:47:01 +0000

    Update MagicSetDefinitions to use standard getJarResourceStream method and to close stream properly using try-with-resources syntax.

--- src/magic/data/MagicSetDefinitions.java
+++ src/magic/data/MagicSetDefinitions.java
@@ -1,32 +1,25 @@
 package magic.data;
 
-import magic.MagicMain;
 import magic.model.MagicCardDefinition;
 import magic.model.MagicSetDefinition;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import magic.utility.MagicFileSystem;
 
 public class MagicSetDefinitions {
 
     private static final HashMap<MagicSets, MagicSetDefinition> loadedSets = new HashMap<>();
 
     private static MagicSetDefinition loadMagicSet(final MagicSets magicSet) {
-        String content = null;
-        final String filename = "/magic/data/sets/" + magicSet.toString().replace("_", "") + ".txt";
-        try {
-            content = FileIO.toStr(MagicMain.rootFrame.getClass().getResourceAsStream(filename));
-        } catch (final IOException ex) {
-            System.err.println("ERROR! Unable to load " + filename);
-            return null;
-        }
 
         final MagicSetDefinition magicSetDef = new MagicSetDefinition(magicSet.toString());
 
-        try (final Scanner sc = new Scanner(content)) {
+        try (final Scanner sc = new Scanner(getSetFileContent(magicSet))) {
             while (sc.hasNextLine()) {
                 final String line = sc.nextLine();
                 magicSetDef.add(line.trim());
@@ -35,6 +28,16 @@ public class MagicSetDefinitions {
 
         return magicSetDef;
 
+    }
+
+    private static String getSetFileContent(final MagicSets magicSet) {
+        final String filename = "/magic/data/sets/" + magicSet.toString().replace("_", "") + ".txt";
+        try (final InputStream inputStream = MagicFileSystem.getJarResourceStream(filename)) {
+            return inputStream != null ? FileIO.toStr(inputStream) : "";
+        } catch (final IOException ex) {
+            System.err.println(filename + " : " + ex.getMessage());
+            return "";
+        }
     }
 
     public static String[] getFilterValues() {
