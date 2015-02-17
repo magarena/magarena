24d69f5a852efafb3382775a4db7d672f7916b91
lodici <spr11766@fastmail.net>
2015-02-17 19:05:23 +0000

    remove UI dependencies from MagicSimpleMayChoice.

--- src/magic/model/choice/MagicSimpleMayChoice.java
+++ src/magic/model/choice/MagicSimpleMayChoice.java
@@ -6,12 +6,9 @@ import magic.model.MagicPlayer;
 import magic.model.MagicSource;
 import magic.model.event.MagicEvent;
 import magic.exception.UndoClickedException;
-import magic.ui.duel.choice.MayChoicePanel;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
 import magic.model.IGameController;
 
 public class MagicSimpleMayChoice extends MagicChoice {
@@ -82,12 +79,7 @@ public class MagicSimpleMayChoice extends MagicChoice {
                     new Object[]{YES_CHOICE};
         }
         controller.disableActionButton(false);
-        final MayChoicePanel choicePanel = controller.waitForInput(new Callable<MayChoicePanel>() {
-            public MayChoicePanel call() {
-                return new MayChoicePanel(controller,source,getDescription());
-            }
-        });
-        if (choicePanel.isYesClicked()) {
+        if (controller.getMayChoice(source, getDescription())) {
             return new Object[]{YES_CHOICE};
         }
         return new Object[]{NO_CHOICE};
