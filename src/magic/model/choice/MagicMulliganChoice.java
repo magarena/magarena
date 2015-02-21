28b28abd90572d0fe9c64208323d5119b47a7062
Mike <mike@otep.ch>
2015-02-21 14:43:57 +0100

    merge with latest master to remove ui references

--- src/magic/model/choice/MagicMulliganChoice.java
+++ src/magic/model/choice/MagicMulliganChoice.java
@@ -1,8 +1,5 @@
 package magic.model.choice;
 
-
-import magic.data.DuelConfig;
-import magic.data.GeneralConfig;
 import magic.model.MagicCard;
 import magic.model.MagicCardDefinition;
 import magic.model.MagicCostManaType;
@@ -13,17 +10,14 @@ import magic.model.MagicSource;
 import magic.model.action.MagicPlayCardAction;
 import magic.model.event.MagicEvent;
 import magic.model.phase.MagicMainPhase;
-import magic.ui.GameController;
-import magic.exceptions.UndoClickedException;
-import magic.ui.duel.choice.MayChoicePanel;
-import magic.ui.duel.choice.MulliganChoicePanel;
-import magic.ui.screen.MulliganScreen;
+import magic.exception.UndoClickedException;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
+
+import magic.model.IUIGameController;
 
 public class MagicMulliganChoice extends MagicChoice {
 
@@ -33,7 +27,7 @@ public class MagicMulliganChoice extends MagicChoice {
             Collections.singletonList(new Object[]{NO_CHOICE});
     private static final List<Object[]> ACTUAL_CHOICE_LIST =
             Collections.singletonList(new Object[]{NO_CHOICE,YES_CHOICE});
-    
+
     public MagicMulliganChoice() {
         super("");
     }
@@ -125,11 +119,10 @@ public class MagicMulliganChoice extends MagicChoice {
         final MagicPayManaCostResultBuilder builder=new MagicPayManaCostResultBuilder(game,player,builderCost);
         return builder.hasResults();
     }
-    
 
     @Override
     public Object[] getPlayerChoiceResults(
-            final GameController controller,
+            final IUIGameController controller,
             final MagicGame game,
             final MagicPlayer player,
             final MagicSource source) throws UndoClickedException {
@@ -138,21 +131,10 @@ public class MagicMulliganChoice extends MagicChoice {
             return new Object[]{NO_CHOICE};
         }
         controller.disableActionButton(false);
-        final MayChoicePanel choicePanel = controller.waitForInput(new Callable<MayChoicePanel>() {
-            public MayChoicePanel call() {
-                final boolean showMulliganScreen =
-                        MulliganScreen.isActive() ||
-                        (player.getHandSize() == DuelConfig.getInstance().getHandSize() && GeneralConfig.getInstance().showMulliganScreen());
-                if (showMulliganScreen) {
-                    return new MulliganChoicePanel(controller, source, "You may take a mulligan.", player.getPrivateHand());
-                } else {
-                    return new MayChoicePanel(controller,source,"You may take a mulligan.");
-                }
-            }
-        });
-        if (choicePanel.isYesClicked()) {
+        if (controller.getTakeMulliganChoice(source, player)) {
             return new Object[]{YES_CHOICE};
         }
         return new Object[]{NO_CHOICE};
     }
+
 }
