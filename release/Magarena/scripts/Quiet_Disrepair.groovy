498bca7e469d944842c05c98f2986f1db57e77f7
PalladiaMors <unknown>
2015-02-23 23:45:20 +0100

    Add script for Quiet Disrepair

---
+++ release/Magarena/scripts/Quiet_Disrepair.groovy
@@ -0,0 +1,27 @@
+def EFFECT2 = MagicRuleEventAction.create("You gain 2 life.");
+
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                new MagicOrChoice(
+                    MagicTargetChoice.NONE,
+                    MagicTargetChoice.NONE
+                ),
+                this,
+                "Choose one\$ - destroy enchanted permanent; " +
+                "or you gain 2 life.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.doAction(new MagicDestroyAction(event.getPermanent().getEnchantedPermanent()));
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
