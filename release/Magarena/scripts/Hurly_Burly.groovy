505c4adb2aaab6a347a63df6e3c186c28ea01651
PalladiaMors <unknown>
2015-02-23 11:19:42 +0100

    Add script for Hurly-Burly

---
+++ release/Magarena/scripts/Hurly_Burly.groovy
@@ -0,0 +1,30 @@
+def EFFECT1 = MagicRuleEventAction.create("SN deals 1 damage to each creature without flying.");
+
+def EFFECT2 = MagicRuleEventAction.create("SN deals 1 damage to each creature with flying.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicChoice.NONE
+                ),
+                this,
+                "Choose one\$ - SN deals 1 damage to each creature without flying; " +
+                "or SN deals 1 damage to each creature with flying."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.addEvent(EFFECT1.getEvent(event.getSource()));
+
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
