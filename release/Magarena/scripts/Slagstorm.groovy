18e5ef8fa4905990acfe4694a87d78ecf70b45db
PalladiaMors <unknown>
2015-02-23 11:19:57 +0100

    Add script for Slagstorm

---
+++ release/Magarena/scripts/Slagstorm.groovy
@@ -0,0 +1,30 @@
+def EFFECT1 = MagicRuleEventAction.create("SN deals 3 damage to each creature.");
+
+def EFFECT2 = MagicRuleEventAction.create("SN deals 3 damage to each player.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicChoice.NONE
+                ),
+                this,
+                "Choose one\$ - SN deals 3 damage to each creature; " +
+                "or SN deals 3 damage to each player."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.addEvent(EFFECT1.getEvent(event.getSource()));
+
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
