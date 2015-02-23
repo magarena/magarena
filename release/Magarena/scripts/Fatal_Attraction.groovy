732d1d9d07fd6dcb7b69c3b4ffd808693c869b74
PalladiaMors <unknown>
2015-02-23 21:47:30 +0100

    Add script for Fatal Attraction

---
+++ release/Magarena/scripts/Fatal_Attraction.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            final MagicPermanent enchantedCreature = permanent.getEnchantedPermanent();
+            return enchantedCreature.isValid() ?
+                new MagicEvent(
+                    permanent,
+                    enchantedCreature,
+                    this,
+                    "SN deals 2 damage to RN"
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDealDamageAction(event.getPermanent(),event.getRefPermanent(),2));
+        }
+    },
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            new MagicEvent(
+                permanent,
+                permanent.getEnchantedPermanent(),
+                this,
+                "SN deals 4 damage to RN."
+            );
+    }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDealDamageAction(event.getPermanent(),event.getRefPermanent(),4));
+        }
+    }
+]
