0a19ff8714009784ce36f22d93c985ca1503a0e9
PalladiaMors <unknown>
2015-02-23 11:18:50 +0100

    Add script for Flash Flood

---
+++ release/Magarena/scripts/Flash_Flood.groovy
@@ -0,0 +1,30 @@
+def EFFECT1 = MagicRuleEventAction.create("Destroy target red permanent.");
+
+def EFFECT2 = MagicRuleEventAction.create("Return target mountain to its owner's hand.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicTargetChoice.Negative("target red permanent"),
+                    MagicTargetChoice.Negative("target Mountain")
+                ),
+                this,
+                "Choose one\$ - destroy target red permanent; " +
+                "or return target Mountain to its owner's hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.addEvent(EFFECT1.getEvent(event.getSource()));
+
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
