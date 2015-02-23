4388096473aba1af05c9b53aea1b0f76339e79b5
PalladiaMors <unknown>
2015-02-23 11:19:27 +0100

    Add script for Healing Leaves

---
+++ release/Magarena/scripts/Healing_Leaves.groovy
@@ -0,0 +1,30 @@
+def EFFECT1 = MagicRuleEventAction.create("Target player gains 3 life.");
+
+def EFFECT2 = MagicRuleEventAction.create("Prevent the next 3 damage that would be dealt to target creature or player this turn.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicTargetChoice.Negative("target player"),
+                    MagicTargetChoice.Negative("target creature or player")
+                ),
+                this,
+                "Choose one\$ - target player gains 3 life; " +
+                "or prevent the next 3 damage that would be dealt to target creature or player this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.addEvent(EFFECT1.getEvent(event.getSource()));
+
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
