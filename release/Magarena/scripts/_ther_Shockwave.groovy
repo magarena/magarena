36e9e6115b7f2fe535ca04d85fc56d83cd64cff8
PalladiaMors <unknown>
2015-02-23 11:20:17 +0100

    Add script for Æther Shockwave

---
+++ release/Magarena/scripts/_ther_Shockwave.groovy
@@ -0,0 +1,30 @@
+def EFFECT1 = MagicRuleEventAction.create("Tap all Spirits.");
+
+def EFFECT2 = MagicRuleEventAction.create("Tap all non-Spirit creatures.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicChoice.NONE
+                ),
+                this,
+                "Choose one\$ - tap all Spirits; " +
+                "or tap all non-Spirit creatures."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(1)) {
+                game.addEvent(EFFECT1.getEvent(event.getSource()));
+
+            } else if (event.isMode(2)) {
+                game.addEvent(EFFECT2.getEvent(event.getSource()));
+            }
+        }
+    }
+]
