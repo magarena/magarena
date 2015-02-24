6f94f5ac9df36db639ebb9d4f03801fdb0776e97
melvinzhang <melvinzhang@gmail.com>
2015-02-24 10:29:59 +0800

    check intervening if condition on resolution

--- release/Magarena/scripts/Barren_Glory.groovy
+++ release/Magarena/scripts/Barren_Glory.groovy
@@ -1,10 +1,12 @@
 [
     new MagicAtYourUpkeepTrigger() {
+        public boolean ifCondition(final MagicPermanent permanent, final MagicPlayer you) {
+            final MagicTargetFilter<MagicPermanent> other = new MagicOtherPermanentTargetFilter(MagicTargetFilterFactory.PERMANENT_YOU_CONTROL, permanent);
+            return you.controlsPermanent(other) == false && you.getHandSize() == 0;
+        }
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return upkeepPlayer.controlsPermanent(new MagicOtherPermanentTargetFilter(
-                MagicTargetFilterFactory.PERMANENT_YOU_CONTROL, permanent)) == false &&
-                upkeepPlayer.getHandSize() == 0 ?
+            return ifCondition(permanent, upkeepPlayer) ?
                 new MagicEvent(
                     permanent,
                     this,
@@ -15,7 +17,9 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicLoseGameAction(event.getPlayer().getOpponent()));
-        };
+            if (ifCondition(event.getPermanent(), event.getPlayer())) {
+                game.doAction(new MagicLoseGameAction(event.getPlayer().getOpponent()));
+            }
+        }
     }
 ]
