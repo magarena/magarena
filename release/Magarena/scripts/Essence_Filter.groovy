c86b2fcea0183acb323c2ee4bb011ed0a84ad443
PalladiaMors <unknown>
2015-02-26 03:38:01 +0100

    Add script for Essence Filter

---
+++ release/Magarena/scripts/Essence_Filter.groovy
@@ -0,0 +1,34 @@
+def NONWHITE_ENCHANTMENT=new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicGame game,final MagicPlayer player,final MagicPermanent target) {
+        return !target.hasColor(MagicColor.White) && target.isEnchantment();
+    } 
+};
+
+def EFFECT1 = MagicRuleEventAction.create("Destroy all enchantments.");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicChoice.NONE
+                ),
+                this,
+                "Choose one\$ - destroy all enchantments; " +
+                "or destroy all nonwhite enchantments.\$" 
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isMode(2)) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(NONWHITE_ENCHANTMENT);
+                game.doAction(new MagicDestroyAction(targets));
+            } else {
+                event.executeModalEvent(game, EFFECT1, EFFECT1, EFFECT1);
+            }
+        }
+    }
+]
