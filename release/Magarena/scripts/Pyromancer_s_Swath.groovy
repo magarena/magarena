20fe5648be21127434ff1a3c90ea364de4120291
melvinzhang <melvinzhang@gmail.com>
2015-03-01 10:52:56 +0800

    simplify trigger with MagicDiscardHandEvent

--- release/Magarena/scripts/Pyromancer_s_Swath.groovy
+++ release/Magarena/scripts/Pyromancer_s_Swath.groovy
@@ -2,27 +2,17 @@
     new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.INCREASE_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            if (damage.getSource().isFriend(permanent) &&
-                damage.getSource().isInstantOrSorcerySpell()) {
-            damage.setAmount(damage.getAmount() + 2);
-            return MagicEvent.NONE;
+            if (damage.getSource().isInstantOrSorcerySpell() &&
+                damage.getSource().isFriend(permanent)) {
+                damage.setAmount(damage.getAmount() + 2);
             }
+            return MagicEvent.NONE;
         }
     },
-   new MagicAtEndOfTurnTrigger() {
+    new MagicAtEndOfTurnTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
-            new MagicEvent(
-                permanent,
-                player,
-                this,
-                "PN discards his or her hand."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getPlayer().getHandSize();
-                game.addEvent(new MagicDiscardEvent(event.getSource(),event.getPlayer(),amount));
+            return new MagicDiscardHandEvent(permanent);
         }
     }
 ]
