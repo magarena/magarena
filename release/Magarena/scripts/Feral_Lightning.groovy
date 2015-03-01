041894ee599e64b277a21bd4deac63fc90d2aa92
melvinzhang <melvinzhang@gmail.com>
2015-03-01 10:55:01 +0800

    change loop variable to idiomatic form

--- release/Magarena/scripts/Feral_Lightning.groovy
+++ release/Magarena/scripts/Feral_Lightning.groovy
@@ -12,13 +12,13 @@
         
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            for (int x=3;x>0;x--) {
+            for (int i = 0; i < 3; i++) {
                 game.doAction(new MagicPlayTokenAction(
                     event.getPlayer(), 
                     TokenCardDefinitions.get("3/1 red Elemental creature token with haste"),
                     MagicPlayMod.EXILE_AT_END_OF_TURN
                 ));
-            }     
+            } 
         }
     }
 ]
