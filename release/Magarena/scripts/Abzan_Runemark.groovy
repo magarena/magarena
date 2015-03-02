b0d5b560dda8d4064d71fb4bc6b8eec509c654bb
melvinzhang <melvinzhang@gmail.com>
2015-03-02 20:35:35 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Abzan_Runemark.groovy
+++ release/Magarena/scripts/Abzan_Runemark.groovy
@@ -6,18 +6,9 @@
         }
         @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return source.getController().controlsPermanent(MagicColor.Black) ||
-            source.getController().controlsPermanent(MagicColor.Green) &&
-            MagicStatic.acceptLinked(game, source, target);
-        }
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            pt.add(2, 2);
-        }
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return MagicStatic.acceptLinked(game, source, target);
+            return MagicStatic.acceptLinked(game, source, target) && 
+                  (source.getController().controlsPermanent(MagicColor.Black) ||
+                   source.getController().controlsPermanent(MagicColor.Green));
         }
     }
 ]
