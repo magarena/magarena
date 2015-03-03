1ee47162279d410c3ee46048f30789ddf374266c
melvinzhang <melvinzhang@gmail.com>
2015-03-03 10:16:00 +0800

    convert mixture of tabs and spaces to only spaces

--- release/Magarena/scripts/Spiritual_Sanctuary.groovy
+++ release/Magarena/scripts/Spiritual_Sanctuary.groovy
@@ -2,20 +2,20 @@
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return (upkeepPlayer.getNrOfPermanents(MagicSubType.Plains)>0) ?
-				new MagicEvent(
-					permanent,
-					upkeepPlayer,
-					this,
-					"PN gains 1 life."
-				):
+            return upkeepPlayer.controlsPermanent(MagicSubType.Plains) ?
+                new MagicEvent(
+                    permanent,
+                    upkeepPlayer,
+                    this,
+                    "PN gains 1 life."
+                ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game,final MagicEvent event) {
-			if (event.getPlayer().getNrOfPermanents(MagicSubType.Plains)>0) {
-				game.doAction(new MagicChangeLifeAction(event.getPlayer(), 1));
-			}
+            if (event.getPlayer().controlsPermanent(MagicSubType.Plains)) {
+                game.doAction(new MagicChangeLifeAction(event.getPlayer(), 1));
+            }
         }
     }
 ]
