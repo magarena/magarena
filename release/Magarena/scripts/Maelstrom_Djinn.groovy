96c8bedfe0d01835673bca9d0dfeabac966fa3cd
PalladiaMors <unknown>
2015-03-04 02:37:12 +0100

    Add script for Maelstrom Djinn

---
+++ release/Magarena/scripts/Maelstrom_Djinn.groovy
@@ -0,0 +1,20 @@
+def Vanishing = MagicAbility.getAbilityList("Vanishing 1");
+
+[
+    new MagicWhenTurnedFaceUpTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Put two time counters on SN and it gains vanishing."
+            )
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Time,2));
+            game.doAction(new MagicGainAbilityAction(event.getPermanent(),Vanishing,MagicStatic.Forever))
+        }
+    }
+]
