92e7139045f18a064401d0defc6ab4c29e218a3c
melvinzhang <melvinzhang@gmail.com>
2015-03-04 17:38:03 +0800

    correct token script, improve description of trigger

--- release/Magarena/scripts/Varchild_s_War_Riders.groovy
+++ release/Magarena/scripts/Varchild_s_War_Riders.groovy
@@ -2,22 +2,26 @@
     new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            game.doAction(new MagicChangeCountersAction(
+                permanent,
+                MagicCounterType.Age,
+                1
+            ));
             return new MagicEvent(
                 permanent,
                 new MagicMayChoice("Pay cumulative upkeep?"),
                 this,
-                "Have an opponent put a 1/1 red Survivor creature token onto the battlefield for each age counter on SN."
+                "PN may\$ have an opponent put a 1/1 red Survivor creature token onto the battlefield for each age counter on SN. " +
+                " If he or she doesn't, sacrifice SN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Age,1));
-            final int age = event.getPermanent().getCounters(MagicCounterType.Age);
             if (event.isYes()) {
                 game.doAction(new MagicPlayTokensAction(
                     event.getPlayer().getOpponent(),
                     TokenCardDefinitions.get("1/1 red Survivor creature token"),
-                    age
+                    event.getPermanent().getCounters(MagicCounterType.Age)
                 ));
             } else {
                 game.doAction(new MagicSacrificeAction(event.getPermanent()));
