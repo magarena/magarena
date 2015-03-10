b3b5c0cd72cb2ac8a5483ee5de41b064eff9c418
PalladiaMors <unknown>
2015-03-10 05:25:08 +0100

    Add script for Silumgar, the Drifting Death

---
+++ release/Magarena/scripts/Silumgar__the_Drifting_Death.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicWhenAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (permanent.isFriend(creature) &&
+                    permanent.hasSubType(MagicSubType.Dragon)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "Creatures defending player controls get -1/-1 until end of turn."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> creatures=
+                game.filterPermanents(event.getPlayer().getOpponent(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            for (final MagicPermanent creature : creatures) {
+                game.doAction(new MagicChangeTurnPTAction(creature, -1, -1));
+            }
+        }
+    }
+]
