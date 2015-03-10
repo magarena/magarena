f2571b28beac40f3586f3ed06ae87438de050d6a
PalladiaMors <unknown>
2015-03-10 04:46:03 +0100

    Add script for Goblin Diplomats

---
+++ release/Magarena/scripts/Goblin_Diplomats.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.MustAttack),
+        "Attacks"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [                 
+                    new MagicTapEvent(source)
+                ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Each creature attacks this turn if able."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> targets = game.filterPermanents(MagicTargetFilterFactory.CREATURE);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicGainAbilityAction(target,MagicAbility.AttacksEachTurnIfAble));
+            }
+        }
+    }
+]
