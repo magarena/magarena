cea8a5215830b8806cb747b2562716614383abb6
melvinzhang <melvinzhang@gmail.com>
2015-03-11 11:22:11 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Anthem_of_Rakdos.groovy
+++ release/Magarena/scripts/Anthem_of_Rakdos.groovy
@@ -1,23 +1,4 @@
 [
-    new MagicWhenAttacksTrigger(1) {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
-            return attacker.isFriend(permanent) ?
-                new MagicEvent(
-                    permanent,
-                    attacker,
-                    this,
-                    "RN gets +2/+0 until end of turn and SN deals 1 damage to PN."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeTurnPTAction(event.getRefPermanent(),2,0));
-            game.doAction(new MagicDealDamageAction(event.getSource(), event.getPlayer(), 1));
-        }
-    },
-    
     new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.INCREASE_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
