08b62bfd0464a274e0e0b8a9fecb6b9ed734e9b2
PalladiaMors <elvanin_1@hotmail.com>
2015-03-10 23:21:32 -0300

    discard cards with action before drawing new cards in Dark Deal

--- release/Magarena/scripts/Dark_Deal.groovy
+++ release/Magarena/scripts/Dark_Deal.groovy
@@ -11,20 +11,13 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int hand1 = event.getPlayer().getHandSize() - 1;
-            final int hand2 = event.getPlayer().getOpponent().getHandSize() - 1;
-                game.addEvent(new MagicDiscardEvent(
-                    event.getSource(),
-                    event.getPlayer(),
-                    event.getPlayer().getHandSize()
-                ));
-                game.addEvent(new MagicDiscardEvent(
-                    event.getSource(),
-                    event.getPlayer().getOpponent(),
-                    event.getPlayer().getOpponent().getHandSize()
-                ));
-                game.doAction(new MagicDrawAction(event.getPlayer(),hand1));
-                game.doAction(new MagicDrawAction(event.getPlayer().getOpponent(),hand2));
+            for (final MagicPlayer player : game.getAPNAP()) {
+                final MagicCardList hand = new MagicCardList(player.getHand());
+                for (final MagicCard card : hand) {
+                    game.doAction(new MagicDiscardCardAction(player,card));
+                }
+                game.doAction(new MagicDrawAction(player, hand.size() - 1));
+            }
         }
     }
 ]
