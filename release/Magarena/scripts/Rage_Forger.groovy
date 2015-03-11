f132ed7abea30aa6df2a6dff75d60f61ecaaba77
melvinzhang <melvinzhang@gmail.com>
2015-03-11 11:34:49 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Rage_Forger.groovy
+++ release/Magarena/scripts/Rage_Forger.groovy
@@ -20,35 +20,5 @@
                 game.doAction(new MagicChangeCountersAction(creature,MagicCounterType.PlusOne,1));
             }
         }
-    },
-    
-    new MagicWhenAttacksTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return creature.isFriend(permanent) &&
-                creature.hasCounters(MagicCounterType.PlusOne) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicMayChoice(
-                        MagicTargetChoice.NEG_TARGET_PLAYER
-                    ),
-                    new MagicDamageTargetPicker(1),
-                    creature,
-                    this,
-                    "PN may\$ have SN creature deal 1 damage to target player\$."
-                ):
-            MagicEvent.NONE;
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                event.processTargetPlayer(game, {
-                    final MagicPermanent permanent = event.getRefPermanent()
-                    final MagicDamage damage = new MagicDamage(permanent,it,1);
-                    game.doAction(new MagicDealDamageAction(damage));
-                });
-            }
-        }
     }
 ]
