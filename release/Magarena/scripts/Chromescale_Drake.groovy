77d3a5d29f843dde612b8cded4cdc9c10071ad10
PalladiaMors <unknown>
2015-03-11 20:07:31 +0100

    Add script for Chromescale Drake

---
+++ release/Magarena/scripts/Chromescale_Drake.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Reveal the top three cards of your library. Put all artifact cards revealed this way into your hand and the rest into your graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCardList top3 = event.getPlayer().getLibrary().getCardsFromTop(3);
+            for (final MagicCard top : top3) {
+                game.doAction(new MagicRevealAction(top3));
+                game.doAction(new MagicRemoveCardAction(
+                    top,
+                    MagicLocationType.OwnersLibrary
+                ));
+                game.doAction(new MagicMoveCardAction(
+                    top,
+                    MagicLocationType.OwnersLibrary,
+                    top.hasType(MagicType.Artifact) ?
+                      MagicLocationType.OwnersHand :
+                      MagicLocationType.Graveyard
+                ));
+            }
+        }
+    }
+]
