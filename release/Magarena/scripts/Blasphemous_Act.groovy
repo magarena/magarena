5aa174615e30b0dc09e7fda6a04224608ebdc50f
melvinzhang <melvinzhang@gmail.com>
2015-03-12 09:54:42 +0800

    use reduce method

--- release/Magarena/scripts/Blasphemous_Act.groovy
+++ release/Magarena/scripts/Blasphemous_Act.groovy
@@ -11,12 +11,13 @@
 
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
-            final MagicGame game = source.getGame();
-            final int n = game.filterPermanents(MagicTargetFilterFactory.CREATURE).size();
-            final int cost= Math.max(0,8-n)
-            return cost==0 ?
-                [new MagicPayManaCostEvent(source,"{R}")]:
-                [new MagicPayManaCostEvent(source,"{"+cost.toString()+"}{R}")];
+            final int n = source.getGame().getNrOfPermanents(MagicTargetFilterFactory.CREATURE);
+            return [
+                new MagicPayManaCostEvent(
+                    source,
+                    source.getCost().reduce(MagicCostManaType.Colorless, n)
+                )
+            ];
         }
     }
 ]
