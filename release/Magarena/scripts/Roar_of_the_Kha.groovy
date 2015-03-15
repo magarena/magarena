b729bcabf7883a00b677b717213ca07bfeed37dc
melvinzhang <melvinzhang@gmail.com>
2015-03-15 14:18:24 +0800

    use entwine instead of kicker in card script, use executeEvent instead of addEvent in groovy script

--- release/Magarena/scripts/Roar_of_the_Kha.groovy
+++ release/Magarena/scripts/Roar_of_the_Kha.groovy
@@ -1,40 +1,38 @@
-def EFFECT1 = MagicRuleEventAction.create("Creatures you control get +1/+1 until end of turn.");
+def TEXT1 = "Creatures you control get +1/+1 until end of turn."
 
-def EFFECT2 = MagicRuleEventAction.create("Untap all creatures you control.");
+def TEXT2 = "Untap all creatures you control."
 
-def CARDTEXT = "Choose one\$ — • Creatures you control get +1/+1 until end of turn." +
-               " • Untap all creatures you control. " +
-               "Choose both if you paid the entwine cost.";
+def EFFECT1 = MagicRuleEventAction.create(TEXT1);
+
+def EFFECT2 = MagicRuleEventAction.create(TEXT2);
+
+def CARDTEXT = "Choose one\$ â€”" +
+               " â€¢ " + TEXT1 +
+               " â€¢ " + TEXT2 +
+               " Choose both if you pay the entwine cost.";
 
 [
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
-            return payedCost.isKicked() ?
-                new MagicEvent(
-                    cardOnStack,
-                    this,
-                    CARDTEXT
-                ):
-                new MagicEvent(
-                    cardOnStack,
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.isKicked() ? 
+                    MagicChoice.NONE :
                     new MagicOrChoice(
                         MagicChoice.NONE,
                         MagicChoice.NONE
                     ),
-                    this,
-                    CARDTEXT
-                );
+                this,
+                CARDTEXT
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isKicked()) {
-                game.addEvent((EFFECT1).getEvent(event.getSource()));
-                game.addEvent((EFFECT2).getEvent(event.getSource()));
-            } else if (event.isMode(1)) {
-                game.addEvent((EFFECT1).getEvent(event.getSource()));
-            } else if (event.isMode(2)) {
-                game.addEvent((EFFECT2).getEvent(event.getSource()));
+                event.executeAllEvents(game, EFFECT1, EFFECT2);
+            } else {
+                event.executeModalEvent(game, EFFECT1, EFFECT2);
             }
         }
     }
