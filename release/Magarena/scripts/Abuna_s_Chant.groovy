28399abe43841db3360d133b06f1cf46e3bff3a4
melvinzhang <melvinzhang@gmail.com>
2015-03-15 14:36:07 +0800

    add Abuna's Chant

---
+++ release/Magarena/scripts/Abuna_s_Chant.groovy
@@ -0,0 +1,36 @@
+def TEXT1 = "You gain 5 life."
+
+def TEXT2 = "Prevent the next 5 damage that would be dealt to target creature this turn."
+
+def EFFECT1 = MagicRuleEventAction.create(TEXT1);
+
+def EFFECT2 = MagicRuleEventAction.create(TEXT2);
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.isKicked() ? 
+                    MagicTargetChoice.POS_TARGET_CREATURE :
+                    new MagicOrChoice(
+                        MagicChoice.NONE,
+                        MagicTargetChoice.POS_TARGET_CREATURE
+                    ),
+                this,
+                payedCost.isKicked() ?
+                    "PN gains 5 life. Prevent the next 5 damage that would be dealt to target creature this turn.\$":
+                    "Choose one\$ — • " + TEXT1 + " • " + TEXT2 + "\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isKicked()) {
+                event.executeAllEvents(game, EFFECT1, EFFECT2);
+            } else {
+                event.executeModalEvent(game, EFFECT1, EFFECT2);
+            }
+        }
+    }
+]
