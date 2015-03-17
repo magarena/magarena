c782f05fd909532312cca92ad3bb3399e2ee1332
melvinzhang <melvinzhang@gmail.com>
2015-03-17 10:49:20 +0800

    simplify expression with string interpolation

--- release/Magarena/scripts/Deadbridge_Chant.groovy
+++ release/Magarena/scripts/Deadbridge_Chant.groovy
@@ -23,7 +23,7 @@
                 } else {
                     game.doAction(new MagicRemoveCardAction(card,MagicLocationType.Graveyard));
                     game.doAction(new MagicMoveCardAction(card,MagicLocationType.Graveyard,MagicLocationType.OwnersHand));
-                    game.logAppendMessage(player,""+player.getName()+" returns ("+card.getName()+") to his or her hand.");
+                    game.logAppendMessage(player, "${player.getName()} returns (${card.getName()}) to his or her hand.");
                 }
             }
         }
