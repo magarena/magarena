magarena/magarena

commit d2e985a33b178ef2c76f5678391c3dfb705c7fb8

Updating player panels with new style (see issue #102). In this commit  player health, poison and damage prevention are displayed as overlays on the player image. The non-turn player image is greyscaled. The four player zone toggle buttons are not yet functional except to display the number of cards in the zone. The name of the active zone is now displayed at the bottom of the game board unless it is the player's hand.

src/magic/ui/widget/TabSelector.java
@@ -1,12 +1,5 @@
 package magic.ui.widget;
 
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import javax.swing.JToggleButton;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -15,10 +8,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
+@SuppressWarnings("serial")
 public class TabSelector extends JPanel implements ActionListener {
-
-    private static final long serialVersionUID = 1L;
 
     private static final Dimension HORIZONTAL_BUTTON_DIMENSION=new Dimension(28,20);
     private static final Dimension VERTICAL_BUTTON_DIMENSION=new Dimension(24,24);
@@ -53,7 +51,7 @@ public class TabSelector extends JPanel implements ActionListener {
             buttonDimension=HORIZONTAL_BUTTON_DIMENSION;
         }
 
-        buttons=new ArrayList<JToggleButton>();
+        buttons=new ArrayList<>();
 
     }
 
@@ -91,18 +89,12 @@ public class TabSelector extends JPanel implements ActionListener {
         }
     }
 
-//    public void addTab(final ImageIcon icon) {
-//
-//        addTab(icon,null);
-//    }
-
     private void showTab(final JToggleButton selectedButton) {
-        showTab(selectedButton, false);
+        showTab(selectedButton, isUserClick);
     }
     private void showTab(final JToggleButton selectedButton, final boolean userClick) {
         this.isUserClick = userClick;
         for (final JToggleButton button : buttons) {
-
             button.setSelected(button==selectedButton);
         }
 
@@ -117,6 +109,10 @@ public class TabSelector extends JPanel implements ActionListener {
 
     public boolean isUserClick() {
         return isUserClick;
+    }
+
+    public void setIsUserClick(final boolean b) {
+        this.isUserClick = b;
     }
 
 }

src/magic/data/MagicIcon.java
@@ -66,10 +66,14 @@ public enum MagicIcon {
     POISON("poison.png"),
     HAND("hand.gif"),
     HAND2("hand2.png"),
+    HAND_ZONE("b_hand_zone.png"),
     LIBRARY2("library2.gif"),
+    LIBRARY_ZONE("b_library_zone.png"),
     GRAVEYARD("graveyard.gif"),
     GRAVEYARD2("graveyard2.gif"),
+    GRAVEYARD_ZONE("b_graveyard_zone.png"),
     EXILE("exile.png"),
+    EXILE_ZONE("b_exile_zone.png"),
     DIFFICULTY("difficulty.png"),
     CANNOTTAP("cannottap.png"),
     SLEEP("sleep.gif"),

src/magic/model/MagicPlayer.java
@@ -1,5 +1,10 @@
 package magic.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import magic.model.action.MagicLoseGameAction;
 import magic.model.choice.MagicBuilderManaCost;
 import magic.model.event.MagicActivationPriority;
@@ -8,15 +13,10 @@ import magic.model.event.MagicSourceManaActivation;
 import magic.model.mstatic.MagicLayer;
 import magic.model.mstatic.MagicPermanentStatic;
 import magic.model.mstatic.MagicStatic;
+import magic.model.player.AiPlayer;
 import magic.model.target.MagicTarget;
-import magic.model.target.MagicTargetType;
 import magic.model.target.MagicTargetFilter;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import magic.model.target.MagicTargetType;
 
 public class MagicPlayer extends MagicObjectImpl implements MagicTarget, MagicMappable<MagicPlayer> {
 
@@ -801,5 +801,9 @@ public class MagicPlayer extends MagicObjectImpl implements MagicTarget, MagicMa
 
     public boolean isHuman() {
         return !getPlayerDefinition().isArtificial();
+    }
+
+    public boolean isAiPlayerProfile() {
+        return getPlayerDefinition().getPlayerProfile() instanceof AiPlayer;
     }
 }

src/magic/model/MagicPlayerZone.java
@@ -1,0 +1,8 @@
+package magic.model;
+
+public enum MagicPlayerZone {
+    HAND,
+    LIBRARY,
+    GRAVEYARD,
+    EXILE;    
+}

commit 238c834c079ca1ecc05e96ef4760762bc60c3f69

fix: regression introduced in d2e985a3. From hand, switch to another zone and back again. Select card and full screen zone is displayed.

src/magic/ui/widget/TabSelector.java
@@ -90,7 +90,7 @@ public class TabSelector extends JPanel implements ActionListener {
     }
 
     private void showTab(final JToggleButton selectedButton) {
-        showTab(selectedButton, isUserClick);
+        showTab(selectedButton, false);
     }
     private void showTab(final JToggleButton selectedButton, final boolean userClick) {
         this.isUserClick = userClick;