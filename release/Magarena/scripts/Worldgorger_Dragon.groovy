fee51129b7e11d94b952675febf734cf292b16c5
melvinzhang <melvinzhang@gmail.com>
2015-03-19 13:56:43 +0800

    use MagicWhenSelfLeavesPlayTrigger, remove player parameter in MagicReturnLinkedExileAction

--- release/Magarena/scripts/Worldgorger_Dragon.groovy
+++ release/Magarena/scripts/Worldgorger_Dragon.groovy
@@ -10,37 +10,33 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicTargetFilter<MagicPermanent> permanents =
-                new MagicOtherPermanentTargetFilter(
-                MagicTargetFilterFactory.PERMANENT_YOU_CONTROL,event.getPermanent());
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(permanents);
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.PERMANENT_YOU_CONTROL,
+                event.getPermanent()
+            );
+            final Collection<MagicPermanent> targets = event.getPlayer().filterPermanents(filter);
             for (final MagicPermanent target : targets) {
                 game.doAction(new MagicExileLinkAction(
                     event.getPermanent(),
                     target
                 ));
+            }
         }
-    }
-},
-    new MagicWhenLeavesPlayTrigger() {
+    },
+    new MagicWhenSelfLeavesPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicRemoveFromPlayAction act) {
-           
-            return act.isPermanent(permanent) ?
-                return new MagicEvent(
-                    permanent,
-                    this,
-                    "Return the exiled cards to the battlefield under their owners' control."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                this,
+                "Return the exiled cards to the battlefield under their owners' control."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new MagicReturnLinkedExileAction(
                 event.getPermanent(),
-                MagicLocationType.Play,
-                event.getPlayer()
+                MagicLocationType.Play
             ));
         }
     }
