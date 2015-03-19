4a28ff6a496cea33ba2ea2112ad448df01594ba9
melvinzhang <melvinzhang@gmail.com>
2015-03-19 14:04:12 +0800

    use MagicWhenSelfLeavesPlayTrigger instead of manual checking, replace you in description with PN

--- release/Magarena/scripts/Slithermuse.groovy
+++ release/Magarena/scripts/Slithermuse.groovy
@@ -1,21 +1,20 @@
 [
-    new MagicWhenLeavesPlayTrigger() {
+    new MagicWhenSelfLeavesPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicRemoveFromPlayAction act) {
-            return act.isPermanent(permanent) &&
-                   permanent.getOpponent().getHandSize() >
+            return permanent.getOpponent().getHandSize() >
                    permanent.getController().getHandSize() ?
                 new MagicEvent(
                     permanent,
                     this,
-                    "If opponent has more cards in hand than you, draw cards equal to the difference."
+                    "If opponent has more cards in hand than PN, PN draws cards equal to the difference."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getPermanent().getOpponent().getHandSize() - 
-                               event.getPermanent().getController().getHandSize();
+            final int amount = event.getPlayer().getOpponent().getHandSize() - 
+                               event.getPlayer().getHandSize();
             game.doAction(new MagicDrawAction(event.getPlayer(),amount));
         }
     }
