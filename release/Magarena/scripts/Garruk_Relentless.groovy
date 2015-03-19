0440b8f067815d76388b3af78d8c26c5ca7e4697
PalladiaMors <unknown>
2015-03-19 13:43:51 +0100

    Add script for Garruk Relentless

---
+++ release/Magarena/scripts/Garruk_Relentless.groovy
@@ -0,0 +1,48 @@
+def EFFECT = MagicRuleEventAction.create("Transform SN."); 
+
+[
+    new MagicStatic(MagicLayer.Game) {
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return source.getCounters(MagicCounterType.Loyalty) <= 2;
+        }
+        @Override
+        public void modGame(final MagicPermanent source, final MagicGame game) {
+            game.doAction(new MagicPutStateTriggerOnStackAction(
+                 EFFECT.getEvent(source)
+            ));               
+        }
+    },
+    new MagicPlaneswalkerActivation(0) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.NEG_TARGET_CREATURE,
+                this,
+                "SN deals 3 damage to target creature. That creature deals damage equal to its power to him."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicDealDamageAction(event.getSource(),it,3));
+                game.doAction(new MagicDealDamageAction(it,event.getPermanent(),it.getPower()));
+            });
+        }
+    },
+    new MagicPlaneswalkerActivation(0) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+             return new MagicEvent(
+                source,
+                this,
+                "Put a 2/2 green Wolf creature token onto the battlefield."
+            );
+       }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicPlayTokenAction(event.getPlayer(),TokenCardDefinitions.get("2/2 green Wolf creature token")));
+        }
+    }
+]
