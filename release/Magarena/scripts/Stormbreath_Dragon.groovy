9618b722edd1edc860d3e0c299d6bc685e65b30a
melvinzhang <melvinzhang@gmail.com>
2015-03-19 14:07:59 +0800

    replace getPermanent().getOpponent() with getPlayer().getOpponent()

--- release/Magarena/scripts/Stormbreath_Dragon.groovy
+++ release/Magarena/scripts/Stormbreath_Dragon.groovy
@@ -6,16 +6,19 @@
                 new MagicEvent(
                     permanent,
                     this,
-                    "SN deals damage to each opponent equal to the number of cards in that players hand."
+                    "SN deals damage to each opponent equal to the number of cards in that player's hand."
                 ):
                 MagicEvent.NONE;
         }
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer opponent=event.getPermanent().getOpponent();
-            final MagicDamage damage=new MagicDamage(event.getPermanent(),opponent,opponent.getHandSize());
-            game.doAction(new MagicDealDamageAction(damage));
+            final MagicPlayer opponent = event.getPlayer().getOpponent();
+            game.doAction(new MagicDealDamageAction(
+                event.getSource(),
+                opponent,
+                opponent.getHandSize()
+            ));
         }
     }
 ]
