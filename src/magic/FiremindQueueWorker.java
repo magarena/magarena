d5f3aad8445e047d7a1439bdfd98e1e6eae012a5
Mike <mike@otep.ch>
2015-03-20 16:09:45 +0100

    fix indentation

--- src/magic/FiremindQueueWorker.java
+++ src/magic/FiremindQueueWorker.java
@@ -1,7 +1,5 @@
 package magic;
 
-import magic.firemind.FiremindClient;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,57 +7,54 @@ import java.io.Reader;
 
 public class FiremindQueueWorker {
 
+    public static boolean shutDownOnEmptyQueue = false;
 
-	public static boolean shutDownOnEmptyQueue = false;
+    public static void main(final String[] args) {
+        parseArguments(args);
 
-	public static void main(final String[] args) {
-		parseArguments(args);
+        int lastExitStatus = 0;
+        while (!(shutDownOnEmptyQueue && lastExitStatus == 1)) {
+            ProcessBuilder pb = new ProcessBuilder("java", "-noverify", "-cp",
+                    "Magarena.jar", "magic.firemind.FiremindDuelRunner");
+            pb.redirectErrorStream(true);
+            try {
+                Process p = pb.start();
+                Reader reader = new InputStreamReader(p.getInputStream());
+                BufferedReader br = new BufferedReader(reader);
+                String line;
+                while ((line = br.readLine()) != null)
+                    System.out.println(line);
 
-		int lastExitStatus = 0;
-		while (!(shutDownOnEmptyQueue && lastExitStatus == 1)) {
-			ProcessBuilder pb = new ProcessBuilder("java", "-noverify", "-cp",
-					"Magarena.jar", "magic.firemind.FiremindDuelRunner");
-			pb.redirectErrorStream(true);
-			try {
-				Process p = pb.start();
-				Reader reader = new InputStreamReader(p.getInputStream());
-				BufferedReader br = new BufferedReader(reader);
-				String line;
-				while ((line = br.readLine()) != null)
-					System.out.println(line);
-				
-				p.waitFor();
-				reader.close();
-				lastExitStatus = p.exitValue();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			if(lastExitStatus != 0){
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					System.out.println("Woken");
-				}
-			}
-		}
+                p.waitFor();
+                reader.close();
+                lastExitStatus = p.exitValue();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            if (lastExitStatus != 0) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("Woken");
+                }
+            }
+        }
 
-	}
+    }
 
-	private static boolean parseArguments(final String[] args) {
-		boolean validArgs = true;
-		for (int i = 0; i < args.length; i += 1) {
-			final String curr = args[i];
-			if ("--self-terminate".equals(curr)) {
-				shutDownOnEmptyQueue = true;
-			}
-		}
-		return validArgs;
-	}
-
-	
+    private static boolean parseArguments(final String[] args) {
+        boolean validArgs = true;
+        for (int i = 0; i < args.length; i += 1) {
+            final String curr = args[i];
+            if ("--self-terminate".equals(curr)) {
+                shutDownOnEmptyQueue = true;
+            }
+        }
+        return validArgs;
+    }
 
 }
