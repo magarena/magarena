9244913c59142eb84a2251a0362c729704ba2459
lodici <spr11766@fastmail.net>
2015-03-21 23:49:12 +0000

    extract NewTurnPanel class from GameStatusPanel.

--- src/magic/ui/duel/viewer/GameStatusPanel.java
+++ src/magic/ui/duel/viewer/GameStatusPanel.java
@@ -90,46 +90,6 @@ public class GameStatusPanel extends TexturedPanel implements ChangeListener {
         refreshLayout();
     }
 
-    private class NewTurnPanel extends TexturedPanel {
-
-        private final MigLayout migLayout = new MigLayout("flowy, gapy 0");
-        private final JLabel iconLabel = new JLabel();
-        private final JLabel turnLabel = new JLabel();
-        private final JLabel playerLabel = new JLabel();
-
-        public NewTurnPanel() {
-            setLookAndFeel();
-            refreshLayout();
-        }
-
-        private void setLookAndFeel() {
-            setOpaque(true);
-            setBorder(BorderFactory.createLineBorder(MagicStyle.getTheme().getColor(Theme.COLOR_TITLE_BACKGROUND), 1));
-            //
-            turnLabel.setFont(turnLabel.getFont().deriveFont(16f));
-            turnLabel.setForeground(MagicStyle.getTheme().getColor(Theme.COLOR_TEXT_FOREGROUND));
-            turnLabel.setHorizontalAlignment(SwingConstants.CENTER);
-            //
-            playerLabel.setFont(turnLabel.getFont().deriveFont(20f));
-            playerLabel.setForeground(MagicStyle.getTheme().getColor(Theme.COLOR_TEXT_FOREGROUND));
-            playerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        }
-
-        private void refreshLayout() {
-            setLayout(migLayout);
-            add(iconLabel, "alignx center");
-            add(playerLabel, "w 100%, h 100%");
-            add(turnLabel, "w 100%, h 100%");
-        }
-
-        public void refreshData(final MagicGame game) {
-            iconLabel.setIcon(IconImages.getIconSize3(game.getTurnPlayer().getPlayerDefinition()));
-            turnLabel.setText("Turn " + game.getTurn());
-            playerLabel.setText(game.getTurnPlayer().getName());
-        }
-
-    }
-
     private class TurnStatusPanel extends JPanel {
 
         private final MigLayout miglayout = new MigLayout("insets 0, gap 0, flowy");
