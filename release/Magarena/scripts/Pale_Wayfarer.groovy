e483309873694154b4db061a3c65dc8979eda1f8
PalladiaMors <unknown>
2015-03-22 17:01:31 +0100

    Add script for Pale Wayfarer

---
+++ release/Magarena/scripts/Pale_Wayfarer.groovy
@@ -0,0 +1,44 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    game.doAction(new MagicGainAbilityAction(
+        event.getRefPermanent(),
+        event.getChosenColor().getProtectionAbility()
+    ));
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Protection"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                    new MagicPayManaCostEvent(source,"{2}{W}{W}"),
+                    new MagicUntapEvent(source)
+                ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.POS_TARGET_CREATURE,
+                this,
+                "Target creature\$ gains protection from the color of its controller's choice until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    it.getController(),
+                    MagicColorChoice.ALL_INSTANCE,
+                    it,
+                    action,
+                    "Chosen color\$."
+                ));
+            });
+        }
+    }
+]
