02b64ea6a134a32da1b6985181bee76b284e44bb
melvinzhang <melvinzhang@gmail.com>
2015-03-22 17:33:16 +0800

    fix fateseal ability, correct indentation

--- release/Magarena/scripts/Jace__the_Mind_Sculptor.groovy
+++ release/Magarena/scripts/Jace__the_Mind_Sculptor.groovy
@@ -1,3 +1,14 @@
+def FATESEAL = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicPlayer PN = event.getPlayer();
+    if (event.isYes()) {
+        game.logAppendMessage(PN, "${PN} move the card to the bottom.");
+        game.doAction(new MagicScryAction(event.getRefPlayer()));
+    } else {
+        game.logAppendMessage(PN, "${PN} puts the card back on top.");
+    }
+}
+
 [
     new MagicPlaneswalkerActivation(2, "Look") {
         @Override
@@ -12,9 +23,16 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
-            for (final MagicCard card : it.getLibrary().getCardsFromTop(1)) {
-                game.doAction(new MagicLookAction(card, "top card of target player's library"));
-                game.addEvent(MagicRuleEventAction.create("Scry 1.").getEvent(event.getSource()));
+                for (final MagicCard card : it.getLibrary().getCardsFromTop(1)) {
+                    game.doAction(new MagicLookAction(card, event.getPlayer(), "top card of ${it}'s library"));
+                    game.addEvent(new MagicEvent(
+                        event.getSource(),
+                        event.getPlayer(),
+                        new MagicMayChoice("Put the card on the bottom of ${it}'s library?"),
+                        it,
+                        FATESEAL,
+                        ""
+                    ));
                 }
             });
         }
@@ -66,13 +84,13 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
-            for (final MagicCard card : new MagicCardList(it.getLibrary())) {
-                game.doAction(new MagicRemoveCardAction(card, MagicLocationType.OwnersLibrary));
-                game.doAction(new MagicMoveCardAction(card, MagicLocationType.OwnersLibrary, MagicLocationType.Exile));
+                for (final MagicCard card : new MagicCardList(it.getLibrary())) {
+                    game.doAction(new MagicRemoveCardAction(card, MagicLocationType.OwnersLibrary));
+                    game.doAction(new MagicMoveCardAction(card, MagicLocationType.OwnersLibrary, MagicLocationType.Exile));
                 }
-            for (final MagicCard hand : new MagicCardList(it.getHand())) {
-                game.doAction(new MagicRemoveCardAction(hand, MagicLocationType.OwnersHand));
-                game.doAction(new MagicMoveCardAction(hand, MagicLocationType.OwnersHand, MagicLocationType.OwnersLibrary));
+                for (final MagicCard hand : new MagicCardList(it.getHand())) {
+                    game.doAction(new MagicRemoveCardAction(hand, MagicLocationType.OwnersHand));
+                    game.doAction(new MagicMoveCardAction(hand, MagicLocationType.OwnersHand, MagicLocationType.OwnersLibrary));
                 }
             });
         }
