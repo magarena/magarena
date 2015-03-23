f83909111937414e299e9940a3b816f4b1639e72
melvinzhang <melvinzhang@gmail.com>
2015-03-23 09:23:16 +0800

    replace multiple if statements with for loop over colors

--- release/Magarena/scripts/Empty_Shrine_Kannushi.groovy
+++ release/Magarena/scripts/Empty_Shrine_Kannushi.groovy
@@ -2,20 +2,10 @@
     new MagicStatic(MagicLayer.Ability) {
         @Override
         public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            if (permanent.getController().controlsPermanent(MagicColor.Black)) {
-                permanent.addAbility(MagicAbility.ProtectionFromBlack, flags);
-            }
-            if (permanent.getController().controlsPermanent(MagicColor.Blue)) {
-                permanent.addAbility(MagicAbility.ProtectionFromBlue, flags);
-            }
-            if (permanent.getController().controlsPermanent(MagicColor.Green)) {
-                permanent.addAbility(MagicAbility.ProtectionFromGreen, flags);
-            }
-            if (permanent.getController().controlsPermanent(MagicColor.Red)) {
-                permanent.addAbility(MagicAbility.ProtectionFromRed, flags);
-            }
-            if (permanent.getController().controlsPermanent(MagicColor.White)) {
-                permanent.addAbility(MagicAbility.ProtectionFromWhite, flags);
+            for (final MagicColor color : MagicColor.values()) {
+                if (permanent.getController().controlsPermanent(color)) {
+                    permanent.addAbility(color.getProtectionAbility(), flags);
+                }
             }
         }
     }
