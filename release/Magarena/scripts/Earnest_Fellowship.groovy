07d88937ba9011caef9eb06bcf42b25a3b55504c
melvinzhang <melvinzhang@gmail.com>
2015-03-23 09:26:28 +0800

    merge multiple static into one with for loop

--- release/Magarena/scripts/Earnest_Fellowship.groovy
+++ release/Magarena/scripts/Earnest_Fellowship.groovy
@@ -5,63 +5,11 @@
     ) {
         @Override
         public void modAbilityFlags(final MagicPermanent source, final MagicPermanent target, final Set<MagicAbility> flags) {
-            target.addAbility(MagicAbility.ProtectionFromBlack, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-            return target.hasColor(MagicColor.Black);
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilterFactory.CREATURE
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent target, final Set<MagicAbility> flags) {
-            target.addAbility(MagicAbility.ProtectionFromBlue, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-            return target.hasColor(MagicColor.Blue);
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilterFactory.CREATURE
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent target, final Set<MagicAbility> flags) {
-            target.addAbility(MagicAbility.ProtectionFromGreen, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-            return target.hasColor(MagicColor.Green);
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilterFactory.CREATURE
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent target, final Set<MagicAbility> flags) {
-            target.addAbility(MagicAbility.ProtectionFromRed, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-            return target.hasColor(MagicColor.Red);
-        }
-    },
-    new MagicStatic(
-        MagicLayer.Ability,
-        MagicTargetFilterFactory.CREATURE
-    ) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent target, final Set<MagicAbility> flags) {
-            target.addAbility(MagicAbility.ProtectionFromWhite, flags);
-        }
-        @Override
-        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-            return target.hasColor(MagicColor.White);
+            for (final MagicColor color : MagicColor.values()) {
+                if (target.hasColor(color)) {
+                    target.addAbility(color.getProtectionAbility(), flags);
+                }
+            }
         }
     }
 ]
