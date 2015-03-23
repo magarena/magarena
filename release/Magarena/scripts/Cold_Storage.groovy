a16f4df2c558968b79f5a69ea2458a90495bd5c8
PalladiaMors <unknown>
2015-03-23 17:56:37 +0100

    Add script for Cold Storage

---
+++ release/Magarena/scripts/Cold_Storage.groovy
@@ -0,0 +1,64 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Exile"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{3}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
+                this,
+                "Exile target creature\$ you control."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new MagicExileLinkAction(
+                    event.getPermanent(),
+                    it
+                )); 
+            });
+        }
+    },
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Return"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Return each creature card exiled with SN to the battlefield under your control."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicReturnLinkedExileAction(
+                event.getPermanent(),
+                MagicLocationType.Play,
+                event.getPlayer()
+            ));
+        }
+    }
+]
