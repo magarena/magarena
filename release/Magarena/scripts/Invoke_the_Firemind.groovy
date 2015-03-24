b12f68840df6a630325dba51ec7fa9390e56ad65
PalladiaMors <unknown>
2015-03-24 15:26:23 +0100

    Add script for Invoke the Firemind

---
+++ release/Magarena/scripts/Invoke_the_Firemind.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicChoice.NONE,
+                    MagicTargetChoice.NEG_TARGET_CREATURE_OR_PLAYER
+                ),
+                payedCost.getX(),
+                this,
+                "Choose one\$ - draw X cards; " +
+                "or SN deals X damage to target creature or player.\$" 
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int X = event.getRefInt();
+            if (event.isMode(1)) {
+                game.doAction(new MagicDrawAction(event.getPlayer(),X));
+            } else if (event.isMode(2)) {
+                event.processTarget(game, {
+                    game.doAction(new MagicDealDamageAction(event.getSource(),it,X));
+                });
+            }
+        }
+    }
+]
