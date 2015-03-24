16fe3f3a4067443606eed9869dde4348746d23de
PalladiaMors <unknown>
2015-03-24 15:01:25 +0100

    Add script for Energy Bolt

---
+++ release/Magarena/scripts/Energy_Bolt.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicTargetChoice.NEG_TARGET_PLAYER,
+                    MagicTargetChoice.POS_TARGET_PLAYER
+                ),
+                payedCost.getX(),
+                this,
+                "Choose one\$ - SN deals X damage to target player; " +
+                "or target player gains X life.\$" 
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int X = event.getRefInt();
+            if (event.isMode(1)) {
+                event.processTargetPlayer(game, {
+                    game.doAction(new MagicDealDamageAction(event.getSource(),it,X));
+                });
+            } else if (event.isMode(2)) {
+                event.processTargetPlayer(game, {
+                    game.doAction(new MagicChangeLifeAction(it,X));
+                });
+            }
+        }
+    }
+]
