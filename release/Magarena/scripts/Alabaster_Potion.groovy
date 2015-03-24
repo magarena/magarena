f05bd127b71f9c1447557e0c89a047ffdf341971
PalladiaMors <unknown>
2015-03-24 15:07:47 +0100

    Add script for Alabaster Potion

---
+++ release/Magarena/scripts/Alabaster_Potion.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicOrChoice(
+                    MagicTargetChoice.POS_TARGET_PLAYER,
+                    MagicTargetChoice.POS_TARGET_CREATURE_OR_PLAYER
+                ),
+                payedCost.getX(),
+                this,
+                "Choose one\$ - target player gains X life; " +
+                "or prevent the next X damage that would be dealt to target creature or player this turn.\$" 
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int X = event.getRefInt();
+            if (event.isMode(1)) {
+                event.processTargetPlayer(game, {
+                    game.doAction(new MagicChangeLifeAction(it,X));
+                });
+            } else if (event.isMode(2)) {
+                event.processTarget(game, {
+                game.doAction(new MagicPreventDamageAction(it,X));
+                });
+            }
+        }
+    }
+]
