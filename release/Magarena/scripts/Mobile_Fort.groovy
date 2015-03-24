11509e9c017003cf438b7cec10aa2493b30263be
PalladiaMors <unknown>
2015-03-24 17:56:57 +0100

    Add script for Mobile Fort

---
+++ release/Magarena/scripts/Mobile_Fort.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "-Defender"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                    new MagicPayManaCostEvent(source, "{3}"),
+                    new MagicPlayAbilityEvent(source)
+                ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +3/-1 until end of turn. SN loses defender until end of turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent,3,-1));
+            game.doAction(MagicChangeStateAction.Set(event.getPermanent(),MagicPermanentState.CanAttackWithDefender));
+        }
+    }
+]
