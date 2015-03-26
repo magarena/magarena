589695a4f6ae1d64b96b3c8058589b865d757da8
PalladiaMors <unknown>
2015-03-26 23:54:00 +0100

    Add script for Infernal Contract

---
+++ release/Magarena/scripts/Infernal_Contract.groovy
@@ -0,0 +1,20 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Draw four cards. You lose half your life, rounded up."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MagicDrawAction(event.getPlayer(),4));
+            game.doAction(new MagicChangeLifeAction(
+                event.getPlayer(),
+                -(int)Math.ceil(event.getPlayer().getLife()/2)
+            ));
+        }
+    }
+]
