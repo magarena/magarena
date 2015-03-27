7d024ceae305e17fbbfcc2b637c89dd981ccf1ba
PalladiaMors <elvanin_1@hotmail.com>
2015-03-27 08:59:01 -0300

    Update Natural_Affinity.groovy

--- release/Magarena/scripts/Natural_Affinity.groovy
+++ release/Magarena/scripts/Natural_Affinity.groovy
@@ -25,7 +25,8 @@ def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final Collection<MagicPermanent> targets = game.filterPermanents(MagicTargetFilterFactory.LAND);
-            for (final MagicPermanent target : targets) {                game.doAction(new MagicBecomesCreatureAction(target,PT,ST));
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicBecomesCreatureAction(target,PT,ST));
             }
         }
     }
