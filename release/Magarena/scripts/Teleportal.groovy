893fb669371ff19e7fd946c8f38eedf0c1898a86
melvinzhang <melvinzhang@gmail.com>
2015-03-28 18:11:42 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Teleportal.groovy
+++ release/Magarena/scripts/Teleportal.groovy
@@ -1,23 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                MagicTargetChoice.TARGET_CREATURE_YOU_CONTROL,
-                MagicUnblockableTargetPicker.create(),
-                this,
-                "Target creature\$ you control gets +1/+0 until end of turn and can't be blocked this turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new MagicChangeTurnPTAction(it, 1, 0));
-                game.doAction(new MagicGainAbilityAction(it, MagicAbility.Unblockable));
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Pump) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
