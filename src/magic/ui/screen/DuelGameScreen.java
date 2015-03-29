8d5fa5c94dcf74dfa9d9853da812604d3aaad364
melvinzhang <melvinzhang@gmail.com>
2015-03-29 18:16:45 +0800

    game.log is cleared per game session when the program starts up
    Revert "fixes #69 : game.log not cleared."
    
    This reverts commit 018ec3d9259dd5d440d6f2984bd80577f84ba461.

--- src/magic/ui/screen/DuelGameScreen.java
+++ src/magic/ui/screen/DuelGameScreen.java
@@ -8,7 +8,6 @@ import javax.swing.SwingWorker;
 import magic.data.GeneralConfig;
 import magic.model.MagicDuel;
 import magic.model.MagicGame;
-import magic.model.MagicGameLog;
 import magic.ui.MagicFrame;
 import magic.ui.ScreenController;
 import magic.ui.ScreenOptionsOverlay;
@@ -32,7 +31,6 @@ public class DuelGameScreen extends AbstractScreen implements IOptionsMenu {
         final SwingWorker<MagicGame, Void> worker = new SwingWorker<MagicGame, Void> () {
             @Override
             protected MagicGame doInBackground() throws Exception {
-                MagicGameLog.initialize();
                 config.setTextView(false);
                 duel.updateDifficulty();
                 return duel.nextGame();
