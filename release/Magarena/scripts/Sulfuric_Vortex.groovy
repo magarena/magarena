c512d360683ce77790405767b907546b377e851c
PalladiaMors <unknown>
2015-03-30 22:41:15 +0200

    Add script for Sulfuric Vortex

---
+++ release/Magarena/scripts/Sulfuric_Vortex.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicIfLifeWouldChangeTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicChangeLifeAction act) {
+            if (act.getLifeChange() > 0) {
+                act.setLifeChange(0);
+            }
+            return MagicEvent.NONE;
+        }
+    },
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                upkeepPlayer,
+                this,
+                "PN loses 2 life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            game.doAction(new MagicDealDamageAction(event.getPermanent(),event.getPlayer(),2));
+        }
+    }
+]
