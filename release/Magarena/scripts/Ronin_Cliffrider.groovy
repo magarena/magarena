4aed43c6922103c9b7b27e5f3c5fe15b7f5b97c2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-03-31 18:20:40 +0100

    fix Ronin Cliffrider groovy

--- release/Magarena/scripts/Ronin_Cliffrider.groovy
+++ release/Magarena/scripts/Ronin_Cliffrider.groovy
@@ -15,11 +15,9 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
                 final MagicSource source = event.getSource();
-                final MagicPlayer defendingPlayer = event.getPlayer().getOpponent();
                 final Collection<MagicPermanent> creatures =
-                        game.filterPermanents(defendingPlayer,MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+                        game.filterPermanents(event.getPlayer().getOpponent(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
                 for (final MagicPermanent creature : creatures) {
-                    final MagicDamage damage = new MagicDamage();
                     game.doAction(new MagicDealDamageAction(source,creature,1));
                 }
             }
