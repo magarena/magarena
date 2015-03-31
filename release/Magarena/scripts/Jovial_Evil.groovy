946d84285b18b52f212a49b491edd6ab33cbb519
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-03-31 15:58:05 +0100

    add missing space to Jovial Evil's log text

--- release/Magarena/scripts/Jovial_Evil.groovy
+++ release/Magarena/scripts/Jovial_Evil.groovy
@@ -13,7 +13,7 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPlayer(game, {
                 final int amount = it.getNrOfPermanents(MagicTargetFilterFactory.WHITE_CREATURE) * 2;
-                game.logAppendMessage(event.getPlayer(),"(X="+amount+")");
+                game.logAppendMessage(event.getPlayer()," (X="+amount+")");
                 if (amount>0) {
                     game.doAction(new MagicDealDamageAction(event.getSource(),it,amount));
                 }
