74729f328bddc3cdedfe601925b03ce34af4a2ce
PalladiaMors <unknown>
2015-04-01 21:21:26 +0200

    Add script for Rewind

---
+++ release/Magarena/scripts/Rewind.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$. Untap up to four lands."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                game.doAction(new MagicCounterItemOnStackAction(it));
+            });
+            game.addEvent(new MagicRepeatedPermanentsEvent(
+                event.getSource(),
+                MagicTargetChoice.TARGET_LAND,
+                4,
+                MagicChainEventFactory.Untap
+            ));
+        }
+    }
+]
