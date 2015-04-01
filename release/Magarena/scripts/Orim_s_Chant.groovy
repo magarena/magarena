5e50002ab22c69a534c44e6dd6eca53c74df58b2
PalladiaMors <unknown>
2015-04-01 16:24:04 +0200

    Add script for Orim's Chant

---
+++ release/Magarena/scripts/Orim_s_Chant.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                MagicTargetChoice.NEG_TARGET_PLAYER,
+                this,
+                "Target player\$ can't cast spells this turn. If SN was kicked, creatures can't attack this turn."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.doAction(new MagicChangePlayerStateAction(
+                    it,
+                    MagicPlayerState.CantCastSpells
+                ));
+            });
+            if (event.isKicked()) {
+            final Collection<MagicPermanent> creatures = game.filterPermanents(MagicTargetFilterFactory.CREATURE);
+            for (final MagicPermanent creature : creatures) {
+                game.doAction(new MagicGainAbilityAction(creature, MagicAbility.CannotAttack));
+                }
+            }
+        }
+    }
+]
