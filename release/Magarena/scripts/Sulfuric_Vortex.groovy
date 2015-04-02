b8cdb12f84da8c444f5eafc02dcd9a5610099be9
melvinzhang <melvinzhang@gmail.com>
2015-04-02 20:33:46 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Sulfuric_Vortex.groovy
+++ release/Magarena/scripts/Sulfuric_Vortex.groovy
@@ -7,20 +7,5 @@
             }
             return MagicEvent.NONE;
         }
-    },
-    new MagicAtUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return new MagicEvent(
-                permanent,
-                upkeepPlayer,
-                this,
-                "SN deals 2 damage to PN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game,final MagicEvent event) {
-            game.doAction(new MagicDealDamageAction(event.getPermanent(),event.getPlayer(),2));
-        }
     }
 ]
