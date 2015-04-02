990fcd69cfffd22301c316ccb62418b0a9e37910
melvinzhang <melvinzhang@gmail.com>
2015-04-02 15:01:32 +0800

    remove EachPlayerDraw constant

--- src/magic/model/trigger/MagicAtDrawTrigger.java
+++ src/magic/model/trigger/MagicAtDrawTrigger.java
@@ -26,22 +26,4 @@ public abstract class MagicAtDrawTrigger extends MagicTrigger<MagicPlayer> {
             }
         };
     }
-
-    public static final MagicAtDrawTrigger EachPlayerDraw = new MagicAtDrawTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer drawPlayer) {
-            return new MagicEvent(
-                permanent,
-                drawPlayer,
-                this,
-                "PN draws a card."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer player = event.getPlayer();
-            game.doAction(new MagicDrawAction(player));
-        }
-    };
 }
