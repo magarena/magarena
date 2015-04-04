7fabfe9a2fe7007de69b16ad2733bc2ded4e22dd
melvinzhang <melvinzhang@gmail.com>
2015-04-04 11:48:31 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Chimeric_Sphere.groovy
+++ release/Magarena/scripts/Chimeric_Sphere.groovy
@@ -1,34 +1,22 @@
-def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
-    @Override
-    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-        pt.set(2,1);
-    }
-};
-def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
-    @Override
-    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-        permanent.addAbility(MagicAbility.Flying, flags);
-    }
-};
 def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
     @Override
     public void modSubTypeFlags(final MagicPermanent permanent,final Set<MagicSubType> flags) {
-        flags.clear();
         flags.add(MagicSubType.Construct);
     }
     @Override
     public int getTypeFlags(final MagicPermanent permanent,final int flags) {
-        return MagicType.Artifact.getMask() | MagicType.Creature.getMask();
+        return flags | MagicType.Artifact.getMask() | MagicType.Creature.getMask();
     }
 };
 
-def PT2 = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
     @Override
     public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
         pt.set(3,2);
     }
 };
-def AB2 = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+
+def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
     @Override
     public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
         flags.remove(MagicAbility.Flying);
@@ -36,27 +24,6 @@ def AB2 = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
 };
 
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Animate),
-        "2/1"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{2}")];
-        }
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Until end of turn, SN becomes a 2/1 Construct artifact creature with flying."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,AB,ST));
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Animate),
         "3/2"
@@ -75,7 +42,7 @@ def AB2 = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT2,AB2,ST));
+            game.doAction(new MagicBecomesCreatureAction(event.getPermanent(),PT,AB,ST));
         }
     }
 ]
