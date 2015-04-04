d354ebc527401c4a9aee500b6a870b36c9217f73
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-04 12:53:02 +0100

    add TestGraft

---
+++ src/magic/test/TestGraft.java
@@ -0,0 +1,43 @@
+
+package magic.test;
+
+import magic.model.MagicDeckProfile;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicPlayerDefinition;
+import magic.model.phase.MagicMainPhase;
+
+class TestGraft extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=new MagicDuel();
+        duel.setDifficulty(6);
+
+        final MagicDeckProfile profile=new MagicDeckProfile("gu");
+        final MagicPlayerDefinition player1=new MagicPlayerDefinition("Player",false,profile);
+        final MagicPlayerDefinition player2=new MagicPlayerDefinition("Computer",true,profile);
+        duel.setPlayers(new MagicPlayerDefinition[]{player1,player2});
+        duel.setStartPlayer(0);
+
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 10);
+        createPermanent(game,P,"Llanowar Reborn",false,2);
+        createPermanent(game,P,"Island",false,1);
+        createPermanent(game,P,"Forest",false,2);
+        addToHand(P, "Ethereal Ambush", 1);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+
+        return game;
+    }
+}
