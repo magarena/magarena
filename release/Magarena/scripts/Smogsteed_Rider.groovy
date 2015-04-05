60cd4c159edf4e0a35565d64ce876901b6c9b387
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-05 22:25:59 +0100

    fix: Smogsteed Rider - Use MagicWhenSelfAttacksTrigger and MagicOtherPermanentTargetFilter

--- release/Magarena/scripts/Smogsteed_Rider.groovy
+++ release/Magarena/scripts/Smogsteed_Rider.groovy
@@ -1,24 +1,22 @@
 [
-    new MagicWhenAttacksTrigger() {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
-            return (permanent==attacker) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "Each other attacking creature gains fear until end of turn."
-                ):
-                MagicEvent.NONE;
+            new MagicEvent(
+                permanent,
+                this,
+                "Each other attacking creature gains fear until end of turn."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                event.getPlayer(),
-                MagicTargetFilterFactory.ATTACKING_CREATURE);
-            for (final MagicPermanent creature : targets) {
-                if (creature != event.getPermanent() && creature.isAttacking()) {
-                    game.doAction(new MagicGainAbilityAction(creature,MagicAbility.Fear));
-                }
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.ATTACKING_CREATURE,
+                event.getPermanent()
+            );
+            final Collection<MagicPermanent> targets = game.filterPermanents(event.getPlayer(),filter);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicGainAbilityAction(target,MagicAbility.Fear));
             }
         }
     }
