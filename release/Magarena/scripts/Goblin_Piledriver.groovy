f59bbef1fecc5aeb6239b5aaac95af4bf6dc73d2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-05 22:03:25 +0100

    fix: Goblin Piledriver - Use MagicWhenSelfAttacksTrigger and MagicOtherPermanentTargetFilter

--- release/Magarena/scripts/Goblin_Piledriver.groovy
+++ release/Magarena/scripts/Goblin_Piledriver.groovy
@@ -1,23 +1,22 @@
 [
-    new MagicWhenAttacksTrigger() {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return (permanent==creature) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "SN gets +2/+0 until end of turn for each other attacking Goblin."
-                ):
-                MagicEvent.NONE;
+            new MagicEvent(
+                permanent,
+                this,
+                "SN gets +2/+0 until end of turn for each other attacking Goblin."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature=event.getPermanent();
-            final Collection<MagicPermanent> targets=
-                game.filterPermanents(creature.getController(),MagicTargetFilterFactory.ATTACKING_GOBLIN);
-            //excluding itself
-            final int power = 2 * (targets.size() - 1);
-            game.doAction(new MagicChangeTurnPTAction(creature,power,0));
+            final MagicPermanent permanent=event.getPermanent();
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.ATTACKING_GOBLIN,
+                permanent
+            );
+            final int amount=game.filterPermanents(filter).size()*2;
+            game.doAction(new MagicChangeTurnPTAction(permanent,amount,0));
         }
     }
 ]
