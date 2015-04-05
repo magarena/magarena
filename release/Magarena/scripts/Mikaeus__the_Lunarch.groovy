f043c802cdc6ba329871069bae8f0abee0870d39
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-05 22:21:45 +0100

    fix: Mikaeus, the Lunarch - Use MagicOtherPermanentTargetFilter

--- release/Magarena/scripts/Mikaeus__the_Lunarch.groovy
+++ release/Magarena/scripts/Mikaeus__the_Lunarch.groovy
@@ -20,13 +20,13 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature = event.getPermanent();
-            final Collection<MagicPermanent> targets =
-                game.filterPermanents(creature.getController(),MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
+                event.getPermanent()
+            );
+            final Collection<MagicPermanent> targets = game.filterPermanents(event.getPlayer(),filter);
             for (final MagicPermanent target : targets) {
-                if (target != creature) {
-                    game.doAction(new MagicChangeCountersAction(target,MagicCounterType.PlusOne,1));
-                }
+                game.doAction(new MagicChangeCountersAction(target,MagicCounterType.PlusOne,1));
             }
         }
     }
