34b364b65952a6ba465c5572aa541f231b0842bb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 00:37:41 +0100

    fix: Sigil Blessing - Use MagicOtherPermanentTargetFilter

--- release/Magarena/scripts/Sigil_Blessing.groovy
+++ release/Magarena/scripts/Sigil_Blessing.groovy
@@ -13,15 +13,14 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-                final Collection<MagicPermanent> targets=game.filterPermanents(
-                        event.getPlayer(),
-                        MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
+                final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
+                    it
+                );
+                final Collection<MagicPermanent> targets = game.filterPermanents(event.getPlayer(),filter);
+                game.doAction(new MagicChangeTurnPTAction(it,3,3));
                 for (final MagicPermanent permanent : targets) {
-                    if (permanent==it) {
-                        game.doAction(new MagicChangeTurnPTAction(permanent,3,3));
-                    } else {
-                        game.doAction(new MagicChangeTurnPTAction(permanent,1,1));
-                    }
+                    game.doAction(new MagicChangeTurnPTAction(permanent,1,1));
                 }
             });
         }
