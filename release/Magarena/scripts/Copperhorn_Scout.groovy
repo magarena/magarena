33f673d163c747a711063e3e65fdec598f0b921b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-05 21:22:15 +0100

    fix: Copperhorn Scout - Use MagicWhenSelfAttacksTrigger and MagicOtherPermanentTargetFilter

--- release/Magarena/scripts/Copperhorn_Scout.groovy
+++ release/Magarena/scripts/Copperhorn_Scout.groovy
@@ -1,28 +1,22 @@
 [
-    new MagicWhenAttacksTrigger() {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
-        public MagicEvent executeTrigger(
-                final MagicGame game,
-                final MagicPermanent permanent,
-                final MagicPermanent attacker) {
-            return (permanent == attacker) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "Untap each other creature you control."
-                ):
-                MagicEvent.NONE;
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
+            new MagicEvent(
+                permanent,
+                this,
+                "PN untaps each other creature he or she controls."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent permanent = event.getPermanent();
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                    event.getPlayer(),
-                    MagicTargetFilterFactory.CREATURE_YOU_CONTROL);
-            for (final MagicPermanent other : targets) {
-                if (other != permanent) {
-                    game.doAction(new MagicUntapAction(other));
-                }
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
+                event.getPermanent()
+            );
+            final Collection targets = game.filterPermanents(event.getPlayer(),filter);
+            for (final MagicPermanent target : targets) {
+                game.doAction(new MagicUntapAction(target));
             }
         }
     }
