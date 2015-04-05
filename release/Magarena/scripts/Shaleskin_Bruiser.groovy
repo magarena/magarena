ab9534044cb0d12ba2c58725ced7628d1f21051c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-05 22:24:34 +0100

    fix: Shaleskin Bruiser - Use MagicWhenSelfAttacksTrigger

--- release/Magarena/scripts/Shaleskin_Bruiser.groovy
+++ release/Magarena/scripts/Shaleskin_Bruiser.groovy
@@ -1,25 +1,23 @@
 [
-    new MagicWhenAttacksTrigger() {
+    new MagicWhenSelfAttacksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
-            return (permanent==creature) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "SN gets +3/+0 until end of turn for each other attacking Beast."
-                ):
-                MagicEvent.NONE;
+            new MagicEvent(
+                permanent,
+                this,
+                "SN gets +3/+0 until end of turn for each other attacking Beast."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent creature = event.getPermanent();
+            final MagicPermanent permanent = event.getPermanent();
             final int power = event.getPlayer().getNrOfPermanents(
                 new MagicOtherPermanentTargetFilter(
                     MagicTargetFilterFactory.ATTACKING_BEAST,
-                    event.getPermanent()
+                    permanent
                 )
             );
-            game.doAction(new MagicChangeTurnPTAction(creature,power*3,0));
+            game.doAction(new MagicChangeTurnPTAction(permanent,power*3,0));
         }
     }
 ]
