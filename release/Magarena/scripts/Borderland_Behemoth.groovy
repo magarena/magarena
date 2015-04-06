9591feebdb3c0eaa38d2cf7bcca0dbc86636678c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 18:39:51 +0100

    refactor: Borderland Behemoth - .except()

--- release/Magarena/scripts/Borderland_Behemoth.groovy
+++ release/Magarena/scripts/Borderland_Behemoth.groovy
@@ -1,10 +1,8 @@
-def GIANT_YOU_CONTROL = MagicTargetFilterFactory.singlePermanent("Giant you control");
-
 [
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amount = 4 * source.getController().getNrOfPermanents(new MagicOtherPermanentTargetFilter(GIANT_YOU_CONTROL, source))
+            final int amount = 4 * source.getController().getNrOfPermanents(MagicTargetFilterFactory.GIANT_YOU_CONTROL.except(source))
             pt.add(amount,amount);
         }
     }
