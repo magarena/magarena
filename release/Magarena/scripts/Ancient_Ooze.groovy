e63a1dba1c59428c739fe854fbb8832e7abc9b9a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 01:14:31 +0100

    fix: Ancient Ooze - use MagicOtherPermanentFilter

--- release/Magarena/scripts/Ancient_Ooze.groovy
+++ release/Magarena/scripts/Ancient_Ooze.groovy
@@ -3,14 +3,13 @@
         @Override
         public void modPowerToughness(final MagicGame game, final MagicPlayer player, final MagicPermanent permanent, final MagicPowerToughness pt) {
             int amount = 0;
-            final Collection<MagicPermanent> targets = game.filterPermanents(
-                player,
-                MagicTargetFilterFactory.CREATURE_YOU_CONTROL
+            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
+                permanent
             );
+            final Collection<MagicPermanent> targets = game.filterPermanents(player,filter);
             for (final MagicPermanent creature:targets) {
-                if (creature != permanent) {
-                    amount += creature.getConvertedCost();
-                }
+                amount += creature.getConvertedCost();
             }
             pt.set(amount, amount);
         }
