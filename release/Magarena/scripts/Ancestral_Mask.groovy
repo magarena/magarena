5f4c6ca6f90835fbdc656f1ce5002a29be048bad
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 03:59:07 +0100

    refactor Ancestral Mask to use .except()

--- release/Magarena/scripts/Ancestral_Mask.groovy
+++ release/Magarena/scripts/Ancestral_Mask.groovy
@@ -3,7 +3,7 @@
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
             final MagicGame game = source.getGame();
-            final int amt = game.getNrOfPermanents(new MagicOtherPermanentTargetFilter(MagicTargetFilterFactory.ENCHANTMENT,source))
+            final int amt = game.getNrOfPermanents(MagicTargetFilterFactory.ENCHANTMENT.except(source));
             pt.add(amt*2, amt*2);
         }
         @Override
