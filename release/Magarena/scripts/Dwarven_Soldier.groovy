194493270f018f99867af3de6043372047934686
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 03:01:11 +0100

    refactor: Dwarven Solider - Use MagicWhenBlocksOrBecomesBlockedTrigger

--- release/Magarena/scripts/Dwarven_Soldier.groovy
+++ release/Magarena/scripts/Dwarven_Soldier.groovy
@@ -1,9 +1,10 @@
 [
-    new MagicWhenSelfBecomesBlockedTrigger() {
+    new MagicWhenBlocksOrBecomesBlockedTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent attacker) {
-            final MagicPermanentList plist = permanent.getBlockingCreatures();
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
             boolean pump = false;
+            final MagicPermanent target = permanent == blocker ? blocker : blocker.getBlockedCreature() ;
+            final MagicPermanentList plist = target.getBlockingCreatures();
             for (final MagicPermanent blocker : plist) {
                 if (blocker.hasSubType(MagicSubType.Orc)) {
                     pump = true;
@@ -20,34 +21,7 @@
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeTurnPTAction(
-                event.getPermanent(),
-                0,
-                2
-            ));
-        }
-    },
-    new MagicWhenBlocksTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
-            final MagicPermanent blocked = permanent.getBlockedCreature();
-            return (permanent == blocker &&
-                    blocked.isValid() &&
-                    (blocked.hasSubType(MagicSubType.Orc))) ?
-                new MagicEvent(
-                    permanent,
-                    this,
-                    "SN gets +0/+2 until end of turn."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicChangeTurnPTAction(
-                event.getPermanent(),
-                0,
-                2
-            ));
+            game.doAction(new MagicChangeTurnPTAction(event.getPermanent(),0,2));
         }
     }
 ]
