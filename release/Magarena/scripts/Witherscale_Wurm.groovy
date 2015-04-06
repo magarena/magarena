816f42df04dedc9c68483297e4c9754f0fed7a3a
melvinzhang <melvinzhang@gmail.com>
2015-04-06 15:49:12 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Witherscale_Wurm.groovy
+++ release/Magarena/scripts/Witherscale_Wurm.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicWhenBlocksOrBecomesBlockedTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
-            return new MagicEvent(
-                permanent,
-                permanent == blocker ? blocker.getBlockedCreature() : blocker,
-                this,
-                "RN gains wither until end of turn."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processRefPermanent(game, {
-                game.doAction(new MagicGainAbilityAction(
-                    it,
-                    MagicAbility.Wither
-                ));
-            });
-        }
-    },
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
