ac5bf0d662c9cb1e6c74c943fff5547c76074fda
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-06 18:58:39 +0100

    refactor: Zhang He, Wei General - .except()

--- release/Magarena/scripts/Zhang_He__Wei_General.groovy
+++ release/Magarena/scripts/Zhang_He__Wei_General.groovy
@@ -10,11 +10,10 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicTargetFilter<MagicPermanent> filter = new MagicOtherPermanentTargetFilter(
-                MagicTargetFilterFactory.CREATURE_YOU_CONTROL,
-                event.getPermanent()
+            final Collection<MagicPermanent> targets = game.filterPermanents(
+                event.getPlayer(),
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL.except(event.getPermanent())
             );
-            final Collection<MagicPermanent> targets = game.filterPermanents(event.getPlayer(),filter);
             for (final MagicPermanent target : targets) {
                 game.doAction(new MagicChangeTurnPTAction(target,1,0));
             }
