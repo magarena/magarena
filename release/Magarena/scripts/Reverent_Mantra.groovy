b9757ea7c3105b70509ceaf2be57cd902a74250b
melvinzhang <melvinzhang@gmail.com>
2015-04-09 09:24:53 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Reverent_Mantra.groovy
+++ release/Magarena/scripts/Reverent_Mantra.groovy
@@ -1,19 +1,4 @@
 [
-    new MagicCardActivation(
-        [MagicCondition.CARD_CONDITION],
-        new MagicActivationHints(MagicTiming.Pump,true),
-        "Alt"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
-            final MagicTargetChoice targetChoice = new MagicTargetChoice(
-                MagicTargetFilterFactory.WHITE_CARD_FROM_HAND.except(source),
-                MagicTargetHint.None,
-                "a white card from your hand"
-            );
-            return [new MagicExileCardEvent(source, targetChoice)];
-        }
-    },
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
