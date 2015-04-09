42b1deb7825d591fcf8dbf5b1ab0a9caa10238f6
melvinzhang <melvinzhang@gmail.com>
2015-04-09 21:17:09 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Ezuri__Renegade_Leader.groovy
+++ release/Magarena/scripts/Ezuri__Renegade_Leader.groovy
@@ -26,32 +26,5 @@
                 game.doAction(new MagicRegenerateAction(it));
             });
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump,true),
-        "Pump"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source,"{2}{G}{G}{G}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Elf creatures PN controls get +3/+3 and gain trample until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            ELF_YOU_CONTROL.filter(event.getPlayer()) each {
-                game.doAction(new MagicChangeTurnPTAction(it,3,3));
-                game.doAction(new MagicGainAbilityAction(it,MagicAbility.Trample));
-            }
-        }
     }
 ]
