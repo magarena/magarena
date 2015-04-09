40bef8c9cbae59db7fbbd2753c44788b24841ce3
melvinzhang <melvinzhang@gmail.com>
2015-04-09 21:01:34 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Quest_for_Renewal.groovy
+++ release/Magarena/scripts/Quest_for_Renewal.groovy
@@ -1,28 +1,4 @@
 [
-    new MagicWhenBecomesTappedTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent tapped) {
-            return (tapped.isFriend(permanent) &&
-                    tapped.isCreature()) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.ADD_POS_COUNTER,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_YES
-                    ),
-                    this,
-                    "PN may\$ put a quest counter on SN."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                game.doAction(new MagicChangeCountersAction(event.getPermanent(),MagicCounterType.Quest,1));
-            }
-        }
-    },
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
