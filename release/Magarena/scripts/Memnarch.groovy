10e04a069db3e2bfd03c9bc626c41fb2ee7d085c
melvinzhang <melvinzhang@gmail.com>
2015-04-10 11:29:07 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Memnarch.groovy
+++ release/Magarena/scripts/Memnarch.groovy
@@ -1,36 +1,6 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
-        "Artifact"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source, "{1}{U}{U}")
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                NEG_TARGET_PERMANENT,
-                MagicExileTargetPicker.create(),
-                this,
-                "Target permanent\$ becomes an artifact in addition to its other types."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new MagicAddStaticAction(it, MagicStatic.Artifact));
-            });
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
         "Control"
     ) {
 
--- release/Magarena/scripts/Neurok_Transmuter.groovy
+++ release/Magarena/scripts/Neurok_Transmuter.groovy
@@ -10,44 +10,8 @@ def nonartifact = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
         return flags & ~MagicType.Artifact.getMask();
     }
 };
-def artifact = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
-    @Override
-    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
-        return flags | MagicType.Artifact.getMask();
-    }
-};
 
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
-        "Artifact"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source, "{U}")
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                NEG_TARGET_CREATURE,
-                MagicExileTargetPicker.create(),
-                this,
-                "Target creature\$ becomes an artifact in addition to its other types until end of turn."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new MagicAddStaticAction(it, artifact));
-            });
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
         "Blue"
