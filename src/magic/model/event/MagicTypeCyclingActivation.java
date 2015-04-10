fb26eff1c3885cf35cb76b53e7f4526c3300a3a2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-10 15:08:29 +0100

    fix: #179 add Cycling triggers to TypeCyclingActivation

--- src/magic/model/event/MagicTypeCyclingActivation.java
+++ src/magic/model/event/MagicTypeCyclingActivation.java
@@ -5,7 +5,13 @@ import magic.model.MagicGame;
 import magic.model.MagicManaCost;
 import magic.model.MagicPayedCost;
 import magic.model.MagicLocationType;
+import magic.model.MagicPermanent;
+import magic.model.MagicSource;
+import magic.model.action.MagicPutItemOnStackAction;
 import magic.model.choice.MagicTargetChoice;
+import magic.model.stack.MagicAbilityOnStack;
+import magic.model.trigger.MagicTrigger;
+import magic.model.trigger.MagicTriggerType;
 
 import java.util.Arrays;
 
@@ -49,4 +55,33 @@ public class MagicTypeCyclingActivation extends MagicCardAbilityActivation {
             MagicLocationType.OwnersHand
         ));
     }
+    
+    @Override
+    public MagicEvent getEvent(final MagicSource source) {
+        return new MagicEvent(
+            source,
+            new MagicEventAction() {
+                @Override
+                public void executeEvent(final MagicGame game, final MagicEvent event) {
+                    final MagicCard card = event.getCard();
+                    final MagicAbilityOnStack abilityOnStack = new MagicAbilityOnStack(
+                        MagicTypeCyclingActivation.this,
+                        getCardEvent(card, game.getPayedCost())
+                    );
+                    game.doAction(new MagicPutItemOnStackAction(abilityOnStack));
+                    game.executeTrigger(MagicTriggerType.WhenOtherCycle, card);
+                    for (final MagicTrigger<MagicCard> trigger : card.getCardDefinition().getCycleTriggers()) {
+                        game.executeTrigger(
+                            trigger,
+                            MagicPermanent.NONE,
+                            card,
+                            card
+                        );
+                    }
+                }
+            },
+            name + " SN."
+        );
+    }
+    
 }
