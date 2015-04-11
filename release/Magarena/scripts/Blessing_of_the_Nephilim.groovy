6da83d0f9c2590824dd38452a6acdd89a31ca0b0
melvinzhang <melvinzhang@gmail.com>
2015-04-11 21:13:23 +0800

    merge multiple MagicStatic

--- release/Magarena/scripts/Blessing_of_the_Nephilim.groovy
+++ release/Magarena/scripts/Blessing_of_the_Nephilim.groovy
@@ -2,51 +2,15 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            pt.add(1, 1);
+            for (final MagicColor color : MagicColor.values()) {
+                if (permanent.hasColor(color)) {
+                    pt.add(1, 1);
+                }
+            }
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Black);
-        }
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            pt.add(1, 1);
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Blue);
-        }
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            pt.add(1, 1);
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Green);
-        }
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            pt.add(1, 1);
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Red);
-        }
-    },
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            pt.add(1, 1);
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+            return MagicStatic.acceptLinked(game, source, target);
         }
     }
 ]
