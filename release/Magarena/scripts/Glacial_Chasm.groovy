32a4b58cae4c10793b1e08e907e70a60f0c39ce8
melvinzhang <melvinzhang@gmail.com>
2015-04-12 09:07:52 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Glacial_Chasm.groovy
+++ release/Magarena/scripts/Glacial_Chasm.groovy
@@ -28,7 +28,6 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
             if (permanent.isController(damage.getTarget())) {
-                // Prevention effect.
                 damage.prevent();
             }
             return MagicEvent.NONE;
