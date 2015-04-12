846fe2e1a204502e1d2749602dbb40c7e090917b
melvinzhang <melvinzhang@gmail.com>
2015-04-12 08:56:17 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Dystopia.groovy
+++ release/Magarena/scripts/Dystopia.groovy
@@ -12,31 +12,6 @@ def A_GREEN_OR_WHITE_PERMANENT_YOU_CONTROL = new MagicTargetChoice(
 );
 
 [
-    new MagicAtYourUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            game.doAction(new MagicChangeCountersAction(
-                permanent,
-                MagicCounterType.Age,
-                1
-            ));
-            return new MagicEvent(
-                permanent,
-                new MagicMayChoice("Pay cumulative upkeep?"),
-                this,
-                "PN may\$ lose 1 life for each Age counter on SN. " +
-                "If he or she doesn't, sacrifice SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                game.doAction(new MagicChangeLifeAction(event.getPlayer(),-event.getPermanent().getCounters(MagicCounterType.Age)));
-            } else {
-                game.doAction(new MagicSacrificeAction(event.getPermanent()));
-            }
-        }
-    },
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
