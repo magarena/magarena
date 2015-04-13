5ae8b0cdb8aaad00e65dacadd5a77ac3164e8190
melvinzhang <melvinzhang@gmail.com>
2015-04-13 09:50:15 +0800

    increase number of Wild Wurm cards in TestCoinFlip

--- src/magic/test/TestCoinFlip.java
+++ src/magic/test/TestCoinFlip.java
@@ -21,7 +21,7 @@ class TestCoinFlip extends TestGameBuilder {
         createPermanent(game,P,"Mountain",false,20);
         createPermanent(game,P,"Tavern Swindler",false,1);
         addToHand(P,"Scoria Wurm",1);
-        addToHand(P, "Wild Wurm", 1);
+        addToHand(P, "Wild Wurm", 3);
 
 
         P = opponent;
