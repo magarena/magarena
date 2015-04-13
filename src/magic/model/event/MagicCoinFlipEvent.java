08430e262cee8337d13ec7617aff755f24daf64a
melvinzhang <melvinzhang@gmail.com>
2015-04-13 11:55:17 +0800

    replaced shared actions with per instance actions

--- src/magic/model/event/MagicCoinFlipEvent.java
+++ src/magic/model/event/MagicCoinFlipEvent.java
@@ -9,50 +9,47 @@ import magic.model.action.MagicAction;
 public class MagicCoinFlipEvent extends MagicEvent {
 
     
-    private static MagicAction winAction;
-    private static MagicAction loseAction;
-    private static Boolean heads;
+    private MagicAction winAction;
+    private MagicAction loseAction;
     
-    public MagicCoinFlipEvent(final MagicSource source, final Boolean aheads, final MagicPlayer player,final MagicAction aWinAction, final MagicAction aLoseAction) {
+    public MagicCoinFlipEvent(final MagicSource source, final boolean aheads, final MagicPlayer player,final MagicAction aWinAction, final MagicAction aLoseAction) {
         super(
             source,
             player, 
-            EventAction,
+            EventAction(aWinAction, aLoseAction, aheads),
             "PN flips a coin."
         );
-        winAction = aWinAction;
-        loseAction = aLoseAction;
-        heads = aheads;
     }
 
-    private static final MagicEventAction EventAction = new MagicEventAction() {
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            //True = Heads
-            final MagicPlayer player = event.getPlayer();
-            boolean coinFace = false;
-            boolean playerFace = heads;
-            final MagicRandom rng = new MagicRandom(event.getSource().getStateId());
-                if (rng.nextInt(2)==0){
-                    game.logAppendMessage(player, "Coin was Heads.");
-                    coinFace = true;
-                    //Heads trigger
-                } else {
-                    game.logAppendMessage(player, "Coin was Tails.");
-                    //Tails trigger
+    private static final MagicEventAction EventAction(final MagicAction winAction, final MagicAction loseAction, final boolean heads) {
+        return new MagicEventAction() {
+            @Override
+            public void executeEvent(final MagicGame game, final MagicEvent event) {
+                //True = Heads
+                final MagicPlayer player = event.getPlayer();
+                boolean coinFace = false;
+                boolean playerFace = heads;
+                final MagicRandom rng = new MagicRandom(event.getSource().getStateId());
+                    if (rng.nextInt(2)==0){
+                        game.logAppendMessage(player, "Coin was Heads.");
+                        coinFace = true;
+                        //Heads trigger
+                    } else {
+                        game.logAppendMessage(player, "Coin was Tails.");
+                        //Tails trigger
+                    }
+                if (playerFace==coinFace) {
+                    game.logAppendMessage(player, player.getName() + " wins.");
+                    if (winAction !=null) {
+                       game.doAction(winAction);
+                       //Win trigger
+                    }
+                } else if (loseAction !=null) {
+                    game.doAction(loseAction);
+                    //Lose trigger
                 }
-            if (playerFace==coinFace) {
-                game.logAppendMessage(player, player.getName() + " wins.");
-                if (winAction !=null) {
-                   game.doAction(winAction);
-                   //Win trigger
-                }
-            } else if (loseAction !=null) {
-                game.doAction(loseAction);
-                //Lose trigger
             }
-        }
-
-    };
+        };
+    }
 }
 
