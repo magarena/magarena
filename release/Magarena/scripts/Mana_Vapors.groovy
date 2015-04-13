14c41ffe0ca8e35868eb201649acc05e63ea77b7
PalladiaMors <unknown>
2015-04-13 20:33:33 +0200

    Add script for Mana Vapors

---
+++ release/Magarena/scripts/Mana_Vapors.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_PLAYER,
+                this,
+                "Lands target player\$ controls don't untap during his or her next untap step."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final Collection<MagicPermanent> targets = game.filterPermanents(it,LAND_YOU_CONTROL);
+                for (final MagicPermanent land : targets) {
+                    game.doAction(MagicChangeStateAction.Set(
+                        land,
+                        MagicPermanentState.DoesNotUntapDuringNext
+                    ));
+                }
+            });
+        }
+    }
+]
