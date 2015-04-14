e2000ab9d8639fcbf1009e7e6d06638f1f1dca2e
melvinzhang <melvinzhang@gmail.com>
2015-04-14 10:19:07 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Borderland_Behemoth.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amount = 4 * source.getController().getNrOfPermanents(GIANT_YOU_CONTROL.except(source))
-            pt.add(amount,amount);
-        }
-    }
-]
--- release/Magarena/scripts/Mogg_Squad.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amount = source.getGame().getNrOfPermanents(MagicType.Creature) - 1;
-            pt.add(-amount,-amount);
-        }
-    }
-]
