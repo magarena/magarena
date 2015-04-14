ec668caef2d8c94a1e834fc0e9b468136a221b97
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-15 00:52:17 +0100

    partial upgrade to March of the Machines #16

--- release/Magarena/scripts/March_of_the_Machines.groovy
+++ release/Magarena/scripts/March_of_the_Machines.groovy
@@ -1,15 +1,17 @@
 [
-    new MagicStatic(MagicLayer.SetPT, NONCREATURE_ARTIFACT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            final int cmc = permanent.getConvertedCost();
-            pt.set(cmc,cmc);
-        }
-    },
     new MagicStatic(MagicLayer.Type, NONCREATURE_ARTIFACT) {
         @Override
         public int getTypeFlags(final MagicPermanent permanent, final int flags) {
             return flags|MagicType.Artifact.getMask()|MagicType.Creature.getMask();
+        }
+    },
+    new MagicStatic(MagicLayer.SetPT, ARTIFACT_CREATURE) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+            if (MagicCondition.WAS_NONCREATURE_ARTIFACT.accept(permanent)) {
+                final int cmc = permanent.getConvertedCost();
+                pt.set(cmc,cmc);
+            }
         }
     }
 ]
