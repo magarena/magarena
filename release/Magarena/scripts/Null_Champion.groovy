2c16e351307c7978d5e7dfc8e26d17f83203cc94
melvinzhang <melvinzhang@gmail.com>
2015-04-14 17:45:33 +0800

    replace gooovy code with card script

--- release/Magarena/scripts/Null_Champion.groovy
+++ release/Magarena/scripts/Null_Champion.groovy
@@ -9,35 +9,5 @@
                 pt.set(4,2);
             }
         }
-    },
-    new MagicPermanentActivation(
-        [
-            MagicConditionFactory.CounterAtLeast(MagicCounterType.Level,4),
-        ],
-        new MagicActivationHints(MagicTiming.Pump),
-        "Regen"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{B}"),
-                new MagicRegenerationConditionsEvent(source,this)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Regenerate SN."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new MagicRegenerateAction(event.getPermanent()));
-        }
     }
 ]
