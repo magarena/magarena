b4a08be3bfe9e88fde2fcf90604ccaaaa5c5c939
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-15 00:51:02 +0100

    add TestMarch for March of the Machines

---
+++ src/magic/test/TestMarch.java
@@ -0,0 +1,35 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestMarch extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(1);
+        addToLibrary(P, "Island", 10);
+        createPermanent(game,P,"Island",false,8);
+        createPermanent(game,P,"Chimeric Staff",false,1);
+        createPermanent(game,P,"Seat of the Synod",false,1);
+        addToHand(P,"Master of Etherium",1);
+        addToHand(P,"March of the Machines",1);
+
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(game,P,"Rupture Spire",false,8);
+
+        return game;
+    }
+}
