1b69b86bc9714d622912c4f7a0439e0e66c15bf1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-15 17:54:22 +0100

    Rishadan Pawnshop and TestGame for #201

---
+++ release/Magarena/scripts/Rishadan_Pawnshop.groovy
@@ -0,0 +1,34 @@
+def choice = new MagicTargetChoice("target nontoken permanent you control")
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Shuffle"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{2}"),
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                MagicRegenerateTargetPicker.create(),
+                this,
+                "PN shuffles target nontoken permanent he or she controls\$ into its owner's library."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new RemoveFromPlayAction(it, MagicLocationType.OwnersLibrary));
+            });
+        }
+    }
+]
