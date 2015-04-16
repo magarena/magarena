213e03841328a124b6c17587a0552b58d0cbf37a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-16 03:05:31 +0100

    add modifier to MagicScryEvent to allow Scry actions without trigger

--- src/magic/model/event/MagicScryEvent.java
+++ src/magic/model/event/MagicScryEvent.java
@@ -9,32 +9,40 @@ import magic.model.trigger.MagicTriggerType;
 
 public class MagicScryEvent extends MagicEvent {
     
+    public MagicScryEvent(final MagicEvent event, boolean aScry) {
+        this(event.getSource(), event.getPlayer(), aScry);
+    }
+    
     public MagicScryEvent(final MagicEvent event) {
-        this(event.getSource(), event.getPlayer());
+        this(event.getSource(), event.getPlayer(), true);
     }
 
-    public MagicScryEvent(final MagicSource source, final MagicPlayer player) {
+    public MagicScryEvent(final MagicSource source, final MagicPlayer player, final Boolean aScry) {
         super(
             source,
             player,
             new MagicScryChoice(),
-            EventAction,
+            EventAction(aScry),
             ""
         );
     }
     
-    private static final MagicEventAction EventAction = new MagicEventAction() {
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer p = event.getPlayer();
-            if (event.isYes()) {
-                game.logAppendMessage(p, p + " looks at the card on the top of his or her library and moves it to the bottom.");
-                game.doAction(new ScryAction(p));
-            } else {
-                game.logAppendMessage(p, p + " looks at the card on the top of his or her library and puts it back on top.");
+    private static final MagicEventAction EventAction(final Boolean scry) {
+        return new MagicEventAction() {
+            @Override
+            public void executeEvent(final MagicGame game, final MagicEvent event) {
+                final MagicPlayer p = event.getPlayer();
+                if (event.isYes()) {
+                    game.logAppendMessage(p, p + " looks at the card on the top of his or her library and moves it to the bottom.");
+                    game.doAction(new ScryAction(p));
+                } else {
+                    game.logAppendMessage(p, p + " looks at the card on the top of his or her library and puts it back on top.");
+                }
+                //Scry triggers even if the card is not moved. Only once regardless of amount of cards scryed
+                if (scry) {
+                    game.executeTrigger(MagicTriggerType.WhenScry,p);
+                }
             }
-            //Scry triggers even if the card is not moved. Only once regardless of amount of cards scryed
-            game.executeTrigger(MagicTriggerType.WhenScry,p);
-        }
+        };
     };
 }
