60f825d8f39e8820b45ffe20cd6abac515d665fe
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-17 02:52:46 +0100

    add Ashcloud Phoenix

---
+++ release/Magarena/scripts/Ashcloud_Phoenix.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent died) {
+            return new MagicEvent(
+                permanent,
+                permanent.getCard(),
+                this,
+                "Return SN to the battlefield face down under your control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard card = event.getRefCard();
+            if (card.isInGraveyard()) {
+                game.doAction(new ReanimateAction(
+                    card,
+                    event.getPlayer(),
+                    MagicPlayMod.MORPH
+                ));
+            }
+        }
+    }
+]
