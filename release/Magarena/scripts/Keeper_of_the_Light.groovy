39ca724e0c007b48e216495f047fb3c70f8223f2
PalladiaMors <unknown>
2015-04-24 00:48:05 +0200

    Add script for Keeper of the Light

---
+++ release/Magarena/scripts/Keeper_of_the_Light.groovy
@@ -0,0 +1,40 @@
+def OPPONENT_WHO_HAS_MORE_LIFE = new MagicPlayerFilterImpl() {
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPlayer target) {
+        return target.isEnemy(player) && target.getLife() > player.getLife();
+    } 
+};
+
+def TARGET_OPPONENT_WHO_HAS_MORE_LIFE = new MagicTargetChoice(
+    OPPONENT_WHO_HAS_MORE_LIFE,
+    "target opponent who has more life than you"
+);
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "+Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{W}"),
+                new MagicTapEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_OPPONENT_WHO_HAS_MORE_LIFE,
+                this,
+                "PN gains 3 life."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(MagicRuleEventAction.create("You gain 3 life.").getEvent(event.getSource()));
+        }
+    }
+]
