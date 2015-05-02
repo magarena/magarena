15419dade61e0ac4153389c68670344886ff843d
melvinzhang <melvinzhang@gmail.com>
2015-05-02 20:02:22 +0800

    should use replace instead of prevent as it is not affected by 'can't be prevented'

--- release/Magarena/scripts/Crumbling_Sanctuary.groovy
+++ release/Magarena/scripts/Crumbling_Sanctuary.groovy
@@ -2,9 +2,8 @@
     new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.REPLACE_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            if (damage.isTargetPlayer()){
-                final int amount = damage.getAmount();
-                damage.prevent();
+            if (damage.isTargetPlayer()) {
+                final int amount = damage.replace();
                 damage.getTargetPlayer().getLibrary().getCardsFromTop(amount) each {
                     game.doAction(new RemoveCardAction(it,MagicLocationType.OwnersLibrary));
                     game.doAction(new MoveCardAction(it,MagicLocationType.OwnersLibrary,MagicLocationType.Exile));
