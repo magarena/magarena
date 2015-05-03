e9b64efbd41f3df50f048987503c749110ed7ec9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-04 00:42:14 +0100

    organise imports

--- src/magic/model/trigger/MagicWhenSpellIsCastTrigger.java
+++ src/magic/model/trigger/MagicWhenSpellIsCastTrigger.java
@@ -1,13 +1,7 @@
 package magic.model.trigger;
 
-import magic.model.MagicGame;
-import magic.model.MagicPermanent;
 import magic.model.MagicCardDefinition;
-import magic.model.MagicLocationType;
 import magic.model.stack.MagicCardOnStack;
-import magic.model.event.MagicEvent;
-import magic.model.action.ChangeCardDestinationAction;
-import magic.model.action.AddTriggerAction;
 
 public abstract class MagicWhenSpellIsCastTrigger extends MagicTrigger<MagicCardOnStack> {
     public MagicWhenSpellIsCastTrigger(final int priority) {
