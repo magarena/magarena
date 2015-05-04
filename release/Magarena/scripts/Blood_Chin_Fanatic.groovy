b9903a416f2bc5da50887db2634dee747cb8ae08
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-04 21:36:51 +0100

    add Blood Chin Fanatic

---
+++ release/Magarena/scripts/Blood_Chin_Fanatic.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{1}{B}"),
+                new MagicSacrificePermanentEvent(source, MagicTargetChoice.Other("a Warrior creature to sacrifice",source))
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_PLAYER,
+                payedCost.getTarget(),
+                this,
+                "Target player\$ loses X life and PN gains X life, where X is RN's power."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final int amount=event.getRefPermanent().getPower();
+                game.doAction(new ChangeLifeAction(it,-amount));
+                game.doAction(new ChangeLifeAction(event.getPlayer(),amount));
+            });
+        }
+    }
+]
