3518d89648b1916329e8712fdad2d66e41507856
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-04 02:23:40 +0100

    add indicator to log for Shrine of Burning Rage amount

--- release/Magarena/scripts/Shrine_of_Burning_Rage.groovy
+++ release/Magarena/scripts/Shrine_of_Burning_Rage.groovy
@@ -19,7 +19,7 @@
                 NEG_TARGET_CREATURE_OR_PLAYER,
                 new MagicDamageTargetPicker(source.getCounters(MagicCounterType.Charge)),
                 this,
-                "SN deals damage equal to the number of charge counters on it to target creature or player\$."
+                "SN deals damage equal to the number of charge counters on it to target creature or player\$. "+ "("+source.getCounters(MagicCounterType.Charge)+")"
             );
         }
 
