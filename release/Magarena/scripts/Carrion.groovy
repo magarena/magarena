22926ba0a827ca041a5c276160d0983a9bf1905c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-04 22:34:00 +0100

    add Carrion

---
+++ release/Magarena/scripts/Carrion.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.getTarget(),
+                this,
+                "PN puts X 0/1 black Insect creature tokens onto the battlefield, where X is RN's power."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount=event.getRefPermanent().getPower();
+            game.doAction(new PlayTokensAction(
+                    event.getPlayer(),
+                    TokenCardDefinitions.get("0/1 black Insect creature token"),
+                    amount
+                ));
+        }
+    }
+]
