201730109c6586eebeca42f49d313fc3ee549020
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-05 20:24:55 +0100

    add Worthy Cause

---
+++ release/Magarena/scripts/Worthy_Cause.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                payedCost.getTarget(),
+                this,
+                "PN gains life equal to RN's toughness."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount=event.getRefPermanent().getToughness();
+            game.logAppendMessage(player,"("+amount+")");
+            game.doAction(new ChangeLifeAction(player, amount));
+            if (event.isBuyback()) {
+                game.doAction(new ChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.OwnersHand));
+            }
+        }
+    }
+]
