12392ded12722f5f1da59deeffd36d629159b294
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-05 02:45:25 +0100

    add Freyalise Supplicant

---
+++ release/Magarena/scripts/Freyalise_Supplicant.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificePermanentEvent(source, new MagicTargetChoice("a red or white creature to sacrifice"))
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_CREATURE_OR_PLAYER,
+                MagicDefaultTargetPicker.create(),
+                payedCost.getTarget(),
+                this,
+                "SN deals damage to target creature or player\$ equal to half RN's power, rounded down."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                def amount = event.getRefPermanent().getPower().intdiv(2);
+                game.logAppendMessage(event.getPlayer(),"("+amount+")");
+                game.doAction(new DealDamageAction(event.getPermanent(),it,amount));
+            });
+        }
+    }
+]
