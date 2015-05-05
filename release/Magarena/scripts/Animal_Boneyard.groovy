a304dd615af7783bc060d4a2b158d615b5298016
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-05 21:12:34 +0100

    add Animal Boneyard

---
+++ release/Magarena/scripts/Animal_Boneyard.groovy
@@ -0,0 +1,43 @@
+def Life = new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "+Life"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificePermanentEvent(source, SACRIFICE_CREATURE)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getTarget(),
+                this,
+                "PN gains life equal to RN's toughness."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount=event.getRefPermanent().getToughness();
+            game.logAppendMessage(player,"("+amount+")");
+            game.doAction(new ChangeLifeAction(player,amount));
+        }
+    }
+
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(Life);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    },
+]
