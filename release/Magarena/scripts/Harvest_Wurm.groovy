46642b33cbaacc4dcad316671358d348e3351260
melvinzhang <melvinzhang@gmail.com>
2015-05-10 20:40:48 +0800

    improve check for whether cost can be paid

--- release/Magarena/scripts/Harvest_Wurm.groovy
+++ release/Magarena/scripts/Harvest_Wurm.groovy
@@ -6,7 +6,7 @@ def effect = MagicRuleEventAction.create("return a basic land card from your gra
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 permanent,
-                new MagicMayChoice("Return a basic land card to your hand?"),
+                new MagicMayChoice("Return a basic land card from your graveyard to your hand?"),
                 this,
                 "PN may\$ return a basic land card from his or her graveyard to PN's hand. " +
                 "If he or she doesn't, sacrifice SN."
@@ -14,8 +14,9 @@ def effect = MagicRuleEventAction.create("return a basic land card from your gra
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes() && BASIC_LAND_CARD_FROM_YOUR_GRAVEYARD.filter(game).size() >= 1) {
-                game.addEvent(effect.getEvent(event));
+            final MagicEvent costEvent = effect.getEvent(event);
+            if (event.isYes() && costEvent.hasOptions()) {
+                game.addEvent(costEvent);
             } else {
                 game.doAction(new SacrificeAction(event.getPermanent()));
             }
