3061d6981614727e3c2a40aab2ee04874c62d562
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-11 23:21:11 +0100

    add Mercy Killing

---
+++ release/Magarena/scripts/Mercy_Killing.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_CREATURE,
+                MagicSacrificeTargetPicker.create(),
+                this,
+                "Target creature's\$ controller sacrifices it, then puts X 1/1 green and white Elf Warrior creature tokens onto the battlefield, where X is that that creature's power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int amount = it.getPower();
+                game.logAppendMessage(event.getPlayer(),"("+amount+")");
+                final MagicPlayer controller=it.getController();
+                game.doAction(new SacrificeAction(it));
+                game.doAction(new PlayTokensAction(
+                    controller,
+                    CardDefinitions.getToken("1/1 green and white Elf Warrior creature token"),
+                    amount
+                ));
+            });
+        }
+    }
+]
