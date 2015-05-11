43611af3a450f21fe28697da84a8ee3f2ca6b027
melvinzhang <melvinzhang@gmail.com>
2015-05-11 19:12:04 +0800

    always return the event so that number of activation is counted correctly

--- src/magic/model/condition/MagicArtificialCondition.java
+++ src/magic/model/condition/MagicArtificialCondition.java
@@ -6,36 +6,24 @@ import magic.model.event.MagicEvent;
 
 public class MagicArtificialCondition extends MagicCondition {
 
-    private final MagicCondition normalCondition;
-    private final MagicCondition artificialCondition;
-
-    public MagicArtificialCondition(final MagicCondition normalCondition,final MagicCondition artificialCondition) {
-        this.normalCondition=normalCondition;
-        this.artificialCondition=artificialCondition;
-    }
+    private final MagicCondition cond;
 
     public MagicArtificialCondition(final MagicCondition artificialCondition) {
-        this(MagicCondition.NONE, artificialCondition);
+        cond = artificialCondition;
     }
 
     @Override
     public boolean accept(final MagicSource source) {
-        final MagicGame game = source.getGame();
-        return game.isArtificial() ? 
-            artificialCondition.accept(source) :
-            normalCondition.accept(source);
+        return source.getGame().isArtificial() ? cond.accept(source) : true;
     }
     
     @Override
     public MagicEvent getEvent(final MagicSource source) {
-        final MagicGame game = source.getGame();
-        return game.isArtificial() ? 
-            artificialCondition.getEvent(source) : 
-            normalCondition.getEvent(source);
+        return cond.getEvent(source);
     }
 
     @Override
     public boolean isIndependent() {
-        return artificialCondition.isIndependent() && normalCondition.isIndependent();
+        return cond.isIndependent();
     }
 }
