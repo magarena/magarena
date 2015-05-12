5e2d1013b94367b330312ad71bec985381c6c121
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 15:42:52 +0100

    add Death Pit Offering

---
+++ release/Magarena/scripts/Death_Pit_Offering.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN sacrifices all creatures he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            CREATURE_YOU_CONTROL.filter(event) each {
+                game.doAction(new SacrificeAction(it));
+            }
+        }
+    }
+]
