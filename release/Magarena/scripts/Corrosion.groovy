591d887eafb16d194a10151255726822b5b9bac9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 23:08:01 +0100

    add Corrosion

---
+++ release/Magarena/scripts/Corrosion.groovy
@@ -0,0 +1,50 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
+            return new MagicEvent(
+                    permanent,
+                    TARGET_OPPONENT,
+                    this,
+                    "PN puts a rust counter on each artifact target opponent\$ controls. "+
+                    "Then destroy each artifact with converted mana cost less than or equal to the number of rust counters on it. "+
+                    "Artifacts destroyed this way can't be regenerated."
+                );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                ARTIFACT_YOU_CONTROL.filter(it) each {
+                    game.doAction(new ChangeCountersAction(it, MagicCounterType.Rust, 1));
+                }
+                ARTIFACT_YOU_CONTROL.filter(it) each {
+                    if (it.getConvertedCost() <= it.getCounters(MagicCounterType.Rust)) {
+                        game.doAction(ChangeStateAction.Set(it, MagicPermanentState.CannotBeRegenerated));
+                        game.doAction(new DestroyAction(it));
+                    }
+                }
+            });
+        }
+    },
+    
+    new MagicWhenSelfLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final RemoveFromPlayAction act) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Remove all Rust counters from all permanents."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            PERMANENT.filter(event) each {
+                game.doAction(new ChangeCountersAction(
+                    it,
+                    MagicCounterType.Rust,
+                    -it.getCounters(MagicCounterType.Rust)
+                ));
+            }
+        }
+    }
+]
