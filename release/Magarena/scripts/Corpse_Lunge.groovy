6ab38f5b9057d88d4d430950bd068e5a3a141c1a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 22:13:19 +0100

    add Corpse Lunge

---
+++ release/Magarena/scripts/Corpse_Lunge.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_CREATURE,
+                payedCost.getTarget(),
+                this,
+                "SN deals damage equal to (RN)'s power to target creature.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int amount = event.getRefCard().getPower();
+                game.logAppendMessage(event.getPlayer(),"("+amount+")");
+                game.doAction(new DealDamageAction(event.getSource(), it, amount));
+            });
+        }
+    }
+]
