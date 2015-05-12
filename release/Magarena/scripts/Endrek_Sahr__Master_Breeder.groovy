e1143cb3cae8e70a30a6994536b9c96d7e325cbb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 02:40:15 +0100

    add Endrek Sahr, Master Breeder

---
+++ release/Magarena/scripts/Endrek_Sahr__Master_Breeder.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            final int amount = spell.getConvertedCost();
+            return spell.isFriend(permanent) && spell.hasType(MagicType.Creature) ?
+                new MagicEvent(
+                    permanent,
+                    amount,
+                    this,
+                    "PN puts X 1/1 black Thrull creature tokens onto the battlefield, where X is ("+spell.getName()+")'s converted mana cost. (X="+amount+")"
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new PlayTokensAction(
+                    event.getPlayer(),
+                    CardDefinitions.getToken("1/1 black Thrull creature token"),
+                    event.getRefInt()
+                ));
+        }
+    }
+]
