8d9f7a812a475faa4be7ff3bbddd920e6d81a206
melvinzhang <melvinzhang@gmail.com>
2015-05-12 18:13:48 +0800

    remove groovy code with card script

--- release/Magarena/scripts/Safe_Haven.groovy
+++ release/Magarena/scripts/Safe_Haven.groovy
@@ -1,37 +1,4 @@
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
-        "Exile"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source,"{2}"),
-                new MagicTapEvent(source)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                TARGET_CREATURE_YOU_CONTROL,
-                this,
-                "Exile target creature you control."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new ExileLinkAction(
-                    event.getPermanent(),
-                    it
-                )); 
-            });
-        }
-    },
     new MagicAtYourUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
