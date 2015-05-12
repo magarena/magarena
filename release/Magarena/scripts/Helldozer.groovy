ce004f03e339bf290fd1466e352ea649d6e83401
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 18:28:09 +0100

    add Helldozer

---
+++ release/Magarena/scripts/Helldozer.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{B}{B}{B}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_LAND,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target land.\$ If that land was nonbasic, untap SN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new DestroyAction(it));
+                if (it.hasType(MagicType.Basic) == false) {
+                    game.doAction(new UntapAction(event.getPermanent()));
+                }
+            });
+        }
+    }
+]
