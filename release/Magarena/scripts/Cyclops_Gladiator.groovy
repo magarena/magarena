4686d0b8e518d75f059b4fa45468a20a94a797ca
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-12 19:05:20 +0100

    add Cyclops Gladiator

---
+++ release/Magarena/scripts/Cyclops_Gladiator.groovy
@@ -0,0 +1,28 @@
+def choice = new MagicTargetChoice("target creature defending player controls");
+
+[
+    new MagicWhenSelfAttacksTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(choice),
+                new MagicDamageTargetPicker(permanent.getPower()),
+                this,
+                "PN may\$ have SN deal damage equal to its power to target creature defending player controls.\$ "+
+                "If you do, that creature deals damage equal to its power to SN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetPermanent(game, {
+                    final MagicPermanent attacker = event.getPermanent();
+                    game.doAction(new DealDamageAction(attacker,it,attacker.getPower()));
+                    game.doAction(new DealDamageAction(it,attacker,it.getPower()));
+                });
+            }
+        }
+    }
+]
