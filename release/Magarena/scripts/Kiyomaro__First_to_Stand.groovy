df46ba066b7eb75fe282cc13a4613d82fc7bb7cf
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-13 05:33:46 +0100

    add Kiyomaro, First to Stand

---
+++ release/Magarena/scripts/Kiyomaro__First_to_Stand.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicWhenDamageIsDealtTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return (damage.getSource() == permanent && permanent.getController().getHandSize() >= 7) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN gains 7 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeLifeAction(event.getPlayer(),7));
+        }
+    }
+]
