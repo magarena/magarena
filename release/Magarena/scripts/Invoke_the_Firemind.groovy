df081c8ba934612b6b6c2081d01628d160db6049
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-13 15:55:00 +0100

    move X value into getEvent() for Invoke the Firemind

--- release/Magarena/scripts/Invoke_the_Firemind.groovy
+++ release/Magarena/scripts/Invoke_the_Firemind.groovy
@@ -2,22 +2,22 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
             return new MagicEvent(
                 cardOnStack,
                 new MagicOrChoice(
                     MagicChoice.NONE,
                     NEG_TARGET_CREATURE_OR_PLAYER
                 ),
-                payedCost.getX(),
+                amount,
                 this,
                 "Choose one\$ - (1) draw X cards; " +
-                "or (2) SN deals X damage to target creature or player.\$" 
+                "or (2) SN deals X damage to target creature or player.\$ (X=${amount})" 
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final int amount = event.getRefInt();
-            game.logAppendMessage(event.getPlayer(),"(X="+amount+")");
             if (event.isMode(1)) {
                 game.doAction(new DrawAction(event.getPlayer(),amount));
             } else if (event.isMode(2)) {
