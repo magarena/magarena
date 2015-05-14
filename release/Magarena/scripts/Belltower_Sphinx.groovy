58c4ac74c89dd4f4241128180b63162a258dcf3d
melvinzhang <melvinzhang@gmail.com>
2015-05-14 10:18:47 +0800

    replace string interpolation with RN

--- release/Magarena/scripts/Belltower_Sphinx.groovy
+++ release/Magarena/scripts/Belltower_Sphinx.groovy
@@ -2,14 +2,13 @@
     new MagicWhenDamageIsDealtTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
-            final int amount = damage.getDealtAmount();
             return damage.getTarget() == permanent ?
                 new MagicEvent(
                     permanent,
                     damage.getSource().getController(),
-                    amount,
+                    damage.getDealtAmount(),
                     this,
-                    "PN puts ${amount} cards from the top of his or her library into his or her graveyard."
+                    "PN puts RN cards from the top of his or her library into his or her graveyard."
                 ):
                 MagicEvent.NONE;
         }
