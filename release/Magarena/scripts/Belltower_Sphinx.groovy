5865b2b7ba118d9948220486a8e6e019fc1f079f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-14 02:23:54 +0100

    fix: Belltower Sphinx's triggered ability should use the stack finally fixes #255

--- release/Magarena/scripts/Belltower_Sphinx.groovy
+++ release/Magarena/scripts/Belltower_Sphinx.groovy
@@ -1,12 +1,22 @@
 [
     new MagicWhenDamageIsDealtTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
             final int amount = damage.getDealtAmount();
-            if (damage.getTarget() == permanent) {
-                game.doAction(new MillLibraryAction(damage.getSource().getController(),amount));
-            }
-            return MagicEvent.NONE;
+            return damage.getTarget() == permanent ?
+                new MagicEvent(
+                    permanent,
+                    damage.getSource().getController(),
+                    amount,
+                    this,
+                    "PN puts ${amount} cards from the top of his or her library into his or her graveyard."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new MillLibraryAction(event.getPlayer(), event.getRefInt()));
         }
     }
 ]
