02ab25003bc49efdb88fdd6836a3a672b0522233
melvinzhang <melvinzhang@gmail.com>
2015-05-15 19:30:34 +0800

    modPlayer should only change the player that is passed in

--- release/Magarena/scripts/Dragonlord_Dromoka.groovy
+++ release/Magarena/scripts/Dragonlord_Dromoka.groovy
@@ -2,8 +2,9 @@
     new MagicStatic(MagicLayer.Player) {
         @Override
         public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            final MagicPlayer opponent = source.getOpponent();
-            opponent.setState(MagicPlayerState.CantCastSpells);
+            if (source.isOpponent(player)) {
+                player.setState(MagicPlayerState.CantCastSpells);
+            }
         }
         @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
