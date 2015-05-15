039a3811ea704fc9bc95fc427f140a6308f03815
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-15 23:26:28 +0100

    add Price of Knowledge

---
+++ release/Magarena/scripts/Price_of_Knowledge.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicAtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer player) {
+            return permanent.isOpponent(player) ?
+                new MagicEvent(
+                    permanent,
+                    player,
+                    this,
+                    "SN deals damage to PN equal to the number of cards in that player's hand."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getHandSize();
+            game.logAppendMessage(event.getPlayer(),"("+amount+")");
+            game.doAction(new DealDamageAction(event.getSource(), event.getPlayer(), amount));
+        }
+    }
+]
