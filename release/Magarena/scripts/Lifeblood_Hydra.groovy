dd3760ca1982936657ea412847c09c43c42b4051
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-16 03:43:34 +0100

    add Lifeblood Hydra

---
+++ release/Magarena/scripts/Lifeblood_Hydra.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN gains life and draw cards equal to SN's power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPermanent().getPower();
+            game.logAppendMessage(event.getPlayer(),"("+amount+")");
+            game.doAction(new ChangeLifeAction(event.getPlayer(), amount));
+            game.doAction(new DrawAction(event.getPlayer(), amount));
+        }
+    }
+]
