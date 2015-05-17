b6a9d5d3474bc8a9fa24ac7087eaa4e2e6de60c3
melvinzhang <melvinzhang@gmail.com>
2015-05-17 11:35:36 +0800

    simplify groovy code

--- release/Magarena/scripts/City_of_Solitude.groovy
+++ release/Magarena/scripts/City_of_Solitude.groovy
@@ -2,25 +2,10 @@
     new MagicStatic(MagicLayer.Player) {
         @Override
         public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            source.getController().setState(MagicPlayerState.CantCastSpells);
-            source.getController().setState(MagicPlayerState.CantActivateAbilities);
-
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return game.getTurnPlayer() == source.getOpponent();
-        }
-    },
-    new MagicStatic(MagicLayer.Player) {
-        @Override
-        public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            source.getOpponent().setState(MagicPlayerState.CantCastSpells);
-            source.getOpponent().setState(MagicPlayerState.CantActivateAbilities);
-
-        }
-        @Override
-        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return game.getTurnPlayer() == source.getController();
+            if (player != player.getGame().getTurnPlayer()) {
+                player.setState(MagicPlayerState.CantCastSpells);
+                player.setState(MagicPlayerState.CantActivateAbilities);
+            }
         }
     }
 ]
