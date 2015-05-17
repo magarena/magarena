2adf4276dda91739ab6f5a7f9a3822d08dd83523
melvinzhang <melvinzhang@gmail.com>
2015-05-17 11:36:08 +0800

    add missing target indication, replace if with ternary operator

--- release/Magarena/scripts/Crater_s_Claws.groovy
+++ release/Magarena/scripts/Crater_s_Claws.groovy
@@ -6,20 +6,20 @@
                 cardOnStack,
                 NEG_TARGET_CREATURE_OR_PLAYER,
                 this,
-                "SN deals X damage to target creature or player. " +
+                "SN deals X damage to target creature or player\$. " +
                 "SN deals X plus 2 damage to that creature or player instead if you control a creature with power 4 or greater."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = event.getCardOnStack().getX();
+            final int X = event.getCardOnStack().getX();
+            final boolean ferocious = event.getPlayer().controlsPermanent(CREATURE_POWER_4_OR_MORE); 
             event.processTarget(game, {
-                if (event.getPlayer().controlsPermanent(CREATURE_POWER_4_OR_MORE)) {
-
-                    game.doAction(new DealDamageAction(event.getSource(),it,amount + 2));
-                } else {
-                    game.doAction(new DealDamageAction(event.getSource(),it,amount));
-                }
+                game.doAction(new DealDamageAction(
+                    event.getSource(),
+                    it,
+                    ferocious ? X + 2 : X
+                ));
             });
         }
     }
