2fb715e35c10f9fc37449c8312a2ef9e2a6b75c3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-17 04:05:31 +0100

    add Volcanic Vision

---
+++ release/Magarena/scripts/Volcanic_Vision.groovy
@@ -0,0 +1,30 @@
+def choice = new MagicTargetChoice("target instant or sorcery card from your graveyard");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                choice,
+                this,
+                "Return target instant or sorcery card from your graveyard to your hand.\$ "+
+                "SN deals damage equal to that card's converted mana cost to each creature your opponents control. "+
+                "Exile SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                final int amount = it.getConvertedCost();
+                game.logAppendMessage(event.getPlayer(), "("+amount+")");
+                game.doAction(new RemoveCardAction(it, MagicLocationType.Graveyard));
+                game.doAction(new MoveCardAction(it, MagicLocationType.Graveyard, MagicLocationType.OwnersHand));
+                CREATURE_YOUR_OPPONENT_CONTROLS.filter(event) each {
+                    game.doAction(new DealDamageAction(event.getSource(), it, amount));
+                }
+                game.doAction(new ChangeCardDestinationAction(event.getCardOnStack(), MagicLocationType.Exile));
+            });
+        }
+    }
+]
