da746730fac146edad8f5a916dcf751e103bb0ec
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-17 02:56:31 +0100

    add Viashino Heretic

---
+++ release/Magarena/scripts/Viashino_Heretic.groovy
@@ -0,0 +1,36 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{1}{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_ARTIFACT,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target artifact.\$ SN deals damage to that artifact's controller equal to the artifact's converted mana cost."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPlayer player = event.getPlayer();
+                final int amount=it.getConvertedCost();
+                game.logAppendMessage(player,"("+amount+")");
+                game.doAction(new DestroyAction(it));
+                game.doAction(new DealDamageAction(event.getPermanent(), it.getController(), amount));
+            });
+        }
+    }
+]
