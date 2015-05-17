c9afecc9d0c7949011d06c22602080a92f7be385
PalladiaMors <unknown>
2015-05-17 21:56:45 +0200

    Add script for Avoid Fate

---
+++ release/Magarena/scripts/Avoid_Fate.groovy
@@ -0,0 +1,32 @@
+def INSTANT_OR_AURA_TARGETS_YOUR_PERM = new MagicStackFilterImpl() {
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicItemOnStack target) {
+        return (target.hasType(MagicType.Instant) || target.hasSubType(MagicSubType.Aura)) &&
+        target.getTarget().isPermanent() && target.getTarget().isFriend(player);
+    } 
+};
+
+def TARGET_INSTANT_OR_AURA_TARGETS_YOUR_PERM = new MagicTargetChoice(
+    INSTANT_OR_AURA_TARGETS_YOUR_PERM,
+    MagicTargetHint.Negative,
+    "target instant or aura that targets a permanent you control"
+);
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                TARGET_INSTANT_OR_AURA_TARGETS_YOUR_PERM,
+                this,
+                "Counter target instant or Aura spell\$ that targets a permanent you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                game.doAction(new CounterItemOnStackAction(it));
+            });
+        }
+    }
+]
