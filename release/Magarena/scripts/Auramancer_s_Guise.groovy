705ac67985623791cb735082f1c7d372389e30bf
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-20 15:52:40 +0100

    add Auramancer's Guise

---
+++ release/Magarena/scripts/Auramancer_s_Guise.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            int amount = permanent.getAuraPermanents().size();
+            pt.add(amount*2,amount*2);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
