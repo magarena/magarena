8d52cea689029519f5dd619898cc18a6dd728e0a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-21 15:23:23 +0100

    add Descendant of Masumaro

---
+++ release/Magarena/scripts/Descendant_of_Masumaro.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                TARGET_OPPONENT,
+                this,
+                "PN puts a +1/+1 counter on SN for each card in his or her hand, "+
+                "then removes a +1/+1 counter from SN for each card in target opponent's hand.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.doAction(new ChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    event.getPlayer().getHandSize()
+                ));
+                game.doAction(new ChangeCountersAction(
+                    event.getPermanent(),
+                    MagicCounterType.PlusOne,
+                    -it.getHandSize()
+                ))
+            });
+        }
+    }
+]
