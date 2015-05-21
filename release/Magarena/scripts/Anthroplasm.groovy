8cf2a2deba5f954b690a86191fcb123131533acb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-21 02:05:25 +0100

    remove final from Anthroplasm

--- release/Magarena/scripts/Anthroplasm.groovy
+++ release/Magarena/scripts/Anthroplasm.groovy
@@ -19,7 +19,7 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int oldCounters=0
+            int oldCounters=0;
             oldCounters-=event.getPermanent().getCounters(MagicCounterType.PlusOne);
             game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,oldCounters));
             game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,event.getRefInt()));
