2cb6bd4d011a773518ad822101d7349a75eceb30
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-21 01:55:24 +0100

    fix: Aurification type and ability change not working

--- release/Magarena/scripts/Aurification.groovy
+++ release/Magarena/scripts/Aurification.groovy
@@ -1,26 +1,27 @@
-def AB = new MagicStatic(MagicLayer.Ability) {
-    @Override
-    public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
-        permanent.addAbility(MagicAbility.Defender, flags);
-    }
-    @Override
-    public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-        return target.getCounters(MagicCounterType.Gold) > 0;
-    }
-};
-
-def ST = new MagicStatic(MagicLayer.Type) {
-    @Override
-    public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
-        flags.add(MagicSubType.Wall);
-    }
-    @Override
-    public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
-        return target.getCounters(MagicCounterType.Gold) > 0;
-    }
-};
-
 [
+    new MagicStatic(MagicLayer.Ability, CREATURE) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.Defender, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return target.getCounters(MagicCounterType.Gold) > 0;
+        }
+    },
+
+    new MagicStatic(MagicLayer.Type, CREATURE) {
+        @Override
+        public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
+            flags.add(MagicSubType.Wall);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return target.getCounters(MagicCounterType.Gold) > 0;
+        }
+    },
+
+
     new MagicWhenSelfLeavesPlayTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final RemoveFromPlayAction act) {
