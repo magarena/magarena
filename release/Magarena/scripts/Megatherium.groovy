b3014e549d4822223beb2371aa9a82036428c16b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-22 22:28:18 +0100

    add Megatherium

---
+++ release/Magarena/scripts/Megatherium.groovy
@@ -0,0 +1,34 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.isNo()) {
+        game.doAction(new SacrificeAction(event.getPermanent()));
+    }
+}
+
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN sacrifices SN unless he or she pays {1} for each card in his or her hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = player.getHandSize();
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                player,
+                new MagicMayChoice(
+                    new MagicPayManaCostChoice(MagicManaCost.create("{"+amount+"}"))
+                ),
+                event.getPermanent(),
+                action,
+                "PN may\$ pay {"+amount+"}."
+            ));
+        }
+    }
+]
