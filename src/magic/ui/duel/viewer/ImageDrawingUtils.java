0f6c444cd9406323c4aaf4de6388baf317d12839
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-24 21:38:03 +0100

    add Wage Counter (use bribe counter image)

--- src/magic/ui/duel/viewer/ImageDrawingUtils.java
+++ src/magic/ui/duel/viewer/ImageDrawingUtils.java
@@ -386,6 +386,10 @@ public class ImageDrawingUtils {
                     g.drawImage(IconImages.getIcon(MagicIcon.GROWTHCOUNTER).getImage(),ax,ay,observer);
                     if (amount > 1){drawStringWithOutline(g, str, ax+6, ay+14, observer);}
                     ax+=inc;
+                }  else if (counterType == MagicCounterType.Wage) {
+                    g.drawImage(IconImages.getIcon(MagicIcon.BRIBECOUNTER).getImage(),ax,ay,observer);
+                    if (amount > 1){drawStringWithOutline(g, str, ax+6, ay+14, observer);}
+                    ax+=inc;
                 }
             }
         }
