b2625e48f15db030d55bbe0402400c65e8f93a9b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-24 21:38:20 +0100

    add Rogue Skycaptain

---
+++ release/Magarena/scripts/Rogue_Skycaptain.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            game.doAction(new ChangeCountersAction(
+                permanent,
+                MagicCounterType.Wage,
+                1
+            ));
+            final int amount = permanent.getCounters(MagicCounterType.Wage) *2;
+            return amount > 0 ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(new MagicPayManaCostChoice(MagicManaCost.create("{"+amount+"}"))),
+                    this,
+                    "PN may\$ pay {"+amount+"}. If he or she doesn't, remove all wage counters from SN "+
+                    "and an opponent gains control of it."
+                ):
+                MagicEvent.NONE;
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            if (event.isNo()) {
+                game.doAction(new ChangeCountersAction(permanent, MagicCounterType.Wage, -permanent.getCounters(MagicCounterType.Wage)));
+                game.doAction(new GainControlAction(event.getPlayer().getOpponent(), permanent));
+            }
+        }
+    }
+]
