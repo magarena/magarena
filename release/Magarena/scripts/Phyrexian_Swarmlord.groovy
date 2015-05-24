0d3b3445d4321fd026eee98fb72745d6549b5ca4
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-24 03:18:44 +0100

    add Phyrexian Swarmlord

---
+++ release/Magarena/scripts/Phyrexian_Swarmlord.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 1/1 green Insect creature token with infect onto the battlefield "+
+                "for each poison counter PN's opponents have."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = player.getOpponent().getPoison();
+            game.logAppendMessage(player, "("+amount+")");
+            game.doAction(new PlayTokensAction(
+                player,
+                CardDefinitions.getToken("1/1 green Insect creature token with infect"),
+                amount
+            ));
+        }
+    }
+]
