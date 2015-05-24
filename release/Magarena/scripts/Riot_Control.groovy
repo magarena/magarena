cb900cf607dc006d04a8287262e4609230e1456f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-24 19:56:11 +0100

    add Riot Control

---
+++ release/Magarena/scripts/Riot_Control.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN gains 1 life for each creature his or her opponents control. "+
+                "Prevent all damage that would be dealt to PN this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = CREATURE_YOU_CONTROL.filter(player.getOpponent()).size();
+            game.logAppendValue(player,amount)
+            game.doAction(new ChangeLifeAction(player, amount));
+            game.doAction(new AddTurnTriggerAction(
+                MagicPreventDamageTrigger.PreventDamageDealtToYou(player)
+            ));
+        }
+    }
+]
