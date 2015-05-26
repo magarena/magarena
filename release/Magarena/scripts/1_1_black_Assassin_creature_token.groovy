37fbc555d30cb60d9c6f16f7897dbe5292544a13
melvinzhang <melvinzhang@gmail.com>
2015-05-26 20:15:55 +0800

    replace checks with more specific trigger

--- release/Magarena/scripts/1_1_black_Assassin_creature_token.groovy
+++ release/Magarena/scripts/1_1_black_Assassin_creature_token.groovy
@@ -1,19 +1,13 @@
 [
-    new MagicWhenDamageIsDealtTrigger() {
+    new MagicWhenSelfCombatDamagePlayerTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            final MagicSource source = damage.getSource();
-            return (source == permanent &&
-                    damage.isCombat() &&
-                    damage.isTargetPlayer()) ?
-                new MagicEvent(
-                    permanent,
-                    permanent.getController(),
-                    damage.getTarget(),
-                    this,
-                    "RN loses the game."
-                ) :
-                MagicEvent.NONE;
+            return new MagicEvent(
+                permanent,
+                damage.getTarget(),
+                this,
+                "RN loses the game."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
