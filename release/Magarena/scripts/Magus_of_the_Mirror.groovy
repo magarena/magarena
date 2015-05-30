95e5300a28ebecd346eff463ce64c54b4b013951
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-30 15:59:34 +0100

    add Magus of the Mirror

---
+++ release/Magarena/scripts/Magus_of_the_Mirror.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.YOUR_UPKEEP_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Swap"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicSacrificeEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_OPPONENT,
+                this,
+                "PN exchanges life totals with target opponent.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer opponent ->
+                final MagicPlayer player = event.getPlayer();
+                final int opponentLife = opponent.getLife();
+                final int playerLife = player.getLife();
+                game.doAction(new ChangeLifeAction(player, opponentLife-playerLife));
+                game.doAction(new ChangeLifeAction(opponent, playerLife-opponentLife));
+            });
+        }
+    }
+]
