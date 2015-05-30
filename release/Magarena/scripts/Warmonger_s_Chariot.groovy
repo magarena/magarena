81ccd71cffc76994d584489cff977405730f691d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-05-30 16:14:30 +0100

    add Warmonger's Chariot

---
+++ release/Magarena/scripts/Warmonger_s_Chariot.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            if (permanent.hasAbility(MagicAbility.Defender)) {
+                permanent.addAbility(MagicAbility.CanAttackWithDefender, flags);
+            }
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Black);
+        }
+    }
+]
