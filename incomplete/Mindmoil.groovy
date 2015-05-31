47d367496cf513ecd230ff1bdc94dcb85545f2c2
melvinzhang <melvinzhang@gmail.com>
2015-05-31 21:12:56 +0800

    move 'Teferi's Puzzle Box' and 'Mindmoil' to incomplete due to slow play in current implementation

--- incomplete/Mindmoil.groovy
+++ incomplete/Mindmoil.groovy
@@ -1,36 +1,26 @@
-def EventAction = {
-    final MagicGame game, final MagicEvent event ->
-    event.processChosenCards(game, {
-        final MagicCard card ->
-        game.doAction(new MagicRemoveCardAction(card,MagicLocationType.OwnersHand));
-        game.doAction(new MagicMoveCardAction(card,MagicLocationType.OwnersHand,MagicLocationType.BottomOfOwnersLibrary));
-    });
-    game.doAction(new MagicDrawAction(event.getPlayer(),event.getRefInt()));
-};
-
 [
     new MagicWhenOtherSpellIsCastTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {
-            return (permanent.isFriend(cardOnStack)) ?
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicCardOnStack spell) {
+            return (permanent.isFriend(spell)) ? 
                 new MagicEvent(
                     permanent,
                     this,
-                    "PN puts the cards in his or her hand on the bottom of his or her library in any order, then draws that many cards."
+                    "PN puts the cards in his or her hand on the bottom of his or her library in any order, "+
+                    "then draws that many cards."
                 ):
                 MagicEvent.NONE;
         }
+
         @Override
-        public void executeEvent(final MagicGame game,final MagicEvent event) {
-            final List<MagicCard> choiceList = event.getPlayer().getHand();
-            game.addEvent(new MagicEvent(
-                event.getSource(),
-                event.getPlayer(),
-                new MagicFromCardListChoice(choiceList, choiceList.size()),
-                choiceList.size(),
-                EventAction,
-                ""
-            )); 
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final MagicCardList hand = new MagicCardList(player.getHand());
+            for (final MagicCard card : hand) {
+                game.addEvent(new MagicTuckCardEvent(event.getPermanent(), event.getPlayer(),false));
+            }
+            game.logAppendMessage(player,"PN puts "+hand.size()+" cards on the bottom of his or her library.");
+            game.addEvent(new MagicDrawEvent(event.getPermanent(), event.getPlayer(), hand.size()));
         }
     }
 ]
