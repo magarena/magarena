772219c3206f100d9a9d3123e434c65be18588c0
lodici <spr11766@fastmail.net>
2015-06-01 17:56:00 +0100

    add legality icon legend to DeckLegalityTabPanel.

--- src/magic/ui/deck/editor/DeckLegalityTabPanel.java
+++ src/magic/ui/deck/editor/DeckLegalityTabPanel.java
@@ -1,11 +1,18 @@
 package magic.ui.deck.editor;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import magic.data.MagicIcon;
 import magic.model.MagicCardDefinition;
 import magic.model.MagicDeck;
+import magic.ui.IconImages;
 import magic.ui.cardtable.CardTablePanel;
+import magic.ui.widget.TexturedPanel;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
@@ -13,6 +20,8 @@ class DeckLegalityTabPanel extends JPanel {
 
     // fired when card selection changes
     public static final String CP_CARD_SELECTED = CardTablePanel.CP_CARD_SELECTED;
+
+    private static final JPanel HELP_PANEL = new LegalityLegendPanel();
 
     private final CardsLegalityPanel cardsLegalityPanel;
     private final FormatsLegalityPanel formatsLegalityPanel;
@@ -59,13 +68,15 @@ class DeckLegalityTabPanel extends JPanel {
     private void setLookAndFeel() {
         setOpaque(false);
         setLayout(miglayout);
+        HELP_PANEL.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.DARK_GRAY));
     }
 
     private void refreshLayout() {
         removeAll();
-        miglayout.setLayoutConstraints("insets 0, gap 0");
+        miglayout.setLayoutConstraints("insets 0, gap 0, wrap 2");
+        add(HELP_PANEL, "w 100%, spanx 2");
         add(formatsLegalityPanel, "w 280!, h 100%");
-        add(cardsLegalityPanel, "w 100%, h 100%");
+        add(cardsLegalityPanel, "w 100%, h 100%");       
         revalidate();
     }
 
@@ -76,6 +87,35 @@ class DeckLegalityTabPanel extends JPanel {
     void setDeck(final MagicDeck aDeck) {
         this.deck = aDeck;
         formatsLegalityPanel.setDeck(aDeck);
+    }
+
+    private static class LegalityLegendPanel extends TexturedPanel {
+
+        public LegalityLegendPanel() {
+
+            setPreferredSize(new Dimension(getWidth(), 30));
+            setMinimumSize(getPreferredSize());
+
+            setLayout(new MigLayout("gapx 20, aligny center"));
+
+            add(getIconLabel(MagicIcon.LEGAL_ICON, "Legal"));
+            add(getIconLabel(MagicIcon.ILLEGAL_ICON, "Illegal"));
+            add(getIconLabel(MagicIcon.BANNED_ICON, "Banned"));
+            add(getIconLabel(MagicIcon.RESTRICTED_ICON, "Restricted", "Only one copy of a card allowed in the deck."));
+            add(getIconLabel(MagicIcon.RESTRICTED_ICON, "Copies exceeded", "<html>Does not apply to basic lands,<br><i>Relentless Rats</i> or <i>Shadowborn Apostle</i>.</html>"));
+        }
+
+        private JLabel getIconLabel(MagicIcon magicIcon, String text, String tooltip) {
+            final JLabel lbl = new JLabel(text);
+            lbl.setIcon(IconImages.getIcon(magicIcon));
+            lbl.setToolTipText(tooltip);
+            return lbl;
+        }
+
+        private JLabel getIconLabel(MagicIcon magicIcon, String text) {
+            return getIconLabel(magicIcon, text, null);
+        }
+
     }
 
 }
