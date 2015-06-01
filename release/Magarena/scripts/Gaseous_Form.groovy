62396b1047845f56e4f64780b65b489fcd68f5ef
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-01 16:13:12 +0100

    add Gaseous Form

---
+++ release/Magarena/scripts/Gaseous_Form.groovy
@@ -0,0 +1,14 @@
+[
+    new MagicPreventDamageTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            if (damage.getSource() == permanent.getEnchantedPermanent() || damage.getTarget() == permanent.getEnchantedPermanent()) {
+                if (damage.isCombat()) {
+                    // Replacement effect. Generates no event or action.
+                    damage.prevent();
+                }
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
