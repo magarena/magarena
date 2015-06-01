edfb502579ff7a00d2dca0eb40a1d8d9d8c88400
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-01 14:51:26 +0100

    add Ritual of the Returned

---
+++ release/Magarena/scripts/Ritual_of_the_Returned.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                this,
+                "PN exiles target creature card from his or her graveyard.\$ PN puts a "+
+                "black Zombie creature token onto the battlefield. Its power is equal to "+
+                "that card's power and its toughness is equal to that card's toughness."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                final MagicCard target ->
+                final int power = target.getPower();
+                final int toughness = target.getToughness();
+                game.doAction(new MoveCardAction(target, MagicLocationType.Graveyard, MagicLocationType.Exile));
+                game.doAction(new RemoveCardAction(target, MagicLocationType.Graveyard));
+                game.doAction(new PlayTokenAction(event.getPlayer(), MagicCardDefinition.create({
+                    it.setName("Zombie");
+                    it.setDistinctName("black Zombie creature token");
+                    it.setPowerToughness(power, toughness);
+                    it.setColors("b");
+                    it.addSubType(MagicSubType.Zombie);
+                    it.addType(MagicType.Creature);
+                    it.setToken();
+                    it.setValue(Math.min((power+toughness)/2,5));
+                })));
+            });
+        }
+    }
+]
