0edb29cab09c6aa4b3f35ea3c61f752dd308cd3d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-03 23:34:29 +0100

    add Dwarven Armory

---
+++ release/Magarena/scripts/Dwarven_Armory.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.ANY_UPKEEP_CONDITION],
+        new MagicActivationHints(MagicTiming.None),
+        "Pump"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}"),
+                new MagicSacrificePermanentEvent(source,SACRIFICE_LAND)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                POS_TARGET_CREATURE,
+                this,
+                "PN puts a +2/+2 counter on target creature.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new ChangeCountersAction(it, MagicCounterType.PlusTwo, 1));
+            });
+        }
+    }
+]
