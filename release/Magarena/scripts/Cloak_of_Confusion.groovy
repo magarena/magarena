5556afea960a1561215c0582ccb0638fd0a5a7f3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-03 23:06:52 +0100

    add Cloak of Confusion

---
+++ release/Magarena/scripts/Cloak_of_Confusion.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicWhenAttacksUnblockedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
+            return (creature == permanent.getEnchantedPermanent()) ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(),
+                    creature,
+                    this,
+                    "PN may\$ have RN assign no combat damage this turn. If PN does, defending player discards a card at random."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(ChangeStateAction.Set(event.getRefPermanent(), MagicPermanentState.NoCombatDamage));
+                game.addEvent(MagicDiscardEvent.Random(event.getSource(), game.getDefendingPlayer(), 1));
+            }
+        }
+    }
+]
