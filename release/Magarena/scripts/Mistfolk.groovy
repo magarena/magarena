c79439bf56393c6e01133a8bb7ab19183764aa5e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-04 16:56:11 +0100

    add Mistfolk

---
+++ release/Magarena/scripts/Mistfolk.groovy
@@ -0,0 +1,44 @@
+def SPELL_THAT_TARGETS_SOURCE = new MagicStackFilterImpl() {
+    public boolean accept(final MagicSource source, final MagicPlayer player, final MagicItemOnStack target) {
+        return target.isSpell() && target.getTarget() == source;
+    }
+};
+
+def TARGET_SPELL_THAT_TARGETS_SN = {
+    final MagicPermanent source ->
+    return new MagicTargetChoice(
+        SPELL_THAT_TARGETS_SOURCE,
+        MagicTargetHint.Negative,
+        "target spell that targets ${source.getName()}"
+    );
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Counter),
+        "Counter"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{U}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_SPELL_THAT_TARGETS_SN(source),
+                this,
+                "Counter target spell that targets SN."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetItemOnStack(game, {
+                game.doAction(new CounterItemOnStackAction(it));
+            });
+        }
+    }
+]
