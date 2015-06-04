bcf4fc873b09c93a4791d8c386468560a9b4234b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-04 03:45:47 +0100

    add Goblin Ski Patrol

---
+++ release/Magarena/scripts/Goblin_Ski_Patrol.groovy
@@ -0,0 +1,46 @@
+def SNOW_MOUNTAIN = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicSource source, final MagicPlayer player, final MagicPermanent target) {
+            return target.hasType(MagicType.Snow) &&
+                   target.hasSubType(MagicSubType.Mountain);
+        }
+}
+
+def SNOW_MOUNTAIN_CONDITION = new MagicCondition() {
+        public boolean accept(final MagicSource source) {
+            return source.getController().controlsPermanent(SNOW_MOUNTAIN);
+        }
+    };
+
+[
+    new MagicPermanentActivation(
+        [MagicCondition.ABILITY_ONCE_CONDITION, SNOW_MOUNTAIN_CONDITION],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Flying"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPlayAbilityEvent(source),
+                new MagicPayManaCostEvent(source, "{1}{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN gets +2/+0 and gains flying. PN sacrifices it at the beginning of the next end step."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new ChangeTurnPTAction(permanent, 2, 0));
+            game.doAction(new GainAbilityAction(permanent, MagicAbility.Flying));
+            game.doAction(new AddTurnTriggerAction(permanent, MagicAtEndOfTurnTrigger.Sacrifice))
+        }
+    }
+]
