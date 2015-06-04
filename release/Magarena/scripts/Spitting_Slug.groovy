cd692bae0621f1a6d50c157a95f74a9086860fa7
melvinzhang <melvinzhang@gmail.com>
2015-06-04 10:00:38 +0800

    should use MagicWhenSelfBlocksTrigger and MagicWhenSelfBecomesBlockedTrigger instead of MagicWhenBlocksOrBecomesBlockedTrigger

--- release/Magarena/scripts/Spitting_Slug.groovy
+++ release/Magarena/scripts/Spitting_Slug.groovy
@@ -1,32 +1,44 @@
+def EVENT_ACTION = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicPermanent permanent = event.getPermanent()
+    if (event.isYes()) {
+        game.doAction(new GainAbilityAction(permanent, MagicAbility.FirstStrike));
+    } else {
+        if (permanent.isBlocking()) {
+            game.doAction(new GainAbilityAction(permanent.getBlockedCreature(), MagicAbility.FirstStrike));
+        } else {
+            permanent.getBlockingCreatures() each {
+                game.doAction(new GainAbilityAction(it, MagicAbility.FirstStrike));
+            }
+        }
+    }
+}
+
+def EVENT = {
+    final MagicPermanent permanent ->
+    return new MagicEvent(
+        permanent,
+        new MagicMayChoice(
+            "Pay {1}{G}?",
+            new MagicPayManaCostChoice(MagicManaCost.create("{1}{G}"))
+        ),
+        EVENT_ACTION,
+        "PN may\$ pay {1}{G}. If PN does, SN gains first strike until end of turn. Otherwise, "+
+        "each creature blocking or blocked by SN gains first strike until end of turn."
+    );
+}
+
 [
-    new MagicWhenBlocksOrBecomesBlockedTrigger() {
+    new MagicWhenSelfBlocksTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocker) {
-            return new MagicEvent(
-                permanent,
-                new MagicMayChoice(
-                    "Pay {1}{G}?",
-                    new MagicPayManaCostChoice(MagicManaCost.create("{1}{G}"))
-                ),
-                this,
-                "PN may\$ pay {1}{G}. If PN does, SN gains first strike until end of turn. Otherwise, "+
-                "each creature blocking or blocked by SN gains first strike until end of turn."
-            );
+            return EVENT(permanent);
         }
+    },
+    new MagicWhenSelfBecomesBlockedTrigger() {
         @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent permanent = event.getPermanent()
-            if (event.isYes()) {
-                game.doAction(new GainAbilityAction(permanent, MagicAbility.FirstStrike));
-            } else {
-                if (permanent.isBlocking()) {
-                    game.doAction(new GainAbilityAction(permanent.getBlockedCreature(), MagicAbility.FirstStrike));
-                } else {
-                    permanent.getBlockingCreatures() each {
-                        game.doAction(new GainAbilityAction(it, MagicAbility.FirstStrike));
-                    }
-                }
-            }
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent blocked) {
+            return EVENT(permanent);
         }
     }
 ]
