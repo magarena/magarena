f354f7123aa2cb82f9925a534e29572ce768d452
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-04 17:27:39 +0100

    add Monsoon

---
+++ release/Magarena/scripts/Monsoon.groovy
@@ -0,0 +1,32 @@
+def UNTAPPED_ISLAND_YOU_CONTROL = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPermanent target) {
+        return target.hasSubType(MagicSubType.Island) && target.isUntapped() && target.isController(player);
+    }
+};
+
+[
+    new MagicAtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
+            return new MagicEvent(
+                permanent,
+                eotPlayer,
+                this,
+                "Tap all untapped Islands PN controls and Monsoon deals X damage to him or her, "+
+                "where X is the number of Islands tapped this way."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final int amount = UNTAPPED_ISLAND_YOU_CONTROL.filter(player).size();
+            game.logAppendX(player, amount);
+            if (amount > 0) {
+                UNTAPPED_ISLAND_YOU_CONTROL.filter(player) each {
+                    game.doAction(new TapAction(it));
+                }
+                game.doAction(new DealDamageAction(event.getSource(), player, amount));
+            }
+        }
+    }
+]
