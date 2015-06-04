8169d8c1a8375332ec829a0a9c07b312283912c1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-04 01:21:39 +0100

    add TestLandPreference for issue #333

---
+++ src/magic/test/TestLandPreference.java
@@ -0,0 +1,36 @@
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicUpkeepPhase;
+
+class TestLandPreference extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS,8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        game.setTurnPlayer(game.getPlayer(1));
+        game.setTurn(24);
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Forest", 22);
+        createPermanent(P,"Steel Wall",4);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Breaching Leviathan", 1);
+        addToLibrary(P, "Island", 5);
+        addToHand(P, "Savaen Elves",4);
+        addToHand(P, "Island", 3);
+        createPermanent(P,"Forest");
+
+        return game;
+    }
+}
