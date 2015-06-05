3d6444ff8ccee0d62395c7930631b4501c6d4b5c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-05 04:31:10 +0100

    add Gnat Miser

---
+++ release/Magarena/scripts/Gnat_Miser.groovy
@@ -0,0 +1,10 @@
+[
+    new MagicStatic(MagicLayer.Player) {
+        @Override
+        public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
+            if (source.isEnemy(player)) {
+                player.reduceMaxHandSize(1);
+            }
+        }
+    }
+]
