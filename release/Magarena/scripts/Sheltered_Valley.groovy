9a43020944260ecfe43af0d15209e1ffc833579e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-05 17:38:24 +0100

    add Sheltered Valley

---
+++ release/Magarena/scripts/Sheltered_Valley.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicWhenComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            final MagicPlayer player = permanent.getController();
+            permanentName("Sheltered Valley", Control.You).except(permanent).filter(player) each {
+                game.doAction(new SacrificeAction(it));
+                game.logAppendMessage(player, "(${it.getName()}) is sacrificed.");
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
