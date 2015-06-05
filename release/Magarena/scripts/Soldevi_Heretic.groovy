bd0e27ffdc3e647f347decc0306fb7ceb215ee8f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-05 18:03:53 +0100

    add Soldevi Heretic

---
+++ release/Magarena/scripts/Soldevi_Heretic.groovy
@@ -0,0 +1,52 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.isYes()) {
+        game.doAction(new DrawAction(event.getPlayer()));
+    }
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Prevent"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{W}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                POS_TARGET_CREATURE,
+                MagicPreventTargetPicker.create(),
+                this,
+                "Prevent the next 2 damage that would be dealt to target creature\$ this turn. "+
+                "Target opponent (${source.getOpponent().getName()}) may draw a card."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new PreventDamageAction(it, 2));
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    event.getSource().getOpponent(),
+                    new MagicSimpleMayChoice(
+                        MagicSimpleMayChoice.DRAW_CARDS,
+                        1,
+                        MagicSimpleMayChoice.DEFAULT_NONE
+                    ),
+                    action,
+                    "PN may\$ draw a card."
+                ));
+            });
+        }
+    }
+]
