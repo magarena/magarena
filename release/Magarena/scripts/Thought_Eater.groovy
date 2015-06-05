daa56aa9ae290ed9573dcc97cb90923c9406438d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-05 04:22:46 +0100

    correct 'Thought' creatures implementation

--- release/Magarena/scripts/Thought_Eater.groovy
+++ release/Magarena/scripts/Thought_Eater.groovy
@@ -2,7 +2,9 @@
     new MagicStatic(MagicLayer.Player) {
         @Override
         public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            source.getController().reduceMaxHandSize(3);
+            if (source.isController(player)) {
+                player.reduceMaxHandSize(3);
+            }
         }
     }
 ]
--- release/Magarena/scripts/Thought_Nibbler.groovy
+++ release/Magarena/scripts/Thought_Nibbler.groovy
@@ -2,7 +2,9 @@
     new MagicStatic(MagicLayer.Player) {
         @Override
         public void modPlayer(final MagicPermanent source, final MagicPlayer player) {
-            source.getController().reduceMaxHandSize(2);
+            if (source.isController(player)){
+                player.reduceMaxHandSize(2);
+            }
         }
     }
 ]
