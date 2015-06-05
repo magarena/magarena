0d9137ad2d087fdb5855c053842c8d75a4dcd088
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-05 03:34:57 +0100

    Test Scenario for hand modification

---
+++ src/magic/test/TestHandSize.java
@@ -0,0 +1,39 @@
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicUpkeepPhase;
+
+// There's 4 elves and a bunch of island in the AI's hand - after 4 draws it
+// will draw a Breaching Leviathan which it won't be able to cast as it hasn't
+// been playing any islands
+class TestHandSize extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS,8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        game.setTurnPlayer(game.getPlayer(1));
+        game.setTurn(24);
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 22);
+        createPermanent(P,"Island",5);
+        addToHand(P, "Thought Nibbler", 1);
+        addToHand(P, "Thought Eater", 1);
+        addToHand(P, "Thought Devourer", 1);
+        addToHand(P, "Island", 3);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 20);
+
+        return game;
+    }
+}
