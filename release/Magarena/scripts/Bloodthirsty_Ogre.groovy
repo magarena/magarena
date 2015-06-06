4a78fee0a600883f89763e9b331d78d9f257687a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-06 22:05:31 +0100

    add Bloodthirsty Ogre

---
+++ release/Magarena/scripts/Bloodthirsty_Ogre.groovy
@@ -0,0 +1,37 @@
+def DEMON_CONDITION = MagicConditionFactory.YouControl(MagicTargetFilterFactory.creature(MagicSubType.Demon, Control.You));
+[
+    new MagicPermanentActivation(
+        [DEMON_CONDITION, new MagicArtificialCondition(MagicConditionFactory.CounterAtLeast(MagicCounterType.Devotion,1))],
+        new MagicActivationHints(MagicTiming.Removal),
+        "Weaken"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final int amount = source.getCounters(MagicCounterType.Devotion);
+            return new MagicEvent(
+                source,
+                NEG_TARGET_CREATURE,
+                new MagicWeakenTargetPicker(amount,amount),
+                amount,
+                this,
+                "Target creature\$ gets -X/-X until end of turn, where X is the number of devotion counters "+
+                "on SN. Activate this ability only if you control a Demon."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final int amount = event.getPermanent().getCounters(MagicCounterType.Devotion);
+                game.logAppendX(event.getPlayer(), amount);
+                game.doAction(new ChangeTurnPTAction(it, -amount, -amount));
+            });
+        }
+    }
+]
