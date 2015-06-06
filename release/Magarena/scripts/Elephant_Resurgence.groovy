eb70dfb6b7b9684c28f05daaca57cdfce6000952
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-06 14:41:30 +0100

    add Elephant Resurgence

---
+++ release/Magarena/scripts/Elephant_Resurgence.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player puts a green Elephant creature token onto the battlefield. "+
+                "Those creatures have \"This creature's power and toughness are each equal to "+
+                "the number of creature cards in its controller's graveyard.\""
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPlayer player : game.getPlayers()) {
+                game.doAction(new PlayTokenAction(
+                    player,
+                    CardDefinitions.getToken("green Elephant creature token")
+                ))
+            }
+        }
+    }
+]
