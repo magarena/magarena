f63ec901a70cda8c203f3392ed0634d83b2d23e1
melvinzhang <melvinzhang@gmail.com>
2015-06-06 08:28:54 +0800

    fix crash due to Curse Artifact referencing non existent RefPlayer

--- release/Magarena/scripts/Curse_Artifact.groovy
+++ release/Magarena/scripts/Curse_Artifact.groovy
@@ -2,24 +2,26 @@
     new MagicAtUpkeepTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.getEnchantedPermanent().getController() == upkeepPlayer ? 
+            final MagicPermanent enchanted = permanent.getEnchantedPermanent();
+            return enchanted.isController(upkeepPlayer) ?
                 new MagicEvent(
                     permanent,
                     upkeepPlayer,
-                    new MagicMayChoice("Have SN deal 2 damage to you?"),
-                    permanent.getEnchantedPermanent(),
+                    new MagicMayChoice(),
+                    enchanted,
                     this,
-                    "SN deals 2 damage to RN's controller unless he or she sacrifices RN."
+                    "PN may\$ sacrifice RN. If he or she doesn't, SN deals 2 damage to PN."
                 ):
                 MagicEvent.NONE
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicPermanent enchanted = event.getRefPermanent();
-            if (event.isYes()) {
-                game.doAction(new DealDamageAction(event.getPermanent(), event.getRefPlayer(), 2));
-            } else {
+            final MagicPlayer player = event.getPlayer();
+            if (event.isYes() && enchanted.isController(player)) {
                 game.doAction(new SacrificeAction(enchanted));
+            } else {
+                game.doAction(new DealDamageAction(event.getPermanent(), event.getPlayer(), 2));
             }
         }
     }
