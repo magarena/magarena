7b036dbd2b046ea6ec102b1f3f3be43b04ad0689
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-06 10:29:45 +0100

    add Branded Brawlers

---
+++ release/Magarena/scripts/Branded_Brawlers.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.CannotAttack, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return game.getDefendingPlayer().controlsPermanent(UNTAPPED_LAND);
+        }
+    },
+    
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.CannotBlock, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return source.getController().controlsPermanent(UNTAPPED_LAND);
+        }
+    }
+]
