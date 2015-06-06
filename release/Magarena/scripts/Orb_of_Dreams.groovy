9961c4fdb5bbedf849ffbc4a077dec0ce56e26d8
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-06 23:26:49 +0100

    add Orb of Dreams

---
+++ release/Magarena/scripts/Orb_of_Dreams.groovy
@@ -0,0 +1,9 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            game.doAction(TapAction.Enters(otherPermanent));
+            return MagicEvent.NONE;
+        }
+    }
+]
