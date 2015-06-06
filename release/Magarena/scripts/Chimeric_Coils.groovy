982cb4cd8d1c3086475134e0266b599f11f1333e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-06 18:52:30 +0100

    add Chimeric Coils

---
+++ release/Magarena/scripts/Chimeric_Coils.groovy
@@ -0,0 +1,52 @@
+def PT = {
+    final int amount ->
+    return new MagicStatic(MagicLayer.SetPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.set(amount,amount);
+        }
+    };
+}
+
+def ST = new MagicStatic(MagicLayer.Type) {
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent, final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Construct);
+    }
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags|MagicType.Creature.getMask()|MagicType.Artifact.getMask();
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Animate),
+        "Becomes"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{X}{1}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            final int amount = payedCost.getX();
+            return new MagicEvent(
+                source,
+                amount,
+                this,
+                "Chimeric Coils becomes an X/X Construct artifact creature. "+
+                "Sacrifice it at the beginning of the next end step. (X=${amount})"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new BecomesCreatureAction(permanent, PT(event.getRefInt()), ST));
+            game.doAction(new AddTriggerAction(permanent, MagicAtEndOfTurnTrigger.Sacrifice));
+        }
+    }
+]
