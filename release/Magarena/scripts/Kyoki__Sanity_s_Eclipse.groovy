46a0b19d49fbd9d9af010e8b97da1d4da93904a9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-07 15:25:39 +0100

    add Kyoki, Sanity's Eclipse

---
+++ release/Magarena/scripts/Kyoki__Sanity_s_Eclipse.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicWhenYouCastSpiritOrArcaneTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack spell) {
+            return new MagicEvent(
+                permanent,
+                TARGET_OPPONENT,
+                this,
+                "Target opponent\$ exiles a card from his or her hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.addEvent(new MagicExileCardEvent(event.getSource(), it, A_CARD_FROM_HAND));
+            });
+        }
+    }
+]
