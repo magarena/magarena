5ebbad2a3b625bdf1f5ecc814f2e75e92a192618
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-07 01:24:44 +0100

    add That Which Was Taken

---
+++ release/Magarena/scripts/That_Which_Was_Taken.groovy
@@ -0,0 +1,58 @@
+def EXCEPT_SELF = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicSource source, final MagicPlayer player, final MagicPermanent target) {
+        return target != source && target.isController(player);
+    }
+};
+
+def TARGET_PERMANENT_EXCEPT = {
+    final MagicSource source ->
+    return new MagicTargetChoice(
+        EXCEPT_SELF,
+        MagicTargetHint.Positive,
+        "a permanent except ${source.getName()}"
+    );
+};
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "+Counter"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source,"{4}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_PERMANENT_EXCEPT(source),
+                MagicIndestructibleTargetPicker.create(),
+                this,
+                "PN puts a divinity counter on target permanent other than SN.\$"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new ChangeCountersAction(it, MagicCounterType.Divinity, 1));
+            })
+        }
+    },
+    
+    new MagicStatic(MagicLayer.Ability, PERMANENT) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.Indestructible, flags);
+        }
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return target.getCounters(MagicCounterType.Divinity) > 0;
+        }
+    },
+]
