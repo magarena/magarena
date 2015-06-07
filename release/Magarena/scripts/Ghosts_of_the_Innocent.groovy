f828cb10b24c5e880ab434f3692671593a5803b7
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-07 18:29:04 +0100

    add Ghosts of the Innocent

---
+++ release/Magarena/scripts/Ghosts_of_the_Innocent.groovy
@@ -0,0 +1,9 @@
+[
+    new MagicIfDamageWouldBeDealtTrigger(MagicTrigger.REPLACE_DAMAGE) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            damage.setAmount((int)Math.floor(damage.getAmount()/2));
+            return MagicEvent.NONE;
+        }
+    }
+]
