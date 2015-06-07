0c5f2a5fe660e3bbc4d65d47595b582e2ad030f7
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-07 22:26:38 +0100

    add Surge of Zeal

---
+++ release/Magarena/scripts/Surge_of_Zeal.groovy
@@ -0,0 +1,41 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                POS_TARGET_CREATURE,
+                MagicHasteTargetPicker.create(),
+                this,
+                "Target creature\$ and each other creature that shares a color with it gain haste until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicPermanent target ->
+                
+                final Set<MagicColor> targetColors = new HashSet<MagicColor>();
+                for (final MagicColor color : MagicColor.values()) {
+                    if(target.hasColor(color)) {
+                        targetColors.add(color);
+                    }
+                }
+
+                final Set<MagicPermanent> targetList = new HashSet<MagicPermanent>();
+                for (final MagicPermanent creature : CREATURE.filter(event)) {
+                    for (final MagicColor color : targetColors) {
+                        if (creature.hasColor(color)) {
+                            targetList.add(creature);
+                            break;
+                        }
+                    }
+                }
+                
+                for (final MagicPermanent creature : targetList) {
+                    game.doAction(new GainAbilityAction(creature, MagicAbility.Haste));
+                }
+            });
+        }
+    }
+]
