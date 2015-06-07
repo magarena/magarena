6f8a23089a8fae9c04d74f70c0cd913cb72abbd9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-07 14:34:42 +0100

    add Yukora, the Prisoner

---
+++ release/Magarena/scripts/Yukora__the_Prisoner.groovy
@@ -0,0 +1,24 @@
+def NON_OGRE_CREATURE_YOU_CONTROL = new MagicPermanentFilterImpl() {
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicPermanent target) {
+        return target.isController(player) && target.isCreature() && target.hasSubType(MagicSubType.Ogre) == false;
+    }
+};
+
+[
+    new MagicWhenSelfLeavesPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final RemoveFromPlayAction act) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN sacrifices all non-Ogre creatures he or she controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            NON_OGRE_CREATURE_YOU_CONTROL.filter(event.getPlayer()) each {
+                game.doAction(new SacrificeAction(it));
+            }
+        }
+    }
+]
