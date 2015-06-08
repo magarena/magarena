b32902776b5aee38a4c9a48fc72954d146b5956c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-08 03:12:00 +0100

    Bloodbond March and TextBloodbond for #347

---
+++ release/Magarena/scripts/Bloodbond_March.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenOtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicCardOnStack cardOnStack) {  
+            return cardOnStack.hasType(MagicType.Creature) ?
+                new MagicEvent(
+                    permanent,
+                    cardOnStack,
+                    this,
+                    "Each player returns all cards with the same name as (${cardOnStack.getName()}) from his or her graveyard to the battlefield."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final String name = event.getRefCard().getName();
+            for (final MagicPlayer player : game.getAPNAP()) {
+                final List<MagicCard> graveyard = cardName(name).from(MagicTargetType.Graveyard).filter(player);
+                for (final MagicCard card : graveyard) {
+                    game.doAction(new ReanimateAction(card, player));
+                }
+            }
+        }
+    }
+]
