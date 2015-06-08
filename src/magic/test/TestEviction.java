1de3fc671bf9ad15a5cdb2c5f13e477c7c3b8c54
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-08 03:30:46 +0100

    add TestEviction

---
+++ src/magic/test/TestEviction.java
@@ -0,0 +1,38 @@
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicUpkeepPhase;
+
+class TestEviction extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS,8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        game.setTurnPlayer(game.getPlayer(1));
+        game.setTurn(24);
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 22);
+        createPermanent(P, "Steel Wall", 4);
+        createPermanent(P, "Island", 4);
+        createPermanent(P, "Cerulean Sphinx");
+        addToHand(P, "Mark of Eviction", 4);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 22);
+        createPermanent(P,"Steel Wall", 4);
+        createPermanent(P, "Island", 4);
+        addToHand(P, "Mark of Eviction", 4);
+
+        return game;
+    }
+}
