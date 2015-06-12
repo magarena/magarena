8b5793e0c4c3a2380b78b7d4a0340f013ba00f93
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-12 02:34:07 +0100

    add TestTreetopRancor re. issue #50

---
+++ src/magic/test/TestTreetopRancor.java
@@ -0,0 +1,39 @@
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicUpkeepPhase;
+
+class TestTreetopRancor extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS,8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        game.setTurnPlayer(game.getPlayer(0));
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Forest", 22);
+        createPermanent(P,"Treetop Village",false,1);
+        createPermanent(P, "Forest", 5);
+        addToHand(P, "Rancor", 1);
+
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Breaching Leviathan", 1);
+        addToLibrary(P, "Island", 5);
+        addToHand(P, "Savaen Elves",4);
+        addToHand(P, "Island", 3);
+        createPermanent(P,"Forest");
+        createPermanent(P, "Steel Wall");
+
+        return game;
+    }
+}
