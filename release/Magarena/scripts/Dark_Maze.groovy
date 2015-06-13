39480113d06a47a0d0a31506fc0721cb96c62955
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-13 19:55:35 +0100

    add Dark Maze

---
+++ release/Magarena/scripts/Dark_Maze.groovy
@@ -0,0 +1,44 @@
+def COULD_ATTACK = new MagicCondition() {
+        public boolean accept(final MagicSource source) {
+            final MagicPermanent permanent = (MagicPermanent)source;
+            return permanent.hasState(MagicPermanentState.Summoned) == false &&
+                   permanent.hasAbility(MagicAbility.Haste) == false &&
+                   permanent.isTapped() == false;
+        }
+    };
+
+[
+    new MagicPermanentActivation(
+        [new MagicArtificialCondition(COULD_ATTACK)],
+        new MagicActivationHints(MagicTiming.FirstMain),
+        "Attack"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{0}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN can attack this turn as though it didn't have defender. "+
+                "Exile it at the beginning of the next end step."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPermanent permanent = event.getPermanent();
+            game.doAction(new GainAbilityAction(permanent, MagicAbility.CanAttackWithDefender));
+            game.doAction(new AddTurnTriggerAction(
+                permanent,
+                MagicAtEndOfTurnTrigger.ExileAtEnd
+            ));
+        }
+    }
+]
