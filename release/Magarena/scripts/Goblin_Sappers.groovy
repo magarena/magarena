f1274b3ee7698aa37cbf3377c7bc9448082e6ff3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-13 18:01:33 +0100

    add Goblin Sappers

---
+++ release/Magarena/scripts/Goblin_Sappers.groovy
@@ -0,0 +1,77 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Attack),
+        "{R}{R}"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{R}{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_CREATURE_YOU_CONTROL,
+                MagicUnblockableTargetPicker.create(),
+                this,
+                "Target creature you control\$ can't be blocked this turn. Destroy it and SN at end of combat."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new GainAbilityAction(it, MagicAbility.Unblockable));
+                game.doAction(new AddTurnTriggerAction(
+                    it,
+                    MagicAtEndOfCombatTrigger.Destroy
+                ));
+                game.doAction(new AddTurnTriggerAction(
+                    event.getPermanent(),
+                    MagicAtEndOfCombatTrigger.Destroy
+                ))
+            });
+        }
+    },
+    
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Attack),
+        "{R}{R}{R}{R}"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{R}{R}{R}{R}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_CREATURE_YOU_CONTROL,
+                MagicUnblockableTargetPicker.create(),
+                this,
+                "Target creature you control\$ can't be blocked this turn. Destroy it at end of combat."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new GainAbilityAction(it, MagicAbility.Unblockable));
+                game.doAction(new AddTurnTriggerAction(
+                    it,
+                    MagicAtEndOfCombatTrigger.Destroy
+                ));
+            });
+        }
+    }
+]
