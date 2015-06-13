3b02777509adbd5204379154105ad5224cf54ab0
PalladiaMors <unknown>
2015-06-13 17:56:46 +0200

    Add script for Mana Short

---
+++ release/Magarena/scripts/Mana_Short.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+
+                NEG_TARGET_PLAYER,
+                this,
+                "Tap all lands target player\$ controls."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                LAND_YOU_CONTROL.filter(it) each {
+                    final MagicPermanent land ->
+                    game.doAction(new TapAction(land));
+                }
+            });
+        }
+    }
+]
