74a8570ff224361d15c92f5977fc2157b3843a2d
PalladiaMors <unknown>
2015-06-13 17:26:43 +0200

    Add script for Volrath the Fallen

---
+++ release/Magarena/scripts/Volrath_the_Fallen.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{1}{B}"),
+                new MagicDiscardChosenEvent(source, A_CREATURE_CARD_FROM_HAND)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                payedCost.getTarget(),
+                this,
+                "SN gets +X/+X until end of turn, where X is the discarded card's converted mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getRefCard().getConvertedCost();
+            game.logAppendValue(event.getPlayer(),amount);
+            game.doAction(new ChangeTurnPTAction(event.getPermanent(), +amount, +amount));
+        }
+    }
+]
