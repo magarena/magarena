7a48c0b54c63195f5898b0d364c3b7f314a611b2
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-14 16:10:53 +0100

    add Vampire Lacerator

---
+++ release/Magarena/scripts/Vampire_Lacerator.groovy
@@ -0,0 +1,18 @@
+[
+    new MagicAtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            return upkeepPlayer.getOpponent().getLife() > 10 ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN loses 1 life."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeLifeAction(event.getPlayer(), -1));
+        }
+    }
+]
