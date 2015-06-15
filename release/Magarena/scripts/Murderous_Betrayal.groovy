be31b669ac1ef4930a4cc9c38be3929b18764c8c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-16 00:19:08 +0100

    add Murderous Betrayal

---
+++ release/Magarena/scripts/Murderous_Betrayal.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Destroy"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{B}{B}"),
+                new MagicPayLifeEvent(source,(source.getController().getLife().abs() + 1).intdiv(2))
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_NONBLACK_CREATURE,
+                MagicDestroyTargetPicker.DestroyNoRegen,
+                this,
+                "Destroy target nonblack creature.\$ It can't be regenerated."
+            );
+        }
+        
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(ChangeStateAction.Set(it, MagicPermanentState.CannotBeRegenerated));
+                game.doAction(new DestroyAction(it));
+            });
+        }
+    }
+]
