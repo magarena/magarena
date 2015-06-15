8f13eac40d12cb46853b379d5e04b4d88dd2ba12
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-15 19:05:17 +0100

    add Early Harvest

---
+++ release/Magarena/scripts/Early_Harvest.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_PLAYER,
+                this,
+                "Target player\$ untaps all basic lands he or she controls."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                for (final MagicPermanent permanent : BASIC_LAND_YOU_CONTROL.filter(it)) {
+                    game.doAction(new UntapAction(permanent));
+                }
+            });
+        }
+    }
+]
