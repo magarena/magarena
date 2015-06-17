f2974c7bc5e5a37daac30ed44f11a88389e3a250
melvinzhang <melvinzhang@gmail.com>
2015-06-17 14:46:57 +0800

    comment out entire MagicAbility.Suspend, it confuses workable card parsing

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -1288,13 +1288,15 @@ public enum MagicAbility {
             card.add(MagicStatic.genABGameStatic(filter, abList));
         }
     },
+    /*
     Suspend("suspend " + ARG.NUMBER + "( |—)" + ARG.MANACOST, 0) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
             final int n = ARG.number(arg);
             final MagicManaCost manaCost = MagicManaCost.create(ARG.manacost(arg));
-            //card.add(new MagicSuspendActivation(n, manaCost));
+            card.add(new MagicSuspendActivation(n, manaCost));
         }
     },
+    */
     ;
 
     public static final Set<MagicAbility> PROTECTION_FLAGS = EnumSet.range(ProtectionFromBlack, ProtectionFromEverything);
