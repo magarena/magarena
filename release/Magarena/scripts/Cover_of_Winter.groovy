5e3b0c314d1f8a409df0801769e9e36128fbdc18
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-06-17 16:47:43 +0100

    add Cover of Winter

---
+++ release/Magarena/scripts/Cover_of_Winter.groovy
@@ -0,0 +1,15 @@
+[
+    new MagicPreventDamageTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            final int amount = permanent.getCounters(MagicCounterType.Age);
+            final int preventAmount = Math.min(amount, damage.getAmount());
+            if (damage.getSource().isCreature() && damage.isCombat() && damage.getTarget().isFriend(permanent)) {
+                // Replacement effect. Generates no event or action.
+                damage.prevent(amount);
+                game.logAppendMessage(permanent.getController(), "${permanent} prevents (${preventAmount}) damage dealt to (${damage.getTarget()}).");
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
