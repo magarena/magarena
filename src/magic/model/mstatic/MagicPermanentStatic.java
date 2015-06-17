48ed608e1901aa45274a33a3a53ec4d2c7fdd0de
melvinzhang <melvinzhang@gmail.com>
2015-06-17 12:10:15 +0800

    fix crash by passing in actual player instead of using source which is MagicPermanent.NONE

--- src/magic/model/mstatic/MagicPermanentStatic.java
+++ src/magic/model/mstatic/MagicPermanentStatic.java
@@ -75,7 +75,7 @@ public class MagicPermanentStatic implements Comparable<MagicPermanentStatic> {
         new MagicStatic(MagicLayer.Game) {
             @Override
             public void modGame(final MagicPermanent source, final MagicGame game) {
-                for (final MagicPermanent permanent : MagicTargetFilterFactory.SNOW_MANA_PERMANENT.filter(source)) {
+                for (final MagicPermanent permanent : MagicTargetFilterFactory.SNOW_MANA_PERMANENT.filter(game.getTurnPlayer())) {
                     permanent.addAbility(MagicTapManaActivation.Snow);
                 }
             }
