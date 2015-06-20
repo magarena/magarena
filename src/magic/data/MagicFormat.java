258b95e675b778a3c124ed46911e7956e8f6b0d0
melvinzhang <melvinzhang@gmail.com>
2015-06-20 10:12:57 +0800

    add MagicFormat source

---
+++ src/magic/data/MagicFormat.java
@@ -0,0 +1,39 @@
+package magic.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import magic.model.MagicCardDefinition;
+import magic.model.MagicDeck;
+import magic.utility.DeckUtils;
+import magic.utility.MagicResources;
+
+public abstract class MagicFormat {
+    
+    public abstract String getName();
+    
+    public abstract CardLegality getCardLegality(MagicCardDefinition card, int cardCount);
+    
+    public abstract int getMinimumDeckSize();
+   
+    public boolean isCardLegal(MagicCardDefinition card, int cardCount) {
+        return getCardLegality(card, cardCount) == CardLegality.Legal;
+    }
+    
+    public boolean isCardLegal(MagicCardDefinition card) {
+        return isCardLegal(card, 1);
+    }
+    
+    public boolean isDeckLegal(final MagicDeck aDeck) {
+        if (aDeck.size() < getMinimumDeckSize()) {
+            return false;
+        }
+        for (final MagicCardDefinition card : DeckUtils.getDistinctCards(aDeck)) {
+            final int cardCountCheck = card.isLand() ? 1 : aDeck.getCardCount(card);
+            if (isCardLegal(card, cardCountCheck) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
