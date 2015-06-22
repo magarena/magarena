6627eba222d2ad0d0fc25ef4ef25cf3fa3b7c04f
lodici <lodici123@gmail.com>
2015-06-22 15:28:13 +0100

    fix raw type warning.

--- src/magic/ui/dialog/DuelPropertiesDialog.java
+++ src/magic/ui/dialog/DuelPropertiesDialog.java
@@ -56,7 +56,7 @@ public class DuelPropertiesDialog extends JDialog {
         cubeComboBox.setSelectedItem(cube);
         cubeComboBox.setRenderer(new DefaultListCellRenderer() {
             @Override
-            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                 MagicFormat fmt = (MagicFormat)value;
                 value = fmt.getLabel();
                 return super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
