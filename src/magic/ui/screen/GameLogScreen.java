5b48046210e606cd94729ba6e62583267d9be631
lodici <lodici123@gmail.com>
2015-07-03 17:46:35 +0100

    make GameLogScreen strings translatable.

--- src/magic/ui/screen/GameLogScreen.java
+++ src/magic/ui/screen/GameLogScreen.java
@@ -11,6 +11,7 @@ import magic.data.MagicIcon;
 import magic.ui.utility.DesktopUtils;
 import magic.ui.IconImages;
 import magic.ui.ScreenController;
+import magic.ui.UiString;
 import magic.ui.screen.interfaces.IStatusBar;
 import magic.ui.screen.widget.ActionBarButton;
 import magic.ui.screen.widget.MenuButton;
@@ -19,6 +20,15 @@ import magic.utility.MagicFileSystem.DataPath;
 
 @SuppressWarnings("serial")
 public class GameLogScreen extends TextFileReaderScreen implements IStatusBar {
+
+    // translatable strings
+    private static final String _S1 = "Logs directory";
+    private static final String _S2 = "Opens the logs directory containing 'game.log' in the default file explorer.";
+    private static final String _S3 = "Could not open 'logs' directory : %s";
+    private static final String _S4 = "Basic View";
+    private static final String _S5 = "Filters log file to remove AI diagnostics.";
+    private static final String _S6 = "Detailed View";
+    private static final String _S7 = "Full log file, including AI diagnostics.";
 
     private static final Path LOGS_DIRECTORY = MagicFileSystem.getDataPath(DataPath.LOGS);
     private static final Path TEXT_FILE = LOGS_DIRECTORY.resolve("game.log");
@@ -56,20 +66,20 @@ public class GameLogScreen extends TextFileReaderScreen implements IStatusBar {
             buttons.add(
                 new ActionBarButton(
                         IconImages.getIcon(MagicIcon.OPEN_ICON),
-                        "Logs directory", "Opens the logs directory containing 'game.log' in the default file explorer.",
+                        UiString.get(_S1), UiString.get(_S2),
                         new AbstractAction() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
                                 try {
                                     DesktopUtils.openMagicDirectory(DataPath.LOGS);
                                 } catch (IOException ex) {
-                                    ScreenController.showWarningMessage("Could not open 'logs' directory : " + ex.getMessage());
+                                    ScreenController.showWarningMessage(UiString.get(_S3, ex.getMessage()));
                                 }
                             }
                         }));
             buttons.add(
                 new ActionBarButton(
-                        "Basic View", "Filters log file to remove AI diagnostics.",
+                        UiString.get(_S4), UiString.get(_S5),
                         new AbstractAction() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
@@ -80,7 +90,7 @@ public class GameLogScreen extends TextFileReaderScreen implements IStatusBar {
         } else {
             buttons.add(
                 new ActionBarButton(
-                        "Detailed View", "Full log file, including AI diagnostics.",
+                        UiString.get(_S6), UiString.get(_S7),
                         new AbstractAction() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
@@ -88,7 +98,7 @@ public class GameLogScreen extends TextFileReaderScreen implements IStatusBar {
                                 reloadTextFile();
                             }
                         }));
-        };
+        }
         return buttons;
     }
 
