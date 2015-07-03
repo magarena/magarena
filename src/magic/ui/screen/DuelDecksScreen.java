bb986059cec87a9dbd562cb1f7bf4e91d9f7a7c3
lodici <lodici123@gmail.com>
2015-07-03 17:25:29 +0100

    make DuelDecksScreen strings translatable; refactor imports and comments.

--- src/magic/ui/screen/DuelDecksScreen.java
+++ src/magic/ui/screen/DuelDecksScreen.java
@@ -1,39 +1,54 @@
 package magic.ui.screen;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.JPanel;
 import magic.data.DuelConfig;
-import magic.ui.IconImages;
+import magic.data.MagicIcon;
+import magic.exception.InvalidDeckException;
+import magic.model.DuelPlayerConfig;
 import magic.model.MagicDeck;
 import magic.model.MagicDeckConstructionRule;
 import magic.model.MagicDeckProfile;
 import magic.model.MagicDuel;
-import magic.model.DuelPlayerConfig;
 import magic.ui.DuelDecksPanel;
+import magic.ui.IconImages;
 import magic.ui.MagicFrame;
+import magic.ui.ScreenController;
 import magic.ui.ScreenOptionsOverlay;
+import magic.ui.UiString;
 import magic.ui.screen.interfaces.IActionBar;
 import magic.ui.screen.interfaces.IOptionsMenu;
 import magic.ui.screen.interfaces.IStatusBar;
+import magic.ui.screen.interfaces.IWikiPage;
 import magic.ui.screen.widget.ActionBarButton;
 import magic.ui.screen.widget.DuelSettingsPanel;
 import magic.ui.screen.widget.MenuButton;
 import magic.ui.screen.widget.MenuPanel;
 import magic.ui.screen.widget.SampleHandActionButton;
 
-import javax.swing.AbstractAction;
-import javax.swing.JPanel;
-
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.List;
-import magic.data.MagicIcon;
-import magic.exception.InvalidDeckException;
-import magic.ui.ScreenController;
-import magic.ui.screen.interfaces.IWikiPage;
-
 @SuppressWarnings("serial")
 public class DuelDecksScreen
     extends AbstractScreen
     implements IStatusBar, IActionBar, IOptionsMenu, IWikiPage {
+
+    // translatable strings
+    private static final String _S1 = "Duel Decks";
+    private static final String _S2 = "Main Menu";
+    private static final String _S3 = "Restart duel";
+    private static final String _S4 = "Game %d";
+    private static final String _S5 = "Deck Editor";
+    private static final String _S6 = "Open the Deck Editor.";
+    private static final String _S7 = "Swap Decks";
+    private static final String _S8 = "Swap your deck with your opponent's.";
+    private static final String _S9 = "%s wins the duel";
+    private static final String _S10 = "Restart Duel";
+    private static final String _S11 = "Same players, same decks, same duel settings. Same result...?";
+    private static final String _S12 = "Deck View";
+    private static final String _S13 = "Shows complete deck using tiled card images.";
+    private static final String _S14 = "%s's deck is illegal.\n\n%s";
 
     private final DuelDecksPanel screenContent;
 
@@ -45,23 +60,17 @@ public class DuelDecksScreen
         }
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.IMagStatusBar#getScreenCaption()
-     */
     @Override
     public String getScreenCaption() {
-        return "Duel Decks";
+        return UiString.get(_S1);
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.IMagActionBar#getLeftAction()
-     */
     @Override
     public MenuButton getLeftAction() {
         if (screenContent.getDuel().getGamesPlayed() == 0) {
-            return MenuButton.getCloseScreenButton("Main menu");
+            return MenuButton.getCloseScreenButton(UiString.get(_S2));
         } else {
-            return new MenuButton("Main Menu", new AbstractAction() {
+            return new MenuButton(UiString.get(_S2), new AbstractAction() {
                 @Override
                 public void actionPerformed(final ActionEvent e) {
                     ScreenController.showMainMenuScreen();
@@ -70,17 +79,11 @@ public class DuelDecksScreen
         }
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.widget.IMagScreenOptionsProvider#showScreenOptionsOverlay()
-     */
     @Override
     public void showOptionsMenuOverlay() {
         new ScreenOptions(getFrame());
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.IMagActionBar#getRightAction()
-     */
     @Override
     public MenuButton getRightAction() {
         if (!screenContent.getDuel().isFinished()) {
@@ -97,7 +100,7 @@ public class DuelDecksScreen
                 }
             });
         } else {
-            return new MenuButton("Restart duel", new AbstractAction() {
+            return new MenuButton(UiString.get(_S3), new AbstractAction() {
                 @Override
                 public void actionPerformed(final ActionEvent e) {
                     try {
@@ -111,20 +114,17 @@ public class DuelDecksScreen
     }
 
     private String getStartDuelCaption() {
-        return "Game " + (screenContent.getDuel().getGamesPlayed() + 1);
+        return UiString.get(_S4, screenContent.getDuel().getGamesPlayed() + 1);
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.IMagActionBar#getMiddleActions()
-     */
     @Override
     public List<MenuButton> getMiddleActions() {
-        final List<MenuButton> buttons = new ArrayList<MenuButton>();
+        final List<MenuButton> buttons = new ArrayList<>();
         if (screenContent.getDuel().getGamesPlayed() == 0) {
             buttons.add(
                     new ActionBarButton(
                             IconImages.getIcon(MagicIcon.DECK_ICON),
-                            "Deck Editor", "Open the Deck Editor.",
+                            UiString.get(_S5), UiString.get(_S6),
                             new AbstractAction() {
                                 @Override
                                 public void actionPerformed(final ActionEvent e) {
@@ -135,7 +135,7 @@ public class DuelDecksScreen
             buttons.add(
                     new ActionBarButton(
                             IconImages.getIcon(MagicIcon.SWAP_ICON),
-                            "Swap Decks", "Swap your deck with your opponent's.",
+                            UiString.get(_S7), UiString.get(_S8),
                             new AbstractAction() {
                                 @Override
                                 public void actionPerformed(final ActionEvent e) {
@@ -151,12 +151,12 @@ public class DuelDecksScreen
         } else {
             if (screenContent.getDuel().isFinished()) {
                 final MagicDuel duel = screenContent.getDuel();
-                buttons.add(new MenuButton(duel.getWinningPlayerProfile().getPlayerName() + " wins the duel", null));
+                buttons.add(new MenuButton(UiString.get(_S9, duel.getWinningPlayerProfile().getPlayerName()), null));
             } else {
                 buttons.add(
                         new ActionBarButton(
                                 IconImages.getIcon(MagicIcon.REFRESH_ICON),
-                                "Restart Duel", "Same players, same decks, same duel settings. Same result...?",
+                                UiString.get(_S10), UiString.get(_S11),
                                 new AbstractAction() {
                                     @Override
                                     public void actionPerformed(final ActionEvent e) {
@@ -175,7 +175,7 @@ public class DuelDecksScreen
             buttons.add(
                     new ActionBarButton(
                             IconImages.getIcon(MagicIcon.TILED_ICON),
-                            "Deck View", "Shows complete deck using tiled card images.",
+                            UiString.get(_S12), UiString.get(_S13),
                             new AbstractAction() {
                                 @Override
                                 public void actionPerformed(final ActionEvent e) {
@@ -192,9 +192,6 @@ public class DuelDecksScreen
         screenContent.updateDecksAfterEdit();
     }
 
-    /**
-     *
-     */
     public void swapDecks() {
         screenContent.getDuel().restart();
         final DuelPlayerConfig[] players = screenContent.getDuel().getPlayers();
@@ -210,9 +207,6 @@ public class DuelDecksScreen
         getFrame().showDuel();
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.MagScreen#isScreenReadyToClose(magic.ui.MagScreen)
-     */
     @Override
     public boolean isScreenReadyToClose(final AbstractScreen nextScreen) {
         return true;
@@ -240,9 +234,6 @@ public class DuelDecksScreen
             super(frame);
         }
 
-        /* (non-Javadoc)
-         * @see magic.ui.ScreenOptionsOverlay#getScreenMenu()
-         */
         @Override
         protected MenuPanel getScreenMenu() {
             return null;
@@ -255,16 +246,13 @@ public class DuelDecksScreen
                 MagicDeckConstructionRule.getRulesText(MagicDeckConstructionRule.checkDeck(deck));
 
         if (brokenRulesText.length() > 0) {
-            ScreenController.showWarningMessage(playerName + "'s deck is illegal.\n\n" + brokenRulesText);
+            ScreenController.showWarningMessage(UiString.get(_S14, playerName, brokenRulesText));
             return false;
         }
 
         return true;
     }
 
-    /* (non-Javadoc)
-     * @see magic.ui.interfaces.IStatusBar#getStatusPanel()
-     */
     @Override
     public JPanel getStatusPanel() {
         final DuelConfig config =  screenContent.getDuel().getConfiguration();
