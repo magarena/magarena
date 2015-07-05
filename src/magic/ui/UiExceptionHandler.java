4c773851c35f3ca24acb46daf3f4b9dda7cbd85c
lodici <lodici123@gmail.com>
2015-07-05 04:29:05 +0100

    make UiExceptionHandler strings translatable.

--- src/magic/ui/UiExceptionHandler.java
+++ src/magic/ui/UiExceptionHandler.java
@@ -13,6 +13,12 @@ import magic.utility.MagicFileSystem;
 
 public class UiExceptionHandler extends FileExceptionHandler {
 
+    // translatable strings.
+    private static final String _S1 = "Fatal Error";
+    private static final String _S2 = "An unexpected error has occurred and Magarena will need to close.";
+    private static final String _S3 = "Please consider submitting a crash report so that the development team can investigate.";
+    private static final String _S4 = "Would you like to open the crash logs directory in file explorer?";
+
     @Override
     public void reportException(final ExceptionReport report) {
         super.reportException(report);
@@ -31,22 +37,20 @@ public class UiExceptionHandler extends FileExceptionHandler {
 
             // By specifying a frame the JOptionPane will be shown in the taskbar.
             // Otherwise if the dialog is hidden it is easy to forget it is still open.
-            final JFrame frame = new JFrame("Fatal Error");
+            final JFrame frame = new JFrame(UiString.get(_S1));
             frame.setUndecorated(true);
             frame.setVisible(true);
             frame.setLocationRelativeTo(null);
 
-            String prompt = "An unexpected error has occurred and Magarena will need to close.";
+            String prompt = UiString.get(_S2);
             if (Desktop.isDesktopSupported()) {
-                prompt +=
-                    "\n\nPlease consider submitting a crash report so that the development team can investigate.\n" +
-                    "Would you like to open the crash logs directory in file explorer?";
-                final int action = JOptionPane.showConfirmDialog(frame, prompt, "Fatal Error", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null);
+                prompt += String.format("\n\n%s\n%s", UiString.get(_S3), UiString.get(_S4));
+                final int action = JOptionPane.showConfirmDialog(frame, prompt, UiString.get(_S1), JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null);
                 if (action == JOptionPane.YES_OPTION) {
                     Desktop.getDesktop().open(MagicFileSystem.getDataPath(MagicFileSystem.DataPath.LOGS).toFile());
                 }
             } else {
-                JOptionPane.showMessageDialog(frame, prompt, "Fatal Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(frame, prompt, UiString.get(_S1), JOptionPane.ERROR_MESSAGE);
             }
 
         } catch (Exception e) {
