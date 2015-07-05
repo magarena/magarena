fb9a3b78fd7ef6e02260f1a1c238f96e753d1fa7
lodici <lodici123@gmail.com>
2015-07-05 02:09:01 +0100

    make PlayerMiniStatsPanel strings translatable; refactor imports.

--- src/magic/ui/player/PlayerMiniStatsPanel.java
+++ src/magic/ui/player/PlayerMiniStatsPanel.java
@@ -1,19 +1,29 @@
 package magic.ui.player;
 
-import magic.model.player.PlayerStatistics;
-import magic.ui.widget.FontsAndBorders;
-import net.miginfocom.swing.MigLayout;
-
+import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
-
-import java.awt.Color;
+import magic.model.player.PlayerStatistics;
+import magic.ui.StringContext;
+import magic.ui.UiString;
+import magic.ui.widget.FontsAndBorders;
+import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
 public class PlayerMiniStatsPanel extends JPanel {
+
+    // translatable strings
+    @StringContext(eg = "player mini-stats heading representing games and duels [P]layed")
+    private static final String _S1 = "P";
+    @StringContext(eg = "player mini-stats heading representing games and duels [W]on")
+    private static final String _S2 = "W";
+    @StringContext(eg = "player mini-stats heading representing games and duels [L]ost")
+    private static final String _S3 = "L";
+    private static final String _S4 = "Duels";
+    private static final String _S5 = "Games";
 
     private final static Border CELL_BORDER = BorderFactory.createDashedBorder(null);
 
@@ -44,14 +54,14 @@ public class PlayerMiniStatsPanel extends JPanel {
         migLayout.setColumnConstraints("[][30!]");
         // stats table
         add(new JLabel());
-        add(getStatsLabel("P"), "w 100%");
-        add(getStatsLabel("W"), "w 100%");
-        add(getStatsLabel("L"), "w 100%");
-        add(getStatsLabel("Duels"), "w 60!");
+        add(getStatsLabel(UiString.get(_S1)), "w 100%");
+        add(getStatsLabel(UiString.get(_S2)), "w 100%");
+        add(getStatsLabel(UiString.get(_S3)), "w 100%");
+        add(getStatsLabel(UiString.get(_S4)), "w 60!");
         add(getStatsLabel(stats.getDuelsPlayed().toString()), "w 100%");
         add(getStatsLabel(stats.getDuelsWon().toString()), "w 100%");
         add(getStatsLabel(stats.getDuelsLost().toString()), "w 100%");
-        add(getStatsLabel("Games"), "w 60!");
+        add(getStatsLabel(UiString.get(_S5)), "w 60!");
         add(getStatsLabel(stats.getGamesPlayed().toString()), "w 100%");
         add(getStatsLabel(stats.getGamesWon().toString()), "w 100%");
         add(getStatsLabel(stats.getGamesLost().toString()), "w 100%");
