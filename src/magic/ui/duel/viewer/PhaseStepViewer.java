25b256dd78550e52311cae7b9c72dedd14d8f099
lodici <lodici123@gmail.com>
2015-07-05 22:24:17 +0100

    make PhaseStepViewer strings translatable.

--- src/magic/ui/duel/viewer/PhaseStepViewer.java
+++ src/magic/ui/duel/viewer/PhaseStepViewer.java
@@ -6,11 +6,26 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import magic.model.phase.MagicPhaseType;
+import magic.ui.UiString;
 import magic.ui.utility.MagicStyle;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
 public class PhaseStepViewer extends JPanel {
+
+    // translatable strings
+    private static final String _S1 = "Beginning Phase : Untap Step";
+    private static final String _S2 = "Beginning Phase : Upkeep Step";
+    private static final String _S3 = "Beginning Phase : Draw Step";
+    private static final String _S4 = "First Main Phase";
+    private static final String _S5 = "Combat Phase : Beginning of Combat Step";
+    private static final String _S6 = "Combat Phase : Declare Attackers Step";
+    private static final String _S7 = "Combat Phase : Declare Blockers Step";
+    private static final String _S8 = "Combat Phase : Combat Damage Step";
+    private static final String _S9 = "Combat Phase : End of Combat Step";
+    private static final String _S10 = "Second Main Phase";
+    private static final String _S11 = "Ending Phase : End of Turn Step";
+    private static final String _S12 = "Ending Phase : Clean Up Step";
 
     private static final Color COLOR_ON = MagicStyle.getRolloverColor();
     private static final Color COLOR_OFF = Color.LIGHT_GRAY;
@@ -26,18 +41,18 @@ public class PhaseStepViewer extends JPanel {
 
     private void setMyLayout() {
         setLayout(new MigLayout("insets 1 5 0 5, gap 0"));
-        setPhaseStepIndicator("UN", "Beginning Phase : Untap Step");
-        setPhaseStepIndicator("UP", "Beginning Phase : Upkeep Step");
-        setPhaseStepIndicator("DR", "Beginning Phase : Draw Step");
-        setPhaseStepIndicator("M1", "First Main Phase");
-        setPhaseStepIndicator("BC", "Combat Phase : Beginning of Combat Step");
-        setPhaseStepIndicator("DA", "Combat Phase : Declare Attackers Step");
-        setPhaseStepIndicator("DB", "Combat Phase : Declare Blockers Step");
-        setPhaseStepIndicator("CD", "Combat Phase : Combat Damage Step");
-        setPhaseStepIndicator("EC", "Combat Phase : End of Combat Step");
-        setPhaseStepIndicator("M2", "Second Main Phase");
-        setPhaseStepIndicator("ET", "Ending Phase : End of Turn Step");
-        setPhaseStepIndicator("CU", "Ending Phase : Clean Up Step");
+        setPhaseStepIndicator("UN", UiString.get(_S1));
+        setPhaseStepIndicator("UP", UiString.get(_S2));
+        setPhaseStepIndicator("DR", UiString.get(_S3));
+        setPhaseStepIndicator("M1", UiString.get(_S4));
+        setPhaseStepIndicator("BC", UiString.get(_S5));
+        setPhaseStepIndicator("DA", UiString.get(_S6));
+        setPhaseStepIndicator("DB", UiString.get(_S7));
+        setPhaseStepIndicator("CD", UiString.get(_S8));
+        setPhaseStepIndicator("EC", UiString.get(_S9));
+        setPhaseStepIndicator("M2", UiString.get(_S10));
+        setPhaseStepIndicator("ET", UiString.get(_S11));
+        setPhaseStepIndicator("CU", UiString.get(_S12));
     }
 
     private void setPhaseStep(int index) {
