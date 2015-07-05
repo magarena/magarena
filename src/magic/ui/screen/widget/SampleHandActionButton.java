6b09065be9c5fc8b6e7cbabbcf5df55b9e488d7c
lodici <lodici123@gmail.com>
2015-07-05 06:16:12 +0100

    make SampleHandActionButton strings translatable; refactor imports.

--- src/magic/ui/screen/widget/SampleHandActionButton.java
+++ src/magic/ui/screen/widget/SampleHandActionButton.java
@@ -1,23 +1,28 @@
 package magic.ui.screen.widget;
 
-import magic.ui.IconImages;
-import magic.model.MagicDeck;
-import magic.ui.MagicFrame;
-
-import javax.swing.AbstractAction;
-
 import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
 import magic.data.MagicIcon;
+import magic.model.MagicDeck;
+import magic.ui.IconImages;
+import magic.ui.MagicFrame;
 import magic.ui.ScreenController;
+import magic.ui.UiString;
 
 @SuppressWarnings("serial")
 public final class SampleHandActionButton extends ActionBarButton {
+
+    // translatable strings
+    private static final String _S1 = "Sample Hand";
+    private static final String _S2 = "Generate sample Hands from this deck.";
+    private static final String _S3 = "A deck with a minimum of 7 cards is required first.";
+
     private SampleHandActionButton() {}
 
     public static ActionBarButton createInstance(final MagicDeck deck, final MagicFrame frame) {
         return new ActionBarButton(
                 IconImages.getIcon(MagicIcon.HAND_ICON),
-                "Sample Hand", "Generate sample Hands from this deck.",
+                UiString.get(_S1), UiString.get(_S2),
                 new SampleHandAction(deck, frame));
     }
 
@@ -36,7 +41,7 @@ public final class SampleHandActionButton extends ActionBarButton {
             if (deck.size() >= 7) {
                 ScreenController.showSampleHandScreen(deck);
             } else {
-                showInvalidActionMessage("A deck with a minimum of 7 cards is required first.");
+                showInvalidActionMessage(UiString.get(_S3));
             }
         }
 
