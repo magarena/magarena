45c4d3e9179eb5673db6d1d40e3629d1e5d0aee8
lodici <lodici123@gmail.com>
2015-07-19 03:33:55 +0100

    Set CancelButton preferred size.

--- src/magic/ui/widget/CancelButton.java
+++ src/magic/ui/widget/CancelButton.java
@@ -1,5 +1,6 @@
 package magic.ui.widget;
 
+import java.awt.Dimension;
 import javax.swing.JButton;
 import magic.data.MagicIcon;
 import magic.ui.IconImages;
@@ -11,9 +12,12 @@ public class CancelButton extends JButton {
     // translatable strings
     private static final String _S1 = "Cancel";
 
+    private static final Dimension PREFERRED_SIZE = new Dimension(100, 30);
+
     public CancelButton() {
         super(UiString.get(_S1));
         setIcon(IconImages.getIcon(MagicIcon.BANNED_ICON));
+        setPreferredSize(PREFERRED_SIZE);
     }
 
 }
