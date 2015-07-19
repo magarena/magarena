a316a410a7f6504a4e1d25c4317fcd6386434c8c
lodici <lodici123@gmail.com>
2015-07-19 21:39:33 +0100

    extract common code into new MagicDialogButton UI class.

--- src/magic/ui/widget/CancelButton.java
+++ src/magic/ui/widget/CancelButton.java
@@ -1,24 +1,18 @@
 package magic.ui.widget;
 
-import java.awt.Dimension;
-import javax.swing.JButton;
 import magic.data.MagicIcon;
 import magic.ui.IconImages;
 import magic.ui.UiString;
 
 @SuppressWarnings("serial")
-public class CancelButton extends JButton {
+public class CancelButton extends MagicDialogButton {
 
     // translatable strings
     private static final String _S1 = "Cancel";
 
-    private static final Dimension PREFERRED_SIZE = new Dimension(100, 30);
-
     public CancelButton() {
         super(UiString.get(_S1));
         setIcon(IconImages.getIcon(MagicIcon.BANNED_ICON));
-        setPreferredSize(PREFERRED_SIZE);
-        setMinimumSize(PREFERRED_SIZE);
     }
 
 }
