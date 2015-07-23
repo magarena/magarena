b80a224f828b6463c50fdebede500afb02e07f6e
lodici <lodici123@gmail.com>
2015-07-23 15:35:49 +0100

    move CardLegalityStrings into CardLegality class file.

--- src/magic/data/CardLegality.java
+++ src/magic/data/CardLegality.java
@@ -19,3 +19,15 @@ public enum CardLegality {
         return description;
     }
 }
+
+/**
+ * translatable strings
+ */
+final class CardLegalityStrings {
+    private CardLegalityStrings() {}
+    static final String _S1 = "is legal";
+    static final String _S2 = "is illegal";
+    static final String _S3 = "is banned";
+    static final String _S4 = "is restricted";
+    static final String _S5 = "has too many copies";
+}
