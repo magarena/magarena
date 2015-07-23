0dbbb0c55ab69bd364ad73bcf9c72401f5028732
lodici <lodici123@gmail.com>
2015-07-23 15:31:12 +0100

    make MagicPlayerZone strings translatable.

--- src/magic/model/MagicPlayerZone.java
+++ src/magic/model/MagicPlayerZone.java
@@ -1,19 +1,20 @@
 package magic.model;
 
 import magic.data.MagicIcon;
+import magic.ui.UiString;
 
 public enum MagicPlayerZone {
     
-    HAND("Hand", MagicIcon.HAND_ZONE),
-    LIBRARY("Library", MagicIcon.LIBRARY_ZONE),
-    GRAVEYARD("Graveyard", MagicIcon.GRAVEYARD_ZONE),
-    EXILE("Exile", MagicIcon.EXILE_ZONE);
+    HAND(MagicPlayerZoneStrings._S1, MagicIcon.HAND_ZONE),
+    LIBRARY(MagicPlayerZoneStrings._S2, MagicIcon.LIBRARY_ZONE),
+    GRAVEYARD(MagicPlayerZoneStrings._S3, MagicIcon.GRAVEYARD_ZONE),
+    EXILE(MagicPlayerZoneStrings._S4, MagicIcon.EXILE_ZONE);
     
     private final String zoneName;
     private final MagicIcon zoneIcon;
 
     private MagicPlayerZone(final String name, final MagicIcon icon) {
-        this.zoneName = name;
+        this.zoneName = UiString.get(name);
         this.zoneIcon = icon;
     }
 
@@ -25,4 +26,15 @@ public enum MagicPlayerZone {
         return this.zoneIcon;
     }
 
+}
+
+/**
+ * translatable strings
+ */
+final class MagicPlayerZoneStrings {
+    private MagicPlayerZoneStrings() {}
+    static final String _S1 = "Hand";
+    static final String _S2 = "Library";
+    static final String _S3 = "Graveyard";
+    static final String _S4 = "Exile";
 }
