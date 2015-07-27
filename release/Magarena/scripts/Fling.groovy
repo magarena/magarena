b16f09392191b2c3ed9ebd25d933f4e04d6f4b51
melvinzhang <melvinzhang@gmail.com>
2015-07-27 17:25:51 +0800

    remove FIXME, engine should ensure that additional cost is paid

--- release/Magarena/scripts/Fling.groovy
+++ release/Magarena/scripts/Fling.groovy
@@ -2,16 +2,13 @@
     new MagicSpellCardEvent() {
         @Override
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            // FIXME: getTarget may be MagicTargetNone when not cast from hand
-            return payedCost.getTarget().isPermanent() ?
-                new MagicEvent(
-                    cardOnStack,
-                    NEG_TARGET_CREATURE_OR_PLAYER,
-                    payedCost.getTarget(),
-                    this,
-                    "SN deals damage equal to RN's power to target creature or player\$."
-                ):
-                MagicEvent.NONE;
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_CREATURE_OR_PLAYER,
+                payedCost.getTarget(),
+                this,
+                "SN deals damage equal to RN's power to target creature or player\$."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
