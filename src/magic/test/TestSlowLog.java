558879d2eb77a77450a0a342a97405fa98bbab4e
melvinzhang <melvinzhang@gmail.com>
2015-08-16 14:13:54 +0800

    add test case for issue #419, game slows down then there are many message in log

---
+++ src/magic/test/TestSlowLog.java
@@ -0,0 +1,45 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicDeckProfile;
+import magic.model.phase.*;
+import magic.ai.MagicAIImpl;
+
+// 1. Click on Lighting Bolt
+// 2. Click on Undo
+// 3. wait for 1000 log messages to appear in log
+// 4. Try to continue the game normally, it will be slower than before
+class TestSlowLog extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS, 6);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(1);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Island", false, 7);
+        createPermanent(P, "Chromatic Lantern", false, 1);
+        createPermanent(P, "Grizzly Bears", false, 1);
+        addToHand(P, "Lightning Bolt", 2);
+        
+        P = opponent;
+
+        P.setLife(2);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Island", false, 7);
+        createPermanent(P, "Chromatic Lantern", false, 1);
+        createPermanent(P, "Grizzly Bears", false, 1);
+        
+        for (int i = 0; i < 1000; i++) {
+            game.logMessage(P, "msg " + i);
+        }
+        
+        return game;
+    }
+}
