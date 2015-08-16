dcacd6e3924f69d90da33e02044dbd9a60b3595b
melvinzhang <melvinzhang@gmail.com>
2015-08-16 23:06:33 +0800

    replace setLogStackLayout with MigLayout's row and component constraints

--- src/magic/ui/duel/viewer/LogStackViewer.java
+++ src/magic/ui/duel/viewer/LogStackViewer.java
@@ -7,7 +7,7 @@ import magic.ui.widget.TexturedPanel;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
-public class LogStackViewer extends TexturedPanel implements IStackViewerListener  {
+public class LogStackViewer extends TexturedPanel {
 
     private final LogBookViewer logBookViewer;
     private final StackViewer stackViewer;
@@ -16,47 +16,14 @@ public class LogStackViewer extends TexturedPanel implements IStackViewerListene
         logBookViewer = logBookViewer0;
         logBookViewer.setOpaque(false);
         stackViewer = stackViewer0;
-        stackViewer.addListener(this);
+        
         setBorder(FontsAndBorders.BLACK_BORDER);
-    }
-
-    @Override
-    public void stackViewerUpdated() {
-        setLogStackLayout();
-        revalidate();
-        logBookViewer.forceVerticalScrollbarToMax();
-    }
-
-    /**
-     * The log should expand or contract depending on the height of the stack.
-     * If the stack would cause the log to shrink below 60 pixels then stack
-     * will cease expanding and use a vertical scrollbar instead.
-     */
-    public void setLogStackLayout() {
-
-        int viewerHeight = getSize().height;
-        int stackHeight = stackViewer.getPreferredSize().height;
-        int logHeight = 60; // minimum log height.
-
-        if (stackHeight > 0) {
-            if (viewerHeight - stackHeight < logHeight) {
-                stackHeight = viewerHeight - logHeight;
-            } else {
-                logHeight = viewerHeight - stackHeight;
-            }
-            String rowConstraints = "[" + logHeight + "][" + stackHeight + "]";
-
-            removeAll();
-            setLayout(new MigLayout("insets 0, gap 0, flowy", "", rowConstraints));
-
-            add(logBookViewer, "w 100%, h 100%");
-            add(stackViewer, "w 100%, h 100%");
-        }
-
+        setLayout(new MigLayout("insets 0, gap 0, wrap 1, filly", "", "[grow, shrinkprio 200][]"));
+        add(logBookViewer, "w 100%, h 60:100%");
+        add(stackViewer, "w 100%");
     }
 
     public Rectangle getStackViewerRectangle(Component canvas) {
         return stackViewer.getStackViewerRectangle(canvas);
     }
-
 }
