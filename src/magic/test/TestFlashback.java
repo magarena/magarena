cf2665514ae6846083ac43eb255219b1a0896471
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-08-20 19:19:40 +0100

    add TestFlashback

---
+++ src/magic/test/TestFlashback.java
@@ -0,0 +1,33 @@
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestFlashback extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS,8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Forest", 10);
+        addToHand(P, "Chatter of the Squirrel", 4);
+        addToHand(P, "Call of the Herd", 4);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Island", 20);
+        createPermanent(P, "Island", 10);
+
+        return game;
+    }
+}
