e22cec4904f2fbea6a24af5adc78606729558cb0
lodici <lodici123@gmail.com>
2015-08-21 10:29:06 +0100

    extract StackButton out of StackViewer into package-private class.

--- src/magic/ui/duel/sidebar/StackViewer.java
+++ src/magic/ui/duel/sidebar/StackViewer.java
@@ -1,14 +1,11 @@
 package magic.ui.duel.sidebar;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -16,10 +13,6 @@ import javax.swing.Scrollable;
 import magic.ui.SwingGameController;
 import magic.ui.duel.viewer.ChoiceViewer;
 import magic.ui.duel.viewer.StackViewerInfo;
-import magic.ui.theme.ThemeFactory;
-import magic.ui.widget.FontsAndBorders;
-import magic.ui.widget.PanelButton;
-import magic.ui.widget.TextLabel;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
@@ -68,7 +61,7 @@ public class StackViewer extends JPanel implements ChoiceViewer {
         // Display stack items
         final List<StackViewerInfo> stack = controller.getViewerInfo().getStack();
         for (final StackViewerInfo stackInfo : stack) {
-            StackButton btn = new StackButton(stackInfo, maxWidth);
+            StackButton btn = new StackButton(controller, stackInfo, maxWidth);
             buttons.add(btn);
             stackScrollablePanel.add(btn, "w 100%");
         }
@@ -86,66 +79,6 @@ public class StackViewer extends JPanel implements ChoiceViewer {
     public void showValidChoices(final Set<?> validChoices) {
         for (final StackButton button : buttons) {
             button.showValidChoices(validChoices);
-        }
-    }
-
-    private final class StackButton extends PanelButton implements ChoiceViewer {
-
-        private static final long serialVersionUID = 1L;
-
-        private final StackViewerInfo stackInfo;
-
-        public StackButton(final StackViewerInfo stackInfo,final int maxWidth) {
-
-            this.stackInfo=stackInfo;
-
-            final JPanel panel=new JPanel();
-            panel.setOpaque(false);
-            panel.setBorder(FontsAndBorders.getPlayerBorder(stackInfo.visible));
-            panel.setLayout(new BorderLayout(0,0));
-            setComponent(panel);
-
-            final JLabel sourceLabel=new JLabel(stackInfo.name);
-            sourceLabel.setIcon(stackInfo.icon);
-            sourceLabel.setForeground(ThemeFactory.getInstance().getCurrentTheme().getNameColor());
-            panel.add(sourceLabel,BorderLayout.NORTH);
-
-            final TextLabel textLabel=new TextLabel(stackInfo.description,maxWidth,false);
-            panel.add(textLabel,BorderLayout.CENTER);
-        }
-
-        @Override
-        public void mouseClicked() {
-            controller.processClick(stackInfo.itemOnStack);
-        }
-
-        @Override
-        public void mouseEntered() {
-            if (isImageMode) {
-                final Rectangle rect=new Rectangle(
-                        StackViewer.this.getLocationOnScreen().x,
-                        getLocationOnScreen().y,
-                        StackViewer.this.getWidth(),
-                        getHeight());
-                controller.viewInfoRight(stackInfo.cardDefinition,0,rect);
-            }
-        }
-
-        @Override
-        public void mouseExited() {
-            if (isImageMode) {
-                controller.hideInfo();
-            }
-        }
-
-        @Override
-        public void showValidChoices(final Set<?> validChoices) {
-            setValid(validChoices.contains(stackInfo.itemOnStack));
-        }
-
-        @Override
-        public Color getValidColor() {
-            return ThemeFactory.getInstance().getCurrentTheme().getChoiceColor();
         }
     }
 
