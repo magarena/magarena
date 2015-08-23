bb40ee34df48c53255d43e8f3daab38b88793d51
melvinzhang <melvinzhang@gmail.com>
2015-08-23 08:50:33 +0800

    fix Shuriken's throw ability to be on itself instead of the equipped creature

--- release/Magarena/scripts/Shuriken.groovy
+++ release/Magarena/scripts/Shuriken.groovy
@@ -1,40 +1,37 @@
-def public MagicPermanentActivation ThrowIt(final MagicPermanent source) {
-    return new MagicPermanentActivation(new MagicActivationHints(MagicTiming.Removal),"Damage"){
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Damage"
+    ) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicPermanent permanent) {
-            return [new MagicTapEvent(permanent)];
+            return [
+                new MagicTapEvent(permanent.getEquippedCreature())
+            ];
         }
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent permanent,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 permanent,
-                new MagicTargetChoice(CREATURE,"target creature."),
-                source,
+                NEG_TARGET_CREATURE,
+                permanent.getEquippedCreature(),
                 this,
-                "Unattach RN from SN, RN deals 2 damage to target creature\$."
+                "Unattack SN. SN deals 2 damage to target creature\$."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event){
-            game.doAction(new AttachAction(source, MagicPermanent.NONE));
-            event.processTarget(game,{
-                game.doAction(new DealDamageAction(source,it,2));
-                if (!event.getPermanent().hasSubType(MagicSubType.Ninja)) {
-                    game.doAction(new GainControlAction(it.getController(),source,MagicStatic.Forever));
-                }
-            });
-        }   
-    }
-};
-[
-    new MagicStatic(MagicLayer.Ability) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
-            permanent.addAbility(ThrowIt(source));
-        }
-        @Override
-        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) { 
-            return MagicStatic.acceptLinked(game,source,target);
+            final MagicPermanent source = event.getPermanent();
+            final MagicPermanent holder = event.getRefPermanent();
+            if (source.getEquippedCreature() == holder) {
+                game.doAction(new AttachAction(source, MagicPermanent.NONE));
+                event.processTarget(game,{
+                    game.doAction(new DealDamageAction(source,it,2));
+                    if (holder.hasSubType(MagicSubType.Ninja) == false) {
+                        game.doAction(new GainControlAction(it.getController(),source));
+                    }
+                });
+            }
         }
     }
 ]
