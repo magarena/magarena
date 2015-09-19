a55be693af43e344fcf562a1a9e857ffae2aebfc
lodici <lodici123@gmail.com>
2015-09-19 17:24:58 +0100

    extract code for building TComponents out of TextLabel and into TComponentBuilder.

---
+++ src/magic/ui/message/TComponentBuilder.java
@@ -0,0 +1,178 @@
+package magic.ui.message;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.List;
+import javax.swing.JComponent;
+import magic.data.TextImages;
+import magic.model.MagicMessage;
+import magic.ui.IconImages;
+import magic.ui.widget.FontsAndBorders;
+import magic.utility.MagicSystem;
+
+class TComponentBuilder {
+    private TComponentBuilder() {}
+    
+    static final TComponent SPACE_COMPONENT = new EmptyComponent();
+    static final TComponent BREAK_COMPONENT = new EmptyComponent();
+
+    private static JComponent container;
+    private static Font defaultFont;
+    private static Color choiceColor;
+    private static List<TComponent> components;
+
+    static void buildTComponents(
+        final List<TComponent> components,
+        final String source,
+        final JComponent aContainer,
+        final Font aFont,
+        final Color aColor) {
+
+        TComponentBuilder.components = components;
+        TComponentBuilder.container = aContainer;
+        TComponentBuilder.defaultFont = aFont;
+        TComponentBuilder.choiceColor = aColor;
+
+        final String msg = replaceWhitespace(source, " ") + ' ';
+
+        boolean isTinyFont = false;
+        boolean isChoice = false;
+        String cardInfo = "";
+
+        int startIndex = 0;
+
+        for (int index = 0; index < msg.length(); index++) {
+
+            final char ch = msg.charAt(index);
+
+            //
+            // space
+            //
+            if (ch == ' ') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                addTComponent(SPACE_COMPONENT);
+                startIndex = index + 1;
+
+            //
+            // new line
+            //
+            } else if (ch == '|') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                addTComponent(BREAK_COMPONENT);
+                startIndex = index + 1;
+
+            //
+            // "{icon_image}"
+            //
+            } else if (ch == '{') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index;
+
+            } else if (ch == '}') {
+                final String textPart = msg.substring(startIndex, index + 1);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index + 1;
+
+            //
+            // "(player_choice)"
+            //
+            } else if (ch == '(') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index;
+                isTinyFont = true;
+                isChoice = true;
+
+            } else if (ch == ')') {
+                final String textPart = msg.substring(startIndex, index + 1);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index + 1;
+                isTinyFont = false;
+                isChoice = false;
+
+            //
+            // "[use_tiny_font]"
+            //
+            } else if (ch == '[') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index + 1;
+                isTinyFont = true;
+
+            } else if (ch == ']') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index + 1;
+                isTinyFont = false;
+
+            //
+            // "<card_name#MagicCard_id>"
+            //
+            } else if (ch == '<') {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                cardInfo = msg.substring(index + 1, msg.indexOf(">", index + 1));
+                startIndex = index + 1;
+
+            } else if (ch == '>') {
+                final String textPart = String.format(isTinyFont ? "#%s" : "%s", msg.substring(startIndex, index));
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                isTinyFont = false;
+                isChoice = false;
+                cardInfo = "";
+                startIndex = index + 1;
+
+            } else if (ch == MagicMessage.CARD_ID_DELIMITER) {
+                final String textPart = msg.substring(startIndex, index);
+                addTComponent(textPart, isTinyFont, isChoice, cardInfo);
+                startIndex = index + 1;
+                // only display card id if running in dev mode.
+                if (MagicSystem.isDevMode()) {
+                    isTinyFont = true;
+                } else {
+                    startIndex = msg.indexOf(">", index + 1);
+                }
+            }
+        }
+
+    }
+
+    // whitespace = multiple spaces, tabs, line breaks, form feeds, etc.
+    private static String replaceWhitespace(final String text, final String replace) {
+        return text.replaceAll("\\s+", replace).trim();
+    }
+
+    private static void addTComponent(final TComponent component) {
+        if (component != null) {
+            components.add(component);
+        }
+    }
+
+    private static void addTComponent(String text, boolean isTinyFont, boolean isChoice, String aCardInfo) {
+        addTComponent(getTComponent(text, isTinyFont, isChoice ,aCardInfo));
+    }
+
+    private static TComponent getTComponent(final String text, boolean isTinyFont, boolean isChoice, String aCardInfo) {
+
+        if (text.isEmpty()) {
+            return null;
+        }
+
+        if (text.charAt(0) == '{' && TextImages.contains(text)) {
+            return new IconComponent(IconImages.getIcon(TextImages.getIcon(text)));
+        }
+
+        return new TextComponent(
+            text,
+            container,
+            isTinyFont ? FontsAndBorders.FONT0 : defaultFont,
+            isChoice,
+            aCardInfo,
+            choiceColor
+        );
+    }
+
+}
--- src/magic/ui/message/TextLabel.java
+++ src/magic/ui/message/TextLabel.java
@@ -11,173 +11,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.swing.JPanel;
-import magic.data.TextImages;
-import magic.model.MagicMessage;
-import magic.ui.IconImages;
 import magic.ui.theme.Theme;
 import magic.ui.utility.MagicStyle;
 import magic.ui.widget.FontsAndBorders;
-import magic.utility.MagicSystem;
 
 @SuppressWarnings("serial")
 public class TextLabel extends JPanel {
-
-    private static final TComponent SPACE_LABEL = new EmptyComponent();
-    private static final TComponent BREAK_LABEL = new EmptyComponent();
 
     private static final int SPACE_WIDTH = 3;
     private static final int LINE_HEIGHT = 16;
 
     private static final Map<?, ?> desktopHintsMap;
 
-    private final List<TComponent> components;
+    private final List<TComponent> components = new ArrayList<>();
     private final int maxWidth;
     private final boolean center;
-    private final Font defaultFont;
-    private final Color choiceColor;
 
     static {
         final Toolkit tk = Toolkit.getDefaultToolkit();
         desktopHintsMap = (Map<?, ?>) (tk.getDesktopProperty("awt.font.desktophints"));
     }
 
+    // CTR
     public TextLabel(String text, Font aFont, int maxWidth, boolean center, Color aColor) {
 
-        this.defaultFont = aFont;
         this.maxWidth = maxWidth;
         this.center = center;
-        this.choiceColor = aColor;
-
-        components = new ArrayList<>();
 
         setOpaque(false);
         setLayout(null);
 
-        buildComponents(text);
-        layoutComponents();
+        TComponentBuilder.buildTComponents(components, text, this, aFont, aColor);
+        layoutTComponents();
     }
 
+    // CTR
     public TextLabel(String text, Font aFont, int maxWidth, boolean center) {
         this(text, aFont, maxWidth, center, MagicStyle.getTheme().getColor(Theme.COLOR_CHOICE_FOREGROUND));
     }
 
+    // CTR
     public TextLabel(final String text, final int maxWidth, final boolean center) {
         this(text, FontsAndBorders.FONT1, maxWidth, center, MagicStyle.getTheme().getColor(Theme.COLOR_CHOICE_FOREGROUND));
     }
 
-    private void addComponent(final TComponent component) {
-        if (component != null) {
-            components.add(component);
-        }
-    }
+    private void layoutTComponents() {
 
-    private TComponent buildComponent(
-            final String textPart,
-            final boolean info,
-            final boolean isBlueInfo,
-            final String aCardInfo) {
-
-        if (textPart.isEmpty()) {
-            return null;
-        }
-
-        final TComponent component;
-        if (textPart.charAt(0) == '{' && TextImages.contains(textPart)) {
-            component = new IconComponent(IconImages.getIcon(TextImages.getIcon(textPart)));
-        } else if (info) {
-            component = new TextComponent(textPart, this, FontsAndBorders.FONT0, isBlueInfo, aCardInfo, choiceColor);
-        } else {
-            component = new TextComponent(textPart, this, defaultFont, false, aCardInfo, choiceColor);
-        }
-        return component;
-    }
-
-    private TComponent buildComponent(final String textPart, final boolean info, final boolean isBlueInfo) {
-        return buildComponent(textPart, info, isBlueInfo, "");
-    }
-
-    private String replaceWhitespace(final String text, final String replace) {
-        // whitespace = mutliple spaces, tabs, line breaks, form feeds, etc.
-        return text.replaceAll("\\s+", replace).trim();
-    }
-
-    private void buildComponents(String text) {
-
-        text = replaceWhitespace(text, " ") + ' ';
-
-        boolean info = false;
-        boolean isBlueInfo = false;
-        String cardInfo = "";
-        int startIndex = 0;
-
-        for (int index = 0; index < text.length(); index++) {
-            final char ch = text.charAt(index);
-            if (ch == ' ') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo, cardInfo));
-                addComponent(SPACE_LABEL);
-                startIndex = index + 1;
-            } else if (ch == '|') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo, cardInfo));
-                addComponent(BREAK_LABEL);
-                startIndex = index + 1;
-            } else if (ch == '{') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo));
-                startIndex = index;
-            } else if (ch == '}') {
-                addComponent(buildComponent(text.substring(startIndex, index + 1), info, isBlueInfo));
-                startIndex = index + 1;
-            } else if (ch == '(') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo));
-                startIndex = index;
-                info = true;
-                isBlueInfo = true;
-            } else if (ch == ')') {
-                addComponent(buildComponent(text.substring(startIndex, index + 1), info, isBlueInfo));
-                startIndex = index + 1;
-                info = false;
-                isBlueInfo = false;
-            } else if (ch == '[') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo));
-                startIndex = index + 1;
-                info = true;
-            } else if (ch == ']') {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo));
-                startIndex = index + 1;
-                info = false;
-            } else if (ch == '<') {
-                cardInfo = text.substring(index + 1, text.indexOf(">", index + 1));
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo));
-                startIndex = index + 1;
-            } else if (ch == '>') {
-                final String s = String.format(info ? "#%s" : "%s", text.substring(startIndex, index));
-                addComponent(buildComponent(s, info, isBlueInfo, cardInfo));
-                info = false;
-                isBlueInfo = false;
-                cardInfo = "";
-                startIndex = index + 1;
-            } else if (ch == MagicMessage.CARD_ID_DELIMITER) {
-                addComponent(buildComponent(text.substring(startIndex, index), info, isBlueInfo, cardInfo));
-                startIndex = index + 1;
-                if (MagicSystem.isDevMode()) {
-                    info = true;
-                    isBlueInfo = true;
-                } else {
-                    startIndex = text.indexOf(">", index + 1);
-                }
-            }
-        }
-    }
-
-    private void layoutComponents() {
         int x = 0;
         int y = 0;
+
         for (final TComponent component : components) {
 
-            if (component == SPACE_LABEL) {
+            if (component == TComponentBuilder.SPACE_COMPONENT) {
                 x += SPACE_WIDTH;
-            } else if (component == BREAK_LABEL) {
+
+            } else if (component == TComponentBuilder.BREAK_COMPONENT) {
                 x = 0;
                 y += LINE_HEIGHT;
+
             } else {
                 final Dimension csize = component.getPreferredSize();
                 if (component.requiresNewLine() && x + csize.width > maxWidth) {
