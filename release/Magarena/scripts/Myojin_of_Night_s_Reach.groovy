6fcc6b0fe43e120a28acdc489a33bc95117bc025
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-02 19:08:56 +0000

    remove duplicate groovy code - 'Myojin of' series

--- release/Magarena/scripts/Myojin_of_Night_s_Reach.groovy
+++ release/Magarena/scripts/Myojin_of_Night_s_Reach.groovy
@@ -1,13 +1,4 @@
 [
-    new MagicComesIntoPlayWithCounterTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            if (permanent.hasState(MagicPermanentState.CastFromHand)) {
-                game.doAction(new ChangeCountersAction(permanent, MagicCounterType.Divinity, 1));
-            } 
-            return MagicEvent.NONE;
-        }
-    },
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
         "Discards"
