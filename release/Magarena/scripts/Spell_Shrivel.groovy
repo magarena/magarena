4255c7507a74291924e90671a129e03d6e139c6e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-04 00:24:10 +0000

    add Spell Shrivel

---
+++ release/Magarena/scripts/Spell_Shrivel.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_SPELL,
+                this,
+                "Counter target spell\$ unless its controller pays {4}. " +
+                "If that spell is countered this way, exile it instead of putting it into its owner's graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                game.addEvent(new MagicCounterUnlessEvent(
+                    event.getSource(), 
+                    it, 
+                    MagicManaCost.create("{4}"),
+                    MagicLocationType.Exile
+                ));
+            });
+        }
+    }
+]
