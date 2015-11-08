ffbe86cfe4af4b271fab62b62c5c05990ea623c3
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-08 22:37:16 +0000

    correct targeting and filter for Blessed Reversal, fixes #491

--- release/Magarena/scripts/Blessed_Reversal.groovy
+++ release/Magarena/scripts/Blessed_Reversal.groovy
@@ -4,15 +4,15 @@
         public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
             return new MagicEvent(
                 cardOnStack,
-                NEG_TARGET_ATTACKING_CREATURE,
                 this,
                 "PN gains 3 life for each creature attacking PN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int amount = 3*event.getPlayer().getNrOfPermanents(ATTACKING_CREATURE_YOUR_OPPONENT_CONTROLS);
+            final int amount = 3*event.getPlayer().getOpponent().getNrOfPermanents(ATTACKING_CREATURE_YOU_CONTROL);
             game.doAction(new ChangeLifeAction(event.getPlayer(),amount));
+            game.logAppendValue(event.getPlayer(),amount);
         }
     }
 ]
