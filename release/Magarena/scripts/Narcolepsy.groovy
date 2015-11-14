924b830d1eb017db1ecb629e278f2b3ced492352
melvinzhang <melvinzhang@gmail.com>
2015-11-14 18:06:56 +0800

    use RN instead of constructing the text manually so that name is interactive in log/stack, fixes #489

--- release/Magarena/scripts/Narcolepsy.groovy
+++ release/Magarena/scripts/Narcolepsy.groovy
@@ -6,16 +6,16 @@
             return (enchantedCreature.isValid() && enchantedCreature.isUntapped()) ?
                 new MagicEvent(
                     permanent,
+                    enchantedCreature,
                     this,
-                    "If "+enchantedCreature+" is untapped, tap it."
+                    "If RN is untapped, tap it."
                 ):
                 MagicEvent.NONE;
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPermanent permanent=event.getPermanent();
-            final MagicPermanent enchantedCreature=permanent.getEnchantedPermanent();
-            if (enchantedCreature.isValid()) {
+            final MagicPermanent enchantedCreature = event.getRefPermanent();
+            if (enchantedCreature.isUntapped()) {
                 game.doAction(new TapAction(enchantedCreature));
             }
         }
