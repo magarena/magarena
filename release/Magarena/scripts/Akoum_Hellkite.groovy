0a24b82a9decc713c35fa2c4a1e9f3d64a089f38
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-17 16:08:14 +0000

    add Akoum Hellkite

---
+++ release/Magarena/scripts/Akoum_Hellkite.groovy
@@ -0,0 +1,25 @@
+[
+    new MagicWhenOtherComesIntoPlayTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
+            final int amount = otherPermanent.hasSubType(MagicSubType.Mountain) ? 2 : 1;
+            return (otherPermanent.isLand() && otherPermanent.isFriend(permanent)) ?
+                new MagicEvent(
+                    permanent,
+                    NEG_TARGET_CREATURE_OR_PLAYER,
+                    new MagicDamageTargetPicker(amount),
+                    amount,
+                    this,
+                    "SN deals "+amount+" damage to target creature or player\$."
+                ) :
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                game.doAction(new DealDamageAction(event.getSource(), it, event.getRefInt()));
+            });
+        }
+    }
+]
