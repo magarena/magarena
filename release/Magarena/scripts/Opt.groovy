c9eb53d6f16b9ad054ee9b7d27655c4165e29b5a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-19 01:12:27 +0000

    correct tense of Opt's log message

--- release/Magarena/scripts/Opt.groovy
+++ release/Magarena/scripts/Opt.groovy
@@ -5,7 +5,7 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "Look at the top card of your library. You may put that card on the bottom of your library. " +
+                "PN looks at the top card of his or her library. PN may put that card on the bottom of his or her library. " +
                 "Draw a card."
             );
         }
