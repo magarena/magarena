8086b99a4ee4557c59420f068d3b9a3925680333
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-19 01:12:02 +0000

    revise Scry effect log text to closer match reminder text

--- src/magic/model/event/MagicScryEvent.java
+++ src/magic/model/event/MagicScryEvent.java
@@ -8,19 +8,19 @@ import magic.model.choice.MagicScryChoice;
 import magic.model.trigger.MagicTriggerType;
 
 public class MagicScryEvent extends MagicEvent {
-    
+
     public MagicScryEvent(final MagicEvent event) {
         this(event.getSource(), event.getPlayer(), true);
     }
-    
+
     public MagicScryEvent(final MagicSource source, final MagicPlayer player) {
         this(source, player, true);
     }
-    
+
     public static MagicScryEvent Pseudo(final MagicEvent event) {
         return new MagicScryEvent(event.getSource(), event.getPlayer(), false);
     }
-    
+
     public static MagicScryEvent Pseudo(final MagicSource source, final MagicPlayer player) {
         return new MagicScryEvent(source, player, false);
     }
@@ -34,17 +34,17 @@ public class MagicScryEvent extends MagicEvent {
             ""
         );
     }
-    
+
     private static final MagicEventAction EventAction(final boolean trigger) {
         return new MagicEventAction() {
             @Override
             public void executeEvent(final MagicGame game, final MagicEvent event) {
                 final MagicPlayer p = event.getPlayer();
                 if (event.isYes()) {
-                    game.logAppendMessage(p, p + " looks at the card on the top of his or her library and moves it to the bottom.");
+                    game.logAppendMessage(p, p + " looks at the top card of his or her library and moves it to the bottom.");
                     game.doAction(new ScryAction(p));
                 } else {
-                    game.logAppendMessage(p, p + " looks at the card on the top of his or her library and puts it back on top.");
+                    game.logAppendMessage(p, p + " looks at the top card of his or her library and puts it back on top.");
                 }
                 //Scry triggers even if the card is not moved. Only once regardless of amount of cards scryed
                 if (trigger) {
