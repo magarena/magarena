bd2bf987ac7e2435ddef9b5bd91b617bc936e062
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-21 12:18:48 +0000

    rename MagicLandfallTrigger to LandfallTrigger

--- release/Magarena/scripts/Roil_Elemental.groovy
+++ release/Magarena/scripts/Roil_Elemental.groovy
@@ -1,5 +1,5 @@
 [
-    new MagicLandfallTrigger() {
+    new LandfallTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent played) {
             return new MagicEvent(
--- src/magic/model/trigger/LandfallTrigger.java
+++ src/magic/model/trigger/LandfallTrigger.java
@@ -5,20 +5,20 @@ import magic.model.MagicPermanent;
 import magic.model.event.MagicEvent;
 import magic.model.event.MagicSourceEvent;
 
-public abstract class MagicLandfallTrigger extends MagicWhenOtherComesIntoPlayTrigger {
-    public MagicLandfallTrigger(final int priority) {
+public abstract class LandfallTrigger extends MagicWhenOtherComesIntoPlayTrigger {
+    public LandfallTrigger(final int priority) {
         super(priority);
     }
 
-    public MagicLandfallTrigger() {}
+    public LandfallTrigger() {}
 
     @Override
     public boolean accept(final MagicPermanent permanent, final MagicPermanent played) {
         return played.isFriend(permanent) && played.isLand();
     }
-    
-    public static final MagicLandfallTrigger create(final MagicSourceEvent sourceEvent) {
-        return new MagicLandfallTrigger() {
+
+    public static final LandfallTrigger create(final MagicSourceEvent sourceEvent) {
+        return new LandfallTrigger() {
             @Override
             public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent played) {
                 return sourceEvent.getEvent(permanent);
