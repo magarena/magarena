d83840ec5cb0ccf314376488056ea1379107f3cd
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-24 15:22:17 +0000

    edit Maelstrom Djinn to use ThisTurnedFaceUpTrigger

--- release/Magarena/scripts/Maelstrom_Djinn.groovy
+++ release/Magarena/scripts/Maelstrom_Djinn.groovy
@@ -1,5 +1,5 @@
 [
-    new TurnedFaceUpTrigger() {
+    new ThisTurnedFaceUpTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return new MagicEvent(
