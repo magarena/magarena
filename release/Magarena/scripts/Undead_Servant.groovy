031b1be075af050725d6410914fcae3ff221761a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-29 14:50:15 +0000

    add Undead Servant

---
+++ release/Magarena/scripts/Undead_Servant.groovy
@@ -0,0 +1,29 @@
+[
+    new EntersBattlefieldTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN puts a 2/2 black Zombie creature token onto the battlefield for each card named "+
+                "Undead Servant in his or her graveyard."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = cardName("Undead Servant")
+                .from(MagicTargetType.Graveyard)
+                .filter(event)
+                .size();
+            final MagicPlayer player = event.getPlayer();
+            event.processTarget(game, {
+                game.logAppendValue(player, amount);
+                game.doAction(new PlayTokensAction(
+                    player,
+                    CardDefinitions.getToken("2/2 black Zombie creature token"),
+                    amount
+                ));
+            });
+        }
+    }
+]
