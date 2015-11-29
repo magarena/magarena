daf9e0ef7b1315eb1d43cde7191a718af77289dc
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-29 16:43:40 +0000

    add Bogbrew Witch

---
+++ release/Magarena/scripts/Bogbrew_Witch.groovy
@@ -0,0 +1,48 @@
+def CARD_NAMED_NEWT_OR_CAULDRON = new MagicCardFilterImpl() {
+        public boolean accept(final MagicSource source,final MagicPlayer player,final MagicCard target) {
+            return target.getName().equals("Festering Newt") || target.getName().equals("Bubbling Cauldron");
+        }
+        public boolean acceptType(final MagicTargetType targetType) {
+            return targetType == MagicTargetType.Library;
+        }
+    }; 
+def A_CARD_NAMED_NEWT_OR_CAULDRON = new MagicTargetChoice(
+        CARD_NAMED_NEWT_OR_CAULDRON,  
+        MagicTargetHint.None,
+        "a card named Festering Newt or Bubbling Cauldron"
+    );
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Token),
+        "Search"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{2}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN searches his or her library for a card named Festering Newt or Bubbling Cauldron, "+
+                "puts it onto the battlefield tapped, then shuffles his or her library."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicSearchOntoBattlefieldEvent(
+                event,
+                A_CARD_NAMED_NEWT_OR_CAULDRON,
+                MagicPlayMod.TAPPED
+            ));
+        }
+    }
+]
