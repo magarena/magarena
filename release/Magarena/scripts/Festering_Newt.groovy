f783a5ba94b4ae72e7f1a5a5cdfcdf5d6e6e28d7
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-11-29 16:44:16 +0000

    add Festering Newt

---
+++ release/Magarena/scripts/Festering_Newt.groovy
@@ -0,0 +1,25 @@
+[
+    new ThisDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent died) {
+            final MagicTargetFilter<MagicPermanent> witch = new MagicNameTargetFilter("Bogbrew Witch");
+            final int amount = (permanent.getController().controlsPermanent(witch))? 4: 1; 
+            return new MagicEvent(
+                permanent,
+                new MagicTargetChoice("target creature an opponent controls"),
+                new MagicWeakenTargetPicker(amount,amount),
+                this,
+                "Target creature an opponent controls\$ gets -1/-1 until end of turn. "+
+                "That creature gets -4/-4 instead if PN controls a creature named Bogbrew Witch."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                final MagicTargetFilter<MagicPermanent> witch = new MagicNameTargetFilter("Bogbrew Witch");
+                final int amount = (event.getPlayer().controlsPermanent(witch))? 4: 1;
+                game.doAction(new ChangeTurnPTAction(it, -amount, -amount));
+            });
+        }
+    }
+]
