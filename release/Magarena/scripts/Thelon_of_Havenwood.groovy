ad685300b72e3ce0b02c4ce15fa9210360dc7e8c
melvinzhang <melvinzhang@gmail.com>
2015-12-06 16:24:55 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Thelon_of_Havenwood.groovy
+++ release/Magarena/scripts/Thelon_of_Havenwood.groovy
@@ -1,5 +1,3 @@
-def choice = new MagicTargetChoice("a Fungus card from a graveyard");
-
 [
     new MagicStatic(MagicLayer.ModPT, CREATURE) {
         @Override
@@ -10,36 +8,6 @@ def choice = new MagicTargetChoice("a Fungus card from a graveyard");
         @Override
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return target.hasSubType(MagicSubType.Fungus);
-        }
-    },
-    
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "Pump"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source, "{B}{G}"),
-                new MagicExileCardEvent(source, choice)
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN puts a spore counter on each Fungus on the battlefield."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            FUNGUS.filter(event) each {
-                game.doAction(new ChangeCountersAction(it,MagicCounterType.Spore,1));
-            }
         }
     }
 ]
