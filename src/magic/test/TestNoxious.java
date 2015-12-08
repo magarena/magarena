c8e07767ec9832f89c7eefea2884b7d0fa2e0a8b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-12-08 12:32:29 +0000

    add TextNoxious for Noxious Revival loop

---
+++ src/magic/test/TestNoxious.java
@@ -0,0 +1,39 @@
+
+package magic.test;
+
+import magic.ai.MagicAIImpl;
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicUpkeepPhase;
+
+class TestNoxious extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS, 8);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P,"Plains");
+        createPermanent(P,"Forest");
+        createPermanent(P, "Voice of Resurgence",true,1);
+
+
+        P = opponent;
+
+        P.setLife(18);
+        addToLibrary(P, "Forest", 18);
+        addToGraveyard(P,"Noxious Revival",1);
+        addToGraveyard(P,"Brave the Elements",1);
+        addToHand(P,"Noxious Revival",1);
+        createPermanent(P, "Forest");
+        createPermanent(P, "Plains");
+
+        return game;
+    }
+}
