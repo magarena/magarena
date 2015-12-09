a707141a02d96599dc94a6881521e3fc35cb7e87
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-12-09 20:37:10 +0000

    add Raging Spirit

---
+++ release/Magarena/scripts/Raging_Spirit.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Colorless"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source,"{2}"),
+                new MagicPlayAbilityEvent(source)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN becomes the colorless until end of turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new AddStaticAction(event.getPermanent(),
+                new MagicStatic(MagicLayer.Color,MagicStatic.UntilEOT) {
+                @Override
+                public int getColorFlags(final MagicPermanent permanent, final int flags) {
+                    return 0;
+                }
+            }));
+        }
+    }
+]
