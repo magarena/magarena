c32aa7f00f391785ca8c20524c0a953428d01aa9
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-12-10 02:36:58 +0000

    add Mercurial Pretender

---
+++ release/Magarena/scripts/Mercurial_Pretender.groovy
@@ -0,0 +1,65 @@
+def Bounce = new MagicPermanentActivation(
+    new MagicActivationHints(MagicTiming.Removal, true),
+    "Bounce"
+) {
+    @Override
+    public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+        return [
+            new MagicPayManaCostEvent(source,"{2}{U}{U}"),
+            new MagicPlayAbilityEvent(source)
+        ];
+    }
+    @Override
+    public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+        return new MagicEvent(
+            source,
+            this,
+            "Return SN to its owner's hand."
+        );
+    }
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        game.doAction(new RemoveFromPlayAction(event.getPermanent(),MagicLocationType.OwnersHand));
+    }
+};
+    
+def GainAct = new MagicStatic(MagicLayer.Ability) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+        permanent.addAbility(Bounce);
+    }
+};
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(A_CREATURE_YOU_CONTROL),
+                MagicCopyPermanentPicker.create(),
+                this,
+                "PN may\$ have SN enter the battlefield as a copy of any creature you control\$, " + 
+                "except it gains \"{2}{U}{U}: Return this creature to its owner's hand.\""
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new EnterAsCopyAction(event.getCardOnStack(), event.getTarget(), {
+                    final MagicPermanent perm ->
+                    final MagicGame G = perm.getGame();
+                    G.doAction(new AddStaticAction(
+                        perm,
+                        GainAct
+                    ));
+                }));
+            } else {
+                game.doAction(new PlayCardFromStackAction(
+                    event.getCardOnStack()
+                ));
+            }
+        }
+    }
+]
