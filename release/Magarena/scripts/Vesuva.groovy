b42c40cddd094c1bcf716cc91e3175a90576c4a7
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-12-10 01:37:05 +0000

    add Vesuva

---
+++ release/Magarena/scripts/Vesuva.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(
+                    new MagicTargetChoice("a land")
+                ),
+                MagicCopyPermanentPicker.create(),
+                this,
+                "PN may\$ have SN enter the battlefield as a copy of any land on the battlefield.\$"
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new EnterAsCopyAction(event.getCardOnStack(), event.getTarget()))
+            } else {
+                game.logAppendMessage(event.getPlayer(), "Put ${event.getCardOnStack()} onto the battlefield.");
+                game.doAction(new PlayCardFromStackAction(
+                    event.getCardOnStack()
+                ));
+            }
+        }
+    }
+]
