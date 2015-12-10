1f02511c8daf68d102917f7bad3854b1c9f4f009
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-12-10 13:48:56 +0000

    add Chain of Vapor

---
+++ release/Magarena/scripts/Chain_of_Vapor.groovy
@@ -0,0 +1,36 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicEvent sac = new MagicSacrificePermanentEvent(event.getSource(),event.getPlayer(),SACRIFICE_LAND); 
+    if (event.isYes() && sac.isSatisfied()) {
+        game.addEvent(sac);
+        game.doAction(new CopyCardOnStackAction(event.getPlayer(),event.getCardOnStack()));
+    }
+}
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_NONLAND_PERMANENT,
+                MagicBounceTargetPicker.create(),
+                this,
+                "Return target nonland permanent\$ to its owner's hand."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new RemoveFromPlayAction(it,MagicLocationType.OwnersHand));
+                game.addEvent(new MagicEvent(
+                    event.getSource(),
+                    it.getController(),
+                    new MagicMayChoice("Sacrifice a land?"),
+                    action,
+                    "PN may sacrifice a land\$. If PN does, he or she may copy this spell and may choose a new target for that copy."
+                ));
+            });
+        }
+    }
+]
