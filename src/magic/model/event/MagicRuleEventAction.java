cad46a5cd581b2be486b0aadd991e8eeb1a3c006
melvinzhang <melvinzhang@gmail.com>
2015-12-13 14:07:03 +0800

    remove debug print

--- src/magic/model/event/MagicRuleEventAction.java
+++ src/magic/model/event/MagicRuleEventAction.java
@@ -2730,7 +2730,6 @@ public enum MagicRuleEventAction {
     public static MagicRuleEventAction build(final String rule) {
         for (final MagicRuleEventAction ruleAction : MagicRuleEventAction.values()) {
             if (ruleAction.matches(rule)) {
-                System.out.println(rule + "--" + ruleAction);
                 return ruleAction;
             }
         }
