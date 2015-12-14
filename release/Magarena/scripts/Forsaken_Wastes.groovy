dfb21ea5224f8bc6f18c1123eebae986589a166c
melvinzhang <melvinzhang@gmail.com>
2015-12-14 23:04:09 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Forsaken_Wastes.groovy
+++ release/Magarena/scripts/Forsaken_Wastes.groovy
@@ -8,21 +8,6 @@
             return MagicEvent.NONE;
         }
     },
-    new AtUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
-            return new MagicEvent(
-                permanent,
-                upkeepPlayer,
-                this,
-                "PN loses 1 life."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game,final MagicEvent event) {
-            game.doAction(new ChangeLifeAction(event.getPlayer(),-1));
-        }
-    },
     new ThisBecomesTargetTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicItemOnStack target) {
--- release/Magarena/scripts/Gwafa_Hazid__Profiteer.groovy
+++ release/Magarena/scripts/Gwafa_Hazid__Profiteer.groovy
@@ -8,37 +8,5 @@
         public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
             return target.hasCounters(MagicCounterType.Bribery);
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Tapping),
-        "Bribe"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicTapEvent(source), new MagicPayManaCostEvent(source, "{W}{U}")
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                TARGET_CREATURE_YOUR_OPPONENT_CONTROLS,
-                new MagicNoCombatTargetPicker(true,true,true),
-                this,
-                "PN puts a bribery counter on target creature\$. " +
-                "Its controller draws a card."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new ChangeCountersAction(it,MagicCounterType.Bribery,1));
-                game.doAction(new DrawAction(it.getController()));
-            });
-        }
     }
 ]
