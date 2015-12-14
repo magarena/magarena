784b9baa51d31781fdc9ad7378b1756fab87a65c
melvinzhang <melvinzhang@gmail.com>
2015-12-14 10:46:54 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Street_Spasm.groovy
+++ release/Magarena/scripts/Street_Spasm.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            final int x = payedCost.getX();
-            return new MagicEvent(
-                cardOnStack,
-                TARGET_CREATURE_WITHOUT_FLYING_YOU_DONT_CONTROL,
-                new MagicDamageTargetPicker(x),
-                x,
-                this,
-                "SN deals " + x + " damage to target creature\$ without flying PN doesn't control."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTarget(game, {
-                game.doAction(new DealDamageAction(event.getSource(),it,event.getRefInt()));
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.Removal) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
