cc453607b944f9e3dbd0ea87053c967803aa5356
melvinzhang <melvinzhang@gmail.com>
2015-12-21 13:44:08 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Basandra__Battle_Seraph.groovy
+++ release/Magarena/scripts/Basandra__Battle_Seraph.groovy
@@ -8,37 +8,5 @@
         public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
             return game.isCombatPhase();
         }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.MustAttack),
-        "Attacks"
-    ) {
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicPayManaCostEvent(source, "{R}")
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                NEG_TARGET_CREATURE,
-                MagicMustAttackTargetPicker.create(),
-                this,
-                "Target creature\$ attacks this turn if able."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new GainAbilityAction(
-                    it,
-                    MagicAbility.AttacksEachTurnIfAble
-                ));
-            });
-        }
     }
 ]
--- release/Magarena/scripts/Chemister_s_Trick.groovy
+++ release/Magarena/scripts/Chemister_s_Trick.groovy
@@ -1,23 +1,4 @@
 [
-    new MagicSpellCardEvent() {
-        @Override
-        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                cardOnStack,
-                TARGET_CREATURE_YOU_DONT_CONTROL,
-                MagicTapTargetPicker.Tap,
-                this,
-                "Target creature\$ PN doesn't control gets -2/-0 until end of turn and attacks this turn if able."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new ChangeTurnPTAction(it, -2, 0));
-                game.doAction(new GainAbilityAction(it, MagicAbility.AttacksEachTurnIfAble));
-            });
-        }
-    },
     new MagicOverloadActivation(MagicTiming.MustAttack) {
         @Override
         public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
