f9a3522a0b74d29a251e4358a6c2040be6d3739a
lodici <lodici123@gmail.com>
2015-12-22 15:00:48 +0000

    Show "preparing game..." tooltip if mouse over disabled StartGameButton.

--- src/magic/ui/widget/StartGameButton.java
+++ src/magic/ui/widget/StartGameButton.java
@@ -1,10 +1,13 @@
 package magic.ui.widget;
 
 import javax.swing.AbstractAction;
+import magic.translate.UiString;
 import magic.ui.screen.widget.MenuButton;
 
 @SuppressWarnings("serial")
 public class StartGameButton extends MenuButton {
+
+    private static final String _S1 = "Preparing game, please wait...";
 
     public StartGameButton(String text, AbstractAction action) {
         super(text, action);
@@ -12,5 +15,11 @@ public class StartGameButton extends MenuButton {
 
     public void setBusy(boolean b) {
         setEnabled(!b);
+    }
+
+    @Override
+    public void setEnabled(boolean b) {
+        super.setEnabled(b);
+        setToolTipText(b ? null : UiString.get(_S1));
     }
 }
