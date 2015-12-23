6e86221d78435359274e13ad175ab8a4e8d19ec9
melvinzhang <melvinzhang@gmail.com>
2015-12-23 18:17:45 +0800

    replace groovy code with card script

--- release/Magarena/scripts/That_Which_Was_Taken.groovy
+++ release/Magarena/scripts/That_Which_Was_Taken.groovy
@@ -1,38 +1,4 @@
-def choice = MagicTargetChoice.Positive("another target permanent")
-
 [
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Pump),
-        "+Counter"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicTapEvent(source),
-                new MagicPayManaCostEvent(source,"{4}")
-            ];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                choice,
-                MagicIndestructibleTargetPicker.create(),
-                this,
-                "PN puts a divinity counter on target permanent other than SN.\$"
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new ChangeCountersAction(it, MagicCounterType.Divinity, 1));
-            })
-        }
-    },
-    
     new MagicStatic(MagicLayer.Ability, PERMANENT) {
         @Override
         public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
@@ -42,5 +8,5 @@ def choice = MagicTargetChoice.Positive("another target permanent")
         public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
             return target.hasCounters(MagicCounterType.Divinity);
         }
-    },
+    }
 ]
