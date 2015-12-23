2438f3fa45f39080e22a49a36087007e3311304c
melvinzhang <melvinzhang@gmail.com>
2015-12-23 13:53:29 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Olivia_Voldaren.groovy
+++ release/Magarena/scripts/Olivia_Voldaren.groovy
@@ -1,38 +1,6 @@
 [
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
-        "Damage"
-    ) {
-
-        @Override
-        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [new MagicPayManaCostEvent(source, "{1}{R}")];
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                NegOther("target creature", source),
-                new MagicDamageTargetPicker(1),
-                this,
-                "SN deals 1 damage to another target creature\$. " +
-                "That creature becomes a Vampire in addition to its other types. " + 
-                "Put a +1/+1 counter on SN."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPermanent(game, {
-                game.doAction(new DealDamageAction(event.getSource(),it,1));
-                game.doAction(new AddStaticAction(it, MagicStatic.Vampire));
-                game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,1));
-            });
-        }
-    },
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
         "Control"
     ) {
 
