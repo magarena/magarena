85a221a7f6c3d822146147035129f98778a52f19
melvinzhang <melvinzhang@gmail.com>
2015-12-23 10:21:43 +0800

    bounce cost should be paid on resolution, to cater for case when selected bounce target is removed before resolution

--- release/Magarena/scripts/Faerie_Impostor.groovy
+++ release/Magarena/scripts/Faerie_Impostor.groovy
@@ -6,21 +6,23 @@ def choice = new MagicTargetChoice("another creature you control");
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
             return new MagicEvent(
                 permanent,
-                new MagicMayChoice(choice),
-                MagicBounceTargetPicker.create(),
+                new MagicMayChoice(),
                 this,
-                "PN may\$ return another creature he or she controls\$ to its owner's hand. " +
+                "PN may\$ return another creature he or she controls to its owner's hand. " +
                 "If PN doesn't, sacrifice SN."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isNo()) {
-                game.doAction(new SacrificeAction(event.getPermanent()));
+            final MagicEvent bounce = new MagicBounceChosenPermanentEvent(
+                event.getSource(),
+                event.getPlayer(),
+                choice
+            );
+            if (event.isYes() && bounce.isSatisfied()) {
+                game.addEvent(bounce);
             } else {
-                event.processTargetPermanent(game, {
-                    game.doAction(new RemoveFromPlayAction(it,MagicLocationType.OwnersHand));
-                });
+                game.doAction(new SacrificeAction(event.getPermanent()));
             }
         }
     }
