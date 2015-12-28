be5c935af70ee20329ea688e2940e19e24abd1d8
melvinzhang <melvinzhang@gmail.com>
2015-12-28 09:30:24 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Lim_D_l_s_Paladin.groovy
+++ release/Magarena/scripts/Lim_D_l_s_Paladin.groovy
@@ -1,26 +1,4 @@
 [
-    new AtYourUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return new MagicEvent(
-                permanent,
-                new MagicMayChoice("Discard a card?"),
-                this,
-                "PN may\$ discard a card. If PN doesn't, sacrifice SN and draw a card."
-            );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicEvent discard = new MagicDiscardEvent(event.getSource(), event.getPlayer());
-            if (event.isYes() && discard.isSatisfied()) {
-                game.addEvent(discard);
-            } else {
-                game.doAction(new SacrificeAction(event.getPermanent()));
-                game.doAction(new DrawAction(event.getPlayer()));
-            }
-        }
-    },
     new AttacksUnblockedTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent creature) {
--- release/Magarena/scripts/Ovinomancer.groovy
+++ release/Magarena/scripts/Ovinomancer.groovy
@@ -1,27 +1,4 @@
-def choice = new MagicTargetChoice("a basic land you control");
-
 [
-    new EntersBattlefieldTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                new MagicMayChoice("Return three basic lands you control to their owner's hand?"),
-                this,
-                "PN may\$ return three basic lands he or she controls to their owner's hand. If PN doesn't, sacrifice SN."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicEvent costEvent = new MagicRepeatedPermanentsEvent(event.getSource(), choice, 3, MagicChainEventFactory.Bounce);
-            if (event.isYes() && costEvent.isSatisfied()) {
-                game.addEvent(costEvent);
-            } else {
-                game.doAction(new SacrificeAction(event.getPermanent()));
-            }
-        }
-    },
-    
     new MagicPermanentActivation(
         new MagicActivationHints(MagicTiming.Removal),
         "Destroy"
