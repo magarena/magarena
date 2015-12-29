698a9ed595dd39b6af4ef9ca7710eccda7b77a8f
melvinzhang <melvinzhang@gmail.com>
2015-12-29 18:26:39 +0800

    add Thalia, Guardian of Thraben

---
+++ release/Magarena/scripts/Thalia__Guardian_of_Thraben.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.CostIncrease) {
+        @Override
+        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.isCreature() == false) {
+                return cost.increase(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
