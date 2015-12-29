8d6930f21916dfb136b85f7ddd4fb19dc9c9886d
melvinzhang <melvinzhang@gmail.com>
2015-12-29 21:58:05 +0800

    add High Seas

---
+++ release/Magarena/scripts/High_Seas.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.CostIncrease) {
+        @Override
+        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.isCreature() && 
+                (card.hasColor(MagicColor.Red) || card.hasColor(MagicColor.Green))) {
+                return cost.increase(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
