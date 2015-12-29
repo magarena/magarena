8d23ef7ca26284c03664b906214cf2242713d1d6
melvinzhang <melvinzhang@gmail.com>
2015-12-29 22:14:18 +0800

    add Aura of Silence

---
+++ release/Magarena/scripts/Aura_of_Silence.groovy
@@ -0,0 +1,13 @@
+[
+    new MagicStatic(MagicLayer.CostIncrease) {
+        @Override
+        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (source.isEnemy(card) && 
+                (card.isArtifact() || card.isEnchantment())) {
+                return cost.increase(MagicCostManaType.Generic, 2);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
