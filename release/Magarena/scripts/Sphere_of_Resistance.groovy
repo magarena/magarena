6b4cfcc0a677e9d1e7513887ddd6e41ccd3aa494
melvinzhang <melvinzhang@gmail.com>
2015-12-29 21:52:13 +0800

    add Sphere of Resistance

---
+++ release/Magarena/scripts/Sphere_of_Resistance.groovy
@@ -0,0 +1,8 @@
+[
+    new MagicStatic(MagicLayer.CostIncrease) {
+        @Override
+        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            return cost.increase(MagicCostManaType.Generic, 1);
+        }
+    }
+]
