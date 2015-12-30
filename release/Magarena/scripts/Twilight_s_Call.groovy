9b7a0a43eaabdeca8a5e314db9c7a17c51c3f11c
melvinzhang <melvinzhang@gmail.com>
2015-12-30 10:53:32 +0800

    add Twilight's Call

---
+++ release/Magarena/scripts/Twilight_s_Call.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicHandCastActivation(
+        new MagicActivationHints(MagicTiming.Token,true),
+        "Flash"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, "{6}{B}{B}")
+            ];
+        }
+    },
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Each player returns all creature cards from his or her graveyard to the battlefield."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.getAPNAP() each {
+                final MagicPlayer player ->
+                CREATURE_CARD_FROM_GRAVEYARD.filter(player) each {
+                    final MagicCard card ->
+                    game.doAction(new ReanimateAction(card, player));
+                }
+            }
+        }
+    }
+]
