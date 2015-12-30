9dc20b5b33ab2dae1b0c65d4822b7039f9df0a06
melvinzhang <melvinzhang@gmail.com>
2015-12-30 10:50:11 +0800

    add Breaking Wave

---
+++ release/Magarena/scripts/Breaking_Wave.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicHandCastActivation(
+        new MagicActivationHints(MagicTiming.Tapping,true),
+        "Flash"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+            return [
+                new MagicPayManaCostEvent(source, "{4}{U}{U}")
+            ];
+        }
+    },
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "Simultaneously untap all tapped creatures and tap all untapped creatures."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final List<MagicPermanent> tapped = TAPPED_CREATURE.filter(event);
+            final List<MagicPermanent> untapped = UNTAPPED_CREATURE.filter(event);
+            for (final MagicPermanent it : tapped) {
+                game.doAction(new UntapAction(it));
+            }
+            for (final MagicPermanent it : untapped) {
+                game.doAction(new TapAction(it));
+            }
+        }
+    }
+]
