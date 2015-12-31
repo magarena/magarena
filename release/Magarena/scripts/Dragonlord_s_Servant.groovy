516b59adfa72536259b4fdcb8fd993ecabae8762
melvinzhang <melvinzhang@gmail.com>
2015-12-31 13:15:28 +0800

    add Dragonlord's Servant

---
+++ release/Magarena/scripts/Dragonlord_s_Servant.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.CostReduction) {
+        @Override
+        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.hasSubType(MagicSubType.Dragon) && source.isFriend(card)) {
+                return cost.reduce(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
