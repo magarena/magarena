436592c12fad1fc3255246436f4fd496924e4bf1
melvinzhang <melvinzhang@gmail.com>
2015-12-31 13:09:32 +0800

    add Grand Arbiter Augustin IV

---
+++ release/Magarena/scripts/Grand_Arbiter_Augustin_IV.groovy
@@ -0,0 +1,32 @@
+[
+    new MagicStatic(MagicLayer.CostReduction) {
+        @Override
+        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.hasColor(MagicColor.White) && source.isFriend(card)) {
+                return cost.reduce(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    },
+    new MagicStatic(MagicLayer.CostReduction) {
+        @Override
+        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.hasColor(MagicColor.Blue) && source.isFriend(card)) {
+                return cost.reduce(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    },
+    new MagicStatic(MagicLayer.CostIncrease) {
+        @Override
+        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (source.isEnemy(card)) {
+                return cost.increase(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
