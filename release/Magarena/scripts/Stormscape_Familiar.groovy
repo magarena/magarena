674a58a7bec76fd7afc7d0c47ddf3a78e190a5a5
melvinzhang <melvinzhang@gmail.com>
2015-12-31 14:53:43 +0800

    add Stormscape Familiar

---
+++ release/Magarena/scripts/Stormscape_Familiar.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.CostReduction) {
+        @Override
+        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if ((card.hasColor(MagicColor.White) || card.hasColor(MagicColor.Black)) && source.isFriend(card)) {
+                return cost.reduce(MagicCostManaType.Generic, 1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
