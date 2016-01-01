3b09277fda657de3a0d6cce3c407742addd9524e
melvinzhang <melvinzhang@gmail.com>
2016-01-01 10:31:37 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Leviathan.groovy
+++ release/Magarena/scripts/Leviathan.groovy
@@ -20,28 +20,5 @@ def choice = new MagicTargetChoice("an Island to sacrifice");
                 game.doAction(new UntapAction(event.getPermanent()));
             }
         }
-    },
-    new AtBeginOfCombatTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer attackingPlayer) {
-            return permanent.isController(attackingPlayer) ?
-                new MagicEvent(
-                permanent,
-                new MagicMayChoice("Sacrifice two Islands?"),
-                this,
-                "PN may\$ sacrifice two Islands. If PN doesn't, SN can't attack."
-            ):
-            MagicEvent.NONE;
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes() && event.getPlayer().getNrOfPermanents(MagicSubType.Island) >= 2) {
-                game.addEvent(new MagicSacrificePermanentEvent(event.getPermanent(),event.getPlayer(),choice));
-                game.addEvent(new MagicSacrificePermanentEvent(event.getPermanent(),event.getPlayer(),choice));
-            } else {
-                game.doAction(new GainAbilityAction(event.getPermanent(), MagicAbility.CannotAttack));
-            }
-        }
     }
 ]
