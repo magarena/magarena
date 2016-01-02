94ea47914a167d072fdd92c4a356f88ce31500b6
lodici <lodici@github>
2016-01-02 22:21:57 +0000

    move remaining SoundEffects code into MagicSound; Delete SoundEffects.

--- src/magic/ui/MagicSound.java
+++ src/magic/ui/MagicSound.java
@@ -1,10 +1,18 @@
 package magic.ui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.BooleanControl;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
 import magic.data.GeneralConfig;
-import magic.data.SoundEffects;
 import magic.model.MagicGame;
 import magic.utility.MagicResources;
 
@@ -24,6 +32,12 @@ public enum MagicSound {
 
     private static final GeneralConfig config = GeneralConfig.getInstance();
     private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private static volatile Clip clip;
+    private static final LineListener closer = (event) -> {
+        if (event.getType() == LineEvent.Type.STOP) {
+            event.getLine().close();
+        }
+    };
 
     private final URL soundUrl;
 
@@ -32,28 +46,79 @@ public enum MagicSound {
     }
 
     /**
-     * plays sound at a volume of between 0 and 100%.
+     * Plays UI sound effect at given volume.
+     *
+     * @param volPercent : volume of sound clip between 0 and 100 percent.
      */
     public void play(int volume) {
         if (config.isUiSound()) {
             executor.submit(() -> {
-                SoundEffects.playSound(soundUrl, volume);
+                playSound(soundUrl, volume);
             });
         }
     }
 
+    /**
+     * Plays UI sound effect at volume specified in settings.
+     */
     public void play() {
         play(config.getUiSoundVolume());
     }
+
+    /**
+     * Plays gameplay sound effect at volume specified in settings.
+     */
+    public void play(MagicGame game) {
+        if (game.isReal() && config.isSound()) {
+            play();
+        }
+    }
+
 
     public static void shutdown() {
         executor.shutdown();
     }
 
-    public void play(final MagicGame game) {
-        if (game.isReal() && config.isSound()) {
-            MagicSound.this.play();
+    private static void setVolume(final Clip aClip, int volPercent) {
+
+        BooleanControl muteControl = (BooleanControl) aClip.getControl(BooleanControl.Type.MUTE);
+        muteControl.setValue(volPercent == 0);
+        if (volPercent == 0)
+            return;
+
+        if (volPercent > 0 && volPercent <= 100) {
+            FloatControl gainControl = (FloatControl) aClip.getControl(FloatControl.Type.MASTER_GAIN);
+            double gain = volPercent / 100D; // number between 0 and 1 (loudest)
+            float dB = (float) (Math.log(gain) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+        } else {
+            throw new IndexOutOfBoundsException("Valid volume range is 0 to 100 (percent).");
         }
+    }
+
+    private static void playClip(AudioInputStream ins, int volPercent) throws IOException, LineUnavailableException {
+
+        if (clip != null && (clip.isRunning() || clip.isActive())) {
+            clip.loop(0);
+        }
+
+        clip = AudioSystem.getClip();
+        clip.addLineListener(closer);
+        clip.open(ins);
+        setVolume(clip, volPercent);
+        clip.start();
+    }
+
+    private static void playSound(URL url, int volPercent) {
+        try (final AudioInputStream ins = AudioSystem.getAudioInputStream(url)) {
+            playClip(ins, volPercent);
+        } catch (Exception ex) {
+            System.err.println("WARNING. Unable to play clip " + url.toExternalForm() + ", " + ex.getMessage());
+            // switch off sound for current session but restore on restart.
+            GeneralConfig.getInstance().setIsUiSound(false);
+            GeneralConfig.getInstance().setSound(false);
+        }
+
     }
 
 }
