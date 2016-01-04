43f0b598fd2c73e64d18a06b9727f0ad226b481b
melvinzhang <melvinzhang@gmail.com>
2016-01-04 13:33:53 +0800

    rewrite using delayed trigger

--- release/Magarena/scripts/Rukh_Egg.groovy
+++ release/Magarena/scripts/Rukh_Egg.groovy
@@ -1,3 +1,25 @@
+def DelayedTrigger = {
+    final MagicSource staleSource, final MagicPlayer stalePlayer ->
+    return new AtEndOfTurnTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer eotPlayer) {
+            game.addDelayedAction(new RemoveTriggerAction(this));
+            return new MagicEvent(
+                game.createDelayedSource(staleSource, stalePlayer),
+                this,
+                "PN puts a 4/4 red Bird creature token with flying onto the battlefield."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new PlayTokenAction(
+                event.getPlayer(),
+                CardDefinitions.getToken("4/4 red Bird creature token with flying")
+            ))
+        }
+    }
+}
+
 [
     new ThisDiesTrigger() {
         @Override
@@ -10,19 +32,10 @@
         }
 
         @Override
-        public void executeEvent(final MagicGame outerGame, final MagicEvent event) {
-            //insert trigger to act at the beginning of the next end step
-            outerGame.doAction(new AddTriggerAction(new AtEndOfTurnTrigger() {
-                @Override
-                public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer eotPlayer) {
-                    game.doAction(new PlayTokenAction(
-                        game.getPlayer(event.getPlayer().getIndex()),
-                        CardDefinitions.getToken("4/4 red Bird creature token with flying")
-                    ));
-                    game.addDelayedAction(new RemoveTriggerAction(this));
-                    return MagicEvent.NONE;
-                }
-            }));
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new AddTriggerAction(
+                DelayedTrigger(event.getSource(), event.getPlayer())
+            ));
         }
     }
 ]
