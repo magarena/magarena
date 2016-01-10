21f0a2e72d043a481d14ab10f5dfc3692501d62f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-01-10 15:41:08 +0000

    add TestSacMana to test issue #639

---
+++ src/magic/test/TestSacMana.java
@@ -0,0 +1,43 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestSacMana extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+// Casting Algae Gharial should prompt for which land to sacrifice for green
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(P,"Abandoned Outpost",false,1);
+        createPermanent(P,"Bog Wreckage",false,1);
+        createPermanent(P,"1/1 colorless Eldrazi Scion creature token");
+        createPermanent(P,"0/1 colorless Eldrazi Spawn creature token");
+        addToHand(P,"Steel Wall",1);
+        addToHand(P,"Armadillo Cloak",1);
+        addToHand(P,"Llanowar Elves",1);
+        addToHand(P,"Aesthir Glider",1);
+        addToHand(P,"Algae Gharial",1);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(P,"Swamp",4);
+        createPermanent(P,"Plains",2);
+
+        return game;
+
+    }
+
+}
+
