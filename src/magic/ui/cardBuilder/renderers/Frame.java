d2d62272d9e3ce50092a2d52df2975029ccd57d0
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-01-16 02:07:58 +0000

    correct Frame.getColorOrder() for color pairings

--- src/magic/ui/cardBuilder/renderers/Frame.java
+++ src/magic/ui/cardBuilder/renderers/Frame.java
@@ -333,30 +333,22 @@ public class Frame {
     static List<MagicColor> getColorOrder(List<MagicColor> colors) {
         //Color order
         List<MagicColor> orderedColors = new ArrayList<>(colors);
-        //Non-color order pairings
+        //Ensure White is only first when with black and blue - prevents WR WG orders
         if (orderedColors.get(0) == MagicColor.White) {
             if (orderedColors.get(1) != MagicColor.Blue && orderedColors.get(1) != MagicColor.Black) {
                 Collections.swap(orderedColors, 0, 1);
             }
         }
+        //Ensure Blue is only first when with black and red - prevents UG order
         if (orderedColors.get(0) == MagicColor.Blue) {
-            if (orderedColors.get(1) != MagicColor.Black) {
+            if (orderedColors.get(1) != MagicColor.Black && orderedColors.get(1) != MagicColor.Red) {
                 Collections.swap(orderedColors, 0, 1);
             }
         }
-        if (orderedColors.get(0) == MagicColor.Black) {
-            if (orderedColors.get(1) != MagicColor.Red && orderedColors.get(1) != MagicColor.Green) {
-                Collections.swap(orderedColors, 0, 1);
-            }
-        }
-        if (orderedColors.get(0) == MagicColor.Red) {
-            if (orderedColors.get(1) != MagicColor.White && orderedColors.get(1) != MagicColor.Blue && orderedColors.get(1) != MagicColor.Green) {
-                Collections.swap(orderedColors, 0, 1);
-            }
-        }
+        //Ensure Green is only first with white and blue - prevents GR order
         if (orderedColors.get(0) == MagicColor.Green) {
             if (orderedColors.get(1) != MagicColor.White && orderedColors.get(1) != MagicColor.Blue) {
-                Collections.swap(orderedColors, 0, 1);
+                Collections.swap(orderedColors, 0 ,1);
             }
         }
         return orderedColors;
