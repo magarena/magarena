112489f4f2ae52325d0350f014d45885c9cb6889
lodici <lodici123@gmail.com>
2016-01-17 14:13:00 +0000

    Consolidate TransparentImagePanel code into CardViewer; remove TransparentImagePanel.

--- src/magic/ui/duel/viewer/CardViewer.java
+++ src/magic/ui/duel/viewer/CardViewer.java
@@ -1,7 +1,7 @@
 package magic.ui.duel.viewer;
 
-import java.awt.BorderLayout;
 import java.awt.Cursor;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -13,21 +13,17 @@ import magic.ui.CachedImagesProvider;
 import magic.ui.MagicImages;
 import magic.ui.cardtable.ICardSelectionListener;
 import magic.ui.utility.GraphicsUtils;
-import magic.ui.widget.TransparentImagePanel;
 
 @SuppressWarnings("serial")
 public class CardViewer extends JPanel implements ICardSelectionListener {
 
-    private final TransparentImagePanel cardPanel = new TransparentImagePanel();
+    private Image thisImage;
     private MagicCardDefinition thisCard;
     private boolean isSwitchedAspect = false;
 
     public CardViewer() {
         setCard(MagicCardDefinition.UNKNOWN);
         setTransformCardListener();
-        setOpaque(false);
-        this.setLayout(new BorderLayout());
-        add(cardPanel,BorderLayout.CENTER);
     }
 
     private void setTransformCardListener() {
@@ -74,30 +70,37 @@ public class CardViewer extends JPanel implements ICardSelectionListener {
 
         if (aCard == null) {
             thisCard = MagicCardDefinition.UNKNOWN;
-            setCardImage(MagicImages.getMissingCardImage());
+            setImage(MagicImages.getMissingCardImage());
 
         } else if (aCard != thisCard) {
             thisCard = aCard;
-            final BufferedImage cardImage;
-            cardImage =  CachedImagesProvider.getInstance().getImage(aCard, false);
+            final BufferedImage cardImage = CachedImagesProvider.getInstance().getImage(aCard, false);
             if (aCard.isInvalid() && cardImage != MagicImages.getMissingCardImage()) {
-                setCardImage(GraphicsUtils.getGreyScaleImage(cardImage));
+                setImage(GraphicsUtils.getGreyScaleImage(cardImage));
             } else {
-                setCardImage(cardImage);
+                setImage(cardImage);
             }
         }
     }
-
-    private void setCardImage(final Image newImage) {
-        cardPanel.setImage(newImage);
-        repaint();
-    }
-
 
     @Override
     public void newCardSelected(final MagicCardDefinition card) {
         SwingUtilities.invokeLater(() -> {
             setCard(card);
         });
+    }
+    
+    private void setImage(final Image image) {
+        this.thisImage = image;
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(final Graphics g) {
+        if (thisImage != null) {
+            g.drawImage(thisImage, 0, 0, null);
+        } else {
+            super.paintComponent(g);
+        }
     }
 }
