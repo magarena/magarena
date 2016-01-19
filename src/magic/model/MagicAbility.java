f6e2256c4ba54c862e946913638f7bc578b9c9c6
melvinzhang <melvinzhang@gmail.com>
2016-01-19 08:32:43 +0800

    split EntersSurgeEffect out of EntersKickedEffect, fixes #645

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -472,11 +472,16 @@ public enum MagicAbility {
             ));
         }
     },
-    EntersKickedEffect("When SN enters the battlefield, if (it was kicked|its surge cost was paid), " + ARG.EFFECT, 10) {
+    EntersKickedEffect("When SN enters the battlefield, if it was kicked, " + ARG.EFFECT, 10) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
             card.add(EntersBattlefieldTrigger.createKicked(
                 MagicRuleEventAction.create(ARG.effect(arg))
             ));
+        }
+    },
+    EntersSurgeEffect("When SN enters the battlefield, if its surge cost was paid, " + ARG.EFFECT, 10) {
+        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
+            EntersKickedEffect.addAbilityImpl(card, arg);
         }
     },
     EntersEffect("When SN enters the battlefield, " + ARG.EFFECT, 10) {
