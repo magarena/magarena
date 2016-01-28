ffbd58d39989624e274dc3d9dd3650ed55475612
melvinzhang <melvinzhang@gmail.com>
2016-01-28 22:55:35 +0800

    avoid storing data in MagicEvent, create a new MagicEventAction for effect

--- src/magic/model/event/MagicHauntEvent.java
+++ src/magic/model/event/MagicHauntEvent.java
@@ -13,39 +13,39 @@ import magic.model.stack.MagicCardOnStack;
 import magic.model.trigger.ThisDiesTrigger;
 
 public class MagicHauntEvent extends MagicEvent {
-    static MagicSourceEvent sourceEvent;
 
-    public MagicHauntEvent(final MagicPermanent permanent, final MagicSourceEvent aSourceEvent) {
-        this(permanent.getCard(), permanent.getController(), aSourceEvent);
+    public MagicHauntEvent(final MagicPermanent permanent, final MagicSourceEvent effect) {
+        this(permanent.getCard(), permanent.getController(), effect);
     }
 
-    public MagicHauntEvent(final MagicCardOnStack cardOnStack, final MagicSourceEvent aSourceEvent) {
-        this(cardOnStack.getCard(), cardOnStack.getController(), aSourceEvent);
+    public MagicHauntEvent(final MagicCardOnStack cardOnStack, final MagicSourceEvent effect) {
+        this(cardOnStack.getCard(), cardOnStack.getController(), effect);
     }
 
-    private MagicHauntEvent(final MagicCard card, final MagicPlayer player, final MagicSourceEvent aSourceEvent) {
+    private MagicHauntEvent(final MagicCard card, final MagicPlayer player, final MagicSourceEvent effect) {
         super(
             card,
             player,
             MagicTargetChoice.TARGET_CREATURE,
-            EVENT_ACTION,
+            EVENT_ACTION(effect),
             "Exile SN haunting target creature$."
         );
-        sourceEvent = aSourceEvent;
     }
 
-    private static final MagicEventAction EVENT_ACTION = (final MagicGame game, final MagicEvent event) -> {
-        final MagicCard card = event.getCard();
-        if (card.isInGraveyard()) {
-            event.processTargetPermanent(game, creatureToHaunt -> {
-                // only exile if valid target
-                game.doAction(new RemoveCardAction(card, MagicLocationType.Graveyard));
-                game.doAction(new MoveCardAction(card, MagicLocationType.Graveyard, MagicLocationType.Exile));
-                game.doAction(new AddTriggerAction(
-                    creatureToHaunt,
-                    ThisDiesTrigger.createDelayed(event.getSource(), event.getPlayer(), sourceEvent)
-                ));
-            });
-        }
-    };
+    private static final MagicEventAction EVENT_ACTION(final MagicSourceEvent effect) {
+        return (final MagicGame game, final MagicEvent event) -> {
+            final MagicCard card = event.getCard();
+            if (card.isInGraveyard()) {
+                event.processTargetPermanent(game, creatureToHaunt -> {
+                    // only exile if valid target
+                    game.doAction(new RemoveCardAction(card, MagicLocationType.Graveyard));
+                    game.doAction(new MoveCardAction(card, MagicLocationType.Graveyard, MagicLocationType.Exile));
+                    game.doAction(new AddTriggerAction(
+                        creatureToHaunt,
+                        ThisDiesTrigger.createDelayed(event.getSource(), event.getPlayer(), effect)
+                    ));
+                });
+            }
+        };
+    }
 }
