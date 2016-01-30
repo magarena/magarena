ba0d7565e52143a7ebdacac584313a7f51a14bae
lodici <lodici123@gmail.com>
2016-01-30 18:25:29 +0000

    fix max size of TitleBar.

--- src/magic/ui/widget/TitleBar.java
+++ src/magic/ui/widget/TitleBar.java
@@ -3,6 +3,7 @@ package magic.ui.widget;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import magic.ui.ScreenController;
 import magic.ui.theme.Theme;
 import magic.ui.utility.MagicStyle;
 import net.miginfocom.swing.MigLayout;
@@ -19,6 +20,7 @@ public class TitleBar extends JPanel {
 
         setPreferredSize(new Dimension(getPreferredSize().width, 22));
         setMinimumSize(getPreferredSize());
+        setMaximumSize(new Dimension(ScreenController.getMainFrame().getSize().width, 22));
 
         setLayout(new MigLayout("insets 0 4 0 4", "", "grow, fill"));
         add(label, "pushx");
