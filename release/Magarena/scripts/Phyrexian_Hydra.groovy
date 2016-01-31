eac8bf68af4e907baa7986118c5b1c708c9a06ad
melvinzhang <melvinzhang@gmail.com>
2016-01-31 09:06:49 +0800

    replace lambda with executeEvent to reduce nesting and avoid using variables from outer context

--- release/Magarena/scripts/Phyrexian_Hydra.groovy
+++ release/Magarena/scripts/Phyrexian_Hydra.groovy
@@ -2,26 +2,23 @@
     new IfDamageWouldBeDealtTrigger(MagicTrigger.REPLACE_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
-            int amount = 0;
-
-            if (damage.getTarget() == permanent) {
-                amount = damage.prevent();
-            }
-
+            final int amount = (damage.getTarget() == permanent) ? damage.prevent() : 0;
             return amount > 0 ?
                 new MagicEvent(
                     permanent,
-                    {
-                        final MagicGame G, final MagicEvent E ->
-                        G.doAction(new ChangeCountersAction(
-                            E.getPermanent(),
-                            MagicCounterType.MinusOne,
-                            amount
-                        ));
-                    },
-                    "Prevent ${amount} damage and put ${amount} -1/-1 counters on SN"
+                    amount,
+                    this,
+                    "Prevent RN damage and put RN -1/-1 counters on SN."
                 ):
                 MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeCountersAction(
+                event.getPermanent(),
+                MagicCounterType.MinusOne,
+                event.getRefInt()
+            ));
         }
     }
 ]
