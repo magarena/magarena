1cfdbfed7d9018fd652111e4042d4669ea45138a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-05 21:53:36 +0000

    add Necromancer's Magemark

---
+++ release/Magarena/scripts/Necromancer_s_Magemark.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicStatic(MagicLayer.ModPT, CREATURE_YOU_CONTROL) {
+        @Override
+        public void modPowerToughness(
+            final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1, 1);
+        }
+
+        @Override
+        public boolean condition(final MagicGame game, final MagicPermanent source, final MagicPermanent target) {
+            return target.isEnchanted();
+        }
+    },
+    new WouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MoveCardAction act) {
+            final MagicPermanent dying = act.permanent;
+            if (permanent.isFriend(dying) &&
+                dying.isCreature() &&
+                dying.isEnchanted() &&
+                act.to(MagicLocationType.Graveyard) &&
+                act.from(MagicLocationType.Battlefield)) {
+                act.setToLocation(MagicLocationType.OwnersHand);
+                game.logAppendMessage(permanent.getController(), "${dying.getName()} is put into ${dying.getController()}'s hand.");
+
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
