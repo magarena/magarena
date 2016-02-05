645f7c3b5938f355dc0fa26cbcd6e66dedd2616a
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-05 20:46:09 +0000

    add Guardian's Magemark

---
+++ release/Magarena/scripts/Guardian_s_Magemark.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.ModPT, CREATURE_YOU_CONTROL) {
+        @Override
+         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return target.isEnchanted();
+        }
+    }
+]
