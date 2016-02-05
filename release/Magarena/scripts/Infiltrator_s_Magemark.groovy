e63408c039aa275169aae09b220019189a7c6006
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-05 20:38:43 +0000

    add Infiltrator's Magemark

---
+++ release/Magarena/scripts/Infiltrator_s_Magemark.groovy
@@ -0,0 +1,23 @@
+def EXCEPT_BY_WALLS = MagicAbility.getAbilityList("can't be blocked except by creature with defender");
+[
+    new MagicStatic(MagicLayer.Ability, CREATURE_YOU_CONTROL) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(EXCEPT_BY_WALLS);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return target.isEnchanted();
+        }
+    },
+    new MagicStatic(MagicLayer.ModPT, CREATURE_YOU_CONTROL) {
+        @Override
+         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(1,1);
+        }
+        @Override
+        public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return target.isEnchanted();
+        }
+    }
+]
