7238fe163c23d3e437adf7a154e66bd33473332c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-12 18:04:12 +0000

    use LeavesBattlefieldTrigger instead of WouldBeMovedTrigger for Gravebane Zombie

--- release/Magarena/scripts/Gravebane_Zombie.groovy
+++ release/Magarena/scripts/Gravebane_Zombie.groovy
@@ -1,11 +1,9 @@
 [
-    new WouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
+    new LeavesBattlefieldTrigger(MagicTrigger.REPLACEMENT) {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MoveCardAction act) {
-            final MagicPermanent dying = act.permanent;
-            if (permanent == dying &&
-                act.to(MagicLocationType.Graveyard) &&
-                act.from(MagicLocationType.Battlefield)) {
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final RemoveFromPlayAction act) {
+            final MagicPermanent dying = act.getPermanent();
+            if (permanent == dying && act.to(MagicLocationType.Graveyard)) {
                 act.setToLocation(MagicLocationType.TopOfOwnersLibrary);
                 game.logAppendMessage(permanent.getController(), "${dying.getName()} is put on top of ${dying.getOwner()}'s library.");
             }
