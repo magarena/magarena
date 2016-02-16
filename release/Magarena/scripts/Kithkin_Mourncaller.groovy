905266a461039c9be86c5277dd6a53bc461d11c3
melvinzhang <melvinzhang@gmail.com>
2016-02-16 09:37:07 +0800

    replace groovy code with MagicRuleEventAction.create, fixes crash due to missing ref card

--- release/Magarena/scripts/Kithkin_Mourncaller.groovy
+++ release/Magarena/scripts/Kithkin_Mourncaller.groovy
@@ -1,3 +1,5 @@
+def effect = MagicRuleEventAction.create("You may draw a card.");
+
 [
     new OtherDiesTrigger() {
         @Override
@@ -5,23 +7,7 @@
             return (otherPermanent.isAttacking() &&
                     (otherPermanent.hasSubType(MagicSubType.Elf) || otherPermanent.hasSubType(MagicSubType.Kithkin)) &&
                     otherPermanent.isOwner(permanent.getController())) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(
-                        MagicSimpleMayChoice.DRAW_CARDS,
-                        1,
-                        MagicSimpleMayChoice.DEFAULT_NONE
-                    ),
-                    this,
-                    "PN may\$ draw a card."
-                ) :
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes() && event.getRefCard().isInGraveyard()) {
-                game.doAction(new DrawAction(event.getPlayer()));
-            }
+                effect.getEvent(permanent) : MagicEvent.NONE;
         }
     }
 ]
