633f6fc5b227e4eed2128df77878b8e5f5feb66f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-17 19:00:42 +0000

    add Mindswipe

---
+++ release/Magarena/scripts/Mindswipe.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_SPELL,
+                payedCost.getX(),
+                this,
+                "Counter target spell\$ unless its controller pays {X}. "+
+                "SN deals X damage to that spell's controller. (X=RN)"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                final int amount = event.getRefInt();
+                game.addEvent(new MagicCounterUnlessEvent(
+                    event.getSource(),
+                    it,
+                    MagicManaCost.create("{"+amount+"}")
+                ));
+                game.doAction(new DealDamageAction(event.getSource(), it.getController(), amount));
+            });
+        }
+    }
+]
