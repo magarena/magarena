8c65d887fadad722035bcf6cb3debbc3d8ef041e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-17 19:16:49 +0000

    add Spell Syphon

---
+++ release/Magarena/scripts/Spell_Syphon.groovy
@@ -0,0 +1,26 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_SPELL,
+                payedCost.getX(),
+                this,
+                "Counter target spell\$ unless its controller pays {1} for each blue permanent you control."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                int amount = BLUE_PERMANENT_YOU_CONTROL.filter(event).size();
+                game.logAppendValue(event.getPlayer(), amount);
+                game.addEvent(new MagicCounterUnlessEvent(
+                    event.getSource(),
+                    it,
+                    MagicManaCost.create("{"+amount+"}")
+                ));
+            });
+        }
+    }
+]
