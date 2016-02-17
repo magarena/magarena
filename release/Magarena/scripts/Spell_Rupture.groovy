28b47cdd4939689a75638a7325f9a2f9c87105b0
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-17 19:04:20 +0000

    add Spell Rupture

---
+++ release/Magarena/scripts/Spell_Rupture.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_SPELL,
+                payedCost.getX(),
+                this,
+                "Counter target spell\$ unless its controller pays {X}, "+
+                "where X is the greatest power among creatures PN controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                int power = 0;
+                CREATURE_YOU_CONTROL.filter(event) each {
+                    power = Math.max(power, it.getPower());
+                }
+                game.logAppendValue(event.getPlayer(), power);
+                game.addEvent(new MagicCounterUnlessEvent(
+                    event.getSource(),
+                    it,
+                    MagicManaCost.create("{"+power+"}")
+                ));
+            });
+        }
+    }
+]
