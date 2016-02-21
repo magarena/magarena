1936304b56f2914cc1b83d09592187125c443666
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-21 13:13:19 +0000

    add Abduction

---
+++ release/Magarena/scripts/Abduction.groovy
@@ -0,0 +1,25 @@
+[
+    new OtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
+            final MagicCard diedCard = died.getCard();
+            return (permanent.getEnchantedPermanent() == died) ?
+                new MagicEvent(
+                    permanent,
+                    diedCard,
+                    this,
+                    "Return RN to the battlefield under its owner's control."
+                ):
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard died = event.getRefCard();
+            game.doAction(new ReturnCardAction(
+                MagicLocationType.Graveyard,
+                died,
+                died.getOwner()
+            ));
+        }
+    }
+]
