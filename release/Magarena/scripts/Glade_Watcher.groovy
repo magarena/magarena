d63827bc6724d7095fd97e09ba02508af3632fba
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-21 16:23:08 +0000

    add Glade Watcher

---
+++ release/Magarena/scripts/Glade_Watcher.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPermanentActivation(
+        [MagicCondition.FORMIDABLE],
+        new MagicActivationHints(MagicTiming.Pump),
+        "Attack"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{G}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "SN can attack this turn as though it didn't have defender."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new GainAbilityAction(event.getPermanent(), MagicAbility.CanAttackWithDefender));
+        }
+    }
+]
