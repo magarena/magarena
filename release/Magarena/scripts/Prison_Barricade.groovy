21930f17d6900e226199c653b9e80d947614400d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-21 16:05:09 +0000

    add Prison Barricade

---
+++ release/Magarena/scripts/Prison_Barricade.groovy
@@ -0,0 +1,12 @@
+[
+    new EntersWithCounterTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            if (payedCost.isKicked()) {
+                game.doAction(new ChangeCountersAction(permanent, MagicCounterType.PlusOne, 1));
+                game.doAction(new GainAbilityAction(permanent, MagicAbility.CanAttackWithDefender, MagicStatic.Forever));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
