913cc9accd3140dbd942aa713a594f5d8ad69efe
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-21 12:03:09 +0000

    add TestCombatControl for #707

---
+++ src/magic/test/TestCombatControl.java
@@ -0,0 +1,33 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestCombatControl extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(1);
+        addToLibrary(P, "Island", 10);
+        createPermanent(P,"Island", 7);
+        createPermanent(P, "Æther Spellbomb");
+        addToHand(P,"Illusory Gains");
+
+        P = opponent;
+
+        P.setLife(1);
+        addToLibrary(P, "Island", 20);
+        createPermanent(P, "Ætherplasm");
+        createPermanent(P, "Dryad Arbor");
+        addToHand(P, "Grizzly Bears");
+        return game;
+    }
+}
