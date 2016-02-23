b7d32a4b019bbc4f3b91a4c88da0b7e2faa65eb0
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-23 16:30:43 +0000

    add Favorable Destiny

---
+++ release/Magarena/scripts/Favorable_Destiny.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicStatic(MagicLayer.ModPT) {
+        @Override
+        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
+            pt.add(2,2);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.White);
+        }
+    },
+    
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(MagicAbility.Shroud, flags);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target) && CREATURE_YOU_CONTROL.except(target).filter(target.getController()).size()>=1;
+        }
+    }
+]
