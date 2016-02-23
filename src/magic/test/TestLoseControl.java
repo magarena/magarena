d1cf1f33d8e9e2442ac678d7d228eef609ea03b5
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-23 15:02:05 +0000

    add TestLoseControl for #714

---
+++ src/magic/test/TestLoseControl.java
@@ -0,0 +1,34 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestLoseControl extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(P,"Forest",5);
+        createPermanent(P,"Island",4);
+        addToHand(P,"Sower of Temptation",1);
+
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Forest", 10);
+        createPermanent(P,"Thelonite Druid");
+        createPermanent(P,"Forest",1);
+
+        return game;
+    }
+}
