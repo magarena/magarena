b269762ed656f78bcbd30208f67a9f820bc12bdb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-26 16:05:54 +0000

    add Sunbond

---
+++ release/Magarena/scripts/Sunbond.groovy
@@ -0,0 +1,32 @@
+def trigger = new LifeIsGainedTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicLifeChangeTriggerData lifeChange) {
+            final int amount = lifeChange.amount;
+            return permanent.isController(lifeChange.player) ?
+                new MagicEvent(
+                    permanent,
+                    amount,
+                    this,
+                    amount > 1 ?
+                        "PN puts " + amount + " +1/+1 counters on SN." :
+                        "PN puts a +1/+1 counter on SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,event.getRefInt()));
+        }
+    }
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(trigger);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
