db41dd6480b8f93350a882d1a6466a5e1267bf46
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-26 14:08:33 +0000

    add Viridian Harvest

---
+++ release/Magarena/scripts/Viridian_Harvest.groovy
@@ -0,0 +1,19 @@
+[
+    new LeavesBattlefieldTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final RemoveFromPlayAction act) {
+            return act.getPermanent() == permanent.getEnchantedPermanent() &&
+               (act.to(MagicLocationType.Graveyard) || act.to(MagicLocationType.OpponentsGraveyard)) ? 
+                 new MagicEvent(
+                    permanent,
+                    this,
+                    "PN gains 6 life."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeLifeAction(event.getPlayer(), 6));
+        }
+    }
+]
