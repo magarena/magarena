2243631d4048e58d9e67b89668a114ba29f09c3e
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-26 01:01:33 +0000

    add Helm of Possession

---
+++ release/Magarena/scripts/Helm_of_Possession.groovy
@@ -0,0 +1,40 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Removal),
+        "Control"
+    ) {
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayManaCostEvent(source, "{2}"),
+                new MagicSacrificePermanentEvent(source, SACRIFICE_CREATURE)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_CREATURE,
+                MagicExileTargetPicker.create(),
+                this,
+                "PN gains control of target creature\$ for as long as PN controls SN and SN remains tapped."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new AddStaticAction(
+                    event.getPermanent(),
+                    MagicStatic.ControlAsLongAsYouControlSourceAndSourceIsTapped(
+                        event.getPlayer(),
+                        it
+                    )
+                ));
+            });
+        }
+    }
+]
