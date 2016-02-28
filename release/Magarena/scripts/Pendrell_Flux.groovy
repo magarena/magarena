49229e15640597fdd738ec38f4091b4e917cefda
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-02-28 23:55:47 +0000

    add Pendrell Flux

---
+++ release/Magarena/scripts/Pendrell_Flux.groovy
@@ -0,0 +1,33 @@
+def ability = new AtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(
+                    "Pay the mana cost?",
+                    new MagicPayManaCostChoice(permanent.getCardDefinition().getCost())
+                ),
+                this,
+                "PN sacrifices SN unless he or she pays its mana cost.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isNo()) {
+                game.doAction(new SacrificeAction(event.getPermanent()));
+            }
+        }
+    }
+
+[
+    new MagicStatic(MagicLayer.Ability) {
+        @Override
+        public void modAbilityFlags(final MagicPermanent source, final MagicPermanent permanent, final Set<MagicAbility> flags) {
+            permanent.addAbility(ability);
+        }
+        @Override
+        public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+            return MagicStatic.acceptLinked(game, source, target);
+        }
+    }
+]
