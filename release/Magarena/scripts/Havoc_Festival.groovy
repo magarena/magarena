b250c8651a859cf030f10f98e3ed444e46123498
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-03-07 13:32:07 +0000

    add Havoc Festival

---
+++ release/Magarena/scripts/Havoc_Festival.groovy
@@ -0,0 +1,18 @@
+[
+    new AtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                upkeepPlayer,
+                this,
+                "PN loses half his or her life, rounded up."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new ChangeLifeAction(player,-(player.getLife()+1)/2));
+        }
+    }
+]
