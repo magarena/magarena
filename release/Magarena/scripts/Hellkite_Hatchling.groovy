c920b58f0ff3c45e6cde9328001c62deb933d79f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-03-07 18:32:02 +0000

    add Hellkite Hatchling

---
+++ release/Magarena/scripts/Hellkite_Hatchling.groovy
@@ -0,0 +1,38 @@
+[
+    new EntersBattlefieldTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            final MagicTargetChoice targetChoice=new MagicTargetChoice(
+                CREATURE_YOU_CONTROL.except(permanent),
+                MagicTargetHint.None,
+                "a creature other than "+permanent+" to sacrifice"
+            );
+            return permanent.getController().getNrOfPermanents(MagicType.Creature) > 1 ?
+                new MagicEvent(
+                    permanent,
+                    new MagicMayChoice(targetChoice),
+                    MagicSacrificeTargetPicker.create(),
+                    this,
+                    "PN may\$ sacrifice a creature\$ to SN."
+                ):
+                MagicEvent.NONE;
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                final MagicPermanent permanent=event.getPermanent();
+                event.processTargetPermanent(game, {
+                    game.doAction(new SacrificeAction(it));
+                    game.doAction(new ChangeCountersAction(permanent,MagicCounterType.PlusOne, 1));
+                    game.doAction(new GainAbilityAction(permanent, MagicAbility.Flying, MagicStatic.Forever));
+                    game.doAction(new GainAbilityAction(permanent, MagicAbility.Trample, MagicStatic.Forever));
+                    final MagicEvent newEvent=executeTrigger(game, permanent, MagicPayedCost.NO_COST);
+                    if (newEvent.isValid()) {
+                        game.addEvent(newEvent);
+                    }
+                });
+            }
+        }
+    }
+]
