4cdc6699699c47c876f938cc02e77cefe786434c
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-03-19 18:09:09 +0000

    add Endbringer

---
+++ release/Magarena/scripts/Endbringer.groovy
@@ -0,0 +1,18 @@
+[
+    new AtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return upkeepPlayer != permanent.getController() ? 
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN untaps SN."
+                ) :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game,final MagicEvent event) {
+            game.doAction(new UntapAction(event.getPermanent()));
+        }
+    }
+]
