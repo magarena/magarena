6bf29c32c02aa23c5a98accd90dd7e00bce5c9bd
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-03-21 17:26:21 +0000

    add TestSunbringer re #734

---
+++ src/magic/test/TestSunbringer.java
@@ -0,0 +1,36 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+
+class TestSunbringer extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(1);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(P,"Plains",10);
+        createPermanent(P, "Chromatic Lantern");
+        createPermanent(P, "Llanowar Elves");
+        addToHand(P,"Clockwork Beetle");
+        addToHand(P, "Sunbringer's Touch");
+        addToHand(P, "Plains", 5);
+
+        P = opponent;
+
+        P.setLife(20);
+        addToLibrary(P, "Plains", 10);
+        createPermanent(P,"Rupture Spire",false,8);
+        addToHand(P,"Thieving Magpie",3);
+
+        return game;
+    }
+}
