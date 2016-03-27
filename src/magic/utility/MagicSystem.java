213e6f10c948fb0e28e052bc5176768d9c842a7e
lodici <lodici123@gmail.com>
2016-03-27 17:11:30 +0100

    bump to version 1.72

--- src/magic/utility/MagicSystem.java
+++ src/magic/utility/MagicSystem.java
@@ -24,7 +24,7 @@ import magic.utility.MagicFileSystem.DataPath;
 final public class MagicSystem {
     private MagicSystem() {}
 
-    public static final String VERSION = "1.71";
+    public static final String VERSION = "1.72";
 
     public static final String SOFTWARE_TITLE;
     private static final boolean DEV_MODE;
