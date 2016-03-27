ed5183b3e45cf41197414f143daf0610ba4fe5ea
lodici <lodici123@gmail.com>
2016-03-27 17:36:01 +0100

    fix F1 link to scripting wiki page.

--- src/magic/utility/WikiPage.java
+++ src/magic/utility/WikiPage.java
@@ -6,7 +6,7 @@ public final class WikiPage {
 
     public static final String HOME = "home";
     public static final String CARDS_EXPLORER = "UICardExplorer";
-    public static final String CARD_SCRIPTING = "ImplementingCards";
+    public static final String CARD_SCRIPTING = "UICardScriptViewer";
     public static final String DECK_EDITOR = "UIDeckEditor";
     public static final String DUEL_DECKS = "UIDeckView";
     public static final String MAIN_MENU = "Main-Menu-Screen";
