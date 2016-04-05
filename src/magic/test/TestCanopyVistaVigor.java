8cb9a2c94a162ab0c37adf6223218d83b0ed4892
melvinzhang <melvinzhang@gmail.com>
2016-04-05 20:00:16 +0800

    add test class to replicate issue #750

---
+++ src/magic/test/TestCanopyVistaVigor.java
@@ -0,0 +1,31 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+import magic.ai.MagicAIImpl;
+
+class TestCanopyVistaVigor extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS, 6);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(10);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Amulet of Vigor", false, 1);
+        addToHand(P, "Canopy Vista", 1);
+
+        P = opponent;
+
+        P.setLife(10);
+        addToLibrary(P, "Forest", 20);
+
+        return game;
+    }
+}
