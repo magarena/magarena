479a23e3803bc801cf765a71f695d96769710915
melvinzhang <melvinzhang@gmail.com>
2016-05-11 20:56:37 +0800

    add test for issue #763, unable to reproduce bug

---
+++ src/magic/test/TestIssue763.java
@@ -0,0 +1,34 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.phase.MagicMainPhase;
+import magic.ai.MagicAIImpl;
+
+class TestIssue763 extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTS, 6);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicMainPhase.getFirstInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(10);
+        addToLibrary(P, "Forest", 20);
+        addToLibrary(P, "Crovax the Cursed", 2);
+        addToLibrary(P, "Ornithopter", 2);
+        createPermanent(P, "Plains", false, 4);
+        addToHand(P, "Ranger of Eos", 1);
+
+        P = opponent;
+
+        P.setLife(1);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Island", false, 4);
+
+        return game;
+    }
+}
