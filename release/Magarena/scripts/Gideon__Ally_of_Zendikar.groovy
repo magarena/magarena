788ea676ee2c2a57a8fc56af009d171023f0336b
muppet <unknown>
2016-05-21 13:03:50 +0800

    add Gideon, Ally of Zendikar

---
+++ release/Magarena/scripts/Gideon__Ally_of_Zendikar.groovy
@@ -0,0 +1,79 @@
+def EFFECT2 = MagicRuleEventAction.create("Put a 2/2 white Knight Ally creature token onto the battlefield.");
+            
+def PT = new MagicStatic(MagicLayer.SetPT, MagicStatic.UntilEOT) {
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        pt.set(5,5);
+    }
+};
+
+def ST = new MagicStatic(MagicLayer.Type, MagicStatic.UntilEOT) {
+    @Override
+    public void modSubTypeFlags(final MagicPermanent permanent,final Set<MagicSubType> flags) {
+        flags.add(MagicSubType.Human);
+        flags.add(MagicSubType.Soldier);
+        flags.add(MagicSubType.Ally);
+    }
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent,final int flags) {
+        return flags|MagicType.Creature.getMask();
+    }
+};
+
+def AB = new MagicStatic(MagicLayer.Ability, MagicStatic.UntilEOT) {
+    @Override
+    public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
+        permanent.addAbility(MagicAbility.Indestructible, flags);
+    }
+};
+
+[
+    new MagicPlaneswalkerActivation(1) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "Until end of turn, SN becomes a 5/5 Human Soldier Ally creature with indestructible that's still a planeswalker. " +
+                "Prevent all damage that would be dealt to him this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new BecomesCreatureAction(event.getPermanent(),PT,AB,ST));
+            game.doAction(new AddTurnTriggerAction(event.getPermanent(), PreventDealtTo));
+        }
+    },    
+    new MagicPlaneswalkerActivation(0) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return EFFECT2.getEvent(source);
+        }
+    },
+    new MagicPlaneswalkerActivation(-4) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                this,
+                "PN gets an emblem with \"Creatures you control get +1/+1.\""
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame outerGame, final MagicEvent outerEvent) {
+            final long pId = outerEvent.getPlayer().getId();
+            outerGame.doAction(new AddStaticAction(
+                new MagicStatic(MagicLayer.ModPT, ANY) {
+                    @Override
+                    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+                        pt.add(1, 1);
+                    }
+                    @Override
+                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+                        return target.getController().getId() == pId && target.isCreature();
+                    }
+                }
+            ));
+        }
+    }
+]
