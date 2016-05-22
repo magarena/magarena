08ba8ee6f3234fcf5ae0e161f0ab963fc732fe8d
melvinzhang <melvinzhang@gmail.com>
2016-05-22 10:24:29 +0800

    Anafenza need a separate LeavesBattlefieldTrigger to prevent dies from triggering, fixes #765

--- release/Magarena/scripts/Anafenza__the_Foremost.groovy
+++ release/Magarena/scripts/Anafenza__the_Foremost.groovy
@@ -1,8 +1,23 @@
 [
+    new LeavesBattlefieldTrigger(MagicTrigger.REPLACEMENT) {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final RemoveFromPlayAction act) {
+            final MagicPermanent it = act.getPermanent();
+            if (it.isNonToken() &&
+                it.isCreature() &&
+                it.getCard().isEnemy(permanent) &&
+                act.to(MagicLocationType.Graveyard)) {
+                act.setToLocation(MagicLocationType.Exile);
+            }
+            return MagicEvent.NONE;
+        }
+    },
     new WouldBeMovedTrigger(MagicTrigger.REPLACEMENT) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MoveCardAction act) {
             if (act.card.hasType(MagicType.Creature) &&
+                act.card.isToken() == false &&
+                act.from(MagicLocationType.Battlefield) == false &&
                 act.card.isEnemy(permanent) &&
                 act.to(MagicLocationType.Graveyard)) {
                 act.setToLocation(MagicLocationType.Exile);
