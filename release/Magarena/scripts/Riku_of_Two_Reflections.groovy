d9a72c482cab35f136328daf3195e6aff390ecc4
melvinzhang <melvinzhang@gmail.com>
2016-06-04 20:32:45 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Riku_of_Two_Reflections.groovy
+++ release/Magarena/scripts/Riku_of_Two_Reflections.groovy
@@ -20,34 +20,5 @@
                 game.doAction(new CopyCardOnStackAction(event.getPlayer(),event.getRefCardOnStack()));
             }
         }
-    },
-    new OtherEntersBattlefieldTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent otherPermanent) {
-            return (otherPermanent != permanent &&
-                    otherPermanent.isNonToken() &&
-                    otherPermanent.isCreature() &&
-                    otherPermanent.isFriend(permanent)) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicMayChoice(
-                        new MagicPayManaCostChoice(MagicManaCost.create("{G}{U}"))
-                    ),
-                    otherPermanent,
-                    this,
-                    "PN may\$ pay {G}{U}. If PN does, he or she puts a token that's a copy of RN onto the battlefield."
-                ):
-                MagicEvent.NONE;
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.isYes()) {
-                game.doAction(new PlayTokenAction(
-                    event.getPlayer(),
-                    event.getRefPermanent()
-                ));
-            }
-        }
     }
 ]
