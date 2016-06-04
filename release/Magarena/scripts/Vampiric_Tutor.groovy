68bf1e6a0afe55cf14dbeaf53da05fe884b3c114
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-06-04 14:37:49 +0100

    remove card reveal from Vampiric Tutor

--- release/Magarena/scripts/Vampiric_Tutor.groovy
+++ release/Magarena/scripts/Vampiric_Tutor.groovy
@@ -5,17 +5,19 @@
             return new MagicEvent(
                 cardOnStack,
                 this,
-                "PN searches his or her library for a card, shuffle his or her library, and put that card on top of it. PN loses 2 life."
+                "PN searches his or her library for a card, shuffles his or her library, and puts that card on top of it. "+
+                "PN loses 2 life."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.doAction(new ChangeLifeAction(event.getPlayer(),-2));
             game.addEvent(new MagicSearchToLocationEvent(
                 event,
                 A_CARD_FROM_LIBRARY,
-                MagicLocationType.TopOfOwnersLibrary
+                MagicLocationType.TopOfOwnersLibrary,
+                false
             ));
+            game.doAction(new ChangeLifeAction(event.getPlayer(),-2));
         }
     }
 ]
