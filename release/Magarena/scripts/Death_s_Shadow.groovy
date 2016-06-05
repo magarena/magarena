8c53585c9c5d42763d87a3e3e842e702f4e29094
melvinzhang <melvinzhang@gmail.com>
2016-06-05 17:58:46 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Death_s_Shadow.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicStatic(MagicLayer.ModPT) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amt = permanent.getController().getLife();
-            pt.add(-amt, -amt);
-        }
-    }
-]
