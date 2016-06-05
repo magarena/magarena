5ce10fda976af8e1e68ed85d0d541482871e2284
melvinzhang <melvinzhang@gmail.com>
2016-06-05 18:12:16 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Sun_Droplet.groovy
+++ release/Magarena/scripts/Sun_Droplet.groovy
@@ -15,25 +15,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,event.getRefInt()));
         }
-    },
-    new AtUpkeepTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
-            return permanent.hasCounters(MagicCounterType.Charge) ?
-                new MagicEvent(
-                    permanent,
-                    new MagicSimpleMayChoice(),
-                    this,
-                    "PN may\$ remove a charge counter from SN. If PN does, PN gains 1 life."
-                ):
-                MagicEvent.NONE;
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            if (event.getPermanent().getCounters(MagicCounterType.Charge)>=1 && event.isYes()) {
-                game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.Charge,-1));
-                game.doAction(new ChangeLifeAction(event.getPlayer(),1));
-            }
-        }
     }
 ]
