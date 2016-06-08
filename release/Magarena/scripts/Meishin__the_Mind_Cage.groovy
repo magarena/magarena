64dcb13d925f1f58d618bda59f20762361cd6821
melvinzhang <melvinzhang@gmail.com>
2016-06-08 14:21:21 +0800

    replace groovy code with card scripts

--- release/Magarena/scripts/Meishin__the_Mind_Cage.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicStatic(MagicLayer.ModPT, CREATURE) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            int X = 0 - permanent.getController().getHandSize();
-            pt.add(X,0);
-        }
-    }
-]
