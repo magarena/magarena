cf6c72d117339330a2a80c08a2f26ef3540d7c13
melvinzhang <melvinzhang@gmail.com>
2016-06-13 22:01:09 +0800

    fix indentation

--- release/Magarena/scripts/Might_of_the_Nephilim.groovy
+++ release/Magarena/scripts/Might_of_the_Nephilim.groovy
@@ -12,21 +12,21 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
-            if (it.hasColor(MagicColor.Black)) {
-                game.doAction(new ChangeTurnPTAction(it,2,2));
-            }
-            if (it.hasColor(MagicColor.Blue)) {
-                game.doAction(new ChangeTurnPTAction(it,2,2));
-            }
-            if (it.hasColor(MagicColor.Green)) {
-                game.doAction(new ChangeTurnPTAction(it,2,2));
-            }
-            if (it.hasColor(MagicColor.Red)) {
-                game.doAction(new ChangeTurnPTAction(it,2,2));
-            }
-            if (it.hasColor(MagicColor.White)) {
-                game.doAction(new ChangeTurnPTAction(it,2,2));
-            }
+                if (it.hasColor(MagicColor.Black)) {
+                    game.doAction(new ChangeTurnPTAction(it,2,2));
+                }
+                if (it.hasColor(MagicColor.Blue)) {
+                    game.doAction(new ChangeTurnPTAction(it,2,2));
+                }
+                if (it.hasColor(MagicColor.Green)) {
+                    game.doAction(new ChangeTurnPTAction(it,2,2));
+                }
+                if (it.hasColor(MagicColor.Red)) {
+                    game.doAction(new ChangeTurnPTAction(it,2,2));
+                }
+                if (it.hasColor(MagicColor.White)) {
+                    game.doAction(new ChangeTurnPTAction(it,2,2));
+                }
             });
         }
     }
