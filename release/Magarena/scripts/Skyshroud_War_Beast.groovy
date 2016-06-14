a18ccc6faeefa7ea436d4e2ee7ae40ce31ac2aea
melvinzhang <melvinzhang@gmail.com>
2016-06-14 11:43:31 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Skyshroud_War_Beast.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new MagicCDA() {
-        @Override
-        public void modPowerToughness(final MagicGame game, final MagicPlayer player, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            final int amt = permanent.getChosenPlayer().getNrOfPermanents(NONBASIC_LAND);
-            pt.set(amt, amt);
-        }
-    }
-]
