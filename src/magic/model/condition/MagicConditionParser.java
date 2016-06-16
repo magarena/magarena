04e63c23f0665df4c3a92c8306c1b6f064ff1dae
melvinzhang <melvinzhang@gmail.com>
2016-06-16 08:44:06 +0800

    rename arf to arg

--- src/magic/model/condition/MagicConditionParser.java
+++ src/magic/model/condition/MagicConditionParser.java
@@ -528,22 +528,22 @@ public enum MagicConditionParser {
         }
     },
     CreatureInYourGraveyard("you have a creature card in your graveyard") {
-        public MagicCondition toCondition(final Matcher arf) {
+        public MagicCondition toCondition(final Matcher arg) {
             return MagicCondition.HAS_CREATURE_IN_GRAVEYARD;
         }
     },
     CreatureInYourHand("you have a creature card in your hand") {
-        public MagicCondition toCondition(final Matcher arf) {
+        public MagicCondition toCondition(final Matcher arg) {
             return MagicCondition.HAS_CREATURE_IN_HAND;
         }
     },
     ArtifactInYourGraveyard("you have an artifact card in your graveyard") {
-        public MagicCondition toCondition(final Matcher arf) {
+        public MagicCondition toCondition(final Matcher arg) {
             return MagicCondition.HAS_ARTIFACT_IN_GRAVEYARD;
         }
     },
     EquipmentInYourHand("you have an equipment card in your hand") {
-        public MagicCondition toCondition(final Matcher arf) {
+        public MagicCondition toCondition(final Matcher arg) {
             return MagicCondition.HAS_EQUIPMENT_IN_HAND;
         }
     },
