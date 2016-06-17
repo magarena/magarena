01878924da083b48ff3fbcab2e7c7aab4d52c179
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-06-17 14:52:40 +0100

    add Mirror Match

---
+++ release/Magarena/scripts/Mirror_Match.groovy
@@ -0,0 +1,34 @@
+def blocking = {
+    final MagicPermanent attacker ->
+    return {
+        final MagicPermanent perm ->
+        final MagicGame game = perm.getGame();
+        game.doAction(new SetBlockerAction(attacker.map(game), perm));
+    };
+}
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "For each creature attacking PN or a planeswalker he or she controls, "+
+                "PN puts a token that's a copy of that creature onto the battlefield blocking that creature. "+
+                "Exile those tokens at end of combat."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            for (final MagicPermanent creature : ATTACKING_CREATURE_OPP_CONTROL.filter(event)) {
+                game.doAction(new PlayTokenAction(
+                    event.getPlayer(),
+                    creature,
+                    MagicPlayMod.EXILE_AT_END_OF_COMBAT,
+                    blocking(creature)
+                ));
+            }
+        }
+    }
+]
