458cc6f63ed964c9937dd9b392291fe3fbbb4e72
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-06-17 13:32:12 +0100

    add Kin-Tree Invocation

---
+++ release/Magarena/scripts/Kin_Tree_Invocation.groovy
@@ -0,0 +1,31 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN puts an X/X black and green Spirit Warrior creature token onto the battlefield, where X is the greatest toughness among creatures PN controls."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            int x = 0;
+            CREATURE_YOU_CONTROL.filter(event) each {
+                x = Math.max(x,it.getToughness());
+            }
+            game.logAppendValue(event.getPlayer(), x);
+            game.doAction(new PlayTokenAction(event.getPlayer(), MagicCardDefinition.create({
+                it.setName("Spirit Warrior");
+                it.setDistinctName("black and green Spirit Warrior creature token");
+                it.setPowerToughness(x, x);
+                it.setColors("bg");
+                it.addSubType(MagicSubType.Spirit);
+                it.addSubType(MagicSubType.Warrior);
+                it.addType(MagicType.Creature);
+                it.setToken();
+                it.setValue(x);
+            })));
+        }
+    }
+]
