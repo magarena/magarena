027e95b533ad47b0800666703235c5aac4a287e8
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-06-17 14:02:53 +0100

    add Ooze Garden

---
+++ release/Magarena/scripts/Ooze_Garden.groovy
@@ -0,0 +1,43 @@
+def choice = new MagicTargetChoice("a non-Ooze creature to sacrifice");
+
+[
+    new MagicPermanentActivation(
+        [MagicCondition.SORCERY_CONDITION],
+        new MagicActivationHints(MagicTiming.Token),
+        "Token"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{1}{G}"),
+                new MagicSacrificePermanentEvent(source,choice)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                payedCost.getTarget(),
+                this,
+                "PN puts an X/X green Ooze creature token onto the battlefield, where X is the sacrificed creature's power."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int x = event.getRefPermanent().getPower();
+            game.logAppendValue(event.getPlayer(), x);
+            game.doAction(new PlayTokenAction(event.getPlayer(), MagicCardDefinition.create({
+                it.setName("Ooze");
+                it.setDistinctName("green Ooze creature token");
+                it.setPowerToughness(x, x);
+                it.setColors("g");
+                it.addSubType(MagicSubType.Ooze);
+                it.addType(MagicType.Creature);
+                it.setToken();
+                it.setValue(x);
+            })));
+        }
+    }
+]
