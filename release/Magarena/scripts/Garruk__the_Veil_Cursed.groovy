b8faf7122bab0156ecd8f967e31cbcffad9b232c
melvinzhang <melvinzhang@gmail.com>
2016-06-19 11:56:59 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Garruk__the_Veil_Cursed.groovy
+++ release/Magarena/scripts/Garruk__the_Veil_Cursed.groovy
@@ -20,23 +20,5 @@
                 ));
             }
         }
-    },
-    new MagicPlaneswalkerActivation(-3) {
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "Creatures PN controls gain trample and get +X/+X until end of turn, where X is the number of creature cards in his or her graveyard."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final int X = CREATURE_CARD_FROM_GRAVEYARD.filter(event).size();
-            CREATURE_YOU_CONTROL.filter(event.getPlayer()) each {
-                game.doAction(new GainAbilityAction(it, MagicAbility.Trample));
-                game.doAction(new ChangeTurnPTAction(it, X, X));
-            }
-        }
     }
 ]
