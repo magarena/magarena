21be71b9e93447c72b9f2280ec5de56c1692232f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-07-21 15:58:15 +0100

    add Scour the Laboratory

---
+++ release/Magarena/scripts/Scour_the_Laboratory.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicHandCastActivation(
+        [MagicCondition.CARD_CONDITION],
+        new MagicActivationHints(MagicTiming.Draw, true),
+        "Cast"
+    ) {
+        @Override
+        public void change(final MagicCardDefinition cdef) {
+            cdef.setHandAct(this);
+        }
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+           final int n = MagicCondition.DELERIUM_CONDITION.accept(source) ? 2 : 0;
+            return [
+                new MagicPayManaCostEvent(
+                    source,
+                    source.getCost().reduce(n)
+                )
+            ];
+        }
+    }
+]
