dfce0ae59763c4b33419c9314edf43a7536bd2cf
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-07-21 16:55:11 +0100

    Correct Groovy for Thalia, Heretic Cathar

--- release/Magarena/scripts/Thalia__Heretic_Cathar.groovy
+++ release/Magarena/scripts/Thalia__Heretic_Cathar.groovy
@@ -3,7 +3,7 @@
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             if (otherPermanent.isEnemy(permanent) &&
-                (otherPermanent.Creature() || (!otherPermanent.isBasic() && otherPermanent.isLand()))) {
+                (otherPermanent.isCreature() || (!otherPermanent.isBasic() && otherPermanent.isLand()))) {
                 game.doAction(TapAction.Enters(otherPermanent));
             }
             return MagicEvent.NONE;
