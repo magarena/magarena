60806331174cd57a94a6f40e9ffd376363b78093
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-07-21 15:58:26 +0100

    add Bedlam Reveler

---
+++ release/Magarena/scripts/Bedlam_Reveler.groovy
@@ -0,0 +1,23 @@
+[
+    new MagicHandCastActivation(
+        [MagicCondition.CARD_CONDITION],
+        new MagicActivationHints(MagicTiming.Main, true),
+        "Cast"
+    ) {
+        @Override
+        public void change(final MagicCardDefinition cdef) {
+            cdef.setHandAct(this);
+        }
+
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicCard source) {
+           final int n = INSTANT_OR_SORCERY_CARD_FROM_GRAVEYARD.filter(source).size();
+            return [
+                new MagicPayManaCostEvent(
+                    source,
+                    source.getCost().reduce(n)
+                )
+            ];
+        }
+    }
+]
