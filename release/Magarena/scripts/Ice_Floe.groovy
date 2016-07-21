2a24cc04c5f7719c3a4cb05fa5e29f29578f4155
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-07-21 21:08:47 +0100

    add Ice Floe

---
+++ release/Magarena/scripts/Ice_Floe.groovy
@@ -0,0 +1,35 @@
+def choice = Negative("target attacking creature without flying");
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Tapping),
+        "Tap"
+    ) {
+        @Override
+        public Iterable<MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicTapEvent(source)];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                MagicTapTargetPicker.Tap,
+                this,
+                "Tap target creature without flying that's attacking PN\$. It doesn't untap during its controller's untap step for as long as SN remains tapped."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new TapAction(it));
+                game.doAction(new AddStaticAction(
+                    event.getPermanent(),
+                    MagicStatic.AsLongAsCond(it, MagicAbility.DoesNotUntap, MagicCondition.TAPPED_CONDITION)
+                ));
+            });
+        }
+    }
+]
