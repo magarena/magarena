0d8af1e168c0d6edb5c694a2a9cb1d873c16660c
melvinzhang <melvinzhang@gmail.com>
2016-07-22 15:03:24 +0800

    fix condition to check that spell is cast by 'you'

--- release/Magarena/scripts/Geist_Fueled_Scarecrow.groovy
+++ release/Magarena/scripts/Geist_Fueled_Scarecrow.groovy
@@ -2,7 +2,7 @@
     new MagicStatic(MagicLayer.CostIncrease) {
         @Override
         public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
-            return card.isCreature() ? cost.increase(1) : cost;
+            return card.isCreature() && source.isFriend(card) ? cost.increase(1) : cost;
         }
     }
 ]
