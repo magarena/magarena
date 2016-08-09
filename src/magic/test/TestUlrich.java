34482b128c5328de2b03336f80e6d3dedbc35ccc
melvinzhang <melvinzhang@gmail.com>
2016-08-09 09:04:41 +0800

    add test case to replicate #822

---
+++ src/magic/test/TestUlrich.java
@@ -0,0 +1,36 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+import magic.model.MagicDeckProfile;
+import magic.model.phase.*;
+import magic.ai.MagicAIImpl;
+
+class TestUlrich extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel(MagicAIImpl.MCTSC, 6);
+        final MagicGame game=duel.nextGame();
+        game.setPhase(MagicUpkeepPhase.getInstance());
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(10);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Wastes", false, 8);
+        createPermanent(P, "Chromatic Lantern", false, 1);
+        createPermanent(P, "Vildin-Pack Alpha", false, 1);
+        addToHand(P, "Ulrich of the Krallenhorde", 1);
+
+        P = opponent;
+
+        P.setLife(10);
+        addToLibrary(P, "Forest", 20);
+        createPermanent(P, "Colossodon Yearling", false, 2);
+        createPermanent(P, "Grizzly Bears", false, 1);
+
+        return game;
+    }
+}
