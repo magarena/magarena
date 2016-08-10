b1743d20a39e7cd55350f3a1d546097d830678de
melvinzhang <melvinzhang@gmail.com>
2016-08-10 20:15:04 +0800

    store copy of getBlockedCreature before destroying the target, fixes #821

--- release/Magarena/scripts/Ride_Down.groovy
+++ release/Magarena/scripts/Ride_Down.groovy
@@ -7,14 +7,16 @@ def NEG_TARGET_BLOCKING_CREATURE = MagicTargetChoice.Negative("target blocking c
                 cardOnStack,
                 NEG_TARGET_BLOCKING_CREATURE,
                 this,
-                "Destroy target blocking creature.\$ Creatures that were blocked by RN this combat gain trample until end of turn."
+                "Destroy target blocking creature\$. " +
+                "Creatures that were blocked by that creature this combat gain trample until end of turn."
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
+                final MagicPermanent blocked = it.getBlockedCreature();
                 game.doAction(new DestroyAction(it));
-                game.doAction(new GainAbilityAction(it.getBlockedCreature(), MagicAbility.Trample));
+                game.doAction(new GainAbilityAction(blocked, MagicAbility.Trample));
             });
         }
     }
