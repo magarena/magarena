ac2246d38b54a6456b7330024e1812e5ceec85ab
kelsioboy <unknown>
2016-09-04 12:25:43 +0800

    fix Warmonger's Chariot by removing extra check for color

--- release/Magarena/scripts/Warmonger_s_Chariot.groovy
+++ release/Magarena/scripts/Warmonger_s_Chariot.groovy
@@ -8,7 +8,7 @@
         }
         @Override
         public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-            return MagicStatic.acceptLinked(game, source, target) && target.hasColor(MagicColor.Black);
+            return MagicStatic.acceptLinked(game, source, target);
         }
     }
 ]
