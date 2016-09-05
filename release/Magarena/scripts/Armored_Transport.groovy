b24db4ffb7703380931e3419fcded7b3bf7c775d
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-09-05 19:51:10 +0100

    add Armored Transport

---
+++ release/Magarena/scripts/Armored_Transport.groovy
@@ -0,0 +1,15 @@
+[
+    new PreventDamageTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (damage.getTarget() == permanent && 
+                damage.isCombat() && 
+                damage.getSource().hasType(MagicType.Creature) &&
+                permanent.getBlockingCreatures().contains(damage.getSource())) {
+                    // Replacement effect. Generates no event or action.
+                    damage.prevent();
+                }
+            return MagicEvent.NONE;
+        }
+    }
+]
