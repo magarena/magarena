2d2730cff015c67935423ef822ad66f6067ab471
kelsioboy <unknown>
2016-09-20 16:17:27 +0800

    add Chimney Imp, closes #853

---
+++ release/Magarena/scripts/Chimney_Imp.groovy
@@ -0,0 +1,19 @@
+[
+    new ThisDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent died) {
+            return new MagicEvent(
+                permanent,
+                TARGET_OPPONENT,
+                this,
+                "Target opponent\$ puts a card from his or her hand on top of his or her library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.addEvent(new MagicReturnCardEvent(event.getSource(), it));
+            });
+        }
+    }
+]
