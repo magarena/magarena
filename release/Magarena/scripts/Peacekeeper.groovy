e090f6a302fb27d47a98623fa0fb4086416d7497
melvinzhang <melvinzhang@gmail.com>
2016-09-21 12:31:42 +0800

    restore phrasing to oracle text

--- release/Magarena/scripts/Peacekeeper.groovy
+++
@@ -1,8 +0,0 @@
-[
-    new MagicStatic(MagicLayer.Ability, CREATURE) {
-        @Override
-        public void modAbilityFlags(final MagicPermanent source,final MagicPermanent permanent,final Set<MagicAbility> flags) {
-            permanent.addAbility(MagicAbility.CannotAttack, flags);
-        }
-    }
-]
