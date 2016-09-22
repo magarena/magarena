38215fece44e14ae864179660d6ae015a308e62c
lodici <lodici123@gmail.com>
2016-09-22 11:31:24 +0100

    Update with translatable strings.

--- src/magic/ui/explorer/filter/FilterResetButton.java
+++ src/magic/ui/explorer/filter/FilterResetButton.java
@@ -6,13 +6,17 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import magic.translate.UiString;
 import magic.ui.MagicUI;
 
 @SuppressWarnings("serial")
 class FilterResetButton extends JButton {
 
+    // translatable strings
+    private static final String _S1 = "Reset";
+
     FilterResetButton(final FilterPanel fbp) {
-        super("Reset");
+        super(UiString.get(_S1));
         setFont(getFont().deriveFont(Font.BOLD, 12));
         setForeground(new Color(127, 23, 23));
         addActionListener(new AbstractAction() {
