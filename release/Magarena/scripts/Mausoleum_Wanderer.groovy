c172b7e737a5acd3da0878b0dd028aef46b81a3f
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-09-22 13:58:08 +0100

    add Mausoleum Wanderer

---
+++ release/Magarena/scripts/Mausoleum_Wanderer.groovy
@@ -0,0 +1,33 @@
+def choice = MagicTargetChoice.Negative("target instant or sorcery spell");
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Counter),
+        "Counter"
+    ) {
+
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicSacrificeEvent(source)
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                choice,
+                this,
+                "Counter target instant or sorcery spell\$ unless its controller pays {X}, where X is SN's power."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCardOnStack(game, {
+                final int amount = event.getPermanent().getPower();
+                game.logAppendX(event.getPlayer(), amount);
+                game.addEvent(new MagicCounterUnlessEvent(event.getSource(), it, MagicManaCost.create("{"+amount+"}")));
+            });
+        }
+    }
+]
