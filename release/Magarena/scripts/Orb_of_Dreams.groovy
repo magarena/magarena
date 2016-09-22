bdae3beb42003ca21d2890107d2f416fcecba760
melvinzhang <melvinzhang@gmail.com>
2016-09-23 00:16:36 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Orb_of_Dreams.groovy
+++
@@ -1,9 +0,0 @@
-[
-    new OtherEntersBattlefieldTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            game.doAction(TapAction.Enters(otherPermanent));
-            return MagicEvent.NONE;
-        }
-    }
-]
