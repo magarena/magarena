dd23352b116f620f5a51561f2f9540abffb6abde
melvinzhang <melvinzhang@gmail.com>
2016-09-23 11:09:12 +0800

    add Heart of Yavimaya

---
+++ release/Magarena/scripts/Heart_of_Yavimaya.groovy
@@ -0,0 +1,36 @@
+def costEffect = new MagicRegularCostEvent("Sacrifice a Forest");
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice("Sacrifice a Forest?"),
+                payedCost,
+                this,
+                "PN may\$ sacrifice a Forest. " + 
+                "If you do, put SN onto the battlefield. " + 
+                "If you don't, put it into its owner's graveyard."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicEvent costEvent = costEffect.getEvent(event.getSource());
+            final MagicCardOnStack spell = event.getCardOnStack();
+            if (event.isYes() && costEvent.isSatisfied()) {
+                game.addEvent(costEvent);
+                spell.setMoveLocation(MagicLocationType.Battlefield);
+                game.addEvent(MagicPlayCardEvent.create().getEvent(spell, event.getRefPayedCost()));
+            } else {
+                game.logAppendMessage(event.getPlayer(), "Put ${spell} into its owner's graveyard.");
+                game.doAction(new MoveCardAction(
+                    event.getCardOnStack().getCard(),
+                    MagicLocationType.OwnersHand,
+                    MagicLocationType.Graveyard
+                ));
+            }
+        }
+    }
+]
