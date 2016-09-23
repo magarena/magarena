337e89ff8905b22f61df33f5042bb6907bf4142a
melvinzhang <melvinzhang@gmail.com>
2016-09-23 13:12:49 +0800

    fix Mana Vapors groovy to follow Exhaustion so that it affects lands are enter the battlefield after the effect

--- release/Magarena/scripts/Mana_Vapors.groovy
+++ release/Magarena/scripts/Mana_Vapors.groovy
@@ -10,14 +10,28 @@
             );
         }
         @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPlayer(game, {
-                LAND_YOU_CONTROL.filter(it) each {
-                    game.doAction(ChangeStateAction.Set(
-                        it,
-                        MagicPermanentState.DoesNotUntapDuringNext
-                    ));
-                }
+        public void executeEvent(final MagicGame outerGame, final MagicEvent event) {
+            event.processTargetPlayer(outerGame, {
+                final MagicPlayer outerPlayer ->
+                outerGame.doAction(new AddStaticAction(new MagicStatic(MagicLayer.Game) {
+                    @Override
+                    public void modGame(final MagicPermanent source, final MagicGame game) {
+                        final MagicPlayer p = outerPlayer.map(game);
+                        LAND_YOU_CONTROL.filter(p) each {
+                            it.addAbility(MagicAbility.DoesNotUntap);
+                        }
+                    }
+                    @Override
+                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+                        final MagicPlayer p = outerPlayer.map(game);
+                        if (game.getTurnPlayer() == p && game.isPhase(MagicPhaseType.Upkeep)) {
+                            game.addDelayedAction(new RemoveStaticAction(this));
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }))
             });
         }
     }
