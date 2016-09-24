0fa07edc6055625e66f42134a3f6f776f92bcc9b
lodici <lodici123@gmail.com>
2016-09-24 19:06:45 +0100

    Remove obsolete code.

--- src/magic/ui/explorer/ExplorerPanel.java
+++ src/magic/ui/explorer/ExplorerPanel.java
@@ -5,15 +5,12 @@ import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import magic.model.MagicCardDefinition;
 import magic.model.MagicRandom;
-import magic.translate.UiString;
 import magic.ui.ICardFilterPanelListener;
 import magic.ui.ScreenController;
 import magic.ui.cardtable.CardTable;
@@ -26,11 +23,6 @@ import net.miginfocom.swing.MigLayout;
 @SuppressWarnings("serial")
 public class ExplorerPanel extends JPanel
         implements ICardSelectionListener, ICardFilterPanelListener {
-
-    // translatable strings
-    private static final String _S1 = "Cards:";
-    private static final String _S2 = "Playable:";
-    private static final String _S3 = "Unimplemented:";
 
     private static final int FILTERS_PANEL_HEIGHT = 88; // pixels
 
@@ -57,7 +49,7 @@ public class ExplorerPanel extends JPanel
         sideBarPanel = new ExplorerSidebarPanel();
         filterPanel = new CardFilterPanel(this);
         final Container cardsPanel = getMainContentContainer();
-        
+
         rhs.setLayout(new MigLayout("flowy, insets 0, gapy 0"));
         rhs.add(filterPanel, "w 100%, h " + FILTERS_PANEL_HEIGHT + "!");
         rhs.add(cardsPanel, "w 100%, h 100%");
@@ -89,23 +81,6 @@ public class ExplorerPanel extends JPanel
 
     }
 
-    public String generatePoolTitle() {
-        final int total = filterPanel.getTotalCardCount();
-        return String.format("%s %s      %s %s      %s %s",
-                UiString.get(_S1),
-                NumberFormat.getInstance().format(total),
-                UiString.get(_S2),
-                getCountCaption(total, filterPanel.getPlayableCardCount()),
-                UiString.get(_S3),
-                getCountCaption(total, filterPanel.getMissingCardCount()));
-    }
-
-    private String getCountCaption(final int total, final int value) {
-        final double percent = value / (double)total * 100;
-        DecimalFormat df = new DecimalFormat("0.0");
-        return NumberFormat.getInstance().format(value) + " (" + (!Double.isNaN(percent) ? df.format(percent) : "0.0") + "%)";
-    }
-
     @Override
     public boolean isDeckEditor() {
         return false;
@@ -114,7 +89,6 @@ public class ExplorerPanel extends JPanel
     public void updateCardPool() {
         cardPoolDefs = filterPanel.getFilteredCards();
         cardPoolTable.setCards(cardPoolDefs);
-        cardPoolTable.setTitle(generatePoolTitle());
     }
 
     @Override
