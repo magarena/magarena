4313b57b558321fb5531090f72760b0d0bdf9467
melvinzhang <melvinzhang@gmail.com>
2016-09-24 09:29:13 +0800

    use custom may choice prompt, add TAPPED play mod, fixes #894

--- release/Magarena/scripts/Vesuva.groovy
+++ release/Magarena/scripts/Vesuva.groovy
@@ -5,6 +5,7 @@
             return new MagicEvent(
                 cardOnStack,
                 new MagicMayChoice(
+                    "Choose a land to copy?",
                     new MagicTargetChoice("a land")
                 ),
                 MagicCopyPermanentPicker.create(),
@@ -16,7 +17,11 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             if (event.isYes()) {
-                game.doAction(new EnterAsCopyAction(event.getCardOnStack(), event.getTarget()))
+                game.doAction(new EnterAsCopyAction(
+                    event.getCardOnStack(),
+                    event.getTarget(),
+                    MagicPlayMod.TAPPED
+                ))
             } else {
                 game.logAppendMessage(event.getPlayer(), "Put ${event.getCardOnStack()} onto the battlefield.");
                 game.doAction(new PlayCardFromStackAction(
