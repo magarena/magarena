12e3da740d10517f123b5b91e644ad86f1b14ed1
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-09-26 17:52:30 +0100

    add Dovin and Saheeli to planeswalker subtypes

--- src/magic/model/MagicSubType.java
+++ src/magic/model/MagicSubType.java
@@ -45,8 +45,8 @@ public enum MagicSubType {
     Trap,
 
     //planeswalker subtypes
-    Ajani, Arlinn, Ashiok, Bolas, Chandra, Dack, Daretti, Domri, Elspeth, Freyalise, Garruk, Gideon, Jace, Karn, Kiora, Koth,
-    Liliana, Nahiri, Narset, Nissa, Nixilis, Ral, Sarkhan, Sorin, Teferi, Tezzeret, Venser, Tamiyo, Tibalt, Ugin, Vraska, Xenagos,
+    Ajani, Arlinn, Ashiok, Bolas, Chandra, Dack, Daretti, Domri, Dovin, Elspeth, Freyalise, Garruk, Gideon, Jace, Karn, Kiora, Koth,
+    Liliana, Nahiri, Narset, Nissa, Nixilis, Ral, Saheeli, Sarkhan, Sorin, Teferi, Tezzeret, Venser, Tamiyo, Tibalt, Ugin, Vraska, Xenagos,
 
     //special handling for Assembly_Worker
     Assembly_Worker() {
