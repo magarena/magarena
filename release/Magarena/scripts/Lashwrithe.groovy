f2785f74c753c903bef163cfa51711c2f81f3ac5
melvinzhang <melvinzhang@gmail.com>
2016-09-29 09:37:23 +0800

    add Lashwrithe, closes #919

---
+++ release/Magarena/scripts/Lashwrithe.groovy
@@ -0,0 +1,7 @@
+def Pay2Life = MagicRegularCostEvent.build("{B/P}, Pay 2 life");
+def Pay4Life = MagicRegularCostEvent.build("Pay 4 life");
+
+[
+    new MagicEquipActivation(Pay2Life, "Equip 2L", MagicTargetFilterFactory.CREATURE_YOU_CONTROL),
+    new MagicEquipActivation(Pay4Life, "Equip 4L", MagicTargetFilterFactory.CREATURE_YOU_CONTROL)
+]
