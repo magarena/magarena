833cf213779e661ce8099e058adeba19cb1ff536
kelsioboy <unknown>
2016-10-01 08:32:46 +0800

    add Nissa, Nature's Artisan, closes #923

---
+++ release/Magarena/scripts/Nissa__Nature_s_Artisan.groovy
@@ -0,0 +1,27 @@
+[
+    new MagicPlaneswalkerActivation(-4) {        
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {             
+            return new MagicEvent(
+                source,
+                this,
+                "PN reveals the top two cards of his or her library. "+
+                "PN puts all land cards from among them onto the battlefield and the rest into his or her hand."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            final MagicCardList cards = player.getLibrary().getCardsFromTop(2);
+            game.doAction(new RevealAction(cards));
+            for (final MagicCard card : cards) {
+                if (card.hasType(MagicType.Land)) {
+                    game.doAction(new ReturnCardAction(MagicLocationType.OwnersLibrary, card, player));
+                } else {
+                    game.doAction(new ShiftCardAction(card, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
+                }
+            }
+        }
+    }
+]
