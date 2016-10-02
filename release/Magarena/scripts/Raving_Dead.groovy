eff14fad30c5a75c11f459714055d0c429fc9e61
kelsioboy <unknown>
2016-10-02 10:03:22 +0800

    add Raving Dead, closes #927

---
+++ release/Magarena/scripts/Raving_Dead.groovy
@@ -0,0 +1,18 @@
+[ 
+    new ThisCombatDamagePlayerTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                this,
+                "PN loses half his or her life, rounded down."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new ChangeLifeAction(player, -player.getLife()/2));
+        }
+    }
+]
