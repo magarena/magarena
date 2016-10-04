30afcab981f55c44c724795d870c4d4304d3e6c3
melvinzhang <melvinzhang@gmail.com>
2016-10-04 09:43:46 +0800

    allow 'has' in ConditionGainGroup

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -1339,20 +1339,7 @@ public enum MagicAbility {
             ConditionPumpGroup.addAbilityImpl(card, arg);
         }
     },
-    ConditionGainGroup("As long as " + ARG.WORDRUN + ", " + ARG.WORDRUN2 + " have " + ARG.ANY, 0) {
-        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            final MagicCondition condition = MagicConditionParser.build(ARG.wordrun(arg));
-            final MagicTargetFilter<MagicPermanent> filter = MagicTargetFilterFactory.Permanent(ARG.wordrun2(arg));
-            final MagicAbilityList abList = MagicAbility.getAbilityList(ARG.any(arg));
-            card.add(MagicStatic.genABStatic(condition, filter, abList));
-        }
-    },
-    ConditionGainGroupAlt(ARG.WORDRUN2 + " (?<any>(has|have|can).+) as long as " + ARG.WORDRUN, 0) {
-        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
-            ConditionGainGroup.addAbilityImpl(card, arg);
-        }
-    },
-    ConditionPumpGain(ARG.WORDRUN2 + " (gets " + ARG.PT + " )?(and )?(" + ARG.ANY + " )?as long as " + ARG.WORDRUN + "\\.", 0) {
+    ConditionPumpGain(ARG.WORDRUN2 + " (?=(get|has|can))(gets " + ARG.PT + " )?(and )?(" + ARG.ANY + " )?as long as " + ARG.WORDRUN + "\\.", 0) {
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
             final MagicTargetFilter<MagicPermanent> filter = MagicTargetFilterFactory.Permanent(ARG.wordrun2(arg));
             final MagicCondition condition = MagicConditionParser.build(ARG.wordrun(arg));
@@ -1392,6 +1379,19 @@ public enum MagicAbility {
                     MagicAbility.getAbilityList(abText)
                 ));
             }
+        }
+    },
+    ConditionGainGroup("As long as " + ARG.WORDRUN + ", " + ARG.WORDRUN2 + " (has|have) " + ARG.ANY, 0) {
+        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
+            final MagicCondition condition = MagicConditionParser.build(ARG.wordrun(arg));
+            final MagicTargetFilter<MagicPermanent> filter = MagicTargetFilterFactory.Permanent(ARG.wordrun2(arg));
+            final MagicAbilityList abList = MagicAbility.getAbilityList(ARG.any(arg));
+            card.add(MagicStatic.genABStatic(condition, filter, abList));
+        }
+    },
+    ConditionGainGroupAlt(ARG.WORDRUN2 + " (?<any>(has|have|can).+) as long as " + ARG.WORDRUN, 0) {
+        protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
+            ConditionGainGroup.addAbilityImpl(card, arg);
         }
     },
     CantBlockPermanent("(SN )?can't block " + ARG.WORDRUN, 10) {
