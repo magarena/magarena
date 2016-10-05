af78240f1532c151ed2fd91ca7b29a029f06f083
melvinzhang <melvinzhang@gmail.com>
2016-10-05 09:29:40 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Liliana_Vess.groovy
+++ release/Magarena/scripts/Liliana_Vess.groovy
@@ -1,22 +1,4 @@
 [
-    new MagicPlaneswalkerActivation(-2) {
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                this,
-                "PN may search his or her library for a card, shuffle his or her library, and put that card on top of it."
-            );
-        }
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            game.addEvent(new MagicSearchToLocationEvent(
-                event,
-                A_CARD_FROM_LIBRARY,
-                MagicLocationType.TopOfOwnersLibrary
-            ));
-        }
-    },
     new MagicPlaneswalkerActivation(-8) {
         @Override
         public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
