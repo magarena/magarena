0e6cd569feb7169acd2f054a08288bd7dfe85e47
lodici <lodici123@gmail.com>
2016-10-09 11:15:59 +0100

    Convert TextFileViewer to MTextFileViewer, an MWidget.

--- src/magic/ui/widget/M/MTextFileViewer.java
+++ src/magic/ui/widget/M/MTextFileViewer.java
@@ -1,6 +1,5 @@
-package magic.ui.widget;
+package magic.ui.widget.M;
 
-import magic.ui.widget.M.MPlainTextViewer;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
@@ -8,42 +7,44 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.nio.file.Path;
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import magic.ui.FontsAndBorders;
 import magic.ui.utility.MagicStyle;
-import magic.ui.widget.M.MFileLink;
+import magic.ui.widget.TexturedPanel;
 import magic.utility.FileIO;
 import net.miginfocom.swing.MigLayout;
 
 /**
  * Displays contents of given text file.
- * Includes banner with clickable link which opens the file in the default
- * text editor or opens the containing directory in file explorer.
+ * Includes optional banner with clickable link which opens the file in the
+ * default text editor or opens the containing directory in file explorer.
  */
 @SuppressWarnings("serial")
-public class TextFileViewer extends TexturedPanel {
+public class MTextFileViewer extends MWidget {
 
     private static final Font FILE_LINK_FONT = new Font("dialog", Font.PLAIN, 14);
-    private final Color FILE_LINK_COLOR = Color.WHITE;
+    private static final Color FILE_LINK_COLOR = Color.WHITE;
 
+    private final TexturedPanel panel = new TexturedPanel();
     private final MPlainTextViewer textViewer = new MPlainTextViewer();
     private final MFileLink fileLink = new MFileLink();
 
-    public TextFileViewer() {
+    public MTextFileViewer() {
         setDefaultProperties();
         setLayout();
     }
 
     protected void setLayout() {
-        removeAll();
-        add(fileLink.component(), "w 100%, h 34!, hidemode 3");
-        add(textViewer.component(), "w 100%, h 100%");
-        revalidate();
+        panel.removeAll();
+        panel.add(fileLink.component(), "w 100%, h 34!, hidemode 3");
+        panel.add(textViewer.component(), "w 100%, h 100%");
+        panel.revalidate();
     }
 
     private void setDefaultProperties() {
 
-        setLayout(new MigLayout("insets 0, gap 0, flowy"));
-        setBackground(FontsAndBorders.TRANSLUCENT_WHITE_STRONG);
+        panel.setLayout(new MigLayout("insets 0, gap 0, flowy"));
+        panel.setBackground(FontsAndBorders.TRANSLUCENT_WHITE_STRONG);
 
         fileLink.setOpaque(true);
         fileLink.setBackground(Color.DARK_GRAY);
@@ -78,5 +79,10 @@ public class TextFileViewer extends TexturedPanel {
     protected void setFileLinkVisible(boolean b) {
         fileLink.setVisible(b);
         setLayout();
+    }
+
+    @Override
+    public JComponent component() {
+        return panel;
     }
 }
