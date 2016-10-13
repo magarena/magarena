b8ba3e56e40d95b0b55f63577cce81ddbf43a8cd
lodici <lodici123@gmail.com>
2016-10-13 11:29:35 +0100

    CardZoneScreen extends HeaderFooterScreen instead of AbstractScreen.

--- src/magic/ui/screen/duel/player/zone/CardZoneScreen.java
+++ src/magic/ui/screen/duel/player/zone/CardZoneScreen.java
@@ -1,63 +1,33 @@
 package magic.ui.screen.duel.player.zone;
 
-import magic.model.MagicCardList;
-import magic.ui.widget.cards.canvas.CardsCanvas;
-import magic.ui.widget.cards.canvas.CardsCanvas.LayoutMode;
-import magic.ui.screen.interfaces.IActionBar;
-import magic.ui.screen.interfaces.IStatusBar;
-import magic.ui.screen.widget.MenuButton;
-
-import javax.swing.JPanel;
-
 import java.util.Collections;
-import java.util.List;
-import magic.translate.UiString;
-import magic.ui.screen.AbstractScreen;
+import magic.model.MagicCardList;
+import magic.ui.screen.HeaderFooterScreen;
+import magic.ui.widget.cards.canvas.CardsCanvas.LayoutMode;
+import magic.ui.widget.cards.canvas.CardsCanvas;
 
 @SuppressWarnings("serial")
-public class CardZoneScreen
-        extends AbstractScreen
-        implements IStatusBar, IActionBar {
-
-    // translatable strings
-    private static final String _S1 = "Close";
+public class CardZoneScreen extends HeaderFooterScreen {
 
     private final CardsCanvas content;
-    private final String screenCaption;
+    private final MagicCardList cards;
 
-    public CardZoneScreen(final MagicCardList cards, final String zoneName, final boolean animateCards) {
-        this.screenCaption = zoneName;
+    public CardZoneScreen(final MagicCardList cardsIn, final String zoneName, final boolean animateCards) {
+
+        super(zoneName);
+
+        this.cards = new MagicCardList(cardsIn);
         this.content = new CardsCanvas();
-        this.content.setAnimationEnabled(animateCards);
-        this.content.setLayoutMode(LayoutMode.SCALE_TO_FIT);
-        Collections.sort(cards);
-        content.refresh(cards);
-        setContent(content);
+        setDefaultProperties(animateCards);
+
+        Collections.sort(this.cards);
+        this.content.refresh(this.cards);
+
+        setMainContent(this.content);
     }
 
-    @Override
-    public String getScreenCaption() {
-        return screenCaption;
+    private void setDefaultProperties(boolean animateCards) {
+        content.setAnimationEnabled(animateCards);
+        content.setLayoutMode(LayoutMode.SCALE_TO_FIT);
     }
-
-    @Override
-    public MenuButton getLeftAction() {
-        return MenuButton.getCloseScreenButton(UiString.get(_S1));
-    }
-
-    @Override
-    public MenuButton getRightAction() {
-        return null;
-    }
-
-    @Override
-    public List<MenuButton> getMiddleActions() {
-        return null;
-    }
-
-    @Override
-    public JPanel getStatusPanel() {
-        return null;
-    }
-
 }
