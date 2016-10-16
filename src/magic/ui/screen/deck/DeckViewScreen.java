dea6ea2e59d5712d6c6c8994e67565d978d4812a
lodici <lodici123@gmail.com>
2016-10-16 08:01:52 +0100

    DeckViewScreen extends HeaderFooterScreen.

--- src/magic/ui/screen/deck/DeckViewScreen.java
+++ src/magic/ui/screen/deck/DeckViewScreen.java
@@ -1,33 +1,22 @@
 package magic.ui.screen.deck;
 
-import java.awt.event.ActionEvent;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.AbstractAction;
-import javax.swing.JPanel;
 import magic.data.DeckType;
 import magic.data.MagicIcon;
 import magic.model.MagicCardDefinition;
-import magic.ui.MagicImages;
 import magic.model.MagicDeck;
 import magic.ui.ScreenController;
 import magic.translate.UiString;
-import magic.ui.screen.interfaces.IActionBar;
 import magic.ui.screen.interfaces.IDeckConsumer;
-import magic.ui.screen.interfaces.IStatusBar;
-import magic.ui.screen.widget.ActionBarButton;
 import magic.ui.screen.widget.MenuButton;
 import magic.ui.widget.deck.DeckStatusPanel;
-import magic.ui.screen.AbstractScreen;
+import magic.ui.screen.HeaderFooterScreen;
 
 @SuppressWarnings("serial")
-public class DeckViewScreen
-    extends AbstractScreen
-    implements IStatusBar, IActionBar, IDeckConsumer {
+public class DeckViewScreen extends HeaderFooterScreen
+    implements IDeckConsumer {
 
     // translatable strings
-    private static final String _S1 = "Close";
     private static final String _S2 = "Sample Hand";
     private static final String _S3 = "See what kind of Hand you might be dealt from this deck.";
     private static final String _S4 = "A deck with a minimum of 7 cards is required first.";
@@ -40,70 +29,41 @@ public class DeckViewScreen
     private final DeckStatusPanel deckStatusPanel = new DeckStatusPanel();
 
     public DeckViewScreen(final MagicDeck deck, final MagicCardDefinition selectedCard) {
-        setupScreen(deck, selectedCard);
+        super(UiString.get(_S8));
+        screenContent = new DeckViewPanel(deck, selectedCard);
+        setMainContent(screenContent);
+        setHeaderContent(deckStatusPanel);
+        addToFooter(
+                MenuButton.build(this::showSampleHand,
+                        MagicIcon.HAND_ICON, UiString.get(_S2), UiString.get(_S3)),
+                MenuButton.build(this::showDeckImageView, 
+                        MagicIcon.TILED, UiString.get(_S5), UiString.get(_S6))                
+        );
+        setDeck(deck);
     }
 
     public DeckViewScreen(final MagicDeck deck) {
-        setupScreen(deck, null);
+        this(deck, null);
     }
 
-    private void setupScreen(final MagicDeck deck, final MagicCardDefinition selectedCard) {
-        this.screenContent = new DeckViewPanel(deck, selectedCard);
-        setDeck(deck);
-        setContent(this.screenContent);
+    private void showSampleHand() {
+        if (screenContent.getDeck().size() >= 7) {
+            ScreenController.showSampleHandScreen(screenContent.getDeck());
+        } else {
+            showInvalidActionMessage(UiString.get(_S4));
+        }
     }
 
-    @Override
-    public MenuButton getLeftAction() {
-        return MenuButton.getCloseScreenButton(UiString.get(_S1));
-    }
-
-    @Override
-    public MenuButton getRightAction() {
-        return null;
-    }
-
-    @Override
-    public List<MenuButton> getMiddleActions() {
-        final List<MenuButton> buttons = new ArrayList<>();
-        buttons.add(new ActionBarButton(
-                        MagicImages.getIcon(MagicIcon.HAND_ICON),
-                        UiString.get(_S2), UiString.get(_S3),
-                        new AbstractAction() {
-                            @Override
-                            public void actionPerformed(final ActionEvent e) {
-                                if (screenContent.getDeck().size() >= 7) {
-                                    ScreenController.showSampleHandScreen(screenContent.getDeck());
-                                } else {
-                                    showInvalidActionMessage(UiString.get(_S4));
-                                }
-                            }
-                        })
-        );
-        buttons.add(new ActionBarButton(
-                        MagicImages.getIcon(MagicIcon.TILED),
-                        UiString.get(_S5), UiString.get(_S6),
-                        new AbstractAction() {
-                            @Override
-                            public void actionPerformed(final ActionEvent e) {
-                                if (screenContent.getDeck().size() > 0) {
-                                    ScreenController.showDeckView(screenContent.getDeck());
-                                } else {
-                                    showInvalidActionMessage(UiString.get(_S7));
-                                }
-                            }
-                        })
-        );
-        return buttons;
+    private void showDeckImageView() {
+        if (screenContent.getDeck().size() > 0) {
+            ScreenController.showDeckView(screenContent.getDeck());
+        } else {
+            showInvalidActionMessage(UiString.get(_S7));
+        }
     }
 
     private void showInvalidActionMessage(final String message) {
         ScreenController.showWarningMessage(message);
-    }
-
-    @Override
-    public String getScreenCaption() {
-        return UiString.get(_S8);
     }
 
     private void setDeck(final MagicDeck deck) {
@@ -118,10 +78,5 @@ public class DeckViewScreen
 
     @Override
     public void setDeck(String deckName, DeckType deckType) { }
-
-    @Override
-    public JPanel getStatusPanel() {
-        return deckStatusPanel;
-    }
 
 }
