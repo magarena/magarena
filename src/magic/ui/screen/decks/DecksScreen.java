0bdd7375ccc492b760df0b9f74595c23f3cfe8a1
lodici <lodici123@gmail.com>
2016-10-16 08:14:25 +0100

    DecksScreen extends HeaderFooterScreen.

--- src/magic/ui/screen/decks/DecksScreen.java
+++ src/magic/ui/screen/decks/DecksScreen.java
@@ -1,25 +1,16 @@
 package magic.ui.screen.decks;
 
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.AbstractAction;
-import javax.swing.JPanel;
 import magic.data.MagicIcon;
 import magic.translate.UiString;
-import magic.ui.MagicImages;
 import magic.ui.ScreenController;
 import magic.ui.widget.deck.DeckStatusPanel;
 import magic.ui.dialog.DecksFilterDialog;
-import magic.ui.screen.AbstractScreen;
-import magic.ui.screen.interfaces.IActionBar;
+import magic.ui.screen.HeaderFooterScreen;
 import magic.ui.screen.interfaces.IDeckConsumer;
-import magic.ui.screen.interfaces.IStatusBar;
-import magic.ui.screen.widget.ActionBarButton;
 import magic.ui.screen.widget.MenuButton;
 
 @SuppressWarnings("serial")
-public class DecksScreen extends AbstractScreen implements IStatusBar, IActionBar {
+public class DecksScreen extends HeaderFooterScreen {
 
     // translatable strings
     private static final String _S1 = "Decks";
@@ -41,80 +32,53 @@ public class DecksScreen extends AbstractScreen implements IStatusBar, IActionBa
     private final DeckStatusPanel deckStatusPanel;
 
     public DecksScreen(final IDeckConsumer deckConsumer) {
+        super(UiString.get(_S1));
         this.deckConsumer = deckConsumer;
         deckStatusPanel = new DeckStatusPanel();
         screenContent = new ScreenPanel(deckStatusPanel);
-        setContent(screenContent);
-    }
-
-    @Override
-    public String getScreenCaption() {
-        return UiString.get(_S1);
-    }
-
-    @Override
-    public MenuButton getLeftAction() {
-        return MenuButton.getCloseScreenButton(UiString.get(_S2));
-    }
-
-    @Override
-    public MenuButton getRightAction() {
-        return new ActionBarButton(
-            UiString.get(_S3),
-            new AbstractAction() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (screenContent.getDeck() == null) {
-                        showInvalidActionMessage(UiString.get(_S4));
-                    } else if (screenContent.getDeck().isValid() == false) {
-                        showInvalidActionMessage(UiString.get(_S5));
-                    } else {
-                        deckConsumer.setDeck(screenContent.getDeck(), screenContent.getDeckPath());
-                        ScreenController.closeActiveScreen(false);
-                    }
-                }
-            }
+        setMainContent(screenContent);
+        setHeaderContent(deckStatusPanel);
+        setLeftFooter(MenuButton.getCloseScreenButton(UiString.get(_S2)));
+        setRightFooter(MenuButton.build(this::doSelectDeck, UiString.get(_S3)));
+        addToFooter(
+                MenuButton.build(this::showSampleHand,
+                        MagicIcon.HAND_ICON, UiString.get(_S6), UiString.get(_S7)),
+                MenuButton.build(this::showDeckImageView,
+                        MagicIcon.TILED, UiString.get(_S10), UiString.get(_S11))
         );
     }
 
-    @Override
-    public List<MenuButton> getMiddleActions() {
-        final List<MenuButton> buttons = new ArrayList<>();
-        buttons.add(new ActionBarButton(
-            MagicImages.getIcon(MagicIcon.HAND_ICON),
-            UiString.get(_S6), UiString.get(_S7),
-            new AbstractAction() {
-                @Override
-                public void actionPerformed(final ActionEvent e) {
-                    if (screenContent.getDeck() == null || screenContent.getDeck().size() < 7) {
-                        showInvalidActionMessage(UiString.get(_S8));
-                    } else if (screenContent.getDeck().isValid() == false) {
-                        showInvalidActionMessage(UiString.get(_S9));
-                    } else {
-                        ScreenController.showSampleHandScreen(screenContent.getDeck());
-                    }
-                }
-            }
-        ));
-        buttons.add(new ActionBarButton(
-            MagicImages.getIcon(MagicIcon.TILED),
-            UiString.get(_S10), UiString.get(_S11),
-            new AbstractAction() {
-                @Override
-                public void actionPerformed(final ActionEvent e) {
-                    if (screenContent.getDeck() == null || screenContent.getDeck().isEmpty()) {
-                        showInvalidActionMessage(UiString.get(_S12));
-                    } else if (screenContent.getDeck().isValid() == false) {
-                        showInvalidActionMessage(UiString.get(_S13));
-                    } else {
-                        ScreenController.showDeckView(screenContent.getDeck());
-                    }
-                }
-            }
-        ));
-        return buttons;
+    private void doSelectDeck() {
+        if (screenContent.getDeck() == null) {
+            showInvalidActionMessage(UiString.get(_S4));
+        } else if (screenContent.getDeck().isValid() == false) {
+            showInvalidActionMessage(UiString.get(_S5));
+        } else {
+            deckConsumer.setDeck(screenContent.getDeck(), screenContent.getDeckPath());
+            ScreenController.closeActiveScreen(false);
+        }
     }
 
+    private void showSampleHand() {
+        if (screenContent.getDeck() == null || screenContent.getDeck().size() < 7) {
+            showInvalidActionMessage(UiString.get(_S8));
+        } else if (screenContent.getDeck().isValid() == false) {
+            showInvalidActionMessage(UiString.get(_S9));
+        } else {
+            ScreenController.showSampleHandScreen(screenContent.getDeck());
+        }
+    }
+
+    private void showDeckImageView() {
+        if (screenContent.getDeck() == null || screenContent.getDeck().isEmpty()) {
+            showInvalidActionMessage(UiString.get(_S12));
+        } else if (screenContent.getDeck().isValid() == false) {
+            showInvalidActionMessage(UiString.get(_S13));
+        } else {
+            ScreenController.showDeckView(screenContent.getDeck());
+        }
+    }
+    
     private void showInvalidActionMessage(final String message) {
         ScreenController.showWarningMessage(message);
     }
@@ -124,10 +88,4 @@ public class DecksScreen extends AbstractScreen implements IStatusBar, IActionBa
         DecksFilterDialog.resetFilterHistory();
         return true;
     }
-
-    @Override
-    public JPanel getStatusPanel() {
-        return deckStatusPanel;
-    }
-
 }
