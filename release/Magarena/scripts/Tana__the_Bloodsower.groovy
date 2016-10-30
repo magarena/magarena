c4bded810b3d706cf45a41a7282228352de5bf11
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-10-30 17:54:10 +0000

    add Tana, the Bloodsower

---
+++ release/Magarena/scripts/Tana__the_Bloodsower.groovy
@@ -0,0 +1,22 @@
+[
+    new ThisCombatDamagePlayerTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            return new MagicEvent(
+                permanent,
+                damage.getDealtAmount(),
+                this,
+                "PN creates RN 1/1 green Saproling creature tokens."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new PlayTokensAction(
+                event.getPlayer(),
+                CardDefinitions.getToken("1/1 green Saproling creature token"),
+                event.getRefInt()
+            ));
+        }
+    }
+]
