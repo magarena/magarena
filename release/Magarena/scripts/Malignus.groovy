ad2bbb14830e3aa1cb9921e674ec6fd3658a5dee
melvinzhang <melvinzhang@gmail.com>
2016-10-31 11:07:50 +0800

    refer to Excruciator's groovy

--- release/Magarena/scripts/Malignus.groovy
+++ release/Magarena/scripts/Malignus.groovy
@@ -5,14 +5,5 @@
             def amount = (player.getOpponent().getLife() + 1).intdiv(2);
             pt.set(amount,amount);
         }
-    },
-    new IfDamageWouldBeDealtTrigger(MagicTrigger.CANT_BE_PREVENTED) {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
-            if (permanent == damage.getSource()) {
-                damage.setUnpreventable();
-            }
-            return MagicEvent.NONE;
-        }
     }
 ]
