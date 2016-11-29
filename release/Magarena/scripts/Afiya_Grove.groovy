f9bde30b0826a65e504b3b80bb9c842dfa9e011d
kelsioboy <unknown>
2016-11-29 19:59:43 +0800

    add Afiya Grove, closes #968

---
+++ release/Magarena/scripts/Afiya_Grove.groovy
@@ -0,0 +1,23 @@
+[
+    new AtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent source, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                source,
+                POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN moves a +1/+1 counter from SN onto target creature\$."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                if (event.getPermanent().hasCounters(MagicCounterType.PlusOne)) {
+                    game.doAction(new ChangeCountersAction(event.getPermanent(),MagicCounterType.PlusOne,-1));
+                    game.doAction(new ChangeCountersAction(it,MagicCounterType.PlusOne,1));
+                }
+            });
+        }
+    }
+]
