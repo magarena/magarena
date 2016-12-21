043f8a7395ad49ed3af8a181d61eee30eadaa465
ShawnieBoy <ShawnieBoyUK@gmail.com>
2016-12-21 01:28:52 +0000

    reparse script folders A-Z(J)

--- release/Magarena/scripts/Jinxed_Ring.groovy
+++ release/Magarena/scripts/Jinxed_Ring.groovy
@@ -15,35 +15,5 @@
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             game.doAction(new DealDamageAction(event.getPermanent(), event.getPlayer(), 1));
         }
-    },
-
-    new MagicPermanentActivation(
-        new MagicActivationHints(MagicTiming.Removal),
-        "Control"
-    ) {
-
-        @Override
-        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
-            return [
-                new MagicSacrificePermanentEvent(source, SACRIFICE_CREATURE)
-            ]
-        }
-
-        @Override
-        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                source,
-                TARGET_OPPONENT,
-                this,
-                "Target opponent\$ gains control of SN."
-            )
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPlayer(game, {
-                game.doAction(new GainControlAction(it, event.getPermanent()))
-            })
-        }
     }
 ]
