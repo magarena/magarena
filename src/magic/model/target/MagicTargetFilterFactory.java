5f60251d5ae5b11f882f3d28273d5d3f943df0bb
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-01-13 15:16:48 +0000

    add 'creature card in a graveyard' to MagicTargetFilterFactory

--- src/magic/model/target/MagicTargetFilterFactory.java
+++ src/magic/model/target/MagicTargetFilterFactory.java
@@ -2267,6 +2267,7 @@ public class MagicTargetFilterFactory {
         addp("instant, sorcery, or creature card", INSTANT_SORCERY_OR_CREATURE_CARD);
         addp("artifact or creature card", ARTIFACT_OR_CREATURE_CARD);
         addp("colorless creature card", COLORLESS_CREATURE_CARD);
+        addp("creature card in a graveyard", CREATURE_CARD_FROM_GRAVEYARD);
 
         // ... card
         addp("instant or sorcery card", INSTANT_OR_SORCERY_CARD);
