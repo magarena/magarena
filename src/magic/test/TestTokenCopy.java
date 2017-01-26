ed777a7465343aa456a0f6f3429f747afcbdd4bc
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-01-26 15:12:20 +0000

    add TestTokenCopy #1040 #1044

---
+++ src/magic/test/TestTokenCopy.java
@@ -0,0 +1,38 @@
+package magic.test;
+
+import magic.model.MagicDuel;
+import magic.model.MagicGame;
+import magic.model.MagicPlayer;
+
+/**
+ * Displays ways of generating a token copy of a permanent -
+ * Issue #1040 regarding Seance, and #1044 regarding their display with cardbuilder
+ */
+
+class TestTokenCopy extends TestGameBuilder {
+    public MagicGame getGame() {
+        final MagicDuel duel=createDuel();
+        final MagicGame game=duel.nextGame();
+        final MagicPlayer player=game.getPlayer(0);
+        final MagicPlayer opponent=game.getPlayer(1);
+
+        MagicPlayer P = player;
+
+        P.setLife(6);
+
+        addToLibrary(P, "Forest", 20);
+        addToGraveyard(P,"Captain of the Watch");
+        createPermanent(P, "Forest", 10);
+        createPermanent(P, "Chromatic Lantern");
+        createPermanent(P, "Cogwork Assembler");
+        createPermanent(P, "Séance");
+        createPermanent(P, "Myr Propagator");
+
+        P = opponent;
+
+        P.setLife(6);
+        addToLibrary(P, "Mountain", 20);
+
+        return game;
+    }
+}
