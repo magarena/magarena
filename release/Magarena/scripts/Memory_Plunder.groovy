ffeb91de73d28d3a209c89f098d2bdef8a565e76
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-01-29 01:21:30 +0000

    add Memory Plunder

---
+++ release/Magarena/scripts/Memory_Plunder.groovy
@@ -0,0 +1,29 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(
+                    TARGET_INSTANT_OR_SORCERY_CARD_FROM_OPPONENTS_GRAVEYARD
+                ),
+                MagicGraveyardTargetPicker.PutOntoBattlefield,
+                this,
+                "PN may\$ cast target instant or sorcery card an opponent's graveyard\$ without paying its mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                event.processTargetCard(game, {
+                    game.doAction(CastCardAction.WithoutManaCost(
+                        event.getPlayer(),
+                        it,
+                        MagicLocationType.OpponentsGraveyard,
+                        MagicLocationType.OpponentsGraveyard
+                    ));
+                });
+            }
+        }
+    }
+]
