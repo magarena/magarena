5feb0b529dd92e9ff0ea8032a4effca8641db5c5
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-02-04 00:06:05 +0000

    add Mechanized Production

---
+++ release/Magarena/scripts/Mechanized_Production.groovy
@@ -0,0 +1,33 @@
+[
+    new AtYourUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPlayer upkeepPlayer) {
+            final MagicPermanent enchanted = permanent.getEnchantedPermanent()
+            return enchanted.isValid() ? 
+                new MagicEvent(
+                    permanent,
+                    enchanted,
+                    this,
+                    "PN creates a token that's a copy RN. "+
+                    "Then if PN controls eight or more artifacts with the same name as one another, PN wins the game."
+                ) :
+            MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicPlayer player = event.getPlayer();
+            game.doAction(new PlayTokenAction(player, event.getRefPermanent()));
+            final HashMap<String, Integer> names = new HashMap<>();
+            ARTIFACT_YOU_CONTROL.filter(player) each {
+                if (names.containsKey(it.getName())) {
+                    names.put(it.getName(), names.get(it.getName())+1);
+                } else {
+                    names.put(it.getName(), 1);  
+                }
+            }
+            if (Collections.max(names.values()) >=8) {
+                game.doAction(new LoseGameAction(player.getOpponent()));
+            }
+        }
+    }
+]
