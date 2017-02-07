b15bc4116503f0514c2ffb32d4ccf7a1642b45a9
lodici <lodici123@gmail.com>
2017-02-07 21:31:44 +0000

    Extract screen content panel class out of NewDuelSettingsScreen.

--- src/magic/ui/screen/duel/setup/NewDuelSettingsScreen.java
+++ src/magic/ui/screen/duel/setup/NewDuelSettingsScreen.java
@@ -1,27 +1,12 @@
 package magic.ui.screen.duel.setup;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.nio.file.Path;
-import javax.swing.JPanel;
-import magic.data.DeckType;
-import magic.utility.DeckUtils;
 import magic.data.DuelConfig;
-import magic.data.MagicFormat;
 import magic.exception.InvalidDeckException;
-import magic.model.MagicDeck;
-import magic.model.player.IPlayerProfileListener;
-import magic.model.player.PlayerProfile;
-import magic.model.player.PlayerProfiles;
-import magic.ui.ScreenController;
 import magic.translate.MText;
-import magic.ui.screen.widget.DuelSettingsPanel;
-import magic.ui.screen.widget.MenuButton;
-import magic.ui.utility.MagicStyle;
+import magic.ui.ScreenController;
 import magic.ui.WikiPage;
-import magic.ui.helpers.MouseHelper;
 import magic.ui.screen.HeaderFooterScreen;
-import net.miginfocom.swing.MigLayout;
+import magic.ui.screen.widget.MenuButton;
 
 @SuppressWarnings("serial")
 public class NewDuelSettingsScreen extends HeaderFooterScreen {
@@ -37,13 +22,13 @@ public class NewDuelSettingsScreen extends HeaderFooterScreen {
     private static final int PLAYERS_COUNT = 2;
     private static final DuelConfig duelConfig = DuelConfig.getInstance();
 
-    private final ScreenContent content;
+    private final ScreenContentPanel contentPanel;
 
     public NewDuelSettingsScreen() {
         super(MText.get(_S1));
         duelConfig.load();
-        content = new ScreenContent(duelConfig);
-        setMainContent(content);
+        contentPanel = new ScreenContentPanel(duelConfig);
+        setMainContent(contentPanel);
         setLeftFooter(MenuButton.getCloseScreenButton(MText.get(_S2)));
         setRightFooter(MenuButton.build(this::doNextAction, MText.get(_S3)));
         setWikiPage(WikiPage.NEW_DUEL);
@@ -64,12 +49,12 @@ public class NewDuelSettingsScreen extends HeaderFooterScreen {
     private boolean isEachPlayerDeckValid(final boolean showErrorDialog) {
         boolean isEachDeckValid = true;
         final StringBuffer sb = new StringBuffer();
-        if (!content.isDeckValid(0)) {
-            sb.append(MText.get(_S5, content.getPlayerProfile(0).getPlayerName())).append("\n");
+        if (!contentPanel.isDeckValid(0)) {
+            sb.append(MText.get(_S5, contentPanel.getPlayerProfile(0).getPlayerName())).append("\n");
             isEachDeckValid = false;
         }
-        if (!content.isDeckValid(1)) {
-            sb.append(MText.get(_S5, content.getPlayerProfile(1).getPlayerName()));
+        if (!contentPanel.isDeckValid(1)) {
+            sb.append(MText.get(_S5, contentPanel.getPlayerProfile(1).getPlayerName()));
             isEachDeckValid = false;
         }
         if (!isEachDeckValid && showErrorDialog) {
@@ -80,13 +65,13 @@ public class NewDuelSettingsScreen extends HeaderFooterScreen {
     }
 
     private void updateDuelConfig() {
-        duelConfig.setStartLife(content.getStartLife());
-        duelConfig.setHandSize(content.getHandSize());
-        duelConfig.setNrOfGames(content.getNrOfGames());
-        duelConfig.setCube(content.getCube());
+        duelConfig.setStartLife(contentPanel.getStartLife());
+        duelConfig.setHandSize(contentPanel.getHandSize());
+        duelConfig.setNrOfGames(contentPanel.getNrOfGames());
+        duelConfig.setCube(contentPanel.getCube());
         for (int i = 0; i < PLAYERS_COUNT; i++) {
-            duelConfig.setPlayerProfile(i, content.getPlayerProfile(i));
-            duelConfig.setPlayerDeckProfile(i, content.getDeckType(i), content.getDeckValue(i));
+            duelConfig.setPlayerProfile(i, contentPanel.getPlayerProfile(i));
+            duelConfig.setPlayerDeckProfile(i, contentPanel.getDeckType(i), contentPanel.getDeckValue(i));
         }
     }
 
@@ -97,148 +82,4 @@ public class NewDuelSettingsScreen extends HeaderFooterScreen {
         }
         return true;
     }
-
-    private class ScreenContent extends JPanel implements IPlayerProfileListener {
-
-        private final MigLayout migLayout = new MigLayout();
-        private final DuelSettingsPanel duelSettingsPanel;
-        private final DuelPlayerPanel[] playerPanels = new DuelPlayerPanel[PLAYERS_COUNT];
-        private final DuelPlayerDeckPanel[] newPlayerDeckPanels = new DuelPlayerDeckPanel[PLAYERS_COUNT];
-
-        public ScreenContent(final DuelConfig config) {
-            this.duelSettingsPanel = new DuelSettingsPanel(config);
-            for (int i = 0; i < PLAYERS_COUNT; i++) {
-                this.playerPanels[i] = getNewDuelPlayerPanel(config.getPlayerProfile(i));
-                this.newPlayerDeckPanels[i] = new DuelPlayerDeckPanel(config.getPlayerDeckProfile(i));
-            }
-            setLookAndFeel();
-            refreshLayout();
-        }
-
-        private void setLookAndFeel() {
-            setOpaque(false);
-            setLayout(migLayout);
-        }
-
-        private void refreshLayout() {
-            migLayout.setLayoutConstraints("insets 0, center, center, wrap 2");
-            migLayout.setColumnConstraints("[310, fill]6[310, fill]");
-            migLayout.setRowConstraints("[40, fill]6[270, fill]4[60, fill]");
-            add(duelSettingsPanel, "spanx 2");
-            add(playerPanels[0]);
-            add(playerPanels[1]);
-            add(newPlayerDeckPanels[0]);
-            add(newPlayerDeckPanels[1]);
-        }
-
-        private DuelPlayerPanel getNewDuelPlayerPanel(final PlayerProfile player) {
-            final DuelPlayerPanel panel = new DuelPlayerPanel(player);
-            panel.addMouseListener(getDuelPlayerPanelMouseAdapter(panel));
-            return panel;
-        }
-
-        private MouseAdapter getDuelPlayerPanelMouseAdapter(final DuelPlayerPanel panel) {
-            return new MouseAdapter() {
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                    MouseHelper.showBusyCursor();
-                    selectNewProfile(panel.getPlayer());
-                    mouseExited(e);
-                    MouseHelper.showDefaultCursor();
-                }
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    MagicStyle.setHightlight(panel, true);
-                }
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    MagicStyle.setHightlight(panel, false);
-                }
-            };
-        }
-
-        private void selectNewProfile(final PlayerProfile playerProfile) {
-            if (playerProfile.isHuman()) {
-                ScreenController.showSelectHumanPlayerScreen(this, playerProfile);
-            } else {
-                ScreenController.showSelectAiProfileScreen(this, playerProfile);
-            }
-        }
-
-        public MagicFormat getCube() {
-            return duelSettingsPanel.getCube();
-        }
-
-        public int getNrOfGames() {
-            return duelSettingsPanel.getNrOfGames();
-        }
-
-        public int getHandSize() {
-            return duelSettingsPanel.getHandSize();
-        }
-
-        public int getStartLife() {
-            return duelSettingsPanel.getStartLife();
-        }
-
-        public String getDeckValue(final int playerIndex) {
-            return newPlayerDeckPanels[playerIndex].getDeckValue();
-        }
-        public DeckType getDeckType(final int playerIndex) {
-            return newPlayerDeckPanels[playerIndex].getDeckType();
-        }
-
-        public PlayerProfile getPlayerProfile(final int index) {
-            return playerPanels[index].getPlayer();
-        }
-
-        private DuelPlayerPanel getDuelPlayerPanel(final PlayerProfile player) {
-            return playerPanels[player.isHuman() ? 0 : 1];
-        }
-
-        @Override
-        public void PlayerProfileUpdated(PlayerProfile player) {
-            getDuelPlayerPanel(player).setPlayer(player);
-        }
-
-        @Override
-        public void PlayerProfileDeleted(PlayerProfile deletedPlayer) {
-            if (deletedPlayer.isHuman()) {
-                final PlayerProfile playerProfile = PlayerProfiles.getDefaultHumanPlayer();
-                DuelConfig.getInstance().setPlayerProfile(0, playerProfile);
-                getDuelPlayerPanel(playerProfile).setPlayer(playerProfile);
-            } else {
-                final PlayerProfile playerProfile = PlayerProfiles.getDefaultAiPlayer();
-                DuelConfig.getInstance().setPlayerProfile(1,  playerProfile);
-                getDuelPlayerPanel(playerProfile).setPlayer(playerProfile);
-            }
-        }
-
-        @Override
-        public void PlayerProfileSelected(PlayerProfile player) {
-            getDuelPlayerPanel(player).setPlayer(player);
-            saveSelectedPlayerProfile(player);
-        }
-
-        private void saveSelectedPlayerProfile(final PlayerProfile player) {
-            DuelConfig.getInstance().setPlayerProfile(player.isHuman() ? 0 : 1, player);
-        }
-
-        private boolean isDeckValid(int i) {
-            if (getDeckType(i) != DeckType.Random) {
-                final String deckFilename = getDeckValue(i) + DeckUtils.DECK_EXTENSION;
-                final Path deckFolder = DeckType.getDeckFolder(getDeckType(i));
-                try {
-                    final MagicDeck deck = DeckUtils.loadDeckFromFile(deckFolder.resolve(deckFilename));
-                    return deck.isValid();
-                } catch (InvalidDeckException ex) {
-                    return false;
-                }
-            } else {
-                return true;
-            }
-        }
-
-    }
-
 }
---
+++ src/magic/ui/screen/duel/setup/ScreenContentPanel.java
@@ -0,0 +1,169 @@
+package magic.ui.screen.duel.setup;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.nio.file.Path;
+import javax.swing.JPanel;
+import magic.data.DeckType;
+import magic.data.DuelConfig;
+import magic.data.MagicFormat;
+import magic.exception.InvalidDeckException;
+import magic.model.MagicDeck;
+import magic.model.player.IPlayerProfileListener;
+import magic.model.player.PlayerProfile;
+import magic.model.player.PlayerProfiles;
+import magic.ui.ScreenController;
+import magic.ui.helpers.MouseHelper;
+import magic.ui.screen.widget.DuelSettingsPanel;
+import magic.ui.utility.MagicStyle;
+import magic.utility.DeckUtils;
+import net.miginfocom.swing.MigLayout;
+
+@SuppressWarnings("serial")
+class ScreenContentPanel extends JPanel implements IPlayerProfileListener {
+
+    private static final int PLAYERS_COUNT = 2;
+
+    private final MigLayout migLayout = new MigLayout();
+    private final DuelSettingsPanel duelSettingsPanel;
+    private final DuelPlayerPanel[] playerPanels = new DuelPlayerPanel[PLAYERS_COUNT];
+    private final DuelPlayerDeckPanel[] newPlayerDeckPanels = new DuelPlayerDeckPanel[PLAYERS_COUNT];
+
+    ScreenContentPanel(final DuelConfig config) {
+        this.duelSettingsPanel = new DuelSettingsPanel(config);
+        for (int i = 0; i < PLAYERS_COUNT; i++) {
+            this.playerPanels[i] = getNewDuelPlayerPanel(config.getPlayerProfile(i));
+            this.newPlayerDeckPanels[i] = new DuelPlayerDeckPanel(config.getPlayerDeckProfile(i));
+        }
+        setLookAndFeel();
+        refreshLayout();
+    }
+
+    private void setLookAndFeel() {
+        setOpaque(false);
+        setLayout(migLayout);
+    }
+
+    private void refreshLayout() {
+        migLayout.setLayoutConstraints("insets 0, center, center, wrap 2");
+        migLayout.setColumnConstraints("[310, fill]6[310, fill]");
+        migLayout.setRowConstraints("[40, fill]6[270, fill]4[60, fill]");
+        add(duelSettingsPanel, "spanx 2");
+        add(playerPanels[0]);
+        add(playerPanels[1]);
+        add(newPlayerDeckPanels[0]);
+        add(newPlayerDeckPanels[1]);
+    }
+
+    private DuelPlayerPanel getNewDuelPlayerPanel(final PlayerProfile player) {
+        final DuelPlayerPanel panel = new DuelPlayerPanel(player);
+        panel.addMouseListener(getDuelPlayerPanelMouseAdapter(panel));
+        return panel;
+    }
+
+    private MouseAdapter getDuelPlayerPanelMouseAdapter(final DuelPlayerPanel panel) {
+        return new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                MouseHelper.showBusyCursor();
+                selectNewProfile(panel.getPlayer());
+                mouseExited(e);
+                MouseHelper.showDefaultCursor();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                MagicStyle.setHightlight(panel, true);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                MagicStyle.setHightlight(panel, false);
+            }
+        };
+    }
+
+    private void selectNewProfile(final PlayerProfile playerProfile) {
+        if (playerProfile.isHuman()) {
+            ScreenController.showSelectHumanPlayerScreen(this, playerProfile);
+        } else {
+            ScreenController.showSelectAiProfileScreen(this, playerProfile);
+        }
+    }
+
+    MagicFormat getCube() {
+        return duelSettingsPanel.getCube();
+    }
+
+    int getNrOfGames() {
+        return duelSettingsPanel.getNrOfGames();
+    }
+
+    int getHandSize() {
+        return duelSettingsPanel.getHandSize();
+    }
+
+    int getStartLife() {
+        return duelSettingsPanel.getStartLife();
+    }
+
+    String getDeckValue(final int playerIndex) {
+        return newPlayerDeckPanels[playerIndex].getDeckValue();
+    }
+
+    DeckType getDeckType(final int playerIndex) {
+        return newPlayerDeckPanels[playerIndex].getDeckType();
+    }
+
+    PlayerProfile getPlayerProfile(final int index) {
+        return playerPanels[index].getPlayer();
+    }
+
+    private DuelPlayerPanel getDuelPlayerPanel(final PlayerProfile player) {
+        return playerPanels[player.isHuman() ? 0 : 1];
+    }
+
+    @Override
+    public void PlayerProfileUpdated(PlayerProfile player) {
+        getDuelPlayerPanel(player).setPlayer(player);
+    }
+
+    @Override
+    public void PlayerProfileDeleted(PlayerProfile deletedPlayer) {
+        if (deletedPlayer.isHuman()) {
+            final PlayerProfile playerProfile = PlayerProfiles.getDefaultHumanPlayer();
+            DuelConfig.getInstance().setPlayerProfile(0, playerProfile);
+            getDuelPlayerPanel(playerProfile).setPlayer(playerProfile);
+        } else {
+            final PlayerProfile playerProfile = PlayerProfiles.getDefaultAiPlayer();
+            DuelConfig.getInstance().setPlayerProfile(1, playerProfile);
+            getDuelPlayerPanel(playerProfile).setPlayer(playerProfile);
+        }
+    }
+
+    @Override
+    public void PlayerProfileSelected(PlayerProfile player) {
+        getDuelPlayerPanel(player).setPlayer(player);
+        saveSelectedPlayerProfile(player);
+    }
+
+    private void saveSelectedPlayerProfile(final PlayerProfile player) {
+        DuelConfig.getInstance().setPlayerProfile(player.isHuman() ? 0 : 1, player);
+    }
+
+    boolean isDeckValid(int i) {
+        if (getDeckType(i) != DeckType.Random) {
+            final String deckFilename = getDeckValue(i) + DeckUtils.DECK_EXTENSION;
+            final Path deckFolder = DeckType.getDeckFolder(getDeckType(i));
+            try {
+                final MagicDeck deck = DeckUtils.loadDeckFromFile(deckFolder.resolve(deckFilename));
+                return deck.isValid();
+            } catch (InvalidDeckException ex) {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+}
