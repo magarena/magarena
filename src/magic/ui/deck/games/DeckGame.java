50d10998682ee6450f342e6bab322c490a2e6217
lodici <lodici123@gmail.com>
2017-02-23 15:55:54 +0000

    Use a more readable approximation for when a game was played.

--- src/magic/ui/deck/games/DeckGame.java
+++ src/magic/ui/deck/games/DeckGame.java
@@ -3,8 +3,7 @@ package magic.ui.deck.games;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 import magic.data.stats.GameStatsInfo;
 import magic.model.MagicDeck;
 import magic.model.player.PlayerProfile;
@@ -14,12 +13,24 @@ import magic.translate.MText;
 class DeckGame {
 
     // translatable UI text (prefix with _S).
-    private static final String _S1 = "version";
     private static final String _S2 = "<br>level: %d, +life: %d";
     private static final String _S3 = "life: %d<br>hand: %d";
     private static final String _S4 = "won in";
     private static final String _S5 = "lost in";
     private static final String _S6 = "conceded";
+    private static final String _S7 = "%d years ago";
+    private static final String _S8 = "%d year ago";
+    private static final String _S9 = "%d months ago";
+    private static final String _S10 = "%d month ago";
+    private static final String _S11 = "%d weeks ago";
+    private static final String _S12 = "%d week ago";
+    private static final String _S13 = "%d days ago";
+    private static final String _S14 = "%d day ago";
+    private static final String _S15 = "%d hours ago";
+    private static final String _S16 = "%d hour ago";
+    private static final String _S17 = "%d minutes ago";
+    private static final String _S18 = "%d minute ago";
+    private static final String _S19 = "a few seconds ago";
 
     private final GameStatsInfo dto;
     private final MagicDeck deck;
@@ -37,9 +48,35 @@ class DeckGame {
     }
 
     String getGameInfo() {
-        LocalDateTime ldt = getLocalTimeFromEpoch(dto.timeStart);
-        String start = ldt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
-        return start + String.format("<br>%s %s", MText.get(_S1), dto.magarenaVersion);
+
+        LocalDateTime timeStart = getLocalTimeFromEpoch(dto.timeStart);
+        LocalDateTime timeEnd = LocalDateTime.now();
+
+        long years = ChronoUnit.YEARS.between(timeStart, timeEnd);
+        if (years > 0) {
+            return years > 1 ? MText.get(_S7, years): MText.get(_S8, years);
+        }
+        long months = ChronoUnit.MONTHS.between(timeStart, timeEnd);
+        if (months > 0) {
+            return months > 1 ? MText.get(_S9, months): MText.get(_S10, months);
+        }
+        long weeks = ChronoUnit.WEEKS.between(timeStart, timeEnd);
+        if (weeks > 0) {
+            return weeks > 1 ? MText.get(_S11, weeks): MText.get(_S12, weeks);
+        }
+        long days = ChronoUnit.DAYS.between(timeStart, timeEnd);
+        if (days > 0) {
+            return days > 1 ? MText.get(_S13, days): MText.get(_S14, days);
+        }
+        long hours = ChronoUnit.HOURS.between(timeStart, timeEnd);
+        if (hours > 0) {
+            return hours > 1 ? MText.get(_S15, hours): MText.get(_S16, hours);
+        }
+        long minutes = ChronoUnit.MINUTES.between(timeStart, timeEnd);
+        if (minutes > 0) {
+            return minutes > 1 ? MText.get(_S17, minutes): MText.get(_S18, minutes);
+        }
+        return _S19;
     }
 
     String getOpponentInfo() {
