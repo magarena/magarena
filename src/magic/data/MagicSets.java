6cc3a612000ef42935414785b5abdab2afb877aa
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-04-14 17:40:51 +0100

    add Amonkhet card list and set

--- src/magic/data/MagicSets.java
+++ src/magic/data/MagicSets.java
@@ -4,6 +4,7 @@ public enum MagicSets {
 
     // Release date string should be in YYYY-MM-DD format.
 
+    AKH("Amonkhet", "2017-04-28"),
     MM3("Modern Masters 2017 Edition", "2017-03-17"),
     AER("Aether Revolt", "2017-01-20"),
     C16("Commander (2016 Edition)", "2016-11-11"),
