4bfa10f382c36f12f7d85c0b21111a4d29c76019
melvinzhang <melvinzhang@gmail.com>
2017-04-23 19:53:54 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Grand_Arbiter_Augustin_IV.groovy
+++ release/Magarena/scripts/Grand_Arbiter_Augustin_IV.groovy
@@ -1,24 +1,4 @@
 [
-    new MagicStatic(MagicLayer.CostReduction) {
-        @Override
-        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
-            if (card.hasColor(MagicColor.White) && source.isFriend(card)) {
-                return cost.reduce(1);
-            } else {
-                return cost;
-            }
-        }
-    },
-    new MagicStatic(MagicLayer.CostReduction) {
-        @Override
-        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
-            if (card.hasColor(MagicColor.Blue) && source.isFriend(card)) {
-                return cost.reduce(1);
-            } else {
-                return cost;
-            }
-        }
-    },
     new MagicStatic(MagicLayer.CostIncrease) {
         @Override
         public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
