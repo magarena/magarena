60a454a5aefef69a29a6981fa0c9c2917cb01111
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-04-23 03:42:09 +0100

    add AKH Monuments

---
+++ release/Magarena/scripts/Rhonas_s_Monument.groovy
@@ -0,0 +1,12 @@
+[
+    new MagicStatic(MagicLayer.CostReduction) {
+        @Override
+        public MagicManaCost reduceCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
+            if (card.hasColor(MagicColor.Green) && card.hasType(MagicType.Creature) && source.isFriend(card)) {
+                return cost.reduce(1);
+            } else {
+                return cost;
+            }
+        }
+    }
+]
