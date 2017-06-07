c6b458fca5cd3644860276a7f50f80758a26c4d0
lodici <lodici123@gmail.com>
2017-06-07 08:44:28 +0100

    Increase dialog save/button width to fit french translation.

--- src/magic/ui/dialog/button/MagicDialogButton.java
+++ src/magic/ui/dialog/button/MagicDialogButton.java
@@ -6,7 +6,7 @@ import javax.swing.JButton;
 @SuppressWarnings("serial")
 public class MagicDialogButton extends JButton {
 
-    private static final Dimension PREFERRED_SIZE = new Dimension(110, 30);
+    private static final Dimension PREFERRED_SIZE = new Dimension(130, 30);
 
     public MagicDialogButton(String text) {
         super(text);
