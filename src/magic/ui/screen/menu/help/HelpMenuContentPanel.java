3cbdc7f8700a778504d0e113042648cbc61b4271
lodici <lodici123@gmail.com>
2017-06-08 20:51:05 +0100

    implements new style for HelpMenuScreen (see #1143).

--- src/magic/ui/screen/menu/help/HelpMenuContentPanel.java
+++ src/magic/ui/screen/menu/help/HelpMenuContentPanel.java
@@ -3,27 +3,26 @@ package magic.ui.screen.menu.help;
 import magic.translate.MText;
 import magic.ui.ScreenController;
 import magic.ui.helpers.UrlHelper;
-import magic.ui.screen.menu.MenuScreenContentPanel;
+import magic.ui.screen.menu.main.NewMenuScreenContentPanel;
 
 @SuppressWarnings("serial")
-class HelpMenuContentPanel extends MenuScreenContentPanel {
+class HelpMenuContentPanel extends NewMenuScreenContentPanel {
 
     // translatable strings
-    private static final String _S1 = "Help Menu";
     private static final String _S2 = "ReadMe";
     private static final String _S3 = "Online help";
     private static final String _S4 = "Keywords glossary";
     private static final String _S5 = "About Magarena";
-    private static final String _S6 = "Close menu";
+    private static final String _S6 = "Main menu";
 
     HelpMenuContentPanel() {
-        super(MText.get(_S1), true);
+        super(false);
         addMenuItem(MText.get(_S2), this::onReadMeMenu);
         addMenuItem(MText.get(_S3), this::onOnlineHelpMenu);
         addMenuItem(MText.get(_S4), this::onKeywordsMenu);
         addMenuItem(MText.get(_S5), this::onAboutMenu);
         addSpace();
-        addMenuItem(MText.get(_S6), this::onCloseMenu);
+        addMenuItem(MText.get(_S6), this::doCloseMenu);
         refreshMenuLayout();
     }
 
@@ -43,7 +42,7 @@ class HelpMenuContentPanel extends MenuScreenContentPanel {
         ScreenController.showAboutScreen();
     }
 
-    void onCloseMenu() {
+    private void doCloseMenu() {
         ScreenController.closeActiveScreen(false);
     }
 }
--- src/magic/ui/screen/menu/help/HelpMenuScreen.java
+++ src/magic/ui/screen/menu/help/HelpMenuScreen.java
@@ -1,11 +1,17 @@
 package magic.ui.screen.menu.help;
 
-import magic.ui.screen.MScreen;
+import magic.translate.MText;
+import magic.ui.screen.HeaderFooterScreen;
 
 @SuppressWarnings("serial")
-public class HelpMenuScreen extends MScreen {
+public class HelpMenuScreen extends HeaderFooterScreen {
+
+    // translatable strings
+    private static final String _S1 = "Help";
 
     public HelpMenuScreen() {
+        super(MText.get(_S1));
+        setLeftFooter(null);
         setMainContent(new HelpMenuContentPanel());
     }
 }
