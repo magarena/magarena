ac4ffe5ab8371c0dc3340f2e7d9d9e4580607d4c
lodici <lodici123@gmail.com>
2017-06-14 09:35:01 +0100

    fix: mouse cursor set to hand icon on edge of selectable stack item.

--- src/magic/ui/widget/duel/player/PlayerAvatarButton.java
+++ src/magic/ui/widget/duel/player/PlayerAvatarButton.java
@@ -1,6 +1,9 @@
 package magic.ui.widget.duel.player;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import magic.ui.duel.viewerinfo.PlayerViewerInfo;
+import magic.ui.helpers.MouseHelper;
 import magic.ui.screen.duel.game.SwingGameController;
 import magic.ui.widget.ChoiceBorderPanelButton;
 
@@ -17,6 +20,19 @@ class PlayerAvatarButton extends ChoiceBorderPanelButton {
         avatarPanel = new PlayerImagePanel(playerInfo);
         avatarPanel.setOpaque(false);
         setComponent(avatarPanel);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(final MouseEvent event) {
+                if (isValidChoice) {
+                    MouseHelper.showHandCursor(PlayerAvatarButton.this);
+                }
+            }
+            @Override
+            public void mouseExited(final MouseEvent event) {
+                MouseHelper.showDefaultCursor(PlayerAvatarButton.this);
+            }
+        });
     }
 
     void showAsValidChoice(boolean isValid) {
