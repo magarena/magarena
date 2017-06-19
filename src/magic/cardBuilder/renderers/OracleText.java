13630c0e6e981e1ab0c1cf893c13bbc0cb09feaa
lodici <lodici123@gmail.com>
2017-06-19 12:39:46 +0100

    remove unused fields in OracleText.

--- src/magic/cardBuilder/renderers/OracleText.java
+++ src/magic/cardBuilder/renderers/OracleText.java
@@ -23,10 +23,9 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
 import magic.awt.MagicFont;
-import magic.cardBuilder.ResourceManager;
 import magic.cardBuilder.CardResource;
+import magic.cardBuilder.ResourceManager;
 import magic.model.IRenderableCard;
 import magic.model.MagicColor;
 import magic.model.MagicType;
@@ -36,9 +35,6 @@ import magic.ui.helpers.ImageHelper;
 public class OracleText {
 
     public static final char NEWLINE = '\n';
-    private static final int maxDistance = 260100;
-    private static final double transparencyTolerance = 0.001;
-    private static final int padding = 1;
     private static final Font cardTextFont = MagicFont.MPlantin.get().deriveFont(Font.PLAIN, 18);//scale down when long string
     private static final int topPadding = 7;
     private static final int leftPadding = 3;
