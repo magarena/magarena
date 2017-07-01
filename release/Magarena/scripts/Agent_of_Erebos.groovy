2b73f55abfb7c02bcef6e2e3ab6ea3aa50813437
melvinzhang <melvinzhang@gmail.com>
2017-07-01 10:48:47 +0800

    merge the two cases into one

--- release/Magarena/scripts/Agent_of_Erebos.groovy
+++ release/Magarena/scripts/Agent_of_Erebos.groovy
@@ -1,37 +1,18 @@
 [
-    new EntersBattlefieldTrigger() {
+    new OtherEntersBattlefieldTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+        public boolean accept(final MagicPermanent permanent, final MagicPermanent played) {
+            return permanent == played || (played.hasType(MagicType.Enchantment) && player.isFriend(permanent);
+        }
+
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
             return new MagicEvent(
                 permanent,
                 NEG_TARGET_PLAYER,
                 this,
                 "Exile all cards from target player's\$ graveyard."
             );
-        }
-
-        @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTargetPlayer(game, {
-                final MagicCardList graveyard = new MagicCardList(it.getGraveyard());
-                for (final MagicCard cardGraveyard : graveyard) {
-                    game.doAction(new ShiftCardAction(cardGraveyard, MagicLocationType.Graveyard, MagicLocationType.Exile));
-                }
-            });
-        }
-    },
-    
-    new OtherEntersBattlefieldTrigger() {
-        @Override
-        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent otherPermanent) {
-            return (otherPermanent.hasType(MagicType.Enchantment) && otherPermanent.isFriend(permanent) && otherPermanent != permanent) ?
-                new MagicEvent(
-                    permanent,
-                    NEG_TARGET_PLAYER,
-                    this,
-                    "Exile all cards from target player's\$ graveyard."
-                ):
-                MagicEvent.NONE;
         }
 
         @Override
