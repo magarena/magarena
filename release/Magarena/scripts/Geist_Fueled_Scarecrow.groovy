26857c8b081331ac8e3613c77fc708982044e098
melvinzhang <melvinzhang@gmail.com>
2017-07-16 12:50:31 +0800

    replace groovy code with card script

--- release/Magarena/scripts/Geist_Fueled_Scarecrow.groovy
+++
@@ -1,8 +0,0 @@
-[
-    new MagicStatic(MagicLayer.CostIncrease) {
-        @Override
-        public MagicManaCost increaseCost(final MagicPermanent source, final MagicCard card, final MagicManaCost cost) {
-            return card.isCreature() && source.isFriend(card) ? cost.increase(1) : cost;
-        }
-    }
-]
