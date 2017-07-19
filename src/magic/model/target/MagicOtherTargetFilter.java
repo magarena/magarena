849890805f2cfc45ca97cc03adbe6770630b31f8
melvinzhang <melvinzhang@gmail.com>
2017-07-19 23:15:47 +0800

    allow other target card, where source is permanent

--- src/magic/model/target/MagicOtherTargetFilter.java
+++ src/magic/model/target/MagicOtherTargetFilter.java
@@ -1,6 +1,7 @@
 package magic.model.target;
 
 import magic.model.MagicPlayer;
+import magic.model.MagicPermanent;
 import magic.model.MagicSource;
 
 // reference can not be used because game is copied.
@@ -19,11 +20,16 @@ public class MagicOtherTargetFilter extends MagicTargetFilterImpl {
         id = invalid.getId();
     }
 
+    private static boolean different(final MagicSource source, final MagicTarget target) {
+        final MagicSource sourceCard = source.isPermanent() ? ((MagicPermanent)source).getCard() : source;
+        return source != target && sourceCard != target;
+    }
+
     @Override
     public boolean accept(final MagicSource source,final MagicPlayer player,final MagicTarget target) {
         return targetFilter.accept(source,player,target) &&
                ((id != 0 && id != target.getId()) ||
-                (id == 0 && source != target));
+                (id == 0 && different(source, target)));
     }
 
     @Override
