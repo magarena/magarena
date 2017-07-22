0a0c2ecab63e001c006116a8fdfa6b7ad624eb28
lodici <lodici123@gmail.com>
2017-07-22 10:13:16 +0100

    move script components into self contained CardScriptPanel.

--- src/magic/ui/screen/card/ContentPanel.java
+++ src/magic/ui/screen/card/ContentPanel.java
@@ -1,13 +1,7 @@
 package magic.ui.screen.card;
 
-import java.io.File;
-import java.nio.file.Path;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import magic.data.CardDefinitions;
 import magic.model.MagicCardDefinition;
-import magic.ui.mwidgets.MTextFileViewer;
-import magic.utility.MagicFileSystem;
 import net.miginfocom.swing.MigLayout;
 
 @SuppressWarnings("serial")
@@ -15,59 +9,34 @@ class ContentPanel extends JPanel {
 
     // UI components
     private final MigLayout migLayout = new MigLayout();
-    private final MTextFileViewer scriptViewer = new MTextFileViewer();
-    private final MTextFileViewer groovyViewer = new MTextFileViewer();
-    private final JSplitPane splitter = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
     private final CardSideBar sidebar;
-
-    private final File scriptFile;
-    private final File groovyFile;
-    private final boolean isGroovy;
+    private final CardScriptPanel scriptPanel;
 
     ContentPanel(final MagicCardDefinition card) {
 
         sidebar = new CardSideBar();
         sidebar.setCard(card);
 
-        final Path scriptsPath = card.isInvalid()
-                ? MagicFileSystem.getDataPath(MagicFileSystem.DataPath.SCRIPTS_MISSING)
-                : MagicFileSystem.getDataPath(MagicFileSystem.DataPath.SCRIPTS);
-
-        scriptFile = scriptsPath.resolve(CardDefinitions.getScriptFilename(card)).toFile();
-        groovyFile = scriptsPath.resolve(CardDefinitions.getGroovyFilename(card)).toFile();
-        isGroovy = groovyFile.exists();
+        scriptPanel = new CardScriptPanel(card);
 
         setLookAndFeel();
         setLayout();
         refreshContent();
-
     }
 
     void refreshContent() {
-        scriptViewer.setTextFile(scriptFile);
-        if (isGroovy) {
-            groovyViewer.setTextFile(groovyFile);
-        }
+        scriptPanel.refreshContent();
     }
 
     private void setLayout() {
         removeAll();
         migLayout.setLayoutConstraints("insets 0, gap 0");
         add(sidebar, "h 100%");
-        add(isGroovy ? splitter : scriptViewer.component(), "w 100%, h 100%");
+        add(scriptPanel, "w 100%, h 100%");
     }
 
     private void setLookAndFeel() {
         setLayout(migLayout);
         setOpaque(false);
-        if (isGroovy) {
-            splitter.setOneTouchExpandable(true);
-            splitter.setLeftComponent(scriptViewer.component());
-            splitter.setRightComponent(groovyViewer.component());
-            splitter.setResizeWeight(0.5);
-            splitter.setDividerSize(14);
-            splitter.setBorder(null);
-            splitter.setOpaque(false);
-        }
     }
 }
