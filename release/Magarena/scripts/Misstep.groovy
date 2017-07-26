f888a0a128869046df01e95895cde8e68fea9c26
melvinzhang <melvinzhang@gmail.com>
2017-07-26 13:41:29 +0800

    simplify groovy code using ChangeStateAction.DoesNotUntapDuringNext

--- release/Magarena/scripts/Misstep.groovy
+++ release/Magarena/scripts/Misstep.groovy
@@ -10,28 +10,12 @@
             );
         }
         @Override
-        public void executeEvent(final MagicGame outerGame, final MagicEvent event) {
-            event.processTargetPlayer(outerGame, {
-                final MagicPlayer outerPlayer ->
-                outerGame.doAction(new AddStaticAction(new MagicStatic(MagicLayer.Game) {
-                    @Override
-                    public void modGame(final MagicPermanent source, final MagicGame game) {
-                        final MagicPlayer p = outerPlayer.map(game);
-                        CREATURE_YOU_CONTROL.filter(p) each {
-                            it.addAbility(MagicAbility.DoesNotUntap);
-                        }
-                    }
-                    @Override
-                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
-                        final MagicPlayer p = outerPlayer.map(game);
-                        if (game.getTurnPlayer() == p && game.isPhase(MagicPhaseType.Upkeep)) {
-                            game.addDelayedAction(new RemoveStaticAction(this));
-                            return false;
-                        } else {
-                            return true;
-                        }
-                    }
-                }))
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer player ->
+                CREATURE_YOU_CONTROL.filter(player) each {
+                    game.doAction(ChangeStateAction.DoesNotUntapDuringNext(it, player));
+                }
             });
         }
     }
