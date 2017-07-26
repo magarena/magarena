1dae04cb1ccf362fc3007b527bc32bcd2687b125
melvinzhang <melvinzhang@gmail.com>
2017-07-26 10:39:53 +0800

    fix indentation of groovy script

--- release/Magarena/scripts/Scourge_of_Nel_Toth.groovy
+++ release/Magarena/scripts/Scourge_of_Nel_Toth.groovy
@@ -10,7 +10,7 @@ def sac = new MagicRegularCostEvent("Sacrifice two creatures");
         public Iterable<? extends MagicEvent> getCostEvent(final MagicCard source) {
             return [
                 new MagicPayManaCostEvent(source, "{B}{B}"),
-				sac.getEvent(source)
+                sac.getEvent(source)
             ];
         }
     }
