9f2b7e8008291855a162ced74378eec238701bee
kelsioboy <unknown>
2017-07-26 09:42:40 +0800

    add Misstep, closes #1182

---
+++ release/Magarena/scripts/Misstep.groovy
@@ -0,0 +1,38 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_PLAYER,
+                this,
+                "Creatures target player\$ controls don't untap during his or her next untap step."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame outerGame, final MagicEvent event) {
+            event.processTargetPlayer(outerGame, {
+                final MagicPlayer outerPlayer ->
+                outerGame.doAction(new AddStaticAction(new MagicStatic(MagicLayer.Game) {
+                    @Override
+                    public void modGame(final MagicPermanent source, final MagicGame game) {
+                        final MagicPlayer p = outerPlayer.map(game);
+                        CREATURE_YOU_CONTROL.filter(p) each {
+                            it.addAbility(MagicAbility.DoesNotUntap);
+                        }
+                    }
+                    @Override
+                    public boolean condition(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+                        final MagicPlayer p = outerPlayer.map(game);
+                        if (game.getTurnPlayer() == p && game.isPhase(MagicPhaseType.Upkeep)) {
+                            game.addDelayedAction(new RemoveStaticAction(this));
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }))
+            });
+        }
+    }
+]
