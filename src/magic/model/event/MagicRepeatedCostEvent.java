5f02bc799078d2b0dd37fd57c8affae819715f48
melvinzhang <melvinzhang@gmail.com>
2017-07-27 17:17:01 +0800

    subclass of MagicEvent with members should implement copy method, fixed MagicRepeatedCostEvent

--- src/magic/model/event/MagicRepeatedCostEvent.java
+++ src/magic/model/event/MagicRepeatedCostEvent.java
@@ -3,6 +3,7 @@ package magic.model.event;
 import magic.model.MagicGame;
 import magic.model.MagicSource;
 import magic.model.MagicPlayer;
+import magic.model.MagicCopyMap;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.condition.MagicCondition;
 import magic.model.condition.MagicConditionFactory;
@@ -10,31 +11,47 @@ import magic.model.condition.MagicConditionFactory;
 public class MagicRepeatedCostEvent extends MagicEvent {
 
     private final MagicCondition cond;
+    private final MagicChainEventFactory factory;
+
+    private static final MagicEventAction action = (final MagicGame game, final MagicEvent event) -> {
+        final MagicChainEventFactory factory = ((MagicRepeatedCostEvent)event).factory;
+        final MagicEvent ev = factory.getEvent(event);
+        ev.executeEvent(game, event.getChosen());
+        for (int i = 0; i < event.getRefInt(); i++) {
+            game.addNextCostEvent(ev);
+        }
+    };
 
     public MagicRepeatedCostEvent(final MagicSource source, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory factory) {
         this(source, source.getController(), targetChoice, amt, factory);
     }
 
-    public MagicRepeatedCostEvent(final MagicSource source, final MagicPlayer player, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory factory) {
+    public MagicRepeatedCostEvent(final MagicSource source, final MagicPlayer player, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory aFactory) {
         super(
             source,
             player,
             targetChoice,
             amt - 1,
-            (final MagicGame game, final MagicEvent event) -> {
-                final MagicEvent ev = factory.getEvent(event);
-                ev.executeEvent(game, event.getChosen());
-                for (int i = 0; i < event.getRefInt(); i++) {
-                    game.addNextCostEvent(ev);
-                }
-            },
-            factory.getEvent(source, targetChoice).getDescription()
+            action,
+            aFactory.getEvent(source, targetChoice).getDescription()
         );
         cond = MagicConditionFactory.YouHaveAtLeast(targetChoice.getFilter(), amt);
+        factory = aFactory;
     }
 
     @Override
     public boolean isSatisfied() {
         return cond.accept(getSource()) && super.isSatisfied();
+    }
+
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicRepeatedCostEvent(
+            copyMap.copy(getSource()),
+            copyMap.copy(getPlayer()),
+            getTargetChoice(),
+            getRefInt(),
+            factory
+        );
     }
 }
