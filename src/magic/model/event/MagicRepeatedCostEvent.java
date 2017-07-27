1a0874fd3e82998b0644c86c9cc7d03e1c763fbc
melvinzhang <melvinzhang@gmail.com>
2017-07-27 10:55:38 +0800

    fix crash by reverting back to dynamically created MagicEventAction

--- src/magic/model/event/MagicRepeatedCostEvent.java
+++ src/magic/model/event/MagicRepeatedCostEvent.java
@@ -10,32 +10,27 @@ import magic.model.condition.MagicConditionFactory;
 public class MagicRepeatedCostEvent extends MagicEvent {
 
     private final MagicCondition cond;
-    private final MagicChainEventFactory factory;
-
-    private static final MagicEventAction action = (final MagicGame game, final MagicEvent event) -> {
-        final MagicChainEventFactory fact = ((MagicRepeatedCostEvent)event).factory;
-        final MagicEvent ev = fact.getEvent(event);
-        ev.executeEvent(game, event.getChosen());
-        for (int i = 0; i < event.getRefInt(); i++) {
-            game.addNextCostEvent(ev);
-        }
-    };
 
     public MagicRepeatedCostEvent(final MagicSource source, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory factory) {
         this(source, source.getController(), targetChoice, amt, factory);
     }
 
-    public MagicRepeatedCostEvent(final MagicSource source, final MagicPlayer player, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory aFactory) {
+    public MagicRepeatedCostEvent(final MagicSource source, final MagicPlayer player, final MagicTargetChoice targetChoice, final int amt, final MagicChainEventFactory factory) {
         super(
             source,
             player,
             targetChoice,
             amt - 1,
-            action,
-            aFactory.getEvent(source, targetChoice).getDescription()
+            (final MagicGame game, final MagicEvent event) -> {
+                final MagicEvent ev = factory.getEvent(event);
+                ev.executeEvent(game, event.getChosen());
+                for (int i = 0; i < event.getRefInt(); i++) {
+                    game.addNextCostEvent(ev);
+                }
+            },
+            factory.getEvent(source, targetChoice).getDescription()
         );
         cond = MagicConditionFactory.YouHaveAtLeast(targetChoice.getFilter(), amt);
-        factory = aFactory;
     }
 
     @Override
