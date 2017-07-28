083589ec46ba1f8db0d953cc05142e9cdb7d5a9d
melvinzhang <melvinzhang@gmail.com>
2017-07-28 15:56:54 +0800

    simplify groovy code with MagicTuple

--- release/Magarena/scripts/Szadek__Lord_of_Secrets.groovy
+++ release/Magarena/scripts/Szadek__Lord_of_Secrets.groovy
@@ -1,32 +1,29 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicTuple tup = event.getRefTuple();
+    game.doAction(new ChangeCountersAction(
+        event.getPermanent(),
+        MagicCounterType.PlusOne,
+        tup.getInt(0)
+    ));
+    game.doAction(new MillLibraryAction(
+        tup.getPlayer(1),
+        tup.getInt(0)
+    ));
+}
+
 [
     new IfDamageWouldBeDealtTrigger(MagicTrigger.REPLACE_DAMAGE) {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
-            int amount = 0;
-            if (permanent == damage.getSource() &&
-                damage.isCombat() &&
-                damage.isTargetPlayer()) {
-                // Replacement effect.
-                amount = damage.replace();
-            }
+            int amount = (damage.isSource(permanent) && damage.isCombat() && damage.isTargetPlayer()) ? damage.replace() : 0;
 
             return amount > 0 ?
                 new MagicEvent(
                     permanent,
-                    damage.getTarget(),
-                    {
-                        final MagicGame G, final MagicEvent E ->
-                        G.doAction(new ChangeCountersAction(
-                            E.getPermanent(),
-                            MagicCounterType.PlusOne,
-                            amount
-                        ));
-                        G.doAction(new MillLibraryAction(
-                            E.getRefPlayer(),
-                            amount
-                        ));
-                    },
-                    "Put ${amount} +1/+1 counters on SN and RN puts that many cards from the top of his or her library into his or her graveyard."
+                    new MagicTuple(amount, damage.getTarget()),
+                    action,
+                    "Put ${amount} +1/+1 counters on SN and ${damage.getTarget()} puts that many cards from the top of his or her library into his or her graveyard."
                 ):
                 MagicEvent.NONE;
         }
