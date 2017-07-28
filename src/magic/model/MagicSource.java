6a73b1a48011fba33888174206e16f083abc495e
melvinzhang <melvinzhang@gmail.com>
2017-07-28 11:03:54 +0800

    remove MagicCopyable from classes which do not need to be copied

--- src/magic/model/MagicSource.java
+++ src/magic/model/MagicSource.java
@@ -3,7 +3,7 @@ package magic.model;
 import java.util.Collection;
 import magic.model.event.MagicSourceActivation;
 
-public interface MagicSource extends MagicCopyable, MagicObject {
+public interface MagicSource extends MagicObject {
     MagicGame getGame();
     Collection<MagicSourceActivation<? extends MagicSource>> getSourceActivations();
     
--- src/magic/model/target/MagicTarget.java
+++ src/magic/model/target/MagicTarget.java
@@ -5,7 +5,7 @@ import magic.model.MagicObject;
 import magic.model.MagicPlayer;
 import magic.model.MagicSource;
 
-public interface MagicTarget extends MagicCopyable, MagicObject {
+public interface MagicTarget extends MagicObject {
     boolean isLegalTarget(final MagicPlayer player, final MagicTargetFilter<? extends MagicTarget> targetFilter);
     boolean isValidTarget(final MagicSource source);
     int     getPreventDamage();
