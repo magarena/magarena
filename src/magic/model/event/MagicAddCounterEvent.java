39fb84fa3abd13c00cb815e8f974d5984a141a20
melvinzhang <melvinzhang@gmail.com>
2017-07-28 17:03:40 +0800

    pass amount and counterType in MagicEvent

--- src/magic/model/event/MagicAddCounterEvent.java
+++ src/magic/model/event/MagicAddCounterEvent.java
@@ -3,6 +3,7 @@ package magic.model.event;
 import magic.model.MagicCounterType;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.MagicTuple;
 import magic.model.action.ChangeCountersAction;
 
 public class MagicAddCounterEvent extends MagicEvent {
@@ -10,15 +11,20 @@ public class MagicAddCounterEvent extends MagicEvent {
     public MagicAddCounterEvent(final MagicPermanent permanent,final MagicCounterType counterType,final int amount) {
         super(
             permanent,
-            (final MagicGame game, final MagicEvent event) ->
-                game.doAction(new ChangeCountersAction(
-                    event.getPermanent(),
-                    counterType,
-                    amount
-                )),
+            new MagicTuple(amount, counterType),
+            EVENT_ACTION,
             genDescription(permanent,counterType,amount)
         );
     }
+
+    private static final MagicEventAction EVENT_ACTION = (final MagicGame game, final MagicEvent event) -> {
+        final MagicTuple tup = event.getRefTuple();
+        game.doAction(new ChangeCountersAction(
+            event.getPermanent(),
+            tup.getCounterType(1),
+            tup.getInt(0)
+        ));
+    };
 
     private static String genDescription(final MagicPermanent permanent,final MagicCounterType counterType,final int amount) {
         final StringBuilder description=new StringBuilder("Put ");
