522292a9c52c52bec2adfc68e36efeb1c9003587
melvinzhang <melvinzhang@gmail.com>
2017-07-28 10:45:23 +0800

    made action static and provide copy method

--- src/magic/model/event/MagicRemoveCounterEvent.java
+++ src/magic/model/event/MagicRemoveCounterEvent.java
@@ -3,6 +3,7 @@ package magic.model.event;
 import magic.model.MagicCounterType;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
+import magic.model.MagicCopyMap;
 import magic.model.action.ChangeCountersAction;
 import magic.model.condition.MagicCondition;
 import magic.model.condition.MagicConditionFactory;
@@ -10,20 +11,27 @@ import magic.model.condition.MagicConditionFactory;
 public class MagicRemoveCounterEvent extends MagicEvent {
 
     private final MagicCondition cond;
+    private final MagicCounterType ctype;
 
     public MagicRemoveCounterEvent(final MagicPermanent permanent,final MagicCounterType counterType,final int amount) {
         super(
             permanent,
-            (final MagicGame game, final MagicEvent event) ->
-                game.doAction(new ChangeCountersAction(
-                    event.getPermanent(),
-                    counterType,
-                    -amount
-                )),
+            amount,
+            EVENT_ACTION,
             genDescription(permanent,counterType,amount)
         );
         cond = MagicConditionFactory.CounterAtLeast(counterType, amount);
+        ctype = counterType;
     }
+
+    private static final MagicEventAction EVENT_ACTION = (final MagicGame game, final MagicEvent event) -> {
+        final MagicCounterType ctype = ((MagicRemoveCounterEvent)event).ctype;
+        game.doAction(new ChangeCountersAction(
+            event.getPermanent(),
+            ctype,
+            -event.getRefInt()
+        ));
+    };
 
     @Override
     public boolean isSatisfied() {
@@ -39,5 +47,13 @@ public class MagicRemoveCounterEvent extends MagicEvent {
         }
         description.append(" from ").append(permanent.getName()).append('.');
         return description.toString();
+    }
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicRemoveCounterEvent(
+            copyMap.copy(getPermanent()),
+            ctype,
+            getRefInt()
+        );
     }
 }
