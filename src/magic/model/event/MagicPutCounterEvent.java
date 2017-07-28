fab647ac3aaae183d7f6aa4ec5b2b75150c4695f
melvinzhang <melvinzhang@gmail.com>
2017-07-28 10:13:35 +0800

    made action static and provide copy method

--- src/magic/model/event/MagicPutCounterEvent.java
+++ src/magic/model/event/MagicPutCounterEvent.java
@@ -4,11 +4,14 @@ import magic.model.MagicCounterType;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicSource;
+import magic.model.MagicCopyMap;
 import magic.model.action.ChangeCountersAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.target.MagicPumpTargetPicker;
 
 public class MagicPutCounterEvent extends MagicEvent {
+
+    final MagicCounterType ctype;
 
     public MagicPutCounterEvent(final MagicSource source, final MagicCounterType type, final int amount) {
         super(
@@ -16,43 +19,51 @@ public class MagicPutCounterEvent extends MagicEvent {
             MagicTargetChoice.POS_TARGET_CREATURE,
             MagicPumpTargetPicker.create(),
             amount,
-            EventActionTarget(type),
+            EventActionTarget,
             "PN puts " + amount + " " + type.getName() + " counters on target creature$."
         );
+        ctype = type;
     }
 
     public MagicPutCounterEvent(final MagicSource source, final int amount) {
         this(source, MagicCounterType.PlusOne, amount);
     }
 
-    private static final MagicEventAction EventActionTarget(final MagicCounterType type) {
-        return (final MagicGame game, final MagicEvent event) -> {
-            event.processTargetPermanent(game, (final MagicPermanent creature) -> {
-                game.doAction(new ChangeCountersAction(
-                    creature,
-                    type,
-                    event.getRefInt()
-                ));
-            });
-        };
-    }
+    private static final MagicEventAction EventActionTarget= (final MagicGame game, final MagicEvent event) -> {
+        event.processTargetPermanent(game, (final MagicPermanent creature) -> {
+            final MagicCounterType type = ((MagicPutCounterEvent)event).ctype;
+            game.doAction(new ChangeCountersAction(
+                creature,
+                type,
+                event.getRefInt()
+            ));
+        });
+    };
 
     public static final MagicEvent Self(final MagicSource source, final MagicCounterType type, final int amount) {
         return new MagicEvent(
             source,
             amount,
-            EventAction(type),
+            EventAction,
             "PN puts " + amount + " " + type.getName() + " counters on SN."
         );
     }
 
-    private static final MagicEventAction EventAction(final MagicCounterType type) {
-        return (final MagicGame game, final MagicEvent event) -> {
-            game.doAction(new ChangeCountersAction(
-                event.getPermanent(),
-                type,
-                event.getRefInt()
-            ));
-        };
+    private static final MagicEventAction EventAction = (final MagicGame game, final MagicEvent event) -> {
+        final MagicCounterType type = ((MagicPutCounterEvent)event).ctype;
+        game.doAction(new ChangeCountersAction(
+            event.getPermanent(),
+            type,
+            event.getRefInt()
+        ));
+    };
+
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicPutCounterEvent(
+            copyMap.copy(getSource()),
+            ctype,
+            getRefInt()
+        );
     }
 }
