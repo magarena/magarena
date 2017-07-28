fa979274ffa60721bb187fd654cbd6829eee9025
melvinzhang <melvinzhang@gmail.com>
2017-07-28 11:41:39 +0800

    made action static and provide copy method

--- src/magic/model/event/MagicAddCounterChosenEvent.java
+++ src/magic/model/event/MagicAddCounterChosenEvent.java
@@ -4,25 +4,40 @@ import magic.model.MagicCounterType;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicSource;
+import magic.model.MagicCopyMap;
 import magic.model.action.ChangeCountersAction;
 import magic.model.choice.MagicTargetChoice;
 
 public class MagicAddCounterChosenEvent extends MagicEvent {
 
+    final MagicCounterType ctype;
+
     public MagicAddCounterChosenEvent(final MagicSource source, final MagicCounterType counterType) {
         super(
             source,
             MagicTargetChoice.A_CREATURE_YOU_CONTROL,
-            (final MagicGame game, final MagicEvent event) -> {
-                event.processTargetPermanent(game, (final MagicPermanent perm) ->
-                    game.doAction(new ChangeCountersAction(
-                        perm,
-                        counterType,
-                        1
-                    ))
-                );
-            },
+            EventAction,
             "Put a " + counterType.getName() + " counter on a creature$ you control."
+        );
+        ctype = counterType;
+    }
+
+    private static final MagicEventAction EventAction = (final MagicGame game, final MagicEvent event) -> {
+        final MagicCounterType ctype = ((MagicAddCounterChosenEvent)event).ctype;
+        event.processTargetPermanent(game, (final MagicPermanent perm) ->
+            game.doAction(new ChangeCountersAction(
+                perm,
+                ctype,
+                1
+            ))
+        );
+    };
+
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicAddCounterChosenEvent(
+            copyMap.copy(getSource()),
+            ctype
         );
     }
 }
