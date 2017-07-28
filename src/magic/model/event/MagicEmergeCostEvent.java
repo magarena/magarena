94038325144d048e9f38cd16a9bf66bdb9ef15e2
melvinzhang <melvinzhang@gmail.com>
2017-07-28 10:01:59 +0800

    made action static and provide copy method

--- src/magic/model/event/MagicEmergeCostEvent.java
+++ src/magic/model/event/MagicEmergeCostEvent.java
@@ -4,6 +4,7 @@ import magic.model.MagicGame;
 import magic.model.MagicManaCost;
 import magic.model.MagicPermanent;
 import magic.model.MagicSource;
+import magic.model.MagicCopyMap;
 import magic.model.action.SacrificeAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.target.MagicSacrificeTargetPicker;
@@ -11,25 +12,34 @@ import magic.model.target.MagicTargetFilterFactory;
 
 public class MagicEmergeCostEvent extends MagicEvent {
 
-    public MagicEmergeCostEvent(final MagicSource source, final MagicManaCost manaCost) {
+    private final MagicManaCost manaCost;
+
+    public MagicEmergeCostEvent(final MagicSource source, final MagicManaCost aManaCost) {
         super(
             source,
-            source.getController(),
             new MagicTargetChoice(
-                MagicTargetFilterFactory.CREATURE_YOU_CONTROL_FOR_EMERGE(manaCost),
+                MagicTargetFilterFactory.CREATURE_YOU_CONTROL_FOR_EMERGE(aManaCost),
                 "a creature you control"
             ),
             MagicSacrificeTargetPicker.create(),
-            EVENT_ACTION(manaCost),
+            EVENT_ACTION,
             "Sacrifice a creature you control$."
         );
+        manaCost = aManaCost;
     }
 
-    public static final MagicEventAction EVENT_ACTION(final MagicManaCost manaCost) {
-        return (final MagicGame game, final MagicEvent event) ->
-            event.processTargetPermanent(game, (final MagicPermanent permanent) -> {
-                game.doAction(new SacrificeAction(permanent));
-                game.addNextCostEvent(new MagicPayManaCostEvent(event.getSource(), manaCost.reduce(permanent.getConvertedCost())));
-            });
+    public static final MagicEventAction EVENT_ACTION = (final MagicGame game, final MagicEvent event) ->
+        event.processTargetPermanent(game, (final MagicPermanent permanent) -> {
+            final MagicManaCost manaCost = ((MagicEmergeCostEvent)event).manaCost;
+            game.doAction(new SacrificeAction(permanent));
+            game.addNextCostEvent(new MagicPayManaCostEvent(event.getSource(), manaCost.reduce(permanent.getConvertedCost())));
+        });
+
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicEmergeCostEvent(
+            copyMap.copy(getSource()),
+            manaCost
+        );
     }
 }
