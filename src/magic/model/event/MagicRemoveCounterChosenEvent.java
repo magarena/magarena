f4d08ae7de0192dfc6f3687b3843846674f7785d
melvinzhang <melvinzhang@gmail.com>
2017-07-28 11:44:11 +0800

    made action static and provide copy method

--- src/magic/model/event/MagicRemoveCounterChosenEvent.java
+++ src/magic/model/event/MagicRemoveCounterChosenEvent.java
@@ -4,12 +4,15 @@ import magic.model.MagicCounterType;
 import magic.model.MagicGame;
 import magic.model.MagicPermanent;
 import magic.model.MagicSource;
+import magic.model.MagicCopyMap;
 import magic.model.action.ChangeCountersAction;
 import magic.model.choice.MagicTargetChoice;
 import magic.model.target.MagicTargetFilterFactory;
 import magic.model.target.MagicTargetFilterFactory.Control;
 
 public class MagicRemoveCounterChosenEvent extends MagicEvent {
+
+    final MagicCounterType ctype;
 
     public MagicRemoveCounterChosenEvent(final MagicSource source, final MagicCounterType counterType) {
         super(
@@ -18,16 +21,28 @@ public class MagicRemoveCounterChosenEvent extends MagicEvent {
                 MagicTargetFilterFactory.creature(counterType, Control.You),
                 "a creature you control with a " + counterType.getName() + " counter on it"
             ),
-            (final MagicGame game, final MagicEvent event) -> {
-                event.processTargetPermanent(game, (final MagicPermanent perm) ->
-                    game.doAction(new ChangeCountersAction(
-                        perm,
-                        counterType,
-                        -1
-                    ))
-                );
-            },
+            EventAction,
             "Remove a " + counterType.getName() + " counter from a creature$ you control."
+        );
+        ctype = counterType;
+    }
+
+    private static final MagicEventAction EventAction = (final MagicGame game, final MagicEvent event) -> {
+        final MagicCounterType ctype = ((MagicRemoveCounterChosenEvent)event).ctype;
+        event.processTargetPermanent(game, (final MagicPermanent perm) ->
+            game.doAction(new ChangeCountersAction(
+                perm,
+                ctype,
+                -1
+            ))
+        );
+    };
+
+    @Override
+    public MagicEvent copy(final MagicCopyMap copyMap) {
+        return new MagicRemoveCounterChosenEvent(
+            copyMap.copy(getSource()),
+            ctype
         );
     }
 }
