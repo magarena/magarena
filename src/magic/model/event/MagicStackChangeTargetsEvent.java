c60c30f6a3e6855cc2516f221ea7918d5b952f5d
melvinzhang <melvinzhang@gmail.com>
2017-08-01 20:07:55 +0800

    fix changed target not triggering when targeted

--- src/magic/model/event/MagicStackChangeTargetsEvent.java
+++ src/magic/model/event/MagicStackChangeTargetsEvent.java
@@ -1,14 +1,15 @@
 package magic.model.event;
 
 import magic.model.MagicGame;
+import magic.model.choice.MagicTargetChoice;
 import magic.model.stack.MagicItemOnStack;
+import magic.model.trigger.MagicTriggerType;
 
 public class MagicStackChangeTargetsEvent extends MagicEvent {
-
     public MagicStackChangeTargetsEvent(final MagicItemOnStack itemOnStack) {
         super(
-            itemOnStack.getSource(),
-            itemOnStack.getController(),
+            itemOnStack.getEvent().getSource(),
+            itemOnStack.getEvent().getPlayer(),
             itemOnStack.getEvent().getTargetChoice(),
             itemOnStack.getEvent().getTargetPicker(),
             itemOnStack,
@@ -20,5 +21,11 @@ public class MagicStackChangeTargetsEvent extends MagicEvent {
     private static final MagicEventAction EVENT_ACTION = (final MagicGame game, final MagicEvent event) -> {
         final MagicItemOnStack itemOnStack = event.getRefItemOnStack();
         event.setTargetChoice(itemOnStack.getChoiceResults());
+
+        // trigger WhenTargeted
+        final MagicTargetChoice tchoice = event.getTargetChoice();
+        if (tchoice != null && tchoice.isTargeted()) {
+            game.executeTrigger(MagicTriggerType.WhenTargeted,itemOnStack);
+        }
     };
 }
