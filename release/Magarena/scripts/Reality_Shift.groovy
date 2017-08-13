ada666bd8e1780a225e9db7166a488ee637b2244
ShawnieBoy <ShawnieBoyUK@gmail.com>
2017-08-13 20:49:56 +0100

    add Reality Shift

---
+++ release/Magarena/scripts/Reality_Shift.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_CREATURE,
+				MagicExileTargetPicker.create(),
+                this,
+                "Exile target creature.\$ power. "+
+				"Its controller manifests the top card of his or her library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+				game.doAction(new RemoveFromPlayAction(it, MagicLocationType.Exile));
+				game.doAction(new ManifestAction(it.getController(), 1));
+            });
+        }
+    }
+]
