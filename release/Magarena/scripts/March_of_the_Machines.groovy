a753da355b3e05deddc4c1e63ad07dfe8e5d45da
melvinzhang <melvinzhang@gmail.com>
2017-08-16 23:21:45 +0800

    fix March of the Machines to use the applied check, closes #202
    apply the PT change when the type change was previously applied to this
    permanent

--- release/Magarena/scripts/March_of_the_Machines.groovy
+++ release/Magarena/scripts/March_of_the_Machines.groovy
@@ -1,17 +1,20 @@
-[
-    new MagicStatic(MagicLayer.Type, NONCREATURE_ARTIFACT) {
-        @Override
-        public int getTypeFlags(final MagicPermanent permanent, final int flags) {
-            return flags|MagicType.Artifact.getMask()|MagicType.Creature.getMask();
-        }
-    },
-    new MagicStatic(MagicLayer.SetPT, ARTIFACT_CREATURE) {
-        @Override
-        public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
-            if (MagicCondition.WAS_NONCREATURE_ARTIFACT.accept(permanent)) {
-                final int cmc = permanent.getConvertedCost();
-                pt.set(cmc,cmc);
-            }
-        }
+def T = new MagicStatic(MagicLayer.Type, NONCREATURE_ARTIFACT) {
+    @Override
+    public int getTypeFlags(final MagicPermanent permanent, final int flags) {
+        return flags|MagicType.Artifact.getMask()|MagicType.Creature.getMask();
     }
-]
+}
+
+def PT = new MagicStatic(MagicLayer.SetPT, ANY) {
+    @Override
+    public boolean accept(final MagicGame game,final MagicPermanent source,final MagicPermanent target) {
+        return target.applied(source, T);
+    }
+    @Override
+    public void modPowerToughness(final MagicPermanent source,final MagicPermanent permanent,final MagicPowerToughness pt) {
+        final int cmc = permanent.getConvertedCost();
+        pt.set(cmc,cmc);
+    }
+}
+
+[T, PT]
