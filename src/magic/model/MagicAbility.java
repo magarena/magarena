6117331d9858032ceb09eca6a65d9bc60bd6edbf
melvinzhang <melvinzhang@gmail.com>
2017-08-25 14:34:18 +0800

    extend regex for LeavesReturnExile

--- src/magic/model/MagicAbility.java
+++ src/magic/model/MagicAbility.java
@@ -720,7 +720,7 @@ public enum MagicAbility {
             ));
         }
     },
-    LeavesReturnExile("When SN leaves the battlefield, (each player returns|return) (the exiled card(s)? |all cards exiled with it )?to the battlefield (under (its|their) owner('s|s') control|all cards he or she owns exiled with SN).", 0) {
+    LeavesReturnExile("When SN leaves the battlefield, (each player returns|return) (the exiled card(s)? |all cards exiled with it |all cards exiled with SN )?to the battlefield (under (its|their) owner('s|s') control|all cards he or she owns exiled with SN).", 0) {
         @Override
         protected void addAbilityImpl(final MagicAbilityStore card, final Matcher arg) {
             card.add(LeavesReturnExiledTrigger.create());
