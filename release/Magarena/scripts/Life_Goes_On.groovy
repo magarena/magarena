e134902fa6fa94ce299a85bb170dd08cf2f298d5
kelsioboy <unknown>
2017-09-01 20:42:27 +0800

    add Life Goes On, closes #1281

---
+++ release/Magarena/scripts/Life_Goes_On.groovy
@@ -0,0 +1,19 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN gains 4 life. " +
+                "PN gains 8 life instead if a creature died this turn."
+            );  
+            MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = game.getCreatureDiedThisTurn() ? 8 : 4;
+            game.doAction(new ChangeLifeAction(event.getPlayer(), amount));
+        }
+    }
+]
