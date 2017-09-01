ce85459376ba9630f6179ce9d77789faaa652994
kelsioboy <unknown>
2017-09-01 16:59:57 +0800

    add Wine of Blood and Iron, closes #1283

---
+++ release/Magarena/scripts/Wine_of_Blood_and_Iron.groovy
@@ -0,0 +1,35 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Pump"
+    ) {
+
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{4}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                POS_TARGET_CREATURE,
+                MagicPumpTargetPicker.create(),
+                this,
+                "Target creature\$ gets +X/+0 until end of turn, where X is its power. " +
+                "Sacrifice SN at the beginning of the next end step."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new ChangeTurnPTAction(
+                    it,
+                    it.getPower(),
+                    0
+                ));
+                game.doAction(new AddTriggerAction(event.getPermanent(), AtEndOfTurnTrigger.Sacrifice));
+            });
+        }
+    }
+]
