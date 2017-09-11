47d29daf2eb6ea1812b983770249f9f144e9ab8b
melvinzhang <melvinzhang@gmail.com>
2017-09-11 13:46:46 +0800

    add missing period in cascade event description

--- src/magic/model/trigger/CascadeTrigger.java
+++ src/magic/model/trigger/CascadeTrigger.java
@@ -29,7 +29,7 @@ public class CascadeTrigger extends ThisSpellIsCastTrigger {
         return new MagicEvent(
             spell,
             this,
-            "Cascade"
+            "Cascade."
         );
     }
 
