b7ef82258ba201cdefbda1125a44be5ae36084d5
kelsioboy <unknown>
2017-09-28 17:23:26 +0800

    add Cytoplast Root-Kin, closes #1272

---
+++ release/Magarena/scripts/Cytoplast_Root_Kin.groovy
@@ -0,0 +1,34 @@
+def choice = MagicTargetChoice.Positive("another target creature you control with a +1/+1 counter on it")
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Pump),
+        "Move"
+    ) {
+
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [new MagicPayManaCostEvent(source, "{2}")];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                choice,
+                MagicPumpTargetPicker.create(),
+                this,
+                "PN moves a +1/+1 counter from target creature you control\$ onto SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                if (it.hasCounters(MagicCounterType.PlusOne)) {
+                    game.doAction(new ChangeCountersAction(it, MagicCounterType.PlusOne, -1));
+                    game.doAction(new ChangeCountersAction(event.getPermanent(), MagicCounterType.PlusOne, 1));
+                }
+            });
+        }
+    }
+]
