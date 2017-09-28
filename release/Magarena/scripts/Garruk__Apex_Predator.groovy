a550f22f88d963c43c2db6a57376b8e738f6f9a2
kelsioboy <unknown>
2017-09-28 17:32:46 +0800

    add Garruk, Apex Predator, closes #1277

---
+++ release/Magarena/scripts/Garruk__Apex_Predator.groovy
@@ -0,0 +1,21 @@
+[
+    new MagicPlaneswalkerActivation(-3) {
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                NEG_TARGET_CREATURE,
+                MagicDestroyTargetPicker.Destroy,
+                this,
+                "Destroy target creature\$. PN gains life equal to its toughness."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPermanent(game, {
+                game.doAction(new DestroyAction(it));
+                game.doAction(new ChangeLifeAction(event.getPlayer(), it.getToughness()));
+            });
+        }
+    }
+]
