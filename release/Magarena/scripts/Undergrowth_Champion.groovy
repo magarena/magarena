7831fc3aa8f05be42f18397be0b28ed93a75dc89
kelsioboy <unknown>
2017-10-01 12:35:21 +0800

    add Undergrowth Champion, closes #1298

---
+++ release/Magarena/scripts/Undergrowth_Champion.groovy
@@ -0,0 +1,12 @@
+[
+    new PreventDamageTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicDamage damage) {
+            if (damage.getTarget() == permanent && permanent.hasCounters(MagicCounterType.PlusOne)) {                       
+                damage.prevent();
+                game.doAction(new ChangeCountersAction(permanent, MagicCounterType.PlusOne,-1));
+            }                    
+            return MagicEvent.NONE;
+        }
+    }
+]
