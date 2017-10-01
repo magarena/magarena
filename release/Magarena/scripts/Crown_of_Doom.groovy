b342d9c8e30f2773ad440e8786e8d159ce4fba04
kelsioboy <unknown>
2017-10-01 12:52:18 +0800

    add Crown of Doom, closes #1306

---
+++ release/Magarena/scripts/Crown_of_Doom.groovy
@@ -0,0 +1,37 @@
+def filter = new MagicPlayerFilterImpl() {
+    @Override
+    public boolean accept(final MagicSource source, final MagicPlayer player, final MagicPlayer target) {
+        return player != ((MagicPermanent)source).getOwner();
+    }
+};
+
+[
+    new MagicPermanentActivation(
+        [MagicCondition.YOUR_TURN_CONDITION],
+        new MagicActivationHints(MagicTiming.Main),
+        "Control"
+    ) {
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicPayManaCostEvent(source, "{2}")
+            ];
+        }
+
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                new MagicTargetChoice(filter, "target player other than " + source + "'s owner"),
+                this,
+                "Target player other than SN's owner\$ gains control of SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                game.doAction(new GainControlAction(it, event.getPermanent()));
+            })
+        }
+    }
+]
