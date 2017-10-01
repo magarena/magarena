1bca0d4dd4f99614484911696ce230563b8d517c
kelsioboy <unknown>
2017-10-01 09:24:31 +0800

    add Vigil for the Lost, closes #1332

---
+++ release/Magarena/scripts/Vigil_for_the_Lost.groovy
@@ -0,0 +1,26 @@
+[
+    new OtherDiesTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPermanent died) {
+            return died.hasType(MagicType.Creature) && died.isFriend(permanent) ?
+                new MagicEvent(
+                permanent,
+                new MagicMayChoice(
+                    new MagicPayManaCostChoice(MagicManaCost.create("{X}"))
+                ),
+                this,
+                "PN may\$ pay {X}\$. If PN does, he or she gains X life."
+            ):
+			MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new ChangeLifeAction(
+                    event.getPlayer(),
+                    event.getPaidMana().getX()
+                ));
+            }
+        }
+    }
+]
