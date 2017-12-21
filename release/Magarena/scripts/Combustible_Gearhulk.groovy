7685aa9851b29159d43be4c638dc7021a309b211
Ada Joule <ada.fulmina@gmail.com>
2017-12-21 15:06:57 +0700

    Fix errors

--- release/Magarena/scripts/Combustible_Gearhulk.groovy
+++ release/Magarena/scripts/Combustible_Gearhulk.groovy
@@ -7,33 +7,35 @@
                 TARGET_OPPONENT,
                 this,
                 "Choose target player\$."
+            );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            controller = event.getSource().getController();
+            final MagicPlayer controller = event.getSource().getController();
             event.processTargetPlayer(game, {
+                final MagicPlayer targetPlayer ->
                 game.addEvent(new MagicEvent(
                     event.getSource(),
-                    it,
+                    targetPlayer,
                     new MagicMayChoice("Have SN's controller draw three cards?"),
                     {
                         final MagicGame g, final MagicEvent e ->
                         if (e.isYes()) {
                             g.doAction(new DrawAction(controller, 3));
-                        }
-                        else {
-                            millAction = new MillLibraryAction(controller, 3);
+                        } else {
+                            final MagicAction millAction = new MillLibraryAction(controller, 3);
                             g.doAction(millAction);
                             g.doAction(new DealDamageAction(
                                 e.getSource(),
-                                it,
-                                millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i });
-                            );
+                                targetPlayer,
+                                (int)millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i })
+                            ));
                         }
                     },
                     "That player may\$ have PN draw three cards. " +
                     "If the player doesn't, put the top three cards of PN's library to the graveyard, " +
                     "then SN deals damage to that player equal to the total converted mana cost of those cards."
+                ));
             });
         }
     }
