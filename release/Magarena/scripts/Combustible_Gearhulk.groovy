38f53751766297eaacf99a2be72e693ef71124a1
Ada Joule <ada.fulmina@gmail.com>
2017-12-28 03:06:36 +0700

    Move action out for real

--- release/Magarena/scripts/Combustible_Gearhulk.groovy
+++ release/Magarena/scripts/Combustible_Gearhulk.groovy
@@ -1,3 +1,22 @@
+def choiceAction = {
+    final MagicPlayer controller, final MagicPlayer opponent -> 
+    return {
+        final MagicGame game, final MagicEvent event ->
+        if (event.isYes()) {
+            game.doAction(new DrawAction(controller, 3));
+        } else {
+            MagicAction millAction = new MillLibraryAction(controller, 3);
+            game.doAction(millAction);
+            int amount = millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i });
+            game.doAction(new DealDamageAction(
+                event.getSource(),
+                opponent,
+                amount
+            ));
+        }
+    }
+}
+
 [
     new EntersBattlefieldTrigger() {
         @Override
@@ -11,28 +30,12 @@
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer controller = event.getSource().getController();
             event.processTargetPlayer(game, {
-                final MagicPlayer targetPlayer ->
                 game.addEvent(new MagicEvent(
                     event.getSource(),
-                    targetPlayer,
+                    it,
                     new MagicMayChoice("Have Combustible Gearhulk's controller draw three cards?"),
-                    {
-                        final MagicGame g, final MagicEvent e ->
-                        if (e.isYes()) {
-                            game.doAction(new DrawAction(controller, 3));
-                        } else {
-                            final MagicAction millAction = new MillLibraryAction(controller, 3);
-                            game.doAction(millAction);
-                            final int amount = millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i })
-                            game.doAction(new DealDamageAction(
-                                e.getSource(),
-                                targetPlayer,
-                                amount
-                            ));
-                        }
-                    },
+                    choiceAction(event.getSource().getController(), it),
                     "PN may\$ have SN's controller draw three cards. " +
                     "If PN doesn't, put the top three cards of SN's controller's library to the graveyard, " +
                     "then SN deals damage to PN equal to the total converted mana cost of those cards."
