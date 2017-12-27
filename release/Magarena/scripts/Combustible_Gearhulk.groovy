3a06bd3aaa3bfd7ff2bb4acc4dae9e2b93071245
Ada Joule <ada.fulmina@gmail.com>
2017-12-28 00:08:22 +0700

    Move Combustible Gearhulk damage/draw action out

--- release/Magarena/scripts/Combustible_Gearhulk.groovy
+++ release/Magarena/scripts/Combustible_Gearhulk.groovy
@@ -1,44 +1,46 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+        if (event.isYes()) {
+            game.doAction(new DrawAction(controller, 3));
+        }
+        else {
+            final MagicAction millAction = new MillLibraryAction(controller, 3);
+            game.doAction(millAction);
+            final int amount = millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i });
+            g.doAction(new DealDamageAction(
+                        e.getSource(),
+                        targetPlayer,
+                        amount
+                        ));
+        }
+}
 [
     new EntersBattlefieldTrigger() {
         @Override
-        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            return new MagicEvent(
-                permanent,
-                TARGET_OPPONENT,
-                this,
-                "Choose target player\$."
-            );
-        }
+            public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPayedCost payedCost) {
+                return new MagicEvent(
+                        permanent,
+                        TARGET_OPPONENT,
+                        this,
+                        "Choose target player\$."
+                        );
+            }
         @Override
-        public void executeEvent(final MagicGame game, final MagicEvent event) {
-            final MagicPlayer controller = event.getSource().getController();
-            event.processTargetPlayer(game, {
-                final MagicPlayer targetPlayer ->
-                game.addEvent(new MagicEvent(
-                    event.getSource(),
-                    targetPlayer,
-                    new MagicMayChoice("Have Combustible Gearhulk's controller draw three cards?"),
-                    {
-                        final MagicGame g, final MagicEvent e ->
-                        if (e.isYes()) {
-                            g.doAction(new DrawAction(controller, 3));
-                        } else {
-                            final MagicAction millAction = new MillLibraryAction(controller, 3);
-                            g.doAction(millAction);
-                            final int amount = millAction.getMilledCards().collect({ it.getConvertedCost() }).inject(0, { result, i -> result + i })
-                            g.doAction(new DealDamageAction(
-                                e.getSource(),
-                                targetPlayer,
-                                amount
-                            ));
-                        }
-                    },
-                    "That player may\$ have PN draw three cards. " +
-                    "If the player doesn't, put the top three cards of PN's library to the graveyard, " +
-                    "then SN deals damage to that player equal to the total converted mana cost of those cards."
-                ));
-            });
-        }
+            public void executeEvent(final MagicGame game, final MagicEvent event) {
+                final MagicPlayer controller = event.getSource().getController();
+                event.processTargetPlayer(game, {
+                        final MagicPlayer targetPlayer ->
+                        game.addEvent(new MagicEvent(
+                                    event.getSource(),
+                                    targetPlayer,
+                                    new MagicMayChoice("Have Combustible Gearhulk's controller draw three cards?"),
+                                    action,
+                                    "That player may\$ have PN draw three cards. " +
+                                    "If the player doesn't, put the top three cards of PN's library to the graveyard, " +
+                                    "then SN deals damage to that player equal to the total converted mana cost of those cards."
+                                    ));
+                        });
+            }
     }
 ]
 
