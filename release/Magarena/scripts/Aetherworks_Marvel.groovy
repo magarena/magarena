1599a4e264b1d88971e5211a06e8ae683797a0d1
Ada Joule <ada.fulmina@gmail.com>
2017-12-29 01:24:34 +0700

    Add Aetherworks Marvel
    Still need to filter choice to only spells

---
+++ release/Magarena/scripts/Aetherworks_Marvel.groovy
@@ -0,0 +1,41 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetCard(game, {
+        game.doAction(CastCardAction.WithoutManaCost(event.getPlayer(), it, MagicLocationType.OwnersLibrary, MagicLocationType.Graveyard));
+        final MagicCardList copyTopCards = new MagicCardList(event.getRefCardList());
+        copyTopCards.removeCard(it);
+        copyTopCards.shuffle();
+        copyTopCards.each({ game.doAction(new ShiftCardAction(it, MagicLocationType.OwnersLibrary, MagicLocationType.BottomOfOwnersLibrary)) });
+    });
+}
+
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.CounterFlash),
+        "Cast"
+    ) {
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source),
+                new MagicPayEnergyEvent(source, 6)
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            final MagicPlayer player = source.getController();
+            final MagicCardList topCards = player.getLibrary().getCardsFromTop(6);
+            return new MagicEvent(
+                source,
+                player,
+                new MagicFromCardListChoice(topCards, 1, true), // TODO filter only spell
+                topCards,
+                action,
+                "Look at the top six cards of your library. " +
+                "PN may cast a card\$ from among them without paying its mana cost. " +
+                "Put the rest on the bottom of PN's library in a random order."
+            );
+        }
+    }
+]
+
