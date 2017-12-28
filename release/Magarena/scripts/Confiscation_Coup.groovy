a603eb5a1213c4e27e64ca0563b477898f8fa6cf
Ada Joule <ada.fulmina@gmail.com>
2017-12-28 15:46:40 +0700

    Get rid of parameter in Confiscation Coup action

--- release/Magarena/scripts/Confiscation_Coup.groovy
+++ release/Magarena/scripts/Confiscation_Coup.groovy
@@ -1,29 +1,28 @@
-def payEnergyAction = {
-    final int amount ->
-    return {
-        final MagicGame game, final MagicEvent event ->
-        if (event.isYes()) {
-            game.doAction(new ChangeCountersAction(event.getPlayer(), MagicCounterType.Energy, -amount));
-            game.doAction(new GainControlAction(event.getPlayer(), event.getRefPermanent()));
-        }
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    final MagicPlayer player = event.getPlayer();
+    final MagicPermanent target = event.getRefPermanent();
+    if (event.isYes()) {
+        game.doAction(new ChangeCountersAction(player, MagicCounterType.Energy, -target.getConvertedCost()));
+        game.doAction(new GainControlAction(player, target);
     }
 }
 
 [
     new MagicSpellCardEvent() {
         @Override
-            public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
-                return new MagicEvent(
-                        cardOnStack,
-                        NEG_TARGET_ARTIFACT_OR_CREATURE,
-                        this,
-                        "Choose target artifact or creature\$. " +
-                        "PN gets {E}{E}{E}{E}."
-                        );
-            }
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_ARTIFACT_OR_CREATURE,
+                this,
+                "Choose target artifact or creature\$. " +
+                "PN gets {E}{E}{E}{E}."
+            );
+        }
         @Override
-            public void executeEvent(final MagicGame game, final MagicEvent event) {
-                game.doAction(new ChangeCountersAction(event.getPlayer(), MagicCounterType.Energy, 4));
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeCountersAction(event.getPlayer(), MagicCounterType.Energy, 4));
 
             event.processTargetPermanent(game, {
                 final int amount = it.getConvertedCost();
@@ -33,7 +32,7 @@ def payEnergyAction = {
                         event.getPlayer(),
                         new MagicMayChoice("Pay {E} to gain control of ${it}?"),
                         it,
-                        payEnergyAction(amount),
+                        action,
                         "PN may\$ pay an amount of {E} equal to RN's converted mana cost. " +
                         "If PN does, PN gains control of RN."
                     ));
