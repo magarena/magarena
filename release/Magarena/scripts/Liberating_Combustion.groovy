73b889e5f72640e4b7743c5798ec138f04042e46
Ada Joule <ada.fulmina@gmail.com>
2018-01-09 13:27:47 +0700

    Add Liberating Combustion

---
+++ release/Magarena/scripts/Liberating_Combustion.groovy
@@ -0,0 +1,52 @@
+def CARD_NAMED_CHANDRA_PYROGENIUS_FROM_YOUR_LIBRARY_OR_GRAVEYARD = new MagicCardFilterImpl() {
+    @Override
+    public boolean accept(final MagicSource source,final MagicPlayer player,final MagicCard target) {
+        return target.getName().equalsIgnoreCase("Chandra, Pyrogenius");
+    }
+    public boolean acceptType(final MagicTargetType targetType) {
+        return targetType == MagicTargetType.Library || targetType == MagicTargetType.Graveyard;
+    }
+}
+
+def searchAction = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.isYes()) {
+        game.addEvent(new MagicSearchToLocationEvent(
+            event,
+            new MagicFromCardFilterChoice(
+                CARD_NAMED_CHANDRA_PYROGENIUS_FROM_YOUR_LIBRARY_OR_GRAVEYARD,
+                1,
+                false,
+                "Search for a Chandra, Pyrogenius"
+            ),
+            MagicLocationType.OwnersHand
+        ));
+    }
+}
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_CREATURE,
+                this,
+                "SN deals 6 damage to target creature.\$"
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTarget(game, {
+                game.doAction(new DealDamageAction(event.getSource(), it, 6));
+            });
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                new MagicMayChoice("Search your library?"),
+                searchAction,
+                "PN may\$ search PN's library and/or graveyard for a card named Chandra, Pyrogenius, reveal it, and put it into PN's hand. If PN search his or her library this way, shuffle it.
+            ));
+        }
+    }
+]
+
