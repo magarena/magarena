bfb910c0b843c4d97c3485ec7f81d7228bd7d7ed
Ada Joule <ada.fulmina@gmail.com>
2018-01-12 02:12:09 +0700

    Add groovy for another ability of Midnight Oil

--- release/Magarena/scripts/Midnight_Oil.groovy
+++ release/Magarena/scripts/Midnight_Oil.groovy
@@ -1,4 +1,25 @@
 [
+    // Draw and remove hour counters
+    new AtDrawTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPlayer drawPlayer) {
+            return (permanent.isController(drawPlayer)) ?
+                new MagicEvent(
+                    permanent,
+                    this,
+                    "PN draws an additional card and remove two hour counters from SN."
+                )
+                :
+                MagicEvent.NONE;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new DrawAction(event.getPlayer(), 1));
+            game.doAction(new ChangeCountersAction(event.getSource(), MagicCounterType.Hour, -2));
+        }
+    }
+    ,
+    // Change maximum hand size
     new MagicStatic(MagicLayer.Game) {
         @Override
         public void modGame(final MagicPermanent source, final MagicGame game) {
