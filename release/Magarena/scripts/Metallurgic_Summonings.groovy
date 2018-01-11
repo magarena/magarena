97f5efaf62e5616cffecbdabd6d0d53dd5df0926
Ada Joule <ada.fulmina@gmail.com>
2018-01-11 23:45:25 +0700

    Add Metallurgic Summoning

---
+++ release/Magarena/scripts/Metallurgic_Summonings.groovy
@@ -0,0 +1,29 @@
+//Whenever you cast an instant or sorcery spell, create an X/X colorless Construct artifact creature token, where X is that spell's converted mana cost.;\
+[
+    new OtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicCardOnStack cardOnStack) {
+            final MagicCard card = cardOnStack.getCard();
+            return ((permanent.isController(card.getController())) &&
+                (card.hasType(MagicType.Instant) || card.hasType(MagicType.Sorcery)))
+                ?
+                new MagicEvent(
+                    cardOnStack,
+                    this,
+                    cardOnStack,
+                    "PN creates an X/X colorless Construct artifact creature token, where X is RN's converted mana cost."
+                )
+                :
+                MagicEvent.NONE
+                ;
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int x = event.getRefCardOnStack().getConvertedCost();
+            game.doAction(new PlayTokenAction(
+                event.getPlayer(),
+                CardDefinitions.getToken(x, x, "colorless Construct artifact creature token")
+            ));
+        }
+    }
+]
