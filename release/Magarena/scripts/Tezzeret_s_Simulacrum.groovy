14140698f711e7ecad90549a76ea0f23a8f70ce6
Ada Joule <ada.fulmina@gmail.com>
2018-01-20 22:19:24 +0700

    Add Tezzeret's Simulacrum

---
+++ release/Magarena/scripts/Tezzeret_s_Simulacrum.groovy
@@ -0,0 +1,34 @@
+[
+    new MagicPermanentActivation(
+        new MagicActivationHints(MagicTiming.Flash),
+        "damage"
+    ) {
+        @Override
+        public Iterable<? extends MagicEvent> getCostEvent(final MagicPermanent source) {
+            return [
+                new MagicTapEvent(source);
+            ];
+        }
+        @Override
+        public MagicEvent getPermanentEvent(final MagicPermanent source,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                source,
+                TARGET_OPPONENT,
+                this,
+                "Target opponent\$ loses 1 life. If PN controls a Tezzeret planeswalker, that player loses 3 life instead."
+            );
+        }
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetPlayer(game, {
+                final MagicPlayer target ->
+                final int amount =
+                    (event.getPlayer().getPermanents().any({
+                        it.hasType(MagicType.Planeswalker) &&
+                        it.hasSubType(MagicSubType.Tezzeret)
+                    }) ? 3 : 1;
+                game.doAction(new ChangeLifeAction(target, -amount));
+            });
+        }
+    }
+]
+
