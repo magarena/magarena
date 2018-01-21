6a130229c3c126a7046d74b49381391533421470
melvinzhang <melvinzhang@gmail.com>
2018-01-21 11:26:54 +0800

    fix static type error and missing parenthesis

--- release/Magarena/scripts/Tezzeret__Master_of_Metal.groovy
+++ release/Magarena/scripts/Tezzeret__Master_of_Metal.groovy
@@ -12,13 +12,12 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicCardList library = event.getPlayer().getLibrary();
-            final MagicCardList nonTarget = library.takeWhile({ !it.hasType(MagicType.Artifact) });
-            if (library.any({ it.hasType(MagicType.Artifact) }) {
+            def nonTarget = library.takeWhile({ !it.hasType(MagicType.Artifact) });
+            if (library.any({ it.hasType(MagicType.Artifact) })) {
                 final MagicCard target = library.find({ it.hasType(MagicType.Artifact) });
                 game.doAction(new RevealAction(nonTarget.plus(target)));
                 game.doAction(new ShiftCardAction(target, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
-            }
-            else {
+            } else {
                 game.doAction(new RevealAction(nonTarget));
             }
             Collections.shuffle(nonTarget);
