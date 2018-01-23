c0c6aa004828b27c069a4aefc6c8fc6ca6a9e2c8
Ada Joule <ada.fulmina@gmail.com>
2018-01-23 11:44:33 +0700

    Fix Supernatural Stamina

--- release/Magarena/scripts/Supernatural_Stamina.groovy
+++ release/Magarena/scripts/Supernatural_Stamina.groovy
@@ -1,4 +1,24 @@
-def Return = MagicAbility.getAbility("When SN dies, return it to the battlefield tapped under its owner's control.");
+def trigger = new ThisDiesTrigger() {
+    @Override
+    public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicPermanent died) {
+        return new MagicEvent(
+            permanent,
+            permanent.getOwner(),
+            permanent.getCard(),
+            this,
+            "Return SN to the battlefield tapped under PN's control."
+        );
+    }
+    @Override
+    public void executeEvent(final MagicGame game, final MagicEvent event) {
+        final MagicPermanent source = (MagicPermanent)event.getSource();
+        game.doAction(new ReanimateAction(
+            event.getRefCard(),
+            event.getPlayer(),
+            MagicPlayMod.TAPPED
+        ));
+    }
+}
 
 [
     new MagicSpellCardEvent() {
@@ -9,14 +29,14 @@ def Return = MagicAbility.getAbility("When SN dies, return it to the battlefield
                 POS_TARGET_CREATURE,
                 this,
                 "Until end of turn, target creature\$ gets +2/+0 " +
-                "and gains \"When this creature dies, return it to the battlefield tapped under its owner's control.\""
+                "and gains \"When creature dies, return it to the battlefield tapped under its owner's control.\""
             );
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             event.processTargetPermanent(game, {
                 game.doAction(new ChangeTurnPTAction(it, 2, 0));
-                game.doAction(new GainAbilityAction(it, Return));
+                game.doAction(new AddTurnTriggerAction(it, trigger));
             });
         }
     }
