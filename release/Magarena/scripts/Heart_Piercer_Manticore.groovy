51f89cf325f42b65ddcbce671d400694e0a6430e
Ada Joule <ada.fulmina@gmail.com>
2018-01-25 12:27:10 +0700

    Fix Heart-Piercer Manticore

--- release/Magarena/scripts/Heart_Piercer_Manticore.groovy
+++ release/Magarena/scripts/Heart_Piercer_Manticore.groovy
@@ -1,42 +1,36 @@
-def sacrificeAction = {
+def damageAction = {
     final MagicGame game, final MagicEvent event ->
-    if (event.isYes()) {
-        event.processTargetPermanent(game, {
-            game.doAction(new SacrificeAction(it));
-        });
-    }
+    event.processTargetPermanent(game, {
+        game.doAction(new DealDamageAction(event.getSource(), it, event.getRefInt()));
+    });
 }
 
 [
     new EntersBattlefieldTrigger() {
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
-            final MagicEvent sacrificeEvent = new MagicEvent(
+            return new MagicEvent(
                 permanent,
                 new MagicMayChoice(ANOTHER_CREATURE_YOU_CONTROL),
-                sacrificeAction,
+                this,
                 "PN may\$ sacrifice another creature PN controls\$."
             );
-            game.addEvent(sacrificeEvent);
-            
-            if (sacrificeEvent.isYes()) {
-                final int amount = ((MagicPermanent)(sacrificeEvent.getTarget())).getPower();
-                return new MagicEvent(
-                    permanent,
-                    NEG_TARGET_CREATURE_OR_PLAYER,
-                    amount,
-                    this,
-                    "SN deals ${amount} damage to target creature or player\$."
-                );
-            } else {
-                return MagicEvent.NONE;
-            }
         }
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-            event.processTarget(game, {
-                game.doAction(new DealDamageAction(event.getSource(), it, event.getRefInt()));
-            });
+            if (event.isYes()) {
+                event.processTargetPermanent(game, {
+                    game.doAction(new SacrificeAction(it));
+                    final int amount = it.getPower();
+                    game.doAction(new EnqueueTriggerAction(new MagicEvent(
+                        event.getSource(),
+                        NEG_TARGET_CREATURE_OR_PLAYER,
+                        amount,
+                        damageAction,
+                        "SN deals ${amount} damage to target creature or player\$."
+                    )));
+                });
+            }
         }
     }
 ]
