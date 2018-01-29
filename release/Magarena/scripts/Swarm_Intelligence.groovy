09e57bcb32ef1a79582aa60ac7d5719e9802af82
Ada Joule <ada.fulmina@gmail.com>
2018-01-29 17:23:17 +0700

    Add Swarm Intelligence

---
+++ release/Magarena/scripts/Swarm_Intelligence.groovy
@@ -0,0 +1,25 @@
+[
+    new OtherSpellIsCastTrigger() {
+        @Override
+        public boolean accept(final MagicPermanent permanent, final MagicCardOnStack cardOnStack) {
+            return cardOnStack.isInstantOrSorcerySpell();
+        }
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicCardOnStack cardOnStack) {
+            return new MagicEvent(
+                cardOnStack,
+                new MagicMayChoice(),
+                cardOnStack,
+                this,
+                "PN may copy RN. PN may choose new targets for the copy."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new CopyCardOnStackAction(event.getPlayer(), event.getRefCardOnStack()));
+            }
+        }
+    }
+]
+
