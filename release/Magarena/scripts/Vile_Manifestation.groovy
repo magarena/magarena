533284ef9c972e27bfbc2d14cf25ce7d8c77fbe6
Ada Joule <ada.fulmina@gmail.com>
2018-01-29 21:35:17 +0700

    Cast graveyard count to int

--- release/Magarena/scripts/Vile_Manifestation.groovy
+++ release/Magarena/scripts/Vile_Manifestation.groovy
@@ -2,7 +2,7 @@
     new MagicStatic(MagicLayer.ModPT) {
         @Override
         public void modPowerToughness(final MagicPermanent source, final MagicPermanent permanent, final MagicPowerToughness pt) {
-            int amount = permanent.getController().getGraveyard().count({ it.hasAbility(MagicAbility.Cycling) });
+            final int amount = (int)permanent.getController().getGraveyard().count({ it.hasAbility(MagicAbility.Cycling) });
             pt.add(amount, 0);
         }
     }
