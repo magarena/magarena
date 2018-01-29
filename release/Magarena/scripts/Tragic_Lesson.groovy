2aaa780b7b69c8e4424c019d64a8629cda0a34d0
Ada Joule <ada.fulmina@gmail.com>
2018-01-29 19:14:27 +0700

    Add Tragic Lesson

---
+++ release/Magarena/scripts/Tragic_Lesson.groovy
@@ -0,0 +1,34 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    if (event.isYes()) {
+        game.processTargetPermanent(game, {
+            game.doAction(new RemoveFromPlayAction(it, MagicLocationType.OwnersHand));
+        });
+    } else {
+        game.addEvent(new MagicDiscardEvent(event.getSource(), event.getPlayer(), 1));
+    }
+}
+
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN draws 2 cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new DrawAction(event.getPlayer(), 2));
+            game.addEvent(new MagicEvent(
+                event.getSource(),
+                new MagicMayChoice(A_LAND_YOU_CONTROL),
+                action,
+                "PN discards a card unless PN returns a land PN controls to its owner's hand\$\$."
+            ));
+        }
+    }
+]
+
