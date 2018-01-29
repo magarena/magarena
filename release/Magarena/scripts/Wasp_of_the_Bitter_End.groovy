6624df0c51eea0720e1a73c817e08fb9b9052bef
Ada Joule <ada.fulmina@gmail.com>
2018-01-29 18:29:22 +0700

    Add Wasp of the Bitter End

---
+++ release/Magarena/scripts/Wasp_of_the_Bitter_End.groovy
@@ -0,0 +1,32 @@
+def action = {
+    final MagicGame game, final MagicEvent event ->
+    event.processTargetPermanent(game, {
+        game.doAction(new DestroyAction(it));
+    }
+}
+
+[
+    new OtherSpellIsCastTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicCardOnStack spell) {
+            return new MagicEvent(
+                permanent,
+                new MagicMayChoice(),
+                this,
+                "PN may\$ sacrifice SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            if (event.isYes()) {
+                game.doAction(new EnqueueTriggerAction(new MagicEvent(
+                    event.getSource(),
+                    NEG_TARGET_CREATURE,
+                    action,
+                    "Destroy target creature\$."
+                )));
+            }
+        }
+    }
+]
+
