5f288d68acbdb86002f13c84544ece1c6d5bf55c
Ada Joule <ada.fulmina@gmail.com>
2018-01-30 01:27:48 +0700

    Use upto CardListChoice in Commune with Dinosaurs

--- release/Magarena/scripts/Commune_with_Dinosaurs.groovy
+++ release/Magarena/scripts/Commune_with_Dinosaurs.groovy
@@ -1,16 +1,14 @@
 def action = {
     final MagicGame game, final MagicEvent event ->
-    if (event.isYes()) {
-        event.processChosenCards(game, {
-            final MagicCard chosen ->
-            game.doAction(new ShiftCardAction(chosen, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
-            final MagicCardList copyTopCards = new MagicCardList(event.getRefCardList());
-            copyTopCards.remove(chosen);
-            copyTopCards.each({
-                game.doAction(new ShiftCardAction(it, MagicLocationType.OwnersLibrary, MagicLocationType.BottomOfOwnersLibrary));
-            });
-        });
-    }
+    final MagicCardList copyTopCards = new MagicCardList(event.getRefCardList());
+    event.processChosenCards(game, {
+        final MagicCard chosen ->
+        game.doAction(new ShiftCardAction(chosen, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
+        copyTopCards.remove(chosen);
+    });
+    copyTopCards.each({
+        game.doAction(new ShiftCardAction(it, MagicLocationType.OwnersLibrary, MagicLocationType.BottomOfOwnersLibrary));
+    });
 }
 
 [
@@ -29,10 +27,16 @@ def action = {
             final MagicCardList topCards = player.getLibrary().getCardsFromTop(5);
             game.addEvent(new MagicEvent(
                 event.getSource(),
-                new MagicMayChoice(MagicFromCardListChoice(topCards, 1)),
+                MagicFromCardListChoice(
+                    topCards.find({
+                        it.hasSubType(MagicSubType.Dinosaur) || it.hasType(MagicType.Land)
+                    }),
+                    1,
+                    true
+                ),
                 topCards,
                 action,
-                "PN may\$ reveal a Dinosaur or land card from among them\$ and put it into PN's hand. " +
+                "PN may reveal a Dinosaur or land card from among them\$ and put it into PN's hand. " +
                 "PN puts the rest on the bottom of PN's library in any order."
             ));
         }
