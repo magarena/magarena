806f14c0394d082990bd41567299df539db7f183
Ada Joule <ada.fulmina@gmail.com>
2018-01-31 03:32:41 +0700

    Add Rowdy Crew

---
+++ release/Magarena/scripts/Rowdy_Crew.groovy
@@ -0,0 +1,29 @@
+[
+    new EntersBattlefieldTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN draws three cards, then PN discards two cards at random. " +
+                "If two cards that share a card type are discarded this way, put two +1/+1 counters on SN."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new DrawAction(event.getPlayer(), 3));
+            game.addEvent(MagicDiscardEvent.Random(
+                event.getsource(),
+                2
+            ));
+            MagicCardList sentCards = event.getPlayer().getGraveyard().getCardsFromTop(2);
+            if (MagicType.ALL_CARD_TYPES.any({
+                final MagicType type -> sentCards.every({ it.hasType(type) })
+            })) {
+
+                game.doAction(new ChangeCountersAction(event.getSource(), MagicCounterType.PlusOne, 2));
+            }
+        }
+    }
+]
+
