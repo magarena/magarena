638e0bfb00eeb56e86b3a44cf2cc768e6f501d45
Ada Joule <ada.fulmina@gmail.com>
2018-01-31 04:10:45 +0700

    Add Ruin Raider

---
+++ release/Magarena/scripts/Ruin_Raider.groovy
@@ -0,0 +1,25 @@
+[
+    new AtEndOfTurnTrigger() {
+        @Override
+        public boolean accept(final MagicPermanent permanent, final MagicPlayer eotPlayer) {
+            return permanent.isController(eotPlayer) && eotPlayer.getCreaturesAttackedThisTurn() > 0;
+        }
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer player) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "PN reveals the top card of PN's library and put that card into PN's hand. " +
+                "PN loses life equal to the card's converted mana cost."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final MagicCard topCard = event.getPlayer().getLibrary().getCardAtTop();
+            game.doAction(new RevealAction(topCard));
+            game.doAction(new ShiftCardAction(topCard, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
+            game.doAction(new ChangeLifeAction(event.getPlayer(), -topCard.getConvertedCost()));
+        }
+    }
+]
+
