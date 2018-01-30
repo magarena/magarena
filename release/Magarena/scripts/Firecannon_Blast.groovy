76c464dc566fed0c767626ce364a073e08165f06
Ada Joule <ada.fulmina@gmail.com>
2018-01-30 19:01:36 +0700

    Add Firecannon Blast

---
+++ release/Magarena/scripts/Firecannon_Blast.groovy
@@ -0,0 +1,22 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_CREATURE,
+                this,
+                "SN deals 3 damage to target creature\$. " +
+                "Raid - SN deals 6 damage to that creature instead if you attacked with a creature this turn."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getCreaturesAttackedThisTurn() > 0;
+            event.processTargetPermanent(game, {
+                game.doAction(new DealDamageAction(event.getSource(), it, amount));
+            });
+        }
+    }
+]
+
