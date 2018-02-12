3d8d26126a94693a2168682e3ef16ca9a0631bd2
Ada Joule <ada.fulmina@gmail.com>
2018-02-12 19:13:43 +0700

    Add Verdant Sun's Avatar

---
+++ release/Magarena/scripts/Verdant_Sun_s_Avatar.groovy
@@ -0,0 +1,22 @@
+[
+    new OtherEntersBattlefieldTrigger() {
+        @Override
+        public boolean accept(final MagicPermanent permanent, final MagicPermanent played) {
+            return permanent.isController(played.getController());
+        }
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPermanent played) {
+            return new MagicEvent(
+                permanent,
+                played,
+                this,
+                "PN gains life equal to RN's toughness."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeLifeAction(event.getPlayer(), event.getRefPermanent().getToughness()));
+        }
+    }
+]
+
