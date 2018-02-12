036b22c8c8942a7eda365ce244d3e4af405a0cdc
Ada Joule <ada.fulmina@gmail.com>
2018-02-12 20:02:47 +0700

    Fix Sunbird's Invocation

--- release/Magarena/scripts/Sunbird_s_Invocation.groovy
+++ release/Magarena/scripts/Sunbird_s_Invocation.groovy
@@ -1,12 +1,14 @@
 def action = {
     final MagicGame game, final MagicEvent event ->
+    final MagicCardList topCards = new MagicCardList(event.getRefCardList());
     event.processChosenCards(game, {
         game.doAction(CastCardAction.WithoutManaCost(event.getPlayer(), it, MagicLocationType.OwnersLibrary, MagicLocationType.Graveyard));
-        final MagicCardList topCards = new MagicCardList(event.getRefCardList());
         topCards.removeCard(it);
-        topCards.shuffle();
-        topCards.each({ game.doAction(new ShiftCardAction(it, MagicLocationType.OwnersLibrary, MagicLocationType.BottomOfOwnersLibrary)) });
     });
+    topCards.shuffle();
+    topCards.each {
+        game.doAction(new ShiftCardAction(it, MagicLocationType.OwnersLibrary, MagicLocationType.BottomOfOwnersLibrary))
+    }
 }
 
 [
@@ -31,10 +33,14 @@ def action = {
             final MagicCardList revealedCards = event.getPlayer().getLibrary().getCardsFromTop(cmc);
             game.addEvent(new MagicEvent(
                 event.getSource(),
-                new MagicMayChoice(new MagicFromCardListChoice(revealedCards, 1, false)),
+                new MagicFromCardListChoice(
+                    revealedCards.findAll({ it.getConvertedCost() <= cmc }),
+                    1,
+                    true
+                ),
                 revealedCards,
                 action,
-                "PN may\$ cast a card revealed this way with converted mana cost ${cmc} or less\$ without paying its mana cost. " +
+                "PN may cast a card revealed this way with converted mana cost ${cmc} or less\$ without paying its mana cost. " +
                 "Put the rest on the bottom of PN's library in a random order."
             ));
         }
