ae4f50419762ee7b58060408426ae02065e90ac0
Ada Joule <ada.fulmina@gmail.com>
2018-02-21 13:34:06 +0700

    Add `MagicAmount.getPositiveAmount`

--- src/magic/model/MagicAmount.java
+++ src/magic/model/MagicAmount.java
@@ -10,17 +10,30 @@ public abstract class MagicAmount {
         return getAmount(event.getSource(), event.getPlayer());
     }
 
+    public int getPositiveAmount(final MagicSource source, final MagicPlayer player) {
+        if (this == MagicAmountFactory.NegXCost) {
+            // Return a negative number for NegXCost
+            return Math.min(getAmount(source, player), 0);
+        } else {
+            return Math.max(getAmount(source, player), 0);
+        }
+    }
+
+    public int getPositiveAmount(final MagicEvent event) {
+        return getPositiveAmount(event.getSource(), event.getPlayer());
+    }
+
     public boolean isConstant() {
         return false;
     }
 
     public static int countEachProduct(final MagicAmount amount, final MagicAmount eachCount, final MagicEvent event) {
         if (eachCount != MagicAmountFactory.One && amount == MagicAmountFactory.XCost) {
-            return eachCount.getAmount(event);
+            return eachCount.getPositiveAmount(event);
         } else if (eachCount != MagicAmountFactory.One && amount == MagicAmountFactory.NegXCost) {
-            return -eachCount.getAmount(event);
+            return -eachCount.getPositiveAmount(event);
         } else {
-            return amount.getAmount(event) * eachCount.getAmount(event);
+            return amount.getPositiveAmount(event) * eachCount.getPositiveAmount(event);
         }
     }
 }
