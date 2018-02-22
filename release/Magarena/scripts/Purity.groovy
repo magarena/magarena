272c7e956a8deed6b82ae57abfeecb11de45f2cc
Ada Joule <ada.fulmina@gmail.com>
2018-02-22 23:53:12 +0700

    Edit code of Purity

--- release/Magarena/scripts/Purity.groovy
+++ release/Magarena/scripts/Purity.groovy
@@ -1,12 +1,24 @@
 [
     new IfDamageWouldBeDealtTrigger(MagicTrigger.REPLACE_DAMAGE) {
         @Override
+        public boolean accept(final MagicPermanent permanent, final MagicDamage damage) {
+            return super.accept(permanent, damage) &&
+                permanent.isController(damage.getTarget()) &&
+                !damage.isCombat();
+        }
+        @Override
         public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
-            final int amount = (permanent.isController(damage.getTarget()) && !damage.isCombat()) ? damage.prevent() : 0;
-            if (amount > 0) {
-                game.doAction(new ChangeLifeAction(permanent.getController(), amount));
-            }
-            return MagicEvent.NONE;
+            final int amount = damage.prevent();
+            return new MagicEvent(
+                permanent,
+                amount,
+                this,
+                "Prevent RN damage. PN gains RN life."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.doAction(new ChangeLifeAction(event.getPlayer(), event.getRefInt()));
         }
     }
 ]
