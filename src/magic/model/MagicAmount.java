146d147d5ed603a3a88ed8dc64fb9595f7b88ed7
Ada Joule <ada.fulmina@gmail.com>
2018-02-22 22:56:55 +0700

    Remove getPositiveAmount special case

--- src/magic/model/MagicAmount.java
+++ src/magic/model/MagicAmount.java
@@ -11,21 +11,11 @@ public abstract class MagicAmount {
     }
 
     public int getPositiveAmount(final MagicSource source, final MagicPlayer player) {
-        if (this == MagicAmountFactory.NegXCost) {
-            // Return a negative number for NegXCost
-            return Math.min(getAmount(source, player), 0);
-        } else {
-            return Math.max(getAmount(source, player), 0);
-        }
+        return Math.max(getAmount(source, player), 0);
     }
 
     public int getPositiveAmount(final MagicEvent event) {
-        if (this == MagicAmountFactory.NegXCost) {
-            // Return a negative number for NegXCost
-            return Math.min(getAmount(event), 0);
-        } else {
-            return Math.max(getAmount(event), 0);
-        }
+        return Math.max(getAmount(event), 0);
     }
 
     public boolean isConstant() {
