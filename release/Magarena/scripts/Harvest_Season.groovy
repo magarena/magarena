1631991826f3d7e71d5bcc38dc8771ee7f4965d4
Ada Joule <ada.fulmina@gmail.com>
2018-03-01 02:36:01 +0700

    Add Harvest Season

---
+++ release/Magarena/scripts/Harvest_Season.groovy
@@ -0,0 +1,30 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                this,
+                "PN searches PN's library for up to X basic land cards, where X is the number of tapped creatures PN controls, " +
+                "and put those cards onto the battlefield tapped. Then shuffle PN's library."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getPermanents().count({
+                it.hasType(MagicType.Creature) && it.hasState(MagicPermanentState.Tapped)
+            });
+            game.addEvent(new MagicSearchOntoBattlefieldEvent(
+                event,
+                new MagicFromCardFilterChoice(
+                    BASIC_LAND_CARD_FROM_LIBRARY,
+                    amount,
+                    true,
+                    "up to ${amount} basic land cards from your library"
+                ),
+                MagicPlayMod.TAPPED
+            ));
+        }
+    }
+]
+
