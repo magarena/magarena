02535cf5e105520d56adbc8f822872d6e401d89b
Ada Joule <ada.fulmina@gmail.com>
2018-02-28 22:20:15 +0700

    Add Fateful Showdown

---
+++ release/Magarena/scripts/Fateful_Showdown.groovy
@@ -0,0 +1,24 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack,final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                NEG_TARGET_CREATURE_OR_PLAYER,
+                this,
+                "SN deals damage to target creature or player\$ equal to the number of cards in PN's hand. " +
+                "PN discards all the cards in PN's hand, then draw that many cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final int amount = event.getPlayer().getHandSize();
+            event.processTarget(game, {
+                game.doAction(new DealDamageAction(event.getSource(), it, amount));
+            });
+            game.addEvent(new MagicDiscardHandEvent(event.getSource()));
+            game.addEvent(new MagicDrawEvent(event.getSource(), event.getPlayer(), amount));
+        }
+    }
+]
+
