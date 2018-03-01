bd763817b093ded0fabac3df90ee32b7e0ef5ce4
melvinzhang <melvinzhang@gmail.com>
2018-03-01 20:26:23 +0800

    add release note for 1.92

--- src/magic/utility/MagicSystem.java
+++ src/magic/utility/MagicSystem.java
@@ -25,7 +25,7 @@ import magic.utility.MagicFileSystem.DataPath;
 final public class MagicSystem {
     private MagicSystem() {}
 
-    public static final String VERSION = "1.91";
+    public static final String VERSION = "1.92";
     static {
         System.setProperty("http.agent", "Magarena " + VERSION);
     }
