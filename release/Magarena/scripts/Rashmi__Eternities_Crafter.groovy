e40a5dfe8fa559910a0f1b78e32ddb2a97770c96
Ada Joule <ada.fulmina@gmail.com>
2018-03-07 23:30:09 +0700

    Fix getGame() called for MagicPlayer.NONE in Rashmi, Eternities Crafter

--- release/Magarena/scripts/Rashmi__Eternities_Crafter.groovy
+++ release/Magarena/scripts/Rashmi__Eternities_Crafter.groovy
@@ -24,17 +24,19 @@ def action = {
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicCard topCard = event.getPlayer().getLibrary().getCardAtTop();
-            game.doAction(new RevealAction(topCard));
-            if (!topCard.hasType(MagicType.Land) && topCard.getConvertedCost() < event.getRefCardOnStack().getConvertedCost()) {
-                game.addEvent(new MagicEvent(
+            if (topCard != MagicCard.NONE) {
+                game.doAction(new RevealAction(topCard));
+                if (!topCard.hasType(MagicType.Land) && topCard.getConvertedCost() < event.getRefCardOnStack().getConvertedCost()) {
+                    game.addEvent(new MagicEvent(
                     event.getSource(),
                     new MagicMayChoice("Cast the card without paying its mana cost?"),
                     topCard,
                     action,
                     "If it's a nonland card with converted mana cost less than that spell's, PN may\$ cast it without paying its mana cost. If PN doesn't cast the revealed card, put it into PN's hand."
-                ));
-            } else {
-                game.doAction(new ShiftCardAction(topCard, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
+                    ));
+                } else {
+                    game.doAction(new ShiftCardAction(topCard, MagicLocationType.OwnersLibrary, MagicLocationType.OwnersHand));
+                }
             }
         }
     }
