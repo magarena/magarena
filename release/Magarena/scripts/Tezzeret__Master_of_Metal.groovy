cbfa5773656467f783749097ff5a5c70fb232bf4
Ada Joule <ada.fulmina@gmail.com>
2018-03-16 15:43:57 +0700

    Fix predicate

--- release/Magarena/scripts/Tezzeret__Master_of_Metal.groovy
+++ release/Magarena/scripts/Tezzeret__Master_of_Metal.groovy
@@ -12,7 +12,7 @@
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
             final MagicCardList library = event.getPlayer().getLibrary();
-            def predicate = { it.hasType(MagicType.Artifact) };
+            def predicate = { final MagicCard card -> card.hasType(MagicType.Artifact) };
             final MagicCardList nonTarget = (MagicCardList)library.takeWhile({ !predicate(it) });
             if (library.any(predicate)) {
                 final MagicCard target = library.find(predicate);
