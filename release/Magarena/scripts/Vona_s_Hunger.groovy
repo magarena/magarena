3c282f05e91fa575972ea1d4eb4ef547151d1959
Ada Joule <ada.fulmina@gmail.com>
2018-03-20 02:12:25 +0700

    Convert the half number of permanents in Vona's Hunger to integer

--- release/Magarena/scripts/Vona_s_Hunger.groovy
+++ release/Magarena/scripts/Vona_s_Hunger.groovy
@@ -2,7 +2,7 @@ def action = {
     final MagicGame game, final MagicEvent event ->
     final MagicPlayer opponent = event.getPlayer();
     final int amount = event.getSource().getController().hasState(MagicPlayerState.CitysBlessing) ?
-        (opponent.getNrOfPermanents(MagicType.Creature) + 1).intdiv(2) :
+        (opponent.getNrOfPermanents(MagicType.Creature) + 1).intdiv(2).toInteger() :
         1;
     amount.times {
         game.addEvent(new MagicSacrificePermanentEvent(
