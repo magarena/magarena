41854c31bb60ff99ecd0d1f3f51eaa915aac3bf5
Ada Joule <ada.fulmina@gmail.com>
2018-04-09 18:14:38 +0700

    Add player data to MagicCounterChangeTriggerData

--- src/magic/model/trigger/MagicCounterChangeTriggerData.java
+++ src/magic/model/trigger/MagicCounterChangeTriggerData.java
@@ -5,11 +5,13 @@ import magic.model.MagicObject;
 
 public class MagicCounterChangeTriggerData {
 
+    public final MagicPlayer player;
     public final MagicObject obj;
     public final MagicCounterType counterType;
     public final int amount;
 
-    public MagicCounterChangeTriggerData(final MagicObject aObj, final MagicCounterType aCounterType, final int aAmount) {
+    public MagicCounterChangeTriggerData(final MagicPlayer aPlayer, final MagicObject aObj, final MagicCounterType aCounterType, final int aAmount) {
+        this.player = aPlayer;
         this.obj = aObj;
         this.counterType = aCounterType;
         this.amount = aAmount;
