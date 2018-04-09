0bba526ba4bf031e7d7e68e0bdc8e05f2dc42ab3
Ada Joule <ada.fulmina@gmail.com>
2018-04-09 14:09:01 +0700

    Simplify condition in Winding Constrictor

--- release/Magarena/scripts/Winding_Constrictor.groovy
+++ release/Magarena/scripts/Winding_Constrictor.groovy
@@ -3,15 +3,9 @@
         @Override
         public boolean accept(final MagicPermanent permanent, final ChangeCountersAction action) {
             final MagicObject obj = action.getObj();
-            if (action.getAmount() > 0 && permanent.isFriend(obj)) {
-                if (obj.isPermanent()) {
-                    final MagicPermanent target = (MagicPermanent)obj;
-                    return (target.hasType(MagicType.Creature) || target.hasType(MagicType.Artifact));
-                } else if (obj.isPlayer()) {
-                    return true;
-                }
-            }
-            return false;
+            return action.getAmount() > 0 &&
+                (obj.isPlayer() || obj.hasType(MagicType.Creature) || obj.hasType(MagicType.Artifact)) &&
+                permanent.isFriend(obj);
         }
         @Override
         public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final ChangeCountersAction action) {
