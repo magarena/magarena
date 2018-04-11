df7e5932b5769a4fb8a5c1ce14b6eb662d87706a
Ada Joule <ada.fulmina@gmail.com>
2018-04-11 18:04:48 +0700

    Remove redundant amount check in ChangeCountersAction.java (#1550)

--- src/magic/model/action/ChangeCountersAction.java
+++ src/magic/model/action/ChangeCountersAction.java
@@ -71,9 +71,7 @@ public class ChangeCountersAction extends MagicAction {
             setScore(obj.getController(), ((MagicPermanent)obj).getScore() - oldScore);
         }
 
-        if (amount != 0) {
-            game.executeTrigger(MagicTriggerType.WhenOneOrMoreCountersAreChanged, new MagicCounterChangeTriggerData(player, obj, counterType, amount));
-        }
+        game.executeTrigger(MagicTriggerType.WhenOneOrMoreCountersAreChanged, new MagicCounterChangeTriggerData(player, obj, counterType, amount));
         for (int i = 0; i < Math.abs(amount); i++) {
             game.executeTrigger(MagicTriggerType.WhenACounterIsChanged, new MagicCounterChangeTriggerData(player, obj, counterType, amount));
         }
