3f344d2fceb350e8f8bf69337657326caec153eb
ShawnieBoy <shawnieboyuk@gmail.com>
2018-08-10 14:10:20 +0100

    add simple Kicker DOM creatures

---
+++ release/Magarena/scripts/Baloth_Gorger.groovy
@@ -0,0 +1,11 @@
+[
+    new EntersWithCounterTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent,final MagicPayedCost payedCost) {
+            if (payedCost.isKicked()) {
+                game.doAction(new ChangeCountersAction(permanent.getController(),permanent,MagicCounterType.PlusOne,3));
+            }
+            return MagicEvent.NONE;
+        }
+    }
+]
