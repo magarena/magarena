a3ec48fb1d2d773a8c5e6f3f337a4bf632dedcaf
Bilbo <tux@centrum.cz>
2018-10-21 22:16:15 +0200

    Generalize milling actions to fixed amount milling and "mill until certain number of cartain cards are milled".

--- src/magic/model/action/MillLibraryAction.java
+++ src/magic/model/action/MillLibraryAction.java
@@ -1,28 +1,30 @@
 package magic.model.action;
 
-import magic.ai.ArtificialScoringSystem;
 import magic.model.MagicCard;
 import magic.model.MagicCardList;
 import magic.model.MagicGame;
 import magic.model.MagicLocationType;
 import magic.model.MagicPlayer;
 
-import java.util.List;
-import magic.model.MagicMessage;
+/**
+ * Action that removes fixed amount of cards from players library and moves them to that player's graveyard.
+ */
+public class MillLibraryAction extends AbstractMillAction {
 
-public class MillLibraryAction extends MagicAction {
-
-    private final MagicPlayer player;
     private final int amount;
-    private final MagicCardList milledCards = new MagicCardList();
 
+    /**
+     * Mill target player's library (move cards to that player's graveyard).
+     *
+     * @param aPlayer target player
+     * @param aAmount number of cards to move
+     */
     public MillLibraryAction(final MagicPlayer aPlayer,final int aAmount) {
-        player = aPlayer;
+        super(aPlayer);
         amount = aAmount;
     }
 
-    @Override
-    public void doAction(final MagicGame game) {
+    protected void setCardsToMill(MagicGame game) {
         final MagicCardList topN = player.getLibrary().getCardsFromTop(amount);
         for (final MagicCard card : topN) {
             milledCards.add(card);
@@ -32,25 +34,5 @@ public class MillLibraryAction extends MagicAction {
                 MagicLocationType.Graveyard
             ));
         }
-        final int count = topN.size();
-        if (count > 0) {
-            setScore(player,ArtificialScoringSystem.getMillScore(count));
-            game.logAppendMessage(
-                player,
-                String.format(
-                    "%s puts the top %d cards of his or her library into his or her graveyard. (%s)",
-                    player,
-                    count,
-                    count > 5 ? "..." : MagicMessage.getTokenizedCardNames(topN)
-                )
-            );
-        }
     }
-
-    public List<MagicCard> getMilledCards() {
-        return milledCards;
-    }
-
-    @Override
-    public void undoAction(final MagicGame game) {}
 }
---
+++ src/magic/model/action/MillLibraryUntilAction.java
@@ -0,0 +1,55 @@
+package magic.model.action;
+
+import magic.model.MagicCard;
+import magic.model.MagicCardList;
+import magic.model.MagicGame;
+import magic.model.MagicLocationType;
+import magic.model.MagicPlayer;
+import magic.model.MagicType;
+
+/**
+ * Action that removes cards from players library and moves them to that player's graveyard,
+ * until certain card type is seen given number of times (or whole library is milled).
+ */
+public class MillLibraryUntilAction extends AbstractMillAction {
+    private final MagicType cardType;
+    private final int cards;
+
+    /**
+     * Mill target player's library (move cards to that player's graveyard) until certain amount of certain cards are
+     * moved.
+     *
+     * @param aPlayer   target player
+     * @param aCardType type of card to check
+     * @param aCards    number of those cards to encounter in order for milling to stop
+     */
+    public MillLibraryUntilAction(final MagicPlayer aPlayer, final MagicType aCardType, int aCards) {
+        super(aPlayer);
+        cardType = aCardType;
+        cards = aCards;
+    }
+
+    @Override
+    protected String getMillDescription(int finalCount) {
+        return String.format("top %d cards - until %d %s(s) are seen -", finalCount, cards, cardType.getDisplayName());
+    }
+
+    protected void setCardsToMill(MagicGame game) {
+        final MagicCardList all = player.getLibrary().getCardsFromTop(Integer.MAX_VALUE);
+        int seenTargets = 0;
+        for (final MagicCard card : all) {
+            milledCards.add(card);
+            game.doAction(new ShiftCardAction(
+                    card,
+                    MagicLocationType.OwnersLibrary,
+                    MagicLocationType.Graveyard
+            ));
+            if (card.hasType(cardType)) {
+                seenTargets++;
+            }
+            if (seenTargets >= cards) {
+                break;
+            }
+        }
+    }
+}
