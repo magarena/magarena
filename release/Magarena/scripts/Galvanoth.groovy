2cfced002e2e46de8d0a11e735449f1bb30887b6
Bilbo <tux@centrum.cz>
2018-10-21 20:50:47 +0200

    Fix indentation in Galvanoth

--- release/Magarena/scripts/Galvanoth.groovy
+++ release/Magarena/scripts/Galvanoth.groovy
@@ -19,19 +19,19 @@ def action = {
 
         @Override
         public void executeEvent(final MagicGame game, final MagicEvent event) {
-                if (event.isYes()) {
-            final MagicPlayer player = event.getPlayer();
-            final MagicCardList cards = player.getLibrary().getCardsFromTop(1);
-            for (final MagicCard card : cards) {
-                game.doAction(new LookAction(card, player, "top card of your library"));
-                if (card.hasType(MagicType.Instant) || card.hasType(MagicType.Sorcery)) {
-                game.addEvent(new MagicEvent(
-                    event.getSource(),
-                    new MagicMayChoice("Cast the card without paying its mana cost?"),
-                    card,
-                    action,
-                    "If it's an instant or sorcery card, PN may\$ cast it without paying its mana cost."
-                       ));
+            if (event.isYes()) {
+                final MagicPlayer player = event.getPlayer();
+                final MagicCardList cards = player.getLibrary().getCardsFromTop(1);
+                for (final MagicCard card : cards) {
+                    game.doAction(new LookAction(card, player, "top card of your library"));
+                    if (card.hasType(MagicType.Instant) || card.hasType(MagicType.Sorcery)) {
+                        game.addEvent(new MagicEvent(
+                            event.getSource(),
+                            new MagicMayChoice("Cast the card without paying its mana cost?"),
+                            card,
+                            action,
+                            "If it's an instant or sorcery card, PN may\$ cast it without paying its mana cost."
+                        ));
                     }
                 }
             }
