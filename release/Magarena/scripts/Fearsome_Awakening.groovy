cbd6c0b298fd0bf1226d17e5463f7e8ba971e8ad
Bilbo <tux@centrum.cz>
2018-10-22 21:38:28 +0200

    Add new cards that are variations to existing cards:
    Abnormal Endurance
    Daybreak Chaplain
    Demonic Vigor
    Fearsome Awakening
    Fungal Infection
    Graveyard Marshal
    Invoke the Divine
    Lich's Caress
    Pinpoint Avalanche

---
+++ release/Magarena/scripts/Fearsome_Awakening.groovy
@@ -0,0 +1,28 @@
+[
+    new MagicSpellCardEvent() {
+        @Override
+        public MagicEvent getEvent(final MagicCardOnStack cardOnStack, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                cardOnStack,
+                TARGET_CREATURE_CARD_FROM_GRAVEYARD,
+                MagicGraveyardTargetPicker.PutOntoBattlefield,
+                this,
+                "PN returns target creature card\$ from his or her graveyard to the " +
+                "battlefield. If it's a Dragon, PN puts two +1/+1 counters on it."
+            );
+        }
+
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            event.processTargetCard(game, {
+                game.doAction(new ReturnCardAction(MagicLocationType.Graveyard,it,event.getPlayer(), {
+                    final MagicPermanent perm ->
+                    final MagicGame G = perm.getGame();
+                    if (perm.hasSubType(MagicSubType.Dragon)) {
+                        G.doAction(new ChangeCountersAction(event.getPlayer(),perm,MagicCounterType.PlusOne,2));
+                    }
+                }));
+            });
+        }
+    }
+]
