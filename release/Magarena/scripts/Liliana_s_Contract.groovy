46ab35094dc91fc9239b98cec28cf4b26ceccd95
Ada Joule <ada.fulmina@gmail.com>
2018-11-10 03:59:06 +0700

    Add Liliana's Contract

---
+++ release/Magarena/scripts/Liliana_s_Contract.groovy
@@ -0,0 +1,19 @@
+[
+    new AtUpkeepTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPlayer upkeepPlayer) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "If PN controls four or more Demons with different names, PN wins the game."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            final Collection<MagicPermanent> demons = event.getPlayer().getPermanents().findAll({ it.hasSubType(MagicSubType.Demon) });
+            if (demons.unique(false, { it.getName() }).size() >= 4) {
+                game.doAction(new LoseGameAction(event.getPlayer().getOpponent()));
+            }
+        }
+    }
+]
