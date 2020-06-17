53d5fa8827df8ab8a7a998cc68e9ac59051b5211
Lucien Pereira <lucienpereiramailbox+github@gmail.com>
2020-06-17 21:56:57 +0200

    Correction of Temple Altisaur's script
    The effect shouldn't apply on the card itself, the text says:
    If a source would deal damage to another Dinosaur you control, prevent all but 1 of that damage.

--- release/Magarena/scripts/Temple_Altisaur.groovy
+++ release/Magarena/scripts/Temple_Altisaur.groovy
@@ -2,8 +2,10 @@
     new IfDamageWouldBeDealtTrigger(MagicTrigger.PREVENT_DAMAGE) {
         @Override
         public boolean accept(final MagicPermanent permanent, final MagicDamage damage) {
+            // If a source would deal damage to another Dinosaur you control, prevent all but 1 of that damage.
             return super.accept(permanent, damage) &&
                 damage.getTarget().hasSubType(MagicSubType.Dinosaur) &&
+                !permanent.equals(damage.getTarget()) &&
                 permanent.isFriend(damage.getTarget());
         }
         @Override
